5caa302b1319cfecd3b3b29a0c9cd50d4ba97275
Aram Limpens <a.limpens@amsterdam.nl>
2023-01-27 13:55:11 +0100

    Remove temporary button components

--- packages/components-react/src/index.ts
+++ packages/components-react/src/index.ts
@@ -2,9 +2,6 @@
  * @license EUPL-1.2
  */
 
-export { ExistingWithBem } from './ExistingWithBem'
-export { ExistingWithTokens } from './ExistingWithTokens'
-export { FromScratch } from './FromScratch'
 export { Grid } from './Grid'
 export { GridItem } from './GridItem'
 export { Heading } from './Heading'
