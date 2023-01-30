04ab63d5e04ba8607d05ebd3dc76f87c28067fe1
Aram Limpens <a.limpens@amsterdam.nl>
2023-01-30 15:44:18 +0100

    Add test, fix config

--- packages/components-react/src/Paragraph.tsx
+++ packages/components-react/src/Paragraph.tsx
@@ -3,22 +3,30 @@
  */
 
 import clsx from 'clsx'
-import { HTMLAttributes, PropsWithChildren } from 'react'
+import { ForwardedRef, forwardRef, HTMLAttributes, PropsWithChildren } from 'react'
 
 export interface ParagraphProps extends HTMLAttributes<HTMLParagraphElement> {
   variant?: 'intro' | 'small'
 }
 
-export const Paragraph = ({ children, variant, className, ...otherProps }: PropsWithChildren<ParagraphProps>) => (
-  <p
-    className={clsx(
-      'amsterdam-paragraph',
-      variant === 'intro' && 'amsterdam-paragraph-intro',
-      variant === 'small' && 'amsterdam-paragraph-small',
-      className,
-    )}
-    {...otherProps}
-  >
-    {children}
-  </p>
+export const Paragraph = forwardRef(
+  (
+    { children, variant, className, ...otherProps }: PropsWithChildren<ParagraphProps>,
+    ref: ForwardedRef<HTMLParagraphElement>,
+  ) => (
+    <p
+      ref={ref}
+      className={clsx(
+        'amsterdam-paragraph',
+        variant === 'intro' && 'amsterdam-paragraph-intro',
+        variant === 'small' && 'amsterdam-paragraph-small',
+        className,
+      )}
+      {...otherProps}
+    >
+      {children}
+    </p>
+  ),
 )
+
+Paragraph.displayName = 'Paragraph'
