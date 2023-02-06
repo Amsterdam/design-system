1b3347f3451e8375db2b2b3ab7a52eee1af400ab
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2023-02-06 15:30:37 +0100

    #46 Remove redundant wrapper components

--- packages/components-react/src/List.tsx
+++ packages/components-react/src/List.tsx
@@ -9,30 +9,23 @@ export interface ListProps extends HTMLAttributes<HTMLElement> {
   variant?: 'ordered' | 'unordered'
 }
 
-const OrderedList = ({ children, ...restProps }: PropsWithChildren<HTMLAttributes<HTMLOListElement>>) => (
-  <ol {...restProps}>{children}</ol>
-)
-const UnorderedList = ({ children, ...restProps }: PropsWithChildren<HTMLAttributes<HTMLUListElement>>) => (
-  <ul {...restProps}>{children}</ul>
-)
-
 export const List = ({ variant, children, className, ...restProps }: PropsWithChildren<ListProps>) => {
   switch (variant) {
     case 'ordered':
       return (
-        <OrderedList className={clsx('amsterdam-list', 'amsterdam-list-ordered', className)} {...restProps}>
+        <ol className={clsx('amsterdam-list', 'amsterdam-list-ordered', className)} {...restProps}>
           {children}
-        </OrderedList>
+        </ol>
       )
     case 'unordered':
     default:
       return (
-        <UnorderedList
+        <ul
           className={clsx('amsterdam-list', variant === 'unordered' && 'amsterdam-list-unordered', className)}
           {...restProps}
         >
           {children}
-        </UnorderedList>
+        </ul>
       )
   }
 }
