225b7b2860a57b3901dc8d6995de087fe5280fc0
Aram Limpens <a.limpens@amsterdam.nl>
2023-02-09 17:50:00 +0100

    Remove ordered and unordered list styling from List component

--- packages/components-react/src/List.tsx
+++ packages/components-react/src/List.tsx
@@ -5,27 +5,10 @@
 import clsx from 'clsx'
 import { HTMLAttributes, PropsWithChildren } from 'react'
 
-export interface ListProps extends HTMLAttributes<HTMLElement> {
-  variant?: 'ordered' | 'unordered'
-}
-
-export const List = ({ variant, children, className, ...restProps }: PropsWithChildren<ListProps>) => {
-  switch (variant) {
-    case 'ordered':
-      return (
-        <ol className={clsx('amsterdam-list', 'amsterdam-list-ordered', className)} {...restProps}>
-          {children}
-        </ol>
-      )
-    case 'unordered':
-    default:
-      return (
-        <ul
-          className={clsx('amsterdam-list', variant === 'unordered' && 'amsterdam-list-unordered', className)}
-          {...restProps}
-        >
-          {children}
-        </ul>
-      )
-  }
+export const List = ({ children, className, ...restProps }: PropsWithChildren<HTMLAttributes<HTMLUListElement>>) => {
+  return (
+    <ul className={clsx('amsterdam-list', className)} {...restProps}>
+      {children}
+    </ul>
+  )
 }
