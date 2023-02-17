bc9151bc5f898926ddb37ef4a0cdebf4ba16bd3f
Aram Limpens <a.limpens@amsterdam.nl>
2023-02-17 14:57:30 +0100

    Setup css storybook

---
+++ storybook/storybook-css/config/main.js
@@ -0,0 +1,27 @@
+/* eslint-env node */
+module.exports = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-a11y',
+    '@storybook/preset-scss',
+  ],
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-webpack5',
+    disableTelemetry: true,
+  },
+  features: {
+    postcss: false,
+    buildStoriesJson: true,
+  },
+  webpackFinal: async (config) => ({
+    ...config,
+    performance: {
+      // Disable warning for: "asset size exceeds the recommended limit (244 KiB)"
+      hints: false,
+    },
+  }),
+}
