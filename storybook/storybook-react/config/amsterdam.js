441fb6ff66ce92a56dd4762e36455d32b9f81240
Niels Roozemond <n.roozemond@amsterdam.nl>
2023-04-13 10:08:26 +0200

    Theming the Amsterdam logo

---
+++ storybook/storybook-react/config/amsterdam.js
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming/create'
+import logo from './logo.svg'
+
+export default create({
+  base: 'light',
+  brandTitle: 'Gemeente Amsterdam',
+  brandImage: logo,
+})
