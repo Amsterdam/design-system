ddc75746a79c84a9efef7e6204c3c6a3d21001a7
Niels Roozemond <n.roozemond@amsterdam.nl>
2023-04-14 15:25:20 +0200

    Removed out of scope theme

--- storybook/storybook-react/config/amsterdam.js
+++
@@ -1,8 +0,0 @@
-import { create } from '@storybook/theming/create'
-import logo from './logo.svg'
-
-export default create({
-  base: 'light',
-  brandTitle: 'Gemeente Amsterdam',
-  brandImage: logo,
-})
--- storybook/storybook-react/config/manager.js
+++
@@ -1,6 +0,0 @@
-import { addons } from '@storybook/manager-api'
-import amsterdam from './amsterdam'
-
-addons.setConfig({
-  theme: amsterdam,
-})
