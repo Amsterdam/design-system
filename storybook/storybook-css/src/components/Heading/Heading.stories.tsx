a2bf5e717344492b3b82d45e6fd002c7064033be
Niels Roozemond <n.roozemond@amsterdam.nl>
2023-04-18 21:11:03 +0200

    HTML Source code and possibly a fix

--- storybook/storybook-css/src/components/Heading/Heading.stories.tsx
+++ storybook/storybook-css/src/components/Heading/Heading.stories.tsx
@@ -9,11 +9,6 @@ const meta = {
   id: 'css-heading',
   component: Heading,
   argTypes: {
-    textContent: {
-      table: {
-        type: { summary: 'string' },
-      },
-    },
     level: {
       table: {
         disable: true,
@@ -34,61 +29,116 @@ const meta = {
     styleLevel: 1,
   },
   tags: ['autodocs'],
-  parameters: {
-    tokensPrefix: 'amsterdam-heading',
-    docs: {
-      description: {
-        // TODO import readme.md file
-        component: 'Heading',
-      },
-      canvas: { sourceState: 'open' },
-      source: { type: 'dynamic' },
-    },
-  },
 } as Meta<typeof Heading>
 
 export default meta
 
+/**
+ * Primary Heading used for page titles
+ */
 export const Heading1: StoryObj<typeof Heading> = {
   name: 'Heading 1',
+  parameters: {
+    docs: {
+      source: {
+        language: 'html',
+        code: '<h1 class="amsterdam-heading amsterdam-heading-1">Jouw typograaf biedt mij zulke exquise schreven!</h1>',
+      },
+    },
+  },
 }
 
+/**
+ * Secondary Heading used for section titles
+ */
 export const Heading2: StoryObj<typeof Heading> = {
   name: 'Heading 2',
   args: {
     level: 2,
     styleLevel: 2,
   },
+  parameters: {
+    docs: {
+      source: {
+        language: 'html',
+        code: '<h2 class="amsterdam-heading amsterdam-heading-2">Jouw typograaf biedt mij zulke exquise schreven!</h2>',
+      },
+    },
+  },
 }
 
+/**
+ * Tertiary Heading used for subsection titles
+ */
 export const Heading3: StoryObj<typeof Heading> = {
   name: 'Heading 3',
   args: {
     level: 3,
     styleLevel: 3,
   },
+  parameters: {
+    docs: {
+      source: {
+        language: 'html',
+        code: '<h3 class="amsterdam-heading amsterdam-heading-3">Jouw typograaf biedt mij zulke exquise schreven!</h3>',
+      },
+    },
+  },
 }
 
+/**
+ * Quaternary Heading used for subsubsection titles
+ */
 export const Heading4: StoryObj<typeof Heading> = {
   name: 'Heading 4',
   args: {
     level: 4,
     styleLevel: 4,
   },
+  parameters: {
+    docs: {
+      source: {
+        language: 'html',
+        code: '<h4 class="amsterdam-heading amsterdam-heading-4">Jouw typograaf biedt mij zulke exquise schreven!</h4>',
+      },
+    },
+  },
 }
 
+/**
+ * Quinary Heading used for subsubsubsection titles
+ */
 export const Heading5: StoryObj<typeof Heading> = {
   name: 'Heading 5',
   args: {
     level: 5,
     styleLevel: 5,
   },
+  parameters: {
+    docs: {
+      source: {
+        language: 'html',
+        code: '<h5 class="amsterdam-heading amsterdam-heading-5">Jouw typograaf biedt mij zulke exquise schreven!</h5>',
+      },
+    },
+  },
 }
 
+/**
+ * Senary Heading used for subsubsubsubsection titles
+ */
 export const Heading6: StoryObj<typeof Heading> = {
   name: 'Heading 6',
   args: {
     level: 6,
     styleLevel: 6,
+  },
+  parameters: {
+    docs: {
+      source: {
+        language: 'html',
+        code: '<h6 class="amsterdam-heading amsterdam-heading-6">Jouw typograaf biedt mij zulke exquise schreven!</h6>',
+      },
+    },
   },
 }
