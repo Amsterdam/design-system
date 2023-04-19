5cd838e412b400b161803945be990c6c534c3537
Hee Chan <h.c.vanderhaar@gmail.com>
2023-04-19 17:04:04 +0200

    feat: override button tokens for secondary

--- storybook/storybook-react/src/components/Button/Button.stories.tsx
+++ storybook/storybook-react/src/components/Button/Button.stories.tsx
@@ -12,8 +12,39 @@ export default {
   title: 'Button',
   id: 'button',
   component: Button,
+  args: {
+    children: 'Default',
+  },
 } as ComponentMeta<typeof Button>
 
-const Template: ComponentStory<typeof Button> = () => <Button />
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />
 
 export const ButtonDefault = Template.bind({})
+
+export const ButtonPrimary = Template.bind({})
+ButtonPrimary.args = {
+  children: 'Primary',
+  variant: 'primary',
+}
+
+export const ButtonPrimaryDisabled = Template.bind({})
+
+ButtonPrimaryDisabled.args = {
+  children: 'Primary Disabled',
+  variant: 'primary',
+  disabled: true,
+}
+
+export const ButtonSecondary = Template.bind({})
+ButtonSecondary.args = {
+  children: 'Secondary',
+  variant: 'secondary',
+}
+
+export const ButtonSecondaryDisabled = Template.bind({})
+
+ButtonSecondaryDisabled.args = {
+  children: 'Secondary Disabled',
+  variant: 'secondary',
+  disabled: true,
+}
