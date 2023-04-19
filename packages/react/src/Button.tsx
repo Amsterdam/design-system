fca8b1e33670540398e16d2150f53ef54c2ff53c
Hee Chan <h.c.vanderhaar@gmail.com>
2023-04-19 13:39:35 +0200

    feat: Button setup with tokens

---
+++ packages/react/src/Button.tsx
@@ -0,0 +1,13 @@
+/**
+ * @license EUPL-1.2+
+ * Copyright (c) 2021 Robbert Broersma
+ * Copyright (c) 2023 Gemeente Amsterdam
+ */
+
+import { Button as UtrechtButton } from '@utrecht/component-library-react'
+
+export const Button = () => {
+  return <UtrechtButton>KLIK HIER</UtrechtButton>
+}
+
+Button.displayName = 'Button'
