6a7ca4c91019f9867d01b6cf5dfbf118b77f7aea
Hee Chan <h.c.vanderhaar@gmail.com>
2023-04-19 16:01:59 +0200

    feat: override button tokens for primary

--- packages/react/src/Button.tsx
+++ packages/react/src/Button.tsx
@@ -4,10 +4,40 @@
  * Copyright (c) 2023 Gemeente Amsterdam
  */
 
-import { Button as UtrechtButton } from '@utrecht/component-library-react'
+import { Button as CommunityButton } from '@utrecht/component-library-react'
+import { ButtonHTMLAttributes, ForwardedRef, forwardRef, PropsWithChildren } from 'react'
 
-export const Button = () => {
-  return <UtrechtButton>KLIK HIER</UtrechtButton>
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: 'primary' | 'secondary' | 'tertiary'
 }
+
+type CommunityButtonAppearance = 'primary-action-button' | 'secondary-action-button' | 'subtle-button'
+
+function getAppearance(variant: ButtonProps['variant']): CommunityButtonAppearance {
+  switch (variant) {
+    case 'secondary':
+      return 'secondary-action-button'
+    case 'tertiary':
+      return 'subtle-button'
+    default:
+      return 'primary-action-button'
+  }
+}
+
+export const Button = forwardRef(
+  ({ children, disabled, variant, ...rest }: PropsWithChildren<ButtonProps>, ref: ForwardedRef<HTMLButtonElement>) => {
+    return (
+      <CommunityButton
+        {...rest}
+        appearance={getAppearance(variant)}
+        ref={ref}
+        aria-disabled={disabled ?? false}
+        disabled={disabled}
+      >
+        {children}
+      </CommunityButton>
+    )
+  },
+)
 
 Button.displayName = 'Button'
