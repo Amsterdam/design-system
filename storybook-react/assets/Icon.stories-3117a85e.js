6be4214b9b79ba71994e3fec041cc03bf811456b
alimpens <37216945+alimpens@users.noreply.github.com>
2023-04-28 15:01:42 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@855cfd4d204b56a21ab0194b80299eb219731d43 🚀

--- storybook-react/assets/Icon.stories-3117a85e.js
+++
@@ -1,4 +0,0 @@
-import{j as r}from"./clsx.m-3764cf42.js";import{S as n}from"./index.esm-18239c69.js";import{I as a}from"./icon-f119d5f2.js";import"./index-f1f749bf.js";import"./_commonjsHelpers-042e6b4d.js";const u={title:"Icon",id:"icon",component:a},m=c=>r(a,{...c,children:r(n,{})}),o=m.bind({});var e,s,t;o.parameters={...o.parameters,docs:{...(e=o.parameters)==null?void 0:e.docs,source:{originalSource:`args => <Icon {...args}>
-    <Email />
-  </Icon>`,...(t=(s=o.parameters)==null?void 0:s.docs)==null?void 0:t.source}}};const I=["Default"];export{o as Default,I as __namedExportsOrder,u as default};
-//# sourceMappingURL=Icon.stories-3117a85e.js.map
---
+++ storybook-react/assets/List.stories-a1e7cf11.js
@@ -0,0 +1,19 @@
+import{j as e,c as m,a as s,F as v}from"./clsx.m-3764cf42.js";import{r as h}from"./index-f1f749bf.js";import{H as d}from"./Heading-a406bd65.js";import"./_commonjsHelpers-042e6b4d.js";const t=h.forwardRef(({children:a,className:n,...l},o)=>e("ul",{ref:o,className:m("amsterdam-list",n),...l,children:a}));t.displayName="List";try{t.displayName="List",t.__docgenInfo={description:"",displayName:"List",props:{}}}catch{}const r=h.forwardRef(({children:a,size:n,className:l,...o},u)=>e("p",{ref:u,className:m("amsterdam-paragraph",n&&`amsterdam-paragraph-${n}`,l),...o,children:a}));r.displayName="Paragraph";try{r.displayName="Paragraph",r.__docgenInfo={description:"",displayName:"Paragraph",props:{size:{defaultValue:null,description:"",name:"size",required:!1,type:{name:"enum",value:[{value:'"small"'},{value:'"large"'}]}}}}}catch{}const P={title:"List",id:"list",component:t},i={args:{children:s(v,{children:[s("li",{children:[e(d,{styleLevel:5,children:"Parkeren"}),e(r,{children:"Alles over parkeren en verkeer in de stad."})]}),s("li",{children:[e(d,{styleLevel:5,children:"Parkeertijd bezoek doorgeven"}),e(r,{children:"Bezoekers- of kraskaart-vergunning? Geef hier parkeertijd van uw bezoek door."})]}),s("li",{children:[e(d,{styleLevel:5,children:"Verhuizing doorgeven"}),e(r,{children:"Naar en binnen Amsterdam."})]})]})}};var p,c,g;i.parameters={...i.parameters,docs:{...(p=i.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    children: <>
+        <li>
+          <Heading styleLevel={5}>Parkeren</Heading>
+          <Paragraph>Alles over parkeren en verkeer in de stad.</Paragraph>
+        </li>
+        <li>
+          <Heading styleLevel={5}>Parkeertijd bezoek doorgeven</Heading>
+          <Paragraph>Bezoekers- of kraskaart-vergunning? Geef hier parkeertijd van uw bezoek door.</Paragraph>
+        </li>
+        <li>
+          <Heading styleLevel={5}>Verhuizing doorgeven</Heading>
+          <Paragraph>Naar en binnen Amsterdam.</Paragraph>
+        </li>
+      </>
+  }
+}`,...(g=(c=i.parameters)==null?void 0:c.docs)==null?void 0:g.source}}};const L=["Default"];export{i as Default,L as __namedExportsOrder,P as default};
+//# sourceMappingURL=List.stories-a1e7cf11.js.map
