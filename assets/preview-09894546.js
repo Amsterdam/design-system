17f886cb9be85e86c1e6acc2d00da4f8e9e7ecca
alimpens <37216945+alimpens@users.noreply.github.com>
2023-06-12 08:52:29 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@8a652a813daff8be5051f1f4e71152226308e1e6 🚀

--- assets/preview-09894546.js
+++
@@ -1,2 +0,0 @@
-import{_ as e}from"./iframe-3f2f3bc1.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-EYKKDMVH-f7dd4102.js"),["./DocsRenderer-EYKKDMVH-f7dd4102.js","./iframe-3f2f3bc1.js","./index-8db94870.js","./_commonjsHelpers-042e6b4d.js","./react-18-f58e807b.js","./index-80ae7d84.js","./index-183e39e2.js","./index-d475d2ea.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
-//# sourceMappingURL=preview-09894546.js.map
---
+++ storybook-react/assets/Button.stories-3084224c.js
@@ -0,0 +1,19 @@
+import{j as v,c as S}from"./clsx.m-51e04ab1.js";import{B as b}from"./index.esm-79b0f6e2.js";import{r as h}from"./index-8db94870.js";function B(t){switch(t){case"secondary":return"secondary-action-button";case"tertiary":return"subtle-button";default:return"primary-action-button"}}const n=h.forwardRef(({children:t,disabled:f,variant:o="primary",...g},_)=>v.jsx(b,{...g,appearance:B(o),ref:_,disabled:f,className:S(o==="secondary"&&"amsterdam-button--secondary"),children:t}));n.displayName="Button";try{n.displayName="Button",n.__docgenInfo={description:"",displayName:"Button",props:{variant:{defaultValue:{value:"primary"},description:"",name:"variant",required:!1,type:{name:"enum",value:[{value:'"primary"'},{value:'"secondary"'},{value:'"tertiary"'}]}}}}}catch{}/**
+ * @license EUPL-1.2+
+ * Copyright (c) 2023 Gemeente Amsterdam
+ */const x={title:"Button",component:n,args:{children:"Default",disabled:!1}},r={args:{children:"Primary"}},e={args:{children:"Secondary",variant:"secondary"}},a={args:{children:"Tertiary",variant:"tertiary"}};var s,c,i;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    children: 'Primary'
+  }
+}`,...(i=(c=r.parameters)==null?void 0:c.docs)==null?void 0:i.source}}};var d,u,m;e.parameters={...e.parameters,docs:{...(d=e.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    children: 'Secondary',
+    variant: 'secondary'
+  }
+}`,...(m=(u=e.parameters)==null?void 0:u.docs)==null?void 0:m.source}}};var p,l,y;a.parameters={...a.parameters,docs:{...(p=a.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    children: 'Tertiary',
+    variant: 'tertiary'
+  }
+}`,...(y=(l=a.parameters)==null?void 0:l.docs)==null?void 0:y.source}}};const P=["Primary","Secondary","Tertiary"],E=Object.freeze(Object.defineProperty({__proto__:null,Primary:r,Secondary:e,Tertiary:a,__namedExportsOrder:P,default:x},Symbol.toStringTag,{value:"Module"}));export{E as B,r as P,e as S,a as T};
+//# sourceMappingURL=Button.stories-3084224c.js.map
