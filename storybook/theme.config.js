2f9721ef56012d611ea960691503347d0a5d88ba
Niels Roozemond <n.roozemond@amsterdam.nl>
2023-06-16 15:31:06 +0200

    Hotfix/storybook logo (#346)
    * Moved and renamed logo to storybook folder
    
    * Removed svgo config
    
    * Logo icon
    
    * Config changes, logo CSS-less
    
    * removed radius

--- storybook/theme.config.js
+++ storybook/theme.config.js
@@ -3,6 +3,9 @@ import Logo from '../proprietary/icons/static/Logo.svg'
 const themeConfig = {
   brandTitle: 'Gemeente Amsterdam',
   brandImage: Logo,
+  base: 'light',
+  appBg: '#ffffff',
+  fontBase: '"Amsterdam Sans", system-ui, sans-serif',
 }
 
 export default themeConfig
