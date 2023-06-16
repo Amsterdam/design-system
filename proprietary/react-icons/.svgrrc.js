7a81f3f2ea6364c88dccfcb6166516af59903a86
Aram Limpens <a.limpens@amsterdam.nl>
2023-06-16 17:31:40 +0200

    Remove custom svgr template

--- proprietary/react-icons/.svgrrc.js
+++ proprietary/react-icons/.svgrrc.js
@@ -2,9 +2,7 @@ module.exports = {
   jsxRuntime: 'automatic',
   outDir: 'src',
   svgProps: {
-    className: '{classNameObj}',
     focusable: '{false}',
   },
-  template: require('./utils/component-template'),
   typescript: true,
 }
