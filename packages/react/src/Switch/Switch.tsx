7cc0a03a237c50a8f1fafe559a30337c78bb75e5
hvdhaar <h.c.vanderhaar@gmail.com>
2023-07-06 17:07:21 +0200

    wip: added checked styles

--- packages/react/src/Switch/Switch.tsx
+++ packages/react/src/Switch/Switch.tsx
@@ -1,3 +1,11 @@
 export const Switch = () => {
-  return <div>Switch</div>
+  return (
+    <label className="amsterdam-switch">
+      <input type="checkbox" className="amsterdam-switch-input" onChange={(e: any) => console.log(e.target.checked)} />
+      {/* outer span: the container where the switcher is hosted*/}
+      <span className="amsterdam-switch-toggle">
+        <span className="amsterdam-switch-toggle-inner"></span>
+      </span>
+    </label>
+  )
 }
