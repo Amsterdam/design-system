90b4d16fe4e979c21ab322604e8486103e63fc1e
hvdhaar <h.c.vanderhaar@gmail.com>
2023-07-07 15:07:15 +0200

    wip: adding toggle behaviour

--- packages/react/src/Switch/Switch.tsx
+++ packages/react/src/Switch/Switch.tsx
@@ -1,11 +1,17 @@
-export const Switch = () => {
+import { ForwardedRef, forwardRef, InputHTMLAttributes, PropsWithChildren } from 'react'
+
+interface SwitchProps extends InputHTMLAttributes<HTMLInputElement> {}
+
+export const Switch = forwardRef((props: PropsWithChildren<SwitchProps>, ref: ForwardedRef<HTMLInputElement>) => {
   return (
     <label className="amsterdam-switch">
-      <input type="checkbox" className="amsterdam-switch-input" onChange={(e: any) => console.log(e.target.checked)} />
-      {/* outer span: the container where the switcher is hosted*/}
-      <span className="amsterdam-switch-toggle">
+      <input {...props} ref={ref} type="checkbox" className="amsterdam-switch-input" />
+
+      <span className="amsterdam-switch-toggle" aria-hidden="true">
         <span className="amsterdam-switch-toggle-inner"></span>
       </span>
     </label>
   )
-}
+})
+
+Switch.displayName = 'Switch'
--- storybook/storybook-react/src/components/Switch/Switch.stories.tsx
+++ storybook/storybook-react/src/components/Switch/Switch.stories.tsx
@@ -18,5 +18,8 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
-  args: {},
+  args: {
+    disabled: false,
+    checked: false,
+  },
 }
