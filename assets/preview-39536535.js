829c354fdc17e1c117afc02667ca38c0d3a91bc4
alimpens <37216945+alimpens@users.noreply.github.com>
2023-07-07 12:43:27 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@55d95537fa5664a17518833af0c07cda3fa1fce0 🚀

---
+++ assets/preview-39536535.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-820459bb.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-EYKKDMVH-103b7fca.js"),["./DocsRenderer-EYKKDMVH-103b7fca.js","./iframe-820459bb.js","./index-8db94870.js","./_commonjsHelpers-042e6b4d.js","./react-18-f58e807b.js","./index-80ae7d84.js","./index-11360af8.js","./index-d475d2ea.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-39536535.js.map
