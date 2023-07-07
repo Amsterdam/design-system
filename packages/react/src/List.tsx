0fc6083b0767039a027f2fc61b020f6253874ed6
Niels Roozemond <n.roozemond@amsterdam.nl>
2023-07-07 14:59:00 +0200

    List restored

---
+++ packages/react/src/List.tsx
@@ -0,0 +1,22 @@
+/**
+ * @license EUPL-1.2+
+ * Copyright (c) 2023 Gemeente Amsterdam
+ */
+
+import clsx from 'clsx'
+import { ForwardedRef, forwardRef, HTMLAttributes, PropsWithChildren } from 'react'
+
+export const List = forwardRef(
+  (
+    { children, className, ...restProps }: PropsWithChildren<HTMLAttributes<HTMLUListElement>>,
+    ref: ForwardedRef<HTMLUListElement>,
+  ) => {
+    return (
+      <ul ref={ref} className={clsx('amsterdam-list', className)} {...restProps}>
+        {children}
+      </ul>
+    )
+  },
+)
+
+List.displayName = 'List'
