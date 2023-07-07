5a83d096a93a99eea510688125edb6015bca19bb
Niels Roozemond <n.roozemond@amsterdam.nl>
2023-07-07 14:02:28 +0200

    Gap

--- storybook/storybook-css/src/components/UnorderedList/UnorderedList.stories.tsx
+++ storybook/storybook-css/src/components/UnorderedList/UnorderedList.stories.tsx
@@ -13,15 +13,21 @@ const meta = {
   component: UnorderedList,
   args: {
     children: [
+      <UnorderedList.Item key={0}>
+        Dit is het eerste niveau opsommingsteken.
+        <UnorderedList>
+          <UnorderedList.Item key={1.1}>Dit is het tweede niveau opsommingsteken.</UnorderedList.Item>
+        </UnorderedList>
+      </UnorderedList.Item>,
       <UnorderedList.Item key={1}>Dit is het eerste niveau opsommingsteken.</UnorderedList.Item>,
-      <UnorderedList.Item key={2}>
+      <UnorderedList.Item key={2}>Dit is het eerste niveau opsommingsteken.</UnorderedList.Item>,
+      <UnorderedList.Item key={3}>
         Dit is het eerste niveau opsommingsteken.
         <UnorderedList>
           <UnorderedList.Item key={1.1}>Dit is het tweede niveau opsommingsteken.</UnorderedList.Item>
           <UnorderedList.Item key={1.2}>Dit is het tweede niveau opsommingsteken.</UnorderedList.Item>
         </UnorderedList>
       </UnorderedList.Item>,
-      <UnorderedList.Item key={3}>Dit is het eerste niveau opsommingsteken.</UnorderedList.Item>,
     ],
   },
 } satisfies Meta<typeof UnorderedList>
