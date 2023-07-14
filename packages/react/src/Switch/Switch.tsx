2a93f5985da4d62366bf08e538bf8ce871a38dbb
hvdhaar <h.c.vanderhaar@gmail.com>
2023-07-14 13:38:02 +0200

    wip: change class names

--- packages/react/src/Switch/Switch.tsx
+++ packages/react/src/Switch/Switch.tsx
@@ -5,10 +5,10 @@ interface SwitchProps extends InputHTMLAttributes<HTMLInputElement> {}
 export const Switch = forwardRef((props: PropsWithChildren<SwitchProps>, ref: ForwardedRef<HTMLInputElement>) => {
   return (
     <label className="amsterdam-switch">
-      <input {...props} className="amsterdam-switch-input" ref={ref} type="checkbox" />
+      <input {...props} className="amsterdam-switch__input" ref={ref} type="checkbox" />
 
-      <span className="amsterdam-switch-toggle" aria-hidden="true">
-        <span className="amsterdam-switch-toggle-inner"></span>
+      <span className="amsterdam-switch__toggle" aria-hidden="true">
+        <span className="amsterdam-switch__toggle-inner"></span>
       </span>
     </label>
   )
