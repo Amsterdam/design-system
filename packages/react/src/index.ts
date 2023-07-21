0e3d32c40f3c2e64df42604a4e929b0cec1c1591
Aram Limpens <a.limpens@amsterdam.nl>
2023-07-21 12:05:57 +0200

    Import all css files in React package index

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -3,6 +3,15 @@
  * Copyright (c) 2023 Gemeente Amsterdam
  */
 
+import '../../css/src/accordion/accordion.scss'
+import '../../css/src/breadcrumb/breadcrumb.scss'
 import '../../css/src/button/button.scss'
+import '../../css/src/form-label/form-label.scss'
+import '../../css/src/heading/heading.scss'
+import '../../css/src/icon/icon.scss'
+import '../../css/src/link/link.scss'
+import '../../css/src/list/list.scss'
+import '../../css/src/paragraph/paragraph.scss'
+import '../../css/src/unordered-list/unordered-list.scss'
 
 export * from './unstyled'
