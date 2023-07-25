26e19bb0ffe9f4614c7431d6e5c299f757e4ee29
Aram Limpens <a.limpens@amsterdam.nl>
2023-07-25 17:53:38 +0200

    Fix story arrays

--- storybook/storybook-react/src/components/grid/PageGrid.stories.tsx
+++ storybook/storybook-react/src/components/grid/PageGrid.stories.tsx
@@ -24,17 +24,13 @@ type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
   args: {
-    children: (
-      <>
-        {Array.from(Array(12).keys()).map((i) => (
-          <GridCell key={i}>
-            <div className="amsterdam-docs-pink-box">
-              <Paragraph>{i + 1}</Paragraph>
-            </div>
-          </GridCell>
-        ))}
-      </>
-    ),
+    children: Array.from(Array(12).keys()).map((i) => (
+      <GridCell key={i}>
+        <div className="amsterdam-docs-pink-box">
+          <Paragraph>{i + 1}</Paragraph>
+        </div>
+      </GridCell>
+    )),
   },
   parameters: {
     docs: {
@@ -45,49 +41,45 @@ export const Default: Story = {
 
 export const ThreeColumns: Story = {
   args: {
-    children: (
-      <>
-        <GridCell gridColumns={4}>
-          <div className="amsterdam-docs-pink-box">
-            <Paragraph>Blok 1: kolom 1 t/m 4</Paragraph>
-          </div>
-        </GridCell>
-        <GridCell gridColumns={4}>
-          <div className="amsterdam-docs-pink-box">
-            <Paragraph>Blok 2: kolom 5 t/m 8</Paragraph>
-          </div>
-        </GridCell>
-        <GridCell gridColumns={4}>
-          <div className="amsterdam-docs-pink-box">
-            <Paragraph>Blok 3: kolom 9 t/m 12</Paragraph>
-          </div>
-        </GridCell>
-      </>
-    ),
+    children: [
+      <GridCell key={1} gridColumns={4}>
+        <div className="amsterdam-docs-pink-box">
+          <Paragraph>Blok 1: kolom 1 t/m 4</Paragraph>
+        </div>
+      </GridCell>,
+      <GridCell key={2} gridColumns={4}>
+        <div className="amsterdam-docs-pink-box">
+          <Paragraph>Blok 2: kolom 5 t/m 8</Paragraph>
+        </div>
+      </GridCell>,
+      <GridCell key={3} gridColumns={4}>
+        <div className="amsterdam-docs-pink-box">
+          <Paragraph>Blok 3: kolom 9 t/m 12</Paragraph>
+        </div>
+      </GridCell>,
+    ],
   },
 }
 
 export const ThreeImages: Story = {
   args: {
-    children: (
-      <>
-        <GridCell gridColumns={4}>
-          <figure className="amsterdam-docs-figure">
-            <img src="https://www.amsterdam.nl/publish/pages/1037047/20230614_poma_visuals_pn_singel-zuid940.jpg" />
-          </figure>
-        </GridCell>
-        <GridCell gridColumns={4}>
-          <figure className="amsterdam-docs-figure">
-            <img src="https://www.amsterdam.nl/publish/pages/987083/33464-amstel-01-01sep2017-e-v-eis9403.jpg" />
-          </figure>
-        </GridCell>
-        <GridCell gridColumns={4}>
-          <figure className="amsterdam-docs-figure">
-            <img src="https://www.amsterdam.nl/publish/pages/1036935/46116-sara-burgerhardtstr-26-17072019-17juli2019-e-v-eis940.jpg" />
-          </figure>
-        </GridCell>
-      </>
-    ),
+    children: [
+      <GridCell key={1} gridColumns={4}>
+        <figure className="amsterdam-docs-figure">
+          <img src="https://www.amsterdam.nl/publish/pages/1037047/20230614_poma_visuals_pn_singel-zuid940.jpg" />
+        </figure>
+      </GridCell>,
+      <GridCell key={2} gridColumns={4}>
+        <figure className="amsterdam-docs-figure">
+          <img src="https://www.amsterdam.nl/publish/pages/987083/33464-amstel-01-01sep2017-e-v-eis9403.jpg" />
+        </figure>
+      </GridCell>,
+      <GridCell key={3} gridColumns={4}>
+        <figure className="amsterdam-docs-figure">
+          <img src="https://www.amsterdam.nl/publish/pages/1036935/46116-sara-burgerhardtstr-26-17072019-17juli2019-e-v-eis940.jpg" />
+        </figure>
+      </GridCell>,
+    ],
   },
 }
 
@@ -105,17 +97,15 @@ export const MiddleBlock: Story = {
 
 export const Paragraphs: Story = {
   args: {
-    children: (
-      <>
-        <Paragraph gridColumns={{ start: 1, span: 3 }}>
-          A paragraph, directly on the grid, with a width of 3 columns.
-        </Paragraph>
-        <Paragraph gridColumns={{ start: 4, span: 9 }}>
-          A paragraph, directly on the grid, with a width of 9 columns. Lorem ipsum dolor sit amet, consectetur
-          adipisicing elit. Eligendi id quibusdam soluta. Architecto aspernatur at debitis, deserunt ea error ex impedit
-          magnam nihil odio perferendis placeat porro quidem repellat temporibus ut voluptate.
-        </Paragraph>
-      </>
-    ),
+    children: [
+      <Paragraph key={1} gridColumns={{ start: 1, span: 3 }}>
+        A paragraph, directly on the grid, with a width of 3 columns.
+      </Paragraph>,
+      <Paragraph key={2} gridColumns={{ start: 4, span: 9 }}>
+        A paragraph, directly on the grid, with a width of 9 columns. Lorem ipsum dolor sit amet, consectetur
+        adipisicing elit. Eligendi id quibusdam soluta. Architecto aspernatur at debitis, deserunt ea error ex impedit
+        magnam nihil odio perferendis placeat porro quidem repellat temporibus ut voluptate.
+      </Paragraph>,
+    ],
   },
 }
