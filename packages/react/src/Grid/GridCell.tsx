12a784a38f7c269fa22a45eeac170173925026e1
Vincent Smedinga <v.smedinga@amsterdam.nl>
2023-07-26 10:21:53 +0200

    Add initial grid components (#408)
    * Prevent incorrect recursion in TS parser config
    
    * Add initial page grid and grid cell components
    
    * Extract tokens for page grid
    
    * Revert changing an interface to a type
    
    * Use objects for grid columns prop and use `span` rather than `end`
    
    * Allow passing in a single column span
    
    * Use consistent syntax for min-width media query
    
    * Use clamped pixel values for the grid gap
    
    A percentage is difficult with nested grid: a width of 2% of an outer grid can’t easily be expressed in an inner grid, in order to align both sets of columns.
    
    * Let Sass generate grid column class names for us
    
    * Make this function return strings consistently
    
    * Fix story arrays
    
    * Fix directory name
    
    * Temp directory rename
    
    * Rename directory
    
    * Fix imports
    
    ---------
    
    Co-authored-by: Aram Limpens <a.limpens@amsterdam.nl>

---
+++ packages/react/src/Grid/GridCell.tsx
@@ -0,0 +1,37 @@
+/**
+ * @license EUPL-1.2+
+ * Copyright (c) 2023 Gemeente Amsterdam
+ */
+
+import clsx from 'clsx'
+import { PropsWithChildren } from 'react'
+import { PageGridColumnNumber } from './PageGrid'
+
+export type GridCellProps = PropsWithChildren<{
+  gridColumns?:
+    | PageGridColumnNumber
+    | {
+        start?: PageGridColumnNumber
+        span: PageGridColumnNumber
+      }
+}>
+
+export const gridColumnClassNames = (gridColumns?: GridCellProps['gridColumns']) => {
+  if (!gridColumns) {
+    return ''
+  }
+
+  if (typeof gridColumns === 'number') {
+    return `amsterdam-grid-column-span-${gridColumns}`
+  }
+
+  const { start, span } = gridColumns
+
+  return clsx(start && `amsterdam-grid-column-start-${start}`, span && `amsterdam-grid-column-span-${span}`)
+}
+
+export const GridCell = ({ children, gridColumns }: GridCellProps) => (
+  <div className={clsx('amsterdam-grid-cell', gridColumnClassNames(gridColumns))}>{children}</div>
+)
+
+GridCell.displayName = 'GridCell'
