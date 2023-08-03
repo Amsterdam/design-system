c796dc59033c6585c28fc97912b38c81aa6e496e
alimpens <37216945+alimpens@users.noreply.github.com>
2023-08-03 11:17:24 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@a62897292786361a698eb93e1c70dcb7ebd3e9b5 🚀

---
+++ storybook-react/assets/PageGrid.docs-f568f7a1.js
@@ -0,0 +1,26 @@
+import{j as e}from"./jsx-runtime-ffb262ed.js";import{M as s,d as o,P as d,e as m,f as t}from"./index-ba0d1eee.js";import{P as l,T as p,a as c,M as h,b as g}from"./PageGrid.stories-b3955a46.js";import{u as a}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./iframe-2f4f7ff2.js";import"../sb-preview/runtime.js";import"./index-d475d2ea.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./index-d893ea64.js";import"./assert-a1982797.js";import"./index-d37d4223.js";import"./index-356e4a49.js";/* empty css                *//* empty css                  */import"./Paragraph-89479e9a.js";import"./clsx-1229b3e0.js";const x=`# Page grid
+
+Onder alle pagina’s en schermen ligt hetzelfde grid. Het grid is responsive, dus de precieze breedte van een kolom is afhankelijk van de schermbreedte.
+
+De maximale breedte van het grid is 1440 pixels. In een later stadium bieden we een grid van 1920 aan.
+
+Op schermen tot 854 pixels breed bestaat het grid uit 4 kolommen, op bredere schermen worden dat er 12. Het grid staat horizontaal gecentreerd op de pagina.
+
+De witruimte tussen de kolommen is ook flexibel: die groeit lineair van 8 pixels bij een breedte van 320 naar 24 pixels bij een breedte van 1440.
+
+Pagina-elementen worden op het grid geplaatst en beslaan een bepaald aantal kolommen.
+`;function i(r){const n=Object.assign({h3:"h3",p:"p"},a(),r.components);return e.jsxs(e.Fragment,{children:[e.jsx(s,{of:l}),`
+`,e.jsx(o,{children:x}),`
+`,e.jsx(d,{}),`
+`,e.jsx(m,{}),`
+`,e.jsx(n.h3,{id:"three-columns",children:"Three columns"}),`
+`,e.jsx(t,{of:p}),`
+`,e.jsx(n.h3,{id:"three-images",children:"Three images"}),`
+`,e.jsx(t,{of:c}),`
+`,e.jsx(n.h3,{id:"a-middle-block",children:"A middle block"}),`
+`,e.jsx(n.p,{children:"Spanning ten columns, numbers 2 up to 11."}),`
+`,e.jsx(t,{of:h}),`
+`,e.jsx(n.h3,{id:"without-the-grid-cell-component",children:"Without the grid cell component"}),`
+`,e.jsx(n.p,{children:"Paragraphs can be used directly on the grid, as they expose the grid cell props as well."}),`
+`,e.jsx(t,{of:g})]})}function S(r={}){const{wrapper:n}=Object.assign({},a(),r.components);return n?e.jsx(n,Object.assign({},r,{children:e.jsx(i,r)})):i(r)}export{S as default};
+//# sourceMappingURL=PageGrid.docs-f568f7a1.js.map
--- storybook-react/assets/preview-8c0f75a1.js
+++
@@ -1,2 +0,0 @@
-const o={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{canvas:{sourceState:"shown"}},viewMode:"docs"};export{o as parameters};
-//# sourceMappingURL=preview-8c0f75a1.js.map
