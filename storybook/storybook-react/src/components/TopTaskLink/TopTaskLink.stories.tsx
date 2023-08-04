6b9b1ee44d26bf7760e177fe5faad369dd543117
Aram Limpens <a.limpens@amsterdam.nl>
2023-08-04 13:57:54 +0200

    Align main and develop

--- storybook/storybook-react/src/components/TopTaskLink/TopTaskLink.stories.tsx
+++ storybook/storybook-react/src/components/TopTaskLink/TopTaskLink.stories.tsx
@@ -3,10 +3,8 @@
  * Copyright (c) 2023 Gemeente Amsterdam
  */
 
-import { GridCell, PageGrid, TopTaskLink } from '@amsterdam/design-system-react/src'
+import { GridCell, PageGrid, TopTaskLink } from '@amsterdam/design-system-react'
 import { Meta, StoryObj } from '@storybook/react'
-
-import '@amsterdam/design-system-css/src/top-task-link/top-task-link.scss'
 
 const meta = {
   title: 'Top task link',
