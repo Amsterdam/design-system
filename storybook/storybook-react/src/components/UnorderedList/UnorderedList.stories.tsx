09cd60b257578ff2e6d64ab6afaea0f1e173b123
Vincent Smedinga <v.smedinga@amsterdam.nl>
2023-08-18 15:09:45 +0200

    Move obsolete example and stylesheet

--- storybook/storybook-react/src/components/UnorderedList/UnorderedList.stories.tsx
+++ storybook/storybook-react/src/components/UnorderedList/UnorderedList.stories.tsx
@@ -3,7 +3,7 @@
  * Copyright (c) 2023 Gemeente Amsterdam
  */
 
-import { UnorderedList } from '@amsterdam/design-system-react'
+import { Heading, Paragraph, UnorderedList } from '@amsterdam/design-system-react'
 import type { Meta, StoryObj } from '@storybook/react'
 
 const meta = {
@@ -41,9 +41,18 @@ export const NoMarkers: Story = {
   args: {
     noMarkers: true,
     children: [
-      <UnorderedList.Item key={0}>Dit is het eerste niveau opsommingsteken.</UnorderedList.Item>,
-      <UnorderedList.Item key={1}>Dit is het eerste niveau opsommingsteken.</UnorderedList.Item>,
-      <UnorderedList.Item key={2}>Dit is het eerste niveau opsommingsteken.</UnorderedList.Item>,
+      <UnorderedList.Item key={0}>
+        <Heading size="level-4">Parkeren</Heading>
+        <Paragraph>Alles over parkeren en verkeer in de stad.</Paragraph>
+      </UnorderedList.Item>,
+      <UnorderedList.Item key={1}>
+        <Heading size="level-4">Parkeertijd bezoek doorgeven</Heading>
+        <Paragraph>Bezoekers- of kraskaart-vergunning? Geef hier parkeertijd van uw bezoek door.</Paragraph>
+      </UnorderedList.Item>,
+      <UnorderedList.Item key={2}>
+        <Heading size="level-4">Verhuizing doorgeven</Heading>
+        <Paragraph>Naar en binnen Amsterdam.</Paragraph>
+      </UnorderedList.Item>,
     ],
   },
 }
