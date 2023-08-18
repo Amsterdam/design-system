a9f654d4a4bee5b1c8b37de1a6bbbc8f87d33e5d
Aram Limpens <a.limpens@amsterdam.nl>
2023-08-18 16:55:41 +0200

    Add tests

--- packages/react/src/Checkbox/Checkbox.test.tsx
+++ packages/react/src/Checkbox/Checkbox.test.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import { createRef } from 'react'
 import { Checkbox } from './Checkbox'
 import '@testing-library/jest-dom'
@@ -7,45 +7,169 @@ describe('Checkbox', () => {
   it('renders', () => {
     const { container } = render(<Checkbox />)
 
-    const component = container.querySelector(':only-child')
+    const wrapper = container.querySelector(':only-child')
+    const input = screen.getByRole('checkbox')
+    const label = container.querySelector('label')
 
-    expect(component).toBeInTheDocument()
-    expect(component).toBeVisible()
+    expect(wrapper).toBeInTheDocument()
+    expect(wrapper).toBeVisible()
+
+    expect(input).toBeInTheDocument()
+    expect(input).toBeVisible()
+
+    expect(label).toBeInTheDocument()
+    expect(label).toBeVisible()
   })
 
   it('renders a design system BEM class name', () => {
     const { container } = render(<Checkbox />)
 
-    const component = container.querySelector(':only-child')
+    const wrapper = container.querySelector(':only-child')
+    const input = screen.getByRole('checkbox')
+    const label = container.querySelector('label')
 
-    expect(component).toHaveClass('amsterdam-checkbox')
+    expect(wrapper).toHaveClass('amsterdam-checkbox')
+    expect(input).toHaveClass('amsterdam-checkbox__input')
+    expect(label).toHaveClass('amsterdam-checkbox__label')
   })
 
   it('can have a custom class name', () => {
     const { container } = render(<Checkbox className="extra" />)
 
-    const component = container.querySelector(':only-child')
+    const wrapper = container.querySelector(':only-child')
 
-    expect(component).toHaveClass('extra')
+    expect(wrapper).toHaveClass('extra')
   })
 
   it('can have a additional class name', () => {
     const { container } = render(<Checkbox className="extra" />)
 
-    const component = container.querySelector(':only-child')
+    const wrapper = container.querySelector(':only-child')
 
-    expect(component).toHaveClass('extra')
+    expect(wrapper).toHaveClass('extra')
 
-    expect(component).toHaveClass('amsterdam-checkbox')
+    expect(wrapper).toHaveClass('amsterdam-checkbox')
+  })
+
+  // checked state
+
+  it('is not checked by default', () => {
+    render(<Checkbox />)
+
+    const input = screen.getByRole('checkbox')
+
+    expect(input).not.toBeChecked()
+  })
+
+  it('can have a checked state', () => {
+    render(<Checkbox checked />)
+
+    const input = screen.getByRole('checkbox')
+
+    expect(input).toBeChecked()
+  })
+
+  // indeterminate state
+
+  it('is not indeterminate by default', () => {
+    render(<Checkbox />)
+
+    const input = screen.getByRole('checkbox')
+
+    expect(input).not.toBePartiallyChecked()
+  })
+
+  it('can have a checked state', () => {
+    render(<Checkbox indeterminate />)
+
+    const input = screen.getByRole('checkbox')
+
+    expect(input).toBePartiallyChecked()
+  })
+
+  // invalid state
+
+  it('is not invalid by default', () => {
+    render(<Checkbox />)
+
+    const input = screen.getByRole('checkbox')
+
+    expect(input).not.toBeInvalid()
+  })
+
+  it('renders a design system BEM modifier class name', () => {
+    const { container } = render(<Checkbox invalid />)
+
+    const invalidMarker = container.querySelector('.amsterdam-checkbox__label__checkmark--invalid')
+
+    expect(invalidMarker).toBeInTheDocument()
+  })
+
+  it('omits non-essential invalid attributes when not invalid', () => {
+    render(<Checkbox invalid={false} />)
+
+    const input = screen.getByRole('checkbox')
+
+    expect(input).not.toHaveAttribute('aria-invalid')
+  })
+
+  it('can have an invalid state', () => {
+    render(<Checkbox invalid />)
+
+    const input = screen.getByRole('checkbox')
+
+    expect(input).toBeInvalid()
+  })
+
+  // disabled state
+
+  it('is not disabled by default', () => {
+    render(<Checkbox />)
+
+    const input = screen.getByRole('checkbox')
+
+    expect(input).not.toBeDisabled()
+  })
+
+  it('can have a disabled state', () => {
+    render(<Checkbox disabled />)
+
+    const input = screen.getByRole('checkbox')
+
+    expect(input).toBeDisabled()
+  })
+
+  it('can trigger a change event', () => {
+    const handleChange = jest.fn()
+
+    render(<Checkbox onChange={handleChange} />)
+
+    const input = screen.getByRole('checkbox')
+
+    input?.click()
+
+    expect(handleChange).toHaveBeenCalled()
+  })
+
+  it('does not trigger a change event when disabled', () => {
+    const handleChange = jest.fn()
+
+    render(<Checkbox disabled onChange={handleChange} />)
+
+    const input = screen.getByRole('checkbox')
+
+    input?.click()
+
+    expect(handleChange).not.toHaveBeenCalled()
   })
 
   it('supports ForwardRef in React', () => {
     const ref = createRef<HTMLInputElement>()
 
-    const { container } = render(<Checkbox ref={ref} />)
+    render(<Checkbox ref={ref} />)
 
-    const component = container.querySelector(':only-child')
+    const input = screen.getByRole('checkbox')
 
-    expect(ref.current).toBe(component)
+    expect(ref.current).toBe(input)
   })
 })
