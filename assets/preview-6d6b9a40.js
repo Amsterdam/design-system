dffe801345bf242f6612a8dcdbd59bc849e4016e
VincentSmedinga <v.smedinga@amsterdam.nl>
2023-08-23 11:58:33 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@4b32d896b6dd9c037736ef5f361b81b94d997bf7 🚀

---
+++ assets/preview-6d6b9a40.js
@@ -0,0 +1,2 @@
+const o={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{canvas:{sourceState:"shown"}},options:{storySort:{order:["Docs",["Intro"]]}},viewMode:"docs"};export{o as parameters};
+//# sourceMappingURL=preview-6d6b9a40.js.map
--- assets/preview-fc88542e.js
+++
@@ -1,2 +0,0 @@
-const o={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{canvas:{sourceState:"shown"}},options:{storySort:{order:["Docs",["Intro"]]}},viewMode:"docs"};export{o as parameters};
-//# sourceMappingURL=preview-fc88542e.js.map
