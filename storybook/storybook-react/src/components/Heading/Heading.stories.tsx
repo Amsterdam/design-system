be199cc0ee2b63f5eb59ef6f9a7164cbac047f07
Aram <37216945+alimpens@users.noreply.github.com>
2023-08-23 13:07:35 +0200

    Add inverse color prop to Heading (#449) (#473)
    * Add inverse color prop to Heading (#449)
    
    * Add Heading inverse color, run prettier
    
    * Use double dashes for modifiers
    
    * Use double dashes in React component as well
    
    * Update story
    
    * Add stories, clean up existing stories
    
    * Add and update tests
    
    * Remove unnecessary args
    
    * Change level argType to radio
    
    * Prevent layout change for inverse prop
    
    Co-authored-by: Vincent Smedinga <v.smedinga@amsterdam.nl>
    
    * Align InvertedColor text with other stories
    
    * Simplify Heading stories
    
    ---------
    
    Co-authored-by: Vincent Smedinga <v.smedinga@amsterdam.nl>

--- storybook/storybook-react/src/components/Heading/Heading.stories.tsx
+++ storybook/storybook-react/src/components/Heading/Heading.stories.tsx
@@ -11,13 +11,18 @@ const meta = {
   component: Heading,
   args: {
     children: 'Jouw typograaf biedt mij zulke exquise schreven!',
-    level: 1,
-    size: 'level-1',
+    inverseColor: false,
   },
   argTypes: {
+    level: {
+      control: 'radio',
+      options: [1, 2, 3, 4],
+    },
     size: {
+      control: 'radio',
       options: ['level-1', 'level-2', 'level-3', 'level-4'],
     },
+    inverseColor: { control: 'boolean' },
   },
 } satisfies Meta<typeof Heading>
 
@@ -25,25 +30,57 @@ export default meta
 
 type Story = StoryObj<typeof meta>
 
+export const Default: Story = {
+  decorators: [
+    (Story, context) => (
+      <div
+        style={{
+          background: context.args.inverseColor ? '#004699' : 'transparent',
+          margin: context.args.inverseColor ? -16 : 0,
+          padding: context.args.inverseColor ? 16 : 0,
+        }}
+      >
+        <Story />
+      </div>
+    ),
+  ],
+}
+
 export const Heading1: Story = {}
 
 export const Heading2: Story = {
   args: {
     level: 2,
-    size: 'level-2',
   },
 }
 
 export const Heading3: Story = {
   args: {
     level: 3,
-    size: 'level-3',
   },
 }
 
 export const Heading4: Story = {
   args: {
     level: 4,
-    size: 'level-4',
   },
+}
+
+export const InvertedColor: Story = {
+  args: {
+    inverseColor: true,
+  },
+  decorators: [
+    (Story) => (
+      <div
+        style={{
+          background: '#004699',
+          margin: -16,
+          padding: 16,
+        }}
+      >
+        <Story />
+      </div>
+    ),
+  ],
 }
