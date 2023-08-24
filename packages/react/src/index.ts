1b4a3de78ebff53c0f67b6e491f5148295606eb2
Aram Limpens <a.limpens@amsterdam.nl>
2023-08-24 13:25:55 +0200

    Add all files to bundled css and styled components

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -15,5 +15,7 @@ import '../../css/src/link/link.scss'
 import '../../css/src/list/list.scss'
 import '../../css/src/paragraph/paragraph.scss'
 import '../../css/src/unordered-list/unordered-list.scss'
+import '../../css/src/grid/grid-cell.scss'
+import '../../css/src/grid/page-grid.scss'
 
 export * from './unstyled'
