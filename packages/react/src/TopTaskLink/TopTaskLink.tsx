241a190f0e9b3a9716beef4a9e34ca2a5df633d8
Aram Limpens <a.limpens@amsterdam.nl>
2023-09-01 15:34:11 +0200

    Add aria-label

--- packages/react/src/TopTaskLink/TopTaskLink.tsx
+++ packages/react/src/TopTaskLink/TopTaskLink.tsx
@@ -13,7 +13,13 @@ export interface TopTaskLinkProps extends AnchorHTMLAttributes<HTMLAnchorElement
 
 export const TopTaskLink = forwardRef(
   ({ className, label, description, ...restProps }: TopTaskLinkProps, ref: ForwardedRef<HTMLAnchorElement>) => (
-    <a {...restProps} ref={ref} className={clsx('amsterdam-top-task-link', className)}>
+    <a
+      {...restProps}
+      ref={ref}
+      className={clsx('amsterdam-top-task-link', className)}
+      // Add an aria-label with a comma between label and description, so screenreaders have a slight pause between the two.
+      aria-label={`${label}, ${description}`}
+    >
       <span className="amsterdam-top-task-link__label">{label}</span>
       {description && <span className="amsterdam-top-task-link__description">{description}</span>}
     </a>
