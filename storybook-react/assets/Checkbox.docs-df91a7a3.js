988c9a918733ed7ec47f19287a758d961ecc6491
VincentSmedinga <v.smedinga@amsterdam.nl>
2023-09-04 08:41:55 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@8c775997f6aba4b7ba9022780c9c469e808a7f9b 🚀

---
+++ storybook-react/assets/Checkbox.docs-df91a7a3.js
@@ -0,0 +1,9 @@
+import{j as o}from"./clsx-1e30d079.js";import{M as e,d as i,P as a,f as m,e as c}from"./index-b18f5913.js";import{C as p,L as x}from"./Checkbox.stories-e9c562ed.js";import{u as r}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./iframe-5c668155.js";import"../sb-preview/runtime.js";import"./index-d475d2ea.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./index-d893ea64.js";import"./assert-a1982797.js";import"./index-d37d4223.js";import"./index-356e4a49.js";import"./index.esm-ac9beee0.js";const j=`# Checkbox
+`;function s(t){const n=Object.assign({h2:"h2",h3:"h3"},r(),t.components);return o.jsxs(o.Fragment,{children:[o.jsx(e,{of:p}),`
+`,o.jsx(i,{children:j}),`
+`,o.jsx(a,{}),`
+`,o.jsx(m,{}),`
+`,o.jsx(n.h2,{id:"stories",children:"Stories"}),`
+`,o.jsx(n.h3,{id:"long-label",children:"Long label"}),`
+`,o.jsx(c,{of:x})]})}function w(t={}){const{wrapper:n}=Object.assign({},r(),t.components);return n?o.jsx(n,Object.assign({},t,{children:o.jsx(s,t)})):s(t)}export{w as default};
+//# sourceMappingURL=Checkbox.docs-df91a7a3.js.map
