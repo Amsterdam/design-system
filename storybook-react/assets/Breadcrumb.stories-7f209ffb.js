2db3185b5fb4afb21a903d22e953162b0ebb54dd
VincentSmedinga <v.smedinga@amsterdam.nl>
2023-09-06 13:37:29 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@bc51c51a09a7f743dc3156766ab71f25d15f31ec 🚀

---
+++ storybook-react/assets/Breadcrumb.stories-7f209ffb.js
@@ -0,0 +1,34 @@
+import{j as r}from"./clsx-1e30d079.js";import{o as e}from"./index.esm-074ed60f.js";const d={title:"Breadcrumb",component:e},m={render:()=>r.jsxs(e,{children:[r.jsx(e.Item,{href:"#",children:"home"}),r.jsx(e.Item,{href:"#",children:"path 1"}),r.jsx(e.Item,{href:"#",children:"path 2"}),r.jsx(e.Item,{href:"#",children:"path 3"}),r.jsx(e.Item,{href:"#",children:"path 5"})]}),parameters:{docs:{source:{language:"jsx",code:`
+<Breadcrumb>
+  <Breadcrumb.Item href="#">home</Breadcrumb.Item>
+  <Breadcrumb.Item href="#">path 1</Breadcrumb.Item>
+  <Breadcrumb.Item href="#">path 2</Breadcrumb.Item>
+  <Breadcrumb.Item href="#">path 3</Breadcrumb.Item>
+  <Breadcrumb.Item href="#">path 5</Breadcrumb.Item>
+</Breadcrumb>
+        `}}}};var a,t,c;m.parameters={...m.parameters,docs:{...(a=m.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  render: () => <Breadcrumb>
+      <Breadcrumb.Item href="#">home</Breadcrumb.Item>
+      <Breadcrumb.Item href="#">path 1</Breadcrumb.Item>
+      <Breadcrumb.Item href="#">path 2</Breadcrumb.Item>
+      <Breadcrumb.Item href="#">path 3</Breadcrumb.Item>
+      <Breadcrumb.Item href="#">path 5</Breadcrumb.Item>
+    </Breadcrumb>,
+  parameters: {
+    docs: {
+      source: {
+        language: 'jsx',
+        code: \`
+<Breadcrumb>
+  <Breadcrumb.Item href="#">home</Breadcrumb.Item>
+  <Breadcrumb.Item href="#">path 1</Breadcrumb.Item>
+  <Breadcrumb.Item href="#">path 2</Breadcrumb.Item>
+  <Breadcrumb.Item href="#">path 3</Breadcrumb.Item>
+  <Breadcrumb.Item href="#">path 5</Breadcrumb.Item>
+</Breadcrumb>
+        \`
+      }
+    }
+  }
+}`,...(c=(t=m.parameters)==null?void 0:t.docs)==null?void 0:c.source}}};const u=["BreadcrumbStory"],b=Object.freeze(Object.defineProperty({__proto__:null,BreadcrumbStory:m,__namedExportsOrder:u,default:d},Symbol.toStringTag,{value:"Module"}));export{b as B};
+//# sourceMappingURL=Breadcrumb.stories-7f209ffb.js.map
