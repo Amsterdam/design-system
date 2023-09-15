0b3deb303327f162607208f37daa72ba0f830214
VincentSmedinga <v.smedinga@amsterdam.nl>
2023-09-15 16:05:41 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@459f5046f05faf78fcb0f5a58a2546b9a69122f9 🚀

---
+++ storybook-react/assets/PageMenu.stories-de44840a.js
@@ -0,0 +1,17 @@
+import{i as e,j as r,k as s,l as i}from"./index.esm-95ec6425.js";const u={title:"Navigation/Page Menu",component:e},n={render:()=>r.jsxs(e,{children:[r.jsx(e.Link,{href:"#",children:"English"}),r.jsx(e.Link,{href:"#",icon:s,children:"Inloggen Mijn Amsterdam"}),r.jsx(e.Button,{icon:i,children:"Alle onderwerpen"})]}),parameters:{docs:{source:{type:"dynamic"}}}};var o,a,t;n.parameters={...n.parameters,docs:{...(o=n.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: () => <PageMenu>
+      <PageMenu.Link href="#">English</PageMenu.Link>
+      <PageMenu.Link href="#" icon={Login}>
+        Inloggen Mijn Amsterdam
+      </PageMenu.Link>
+      <PageMenu.Button icon={Menu}>Alle onderwerpen</PageMenu.Button>
+    </PageMenu>,
+  parameters: {
+    docs: {
+      source: {
+        type: 'dynamic'
+      }
+    }
+  }
+}`,...(t=(a=n.parameters)==null?void 0:a.docs)==null?void 0:t.source}}};const c=["Default"],l=Object.freeze(Object.defineProperty({__proto__:null,Default:n,__namedExportsOrder:c,default:u},Symbol.toStringTag,{value:"Module"}));export{l as P};
+//# sourceMappingURL=PageMenu.stories-de44840a.js.map
