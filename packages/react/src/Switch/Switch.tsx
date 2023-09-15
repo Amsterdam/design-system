c8de61eb7d6fcfc24e3c8cae3c4d7e28be31e1a3
hvdhaar <h.c.vanderhaar@gmail.com>
2023-09-15 10:36:46 +0200

    making the input less visible

--- packages/react/src/Switch/Switch.tsx
+++ packages/react/src/Switch/Switch.tsx
@@ -1,14 +1,24 @@
 import { ForwardedRef, forwardRef, InputHTMLAttributes, PropsWithChildren, useId } from 'react'
 
 export const Switch = forwardRef(
-  ({ ...restProps }: PropsWithChildren<InputHTMLAttributes<HTMLInputElement>>, ref: ForwardedRef<HTMLInputElement>) => {
-    const id = useId()
+  (
+    { id, ...restProps }: PropsWithChildren<InputHTMLAttributes<HTMLInputElement>>,
+    ref: ForwardedRef<HTMLInputElement>,
+  ) => {
+    const inputId = id || useId()
 
     return (
       <div className="amsterdam-switch">
-        <input {...restProps} id={id} className="amsterdam-switch__input" ref={ref} type="checkbox" role="switch" />
+        <input
+          {...restProps}
+          className="amsterdam-switch__input"
+          id={inputId}
+          ref={ref}
+          type="checkbox"
+          role="switch"
+        />
 
-        <label className="amsterdam-switch__label" htmlFor={id} aria-hidden="true">
+        <label className="amsterdam-switch__label" htmlFor={inputId} aria-hidden="true">
           <span className="amsterdam-switch__toggle">
             <span className="amsterdam-switch__toggle-inner"></span>
           </span>
