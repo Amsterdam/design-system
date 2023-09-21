fa00d301c14be6133ad0d6e4850a18b1b293a25f
Aram <37216945+alimpens@users.noreply.github.com>
2023-09-21 10:21:49 +0200

    Flatten React storybook directory (#542)
    * Flatten storybook-react src directory
    
    * Fix import paths
    
    * Fix links
    
    * Fix images in Page Grid story
    
    * Update template

---
+++ storybook/storybook-react/src/Blockquote/Blockquote.stories.tsx
@@ -0,0 +1,47 @@
+/**
+ * @license EUPL-1.2+
+ * Copyright (c) 2023 Gemeente Amsterdam
+ */
+
+import { Blockquote } from '@amsterdam/design-system-react'
+import { Meta, StoryObj } from '@storybook/react'
+import '../../../storybook-docs/src/stories.scss'
+
+const meta = {
+  title: 'Text/Blockquote',
+  component: Blockquote,
+  args: {
+    children: 'Jouw typograaf biedt mij zulke exquise schreven!',
+    inverseColor: false,
+  },
+  argTypes: {
+    inverseColor: { control: 'boolean' },
+  },
+} satisfies Meta<typeof Blockquote>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  decorators: [
+    (Story, context) => (
+      <div className={context.args.inverseColor ? 'amsterdam-docs-dark-background' : undefined}>
+        <Story />
+      </div>
+    ),
+  ],
+}
+
+export const InvertedColor: Story = {
+  args: {
+    inverseColor: true,
+  },
+  decorators: [
+    (Story, context) => (
+      <div className={context.args.inverseColor ? 'amsterdam-docs-dark-background' : undefined}>
+        <Story />
+      </div>
+    ),
+  ],
+}
