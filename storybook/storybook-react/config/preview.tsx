8df10d68849ae84b61dbd8eb923bf5741e5a6dfd
Vincent Smedinga <v.smedinga@amsterdam.nl>
2023-09-28 23:44:47 +0200

    Add custom viewports to Storybook

--- storybook/storybook-react/config/preview.tsx
+++ storybook/storybook-react/config/preview.tsx
@@ -2,8 +2,48 @@ import '@amsterdam/design-system-tokens/dist/root.css'
 import '@amsterdam/design-system-assets/font/index.css'
 import '@amsterdam/design-system-css/dist/index.css'
 
+const viewports = {
+  narrow: {
+    name: 'Breakpoint: Narrow',
+    styles: {
+      width: '320px',
+      height: '640px',
+    },
+  },
+  medium: {
+    name: 'Breakpoint: Medium',
+    styles: {
+      width: '576px',
+      height: '640px',
+    },
+  },
+  wide: {
+    name: 'Breakpoint: Wide',
+    styles: {
+      width: '1088px',
+      height: '640px',
+    },
+  },
+  'x-wide': {
+    name: 'Max width: website',
+    styles: {
+      width: '1600px',
+      height: '640px',
+    },
+  },
+  'xx-wide': {
+    name: 'Max width: application',
+    styles: {
+      width: '2112px',
+      height: '640px',
+    },
+  },
+}
+
 export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' },
+  actions: {
+    argTypesRegex: '^on[A-Z].*',
+  },
   controls: {
     matchers: {
       color: /(background|color)$/i,
@@ -20,6 +60,10 @@ export const parameters = {
     storySort: {
       order: ['Buttons', 'Containers', 'Feedback', 'Forms', 'Layout', 'Media', 'Navigation', 'Text'],
     },
+  },
+  viewport: {
+    defaultViewport: 'responsive',
+    viewports,
   },
   viewMode: 'docs',
 }
