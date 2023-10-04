24f4f5d255232474766f247264ccb290a109b046
Aram Limpens <a.limpens@amsterdam.nl>
2023-10-04 09:30:20 +0200

    Remove text overlay

--- storybook/storybook-react/src/AspectRatio/AspectRatio.stories.tsx
+++ storybook/storybook-react/src/AspectRatio/AspectRatio.stories.tsx
@@ -3,7 +3,7 @@
  * Copyright (c) 2023 Gemeente Amsterdam
  */
 
-import { AspectRatio, Paragraph } from '@amsterdam/design-system-react'
+import { AspectRatio } from '@amsterdam/design-system-react'
 import type { Ratio } from '@amsterdam/design-system-react'
 import { Meta, StoryObj } from '@storybook/react'
 
@@ -32,47 +32,32 @@ export default meta
 
 type Story = StoryObj<typeof meta>
 
-const textStyle = {
-  backgroundColor: 'rgba(0, 0, 0, 0.6)',
-  color: 'white',
-  left: '50%',
-  padding: '16px',
-  position: 'absolute' as 'absolute',
-  top: '50%',
-  transform: 'translate(-50%, -50%)',
-}
-
 function getStoryConfig(ratio: Ratio | undefined) {
   switch (ratio) {
     case 'extra-tall':
       return {
         image: 'https://picsum.photos/900/1600',
         maxWidth: '300px',
-        text: 'Mijn breedte is negenzestiende van mijn lengte, ik heb een 9-op-16 verhouding',
       }
     case 'tall':
       return {
         image: 'https://picsum.photos/800/1000',
         maxWidth: '400px',
-        text: 'Mijn breedte is viervijfde van mijn lengte, ik heb een 4-op-5 verhouding',
       }
     case 'wide':
       return {
         image: 'https://picsum.photos/1000/800',
         maxWidth: '625px',
-        text: 'Mijn breedte is vijfvierde van mijn lengte, ik heb een 5-op-4 verhouding',
       }
     case 'extra-wide':
       return {
         image: 'https://picsum.photos/1600/900',
         maxWidth: '888px',
-        text: 'Mijn breedte is zestiennegende van mijn lengte, ik heb een 16-op-9 verhouding',
       }
     default:
       return {
         image: 'https://picsum.photos/800/800',
         maxWidth: '500px',
-        text: 'Mijn breedte is even groot als mijn lengte, ik heb een 1-op-1 verhouding',
       }
   }
 }
@@ -87,10 +72,7 @@ const StoryTemplate: Story = {
   ],
   render: ({ ratio }) => (
     <AspectRatio ratio={ratio} style={{ maxWidth: getStoryConfig(ratio).maxWidth }}>
-      <img key={1} src={getStoryConfig(ratio).image} style={{ width: '100%' }} />
-      <Paragraph key={2} style={textStyle}>
-        {getStoryConfig(ratio).text}
-      </Paragraph>
+      <img src={getStoryConfig(ratio).image} style={{ width: '100%' }} />
     </AspectRatio>
   ),
 }
