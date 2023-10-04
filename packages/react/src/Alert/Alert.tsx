31a86df50a50dd5c529518561afd943625a25c45
Niels Roozemond <n.roozemond@amsterdam.nl>
2023-10-04 13:40:43 +0200

    AlertIcon memoized function

--- packages/react/src/Alert/Alert.tsx
+++ packages/react/src/Alert/Alert.tsx
@@ -5,12 +5,13 @@
 
 import { Alert as AlertIcon, Checkmark, Close } from '@amsterdam/design-system-react-icons'
 import clsx from 'clsx'
-import { ForwardedRef, forwardRef, HTMLAttributes, PropsWithChildren } from 'react'
+import React, { ForwardedRef, forwardRef, HTMLAttributes, PropsWithChildren } from 'react'
 import { Icon } from '../Icon'
+import { VisuallyHidden } from '../VisuallyHidden'
 
 export interface AlertProps extends PropsWithChildren<HTMLAttributes<HTMLDivElement>> {
   title?: string
-  type?: 'info' | 'error' | 'success'
+  type?: 'error' | 'success'
   closeable?: boolean
   icon?: boolean
   onClose?: () => void
@@ -21,34 +22,42 @@ interface AlertCloseProps extends HTMLAttributes<HTMLButtonElement> {
 }
 
 const AlertClose = forwardRef(
-  ({ className, size, ...restProps }: AlertCloseProps, ref: ForwardedRef<HTMLButtonElement>) => {
-    return (
-      <button {...restProps} ref={ref} className={clsx('amsterdam-alert__close', className)}>
-        <Icon svg={Close} size={size} />
-      </button>
-    )
-  },
+  ({ className, size, ...restProps }: AlertCloseProps, ref: ForwardedRef<HTMLButtonElement>) => (
+    <button {...restProps} ref={ref} className={clsx('amsterdam-alert__close', className)} title="close">
+      <VisuallyHidden>Sluiten</VisuallyHidden>
+      <Icon svg={Close} size={size} />
+    </button>
+  ),
 )
+
+const iconSvgByType = {
+  error: AlertIcon,
+  success: Checkmark,
+}
 
 export const Alert = forwardRef(
   (
     { children, className, title, type, closeable, icon, onClose, ...restProps }: AlertProps,
     ref: ForwardedRef<HTMLDivElement>,
   ) => {
-    const alertIcon = (title?: boolean) => {
-      if (icon && type === 'error') return <Icon svg={AlertIcon} size={title ? 'level-5' : 'level-6'} />
-      else if (icon && type === 'success') return <Icon svg={Checkmark} size={title ? 'level-5' : 'level-6'} />
-      else return null
-    }
+    const alertSize = title ? 'level-5' : 'level-6'
+
+    const alertIcon = React.useMemo(() => {
+      if (!icon || !type) {
+        return null
+      }
+
+      return <Icon size={alertSize} svg={iconSvgByType[type]} />
+    }, [icon, type, alertSize])
 
     return (
       <div {...restProps} ref={ref} className={clsx('amsterdam-alert', type && `amsterdam-alert--${type}`, className)}>
-        {!!alertIcon && <div className="amsterdam-alert__icon">{alertIcon(!!title)}</div>}
+        {alertIcon && <div className="amsterdam-alert__icon">{alertIcon}</div>}
         <div className="amsterdam-alert__content">
           {title && <span className="amsterdam-alert__title">{title}</span>}
           {children}
         </div>
-        {closeable && <AlertClose size={title ? 'level-5' : 'level-6'} onClick={onClose} />}
+        {closeable && <AlertClose size={alertSize} onClick={onClose} />}
       </div>
     )
   },
