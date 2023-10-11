284abeeecb86ceaeb07f56af1cc763d0ab02c850
Aram <37216945+alimpens@users.noreply.github.com>
2023-10-11 11:40:30 +0200

    Update proprietary/react-icons/indexTemplate.js
    Co-authored-by: Vincent Smedinga <v.smedinga@amsterdam.nl>

--- proprietary/react-icons/indexTemplate.js
+++ proprietary/react-icons/indexTemplate.js
@@ -4,6 +4,8 @@ function indexTemplate(filePaths) {
   const exportEntries = filePaths.map(({ path: filePath }) => {
     const basename = path.basename(filePath, path.extname(filePath))
     const exportName = /^\d/.test(basename) ? `Svg${basename}` : basename
+
+    // The name of the React component for each icon ends with `Icon`
     return `export { default as ${exportName}Icon } from './${basename}'`
   })
   return exportEntries.join('\n')
