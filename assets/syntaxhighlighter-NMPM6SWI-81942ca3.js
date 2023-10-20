5b7da8749843ce4d17d4c0252b4003bd18683c70
alimpens <37216945+alimpens@users.noreply.github.com>
2023-10-20 11:43:41 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@08f2b9a2483067ef03962ce21639af6ea0f47627 🚀

---
+++ assets/syntaxhighlighter-NMPM6SWI-81942ca3.js
@@ -0,0 +1,2 @@
+import{S as s,c,s as l}from"./index-928c29fe.js";import"./iframe-c9c3c0b5.js";import"../sb-preview/runtime.js";import"./index-565f84a4.js";import"./_commonjsHelpers-de833af9.js";import"./index-d37d4223.js";import"./index-356e4a49.js";export{s as SyntaxHighlighter,c as createCopyToClipboardFunction,l as default};
+//# sourceMappingURL=syntaxhighlighter-NMPM6SWI-81942ca3.js.map
---
+++ storybook-react/assets/Highlight.stories-dae05a91.js
@@ -0,0 +1,136 @@
+import{q as E,j as e,n,G as r,B as o}from"./index.esm-38858419.js";const T={title:"Containers/Highlight",component:E,argTypes:{color:{options:["blue","dark-green","green","light-blue","magenta","orange","purple","yellow"],control:{type:"radio",labels:{blue:"Blauw","dark-green":"Donkergroen",green:"Groen","light-blue":"Lichtblauw",magenta:"Magenta",orange:"Oranje",purple:"Paars",yellow:"Geel"}}},children:{control:{disable:!0}}}},a={args:{children:[e.jsx(n,{children:e.jsx(r,{gridColumns:{start:3,span:8},children:e.jsx(o,{inverseColor:!0,children:"We kunnen in heel Nederland schoolpleinen creëren waar kinderen worden uitgedaagd om samen te spelen en te sporten. Buitenspelen zou een vak moeten zijn op school."})},1)},1)]}},l={args:{color:"yellow",children:[e.jsx(n,{children:e.jsx(r,{gridColumns:{start:3,span:8},children:e.jsx(o,{children:"We kunnen in heel Nederland schoolpleinen creëren waar kinderen worden uitgedaagd om samen te spelen en te sporten. Buitenspelen zou een vak moeten zijn op school."})},1)},1)]}},s={args:{color:"orange",children:[e.jsx(n,{children:e.jsx(r,{gridColumns:{start:3,span:8},children:e.jsx(o,{children:"We kunnen in heel Nederland schoolpleinen creëren waar kinderen worden uitgedaagd om samen te spelen en te sporten. Buitenspelen zou een vak moeten zijn op school."})},1)},1)]}},i={args:{color:"magenta",children:[e.jsx(n,{children:e.jsx(r,{gridColumns:{start:3,span:8},children:e.jsx(o,{inverseColor:!0,children:"We kunnen in heel Nederland schoolpleinen creëren waar kinderen worden uitgedaagd om samen te spelen en te sporten. Buitenspelen zou een vak moeten zijn op school."})},1)},1)]}},t={args:{color:"purple",children:[e.jsx(n,{children:e.jsx(r,{gridColumns:{start:3,span:8},children:e.jsx(o,{inverseColor:!0,children:"We kunnen in heel Nederland schoolpleinen creëren waar kinderen worden uitgedaagd om samen te spelen en te sporten. Buitenspelen zou een vak moeten zijn op school."})},1)},1)]}},d={args:{color:"blue",children:[e.jsx(n,{children:e.jsx(r,{gridColumns:{start:3,span:8},children:e.jsx(o,{inverseColor:!0,children:"We kunnen in heel Nederland schoolpleinen creëren waar kinderen worden uitgedaagd om samen te spelen en te sporten. Buitenspelen zou een vak moeten zijn op school."})},1)},1)]}},c={args:{color:"light-blue",children:[e.jsx(n,{children:e.jsx(r,{gridColumns:{start:3,span:8},children:e.jsx(o,{children:"We kunnen in heel Nederland schoolpleinen creëren waar kinderen worden uitgedaagd om samen te spelen en te sporten. Buitenspelen zou een vak moeten zijn op school."})},1)},1)]}},u={args:{color:"green",children:[e.jsx(n,{children:e.jsx(r,{gridColumns:{start:3,span:8},children:e.jsx(o,{children:"We kunnen in heel Nederland schoolpleinen creëren waar kinderen worden uitgedaagd om samen te spelen en te sporten. Buitenspelen zou een vak moeten zijn op school."})},1)},1)]}},p={args:{color:"dark-green",children:[e.jsx(n,{children:e.jsx(r,{gridColumns:{start:3,span:8},children:e.jsx(o,{inverseColor:!0,children:"We kunnen in heel Nederland schoolpleinen creëren waar kinderen worden uitgedaagd om samen te spelen en te sporten. Buitenspelen zou een vak moeten zijn op school."})},1)},1)]}};var g,m,h;a.parameters={...a.parameters,docs:{...(g=a.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    children: [<PageGrid key={1}>
+        <GridCell key={1} gridColumns={{
+        start: 3,
+        span: 8
+      }}>
+          <Blockquote inverseColor>
+            We kunnen in heel Nederland schoolpleinen creëren waar kinderen worden uitgedaagd om samen te spelen en te
+            sporten. Buitenspelen zou een vak moeten zijn op school.
+          </Blockquote>
+        </GridCell>
+      </PageGrid>]
+  }
+}`,...(h=(m=a.parameters)==null?void 0:m.docs)==null?void 0:h.source}}};var k,j,C;l.parameters={...l.parameters,docs:{...(k=l.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  args: {
+    color: 'yellow',
+    children: [<PageGrid key={1}>
+        <GridCell key={1} gridColumns={{
+        start: 3,
+        span: 8
+      }}>
+          <Blockquote>
+            We kunnen in heel Nederland schoolpleinen creëren waar kinderen worden uitgedaagd om samen te spelen en te
+            sporten. Buitenspelen zou een vak moeten zijn op school.
+          </Blockquote>
+        </GridCell>
+      </PageGrid>]
+  }
+}`,...(C=(j=l.parameters)==null?void 0:j.docs)==null?void 0:C.source}}};var G,w,B;s.parameters={...s.parameters,docs:{...(G=s.parameters)==null?void 0:G.docs,source:{originalSource:`{
+  args: {
+    color: 'orange',
+    children: [<PageGrid key={1}>
+        <GridCell key={1} gridColumns={{
+        start: 3,
+        span: 8
+      }}>
+          <Blockquote>
+            We kunnen in heel Nederland schoolpleinen creëren waar kinderen worden uitgedaagd om samen te spelen en te
+            sporten. Buitenspelen zou een vak moeten zijn op school.
+          </Blockquote>
+        </GridCell>
+      </PageGrid>]
+  }
+}`,...(B=(w=s.parameters)==null?void 0:w.docs)==null?void 0:B.source}}};var z,x,v;i.parameters={...i.parameters,docs:{...(z=i.parameters)==null?void 0:z.docs,source:{originalSource:`{
+  args: {
+    color: 'magenta',
+    children: [<PageGrid key={1}>
+        <GridCell key={1} gridColumns={{
+        start: 3,
+        span: 8
+      }}>
+          <Blockquote inverseColor>
+            We kunnen in heel Nederland schoolpleinen creëren waar kinderen worden uitgedaagd om samen te spelen en te
+            sporten. Buitenspelen zou een vak moeten zijn op school.
+          </Blockquote>
+        </GridCell>
+      </PageGrid>]
+  }
+}`,...(v=(x=i.parameters)==null?void 0:x.docs)==null?void 0:v.source}}};var y,P,q;t.parameters={...t.parameters,docs:{...(y=t.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    color: 'purple',
+    children: [<PageGrid key={1}>
+        <GridCell key={1} gridColumns={{
+        start: 3,
+        span: 8
+      }}>
+          <Blockquote inverseColor>
+            We kunnen in heel Nederland schoolpleinen creëren waar kinderen worden uitgedaagd om samen te spelen en te
+            sporten. Buitenspelen zou een vak moeten zijn op school.
+          </Blockquote>
+        </GridCell>
+      </PageGrid>]
+  }
+}`,...(q=(P=t.parameters)==null?void 0:P.docs)==null?void 0:q.source}}};var N,W,b;d.parameters={...d.parameters,docs:{...(N=d.parameters)==null?void 0:N.docs,source:{originalSource:`{
+  args: {
+    color: 'blue',
+    children: [<PageGrid key={1}>
+        <GridCell key={1} gridColumns={{
+        start: 3,
+        span: 8
+      }}>
+          <Blockquote inverseColor>
+            We kunnen in heel Nederland schoolpleinen creëren waar kinderen worden uitgedaagd om samen te spelen en te
+            sporten. Buitenspelen zou een vak moeten zijn op school.
+          </Blockquote>
+        </GridCell>
+      </PageGrid>]
+  }
+}`,...(b=(W=d.parameters)==null?void 0:W.docs)==null?void 0:b.source}}};var S,O,f;c.parameters={...c.parameters,docs:{...(S=c.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    color: 'light-blue',
+    children: [<PageGrid key={1}>
+        <GridCell key={1} gridColumns={{
+        start: 3,
+        span: 8
+      }}>
+          <Blockquote>
+            We kunnen in heel Nederland schoolpleinen creëren waar kinderen worden uitgedaagd om samen te spelen en te
+            sporten. Buitenspelen zou een vak moeten zijn op school.
+          </Blockquote>
+        </GridCell>
+      </PageGrid>]
+  }
+}`,...(f=(O=c.parameters)==null?void 0:O.docs)==null?void 0:f.source}}};var D,_,M;u.parameters={...u.parameters,docs:{...(D=u.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  args: {
+    color: 'green',
+    children: [<PageGrid key={1}>
+        <GridCell key={1} gridColumns={{
+        start: 3,
+        span: 8
+      }}>
+          <Blockquote>
+            We kunnen in heel Nederland schoolpleinen creëren waar kinderen worden uitgedaagd om samen te spelen en te
+            sporten. Buitenspelen zou een vak moeten zijn op school.
+          </Blockquote>
+        </GridCell>
+      </PageGrid>]
+  }
+}`,...(M=(_=u.parameters)==null?void 0:_.docs)==null?void 0:M.source}}};var H,L,Y;p.parameters={...p.parameters,docs:{...(H=p.parameters)==null?void 0:H.docs,source:{originalSource:`{
+  args: {
+    color: 'dark-green',
+    children: [<PageGrid key={1}>
+        <GridCell key={1} gridColumns={{
+        start: 3,
+        span: 8
+      }}>
+          <Blockquote inverseColor>
+            We kunnen in heel Nederland schoolpleinen creëren waar kinderen worden uitgedaagd om samen te spelen en te
+            sporten. Buitenspelen zou een vak moeten zijn op school.
+          </Blockquote>
+        </GridCell>
+      </PageGrid>]
+  }
+}`,...(Y=(L=p.parameters)==null?void 0:L.docs)==null?void 0:Y.source}}};const R=["Default","Yellow","Orange","Magenta","Purple","Blue","LightBlue","Green","DarkGreen"],F=Object.freeze(Object.defineProperty({__proto__:null,Blue:d,DarkGreen:p,Default:a,Green:u,LightBlue:c,Magenta:i,Orange:s,Purple:t,Yellow:l,__namedExportsOrder:R,default:T},Symbol.toStringTag,{value:"Module"}));export{d as B,p as D,u as G,F as H,c as L,i as M,s as O,t as P,l as Y};
+//# sourceMappingURL=Highlight.stories-dae05a91.js.map
