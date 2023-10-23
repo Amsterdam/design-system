8b9c2aeb436120905a460d7c38b77d34912cbd2c
VincentSmedinga <v.smedinga@amsterdam.nl>
2023-10-23 15:02:03 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@0ae1b8879ecb6bc24aefc241cb582ec0c49bcf59 🚀

---
+++ feature-DES-335-narrow-grid/storybook-react/assets/GridCell.stories-424f19fe.js
@@ -0,0 +1,30 @@
+import{G as k,j as e,i as f,k as j}from"./index.esm-ca61fd3a.js";const z={title:"Layout/Grid Cell",component:k,parameters:{docs:{canvas:{sourceState:"hidden"}}}},o={decorators:[S=>e.jsx(f,{children:e.jsx(j,{children:e.jsx(S,{})})})]},a={...o,args:{children:e.jsx("p",{className:"amsterdam-docs-paragraph amsterdam-docs-pink-box",children:"Deze cel beslaat 4 kolommen."}),span:4}},r={...o,args:{children:e.jsx("p",{className:"amsterdam-docs-paragraph amsterdam-docs-pink-box",children:"Deze cel start in kolom 2."}),start:2,span:2}},s={...o,args:{children:e.jsx("p",{className:"amsterdam-docs-paragraph amsterdam-docs-pink-box",children:"Deze cel heeft 3 instellingen voor de breedte."}),span:[4,6,8],fullWidth:!0}},t={...o,args:{children:e.jsx("p",{className:"amsterdam-docs-paragraph amsterdam-docs-pink-box",children:"Deze cel beslaat de volledige breedte van het grid."}),fullWidth:!0}};var n,d,c;a.parameters={...a.parameters,docs:{...(n=a.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  ...StoryTemplate,
+  args: {
+    children: <p className="amsterdam-docs-paragraph amsterdam-docs-pink-box">Deze cel beslaat 4 kolommen.</p>,
+    span: 4
+  }
+}`,...(c=(d=a.parameters)==null?void 0:d.docs)==null?void 0:c.source}}};var l,m,p;r.parameters={...r.parameters,docs:{...(l=r.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  ...StoryTemplate,
+  args: {
+    children: <p className="amsterdam-docs-paragraph amsterdam-docs-pink-box">Deze cel start in kolom 2.</p>,
+    start: 2,
+    span: 2
+  }
+}`,...(p=(m=r.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var i,h,u;s.parameters={...s.parameters,docs:{...(i=s.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  ...StoryTemplate,
+  args: {
+    children: <p className="amsterdam-docs-paragraph amsterdam-docs-pink-box">Deze cel heeft 3 instellingen voor de breedte.</p>,
+    span: [4, 6, 8],
+    fullWidth: true
+  }
+}`,...(u=(h=s.parameters)==null?void 0:h.docs)==null?void 0:u.source}}};var g,b,x;t.parameters={...t.parameters,docs:{...(g=t.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  ...StoryTemplate,
+  args: {
+    children: <p className="amsterdam-docs-paragraph amsterdam-docs-pink-box">
+        Deze cel beslaat de volledige breedte van het grid.
+      </p>,
+    fullWidth: true
+  }
+}`,...(x=(b=t.parameters)==null?void 0:b.docs)==null?void 0:x.source}}};const D=["Default","StartPosition","MultipleSizes","FullWidth"],y=Object.freeze(Object.defineProperty({__proto__:null,Default:a,FullWidth:t,MultipleSizes:s,StartPosition:r,__namedExportsOrder:D,default:z},Symbol.toStringTag,{value:"Module"}));export{t as F,y as G,s as M,r as S};
+//# sourceMappingURL=GridCell.stories-424f19fe.js.map
