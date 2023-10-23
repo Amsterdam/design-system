a3e65ad4929c2ced5090920bef985cc06a06ba6e
dlnr <niels@dlnr.dev>
2023-10-23 15:10:57 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@c2974088eec74ae383a552a32896cc10f038336c 🚀

---
+++ feature-DES-253-modal-dialog/assets/preview-0f452574.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-da261a13.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-bfe6a2dd.js"),["./DocsRenderer-NNNQARDV-bfe6a2dd.js","./iframe-da261a13.js","./index-565f84a4.js","./_commonjsHelpers-de833af9.js","./react-18-e033092d.js","./index-92728610.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-0f452574.js.map
---
+++ feature-DES-253-modal-dialog/storybook-react/assets/Dialog.docs-7a9aaa67.js
@@ -0,0 +1,6 @@
+import{j as t}from"./index.esm-d2c8d5bf.js";import{M as s,d as i,P as e,e as m}from"./index-2998e326.js";import{D as a}from"./Dialog.stories-a8a9e4bb.js";import{u as p}from"./index-a1cf9e47.js";import"./extends-98964cd2.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./iframe-61b47a05.js";import"../sb-preview/runtime.js";import"./index-932496f1.js";import"./index-d37d4223.js";import"./index-356e4a49.js";const c=`# Dialog
+`;function n(o){return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:a}),`
+`,t.jsx(i,{children:c}),`
+`,t.jsx(e,{}),`
+`,t.jsx(m,{})]})}function X(o={}){const{wrapper:r}=Object.assign({},p(),o.components);return r?t.jsx(r,Object.assign({},o,{children:t.jsx(n,o)})):n()}export{X as default};
+//# sourceMappingURL=Dialog.docs-7a9aaa67.js.map
