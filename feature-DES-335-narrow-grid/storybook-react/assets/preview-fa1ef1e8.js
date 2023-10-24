b0dc9aefad99d0320a30cad33458917190bd058a
VincentSmedinga <v.smedinga@amsterdam.nl>
2023-10-24 08:25:59 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@8964009d9f00d7fed95f4d4375e0eeef755da1bf 🚀

--- feature-DES-335-narrow-grid/storybook-react/assets/preview-fa1ef1e8.js
+++
@@ -1,2 +0,0 @@
-import{_ as e}from"./iframe-41d0d7b5.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-4e769dee.js"),["./DocsRenderer-NNNQARDV-4e769dee.js","./iframe-41d0d7b5.js","./index-76fb7be0.js","./_commonjsHelpers-de833af9.js","./react-18-063a39db.js","./index-932496f1.js","./index-c2a6e88d.js","./index-d37d4223.js","./extends-98964cd2.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
-//# sourceMappingURL=preview-fa1ef1e8.js.map
