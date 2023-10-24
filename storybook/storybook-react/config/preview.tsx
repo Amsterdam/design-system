68f8e98efe8b6a86d0184a611351d7abe2a2a648
Vincent Smedinga <v.smedinga@amsterdam.nl>
2023-10-24 10:24:11 +0200

    Configure phone, tablet and desktop viewports

--- storybook/storybook-react/config/preview.tsx
+++ storybook/storybook-react/config/preview.tsx
@@ -1,42 +1,80 @@
 import '@amsterdam/design-system-tokens/dist/root.css'
 import '@amsterdam/design-system-assets/font/index.css'
 import '@amsterdam/design-system-css/dist/index.css'
+import { ViewportMap } from '@storybook/addon-viewport/dist/models'
 
-const viewports = {
-  narrow: {
-    name: 'Breakpoint: Narrow',
+export const viewports: ViewportMap = {
+  phoneSm: {
+    name: 'Small phone',
     styles: {
-      width: '320px',
-      height: '640px',
+      height: '812px',
+      width: '375px',
     },
+    type: 'mobile',
   },
-  medium: {
-    name: 'Breakpoint: Medium',
+  phoneMd: {
+    name: 'Medium phone',
     styles: {
-      width: '576px',
-      height: '640px',
+      height: '844px',
+      width: '390px',
     },
+    type: 'mobile',
   },
-  wide: {
-    name: 'Breakpoint: Wide',
+  phoneLg: {
+    name: 'Large phone',
     styles: {
-      width: '1088px',
-      height: '640px',
+      height: '926px',
+      width: '428px',
     },
+    type: 'mobile',
   },
-  'x-wide': {
-    name: 'Max width: website',
+  tabletSm: {
+    name: 'Small tablet',
     styles: {
-      width: '1600px',
-      height: '640px',
+      height: '768px',
+      width: '1024px',
     },
+    type: 'tablet',
   },
-  'xx-wide': {
-    name: 'Max width: application',
+  tabletMd: {
+    name: 'Medium tablet',
     styles: {
-      width: '2112px',
-      height: '640px',
+      height: '800px',
+      width: '1280px',
     },
+    type: 'tablet',
+  },
+  tabletLg: {
+    name: 'Large tablet',
+    styles: {
+      height: '1024px',
+      width: '1366px',
+    },
+    type: 'tablet',
+  },
+  desktopSm: {
+    name: 'Small desktop',
+    styles: {
+      height: '900px',
+      width: '1440px',
+    },
+    type: 'desktop',
+  },
+  desktopMd: {
+    name: 'Medium desktop',
+    styles: {
+      height: '1080px',
+      width: '1920px',
+    },
+    type: 'desktop',
+  },
+  desktopLg: {
+    name: 'Large desktop',
+    styles: {
+      height: '1440px',
+      width: '2560px',
+    },
+    type: 'desktop',
   },
 }
 
