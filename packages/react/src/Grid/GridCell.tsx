b5371bc493cd01a516b2efe122c43c7c4a9600ed
Vincent Smedinga <v.smedinga@amsterdam.nl>
2023-10-25 01:05:11 +0200

    Prevent using `fullWidth` together with `span` or `start` on Grid Cell

--- packages/react/src/Grid/GridCell.tsx
+++ packages/react/src/Grid/GridCell.tsx
@@ -8,20 +8,27 @@ import { ForwardedRef, forwardRef, HTMLAttributes, PropsWithChildren } from 'rea
 import { PageGridColumnNumber, PageGridColumnNumbers } from './PageGrid'
 import { gridCellClasses } from './gridCellClasses'
 
-export interface GridCellProps extends HTMLAttributes<HTMLDivElement> {
+type GridCellFullWidthProp = {
   /** Whether the cell spans the full width of the grid. */
   fullWidth?: boolean
+  span?: never
+  start?: never
+}
+
+type GridCellColumnProps = {
+  fullWidth?: never
   /** The amount of grid columns the cell spans. */
   span?: PageGridColumnNumber | PageGridColumnNumbers
   /** The index of the grid column the cell starts at. */
   start?: PageGridColumnNumber | PageGridColumnNumbers
 }
 
+// The discriminated union and the `never` types prevent using `fullWidth` together with `span` or `start`.
+export type GridCellProps = (GridCellFullWidthProp | GridCellColumnProps) &
+  PropsWithChildren<HTMLAttributes<HTMLDivElement>>
+
 export const GridCell = forwardRef(
-  (
-    { children, className, fullWidth, span, start, ...restProps }: PropsWithChildren<GridCellProps>,
-    ref: ForwardedRef<HTMLDivElement>,
-  ) => (
+  ({ children, className, fullWidth, span, start, ...restProps }: GridCellProps, ref: ForwardedRef<HTMLDivElement>) => (
     <div
       {...restProps}
       ref={ref}
