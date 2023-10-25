21e184e7c707de4d7f7a1453f2934df85bf9b279
alimpens <37216945+alimpens@users.noreply.github.com>
2023-10-25 11:31:55 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@6c4403ecb7c5cf1eb2a630ba54ae0a84657198e3 🚀

---
+++ feature-DES-399-add-pagination-component/assets/preview-b327707f.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-1089f79b.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-bccc13a4.js"),["./DocsRenderer-NNNQARDV-bccc13a4.js","./iframe-1089f79b.js","./index-565f84a4.js","./_commonjsHelpers-de833af9.js","./react-18-e033092d.js","./index-ac89d8bc.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-b327707f.js.map
---
+++ feature-DES-399-add-pagination-component/storybook-react/assets/TopTaskLink.stories-79590591.js
@@ -0,0 +1,60 @@
+import{T as r,j as e,o as a,G as i}from"./index.esm-0f260fe4.js";const k={title:"Navigation/Top Task Link",component:r,args:{href:"/"}},n={decorators:[C=>e.jsx(a,{children:e.jsx(i,{gridColumns:4,children:e.jsx(C,{})})})],args:{href:"/",label:"Titel",description:"Omschrijving"}},s={args:{label:"Titel"},render:()=>e.jsxs(a,{children:[e.jsx(i,{gridColumns:4,children:e.jsx(r,{href:"/",label:"Melding openbare ruimte en overlast"})}),e.jsx(i,{gridColumns:4,children:e.jsx(r,{href:"/",label:"Verhuizing doorgeven"})}),e.jsx(i,{gridColumns:4,children:e.jsx(r,{href:"/",label:"Kennisgevingen en bekendmakingen"})})]}),parameters:{docs:{source:{type:"dynamic"}}}},l={args:{label:"Titel"},render:()=>e.jsxs(a,{children:[e.jsx(i,{gridColumns:3,children:e.jsx(r,{href:"/",label:"Stadsloket",description:"Locaties en openingstijden"})}),e.jsx(i,{gridColumns:3,children:e.jsx(r,{href:"/",label:"P+R",description:"Parkeren en reizen"})}),e.jsx(i,{gridColumns:3,children:e.jsx(r,{href:"/",label:"Documenten",description:"Paspoort, ID-kaart en rijbewijs"})}),e.jsx(i,{gridColumns:3,children:e.jsx(r,{href:"/",label:"Meldingen",description:"Melding openbare ruimte en overlast"})})]}),parameters:{docs:{source:{type:"dynamic"}}}};var o,d,t;n.parameters={...n.parameters,docs:{...(o=n.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  decorators: [Story => <PageGrid>
+        <GridCell gridColumns={4}>
+          <Story />
+        </GridCell>
+      </PageGrid>],
+  args: {
+    href: '/',
+    label: 'Titel',
+    description: 'Omschrijving'
+  }
+}`,...(t=(d=n.parameters)==null?void 0:d.docs)==null?void 0:t.source}}};var c,p,g;s.parameters={...s.parameters,docs:{...(c=s.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    label: 'Titel'
+  },
+  render: () => <PageGrid>
+      <GridCell gridColumns={4}>
+        <TopTaskLink href="/" label="Melding openbare ruimte en overlast" />
+      </GridCell>
+      <GridCell gridColumns={4}>
+        <TopTaskLink href="/" label="Verhuizing doorgeven" />
+      </GridCell>
+      <GridCell gridColumns={4}>
+        <TopTaskLink href="/" label="Kennisgevingen en bekendmakingen" />
+      </GridCell>
+    </PageGrid>,
+  parameters: {
+    docs: {
+      source: {
+        type: 'dynamic'
+      }
+    }
+  }
+}`,...(g=(p=s.parameters)==null?void 0:p.docs)==null?void 0:g.source}}};var m,u,h;l.parameters={...l.parameters,docs:{...(m=l.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    label: 'Titel'
+  },
+  render: () => <PageGrid>
+      <GridCell gridColumns={3}>
+        <TopTaskLink href="/" label="Stadsloket" description="Locaties en openingstijden" />
+      </GridCell>
+      <GridCell gridColumns={3}>
+        <TopTaskLink href="/" label="P+R" description="Parkeren en reizen" />
+      </GridCell>
+      <GridCell gridColumns={3}>
+        <TopTaskLink href="/" label="Documenten" description="Paspoort, ID-kaart en rijbewijs" />
+      </GridCell>
+      <GridCell gridColumns={3}>
+        <TopTaskLink href="/" label="Meldingen" description="Melding openbare ruimte en overlast" />
+      </GridCell>
+    </PageGrid>,
+  parameters: {
+    docs: {
+      source: {
+        type: 'dynamic'
+      }
+    }
+  }
+}`,...(h=(u=l.parameters)==null?void 0:u.docs)==null?void 0:h.source}}};const b=["Default","WithoutDescription","WithDescription"],T=Object.freeze(Object.defineProperty({__proto__:null,Default:n,WithDescription:l,WithoutDescription:s,__namedExportsOrder:b,default:k},Symbol.toStringTag,{value:"Module"}));export{T,s as W,l as a};
+//# sourceMappingURL=TopTaskLink.stories-79590591.js.map
