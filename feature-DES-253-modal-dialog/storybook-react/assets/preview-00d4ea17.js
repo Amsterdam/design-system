b03d999b75fb8743ba91245d57799927f32e1f25
dlnr <niels@dlnr.dev>
2023-10-25 09:58:56 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@24738c62923e8a9b06271938664ab96ee0341992 🚀

---
+++ feature-DES-253-modal-dialog/storybook-react/assets/preview-00d4ea17.js
@@ -0,0 +1,2 @@
+const o={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{canvas:{sourceState:"shown"}},options:{storySort:{order:["Buttons","Containers","Feedback","Forms","Layout","Media","Navigation","Text"]}},viewMode:"docs"};export{o as parameters};
+//# sourceMappingURL=preview-00d4ea17.js.map
