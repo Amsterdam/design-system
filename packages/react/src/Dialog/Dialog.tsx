24738c62923e8a9b06271938664ab96ee0341992
Niels Roozemond <n.roozemond@amsterdam.nl>
2023-10-25 11:56:52 +0200

    Long content story and some props

--- packages/react/src/Dialog/Dialog.tsx
+++ packages/react/src/Dialog/Dialog.tsx
@@ -5,12 +5,13 @@
 
 import { CloseIcon } from '@amsterdam/design-system-react-icons'
 import clsx from 'clsx'
-import { DialogHTMLAttributes, ForwardedRef, forwardRef, PropsWithChildren } from 'react'
-import { Button } from '../Button'
+import { DialogHTMLAttributes, ForwardedRef, forwardRef, PropsWithChildren, ReactNode } from 'react'
 import { Icon } from '../Icon'
 import { VisuallyHidden } from '../VisuallyHidden'
 
-export interface DialogProps extends PropsWithChildren<DialogHTMLAttributes<HTMLDialogElement>> {}
+export interface DialogProps extends PropsWithChildren<DialogHTMLAttributes<HTMLDialogElement>> {
+  actions?: ReactNode
+}
 
 const DialogClose = forwardRef(({ ...restProps }, ref: ForwardedRef<HTMLButtonElement>) => (
   <button {...restProps} ref={ref} className="amsterdam-dialog__close">
@@ -20,17 +21,15 @@ const DialogClose = forwardRef(({ ...restProps }, ref: ForwardedRef<HTMLButtonEl
 ))
 
 export const Dialog = forwardRef(
-  ({ children, className, ...restProps }: DialogProps, ref: ForwardedRef<HTMLDialogElement>) => (
+  ({ children, className, title, actions, ...restProps }: DialogProps, ref: ForwardedRef<HTMLDialogElement>) => (
     <dialog {...restProps} ref={ref} className={clsx('amsterdam-dialog', className)}>
       <form method="dialog" className="amsterdam-dialog__form">
         <header className="amsterdam-dialog__header">
-          <span className="amsterdam-dialog__title">Dialog</span>
+          <span className="amsterdam-dialog__title">{title}</span>
           <DialogClose />
         </header>
         <article className="amsterdam-dialog__article">{children}</article>
-        <footer className="amsterdam-dialog__footer">
-          <Button type="submit">Submit</Button>
-        </footer>
+        {actions && <footer className="amsterdam-dialog__footer">{actions}</footer>}
       </form>
     </dialog>
   ),
--- storybook/storybook-react/src/Dialog/Dialog.stories.tsx
+++ storybook/storybook-react/src/Dialog/Dialog.stories.tsx
@@ -3,15 +3,31 @@
  * Copyright (c) 2023 Gemeente Amsterdam
  */
 
-import { Button, Dialog } from '@amsterdam/design-system-react'
+import { Button, Dialog, Heading, Paragraph } from '@amsterdam/design-system-react'
 import { Meta, StoryObj } from '@storybook/react'
 
 const meta = {
   title: 'Feedback/Dialog',
   component: Dialog,
   args: {
-    children:
-      'U kunt de ingevulde antwoorden opslaan in onze beveiligde database, deze kunt u later openen om verder te gaan met invullen. Nadat u op opslaan heeft geklikt ontvangt u een mail. Met de link in deze mail kunt verder gaan met het formulier.',
+    title: 'Later verder gaan',
+    children: [
+      <Paragraph key={1}>
+        U kunt de ingevulde antwoorden opslaan in onze beveiligde database, deze kunt u later openen om verder te gaan
+        met invullen. Nadat u op opslaan heeft geklikt ontvangt u een mail. Met de link in deze mail kunt verder gaan
+        met het formulier.
+      </Paragraph>,
+    ],
+    actions: [
+      <>
+        <Button key={1} type="button" variant="tertiary">
+          Terug
+        </Button>
+        <Button key={2} type="submit">
+          Verder
+        </Button>
+      </>,
+    ],
   },
 } satisfies Meta<typeof Dialog>
 
@@ -23,7 +39,68 @@ export const Default: Story = {
   args: {
     open: true,
   },
-  render: (args) => <Dialog {...args} />,
+  render: (args) => (
+    <div style={{ minHeight: '500px' }}>
+      <Dialog {...args} />
+    </div>
+  ),
+  parameters: {
+    backgrounds: {
+      default: 'dark',
+    },
+  },
+}
+
+export const LongContent: Story = {
+  args: {
+    open: true,
+    title: 'Privacyverklaring gemeente Amsterdam',
+    children: [
+      <>
+        <Heading key={0} size="level-4">
+          Algemeen
+        </Heading>
+        <Paragraph key={1}>
+          De gemeente Amsterdam verwerkt bij de uitvoering van haar taken en verplichtingen persoonsgegevens. De manier
+          waarop de gemeente Amsterdam om gaat met persoonsgegevens is vastgelegd in het stedelijk kader verwerken
+          persoonsgegevens.
+        </Paragraph>
+        <Paragraph key={2}>
+          Deze verklaring geeft aanvullende informatie over de omgang met persoonsgegevens door de gemeente Amsterdam en
+          over uw mogelijkheden tot het uitoefenen van uw rechten met betrekking tot persoonsgegevens.
+        </Paragraph>
+        <Paragraph key={3}>
+          Meer specifieke informatie over privacy en de verwerking van persoonsgegevens door de gemeente Amsterdam kunt
+          u op de hoofdpagina vinden.
+        </Paragraph>
+        <Paragraph key={4}>
+          Vanwege nieuwe wetgeving of andere ontwikkelingen, past de gemeente regelmatig haar processen aan. Dit kunnen
+          ook wijzigingen zijn in de wijze van het verwerken van persoonsgegevens. Wij raden u daarom aan om regelmatig
+          deze pagina te bekijken. Deze pagina wordt doorlopend geactualiseerd.
+        </Paragraph>
+        <Heading key={5} size="level-4">
+          Geldende wet- en regelgeving en reikwijdte
+        </Heading>
+        <Paragraph key={6}>
+          Vanaf 25 mei 2018 is de Algemene verordening gegevensbescherming (Avg) van toepassing op alle verwerkingen van
+          persoonsgegevens. Deze Europese wetgeving heeft directe werking in Nederland. Voor die zaken die nationaal
+          geregeld moeten worden, is de Uitvoeringswet Avg in Nederland aanvullend van toepassing. Deze wetteksten kunt
+          u vinden op de website van Autoriteit Persoonsgegevens.
+        </Paragraph>
+        ,
+      </>,
+    ],
+  },
+  render: (args) => (
+    <div style={{ minHeight: '100vh' }}>
+      <Dialog {...args} />
+    </div>
+  ),
+  parameters: {
+    backgrounds: {
+      default: 'dark',
+    },
+  },
 }
 
 export const ShowDialog: Story = {
