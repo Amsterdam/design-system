80ed330deaac510a1527d08aa7713edb643bade1
Vincent Smedinga <v.smedinga@amsterdam.nl>
2023-10-26 15:12:43 +0200

    Replace Footer.Column with explicit PageGrid in Footer.Top

--- packages/react/src/Footer/Footer.tsx
+++ packages/react/src/Footer/Footer.tsx
@@ -12,32 +12,19 @@ import {
   PropsWithChildren,
   RefAttributes,
 } from 'react'
-import { GridCell, GridCellProps, PageGrid } from '../Grid'
 
 export const FooterTop = forwardRef(
   (
     { children, className, ...restProps }: PropsWithChildren<HTMLAttributes<HTMLDivElement>>,
     ref: ForwardedRef<HTMLDivElement>,
   ) => (
-    <PageGrid className={clsx('amsterdam-footer__top', className)} ref={ref} {...restProps}>
+    <div className={clsx('amsterdam-footer__top', className)} ref={ref} {...restProps}>
       {children}
-    </PageGrid>
+    </div>
   ),
 )
 
 FooterTop.displayName = 'FooterTop'
-
-export type FooterColumnProps = HTMLAttributes<HTMLDivElement> & GridCellProps
-
-export const FooterColumn = forwardRef(
-  ({ children, className, ...restProps }: PropsWithChildren<FooterColumnProps>, ref: ForwardedRef<HTMLDivElement>) => (
-    <GridCell className={clsx('amsterdam-footer__column', className)} ref={ref} {...restProps}>
-      {children}
-    </GridCell>
-  ),
-)
-
-FooterColumn.displayName = 'FooterColumn'
 
 export const FooterBottom = forwardRef(
   (
@@ -56,7 +43,6 @@ interface FooterComponent
   extends ForwardRefExoticComponent<PropsWithChildren<HTMLAttributes<HTMLElement>> & RefAttributes<HTMLElement>> {
   Top: typeof FooterTop
   Bottom: typeof FooterBottom
-  Column: typeof FooterColumn
 }
 
 export const Footer = forwardRef(
@@ -72,9 +58,7 @@ export const Footer = forwardRef(
 
 Footer.Top = FooterTop
 Footer.Bottom = FooterBottom
-Footer.Column = FooterColumn
 
 Footer.displayName = 'Footer'
 Footer.Top.displayName = 'Footer.Top'
 Footer.Bottom.displayName = 'Footer.Bottom'
-Footer.Column.displayName = 'Footer.Column'
--- storybook/storybook-react/src/Footer/Footer.stories.tsx
+++ storybook/storybook-react/src/Footer/Footer.stories.tsx
@@ -42,78 +42,80 @@ export const Default: Story = {
         <VisuallyHidden>
           <Heading>Colofon</Heading>
         </VisuallyHidden>
-        <Footer.Column span={{ narrow: 4, medium: 3, wide: 3 }}>
-          <Heading level={2} size="level-4" inverseColor>
-            Contact
-          </Heading>
-          <Paragraph size="small" inverseColor>
-            Heeft u een vraag en kunt u het antwoord niet vinden op deze site? Neem dan contact met ons op.
-          </Paragraph>
-          <UnorderedList markers={false}>
-            <li>
-              <Link href="mailto:redactie.os@amsterdam.nl" variant="inList" onBackground="dark">
-                E-mail
-              </Link>
-            </li>
-            <li>
-              <Link href="tel:+31202510333" variant="inList" onBackground="dark">
-                020 251 0333
-              </Link>
-            </li>
-          </UnorderedList>
-        </Footer.Column>
-        <Footer.Column span={{ narrow: 4, medium: 3, wide: 3 }} start={{ narrow: 1, medium: 5, wide: 5 }}>
-          <Heading level={2} size="level-4" inverseColor>
-            Panels en enquêtes
-          </Heading>
-          <Paragraph size="small" inverseColor>
-            Bent u uitgenodigd om mee te doen aan onderzoek of heeft u vragen over het panel of stadspaspanel?
-          </Paragraph>
-          <UnorderedList markers={false}>
-            <li>
-              <Link href="/" variant="inList" onBackground="dark" rel="external">
-                Meedoen aan onderzoek
-              </Link>
-            </li>
-            <li>
-              <Link href="/" variant="inList" onBackground="dark" rel="external">
-                Panel Amsterdam
-              </Link>
-            </li>
-            <li>
-              <Link href="/" variant="inList" onBackground="dark" rel="external">
-                Stadspaspanel Amsterdam
-              </Link>
-            </li>
-          </UnorderedList>
-        </Footer.Column>
-        <Footer.Column span={{ narrow: 4, medium: 3, wide: 4 }} start={{ narrow: 1, medium: 1, wide: 9 }}>
-          <Heading level={2} size="level-4" inverseColor>
-            Onderzoek en Statistiek
-          </Heading>
-          <UnorderedList markers={false}>
-            <li>
-              <Link href="/" variant="inList" onBackground="dark">
-                Over Onderzoek en Statistiek
-              </Link>
-            </li>
-            <li>
-              <Link href="/" variant="inList" onBackground="dark">
-                Veelgestelde vragen
-              </Link>
-            </li>
-            <li>
-              <Link href="/" variant="inList" onBackground="dark" rel="external">
-                Nieuwsbrief
-              </Link>
-            </li>
-            <li>
-              <Link href="/" variant="inList" onBackground="dark">
-                Vacatures
-              </Link>
-            </li>
-          </UnorderedList>
-        </Footer.Column>
+        <PageGrid>
+          <GridCell span={{ narrow: 4, medium: 3, wide: 3 }}>
+            <Heading level={2} size="level-4" inverseColor>
+              Contact
+            </Heading>
+            <Paragraph size="small" inverseColor>
+              Heeft u een vraag en kunt u het antwoord niet vinden op deze site? Neem dan contact met ons op.
+            </Paragraph>
+            <UnorderedList markers={false}>
+              <li>
+                <Link href="mailto:redactie.os@amsterdam.nl" variant="inList" onBackground="dark">
+                  E-mail
+                </Link>
+              </li>
+              <li>
+                <Link href="tel:+31202510333" variant="inList" onBackground="dark">
+                  020 251 0333
+                </Link>
+              </li>
+            </UnorderedList>
+          </GridCell>
+          <GridCell span={{ narrow: 4, medium: 3, wide: 3 }} start={{ narrow: 1, medium: 5, wide: 5 }}>
+            <Heading level={2} size="level-4" inverseColor>
+              Panels en enquêtes
+            </Heading>
+            <Paragraph size="small" inverseColor>
+              Bent u uitgenodigd om mee te doen aan onderzoek of heeft u vragen over het panel of stadspaspanel?
+            </Paragraph>
+            <UnorderedList markers={false}>
+              <li>
+                <Link href="/" variant="inList" onBackground="dark" rel="external">
+                  Meedoen aan onderzoek
+                </Link>
+              </li>
+              <li>
+                <Link href="/" variant="inList" onBackground="dark" rel="external">
+                  Panel Amsterdam
+                </Link>
+              </li>
+              <li>
+                <Link href="/" variant="inList" onBackground="dark" rel="external">
+                  Stadspaspanel Amsterdam
+                </Link>
+              </li>
+            </UnorderedList>
+          </GridCell>
+          <GridCell span={{ narrow: 4, medium: 3, wide: 4 }} start={{ narrow: 1, medium: 1, wide: 9 }}>
+            <Heading level={2} size="level-4" inverseColor>
+              Onderzoek en Statistiek
+            </Heading>
+            <UnorderedList markers={false}>
+              <li>
+                <Link href="/" variant="inList" onBackground="dark">
+                  Over Onderzoek en Statistiek
+                </Link>
+              </li>
+              <li>
+                <Link href="/" variant="inList" onBackground="dark">
+                  Veelgestelde vragen
+                </Link>
+              </li>
+              <li>
+                <Link href="/" variant="inList" onBackground="dark" rel="external">
+                  Nieuwsbrief
+                </Link>
+              </li>
+              <li>
+                <Link href="/" variant="inList" onBackground="dark">
+                  Vacatures
+                </Link>
+              </li>
+            </UnorderedList>
+          </GridCell>
+        </PageGrid>
       </Footer.Top>,
       <Footer.Bottom key="footer-bottom">
         <VisuallyHidden>
