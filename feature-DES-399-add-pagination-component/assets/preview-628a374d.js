e9e3a270c76d74eeae92a9ffe82660545766f8f8
alimpens <37216945+alimpens@users.noreply.github.com>
2023-10-26 15:32:09 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@3cd9fb0cce0786eb010cb3f2985310fbd68e3206 🚀

---
+++ feature-DES-399-add-pagination-component/assets/preview-628a374d.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-e80b5d3e.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-f12de318.js"),["./DocsRenderer-NNNQARDV-f12de318.js","./iframe-e80b5d3e.js","./index-565f84a4.js","./_commonjsHelpers-de833af9.js","./react-18-e033092d.js","./index-bb702d1c.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-628a374d.js.map
