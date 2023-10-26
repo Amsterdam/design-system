e6724ed30bc4010882ba7209e4157fca81572d90
Vincent Smedinga <v.smedinga@amsterdam.nl>
2023-10-26 11:48:40 +0200

    Update unit tests

--- packages/react/src/Grid/gridColumnClassNames.test.ts
+++ packages/react/src/Grid/gridColumnClassNames.test.ts
@@ -6,9 +6,49 @@ describe('gridColumnClassNames', () => {
     expect(result).toEqual([])
   })
 
+  it('should return an empty array when undefined parameters are provided', () => {
+    const result = gridColumnClassNames(undefined, undefined)
+    expect(result).toEqual([])
+  })
+
   it('should generate class names for single number values for start and span', () => {
-    const result = gridColumnClassNames(3, 5)
-    expect(result).toEqual(['amsterdam-grid-span-3', 'amsterdam-grid-start-5'])
+    const result = gridColumnClassNames(4, 2)
+    expect(result).toEqual(['amsterdam-grid-span-4', 'amsterdam-grid-start-2'])
+  })
+
+  it('should generate class names for a single number value for start', () => {
+    const result = gridColumnClassNames(8, undefined)
+    expect(result).toEqual(['amsterdam-grid-span-8'])
+  })
+
+  it('should generate class names for an invalid number value for start', () => {
+    const result = gridColumnClassNames(13, undefined)
+    expect(result).toEqual(['amsterdam-grid-span-13'])
+  })
+
+  it('should generate class names for a single number value for span', () => {
+    const result = gridColumnClassNames(undefined, 6)
+    expect(result).toEqual(['amsterdam-grid-start-6'])
+  })
+
+  it('should generate class names for a single number for span and array values for start', () => {
+    const result = gridColumnClassNames(8, { narrow: 2, medium: 4, wide: 6 })
+    expect(result).toEqual([
+      'amsterdam-grid-span-8',
+      'amsterdam-grid-start-2',
+      'amsterdam-medium-grid-start-4',
+      'amsterdam-wide-grid-start-6',
+    ])
+  })
+
+  it('should generate class names for a single number for span and array values for start', () => {
+    const result = gridColumnClassNames({ narrow: 3, medium: 5, wide: 7 }, 2)
+    expect(result).toEqual([
+      'amsterdam-grid-span-3',
+      'amsterdam-medium-grid-span-5',
+      'amsterdam-wide-grid-span-7',
+      'amsterdam-grid-start-2',
+    ])
   })
 
   it('should generate class names for an array of three numbers for start and span', () => {
@@ -21,25 +61,5 @@ describe('gridColumnClassNames', () => {
       'amsterdam-medium-grid-start-3',
       'amsterdam-wide-grid-start-5',
     ])
-  })
-
-  it('should generate class names for a mix of single number and array values for start and span', () => {
-    const result = gridColumnClassNames(8, { narrow: 2, medium: 4, wide: 6 })
-    expect(result).toEqual([
-      'amsterdam-grid-span-8',
-      'amsterdam-grid-start-2',
-      'amsterdam-medium-grid-start-4',
-      'amsterdam-wide-grid-start-6',
-    ])
-  })
-
-  it('should generate class names for a single number value for start', () => {
-    const result = gridColumnClassNames(8, undefined)
-    expect(result).toEqual(['amsterdam-grid-span-8'])
-  })
-
-  it('should generate class names for an array of three numbers for span', () => {
-    const result = gridColumnClassNames(undefined, { narrow: 2, medium: 4, wide: 6 })
-    expect(result).toEqual(['amsterdam-grid-start-2', 'amsterdam-medium-grid-start-4', 'amsterdam-wide-grid-start-6'])
   })
 })
