d572109198e5a4870dc67b2595f86c2f7966f526
alimpens <37216945+alimpens@users.noreply.github.com>
2023-10-26 08:22:33 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@bcf7ab374fe0f293078d74d774901aaf6bc31aed 🚀

---
+++ feature-DES-335-narrow-grid/storybook-react/assets/Screen.stories-2443ff7a.js
@@ -0,0 +1,180 @@
+import{i as P,j as e,k as c,G as r,l as y,m as n,n as M,o as G,H,B as N,p as s,V as z,q as o,P as g,U as m,L as a}from"./index.esm-988b8eb9.js";const E={title:"Layout/Screen",component:P,parameters:{docs:{canvas:{sourceState:"hidden"}},layout:"fullscreen"}},i={args:{children:e.jsx("p",{className:"amsterdam-docs-paragraph amsterdam-docs-pink-box",children:"Ik pas in het scherm."})}},l={args:{children:[e.jsxs(c,{style:{marginBlockEnd:"2rem"},children:[e.jsx(r,{fullWidth:!0,children:e.jsx("header",{children:e.jsxs("div",{style:{display:"flex",flexDirection:"row",gap:"2rem",alignItems:"center"},children:[e.jsx("div",{style:{flex:2},children:e.jsx("img",{alt:"Logo van de gemeente Amsterdam",className:"amsterdam-docs-image",height:40,src:"https://amsterdam.nl/publish/pages/963333/basis_variant_1_rgb_1.png",width:92})}),e.jsx(y,{style:{flex:2},children:"Titel"}),e.jsxs(n,{style:{flex:8,justifyContent:"flex-end"},children:[e.jsx(n.Link,{href:"#",children:"English"}),e.jsx(n.Link,{href:"#",children:"Contact"}),e.jsx(n.Link,{href:"#",icon:M,children:"Mijn Amsterdam"}),e.jsx(n.Button,{icon:G,children:"Menu"})]})]})})}),e.jsx(r,{span:[4,8,4],children:e.jsx("figure",{className:"amsterdam-docs-figure",children:e.jsx("img",{alt:"Voorbeeld van een afbeelding",src:"https://picsum.photos/1024/576?random=1"})})}),e.jsx(r,{span:4,children:e.jsx("figure",{className:"amsterdam-docs-figure",children:e.jsx("img",{alt:"Voorbeeld van een afbeelding",src:"https://picsum.photos/1024/576?random=2"})})}),e.jsx(r,{span:4,children:e.jsx("figure",{className:"amsterdam-docs-figure",children:e.jsx("img",{alt:"Voorbeeld van een afbeelding",src:"https://picsum.photos/1024/576?random=3"})})})]},Math.random()),e.jsx(H,{color:"purple",style:{marginBlockEnd:"2rem"},children:e.jsxs(c,{children:[e.jsx(r,{span:[4,3,3],children:e.jsx(N,{inverseColor:!0,children:"Er is maar één moment dat je op tijd kunt komen. Ben je er niet, dan ben je óf te vroeg, óf te laat."})}),e.jsx(r,{start:[1,4,5],span:[4,5,8],children:e.jsx("figure",{className:"amsterdam-docs-figure",children:e.jsx("img",{alt:"Voorbeeld van een afbeelding",src:"https://picsum.photos/1600/900?random=3"})})})]})},Math.random()),e.jsxs(c,{style:{marginBlockEnd:"2rem"},children:[e.jsx(r,{span:[4,4,6],children:e.jsx("div",{style:{backgroundColor:"#BED200",padding:"2rem",textAlign:"center"},children:e.jsx("p",{className:"amsterdam-docs-paragraph",children:"Elk de helft"})})}),e.jsx(r,{span:[4,4,6],children:e.jsx("div",{style:{backgroundColor:"#BED200",padding:"2rem",textAlign:"center"},children:e.jsx("p",{className:"amsterdam-docs-paragraph",children:"Elk de helft"})})})]},Math.random()),e.jsx(s,{children:e.jsxs(s.Top,{children:[e.jsx(z,{children:e.jsx(o,{children:"Colofon"})}),e.jsxs(s.Column,{span:[4,3,3],children:[e.jsx(o,{level:2,size:"level-4",inverseColor:!0,children:"Contact"}),e.jsx(g,{size:"small",inverseColor:!0,children:"Heeft u een vraag en kunt u het antwoord niet vinden op deze site? Neem dan contact met ons op."}),e.jsxs(m,{markers:!1,children:[e.jsx("li",{children:e.jsx(a,{href:"mailto:redactie.os@amsterdam.nl",variant:"inList",onBackground:"dark",children:"E-mail"})}),e.jsx("li",{children:e.jsx(a,{href:"tel:+31202510333",variant:"inList",onBackground:"dark",children:"020 251 0333"})})]})]}),e.jsxs(s.Column,{span:[4,3,3],start:[1,5,5],children:[e.jsx(o,{level:2,size:"level-4",inverseColor:!0,children:"Panels en enquêtes"}),e.jsx(g,{size:"small",inverseColor:!0,children:"Bent u uitgenodigd om mee te doen aan onderzoek of heeft u vragen over het panel of stadspaspanel?"}),e.jsxs(m,{markers:!1,children:[e.jsx("li",{children:e.jsx(a,{href:"/",variant:"inList",onBackground:"dark",rel:"external",children:"Meedoen aan onderzoek"})}),e.jsx("li",{children:e.jsx(a,{href:"/",variant:"inList",onBackground:"dark",rel:"external",children:"Panel Amsterdam"})}),e.jsx("li",{children:e.jsx(a,{href:"/",variant:"inList",onBackground:"dark",rel:"external",children:"Stadspaspanel Amsterdam"})})]})]}),e.jsxs(s.Column,{span:[4,3,4],start:[1,1,9],children:[e.jsx(o,{level:2,size:"level-4",inverseColor:!0,children:"Onderzoek en Statistiek"}),e.jsxs(m,{markers:!1,children:[e.jsx("li",{children:e.jsx(a,{href:"/",variant:"inList",onBackground:"dark",children:"Over Onderzoek en Statistiek"})}),e.jsx("li",{children:e.jsx(a,{href:"/",variant:"inList",onBackground:"dark",children:"Veelgestelde vragen"})}),e.jsx("li",{children:e.jsx(a,{href:"/",variant:"inList",onBackground:"dark",rel:"external",children:"Nieuwsbrief"})}),e.jsx("li",{children:e.jsx(a,{href:"/",variant:"inList",onBackground:"dark",children:"Vacatures"})})]})]})]})},Math.random())]}},t={args:{children:e.jsx("p",{className:"amsterdam-docs-paragraph amsterdam-docs-pink-box",children:"Overzichtspagina nog te maken."})}},d={args:{children:e.jsx("p",{className:"amsterdam-docs-paragraph amsterdam-docs-pink-box",children:"Detailpagina nog te maken."})}};var h,p,u;i.parameters={...i.parameters,docs:{...(h=i.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    children: <p className="amsterdam-docs-paragraph amsterdam-docs-pink-box">Ik pas in het scherm.</p>
+  }
+}`,...(u=(p=i.parameters)==null?void 0:p.docs)==null?void 0:u.source}}};var k,x,f;l.parameters={...l.parameters,docs:{...(k=l.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  args: {
+    children: [<PageGrid key={Math.random()} style={{
+      marginBlockEnd: '2rem'
+    }}>
+        <GridCell fullWidth>
+          <header>
+            <div style={{
+            display: 'flex',
+            flexDirection: 'row',
+            gap: '2rem',
+            alignItems: 'center'
+          }}>
+              <div style={{
+              flex: 2
+            }}>
+                <img alt="Logo van de gemeente Amsterdam" className="amsterdam-docs-image" height={40} src="https://amsterdam.nl/publish/pages/963333/basis_variant_1_rgb_1.png" width={92} />
+              </div>
+              <PageHeading style={{
+              flex: 2
+            }}>Titel</PageHeading>
+              <PageMenu style={{
+              flex: 8,
+              justifyContent: 'flex-end'
+            }}>
+                <PageMenu.Link href="#">English</PageMenu.Link>
+                <PageMenu.Link href="#">Contact</PageMenu.Link>
+                <PageMenu.Link href="#" icon={LoginIcon}>
+                  Mijn Amsterdam
+                </PageMenu.Link>
+                <PageMenu.Button icon={MenuIcon}>Menu</PageMenu.Button>
+              </PageMenu>
+            </div>
+          </header>
+        </GridCell>
+        <GridCell span={[4, 8, 4]}>
+          <figure className="amsterdam-docs-figure">
+            <img alt="Voorbeeld van een afbeelding" src="https://picsum.photos/1024/576?random=1" />
+          </figure>
+        </GridCell>
+        <GridCell span={4}>
+          <figure className="amsterdam-docs-figure">
+            <img alt="Voorbeeld van een afbeelding" src="https://picsum.photos/1024/576?random=2" />
+          </figure>
+        </GridCell>
+        <GridCell span={4}>
+          <figure className="amsterdam-docs-figure">
+            <img alt="Voorbeeld van een afbeelding" src="https://picsum.photos/1024/576?random=3" />
+          </figure>
+        </GridCell>
+      </PageGrid>, <Highlight color="purple" key={Math.random()} style={{
+      marginBlockEnd: '2rem'
+    }}>
+        <PageGrid>
+          <GridCell span={[4, 3, 3]}>
+            <Blockquote inverseColor>
+              Er is maar één moment dat je op tijd kunt komen. Ben je er niet, dan ben je óf te vroeg, óf te laat.
+            </Blockquote>
+          </GridCell>
+          <GridCell start={[1, 4, 5]} span={[4, 5, 8]}>
+            <figure className="amsterdam-docs-figure">
+              <img alt="Voorbeeld van een afbeelding" src="https://picsum.photos/1600/900?random=3" />
+            </figure>
+          </GridCell>
+        </PageGrid>
+      </Highlight>, <PageGrid key={Math.random()} style={{
+      marginBlockEnd: '2rem'
+    }}>
+        <GridCell span={[4, 4, 6]}>
+          <div style={{
+          backgroundColor: '#BED200',
+          padding: '2rem',
+          textAlign: 'center'
+        }}>
+            <p className="amsterdam-docs-paragraph">Elk de helft</p>
+          </div>
+        </GridCell>
+        <GridCell span={[4, 4, 6]}>
+          <div style={{
+          backgroundColor: '#BED200',
+          padding: '2rem',
+          textAlign: 'center'
+        }}>
+            <p className="amsterdam-docs-paragraph">Elk de helft</p>
+          </div>
+        </GridCell>
+      </PageGrid>, <Footer key={Math.random()}>
+        <Footer.Top>
+          <VisuallyHidden>
+            <Heading>Colofon</Heading>
+          </VisuallyHidden>
+          <Footer.Column span={[4, 3, 3]}>
+            <Heading level={2} size="level-4" inverseColor>
+              Contact
+            </Heading>
+            <Paragraph size="small" inverseColor>
+              Heeft u een vraag en kunt u het antwoord niet vinden op deze site? Neem dan contact met ons op.
+            </Paragraph>
+            <UnorderedList markers={false}>
+              <li>
+                <Link href="mailto:redactie.os@amsterdam.nl" variant="inList" onBackground="dark">
+                  E-mail
+                </Link>
+              </li>
+              <li>
+                <Link href="tel:+31202510333" variant="inList" onBackground="dark">
+                  020 251 0333
+                </Link>
+              </li>
+            </UnorderedList>
+          </Footer.Column>
+          <Footer.Column span={[4, 3, 3]} start={[1, 5, 5]}>
+            <Heading level={2} size="level-4" inverseColor>
+              Panels en enquêtes
+            </Heading>
+            <Paragraph size="small" inverseColor>
+              Bent u uitgenodigd om mee te doen aan onderzoek of heeft u vragen over het panel of stadspaspanel?
+            </Paragraph>
+            <UnorderedList markers={false}>
+              <li>
+                <Link href="/" variant="inList" onBackground="dark" rel="external">
+                  Meedoen aan onderzoek
+                </Link>
+              </li>
+              <li>
+                <Link href="/" variant="inList" onBackground="dark" rel="external">
+                  Panel Amsterdam
+                </Link>
+              </li>
+              <li>
+                <Link href="/" variant="inList" onBackground="dark" rel="external">
+                  Stadspaspanel Amsterdam
+                </Link>
+              </li>
+            </UnorderedList>
+          </Footer.Column>
+          <Footer.Column span={[4, 3, 4]} start={[1, 1, 9]}>
+            <Heading level={2} size="level-4" inverseColor>
+              Onderzoek en Statistiek
+            </Heading>
+            <UnorderedList markers={false}>
+              <li>
+                <Link href="/" variant="inList" onBackground="dark">
+                  Over Onderzoek en Statistiek
+                </Link>
+              </li>
+              <li>
+                <Link href="/" variant="inList" onBackground="dark">
+                  Veelgestelde vragen
+                </Link>
+              </li>
+              <li>
+                <Link href="/" variant="inList" onBackground="dark" rel="external">
+                  Nieuwsbrief
+                </Link>
+              </li>
+              <li>
+                <Link href="/" variant="inList" onBackground="dark">
+                  Vacatures
+                </Link>
+              </li>
+            </UnorderedList>
+          </Footer.Column>
+        </Footer.Top>
+      </Footer>]
+  }
+}`,...(f=(x=l.parameters)==null?void 0:x.docs)==null?void 0:f.source}}};var v,j,L;t.parameters={...t.parameters,docs:{...(v=t.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    children: <p className="amsterdam-docs-paragraph amsterdam-docs-pink-box">Overzichtspagina nog te maken.</p>
+  }
+}`,...(L=(j=t.parameters)==null?void 0:j.docs)==null?void 0:L.source}}};var C,b,B;d.parameters={...d.parameters,docs:{...(C=d.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  args: {
+    children: <p className="amsterdam-docs-paragraph amsterdam-docs-pink-box">Detailpagina nog te maken.</p>
+  }
+}`,...(B=(b=d.parameters)==null?void 0:b.docs)==null?void 0:B.source}}};const S=["Default","HomePage","OverviewPage","ArticlePage"],A=Object.freeze(Object.defineProperty({__proto__:null,ArticlePage:d,Default:i,HomePage:l,OverviewPage:t,__namedExportsOrder:S,default:E},Symbol.toStringTag,{value:"Module"}));export{d as A,l as H,t as O,A as S};
+//# sourceMappingURL=Screen.stories-2443ff7a.js.map
