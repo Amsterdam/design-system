d207141018193e240aa6b072d4a1bba0b2ad8fc8
VincentSmedinga <v.smedinga@amsterdam.nl>
2023-10-26 14:12:43 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@f9379175eefdfc5739da11eae5349cc54496ba4b 🚀

---
+++ feature-DES-335-narrow-grid/storybook-react/assets/GridCell.docs-fecbe2ec.js
@@ -0,0 +1,35 @@
+import{j as e}from"./index.esm-0adeae5b.js";import{M as i,d as a,P as d,f as r}from"./index-79260585.js";import{G as l,S as c,M as h,F as m}from"./GridCell.stories-a930842b.js";import{u as s}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./extends-98964cd2.js";import"./iframe-3175cee6.js";import"../sb-preview/runtime.js";import"./index-932496f1.js";import"./index-d37d4223.js";import"./index-356e4a49.js";const p=`# Grid cell
+
+Beslaat een aantal kolommen in een [Page Grid](?path=/docs/react_layout-page-grid--docs).
+
+## Gebruik
+
+Gebruik het \`GridCell\` component om een grid in cellen te verdelen.
+Op de cel kun je aangeven hoeveel kolommen van het grid het element beslaat.
+In elke cel plaats je vervolgens een component of element.
+`;function o(t){const n=Object.assign({p:"p",code:"code",h3:"h3",h2:"h2",ol:"ol",li:"li"},s(),t.components);return e.jsxs(e.Fragment,{children:[e.jsx(i,{of:l}),`
+`,e.jsx(a,{children:p}),`
+`,e.jsxs(n.p,{children:[`Een cel beslaat standaard 1 kolom in het grid.
+Om een cel meer kolommen te laten beslaan, gebruik je de `,e.jsx(n.code,{children:"span"})," prop."]}),`
+`,e.jsx(d,{}),`
+`,e.jsx(n.h3,{id:"startpositie",children:"Startpositie"}),`
+`,e.jsxs(n.p,{children:[`Elke cel start automatisch in de eerstvolgende beschikbare positie in het grid.
+Je kunt de startpositie van een cel aanpassen met de `,e.jsx(n.code,{children:"start"}),` prop.
+In dit geval `,e.jsx(n.code,{children:"start={2}"}),":"]}),`
+`,e.jsx(r,{of:c}),`
+`,e.jsx(n.h3,{id:"verschillende-breedtes",children:"Verschillende breedtes"}),`
+`,e.jsxs(n.p,{children:[`Je kunt het aantal kolommen dat een cel beslaat laten afhangen van de vensterbreedte.
+Gebruik hiervoor de `,e.jsx(n.code,{children:"span"})," prop met 3 waarden voor respectievelijk smalle, middelbrede en brede vensters."]}),`
+`,e.jsxs(n.p,{children:["Via ",e.jsx(n.code,{children:"span={[4, 6, 8]}"})," is deze cel 4 van de 4 kolommen breed op smalle vensters, 6 van de 8 op middelbrede en 8 van de 12 op brede vensters:"]}),`
+`,e.jsx(r,{of:h}),`
+`,e.jsxs(n.p,{children:[`Het kan zelfs voorkomen dat ook de startpositie van een cel afhangt van de vensterbreedte.
+Gebruik hiervoor de `,e.jsx(n.code,{children:"start"})," prop met 3 waarden."]}),`
+`,e.jsx(n.h3,{id:"volledige-breedte",children:"Volledige breedte"}),`
+`,e.jsxs(n.p,{children:["Om de cel de volledige breedte te geven – of het grid nu 4, 8, of 12 kolommen heeft – gebruik je de ",e.jsx(n.code,{children:"fullWidth"})," prop."]}),`
+`,e.jsx(r,{of:m}),`
+`,e.jsx(n.h2,{id:"richtlijnen",children:"Richtlijnen"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Zorg ervoor dat het aantal kolommen dat je aan een cel toekent past bij het aantal kolommen van het grid voor de betreffende vensterbreedte. Datzelfde geldt voor het starten van een cel in een latere kolom. Als het totaal van beide waarden is te groot is voegt de browser kolommen toe aan het grid. Dit is niet toegestaan."}),`
+`,e.jsxs(n.li,{children:["Gebruik de ",e.jsx(n.code,{children:"fullWidth"})," prop niet tegelijk met de ",e.jsx(n.code,{children:"span"})," of ",e.jsx(n.code,{children:"start"})," props."]}),`
+`]})]})}function D(t={}){const{wrapper:n}=Object.assign({},s(),t.components);return n?e.jsx(n,Object.assign({},t,{children:e.jsx(o,t)})):o(t)}export{D as default};
+//# sourceMappingURL=GridCell.docs-fecbe2ec.js.map
---
+++ feature-DES-335-narrow-grid/storybook-react/assets/preview-4f8f226c.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-3175cee6.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-69790480.js"),["./DocsRenderer-NNNQARDV-69790480.js","./iframe-3175cee6.js","./index-76fb7be0.js","./_commonjsHelpers-de833af9.js","./react-18-063a39db.js","./index-932496f1.js","./index-79260585.js","./index-d37d4223.js","./extends-98964cd2.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-4f8f226c.js.map
