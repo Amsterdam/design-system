5145925f046e97b1299f64f19988ac3511c586a8
Vincent Smedinga <v.smedinga@amsterdam.nl>
2023-10-26 15:15:13 +0200

    Fix source order of grid class names and improve naming

--- packages/react/src/Grid/gridCellClasses.ts
+++ packages/react/src/Grid/gridCellClasses.ts
@@ -8,24 +8,24 @@ export const gridCellClasses = (span?: GridCellProps['span'], start?: GridCellPr
   const classes = [] as string[]
 
   if (typeof span === 'number') {
-    classes.push(`amsterdam-grid-span-${span}`)
+    classes.push(`amsterdam-grid-cell-span-${span}`)
   } else if (span) {
     const { narrow, medium, wide } = span
     classes.push(
-      `amsterdam-grid-span-${narrow}`,
-      `amsterdam-medium-grid-span-${medium}`,
-      `amsterdam-wide-grid-span-${wide}`,
+      `amsterdam-grid-cell-span-${narrow}`,
+      `amsterdam-grid-cell-span-${medium}-medium`,
+      `amsterdam-grid-cell-span-${wide}-wide`,
     )
   }
 
   if (typeof start === 'number') {
-    classes.push(`amsterdam-grid-start-${start}`)
+    classes.push(`amsterdam-grid-cell-start-${start}`)
   } else if (start) {
     const { narrow, medium, wide } = start
     classes.push(
-      `amsterdam-grid-start-${narrow}`,
-      `amsterdam-medium-grid-start-${medium}`,
-      `amsterdam-wide-grid-start-${wide}`,
+      `amsterdam-grid-cell-start-${narrow}`,
+      `amsterdam-grid-cell-start-${medium}-medium`,
+      `amsterdam-grid-cell-start-${wide}-wide`,
     )
   }
 
