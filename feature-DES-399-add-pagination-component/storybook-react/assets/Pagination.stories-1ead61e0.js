7a069f724db24f483f6d977a19b10e882bffde1e
VincentSmedinga <VincentSmedinga@users.noreply.github.com>
2023-10-27 14:36:29 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@cd2906387092231d8309447636ea8c77e904b30b 🚀

--- feature-DES-399-add-pagination-component/storybook-react/assets/Pagination.stories-1ead61e0.js
+++
@@ -1,5 +0,0 @@
-import{i as r}from"./index.esm-7cb3a135.js";/**
- * @license EUPL-1.2+
- * Copyright (c) 2023 Gemeente Amsterdam
- */const n={title:"Navigation/Pagination",component:r,args:{collectionSize:100,page:1,pageSize:10,maxVisiblePages:7},argTypes:{onPageChange:{action:"page changed"}}},e={};var a,o,t;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:"{}",...(t=(o=e.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};const i=["Default"],c=Object.freeze(Object.defineProperty({__proto__:null,Default:e,__namedExportsOrder:i,default:n},Symbol.toStringTag,{value:"Module"}));export{c as P};
-//# sourceMappingURL=Pagination.stories-1ead61e0.js.map
--- storybook-react/assets/preview-74c1e137.js
+++
@@ -1,2 +0,0 @@
-import{_ as e}from"./iframe-8e1c6d92.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-be56e9b0.js"),["./DocsRenderer-NNNQARDV-be56e9b0.js","./iframe-8e1c6d92.js","./index-76fb7be0.js","./_commonjsHelpers-de833af9.js","./react-18-063a39db.js","./index-932496f1.js","./index-d19ccee2.js","./index-d37d4223.js","./extends-98964cd2.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
-//# sourceMappingURL=preview-74c1e137.js.map
