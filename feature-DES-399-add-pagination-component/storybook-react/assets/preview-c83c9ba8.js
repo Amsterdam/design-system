bc61720a905ceff93f63257333aa6bc14315093f
VincentSmedinga <VincentSmedinga@users.noreply.github.com>
2023-10-27 13:58:48 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@9762155b16367f707d82380552c536494a6b6fa9 🚀

--- feature-DES-399-add-pagination-component/storybook-react/assets/preview-c83c9ba8.js
+++
@@ -1,2 +0,0 @@
-const o={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{canvas:{sourceState:"shown"}},options:{storySort:{order:["Buttons","Containers","Feedback","Forms","Layout","Media","Navigation","Text"]}},viewMode:"docs"};export{o as parameters};
-//# sourceMappingURL=preview-c83c9ba8.js.map
--- feature-DES-399-add-pagination-component/storybook-react/assets/preview-f7ffa658.js
+++
@@ -1,2 +0,0 @@
-import{_ as e}from"./iframe-bcba8401.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-7e70028c.js"),["./DocsRenderer-NNNQARDV-7e70028c.js","./iframe-bcba8401.js","./index-76fb7be0.js","./_commonjsHelpers-de833af9.js","./react-18-063a39db.js","./index-932496f1.js","./index-360a6233.js","./index-d37d4223.js","./extends-98964cd2.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
-//# sourceMappingURL=preview-f7ffa658.js.map
