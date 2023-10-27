92bd345455f1f132ef263cce3ae0a6b92ea50e5b
alimpens <37216945+alimpens@users.noreply.github.com>
2023-10-27 11:04:43 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@db56c87199645012b17a5809fcd7192d067ba04f 🚀

--- task-simplify-highlight-story/storybook-react/assets/preview-efe26605.js
+++
@@ -1,2 +0,0 @@
-import{_ as e}from"./iframe-ad3c55bd.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-cfb70472.js"),["./DocsRenderer-NNNQARDV-cfb70472.js","./iframe-ad3c55bd.js","./index-76fb7be0.js","./_commonjsHelpers-de833af9.js","./react-18-063a39db.js","./index-932496f1.js","./index-e984bfbe.js","./index-d37d4223.js","./extends-98964cd2.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
-//# sourceMappingURL=preview-efe26605.js.map
