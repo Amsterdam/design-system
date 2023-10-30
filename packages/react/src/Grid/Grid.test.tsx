c36d133cb2526be19a34254ddee0737c9145b460
Vincent Smedinga <v.smedinga@amsterdam.nl>
2023-10-30 15:14:15 +0100

    Remove unnecessary whitespace in tests file

--- packages/react/src/Grid/Grid.test.tsx
+++ packages/react/src/Grid/Grid.test.tsx
@@ -6,38 +6,28 @@ import '@testing-library/jest-dom'
 describe('Grid', () => {
   it('renders', () => {
     const { container } = render(<Grid />)
-
     const component = container.querySelector(':only-child')
-
     expect(component).toBeInTheDocument()
     expect(component).toBeVisible()
   })
 
   it('renders a design system BEM class name', () => {
     const { container } = render(<Grid />)
-
     const component = container.querySelector(':only-child')
-
     expect(component).toHaveClass('amsterdam-grid')
   })
 
   it('can have a additional class name', () => {
     const { container } = render(<Grid className="extra" />)
-
     const component = container.querySelector(':only-child')
-
     expect(component).toHaveClass('extra')
-
     expect(component).toHaveClass('amsterdam-grid')
   })
 
   it('supports ForwardRef in React', () => {
     const ref = createRef<HTMLDivElement>()
-
     const { container } = render(<Grid ref={ref} />)
-
     const component = container.querySelector(':only-child')
-
     expect(ref.current).toBe(component)
   })
 })
