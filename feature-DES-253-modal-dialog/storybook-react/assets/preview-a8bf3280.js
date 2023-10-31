77b13bb626aa94bf55ffaaef84068c173719f13c
VincentSmedinga <VincentSmedinga@users.noreply.github.com>
2023-10-31 13:39:28 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@a5d2333f79b737f4a3bf5dbc5e52b8154aacdb82 🚀

--- feature-DES-253-modal-dialog/storybook-react/assets/preview-a8bf3280.js
+++
@@ -1,2 +0,0 @@
-const o={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{canvas:{sourceState:"shown"}},options:{storySort:{order:["Buttons","Containers","Feedback","Forms","Layout","Media","Navigation","Text"]}},viewMode:"docs"};export{o as parameters};
-//# sourceMappingURL=preview-a8bf3280.js.map
