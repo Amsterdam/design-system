6391e5cfd54b02095d6611a9f6d958de824f7068
alimpens <37216945+alimpens@users.noreply.github.com>
2023-11-07 08:04:04 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@bda7726a91cf40ef4cb11de467cbb86f1b35ea0c 🚀

---
+++ feature-DES-399-add-pagination-component/storybook-react/assets/VisuallyHidden.docs-177a382c.js
@@ -0,0 +1,10 @@
+import{j as e}from"./index.esm-7cb3a135.js";import{M as i,d as t,P as s,e as a}from"./index-f4506e5b.js";import{V as d}from"./VisuallyHidden.stories-d1767f80.js";import{u as m}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./extends-98964cd2.js";import"./iframe-6030862a.js";import"../sb-preview/runtime.js";import"./index-932496f1.js";import"./index-d37d4223.js";import"./index-356e4a49.js";const u=`# Visually Hidden
+
+Het Visually Hidden component kan gebruikt worden om inhoud te verbergen voor ziende gebruikers, maar beschikbaar te houden voor niet-visuele _user agents_, zoals schermlezers.
+
+Let op: In de overgrote meerderheid van de gevallen moet inhoud die visueel beschikbaar is, ook beschikbaar zijn voor niet-visuele _user agents_, en omgekeerd. Uitgebreide aanwijzingen of instructies die alleen door niet-visuele _user agents_ worden gelezen, doen al snel meer kwaad dan goed.
+`;function o(n){return e.jsxs(e.Fragment,{children:[e.jsx(i,{of:d}),`
+`,e.jsx(t,{children:u}),`
+`,e.jsx(s,{}),`
+`,e.jsx(a,{})]})}function _(n={}){const{wrapper:r}=Object.assign({},m(),n.components);return r?e.jsx(r,Object.assign({},n,{children:e.jsx(o,n)})):o()}export{_ as default};
+//# sourceMappingURL=VisuallyHidden.docs-177a382c.js.map
