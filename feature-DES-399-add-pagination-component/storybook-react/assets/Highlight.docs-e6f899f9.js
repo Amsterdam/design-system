16dd6f54726b3672e704bf4af80ea541946b5ff9
alimpens <37216945+alimpens@users.noreply.github.com>
2023-11-08 11:24:55 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@a7e651c43659a47d2acf6c739043534d1e202e69 🚀

---
+++ feature-DES-399-add-pagination-component/storybook-react/assets/Highlight.docs-e6f899f9.js
@@ -0,0 +1,36 @@
+import{j as e}from"./index.esm-9295b68c.js";import{M as o,d as i,P as l,e as c,f as t}from"./index-8d040583.js";import{H as d,B as h,D as j,Y as m,G as x,L as g,M as p,O as u,P as f}from"./Highlight.stories-e91bb28c.js";import{u as r}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./extends-98964cd2.js";import"./iframe-9f7430a1.js";import"../sb-preview/runtime.js";import"./index-932496f1.js";import"./index-d37d4223.js";import"./index-356e4a49.js";const w=`# Highlight
+
+Benadrukt een sectie op een pagina middels een opvallende achtergrondkleur.
+
+## Richtlijnen
+
+Raadpleeg [dit overzicht op Stijlweb](https://amsterdam.nl/stijlweb/basiselementen/kleuren/#PagCls_15671872) om na te gaan of je zwarte of witte tekst kunt gebruiken op de achtergrondkleur van je keuze.
+
+## Relevante WCAG eisen
+
+- [WCAG 1.4.3](https://www.w3.org/TR/WCAG21/#contrast-minimum): Large-scale text and images of large-scale text have a contrast ratio of at least 3:1;
+
+## Referenties
+
+- [Color and contrast accessibility](https://web.dev/articles/color-and-contrast-accessibility)
+`;function s(a){const n=Object.assign({h3:"h3"},r(),a.components);return e.jsxs(e.Fragment,{children:[e.jsx(o,{of:d}),`
+`,e.jsx(i,{children:w}),`
+`,e.jsx(l,{}),`
+`,e.jsx(c,{}),`
+`,e.jsx(n.h3,{id:"blauw",children:"Blauw"}),`
+`,e.jsx(t,{of:h}),`
+`,e.jsx(n.h3,{id:"donkergroen",children:"Donkergroen"}),`
+`,e.jsx(t,{of:j}),`
+`,e.jsx(n.h3,{id:"geel",children:"Geel"}),`
+`,e.jsx(t,{of:m}),`
+`,e.jsx(n.h3,{id:"groen",children:"Groen"}),`
+`,e.jsx(t,{of:x}),`
+`,e.jsx(n.h3,{id:"lichtblauw",children:"Lichtblauw"}),`
+`,e.jsx(t,{of:g}),`
+`,e.jsx(n.h3,{id:"magenta",children:"Magenta"}),`
+`,e.jsx(t,{of:p}),`
+`,e.jsx(n.h3,{id:"oranje",children:"Oranje"}),`
+`,e.jsx(t,{of:u}),`
+`,e.jsx(n.h3,{id:"paars",children:"Paars"}),`
+`,e.jsx(t,{of:f})]})}function y(a={}){const{wrapper:n}=Object.assign({},r(),a.components);return n?e.jsx(n,Object.assign({},a,{children:e.jsx(s,a)})):s(a)}export{y as default};
+//# sourceMappingURL=Highlight.docs-e6f899f9.js.map
