afbc1bc6d75e01e1fe6e1b07d393db1a5a817cbf
dlnr <niels@dlnr.dev>
2023-11-08 10:21:16 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@473fd73c1f6f5a447d432b4dbe1748995c7f4af8 🚀

---
+++ feature-DES-253-modal-dialog/storybook-react/assets/preview-3d0ef251.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-7aa87956.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-1df961b6.js"),["./DocsRenderer-NNNQARDV-1df961b6.js","./iframe-7aa87956.js","./index-76fb7be0.js","./_commonjsHelpers-de833af9.js","./react-18-063a39db.js","./index-932496f1.js","./index-99f862ae.js","./index-d37d4223.js","./extends-98964cd2.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-3d0ef251.js.map
