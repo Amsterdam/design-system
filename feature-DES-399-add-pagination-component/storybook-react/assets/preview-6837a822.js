48fe9a8d4b1ca3d93bc34af5dde5442d7ec79826
alimpens <37216945+alimpens@users.noreply.github.com>
2023-11-08 12:57:13 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@d5ef47c0818465789ca553d0f9356626d0e1e66b 🚀

---
+++ feature-DES-399-add-pagination-component/storybook-react/assets/preview-6837a822.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-b4cbe756.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-396846c5.js"),["./DocsRenderer-NNNQARDV-396846c5.js","./iframe-b4cbe756.js","./index-76fb7be0.js","./_commonjsHelpers-de833af9.js","./react-18-063a39db.js","./index-932496f1.js","./index-e7959f17.js","./index-d37d4223.js","./extends-98964cd2.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-6837a822.js.map
--- feature-DES-399-add-pagination-component/storybook-react/assets/preview-b6ec458f.js
+++
@@ -1,2 +0,0 @@
-import{_ as e}from"./iframe-9f7430a1.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-1df534f1.js"),["./DocsRenderer-NNNQARDV-1df534f1.js","./iframe-9f7430a1.js","./index-76fb7be0.js","./_commonjsHelpers-de833af9.js","./react-18-063a39db.js","./index-932496f1.js","./index-8d040583.js","./index-d37d4223.js","./extends-98964cd2.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
-//# sourceMappingURL=preview-b6ec458f.js.map
