3207e0577997ae816baa7e3f1c72ef114f4519af
dlnr <niels@dlnr.dev>
2023-11-08 10:23:15 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@e4951a6b9a6feadcba3389ae4dad7bcaa7c7a70d 🚀

---
+++ feature-DES-253-modal-dialog/storybook-react/assets/TopTaskLink.docs-54c7a919.js
@@ -0,0 +1,24 @@
+import{j as e}from"./index.esm-e287e5e1.js";import{M as a,d as r,P as d,e as l,f as i}from"./index-01543a72.js";import{T as m,W as k,a as p}from"./TopTaskLink.stories-d9cc2f5d.js";import{u as s}from"./index-a1cf9e47.js";import"./extends-98964cd2.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./iframe-6ac3185a.js";import"../sb-preview/runtime.js";import"./index-932496f1.js";import"./index-d37d4223.js";import"./index-356e4a49.js";const c=`# Top Task Link
+
+De Top Task Link is een navigatie-element dat opvallender is dan een link, om de aandacht te trekken voor de meest belangrijke informatie.
+Zet de Top Task Link in zodat de gebruiker snel de juiste informatie vindt én een bijbehorende taak makkelijk kan uitvoeren.
+
+## Richtlijnen
+
+- Een Top Task Link moet een titel hebben. Hou deze titel zo kort mogelijk, hij mag maximaal over 2 regels lopen.
+- Een Top Task Link kan een omschrijving hebben, maar dat is niet verplicht. De omschrijving staat onder de titel en is ook maximaal 2 regels.
+- Plaats voor brede vensters 3 of 4 Top Task Links naast elkaar. Als je meer Top Task Links hebt, zet ze dan op de volgende rij.
+- Probeer het aantal links op brede vensters te verdelen over rijen van 3 of 4 links. Als dit niet lukt, laat de laatste cellen van het grid dan leeg.
+
+## Relevante WCAG eisen
+
+Voor de Top Task Link gelden dezelfde eisen als voor [reguliere links](https://amsterdam.github.io/design-system/?path=/docs/react_navigation-link--docs).
+`;function o(n){const t=Object.assign({h3:"h3"},s(),n.components);return e.jsxs(e.Fragment,{children:[e.jsx(a,{of:m}),`
+`,e.jsx(r,{children:c}),`
+`,e.jsx(d,{}),`
+`,e.jsx(l,{}),`
+`,e.jsx(t.h3,{id:"without-description",children:"Without description"}),`
+`,e.jsx(i,{of:k}),`
+`,e.jsx(t.h3,{id:"with-description",children:"With description"}),`
+`,e.jsx(i,{of:p})]})}function z(n={}){const{wrapper:t}=Object.assign({},s(),n.components);return t?e.jsx(t,Object.assign({},n,{children:e.jsx(o,n)})):o(n)}export{z as default};
+//# sourceMappingURL=TopTaskLink.docs-54c7a919.js.map
