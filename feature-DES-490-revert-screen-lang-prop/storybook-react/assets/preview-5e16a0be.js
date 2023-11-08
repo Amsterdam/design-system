deab13e1b4e4567d4249e999dd3632e4581523ed
VincentSmedinga <v.smedinga@amsterdam.nl>
2023-11-08 11:29:18 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@00129f719b56c69e97f38eb1a88403e410284014 🚀

---
+++ feature-DES-490-revert-screen-lang-prop/storybook-react/assets/preview-5e16a0be.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-20411a39.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-5bc98315.js"),["./DocsRenderer-NNNQARDV-5bc98315.js","./iframe-20411a39.js","./index-76fb7be0.js","./_commonjsHelpers-de833af9.js","./react-18-063a39db.js","./index-932496f1.js","./index-a48f59ca.js","./index-d37d4223.js","./extends-98964cd2.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-5e16a0be.js.map
