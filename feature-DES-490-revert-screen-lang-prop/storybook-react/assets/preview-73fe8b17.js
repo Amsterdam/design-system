efa8fb8bc804a4d8b9241d5ddbca480fa1603261
alimpens <runner@fv-az1236-106.mlxw3kumjgxepk310yhxrkxswa.cx.internal.cloudapp.net>
2023-11-09 10:07:27 +0000

    Remove feature branch folder

--- feature-DES-490-revert-screen-lang-prop/storybook-react/assets/preview-73fe8b17.js
+++
@@ -1,2 +0,0 @@
-import{_ as e}from"./iframe-79b1bfdd.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-68c5fc20.js"),["./DocsRenderer-NNNQARDV-68c5fc20.js","./iframe-79b1bfdd.js","./index-76fb7be0.js","./_commonjsHelpers-de833af9.js","./react-18-063a39db.js","./index-932496f1.js","./index-14516c2a.js","./index-d37d4223.js","./extends-98964cd2.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
-//# sourceMappingURL=preview-73fe8b17.js.map
