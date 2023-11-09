18a937357e691ae2b62343e1fa88ee8cc3ec3e01
alimpens <37216945+alimpens@users.noreply.github.com>
2023-11-09 09:51:35 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@906f4cc4355e0b5fc55cdf9c77f8ed066d808a98 🚀

---
+++ feature-DES-490-revert-screen-lang-prop/storybook-react/assets/syntaxhighlighter-NMPM6SWI-ee9d17da.js
@@ -0,0 +1,2 @@
+import{S as l,c as n,s as d}from"./index-14516c2a.js";import"./iframe-79b1bfdd.js";import"../sb-preview/runtime.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./index-932496f1.js";import"./index-d37d4223.js";import"./extends-98964cd2.js";import"./index-356e4a49.js";export{l as SyntaxHighlighter,n as createCopyToClipboardFunction,d as default};
+//# sourceMappingURL=syntaxhighlighter-NMPM6SWI-ee9d17da.js.map
