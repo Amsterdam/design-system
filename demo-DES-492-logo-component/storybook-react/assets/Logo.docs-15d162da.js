87e51feb9ca6104271bdc9a10ea22a96666e22a6
dlnr <niels@dlnr.dev>
2023-11-10 15:07:52 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@2cad227ea9af56befa5f4a30a80157ec4beeaedd 🚀

---
+++ demo-DES-492-logo-component/storybook-react/assets/Logo.docs-15d162da.js
@@ -0,0 +1,14 @@
+import{j as n}from"./index.esm-feccb5b0.js";import{M as o,d as i,P as m,e as d,f as t}from"./index-015addd8.js";import{L as c,G as j,S as x,a as f,V as h}from"./Logo.stories-5e622327.js";import{u as a}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./extends-98964cd2.js";import"./iframe-5eed0b7c.js";import"../sb-preview/runtime.js";import"./index-932496f1.js";import"./index-d37d4223.js";import"./index-356e4a49.js";const g=`# Logo
+`;function r(e){const s=Object.assign({h2:"h2"},a(),e.components);return n.jsxs(n.Fragment,{children:[n.jsx(o,{of:c}),`
+`,n.jsx(i,{children:g}),`
+`,n.jsx(m,{}),`
+`,n.jsx(d,{}),`
+`,n.jsx(s.h2,{id:"ggd-amsterdam",children:"GGD Amsterdam"}),`
+`,n.jsx(t,{of:j}),`
+`,n.jsx(s.h2,{id:"stadsarchief",children:"Stadsarchief"}),`
+`,n.jsx(t,{of:x}),`
+`,n.jsx(s.h2,{id:"stadsbank-van-lening",children:"Stadsbank van Lening"}),`
+`,n.jsx(t,{of:f}),`
+`,n.jsx(s.h2,{id:"vga-verzekeringen",children:"VGA Verzekeringen"}),`
+`,n.jsx(t,{of:h})]})}function C(e={}){const{wrapper:s}=Object.assign({},a(),e.components);return s?n.jsx(s,Object.assign({},e,{children:n.jsx(r,e)})):r(e)}export{C as default};
+//# sourceMappingURL=Logo.docs-15d162da.js.map
