77e3cf7d5b30a3bbddb36e58766c56cd87b2f788
VincentSmedinga <VincentSmedinga@users.noreply.github.com>
2023-11-10 14:33:41 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@86cae8001f1188979881a48196c6d1475306bf1e 🚀

--- feature-DES-253-modal-dialog/assets/syntaxhighlighter-NMPM6SWI-f1ebd6d2.js
+++
@@ -1,2 +0,0 @@
-import{S as s,c,s as l}from"./index-7a6fc507.js";import"./iframe-ae3e528b.js";import"../sb-preview/runtime.js";import"./index-565f84a4.js";import"./_commonjsHelpers-de833af9.js";import"./index-d37d4223.js";import"./index-356e4a49.js";export{s as SyntaxHighlighter,c as createCopyToClipboardFunction,l as default};
-//# sourceMappingURL=syntaxhighlighter-NMPM6SWI-f1ebd6d2.js.map
--- feature-DES-253-modal-dialog/storybook-react/assets/FormLabel.docs-94210b5d.js
+++
@@ -1,5 +0,0 @@
-import{j as e}from"./index.esm-c13d9990.js";import{M as o,d as s,P as m,e as a}from"./index-52e0715d.js";import{F as i}from"./FormLabel.stories-c7d0f029.js";import{u as l}from"./index-a1cf9e47.js";import"./extends-98964cd2.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./iframe-ba30083f.js";import"../sb-preview/runtime.js";import"./index-932496f1.js";import"./index-d37d4223.js";import"./index-356e4a49.js";const p="# Form Label\n\nEen label element gebruik je om een formulierelement te beschrijven. Koppel een formulierelement (zoals een `input` of `textarea`) altijd aan een label.\n";function r(t){return e.jsxs(e.Fragment,{children:[e.jsx(o,{of:i}),`
-`,e.jsx(s,{children:p}),`
-`,e.jsx(m,{}),`
-`,e.jsx(a,{})]})}function E(t={}){const{wrapper:n}=Object.assign({},l(),t.components);return n?e.jsx(n,Object.assign({},t,{children:e.jsx(r,t)})):r()}export{E as default};
-//# sourceMappingURL=FormLabel.docs-94210b5d.js.map
