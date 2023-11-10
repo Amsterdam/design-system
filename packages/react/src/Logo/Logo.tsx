1cfa8f13ff2e3cfbb4654a69ad17682aae3dd895
Niels Roozemond <n.roozemond@amsterdam.nl>
2023-11-10 16:00:19 +0100

    Logo Variants

--- packages/react/src/Logo/Logo.tsx
+++ packages/react/src/Logo/Logo.tsx
@@ -5,14 +5,39 @@
 
 import clsx from 'clsx'
 import { ForwardedRef, forwardRef, HTMLAttributes } from 'react'
-import LogoAsset from '../../../../proprietary/assets/Logo.svg'
+import LogoAmsterdam from '../../../../proprietary/assets/logo/amsterdam.svg'
+import LogoGGD from '../../../../proprietary/assets/logo/ggd-amsterdam.svg'
+import LogoStadsarchief from '../../../../proprietary/assets/logo/stadsarchief.svg'
+import LogoStadsbankLening from '../../../../proprietary/assets/logo/stadsbank-lening.svg'
+import LogoVGA from '../../../../proprietary/assets/logo/vga-verzekeringen.svg'
 
-export interface LogoProps extends HTMLAttributes<HTMLElement> {}
+export interface LogoProps extends HTMLAttributes<HTMLElement> {
+  variant?: 'amsterdam' | 'ggd-amsterdam' | 'stadsarchief' | 'stadsbank-lening' | 'vga-verzekeringen'
+}
 
-export const Logo = forwardRef(({ className, ...restProps }: LogoProps, ref: ForwardedRef<HTMLElement>) => (
-  <span {...restProps} ref={ref} className={clsx('amsterdam-logo', className)}>
-    <img src={LogoAsset} />
-  </span>
-))
+function getLogoVariant(variant: LogoProps['variant']) {
+  switch (variant) {
+    case 'amsterdam':
+      return LogoAmsterdam
+    case 'ggd-amsterdam':
+      return LogoGGD
+    case 'stadsarchief':
+      return LogoStadsarchief
+    case 'stadsbank-lening':
+      return LogoStadsbankLening
+    case 'vga-verzekeringen':
+      return LogoVGA
+    default:
+      return LogoAmsterdam
+  }
+}
+
+export const Logo = forwardRef(
+  ({ variant = 'amsterdam', className, ...restProps }: LogoProps, ref: ForwardedRef<HTMLElement>) => (
+    <span {...restProps} ref={ref} className={clsx('amsterdam-logo', className)}>
+      <img src={getLogoVariant(variant)} />
+    </span>
+  ),
+)
 
 Logo.displayName = 'Logo'
--- storybook/storybook-react/src/Logo/Logo.stories.tsx
+++ storybook/storybook-react/src/Logo/Logo.stories.tsx
@@ -9,6 +9,14 @@ import { Meta, StoryObj } from '@storybook/react'
 const meta = {
   title: 'Media/Logo',
   component: Logo,
+  argTypes: {
+    variant: {
+      control: {
+        type: 'select',
+      },
+      options: ['amsterdam', 'ggd-amsterdam', 'stadsarchief', 'stadsbank-lening', 'vga-verzekeringen'],
+    },
+  },
 } satisfies Meta<typeof Logo>
 
 export default meta
@@ -16,3 +24,27 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const Default: Story = {}
+
+export const GGDAmsterdam: Story = {
+  args: {
+    variant: 'ggd-amsterdam',
+  },
+}
+
+export const Stadsarchief: Story = {
+  args: {
+    variant: 'stadsarchief',
+  },
+}
+
+export const StadsbankLening: Story = {
+  args: {
+    variant: 'stadsbank-lening',
+  },
+}
+
+export const VGAVerzekeringen: Story = {
+  args: {
+    variant: 'vga-verzekeringen',
+  },
+}
