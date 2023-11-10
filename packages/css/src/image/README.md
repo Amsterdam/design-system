52534369eff4a612d9f6b5dffd2972128138a2ea
Vincent Smedinga <v.smedinga@amsterdam.nl>
2023-11-10 15:49:03 +0100

    Add initial Image component

---
+++ packages/css/src/image/README.md
@@ -0,0 +1,16 @@
+# Image
+
+Toont een afbeelding.
+
+## Richtlijnen
+
+- Vergeet niet om een beschrijving van de afbeelding op te nemen in het `alt`-attribuut.
+  Dit zorgt ervoor dat gebruikers van schermlezers deze informatie ook tot zich kunnen nemen.
+  Daarnaast kan het helpen bij zoekmachineoptimalisatie.
+- Alleen voor decoratieve afbeeldingen is zo’n beschrijving niet nodig.
+  Denk aan afbeeldingen die weinig toevoegen aan de nabije tekst of afbeeldingen die louter bijdragen aan het ontwerp of de sfeer van de pagina (bron: [W3C Web Accessibility Initiative](https://www.w3.org/WAI/tutorials/images/decorative/)).
+- Zorg ervoor dat de afbeelding een beeldverhouding heeft die ondersteund wordt door het [Aspect Ratio](?path=/docs/layout-aspect-ratio--docs) component.
+
+## Relevante WCAG-eisen
+
+- [WCAG 1.1.1](https://www.w3.org/TR/WCAG22/#non-text-content): niet-tekstuele content heeft een tekstueel alternatief
---
+++ packages/react/src/Image/Image.test.tsx
@@ -0,0 +1,33 @@
+import { render } from '@testing-library/react'
+import { createRef } from 'react'
+import { Image } from './Image'
+import '@testing-library/jest-dom'
+
+describe('Image', () => {
+  it('renders', () => {
+    const { container } = render(<Image />)
+    const component = container.querySelector(':only-child')
+    expect(component).toBeInTheDocument()
+    expect(component).toBeVisible()
+  })
+
+  it('renders a design system BEM class name', () => {
+    const { container } = render(<Image />)
+    const component = container.querySelector(':only-child')
+    expect(component).toHaveClass('amsterdam-image')
+  })
+
+  it('renders an additional class name', () => {
+    const { container } = render(<Image className="extra" />)
+    const component = container.querySelector(':only-child')
+    expect(component).toHaveClass('extra')
+    expect(component).toHaveClass('amsterdam-image')
+  })
+
+  it('supports ForwardRef in React', () => {
+    const ref = createRef<HTMLImageElement>()
+    const { container } = render(<Image ref={ref} />)
+    const component = container.querySelector(':only-child')
+    expect(ref.current).toBe(component)
+  })
+})
