ecf681c658caa420b6f38fbd0c72e086c97d1a7f
dlnr <niels@dlnr.dev>
2023-11-10 15:02:47 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@1cfa8f13ff2e3cfbb4654a69ad17682aae3dd895 🚀

--- demo-DES-492-logo-component/storybook-react/assets/Logo.docs-19962fd9.js
+++
@@ -1,6 +0,0 @@
-import{j as o}from"./index.esm-d2679b21.js";import{M as s,d as e,P as i,e as m}from"./index-cf4d99cf.js";import{L as a}from"./Logo.stories-3ec7c238.js";import{u as p}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./extends-98964cd2.js";import"./iframe-779ee16f.js";import"../sb-preview/runtime.js";import"./index-932496f1.js";import"./index-d37d4223.js";import"./index-356e4a49.js";const c=`# Logo
-`;function n(t){return o.jsxs(o.Fragment,{children:[o.jsx(s,{of:a}),`
-`,o.jsx(e,{children:c}),`
-`,o.jsx(i,{}),`
-`,o.jsx(m,{})]})}function E(t={}){const{wrapper:r}=Object.assign({},p(),t.components);return r?o.jsx(r,Object.assign({},t,{children:o.jsx(n,t)})):n()}export{E as default};
-//# sourceMappingURL=Logo.docs-19962fd9.js.map
