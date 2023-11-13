cbc2c887f5f286764a347a3d5f6548ff008e9a64
VincentSmedinga <v.smedinga@amsterdam.nl>
2023-11-13 13:54:25 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@7f3adf0d5d48b79319c5a56af99ea0a549bd171d 🚀

---
+++ demo-extra-prefix/storybook-react/assets/AspectRatio.docs-a9c97570.js
@@ -0,0 +1,32 @@
+import{j as e}from"./index.esm-0a3ca4ab.js";import{M as r,d as i,P as d,e as l,f as s}from"./index-e924fd46.js";import{A as m,E as x,W as c,S as h,T as j,a as p}from"./AspectRatio.stories-be3e4efb.js";import{u as a}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./extends-98964cd2.js";import"./iframe-c3312f75.js";import"../sb-preview/runtime.js";import"./index-932496f1.js";import"./index-d37d4223.js";import"./index-356e4a49.js";const u=`# Aspect Ratio
+
+Aspect Ratio (in het Nederlands: beeldverhouding) koppelt de hoogte van een element aan de breedte.
+
+Om de vormgeving consistent te houden gebruiken we een vaste set verhoudingen (breedte:hoogte):
+
+| _keyword_    | verhouding |
+| :----------- | ---------: |
+| \`extra-wide\` |       16:9 |
+| \`wide\`       |        5:4 |
+| \`square\`     |        1:1 |
+| \`tall\`       |        4:5 |
+| \`extra-tall\` |       9:16 |
+
+## Richtlijnen
+
+- Gebruik dit component om plaatjes, videos en andere elementen een vaste beeldverhouding te geven, zodat ze consistent schalen.
+`;function o(t){const n=Object.assign({h2:"h2"},a(),t.components);return e.jsxs(e.Fragment,{children:[e.jsx(r,{of:m}),`
+`,e.jsx(i,{children:u}),`
+`,e.jsx(d,{}),`
+`,e.jsx(l,{}),`
+`,e.jsx(n.h2,{id:"extra-wide",children:"Extra wide"}),`
+`,e.jsx(s,{of:x}),`
+`,e.jsx(n.h2,{id:"wide",children:"Wide"}),`
+`,e.jsx(s,{of:c}),`
+`,e.jsx(n.h2,{id:"square",children:"Square"}),`
+`,e.jsx(s,{of:h}),`
+`,e.jsx(n.h2,{id:"tall",children:"Tall"}),`
+`,e.jsx(s,{of:j}),`
+`,e.jsx(n.h2,{id:"extra-tall",children:"Extra Tall"}),`
+`,e.jsx(s,{of:p})]})}function q(t={}){const{wrapper:n}=Object.assign({},a(),t.components);return n?e.jsx(n,Object.assign({},t,{children:e.jsx(o,t)})):o(t)}export{q as default};
+//# sourceMappingURL=AspectRatio.docs-a9c97570.js.map
