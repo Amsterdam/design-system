fbd2f42e9b48ff4333c1f9bc751ce53c2f258d76
alimpens <alimpens@users.noreply.github.com>
2023-11-13 13:40:21 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@6d1d21ba3c1a0b21f94016a40012bcc8976bd8b9 🚀

---
+++ storybook-react/assets/Alert.docs-874980ea.js
@@ -0,0 +1,31 @@
+import{j as e}from"./index.esm-0a3ca4ab.js";import{M as s,d as a,P as l,e as d,f as t}from"./index-b20a4823.js";import{A as m,I as c,E as f,a as h,S as g}from"./Alert.stories-1c83e578.js";import{u as i}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./extends-98964cd2.js";import"./iframe-73fdff9f.js";import"../sb-preview/runtime.js";import"./index-932496f1.js";import"./index-d37d4223.js";import"./index-356e4a49.js";const j=`# Alert
+
+Met een Alert breng je de gebruiker op de hoogte van een belangrijke of tijdgevoelige boodschap. De taak van de gebruiker wordt niet onderbroken.
+
+## Specificaties
+
+### Kleurdefinities
+
+- Geel: informatie
+- Rood: fout
+- Groen: goed
+
+## Richtlijnen
+
+- Standaard heeft het element geen sluiten knop. Optioneel kan deze toegevoegd worden.
+- Optioneel kan een icoon worden weggelaten. Een geel Alert heeft nooit een icoon.
+- Optioneel kan een titel worden weggelaten.
+- Aangeraden wordt om een Alert in ieder geval van een paragraaf text te voorzien en deze eventueel te ondersteunen met een link, opsomming of knop.
+`;function r(o){const n=Object.assign({h3:"h3"},i(),o.components);return e.jsxs(e.Fragment,{children:[e.jsx(s,{of:m}),`
+`,e.jsx(a,{children:j}),`
+`,e.jsx(l,{}),`
+`,e.jsx(d,{}),`
+`,e.jsx(n.h3,{id:"information-with-inline-link",children:"Information with inline link"}),`
+`,e.jsx(t,{of:c}),`
+`,e.jsx(n.h3,{id:"error",children:"Error"}),`
+`,e.jsx(t,{of:f}),`
+`,e.jsx(n.h3,{id:"error-with-list",children:"Error with list"}),`
+`,e.jsx(t,{of:h}),`
+`,e.jsx(n.h3,{id:"success",children:"Success"}),`
+`,e.jsx(t,{of:g})]})}function C(o={}){const{wrapper:n}=Object.assign({},i(),o.components);return n?e.jsx(n,Object.assign({},o,{children:e.jsx(r,o)})):r(o)}export{C as default};
+//# sourceMappingURL=Alert.docs-874980ea.js.map
---
+++ storybook-react/assets/preview-023ff638.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-73fdff9f.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-a42e38e3.js"),["./DocsRenderer-NNNQARDV-a42e38e3.js","./iframe-73fdff9f.js","./index-76fb7be0.js","./_commonjsHelpers-de833af9.js","./react-18-063a39db.js","./index-932496f1.js","./index-b20a4823.js","./index-d37d4223.js","./extends-98964cd2.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-023ff638.js.map
