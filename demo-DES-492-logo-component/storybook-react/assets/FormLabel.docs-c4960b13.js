bedf1332bcd09c893fe6f0f1508d1947a46ca52e
VincentSmedinga <v.smedinga@amsterdam.nl>
2023-11-14 19:40:31 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@55fcdfddbd1658c2fa80b176e1297be1cce4941a 🚀

--- demo-DES-492-logo-component/storybook-react/assets/FormLabel.docs-c4960b13.js
+++
@@ -1,5 +0,0 @@
-import{j as e}from"./index.esm-c313fb02.js";import{M as o,d as s,P as m,e as a}from"./index-287054bf.js";import{F as i}from"./FormLabel.stories-28744ed5.js";import{u as l}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./extends-98964cd2.js";import"./iframe-38a2f99c.js";import"../sb-preview/runtime.js";import"./index-932496f1.js";import"./index-d37d4223.js";import"./index-356e4a49.js";const p="# Form Label\n\nEen label element gebruik je om een formulierelement te beschrijven. Koppel een formulierelement (zoals een `input` of `textarea`) altijd aan een label.\n";function r(t){return e.jsxs(e.Fragment,{children:[e.jsx(o,{of:i}),`
-`,e.jsx(s,{children:p}),`
-`,e.jsx(m,{}),`
-`,e.jsx(a,{})]})}function E(t={}){const{wrapper:n}=Object.assign({},l(),t.components);return n?e.jsx(n,Object.assign({},t,{children:e.jsx(r,t)})):r()}export{E as default};
-//# sourceMappingURL=FormLabel.docs-c4960b13.js.map
--- demo-DES-492-logo-component/storybook-react/assets/preview-25d5afa9.js
+++
@@ -1,2 +0,0 @@
-import{_ as e}from"./iframe-38a2f99c.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-ff86f886.js"),["./DocsRenderer-NNNQARDV-ff86f886.js","./iframe-38a2f99c.js","./index-76fb7be0.js","./_commonjsHelpers-de833af9.js","./react-18-063a39db.js","./index-932496f1.js","./index-287054bf.js","./index-d37d4223.js","./extends-98964cd2.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
-//# sourceMappingURL=preview-25d5afa9.js.map
