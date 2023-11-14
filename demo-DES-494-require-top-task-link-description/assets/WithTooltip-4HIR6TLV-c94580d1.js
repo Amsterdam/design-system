68f3e13e0fc4854f3d4058cd32eab6e781367fd6
VincentSmedinga <v.smedinga@amsterdam.nl>
2023-11-14 11:25:34 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@d204d05f787777d39c7f4dfdace26f910238af9c 🚀

--- demo-DES-494-require-top-task-link-description/assets/WithTooltip-4HIR6TLV-c94580d1.js
+++
@@ -1,2 +0,0 @@
-import{W as T,W as e,a as h}from"./index-32154d3a.js";import"./iframe-3991f6c2.js";import"../sb-preview/runtime.js";import"./index-565f84a4.js";import"./_commonjsHelpers-de833af9.js";import"./index-d37d4223.js";import"./index-356e4a49.js";export{T as WithToolTipState,e as WithTooltip,h as WithTooltipPure};
-//# sourceMappingURL=WithTooltip-4HIR6TLV-c94580d1.js.map
---
+++ demo-DES-494-require-top-task-link-description/storybook-react/assets/TopTaskLink.docs-07c6cc8a.js
@@ -0,0 +1,29 @@
+import{j as e}from"./index.esm-76bf233d.js";import{M as o,d as r,P as s,e as d,f as l}from"./index-b83a8f71.js";import{T as p,M as k}from"./TopTaskLink.stories-1f100791.js";import{u as a}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./extends-98964cd2.js";import"./iframe-7bfce228.js";import"../sb-preview/runtime.js";import"./index-932496f1.js";import"./index-d37d4223.js";import"./index-356e4a49.js";const m=`# Top Task Link
+
+Via dit opvallende navigatie-element vindt de Amsterdammer snel de weg naar veel voorkomende taken.
+
+## Richtlijnen
+
+- Een Top Task Link heeft zowel een titel als een beschrijving.
+- De titel beschrijft de essentie van de pagina waarnaar wordt verwezen.
+  Deze trekt de aandacht bij het scannen van de pagina.
+- De beschrijving geeft meer context.
+  Dit helpt de Amsterdammer bevestigen dat die zijn doel op de vervolgpagina inderdaad kan bereiken.
+- Houd beide teksten bondig. Een titel bestaat uit een paar woorden, de beschrijving uit 1 of 2 korte zinnen. De beschrijving eindigt dan ook op een punt of ander geschikt leesteken.
+- Plaats voor brede vensters 3 of 4 Top Task Links naast elkaar op het [Grid](?path=/docs/react_layout-grid--docs).
+  Als je meer Top Task Links hebt, zet ze dan op de volgende rij.
+  Probeer het aantal links op brede vensters te verdelen over rijen van 3 of 4 links. Als dit niet lukt, laat de laatste cellen van het grid dan leeg.
+- Heb je een groep verwijzingen waarvoor een beschrijving niet nodig of logisch is, gebruik dan een [Link List](?path=/docs/react_navigation-link--docs).
+
+## Relevante WCAG eisen
+
+Voor de Top Task Link gelden dezelfde eisen als voor [reguliere links](https://amsterdam.github.io/design-system/?path=/docs/react_navigation-link--docs).
+`;function i(n){const t=Object.assign({h3:"h3",p:"p"},a(),n.components);return e.jsxs(e.Fragment,{children:[e.jsx(o,{of:p}),`
+`,e.jsx(r,{children:m}),`
+`,e.jsx(s,{}),`
+`,e.jsx(d,{}),`
+`,e.jsx(t.h3,{id:"meerdere-links",children:"Meerdere links"}),`
+`,e.jsx(t.p,{children:`Normaal gesproken zijn er meerdere toptaken.
+Deze links komen dan ook vaak voor in een grid:`}),`
+`,e.jsx(l,{of:k})]})}function w(n={}){const{wrapper:t}=Object.assign({},a(),n.components);return t?e.jsx(t,Object.assign({},n,{children:e.jsx(i,n)})):i(n)}export{w as default};
+//# sourceMappingURL=TopTaskLink.docs-07c6cc8a.js.map
