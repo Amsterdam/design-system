2234c777da7a3034450a4dd52018e7581e6cc053
VincentSmedinga <runner@fv-az837-992.ymagj3ilz1se1nskuisabcixse.ex.internal.cloudapp.net>
2023-11-14 11:50:09 +0000

    Remove feature branch folder

--- demo-DES-494-require-top-task-link-description/storybook-react/assets/preview-1863c07b.js
+++
@@ -1,2 +0,0 @@
-import{_ as e}from"./iframe-7bfce228.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-c8aaa252.js"),["./DocsRenderer-NNNQARDV-c8aaa252.js","./iframe-7bfce228.js","./index-76fb7be0.js","./_commonjsHelpers-de833af9.js","./react-18-063a39db.js","./index-932496f1.js","./index-b83a8f71.js","./index-d37d4223.js","./extends-98964cd2.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
-//# sourceMappingURL=preview-1863c07b.js.map
