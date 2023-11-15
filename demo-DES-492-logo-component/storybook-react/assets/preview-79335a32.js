dc85d9fc7f605e506f38f32dffc7e414b6086733
dlnr <niels@dlnr.dev>
2023-11-15 15:33:06 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@3e7ea1c8643b155e06fdb764c0ab207d8c0c89ae 🚀

---
+++ demo-DES-492-logo-component/storybook-react/assets/preview-79335a32.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-cee3ac1f.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-c86971d2.js"),["./DocsRenderer-NNNQARDV-c86971d2.js","./iframe-cee3ac1f.js","./index-76fb7be0.js","./_commonjsHelpers-de833af9.js","./react-18-063a39db.js","./index-932496f1.js","./index-28fbfe42.js","./index-d37d4223.js","./extends-98964cd2.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-79335a32.js.map
