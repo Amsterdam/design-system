d025b575ef7148eecf2937a5a8a387dacc9783ab
dlnr <niels@dlnr.dev>
2023-11-16 16:20:45 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@3620528211bebb6591599bbb8d30134e0e984c2e 🚀

---
+++ demo-DES-492-logo-component/storybook-react/assets/Logo.docs-9a080b70.js
@@ -0,0 +1,49 @@
+import{j as e}from"./index.esm-0ccc5f96.js";import{M as i,d as r,P as d,e as l,f as a}from"./index-1177ce21.js";import{L as g,G as m,S as p,a as b,V as c}from"./Logo.stories-fbf4c8df.js";import{u as s}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./extends-98964cd2.js";import"./iframe-198fe083.js";import"../sb-preview/runtime.js";import"./index-932496f1.js";import"./index-d37d4223.js";import"./index-356e4a49.js";const h=`# Logo
+
+Het logo van de gemeente bestaat uit een beeldmerk (3 Andreaskruisen) en een woordmerk (Gemeente Amsterdam).
+Het logo heeft een vaste kleur, grootte en positie en staat op al onze websites.
+
+## Specificaties
+
+Het design system biedt het correcte logo voor online gebruik aan.
+Er bestaan andere varianten van het logo, maar die mogen op websites en in applicaties niet gebruikt worden.
+Het Nederlandse logo voor Nederlandstalige websites en anderstalige websites behalve Engels.
+Het Engelstalige logo gebruiken we voor Engelstalige websites.
+
+## Uitzondering
+
+Gebruik het submerklogo als de website eigendom is van 1 van de 4 submerken.
+Zij hebben een aparte status omdat er sprake is van een bijzondere vorm van dienstverlening die niet direct met de gemeente in verband wordt gebracht.
+
+De submerken zijn:
+
+- GGD Amsterdam
+- Stadsarchief
+- Stadsbank van Lening
+- VGA Verzekeringen
+
+## Eisen voor alle logo´s
+
+- Het logo wordt altijd getoond.
+- Het logo is linksboven uitgelijnd in de header.
+- Het logo verwijst naar de homepage van je website of applicatie.
+- Als de applicatie een formulier, applicatie of tool is zonder homepage, dan linkt het logo naar de pagina van waar naar het formulier, applicatie of tool wordt verwezen.
+
+De hoogte van het logo is altijd 40 pixels. Dit geldt ook voor de submerklogo´s.
+
+## Download
+
+- [Logoset voor website en apps (ZIP, 10 kB)](https://assets.amsterdam.nl/publish/pages/1007650/logoset_voor_websites_en_apps.zip)
+`;function o(t){const n=Object.assign({h2:"h2"},s(),t.components);return e.jsxs(e.Fragment,{children:[e.jsx(i,{of:g}),`
+`,e.jsx(r,{children:h}),`
+`,e.jsx(d,{}),`
+`,e.jsx(l,{}),`
+`,e.jsx(n.h2,{id:"ggd-amsterdam",children:"GGD Amsterdam"}),`
+`,e.jsx(a,{of:m}),`
+`,e.jsx(n.h2,{id:"stadsarchief",children:"Stadsarchief"}),`
+`,e.jsx(a,{of:p}),`
+`,e.jsx(n.h2,{id:"stadsbank-van-lening",children:"Stadsbank van Lening"}),`
+`,e.jsx(a,{of:b}),`
+`,e.jsx(n.h2,{id:"vga-verzekeringen",children:"VGA Verzekeringen"}),`
+`,e.jsx(a,{of:c})]})}function E(t={}){const{wrapper:n}=Object.assign({},s(),t.components);return n?e.jsx(n,Object.assign({},t,{children:e.jsx(o,t)})):o(t)}export{E as default};
+//# sourceMappingURL=Logo.docs-9a080b70.js.map
---
+++ demo-DES-492-logo-component/storybook-react/assets/VisuallyHidden.docs-8114a732.js
@@ -0,0 +1,10 @@
+import{j as e}from"./index.esm-0ccc5f96.js";import{M as i,d as t,P as s,e as a}from"./index-1177ce21.js";import{V as d}from"./VisuallyHidden.stories-9b3c72b8.js";import{u as m}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./extends-98964cd2.js";import"./iframe-198fe083.js";import"../sb-preview/runtime.js";import"./index-932496f1.js";import"./index-d37d4223.js";import"./index-356e4a49.js";const u=`# Visually Hidden
+
+Het Visually Hidden component kan gebruikt worden om inhoud te verbergen voor ziende gebruikers, maar beschikbaar te houden voor niet-visuele _user agents_, zoals schermlezers.
+
+Let op: In de overgrote meerderheid van de gevallen moet inhoud die visueel beschikbaar is, ook beschikbaar zijn voor niet-visuele _user agents_, en omgekeerd. Uitgebreide aanwijzingen of instructies die alleen door niet-visuele _user agents_ worden gelezen, doen al snel meer kwaad dan goed.
+`;function o(n){return e.jsxs(e.Fragment,{children:[e.jsx(i,{of:d}),`
+`,e.jsx(t,{children:u}),`
+`,e.jsx(s,{}),`
+`,e.jsx(a,{})]})}function _(n={}){const{wrapper:r}=Object.assign({},m(),n.components);return r?e.jsx(r,Object.assign({},n,{children:e.jsx(o,n)})):o()}export{_ as default};
+//# sourceMappingURL=VisuallyHidden.docs-8114a732.js.map
