ea424e0e09e3efaa63324ad8924bca1dfea0fc2f
dlnr <niels@dlnr.dev>
2023-11-16 16:15:25 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@f2e7a56058928857f1cc2ce150d145d1d495af7b 🚀

---
+++ demo-DES-492-logo-component/storybook-react/assets/Logo.docs-d6aabad8.js
@@ -0,0 +1,48 @@
+import{j as e}from"./index.esm-83239962.js";import{M as i,d as r,P as d,e as l,f as o}from"./index-86d254b5.js";import{L as g,G as m,S as p,a as b,V as h}from"./Logo.stories-48b7c02a.js";import{u as s}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./extends-98964cd2.js";import"./iframe-45e6df5f.js";import"../sb-preview/runtime.js";import"./index-932496f1.js";import"./index-d37d4223.js";import"./index-356e4a49.js";const c=`# Logo
+
+Het logo van de gemeente bestaat uit een beeldmerk (3 Andreaskruisen) en een woordmerk (Gemeente Amsterdam).
+Het logo heeft een vaste kleur, grootte en positie en staat op al onze websites.
+
+## Specificaties
+
+Gebruik altijd het online logo.
+Het Nederlandse logo voor Nederlandstalige websites en anderstalige websites behalve Engels.
+Het Engelstalige logo gebruiken we voor Engelstalige websites.
+
+## Uitzondering
+
+Gebruik het submerklogo als de website eigendom is van 1 van de 4 submerken.
+Zij hebben een aparte status omdat er sprake is van een bijzondere vorm van dienstverlening die niet direct met de gemeente in verband wordt gebracht.
+
+De submerken zijn:
+
+- GGD Amsterdam
+- Stadsarchief
+- Stadsbank van Lening
+- VGA Verzekeringen
+
+## Eisen voor alle logo´s
+
+- Het logo wordt altijd getoond.
+- Het logo is linksboven uitgelijnd in de header.
+- Het logo verwijst naar de homepage van je website of applicatie.
+- Als de applicatie een formulier, applicatie of tool is zonder homepage, dan linkt het logo naar de pagina van waar naar het formulier, applicatie of tool wordt verwezen.
+
+De hoogte van het logo is altijd 40 pixels. Dit geldt ook voor de submerklogo´s.
+
+## Download
+
+- [Logoset voor website en apps (ZIP, 10 kB)](https://assets.amsterdam.nl/publish/pages/1007650/logoset_voor_websites_en_apps.zip)
+`;function a(t){const n=Object.assign({h2:"h2"},s(),t.components);return e.jsxs(e.Fragment,{children:[e.jsx(i,{of:g}),`
+`,e.jsx(r,{children:c}),`
+`,e.jsx(d,{}),`
+`,e.jsx(l,{}),`
+`,e.jsx(n.h2,{id:"ggd-amsterdam",children:"GGD Amsterdam"}),`
+`,e.jsx(o,{of:m}),`
+`,e.jsx(n.h2,{id:"stadsarchief",children:"Stadsarchief"}),`
+`,e.jsx(o,{of:p}),`
+`,e.jsx(n.h2,{id:"stadsbank-van-lening",children:"Stadsbank van Lening"}),`
+`,e.jsx(o,{of:b}),`
+`,e.jsx(n.h2,{id:"vga-verzekeringen",children:"VGA Verzekeringen"}),`
+`,e.jsx(o,{of:h})]})}function L(t={}){const{wrapper:n}=Object.assign({},s(),t.components);return n?e.jsx(n,Object.assign({},t,{children:e.jsx(a,t)})):a(t)}export{L as default};
+//# sourceMappingURL=Logo.docs-d6aabad8.js.map
