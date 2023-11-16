a3e4f13c22b509f22b60f4a885e5110be116068f
dlnr <niels@dlnr.dev>
2023-11-16 09:58:17 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@6b6473f42a398241cecde6945d2d12d639af0422 🚀

---
+++ demo-DES-492-logo-component/storybook-react/assets/Image.docs-893d1c2e.js
@@ -0,0 +1,23 @@
+import{j as e}from"./index.esm-369b0f08.js";import{M as o,d as a,P as r,e as s}from"./index-849a5c04.js";import{I as d}from"./Image.stories-d85faab3.js";import{u as g}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./extends-98964cd2.js";import"./iframe-304ffae9.js";import"../sb-preview/runtime.js";import"./index-932496f1.js";import"./index-d37d4223.js";import"./index-356e4a49.js";const l=`# Image
+
+Toont een afbeelding.
+
+## Richtlijnen
+
+- Vergeet niet om een beschrijving van de afbeelding op te nemen in het \`alt\`-attribuut.
+  Dit zorgt ervoor dat gebruikers van schermlezers deze informatie ook tot zich kunnen nemen.
+  Daarnaast kan het helpen bij zoekmachineoptimalisatie.
+- Alleen voor decoratieve afbeeldingen is zo’n beschrijving niet nodig. Gebruik in dit geval \`alt=""\`.
+  Denk aan afbeeldingen die weinig toevoegen aan de nabije tekst of afbeeldingen die louter bijdragen aan het ontwerp of de sfeer van de pagina (bron: [W3C Web Accessibility Initiative](https://www.w3.org/WAI/tutorials/images/decorative/)).
+- Zorg ervoor dat de afbeelding een beeldverhouding heeft die ondersteund wordt door het [Aspect Ratio](?path=/docs/layout-aspect-ratio--docs) component.
+
+## Relevante WCAG-eisen
+
+- [WCAG 1.1.1](https://www.w3.org/TR/WCAG22/#non-text-content): niet-tekstuele content heeft een tekstueel alternatief
+- [WCAG 1.4.5](https://www.w3.org/TR/WCAG22/#images-of-text): gebruik tekst in plaats van afbeeldingen van tekst
+- [WCAG 1.4.9](https://www.w3.org/TR/WCAG22/#images-of-text-no-exception): gebruik afbeeldingen van tekst alleen als er geen alternatief is
+`;function i(n){return e.jsxs(e.Fragment,{children:[e.jsx(o,{of:d}),`
+`,e.jsx(a,{children:l}),`
+`,e.jsx(r,{}),`
+`,e.jsx(s,{})]})}function A(n={}){const{wrapper:t}=Object.assign({},g(),n.components);return t?e.jsx(t,Object.assign({},n,{children:e.jsx(i,n)})):i()}export{A as default};
+//# sourceMappingURL=Image.docs-893d1c2e.js.map
---
+++ demo-DES-492-logo-component/storybook-react/assets/preview-93f86b4c.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-304ffae9.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-c763b49f.js"),["./DocsRenderer-NNNQARDV-c763b49f.js","./iframe-304ffae9.js","./index-76fb7be0.js","./_commonjsHelpers-de833af9.js","./react-18-063a39db.js","./index-932496f1.js","./index-849a5c04.js","./index-d37d4223.js","./extends-98964cd2.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-93f86b4c.js.map
