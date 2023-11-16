3a8de0535e91424a495fb04fc88c2fb93af7a4f9
Vincent Smedinga <v.smedinga@amsterdam.nl>
2023-11-16 18:01:40 +0100

    Revert "Allow start alignment of screen"
    This reverts commit b09357d45d0e58381df150f93520ade6aa318645.

--- packages/react/src/Screen/Screen.tsx
+++ packages/react/src/Screen/Screen.tsx
@@ -6,24 +6,15 @@
 import clsx from 'clsx'
 import { ForwardedRef, forwardRef, HTMLAttributes, PropsWithChildren } from 'react'
 
-type ScreenAlign = 'center' | 'start'
 type ScreenMaxWidth = 'wide' | 'x-wide'
 
 export interface ScreenProps extends PropsWithChildren<HTMLAttributes<HTMLDivElement>> {
-  align?: ScreenAlign
   maxWidth?: ScreenMaxWidth
 }
 
 export const Screen = forwardRef(
-  (
-    { align = 'center', children, className, maxWidth, ...restProps }: ScreenProps,
-    ref: ForwardedRef<HTMLDivElement>,
-  ) => (
-    <div
-      {...restProps}
-      ref={ref}
-      className={clsx('amsterdam-screen', `amsterdam-screen--${align}`, `amsterdam-screen--${maxWidth}`, className)}
-    >
+  ({ children, className, maxWidth = 'wide', ...restProps }: ScreenProps, ref: ForwardedRef<HTMLDivElement>) => (
+    <div {...restProps} ref={ref} className={clsx('amsterdam-screen', `amsterdam-screen--${maxWidth}`, className)}>
       {children}
     </div>
   ),
