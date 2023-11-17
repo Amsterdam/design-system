4c0f45ee5d871b5b89af81b24a4247be23ade866
alimpens <alimpens@users.noreply.github.com>
2023-11-17 13:07:28 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@4b7bdfd7283158b2a5a0d09f1cbea0e65c36ddc2 🚀

---
+++ storybook-react/assets/preview-7d8726ed.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-71d9780a.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-ea9c9e80.js"),["./DocsRenderer-NNNQARDV-ea9c9e80.js","./iframe-71d9780a.js","./index-76fb7be0.js","./_commonjsHelpers-de833af9.js","./react-18-063a39db.js","./index-932496f1.js","./index-30bccf76.js","./index-d37d4223.js","./extends-98964cd2.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-7d8726ed.js.map
