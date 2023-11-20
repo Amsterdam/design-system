fa71429e10bd94b2ee346c0f72df37fb3afc675f
VincentSmedinga <v.smedinga@amsterdam.nl>
2023-11-20 23:09:09 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@1f134bc122bc03712792585c68335e9f40513151 🚀

---
+++ demo-typography-doc-blocks/assets/syntaxhighlighter-NMPM6SWI-9f1c4b6e.js
@@ -0,0 +1,2 @@
+import{S as s,c,s as l}from"./index-d175a39d.js";import"./iframe-fb2c350c.js";import"../sb-preview/runtime.js";import"./index-565f84a4.js";import"./_commonjsHelpers-de833af9.js";import"./index-d37d4223.js";import"./index-356e4a49.js";export{s as SyntaxHighlighter,c as createCopyToClipboardFunction,l as default};
+//# sourceMappingURL=syntaxhighlighter-NMPM6SWI-9f1c4b6e.js.map
---
+++ demo-typography-doc-blocks/storybook-react/assets/Pagination.docs-04ae09d5.js
@@ -0,0 +1,21 @@
+import{j as e}from"./index.esm-9e5b3829.js";import{M as t,d as r,P as o,e as s}from"./index-4837b62a.js";import{P as g}from"./Pagination.stories-59945e26.js";import{u as d}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./extends-98964cd2.js";import"./iframe-940e2877.js";import"../sb-preview/runtime.js";import"./index-932496f1.js";import"./index-d37d4223.js";import"./index-356e4a49.js";const m=`# Pagination
+
+Pagination (in het Nederlands: paginering) is een navigatie-element om tussen meerdere pagina’s met overzichtslijsten te navigeren. Denk hierbij aan zoekresultaten of een lijst met nieuwsartikelen. Bij lange overzichtslijsten kan het duidelijker of functioneler zijn om de inhoud over meerdere pagina’s te verdelen. Paginering laat zien op welke pagina de gebruiker zich bevindt en de gebruiker kan hiermee naar een andere pagina navigeren.
+
+## Richtlijnen
+
+- Gebruik paginering alleen als een overzichtslijst te lang is om op 1 pagina te tonen.
+- Voeg de paginering toe onder de overzichtslijst.
+- De paginering wordt niet getoond als er maar 1 pagina is.
+- Verwijs de gebruikers door naar de eerste pagina als ze een URL opgeven van een paginanummer dat niet (meer) bestaat.
+- De paginering kan gecombineerd worden met een teller bovenaan de pagina die “Pagina # van ##” aanduidt.
+- Start een pagina met een overzichtslijst bovenaan na het veranderen van pagina.
+
+## Relevante WCAG regels
+
+Pagination is een interactief element, hier gelden [de algemene eisen en richtlijnen voor interactieve elementen](https://amsterdam.github.io/design-system/?path=/docs/docs-designrichtlijnen-interactieve-elementen--docs) voor.
+`;function a(n){return e.jsxs(e.Fragment,{children:[e.jsx(t,{of:g}),`
+`,e.jsx(r,{children:m}),`
+`,e.jsx(o,{}),`
+`,e.jsx(s,{})]})}function P(n={}){const{wrapper:i}=Object.assign({},d(),n.components);return i?e.jsx(i,Object.assign({},n,{children:e.jsx(a,n)})):a()}export{P as default};
+//# sourceMappingURL=Pagination.docs-04ae09d5.js.map
