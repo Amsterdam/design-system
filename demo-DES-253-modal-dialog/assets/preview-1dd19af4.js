2bc36905315c11fcd9d78a6a0055b4da4f041b1b
alimpens <runner@fv-az1542-482.uwdtf5zkgycefedjt5dtsy0pzd.bx.internal.cloudapp.net>
2023-11-21 15:54:55 +0000

    Remove feature branch folder

--- demo-DES-253-modal-dialog/assets/preview-1dd19af4.js
+++
@@ -1,2 +0,0 @@
-const o={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},options:{storySort:{order:["Docs",["Intro"]]}},viewMode:"docs"};export{o as parameters};
-//# sourceMappingURL=preview-1dd19af4.js.map
