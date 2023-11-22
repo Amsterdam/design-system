8b06dc057b28d5fea6cc6329823a9600c61643e0
VincentSmedinga <v.smedinga@amsterdam.nl>
2023-11-22 14:25:05 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@31215607c5df6bfbf3e6188ba4f04ada1df29bab 🚀

---
+++ demo-DES-457-update-typography/storybook-react/assets/Footer.stories-58ef534e.js
@@ -0,0 +1,119 @@
+import{t as s,j as e,V as t,H as n,G as i,P as o,U as l,L as r,l as d}from"./index.esm-e58d8537.js";const g={title:"Containers/Footer",component:s},a={args:{children:[e.jsxs(s.Top,{children:[e.jsx(t,{children:e.jsx(n,{children:"Colofon"})}),e.jsxs(i,{children:[e.jsx(i.Cell,{span:3,children:e.jsxs("div",{style:{display:"grid",gap:"2.5rem"},children:[e.jsx(n,{level:2,size:"level-4",inverseColor:!0,children:"Contact"}),e.jsx(o,{size:"small",inverseColor:!0,children:"Heeft u een vraag en kunt u het antwoord niet vinden op deze site? Neem dan contact met ons op."}),e.jsxs(l,{markers:!1,children:[e.jsx("li",{children:e.jsx(r,{href:"mailto:redactie.os@amsterdam.nl",variant:"inList",onBackground:"dark",children:"E-mail"})}),e.jsx("li",{children:e.jsx(r,{href:"tel:+31202510333",variant:"inList",onBackground:"dark",children:"020 251 0333"})})]})]})}),e.jsx(i.Cell,{span:3,start:{narrow:1,medium:5,wide:5},children:e.jsxs("div",{style:{display:"grid",gap:"2.5rem"},children:[e.jsx(n,{level:2,size:"level-4",inverseColor:!0,children:"Panels en enquêtes"}),e.jsx(o,{size:"small",inverseColor:!0,children:"Bent u uitgenodigd om mee te doen aan onderzoek of heeft u vragen over het panel of stadspaspanel?"}),e.jsxs(l,{markers:!1,children:[e.jsx("li",{children:e.jsx(r,{href:"/",variant:"inList",onBackground:"dark",rel:"external",children:"Meedoen aan onderzoek"})}),e.jsx("li",{children:e.jsx(r,{href:"/",variant:"inList",onBackground:"dark",rel:"external",children:"Panel Amsterdam"})}),e.jsx("li",{children:e.jsx(r,{href:"/",variant:"inList",onBackground:"dark",rel:"external",children:"Stadspaspanel Amsterdam"})})]})]})}),e.jsx(i.Cell,{span:3,start:{narrow:1,medium:1,wide:9},children:e.jsxs("div",{style:{display:"grid",gap:"2.5rem"},children:[e.jsx(n,{level:2,size:"level-4",inverseColor:!0,children:"Onderzoek en Statistiek"}),e.jsxs(l,{markers:!1,children:[e.jsx("li",{children:e.jsx(r,{href:"/",variant:"inList",onBackground:"dark",children:"Over Onderzoek en Statistiek"})}),e.jsx("li",{children:e.jsx(r,{href:"/",variant:"inList",onBackground:"dark",children:"Veelgestelde vragen"})}),e.jsx("li",{children:e.jsx(r,{href:"/",variant:"inList",onBackground:"dark",rel:"external",children:"Nieuwsbrief"})}),e.jsx("li",{children:e.jsx(r,{href:"/",variant:"inList",onBackground:"dark",children:"Vacatures"})})]})]})})]})]},"footer-top"),e.jsxs(s.Bottom,{children:[e.jsx(t,{children:e.jsx(n,{level:2,children:"Over deze website"})}),e.jsx(i,{children:e.jsx(i.Cell,{fullWidth:!0,children:e.jsxs(d,{children:[e.jsx(d.Link,{href:"/",children:"Privacy"}),e.jsx(d.Link,{href:"/",children:"Toegankelijkheid"})]})})})]},"footer-bottom")]}};var c,k,h;a.parameters={...a.parameters,docs:{...(c=a.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    children: [<Footer.Top key="footer-top">
+        <VisuallyHidden>
+          <Heading>Colofon</Heading>
+        </VisuallyHidden>
+        <Grid>
+          <Grid.Cell span={3}>
+            <div style={{
+            display: 'grid',
+            gap: '2.5rem'
+          }}>
+              <Heading level={2} size="level-4" inverseColor>
+                Contact
+              </Heading>
+              <Paragraph size="small" inverseColor>
+                Heeft u een vraag en kunt u het antwoord niet vinden op deze site? Neem dan contact met ons op.
+              </Paragraph>
+              <UnorderedList markers={false}>
+                <li>
+                  <Link href="mailto:redactie.os@amsterdam.nl" variant="inList" onBackground="dark">
+                    E-mail
+                  </Link>
+                </li>
+                <li>
+                  <Link href="tel:+31202510333" variant="inList" onBackground="dark">
+                    020 251 0333
+                  </Link>
+                </li>
+              </UnorderedList>
+            </div>
+          </Grid.Cell>
+          <Grid.Cell span={3} start={{
+          narrow: 1,
+          medium: 5,
+          wide: 5
+        }}>
+            <div style={{
+            display: 'grid',
+            gap: '2.5rem'
+          }}>
+              <Heading level={2} size="level-4" inverseColor>
+                Panels en enquêtes
+              </Heading>
+              <Paragraph size="small" inverseColor>
+                Bent u uitgenodigd om mee te doen aan onderzoek of heeft u vragen over het panel of stadspaspanel?
+              </Paragraph>
+              <UnorderedList markers={false}>
+                <li>
+                  <Link href="/" variant="inList" onBackground="dark" rel="external">
+                    Meedoen aan onderzoek
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/" variant="inList" onBackground="dark" rel="external">
+                    Panel Amsterdam
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/" variant="inList" onBackground="dark" rel="external">
+                    Stadspaspanel Amsterdam
+                  </Link>
+                </li>
+              </UnorderedList>
+            </div>
+          </Grid.Cell>
+          <Grid.Cell span={3} start={{
+          narrow: 1,
+          medium: 1,
+          wide: 9
+        }}>
+            <div style={{
+            display: 'grid',
+            gap: '2.5rem'
+          }}>
+              <Heading level={2} size="level-4" inverseColor>
+                Onderzoek en Statistiek
+              </Heading>
+              <UnorderedList markers={false}>
+                <li>
+                  <Link href="/" variant="inList" onBackground="dark">
+                    Over Onderzoek en Statistiek
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/" variant="inList" onBackground="dark">
+                    Veelgestelde vragen
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/" variant="inList" onBackground="dark" rel="external">
+                    Nieuwsbrief
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/" variant="inList" onBackground="dark">
+                    Vacatures
+                  </Link>
+                </li>
+              </UnorderedList>
+            </div>
+          </Grid.Cell>
+        </Grid>
+      </Footer.Top>, <Footer.Bottom key="footer-bottom">
+        <VisuallyHidden>
+          <Heading level={2}>Over deze website</Heading>
+        </VisuallyHidden>
+        <Grid>
+          <Grid.Cell fullWidth>
+            <PageMenu>
+              <PageMenu.Link href="/">Privacy</PageMenu.Link>
+              <PageMenu.Link href="/">Toegankelijkheid</PageMenu.Link>
+            </PageMenu>
+          </Grid.Cell>
+        </Grid>
+      </Footer.Bottom>]
+  }
+}`,...(h=(k=a.parameters)==null?void 0:k.docs)==null?void 0:h.source}}};const u=["Default"],m=Object.freeze(Object.defineProperty({__proto__:null,Default:a,__namedExportsOrder:u,default:g},Symbol.toStringTag,{value:"Module"}));export{m as F};
+//# sourceMappingURL=Footer.stories-58ef534e.js.map
---
+++ demo-DES-457-update-typography/storybook-react/assets/preview-d372d7a2.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-14e1f117.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-0bc85d9d.js"),["./DocsRenderer-NNNQARDV-0bc85d9d.js","./iframe-14e1f117.js","./index-76fb7be0.js","./_commonjsHelpers-de833af9.js","./react-18-063a39db.js","./index-932496f1.js","./index-b2f4b10d.js","./index-d37d4223.js","./extends-98964cd2.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-d372d7a2.js.map
