a304c57e346ddc210a0c9271a03fa7469f8494c8
VincentSmedinga <v.smedinga@amsterdam.nl>
2023-11-22 15:27:01 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@ad6efd9628ba13d59eb9141b788230328656c0f5 🚀

--- demo-grid-sizing-docs/assets/preview-0b17e205.js
+++
@@ -1,2 +0,0 @@
-import{_ as e}from"./iframe-022850ab.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-f5ec7713.js"),["./DocsRenderer-NNNQARDV-f5ec7713.js","./iframe-022850ab.js","./index-565f84a4.js","./_commonjsHelpers-de833af9.js","./react-18-e033092d.js","./index-4bfc7ddc.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
-//# sourceMappingURL=preview-0b17e205.js.map
