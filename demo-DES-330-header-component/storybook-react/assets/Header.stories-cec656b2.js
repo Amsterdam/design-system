64056c4a54a4f9249cc40cb80d887fc575b6f4a4
dlnr <niels@dlnr.dev>
2023-11-23 14:50:58 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@950b5a5df0653364500521dd1637262d35e4a6b3 🚀

--- demo-DES-330-header-component/storybook-react/assets/Header.stories-cec656b2.js
+++
@@ -1,5 +0,0 @@
-import{u as a}from"./index.esm-f7521f41.js";/**
- * @license EUPL-1.2+
- * Copyright (c) 2023 Gemeente Amsterdam
- */const s={title:"Header",component:a,args:{children:"Nieuw component"}},e={};var r,o,t;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:"{}",...(t=(o=e.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};const c=["Default"],d=Object.freeze(Object.defineProperty({__proto__:null,Default:e,__namedExportsOrder:c,default:s},Symbol.toStringTag,{value:"Module"}));export{d as H};
-//# sourceMappingURL=Header.stories-cec656b2.js.map
