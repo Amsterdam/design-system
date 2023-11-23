60df135f202be78c1a9480685d5fcbd08493ca59
dlnr <niels@dlnr.dev>
2023-11-23 15:12:57 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@db020025aef6ed9847fe6b7e7427a3bcd6a1c19a 🚀

--- demo-DES-330-header-component/storybook-react/assets/preview-dfef63df.js
+++
@@ -1,2 +0,0 @@
-import{_ as e}from"./iframe-260dc5da.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-bff49350.js"),["./DocsRenderer-NNNQARDV-bff49350.js","./iframe-260dc5da.js","./index-76fb7be0.js","./_commonjsHelpers-de833af9.js","./react-18-063a39db.js","./index-932496f1.js","./index-21dcc4d6.js","./index-d37d4223.js","./extends-98964cd2.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
-//# sourceMappingURL=preview-dfef63df.js.map
