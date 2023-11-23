62bb972a8ebcf2903e5814d1eb9b6556ff6e2850
VincentSmedinga <v.smedinga@amsterdam.nl>
2023-11-23 09:24:17 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@7722de938180de91f6c1912ea36aeaae028b7e37 🚀

---
+++ demo-DES-457-update-typography/storybook-react/assets/preview-d95905c8.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-558320d4.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-4e9389d2.js"),["./DocsRenderer-NNNQARDV-4e9389d2.js","./iframe-558320d4.js","./index-76fb7be0.js","./_commonjsHelpers-de833af9.js","./react-18-063a39db.js","./index-932496f1.js","./index-8b6d63dd.js","./index-d37d4223.js","./extends-98964cd2.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-d95905c8.js.map
