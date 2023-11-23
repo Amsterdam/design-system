72bfcd9a2f37837cef99264560b0c39bac0a1053
Vincent Smedinga <v.smedinga@amsterdam.nl>
2023-11-23 09:38:25 +0100

    Simplify viewports in Storybook and match Figma (#805)

--- storybook/storybook-react/config/viewports.ts
+++ storybook/storybook-react/config/viewports.ts
@@ -1,75 +1,27 @@
 import { ViewportMap } from './types'
 
 export const viewports: ViewportMap = {
-  phoneSm: {
-    name: 'Small phone',
+  phone: {
+    name: 'Phone',
     styles: {
-      height: '812px',
-      width: '375px',
+      height: '680px',
+      width: '320px',
     },
     type: 'mobile',
   },
-  phoneMd: {
-    name: 'Medium phone',
-    styles: {
-      height: '844px',
-      width: '390px',
-    },
-    type: 'mobile',
-  },
-  phoneLg: {
-    name: 'Large phone',
-    styles: {
-      height: '926px',
-      width: '428px',
-    },
-    type: 'mobile',
-  },
-  tabletSm: {
-    name: 'Small tablet',
-    styles: {
-      height: '768px',
-      width: '1024px',
-    },
-    type: 'tablet',
-  },
-  tabletMd: {
-    name: 'Medium tablet',
-    styles: {
-      height: '800px',
-      width: '1280px',
-    },
-    type: 'tablet',
-  },
-  tabletLg: {
-    name: 'Large tablet',
-    styles: {
-      height: '1024px',
-      width: '1366px',
-    },
-    type: 'tablet',
-  },
-  desktopSm: {
-    name: 'Small desktop',
-    styles: {
-      height: '900px',
-      width: '1440px',
-    },
-    type: 'desktop',
-  },
-  desktopMd: {
-    name: 'Medium desktop',
+  tablet: {
+    name: 'Tablet',
     styles: {
       height: '1080px',
-      width: '1920px',
+      width: '832px',
     },
-    type: 'desktop',
+    type: 'tablet',
   },
-  desktopLg: {
-    name: 'Large desktop',
+  desktop: {
+    name: 'Desktop',
     styles: {
-      height: '1440px',
-      width: '2560px',
+      height: '1080px',
+      width: '1600px',
     },
     type: 'desktop',
   },
