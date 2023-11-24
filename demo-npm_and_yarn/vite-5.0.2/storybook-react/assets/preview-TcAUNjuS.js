faec0613e1139b30fcd9e4911b0b4bd63a1960ae
alimpens <runner@fv-az738-302.ayi0s4js3kfu5apuyubnvt3std.cx.internal.cloudapp.net>
2023-11-24 13:19:24 +0000

    Remove feature branch folder

--- demo-npm_and_yarn/vite-5.0.2/storybook-react/assets/preview-TcAUNjuS.js
+++
@@ -1,8 +0,0 @@
-import{_ as e}from"./iframe-Qeq-DOja.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-fU9Y71Y6.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9]),import.meta.url);return new r}}};export{a as parameters};
-//# sourceMappingURL=preview-TcAUNjuS.js.map
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-fU9Y71Y6.js","./iframe-Qeq-DOja.js","./index-4g5l5LRQ.js","./_commonjsHelpers-4gQjN7DL.js","./react-18-6f2S3HzO.js","./index-f2OjG03D.js","./index--oTw7p_Q.js","./index-aKxH7ZY0.js","./extends-dGVwEr9R.js","./index-PPLHz8o0.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
