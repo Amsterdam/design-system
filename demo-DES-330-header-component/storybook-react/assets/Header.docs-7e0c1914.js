3a520614faaa50e046c81ef781bcc61451023576
dlnr <niels@dlnr.dev>
2023-11-24 12:05:15 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@1f5d146f8cce6293f4fd1bca9b13a86761437305 🚀

---
+++ demo-DES-330-header-component/storybook-react/assets/Header.docs-7e0c1914.js
@@ -0,0 +1,14 @@
+import{j as t}from"./index.esm-92970298.js";import{M as r,d as a,P as h,e as m,f as o}from"./index-4974506f.js";import{H as c,W as d,a as j,b as x,c as l}from"./Header.stories-c0e497a5.js";import{u as s}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./extends-98964cd2.js";import"./iframe-291c03de.js";import"../sb-preview/runtime.js";import"./index-932496f1.js";import"./index-d37d4223.js";import"./index-356e4a49.js";const p=`# Header
+`;function e(i){const n=Object.assign({h2:"h2"},s(),i.components);return t.jsxs(t.Fragment,{children:[t.jsx(r,{of:c}),`
+`,t.jsx(a,{children:p}),`
+`,t.jsx(h,{}),`
+`,t.jsx(m,{}),`
+`,t.jsx(n.h2,{id:"with-site-title",children:"With Site Title"}),`
+`,t.jsx(o,{of:d}),`
+`,t.jsx(n.h2,{id:"with-logo-variant",children:"With logo variant"}),`
+`,t.jsx(o,{of:j}),`
+`,t.jsx(n.h2,{id:"with-header-menu",children:"With header menu"}),`
+`,t.jsx(o,{of:x}),`
+`,t.jsx(n.h2,{id:"with-both",children:"With both"}),`
+`,t.jsx(o,{of:l})]})}function O(i={}){const{wrapper:n}=Object.assign({},s(),i.components);return n?t.jsx(n,Object.assign({},i,{children:t.jsx(e,i)})):e(i)}export{O as default};
+//# sourceMappingURL=Header.docs-7e0c1914.js.map
--- demo-DES-330-header-component/storybook-react/assets/preview-33251203.js
+++
@@ -1,2 +0,0 @@
-import{_ as e}from"./iframe-7125b2dd.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-eff65b2c.js"),["./DocsRenderer-NNNQARDV-eff65b2c.js","./iframe-7125b2dd.js","./index-76fb7be0.js","./_commonjsHelpers-de833af9.js","./react-18-063a39db.js","./index-932496f1.js","./index-6ec60645.js","./index-d37d4223.js","./extends-98964cd2.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
-//# sourceMappingURL=preview-33251203.js.map
