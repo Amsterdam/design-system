47f44851f95c6ff2a3e2dbab29fe6fdd442bf210
VincentSmedinga <v.smedinga@amsterdam.nl>
2023-11-25 00:37:46 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@942a56047f0091ef4de9fefbba20583eb56a6105 🚀

---
+++ demo-use-highlight-in-footer/assets/preview-QX8XRWGL.js
@@ -0,0 +1,8 @@
+import{_ as e}from"./iframe-gF6DPlNT.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-P3337vKU.js"),__vite__mapDeps([0,1,2,3,4,5,6,7]),import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-QX8XRWGL.js.map
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-P3337vKU.js","./iframe-gF6DPlNT.js","./index-7RSFs7Cb.js","./_commonjsHelpers-4gQjN7DL.js","./react-18-e2qAuQ2N.js","./index-yahOlxAr.js","./index-aKxH7ZY0.js","./index-PPLHz8o0.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
---
+++ demo-use-highlight-in-footer/storybook-react/assets/FormLabel.docs-KXKoXmvy.js
@@ -0,0 +1,5 @@
+import{j as e}from"./index.esm-0rYbzZDP.js";import{M as o,d as s,P as m,e as a}from"./index-T1kIN69d.js";import{F as i}from"./FormLabel.stories-i2zo1VqZ.js";import{u as l}from"./index-Dbo06S9W.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./extends-dGVwEr9R.js";import"./iframe-I4AcM3UE.js";import"../sb-preview/runtime.js";import"./index-f2OjG03D.js";import"./index-aKxH7ZY0.js";import"./index-PPLHz8o0.js";const p="# Form Label\n\nEen label element gebruik je om een formulierelement te beschrijven. Koppel een formulierelement (zoals een `input` of `textarea`) altijd aan een label.\n";function r(t){return e.jsxs(e.Fragment,{children:[e.jsx(o,{of:i}),`
+`,e.jsx(s,{children:p}),`
+`,e.jsx(m,{}),`
+`,e.jsx(a,{})]})}function E(t={}){const{wrapper:n}=Object.assign({},l(),t.components);return n?e.jsx(n,Object.assign({},t,{children:e.jsx(r,t)})):r()}export{E as default};
+//# sourceMappingURL=FormLabel.docs-KXKoXmvy.js.map
