55d999bdd007927c16e354253aa7f1bf1700b6c2
VincentSmedinga <v.smedinga@amsterdam.nl>
2023-11-26 17:41:40 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@e4c5ff425944847046bd2bc491ae2620b1a1145e 🚀

---
+++ demo-DES-457-update-typography/assets/typography.stories-FgZ40pFb.js
@@ -0,0 +1,125 @@
+import{M as l,T as t}from"./index-h8sTZtSt.js";import{j as e}from"./jsx-runtime-0GPuVWNr.js";import{u as a}from"./index-MVO-xk_1.js";import"./iframe-j6vzvQcQ.js";import"../sb-preview/runtime.js";import"./index-7RSFs7Cb.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./index-aKxH7ZY0.js";import"./index-PPLHz8o0.js";function s(i){const n=Object.assign({h1:"h1",h2:"h2",p:"p",h3:"h3",a:"a",h4:"h4",table:"table",thead:"thead",tr:"tr",th:"th",tbody:"tbody",td:"td",ol:"ol",li:"li",em:"em",code:"code",pre:"pre",ul:"ul"},a(),i.components);return e.jsxs(e.Fragment,{children:[e.jsx(l,{title:"Docs/Designrichtlijnen/Typografie"}),`
+`,e.jsx(n.h1,{id:"typografie",children:"Typografie"}),`
+`,e.jsx(n.h2,{id:"lettertype",children:"Lettertype"}),`
+`,e.jsx(n.p,{children:"De gemeente gebruikt de Amsterdam Sans als beeldbepalend lettertype."}),`
+`,e.jsx(n.p,{children:`Dat is een letterfamilie met een helder, open tekstbeeld dat goed bij de Andreaskruisen past.
+Het lettertype is zowel offline als online goed leesbaar.
+Daarom wordt het in alle communicatiemiddelen ingezet.`}),`
+`,e.jsx(n.p,{children:"Deze zin demonstreert alle letters van het Latijnse alfabet en cijfers in de Amsterdam Sans:"}),`
+`,e.jsx(t,{fontFamily:"Amsterdam Sans, Arial, sans-serif",fontSizes:[24],fontWeight:400,sampleText:"Jouw typograaf biedt mij zulke exquise schreven 012345689"}),`
+`,e.jsx(n.h2,{id:"tekstgrootte",children:"Tekstgrootte"}),`
+`,e.jsx(n.h3,{id:"acht-tekstniveaus",children:"Acht tekstniveau’s"}),`
+`,e.jsx(n.p,{children:`Er zijn 8 niveaus van tekstgrootte en bijbehorende regelhoogte.
+Elk typografisch element zit op 1 zo’n niveau: titel, paragraaf, link, citaat, formuliertekst, enzovoort.
+Ze zijn genummerd van 0 tot en met 7 – zo komen ze voor de diverse titels mooi overeen.
+Voorbeelden:`}),`
+`,e.jsxs("ol",{start:0,children:[e.jsx("li",{children:"Page heading"}),e.jsx("li",{children:"Heading 1"}),e.jsx("li",{children:"Heading 2"}),e.jsx("li",{children:"Heading 3, Blockquote"}),e.jsx("li",{children:"Heading 4, Top Task Link Title"}),e.jsx("li",{children:"Large Paragraph, Alert Title"}),e.jsx("li",{children:"Paragraph, Button"}),e.jsx("li",{children:"Small Paragraph, Link List, Form Label, Top Task Link Description, Breadcrumb"})]}),`
+`,e.jsx(n.h3,{id:"responsive",children:"Responsive"}),`
+`,e.jsxs(n.p,{children:[`De grootte van tekst is afhankelijk van de breedte van het venster.
+Hiermee zorgen we ervoor dat het op schermen van alle afmetingen prettig lezen is.
+Ook werkt dat goed samen met het responsive `,e.jsx(n.a,{href:"?path=/docs/react_layout-grid--docs",children:"grid"}),"."]}),`
+`,e.jsx(n.p,{children:`Elk niveau heeft een minimale tekstgrootte.
+Dat is actief tot een vensterbreedte van 320 pixels.`}),`
+`,e.jsx(n.p,{children:`Ook is er een maximale tekstgrootte.
+Dat is van toepassing vanaf een vensterbreedte van 1600 pixels.`}),`
+`,e.jsx(n.p,{children:"Daartussen groeit de tekstgrootte in een rechte lijn."}),`
+`,e.jsx(n.h3,{id:"typografische-schaal",children:"Typografische schaal"}),`
+`,e.jsx(n.p,{children:`De typografische schaal is gebaseerd op een factor van 5 ÷ 4 = 1,25.
+Voor elk naastliggende niveau is de tekst dus zo veel keer groter of kleiner.`}),`
+`,e.jsx(n.p,{children:`Bij de minimale vensterbreedte groeit de tekstgrootte voor de niveau’s 0 tot en met 4 langzamer, met een factor van 9 ÷ 8 = 1,125.
+Zo worden titels niet te groot op telefoons en andere smalle vensters.`}),`
+`,e.jsx(n.p,{children:"De getallen die uit deze systematiek rollen zijn niet afgerond op ronde getallen."}),`
+`,e.jsx(n.h3,{id:"twee-themas-voor-websites-en-applicaties",children:"Twee thema’s voor websites en applicaties"}),`
+`,e.jsx(n.h4,{id:"ruimtelijk",children:"Ruimtelijk"}),`
+`,e.jsx(n.p,{children:`Voor websites is de typografie behoorlijk ruimtelijk.
+Zo is de huisstijl ontworpen.`}),`
+`,e.jsx(n.p,{children:`Een standaard paragraaf is 16 pixels groot bij een vensterbreedte van 320 pixels.
+In een venster van 1600 pixels breed is dat de helft meer: 24 pixels.`}),`
+`,e.jsx(n.p,{children:"De maximale tekstgroottes voor alle niveau’s in het ruimtelijke thema:"}),`
+`,e.jsx(t,{fontFamily:"Amsterdam Sans, Arial, sans-serif",fontSizes:[73.2,58.6,46.9,37.5,30,30,24,19.2],fontWeight:400,sampleText:"Jouw typograaf biedt mij zulke exquise schreven"}),`
+`,e.jsx(n.h4,{id:"compact",children:"Compact"}),`
+`,e.jsx(n.p,{children:`Voor applicaties is zulke grote tekst niet nodig, zelfs contraproductief.
+Daarom is er een compact thema voor de typografie.`}),`
+`,e.jsx(n.p,{children:`Hier is een paragraaf 14 pixels groot bij de minimale vensterbreedte en 18 pixels bij de maximale.
+Een paragraaf groeit hier dus niet met de helft, maar met iets meer dan een kwart.`}),`
+`,e.jsx(n.p,{children:"De maximale tekstgroottes voor alle niveau’s in het compacte thema:"}),`
+`,e.jsx(t,{fontFamily:"Amsterdam Sans, Arial, sans-serif",fontSizes:[54.9,43.9,35.2,28.1,22.5,22.5,18,14.4],fontWeight:400,sampleText:"Jouw typograaf biedt mij zulke exquise schreven"}),`
+`,e.jsx(n.h3,{id:"referentie-breedtes-voor-ontwerpers",children:"Referentie-breedtes voor ontwerpers"}),`
+`,e.jsx(n.p,{children:`In Figma werken we met drie referentie-breedtes.
+Op die breedtes werken we het ontwerp van een scherm uit voor die drie klassen van apparaten.
+Op deze breedtes is de maatvoering als volgt, afgerond op 1 cijfer achter de komma.`}),`
+`,e.jsx(n.h4,{id:"ruimtelijk-1",children:"Ruimtelijk"}),`
+`,e.jsxs(n.table,{children:[e.jsx(n.thead,{children:e.jsxs(n.tr,{children:[e.jsx(n.th,{align:"left",children:"Klasse apparaat"}),e.jsx(n.th,{align:"left",children:"Referentie-breedte"}),e.jsx(n.th,{align:"right",children:"0"}),e.jsx(n.th,{align:"right",children:"1"}),e.jsx(n.th,{align:"right",children:"2"}),e.jsx(n.th,{align:"right",children:"3"}),e.jsx(n.th,{align:"right",children:"4"}),e.jsx(n.th,{align:"right",children:"5"}),e.jsx(n.th,{align:"right",children:"6"}),e.jsx(n.th,{align:"right",children:"7"})]})}),e.jsxs(n.tbody,{children:[e.jsxs(n.tr,{children:[e.jsx(n.td,{align:"left",children:"telefoon"}),e.jsx(n.td,{align:"left",children:"320"}),e.jsx(n.td,{align:"right",children:"28,8"}),e.jsx(n.td,{align:"right",children:"25,6"}),e.jsx(n.td,{align:"right",children:"22,8"}),e.jsx(n.td,{align:"right",children:"20,3"}),e.jsx(n.td,{align:"right",children:"18"}),e.jsx(n.td,{align:"right",children:"20"}),e.jsx(n.td,{align:"right",children:"16"}),e.jsx(n.td,{align:"right",children:"12,8"})]}),e.jsxs(n.tr,{children:[e.jsx(n.td,{align:"left",children:"tablet"}),e.jsx(n.td,{align:"left",children:"832"}),e.jsx(n.td,{align:"right",children:"49,3"}),e.jsx(n.td,{align:"right",children:"40,8"}),e.jsx(n.td,{align:"right",children:"33,9"}),e.jsx(n.td,{align:"right",children:"28,2"}),e.jsx(n.td,{align:"right",children:"23,5"}),e.jsx(n.td,{align:"right",children:"24,6"}),e.jsx(n.td,{align:"right",children:"19,7"}),e.jsx(n.td,{align:"right",children:"15,7"})]}),e.jsxs(n.tr,{children:[e.jsx(n.td,{align:"left",children:"desktop"}),e.jsx(n.td,{align:"left",children:"1600"}),e.jsx(n.td,{align:"right",children:"73,2"}),e.jsx(n.td,{align:"right",children:"58,6"}),e.jsx(n.td,{align:"right",children:"46,9"}),e.jsx(n.td,{align:"right",children:"37,5"}),e.jsx(n.td,{align:"right",children:"30"}),e.jsx(n.td,{align:"right",children:"30"}),e.jsx(n.td,{align:"right",children:"24"}),e.jsx(n.td,{align:"right",children:"19,2"})]})]})]}),`
+`,e.jsx(n.h4,{id:"compact-1",children:"Compact"}),`
+`,e.jsxs(n.table,{children:[e.jsx(n.thead,{children:e.jsxs(n.tr,{children:[e.jsx(n.th,{align:"left",children:"Klasse apparaat"}),e.jsx(n.th,{align:"left",children:"Referentie-breedte"}),e.jsx(n.th,{align:"right",children:"0"}),e.jsx(n.th,{align:"right",children:"1"}),e.jsx(n.th,{align:"right",children:"2"}),e.jsx(n.th,{align:"right",children:"3"}),e.jsx(n.th,{align:"right",children:"4"}),e.jsx(n.th,{align:"right",children:"5"}),e.jsx(n.th,{align:"right",children:"6"}),e.jsx(n.th,{align:"right",children:"7"})]})}),e.jsxs(n.tbody,{children:[e.jsxs(n.tr,{children:[e.jsx(n.td,{align:"left",children:"telefoon"}),e.jsx(n.td,{align:"left",children:"320"}),e.jsx(n.td,{align:"right",children:"25,2"}),e.jsx(n.td,{align:"right",children:"22,4"}),e.jsx(n.td,{align:"right",children:"19,9"}),e.jsx(n.td,{align:"right",children:"17,7"}),e.jsx(n.td,{align:"right",children:"15,8"}),e.jsx(n.td,{align:"right",children:"17,5"}),e.jsx(n.td,{align:"right",children:"14,0"}),e.jsx(n.td,{align:"right",children:"11,2"})]}),e.jsxs(n.tr,{children:[e.jsx(n.td,{align:"left",children:"tablet"}),e.jsx(n.td,{align:"left",children:"832"}),e.jsx(n.td,{align:"right",children:"38,8"}),e.jsx(n.td,{align:"right",children:"32,3"}),e.jsx(n.td,{align:"right",children:"26,9"}),e.jsx(n.td,{align:"right",children:"22,5"}),e.jsx(n.td,{align:"right",children:"18,9"}),e.jsx(n.td,{align:"right",children:"19,8"}),e.jsx(n.td,{align:"right",children:"15,8"}),e.jsx(n.td,{align:"right",children:"12,7"})]}),e.jsxs(n.tr,{children:[e.jsx(n.td,{align:"left",children:"desktop"}),e.jsx(n.td,{align:"left",children:"1600"}),e.jsx(n.td,{align:"right",children:"54,9"}),e.jsx(n.td,{align:"right",children:"43,9"}),e.jsx(n.td,{align:"right",children:"35,2"}),e.jsx(n.td,{align:"right",children:"28,1"}),e.jsx(n.td,{align:"right",children:"22,5"}),e.jsx(n.td,{align:"right",children:"22,5"}),e.jsx(n.td,{align:"right",children:"18,0"}),e.jsx(n.td,{align:"right",children:"14,4"})]})]})]}),`
+`,e.jsx(n.h2,{id:"accentuering",children:"Accentuering"}),`
+`,e.jsx(n.p,{children:`Er zijn verschillende manieren om tekst te accentueren.
+Hierin hebben we de volgende keuzes gemaakt.`}),`
+`,e.jsx(n.h3,{id:"gewicht",children:"Gewicht"}),`
+`,e.jsx(n.p,{children:"Online gebruiken we 2 gewichten: ‘regular’ en ‘extra bold’."}),`
+`,e.jsx(t,{fontFamily:"Amsterdam Sans, Arial, sans-serif",fontSizes:[24],fontWeight:400,sampleText:"Jouw typograaf biedt mij zulke exquise schreven"}),`
+`,e.jsx(n.p,{children:"Tekst wordt vetgedrukt in titels en decoratieve citaten."}),`
+`,e.jsx(t,{fontFamily:"Amsterdam Sans, Arial, sans-serif",fontSizes:[24],fontWeight:800,sampleText:"Jouw typograaf biedt mij zulke exquise schreven"}),`
+`,e.jsx(n.h3,{id:"stijl",children:"Stijl"}),`
+`,e.jsx(n.p,{children:`Gebruik de cursieve weergave uitsluitend om fragmenten in tekst te accentueren.
+Cursieve tekst is minder goed leesbaar dan reguliere of vetgedrukte tekst.`}),`
+`,e.jsx(n.p,{children:`Pas het niet toe op hele tekstblokken.
+Er zijn betere manieren om lange teksten nadruk te geven.`}),`
+`,e.jsx(t,{fontFamily:"Amsterdam Sans, Arial, sans-serif",fontSizes:[24],sampleText:"Jouw typograaf biedt mij zulke exquise schreven",style:{fontStyle:"italic"}}),`
+`,e.jsx(n.h3,{id:"onderstreping",children:"Onderstreping"}),`
+`,e.jsx(n.p,{children:`Uitsluitend de tekst van een hyperlink wordt onderstreept.
+Rond staartletters wordt de lijn onderbroken voor betere leesbaarheid.`}),`
+`,e.jsx(n.p,{children:`Onderstreepte tekst zonder hyperlink geeft toch de suggestie van een link.
+Dat is verwarrend en daarom niet toegestaan.
+Gebruik een andere manier om te accentueren.`}),`
+`,e.jsx(t,{fontFamily:"Amsterdam Sans, Arial, sans-serif",fontSizes:[24],sampleText:"Jouw typograaf biedt mij zulke exquise schreven",style:{color:"#004699",textDecoration:"underline"}}),`
+`,e.jsx(n.h3,{id:"overhang",children:"Overhang"}),`
+`,e.jsx(n.p,{children:`De ruimte tussen letters (kerning) is geoptimaliseerd in het lettertype.
+Het is niet toegestaan om de kerning aan te passen.`}),`
+`,e.jsx(n.h2,{id:"andere-eigenschappen",children:"Andere eigenschappen"}),`
+`,e.jsx(n.h3,{id:"contrast",children:"Contrast"}),`
+`,e.jsxs(n.p,{children:["Het contrast tussen tekst en achtergrondkleur moet voldoen aan de laatste wettelijke toegankelijkheidseisen van ",e.jsx(n.a,{href:"https://www.w3.org/WAI/WCAG21/Understanding/contrast-minimum.html",target:"_blank",rel:"nofollow noopener noreferrer",children:"de WCAG, niveau AA"}),"."]}),`
+`,e.jsx(n.p,{children:`De meeste tekst in het design system is puur zwart op puur wit en dat voldoet daar ruim aan.
+Op de meeste donkere achtergronden maken we tekst wit; ook dat biedt voldoende contrast.
+Voor sommige achtergrondkleuren is ook zwarte tekst goed leesbaar.
+Raadpleeg de betreffende componenten voor specifieke richtlijnen.`}),`
+`,e.jsx(n.h3,{id:"uitlijning",children:"Uitlijning"}),`
+`,e.jsx(n.p,{children:"Tekst wordt nagenoeg altijd links uitgelijnd."}),`
+`,e.jsx(n.p,{children:"Uitzonderingen:"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Het label van een knop wordt gecentreerd."}),`
+`,e.jsx(n.li,{children:"In een tabel kan het beter zijn tekst te centreren of rechts uit te lijnen."}),`
+`]}),`
+`,e.jsx(n.h3,{id:"woordafbreking",children:"Woordafbreking"}),`
+`,e.jsxs(n.p,{children:[`De Nederlandse taal kent veel lange woorden, zoals ‘gemeenteraadsverkiezingen’.
+Zorg er in je applicatie voor dat deze woorden netjes afgebroken worden en niet uit hun container breken of verborgen worden.
+Een manier om dat te doen is met de volgende `,e.jsx(n.em,{children:"css snippet"}),", waarin ",e.jsx(n.code,{children:"hyphens"})," en ",e.jsx(n.code,{children:"word-break"})," gecombineerd worden:"]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-css",children:`.class {
+  hyphens: auto;
+  /* only hypenate words longer than 12 chars, for browsers that support it */
+  hyphenate-limit-chars: 12;
+  /* break overflowing words for browsers that don't support css hyphens */
+  overflow-wrap: break-word;
+}
+`})}),`
+`,e.jsxs(n.p,{children:["Voor verdere informatie: ",e.jsx(n.a,{href:"https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_text/Wrapping_breaking_text",target:"_blank",rel:"nofollow noopener noreferrer",children:"Wrapping and breaking text op MDN"})]}),`
+`,e.jsx(n.h2,{id:"het-lettertype-installeren",children:"Het lettertype installeren"}),`
+`,e.jsx(n.p,{children:`Als je de React-componenten gebruikt hoef je niets te doen.
+Deze gebruiken allemaal het juiste lettertype.`}),`
+`,e.jsxs(n.p,{children:["Werk je met andere technologie, maar wel met npm, dan installeer je ",e.jsx(n.code,{children:"@amsterdam/design-system-assets"})," en importeer je ",e.jsx(n.code,{children:"@amsterdam/design-system-assets/font/index.css"}),"."]}),`
+`,e.jsx(n.p,{children:`Ook kun je het lettertype zelf hosten en opnemen in je applicatie, vraag het dan aan via het formulier hieronder.
+Als het helemaal niet mogelijk is Amsterdam Sans te gebruiken pas je het lettertype Arial toe, of het generieke sans-serif lettertype van het apparaat.
+In alle gevallen voer je die alternatieven op voor als het downloaden van het font voor de gebruiker niet lukt, of terwijl dat bezig is:`}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-css",children:`.class {
+  font-family: "Amsterdam Sans", Arial, sans-serif;
+}
+`})}),`
+`,e.jsx(n.h3,{id:"licentie-en-aanvraag",children:"Licentie en aanvraag"}),`
+`,e.jsx(n.p,{children:`Het lettertype is uitdrukkelijk niet vrij beschikbaar.
+Amsterdam Sans is uitsluitend te gebruiken door medewerkers van de gemeente Amsterdam en amsterdam&partners, en door leveranciers werkend voor deze organisaties.`}),`
+`,e.jsxs(n.p,{children:["Vraag de lettertypebestanden aan via het formulier ",e.jsx(n.a,{href:"https://formulier.amsterdam.nl/thema/huisstijl/amsterdam-sans-aanvragen",target:"_blank",rel:"nofollow noopener noreferrer",children:"Amsterdam Sans aanvragen"}),"."]}),`
+`,e.jsx(n.h2,{id:"meer-informatie",children:"Meer informatie"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["Alle informatie over het lettertype vind je op de pagina ",e.jsx(n.a,{href:"https://amsterdam.nl/stijlweb/basiselementen/typografie/",target:"_blank",rel:"nofollow noopener noreferrer",children:"Typografie op Stijlweb"}),"."]}),`
+`]})]})}function d(i={}){const{wrapper:n}=Object.assign({},a(),i.components);return n?e.jsx(n,{...i,children:e.jsx(s,{...i})}):s(i)}const o=()=>{throw new Error("Docs-only story")};o.parameters={docsOnly:!0};const r={title:"Docs/Designrichtlijnen/Typografie",tags:["stories-mdx"],includeStories:["__page"]};r.parameters=r.parameters||{};r.parameters.docs={...r.parameters.docs||{},page:d};const u=["__page"];export{u as __namedExportsOrder,o as __page,r as default};
+//# sourceMappingURL=typography.stories-FgZ40pFb.js.map
--- demo-DES-457-update-typography/storybook-react/assets/FormLabel.docs-uKyx1QLF.js
+++
@@ -1,5 +0,0 @@
-import{j as e}from"./index.esm-goQfuVmR.js";import{M as o,d as s,P as m,e as a}from"./index-lYsHZeH9.js";import{F as i}from"./FormLabel.stories-o0mqCfvT.js";import{u as l}from"./index-Dbo06S9W.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./extends-dGVwEr9R.js";import"./iframe-DNYWBIo8.js";import"../sb-preview/runtime.js";import"./index-f2OjG03D.js";import"./index-aKxH7ZY0.js";import"./index-PPLHz8o0.js";const p="# Form Label\n\nEen label element gebruik je om een formulierelement te beschrijven. Koppel een formulierelement (zoals een `input` of `textarea`) altijd aan een label.\n";function r(t){return e.jsxs(e.Fragment,{children:[e.jsx(o,{of:i}),`
-`,e.jsx(s,{children:p}),`
-`,e.jsx(m,{}),`
-`,e.jsx(a,{})]})}function E(t={}){const{wrapper:n}=Object.assign({},l(),t.components);return n?e.jsx(n,Object.assign({},t,{children:e.jsx(r,t)})):r()}export{E as default};
-//# sourceMappingURL=FormLabel.docs-uKyx1QLF.js.map
