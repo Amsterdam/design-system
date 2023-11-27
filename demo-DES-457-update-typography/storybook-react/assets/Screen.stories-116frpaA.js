7260e06a14316e8b618090a19d00f5ff7d0b651c
VincentSmedinga <v.smedinga@amsterdam.nl>
2023-11-27 20:30:32 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@e9cbd94cb4a6a4994d3fc7daf6d9c94042985e20 🚀

---
+++ demo-DES-457-update-typography/storybook-react/assets/Screen.stories-116frpaA.js
@@ -0,0 +1,11 @@
+import{i as n,j as d}from"./index.esm-ZOrJeqrL.js";const p={title:"Layout/Screen",component:n,parameters:{layout:"fullscreen"}},e={args:{children:d.jsx("p",{className:"amsterdam-docs-paragraph amsterdam-docs-pink-box",children:"Ik pas in het scherm."})}},a={args:{children:d.jsx("p",{className:"amsterdam-docs-paragraph amsterdam-docs-pink-box",children:"Ik pas in een extra breed scherm."}),maxWidth:"x-wide"}};var r,s,t;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    children: <p className="amsterdam-docs-paragraph amsterdam-docs-pink-box">Ik pas in het scherm.</p>
+  }
+}`,...(t=(s=e.parameters)==null?void 0:s.docs)==null?void 0:t.source}}};var o,c,m;a.parameters={...a.parameters,docs:{...(o=a.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    children: <p className="amsterdam-docs-paragraph amsterdam-docs-pink-box">Ik pas in een extra breed scherm.</p>,
+    maxWidth: 'x-wide'
+  }
+}`,...(m=(c=a.parameters)==null?void 0:c.docs)==null?void 0:m.source}}};const i=["Default","ExtraWide"],h=Object.freeze(Object.defineProperty({__proto__:null,Default:e,ExtraWide:a,__namedExportsOrder:i,default:p},Symbol.toStringTag,{value:"Module"}));export{a as E,h as S};
+//# sourceMappingURL=Screen.stories-116frpaA.js.map
---
+++ demo-DES-457-update-typography/storybook-react/assets/preview-0TLxtGHU.js
@@ -0,0 +1,8 @@
+import{_ as e}from"./iframe-CWpg-YOr.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-wRDM_7vQ.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9]),import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-0TLxtGHU.js.map
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-wRDM_7vQ.js","./iframe-CWpg-YOr.js","./index-4g5l5LRQ.js","./_commonjsHelpers-4gQjN7DL.js","./react-18-6f2S3HzO.js","./index-f2OjG03D.js","./index-lGPoGNYr.js","./index-aKxH7ZY0.js","./extends-dGVwEr9R.js","./index-PPLHz8o0.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
