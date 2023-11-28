e44c8f3ff80a67d4d2f148ed185822d85828c3ad
Niels Roozemond <n.roozemond@amsterdam.nl>
2023-11-28 12:19:45 +0100

    Column and span version with grid element

--- packages/react/src/Header/Header.tsx
+++ packages/react/src/Header/Header.tsx
@@ -5,6 +5,7 @@
 
 import clsx from 'clsx'
 import { ForwardedRef, forwardRef, HTMLAttributes, ReactNode } from 'react'
+import { Grid } from '../Grid'
 import { Heading } from '../Heading'
 import { Logo, LogoBrand } from '../Logo'
 import { VisuallyHidden } from '../VisuallyHidden'
@@ -32,24 +33,26 @@ export const Header = forwardRef(
         className,
       )}
     >
-      <div className="amsterdam-header__column amsterdam-header__column--logo">
-        {logoLink ? (
-          <a href={logoLink} className="amsterdam-header__logo-link">
-            {logoLinkTitle && <VisuallyHidden>{logoLinkTitle}</VisuallyHidden>}
+      <Grid>
+        <div className="amsterdam-header__column amsterdam-header__column--logo">
+          {logoLink ? (
+            <a href={logoLink} className="amsterdam-header__logo-link">
+              {logoLinkTitle && <VisuallyHidden>{logoLinkTitle}</VisuallyHidden>}
+              <Logo brand={logoBrand} />
+            </a>
+          ) : (
             <Logo brand={logoBrand} />
-          </a>
-        ) : (
-          <Logo brand={logoBrand} />
-        )}
-      </div>
-      {title && (
-        <div className="amsterdam-header__column amsterdam-header__column--title">
-          <Heading level={3} size="level-3">
-            {title}
-          </Heading>
+          )}
         </div>
-      )}
-      {menu && <div className="amsterdam-header__column amsterdam-header__column--menu">{menu}</div>}
+        {title && (
+          <div className="amsterdam-header__column amsterdam-header__column--title">
+            <Heading level={3} size="level-3">
+              {title}
+            </Heading>
+          </div>
+        )}
+        {menu && <div className="amsterdam-header__column amsterdam-header__column--menu">{menu}</div>}
+      </Grid>
     </header>
   ),
 )
