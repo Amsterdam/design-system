069d3ec3e52d8b3dd504cf1ffc8b6bab6c97c961
alimpens <runner@fv-az1215-982.g4nh0zpvl5iute0urgogs1ja4e.bx.internal.cloudapp.net>
2023-11-28 15:49:44 +0000

    Remove feature branch folder

--- demo-DES-521-page-menu-right-alignment/assets/preview-S_5I-mL2.js
+++
@@ -1,8 +0,0 @@
-import{_ as e}from"./iframe-sPG83acy.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-Aiqs-xN5.js"),__vite__mapDeps([0,1,2,3,4,5,6,7]),import.meta.url);return new r}}};export{a as parameters};
-//# sourceMappingURL=preview-S_5I-mL2.js.map
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-Aiqs-xN5.js","./iframe-sPG83acy.js","./index-7RSFs7Cb.js","./_commonjsHelpers-4gQjN7DL.js","./react-18-e2qAuQ2N.js","./index-iT8Z8hhO.js","./index-aKxH7ZY0.js","./index-PPLHz8o0.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
--- demo-DES-521-page-menu-right-alignment/assets/preview-rx_G7Yy2.js
+++
@@ -1,2 +0,0 @@
-const o={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},options:{storySort:{order:["Docs",["Intro"]]}},viewMode:"docs"};export{o as parameters};
-//# sourceMappingURL=preview-rx_G7Yy2.js.map
