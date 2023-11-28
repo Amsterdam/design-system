596b0ae052158bd67e93d0b778c5a5cd2096c1c8
VincentSmedinga <v.smedinga@amsterdam.nl>
2023-11-28 15:05:26 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@028c77434edb98a8ae0a68a3894c0d56e929eda2 🚀

---
+++ demo-DES-525-add-icon-gallery/assets/preview-U0hhzcqY.js
@@ -0,0 +1,8 @@
+import{_ as e}from"./iframe-AF2tq0N2.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-y4ocRX9_.js"),__vite__mapDeps([0,1,2,3,4,5,6,7]),import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-U0hhzcqY.js.map
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-y4ocRX9_.js","./iframe-AF2tq0N2.js","./index-7RSFs7Cb.js","./_commonjsHelpers-4gQjN7DL.js","./react-18-e2qAuQ2N.js","./index-uQhL_gZd.js","./index-aKxH7ZY0.js","./index-PPLHz8o0.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
