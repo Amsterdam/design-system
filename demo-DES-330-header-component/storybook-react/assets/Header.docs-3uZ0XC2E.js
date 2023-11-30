366bd34043a69fdaaecb74513f5ff1b3d46c6ec8
dlnr <niels@dlnr.dev>
2023-11-30 15:30:28 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@3c20d7ac7c80ce099d663d8adb018ae8a6ab6ef0 🚀

---
+++ demo-DES-330-header-component/storybook-react/assets/Header.docs-3uZ0XC2E.js
@@ -0,0 +1,14 @@
+import{j as t}from"./index.esm-71OefzFf.js";import{M as r,d as a,P as h,e as m,f as o}from"./index-udyQGzfY.js";import{H as c,W as d,a as j,b as x,c as l}from"./Header.stories-scgkpV96.js";import{u as s}from"./index-Dbo06S9W.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./extends-dGVwEr9R.js";import"./iframe-n6WISTLf.js";import"../sb-preview/runtime.js";import"./index-f2OjG03D.js";import"./index-aKxH7ZY0.js";import"./index-PPLHz8o0.js";const p=`# Header
+`;function e(i){const n=Object.assign({h2:"h2"},s(),i.components);return t.jsxs(t.Fragment,{children:[t.jsx(r,{of:c}),`
+`,t.jsx(a,{children:p}),`
+`,t.jsx(h,{}),`
+`,t.jsx(m,{}),`
+`,t.jsx(n.h2,{id:"with-site-title",children:"With Site Title"}),`
+`,t.jsx(o,{of:d}),`
+`,t.jsx(n.h2,{id:"with-logo-variant",children:"With logo variant"}),`
+`,t.jsx(o,{of:j}),`
+`,t.jsx(n.h2,{id:"with-header-menu",children:"With header menu"}),`
+`,t.jsx(o,{of:x}),`
+`,t.jsx(n.h2,{id:"with-both",children:"With both"}),`
+`,t.jsx(o,{of:l})]})}function O(i={}){const{wrapper:n}=Object.assign({},s(),i.components);return n?t.jsx(n,Object.assign({},i,{children:t.jsx(e,i)})):e(i)}export{O as default};
+//# sourceMappingURL=Header.docs-3uZ0XC2E.js.map
---
+++ demo-DES-330-header-component/storybook-react/assets/PageMenu.stories-bd12X17y.js
@@ -0,0 +1,17 @@
+import{l as e,j as r,m as s,n as i}from"./index.esm-71OefzFf.js";const c={title:"Navigation/Page Menu",component:e},n={render:()=>r.jsxs(e,{children:[r.jsx(e.Link,{href:"#",children:"English"}),r.jsx(e.Link,{href:"#",icon:s,children:"Inloggen Mijn Amsterdam"}),r.jsx(e.Button,{icon:i,children:"Alle onderwerpen"})]}),parameters:{docs:{source:{type:"dynamic"}}}};var o,a,t;n.parameters={...n.parameters,docs:{...(o=n.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: () => <PageMenu>
+      <PageMenu.Link href="#">English</PageMenu.Link>
+      <PageMenu.Link href="#" icon={LoginIcon}>
+        Inloggen Mijn Amsterdam
+      </PageMenu.Link>
+      <PageMenu.Button icon={MenuIcon}>Alle onderwerpen</PageMenu.Button>
+    </PageMenu>,
+  parameters: {
+    docs: {
+      source: {
+        type: 'dynamic'
+      }
+    }
+  }
+}`,...(t=(a=n.parameters)==null?void 0:a.docs)==null?void 0:t.source}}};const u=["Default"],l=Object.freeze(Object.defineProperty({__proto__:null,Default:n,__namedExportsOrder:u,default:c},Symbol.toStringTag,{value:"Module"}));export{l as P};
+//# sourceMappingURL=PageMenu.stories-bd12X17y.js.map
