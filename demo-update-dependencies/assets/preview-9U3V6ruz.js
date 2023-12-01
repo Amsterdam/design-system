c9ceb3b956badf2e04bbe5399a647e638d6affe2
VincentSmedinga <v.smedinga@amsterdam.nl>
2023-12-01 14:26:01 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@a9584d648a77c05cd17fd39306e26a30d05e5962 🚀

---
+++ demo-update-dependencies/assets/preview-9U3V6ruz.js
@@ -0,0 +1,1 @@
+const o={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},options:{storySort:{order:["Docs",["Intro","Assets","Designrichtlijnen"]]}},viewMode:"docs"};export{o as parameters};
