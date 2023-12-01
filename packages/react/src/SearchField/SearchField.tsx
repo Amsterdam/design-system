4a784e8266a723b3d29b46c571e1305120ed4632
Aram Limpens <a.limpens@amsterdam.nl>
2023-12-01 16:47:28 +0100

    Add Search Field component

--- packages/react/src/SearchField/SearchField.tsx
+++ packages/react/src/SearchField/SearchField.tsx
@@ -3,17 +3,50 @@
  * Copyright (c) 2023 Gemeente Amsterdam
  */
 
+import { SearchIcon } from '@amsterdam/design-system-react-icons'
 import clsx from 'clsx'
-import { ForwardedRef, forwardRef, HTMLAttributes, PropsWithChildren } from 'react'
+import { ForwardedRef, forwardRef, HTMLAttributes, useId } from 'react'
+import { Icon } from '../Icon'
+import { VisuallyHidden } from '../VisuallyHidden'
 
-export interface SearchFieldProps extends PropsWithChildren<HTMLAttributes<HTMLElement>> {}
+export interface SearchFieldProps extends HTMLAttributes<HTMLFormElement> {}
+
+interface SearchFieldButtonProps extends HTMLAttributes<HTMLButtonElement> {}
+
+// TODO: replace this with IconButton when that's done
+const SearchFieldButton = forwardRef(
+  ({ className, ...restProps }: SearchFieldButtonProps, ref: ForwardedRef<HTMLButtonElement>) => (
+    <button {...restProps} ref={ref} className={clsx('amsterdam-search-field__button', className)}>
+      <VisuallyHidden>Zoeken</VisuallyHidden>
+      <Icon svg={SearchIcon} size="level-6" />
+    </button>
+  ),
+)
+
+SearchFieldButton.displayName = 'SearchFieldButton'
 
 export const SearchField = forwardRef(
-  ({ children, className, ...restProps }: SearchFieldProps, ref: ForwardedRef<HTMLElement>) => (
-    <span {...restProps} ref={ref} className={clsx('amsterdam-search-field', className)}>
-      {children}
-    </span>
-  ),
+  ({ className, ...restProps }: SearchFieldProps, ref: ForwardedRef<HTMLFormElement>) => {
+    const id = useId()
+
+    return (
+      <form role="search" {...restProps} ref={ref} className={clsx('amsterdam-search-field', className)}>
+        <label htmlFor={id}>
+          <VisuallyHidden>Zoeken</VisuallyHidden>
+        </label>
+        <input
+          autoComplete="off"
+          className="amsterdam-search-field__input"
+          enterKeyHint="search"
+          id={id}
+          placeholder="Wat kunnen we voor u vinden?"
+          spellCheck="false"
+          type="search"
+        />
+        <SearchFieldButton />
+      </form>
+    )
+  },
 )
 
 SearchField.displayName = 'SearchField'
