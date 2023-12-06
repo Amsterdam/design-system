0b989a8256551929e4e91d82e9584333e5390980
Vincent Smedinga <vincent@smedinga.nl>
2023-12-06 13:28:14 +0100

    WIP Attempt to hide the ‘Grid Cell’ entry from the sidebar

--- storybook/storybook-docs/config/manager.js
+++ storybook/storybook-docs/config/manager.js
@@ -2,4 +2,14 @@ import { addons } from '@storybook/manager-api'
 import { create } from '@storybook/theming/create'
 import themeConfig from '../../../storybook/theme.config'
 
-addons.setConfig({ theme: create(themeConfig) })
+addons.setConfig({
+  theme: create(themeConfig),
+  sidebar: {
+    filters: {
+      hidden: (item) => {
+        console.log(item.title, item.tags)
+        return !item.tags.includes('hidden')
+      },
+    },
+  },
+})
--- storybook/storybook-react/src/Grid/Grid.Cell.stories.tsx
+++ storybook/storybook-react/src/Grid/Grid.Cell.stories.tsx
@@ -24,6 +24,7 @@ const meta = {
       options: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
     },
   },
+  tags: ['hidden'],
 } satisfies Meta<typeof Grid.Cell>
 
 export default meta
