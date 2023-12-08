7e61af764c4e7cb560fcd585914317e22537a9b0
Aram Limpens <a.limpens@amsterdam.nl>
2023-12-08 18:13:44 +0100

    Add tests

--- packages/react/src/TextInput/TextInput.test.tsx
+++ packages/react/src/TextInput/TextInput.test.tsx
@@ -1,42 +1,75 @@
-import { render } from '@testing-library/react'
-import { createRef } from 'react'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { createRef, useState } from 'react'
 import { TextInput } from './TextInput'
 import '@testing-library/jest-dom'
 
 describe('Text input', () => {
   it('renders', () => {
-    const { container } = render(<TextInput />)
+    render(<TextInput />)
 
-    const component = container.querySelector(':only-child')
+    const component = screen.getByRole('textbox')
 
     expect(component).toBeInTheDocument()
     expect(component).toBeVisible()
   })
 
   it('renders a design system BEM class name', () => {
-    const { container } = render(<TextInput />)
+    render(<TextInput />)
 
-    const component = container.querySelector(':only-child')
+    const component = screen.getByRole('textbox')
 
     expect(component).toHaveClass('amsterdam-text-input')
   })
 
   it('renders an additional class name', () => {
-    const { container } = render(<TextInput className="extra" />)
+    render(<TextInput className="extra" />)
 
-    const component = container.querySelector(':only-child')
+    const component = screen.getByRole('textbox')
 
     expect(component).toHaveClass('extra')
 
     expect(component).toHaveClass('amsterdam-text-input')
   })
 
+  it('should be working in a controlled state', async () => {
+    function ControlledComponent() {
+      const [value, setValue] = useState('Hello')
+
+      return <TextInput value={value} onChange={(e) => setValue(e.target.value)} />
+    }
+
+    render(<ControlledComponent />)
+
+    const componentText = screen.getByDisplayValue('Hello')
+    expect(componentText).toBeInTheDocument()
+
+    const component = screen.getByRole('textbox')
+    if (component) {
+      await userEvent.type(component, ', World!')
+    }
+
+    const newComponentText = screen.getByDisplayValue('Hello, World!')
+    expect(newComponentText).toBeInTheDocument()
+  })
+
+  it('should not update the value when disabled', async () => {
+    render(<TextInput disabled defaultValue="Hello" />)
+
+    const component = screen.getByRole('textbox')
+    if (component) {
+      await userEvent.type(component, ', World!')
+    }
+
+    expect(component).toHaveValue('Hello')
+  })
+
   it('supports ForwardRef in React', () => {
     const ref = createRef<HTMLInputElement>()
 
-    const { container } = render(<TextInput ref={ref} />)
+    render(<TextInput ref={ref} />)
 
-    const component = container.querySelector(':only-child')
+    const component = screen.getByRole('textbox')
 
     expect(ref.current).toBe(component)
   })
