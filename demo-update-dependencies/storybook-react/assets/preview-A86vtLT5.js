fa86f819100159e441aa0da775ae7908516c2c5b
VincentSmedinga <runner@fv-az568-53.kq21xgauaq5efpa1vyplvqtvth.dx.internal.cloudapp.net>
2023-12-08 16:18:56 +0000

    Remove feature branch folder

--- demo-update-dependencies/storybook-react/assets/preview-A86vtLT5.js
+++
@@ -1,7 +0,0 @@
-import{_ as e}from"./iframe-VQITTode.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-3pdV6ult.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10]),import.meta.url);return new r}}};export{a as parameters};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-3pdV6ult.js","./iframe-VQITTode.js","./index-4g5l5LRQ.js","./_commonjsHelpers-4gQjN7DL.js","./react-18-ba7OOUbL.js","./index-jmm5gWkb.js","./index-yY1PqlEP.js","./index-ogXoivrg.js","./extends-dGVwEr9R.js","./index-MVbLLYTZ.js","./index-PPLHz8o0.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
