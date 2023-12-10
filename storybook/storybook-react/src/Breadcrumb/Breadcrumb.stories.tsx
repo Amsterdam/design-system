721a54e1e2a13b31e23afdb6f4b44a9995f9713e
Vincent Smedinga <vincent@smedinga.nl>
2023-12-10 23:37:30 +0100

    Use consistent name for default story

--- storybook/storybook-react/src/Breadcrumb/Breadcrumb.stories.tsx
+++ storybook/storybook-react/src/Breadcrumb/Breadcrumb.stories.tsx
@@ -15,7 +15,7 @@ export default meta
 
 type Story = StoryObj<typeof meta>
 
-export const BreadcrumbStory: Story = {
+export const Default: Story = {
   render: () => (
     <Breadcrumb>
       <Breadcrumb.Item href="#">Home</Breadcrumb.Item>
