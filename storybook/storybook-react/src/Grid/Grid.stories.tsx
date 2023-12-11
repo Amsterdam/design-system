4be16042ecd1c68156882874d6241d77d0831c57
Vincent Smedinga <vincent@smedinga.nl>
2023-12-11 16:17:03 +0100

    Move grid cell stories into grid docs

--- storybook/storybook-react/src/Grid/Grid.stories.tsx
+++ storybook/storybook-react/src/Grid/Grid.stories.tsx
@@ -3,25 +3,62 @@
  * Copyright (c) 2023 Gemeente Amsterdam
  */
 
+import type { GridCellProps, GridProps } from '@amsterdam/design-system-react'
 import { Grid, Screen } from '@amsterdam/design-system-react'
 import { Meta, StoryObj } from '@storybook/react'
 import { paddingArgType } from '../shared/argTypes'
 
+type StoryProps = GridProps & GridCellProps
+
+const argTypes = {
+  children: {
+    table: { disable: true },
+  },
+}
+
+const gridArgTypes = {
+  compact: {
+    control: { type: 'boolean' },
+  },
+  gapVertical: {
+    control: {
+      type: 'radio',
+      labels: { small: 'small', undefined: 'medium', large: 'large' },
+    },
+    options: ['small', undefined, 'large'],
+  },
+  paddingVertical: paddingArgType,
+  paddingTop: paddingArgType,
+  paddingBottom: paddingArgType,
+}
+
+const gridCellArgTypes = {
+  span: {
+    control: { type: 'inline-radio' },
+    options: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+  },
+  start: {
+    control: { type: 'inline-radio' },
+    options: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+  },
+}
+
 const meta = {
   title: 'Layout/Grid',
   component: Grid,
-  argTypes: {
-    paddingVertical: paddingArgType,
-    paddingTop: paddingArgType,
-    paddingBottom: paddingArgType,
-  },
-} satisfies Meta<typeof Grid>
+  argTypes,
+} satisfies Meta<StoryProps>
 
 export default meta
 
-type Story = StoryObj<typeof meta>
+const gridMeta = { ...meta }
+const gridCellMeta = { ...meta, component: Grid.Cell }
 
-const StoryTemplate: Story = {
+type GridStory = StoryObj<typeof gridMeta>
+type GridCellStory = StoryObj<typeof gridCellMeta>
+
+const GridStoryTemplate: GridStory = {
+  argTypes: gridArgTypes,
   decorators: [
     (Story) => (
       <Screen>
@@ -31,52 +68,96 @@ const StoryTemplate: Story = {
   ],
 }
 
-export const Default: Story = {
-  ...StoryTemplate,
+const GridCellStoryTemplate: GridCellStory = {
+  argTypes: gridCellArgTypes,
+  decorators: [
+    (Story) => (
+      <Screen>
+        <Grid>
+          <Story />
+        </Grid>
+      </Screen>
+    ),
+  ],
+  render: ({ children, ...args }) => <Grid.Cell {...args}>{children}</Grid.Cell>,
+}
+
+export const Default: GridStory = {
+  ...GridStoryTemplate,
   args: {
     children: Array.from(Array(12).keys()).map((i) => <Grid.Cell className="amsterdam-docs-pink-box" key={i} />),
   },
-  name: 'Basis',
+  name: 'Ruimtelijk',
 }
 
-export const Compact: Story = {
-  ...StoryTemplate,
+export const Compact: GridStory = {
+  ...GridStoryTemplate,
   args: {
     children: Array.from(Array(12).keys()).map((i) => <Grid.Cell className="amsterdam-docs-pink-box" key={i} />),
     compact: true,
   },
   name: 'Compact',
-  parameters: {
-    outline: 'visible',
-  },
 }
 
-export const VerticalSpace: Story = {
-  ...StoryTemplate,
+export const VerticalSpace: GridStory = {
+  ...GridStoryTemplate,
   args: {
     children: Array.from(Array(12).keys()).map((i) => <Grid.Cell className="amsterdam-docs-pink-box" key={i} />),
-    compact: false,
     paddingVertical: 'medium',
   },
   name: 'Verticale marge',
 }
 
-export const VerticalGap: Story = {
-  ...StoryTemplate,
+export const VerticalGap: GridStory = {
+  ...GridStoryTemplate,
   args: {
     children: Array.from(Array(6).keys()).map((i) => (
       <Grid.Cell className="amsterdam-docs-pink-box" span={4} key={i} />
     )),
     gapVertical: 'small',
   },
-  argTypes: {
-    gapVertical: {
-      control: {
-        type: 'radio',
-        labels: { small: 'small', undefined: 'medium', large: 'large' },
-      },
-      options: ['small', undefined, 'large'],
-    },
-  },
   name: 'Verticale witruimte',
+}
+
+export const CellSpan: GridCellStory = {
+  ...GridCellStoryTemplate,
+  args: {
+    children: <p className="amsterdam-docs-paragraph amsterdam-docs-pink-box">Deze cel beslaat 4 kolommen.</p>,
+    span: 4,
+  },
+  name: 'Meerdere kolommen',
+}
+
+export const CellSpans: GridCellStory = {
+  ...GridCellStoryTemplate,
+  args: {
+    children: (
+      <p className="amsterdam-docs-paragraph amsterdam-docs-pink-box">Deze cel heeft 3 instellingen voor de breedte.</p>
+    ),
+    span: { narrow: 4, medium: 6, wide: 8 },
+  },
+  name: 'Meerdere kolombreedtes',
+}
+
+export const FullWidth: GridCellStory = {
+  ...GridCellStoryTemplate,
+  args: {
+    children: (
+      <p className="amsterdam-docs-paragraph amsterdam-docs-pink-box">
+        Deze cel beslaat de volledige breedte van het grid.
+      </p>
+    ),
+    fullWidth: true,
+  },
+  name: 'Volledige breedte',
+}
+
+export const StartPosition: GridCellStory = {
+  ...GridCellStoryTemplate,
+  args: {
+    children: <p className="amsterdam-docs-paragraph amsterdam-docs-pink-box">Deze cel start in kolom 2.</p>,
+    span: 2,
+    start: 2,
+  },
+  name: 'Startpositie',
 }
