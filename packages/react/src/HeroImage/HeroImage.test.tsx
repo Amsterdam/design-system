60cc3b456f205da9b41563e005496c99ff44b3f6
Vincent Smedinga <vincent@smedinga.nl>
2023-12-12 16:25:45 +0100

    Remove unnecessary quotes

--- packages/react/src/HeroImage/HeroImage.test.tsx
+++ packages/react/src/HeroImage/HeroImage.test.tsx
@@ -5,20 +5,20 @@ import '@testing-library/jest-dom'
 
 describe('Hero image', () => {
   it('renders', () => {
-    const { container } = render(<HeroImage src="'https://picsum.photos/1280/360'" />)
+    const { container } = render(<HeroImage src="https://picsum.photos/1280/360" />)
     const component = container.querySelector(':only-child')
     expect(component).toBeInTheDocument()
     expect(component).toBeVisible()
   })
 
   it('renders a design system BEM class name', () => {
-    const { container } = render(<HeroImage src="'https://picsum.photos/1280/360'" />)
+    const { container } = render(<HeroImage src="https://picsum.photos/1280/360" />)
     const component = container.querySelector(':only-child')
     expect(component).toHaveClass('amsterdam-hero-image')
   })
 
   it('renders an additional class name', () => {
-    const { container } = render(<HeroImage src="'https://picsum.photos/1280/360'" className="extra" />)
+    const { container } = render(<HeroImage src="https://picsum.photos/1280/360" className="extra" />)
     const component = container.querySelector(':only-child')
     expect(component).toHaveClass('amsterdam-hero-image extra')
   })
@@ -31,7 +31,7 @@ describe('Hero image', () => {
 
   it('supports ForwardRef in React', () => {
     const ref = createRef<HTMLDivElement>()
-    const { container } = render(<HeroImage src="'https://picsum.photos/1280/360'" ref={ref} />)
+    const { container } = render(<HeroImage src="https://picsum.photos/1280/360" ref={ref} />)
     const component = container.querySelector(':only-child')
     expect(ref.current).toBe(component)
   })
