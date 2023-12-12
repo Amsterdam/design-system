a1db3cccbec47f09125858a885d859b19b683297
VincentSmedinga <v.smedinga@amsterdam.nl>
2023-12-12 14:39:01 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@af364e964f1c10ca0ece1c8930e6eb96a2f67650 🚀

---
+++ demo-DES-488-hero-image/storybook-react/assets/HeroImage.docs-ghqaCoNN.js
@@ -0,0 +1,15 @@
+import{j as e}from"./index.esm-fN5LRh1x.js";import{M as i,d as a,P as s,f as d}from"./index-hpq0IHqY.js";import{H as c,W as m}from"./HeroImage.stories-jf0Peepv.js";import{u as r}from"./index-Dbo06S9W.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./extends-dGVwEr9R.js";import"./iframe-dRTaRoK2.js";import"../sb-preview/runtime.js";import"./index-jmm5gWkb.js";import"./index-ogXoivrg.js";import"./index-MVbLLYTZ.js";import"./index-PPLHz8o0.js";const l=`# Hero Image
+
+Een sfeerbepalende afbeelding die vaak over de volle breedte bovenaan een pagina wordt gebruikt.
+`;function o(t){const n=Object.assign({h2:"h2",h3:"h3",p:"p",code:"code",a:"a"},r(),t.components);return e.jsxs(e.Fragment,{children:[e.jsx(i,{of:c}),`
+`,e.jsx(a,{children:l}),`
+`,e.jsx(n.h2,{id:"voorbeelden",children:"Voorbeelden"}),`
+`,e.jsx(n.h3,{id:"basis",children:"Basis"}),`
+`,e.jsxs(n.p,{children:[`De beeldverhouding van de afbeelding is 32:9.
+Dat komt overeen met de `,e.jsx(n.code,{children:"2x-wide"})," ",e.jsx(n.a,{href:"?path=/docs/react_layout-aspect-ratio--docs",children:"aspect ratio"}),"."]}),`
+`,e.jsx(s,{}),`
+`,e.jsx(n.h3,{id:"met-zoekveld",children:"Met zoekveld"}),`
+`,e.jsx(n.p,{children:`De inhoud van een hero-afbeelding zal doorgaans in een grid staan.
+De horizontale witruimte en maatvoering is dan verzorgd.`}),`
+`,e.jsx(n.p,{children:"In dit geval specificeert het zoekveld een maximale breedte en centreert het zichzelf binnen de cel van het grid."}),`
+`,e.jsx(d,{of:m})]})}function w(t={}){const{wrapper:n}=Object.assign({},r(),t.components);return n?e.jsx(n,Object.assign({},t,{children:e.jsx(o,t)})):o(t)}export{w as default};
