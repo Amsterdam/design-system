37b165944f410dc23332ceb5671ab7cdf21df0b0
VincentSmedinga <v.smedinga@amsterdam.nl>
2023-12-12 22:52:25 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@33438015a7c54f05124ea3ae6f143f1aa697ec70 🚀

---
+++ demo-DES-488-hero-image/storybook-react/assets/HeroImage.docs-mCJFfZuQ.js
@@ -0,0 +1,16 @@
+import{j as e}from"./index.esm-epGmzYux.js";import{M as a,d as i,P as s,f as d}from"./index-tkVGi7Ro.js";import{H as l,W as c}from"./HeroImage.stories-dRH-I5Jc.js";import{u as r}from"./index-Dbo06S9W.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./extends-dGVwEr9R.js";import"./iframe-2_PEK0hu.js";import"../sb-preview/runtime.js";import"./index-jmm5gWkb.js";import"./index-ogXoivrg.js";import"./index-MVbLLYTZ.js";import"./index-PPLHz8o0.js";const m=`# Hero Image
+
+Een sfeerbepalende afbeelding die vaak over de volle breedte bovenaan een pagina wordt gebruikt.
+`;function t(o){const n=Object.assign({h2:"h2",h3:"h3",p:"p",code:"code",a:"a"},r(),o.components);return e.jsxs(e.Fragment,{children:[e.jsx(a,{of:l}),`
+`,e.jsx(i,{children:m}),`
+`,e.jsx(n.h2,{id:"voorbeelden",children:"Voorbeelden"}),`
+`,e.jsx(n.h3,{id:"basis",children:"Basis"}),`
+`,e.jsxs(n.p,{children:[`De beeldverhouding van de afbeelding is 32:9.
+Dat komt overeen met de `,e.jsx(n.code,{children:"2x-wide"})," ",e.jsx(n.a,{href:"?path=/docs/react_layout-aspect-ratio--docs",children:"aspect ratio"}),"."]}),`
+`,e.jsx(n.p,{children:`Kies een voldoende grote afbeelding.
+Als het nodig is wordt deze uitgerekt om de volledige oppervlakte in te nemen.`}),`
+`,e.jsx(s,{}),`
+`,e.jsx(n.h3,{id:"met-zoekveld",children:"Met zoekveld"}),`
+`,e.jsxs(n.p,{children:["Een ",e.jsx(n.a,{href:"?path=/docs/react_layout-grid--docs",children:"Grid"}),` verzorgt horizontale witruimte aan weerszijden en kolommen voor layout.
+Zo kan bijvoorbeeld een zoekveld geplaatst worden. Verticaal centreren gaat vanzelf.`]}),`
+`,e.jsx(d,{of:c})]})}function D(o={}){const{wrapper:n}=Object.assign({},r(),o.components);return n?e.jsx(n,Object.assign({},o,{children:e.jsx(t,o)})):t(o)}export{D as default};
