2c625d4131a81ce20b8f722a398f0c9d37a0b6d2
Vincent Smedinga <vincent@smedinga.nl>
2023-12-13 12:00:12 +0100

    Remove unused Storybook config

--- storybook/storybook-react/config/manager.js
+++ storybook/storybook-react/config/manager.js
@@ -2,14 +2,4 @@ import { addons } from '@storybook/manager-api'
 import { create } from '@storybook/theming/create'
 import themeConfig from '../../../storybook/theme.config'
 
-addons.setConfig({
-  theme: create(themeConfig),
-  sidebar: {
-    filters: {
-      hidden: (item) => {
-        console.log(item.title, item.tags)
-        return !item.tags.includes('hidden')
-      },
-    },
-  },
-})
+addons.setConfig({ theme: create(themeConfig) })
