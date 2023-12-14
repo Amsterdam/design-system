25e93dfe94a479dcbdfa6ccaa904f2ada874708c
Vincent Smedinga <vincent@smedinga.nl>
2023-12-14 12:32:27 +0100

    Add support for responsive images

--- packages/react/src/HeroImage/HeroImage.tsx
+++ packages/react/src/HeroImage/HeroImage.tsx
@@ -7,15 +7,19 @@ import clsx from 'clsx'
 import { forwardRef, HTMLAttributes, ImgHTMLAttributes, PropsWithChildren } from 'react'
 
 export interface HeroImageProps extends PropsWithChildren<HTMLAttributes<HTMLElement>> {
-  /** The source of the image. */
+  /** A set of source sizes of the image. */
+  sizes?: ImgHTMLAttributes<HTMLImageElement>['sizes']
+  /** A single or candidate source of the image. */
   src: ImgHTMLAttributes<HTMLImageElement>['src']
+  /** A list of candidate sources of the image. */
+  srcSet?: ImgHTMLAttributes<HTMLImageElement>['srcSet']
 }
 
 export const HeroImage = forwardRef<HTMLDivElement, HeroImageProps>(
-  ({ children, className, src, ...restProps }: HeroImageProps, ref) => (
+  ({ children, className, sizes, src, srcSet, ...restProps }: HeroImageProps, ref) => (
     <div {...restProps} ref={ref} className={clsx('amsterdam-hero-image', className)}>
       <div className="amsterdam-hero-image__image">
-        <img alt="" src={src} />
+        <img alt="" sizes={sizes} src={src} srcSet={srcSet} />
       </div>
       {children}
     </div>
--- storybook/storybook-react/src/HeroImage/HeroImage.stories.tsx
+++ storybook/storybook-react/src/HeroImage/HeroImage.stories.tsx
@@ -20,13 +20,19 @@ type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
   args: {
-    src: 'https://picsum.photos/1280/360?random=1',
+    sizes: '(max-width: 36rem) 640px, (max-width: 68rem) 1280px, 1600px',
+    src: 'https://picsum.photos/640/180?random=1',
+    srcSet:
+      'https://picsum.photos/640/180?random=1 640w, https://picsum.photos/1280/360?random=1 1280w, https://picsum.photos/1600/450?random=1 1600w',
   },
 }
 
 export const WithSearchField: Story = {
   args: {
-    src: 'https://picsum.photos/1280/360?random=2',
+    sizes: '(max-width: 36rem) 640px, (max-width: 68rem) 1280px, 1600px',
+    src: 'https://picsum.photos/640/180?random=2',
+    srcSet:
+      'https://picsum.photos/640/180?random=2 640w, https://picsum.photos/1280/360?random=2 1280w, https://picsum.photos/1600/450?random=2 1600w',
     children: (
       <Grid>
         <Grid.Cell span={{ narrow: 4, medium: 6, wide: 6 }} start={{ narrow: 1, medium: 2, wide: 4 }}>
