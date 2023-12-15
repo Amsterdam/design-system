19be0fe4de1e4003825ef2acdafb4f1ff9c59994
VincentSmedinga <v.smedinga@amsterdam.nl>
2023-12-15 13:07:03 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@4e894d99aeb9d018d6ed13bc44ae926b0a479db5 🚀

--- demo-merge-grid-and-cell/assets/preview-9Bcjs0kV.js
+++
@@ -1,7 +0,0 @@
-import{_ as e}from"./iframe-yr2E7Xzu.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-4QyFWl-q.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9]),import.meta.url);return new r}}};export{a as parameters};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-4QyFWl-q.js","./iframe-yr2E7Xzu.js","./index-4g5l5LRQ.js","./_commonjsHelpers-4gQjN7DL.js","./react-18-ba7OOUbL.js","./index-jmm5gWkb.js","./index-8Ae4Lprc.js","./index-ogXoivrg.js","./index-MVbLLYTZ.js","./index-PPLHz8o0.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
