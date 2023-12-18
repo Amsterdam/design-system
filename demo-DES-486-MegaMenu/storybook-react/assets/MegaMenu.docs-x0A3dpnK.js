4a762584ffee7814b6e4ad47fcbb1abe1b9af6a7
dlnr <niels@dlnr.dev>
2023-12-18 14:49:42 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@5d4f86fa02581a3c67b813f8d888e62082934dc9 🚀

---
+++ demo-DES-486-MegaMenu/storybook-react/assets/MegaMenu.docs-x0A3dpnK.js
@@ -0,0 +1,5 @@
+import{j as t}from"./index.esm-CD9i34by.js";import{M as s,d as e,P as i,e as m}from"./index-HHVvfS5t.js";import{M as a}from"./MegaMenu.stories-7fxO5u6w.js";import{useMDXComponents as p}from"./index-_VGcpBFS.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./extends-dGVwEr9R.js";import"./iframe-amHMkX0i.js";import"../sb-preview/runtime.js";import"./index-jmm5gWkb.js";import"./index-ogXoivrg.js";import"./index-MVbLLYTZ.js";import"./index-PPLHz8o0.js";const M=`# Mega Menu
+`;function n(o){return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:a}),`
+`,t.jsx(e,{children:M}),`
+`,t.jsx(i,{}),`
+`,t.jsx(m,{})]})}function b(o={}){const{wrapper:r}=Object.assign({},p(),o.components);return r?t.jsx(r,Object.assign({},o,{children:t.jsx(n,o)})):n()}export{b as default};
