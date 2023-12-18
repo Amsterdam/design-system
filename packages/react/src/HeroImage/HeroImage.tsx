3f26198c50d9053cbe83f61a497570e632e555a9
Vincent Smedinga <vincent@smedinga.nl>
2023-12-18 12:22:57 +0100

    Allow using a custom image component in `HeroImage`

--- packages/react/src/HeroImage/HeroImage.tsx
+++ packages/react/src/HeroImage/HeroImage.tsx
@@ -4,16 +4,22 @@
  */
 
 import clsx from 'clsx'
-import { forwardRef, type ImgHTMLAttributes, type PropsWithChildren } from 'react'
+import { forwardRef, type ImgHTMLAttributes, type PropsWithChildren, type ReactNode } from 'react'
 import { Image } from '../Image/Image'
 
-export interface HeroImageProps extends PropsWithChildren<ImgHTMLAttributes<HTMLImageElement>> {}
+export interface HeroImageProps extends PropsWithChildren<ImgHTMLAttributes<HTMLImageElement>> {
+  /*
+   * A custom image component to use instead of the one from this library.
+   * Note that the image must fill the entire content box, e.g. through `object-fit: cover`.
+   */
+  ImageComponent?: ReactNode
+}
 
 export const HeroImage = forwardRef<HTMLImageElement, HeroImageProps>(
-  ({ children, className, ...restProps }: HeroImageProps, ref) => (
+  ({ children, className, ImageComponent, ...restProps }: HeroImageProps, ref) => (
     <div className={clsx('amsterdam-hero-image', className)}>
       <div className="amsterdam-hero-image__image">
-        <Image {...restProps} alt="" ref={ref} objectFit="cover" />
+        {ImageComponent ?? <Image {...restProps} alt="" objectFit="cover" ref={ref} />}
       </div>
       {children}
       <HeroImage src="abc.jpg" />
