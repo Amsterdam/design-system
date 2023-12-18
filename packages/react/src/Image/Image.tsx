76af071a8f79c35b407261ea66326af268e5cb33
Vincent Smedinga <vincent@smedinga.nl>
2023-12-18 12:20:27 +0100

    Allow an image to fill its entire content box

--- packages/react/src/Image/Image.tsx
+++ packages/react/src/Image/Image.tsx
@@ -4,12 +4,16 @@
  */
 
 import clsx from 'clsx'
-import { ForwardedRef, forwardRef, ImgHTMLAttributes } from 'react'
+import { type ForwardedRef, forwardRef, type ImgHTMLAttributes } from 'react'
 
-export interface ImageProps extends ImgHTMLAttributes<HTMLImageElement> {}
+export interface ImageProps extends ImgHTMLAttributes<HTMLImageElement> {
+  objectFit?: 'cover' | 'contain'
+}
 
-export const Image = forwardRef(({ className, ...restProps }: ImageProps, ref: ForwardedRef<HTMLImageElement>) => (
-  <img {...restProps} ref={ref} className={clsx('amsterdam-image', className)} />
-))
+export const Image = forwardRef(
+  ({ className, objectFit, ...restProps }: ImageProps, ref: ForwardedRef<HTMLImageElement>) => (
+    <img {...restProps} ref={ref} className={clsx('amsterdam-image', className)} style={{ objectFit }} />
+  ),
+)
 
 Image.displayName = 'Image'
