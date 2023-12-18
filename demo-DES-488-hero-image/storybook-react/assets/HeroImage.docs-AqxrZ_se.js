24b371e7519726746877b4d62e11978ec7f93feb
VincentSmedinga <v.smedinga@amsterdam.nl>
2023-12-18 12:20:00 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@7f89c095b647b709eae58dd0765343949e61b393 🚀

---
+++ demo-DES-488-hero-image/storybook-react/assets/HeroImage.docs-AqxrZ_se.js
@@ -0,0 +1,25 @@
+import{j as e}from"./index.esm-xGaYUluS.js";import{M as d,d as t,P as a,f as s}from"./index-OPytI2DY.js";import{H as l,W as c}from"./HeroImage.stories-VjkEISYm.js";import{useMDXComponents as i}from"./index-_VGcpBFS.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./extends-dGVwEr9R.js";import"./iframe-R-8-BeP3.js";import"../sb-preview/runtime.js";import"./index-jmm5gWkb.js";import"./index-ogXoivrg.js";import"./index-MVbLLYTZ.js";import"./index-PPLHz8o0.js";const h=`# Hero Image
+
+Een sfeerbepalende afbeelding die vaak over de volle breedte bovenaan een pagina wordt gebruikt.
+`;function r(o){const n=Object.assign({h2:"h2",h3:"h3",p:"p",code:"code",a:"a",ul:"ul",li:"li"},i(),o.components);return e.jsxs(e.Fragment,{children:[e.jsx(d,{of:l}),`
+`,e.jsx(t,{children:h}),`
+`,e.jsx(n.h2,{id:"voorbeelden",children:"Voorbeelden"}),`
+`,e.jsx(n.h3,{id:"basis",children:"Basis"}),`
+`,e.jsxs(n.p,{children:[`De beeldverhouding van de afbeelding is 32:9.
+Dat komt overeen met de `,e.jsx(n.code,{children:"2x-wide"})," ",e.jsx(n.a,{href:"?path=/docs/react_layout-aspect-ratio--docs",children:"aspect ratio"}),"."]}),`
+`,e.jsx(a,{}),`
+`,e.jsx(n.h3,{id:"met-zoekveld",children:"Met zoekveld"}),`
+`,e.jsxs(n.p,{children:["Een ",e.jsx(n.a,{href:"?path=/docs/react_layout-grid--docs",children:"Grid"}),` verzorgt horizontale witruimte aan weerszijden en kolommen voor layout.
+Zo kan bijvoorbeeld een zoekveld geplaatst worden.
+Verticaal centreren gaat vanzelf.`]}),`
+`,e.jsx(s,{of:c}),`
+`,e.jsx(n.h2,{id:"richtlijnen",children:"Richtlijnen"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:`Gebruik een voldoende grote afbeelding.
+Als het nodig is, wordt de afbeelding uitgerekt om de volledige oppervlakte te bedekken.`}),`
+`,e.jsxs(n.li,{children:["Specificeer meerdere kandidaten voor de afbeelding via de ",e.jsx(n.code,{children:"srcSet"}),` prop.
+Bijvoorbeeld een kleine, middelgrote en grote variant voor de diverse schermformaten.
+Zo voorkom je het onnodige downloaden van grote bestanden.`]}),`
+`,e.jsx(n.li,{children:`Zorg dat de beeldverhouding van de afbeelding 32:9 is.
+Anders wordt de afbeelding boven en onder of links en rechts bijgesneden.`}),`
+`]})]})}function D(o={}){const{wrapper:n}=Object.assign({},i(),o.components);return n?e.jsx(n,Object.assign({},o,{children:e.jsx(r,o)})):r(o)}export{D as default};
---
+++ demo-DES-488-hero-image/storybook-react/assets/preview-ppfIEChT.js
@@ -0,0 +1,7 @@
+import{_ as e}from"./iframe-R-8-BeP3.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-Djw1p6OW.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10]),import.meta.url);return new r}}};export{a as parameters};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-Djw1p6OW.js","./iframe-R-8-BeP3.js","./index-4g5l5LRQ.js","./_commonjsHelpers-4gQjN7DL.js","./react-18-ba7OOUbL.js","./index-jmm5gWkb.js","./index-OPytI2DY.js","./index-ogXoivrg.js","./extends-dGVwEr9R.js","./index-MVbLLYTZ.js","./index-PPLHz8o0.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
