804e5fcb3e47ecde84ab61019b974caebc0dd6fd
VincentSmedinga <v.smedinga@amsterdam.nl>
2023-12-18 16:58:53 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@9116613a9da54d3763bd3de8c230077458bea22c 🚀

---
+++ demo-DES-488-hero-image/storybook-react/assets/HeroImage.docs-KlB-IkpB.js
@@ -0,0 +1,24 @@
+import{j as e}from"./index.esm-qqBxln0-.js";import{M as t,d,P as a,f as s}from"./index-rUKGEHxV.js";import{H as l,W as c}from"./HeroImage.stories-HerVE-ck.js";import{useMDXComponents as r}from"./index-_VGcpBFS.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./extends-dGVwEr9R.js";import"./iframe-gMPNzQ1f.js";import"../sb-preview/runtime.js";import"./index-jmm5gWkb.js";import"./index-ogXoivrg.js";import"./index-MVbLLYTZ.js";import"./index-PPLHz8o0.js";const h=`# Hero Image
+
+Een sfeerbepalende afbeelding die vaak over de volle breedte bovenaan een pagina wordt gebruikt.
+`;function i(o){const n=Object.assign({h2:"h2",h3:"h3",p:"p",code:"code",a:"a",ul:"ul",li:"li"},r(),o.components);return e.jsxs(e.Fragment,{children:[e.jsx(t,{of:l}),`
+`,e.jsx(d,{children:h}),`
+`,e.jsx(n.h2,{id:"voorbeelden",children:"Voorbeelden"}),`
+`,e.jsx(n.h3,{id:"basis",children:"Basis"}),`
+`,e.jsxs(n.p,{children:[`De beeldverhouding van de afbeelding is 32:9.
+Dat komt overeen met de `,e.jsx(n.code,{children:"2x-wide"})," ",e.jsx(n.a,{href:"?path=/docs/react_layout-aspect-ratio--docs",children:"aspect ratio"}),"."]}),`
+`,e.jsx(a,{}),`
+`,e.jsx(n.h3,{id:"met-zoekveld",children:"Met zoekveld"}),`
+`,e.jsxs(n.p,{children:["Een ",e.jsx(n.a,{href:"?path=/docs/react_layout-grid--docs",children:"Grid"}),` verzorgt horizontale witruimte aan weerszijden en kolommen voor layout.
+Zo kan bijvoorbeeld een zoekveld geplaatst worden.
+Verticaal centreren gaat vanzelf.`]}),`
+`,e.jsx(s,{of:c}),`
+`,e.jsx(n.h2,{id:"richtlijnen",children:"Richtlijnen"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["Volg ",e.jsx(n.a,{href:"?path=/docs/react_media-image--docs#richtlijnen",children:"de algemene richtlijnen voor afbeeldingen"}),"."]}),`
+`,e.jsx(n.li,{children:`Gebruik zeker voor de ‘hero’ een voldoende grote afbeelding.
+Als het nodig is, wordt de afbeelding uitgerekt om de volledige oppervlakte te bedekken.
+Dat kan ten koste gaan van de beeldkwaliteit.`}),`
+`,e.jsx(n.li,{children:`Zorg dat de beeldverhouding van de afbeelding 32:9 is.
+Anders wordt de afbeelding bijgesneden.`}),`
+`]})]})}function D(o={}){const{wrapper:n}=Object.assign({},r(),o.components);return n?e.jsx(n,Object.assign({},o,{children:e.jsx(i,o)})):i(o)}export{D as default};
