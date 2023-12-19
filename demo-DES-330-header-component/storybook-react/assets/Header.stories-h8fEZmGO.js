dbb6249d0d34fdb7e4aea3305d569b3a208c50e2
dlnr <niels@dlnr.dev>
2023-12-19 10:12:34 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@5ac021515d5204c55c6fe9e071e11ea085153722 🚀

---
+++ demo-DES-330-header-component/storybook-react/assets/Header.stories-h8fEZmGO.js
@@ -0,0 +1,32 @@
+import{h as S,j as e,f as n,S as A}from"./index.esm-u8MMFdpo.js";const x={title:"Containers/Header",component:S},a={},r={args:{logoBrand:"ggd-amsterdam",logoLink:"https://www.ggd.amsterdam.nl/",logoLinkTitle:"Naar de homepage van de GGD Amsterdam"}},s={args:{title:"Aan de Amsterdamse grachten"}},t={args:{menu:e.jsxs(n,{alignEnd:!0,children:[e.jsx(n.Link,{href:"#",children:"Contact"}),e.jsx(n.Link,{href:"#",children:"Mijn Amsterdam"}),e.jsx(n.Link,{href:"#",icon:A,children:"Zoeken"})]})}},o={args:{title:"Aan de Amsterdamse grachten",menu:e.jsxs(n,{alignEnd:!0,children:[e.jsx(n.Link,{href:"#",children:"Contact"}),e.jsx(n.Link,{href:"#",children:"Mijn Amsterdam"}),e.jsx(n.Link,{href:"#",icon:A,children:"Zoeken"})]})}};var i,c,d;a.parameters={...a.parameters,docs:{...(i=a.parameters)==null?void 0:i.docs,source:{originalSource:"{}",...(d=(c=a.parameters)==null?void 0:c.docs)==null?void 0:d.source}}};var m,g,u;r.parameters={...r.parameters,docs:{...(m=r.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    logoBrand: 'ggd-amsterdam',
+    logoLink: 'https://www.ggd.amsterdam.nl/',
+    logoLinkTitle: 'Naar de homepage van de GGD Amsterdam'
+  }
+}`,...(u=(g=r.parameters)==null?void 0:g.docs)==null?void 0:u.source}}};var l,h,p;s.parameters={...s.parameters,docs:{...(l=s.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    title: 'Aan de Amsterdamse grachten'
+  }
+}`,...(p=(h=s.parameters)==null?void 0:h.docs)==null?void 0:p.source}}};var k,M,L;t.parameters={...t.parameters,docs:{...(k=t.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  args: {
+    menu: <PageMenu alignEnd>
+        <PageMenu.Link href="#">Contact</PageMenu.Link>
+        <PageMenu.Link href="#">Mijn Amsterdam</PageMenu.Link>
+        <PageMenu.Link href="#" icon={SearchIcon}>
+          Zoeken
+        </PageMenu.Link>
+      </PageMenu>
+  }
+}`,...(L=(M=t.parameters)==null?void 0:M.docs)==null?void 0:L.source}}};var f,P,j;o.parameters={...o.parameters,docs:{...(f=o.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    title: 'Aan de Amsterdamse grachten',
+    menu: <PageMenu alignEnd>
+        <PageMenu.Link href="#">Contact</PageMenu.Link>
+        <PageMenu.Link href="#">Mijn Amsterdam</PageMenu.Link>
+        <PageMenu.Link href="#" icon={SearchIcon}>
+          Zoeken
+        </PageMenu.Link>
+      </PageMenu>
+  }
+}`,...(j=(P=o.parameters)==null?void 0:P.docs)==null?void 0:j.source}}};const W=["Default","WithLogoVariant","WithTitle","WithHeaderMenu","WithTitleAndMenu"],w=Object.freeze(Object.defineProperty({__proto__:null,Default:a,WithHeaderMenu:t,WithLogoVariant:r,WithTitle:s,WithTitleAndMenu:o,__namedExportsOrder:W,default:x},Symbol.toStringTag,{value:"Module"}));export{w as H,r as W,s as a,t as b,o as c};
