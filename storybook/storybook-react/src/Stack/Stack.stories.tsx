91557db80f5de943cb6133b7bc50e6e30719e44a
Vincent Smedinga <vincent@smedinga.nl>
2023-12-20 22:53:08 +0100

    Use aspect ratio in example

--- storybook/storybook-react/src/Stack/Stack.stories.tsx
+++ storybook/storybook-react/src/Stack/Stack.stories.tsx
@@ -3,7 +3,7 @@
  * Copyright (c) 2023 Gemeente Amsterdam
  */
 
-import { Grid, Image, SearchField, Stack } from '@amsterdam/design-system-react'
+import { AspectRatio, Grid, Image, SearchField, Stack } from '@amsterdam/design-system-react'
 import { Meta, StoryObj } from '@storybook/react'
 
 const meta = {
@@ -19,12 +19,14 @@ export const Default: Story = {
   args: {
     children: (
       <>
-        <Image
-          cover
-          sizes="(max-width: 36rem) 640px, (max-width: 68rem) 1280px, 1600px"
-          src="https://picsum.photos/640/180"
-          srcSet="https://picsum.photos/640/180 640w, https://picsum.photos/1280/360 1280w, https://picsum.photos/1600/450 1600w"
-        />
+        <AspectRatio ratio="2x-wide">
+          <Image
+            cover
+            sizes="(max-width: 36rem) 640px, (max-width: 68rem) 1280px, 1600px"
+            src="https://picsum.photos/640/180"
+            srcSet="https://picsum.photos/640/180 640w, https://picsum.photos/1280/360 1280w, https://picsum.photos/1600/450 1600w"
+          />
+        </AspectRatio>
         <Grid style={{ alignSelf: 'center' }}>
           <Grid.Cell span={{ narrow: 4, medium: 6, wide: 8 }} start={{ narrow: 1, medium: 2, wide: 3 }}>
             <SearchField onSubmit={(e) => e.preventDefault()}>
