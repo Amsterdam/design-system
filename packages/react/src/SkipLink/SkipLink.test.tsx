ee3ee75e6165b3c54e010af054ca2013abdf9c00
Aram Limpens <a.limpens@amsterdam.nl>
2023-12-20 15:08:41 +0100

    Update tests

--- packages/react/src/SkipLink/SkipLink.test.tsx
+++ packages/react/src/SkipLink/SkipLink.test.tsx
@@ -1,42 +1,41 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import { createRef } from 'react'
 import { SkipLink } from './SkipLink'
 import '@testing-library/jest-dom'
 
 describe('Skip link', () => {
   it('renders', () => {
-    const { container } = render(<SkipLink />)
+    render(<SkipLink href="/" />)
 
-    const component = container.querySelector(':only-child')
+    const component = screen.getByRole('link')
 
     expect(component).toBeInTheDocument()
     expect(component).toBeVisible()
   })
 
   it('renders a design system BEM class name', () => {
-    const { container } = render(<SkipLink />)
+    render(<SkipLink href="/" />)
 
-    const component = container.querySelector(':only-child')
+    const component = screen.getByRole('link')
 
     expect(component).toHaveClass('amsterdam-skip-link')
   })
 
   it('renders an additional class name', () => {
-    const { container } = render(<SkipLink className="extra" />)
+    render(<SkipLink href="/" className="extra" />)
 
-    const component = container.querySelector(':only-child')
+    const component = screen.getByRole('link')
 
     expect(component).toHaveClass('extra')
-
     expect(component).toHaveClass('amsterdam-skip-link')
   })
 
   it('supports ForwardRef in React', () => {
-    const ref = createRef<HTMLElement>()
+    const ref = createRef<HTMLAnchorElement>()
 
-    const { container } = render(<SkipLink ref={ref} />)
+    render(<SkipLink href="/" ref={ref} />)
 
-    const component = container.querySelector(':only-child')
+    const component = screen.getByRole('link')
 
     expect(ref.current).toBe(component)
   })
