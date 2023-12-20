e8c3d39284ba0e046cd4dc47bd6634d349694358
Aram Limpens <a.limpens@amsterdam.nl>
2023-12-20 16:15:40 +0100

    Add stories

--- storybook/storybook-react/src/SkipLink/SkipLink.stories.tsx
+++ storybook/storybook-react/src/SkipLink/SkipLink.stories.tsx
@@ -3,7 +3,7 @@
  * Copyright (c) 2023 Gemeente Amsterdam
  */
 
-import { SkipLink } from '@amsterdam/design-system-react'
+import { Grid, Screen, SkipLink } from '@amsterdam/design-system-react'
 import { Meta, StoryObj } from '@storybook/react'
 
 const meta = {
@@ -13,10 +13,51 @@ const meta = {
     children: 'Direct naar inhoud',
     href: '#',
   },
+  argTypes: {
+    style: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+  decorators: [
+    (Story) => (
+      <Screen>
+        <Grid>
+          <Grid.Cell span="all">
+            <Story />
+          </Grid.Cell>
+        </Grid>
+      </Screen>
+    ),
+  ],
 } satisfies Meta<typeof SkipLink>
 
 export default meta
 
 type Story = StoryObj<typeof meta>
 
-export const Default: Story = {}
+export const Default: Story = {
+  args: {
+    style: {
+      clip: 'auto',
+      clipPath: 'none',
+      height: 'auto',
+      overflow: 'auto',
+      position: 'static',
+      whiteSpace: 'normal',
+      width: 'auto',
+    },
+  },
+}
+
+export const OnFocus: Story = {}
+
+export const MultipleLinks: Story = {
+  render: () => (
+    <>
+      <SkipLink href="#">Direct naar inhoud</SkipLink>
+      <SkipLink href="#">Direct naar footer</SkipLink>
+    </>
+  ),
+}
