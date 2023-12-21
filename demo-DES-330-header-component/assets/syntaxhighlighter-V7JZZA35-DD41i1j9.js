041e79bc16840e88ddb3ca0ddd24c8927c5adf94
dlnr <niels@dlnr.dev>
2023-12-21 09:04:40 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@fd8d74d1e6aa30b80b0f55f5dd42b46e5e7afc3d 🚀

---
+++ demo-DES-330-header-component/assets/syntaxhighlighter-V7JZZA35-DD41i1j9.js
@@ -0,0 +1,1 @@
+import{S as l,c as n,s as d}from"./index--ZicTJkQ.js";import"./iframe-nPILMYi4.js";import"../sb-preview/runtime.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./index-jmm5gWkb.js";import"./index-ogXoivrg.js";import"./index-MVbLLYTZ.js";import"./index-PPLHz8o0.js";export{l as SyntaxHighlighter,n as createCopyToClipboardFunction,d as default};
