b9be0d3badd1d6546fb4448253a681c6b485e540
Vincent Smedinga <vincent@smedinga.nl>
2023-12-22 11:26:02 +0100

    List story children as array

--- storybook/storybook-react/src/Overlap/Overlap.stories.tsx
+++ storybook/storybook-react/src/Overlap/Overlap.stories.tsx
@@ -24,26 +24,24 @@ type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
   args: {
-    children: (
-      <>
-        <AspectRatio ratio="2x-wide">
-          <Image
-            alt=""
-            cover
-            sizes="(max-width: 36rem) 640px, (max-width: 68rem) 1280px, 1600px"
-            src="https://picsum.photos/640/180"
-            srcSet="https://picsum.photos/640/180 640w, https://picsum.photos/1280/360 1280w, https://picsum.photos/1600/450 1600w"
-          />
-        </AspectRatio>
-        <Grid style={{ alignSelf: 'center' }}>
-          <Grid.Cell span={{ narrow: 4, medium: 6, wide: 8 }} start={{ narrow: 1, medium: 2, wide: 3 }}>
-            <SearchField onSubmit={(e) => e.preventDefault()}>
-              <SearchField.Input label="Zoeken" placeholder="Wat kunnen we voor u vinden?" />
-              <SearchField.Button />
-            </SearchField>
-          </Grid.Cell>
-        </Grid>
-      </>
-    ),
+    children: [
+      <AspectRatio key="background-image" ratio="2x-wide">
+        <Image
+          alt=""
+          cover
+          sizes="(max-width: 36rem) 640px, (max-width: 68rem) 1280px, 1600px"
+          src="https://picsum.photos/640/180"
+          srcSet="https://picsum.photos/640/180 640w, https://picsum.photos/1280/360 1280w, https://picsum.photos/1600/450 1600w"
+        />
+      </AspectRatio>,
+      <Grid key="search-field" style={{ alignSelf: 'center' }}>
+        <Grid.Cell span={{ narrow: 4, medium: 6, wide: 8 }} start={{ narrow: 1, medium: 2, wide: 3 }}>
+          <SearchField onSubmit={(e) => e.preventDefault()}>
+            <SearchField.Input label="Zoeken" placeholder="Wat kunnen we voor u vinden?" />
+            <SearchField.Button />
+          </SearchField>
+        </Grid.Cell>
+      </Grid>,
+    ],
   },
 }
