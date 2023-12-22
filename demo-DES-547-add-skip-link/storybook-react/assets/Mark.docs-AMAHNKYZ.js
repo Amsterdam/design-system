67eb151bab8ca089b148bbca6e0823a1aa06a3b5
alimpens <37216945+alimpens@users.noreply.github.com>
2023-12-22 10:27:10 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@a4b80576bbc26ec5ed04075376bf82d56ba3e69e 🚀

---
+++ demo-DES-547-add-skip-link/storybook-react/assets/Mark.docs-AMAHNKYZ.js
@@ -0,0 +1,22 @@
+import{j as e}from"./index.esm-HoHouYBq.js";import{M as a,d as i,P as s}from"./index-kr1xRfbi.js";import{M as m}from"./Mark.stories-JDZdvuII.js";import{useMDXComponents as o}from"./index-_VGcpBFS.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./extends-dGVwEr9R.js";import"./iframe-mzFof4G4.js";import"../sb-preview/runtime.js";import"./index-jmm5gWkb.js";import"./index-ogXoivrg.js";import"./index-MVbLLYTZ.js";import"./index-PPLHz8o0.js";const d=`# Mark
+
+Markeert een fragment tekst met een gele achtergrond.
+Het zorgt voor sterke nadruk op de tekst en leidt de aandacht ernaartoe.
+
+## Richtlijnen
+
+### Zo gebruiken
+
+- Maximaal 1 markering per pagina.
+  De voorkeur is geen markering.
+- Maximaal 1 zin.
+
+### Dit vermijden
+
+- Niet in de titel en subtitels gebruiken.
+  Zorg voor een belangrijke, pakkende titel als de hele paragraaf belangrijk is.
+- Mag niet voor een hele paragraaf gebruikt worden.
+`;function t(n){const r=Object.assign({h2:"h2"},o(),n.components);return e.jsxs(e.Fragment,{children:[e.jsx(a,{of:m}),`
+`,e.jsx(i,{children:d}),`
+`,e.jsx(r.h2,{id:"voorbeeld",children:"Voorbeeld"}),`
+`,e.jsx(s,{})]})}function D(n={}){const{wrapper:r}=Object.assign({},o(),n.components);return r?e.jsx(r,Object.assign({},n,{children:e.jsx(t,n)})):t(n)}export{D as default};
