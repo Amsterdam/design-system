23a0f0c34568d0acee0ab8d3d3a141ba53e749f6
dlnr <niels@dlnr.dev>
2024-01-02 12:06:21 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@48341dea9fe036a11f9993c09a546170a1bbace4 🚀

--- demo-DES-486-MegaMenu/storybook-react/assets/preview-1byKxOz7.js
+++
@@ -1,7 +0,0 @@
-import{_ as e}from"./iframe-bzvbkwz7.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-QokxRuoB.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10]),import.meta.url);return new r}}};export{a as parameters};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-QokxRuoB.js","./iframe-bzvbkwz7.js","./index-4g5l5LRQ.js","./_commonjsHelpers-4gQjN7DL.js","./react-18-ba7OOUbL.js","./index-jmm5gWkb.js","./index-Sf8ef8JX.js","./index-ogXoivrg.js","./extends-dGVwEr9R.js","./index-MVbLLYTZ.js","./index-PPLHz8o0.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
