4334ee8215ac94ff53e9ceeba3c21d3b09a4b700
dlnr <niels@dlnr.dev>
2024-01-02 14:55:07 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@3160a9e04b6cc040f5a7c1f09c36032a85a4d93b 🚀

---
+++ demo-DES-565-Header-Menu/storybook-react/assets/Header.stories-J59H9Zvo.js
@@ -0,0 +1,201 @@
+import{h as E,j as e,f as i,S as u,G as c,H as h,U as n,L as r}from"./index.esm-LlK2b1qM.js";const D={title:"Containers/Header",component:E,argTypes:{logoBrand:{control:{type:"select"},options:["amsterdam","ggd-amsterdam","stadsarchief","stadsbank-van-lening","vga-verzekeringen"]}},parameters:{layout:"fullscreen"}},t={},s={args:{logoBrand:"ggd-amsterdam",logoLink:"https://www.ggd.amsterdam.nl/",logoLinkTitle:"Naar de homepage van de GGD Amsterdam"}},a={args:{title:"Aan de Amsterdamse grachten"}},d={args:{links:e.jsxs(i,{alignEnd:!0,children:[e.jsx(i.Link,{href:"#",children:"Contact"}),e.jsx(i.Link,{href:"#",children:"Mijn Amsterdam"}),e.jsx(i.Link,{href:"#",icon:u,children:"Zoeken"})]})}},o={args:{menu:e.jsx("button",{className:"amsterdam-header__menu-button",children:"Menu"})}},m={args:{menu:e.jsx("button",{className:"amsterdam-header__menu-button",children:"Menu"}),links:e.jsxs(i,{alignEnd:!0,children:[e.jsx(i.Link,{href:"#",children:"Contact"}),e.jsx(i.Link,{href:"#",children:"Mijn Amsterdam"}),e.jsx(i.Link,{href:"#",icon:u,children:"Zoeken"})]})}},l={args:{title:"Aan de Amsterdamse grachten",menu:e.jsx("button",{className:"amsterdam-header__menu-button",children:"Menu"})}},L={args:{title:"Aan de Amsterdamse grachten",links:e.jsxs(i,{alignEnd:!0,children:[e.jsx(i.Link,{href:"#",children:"Contact"}),e.jsx(i.Link,{href:"#",children:"Mijn Amsterdam"}),e.jsx(i.Link,{href:"#",icon:u,children:"Zoeken"})]}),menu:e.jsxs(c,{paddingVertical:"medium",children:[e.jsxs(c.Cell,{span:{narrow:4,medium:8,wide:8},children:[e.jsx(h,{level:3,size:"level-3",children:"Thema’s"}),e.jsxs(n,{markers:!1,children:[e.jsx(n.Item,{children:e.jsx(r,{variant:"inList",href:"#",children:"Afval"})}),e.jsx(n.Item,{children:e.jsx(r,{variant:"inList",href:"#",children:"Bestuur en organisatie"})}),e.jsx(n.Item,{children:e.jsx(r,{variant:"inList",href:"#",children:"Bouw- en verkeersprojecten"})}),e.jsx(n.Item,{children:e.jsx(r,{variant:"inList",href:"#",children:"Burgerzaken"})}),e.jsx(n.Item,{children:e.jsx(r,{variant:"inList",href:"#",children:"Diversiteit"})}),e.jsx(n.Item,{children:e.jsx(r,{variant:"inList",href:"#",children:"Gemeentebelastingen"})}),e.jsx(n.Item,{children:e.jsx(r,{variant:"inList",href:"#",children:"Gezondheidsdienst (GGD)"})}),e.jsx(n.Item,{children:e.jsx(r,{variant:"inList",href:"#",children:"Kunst en cultuur"})}),e.jsx(n.Item,{children:e.jsx(r,{variant:"inList",href:"#",children:"Ondernemen"})}),e.jsx(n.Item,{children:e.jsx(r,{variant:"inList",href:"#",children:"Onderwijs en jeugd"})}),e.jsx(n.Item,{children:e.jsx(r,{variant:"inList",href:"#",children:"Parkeren"})}),e.jsx(n.Item,{children:e.jsx(r,{variant:"inList",href:"#",children:"Sport"})}),e.jsx(n.Item,{children:e.jsx(r,{variant:"inList",href:"#",children:"Stadsdelen"})}),e.jsx(n.Item,{children:e.jsx(r,{variant:"inList",href:"#",children:"Subsidies"})})]})]}),e.jsxs(c.Cell,{span:{narrow:4,medium:8,wide:4},children:[e.jsx(h,{level:3,size:"level-3",children:"Categorieën"}),e.jsxs(n,{markers:!1,children:[e.jsx(n.Item,{children:e.jsx(r,{variant:"inList",href:"#",children:"Vacatures"})}),e.jsx(n.Item,{children:e.jsx(r,{variant:"inList",href:"#",children:"Verkeer en vervoer"})}),e.jsx(n.Item,{children:e.jsx(r,{variant:"inList",href:"#",children:"Vrije tijd"})}),e.jsx(n.Item,{children:e.jsx(r,{variant:"inList",href:"#",children:"Werk aan de weg"})}),e.jsx(n.Item,{children:e.jsx(r,{variant:"inList",href:"#",children:"Werk en inkomen"})}),e.jsx(n.Item,{children:e.jsx(r,{variant:"inList",href:"#",children:"Wonen en leefomgeving"})})]}),e.jsx(h,{level:3,size:"level-3",children:"Snel naar"}),e.jsxs(n,{markers:!1,children:[e.jsx(n.Item,{children:e.jsx(r,{variant:"inList",href:"#",children:"Vacatures"})}),e.jsx(n.Item,{children:e.jsx(r,{variant:"inList",href:"#",children:"Verkeer en vervoer"})}),e.jsx(n.Item,{children:e.jsx(r,{variant:"inList",href:"#",children:"Vrije tijd"})}),e.jsx(n.Item,{children:e.jsx(r,{variant:"inList",href:"#",children:"Werk aan de weg"})})]})]})]},"nav")}};var k,g,j;t.parameters={...t.parameters,docs:{...(k=t.parameters)==null?void 0:k.docs,source:{originalSource:"{}",...(j=(g=t.parameters)==null?void 0:g.docs)==null?void 0:j.source}}};var f,v,x;s.parameters={...s.parameters,docs:{...(f=s.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    logoBrand: 'ggd-amsterdam',
+    logoLink: 'https://www.ggd.amsterdam.nl/',
+    logoLinkTitle: 'Naar de homepage van de GGD Amsterdam'
+  }
+}`,...(x=(v=s.parameters)==null?void 0:v.docs)==null?void 0:x.source}}};var I,p,U;a.parameters={...a.parameters,docs:{...(I=a.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  args: {
+    title: 'Aan de Amsterdamse grachten'
+  }
+}`,...(U=(p=a.parameters)==null?void 0:p.docs)==null?void 0:U.source}}};var M,A,P;d.parameters={...d.parameters,docs:{...(M=d.parameters)==null?void 0:M.docs,source:{originalSource:`{
+  args: {
+    links: <PageMenu alignEnd>
+        <PageMenu.Link href="#">Contact</PageMenu.Link>
+        <PageMenu.Link href="#">Mijn Amsterdam</PageMenu.Link>
+        <PageMenu.Link href="#" icon={SearchIcon}>
+          Zoeken
+        </PageMenu.Link>
+      </PageMenu>
+  }
+}`,...(P=(A=d.parameters)==null?void 0:A.docs)==null?void 0:P.source}}};var S,b,W;o.parameters={...o.parameters,docs:{...(S=o.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    menu: <button className="amsterdam-header__menu-button">Menu</button>
+  }
+}`,...(W=(b=o.parameters)==null?void 0:b.docs)==null?void 0:W.source}}};var w,G,_;m.parameters={...m.parameters,docs:{...(w=m.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  args: {
+    menu: <button className="amsterdam-header__menu-button">Menu</button>,
+    links: <PageMenu alignEnd>
+        <PageMenu.Link href="#">Contact</PageMenu.Link>
+        <PageMenu.Link href="#">Mijn Amsterdam</PageMenu.Link>
+        <PageMenu.Link href="#" icon={SearchIcon}>
+          Zoeken
+        </PageMenu.Link>
+      </PageMenu>
+  }
+}`,...(_=(G=m.parameters)==null?void 0:G.docs)==null?void 0:_.source}}};var V,C,z;l.parameters={...l.parameters,docs:{...(V=l.parameters)==null?void 0:V.docs,source:{originalSource:`{
+  args: {
+    title: 'Aan de Amsterdamse grachten',
+    menu: <button className="amsterdam-header__menu-button">Menu</button>
+  }
+}`,...(z=(C=l.parameters)==null?void 0:C.docs)==null?void 0:z.source}}};var H,T,B;L.parameters={...L.parameters,docs:{...(H=L.parameters)==null?void 0:H.docs,source:{originalSource:`{
+  args: {
+    title: 'Aan de Amsterdamse grachten',
+    links: <PageMenu alignEnd>
+        <PageMenu.Link href="#">Contact</PageMenu.Link>
+        <PageMenu.Link href="#">Mijn Amsterdam</PageMenu.Link>
+        <PageMenu.Link href="#" icon={SearchIcon}>
+          Zoeken
+        </PageMenu.Link>
+      </PageMenu>,
+    menu: <Grid key="nav" paddingVertical="medium">
+        <Grid.Cell span={{
+        narrow: 4,
+        medium: 8,
+        wide: 8
+      }}>
+          <Heading level={3} size="level-3">
+            Thema&rsquo;s
+          </Heading>
+          <UnorderedList markers={false}>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Afval
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Bestuur en organisatie
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Bouw- en verkeersprojecten
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Burgerzaken
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Diversiteit
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Gemeentebelastingen
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Gezondheidsdienst (GGD)
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Kunst en cultuur
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Ondernemen
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Onderwijs en jeugd
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Parkeren
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Sport
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Stadsdelen
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Subsidies
+              </Link>
+            </UnorderedList.Item>
+          </UnorderedList>
+        </Grid.Cell>
+        <Grid.Cell span={{
+        narrow: 4,
+        medium: 8,
+        wide: 4
+      }}>
+          <Heading level={3} size="level-3">
+            Categorieën
+          </Heading>
+          <UnorderedList markers={false}>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Vacatures
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Verkeer en vervoer
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Vrije tijd
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Werk aan de weg
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Werk en inkomen
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Wonen en leefomgeving
+              </Link>
+            </UnorderedList.Item>
+          </UnorderedList>
+          <Heading level={3} size="level-3">
+            Snel naar
+          </Heading>
+          <UnorderedList markers={false}>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Vacatures
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Verkeer en vervoer
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Vrije tijd
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Werk aan de weg
+              </Link>
+            </UnorderedList.Item>
+          </UnorderedList>
+        </Grid.Cell>
+      </Grid>
+  }
+}`,...(B=(T=L.parameters)==null?void 0:T.docs)==null?void 0:B.source}}};const N=["Default","WithLogoVariant","WithTitle","WithLinks","WithMenu","WithLinksAndMenu","WithTitleAndMenu","WithTitleLinksAndMenu"],y=Object.freeze(Object.defineProperty({__proto__:null,Default:t,WithLinks:d,WithLinksAndMenu:m,WithLogoVariant:s,WithMenu:o,WithTitle:a,WithTitleAndMenu:l,WithTitleLinksAndMenu:L,__namedExportsOrder:N,default:D},Symbol.toStringTag,{value:"Module"}));export{y as H,s as W,a,o as b,d as c,m as d,l as e,L as f};
