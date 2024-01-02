b616d68512d584d6d3b1c98a9c8d3e731f9e3dc6
dlnr <niels@dlnr.dev>
2024-01-02 12:11:22 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@48341dea9fe036a11f9993c09a546170a1bbace4 🚀

---
+++ demo-DES-565-Header-Menu/assets/preview-Aks7g1Tb.js
@@ -0,0 +1,7 @@
+import{_ as e}from"./iframe-9V1uR7F-.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-V8FWHcD7.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9]),import.meta.url);return new r}}};export{a as parameters};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-V8FWHcD7.js","./iframe-9V1uR7F-.js","./index-4g5l5LRQ.js","./_commonjsHelpers-4gQjN7DL.js","./react-18-ba7OOUbL.js","./index-jmm5gWkb.js","./index-9kxpaTzn.js","./index-ogXoivrg.js","./index-MVbLLYTZ.js","./index-PPLHz8o0.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
