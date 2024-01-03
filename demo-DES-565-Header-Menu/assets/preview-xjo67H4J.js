7248cf99cf8d3f2f472c338bd6b94752c43ac1d0
dlnr <niels@dlnr.dev>
2024-01-03 15:33:13 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@8f4e826cf4dd9d3307917b426403bdd20e2065e8 🚀

---
+++ demo-DES-565-Header-Menu/assets/preview-xjo67H4J.js
@@ -0,0 +1,7 @@
+import{_ as e}from"./iframe-DB4-MaXC.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-C4dMyJ2H.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9]),import.meta.url);return new r}}};export{a as parameters};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-C4dMyJ2H.js","./iframe-DB4-MaXC.js","./index-4g5l5LRQ.js","./_commonjsHelpers-4gQjN7DL.js","./react-18-ba7OOUbL.js","./index-jmm5gWkb.js","./index-I5Rl9KUF.js","./index-ogXoivrg.js","./index-MVbLLYTZ.js","./index-PPLHz8o0.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
---
+++ demo-DES-565-Header-Menu/storybook-react/assets/MegaMenu.stories-nnBOt7RC.js
@@ -0,0 +1,265 @@
+import{o as c,j as e,G as s,H as d,U as n,L as i}from"./index.esm-TWovk5rU.js";const v={title:"Navigation/Mega Menu",component:c,args:{isOpen:!0},parameters:{layout:"fullscreen"}},r={args:{children:e.jsx(s,{paddingVertical:"medium",children:e.jsxs(s.Cell,{span:"all",children:[e.jsx(d,{level:1,size:"level-2",className:"amsterdam-mega-menu__heading",children:"Alle onderwerpen"}),e.jsxs(n,{markers:!1,children:[e.jsx(n.Item,{children:e.jsx(i,{variant:"inList",href:"#",children:"Afval"})}),e.jsx(n.Item,{children:e.jsx(i,{variant:"inList",href:"#",children:"Bestuur en organisatie"})}),e.jsx(n.Item,{children:e.jsx(i,{variant:"inList",href:"#",children:"Bouw- en verkeersprojecten"})}),e.jsx(n.Item,{children:e.jsx(i,{variant:"inList",href:"#",children:"Burgerzaken"})}),e.jsx(n.Item,{children:e.jsx(i,{variant:"inList",href:"#",children:"Diversiteit"})}),e.jsx(n.Item,{children:e.jsx(i,{variant:"inList",href:"#",children:"Gemeentebelastingen"})}),e.jsx(n.Item,{children:e.jsx(i,{variant:"inList",href:"#",children:"Gezondheidsdienst (GGD)"})}),e.jsx(n.Item,{children:e.jsx(i,{variant:"inList",href:"#",children:"Kunst en cultuur"})}),e.jsx(n.Item,{children:e.jsx(i,{variant:"inList",href:"#",children:"Ondernemen"})}),e.jsx(n.Item,{children:e.jsx(i,{variant:"inList",href:"#",children:"Onderwijs en jeugd"})}),e.jsx(n.Item,{children:e.jsx(i,{variant:"inList",href:"#",children:"Parkeren"})}),e.jsx(n.Item,{children:e.jsx(i,{variant:"inList",href:"#",children:"Sport"})}),e.jsx(n.Item,{children:e.jsx(i,{variant:"inList",href:"#",children:"Stadsdelen"})}),e.jsx(n.Item,{children:e.jsx(i,{variant:"inList",href:"#",children:"Subsidies"})}),e.jsx(n.Item,{children:e.jsx(i,{variant:"inList",href:"#",children:"Vacatures"})}),e.jsx(n.Item,{children:e.jsx(i,{variant:"inList",href:"#",children:"Verkeer en vervoer"})}),e.jsx(n.Item,{children:e.jsx(i,{variant:"inList",href:"#",children:"Vrije tijd"})}),e.jsx(n.Item,{children:e.jsx(i,{variant:"inList",href:"#",children:"Werk aan de weg"})}),e.jsx(n.Item,{children:e.jsx(i,{variant:"inList",href:"#",children:"Werk en inkomen"})}),e.jsx(n.Item,{children:e.jsx(i,{variant:"inList",href:"#",children:"Wonen en leefomgeving"})})]})]})},"nav")}},t={args:{children:e.jsxs(s,{paddingVertical:"medium",children:[e.jsxs(s.Cell,{span:{narrow:4,medium:8,wide:8},children:[e.jsx(d,{level:3,size:"level-3",children:"Thema’s"}),e.jsxs(n,{markers:!1,children:[e.jsx(n.Item,{children:e.jsx(i,{variant:"inList",href:"#",children:"Afval"})}),e.jsx(n.Item,{children:e.jsx(i,{variant:"inList",href:"#",children:"Bestuur en organisatie"})}),e.jsx(n.Item,{children:e.jsx(i,{variant:"inList",href:"#",children:"Bouw- en verkeersprojecten"})}),e.jsx(n.Item,{children:e.jsx(i,{variant:"inList",href:"#",children:"Burgerzaken"})}),e.jsx(n.Item,{children:e.jsx(i,{variant:"inList",href:"#",children:"Diversiteit"})}),e.jsx(n.Item,{children:e.jsx(i,{variant:"inList",href:"#",children:"Gemeentebelastingen"})}),e.jsx(n.Item,{children:e.jsx(i,{variant:"inList",href:"#",children:"Gezondheidsdienst (GGD)"})}),e.jsx(n.Item,{children:e.jsx(i,{variant:"inList",href:"#",children:"Kunst en cultuur"})}),e.jsx(n.Item,{children:e.jsx(i,{variant:"inList",href:"#",children:"Ondernemen"})}),e.jsx(n.Item,{children:e.jsx(i,{variant:"inList",href:"#",children:"Onderwijs en jeugd"})}),e.jsx(n.Item,{children:e.jsx(i,{variant:"inList",href:"#",children:"Parkeren"})}),e.jsx(n.Item,{children:e.jsx(i,{variant:"inList",href:"#",children:"Sport"})}),e.jsx(n.Item,{children:e.jsx(i,{variant:"inList",href:"#",children:"Stadsdelen"})}),e.jsx(n.Item,{children:e.jsx(i,{variant:"inList",href:"#",children:"Subsidies"})})]})]}),e.jsxs(s.Cell,{span:{narrow:4,medium:8,wide:4},children:[e.jsx(d,{level:3,size:"level-3",children:"Categorieën"}),e.jsxs(n,{markers:!1,children:[e.jsx(n.Item,{children:e.jsx(i,{variant:"inList",href:"#",children:"Vacatures"})}),e.jsx(n.Item,{children:e.jsx(i,{variant:"inList",href:"#",children:"Verkeer en vervoer"})}),e.jsx(n.Item,{children:e.jsx(i,{variant:"inList",href:"#",children:"Vrije tijd"})}),e.jsx(n.Item,{children:e.jsx(i,{variant:"inList",href:"#",children:"Werk aan de weg"})}),e.jsx(n.Item,{children:e.jsx(i,{variant:"inList",href:"#",children:"Werk en inkomen"})}),e.jsx(n.Item,{children:e.jsx(i,{variant:"inList",href:"#",children:"Wonen en leefomgeving"})})]}),e.jsx(d,{level:3,size:"level-3",children:"Snel naar"}),e.jsxs(n,{markers:!1,children:[e.jsx(n.Item,{children:e.jsx(i,{variant:"inList",href:"#",children:"Vacatures"})}),e.jsx(n.Item,{children:e.jsx(i,{variant:"inList",href:"#",children:"Verkeer en vervoer"})}),e.jsx(n.Item,{children:e.jsx(i,{variant:"inList",href:"#",children:"Vrije tijd"})}),e.jsx(n.Item,{children:e.jsx(i,{variant:"inList",href:"#",children:"Werk aan de weg"})})]})]})]},"nav")}};var a,L,l;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    children: <Grid key="nav" paddingVertical="medium">
+        <Grid.Cell span="all">
+          <Heading level={1} size="level-2" className="amsterdam-mega-menu__heading">
+            Alle onderwerpen
+          </Heading>
+          <UnorderedList markers={false}>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Afval
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Bestuur en organisatie
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Bouw- en verkeersprojecten
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Burgerzaken
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Diversiteit
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Gemeentebelastingen
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Gezondheidsdienst (GGD)
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Kunst en cultuur
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Ondernemen
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Onderwijs en jeugd
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Parkeren
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Sport
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Stadsdelen
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Subsidies
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Vacatures
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Verkeer en vervoer
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Vrije tijd
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Werk aan de weg
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Werk en inkomen
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Wonen en leefomgeving
+              </Link>
+            </UnorderedList.Item>
+          </UnorderedList>
+        </Grid.Cell>
+      </Grid>
+  }
+}`,...(l=(L=r.parameters)==null?void 0:L.docs)==null?void 0:l.source}}};var h,m,o;t.parameters={...t.parameters,docs:{...(h=t.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    children: <Grid key="nav" paddingVertical="medium">
+        <Grid.Cell span={{
+        narrow: 4,
+        medium: 8,
+        wide: 8
+      }}>
+          <Heading level={3} size="level-3">
+            Thema&rsquo;s
+          </Heading>
+          <UnorderedList markers={false}>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Afval
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Bestuur en organisatie
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Bouw- en verkeersprojecten
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Burgerzaken
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Diversiteit
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Gemeentebelastingen
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Gezondheidsdienst (GGD)
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Kunst en cultuur
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Ondernemen
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Onderwijs en jeugd
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Parkeren
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Sport
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Stadsdelen
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Subsidies
+              </Link>
+            </UnorderedList.Item>
+          </UnorderedList>
+        </Grid.Cell>
+        <Grid.Cell span={{
+        narrow: 4,
+        medium: 8,
+        wide: 4
+      }}>
+          <Heading level={3} size="level-3">
+            Categorieën
+          </Heading>
+          <UnorderedList markers={false}>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Vacatures
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Verkeer en vervoer
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Vrije tijd
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Werk aan de weg
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Werk en inkomen
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Wonen en leefomgeving
+              </Link>
+            </UnorderedList.Item>
+          </UnorderedList>
+          <Heading level={3} size="level-3">
+            Snel naar
+          </Heading>
+          <UnorderedList markers={false}>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Vacatures
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Verkeer en vervoer
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Vrije tijd
+              </Link>
+            </UnorderedList.Item>
+            <UnorderedList.Item>
+              <Link variant="inList" href="#">
+                Werk aan de weg
+              </Link>
+            </UnorderedList.Item>
+          </UnorderedList>
+        </Grid.Cell>
+      </Grid>
+  }
+}`,...(o=(m=t.parameters)==null?void 0:m.docs)==null?void 0:o.source}}};const I=["Default","MultipleCategories"],j=Object.freeze(Object.defineProperty({__proto__:null,Default:r,MultipleCategories:t,__namedExportsOrder:I,default:v},Symbol.toStringTag,{value:"Module"}));export{j as M,t as a};
