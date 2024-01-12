05db1cbb33549673e39615c1806da0b4266428bc
alimpens <37216945+alimpens@users.noreply.github.com>
2024-01-12 15:07:39 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@e26519aaf47dcc8f369f29fb2e69c07bfc0dcda7 🚀

---
+++ demo-DES-546-add-table/storybook-react/assets/Table.docs-BXSLvGmR.js
@@ -0,0 +1,7 @@
+import{j as t}from"./index.esm-OD6yB53t.js";import{M as s,d as i,P as a,f as m}from"./index-GZUvbDBT.js";import{T as c,W as d}from"./Table.stories-fzTnCh8A.js";import{useMDXComponents as e}from"./index-_VGcpBFS.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./extends-dGVwEr9R.js";import"./iframe-QQTmtyR5.js";import"../sb-preview/runtime.js";import"./index-jmm5gWkb.js";import"./index-ogXoivrg.js";import"./index-MVbLLYTZ.js";import"./index-PPLHz8o0.js";const h=`# Table
+`;function n(o){const r=Object.assign({h2:"h2",h3:"h3"},e(),o.components);return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:c}),`
+`,t.jsx(i,{children:h}),`
+`,t.jsx(a,{}),`
+`,t.jsx(r.h2,{id:"stories",children:"Stories"}),`
+`,t.jsx(r.h3,{id:"with-footer-and-row-headers",children:"With footer and row headers"}),`
+`,t.jsx(m,{of:d})]})}function O(o={}){const{wrapper:r}=Object.assign({},e(),o.components);return r?t.jsx(r,Object.assign({},o,{children:t.jsx(n,o)})):n(o)}export{O as default};
