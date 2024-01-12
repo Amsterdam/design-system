390ca0fa794460e3bb57ae6c737398623094068b
alimpens <37216945+alimpens@users.noreply.github.com>
2024-01-12 14:34:57 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@e60ee17b1d4144aca26a5b4340ea0bdec8667c41 🚀

--- demo-DES-546-add-table/storybook-react/assets/Table.docs-1kmX7SVD.js
+++
@@ -1,5 +0,0 @@
-import{j as t}from"./index.esm--OIS7uAl.js";import{M as s,d as e,P as i,e as m}from"./index-7JiANIOQ.js";import{T as a}from"./Table.stories-LUGzTqi3.js";import{useMDXComponents as p}from"./index-_VGcpBFS.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./extends-dGVwEr9R.js";import"./iframe-y2-IdRiX.js";import"../sb-preview/runtime.js";import"./index-jmm5gWkb.js";import"./index-ogXoivrg.js";import"./index-MVbLLYTZ.js";import"./index-PPLHz8o0.js";const c=`# Table
-`;function n(o){return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:a}),`
-`,t.jsx(e,{children:c}),`
-`,t.jsx(i,{}),`
-`,t.jsx(m,{})]})}function T(o={}){const{wrapper:r}=Object.assign({},p(),o.components);return r?t.jsx(r,Object.assign({},o,{children:t.jsx(n,o)})):n()}export{T as default};
