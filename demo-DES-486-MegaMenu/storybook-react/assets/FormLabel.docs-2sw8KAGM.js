dc162e24fa5dfc9ff83c75492080aff89102cc4c
dlnr <niels@dlnr.dev>
2024-01-12 13:03:24 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@e41ebd1572422deeacd0f133141894fe433c042c 🚀

--- demo-DES-486-MegaMenu/storybook-react/assets/FormLabel.docs-2sw8KAGM.js
+++
@@ -1,4 +0,0 @@
-import{j as e}from"./index.esm-oVJlwA_G.js";import{M as o,d as m,P as i,e as s}from"./index-Oe5kz9w3.js";import{F as a}from"./FormLabel.stories-HXwkk2YS.js";import{useMDXComponents as p}from"./index-_VGcpBFS.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./extends-dGVwEr9R.js";import"./iframe-jzO9lr-y.js";import"../sb-preview/runtime.js";import"./index-jmm5gWkb.js";import"./index-ogXoivrg.js";import"./index-MVbLLYTZ.js";import"./index-PPLHz8o0.js";const l="# Form Label\n\nEen label element gebruik je om een formulierelement te beschrijven. Koppel een formulierelement (zoals een `input` of `textarea`) altijd aan een label.\n";function n(t){return e.jsxs(e.Fragment,{children:[e.jsx(o,{of:a}),`
-`,e.jsx(m,{children:l}),`
-`,e.jsx(i,{}),`
-`,e.jsx(s,{})]})}function F(t={}){const{wrapper:r}=Object.assign({},p(),t.components);return r?e.jsx(r,Object.assign({},t,{children:e.jsx(n,t)})):n()}export{F as default};
