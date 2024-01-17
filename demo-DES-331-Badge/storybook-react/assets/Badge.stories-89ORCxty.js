b299106c51472d994c433d88e2d09d71f927c418
dlnr <niels@dlnr.dev>
2024-01-17 08:00:44 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@2bfa7e76ad91a82c9d055d93dd3babac175206eb 🚀

---
+++ demo-DES-331-Badge/storybook-react/assets/Badge.stories-89ORCxty.js
@@ -0,0 +1,4 @@
+import{B as t}from"./index.esm-yb5-WSZz.js";/**
+ * @license EUPL-1.2+
+ * Copyright (c) 2023 Gemeente Amsterdam
+ */const n={title:"Feedback/Badge",component:t,args:{children:"Tip"},argTypes:{color:{control:{type:"inline-radio"},options:["blue","dark-blue","green","dark-green","orange","magenta","purple","yellow"],selected:"dark-green"}}},e={};var r,o,a;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:"{}",...(a=(o=e.parameters)==null?void 0:o.docs)==null?void 0:a.source}}};const s=["Default"],c=Object.freeze(Object.defineProperty({__proto__:null,Default:e,__namedExportsOrder:s,default:n},Symbol.toStringTag,{value:"Module"}));export{c as B};
