40fde932de1a3f54460f62c349b1a1bf6aac71f2
dlnr <niels@dlnr.dev>
2024-01-18 14:29:44 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@69a35df7e3b7d821e4d858755e8b4fea8bd8b2fd 🚀

---
+++ demo-DES-331-Badge/storybook-react/assets/Badge.docs-wTNCFl9c.js
@@ -0,0 +1,5 @@
+import{j as t}from"./index.esm-yb5-WSZz.js";import{M as s,d as e,P as i,e as m}from"./index-lvFf_CT5.js";import{B as a}from"./Badge.stories-89ORCxty.js";import{useMDXComponents as p}from"./index-_VGcpBFS.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./extends-dGVwEr9R.js";import"./iframe-V86k4liM.js";import"../sb-preview/runtime.js";import"./index-jmm5gWkb.js";import"./index-ogXoivrg.js";import"./index-MVbLLYTZ.js";import"./index-PPLHz8o0.js";const c=`# Badge
+`;function n(o){return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:a}),`
+`,t.jsx(e,{children:c}),`
+`,t.jsx(i,{}),`
+`,t.jsx(m,{})]})}function X(o={}){const{wrapper:r}=Object.assign({},p(),o.components);return r?t.jsx(r,Object.assign({},o,{children:t.jsx(n,o)})):n()}export{X as default};
---
+++ demo-DES-331-Badge/storybook-react/assets/Table.docs-yAdiSOMx.js
@@ -0,0 +1,18 @@
+import{j as e}from"./index.esm-yb5-WSZz.js";import{M as r,d as a,P as i,f as d}from"./index-lvFf_CT5.js";import{T as m,W as c}from"./Table.stories-Lbna8hoS.js";import{useMDXComponents as s}from"./index-_VGcpBFS.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./extends-dGVwEr9R.js";import"./iframe-V86k4liM.js";import"../sb-preview/runtime.js";import"./index-jmm5gWkb.js";import"./index-ogXoivrg.js";import"./index-MVbLLYTZ.js";import"./index-PPLHz8o0.js";const h=`# Table
+
+Use the table component to let users compare information in rows and columns.
+Table is used to display simple information that does not need to be filtered or edited.
+
+## Guidelines
+
+- Use \`<Table.Caption>\` to describe a table in the same way you would use a heading. A caption helps users find, navigate and understand tables.
+- Never use the table component to layout content on a page. Instead, use [Grid](https://amsterdam.github.io/design-system/?path=/docs/react_layout-grid--docs).
+`;function n(o){const t=Object.assign({h2:"h2",h3:"h3",p:"p",code:"code"},s(),o.components);return e.jsxs(e.Fragment,{children:[e.jsx(r,{of:m}),`
+`,e.jsx(a,{children:h}),`
+`,e.jsx(i,{}),`
+`,e.jsx(t.h2,{id:"stories",children:"Stories"}),`
+`,e.jsx(t.h3,{id:"with-footer-and-row-headers",children:"With footer and row headers"}),`
+`,e.jsxs(t.p,{children:[`Use table headers to tell users what the rows and columns represent.
+Use the scope attribute to help users of assistive technology distinguish between row and column headers.
+Use `,e.jsx(t.code,{children:"Table.Footer"})," for a summary row."]}),`
+`,e.jsx(d,{of:c})]})}function U(o={}){const{wrapper:t}=Object.assign({},s(),o.components);return t?e.jsx(t,Object.assign({},o,{children:e.jsx(n,o)})):n(o)}export{U as default};
