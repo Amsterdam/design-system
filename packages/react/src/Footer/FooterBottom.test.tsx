ab3ca0e9dd23e7d99f99b9b1b2dc3e24f9703ba4
Aram Limpens <a.limpens@amsterdam.nl>
2024-01-19 14:48:24 +0100

    Refactor Footer

---
+++ packages/react/src/Footer/FooterBottom.test.tsx
@@ -0,0 +1,41 @@
+import { render } from '@testing-library/react'
+import { createRef } from 'react'
+import { FooterBottom } from './FooterBottom'
+import '@testing-library/jest-dom'
+
+describe('Footer', () => {
+  it('renders', () => {
+    const { container } = render(<FooterBottom />)
+
+    const component = container.querySelector(':only-child')
+
+    expect(component).toBeInTheDocument()
+    expect(component).toBeVisible()
+  })
+
+  it('renders a design system BEM class name', () => {
+    const { container } = render(<FooterBottom />)
+
+    const component = container.querySelector(':only-child')
+
+    expect(component).toHaveClass('amsterdam-footer__bottom')
+  })
+
+  it('renders an additional class name', () => {
+    const { container } = render(<FooterBottom className="extra" />)
+
+    const component = container.querySelector(':only-child')
+
+    expect(component).toHaveClass('amsterdam-footer__bottom extra')
+  })
+
+  it('supports ForwardRef in React', () => {
+    const ref = createRef<HTMLDivElement>()
+
+    const { container } = render(<FooterBottom ref={ref} />)
+
+    const component = container.querySelector(':only-child')
+
+    expect(ref.current).toBe(component)
+  })
+})
