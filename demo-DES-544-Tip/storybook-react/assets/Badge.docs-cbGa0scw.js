170bda4daa027eec415eac36eb5b4f043c6ac78c
dlnr <niels@dlnr.dev>
2024-01-19 14:37:24 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@0f02f9c499e8c2629e669de35a37c532baaa9dcf 🚀

--- demo-DES-544-Tip/storybook-react/assets/Badge.docs-cbGa0scw.js
+++
@@ -1,5 +0,0 @@
-import{j as t}from"./index.esm-hsfH36rw.js";import{M as s,d as e,P as i,e as m}from"./index-GLmvujst.js";import{B as a}from"./Badge.stories--SBWryUB.js";import{useMDXComponents as p}from"./index-_VGcpBFS.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./extends-dGVwEr9R.js";import"./iframe-Eeb32UEh.js";import"../sb-preview/runtime.js";import"./index-jmm5gWkb.js";import"./index-ogXoivrg.js";import"./index-MVbLLYTZ.js";import"./index-PPLHz8o0.js";const c=`# Badge
-`;function n(o){return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:a}),`
-`,t.jsx(e,{children:c}),`
-`,t.jsx(i,{}),`
-`,t.jsx(m,{})]})}function X(o={}){const{wrapper:r}=Object.assign({},p(),o.components);return r?t.jsx(r,Object.assign({},o,{children:t.jsx(n,o)})):n()}export{X as default};
