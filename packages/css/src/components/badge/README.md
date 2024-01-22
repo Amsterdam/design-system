7aaf67198c95367da7e9029eab5efb9693ab0f24
Vincent Smedinga <vincent@smedinga.nl>
2024-01-22 10:40:23 +0100

    Add initial documentation

--- packages/css/src/components/badge/README.md
+++ packages/css/src/components/badge/README.md
@@ -1,1 +1,14 @@
 # Badge
+
+A prominently colored box containing 1 or 2 words to guide the user in taking a specific action.
+
+## Design
+
+The text in the Badge is of the same size as the surrounding text.
+Its color is either white or black, depending on the background colour.
+The box aligns to the left with the running text.
+
+## Guidelines
+
+The default background colour is green.
+Suggestions when to use the other colours will follow soon.
--- storybook/storybook-react/src/Badge/Badge.stories.tsx
+++ storybook/storybook-react/src/Badge/Badge.stories.tsx
@@ -3,7 +3,7 @@
  * Copyright (c) 2023 Gemeente Amsterdam
  */
 
-import { Badge } from '@amsterdam/design-system-react'
+import { Badge, Heading, OrderedList } from '@amsterdam/design-system-react'
 import { Meta, StoryObj } from '@storybook/react'
 
 const meta = {
@@ -28,3 +28,26 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const Default: Story = {}
+
+export const InList: Story = {
+  decorators: [
+    (Story) => (
+      <div>
+        <Heading level={4}>Verwerkingstijd en data</Heading>
+        <OrderedList>
+          <OrderedList.Item>Binnen Amsterdam op werkdagen binnen 24 uur.</OrderedList.Item>
+          <OrderedList.Item>
+            Vanuit een andere gemeente ongeveer 5 werkdagen.
+            <br />
+            <Story /> Geeft u uw verhuizing vantevoren door, dan wordt uw melding bewaard tot de verhuisdatum. Dan pas
+            begint de termijn van 24 uur of 5 werkdagen.
+          </OrderedList.Item>
+          <OrderedList.Item>
+            Een verhuizing doorgeven kan 4 weken van tevoren en maximaal 5 dagen erna. Geeft u het later door? Dan wordt
+            de dag van doorgeven de verhuisdatum.
+          </OrderedList.Item>
+        </OrderedList>
+      </div>
+    ),
+  ],
+}
