1af605f9bc5ae34bf12bf17cea760a258e181570
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-01-22 09:41:50 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@7aaf67198c95367da7e9029eab5efb9693ab0f24 🚀

---
+++ demo-DES-331-Badge/storybook-react/assets/Badge.docs-fT21EJtN.js
@@ -0,0 +1,22 @@
+import{j as n}from"./index.esm-yb5-WSZz.js";import{M as s,d as r,P as a,f as c}from"./index-CXJuvauO.js";import{B as d,I as h}from"./Badge.stories-ndPtXQ8I.js";import{useMDXComponents as i}from"./index-_VGcpBFS.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./extends-dGVwEr9R.js";import"./iframe-QFQ7K3nt.js";import"../sb-preview/runtime.js";import"./index-jmm5gWkb.js";import"./index-ogXoivrg.js";import"./index-MVbLLYTZ.js";import"./index-PPLHz8o0.js";const l=`# Badge
+
+A prominently colored box containing 1 or 2 words to guide the user in taking a specific action.
+
+## Design
+
+The text in the Badge is of the same size as the surrounding text.
+Its color is either white or black, depending on the background colour.
+The box aligns to the left with the running text.
+
+## Guidelines
+
+The default background colour is green.
+Suggestions when to use the other colours will follow soon.
+`;function o(t){const e=Object.assign({h2:"h2",h3:"h3",p:"p"},i(),t.components);return n.jsxs(n.Fragment,{children:[n.jsx(s,{of:d}),`
+`,n.jsx(r,{children:l}),`
+`,n.jsx(e.h2,{id:"voorbeelden",children:"Voorbeelden"}),`
+`,n.jsx(e.h3,{id:"default",children:"Default"}),`
+`,n.jsx(a,{}),`
+`,n.jsx(e.h3,{id:"in-a-list",children:"In a list"}),`
+`,n.jsx(e.p,{children:"A tip can be used in a list."}),`
+`,n.jsx(c,{of:h})]})}function C(t={}){const{wrapper:e}=Object.assign({},i(),t.components);return e?n.jsx(e,Object.assign({},t,{children:n.jsx(o,t)})):o(t)}export{C as default};
---
+++ demo-DES-331-Badge/storybook-react/assets/Badge.stories-ndPtXQ8I.js
@@ -0,0 +1,18 @@
+import{B as m,j as e,H as l,O as a}from"./index.esm-yb5-WSZz.js";const c={title:"Feedback/Badge",component:m,args:{children:"Tip"},argTypes:{color:{control:{type:"inline-radio"},options:["blue","dark-blue","green","dark-green","orange","magenta","purple","yellow"],selected:"dark-green"}}},r={},n={decorators:[g=>e.jsxs("div",{children:[e.jsx(l,{level:4,children:"Verwerkingstijd en data"}),e.jsxs(a,{children:[e.jsx(a.Item,{children:"Binnen Amsterdam op werkdagen binnen 24 uur."}),e.jsxs(a.Item,{children:["Vanuit een andere gemeente ongeveer 5 werkdagen.",e.jsx("br",{}),e.jsx(g,{})," Geeft u uw verhuizing vantevoren door, dan wordt uw melding bewaard tot de verhuisdatum. Dan pas begint de termijn van 24 uur of 5 werkdagen."]}),e.jsx(a.Item,{children:"Een verhuizing doorgeven kan 4 weken van tevoren en maximaal 5 dagen erna. Geeft u het later door? Dan wordt de dag van doorgeven de verhuisdatum."})]})]})]};var d,t,o;r.parameters={...r.parameters,docs:{...(d=r.parameters)==null?void 0:d.docs,source:{originalSource:"{}",...(o=(t=r.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};var s,i,u;n.parameters={...n.parameters,docs:{...(s=n.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  decorators: [Story => <div>
+        <Heading level={4}>Verwerkingstijd en data</Heading>
+        <OrderedList>
+          <OrderedList.Item>Binnen Amsterdam op werkdagen binnen 24 uur.</OrderedList.Item>
+          <OrderedList.Item>
+            Vanuit een andere gemeente ongeveer 5 werkdagen.
+            <br />
+            <Story /> Geeft u uw verhuizing vantevoren door, dan wordt uw melding bewaard tot de verhuisdatum. Dan pas
+            begint de termijn van 24 uur of 5 werkdagen.
+          </OrderedList.Item>
+          <OrderedList.Item>
+            Een verhuizing doorgeven kan 4 weken van tevoren en maximaal 5 dagen erna. Geeft u het later door? Dan wordt
+            de dag van doorgeven de verhuisdatum.
+          </OrderedList.Item>
+        </OrderedList>
+      </div>]
+}`,...(u=(i=n.parameters)==null?void 0:i.docs)==null?void 0:u.source}}};const v=["Default","InList"],w=Object.freeze(Object.defineProperty({__proto__:null,Default:r,InList:n,__namedExportsOrder:v,default:c},Symbol.toStringTag,{value:"Module"}));export{w as B,n as I};
