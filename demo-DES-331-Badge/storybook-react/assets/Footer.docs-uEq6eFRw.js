42910be40602d4b7a42cdb4003412faa25721f3b
dlnr <niels@dlnr.dev>
2024-01-24 10:42:53 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@8bcae298813c6a64c412f3a98edfbe9c94bf81e3 🚀

---
+++ demo-DES-331-Badge/storybook-react/assets/Footer.docs-uEq6eFRw.js
@@ -0,0 +1,15 @@
+import{j as e}from"./index.esm-Iejdv-jp.js";import{M as i,d as r,P as a,e as s}from"./index-81ghsZi9.js";import{F as d}from"./Footer.stories-267vRUbP.js";import{useMDXComponents as l}from"./index-_VGcpBFS.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./extends-dGVwEr9R.js";import"./iframe-Gkj3Wa--.js";import"../sb-preview/runtime.js";import"./index-jmm5gWkb.js";import"./index-ogXoivrg.js";import"./index-MVbLLYTZ.js";import"./index-PPLHz8o0.js";const m=`# Footer
+
+De footer (in het Nederlands: voettekst) sluit iedere webpagina af. De inhoud is service-informatie. Een gedeelte is voorgeschreven en een deel wordt ingevuld vanuit de doelstelling van de website.
+
+## Richtlijnen
+
+- De voettekst is verplicht, er staat er 1 op elke pagina.
+- De eerste kolom richt zich op contactinformatie. De informatie is zo specifiek mogelijk gebonden aan de betreffende website of pagina (bijvoorbeeld een chatmogelijkheid indien aanwezig).
+- De tweede kolom bevat links naar relevante (online) sites of bronnen.
+- De derde kolom verwijst naar nieuwsbrieven, socials enz.
+- Het laatste deel van de voettekst is bedoeld voor koppelingen naar privacy, cookieverklaring, uitleg over de website e.d. Het is niet bedoeld voor koppelingen naar bijvoorbeeld contactinformatie.
+`;function t(n){return e.jsxs(e.Fragment,{children:[e.jsx(i,{of:d}),`
+`,e.jsx(r,{children:m}),`
+`,e.jsx(a,{}),`
+`,e.jsx(s,{})]})}function M(n={}){const{wrapper:o}=Object.assign({},l(),n.components);return o?e.jsx(o,Object.assign({},n,{children:e.jsx(t,n)})):t()}export{M as default};
