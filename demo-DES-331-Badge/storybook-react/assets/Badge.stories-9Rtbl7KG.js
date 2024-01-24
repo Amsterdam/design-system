4b82ee8d254c95021eafef4784e3ea58c618c550
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-01-24 11:58:45 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@334eef8088683e9d7de6885a4eb146bf69c22fd4 🚀

---
+++ demo-DES-331-Badge/storybook-react/assets/Badge.stories-9Rtbl7KG.js
@@ -0,0 +1,4 @@
+import{B as t}from"./index.esm-Iejdv-jp.js";/**
+ * @license EUPL-1.2+
+ * Copyright (c) 2024 Gemeente Amsterdam
+ */const l={title:"Feedback/Badge",component:t,args:{label:"Tip"},argTypes:{color:{control:{type:"inline-radio"},options:["black","blue","dark-blue","dark-green","green","magenta","orange","purple","white","yellow"],selected:"dark-green"}}},e={};var r,a,o;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:"{}",...(o=(a=e.parameters)==null?void 0:a.docs)==null?void 0:o.source}}};const s=["Default"],c=Object.freeze(Object.defineProperty({__proto__:null,Default:e,__namedExportsOrder:s,default:l},Symbol.toStringTag,{value:"Module"}));export{c as B};
--- demo-DES-331-Badge/storybook-react/assets/Badge.stories-9tj1cTgq.js
+++
@@ -1,4 +0,0 @@
-import{B as t}from"./index.esm-Iejdv-jp.js";/**
- * @license EUPL-1.2+
- * Copyright (c) 2024 Gemeente Amsterdam
- */const l={title:"Feedback/Badge",component:t,args:{label:"Tip"},argTypes:{color:{control:{type:"inline-radio"},options:["blue","dark-blue","green","dark-green","orange","magenta","purple","yellow","black","white"],selected:"dark-green"}}},e={};var r,a,o;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:"{}",...(o=(a=e.parameters)==null?void 0:a.docs)==null?void 0:o.source}}};const s=["Default"],c=Object.freeze(Object.defineProperty({__proto__:null,Default:e,__namedExportsOrder:s,default:l},Symbol.toStringTag,{value:"Module"}));export{c as B};
