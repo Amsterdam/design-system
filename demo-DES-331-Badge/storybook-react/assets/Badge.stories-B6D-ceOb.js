5fe791d3407097247af5fc0b38a5f501de555290
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-01-25 10:37:36 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@869e6ea815d82c5c24393bcf54b464bd9517486a 🚀

---
+++ demo-DES-331-Badge/storybook-react/assets/Badge.stories-B6D-ceOb.js
@@ -0,0 +1,4 @@
+import{B as t}from"./index.esm-4jQ2C1d2.js";/**
+ * @license EUPL-1.2+
+ * Copyright (c) 2024 Gemeente Amsterdam
+ */const l={title:"Feedback/Badge",component:t,args:{label:"Tip"},argTypes:{color:{control:{type:"inline-radio"},options:["blue","dark-blue","dark-green","green","magenta","orange","purple","yellow"],selected:"dark-green"}}},e={};var r,o,a;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:"{}",...(a=(o=e.parameters)==null?void 0:o.docs)==null?void 0:a.source}}};const s=["Default"],c=Object.freeze(Object.defineProperty({__proto__:null,Default:e,__namedExportsOrder:s,default:l},Symbol.toStringTag,{value:"Module"}));export{c as B};
