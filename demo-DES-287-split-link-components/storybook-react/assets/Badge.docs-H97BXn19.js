836c042fe69ab01b8f30032218879198c9c80acb
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-01-26 12:44:56 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@cd0915f0a5d37364f909edd60f7e1169c90bcab9 🚀

---
+++ demo-DES-287-split-link-components/storybook-react/assets/Badge.docs-H97BXn19.js
@@ -0,0 +1,16 @@
+import{j as n}from"./index.esm-pqJm4mAe.js";import{M as s,d as i,P as a,e as c}from"./index-hFmvdooK.js";import{B as m}from"./Badge.stories--pYwPjvN.js";import{useMDXComponents as r}from"./index-_VGcpBFS.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./extends-dGVwEr9R.js";import"./iframe-G7TUci0o.js";import"../sb-preview/runtime.js";import"./index-jmm5gWkb.js";import"./index-ogXoivrg.js";import"./index-MVbLLYTZ.js";import"./index-PPLHz8o0.js";const d=`# Badge
+
+A prominently coloured box containing 1 or 2 words.
+Guides the user in taking a specific action or describes its surrounding content.
+
+## Design
+
+The badge can contain a short text or a number.
+The default background colour is dark green.
+Suggestions on when to use the other colours will follow soon.
+`;function e(o){const t=Object.assign({h2:"h2",h3:"h3"},r(),o.components);return n.jsxs(n.Fragment,{children:[n.jsx(s,{of:m}),`
+`,n.jsx(i,{children:d}),`
+`,n.jsx(t.h2,{id:"stories",children:"Stories"}),`
+`,n.jsx(t.h3,{id:"default",children:"Default"}),`
+`,n.jsx(a,{}),`
+`,n.jsx(c,{})]})}function C(o={}){const{wrapper:t}=Object.assign({},r(),o.components);return t?n.jsx(t,Object.assign({},o,{children:n.jsx(e,o)})):e(o)}export{C as default};
