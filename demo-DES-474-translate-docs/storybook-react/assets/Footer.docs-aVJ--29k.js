ce8d4027a4469102f4c5491954e19318e5a13cff
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-01-26 14:34:26 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@eafed52208a81a73ae8402b8b28d3312cd9e58f0 🚀

---
+++ demo-DES-474-translate-docs/storybook-react/assets/Footer.docs-aVJ--29k.js
@@ -0,0 +1,19 @@
+import{j as e}from"./index.esm-yhfThtqi.js";import{M as i,d as s,P as r,e as a}from"./index-2qHUMmcB.js";import{F as c}from"./Footer.stories-q3cxuugH.js";import{useMDXComponents as m}from"./index-_VGcpBFS.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./extends-dGVwEr9R.js";import"./iframe-9G6G5meE.js";import"../sb-preview/runtime.js";import"./index-jmm5gWkb.js";import"./index-ogXoivrg.js";import"./index-MVbLLYTZ.js";import"./index-PPLHz8o0.js";const p=`# Footer
+
+The footer is the closing section on every web page.
+Its content provides service information.
+A part is prescribed, and a part is filled based on the website’s objectives.
+
+## Guidelines
+
+- The footer is mandatory, and there is one on every page.
+- The first column focuses on contact information.
+  The information is as specific as possible and tailored to the respective website or page (e.g., including a chat option if available).
+- The second column contains links to relevant (online) sites or sources.
+- The third column refers to newsletters, social media, etc.
+- The last part of the footer is intended for links to privacy policies, cookie statements, explanations about the website, etc.
+  It is not intended for links to contact information, for example.
+`;function n(t){return e.jsxs(e.Fragment,{children:[e.jsx(i,{of:c}),`
+`,e.jsx(s,{children:p}),`
+`,e.jsx(r,{}),`
+`,e.jsx(a,{})]})}function y(t={}){const{wrapper:o}=Object.assign({},m(),t.components);return o?e.jsx(o,Object.assign({},t,{children:e.jsx(n,t)})):n()}export{y as default};
--- demo-DES-474-translate-docs/storybook-react/assets/preview-nnLvEGy3.js
+++
@@ -1,7 +0,0 @@
-import{_ as e}from"./iframe-Htnq5YRx.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-K_ZKHJDC.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10]),import.meta.url);return new r}}};export{a as parameters};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-K_ZKHJDC.js","./iframe-Htnq5YRx.js","./index-4g5l5LRQ.js","./_commonjsHelpers-4gQjN7DL.js","./react-18-ba7OOUbL.js","./index-jmm5gWkb.js","./index-aFHDm9vc.js","./index-ogXoivrg.js","./extends-dGVwEr9R.js","./index-MVbLLYTZ.js","./index-PPLHz8o0.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
