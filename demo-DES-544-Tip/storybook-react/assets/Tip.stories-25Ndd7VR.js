d46c1229dbc92b807e62ed6c7e56473f5d90da86
dlnr <niels@dlnr.dev>
2024-01-26 12:44:28 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@48db3a907e6f273c3c4c29784ed8af5a2a9d3973 🚀

--- demo-DES-544-Tip/storybook-react/assets/Tip.stories-25Ndd7VR.js
+++
@@ -1,9 +0,0 @@
-import{E as u,j as n,B as p,P as g,U as a}from"./index.esm-dYAxzidT.js";const l={title:"Feedback/Tip",component:u,args:{children:[n.jsx(p,{label:"Tip",color:"dark-green",children:"Tip"},"badge"),n.jsx(g,{children:"Geeft u uw verhuizing van tevoren door, dan wordt uw melding bewaard tot de verhuisdatum. Dan pas begint de termijn van 24 uur of 5 werkdagen."},"paragraph")]}},e={},r={decorators:m=>n.jsxs(a,{children:[n.jsx(a.Item,{children:"Binnen Amsterdam op werkdagen binnen 24 uur."}),n.jsxs(a.Item,{children:["Vanuit een andere gemeente ongeveer 5 werkdagen.",m()]})]})};var t,s,o;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:"{}",...(o=(s=e.parameters)==null?void 0:s.docs)==null?void 0:o.source}}};var d,i,c;r.parameters={...r.parameters,docs:{...(d=r.parameters)==null?void 0:d.docs,source:{originalSource:`{
-  decorators: story => <UnorderedList>
-      <UnorderedList.Item>Binnen Amsterdam op werkdagen binnen 24 uur.</UnorderedList.Item>
-      <UnorderedList.Item>
-        Vanuit een andere gemeente ongeveer 5 werkdagen.
-        {story()}
-      </UnorderedList.Item>
-    </UnorderedList>
-}`,...(c=(i=r.parameters)==null?void 0:i.docs)==null?void 0:c.source}}};const b=["Default","InAList"],j=Object.freeze(Object.defineProperty({__proto__:null,Default:e,InAList:r,__namedExportsOrder:b,default:l},Symbol.toStringTag,{value:"Module"}));export{r as I,j as T};
