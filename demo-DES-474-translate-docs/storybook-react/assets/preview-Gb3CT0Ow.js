1056de5034f584845cc0eca1c8f78df994dcb817
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-01-26 14:48:16 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@9ad12d571e4b1b3cddc4c9d0f0b682be81ec105b 🚀

--- demo-DES-474-translate-docs/storybook-react/assets/preview-Gb3CT0Ow.js
+++
@@ -1,7 +0,0 @@
-import{_ as e}from"./iframe-9G6G5meE.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-H8lg8jZb.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10]),import.meta.url);return new r}}};export{a as parameters};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-H8lg8jZb.js","./iframe-9G6G5meE.js","./index-4g5l5LRQ.js","./_commonjsHelpers-4gQjN7DL.js","./react-18-ba7OOUbL.js","./index-jmm5gWkb.js","./index-2qHUMmcB.js","./index-ogXoivrg.js","./extends-dGVwEr9R.js","./index-MVbLLYTZ.js","./index-PPLHz8o0.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
