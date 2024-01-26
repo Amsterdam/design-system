964f74758799ab78bee1930ac4f38483ba94ca6c
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-01-26 12:31:30 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@d346cdfd2b3c7fab08e6bae10314420f1fde8a5f 🚀

---
+++ demo-develop/storybook-react/assets/Breadcrumb.stories-SiK1hJTH.js
@@ -0,0 +1,27 @@
+import{d as e,j as n}from"./index.esm-4jQ2C1d2.js";const c={title:"Navigation/Breadcrumb",component:e},r={render:()=>n.jsxs(e,{children:[n.jsx(e.Item,{href:"#",children:"Home"}),n.jsx(e.Item,{href:"#",children:"Nieuws"}),n.jsx(e.Item,{href:"#",children:"Kennisgevingen en bekendmakingen"})]}),parameters:{docs:{source:{language:"jsx",code:`
+<Breadcrumb>
+  <Breadcrumb.Item href="#">Home</Breadcrumb.Item>
+  <Breadcrumb.Item href="#">Nieuws</Breadcrumb.Item>
+  <Breadcrumb.Item href="#">Kennisgevingen en bekendmakingen</Breadcrumb.Item>
+</Breadcrumb>
+        `}}}};var m,a,t;r.parameters={...r.parameters,docs:{...(m=r.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  render: () => <Breadcrumb>
+      <Breadcrumb.Item href="#">Home</Breadcrumb.Item>
+      <Breadcrumb.Item href="#">Nieuws</Breadcrumb.Item>
+      <Breadcrumb.Item href="#">Kennisgevingen en bekendmakingen</Breadcrumb.Item>
+    </Breadcrumb>,
+  parameters: {
+    docs: {
+      source: {
+        language: 'jsx',
+        code: \`
+<Breadcrumb>
+  <Breadcrumb.Item href="#">Home</Breadcrumb.Item>
+  <Breadcrumb.Item href="#">Nieuws</Breadcrumb.Item>
+  <Breadcrumb.Item href="#">Kennisgevingen en bekendmakingen</Breadcrumb.Item>
+</Breadcrumb>
+        \`
+      }
+    }
+  }
+}`,...(t=(a=r.parameters)==null?void 0:a.docs)==null?void 0:t.source}}};const d=["Default"],s=Object.freeze(Object.defineProperty({__proto__:null,Default:r,__namedExportsOrder:d,default:c},Symbol.toStringTag,{value:"Module"}));export{s as B};
