69b9a9bd59ecb0c0a20496ebc140baffe7329434
dlnr <niels@dlnr.dev>
2024-01-29 09:12:56 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@19e758892cb74550c5c1de722c0612446f3e99c6 🚀

--- demo-DES-515-long-words/storybook-react/assets/preview-ksmidpKB.js
+++
@@ -1,7 +0,0 @@
-import{_ as e}from"./iframe-OYo_OpwI.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-Y3K4VoX0.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10]),import.meta.url);return new r}}};export{a as parameters};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-Y3K4VoX0.js","./iframe-OYo_OpwI.js","./index-4g5l5LRQ.js","./_commonjsHelpers-4gQjN7DL.js","./react-18-ba7OOUbL.js","./index-jmm5gWkb.js","./index-8FTTAHxP.js","./index-ogXoivrg.js","./extends-dGVwEr9R.js","./index-MVbLLYTZ.js","./index-PPLHz8o0.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
