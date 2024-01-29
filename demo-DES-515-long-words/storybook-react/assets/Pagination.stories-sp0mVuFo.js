87a3717475071c61877de1b8dd84bf408cbb49cd
dlnr <niels@dlnr.dev>
2024-01-29 09:22:44 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@84e8a297bdf2033cd31d8b84919a1d5750edb540 🚀

--- demo-DES-515-long-words/storybook-react/assets/Pagination.stories-sp0mVuFo.js
+++
@@ -1,4 +0,0 @@
-import{v as r}from"./index.esm-4jQ2C1d2.js";/**
- * @license EUPL-1.2+
- * Copyright (c) 2023 Gemeente Amsterdam
- */const n={title:"Navigation/Pagination",component:r,args:{page:1,maxVisiblePages:7,totalPages:10},argTypes:{onPageChange:{action:"page changed"}}},e={};var a,t,o;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:"{}",...(o=(t=e.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};const s=["Default"],g=Object.freeze(Object.defineProperty({__proto__:null,Default:e,__namedExportsOrder:s,default:n},Symbol.toStringTag,{value:"Module"}));export{g as P};
