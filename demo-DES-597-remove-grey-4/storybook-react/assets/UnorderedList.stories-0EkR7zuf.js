04bd0310bcc05fbb7378da3c2890876e9e30bb5f
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-01-31 09:49:26 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@ba1ccca157bff278effe6165b4a6b5ee641f9eb2 🚀

---
+++ demo-DES-597-remove-grey-4/storybook-react/assets/UnorderedList.stories-0EkR7zuf.js
@@ -0,0 +1,65 @@
+import{j as e,U as r,k as s,W as x,P as n,X as L,Y as U,Z as k,_ as u,r as S,$ as P,a0 as z}from"./index.esm-pqJm4mAe.js";import{f as y}from"./exampleContent-GJvLNqvD.js";const N=y().map((j,p)=>e.jsx(r.Item,{children:j},p)),f={title:"Text/Unordered List",component:r,args:{children:N},argTypes:{markers:{control:"boolean"}}},a={},d={args:{children:[e.jsxs(r.Item,{children:["Stadsdeel West",e.jsxs(r,{children:[e.jsx(r.Item,{children:"Bos en Lommer"},1.1),e.jsx(r.Item,{children:"Oud West / De Baarsjes"},1.2),e.jsx(r.Item,{children:"Westerpark"},1.3)]})]},1),e.jsxs(r.Item,{children:["Stadsdeel Nieuw-West",e.jsxs(r,{children:[e.jsx(r.Item,{children:"De Aker, Sloten en Nieuw Sloten"},2.1),e.jsx(r.Item,{children:"Geuzenveld, Slotermeer en Sloterdijken"},2.2),e.jsx(r.Item,{children:"Osdorp"},2.3),e.jsx(r.Item,{children:"Slotervaart"},2.4)]})]},2)]}},t={args:{children:[e.jsx(r.Item,{children:e.jsxs("div",{className:"amsterdam-docs-card",children:[e.jsx(s,{svg:x,size:"level-6"}),e.jsx(n,{children:"Stadsloket: locaties en openingstijden"})]})},1),e.jsx(r.Item,{children:e.jsxs("div",{className:"amsterdam-docs-card",children:[e.jsx(s,{svg:L,size:"level-6"}),e.jsx(n,{children:"Parkeren + Reizen (P+R)"})]})},2),e.jsx(r.Item,{children:e.jsxs("div",{className:"amsterdam-docs-card",children:[e.jsx(s,{svg:U,size:"level-6"}),e.jsx(n,{children:"Paspoort, ID-kaart en Rijbewijs"})]})},3),e.jsx(r.Item,{children:e.jsxs("div",{className:"amsterdam-docs-card",children:[e.jsx(s,{svg:k,size:"level-6"}),e.jsx(n,{children:"Gemeentebelastingen"})]})},4),e.jsx(r.Item,{children:e.jsxs("div",{className:"amsterdam-docs-card",children:[e.jsx(s,{svg:u,size:"level-6"}),e.jsx(n,{children:"Melding openbare ruimte en overlast"})]})},5),e.jsx(r.Item,{children:e.jsxs("div",{className:"amsterdam-docs-card",children:[e.jsx(s,{svg:S,size:"level-6"}),e.jsx(n,{children:"Verhuizing doorgeven"})]})},6),e.jsx(r.Item,{children:e.jsxs("div",{className:"amsterdam-docs-card",children:[e.jsx(s,{svg:P,size:"level-6"}),e.jsx(n,{children:"Grof afval"})]})},7),e.jsx(r.Item,{children:e.jsxs("div",{className:"amsterdam-docs-card",children:[e.jsx(s,{svg:z,size:"level-6"}),e.jsx(n,{children:"Kennisgevingen en bekendmakingen"})]})},8)],markers:!1}};var i,o,c;a.parameters={...a.parameters,docs:{...(i=a.parameters)==null?void 0:i.docs,source:{originalSource:"{}",...(c=(o=a.parameters)==null?void 0:o.docs)==null?void 0:c.source}}};var m,l,v;d.parameters={...d.parameters,docs:{...(m=d.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    children: [<UnorderedList.Item key={1}>
+        Stadsdeel West
+        <UnorderedList>
+          <UnorderedList.Item key={1.1}>Bos en Lommer</UnorderedList.Item>
+          <UnorderedList.Item key={1.2}>Oud West / De Baarsjes</UnorderedList.Item>
+          <UnorderedList.Item key={1.3}>Westerpark</UnorderedList.Item>
+        </UnorderedList>
+      </UnorderedList.Item>, <UnorderedList.Item key={2}>
+        Stadsdeel Nieuw-West
+        <UnorderedList>
+          <UnorderedList.Item key={2.1}>De Aker, Sloten en Nieuw Sloten</UnorderedList.Item>
+          <UnorderedList.Item key={2.2}>Geuzenveld, Slotermeer en Sloterdijken</UnorderedList.Item>
+          <UnorderedList.Item key={2.3}>Osdorp</UnorderedList.Item>
+          <UnorderedList.Item key={2.4}>Slotervaart</UnorderedList.Item>
+        </UnorderedList>
+      </UnorderedList.Item>]
+  }
+}`,...(v=(l=d.parameters)==null?void 0:l.docs)==null?void 0:v.source}}};var g,I,h;t.parameters={...t.parameters,docs:{...(g=t.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    children: [<UnorderedList.Item key={1}>
+        <div className="amsterdam-docs-card">
+          <Icon svg={LocationIcon} size="level-6" />
+          <Paragraph>Stadsloket: locaties en openingstijden</Paragraph>
+        </div>
+      </UnorderedList.Item>, <UnorderedList.Item key={2}>
+        <div className="amsterdam-docs-card">
+          <Icon svg={CarIcon} size="level-6" />
+          <Paragraph>Parkeren + Reizen (P+R)</Paragraph>
+        </div>
+      </UnorderedList.Item>, <UnorderedList.Item key={3}>
+        <div className="amsterdam-docs-card">
+          <Icon svg={PassportIcon} size="level-6" />
+          <Paragraph>Paspoort, ID-kaart en Rijbewijs</Paragraph>
+        </div>
+      </UnorderedList.Item>, <UnorderedList.Item key={4}>
+        <div className="amsterdam-docs-card">
+          <Icon svg={DocumentEuroSignIcon} size="level-6" />
+          <Paragraph>Gemeentebelastingen</Paragraph>
+        </div>
+      </UnorderedList.Item>, <UnorderedList.Item key={5}>
+        <div className="amsterdam-docs-card">
+          <Icon svg={AlertIcon} size="level-6" />
+          <Paragraph>Melding openbare ruimte en overlast</Paragraph>
+        </div>
+      </UnorderedList.Item>, <UnorderedList.Item key={6}>
+        <div className="amsterdam-docs-card">
+          <Icon svg={HousingIcon} size="level-6" />
+          <Paragraph>Verhuizing doorgeven</Paragraph>
+        </div>
+      </UnorderedList.Item>, <UnorderedList.Item key={7}>
+        <div className="amsterdam-docs-card">
+          <Icon svg={TrashBinIcon} size="level-6" />
+          <Paragraph>Grof afval</Paragraph>
+        </div>
+      </UnorderedList.Item>, <UnorderedList.Item key={8}>
+        <div className="amsterdam-docs-card">
+          <Icon svg={AnnouncementIcon} size="level-6" />
+          <Paragraph>Kennisgevingen en bekendmakingen</Paragraph>
+        </div>
+      </UnorderedList.Item>],
+    markers: false
+  }
+}`,...(h=(I=t.parameters)==null?void 0:I.docs)==null?void 0:h.source}}};const b=["Default","TwoLevels","WithoutMarkers"],w=Object.freeze(Object.defineProperty({__proto__:null,Default:a,TwoLevels:d,WithoutMarkers:t,__namedExportsOrder:b,default:f},Symbol.toStringTag,{value:"Module"}));export{a as D,d as T,w as U,t as W};
