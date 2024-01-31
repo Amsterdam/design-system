ddd8d780da7fc9e29fedd9d866ebf3f31cb3d55f
alimpens <37216945+alimpens@users.noreply.github.com>
2024-01-31 11:21:46 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@e565e3cee3651c5c0f468d9759552737198ee772 🚀

--- demo-DES-594-spotlight-color-api/storybook-react/assets/preview-DLaniDaq.js
+++
@@ -1,7 +0,0 @@
-import{_ as e}from"./iframe-bRxBeI5z.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV--NZX_u8A.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10]),import.meta.url);return new r}}};export{a as parameters};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV--NZX_u8A.js","./iframe-bRxBeI5z.js","./index-4g5l5LRQ.js","./_commonjsHelpers-4gQjN7DL.js","./react-18-ba7OOUbL.js","./index-jmm5gWkb.js","./index-JG-87Ayv.js","./index-ogXoivrg.js","./extends-dGVwEr9R.js","./index-MVbLLYTZ.js","./index-PPLHz8o0.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
