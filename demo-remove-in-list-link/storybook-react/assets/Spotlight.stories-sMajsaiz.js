6345b1eb25846ae6add075be09d41cf641d1b4de
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-01-31 22:48:51 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@338ae109804bd12e10b6b78d1f5c5033bb595e74 🚀

---
+++ demo-remove-in-list-link/storybook-react/assets/Spotlight.stories-sMajsaiz.js
@@ -0,0 +1,37 @@
+import{K as d,j as u,G as g,c as H}from"./index.esm-9d7ZuBKV.js";import{a as I}from"./exampleContent-GJvLNqvD.js";const J=I(),L={title:"Containers/Spotlight",component:d,argTypes:{as:{control:{type:"radio"},options:["article","aside","div","footer","section"]},color:{control:{type:"select"},options:["blue","dark-blue","dark-green","green","magenta","orange","purple","yellow"]},children:{control:{disable:!0}}},render:({as:F,color:m})=>u.jsx(d,{as:F,color:m,children:u.jsx(g,{paddingVertical:"medium",children:u.jsx(g.Cell,{span:"all",children:u.jsx(H,{inverseColor:!m||!["green","yellow"].includes(m),children:J})})})})},r={},e={args:{color:"blue"}},a={args:{color:"dark-blue"}},o={args:{color:"dark-green"}},s={args:{color:"green"}},c={args:{color:"magenta"}},n={args:{color:"orange"}},t={args:{color:"purple"}},l={args:{color:"yellow"}},p={args:{as:"section"}};var i,S,k;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:"{}",...(k=(S=r.parameters)==null?void 0:S.docs)==null?void 0:k.source}}};var b,x,y;e.parameters={...e.parameters,docs:{...(b=e.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    color: 'blue'
+  }
+}`,...(y=(x=e.parameters)==null?void 0:x.docs)==null?void 0:y.source}}};var f,h,j;a.parameters={...a.parameters,docs:{...(f=a.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    color: 'dark-blue'
+  }
+}`,...(j=(h=a.parameters)==null?void 0:h.docs)==null?void 0:j.source}}};var D,G,w;o.parameters={...o.parameters,docs:{...(D=o.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  args: {
+    color: 'dark-green'
+  }
+}`,...(w=(G=o.parameters)==null?void 0:G.docs)==null?void 0:w.source}}};var B,C,O;s.parameters={...s.parameters,docs:{...(B=s.parameters)==null?void 0:B.docs,source:{originalSource:`{
+  args: {
+    color: 'green'
+  }
+}`,...(O=(C=s.parameters)==null?void 0:C.docs)==null?void 0:O.source}}};var _,M,P;c.parameters={...c.parameters,docs:{...(_=c.parameters)==null?void 0:_.docs,source:{originalSource:`{
+  args: {
+    color: 'magenta'
+  }
+}`,...(P=(M=c.parameters)==null?void 0:M.docs)==null?void 0:P.source}}};var T,v,Y;n.parameters={...n.parameters,docs:{...(T=n.parameters)==null?void 0:T.docs,source:{originalSource:`{
+  args: {
+    color: 'orange'
+  }
+}`,...(Y=(v=n.parameters)==null?void 0:v.docs)==null?void 0:Y.source}}};var q,E,N;t.parameters={...t.parameters,docs:{...(q=t.parameters)==null?void 0:q.docs,source:{originalSource:`{
+  args: {
+    color: 'purple'
+  }
+}`,...(N=(E=t.parameters)==null?void 0:E.docs)==null?void 0:N.source}}};var z,K,Q;l.parameters={...l.parameters,docs:{...(z=l.parameters)==null?void 0:z.docs,source:{originalSource:`{
+  args: {
+    color: 'yellow'
+  }
+}`,...(Q=(K=l.parameters)==null?void 0:K.docs)==null?void 0:Q.source}}};var R,V,A;p.parameters={...p.parameters,docs:{...(R=p.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  args: {
+    as: 'section'
+  }
+}`,...(A=(V=p.parameters)==null?void 0:V.docs)==null?void 0:A.source}}};const U=["Default","Blue","DarkBlue","DarkGreen","Green","Magenta","Orange","Purple","Yellow","CustomTagName"],Z=Object.freeze(Object.defineProperty({__proto__:null,Blue:e,CustomTagName:p,DarkBlue:a,DarkGreen:o,Default:r,Green:s,Magenta:c,Orange:n,Purple:t,Yellow:l,__namedExportsOrder:U,default:L},Symbol.toStringTag,{value:"Module"}));export{e as B,p as C,a as D,s as G,c as M,n as O,t as P,Z as S,l as Y,o as a};
---
+++ demo-remove-in-list-link/storybook-react/assets/UnorderedList.docs-yiLZ2duE.js
@@ -0,0 +1,28 @@
+import{j as e}from"./index.esm-9d7ZuBKV.js";import{M as a,d as r,f as n}from"./index-_JHVf8pu.js";import{U as l,D as h,T as d,W as c}from"./UnorderedList.stories-4rWT6EUF.js";import{useMDXComponents as o}from"./index-_VGcpBFS.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./extends-dGVwEr9R.js";import"./iframe-7dJ6qt8y.js";import"../sb-preview/runtime.js";import"./index-jmm5gWkb.js";import"./index-ogXoivrg.js";import"./index-MVbLLYTZ.js";import"./index-PPLHz8o0.js";import"./exampleContent-GJvLNqvD.js";const p=`# Unordered List
+
+A list is a vertical group of related content.
+This list can have 2 levels.
+The first level consists of squares.
+The second level consists of en dashes (–).
+Text in the list items is indented by a fixed distance.
+`;function i(s){const t=Object.assign({h2:"h2",h3:"h3",p:"p"},o(),s.components);return e.jsxs(e.Fragment,{children:[e.jsx(a,{of:l}),`
+`,e.jsx(r,{children:p}),`
+`,e.jsx(t.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(t.h3,{id:"default",children:"Default"}),`
+`,e.jsx(t.p,{children:`An unordered list has square bullet points.
+There is extra white space between the items.
+This provides better distinction between the items, especially when they consist of multiple lines of text.`}),`
+`,e.jsx(n,{of:h}),`
+`,e.jsx(t.h3,{id:"two-levels",children:"Two levels"}),`
+`,e.jsx(t.p,{children:`A list can have one deeper level.
+In this case, an en dash is used as the bullet point.
+It is left-aligned with the text of the higher-level item.`}),`
+`,e.jsx(n,{of:d}),`
+`,e.jsx(t.h3,{id:"list-without-bullet-points",children:"List without bullet points"}),`
+`,e.jsx(t.p,{children:`An overview of equivalent elements is also an unordered list.
+In such cases, use a list without bullet points to convey that semantics.
+This helps screen readers understand and navigate the list.`}),`
+`,e.jsx(t.p,{children:`Note: In this variant, each item typically contains a container with titles and other text components.
+Therefore, this variant does not define typography for the items.`}),`
+`,e.jsx(t.p,{children:"This example is based on the top tasks on the homepage of the website:"}),`
+`,e.jsx(n,{of:c})]})}function L(s={}){const{wrapper:t}=Object.assign({},o(),s.components);return t?e.jsx(t,Object.assign({},s,{children:e.jsx(i,s)})):i(s)}export{L as default};
