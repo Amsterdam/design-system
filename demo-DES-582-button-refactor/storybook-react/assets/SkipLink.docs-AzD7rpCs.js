150daa2f11f76fac6a6f009a37f61f2ea6f32ad7
dlnr <niels@dlnr.dev>
2024-02-01 12:55:08 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@767a4a5d4490c9719b55b0e2658eabc6e5f92bd4 🚀

---
+++ demo-DES-582-button-refactor/storybook-react/assets/SkipLink.docs-AzD7rpCs.js
@@ -0,0 +1,36 @@
+import{j as n}from"./index.esm-Oi0OnaUX.js";import{M as a,d as r,P as p,e as c,f as t}from"./index-M72uV9Zn.js";import{S as h,O as l,M as m}from"./SkipLink.stories-bNhUhl62.js";import{useMDXComponents as s}from"./index-_VGcpBFS.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./iframe-RnBqTgwB.js";import"../sb-preview/runtime.js";import"./index-jmm5gWkb.js";import"./index-ogXoivrg.js";import"./index-MVbLLYTZ.js";import"./index-PPLHz8o0.js";const k=`# Skip Link
+
+Use a Skip Link to navigate to the main content quickly with the keyboard.
+A Skip Link allows skipping recurring navigation blocks, such as the main menu or breadcrumb trail.
+
+The Skip Link is placed above the header.
+The link is hidden until activated with the tab key.
+When the link is shown, it pushes the entire page down.
+
+## Guidelines
+
+### How to Use
+
+- Place the Skip Link as the first element in \`<body>\` unless you have a cookie banner.
+  In that case, place the Skip Link immediately after the cookie banner.
+- Use the Skip Link to navigate to the main content.
+  On an article page, for example, it could be the title of the article; on a search page, it could be the search field.
+- Set \`id="example-id"\` on the container of that element and then use \`href="#example-id"\` on the Skip Link.
+- You can use more than one Skip Link for complex pages with multiple sections.
+  In most cases, this is not necessary.
+
+### Avoid
+
+- Skip Links are unnecessary on a simple page with only text and minimal navigation.
+  The purpose of a Skip Link is to bypass recurring navigation blocks.
+  If those blocks are not present, a Skip Link is not needed.
+`;function o(i){const e=Object.assign({h2:"h2",p:"p"},s(),i.components);return n.jsxs(n.Fragment,{children:[n.jsx(a,{of:h}),`
+`,n.jsx(r,{children:k}),`
+`,n.jsx(p,{}),`
+`,n.jsx(c,{}),`
+`,n.jsx(e.h2,{id:"display-on-focus",children:"Display on Focus"}),`
+`,n.jsx(e.p,{children:"A Skip Link is only displayed when it receives focus."}),`
+`,n.jsx(t,{of:l}),`
+`,n.jsx(e.h2,{id:"multiple-links",children:"Multiple Links"}),`
+`,n.jsx(e.p,{children:"You can use more than one Skip Link if you have a complex page with multiple sections."}),`
+`,n.jsx(t,{of:m})]})}function M(i={}){const{wrapper:e}=Object.assign({},s(),i.components);return e?n.jsx(e,Object.assign({},i,{children:n.jsx(o,i)})):o(i)}export{M as default};
