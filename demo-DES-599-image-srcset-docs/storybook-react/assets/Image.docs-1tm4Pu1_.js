a8cd8f1e79affd23f01fc18b1a44f24aa64da649
dlnr <niels@dlnr.dev>
2024-02-02 11:24:36 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@2a512c4f164dd4f2b2d2d3a90eea8b9f2ca94862 🚀

---
+++ demo-DES-599-image-srcset-docs/storybook-react/assets/Image.docs-1tm4Pu1_.js
@@ -0,0 +1,45 @@
+import{j as e}from"./index.esm-z1nXSmea.js";import{M as a,d as r,P as l,e as c,f as i}from"./index-gSFkEFMw.js";import{I as h,S as d,a as m,L as p}from"./Image.stories-DpBnGyDV.js";import{useMDXComponents as o}from"./index-_VGcpBFS.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./extends-dGVwEr9R.js";import"./iframe-a4rDuTIx.js";import"../sb-preview/runtime.js";import"./index-jmm5gWkb.js";import"./index-ogXoivrg.js";import"./index-MVbLLYTZ.js";import"./index-PPLHz8o0.js";const g=`# Image
+
+Displays an image.
+
+## Guidelines
+
+- Do not forget to include a description of the image in the \`alt\` attribute.
+  This ensures that screen reader users can also access this information.
+  Additionally, it can aid in search engine optimization.
+- A description is unnecessary for decorative images; use \`alt=""\` for these.
+  Examples are images that add little to the nearby text or pictures that purely contribute to the design or atmosphere of the page (source: [W3C Web Accessibility Initiative](https://www.w3.org/WAI/tutorials/images/decorative/)).
+- Optionally specify multiple candidates for the image through the \`srcSet\` attribute.
+  For example, provide small, medium, and large variants for various screen sizes.
+  This prevents unnecessary downloading of large files.
+  Do this especially for the main image of a page, where the difference between sizes on a narrow and wide screen is most significant.
+- Ensure that the aspect ratio of the image is supported by the [Aspect Ratio](?path=/docs/layout-aspect-ratio--docs) component.
+
+## Relevant WCAG Requirements
+
+- [WCAG 1.1.1](https://www.w3.org/TR/WCAG22/#non-text-content): Non-text content must have a text alternative.
+- [WCAG 1.4.5](https://www.w3.org/TR/WCAG22/#images-of-text): Use text instead of images of text.
+- [WCAG 1.4.9](https://www.w3.org/TR/WCAG22/#images-of-text-no-exception): Use images of text only when there is no alternative.
+`;function s(n){const t=Object.assign({h2:"h2",p:"p",ul:"ul",li:"li",a:"a",code:"code"},o(),n.components);return e.jsxs(e.Fragment,{children:[e.jsx(a,{of:h}),`
+`,e.jsx(r,{children:g}),`
+`,e.jsx(l,{}),`
+`,e.jsx(c,{}),`
+`,e.jsx(t.h2,{id:"image-with-srcset",children:"Image with srcset"}),`
+`,e.jsx(t.p,{children:`This is an example of an image with a srcset attribute.
+Read this documentation to learn more about the srcset attribute:`}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsx(t.li,{children:e.jsx(t.a,{href:"https://developer.mozilla.org/en-US/docs/Web/HTML/Element/img#srcset",target:"_blank",rel:"nofollow noopener noreferrer",children:"MDN Web Docs HTML"})}),`
+`,e.jsx(t.li,{children:e.jsx(t.a,{href:"https://developer.mozilla.org/en-US/docs/Web/API/HTMLImageElement/srcset",target:"_blank",rel:"nofollow noopener noreferrer",children:"MDN Web Docs API"})}),`
+`]}),`
+`,e.jsxs(t.p,{children:["When the ",e.jsx(t.code,{children:"(max-width: 832px)"}),` media query condition matches, the 640px image will load, otherwise the 1280px image will be used.
+If the srcset attribute uses width descriptors, the sizes attribute must also be present, or the srcset itself will be ignored.`]}),`
+`,e.jsx(i,{of:d}),`
+`,e.jsxs(t.p,{children:[`When an image element's srcset contains "x" descriptors, browsers also consider the URL in the `,e.jsx(t.code,{children:"src"})," attribute (if present) as a candidate, and assign it a default descriptor of 1x."]}),`
+`,e.jsx(i,{of:m}),`
+`,e.jsx(t.h2,{id:"lazy-loading",children:"Lazy loading"}),`
+`,e.jsxs(t.p,{children:["This is an example of native lazy loading using the ",e.jsx(t.code,{children:"loading"})," attribute. The default behaviour is ",e.jsx(t.code,{children:"eager"}),`, which means the image will load immediately.
+The `,e.jsx(t.code,{children:"lazy"})," value will defer the loading of the image until it is within the viewport."]}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsx(t.li,{children:e.jsx(t.a,{href:"https://developer.mozilla.org/en-US/docs/Web/API/HTMLImageElement/loading",target:"_blank",rel:"nofollow noopener noreferrer",children:"MDN Web Docs (HTML)"})}),`
+`]}),`
+`,e.jsx(i,{of:p})]})}function C(n={}){const{wrapper:t}=Object.assign({},o(),n.components);return t?e.jsx(t,Object.assign({},n,{children:e.jsx(s,n)})):s(n)}export{C as default};
---
+++ demo-DES-599-image-srcset-docs/storybook-react/assets/Image.stories-DpBnGyDV.js
@@ -0,0 +1,27 @@
+import{I as S}from"./index.esm-z1nXSmea.js";/**
+ * @license EUPL-1.2+
+ * Copyright (c) 2023 Gemeente Amsterdam
+ */const x={title:"Media/Image",component:S},s={args:{alt:"",src:"https://picsum.photos/640/360"}},t={args:{alt:"",src:"https://picsum.photos/640/360",srcSet:"https://picsum.photos/640/360 640w, https://picsum.photos/1280/720 1280w",sizes:"(max-width: 36rem) 640px, 50vw"}},e={args:{alt:"",srcSet:"https://picsum.photos/640/360 1x, https://picsum.photos/1280/720 2x"}},o={args:{alt:"",src:"https://picsum.photos/2560/1440",loading:"lazy"}};var r,a,p;s.parameters={...s.parameters,docs:{...(r=s.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    alt: '',
+    src: 'https://picsum.photos/640/360'
+  }
+}`,...(p=(a=s.parameters)==null?void 0:a.docs)==null?void 0:p.source}}};var c,n,m;t.parameters={...t.parameters,docs:{...(c=t.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    alt: '',
+    src: 'https://picsum.photos/640/360',
+    srcSet: 'https://picsum.photos/640/360 640w, https://picsum.photos/1280/720 1280w',
+    sizes: '(max-width: 36rem) 640px, 50vw'
+  }
+}`,...(m=(n=t.parameters)==null?void 0:n.docs)==null?void 0:m.source}}};var i,u,h;e.parameters={...e.parameters,docs:{...(i=e.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    alt: '',
+    srcSet: 'https://picsum.photos/640/360 1x, https://picsum.photos/1280/720 2x'
+  }
+}`,...(h=(u=e.parameters)==null?void 0:u.docs)==null?void 0:h.source}}};var d,l,g;o.parameters={...o.parameters,docs:{...(d=o.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    alt: '',
+    src: 'https://picsum.photos/2560/1440',
+    loading: 'lazy'
+  }
+}`,...(g=(l=o.parameters)==null?void 0:l.docs)==null?void 0:g.source}}};const w=["Default","SourceSetWidth","SourceSetDensity","LazyLoading"],z=Object.freeze(Object.defineProperty({__proto__:null,Default:s,LazyLoading:o,SourceSetDensity:e,SourceSetWidth:t,__namedExportsOrder:w,default:x},Symbol.toStringTag,{value:"Module"}));export{z as I,o as L,t as S,e as a};
