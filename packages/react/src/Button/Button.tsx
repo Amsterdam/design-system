05288f0f60d5fe73a709183153b4c000655f1247
Niels Roozemond <n.roozemond@amsterdam.nl>
2024-02-02 11:15:46 +0100

    Too much JSDoc

--- packages/react/src/Button/Button.tsx
+++ packages/react/src/Button/Button.tsx
@@ -8,21 +8,11 @@ import { forwardRef } from 'react'
 import type { ButtonHTMLAttributes, ForwardedRef, PropsWithChildren } from 'react'
 
 export type ButtonProps = {
-  /** Variant, choose between 'primary' (default), 'secondary' or 'tertiary' */
   variant?: 'primary' | 'secondary' | 'tertiary'
   /** Render the button in a busy state to indicate something has to finish loading */
   busy?: boolean
 } & PropsWithChildren<ButtonHTMLAttributes<HTMLButtonElement>>
 
-/**
- * A button component.
- * @example
- * <Button onClick={handleClick}>Click me</Button>
- * @example
- * <Button variant="secondary" onClick={handleClick}>Click me</Button>
- * @example
- * <Button variant="tertiary" onClick={handleClick}>Close <Icon key="icon" svg={CloseIcon} size="level-5" /></Button>
- */
 export const Button = forwardRef(
   (
     { children, type, disabled, busy, variant = 'primary', ...restProps }: ButtonProps,
