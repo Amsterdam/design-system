c135959ed302493e9fcfc562a7f98d327591975b
alimpens <37216945+alimpens@users.noreply.github.com>
2024-02-03 10:52:46 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@34681764b7aa5a1c022b296a182ae63a063f9d40 🚀

---
+++ demo-DES-551-add-theme-setup/storybook-react/assets/Mark.docs-lB8LXbeb.js
@@ -0,0 +1,21 @@
+import{j as n}from"./index.esm-z1nXSmea.js";import{M as r,d as a,P as s}from"./index-pvVfMeT3.js";import{M as m}from"./Mark.stories--jp9CHaa.js";import{useMDXComponents as o}from"./index-_VGcpBFS.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./extends-dGVwEr9R.js";import"./iframe-4-XxLt7W.js";import"../sb-preview/runtime.js";import"./index-jmm5gWkb.js";import"./index-ogXoivrg.js";import"./index-MVbLLYTZ.js";import"./index-PPLHz8o0.js";const p=`# Mark
+
+Highlights a text fragment using a yellow background.
+Provides strong emphasis on the text and draws attention to it.
+
+## Guidelines
+
+### How to Use
+
+- Use a maximum of 1 mark per page.
+- Limit the marked text to 1 sentence.
+
+### Avoid This
+
+- Do not use it in titles and subtitles.
+  Use a significant and engaging title if the entire paragraph is important.
+- Do not mark an entire paragraph.
+`;function i(t){const e=Object.assign({h2:"h2"},o(),t.components);return n.jsxs(n.Fragment,{children:[n.jsx(r,{of:m}),`
+`,n.jsx(a,{children:p}),`
+`,n.jsx(e.h2,{id:"example",children:"Example"}),`
+`,n.jsx(s,{})]})}function w(t={}){const{wrapper:e}=Object.assign({},o(),t.components);return e?n.jsx(e,Object.assign({},t,{children:n.jsx(i,t)})):i(t)}export{w as default};
