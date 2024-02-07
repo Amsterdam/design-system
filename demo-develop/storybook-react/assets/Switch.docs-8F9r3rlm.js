a7fb5117d46e054aff70d067aa82895dd543a795
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-02-07 10:31:27 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@f8751716018d4ed269a086e1764741ad29714fa0 🚀

---
+++ demo-develop/storybook-react/assets/Switch.docs-8F9r3rlm.js
@@ -0,0 +1,34 @@
+import{j as e}from"./jsx-runtime-vNq4Oc-g.js";import{M as a,d as r,P as c,e as l,f as s}from"./index-FAjkrFEg.js";import{S as h,D as m,W as d}from"./Switch.stories-_gdpkJtT.js";import{useMDXComponents as i}from"./index-_VGcpBFS.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./iframe-OjIlwWeo.js";import"../sb-preview/runtime.js";import"./index-jmm5gWkb.js";import"./index-ogXoivrg.js";import"./extends-dGVwEr9R.js";import"./index-MVbLLYTZ.js";import"./index-PPLHz8o0.js";import"./index.esm-KtP8z46G.js";const p=`# Switch
+
+A switch is a control element to toggle between two different states quickly.
+A switch applies to a page or the entire system, such as an on/off switch.
+
+## Guidelines
+
+- Switch only between two different states.
+  It is a binary action.
+- Use labels with a switch to make the action clear.
+- The action takes place immediately when the user operates the switch.
+
+## Relevant WCAG Requirements
+
+- [WCAG 1.3.5](https://www.w3.org/WAI/WCAG21/Understanding/identify-input-purpose.html): it is both clear for a user and programmatically what the purpose of a form field is.
+
+The Switch is an interactive element; general requirements and guidelines for interactive elements apply [here](https://amsterdam.github.io/design-system/?path=/docs/docs-designrichtlijnen-interactieve-elementen--docs).
+
+## References
+
+- [Switch Pattern](https://www.w3.org/WAI/ARIA/apg/patterns/switch/)
+- [Role switch](https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/switch_role)
+`;function o(n){const t=Object.assign({p:"p",code:"code",h2:"h2",h3:"h3"},i(),n.components);return e.jsxs(e.Fragment,{children:[e.jsx(a,{of:h}),`
+`,e.jsx(r,{children:p}),`
+`,e.jsxs(t.p,{children:[`It can be used as a standalone component or as a form control element.
+When used as a standalone component provide an `,e.jsx(t.code,{children:"aria-label"})," or ",e.jsx(t.code,{children:"aria-labelledby"}),` prop to keep it accessible.
+When used as a formcontrol, make sure to add the associated `,e.jsx(t.code,{children:"<FormLabel>"})," component."]}),`
+`,e.jsx(c,{}),`
+`,e.jsx(l,{}),`
+`,e.jsx(t.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(t.h3,{id:"default",children:"Default"}),`
+`,e.jsx(s,{of:m}),`
+`,e.jsx(t.h3,{id:"with-label",children:"With Label"}),`
+`,e.jsx(s,{of:d})]})}function v(n={}){const{wrapper:t}=Object.assign({},i(),n.components);return t?e.jsx(t,Object.assign({},n,{children:e.jsx(o,n)})):o(n)}export{v as default};
