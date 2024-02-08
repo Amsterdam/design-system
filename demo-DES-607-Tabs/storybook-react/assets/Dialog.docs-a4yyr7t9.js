e69e7d952472977c94c76c752453994b0f44a654
dlnr <niels@dlnr.dev>
2024-02-08 14:12:35 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@6fc764c64fed60455d8700d0c212951326f46bfd 🚀

---
+++ demo-DES-607-Tabs/storybook-react/assets/Dialog.docs-a4yyr7t9.js
@@ -0,0 +1,34 @@
+import{j as o}from"./index.esm-7d1irJ6y.js";import{M as i,d as r,P as l,e as m,f as t}from"./index-v1H4e5Ru.js";import{D as p,W as d,T as c}from"./Dialog.stories-glSwt382.js";import{useMDXComponents as a}from"./index-_VGcpBFS.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./extends-dGVwEr9R.js";import"./iframe-uGePFlNh.js";import"../sb-preview/runtime.js";import"./index-jmm5gWkb.js";import"./index-ogXoivrg.js";import"./index-MVbLLYTZ.js";import"./index-PPLHz8o0.js";const h=`# Dialog
+
+A Dialog allows the user to focus on one task or a piece of information by popping up and blocking the page content until the modal task is completed or until the user dismisses the action.
+
+## Guidelines
+
+- Use dialogs sparingly because they interrupt the user’s workflow.
+- Use a dialog for short and non-frequent tasks.
+  Consider using the main flow for regular tasks.
+
+## Keyboard Support
+
+| key         | function                                                         |
+| :---------- | :--------------------------------------------------------------- |
+| Tab         | Moves focus to the next focusable element inside the dialog.     |
+| Shift + Tab | Moves focus to the previous focusable element inside the dialog. |
+| Escape      | Closes the dialog.                                               |
+
+## References
+
+- [HTMLDialogElement](https://developer.mozilla.org/en-US/docs/Web/API/HTMLDialogElement)
+- [Return value](https://developer.mozilla.org/en-US/docs/Web/API/HTMLDialogElement/returnValue)
+- [Modal & Nonmodal Dialogs: When (& When Not) to Use Them](https://www.nngroup.com/articles/modal-nonmodal-dialog/)
+- [Patterns](https://www.w3.org/WAI/ARIA/apg/patterns/dialog-modal/)
+`;function s(e){const n=Object.assign({h2:"h2",p:"p"},a(),e.components);return o.jsxs(o.Fragment,{children:[o.jsx(i,{of:p}),`
+`,o.jsx(r,{children:h}),`
+`,o.jsx(l,{}),`
+`,o.jsx(m,{}),`
+`,o.jsx(n.h2,{id:"with-scrollbar",children:"With Scrollbar"}),`
+`,o.jsx(n.p,{children:"Content taller than the dialog itself will scroll."}),`
+`,o.jsx(t,{of:d,className:"amsterdam-dialog-story"}),`
+`,o.jsx(n.h2,{id:"open-dialog-button",children:"Open Dialog Button"}),`
+`,o.jsx(n.p,{children:"Click or tap the button to open a dialog."}),`
+`,o.jsx(t,{of:c})]})}function W(e={}){const{wrapper:n}=Object.assign({},a(),e.components);return n?o.jsx(n,Object.assign({},e,{children:o.jsx(s,e)})):s(e)}export{W as default};
