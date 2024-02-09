720defb9d521bc7817368a20bcc1044e6fb28e58
alimpens <37216945+alimpens@users.noreply.github.com>
2024-02-09 13:01:08 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@3ecbafd54e10abe6ae83be811e969c022772b2e1 🚀

---
+++ demo-remove-in-list-link/storybook-react/assets/Overlap.docs-KGdRRSW5.js
@@ -0,0 +1,18 @@
+import{j as e}from"./jsx-runtime-vNq4Oc-g.js";import{M as r,d as s,P as a}from"./index-R3OLSBub.js";import{O as l}from"./Overlap.stories-cYveIcIt.js";import{useMDXComponents as t}from"./index-_VGcpBFS.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./iframe-D3bbKFH-.js";import"../sb-preview/runtime.js";import"./index-jmm5gWkb.js";import"./index-ogXoivrg.js";import"./extends-dGVwEr9R.js";import"./index-MVbLLYTZ.js";import"./index-PPLHz8o0.js";import"./index.esm-ysgW4aSw.js";const c=`# Overlap
+
+Displays two or more components on top of each other.
+`;function i(o){const n=Object.assign({h2:"h2",h3:"h3",p:"p",a:"a",h4:"h4",ul:"ul",li:"li"},t(),o.components);return e.jsxs(e.Fragment,{children:[e.jsx(r,{of:l}),`
+`,e.jsx(s,{children:c}),`
+`,e.jsx(n.h2,{id:"example",children:"Example"}),`
+`,e.jsx(n.h3,{id:"hero-image-with-search-field",children:"Hero Image with Search Field"}),`
+`,e.jsxs(n.p,{children:[`This allows for the use of a mood-setting image as a background.
+A `,e.jsx(n.a,{href:"?path=/docs/react_layout-grid--docs",children:"Grid"}),` provides horizontal spacing on both sides and columns for layout.
+Note: the library does not yet offer an option to vertically center the form.`]}),`
+`,e.jsx(a,{}),`
+`,e.jsx(n.h4,{id:"guidelines",children:"Guidelines"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"First, implement the underlying component."}),`
+`,e.jsxs(n.li,{children:["Follow ",e.jsx(n.a,{href:"?path=/docs/react_media-image--docs#guidelines",children:"the general guidelines for images"}),"."]}),`
+`,e.jsx(n.li,{children:`Use a sufficiently large image.
+The aspect ratio of 32:9 is most suitable.`}),`
+`]})]})}function D(o={}){const{wrapper:n}=Object.assign({},t(),o.components);return n?e.jsx(n,Object.assign({},o,{children:e.jsx(i,o)})):i(o)}export{D as default};
