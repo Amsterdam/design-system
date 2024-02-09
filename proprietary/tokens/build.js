6d55d9a7ffcc9c94be2f724dc4f400212e27f831
Aram Limpens <a.limpens@amsterdam.nl>
2024-02-09 13:45:57 +0100

    Build all agreed upon files

--- proprietary/tokens/build.js
+++ proprietary/tokens/build.js
@@ -1,59 +1,99 @@
-// TODO: build all currently built files using this setup
-
 const StyleDictionary = require('style-dictionary')
 
 const modes = ['compact']
 
-console.log(`Building default mode...`)
+function generateSharedConfig(mode) {
+  const prefix = mode ? `${mode}.` : ''
+
+  return {
+    css: {
+      transforms: ['attribute/cti', 'name/cti/kebab', 'color/hsl-4'],
+      transformGroup: 'css',
+      buildPath: 'dist/',
+      files: [
+        {
+          destination: `${prefix}root.css`,
+          format: 'css/variables',
+          options: {
+            outputReferences: true,
+          },
+        },
+      ],
+    },
+    cssTheme: {
+      transforms: ['attribute/cti', 'name/cti/kebab', 'color/hsl-4'],
+      buildPath: 'dist/',
+      files: [
+        {
+          destination: `${prefix}index.css`,
+          format: 'css/variables',
+          options: {
+            selector: '.amsterdam-theme',
+            outputReferences: true,
+          },
+        },
+      ],
+    },
+    js: {
+      transforms: ['attribute/cti', 'name/cti/kebab', 'color/hsl-4'],
+      buildPath: 'dist/',
+      files: [
+        {
+          format: 'javascript/module',
+          destination: `${prefix}tokens.js`,
+        },
+      ],
+    },
+    json: {
+      transforms: ['attribute/cti', 'name/cti/kebab', 'color/hsl-4'],
+      buildPath: 'dist/',
+      files: [
+        {
+          destination: `${prefix}index.tokens.json`,
+          format: 'json/nested',
+        },
+      ],
+    },
+    scss: {
+      transforms: ['attribute/cti', 'name/cti/kebab', 'color/hsl-4'],
+      buildPath: 'dist/',
+      files: [
+        {
+          destination: `${prefix}variables.scss`,
+          format: 'scss/variables',
+          options: {
+            outputReferences: true,
+          },
+        },
+      ],
+    },
+    typescript: {
+      transforms: ['attribute/cti', 'name/cti/kebab', 'color/hsl-4'],
+      transformGroup: 'js',
+      buildPath: 'dist/',
+      files: [
+        {
+          format: 'typescript/module-declarations',
+          destination: `${prefix}tokens.d.ts`,
+        },
+      ],
+    },
+  }
+}
+
+console.log('Building default mode...')
 StyleDictionary.extend({
   source: [
     // exclude non-default modes from source
     `./src/**/!(*.${modes.join(`|*.`)}).tokens.json`,
   ],
-
-  platforms: {
-    css: {
-      transformGroup: 'css',
-      buildPath: 'dist/',
-      files: [
-        {
-          destination: `root.css`,
-          format: `css/variables`,
-          options: {
-            outputReferences: true,
-          },
-        },
-      ],
-    },
-
-    json: {
-      buildPath: 'dist/',
-      files: [
-        {
-          destination: 'index.tokens.json',
-          format: 'json/nested',
-        },
-      ],
-    },
-  },
+  platforms: generateSharedConfig(),
 }).buildAllPlatforms()
 
-console.log(`\n\n Building compact mode...`)
-StyleDictionary.extend({
-  source: [`./src/**/*.compact.tokens.json`],
-  platforms: {
-    css: {
-      transformGroup: `css`,
-      buildPath: 'dist/',
-      files: [
-        {
-          destination: `compact.css`,
-          format: `css/variables`,
-          options: {
-            outputReferences: true,
-          },
-        },
-      ],
-    },
-  },
-}).buildAllPlatforms()
+modes.forEach((mode) => {
+  console.log(`\n\nBuilding ${mode} mode...`)
+  StyleDictionary.extend({
+    source: [`./src/**/*.${mode}.tokens.json`],
+    platforms: generateSharedConfig(mode),
+  }).buildAllPlatforms()
+})
