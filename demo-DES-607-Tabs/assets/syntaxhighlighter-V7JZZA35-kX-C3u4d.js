9e9c12556dec24ceb45e8f70c66af06d8fafa791
dlnr <niels@dlnr.dev>
2024-02-09 14:23:24 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@13b45581e511b905d019e755aa62b8d5cc9186d3 🚀

---
+++ demo-DES-607-Tabs/assets/syntaxhighlighter-V7JZZA35-kX-C3u4d.js
@@ -0,0 +1,1 @@
+import{S as l,c as n,s as d}from"./index-Cmfe2DJ7.js";import"./iframe-e3-K3tCi.js";import"../sb-preview/runtime.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./index-jmm5gWkb.js";import"./index-ogXoivrg.js";import"./index-MVbLLYTZ.js";import"./index-PPLHz8o0.js";export{l as SyntaxHighlighter,n as createCopyToClipboardFunction,d as default};
