bb53833038424ae672d775bf4eff0f7eb221d9b6
dlnr <niels@dlnr.dev>
2024-02-09 22:24:00 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@f08368e2ae72246a057e6e02e710a1d558951958 🚀

---
+++ demo-DES-607-Tabs/storybook-react/assets/IconButton.docs-8lNVL6Wc.js
@@ -0,0 +1,31 @@
+import{j as n}from"./index.esm-jvAqEAqV.js";import{M as i,d as a,P as l,e as c,f as s}from"./index-B_R_mcj3.js";import{I as d,O as h,a as u,L as m,b as x,c as j,d as f}from"./IconButton.stories-zaAPZzdz.js";import{useMDXComponents as r}from"./index-_VGcpBFS.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./extends-dGVwEr9R.js";import"./iframe-OpzZX7I5.js";import"../sb-preview/runtime.js";import"./index-jmm5gWkb.js";import"./index-ogXoivrg.js";import"./index-MVbLLYTZ.js";import"./index-PPLHz8o0.js";const p=`# Icon Button
+
+A button containing only an icon.
+
+## Guidelines
+
+- Use this button only if the icon is entirely clear and understandable.
+  Many icons are interpreted differently by different users.
+  Only use universally recognized icons, such as a close icon or a play icon.
+- Provide a label text that describes what the button does for screen reader users.
+
+## Relevant WCAG Requirements
+
+The Icon Button follows [the same requirements and guidelines as a regular button](https://amsterdam.github.io/design-system/?path=/docs/react_buttons-button--docs).
+`;function o(t){const e=Object.assign({h2:"h2",h3:"h3"},r(),t.components);return n.jsxs(n.Fragment,{children:[n.jsx(i,{of:d}),`
+`,n.jsx(a,{children:p}),`
+`,n.jsx(l,{}),`
+`,n.jsx(c,{}),`
+`,n.jsx(e.h2,{id:"examples",children:"Examples"}),`
+`,n.jsx(e.h3,{id:"on-light-background",children:"On Light Background"}),`
+`,n.jsx(s,{of:h}),`
+`,n.jsx(e.h3,{id:"on-dark-background",children:"On Dark Background"}),`
+`,n.jsx(s,{of:u}),`
+`,n.jsx(e.h3,{id:"level-3",children:"Level 3"}),`
+`,n.jsx(s,{of:m}),`
+`,n.jsx(e.h3,{id:"level-4",children:"Level 4"}),`
+`,n.jsx(s,{of:x}),`
+`,n.jsx(e.h3,{id:"level-5",children:"Level 5"}),`
+`,n.jsx(s,{of:j}),`
+`,n.jsx(e.h3,{id:"level-6",children:"Level 6"}),`
+`,n.jsx(s,{of:f})]})}function I(t={}){const{wrapper:e}=Object.assign({},r(),t.components);return e?n.jsx(e,Object.assign({},t,{children:n.jsx(o,t)})):o(t)}export{I as default};
