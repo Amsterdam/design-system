5ddfe9058774b2976d55b0e5da279955f8516e08
alimpens <37216945+alimpens@users.noreply.github.com>
2024-02-12 11:09:20 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@753791278eac52d9e9a943c0296f1307d112f3aa 🚀

---
+++ demo-separate-token-classes/storybook-react/assets/TextInput.docs-iOETt8XA.js
@@ -0,0 +1,32 @@
+import{j as e}from"./jsx-runtime-vNq4Oc-g.js";import{M as a,d as r,P as l,e as p,f as s}from"./index-4nysNVXu.js";import{T as d,P as c,I as h,D as m}from"./TextInput.stories-x0eJi_ZR.js";import{useMDXComponents as o}from"./index-_VGcpBFS.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./iframe-Tb-RPiPv.js";import"../sb-preview/runtime.js";import"./index-jmm5gWkb.js";import"./index-ogXoivrg.js";import"./extends-dGVwEr9R.js";import"./index-MVbLLYTZ.js";import"./index-PPLHz8o0.js";import"./index.esm-pZj_1j2-.js";const u=`# Text Input
+
+A form field in which a user can enter text.
+
+## Guidelines
+
+- Use a Text Input when users need to enter a single line of text, such as their name or phone number.
+- Do not use a Text Input when users could provide more than 1 sentence of text.
+- The width of the Text Input should be appropriate for the information to be entered.
+- A Text Input must have a label, and in most cases, this label should be visible.
+- Use \`spellcheck="false"\` for fields that may contain sensitive information, such as passwords and personal data.
+  Some browser extensions for spell-checking send this information to external servers.
+- Apply automatic assistance where possible.
+  For example, in logged-in systems, pre-filling input can prevent errors and save time.
+`;function i(t){const n=Object.assign({h2:"h2",h3:"h3",p:"p"},o(),t.components);return e.jsxs(e.Fragment,{children:[e.jsx(a,{of:d}),`
+`,e.jsx(r,{children:u}),`
+`,e.jsx(l,{}),`
+`,e.jsx(p,{}),`
+`,e.jsx(n.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(n.h3,{id:"placeholder",children:"Placeholder"}),`
+`,e.jsx(n.p,{children:`It is possible to place placeholder text in the text input.
+A placeholder should not contain crucial information.
+Using a suggestion or example can help clarify something for the user.
+Suggestions are not a substitute for an explanation or instruction provided in an error message or description.
+Placeholder text disappears as soon as the user starts entering data.`}),`
+`,e.jsx(s,{of:c}),`
+`,e.jsx(n.h3,{id:"invalid",children:"Invalid"}),`
+`,e.jsx(n.p,{children:`Whenever possible, use real-time validation of data when filling out forms.
+Incorrectly filled input fields become visible immediately.`}),`
+`,e.jsx(s,{of:h}),`
+`,e.jsx(n.h3,{id:"disabled",children:"Disabled"}),`
+`,e.jsx(s,{of:m})]})}function C(t={}){const{wrapper:n}=Object.assign({},o(),t.components);return n?e.jsx(n,Object.assign({},t,{children:e.jsx(i,t)})):i(t)}export{C as default};
