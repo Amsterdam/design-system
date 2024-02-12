ffddd3403f7c67afa5a3cc3606966587cc6cc95a
dlnr <niels@dlnr.dev>
2024-02-12 11:14:23 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@81a05ccdd5327025af69d90f22221140ab027c10 🚀

---
+++ demo-DES-582-button-refactor/storybook-react/assets/Button.stories--xxzQ0KX.js
@@ -0,0 +1,24 @@
+import{j as B}from"./jsx-runtime-vNq4Oc-g.js";import{e as P,f,S as T,B as j}from"./index.esm-TjYPpUpR.js";const x={title:"Buttons/Button",component:P,args:{children:"Default",variant:"primary",disabled:!1,busy:!1},argTypes:{children:{table:{disable:!0}},disabled:{control:"boolean"},variant:{control:"select",options:["primary","secondary","tertiary"]}}},r={args:{children:"Primary"}},a={args:{children:["Primary",B.jsx(f,{svg:T,size:"level-5"},"icon")]}},e={args:{children:"Secondary",variant:"secondary"}},s={args:{variant:"secondary",children:["Secondary",B.jsx(j,{label:99,color:"dark-blue"},"badge")]}},o={args:{children:"Tertiary",variant:"tertiary"}};var n,t,c;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    children: 'Primary'
+  }
+}`,...(c=(t=r.parameters)==null?void 0:t.docs)==null?void 0:c.source}}};var i,d,l;a.parameters={...a.parameters,docs:{...(i=a.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    children: ['Primary', <Icon key="icon" svg={ShareIcon} size="level-5" />]
+  }
+}`,...(l=(d=a.parameters)==null?void 0:d.docs)==null?void 0:l.source}}};var m,y,p;e.parameters={...e.parameters,docs:{...(m=e.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    children: 'Secondary',
+    variant: 'secondary'
+  }
+}`,...(p=(y=e.parameters)==null?void 0:y.docs)==null?void 0:p.source}}};var u,g,S;s.parameters={...s.parameters,docs:{...(u=s.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    variant: 'secondary',
+    children: ['Secondary', <Badge key="badge" label={99} color="dark-blue" />]
+  }
+}`,...(S=(g=s.parameters)==null?void 0:g.docs)==null?void 0:S.source}}};var h,b,v;o.parameters={...o.parameters,docs:{...(h=o.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    children: 'Tertiary',
+    variant: 'tertiary'
+  }
+}`,...(v=(b=o.parameters)==null?void 0:b.docs)==null?void 0:v.source}}};const _=["Primary","PrimaryWithIcon","Secondary","SecondaryWithBadge","Tertiary"],W=Object.freeze(Object.defineProperty({__proto__:null,Primary:r,PrimaryWithIcon:a,Secondary:e,SecondaryWithBadge:s,Tertiary:o,__namedExportsOrder:_,default:x},Symbol.toStringTag,{value:"Module"}));export{W as B,r as P,e as S,o as T,a,s as b};
