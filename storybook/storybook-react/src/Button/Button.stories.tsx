81a05ccdd5327025af69d90f22221140ab027c10
Niels Roozemond <n.roozemond@amsterdam.nl>
2024-02-12 12:12:58 +0100

    Resolved comments

--- storybook/storybook-react/src/Button/Button.stories.tsx
+++ storybook/storybook-react/src/Button/Button.stories.tsx
@@ -4,7 +4,7 @@
  */
 
 import { Badge, Button, Icon } from '@amsterdam/design-system-react'
-import { CloseIcon, ShareIcon } from '@amsterdam/design-system-react-icons'
+import { ShareIcon } from '@amsterdam/design-system-react-icons'
 import { Meta, StoryObj } from '@storybook/react'
 
 const meta = {
@@ -12,8 +12,9 @@ const meta = {
   component: Button,
   args: {
     children: 'Default',
-    disabled: false,
     variant: 'primary',
+    disabled: false,
+    busy: false,
   },
   argTypes: {
     children: {
@@ -54,80 +55,16 @@ export const Secondary: Story = {
   },
 }
 
-export const SecondaryWithIcon: Story = {
+export const SecondaryWithBadge: Story = {
   args: {
     variant: 'secondary',
-    children: ['Secondary', <Icon key="icon" svg={ShareIcon} size="level-5" />],
-  },
-}
-
-export const SecondaryWithIconAndBadge: Story = {
-  args: {
-    variant: 'secondary',
-    children: [
-      'Secondary',
-      <Badge key="badge" label={99} color="dark-blue" />,
-      <Icon key="icon" svg={CloseIcon} size="level-5" />,
-    ],
+    children: ['Secondary', <Badge key="badge" label={99} color="dark-blue" />],
   },
 }
 
 export const Tertiary: Story = {
   args: {
     children: 'Tertiary',
-    variant: 'tertiary',
-  },
-}
-
-export const TertiaryWithIcon: Story = {
-  args: {
-    children: ['Tertiary', <Icon key="icon" svg={ShareIcon} size="level-5" />],
-    variant: 'tertiary',
-  },
-}
-
-export const PrimaryDisabled: Story = {
-  args: {
-    children: 'Primary',
-    disabled: true,
-  },
-}
-
-export const SecondaryDisabled: Story = {
-  args: {
-    children: 'Secondary',
-    disabled: true,
-    variant: 'secondary',
-  },
-}
-
-export const TertiaryDisabled: Story = {
-  args: {
-    children: 'Tertiary',
-    disabled: true,
-    variant: 'tertiary',
-  },
-}
-
-export const PrimaryBusy: Story = {
-  args: {
-    children: 'Primary',
-    busy: true,
-  },
-}
-
-export const SecondaryBusy: Story = {
-  args: {
-    children: 'Secondary',
-    busy: true,
-    variant: 'secondary',
-  },
-}
-
-export const TertiaryBusy: Story = {
-  args: {
-    children: 'Tertiary',
-    busy: true,
     variant: 'tertiary',
   },
 }
