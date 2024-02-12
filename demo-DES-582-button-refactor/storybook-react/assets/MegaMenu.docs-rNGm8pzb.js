6d616ad9c66ea27a42b3017f60b2109c3f2ec2f7
dlnr <niels@dlnr.dev>
2024-02-12 15:16:02 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@54922d25b3de150ad489e831d487df5da5d4f4ee 🚀

--- demo-DES-582-button-refactor/storybook-react/assets/MegaMenu.docs-rNGm8pzb.js
+++
@@ -1,6 +0,0 @@
-import{j as t}from"./jsx-runtime-vNq4Oc-g.js";import{M as s,d as i,P as a,f as m}from"./index-k8k2eg1f.js";import{M as c,a as p}from"./MegaMenu.stories-aF5_EbuE.js";import{useMDXComponents as r}from"./index-_VGcpBFS.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./iframe-1woguvYf.js";import"../sb-preview/runtime.js";import"./index-jmm5gWkb.js";import"./index-ogXoivrg.js";import"./index-MVbLLYTZ.js";import"./index-PPLHz8o0.js";import"./index.esm-OTULPyzz.js";const M=`# Mega Menu
-`;function e(n){const o=Object.assign({h2:"h2"},r(),n.components);return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:c}),`
-`,t.jsx(i,{children:M}),`
-`,t.jsx(a,{}),`
-`,t.jsx(o.h2,{id:"with-categories",children:"With Categories"}),`
-`,t.jsx(m,{of:p})]})}function O(n={}){const{wrapper:o}=Object.assign({},r(),n.components);return o?t.jsx(o,Object.assign({},n,{children:t.jsx(e,n)})):e(n)}export{O as default};
---
+++ demo-DES-582-button-refactor/storybook-react/assets/VisuallyHidden.docs-5uXmVCRx.js
@@ -0,0 +1,10 @@
+import{j as s}from"./jsx-runtime-vNq4Oc-g.js";import{M as o,d as r,P as a,e as i}from"./index-m2mZmI3R.js";import{V as m}from"./VisuallyHidden.stories-D3Y4ZuvJ.js";import{useMDXComponents as c}from"./index-_VGcpBFS.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./iframe-s46AW8Vl.js";import"../sb-preview/runtime.js";import"./index-jmm5gWkb.js";import"./index-ogXoivrg.js";import"./index-MVbLLYTZ.js";import"./index-PPLHz8o0.js";import"./index.esm-OTULPyzz.js";const u=`# Visually Hidden
+
+Hides content from sighted users but keeps it accessible to non-visual user agents, such as screen readers.
+
+Note: in most cases, visually available content should be accessible to non-visual user agents and vice versa.
+Elaborate instructions or guidance read only by non-visual user agents can do more harm than good.
+`;function t(n){return s.jsxs(s.Fragment,{children:[s.jsx(o,{of:m}),`
+`,s.jsx(r,{children:u}),`
+`,s.jsx(a,{}),`
+`,s.jsx(i,{})]})}function D(n={}){const{wrapper:e}=Object.assign({},c(),n.components);return e?s.jsx(e,Object.assign({},n,{children:s.jsx(t,n)})):t()}export{D as default};
