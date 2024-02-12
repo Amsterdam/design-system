bb0b2b0ec67d177265aa66d748fa7f3a43df6d24
dlnr <niels@dlnr.dev>
2024-02-12 09:53:41 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@a72cfe59851264b7ba8d5f36042491c56d9e8599 🚀

---
+++ demo-DES-607-Tabs/assets/syntaxhighlighter-V7JZZA35-QN3M7mQQ.js
@@ -0,0 +1,1 @@
+import{S as l,c as n,s as d}from"./index-AAw9biti.js";import"./iframe-yIE5yVCj.js";import"../sb-preview/runtime.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./index-jmm5gWkb.js";import"./index-ogXoivrg.js";import"./index-MVbLLYTZ.js";import"./index-PPLHz8o0.js";export{l as SyntaxHighlighter,n as createCopyToClipboardFunction,d as default};
---
+++ demo-DES-607-Tabs/storybook-react/assets/Spotlight.stories-snU6aWRJ.js
@@ -0,0 +1,37 @@
+import{j as m}from"./jsx-runtime-vNq4Oc-g.js";import{J as d,G as g,c as H}from"./index.esm-YXIRo6kd.js";import{a as I}from"./exampleContent-GJvLNqvD.js";const K=I(),L={title:"Containers/Spotlight",component:d,argTypes:{as:{control:{type:"radio"},options:["article","aside","div","footer","section"]},color:{control:{type:"select"},options:["blue","dark-blue","dark-green","green","magenta","orange","purple","yellow"]},children:{control:{disable:!0}}},render:({as:F,color:u})=>m.jsx(d,{as:F,color:u,children:m.jsx(g,{paddingVertical:"medium",children:m.jsx(g.Cell,{span:"all",children:m.jsx(H,{inverseColor:!u||!["green","yellow"].includes(u),children:K})})})})},r={},e={args:{color:"blue"}},a={args:{color:"dark-blue"}},o={args:{color:"dark-green"}},s={args:{color:"green"}},c={args:{color:"magenta"}},n={args:{color:"orange"}},t={args:{color:"purple"}},l={args:{color:"yellow"}},p={args:{as:"section"}};var i,S,k;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:"{}",...(k=(S=r.parameters)==null?void 0:S.docs)==null?void 0:k.source}}};var b,f,x;e.parameters={...e.parameters,docs:{...(b=e.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    color: 'blue'
+  }
+}`,...(x=(f=e.parameters)==null?void 0:f.docs)==null?void 0:x.source}}};var y,h,j;a.parameters={...a.parameters,docs:{...(y=a.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    color: 'dark-blue'
+  }
+}`,...(j=(h=a.parameters)==null?void 0:h.docs)==null?void 0:j.source}}};var D,G,w;o.parameters={...o.parameters,docs:{...(D=o.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  args: {
+    color: 'dark-green'
+  }
+}`,...(w=(G=o.parameters)==null?void 0:G.docs)==null?void 0:w.source}}};var B,C,O;s.parameters={...s.parameters,docs:{...(B=s.parameters)==null?void 0:B.docs,source:{originalSource:`{
+  args: {
+    color: 'green'
+  }
+}`,...(O=(C=s.parameters)==null?void 0:C.docs)==null?void 0:O.source}}};var _,M,P;c.parameters={...c.parameters,docs:{...(_=c.parameters)==null?void 0:_.docs,source:{originalSource:`{
+  args: {
+    color: 'magenta'
+  }
+}`,...(P=(M=c.parameters)==null?void 0:M.docs)==null?void 0:P.source}}};var T,v,Y;n.parameters={...n.parameters,docs:{...(T=n.parameters)==null?void 0:T.docs,source:{originalSource:`{
+  args: {
+    color: 'orange'
+  }
+}`,...(Y=(v=n.parameters)==null?void 0:v.docs)==null?void 0:Y.source}}};var q,E,N;t.parameters={...t.parameters,docs:{...(q=t.parameters)==null?void 0:q.docs,source:{originalSource:`{
+  args: {
+    color: 'purple'
+  }
+}`,...(N=(E=t.parameters)==null?void 0:E.docs)==null?void 0:N.source}}};var z,J,Q;l.parameters={...l.parameters,docs:{...(z=l.parameters)==null?void 0:z.docs,source:{originalSource:`{
+  args: {
+    color: 'yellow'
+  }
+}`,...(Q=(J=l.parameters)==null?void 0:J.docs)==null?void 0:Q.source}}};var R,V,A;p.parameters={...p.parameters,docs:{...(R=p.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  args: {
+    as: 'section'
+  }
+}`,...(A=(V=p.parameters)==null?void 0:V.docs)==null?void 0:A.source}}};const U=["Default","Blue","DarkBlue","DarkGreen","Green","Magenta","Orange","Purple","Yellow","CustomTagName"],$=Object.freeze(Object.defineProperty({__proto__:null,Blue:e,CustomTagName:p,DarkBlue:a,DarkGreen:o,Default:r,Green:s,Magenta:c,Orange:n,Purple:t,Yellow:l,__namedExportsOrder:U,default:L},Symbol.toStringTag,{value:"Module"}));export{e as B,p as C,a as D,s as G,c as M,n as O,t as P,$ as S,l as Y,o as a};
