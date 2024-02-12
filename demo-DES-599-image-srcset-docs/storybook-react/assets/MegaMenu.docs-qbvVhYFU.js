30b76afcbdba05f9a6ecaa6953696ca513c77b27
dlnr <niels@dlnr.dev>
2024-02-12 10:50:27 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@6c2de4c46a05077e14832ce1d927844b37df6fe6 🚀

---
+++ demo-DES-599-image-srcset-docs/storybook-react/assets/MegaMenu.docs-qbvVhYFU.js
@@ -0,0 +1,6 @@
+import{j as t}from"./jsx-runtime-vNq4Oc-g.js";import{M as s,d as i,P as a,f as m}from"./index-nN5qrmgV.js";import{M as c,a as p}from"./MegaMenu.stories-g5PhJ7w7.js";import{useMDXComponents as r}from"./index-_VGcpBFS.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./iframe-x84v__37.js";import"../sb-preview/runtime.js";import"./index-jmm5gWkb.js";import"./index-ogXoivrg.js";import"./extends-dGVwEr9R.js";import"./index-MVbLLYTZ.js";import"./index-PPLHz8o0.js";import"./index.esm-pZj_1j2-.js";const M=`# Mega Menu
+`;function e(o){const n=Object.assign({h2:"h2"},r(),o.components);return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:c}),`
+`,t.jsx(i,{children:M}),`
+`,t.jsx(a,{}),`
+`,t.jsx(n.h2,{id:"with-categories",children:"With Categories"}),`
+`,t.jsx(m,{of:p})]})}function X(o={}){const{wrapper:n}=Object.assign({},r(),o.components);return n?t.jsx(n,Object.assign({},o,{children:t.jsx(e,o)})):e(o)}export{X as default};
---
+++ demo-DES-599-image-srcset-docs/storybook-react/assets/TopTaskLink.docs-CZEyOtxW.js
@@ -0,0 +1,29 @@
+import{j as e}from"./jsx-runtime-vNq4Oc-g.js";import{M as i,d as r,P as a,e as c,f as p}from"./index-nN5qrmgV.js";import{T as d,M as h}from"./TopTaskLink.stories-n06nsqON.js";import{useMDXComponents as o}from"./index-_VGcpBFS.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./iframe-x84v__37.js";import"../sb-preview/runtime.js";import"./index-jmm5gWkb.js";import"./index-ogXoivrg.js";import"./extends-dGVwEr9R.js";import"./index-MVbLLYTZ.js";import"./index-PPLHz8o0.js";import"./index.esm-pZj_1j2-.js";const m=`# Top Task Link
+
+A prominent navigation element to quickly start a task.
+
+## Guidelines
+
+- A Top Task Link has both a title and a description.
+- The title describes the essence of the page being referred to, attracting attention when scanning the page.
+- The description provides more context, helping the user confirm that they can indeed achieve their goal on the linked page.
+- Keep both texts concise.
+  A title consists of a few words, and the description consists of 1 or 2 short sentences.
+  The description should end with a period or another appropriate punctuation mark.
+- For wide windows, place 3 or 4 Top Task Links side by side on the [Grid](?path=/docs/react_layout-grid--docs).
+  If you have more Top Task Links, place them in the next row.
+  Try to distribute the amount of links on wide windows over rows of 3 or 4 links.
+  If this is not possible, leave the last cells of the grid empty.
+- If you have a group of links for which a description is unnecessary or illogical, use a [Link List](?path=/docs/react_navigation-link--docs).
+
+## Relevant WCAG requirements
+
+The Top Task Link is subject to the same requirements as [regular links](https://amsterdam.github.io/design-system/?path=/docs/react_navigation-link--docs).
+`;function s(n){const t=Object.assign({h3:"h3",p:"p"},o(),n.components);return e.jsxs(e.Fragment,{children:[e.jsx(i,{of:d}),`
+`,e.jsx(r,{children:m}),`
+`,e.jsx(a,{}),`
+`,e.jsx(c,{}),`
+`,e.jsx(t.h3,{id:"meerdere-links",children:"Meerdere links"}),`
+`,e.jsx(t.p,{children:`Top tasks usually come in groups.
+Present them in a grid.`}),`
+`,e.jsx(p,{of:h})]})}function C(n={}){const{wrapper:t}=Object.assign({},o(),n.components);return t?e.jsx(t,Object.assign({},n,{children:e.jsx(s,n)})):s(n)}export{C as default};
