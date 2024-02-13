a4de04ec573451378ee605d67fffe3f021e572bb
alimpens <37216945+alimpens@users.noreply.github.com>
2024-02-13 13:34:33 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@bb54fd2557aa1af60e8902c953d81c3c10ed8a5f 🚀

---
+++ demo-visually-hidden-span/storybook-react/assets/Footer.docs-DnAbUet8.js
@@ -0,0 +1,19 @@
+import{j as e}from"./jsx-runtime-vNq4Oc-g.js";import{M as i,d as s,P as r,e as a}from"./index-AJNKfOK1.js";import{F as c}from"./Footer.stories-ecmr04yF.js";import{useMDXComponents as m}from"./index-_VGcpBFS.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./iframe-1ogz312L.js";import"../sb-preview/runtime.js";import"./index-jmm5gWkb.js";import"./index-ogXoivrg.js";import"./index-MVbLLYTZ.js";import"./index-PPLHz8o0.js";import"./index.esm-V251aYq6.js";const p=`# Footer
+
+The footer is the closing section on every web page.
+Its content provides service information.
+A part is prescribed, and a part is filled based on the website’s objectives.
+
+## Guidelines
+
+- The footer is mandatory, and there is one on every page.
+- The first column focuses on contact information.
+  The information is as specific as possible and tailored to the respective website or page (e.g., including a chat option if available).
+- The second column contains links to relevant (online) sites or sources.
+- The third column refers to newsletters, social media, etc.
+- The last part of the footer is intended for links to privacy policies, cookie statements, explanations about the website, etc.
+  It is not intended for links to contact information, for example.
+`;function n(t){return e.jsxs(e.Fragment,{children:[e.jsx(i,{of:c}),`
+`,e.jsx(s,{children:p}),`
+`,e.jsx(r,{}),`
+`,e.jsx(a,{})]})}function y(t={}){const{wrapper:o}=Object.assign({},m(),t.components);return o?e.jsx(o,Object.assign({},t,{children:e.jsx(n,t)})):n()}export{y as default};
---
+++ demo-visually-hidden-span/storybook-react/assets/FormLabel.docs--YM5CHCz.js
@@ -0,0 +1,11 @@
+import{j as t}from"./jsx-runtime-vNq4Oc-g.js";import{M as e,d as s,P as a,e as i}from"./index-AJNKfOK1.js";import{F as m}from"./FormLabel.stories--kAt_lL_.js";import{useMDXComponents as p}from"./index-_VGcpBFS.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./iframe-1ogz312L.js";import"../sb-preview/runtime.js";import"./index-jmm5gWkb.js";import"./index-ogXoivrg.js";import"./index-MVbLLYTZ.js";import"./index-PPLHz8o0.js";import"./index.esm-V251aYq6.js";const c=`# Form Label
+
+Describes a form control.
+
+## Guidelines
+
+Always associate a form element (such as an \`input\` or \`textarea\`) with a label.
+`;function n(o){return t.jsxs(t.Fragment,{children:[t.jsx(e,{of:m}),`
+`,t.jsx(s,{children:c}),`
+`,t.jsx(a,{}),`
+`,t.jsx(i,{})]})}function g(o={}){const{wrapper:r}=Object.assign({},p(),o.components);return r?t.jsx(r,Object.assign({},o,{children:t.jsx(n,o)})):n()}export{g as default};
