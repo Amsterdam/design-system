e1401738918be0a0518d61f3f4e9925db5f91d57
dlnr <niels@dlnr.dev>
2024-02-13 08:24:12 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@3e6ec33408869b40bc8e1641e068f3fa0db27ca5 🚀

---
+++ demo-DES-582-button-refactor/assets/preview-gJqTTFT2.js
@@ -0,0 +1,7 @@
+import{_ as e}from"./iframe-fF-X_F1A.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-mrtY96h3.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9]),import.meta.url);return new r}}};export{a as parameters};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-mrtY96h3.js","./iframe-fF-X_F1A.js","./index-4g5l5LRQ.js","./_commonjsHelpers-4gQjN7DL.js","./react-18-ba7OOUbL.js","./index-jmm5gWkb.js","./index-xuMEpmi-.js","./index-ogXoivrg.js","./index-MVbLLYTZ.js","./index-PPLHz8o0.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
---
+++ demo-DES-582-button-refactor/storybook-react/assets/FormLabel.docs-7gXSk-Cr.js
@@ -0,0 +1,11 @@
+import{j as t}from"./jsx-runtime-vNq4Oc-g.js";import{M as e,d as s,P as a,e as i}from"./index-epPQHbwG.js";import{F as m}from"./FormLabel.stories-PVBWf26S.js";import{useMDXComponents as p}from"./index-_VGcpBFS.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./iframe-OC_quB7A.js";import"../sb-preview/runtime.js";import"./index-jmm5gWkb.js";import"./index-ogXoivrg.js";import"./index-MVbLLYTZ.js";import"./index-PPLHz8o0.js";import"./index.esm-OTULPyzz.js";const c=`# Form Label
+
+Describes a form control.
+
+## Guidelines
+
+Always associate a form element (such as an \`input\` or \`textarea\`) with a label.
+`;function n(o){return t.jsxs(t.Fragment,{children:[t.jsx(e,{of:m}),`
+`,t.jsx(s,{children:c}),`
+`,t.jsx(a,{}),`
+`,t.jsx(i,{})]})}function g(o={}){const{wrapper:r}=Object.assign({},p(),o.components);return r?t.jsx(r,Object.assign({},o,{children:t.jsx(n,o)})):n()}export{g as default};
