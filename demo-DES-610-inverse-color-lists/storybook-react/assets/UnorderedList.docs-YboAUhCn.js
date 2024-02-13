0fc2d366914e3f40335039e45f699e56ec6f269f
dlnr <niels@dlnr.dev>
2024-02-13 09:27:34 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@10885f007086596748c9bbf198dadd3f2f861bdd 🚀

---
+++ demo-DES-610-inverse-color-lists/storybook-react/assets/UnorderedList.docs-YboAUhCn.js
@@ -0,0 +1,31 @@
+import{j as e}from"./jsx-runtime-vNq4Oc-g.js";import{M as r,d as a,f as s}from"./index-YTJz7hE4.js";import{U as l,T as h,W as d}from"./UnorderedList.stories-XA-OsXcI.js";import{useMDXComponents as o}from"./index-_VGcpBFS.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./iframe-z5xz5wQg.js";import"../sb-preview/runtime.js";import"./index-jmm5gWkb.js";import"./index-ogXoivrg.js";import"./extends-dGVwEr9R.js";import"./index-MVbLLYTZ.js";import"./index-PPLHz8o0.js";import"./index.esm-iAewYi3C.js";import"./exampleContent-GJvLNqvD.js";const c=`# Unordered List
+
+A list is a vertical group of related content.
+This list can have 2 levels.
+The first level consists of squares.
+The second level consists of en dashes (–).
+Text in the list items is indented by a fixed distance.
+`;function i(n){const t=Object.assign({h2:"h2",h3:"h3",p:"p"},o(),n.components);return e.jsxs(e.Fragment,{children:[e.jsx(r,{of:l}),`
+`,e.jsx(a,{children:c}),`
+`,e.jsx(t.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(t.h3,{id:"default",children:"Default"}),`
+`,e.jsx(t.p,{children:`An unordered list has square bullet points.
+There is extra white space between the items.
+This provides better distinction between the items, especially when they consist of multiple lines of text.`}),`
+`,e.jsx(s,{of:void 0}),`
+`,e.jsx(t.h3,{id:"two-levels",children:"Two levels"}),`
+`,e.jsx(t.p,{children:`A list can have one deeper level.
+In this case, an en dash is used as the bullet point.
+It is left-aligned with the text of the higher-level item.`}),`
+`,e.jsx(s,{of:h}),`
+`,e.jsx(t.h3,{id:"list-without-bullet-points",children:"List without bullet points"}),`
+`,e.jsx(t.p,{children:`An overview of equivalent elements is also an unordered list.
+In such cases, use a list without bullet points to convey that semantics.
+This helps screen readers understand and navigate the list.`}),`
+`,e.jsx(t.p,{children:`Note: In this variant, each item typically contains a container with titles and other text components.
+Therefore, this variant does not define typography for the items.`}),`
+`,e.jsx(t.p,{children:"This example is based on the top tasks on the homepage of the website:"}),`
+`,e.jsx(s,{of:d}),`
+`,e.jsx(t.h3,{id:"list-with-inverted-colors-for-dark-backgrounds",children:"List with inverted colors for dark backgrounds"}),`
+`,e.jsx(t.p,{children:"When using a dark background, the list can have inverted colors to ensure readability."}),`
+`,e.jsx(s,{of:void 0})]})}function A(n={}){const{wrapper:t}=Object.assign({},o(),n.components);return t?e.jsx(t,Object.assign({},n,{children:e.jsx(i,n)})):i(n)}export{A as default};
---
+++ demo-DES-610-inverse-color-lists/storybook-react/assets/preview-dpOkVAf-.js
@@ -0,0 +1,7 @@
+import{_ as e}from"./iframe-z5xz5wQg.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-SOFxJS00.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10]),import.meta.url);return new r}}};export{a as parameters};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-SOFxJS00.js","./iframe-z5xz5wQg.js","./index-4g5l5LRQ.js","./_commonjsHelpers-4gQjN7DL.js","./react-18-ba7OOUbL.js","./index-jmm5gWkb.js","./index-YTJz7hE4.js","./index-ogXoivrg.js","./extends-dGVwEr9R.js","./index-MVbLLYTZ.js","./index-PPLHz8o0.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
