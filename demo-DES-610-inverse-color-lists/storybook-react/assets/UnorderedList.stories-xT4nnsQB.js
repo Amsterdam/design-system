e15454c520ba7cd8abb8be90a24517675735b7a1
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-02-13 10:49:06 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@7fb41b545d95eb493bbf444f8c7b3c396c43a6bc 🚀

---
+++ demo-DES-610-inverse-color-lists/storybook-react/assets/UnorderedList.stories-xT4nnsQB.js
@@ -0,0 +1,72 @@
+import{j as e}from"./jsx-runtime-vNq4Oc-g.js";import{U as r,m as s,R as k,P as n,W as S,X as P,Y as z,Z as y,r as N,_ as f,$ as b}from"./index.esm-iAewYi3C.js";import{f as W}from"./exampleContent-GJvLNqvD.js";const D=W().map((i,c)=>e.jsx(r.Item,{children:i},c)),w={title:"Text/Unordered List",component:r,args:{children:D},argTypes:{markers:{control:"boolean"}}},a={},d={args:{children:[e.jsxs(r.Item,{children:["Stadsdeel West",e.jsxs(r,{children:[e.jsx(r.Item,{children:"Bos en Lommer"},1.1),e.jsx(r.Item,{children:"Oud West / De Baarsjes"},1.2),e.jsx(r.Item,{children:"Westerpark"},1.3)]})]},1),e.jsxs(r.Item,{children:["Stadsdeel Nieuw-West",e.jsxs(r,{children:[e.jsx(r.Item,{children:"De Aker, Sloten en Nieuw Sloten"},2.1),e.jsx(r.Item,{children:"Geuzenveld, Slotermeer en Sloterdijken"},2.2),e.jsx(r.Item,{children:"Osdorp"},2.3),e.jsx(r.Item,{children:"Slotervaart"},2.4)]})]},2)]}},t={args:{children:[e.jsx(r.Item,{children:e.jsxs("div",{className:"amsterdam-docs-card",children:[e.jsx(s,{svg:k,size:"level-6"}),e.jsx(n,{children:"Stadsloket: locaties en openingstijden"})]})},1),e.jsx(r.Item,{children:e.jsxs("div",{className:"amsterdam-docs-card",children:[e.jsx(s,{svg:S,size:"level-6"}),e.jsx(n,{children:"Parkeren + Reizen (P+R)"})]})},2),e.jsx(r.Item,{children:e.jsxs("div",{className:"amsterdam-docs-card",children:[e.jsx(s,{svg:P,size:"level-6"}),e.jsx(n,{children:"Paspoort, ID-kaart en Rijbewijs"})]})},3),e.jsx(r.Item,{children:e.jsxs("div",{className:"amsterdam-docs-card",children:[e.jsx(s,{svg:z,size:"level-6"}),e.jsx(n,{children:"Gemeentebelastingen"})]})},4),e.jsx(r.Item,{children:e.jsxs("div",{className:"amsterdam-docs-card",children:[e.jsx(s,{svg:y,size:"level-6"}),e.jsx(n,{children:"Melding openbare ruimte en overlast"})]})},5),e.jsx(r.Item,{children:e.jsxs("div",{className:"amsterdam-docs-card",children:[e.jsx(s,{svg:N,size:"level-6"}),e.jsx(n,{children:"Verhuizing doorgeven"})]})},6),e.jsx(r.Item,{children:e.jsxs("div",{className:"amsterdam-docs-card",children:[e.jsx(s,{svg:f,size:"level-6"}),e.jsx(n,{children:"Grof afval"})]})},7),e.jsx(r.Item,{children:e.jsxs("div",{className:"amsterdam-docs-card",children:[e.jsx(s,{svg:b,size:"level-6"}),e.jsx(n,{children:"Kennisgevingen en bekendmakingen"})]})},8)],markers:!1}},o={args:{inverseColor:!0},decorators:[(i,c)=>e.jsx("div",{className:c.args.inverseColor?"amsterdam-docs-dark-background":void 0,children:e.jsx(i,{})})]};var m,l,v;a.parameters={...a.parameters,docs:{...(m=a.parameters)==null?void 0:m.docs,source:{originalSource:"{}",...(v=(l=a.parameters)==null?void 0:l.docs)==null?void 0:v.source}}};var g,I,h;d.parameters={...d.parameters,docs:{...(g=d.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    children: [<UnorderedList.Item key={1}>
+        Stadsdeel West
+        <UnorderedList>
+          <UnorderedList.Item key={1.1}>Bos en Lommer</UnorderedList.Item>
+          <UnorderedList.Item key={1.2}>Oud West / De Baarsjes</UnorderedList.Item>
+          <UnorderedList.Item key={1.3}>Westerpark</UnorderedList.Item>
+        </UnorderedList>
+      </UnorderedList.Item>, <UnorderedList.Item key={2}>
+        Stadsdeel Nieuw-West
+        <UnorderedList>
+          <UnorderedList.Item key={2.1}>De Aker, Sloten en Nieuw Sloten</UnorderedList.Item>
+          <UnorderedList.Item key={2.2}>Geuzenveld, Slotermeer en Sloterdijken</UnorderedList.Item>
+          <UnorderedList.Item key={2.3}>Osdorp</UnorderedList.Item>
+          <UnorderedList.Item key={2.4}>Slotervaart</UnorderedList.Item>
+        </UnorderedList>
+      </UnorderedList.Item>]
+  }
+}`,...(h=(I=d.parameters)==null?void 0:I.docs)==null?void 0:h.source}}};var j,p,x;t.parameters={...t.parameters,docs:{...(j=t.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  args: {
+    children: [<UnorderedList.Item key={1}>
+        <div className="amsterdam-docs-card">
+          <Icon svg={LocationIcon} size="level-6" />
+          <Paragraph>Stadsloket: locaties en openingstijden</Paragraph>
+        </div>
+      </UnorderedList.Item>, <UnorderedList.Item key={2}>
+        <div className="amsterdam-docs-card">
+          <Icon svg={CarIcon} size="level-6" />
+          <Paragraph>Parkeren + Reizen (P+R)</Paragraph>
+        </div>
+      </UnorderedList.Item>, <UnorderedList.Item key={3}>
+        <div className="amsterdam-docs-card">
+          <Icon svg={PassportIcon} size="level-6" />
+          <Paragraph>Paspoort, ID-kaart en Rijbewijs</Paragraph>
+        </div>
+      </UnorderedList.Item>, <UnorderedList.Item key={4}>
+        <div className="amsterdam-docs-card">
+          <Icon svg={DocumentEuroSignIcon} size="level-6" />
+          <Paragraph>Gemeentebelastingen</Paragraph>
+        </div>
+      </UnorderedList.Item>, <UnorderedList.Item key={5}>
+        <div className="amsterdam-docs-card">
+          <Icon svg={AlertIcon} size="level-6" />
+          <Paragraph>Melding openbare ruimte en overlast</Paragraph>
+        </div>
+      </UnorderedList.Item>, <UnorderedList.Item key={6}>
+        <div className="amsterdam-docs-card">
+          <Icon svg={HousingIcon} size="level-6" />
+          <Paragraph>Verhuizing doorgeven</Paragraph>
+        </div>
+      </UnorderedList.Item>, <UnorderedList.Item key={7}>
+        <div className="amsterdam-docs-card">
+          <Icon svg={TrashBinIcon} size="level-6" />
+          <Paragraph>Grof afval</Paragraph>
+        </div>
+      </UnorderedList.Item>, <UnorderedList.Item key={8}>
+        <div className="amsterdam-docs-card">
+          <Icon svg={AnnouncementIcon} size="level-6" />
+          <Paragraph>Kennisgevingen en bekendmakingen</Paragraph>
+        </div>
+      </UnorderedList.Item>],
+    markers: false
+  }
+}`,...(x=(p=t.parameters)==null?void 0:p.docs)==null?void 0:x.source}}};var L,u,U;o.parameters={...o.parameters,docs:{...(L=o.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  args: {
+    inverseColor: true
+  },
+  decorators: [(Story, context) => <div className={context.args.inverseColor ? 'amsterdam-docs-dark-background' : undefined}>
+        <Story />
+      </div>]
+}`,...(U=(u=o.parameters)==null?void 0:u.docs)==null?void 0:U.source}}};const C=["Default","TwoLevels","WithoutMarkers","InverseColor"],_=Object.freeze(Object.defineProperty({__proto__:null,Default:a,InverseColor:o,TwoLevels:d,WithoutMarkers:t,__namedExportsOrder:C,default:w},Symbol.toStringTag,{value:"Module"}));export{a as D,o as I,d as T,_ as U,t as W};
