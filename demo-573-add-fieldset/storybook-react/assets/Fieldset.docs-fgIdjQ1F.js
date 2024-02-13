b354e71e82cde62e8dfa5de9702cb99c9216c7d5
alimpens <37216945+alimpens@users.noreply.github.com>
2024-02-13 16:08:55 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@2027e741f6e76006b5a57a11dfbdc50cf9fb6e72 🚀

---
+++ demo-573-add-fieldset/storybook-react/assets/Fieldset.docs-fgIdjQ1F.js
@@ -0,0 +1,20 @@
+import{j as e}from"./jsx-runtime-vNq4Oc-g.js";import{M as i,d as r,P as a,e as p,f as u}from"./index-or537XB4.js";import{F as d,C as l}from"./Fieldset.stories-U-dqDCvw.js";import{useMDXComponents as s}from"./index-_VGcpBFS.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./iframe-54cYQv0y.js";import"../sb-preview/runtime.js";import"./index-jmm5gWkb.js";import"./index-ogXoivrg.js";import"./index-MVbLLYTZ.js";import"./index-PPLHz8o0.js";import"./index.esm-3Uge9dsw.js";const m=`# Fieldset
+
+A component to group related form inputs.
+
+## Guidelines
+
+- Use Fieldset when you need to show a relationship between multiple form inputs. For example, you may need to group a set of text inputs into a single fieldset when asking for an address.
+- When grouping radio inputs, use \`role="radiogroup"\` on Fieldset to have this grouping explicitly announced as a radio group. Fieldset has a default role of \`group\`.
+
+## Relevant WCAG Requirements
+
+- [WCAG 1.3.5](https://www.w3.org/WAI/WCAG22/Understanding/identify-input-purpose.html): Fieldset labels the purpose of a group of inputs.
+`;function t(n){const o=Object.assign({h2:"h2",h3:"h3",p:"p"},s(),n.components);return e.jsxs(e.Fragment,{children:[e.jsx(i,{of:d}),`
+`,e.jsx(r,{children:m}),`
+`,e.jsx(a,{}),`
+`,e.jsx(p,{}),`
+`,e.jsx(o.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(o.h3,{id:"checkbox-group",children:"Checkbox group"}),`
+`,e.jsx(o.p,{children:"Fieldset is used to group related form inputs, like checkboxes."}),`
+`,e.jsx(u,{of:l})]})}function A(n={}){const{wrapper:o}=Object.assign({},s(),n.components);return o?e.jsx(o,Object.assign({},n,{children:e.jsx(t,n)})):t(n)}export{A as default};
---
+++ demo-573-add-fieldset/storybook-react/assets/preview-IC08_wcb.js
@@ -0,0 +1,7 @@
+import{_ as e}from"./iframe-54cYQv0y.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-1gUStCLG.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9]),import.meta.url);return new r}}};export{a as parameters};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-1gUStCLG.js","./iframe-54cYQv0y.js","./index-4g5l5LRQ.js","./_commonjsHelpers-4gQjN7DL.js","./react-18-ba7OOUbL.js","./index-jmm5gWkb.js","./index-or537XB4.js","./index-ogXoivrg.js","./index-MVbLLYTZ.js","./index-PPLHz8o0.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
