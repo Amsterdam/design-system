f491808198ecd4995b0f000f97e16c4f74cf2f56
VincentSmedinga <runner@fv-az566-50.ut545be5203uvep113pmbghppc.ex.internal.cloudapp.net>
2024-02-14 21:58:01 +0000

    Remove feature branch folder

--- demo-visually-hidden-span/storybook-react/assets/Badge.stories-C2p9k3wy.js
+++
@@ -1,4 +0,0 @@
-import{B as t}from"./index.esm-ChswzL_L.js";/**
- * @license EUPL-1.2+
- * Copyright (c) 2024 Gemeente Amsterdam
- */const s={title:"Feedback/Badge",component:t,args:{label:"Tip"},argTypes:{color:{control:{type:"select"},options:["blue","dark-blue","dark-green","green","magenta","orange","purple","yellow"],selected:"dark-green"}}},e={};var r,o,a;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:"{}",...(a=(o=e.parameters)==null?void 0:o.docs)==null?void 0:a.source}}};const l=["Default"],n=Object.freeze(Object.defineProperty({__proto__:null,Default:e,__namedExportsOrder:l,default:s},Symbol.toStringTag,{value:"Module"}));export{n as B};
