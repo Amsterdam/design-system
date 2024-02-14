aa66c55407c0fd927289b95445e154a9fa8cb1a6
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-02-14 21:59:01 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@b839763066c7debe082fbe9ec2530c363a17b166 🚀

--- demo-develop/storybook-react/assets/preview-8I9Y9hJw.js
+++
@@ -1,7 +0,0 @@
-import{_ as e}from"./iframe-BMmjn3_m.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-_I4_8IWM.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9]),import.meta.url);return new r}}};export{a as parameters};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-_I4_8IWM.js","./iframe-BMmjn3_m.js","./index-CBqU2yxZ.js","./_commonjsHelpers-BosuxZz1.js","./react-18-D8cruF67.js","./index-BtM5VmRH.js","./index-DQPuBdJr.js","./index-Bw8VTzHM.js","./index-Cmc67Rxs.js","./index-DrFu-skq.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
