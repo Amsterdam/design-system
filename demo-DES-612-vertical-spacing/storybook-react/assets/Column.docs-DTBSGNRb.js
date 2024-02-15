526b8cef60d3b2623d01224ea868f9ccaf40fced
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-02-15 12:25:38 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@dffa340243c46f7618653ac08eba15aeb02435e9 🚀

--- demo-DES-612-vertical-spacing/storybook-react/assets/Column.docs-DTBSGNRb.js
+++
@@ -1,5 +0,0 @@
-import{j as t}from"./jsx-runtime-CKrituN3.js";import{M as s,d as m,P as i,e}from"./index-DTTcMO7t.js";import{C as a}from"./Column.stories-GBWrtdY8.js";import{useMDXComponents as p}from"./index-DI5IigMn.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-BZ7Iaf65.js";import"../sb-preview/runtime.js";import"./index-BtM5VmRH.js";import"./index-Bw8VTzHM.js";import"./index-Cmc67Rxs.js";import"./index-DrFu-skq.js";import"./index.esm-CMYuXqx7.js";const c=`# Column
-`;function n(o){return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:a}),`
-`,t.jsx(m,{children:c}),`
-`,t.jsx(i,{}),`
-`,t.jsx(e,{})]})}function b(o={}){const{wrapper:r}=Object.assign({},p(),o.components);return r?t.jsx(r,Object.assign({},o,{children:t.jsx(n,o)})):n()}export{b as default};
