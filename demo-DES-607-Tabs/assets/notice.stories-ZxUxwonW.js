696e9cb0e63dc98aa9ca4704730c5f21dbf9625a
dlnr <niels@dlnr.dev>
2024-02-15 13:57:27 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@4f4f19dae92789755f25445d69bd0423f11be256 🚀

---
+++ demo-DES-607-Tabs/assets/notice.stories-ZxUxwonW.js
@@ -0,0 +1,25 @@
+import{M as s,d as a}from"./index-1JQia_-U.js";import{j as e}from"./jsx-runtime-vNq4Oc-g.js";import{useMDXComponents as i}from"./index-_VGcpBFS.js";import"./iframe-jYfkfoRZ.js";import"../sb-preview/runtime.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./index-jmm5gWkb.js";import"./index-ogXoivrg.js";import"./index-MVbLLYTZ.js";import"./index-PPLHz8o0.js";const d=`<!-- @license CC0-1.0 -->
+
+# Copyright City of Amsterdam
+
+Copyright (c) 2023 City of Amsterdam
+
+## Logo and Branding
+
+Copyright applies to the branding and logo.
+Using the logo and branding is permitted solely for the City of Amsterdam.
+
+If you intend to use a modified version of the software for other purposes, you are not allowed to use the City of Amsterdam’s logo.
+Instead, you should design your own branding.
+
+## Font
+
+Not all fonts used in the branding are free and open source.
+Please note that you must arrange a (paid) license if you use the provided fonts.
+Alternatively, adjust the configuration to use fewer or different fonts.
+
+## Permission
+
+For permission to use the logo or branding, please contact <designsystem@amsterdam.nl>.
+`;function r(o){return e.jsxs(e.Fragment,{children:[e.jsx(s,{title:"Docs/Permission to Use"}),`
+`,e.jsx(a,{children:d})]})}function m(o={}){const{wrapper:n}=Object.assign({},i(),o.components);return n?e.jsx(n,{...o,children:e.jsx(r,{...o})}):r()}const p=()=>{throw new Error("Docs-only story")};p.parameters={docsOnly:!0};const t={title:"Docs/Permission to Use",tags:["stories-mdx"],includeStories:["__page"]};t.parameters=t.parameters||{};t.parameters.docs={...t.parameters.docs||{},page:m};const w=["__page"];export{w as __namedExportsOrder,p as __page,t as default};
