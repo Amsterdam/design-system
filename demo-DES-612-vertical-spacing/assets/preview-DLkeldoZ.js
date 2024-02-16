d2c9c71374be60fa84ae760af25182b111a11bbd
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-02-16 11:04:25 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@74f4892bc34b0189816ec6729683097c1d9220f6 🚀

---
+++ demo-DES-612-vertical-spacing/assets/preview-DLkeldoZ.js
@@ -0,0 +1,7 @@
+import{_ as e}from"./iframe-CV1A_xMb.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-Cam0yZca.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9]),import.meta.url);return new r}}};export{a as parameters};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-Cam0yZca.js","./iframe-CV1A_xMb.js","./index-CBqU2yxZ.js","./_commonjsHelpers-BosuxZz1.js","./react-18-D8cruF67.js","./index-BtM5VmRH.js","./index-CFl8jOdt.js","./index-Bw8VTzHM.js","./index-Cmc67Rxs.js","./index-DrFu-skq.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
