207d9a0e53af8163f6b54fe617ea3b513d5215a3
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-02-16 10:31:40 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@5ad00826b062e9aa1123e1af56c2b2063cacb86d 🚀

---
+++ demo-DES-612-vertical-spacing/assets/preview-8UeTzpkv.js
@@ -0,0 +1,7 @@
+import{_ as e}from"./iframe-B6n6-6KL.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-CQoe0vMh.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9]),import.meta.url);return new r}}};export{a as parameters};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-CQoe0vMh.js","./iframe-B6n6-6KL.js","./index-CBqU2yxZ.js","./_commonjsHelpers-BosuxZz1.js","./react-18-D8cruF67.js","./index-BtM5VmRH.js","./index-B5Z_xql3.js","./index-Bw8VTzHM.js","./index-Cmc67Rxs.js","./index-DrFu-skq.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
---
+++ demo-DES-612-vertical-spacing/assets/syntaxhighlighter-V7JZZA35-Cw8xA72S.js
@@ -0,0 +1,1 @@
+import{S as l,c as n,s as d}from"./index-B5Z_xql3.js";import"./iframe-B6n6-6KL.js";import"../sb-preview/runtime.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-BtM5VmRH.js";import"./index-Bw8VTzHM.js";import"./index-Cmc67Rxs.js";import"./index-DrFu-skq.js";export{l as SyntaxHighlighter,n as createCopyToClipboardFunction,d as default};
