c750defa63d13f1ecbb49142a456801c24b3f87f
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-02-20 10:11:42 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@6ab142c539c0b3321c92d19d20cc63483aa08756 🚀

---
+++ demo-list-stories-args/storybook-react/assets/Table.docs-JnQM9pDX.js
@@ -0,0 +1,20 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{M as a,d as r,P as i,f as d}from"./index-DSYmj2uJ.js";import{T as m,W as c}from"./Table.stories-BK5kJmAw.js";import{useMDXComponents as s}from"./index-DI5IigMn.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-C5UQUD2m.js";import"../sb-preview/runtime.js";import"./index-BtM5VmRH.js";import"./index-Bw8VTzHM.js";import"./index-Cmc67Rxs.js";import"./index-DrFu-skq.js";import"./index.esm-B56C_5T6.js";const l=`# Table
+
+Use the table component to let users compare information in rows and columns.
+Table is used to display simple information that does not need to be filtered or edited.
+
+## Guidelines
+
+- Use \`<Table.Caption>\` to describe a table in the same way you would use a heading.
+  A caption helps users find, navigate and understand tables.
+- Never use the table component to layout content on a page.
+  Instead, use [Grid](https://amsterdam.github.io/design-system/?path=/docs/react_layout-grid--docs).
+`;function o(n){const t=Object.assign({h2:"h2",h3:"h3",p:"p",code:"code"},s(),n.components);return e.jsxs(e.Fragment,{children:[e.jsx(a,{of:m}),`
+`,e.jsx(r,{children:l}),`
+`,e.jsx(i,{}),`
+`,e.jsx(t.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(t.h3,{id:"with-footer-and-row-headers",children:"With footer and row headers"}),`
+`,e.jsxs(t.p,{children:[`Use table headers to tell users what the rows and columns represent.
+Use the scope attribute to help users of assistive technology distinguish between row and column headers.
+Use `,e.jsx(t.code,{children:"Table.Footer"})," for a summary row."]}),`
+`,e.jsx(d,{of:c})]})}function U(n={}){const{wrapper:t}=Object.assign({},s(),n.components);return t?e.jsx(t,Object.assign({},n,{children:e.jsx(o,n)})):o(n)}export{U as default};
---
+++ demo-list-stories-args/storybook-react/assets/preview-DyPAs36e.js
@@ -0,0 +1,7 @@
+import{_ as e}from"./iframe-C5UQUD2m.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-TYoWnscp.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9]),import.meta.url);return new r}}};export{a as parameters};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-TYoWnscp.js","./iframe-C5UQUD2m.js","./index-CBqU2yxZ.js","./_commonjsHelpers-BosuxZz1.js","./react-18-D8cruF67.js","./index-BtM5VmRH.js","./index-DSYmj2uJ.js","./index-Bw8VTzHM.js","./index-Cmc67Rxs.js","./index-DrFu-skq.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
