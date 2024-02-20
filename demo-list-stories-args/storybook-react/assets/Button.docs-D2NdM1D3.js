77171f8cec9309cdee629531b1d48d13d8df176f
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-02-20 14:22:26 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@f21512f239b4aa8eccbbdca4153bf6cb2123fe55 🚀

---
+++ demo-list-stories-args/storybook-react/assets/Button.docs-D2NdM1D3.js
@@ -0,0 +1,47 @@
+import{j as t}from"./jsx-runtime-CKrituN3.js";import{M as a,d as i,P as c,e as l,f as o}from"./index-DVwOvVrI.js";import{B as u,P as h,S as d,T as m,a as p}from"./Button.stories-CZ54-HWT.js";import{useMDXComponents as r}from"./index-DI5IigMn.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-DaUo0OQX.js";import"../sb-preview/runtime.js";import"./index-BtM5VmRH.js";import"./index-Bw8VTzHM.js";import"./index-Cmc67Rxs.js";import"./index-DrFu-skq.js";import"./index.esm-B56C_5T6.js";const b=`# Button
+
+Allows the user to perform actions and operate the user interface.
+
+## Guidelines
+
+- A label text that describes the function of the button.
+- A clear contrasting colour scheme so that it is easy to recognize and quickly locate.
+- Use the correct type of button for the corresponding application.
+  For example, a button within a form must always be of the type \`submit\`.
+- Make sure one can operate a button through a keyboard.
+
+## Relevant WCAG requirements
+
+- [WCAG requirement 2.1.3](https://www.w3.org/TR/WCAG21/#keyboard-no-exception):
+  You can activate the button with \`Enter\` or \`Space\`, and it is possible with ‘Tab’ the button to focus.
+  Using \`Space\` does not result in scrolling the page.
+- [WCAG requirement 2.4.6](https://www.w3.org/TR/WCAG21/#headings-and-labels): the label of the button must be clear
+- [WCAG requirement 2.5.2](https://www.w3.org/TR/WCAG21/#pointer-cancellation):
+  - do not use the \`mousedown\` event to activate the button, instead use the \`click\` event
+  - \`mousedown\` may only be used if \`mouseup\` undoes the effect, for example, a “fast forward” button to fast-forward audio or video.
+- [WCAG requirement 2.5.5](https://www.w3.org/TR/WCAG21/#target-size): The button must be large enough to click, and small buttons should not be too close to another button.
+
+## References
+
+- [W3C - Focus Visible](https://www.w3.org/WAI/WCAG21/Understanding/focus-visible.html)
+- [W3C - Headings & labels](https://www.w3.org/TR/WCAG21/#headings-and-labels)
+`;function s(n){const e=Object.assign({h2:"h2",p:"p",h3:"h3"},r(),n.components);return t.jsxs(t.Fragment,{children:[t.jsx(a,{of:u}),`
+`,t.jsx(i,{children:b}),`
+`,t.jsx(c,{}),`
+`,t.jsx(l,{}),`
+`,t.jsx(e.h2,{id:"examples",children:"Examples"}),`
+`,t.jsx(e.p,{children:`Primary, secondary and tertiary buttons can be used side by side.
+Skipping levels is allowed.`}),`
+`,t.jsx(e.h3,{id:"primary",children:"Primary"}),`
+`,t.jsx(e.p,{children:`The most important call-to-action button.
+One primary button may be used per screen.`}),`
+`,t.jsx(o,{of:h}),`
+`,t.jsx(e.h3,{id:"secondary",children:"Secondary"}),`
+`,t.jsx(e.p,{children:`Use the secondary button for less prominent calls to action.
+It is possible to use more than 1 secondary button.`}),`
+`,t.jsx(o,{of:d}),`
+`,t.jsx(e.h3,{id:"tertiary",children:"Tertiary"}),`
+`,t.jsx(e.p,{children:"Use tertiary buttons for unimportant calls to action – as many as necessary."}),`
+`,t.jsx(o,{of:m}),`
+`,t.jsx(e.h3,{id:"button-with-an-icon",children:"Button with an icon"}),`
+`,t.jsx(o,{of:p})]})}function R(n={}){const{wrapper:e}=Object.assign({},r(),n.components);return e?t.jsx(e,Object.assign({},n,{children:t.jsx(s,n)})):s(n)}export{R as default};
---
+++ demo-list-stories-args/storybook-react/assets/FormLabel.stories-xtwV0DDw.js
@@ -0,0 +1,8 @@
+import{m as t}from"./index.esm-B56C_5T6.js";/**
+ * @license EUPL-1.2+
+ * Copyright (c) 2023 Gemeente Amsterdam
+ */const s={title:"Forms/Form Label",component:t,args:{children:"Tekst"},argTypes:{children:{table:{disable:!1}}}},e={args:{children:"Label"}};var r,a,o;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    children: 'Label'
+  }
+}`,...(o=(a=e.parameters)==null?void 0:a.docs)==null?void 0:o.source}}};const l=["Default"],c=Object.freeze(Object.defineProperty({__proto__:null,Default:e,__namedExportsOrder:l,default:s},Symbol.toStringTag,{value:"Module"}));export{c as F};
