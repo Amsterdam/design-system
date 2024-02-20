2abe5e6117ed118ac0ffcca1ab291253a2ade8ce
Vincent Smedinga <v.smedinga@amsterdam.nl>
2024-02-20 11:08:45 +0100

    Apply args to all lists and display controls

--- storybook/storybook-react/src/OrderedList/OrderedList.stories.tsx
+++ storybook/storybook-react/src/OrderedList/OrderedList.stories.tsx
@@ -7,110 +7,20 @@ import { Heading, OrderedList, Paragraph } from '@amsterdam/design-system-react'
 import type { Meta, StoryObj } from '@storybook/react'
 import { exampleOrderedList } from '../shared/exampleContent'
 
-const orderedList = exampleOrderedList().map((text, index) => <OrderedList.Item key={index}>{text}</OrderedList.Item>)
+const orderedListItems = exampleOrderedList().map((text, index) => (
+  <OrderedList.Item key={index}>{text}</OrderedList.Item>
+))
 
 const meta = {
   title: 'Text/Ordered List',
   component: OrderedList,
   args: {
-    children: orderedList,
     inverseColor: false,
-    markers: true,
+    markers: undefined,
   },
   argTypes: {
     inverseColor: { control: 'boolean' },
     markers: { control: 'boolean' },
-  },
-} satisfies Meta<typeof OrderedList>
-
-export default meta
-
-type Story = StoryObj<typeof meta>
-
-export const Default: Story = {}
-
-export const TwoLevels: Story = {
-  args: {
-    children: [
-      <OrderedList.Item key={1}>
-        Stadsdeel West
-        <OrderedList>
-          <OrderedList.Item key={1.1}>Bos en Lommer</OrderedList.Item>
-          <OrderedList.Item key={1.2}>Oud West / De Baarsjes</OrderedList.Item>
-          <OrderedList.Item key={1.3}>Westerpark</OrderedList.Item>
-        </OrderedList>
-      </OrderedList.Item>,
-      <OrderedList.Item key={2}>
-        Stadsdeel Nieuw-West
-        <OrderedList>
-          <OrderedList.Item key={2.1}>De Aker, Sloten en Nieuw Sloten</OrderedList.Item>
-          <OrderedList.Item key={2.2}>Geuzenveld, Slotermeer en Sloterdijken</OrderedList.Item>
-          <OrderedList.Item key={2.3}>Osdorp</OrderedList.Item>
-          <OrderedList.Item key={2.4}>Slotervaart</OrderedList.Item>
-        </OrderedList>
-      </OrderedList.Item>,
-    ],
-  },
-}
-
-export const StartingNumber: Story = {
-  args: {
-    children: [
-      <OrderedList.Item key={6}>Zes</OrderedList.Item>,
-      <OrderedList.Item key={7}>Zeven</OrderedList.Item>,
-      <OrderedList.Item key={8}>Acht</OrderedList.Item>,
-    ],
-    start: 6,
-  },
-}
-
-export const DescendingNumbers: Story = {
-  args: {
-    children: [
-      <OrderedList.Item key={3}>Drie</OrderedList.Item>,
-      <OrderedList.Item key={2}>Twee</OrderedList.Item>,
-      <OrderedList.Item key={1}>Eén</OrderedList.Item>,
-    ],
-    reversed: true,
-    start: 3,
-  },
-}
-
-export const WithoutMarkers: Story = {
-  args: {
-    children: [
-      <OrderedList.Item key={0}>
-        <Heading size="level-4">Weg met steen, hallo extra groen en koelte</Heading>
-        <Paragraph>
-          Sinds 2021 kwamen er maar liefst 60 nieuwe groene plekken bij in de stad. Groen is fijn en het verkoelt de
-          stad in de zomer. Een paar voorbeelden.
-        </Paragraph>
-        <Paragraph size="small">16 augustus 2023</Paragraph>
-      </OrderedList.Item>,
-      <OrderedList.Item key={1}>
-        <Heading size="level-4">Amsterdam bindt de strijd aan met lawaaierige voertuigen</Heading>
-        <Paragraph>
-          Deze zomer testen we of digitale borden langs de weg kunnen helpen om geluidsoverlast van voertuigen zoals
-          motoren en auto’s tegen te gaan.
-        </Paragraph>
-        <Paragraph size="small">10 augustus 2023</Paragraph>
-      </OrderedList.Item>,
-      <OrderedList.Item key={2}>
-        <Heading size="level-4">Een prachtroute door de wonderlijke Baarsjes</Heading>
-        <Paragraph>
-          In de Baarsjes zijn kunst en cultuur met elkaar vervlochten. We zetten een prachtige wandelroute voor u uit en
-          laten zien hoe het was en hoe het nu is.
-        </Paragraph>
-        <Paragraph size="small">8 augustus 2023</Paragraph>
-      </OrderedList.Item>,
-    ],
-    markers: false,
-  },
-}
-
-export const InverseColor: Story = {
-  args: {
-    inverseColor: true,
   },
   decorators: [
     (Story, context) => (
@@ -119,6 +29,110 @@ export const InverseColor: Story = {
       </div>
     ),
   ],
+} satisfies Meta<typeof OrderedList>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  render: (args) => <OrderedList {...args}>{orderedListItems}</OrderedList>,
+}
+
+export const TwoLevels: Story = {
+  render: (args) => (
+    <OrderedList {...args}>
+      <OrderedList.Item key={1}>
+        Stadsdeel West
+        <OrderedList {...args}>
+          <OrderedList.Item key={1.1}>Bos en Lommer</OrderedList.Item>
+          <OrderedList.Item key={1.2}>Oud West / De Baarsjes</OrderedList.Item>
+          <OrderedList.Item key={1.3}>Westerpark</OrderedList.Item>
+        </OrderedList>
+      </OrderedList.Item>
+      <OrderedList.Item key={2}>
+        Stadsdeel Nieuw-West
+        <OrderedList {...args}>
+          <OrderedList.Item key={2.1}>De Aker, Sloten en Nieuw Sloten</OrderedList.Item>
+          <OrderedList.Item key={2.2}>Geuzenveld, Slotermeer en Sloterdijken</OrderedList.Item>
+          <OrderedList.Item key={2.3}>Osdorp</OrderedList.Item>
+          <OrderedList.Item key={2.4}>Slotervaart</OrderedList.Item>
+        </OrderedList>
+      </OrderedList.Item>
+    </OrderedList>
+  ),
+}
+
+export const StartingNumber: Story = {
+  args: {
+    start: 6,
+  },
+  render: (args) => (
+    <OrderedList {...args}>
+      <OrderedList.Item key={6}>Zes</OrderedList.Item>
+      <OrderedList.Item key={7}>Zeven</OrderedList.Item>
+      <OrderedList.Item key={8}>Acht</OrderedList.Item>
+    </OrderedList>
+  ),
+}
+
+export const DescendingNumbers: Story = {
+  args: {
+    reversed: true,
+    start: 3,
+  },
+  render: (args) => (
+    <OrderedList {...args}>
+      <OrderedList.Item key={3}>Drie</OrderedList.Item>
+      <OrderedList.Item key={2}>Twee</OrderedList.Item>
+      <OrderedList.Item key={1}>Eén</OrderedList.Item>
+    </OrderedList>
+  ),
+}
+
+export const WithoutMarkers: Story = {
+  args: {
+    markers: false,
+  },
+  argTypes: {
+    inverseColor: {
+      table: { disable: true },
+    },
+  },
+  render: (args) => (
+    <OrderedList {...args}>
+      <OrderedList.Item key={0}>
+        <Heading size="level-4">Weg met steen, hallo extra groen en koelte</Heading>
+        <Paragraph>
+          Sinds 2021 kwamen er maar liefst 60 nieuwe groene plekken bij in de stad. Groen is fijn en het verkoelt de
+          stad in de zomer. Een paar voorbeelden.
+        </Paragraph>
+        <Paragraph size="small">16 augustus 2023</Paragraph>
+      </OrderedList.Item>
+      <OrderedList.Item key={1}>
+        <Heading size="level-4">Amsterdam bindt de strijd aan met lawaaierige voertuigen</Heading>
+        <Paragraph>
+          Deze zomer testen we of digitale borden langs de weg kunnen helpen om geluidsoverlast van voertuigen zoals
+          motoren en auto’s tegen te gaan.
+        </Paragraph>
+        <Paragraph size="small">10 augustus 2023</Paragraph>
+      </OrderedList.Item>
+      <OrderedList.Item key={2}>
+        <Heading size="level-4">Een prachtroute door de wonderlijke Baarsjes</Heading>
+        <Paragraph>
+          In de Baarsjes zijn kunst en cultuur met elkaar vervlochten. We zetten een prachtige wandelroute voor u uit en
+          laten zien hoe het was en hoe het nu is.
+        </Paragraph>
+        <Paragraph size="small">8 augustus 2023</Paragraph>
+      </OrderedList.Item>
+    </OrderedList>
+  ),
+}
+
+export const InverseColor: Story = {
+  args: {
+    inverseColor: true,
+  },
   render: (args) => (
     <OrderedList {...args}>
       <OrderedList.Item>
--- storybook/storybook-react/src/UnorderedList/UnorderedList.stories.tsx
+++ storybook/storybook-react/src/UnorderedList/UnorderedList.stories.tsx
@@ -17,7 +17,7 @@ import {
 import type { Meta, StoryObj } from '@storybook/react'
 import { exampleUnorderedList } from '../shared/exampleContent'
 
-const unorderedList = exampleUnorderedList().map((text, index) => (
+const unorderedListItems = exampleUnorderedList().map((text, index) => (
   <UnorderedList.Item key={index}>{text}</UnorderedList.Item>
 ))
 
@@ -25,105 +25,12 @@ const meta = {
   title: 'Text/Unordered List',
   component: UnorderedList,
   args: {
-    children: unorderedList,
     inverseColor: false,
-    markers: true,
+    markers: undefined,
   },
   argTypes: {
     inverseColor: { control: 'boolean' },
     markers: { control: 'boolean' },
-  },
-} satisfies Meta<typeof UnorderedList>
-
-export default meta
-
-type Story = StoryObj<typeof meta>
-
-export const Default: Story = {}
-
-export const TwoLevels: Story = {
-  args: {
-    children: [
-      <UnorderedList.Item key={1}>
-        Stadsdeel West
-        <UnorderedList>
-          <UnorderedList.Item key={1.1}>Bos en Lommer</UnorderedList.Item>
-          <UnorderedList.Item key={1.2}>Oud West / De Baarsjes</UnorderedList.Item>
-          <UnorderedList.Item key={1.3}>Westerpark</UnorderedList.Item>
-        </UnorderedList>
-      </UnorderedList.Item>,
-      <UnorderedList.Item key={2}>
-        Stadsdeel Nieuw-West
-        <UnorderedList>
-          <UnorderedList.Item key={2.1}>De Aker, Sloten en Nieuw Sloten</UnorderedList.Item>
-          <UnorderedList.Item key={2.2}>Geuzenveld, Slotermeer en Sloterdijken</UnorderedList.Item>
-          <UnorderedList.Item key={2.3}>Osdorp</UnorderedList.Item>
-          <UnorderedList.Item key={2.4}>Slotervaart</UnorderedList.Item>
-        </UnorderedList>
-      </UnorderedList.Item>,
-    ],
-  },
-}
-
-export const WithoutMarkers: Story = {
-  args: {
-    children: [
-      <UnorderedList.Item key={1}>
-        <div className="amsterdam-docs-card">
-          <Icon svg={LocationIcon} size="level-6" />
-          <Paragraph>Stadsloket: locaties en openingstijden</Paragraph>
-        </div>
-      </UnorderedList.Item>,
-      <UnorderedList.Item key={2}>
-        <div className="amsterdam-docs-card">
-          <Icon svg={CarIcon} size="level-6" />
-          <Paragraph>Parkeren + Reizen (P+R)</Paragraph>
-        </div>
-      </UnorderedList.Item>,
-      <UnorderedList.Item key={3}>
-        <div className="amsterdam-docs-card">
-          <Icon svg={PassportIcon} size="level-6" />
-          <Paragraph>Paspoort, ID-kaart en Rijbewijs</Paragraph>
-        </div>
-      </UnorderedList.Item>,
-      <UnorderedList.Item key={4}>
-        <div className="amsterdam-docs-card">
-          <Icon svg={DocumentEuroSignIcon} size="level-6" />
-          <Paragraph>Gemeentebelastingen</Paragraph>
-        </div>
-      </UnorderedList.Item>,
-      <UnorderedList.Item key={5}>
-        <div className="amsterdam-docs-card">
-          <Icon svg={AlertIcon} size="level-6" />
-          <Paragraph>Melding openbare ruimte en overlast</Paragraph>
-        </div>
-      </UnorderedList.Item>,
-      <UnorderedList.Item key={6}>
-        <div className="amsterdam-docs-card">
-          <Icon svg={HousingIcon} size="level-6" />
-          <Paragraph>Verhuizing doorgeven</Paragraph>
-        </div>
-      </UnorderedList.Item>,
-      <UnorderedList.Item key={7}>
-        <div className="amsterdam-docs-card">
-          <Icon svg={TrashBinIcon} size="level-6" />
-          <Paragraph>Grof afval</Paragraph>
-        </div>
-      </UnorderedList.Item>,
-      <UnorderedList.Item key={8}>
-        <div className="amsterdam-docs-card">
-          <Icon svg={AnnouncementIcon} size="level-6" />
-          <Paragraph>Kennisgevingen en bekendmakingen</Paragraph>
-        </div>
-      </UnorderedList.Item>,
-    ],
-    markers: false,
-  },
-}
-
-export const InverseColor: Story = {
-  args: {
-    inverseColor: true,
   },
   decorators: [
     (Story, context) => (
@@ -132,4 +39,117 @@ export const InverseColor: Story = {
       </div>
     ),
   ],
+} satisfies Meta<typeof UnorderedList>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  render: (args) => <UnorderedList {...args}>{unorderedListItems}</UnorderedList>,
+}
+
+export const TwoLevels: Story = {
+  render: (args) => (
+    <UnorderedList {...args}>
+      <UnorderedList.Item key={1}>
+        Stadsdeel West
+        <UnorderedList {...args}>
+          <UnorderedList.Item key={1.1}>Bos en Lommer</UnorderedList.Item>
+          <UnorderedList.Item key={1.2}>Oud West / De Baarsjes</UnorderedList.Item>
+          <UnorderedList.Item key={1.3}>Westerpark</UnorderedList.Item>
+        </UnorderedList>
+      </UnorderedList.Item>
+      <UnorderedList.Item key={2}>
+        Stadsdeel Nieuw-West
+        <UnorderedList {...args}>
+          <UnorderedList.Item key={2.1}>De Aker, Sloten en Nieuw Sloten</UnorderedList.Item>
+          <UnorderedList.Item key={2.2}>Geuzenveld, Slotermeer en Sloterdijken</UnorderedList.Item>
+          <UnorderedList.Item key={2.3}>Osdorp</UnorderedList.Item>
+          <UnorderedList.Item key={2.4}>Slotervaart</UnorderedList.Item>
+        </UnorderedList>
+      </UnorderedList.Item>
+    </UnorderedList>
+  ),
+}
+
+export const WithoutMarkers: Story = {
+  args: {
+    markers: false,
+  },
+  argTypes: {
+    inverseColor: {
+      table: { disable: true },
+    },
+  },
+  render: (args) => (
+    <UnorderedList {...args}>
+      <UnorderedList.Item key={1}>
+        <div className="amsterdam-docs-card">
+          <Icon svg={LocationIcon} size="level-6" />
+          <Paragraph>Stadsloket: locaties en openingstijden</Paragraph>
+        </div>
+      </UnorderedList.Item>
+      <UnorderedList.Item key={2}>
+        <div className="amsterdam-docs-card">
+          <Icon svg={CarIcon} size="level-6" />
+          <Paragraph>Parkeren + Reizen (P+R)</Paragraph>
+        </div>
+      </UnorderedList.Item>
+      <UnorderedList.Item key={3}>
+        <div className="amsterdam-docs-card">
+          <Icon svg={PassportIcon} size="level-6" />
+          <Paragraph>Paspoort, ID-kaart en Rijbewijs</Paragraph>
+        </div>
+      </UnorderedList.Item>
+      <UnorderedList.Item key={4}>
+        <div className="amsterdam-docs-card">
+          <Icon svg={DocumentEuroSignIcon} size="level-6" />
+          <Paragraph>Gemeentebelastingen</Paragraph>
+        </div>
+      </UnorderedList.Item>
+      <UnorderedList.Item key={5}>
+        <div className="amsterdam-docs-card">
+          <Icon svg={AlertIcon} size="level-6" />
+          <Paragraph>Melding openbare ruimte en overlast</Paragraph>
+        </div>
+      </UnorderedList.Item>
+      <UnorderedList.Item key={6}>
+        <div className="amsterdam-docs-card">
+          <Icon svg={HousingIcon} size="level-6" />
+          <Paragraph>Verhuizing doorgeven</Paragraph>
+        </div>
+      </UnorderedList.Item>
+      <UnorderedList.Item key={7}>
+        <div className="amsterdam-docs-card">
+          <Icon svg={TrashBinIcon} size="level-6" />
+          <Paragraph>Grof afval</Paragraph>
+        </div>
+      </UnorderedList.Item>
+      <UnorderedList.Item key={8}>
+        <div className="amsterdam-docs-card">
+          <Icon svg={AnnouncementIcon} size="level-6" />
+          <Paragraph>Kennisgevingen en bekendmakingen</Paragraph>
+        </div>
+      </UnorderedList.Item>
+    </UnorderedList>
+  ),
+}
+
+export const InverseColor: Story = {
+  args: {
+    inverseColor: true,
+  },
+  render: (args) => (
+    <UnorderedList {...args}>
+      <UnorderedList.Item key={1}>
+        Stadsdeel West
+        <UnorderedList {...args}>
+          <UnorderedList.Item key={1.1}>Bos en Lommer</UnorderedList.Item>
+          <UnorderedList.Item key={1.2}>Oud West / De Baarsjes</UnorderedList.Item>
+          <UnorderedList.Item key={1.3}>Westerpark</UnorderedList.Item>
+        </UnorderedList>
+      </UnorderedList.Item>
+    </UnorderedList>
+  ),
 }
