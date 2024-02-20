cb524d9abb270e0a0c0e21dc106929aee1598967
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-02-20 16:05:39 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@ca75938e4e09aa59cb7dde5228f44edd2d149ec5 🚀

---
+++ demo-list-stories-args/storybook-react/assets/Button.stories-C4t5LIq2.js
@@ -0,0 +1,26 @@
+import{j as h}from"./jsx-runtime-CKrituN3.js";import{e as S,f as b,S as v}from"./index.esm-B56C_5T6.js";const T={title:"Buttons/Button",component:S,args:{children:"Tekst",variant:"primary",disabled:!1,busy:!1},argTypes:{children:{table:{disable:!1}},disabled:{control:"boolean"},variant:{control:"select",options:["primary","secondary","tertiary"]}}},r={args:{children:"Primary"}},e={args:{children:"Secondary",variant:"secondary"}},a={args:{children:"Tertiary",variant:"tertiary"}},s={args:{children:["Primary",h.jsx(b,{svg:v,size:"level-5"},"icon")]},argTypes:{children:{table:{disable:!0}}}};var n,t,o;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    children: 'Primary'
+  }
+}`,...(o=(t=r.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};var c,i,d;e.parameters={...e.parameters,docs:{...(c=e.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    children: 'Secondary',
+    variant: 'secondary'
+  }
+}`,...(d=(i=e.parameters)==null?void 0:i.docs)==null?void 0:d.source}}};var l,m,p;a.parameters={...a.parameters,docs:{...(l=a.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    children: 'Tertiary',
+    variant: 'tertiary'
+  }
+}`,...(p=(m=a.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var y,u,g;s.parameters={...s.parameters,docs:{...(y=s.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    children: ['Primary', <Icon key="icon" svg={ShareIcon} size="level-5" />]
+  },
+  argTypes: {
+    children: {
+      table: {
+        disable: true
+      }
+    }
+  }
+}`,...(g=(u=s.parameters)==null?void 0:u.docs)==null?void 0:g.source}}};const f=["Primary","Secondary","Tertiary","ButtonWithAnIcon"],_=Object.freeze(Object.defineProperty({__proto__:null,ButtonWithAnIcon:s,Primary:r,Secondary:e,Tertiary:a,__namedExportsOrder:f,default:T},Symbol.toStringTag,{value:"Module"}));export{_ as B,r as P,e as S,a as T,s as a};
