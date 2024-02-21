70702f3db6128e49644ca544cc05c255fcd99d58
dlnr <niels@dlnr.dev>
2024-02-21 10:43:30 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@06b60dc974ba445d48f38f1db2b15f77701e9ae1 🚀

---
+++ demo-DES-589-logo-museum-weesp/storybook-react/assets/VisuallyHidden.stories-DUaA7u-3.js
@@ -0,0 +1,14 @@
+import{j as a}from"./jsx-runtime-CKrituN3.js";import{V as i,P as r}from"./index.esm-B3Fw9ay8.js";const l={title:"Containers/Visually Hidden",component:i,args:{children:"Nieuw component"}},e={args:{children:"This text can be read out by a screen reader."},decorators:[o=>a.jsxs("div",{children:[a.jsx(r,{children:"This text is available to all users. The text below this sentence is only available for non-visual user agents."}),a.jsx(r,{children:a.jsx(o,{})})]})]};var s,t,n;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    children: 'This text can be read out by a screen reader.'
+  },
+  decorators: [Story => <div>
+        <Paragraph>
+          This text is available to all users. The text below this sentence is only available for non-visual user
+          agents.
+        </Paragraph>
+        <Paragraph>
+          <Story />
+        </Paragraph>
+      </div>]
+}`,...(n=(t=e.parameters)==null?void 0:t.docs)==null?void 0:n.source}}};const d=["Default"],h=Object.freeze(Object.defineProperty({__proto__:null,Default:e,__namedExportsOrder:d,default:l},Symbol.toStringTag,{value:"Module"}));export{h as V};
--- demo-DES-589-logo-museum-weesp/storybook-react/assets/preview-DUgW8UrG.js
+++
@@ -1,7 +0,0 @@
-import{_ as e}from"./iframe-_68nqIzh.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-CkHVgMrr.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9]),import.meta.url);return new r}}};export{a as parameters};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-CkHVgMrr.js","./iframe-_68nqIzh.js","./index-CBqU2yxZ.js","./_commonjsHelpers-BosuxZz1.js","./react-18-D8cruF67.js","./index-BtM5VmRH.js","./index-DYbuS03E.js","./index-Bw8VTzHM.js","./index-Cmc67Rxs.js","./index-DrFu-skq.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
