e1b639e6d870f9a57559f99be0417ec83f1a2e94
Vincent Smedinga <v.smedinga@amsterdam.nl>
2024-02-21 14:48:02 +0100

    Sort the export as well

--- packages/react/src/Logo/brand/index.ts
+++ packages/react/src/Logo/brand/index.ts
@@ -1,6 +1,6 @@
 export { default as LogoAmsterdam } from './LogoAmsterdam'
 export { default as LogoGgdAmsterdam } from './LogoGgdAmsterdam'
+export { default as LogoMuseumWeesp } from './LogoMuseumWeesp'
 export { default as LogoStadsarchief } from './LogoStadsarchief'
 export { default as LogoStadsbankVanLening } from './LogoStadsbankVanLening'
 export { default as LogoVgaVerzekeringen } from './LogoVgaVerzekeringen'
-export { default as LogoMuseumWeesp } from './LogoMuseumWeesp'
