a1309ad8daaab4f2448fe4dfa594b7c080be868f
dlnr <niels@dlnr.dev>
2024-02-21 09:54:50 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@412dd10193eeaedc194f657874af7973aaa3fce9 🚀

---
+++ demo-DES-608-storybook-themes/storybook-react/assets/Header.docs-D8-uC7NQ.js
@@ -0,0 +1,31 @@
+import{j as n}from"./jsx-runtime-CKrituN3.js";import{M as o,d as r,P as h,f as e}from"./index-DbHiN__L.js";import{H as m,W as d,a as l,b as u,c,d as p,e as j,f as x}from"./Header.stories-BvyI3BpT.js";import{useMDXComponents as a}from"./index-DI5IigMn.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-f_T8RDjZ.js";import"../sb-preview/runtime.js";import"./index-BtM5VmRH.js";import"./index-Bw8VTzHM.js";import"./index-Cmc67Rxs.js";import"./index-DrFu-skq.js";import"./index.esm-B56C_5T6.js";const g=`# Header
+
+Arranges the City’s logo, the title of the website, and a page menu.
+
+## Guidelines
+
+- Use the Header when the site is hosted on amsterdam.nl or one of its subdomains.
+- The page menu can contain a maximum of 5 items, including menu and search.
+- When you have a long subsite title, use no or as few page menu items as possible.
+
+## References
+
+- A Header is a [landmark](https://www.w3.org/TR/wai-aria-practices-1.1/#aria_landmark_roles) and can be use to group navigation elements.
+- [WCAG 3.2.3](https://wcag.com/designers/3-2-3-consistent-navigation/) Consistent Navigation: Navigation menus that appear on multiple pages are consistent.
+`;function s(i){const t=Object.assign({h2:"h2"},a(),i.components);return n.jsxs(n.Fragment,{children:[n.jsx(o,{of:m}),`
+`,n.jsx(r,{children:g}),`
+`,n.jsx(h,{}),`
+`,n.jsx(t.h2,{id:"with-logo-variant",children:"With logo variant"}),`
+`,n.jsx(e,{of:d}),`
+`,n.jsx(t.h2,{id:"with-subsite-title",children:"With subsite title"}),`
+`,n.jsx(e,{of:l}),`
+`,n.jsx(t.h2,{id:"with-menu",children:"With menu"}),`
+`,n.jsx(e,{of:u}),`
+`,n.jsx(t.h2,{id:"with-links",children:"With links"}),`
+`,n.jsx(e,{of:c}),`
+`,n.jsx(t.h2,{id:"with-links-and-menu",children:"With links and menu"}),`
+`,n.jsx(e,{of:p}),`
+`,n.jsx(t.h2,{id:"with-a-title-and-menu",children:"With a title and menu"}),`
+`,n.jsx(e,{of:j}),`
+`,n.jsx(t.h2,{id:"with-a-title-links-and-menu",children:"With a title, links and menu"}),`
+`,n.jsx(e,{of:x})]})}function D(i={}){const{wrapper:t}=Object.assign({},a(),i.components);return t?n.jsx(t,Object.assign({},i,{children:n.jsx(s,i)})):s(i)}export{D as default};
