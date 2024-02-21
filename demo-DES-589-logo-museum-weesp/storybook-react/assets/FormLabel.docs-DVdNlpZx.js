5efec4c017c23e0fa9ebcbc4b97b9548d2d5f2cc
dlnr <niels@dlnr.dev>
2024-02-21 10:36:16 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@42829ba3c6c12515d498435caa237dc24792ed59 🚀

---
+++ demo-DES-589-logo-museum-weesp/storybook-react/assets/FormLabel.docs-DVdNlpZx.js
@@ -0,0 +1,11 @@
+import{j as t}from"./jsx-runtime-CKrituN3.js";import{M as e,d as s,P as a,e as i}from"./index-DYbuS03E.js";import{F as m}from"./FormLabel.stories-bioTskte.js";import{useMDXComponents as p}from"./index-DI5IigMn.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-_68nqIzh.js";import"../sb-preview/runtime.js";import"./index-BtM5VmRH.js";import"./index-Bw8VTzHM.js";import"./index-Cmc67Rxs.js";import"./index-DrFu-skq.js";import"./index.esm-BlW9Fa3t.js";const c=`# Form Label
+
+Describes a form control.
+
+## Guidelines
+
+Always associate a form element (such as an \`input\` or \`textarea\`) with a label.
+`;function n(o){return t.jsxs(t.Fragment,{children:[t.jsx(e,{of:m}),`
+`,t.jsx(s,{children:c}),`
+`,t.jsx(a,{}),`
+`,t.jsx(i,{})]})}function g(o={}){const{wrapper:r}=Object.assign({},p(),o.components);return r?t.jsx(r,Object.assign({},o,{children:t.jsx(n,o)})):n()}export{g as default};
