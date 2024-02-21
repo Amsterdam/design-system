d1eaed2ee7d15776a0ab7b546f559957072927d7
dlnr <niels@dlnr.dev>
2024-02-21 09:50:50 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@358639c3640b7c888fd8c15493e1887abeb29678 🚀

---
+++ demo-DES-608-storybook-themes/storybook-react/assets/VisuallyHidden.docs-BEveBk3b.js
@@ -0,0 +1,10 @@
+import{j as s}from"./jsx-runtime-CKrituN3.js";import{M as o,d as r,P as a,e as i}from"./index-DBYCIfgf.js";import{V as m}from"./VisuallyHidden.stories-NSbtrCeK.js";import{useMDXComponents as c}from"./index-DI5IigMn.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-DeAZoLDb.js";import"../sb-preview/runtime.js";import"./index-BtM5VmRH.js";import"./index-Bw8VTzHM.js";import"./index-Cmc67Rxs.js";import"./index-DrFu-skq.js";import"./index.esm-B56C_5T6.js";const u=`# Visually Hidden
+
+Hides content from sighted users but keeps it accessible to non-visual user agents, such as screen readers.
+
+Note: in most cases, visually available content should be accessible to non-visual user agents and vice versa.
+Elaborate instructions or guidance read only by non-visual user agents can do more harm than good.
+`;function t(n){return s.jsxs(s.Fragment,{children:[s.jsx(o,{of:m}),`
+`,s.jsx(r,{children:u}),`
+`,s.jsx(a,{}),`
+`,s.jsx(i,{})]})}function D(n={}){const{wrapper:e}=Object.assign({},c(),n.components);return e?s.jsx(e,Object.assign({},n,{children:s.jsx(t,n)})):t()}export{D as default};
