24e5f928fd98aa80ed246025a3e353bc2bff9f20
alimpens <37216945+alimpens@users.noreply.github.com>
2024-02-22 15:21:50 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@92f9af3295332025ba78f106b5c9a95519e1acf6 🚀

---
+++ demo-DES-621-remove-storybook-composition/assets/PageHeading.stories-D6CTc0Fp.js
@@ -0,0 +1,20 @@
+import{j as o}from"./jsx-runtime-CKrituN3.js";import{E as g}from"./index.esm-CCYNpFT6.js";const m={title:"React Components/Text/Page Heading",component:g,args:{children:"Gemeente Amsterdam",inverseColor:!1},argTypes:{children:{table:{disable:!1}},inverseColor:{control:"boolean"}}},e={decorators:[(n,a)=>o.jsx("div",{style:{background:a.args.inverseColor?"#004699":"transparent",margin:a.args.inverseColor?-16:0,padding:a.args.inverseColor?16:0},children:o.jsx(n,{})})]},r={args:{inverseColor:!0},decorators:[n=>o.jsx("div",{style:{background:"#004699",margin:-16,padding:16},children:o.jsx(n,{})})]};var s,t,d;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  decorators: [(Story, context) => <div style={{
+    background: context.args.inverseColor ? '#004699' : 'transparent',
+    margin: context.args.inverseColor ? -16 : 0,
+    padding: context.args.inverseColor ? 16 : 0
+  }}>
+        <Story />
+      </div>]
+}`,...(d=(t=e.parameters)==null?void 0:t.docs)==null?void 0:d.source}}};var i,l,c;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    inverseColor: true
+  },
+  decorators: [Story => <div style={{
+    background: '#004699',
+    margin: -16,
+    padding: 16
+  }}>
+        <Story />
+      </div>]
+}`,...(c=(l=r.parameters)==null?void 0:l.docs)==null?void 0:c.source}}};const p=["Default","InvertedColor"],C=Object.freeze(Object.defineProperty({__proto__:null,Default:e,InvertedColor:r,__namedExportsOrder:p,default:m},Symbol.toStringTag,{value:"Module"}));export{r as I,C as P};
