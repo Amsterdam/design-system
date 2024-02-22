b5059c28b04f03e1f75a169314edefe3ff993c55
dlnr <niels@dlnr.dev>
2024-02-22 12:51:50 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@b4378c44b2e57840b63efd670ca4de5ff87cedbd 🚀

---
+++ demo-DES-607-Tabs/storybook-react/assets/Icon.stories-End-YyQY.js
@@ -0,0 +1,41 @@
+import{j as l}from"./jsx-runtime-CKrituN3.js";import{f as T,p as i,j as e,H as D}from"./index.esm-hgZUMT7J.js";const M={title:"Media/Icon",component:T,argTypes:{size:{control:{type:"radio"},options:["level-3","level-4","level-5","level-6"]},square:{control:{type:"boolean"}},svg:{control:{type:"select"},options:Object.keys(i),mapping:i}}},s={args:{svg:e}},r={render:W=>l.jsxs("span",{style:{display:"flex",gap:"1rem"},children:[l.jsx(T,{...W,size:"level-3"}),l.jsx(D,{size:"level-3",children:"Inline text"})]}),args:{svg:e}},a={args:{svg:e,square:!0}},o={args:{svg:e,size:"level-3"}},n={args:{svg:e,size:"level-4"}},c={args:{svg:e,size:"level-5"}},t={args:{svg:e,size:"level-6"}};var p,g,m;s.parameters={...s.parameters,docs:{...(p=s.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    svg: Icons.EmailIcon
+  }
+}`,...(m=(g=s.parameters)==null?void 0:g.docs)==null?void 0:m.source}}};var v,d,u;r.parameters={...r.parameters,docs:{...(v=r.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  render: args => <span style={{
+    display: 'flex',
+    gap: '1rem'
+  }}>
+      <Icon {...args} size="level-3" />
+      <Heading size="level-3">Inline text</Heading>
+    </span>,
+  args: {
+    svg: Icons.EmailIcon
+  }
+}`,...(u=(d=r.parameters)==null?void 0:d.docs)==null?void 0:u.source}}};var I,z,S;a.parameters={...a.parameters,docs:{...(I=a.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  args: {
+    svg: Icons.EmailIcon,
+    square: true
+  }
+}`,...(S=(z=a.parameters)==null?void 0:z.docs)==null?void 0:S.source}}};var x,y,f;o.parameters={...o.parameters,docs:{...(x=o.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  args: {
+    svg: Icons.EmailIcon,
+    size: 'level-3'
+  }
+}`,...(f=(y=o.parameters)==null?void 0:y.docs)==null?void 0:f.source}}};var E,j,L;n.parameters={...n.parameters,docs:{...(E=n.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  args: {
+    svg: Icons.EmailIcon,
+    size: 'level-4'
+  }
+}`,...(L=(j=n.parameters)==null?void 0:j.docs)==null?void 0:L.source}}};var b,_,q;c.parameters={...c.parameters,docs:{...(b=c.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    svg: Icons.EmailIcon,
+    size: 'level-5'
+  }
+}`,...(q=(_=c.parameters)==null?void 0:_.docs)==null?void 0:q.source}}};var h,H,O;t.parameters={...t.parameters,docs:{...(h=t.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    svg: Icons.EmailIcon,
+    size: 'level-6'
+  }
+}`,...(O=(H=t.parameters)==null?void 0:H.docs)==null?void 0:O.source}}};const k=["Default","WithText","Square","Level3","Level4","Level5","Level6"],w=Object.freeze(Object.defineProperty({__proto__:null,Default:s,Level3:o,Level4:n,Level5:c,Level6:t,Square:a,WithText:r,__namedExportsOrder:k,default:M},Symbol.toStringTag,{value:"Module"}));export{w as I,o as L,a as S,r as W,n as a,c as b,t as c};
