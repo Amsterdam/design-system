617734a4296c33d345d416d5fefd62e98e2d03c7
alimpens <37216945+alimpens@users.noreply.github.com>
2024-02-23 15:45:45 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@6af555a4db055e4d46143189644ac3ca6b9dc918 🚀

---
+++ demo-DES-581-update-copyright-notices/assets/preview-ClAxGyOX.js
@@ -0,0 +1,7 @@
+import{_ as e}from"./iframe-DJkLF6k-.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-CM--_U0V.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9]),import.meta.url);return new r}}};export{a as parameters};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-CM--_U0V.js","./iframe-DJkLF6k-.js","./index-CBqU2yxZ.js","./_commonjsHelpers-BosuxZz1.js","./react-18-D8cruF67.js","./index-BtM5VmRH.js","./index-Cm66-rO9.js","./index-Bw8VTzHM.js","./index-Cmc67Rxs.js","./index-DrFu-skq.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
