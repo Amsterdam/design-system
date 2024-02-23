eb5b9eb0ebc3f680e847cc1caef907f9045ad75b
alimpens <37216945+alimpens@users.noreply.github.com>
2024-02-23 12:33:45 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@f0a5aeedca0b72f2dea186685880f2cd914b5705 🚀

---
+++ demo-DES-621-remove-storybook-composition/assets/Pagination.docs-CDmmlXdd.js
@@ -0,0 +1,23 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{M as a,e as o,P as s,f as r}from"./index-CdAUCd2f.js";import{P as l}from"./Pagination.stories-8zihUkqP.js";import{useMDXComponents as p}from"./index-DI5IigMn.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-C4ME1xwO.js";import"../sb-preview/runtime.js";import"./index-BtM5VmRH.js";import"./index-Bw8VTzHM.js";import"./index-Cmc67Rxs.js";import"./index-DrFu-skq.js";import"./index.esm-CCYNpFT6.js";const g=`# Pagination
+
+Pagination is a navigation element used to navigate between multiple pages of overview lists.
+This is commonly used for search results or a list of news articles.
+In cases where overview lists are too long to display on a single page, pagination can provide a clearer and more functional way to divide the content across multiple pages.
+Pagination indicates the current page and allows users to navigate to other pages.
+
+## Guidelines
+
+- Use pagination only when an overview list is too long to display on a single page.
+- Place pagination below the overview list.
+- Pagination should not be displayed if there is only one page.
+- Redirect users to the first page if they enter a URL with a page number that doesn’t exist.
+- Pagination can be combined with a counter at the top of the page indicating “Page # of ##.”
+- Start a page with an overview list at the top after changing the page.
+
+## Relevant WCAG Rules
+
+Pagination is an interactive element, and the [general requirements and guidelines for interactive elements](/docs/docs-design-guidelines-interactive-elements--docs) apply.
+`;function i(n){return e.jsxs(e.Fragment,{children:[e.jsx(a,{of:l}),`
+`,e.jsx(o,{children:g}),`
+`,e.jsx(s,{}),`
+`,e.jsx(r,{})]})}function M(n={}){const{wrapper:t}=Object.assign({},p(),n.components);return t?e.jsx(t,Object.assign({},n,{children:e.jsx(i,n)})):i()}export{M as default};
