30416e7d474d5190978bdc0ee5e0a819b2786e27
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-02-23 11:44:44 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@849cbf73f9ca731ffab9f4f4f33af6b98787f0ee 🚀

---
+++ demo-DES-589-logo-museum-weesp/storybook-react/assets/preview-Dya1YKvZ.js
@@ -0,0 +1,7 @@
+import{_ as e}from"./iframe-C9O22Wx7.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-DUzwm3uc.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9]),import.meta.url);return new r}}};export{a as parameters};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-DUzwm3uc.js","./iframe-C9O22Wx7.js","./index-CBqU2yxZ.js","./_commonjsHelpers-BosuxZz1.js","./react-18-D8cruF67.js","./index-BtM5VmRH.js","./index-DmkVj5bj.js","./index-Bw8VTzHM.js","./index-Cmc67Rxs.js","./index-DrFu-skq.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
