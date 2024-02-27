afb04aeede1b86e2423deb95163a50a1f1d52b8f
alimpens <37216945+alimpens@users.noreply.github.com>
2024-02-27 16:07:14 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@3bdac542e3dd95283579bf3e9c9b55730b59508f 🚀

--- demo-DES-607-Tabs/assets/preview-CYHCrIVS.js
+++
@@ -1,7 +0,0 @@
-import{_ as e}from"./iframe-to317bdz.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-DiWOxXa1.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9]),import.meta.url);return new r}}};export{a as parameters};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-DiWOxXa1.js","./iframe-to317bdz.js","./index-CBqU2yxZ.js","./_commonjsHelpers-BosuxZz1.js","./react-18-D8cruF67.js","./index-BtM5VmRH.js","./index-CyHgJFnr.js","./index-Bw8VTzHM.js","./index-Cmc67Rxs.js","./index-DrFu-skq.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
---
+++ demo-DES-607-Tabs/assets/preview-zFKhTbq_.js
@@ -0,0 +1,7 @@
+import{_ as e}from"./iframe-D5jm7xxe.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-ioLuOZdB.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9]),import.meta.url);return new r}}};export{a as parameters};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-ioLuOZdB.js","./iframe-D5jm7xxe.js","./index-CBqU2yxZ.js","./_commonjsHelpers-BosuxZz1.js","./react-18-D8cruF67.js","./index-BtM5VmRH.js","./index-wtMO9HlK.js","./index-Bw8VTzHM.js","./index-Cmc67Rxs.js","./index-DrFu-skq.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
