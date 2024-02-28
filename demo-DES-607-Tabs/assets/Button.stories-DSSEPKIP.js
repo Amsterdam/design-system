a7c363f3105f43cddbf247712902ef8c9d11ebeb
alimpens <37216945+alimpens@users.noreply.github.com>
2024-02-28 10:54:36 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@281613e12a79180e572954d995a27e95d802a095 🚀

---
+++ demo-DES-607-Tabs/assets/Button.stories-DSSEPKIP.js
@@ -0,0 +1,20 @@
+import{j as b}from"./jsx-runtime-CKrituN3.js";import{e as S,f as v,S as h}from"./index.esm-C-uAFSFy.js";const B={title:"Components/Buttons/Button",component:S,args:{children:"Button label",variant:"primary",disabled:!1,busy:!1},argTypes:{children:{table:{disable:!1}},disabled:{control:"boolean"},variant:{control:"select",options:["primary","secondary","tertiary"]}}},r={},e={args:{variant:"secondary"}},a={args:{variant:"tertiary"}},s={args:{children:["Button label",b.jsx(v,{svg:h,size:"level-5"},"icon")]},argTypes:{children:{table:{disable:!0}}}};var t,o,n;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:"{}",...(n=(o=r.parameters)==null?void 0:o.docs)==null?void 0:n.source}}};var c,i,l;e.parameters={...e.parameters,docs:{...(c=e.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    variant: 'secondary'
+  }
+}`,...(l=(i=e.parameters)==null?void 0:i.docs)==null?void 0:l.source}}};var d,p,m;a.parameters={...a.parameters,docs:{...(d=a.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    variant: 'tertiary'
+  }
+}`,...(m=(p=a.parameters)==null?void 0:p.docs)==null?void 0:m.source}}};var u,y,g;s.parameters={...s.parameters,docs:{...(u=s.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    children: ['Button label', <Icon key="icon" svg={ShareIcon} size="level-5" />]
+  },
+  argTypes: {
+    children: {
+      table: {
+        disable: true
+      }
+    }
+  }
+}`,...(g=(y=s.parameters)==null?void 0:y.docs)==null?void 0:g.source}}};const f=["Primary","Secondary","Tertiary","ButtonWithAnIcon"],j=Object.freeze(Object.defineProperty({__proto__:null,ButtonWithAnIcon:s,Primary:r,Secondary:e,Tertiary:a,__namedExportsOrder:f,default:B},Symbol.toStringTag,{value:"Module"}));export{j as B,r as P,e as S,a as T,s as a};
