4eec887c579f478b552126e684aba28d037bf9cb
Niels Roozemond <n.roozemond@amsterdam.nl>
2024-02-29 12:05:01 +0100

    feat: Add Tabs component (#1068)
    Co-authored-by: Vincent Smedinga <v.smedinga@amsterdam.nl>
    Co-authored-by: Aram Limpens <a.limpens@amsterdam.nl>
    Co-authored-by: Aram <37216945+alimpens@users.noreply.github.com>

---
+++ packages/react/src/Tabs/TabsButton.tsx
@@ -0,0 +1,41 @@
+/**
+ * @license EUPL-1.2+
+ * Copyright Gemeente Amsterdam
+ */
+
+import clsx from 'clsx'
+import { forwardRef, startTransition, useContext } from 'react'
+import type { ButtonHTMLAttributes, ForwardedRef, PropsWithChildren } from 'react'
+import { TabsContext } from './TabsContext'
+
+export type TabsButtonProps = {
+  tab: number
+} & PropsWithChildren<ButtonHTMLAttributes<HTMLButtonElement>>
+
+export const TabsButton = forwardRef(
+  ({ children, className, tab = 0, ...restProps }: TabsButtonProps, ref: ForwardedRef<HTMLButtonElement>) => {
+    const { activeTab, updateTab, tabsId } = useContext(TabsContext)
+
+    return (
+      <button
+        {...restProps}
+        role="tab"
+        id={`${tabsId}-tab-${tab}`}
+        aria-controls={`${tabsId}-panel-${tab}`}
+        aria-selected={activeTab === tab}
+        tabIndex={activeTab === tab ? 0 : -1}
+        ref={ref}
+        onClick={() => {
+          startTransition(() => {
+            updateTab(tab)
+          })
+        }}
+        className={clsx('amsterdam-tabs__button', className)}
+      >
+        {children}
+      </button>
+    )
+  },
+)
+
+TabsButton.displayName = 'Tabs.Button'
