21533bc87f5875bfcb055ec6ba3b4b8c35d3a5f6
alimpens <37216945+alimpens@users.noreply.github.com>
2024-03-01 14:06:06 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@681b8f42df6d8a055958e4b7c475f05ddc3508e3 🚀

---
+++ demo-remove-unused-class/assets/Checkbox.stories-BPNFCyQM.js
@@ -0,0 +1,8 @@
+import{g as l}from"./index.esm-CT4Vvfhk.js";/**
+ * @license EUPL-1.2+
+ * Copyright Gemeente Amsterdam
+ */const c={title:"Components/Forms/Checkbox",component:l,args:{children:"Checkbox label",disabled:!1,indeterminate:!1,invalid:!1},argTypes:{children:{table:{disable:!1}},onChange:{action:"clicked"}}},e={},r={args:{children:"Ik heb alle gegevens correct en volledig ingevuld. Ik begrijp dat een verhuizing via internet dezelfde juridische status heeft als een verhuizing met een geschreven geldige handtekening."}};var n,a,t;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:"{}",...(t=(a=e.parameters)==null?void 0:a.docs)==null?void 0:t.source}}};var s,i,o;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    children: 'Ik heb alle gegevens correct en volledig ingevuld. Ik begrijp dat een verhuizing via internet dezelfde juridische status heeft als een verhuizing met een geschreven geldige handtekening.'
+  }
+}`,...(o=(i=r.parameters)==null?void 0:i.docs)==null?void 0:o.source}}};const d=["Default","LongLabel"],h=Object.freeze(Object.defineProperty({__proto__:null,Default:e,LongLabel:r,__namedExportsOrder:d,default:c},Symbol.toStringTag,{value:"Module"}));export{h as C,r as L};
