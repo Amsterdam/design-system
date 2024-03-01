b91b2a3018d23aaa6348744a7c6351c11fe0925b
Aram Limpens <a.limpens@amsterdam.nl>
2024-03-01 14:02:28 +0100

    Update stories, add docs

--- storybook/src/components/Radio/Radio.stories.tsx
+++ storybook/src/components/Radio/Radio.stories.tsx
@@ -3,12 +3,12 @@
  * Copyright Gemeente Amsterdam
  */
 
-import { Radio } from '@amsterdam/design-system-react'
+import { Fieldset, Radio } from '@amsterdam/design-system-react'
 import { useArgs } from '@storybook/preview-api'
 import { Meta, StoryObj } from '@storybook/react'
 
 const meta = {
-  title: 'Radio',
+  title: 'Components/Forms/Radio',
   component: Radio,
   args: {
     checked: false,
@@ -38,3 +38,37 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const Default: Story = {}
+
+export const RadioGroup: Story = {
+  argTypes: {
+    checked: {
+      table: { disable: true },
+    },
+    children: {
+      table: { disable: true },
+    },
+    disabled: {
+      table: { disable: true },
+    },
+    invalid: {
+      table: { disable: true },
+    },
+    onChange: { action: 'clicked', table: { disable: true } },
+  },
+  render: () => (
+    <Fieldset legend="Waar gaat uw melding over?">
+      <Radio name="soort" value="horecabedrijf">
+        Horecabedrijf
+      </Radio>
+      <Radio name="soort" value="ander_soort_bedrijf">
+        Ander soort bedrijf
+      </Radio>
+      <Radio name="soort" value="evenement">
+        Evenement
+      </Radio>
+      <Radio name="soort" value="iets_anders">
+        Iets anders
+      </Radio>
+    </Fieldset>
+  ),
+}
