cf7f80fcc5e9c0cba462177aa21bcf196b379bfe
alimpens <37216945+alimpens@users.noreply.github.com>
2024-03-06 08:56:44 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@dbdb5fd9a213271f32d2829624cefb33223dbd10 🚀

--- demo-clean-up-stories/assets/preview-eSNltmAy.js
+++
@@ -1,7 +0,0 @@
-import{_ as e}from"./iframe-C976AW2B.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-CXQMj4cd.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9]),import.meta.url);return new r}}};export{a as parameters};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-CXQMj4cd.js","./iframe-C976AW2B.js","./index-CBqU2yxZ.js","./_commonjsHelpers-BosuxZz1.js","./react-18-D8cruF67.js","./index-BtM5VmRH.js","./index-Bzxjtk5Q.js","./index-Bw8VTzHM.js","./index-Cmc67Rxs.js","./index-DrFu-skq.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
