21b622d39a895ac24b2f779c80309e81b615559a
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-03-07 13:30:24 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@99c2e587215327a58f5928b7888ee278392ae14b 🚀

---
+++ demo-DES-617-horizontal-stack-space/assets/Row.docs-BPXO-bq5.js
@@ -0,0 +1,17 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{M as r,d as a,P as i,e as c,f as d}from"./index-Cn4C1rPa.js";import{R as m,C as h}from"./Row.stories-B_pi65DY.js";import{useMDXComponents as t}from"./index-DI5IigMn.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-B_G8VA9U.js";import"../sb-preview/runtime.js";import"./index-BtM5VmRH.js";import"./index-Bw8VTzHM.js";import"./index-Cmc67Rxs.js";import"./index-DrFu-skq.js";import"./index.esm-CNBHPF_M.js";const p=`<!-- @license CC0-1.0 -->
+
+# Row
+`;function o(s){const n=Object.assign({h2:"h2",p:"p",a:"a",h3:"h3",code:"code"},t(),s.components);return e.jsxs(e.Fragment,{children:[e.jsx(r,{of:m}),`
+`,e.jsx(a,{children:p}),`
+`,e.jsx(n.h2,{id:"design",children:"Design"}),`
+`,e.jsxs(n.p,{children:["The five ",e.jsx(n.a,{href:"/docs/docs-design-guidelines-space--docs",children:"spacing"})," sizes are available for the size of the gap."]}),`
+`,e.jsx(n.h2,{id:"how-to-use",children:"How to Use"}),`
+`,e.jsx(n.p,{children:"Wrap a Row around a set of components that need the same amount of white space between them."}),`
+`,e.jsx(n.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(n.h3,{id:"default",children:"Default"}),`
+`,e.jsx(i,{}),`
+`,e.jsx(c,{}),`
+`,e.jsx(n.h3,{id:"use-another-html-element",children:"Use Another HTML Element"}),`
+`,e.jsxs(n.p,{children:["By default, a Row renders a ",e.jsx(n.code,{children:"<div>"}),`.
+Use the `,e.jsx(n.code,{children:"as"})," prop to make your markup more semantic."]}),`
+`,e.jsx(d,{of:h})]})}function v(s={}){const{wrapper:n}=Object.assign({},t(),s.components);return n?e.jsx(n,Object.assign({},s,{children:e.jsx(o,s)})):o(s)}export{v as default};
