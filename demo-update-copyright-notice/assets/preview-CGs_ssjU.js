eb11fb0fdff4e0f4c20c166d0fbe2d3fbd42526e
GitHub <github-action@users.noreply.github.com>
2024-03-07 12:41:06 +0000

    Remove feature branch folder

--- demo-update-copyright-notice/assets/preview-CGs_ssjU.js
+++
@@ -1,7 +0,0 @@
-import{_ as e}from"./iframe-CIXoGYcB.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-BM5I1je8.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9]),import.meta.url);return new r}}};export{a as parameters};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-BM5I1je8.js","./iframe-CIXoGYcB.js","./index-CBqU2yxZ.js","./_commonjsHelpers-BosuxZz1.js","./react-18-D8cruF67.js","./index-BtM5VmRH.js","./index-Czq9OLRA.js","./index-Bw8VTzHM.js","./index-Cmc67Rxs.js","./index-DrFu-skq.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
