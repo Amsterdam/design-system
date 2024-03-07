1a41a265cfb07d3f62674aa965fcde4fdefc98c3
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-03-07 10:44:10 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@7c68038db76786738332431179eb344771a01ac3 🚀

---
+++ demo-DES-613-group-grid-space-tokens/assets/syntaxhighlighter-B5GMVT5T-BLsntcmN.js
@@ -0,0 +1,1 @@
+import{S as l,c as n,s as d}from"./index-DKQg9j8a.js";import"./iframe-bDZc5IeN.js";import"../sb-preview/runtime.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-BtM5VmRH.js";import"./index-Bw8VTzHM.js";import"./index-Cmc67Rxs.js";import"./index-DrFu-skq.js";export{l as SyntaxHighlighter,n as createCopyToClipboardFunction,d as default};
