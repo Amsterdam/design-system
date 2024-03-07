79eadaf48e4df9b7a4643a17d6694e88a94bc57d
Aram Limpens <a.limpens@amsterdam.nl>
2024-03-07 17:27:48 +0100

    Use VisuallyHidden instead of aria-label for screen reader only text

--- packages/react/src/TopTaskLink/TopTaskLink.tsx
+++ packages/react/src/TopTaskLink/TopTaskLink.tsx
@@ -6,6 +6,7 @@
 import clsx from 'clsx'
 import { forwardRef } from 'react'
 import type { AnchorHTMLAttributes, ForwardedRef } from 'react'
+import { VisuallyHidden } from '../VisuallyHidden'
 
 export type TopTaskLinkProps = {
   label: string
@@ -14,14 +15,10 @@ export type TopTaskLinkProps = {
 
 export const TopTaskLink = forwardRef(
   ({ className, label, description, ...restProps }: TopTaskLinkProps, ref: ForwardedRef<HTMLAnchorElement>) => (
-    <a
-      {...restProps}
-      ref={ref}
-      className={clsx('amsterdam-top-task-link', className)}
-      // The comma makes screen readers add a slight pause between the label and the description.
-      aria-label={`${label}, ${description}`}
-    >
+    <a {...restProps} ref={ref} className={clsx('amsterdam-top-task-link', className)}>
       <span className="amsterdam-top-task-link__label">{label}</span>
+      {/* This comma makes screen readers add a slight pause between the label and the description. */}
+      <VisuallyHidden>,</VisuallyHidden>
       <span className="amsterdam-top-task-link__description">{description}</span>
     </a>
   ),
