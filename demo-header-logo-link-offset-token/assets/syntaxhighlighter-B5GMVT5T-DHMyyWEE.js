0f80a90fb61458b8e48b7ba5a8b3d8d55328063d
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-03-07 12:50:24 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@645bf0d2eb2a052785e9950da1e94e203153e716 🚀

---
+++ demo-header-logo-link-offset-token/assets/syntaxhighlighter-B5GMVT5T-DHMyyWEE.js
@@ -0,0 +1,1 @@
+import{S as l,c as n,s as d}from"./index-DRm7sR1Q.js";import"./iframe-h_Gz4uNE.js";import"../sb-preview/runtime.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-BtM5VmRH.js";import"./index-Bw8VTzHM.js";import"./index-Cmc67Rxs.js";import"./index-DrFu-skq.js";export{l as SyntaxHighlighter,n as createCopyToClipboardFunction,d as default};
