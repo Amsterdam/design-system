ee5b625ae42f5eaa09190f506d0fc7e158a8f9b9
Vincent Smedinga <v.smedinga@amsterdam.nl>
2024-03-08 11:26:01 +0100

    Remove semantically incorrect example

--- storybook/src/components/Row/Row.stories.tsx
+++ storybook/src/components/Row/Row.stories.tsx
@@ -3,7 +3,7 @@
  * Copyright Gemeente Amsterdam
  */
 
-import { Button, Card, Heading, Paragraph, Row } from '@amsterdam/design-system-react'
+import { Button, Row } from '@amsterdam/design-system-react'
 import { Meta, StoryObj } from '@storybook/react'
 
 const ThreeButtons = Array.from(Array(3).keys()).map((i) => <Button key={i}>Button {i + 1}</Button>)
@@ -31,36 +31,3 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const Default: Story = {}
-
-export const CustomTagName: Story = {
-  args: {
-    as: 'section',
-    children: [
-      <Card key={1}>
-        <Card.HeadingGroup tagline="Nieuws">
-          <Heading level={2} size="level-3">
-            <Card.Link href="#">Nieuwe manieren om afval op te halen</Card.Link>
-          </Heading>
-        </Card.HeadingGroup>
-        <Paragraph>
-          Afvalboten, bakfietsen en ondergrondse containers. We experimenteren met nieuwe manieren om afval op te halen
-          in het centrum.
-        </Paragraph>
-        <Paragraph size="small">Gepubliceerd: 1 juli 2023</Paragraph>
-      </Card>,
-      <Card key={2}>
-        <Card.HeadingGroup tagline="Nieuws">
-          <Heading level={2} size="level-3">
-            <Card.Link href="#">Verlenging proef ophalen afval per boot</Card.Link>
-          </Heading>
-        </Card.HeadingGroup>
-        <Paragraph>
-          Een proef met het anders ophalen van afval. We halen vuilniszakken hier op met kleine wagentjes, kleine
-          vuilniswagens en een afvalboot.
-        </Paragraph>
-        <Paragraph size="small">Gepubliceerd: 15 juni 2023</Paragraph>
-      </Card>,
-    ],
-    gap: 'extra-large',
-  },
-}
