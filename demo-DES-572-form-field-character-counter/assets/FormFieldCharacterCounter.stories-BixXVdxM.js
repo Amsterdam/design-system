befd9fc7c96290b6f90151e8f6e784efef1bbf95
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-03-12 15:54:36 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@e5f2fadf1f05828b48928d71c705816ca5c71ebf 🚀

--- demo-DES-572-form-field-character-counter/assets/FormFieldCharacterCounter.stories-BixXVdxM.js
+++
@@ -1,4 +0,0 @@
-import{n as a}from"./index.esm-DvPligYL.js";/**
- * @license EUPL-1.2+
- * Copyright Gemeente Amsterdam
- */const s={title:"Components/Forms/Form Field Character Counter",component:a,args:{label:"0 van 1000 tekens"}},e={};var r,o,t;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:"{}",...(t=(o=e.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};const n=["Default"],l=Object.freeze(Object.defineProperty({__proto__:null,Default:e,__namedExportsOrder:n,default:s},Symbol.toStringTag,{value:"Module"}));export{l as F};
---
+++ demo-DES-572-form-field-character-counter/assets/TopTaskLink.stories-BLg_MMl_.js
@@ -0,0 +1,33 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{a0 as r,G as n}from"./index.esm-DIbgvOcc.js";const c={title:"Components/Navigation/Top Task Link",component:r},a={args:{description:"Geef uw nieuwe adres door als u binnen Amsterdam of naar Amsterdam verhuist.",href:"#",label:"Verhuizing doorgeven"}},i={args:{description:"Omschrijving",label:"Titel"},render:()=>e.jsxs(n,{children:[e.jsx(n.Cell,{span:3,children:e.jsx(r,{description:"Bekijk welke belastingen en heffingen er zijn, hoe u bezwaar maakt of een betalingsregeling treft.",href:"#",label:"Gemeentebelastingen"})}),e.jsx(n.Cell,{span:3,children:e.jsx(r,{description:"U kunt met uw auto goedkoop parkeren bij een P+R-locatie aan de rand van de stad.",href:"#",label:"Parkeren + Reizen (P+R)"})}),e.jsx(n.Cell,{span:3,children:e.jsx(r,{description:"Vraag deze bewijzen aan of verleng ze. Geef een vermissing aan.",href:"#",label:"Paspoort, ID-kaart en rijbewijs"})}),e.jsx(n.Cell,{span:3,children:e.jsx(r,{description:"Meld een kind aan voor basisschool, middelbare school of kinderopvang. Bekijk het kindtegoed op uw Stadspas.",href:"#",label:"Onderwijs"})})]}),parameters:{docs:{source:{type:"dynamic"}}}};var s,o,t;a.parameters={...a.parameters,docs:{...(s=a.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    description: 'Geef uw nieuwe adres door als u binnen Amsterdam of naar Amsterdam verhuist.',
+    href: '#',
+    label: 'Verhuizing doorgeven'
+  }
+}`,...(t=(o=a.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};var l,d,p;i.parameters={...i.parameters,docs:{...(l=i.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    description: 'Omschrijving',
+    label: 'Titel'
+  },
+  render: () => <Grid>
+      <Grid.Cell span={3}>
+        <TopTaskLink description="Bekijk welke belastingen en heffingen er zijn, hoe u bezwaar maakt of een betalingsregeling treft." href="#" label="Gemeentebelastingen" />
+      </Grid.Cell>
+      <Grid.Cell span={3}>
+        <TopTaskLink description="U kunt met uw auto goedkoop parkeren bij een P+R-locatie aan de rand van de stad." href="#" label="Parkeren + Reizen (P+R)" />
+      </Grid.Cell>
+      <Grid.Cell span={3}>
+        <TopTaskLink description="Vraag deze bewijzen aan of verleng ze. Geef een vermissing aan." href="#" label="Paspoort, ID-kaart en rijbewijs" />
+      </Grid.Cell>
+      <Grid.Cell span={3}>
+        <TopTaskLink description="Meld een kind aan voor basisschool, middelbare school of kinderopvang. Bekijk het kindtegoed op uw Stadspas." href="#" label="Onderwijs" />
+      </Grid.Cell>
+    </Grid>,
+  parameters: {
+    docs: {
+      source: {
+        type: 'dynamic'
+      }
+    }
+  }
+}`,...(p=(d=i.parameters)==null?void 0:d.docs)==null?void 0:p.source}}};const k=["Default","Multiple"],u=Object.freeze(Object.defineProperty({__proto__:null,Default:a,Multiple:i,__namedExportsOrder:k,default:c},Symbol.toStringTag,{value:"Module"}));export{i as M,u as T};
