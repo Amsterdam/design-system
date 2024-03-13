d7c6b1cf45d307ed02d33ddbf0e1c27bc6089135
alimpens <37216945+alimpens@users.noreply.github.com>
2024-03-13 12:19:17 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@7ab5bf21c6ebc382504e3353c4caf203340e26a8 🚀

---
+++ demo-DES-591-finetune-subcomponent-type/assets/WithTooltip-V3YHNWJZ-B99agX8s.js
@@ -0,0 +1,1 @@
+import{W as h,W as l,a as s}from"./index-BuJms1vp.js";import"./iframe-BayK-ZNB.js";import"../sb-preview/runtime.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-BtM5VmRH.js";import"./index-Bw8VTzHM.js";import"./index-Cmc67Rxs.js";import"./index-DrFu-skq.js";export{h as WithToolTipState,l as WithTooltip,s as WithTooltipPure};
---
+++ demo-DES-591-finetune-subcomponent-type/assets/notice.docs-C_UEq1TF.js
@@ -0,0 +1,23 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{M as s,d as r}from"./index-BuJms1vp.js";import{useMDXComponents as a}from"./index-DI5IigMn.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-BayK-ZNB.js";import"../sb-preview/runtime.js";import"./index-BtM5VmRH.js";import"./index-Bw8VTzHM.js";import"./index-Cmc67Rxs.js";import"./index-DrFu-skq.js";const i=`<!-- @license CC0-1.0 -->
+
+# Terms of Use
+
+## Branding, logo and authors’ rights
+
+The branding and logo are used exclusively by the City of Amsterdam.
+Authors’ rights and trademark rights apply to the logo.
+If you intend to use a modified version of the software for other purposes, you are not allowed to use the City of Amsterdam’s logo.
+Instead, you should design your own branding.
+
+## Fonts
+
+Not all fonts used in the branding are free and open source.
+Please note that you must arrange a (paid) license if you use the provided fonts.
+Alternatively, adjust the configuration to use fewer or different fonts.
+Amsterdam Sans is licensed exclusively to the City of Amsterdam and amsterdam&partners.
+
+## Permission
+
+For permission to use the logo or branding, please contact <designsystem@amsterdam.nl>.
+`;function o(n){return e.jsxs(e.Fragment,{children:[e.jsx(s,{title:"Docs/Terms of Use"}),`
+`,e.jsx(r,{children:i})]})}function j(n={}){const{wrapper:t}=Object.assign({},a(),n.components);return t?e.jsx(t,Object.assign({},n,{children:e.jsx(o,n)})):o()}export{j as default};
