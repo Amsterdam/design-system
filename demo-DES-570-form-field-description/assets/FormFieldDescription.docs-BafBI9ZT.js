fe9bc3f485a178ce8778fddff76eab3ce3495ddd
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-03-13 11:51:23 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@54d41835f9ed651b4e53b648ad12f930e53215d9 🚀

---
+++ demo-DES-570-form-field-description/assets/FormFieldDescription.docs-BafBI9ZT.js
@@ -0,0 +1,21 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{M as r,d as s,P as a,f as c}from"./index-tYGlYXyE.js";import{F as d,W as m}from"./FormFieldDescription.stories-BThqTmbI.js";import{useMDXComponents as o}from"./index-DI5IigMn.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-CvKh8SAX.js";import"../sb-preview/runtime.js";import"./index-BtM5VmRH.js";import"./index-Bw8VTzHM.js";import"./index-Cmc67Rxs.js";import"./index-DrFu-skq.js";import"./index.esm-CPKrfbkJ.js";const p=`<!-- @license CC0-1.0 -->
+
+# Form Field Description
+
+This component is used to provide additional information about a form field. This information is typically used to provide context or instructions for the form field.
+
+## Described by
+
+The description can be associated with a form field using the \`aria-describedby\` attribute. This attribute is used to reference the \`id\` of the \`FormFieldDescription\` component.
+
+## References
+
+- [Understanding Success Criterion 3.3.2: Labels or Instructions (W3C)](https://www.w3.org/WAI/WCAG21/Understanding/labels-or-instructions)
+- [Describing what will happen before a change to a form control that causes a change of context to occur is made](https://www.w3.org/WAI/WCAG21/Techniques/general/G13)
+- [Using the aria-describedby property to provide a descriptive label for user interface controls](https://www.w3.org/WAI/WCAG21/Techniques/aria/ARIA1)
+`;function i(t){const n=Object.assign({h2:"h2",h3:"h3"},o(),t.components);return e.jsxs(e.Fragment,{children:[e.jsx(r,{of:d}),`
+`,e.jsx(s,{children:p}),`
+`,e.jsx(a,{}),`
+`,e.jsx(n.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(n.h3,{id:"with-a-form-field",children:"With a form field"}),`
+`,e.jsx(c,{of:m})]})}function D(t={}){const{wrapper:n}=Object.assign({},o(),t.components);return n?e.jsx(n,Object.assign({},t,{children:e.jsx(i,t)})):i(t)}export{D as default};
