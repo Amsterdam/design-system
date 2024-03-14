24c1f6635e831bac772d7438ffc41a631421993f
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-03-14 13:39:05 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@5f4dfc019bdac1f331c0519c9ef874a74915f864 🚀

---
+++ demo-documentation-updates/assets/Footer.docs-BylZOiQC.js
@@ -0,0 +1,20 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{M as i,d as s,P as r}from"./index-CMKzkGYi.js";import{F as a}from"./Footer.stories-DFqV9qwn.js";import{useMDXComponents as c}from"./index-DI5IigMn.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-DuX8CP_M.js";import"../sb-preview/runtime.js";import"./index-BtM5VmRH.js";import"./index-Bw8VTzHM.js";import"./index-Cmc67Rxs.js";import"./index-DrFu-skq.js";import"./index.esm-DlodUMu8.js";const m=`<!-- @license CC0-1.0 -->
+
+# Footer
+
+The footer is the closing section on every web page.
+Its content provides service information.
+A part is prescribed, and a part is filled based on the website’s objectives.
+
+## Guidelines
+
+- The footer is mandatory, and there is one on every page.
+- The first column focuses on contact information.
+  The information is as specific as possible and tailored to the respective website or page (e.g., including a chat option if available).
+- The second column contains links to relevant (online) sites or sources.
+- The third column refers to newsletters, social media, etc.
+- The last part of the footer is intended for links to privacy policies, cookie statements, explanations about the website, etc.
+  It is not intended for links to contact information, for example.
+`;function n(t){return e.jsxs(e.Fragment,{children:[e.jsx(i,{of:a}),`
+`,e.jsx(s,{children:m}),`
+`,e.jsx(r,{})]})}function T(t={}){const{wrapper:o}=Object.assign({},c(),t.components);return o?e.jsx(o,Object.assign({},t,{children:e.jsx(n,t)})):n()}export{T as default};
