4b9e44e314f0058dede1da7700a0cb5a443e9a04
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-03-14 11:52:03 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@54f1b82c977e263efa14e93d1cccf571de5595eb 🚀

--- demo-DES-603-Dialog-heading/assets/Fieldset.stories-V9nu4d0k.js
+++
@@ -1,6 +0,0 @@
-import{j as o}from"./jsx-runtime-CKrituN3.js";import{F as l,g as s}from"./index.esm-O2aepYll.js";const m={title:"Components/Forms/Fieldset",component:l,args:{children:"Body van de fieldset",legend:"Label van de fieldset"}},e={},r={args:{children:[o.jsx(s,{children:"Horecabedrijf"},1),o.jsx(s,{children:"Ander soort bedrijf"},2),o.jsx(s,{children:"Evenement"},3),o.jsx(s,{children:"Iets anders"},4)],legend:"Waar gaat uw melding over?"}};var a,t,n;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:"{}",...(n=(t=e.parameters)==null?void 0:t.docs)==null?void 0:n.source}}};var c,d,i;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:`{
-  args: {
-    children: [<Checkbox key={1}>Horecabedrijf</Checkbox>, <Checkbox key={2}>Ander soort bedrijf</Checkbox>, <Checkbox key={3}>Evenement</Checkbox>, <Checkbox key={4}>Iets anders</Checkbox>],
-    legend: 'Waar gaat uw melding over?'
-  }
-}`,...(i=(d=r.parameters)==null?void 0:d.docs)==null?void 0:i.source}}};const b=["Default","CheckboxGroup"],h=Object.freeze(Object.defineProperty({__proto__:null,CheckboxGroup:r,Default:e,__namedExportsOrder:b,default:m},Symbol.toStringTag,{value:"Module"}));export{r as C,h as F};
