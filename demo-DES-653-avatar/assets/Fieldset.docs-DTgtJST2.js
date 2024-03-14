8b5d49456249731f56adf2757d1119309e07e648
RubenSibon <mail@rubensibon.nl>
2024-03-14 15:54:06 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@56c5caf921363e8125f1e29b205c367b93cb3b87 🚀

---
+++ demo-DES-653-avatar/assets/Fieldset.docs-DTgtJST2.js
@@ -0,0 +1,26 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{M as i,d as r,P as p,e as a,f as l}from"./index-CMhO059i.js";import{F as d,C as u}from"./Fieldset.stories-lHKxcY1q.js";import{useMDXComponents as s}from"./index-DI5IigMn.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-DZjtaYb0.js";import"../sb-preview/runtime.js";import"./index-BtM5VmRH.js";import"./index-Bw8VTzHM.js";import"./index-Cmc67Rxs.js";import"./index-DrFu-skq.js";import"./index.esm-D-oNEv0H.js";const m=`<!-- @license CC0-1.0 -->
+
+# Fieldset
+
+A component to group related form inputs.
+
+## Guidelines
+
+- Use Fieldset when you need to show a relationship between multiple form inputs. For example, you may need to group a set of text inputs into a single fieldset when asking for an address.
+- When grouping radio inputs, use \`role="radiogroup"\` on Fieldset to have this grouping explicitly announced as a radio group. Fieldset has a default role of \`group\`.
+
+## Relevant WCAG Requirements
+
+- [WCAG 1.3.5](https://www.w3.org/WAI/WCAG22/Understanding/identify-input-purpose.html): Fieldset labels the purpose of a group of inputs.
+
+## References
+
+- [Providing a description for groups of form controls using fieldset and legend elements](https://www.w3.org/WAI/WCAG22/Techniques/html/H71)
+`;function t(n){const o=Object.assign({h2:"h2",h3:"h3",p:"p"},s(),n.components);return e.jsxs(e.Fragment,{children:[e.jsx(i,{of:d}),`
+`,e.jsx(r,{children:m}),`
+`,e.jsx(p,{}),`
+`,e.jsx(a,{}),`
+`,e.jsx(o.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(o.h3,{id:"checkbox-group",children:"Checkbox group"}),`
+`,e.jsx(o.p,{children:"Fieldset is used to group related form inputs, like checkboxes."}),`
+`,e.jsx(l,{of:u})]})}function y(n={}){const{wrapper:o}=Object.assign({},s(),n.components);return o?e.jsx(o,Object.assign({},n,{children:e.jsx(t,n)})):t(n)}export{y as default};
