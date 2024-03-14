ca2bfad02704ee55213d28e6f6f97e412f3195fa
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-03-14 13:41:09 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@982adea332b1de51ca6a77daf2d54ff4f53cf6d0 🚀

--- demo-documentation-updates/assets/FormLabel.stories-BrXR6G-l.js
+++
@@ -1,4 +0,0 @@
-import{n as t}from"./index.esm-DlodUMu8.js";/**
- * @license EUPL-1.2+
- * Copyright Gemeente Amsterdam
- */const s={title:"Components/Forms/Form Label",component:t,args:{children:"Form label"},argTypes:{children:{table:{disable:!1}}}},e={};var r,o,a;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:"{}",...(a=(o=e.parameters)==null?void 0:o.docs)==null?void 0:a.source}}};const l=["Default"],n=Object.freeze(Object.defineProperty({__proto__:null,Default:e,__namedExportsOrder:l,default:s},Symbol.toStringTag,{value:"Module"}));export{n as F};
---
+++ demo-documentation-updates/assets/Row.docs-Bb1FYpHM.js
@@ -0,0 +1,13 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{M as i,d as r,P as a,e as c}from"./index-DLyMTbRq.js";import{R as m}from"./Row.stories-Sv1c185j.js";import{useMDXComponents as o}from"./index-DI5IigMn.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-BEEsUp4c.js";import"../sb-preview/runtime.js";import"./index-BtM5VmRH.js";import"./index-Bw8VTzHM.js";import"./index-Cmc67Rxs.js";import"./index-DrFu-skq.js";import"./index.esm-1ztBE3Ny.js";const d=`<!-- @license CC0-1.0 -->
+
+# Row
+`;function t(s){const n=Object.assign({h2:"h2",p:"p",a:"a",h3:"h3"},o(),s.components);return e.jsxs(e.Fragment,{children:[e.jsx(i,{of:m}),`
+`,e.jsx(r,{children:d}),`
+`,e.jsx(n.h2,{id:"design",children:"Design"}),`
+`,e.jsxs(n.p,{children:["The five ",e.jsx(n.a,{href:"/docs/docs-design-guidelines-space--docs",children:"space"})," sizes are available for the width of the gap."]}),`
+`,e.jsx(n.h2,{id:"how-to-use",children:"How to Use"}),`
+`,e.jsx(n.p,{children:"Wrap a Row around a set of components that need the same amount of white space between them."}),`
+`,e.jsx(n.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(n.h3,{id:"default",children:"Default"}),`
+`,e.jsx(a,{}),`
+`,e.jsx(c,{})]})}function b(s={}){const{wrapper:n}=Object.assign({},o(),s.components);return n?e.jsx(n,Object.assign({},s,{children:e.jsx(t,s)})):t(s)}export{b as default};
