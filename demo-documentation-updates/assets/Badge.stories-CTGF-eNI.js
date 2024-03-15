1e60074d8aad957128fa9e62b7280b7e38ef092d
GitHub <github-action@users.noreply.github.com>
2024-03-15 09:51:01 +0000

    Remove feature branch folder

--- demo-documentation-updates/assets/Badge.stories-CTGF-eNI.js
+++
@@ -1,4 +0,0 @@
-import{B as a}from"./index.esm-1ztBE3Ny.js";/**
- * @license EUPL-1.2+
- * Copyright Gemeente Amsterdam
- */const s={title:"Components/Feedback/Badge",component:a,args:{label:"Tip"},argTypes:{color:{control:{type:"select"},options:["blue","dark-blue","dark-green","green","magenta","orange","purple","yellow"],selected:"dark-green"}}},e={};var r,o,t;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:"{}",...(t=(o=e.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};const l=["Default"],c=Object.freeze(Object.defineProperty({__proto__:null,Default:e,__namedExportsOrder:l,default:s},Symbol.toStringTag,{value:"Module"}));export{c as B};
