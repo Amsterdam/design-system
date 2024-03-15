cc380bce78987192bd77eab2d5426d54159c9d20
alimpens <37216945+alimpens@users.noreply.github.com>
2024-03-15 09:51:57 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@1c91a9a653ef2d19c415b5cbe405ea022c4636ef 🚀

---
+++ demo-develop/assets/breakpoints.docs-TRuhxVeU.js
@@ -0,0 +1,15 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{M as o}from"./index-DLyMTbRq.js";import{useMDXComponents as r}from"./index-DI5IigMn.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-BEEsUp4c.js";import"../sb-preview/runtime.js";import"./index-BtM5VmRH.js";import"./index-Bw8VTzHM.js";import"./index-Cmc67Rxs.js";import"./index-DrFu-skq.js";function t(s){const n=Object.assign({h1:"h1",p:"p",pre:"pre",code:"code",a:"a"},r(),s.components);return e.jsxs(e.Fragment,{children:[e.jsx(o,{title:"Docs/Design Guidelines/Breakpoints"}),`
+`,e.jsx(n.h1,{id:"breakpoints",children:"Breakpoints"}),`
+`,e.jsx(n.p,{children:"We use 2 breakpoints where the design of a website changes to ensure a good user experience: at screen widths of 576px and 1088px."}),`
+`,e.jsx(n.p,{children:"We follow a mobile-first approach: first, we create the design below the breakpoint, and then we use a media query to create the design above the breakpoint."}),`
+`,e.jsx(n.p,{children:"For example:"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-css",children:`.class {
+  /* Your narrow styling */
+
+  @media screen and (min-width: $ams-breakpoint-wide) {
+    /* Your wide styling */
+  }
+}
+`})}),`
+`,e.jsxs(n.p,{children:[`Within this repository, you can import the breakpoint as a Sass variable.
+You can find this variable in `,e.jsx(n.a,{href:"https://github.com/Amsterdam/design-system/blob/main/packages/css/src/common/breakpoint.scss",target:"_blank",rel:"nofollow noopener noreferrer",children:"packages/css/src/common/breakpoint.scss"}),"."]})]})}function g(s={}){const{wrapper:n}=Object.assign({},r(),s.components);return n?e.jsx(n,Object.assign({},s,{children:e.jsx(t,s)})):t(s)}export{g as default};
