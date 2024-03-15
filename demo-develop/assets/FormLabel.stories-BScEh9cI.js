e814e0fe2dc37ddb00d050659bdb3cac3022fad3
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-03-15 16:14:26 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@827ac4e1197730fb0c69de197347cd4be3a77ebf 🚀

---
+++ demo-develop/assets/FormLabel.stories-BScEh9cI.js
@@ -0,0 +1,4 @@
+import{o as t}from"./index.esm-DuzQ3ql0.js";/**
+ * @license EUPL-1.2+
+ * Copyright Gemeente Amsterdam
+ */const s={title:"Components/Forms/Form Label",component:t,args:{children:"Form label"},argTypes:{children:{table:{disable:!1}}}},e={};var r,o,a;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:"{}",...(a=(o=e.parameters)==null?void 0:o.docs)==null?void 0:a.source}}};const l=["Default"],c=Object.freeze(Object.defineProperty({__proto__:null,Default:e,__namedExportsOrder:l,default:s},Symbol.toStringTag,{value:"Module"}));export{c as F};
