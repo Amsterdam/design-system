8624ea1e223a72cc744ea042187e480e105566b8
Aram Limpens <a.limpens@amsterdam.nl>
2024-03-15 13:40:07 +0100

    Update Footer

--- packages/react/src/Footer/Footer.tsx
+++ packages/react/src/Footer/Footer.tsx
@@ -5,28 +5,20 @@
 
 import clsx from 'clsx'
 import { forwardRef } from 'react'
-import type { ForwardedRef, ForwardRefExoticComponent, HTMLAttributes, PropsWithChildren, RefAttributes } from 'react'
+import type { ForwardedRef, HTMLAttributes, PropsWithChildren } from 'react'
 import { FooterBottom } from './FooterBottom'
 import { FooterTop } from './FooterTop'
 
 export type FooterProps = PropsWithChildren<HTMLAttributes<HTMLElement>>
 
-type FooterComponent = {
-  Top: typeof FooterTop
-  Bottom: typeof FooterBottom
-} & ForwardRefExoticComponent<FooterProps & RefAttributes<HTMLElement>>
-
-export const Footer = forwardRef(
+export const FooterRoot = forwardRef(
   ({ children, className, ...restProps }: FooterProps, ref: ForwardedRef<HTMLElement>) => (
     <footer {...restProps} ref={ref} className={clsx('ams-footer', className)}>
       {children}
     </footer>
   ),
-) as FooterComponent
+)
 
-Footer.Top = FooterTop
-Footer.Bottom = FooterBottom
+FooterRoot.displayName = 'Footer'
 
-Footer.displayName = 'Footer'
-Footer.Top.displayName = 'Footer.Top'
-Footer.Bottom.displayName = 'Footer.Bottom'
+export const Footer = Object.assign(FooterRoot, { Bottom: FooterBottom, Top: FooterTop })
--- packages/react/src/Footer/FooterBottom.tsx
+++ packages/react/src/Footer/FooterBottom.tsx
@@ -17,4 +17,4 @@ export const FooterBottom = forwardRef(
   ),
 )
 
-FooterBottom.displayName = 'FooterBottom'
+FooterBottom.displayName = 'Footer.Bottom'
