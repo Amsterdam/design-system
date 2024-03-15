b95f9731100b5fa3673bec2a9b4b6e1f08dcf2bd
Aram Limpens <a.limpens@amsterdam.nl>
2024-03-15 13:37:03 +0100

    Update Card

--- packages/react/src/Card/Card.tsx
+++ packages/react/src/Card/Card.tsx
@@ -5,25 +5,20 @@
 
 import clsx from 'clsx'
 import { forwardRef } from 'react'
-import type { ForwardedRef, ForwardRefExoticComponent, HTMLAttributes, PropsWithChildren, RefAttributes } from 'react'
+import type { ForwardedRef, HTMLAttributes, PropsWithChildren } from 'react'
 import { CardHeadingGroup } from './CardHeadingGroup'
 import { CardLink } from './CardLink'
 
 export type CardProps = PropsWithChildren<HTMLAttributes<HTMLElement>>
 
-type CardComponent = {
-  HeadingGroup: typeof CardHeadingGroup
-  Link: typeof CardLink
-} & ForwardRefExoticComponent<CardProps & RefAttributes<HTMLElement>>
+export const CardRoot = forwardRef(
+  ({ children, className, ...restProps }: CardProps, ref: ForwardedRef<HTMLElement>) => (
+    <article {...restProps} ref={ref} className={clsx('ams-card', className)}>
+      {children}
+    </article>
+  ),
+)
 
-export const Card = forwardRef(({ children, className, ...restProps }: CardProps, ref: ForwardedRef<HTMLElement>) => (
-  <article {...restProps} ref={ref} className={clsx('ams-card', className)}>
-    {children}
-  </article>
-)) as CardComponent
+CardRoot.displayName = 'Card'
 
-Card.HeadingGroup = CardHeadingGroup
-Card.Link = CardLink
-Card.displayName = 'Card'
-Card.HeadingGroup.displayName = 'Card.HeadingGroup'
-Card.Link.displayName = 'Card.Link'
+export const Card = Object.assign(CardRoot, { HeadingGroup: CardHeadingGroup, Link: CardLink })
