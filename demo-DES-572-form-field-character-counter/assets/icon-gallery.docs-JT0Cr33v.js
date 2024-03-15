8f82c8f6ccd60bb83f673f92b05bc5e1f6386ba6
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-03-15 12:40:28 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@e42609a040e7ec8ad8650ff52c455b548522348a 🚀

---
+++ demo-DES-572-form-field-character-counter/assets/icon-gallery.docs-JT0Cr33v.js
@@ -0,0 +1,8 @@
+import{j as n}from"./jsx-runtime-CKrituN3.js";import{I as c,g as i,M as a}from"./index-JBEh8hKu.js";import{f as m,r as t}from"./index.esm-KHl-zq0S.js";import{useMDXComponents as r}from"./index-DI5IigMn.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-aKAHDp3A.js";import"../sb-preview/runtime.js";import"./index-BtM5VmRH.js";import"./index-Bw8VTzHM.js";import"./index-Cmc67Rxs.js";import"./index-DrFu-skq.js";const l=()=>{const o=Object.keys(t);return n.jsx(c,{children:o.map(s=>n.jsx(i,{name:s.substring(0,s.length-4),children:n.jsx(m,{svg:t[s]})},s))})};function e(o){const s=Object.assign({h1:"h1",p:"p",h2:"h2",ul:"ul",li:"li",a:"a"},r(),o.components);return n.jsxs(n.Fragment,{children:[n.jsx(a,{title:"Docs/Assets/Icons"}),`
+`,n.jsx(s.h1,{id:"icons",children:"Icons"}),`
+`,n.jsx(s.p,{children:"An overview of all currently available icons."}),`
+`,n.jsx(l,{}),`
+`,n.jsx(s.h2,{id:"related-components",children:"Related components"}),`
+`,n.jsxs(s.ul,{children:[`
+`,n.jsx(s.li,{children:n.jsx(s.a,{href:"/docs/components-media-icon--docs",children:"Icon"})}),`
+`]})]})}function D(o={}){const{wrapper:s}=Object.assign({},r(),o.components);return s?n.jsx(s,Object.assign({},o,{children:n.jsx(e,o)})):e(o)}export{D as default};
