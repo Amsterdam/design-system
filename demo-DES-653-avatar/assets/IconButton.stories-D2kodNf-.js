5fbf5c88cd2cc5ac1ea02bbdeccdb75da623174b
RubenSibon <mail@rubensibon.nl>
2024-03-15 13:44:52 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@44d71016fa2430a82a70ce42ac992635fe924808 🚀

---
+++ demo-DES-653-avatar/assets/IconButton.stories-D2kodNf-.js
@@ -0,0 +1,28 @@
+import{s as j,r as c}from"./index.esm-Ck5SMtPJ.js";/**
+ * @license EUPL-1.2+
+ * Copyright Gemeente Amsterdam
+ */const x={title:"Components/Buttons/Icon Button",component:j,args:{label:"Sluiten",onBackground:void 0,disabled:!1,size:void 0},argTypes:{disabled:{control:"boolean"},onBackground:{control:{type:"radio",labels:{undefined:"default",light:"light",dark:"dark"}},options:[void 0,"light","dark"]},size:{control:{type:"radio",labels:{undefined:"level-5"}},options:["level-3","level-4",void 0,"level-6"]},svg:{control:{type:"select"},options:Object.keys(c),mapping:c}}},e={},r={args:{onBackground:"light"}},s={args:{onBackground:"dark"}},a={args:{size:"level-3"}},o={args:{size:"level-4"}},n={args:{size:"level-5"}},t={args:{size:"level-6"}};var l,d,i;e.parameters={...e.parameters,docs:{...(l=e.parameters)==null?void 0:l.docs,source:{originalSource:"{}",...(i=(d=e.parameters)==null?void 0:d.docs)==null?void 0:i.source}}};var u,p,g;r.parameters={...r.parameters,docs:{...(u=r.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    onBackground: 'light'
+  }
+}`,...(g=(p=r.parameters)==null?void 0:p.docs)==null?void 0:g.source}}};var m,v,k;s.parameters={...s.parameters,docs:{...(m=s.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    onBackground: 'dark'
+  }
+}`,...(k=(v=s.parameters)==null?void 0:v.docs)==null?void 0:k.source}}};var B,b,z;a.parameters={...a.parameters,docs:{...(B=a.parameters)==null?void 0:B.docs,source:{originalSource:`{
+  args: {
+    size: 'level-3'
+  }
+}`,...(z=(b=a.parameters)==null?void 0:b.docs)==null?void 0:z.source}}};var L,S,f;o.parameters={...o.parameters,docs:{...(L=o.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  args: {
+    size: 'level-4'
+  }
+}`,...(f=(S=o.parameters)==null?void 0:S.docs)==null?void 0:f.source}}};var O,h,y;n.parameters={...n.parameters,docs:{...(O=n.parameters)==null?void 0:O.docs,source:{originalSource:`{
+  args: {
+    size: 'level-5'
+  }
+}`,...(y=(h=n.parameters)==null?void 0:h.docs)==null?void 0:y.source}}};var _,I,D;t.parameters={...t.parameters,docs:{...(_=t.parameters)==null?void 0:_.docs,source:{originalSource:`{
+  args: {
+    size: 'level-6'
+  }
+}`,...(D=(I=t.parameters)==null?void 0:I.docs)==null?void 0:D.source}}};const T=["Default","OnLightBackground","OnDarkBackground","Level3","Level4","Level5","Level6"],E=Object.freeze(Object.defineProperty({__proto__:null,Default:e,Level3:a,Level4:o,Level5:n,Level6:t,OnDarkBackground:s,OnLightBackground:r,__namedExportsOrder:T,default:x},Symbol.toStringTag,{value:"Module"}));export{E as I,a as L,r as O,s as a,o as b,n as c,t as d};
