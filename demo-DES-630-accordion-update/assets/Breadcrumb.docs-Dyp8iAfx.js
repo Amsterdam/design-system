916ee5dd738f11c5e47a5748b4e01b0a800e6ebe
dlnr <niels@dlnr.dev>
2024-03-15 12:08:16 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@5ec102d400377bb5c4a416c800b8703f5f9518e7 🚀

---
+++ demo-DES-630-accordion-update/assets/Breadcrumb.docs-Dyp8iAfx.js
@@ -0,0 +1,27 @@
+import{j as t}from"./jsx-runtime-CKrituN3.js";import{M as i,d as a,P as o}from"./index-DyrhLlrv.js";import{B as c}from"./Breadcrumb.stories-Di5pEACC.js";import{useMDXComponents as s}from"./index-DI5IigMn.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-H1oHjCMQ.js";import"../sb-preview/runtime.js";import"./index-BtM5VmRH.js";import"./index-Bw8VTzHM.js";import"./index-Cmc67Rxs.js";import"./index-DrFu-skq.js";import"./index.esm-nALxYX64.js";const m=`<!-- @license CC0-1.0 -->
+
+# Breadcrumb
+
+The breadcrumb is a secondary navigation pattern that helps a user understand the structure between levels in the website up to the current page.
+Users can use it to navigate.
+
+## Guidelines
+
+### Use like this
+
+Only use the breadcrumb trail if it adds something functional for the user and the structure is ‘static’.
+
+### Avoid this
+
+Do not display the breadcrumb trail on a form page.
+It distracts the user from their task, or one can accidentally interpret it as a Progress Indicator.
+
+It is a secondary navigation pattern.
+It can’t replace the main navigation.
+`;function r(n){const e=Object.assign({h2:"h2",ul:"ul",li:"li"},s(),n.components);return t.jsxs(t.Fragment,{children:[t.jsx(i,{of:c}),`
+`,t.jsx(a,{children:m}),`
+`,t.jsx(o,{}),`
+`,t.jsx(e.h2,{id:"usage",children:"Usage"}),`
+`,t.jsxs(e.ul,{children:[`
+`,t.jsx(e.li,{children:"A breadcrumb should not display more than 8 items."}),`
+`]})]})}function C(n={}){const{wrapper:e}=Object.assign({},s(),n.components);return e?t.jsx(e,Object.assign({},n,{children:t.jsx(r,n)})):r(n)}export{C as default};
---
+++ demo-DES-630-accordion-update/assets/TextArea.docs-mKQRBKCn.js
@@ -0,0 +1,31 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{M as r,d as a,P as l,e as h,f as t}from"./index-DyrhLlrv.js";import{T as m,V as c,H as d,N as x,I as p,D as f}from"./TextArea.stories-De45vLI7.js";import{useMDXComponents as o}from"./index-DI5IigMn.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-H1oHjCMQ.js";import"../sb-preview/runtime.js";import"./index-BtM5VmRH.js";import"./index-Bw8VTzHM.js";import"./index-Cmc67Rxs.js";import"./index-DrFu-skq.js";import"./index.esm-nALxYX64.js";import"./exampleContent-CyZEwIqm.js";const j=`<!-- @license CC0-1.0 -->
+
+# TextArea
+
+A form field that allows the user to input text over multiple lines.
+
+## Guidelines
+
+- Use a TextArea when users need to enter more than 1 sentence of text, such as a comment or description.
+- The height of the TextArea should be appropriate for the information to be entered.
+- A TextArea must have a label, and in most cases, this label should be visible.
+- Use \`spellcheck="false"\` for fields that may contain sensitive information, such as passwords and personal data.
+  Some browser extensions for spell-checking send this information to external servers.
+
+## Accessibility
+
+- [WCAG 2.1](https://www.w3.org/WAI/WCAG21/Techniques/html/H91.html) requires that the TextArea has a label or title attribute.
+`;function i(s){const n=Object.assign({h2:"h2"},o(),s.components);return e.jsxs(e.Fragment,{children:[e.jsx(r,{of:m}),`
+`,e.jsx(a,{children:j}),`
+`,e.jsx(l,{}),`
+`,e.jsx(h,{}),`
+`,e.jsx(n.h2,{id:"vertical-resize",children:"Vertical Resize"}),`
+`,e.jsx(t,{of:c}),`
+`,e.jsx(n.h2,{id:"horizontal-resize",children:"Horizontal Resize"}),`
+`,e.jsx(t,{of:d}),`
+`,e.jsx(n.h2,{id:"no-resize",children:"No Resize"}),`
+`,e.jsx(t,{of:x}),`
+`,e.jsx(n.h2,{id:"invalid",children:"Invalid"}),`
+`,e.jsx(t,{of:p}),`
+`,e.jsx(n.h2,{id:"disabled",children:"Disabled"}),`
+`,e.jsx(t,{of:f})]})}function I(s={}){const{wrapper:n}=Object.assign({},o(),s.components);return n?e.jsx(n,Object.assign({},s,{children:e.jsx(i,s)})):i(s)}export{I as default};
