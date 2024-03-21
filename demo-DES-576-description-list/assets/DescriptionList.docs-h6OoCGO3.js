4292ff57fe92c2252b07220bcf80e20e154d6833
dlnr <niels@dlnr.dev>
2024-03-21 10:56:37 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@0571d3cc4b7ee094cdb3ea9c661b36d4f5097d8e 🚀

--- demo-DES-576-description-list/assets/DescriptionList.docs-h6OoCGO3.js
+++
@@ -1,9 +0,0 @@
-import{j as o}from"./jsx-runtime-CKrituN3.js";import{useMDXComponents as s}from"./index-DSz_1G2r.js";import{M as i,d as e,P as m,f as p}from"./index-izeSktmt.js";import{D as a,W as c}from"./DescriptionList.stories-CQrelzZh.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-MAjPZi0q.js";import"../sb-preview/runtime.js";import"./index-BtM5VmRH.js";import"./index-Bw8VTzHM.js";import"./index-k_s-dmML.js";import"./index-DrFu-skq.js";import"./index.esm-DXf3ROm4.js";import"./exampleContent-CyZEwIqm.js";const d=`<!-- @license CC0-1.0 -->
-
-# Description List
-`;function r(t){const n={code:"code",h2:"h2",p:"p",...s(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(i,{of:a}),`
-`,o.jsx(e,{children:d}),`
-`,o.jsx(m,{}),`
-`,o.jsx(n.h2,{id:"wrap-with-rows",children:"Wrap with rows"}),`
-`,o.jsxs(n.p,{children:["If you need to use multiple details in a row, you can use a ",o.jsx(n.code,{children:"DescriptionListRow"})," component."]}),`
-`,o.jsx(p,{of:c})]})}function W(t={}){const{wrapper:n}={...s(),...t.components};return n?o.jsx(n,{...t,children:o.jsx(r,{...t})}):r(t)}export{W as default};
