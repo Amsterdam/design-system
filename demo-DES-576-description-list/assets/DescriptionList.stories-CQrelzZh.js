936ab062ecc3d9665890adf05f00404ec6210fb2
dlnr <niels@dlnr.dev>
2024-03-21 10:50:18 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@461a38bd6d094ba4865df47f3ce30db10ff812b6 🚀

---
+++ demo-DES-576-description-list/assets/DescriptionList.stories-CQrelzZh.js
@@ -0,0 +1,19 @@
+import{j as i}from"./jsx-runtime-CKrituN3.js";import{D as e}from"./index.esm-DXf3ROm4.js";import{e as D}from"./exampleContent-CyZEwIqm.js";const l=D(),d={title:"Components/Text/Description List",component:e},s={args:{children:[i.jsx(e.Term,{children:"Gebied"},3),i.jsx(e.Details,{children:"Gemeente Amsterdam"},4),i.jsx(e.Term,{children:"Stadsdeel"},5),i.jsx(e.Details,{children:"West"},6),i.jsx(e.Term,{children:"Opmerkingen"},1),i.jsx(e.Details,{children:l},2)]}},t={render:()=>i.jsxs(e,{children:[i.jsxs(e.Row,{children:[i.jsx(e.Term,{children:"Gebied"}),i.jsx(e.Details,{children:"Gemeente Amsterdam"})]}),i.jsxs(e.Row,{children:[i.jsx(e.Term,{children:"Stadsdelen"}),i.jsx(e.Details,{children:"Noord"}),i.jsx(e.Details,{children:"Oost"}),i.jsx(e.Details,{children:"Zuid"}),i.jsx(e.Details,{children:"West"})]})]})};var r,n,o;s.parameters={...s.parameters,docs:{...(r=s.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    children: [<DescriptionList.Term key={3}>Gebied</DescriptionList.Term>, <DescriptionList.Details key={4}>Gemeente Amsterdam</DescriptionList.Details>, <DescriptionList.Term key={5}>Stadsdeel</DescriptionList.Term>, <DescriptionList.Details key={6}>West</DescriptionList.Details>, <DescriptionList.Term key={1}>Opmerkingen</DescriptionList.Term>, <DescriptionList.Details key={2}>{paragraph}</DescriptionList.Details>]
+  }
+}`,...(o=(n=s.parameters)==null?void 0:n.docs)==null?void 0:o.source}}};var a,c,p;t.parameters={...t.parameters,docs:{...(a=t.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  render: () => <DescriptionList>
+      <DescriptionList.Row>
+        <DescriptionList.Term>Gebied</DescriptionList.Term>
+        <DescriptionList.Details>Gemeente Amsterdam</DescriptionList.Details>
+      </DescriptionList.Row>
+      <DescriptionList.Row>
+        <DescriptionList.Term>Stadsdelen</DescriptionList.Term>
+        <DescriptionList.Details>Noord</DescriptionList.Details>
+        <DescriptionList.Details>Oost</DescriptionList.Details>
+        <DescriptionList.Details>Zuid</DescriptionList.Details>
+        <DescriptionList.Details>West</DescriptionList.Details>
+      </DescriptionList.Row>
+    </DescriptionList>
+}`,...(p=(c=t.parameters)==null?void 0:c.docs)==null?void 0:p.source}}};const m=["Default","WrapRows"],j=Object.freeze(Object.defineProperty({__proto__:null,Default:s,WrapRows:t,__namedExportsOrder:m,default:d},Symbol.toStringTag,{value:"Module"}));export{j as D,t as W};
