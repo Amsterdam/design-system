ef4721fe56fe7060df062dc418174ea5e6d550c4
dlnr <niels@dlnr.dev>
2024-03-21 13:40:45 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@9444997c1ec18002e3184116cab834f884c27f0d 🚀

---
+++ demo-DES-576-description-list/assets/TopTaskLink.docs-DF3FuDFQ.js
@@ -0,0 +1,31 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{useMDXComponents as s}from"./index-DSz_1G2r.js";import{M as i,d as r,P as a,e as c,f as p}from"./index-Dg1HuVlB.js";import{T as d,M as h}from"./TopTaskLink.stories-BdWh6cJv.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-DyiDMTxd.js";import"../sb-preview/runtime.js";import"./index-BtM5VmRH.js";import"./index-Bw8VTzHM.js";import"./index-k_s-dmML.js";import"./index-DrFu-skq.js";import"./index.esm-CMVADTw6.js";const l=`<!-- @license CC0-1.0 -->
+
+# Top Task Link
+
+A prominent navigation element to quickly start a task.
+
+## Guidelines
+
+- A Top Task Link has both a title and a description.
+- The title describes the essence of the page being referred to, attracting attention when scanning the page.
+- The description provides more context, helping the user confirm that they can indeed achieve their goal on the linked page.
+- Keep both texts concise.
+  A title consists of a few words, and the description consists of 1 or 2 short sentences.
+  The description should end with a period or another appropriate punctuation mark.
+- For wide windows, place 3 or 4 Top Task Links side by side on the [Grid](/docs/components-layout-grid--docs).
+  If you have more Top Task Links, place them in the next row.
+  Try to distribute the amount of links on wide windows over rows of 3 or 4 links.
+  If this is not possible, leave the last cells of the grid empty.
+- If you have a group of links for which a description is unnecessary or illogical, use a [Link List](/docs/components-navigation-link--docs).
+
+## Relevant WCAG requirements
+
+The Top Task Link is subject to the same requirements as [regular links](/docs/components-navigation-link--docs).
+`;function t(n){const o={h3:"h3",p:"p",...s(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(i,{of:d}),`
+`,e.jsx(r,{children:l}),`
+`,e.jsx(a,{}),`
+`,e.jsx(c,{}),`
+`,e.jsx(o.h3,{id:"meerdere-links",children:"Meerdere links"}),`
+`,e.jsx(o.p,{children:`Top tasks usually come in groups.
+Present them in a grid.`}),`
+`,e.jsx(p,{of:h})]})}function b(n={}){const{wrapper:o}={...s(),...n.components};return o?e.jsx(o,{...n,children:e.jsx(t,{...n})}):t(n)}export{b as default};
