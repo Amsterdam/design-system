f9915998ec931b55ef3c71bbaf97d2f2b3f3fdc7
dlnr <niels@dlnr.dev>
2024-03-22 10:19:31 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@d1610c3bc29d855f071e3fc121f1bb8ed431127e 🚀

--- demo-DES-630-accordion-update/assets/preview-CgTC1nLq.js
+++
@@ -1,7 +0,0 @@
-import{_ as e}from"./iframe-H1oHjCMQ.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-BPmySE0r.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9]),import.meta.url);return new r}}};export{a as parameters};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-BPmySE0r.js","./iframe-H1oHjCMQ.js","./index-CBqU2yxZ.js","./_commonjsHelpers-BosuxZz1.js","./react-18-D8cruF67.js","./index-BtM5VmRH.js","./index-DyrhLlrv.js","./index-Bw8VTzHM.js","./index-Cmc67Rxs.js","./index-DrFu-skq.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
