94068fc3c3070cd36b964e49d8c07be32bc67399
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-03-25 09:18:07 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@51f7156ec8a22677bf1cbe1d722339a52531d227 🚀

---
+++ demo-DES-631-page-menu-wrap/assets/Row.docs-Clsjy2nC.js
@@ -0,0 +1,13 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{useMDXComponents as s}from"./index-DSz_1G2r.js";import{M as r,d as i,P as a,e as m}from"./index-BAQoKc3b.js";import{R as d}from"./Row.stories-BPWKZMj7.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-Cx-JjC86.js";import"../sb-preview/runtime.js";import"./index-BtM5VmRH.js";import"./index-Bw8VTzHM.js";import"./index-k_s-dmML.js";import"./index-DrFu-skq.js";import"./index.esm-PN9vWBns.js";const h=`<!-- @license CC0-1.0 -->
+
+# Row
+`;function t(o){const n={a:"a",h2:"h2",h3:"h3",p:"p",...s(),...o.components};return e.jsxs(e.Fragment,{children:[e.jsx(r,{of:d}),`
+`,e.jsx(i,{children:h}),`
+`,e.jsx(n.h2,{id:"design",children:"Design"}),`
+`,e.jsxs(n.p,{children:["The five ",e.jsx(n.a,{href:"/docs/docs-design-guidelines-space--docs",children:"space"})," sizes are available for the width of the gap."]}),`
+`,e.jsx(n.h2,{id:"how-to-use",children:"How to Use"}),`
+`,e.jsx(n.p,{children:"Wrap a Row around a set of components that need the same amount of white space between them."}),`
+`,e.jsx(n.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(n.h3,{id:"default",children:"Default"}),`
+`,e.jsx(a,{}),`
+`,e.jsx(m,{})]})}function E(o={}){const{wrapper:n}={...s(),...o.components};return n?e.jsx(n,{...o,children:e.jsx(t,{...o})}):t(o)}export{E as default};
