ebd2810b5dfcee6dc6575e6fe5f2693823232182
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-03-26 15:33:45 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@bfcc6a06a7c6e4a8b3f509167fc59dbf852ce290 🚀

---
+++ demo-tabs-background-colours/assets/PageMenu.docs-DRjXmU0T.js
@@ -0,0 +1,29 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{useMDXComponents as o}from"./index-DSz_1G2r.js";import{M as s,d as a,P as r,f as m}from"./index-R2j25Ncn.js";import{P as l,A as h}from"./PageMenu.stories-C8vzOQw-.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-B_Uo-XFQ.js";import"../sb-preview/runtime.js";import"./index-BtM5VmRH.js";import"./index-Bw8VTzHM.js";import"./index-k_s-dmML.js";import"./index-DrFu-skq.js";import"./index.esm-Bc_3i2zz.js";const d=`<!-- @license CC0-1.0 -->
+
+# Page Menu
+
+A small set of links for use in the Header and Footer.
+
+## Guidelines
+
+- Limit the amount of menu items in the Header to 5 items or less, including ‘Search’ and ‘Menu’.
+- The menu should fit on a single line and is right-aligned.
+- The menu in the footer is left-aligned.
+- Submenus are not allowed.
+- The ‘Menu’ button opens the Mega Menu.
+- On narrow screens, menu items other than ‘Search’ and ‘Menu’ move from the Page Menu to the Mega Menu.
+
+## Relevant WCAG Requirements
+
+- [Consistent Navigation (Level AA)](https://www.w3.org/WAI/WCAG21/Understanding/consistent-navigation.html)
+
+PageMenu is an interactive element, and the [general requirements and guidelines for interactive elements](/docs/docs-design-guidelines-interactive-elements--docs) apply.
+`;function i(t){const n={h2:"h2",h3:"h3",p:"p",...o(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(s,{of:l}),`
+`,e.jsx(a,{children:d}),`
+`,e.jsx(n.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(n.h3,{id:"default",children:"Default"}),`
+`,e.jsx(r,{}),`
+`,e.jsx(n.h2,{id:"right-alignment",children:"Right Alignment"}),`
+`,e.jsx(n.p,{children:`It may happen that all menu items do not fit on a single line, for example, in a medium-width window or with zoomed-in text.
+If the menu itself is right-aligned, as is often the case in headers, then you should also right-align the items within it.`}),`
+`,e.jsx(m,{of:h})]})}function y(t={}){const{wrapper:n}={...o(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(i,{...t})}):i(t)}export{y as default};
--- demo-tabs-background-colours/assets/icon-gallery.docs-BlKr61aC.js
+++
@@ -1,8 +0,0 @@
-import{j as n}from"./jsx-runtime-CKrituN3.js";import{useMDXComponents as r}from"./index-DSz_1G2r.js";import{I as i,g as m,M as l}from"./index-Tq45gzr9.js";import{h as a,s as e}from"./index.esm-Bc_3i2zz.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-IlNp4KAS.js";import"../sb-preview/runtime.js";import"./index-BtM5VmRH.js";import"./index-Bw8VTzHM.js";import"./index-k_s-dmML.js";import"./index-DrFu-skq.js";const c=()=>{const s=Object.keys(e);return n.jsx(i,{children:s.map(o=>n.jsx(m,{name:o.substring(0,o.length-4),children:n.jsx(a,{svg:e[o]})},o))})};c.__docgenInfo={description:"",methods:[],displayName:"AmsterdamIconGallery"};function t(s){const o={a:"a",h1:"h1",h2:"h2",li:"li",p:"p",ul:"ul",...r(),...s.components};return n.jsxs(n.Fragment,{children:[n.jsx(l,{title:"Docs/Assets/Icons"}),`
-`,n.jsx(o.h1,{id:"icons",children:"Icons"}),`
-`,n.jsx(o.p,{children:"An overview of all currently available icons."}),`
-`,n.jsx(c,{}),`
-`,n.jsx(o.h2,{id:"related-components",children:"Related components"}),`
-`,n.jsxs(o.ul,{children:[`
-`,n.jsx(o.li,{children:n.jsx(o.a,{href:"/docs/components-media-icon--docs",children:"Icon"})}),`
-`]})]})}function A(s={}){const{wrapper:o}={...r(),...s.components};return o?n.jsx(o,{...s,children:n.jsx(t,{...s})}):t(s)}export{A as default};
