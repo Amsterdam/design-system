dc71f2bd6a4cbffed773f30315219d63a4ecb593
dlnr <niels@dlnr.dev>
2024-03-27 10:33:28 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@33099a6a7b72fbbb8168d1314462926120ccaf6d 🚀

---
+++ demo-DES-576-description-list/assets/Column.docs-Chqb5QEc.js
@@ -0,0 +1,20 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{useMDXComponents as o}from"./index-DSz_1G2r.js";import{M as a,d as r,P as i,e as m,f as d}from"./index-ZIcihdbM.js";import{C as c,a as l}from"./Column.stories-1Nr2VQ5v.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-DHcS1HdD.js";import"../sb-preview/runtime.js";import"./index-BtM5VmRH.js";import"./index-Bw8VTzHM.js";import"./index-k_s-dmML.js";import"./index-DrFu-skq.js";import"./index.esm-DpzRHIA4.js";const h=`<!-- @license CC0-1.0 -->
+
+# Column
+
+Stacks its children vertically and adds a vertical gap between them.
+`;function t(s){const n={a:"a",code:"code",h2:"h2",h3:"h3",p:"p",...o(),...s.components};return e.jsxs(e.Fragment,{children:[e.jsx(a,{of:c}),`
+`,e.jsx(r,{children:h}),`
+`,e.jsx(n.h2,{id:"design",children:"Design"}),`
+`,e.jsxs(n.p,{children:["The five ",e.jsx(n.a,{href:"/docs/docs-design-guidelines-space--docs",children:"space"})," sizes are available for the size of the gap."]}),`
+`,e.jsx(n.h2,{id:"how-to-use",children:"How to Use"}),`
+`,e.jsx(n.p,{children:"Wrap a Column around a set of components that need the same amount of white space between them."}),`
+`,e.jsxs(n.p,{children:["To add white space below a single element, you can alternatively use a utility class for bottom margins, e.g. ",e.jsx(n.code,{children:'class="ams-mb--md"'}),"."]}),`
+`,e.jsx(n.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(n.h3,{id:"default",children:"Default"}),`
+`,e.jsx(i,{}),`
+`,e.jsx(m,{}),`
+`,e.jsx(n.h3,{id:"use-another-html-element",children:"Use Another HTML Element"}),`
+`,e.jsxs(n.p,{children:["By default, a Column renders a ",e.jsx(n.code,{children:"<div>"}),`.
+Use the `,e.jsx(n.code,{children:"as"})," prop to make your markup more semantic."]}),`
+`,e.jsx(d,{of:l})]})}function E(s={}){const{wrapper:n}={...o(),...s.components};return n?e.jsx(n,{...s,children:e.jsx(t,{...s})}):t(s)}export{E as default};
---
+++ demo-DES-576-description-list/assets/Icon.stories-DO1quI1l.js
@@ -0,0 +1,44 @@
+import{j as s}from"./jsx-runtime-CKrituN3.js";import{h as W,t as p,p as e,j as D}from"./index.esm-DpzRHIA4.js";const M={title:"Components/Media/Icon",component:W,argTypes:{size:{control:{type:"radio"},options:["level-3","level-4","level-5","level-6"]},square:{control:{type:"boolean"}},svg:{control:{type:"select"},options:Object.keys(p),mapping:p}}},r={args:{svg:e}},a={decorators:[i=>s.jsx("span",{style:{display:"flex",gap:"1rem"},children:s.jsx(i,{})})],render:i=>s.jsxs(s.Fragment,{children:[s.jsx(W,{...i,size:"level-3"}),s.jsx(D,{size:"level-3",children:"Inline text"})]}),args:{svg:e}},o={args:{svg:e,square:!0}},n={args:{svg:e,size:"level-3"}},t={args:{svg:e,size:"level-4"}},c={args:{svg:e,size:"level-5"}},l={args:{svg:e,size:"level-6"}};var m,g,v;r.parameters={...r.parameters,docs:{...(m=r.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    svg: Icons.EmailIcon
+  }
+}`,...(v=(g=r.parameters)==null?void 0:g.docs)==null?void 0:v.source}}};var d,u,I;a.parameters={...a.parameters,docs:{...(d=a.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  decorators: [Story => <span style={{
+    display: 'flex',
+    gap: '1rem'
+  }}>
+        <Story />
+      </span>],
+  render: args => <>
+      <Icon {...args} size="level-3" />
+      <Heading size="level-3">Inline text</Heading>
+    </>,
+  args: {
+    svg: Icons.EmailIcon
+  }
+}`,...(I=(u=a.parameters)==null?void 0:u.docs)==null?void 0:I.source}}};var S,x,z;o.parameters={...o.parameters,docs:{...(S=o.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    svg: Icons.EmailIcon,
+    square: true
+  }
+}`,...(z=(x=o.parameters)==null?void 0:x.docs)==null?void 0:z.source}}};var y,j,E;n.parameters={...n.parameters,docs:{...(y=n.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    svg: Icons.EmailIcon,
+    size: 'level-3'
+  }
+}`,...(E=(j=n.parameters)==null?void 0:j.docs)==null?void 0:E.source}}};var f,L,b;t.parameters={...t.parameters,docs:{...(f=t.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    svg: Icons.EmailIcon,
+    size: 'level-4'
+  }
+}`,...(b=(L=t.parameters)==null?void 0:L.docs)==null?void 0:b.source}}};var h,_,q;c.parameters={...c.parameters,docs:{...(h=c.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    svg: Icons.EmailIcon,
+    size: 'level-5'
+  }
+}`,...(q=(_=c.parameters)==null?void 0:_.docs)==null?void 0:q.source}}};var O,T,H;l.parameters={...l.parameters,docs:{...(O=l.parameters)==null?void 0:O.docs,source:{originalSource:`{
+  args: {
+    svg: Icons.EmailIcon,
+    size: 'level-6'
+  }
+}`,...(H=(T=l.parameters)==null?void 0:T.docs)==null?void 0:H.source}}};const k=["Default","WithText","Square","Level3","Level4","Level5","Level6"],P=Object.freeze(Object.defineProperty({__proto__:null,Default:r,Level3:n,Level4:t,Level5:c,Level6:l,Square:o,WithText:a,__namedExportsOrder:k,default:M},Symbol.toStringTag,{value:"Module"}));export{P as I,n as L,o as S,a as W,t as a,c as b,l as c};
