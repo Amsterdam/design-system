9036e451b31a5b82abd606cbbd3abdc03f4e7d25
dlnr <niels@dlnr.dev>
2024-04-02 08:00:33 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@b9bd5ca1a190ec5f575a18fbeaca9ffdeaab3bf4 🚀

---
+++ demo-DES-576-description-list/assets/DescriptionList.stories-C-O8FCt1.js
@@ -0,0 +1,12 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{D as s}from"./index.esm-CkaSE4JO.js";import{e as j}from"./exampleContent-BoywlOQx.js";const h=j(),g={title:"Components/Text/Description List",component:s,decorators:[(o,a)=>e.jsx("div",{className:a.args.inverseColor?"ams-docs-dark-background":void 0,children:e.jsx(o,{})})],args:{children:[e.jsx(s.Term,{children:"Gebied"},1),e.jsx(s.Details,{children:"Gemeente Amsterdam"},2),e.jsx(s.Term,{children:"Stadsdeel"},3),e.jsx(s.Details,{children:"West"},4),e.jsx(s.Term,{children:"Opmerkingen"},5),e.jsx(s.Details,{children:h},6)],inverseColor:!1}},r={},t={args:{children:[e.jsx(s.Term,{children:"Gebied"},1),e.jsx(s.Details,{children:"Gemeente Amsterdam"},2),e.jsx(s.Term,{children:"Stadsdeel"},3),e.jsx(s.Details,{children:"Noord"},4),e.jsx(s.Details,{children:"Oost"},5),e.jsx(s.Details,{children:"Zuid"},6),e.jsx(s.Details,{children:"West"},7)]}},i={args:{inverseColor:!0},decorators:[(o,a)=>e.jsx("div",{className:a.args.inverseColor?"ams-docs-dark-background":void 0,children:e.jsx(o,{})})]};var n,c,d;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:"{}",...(d=(c=r.parameters)==null?void 0:c.docs)==null?void 0:d.source}}};var l,m,p;t.parameters={...t.parameters,docs:{...(l=t.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    children: [<DescriptionList.Term key={1}>Gebied</DescriptionList.Term>, <DescriptionList.Details key={2}>Gemeente Amsterdam</DescriptionList.Details>, <DescriptionList.Term key={3}>Stadsdeel</DescriptionList.Term>, <DescriptionList.Details key={4}>Noord</DescriptionList.Details>, <DescriptionList.Details key={5}>Oost</DescriptionList.Details>, <DescriptionList.Details key={6}>Zuid</DescriptionList.Details>, <DescriptionList.Details key={7}>West</DescriptionList.Details>]
+  }
+}`,...(p=(m=t.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var D,u,x;i.parameters={...i.parameters,docs:{...(D=i.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  args: {
+    inverseColor: true
+  },
+  decorators: [(Story, context) => <div className={context.args['inverseColor'] ? 'ams-docs-dark-background' : undefined}>
+        <Story />
+      </div>]
+}`,...(x=(u=i.parameters)==null?void 0:u.docs)==null?void 0:x.source}}};const L=["Default","MultipleDetails","InvertedColor"],S=Object.freeze(Object.defineProperty({__proto__:null,Default:r,InvertedColor:i,MultipleDetails:t,__namedExportsOrder:L,default:g},Symbol.toStringTag,{value:"Module"}));export{S as D,i as I,t as M};
--- demo-DES-576-description-list/assets/FormLabel.stories-CMDn7HG_.js
+++
@@ -1,4 +0,0 @@
-import{s as t}from"./index.esm-DpzRHIA4.js";/**
- * @license EUPL-1.2+
- * Copyright Gemeente Amsterdam
- */const s={title:"Components/Forms/Form Label",component:t,args:{children:"Form label"},argTypes:{children:{table:{disable:!1}}}},e={};var r,o,a;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:"{}",...(a=(o=e.parameters)==null?void 0:o.docs)==null?void 0:a.source}}};const l=["Default"],c=Object.freeze(Object.defineProperty({__proto__:null,Default:e,__namedExportsOrder:l,default:s},Symbol.toStringTag,{value:"Module"}));export{c as F};
