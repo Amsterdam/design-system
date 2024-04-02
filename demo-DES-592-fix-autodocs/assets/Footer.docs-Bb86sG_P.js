a795d94b98ae110d3ee117a35fa549bbe554a70d
alimpens <37216945+alimpens@users.noreply.github.com>
2024-04-02 15:56:54 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@06a555c5f1e77246a97ab2daf2ab7257de9a0ef6 🚀

---
+++ demo-DES-592-fix-autodocs/assets/Footer.docs-Bb86sG_P.js
@@ -0,0 +1,20 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{useMDXComponents as i}from"./index-DSz_1G2r.js";import{M as r,d as s,P as a}from"./index-COzN0FKY.js";import{F as m}from"./Footer.stories-tgdWbAIo.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-PYhd2Q3x.js";import"../sb-preview/runtime.js";import"./index-BtM5VmRH.js";import"./index-Bw8VTzHM.js";import"./index-k_s-dmML.js";import"./index-DrFu-skq.js";import"./Icon-AGHld3Gl.js";import"./clsx-B-dksMZM.js";import"./Spotlight-dALbVyoJ.js";import"./VisuallyHidden-DSeislZq.js";import"./Grid-Cb2xqfsw.js";import"./LinkList-D7bHoBUL.js";import"./Heading-NgU1dxwu.js";import"./Paragraph-Im3SnT_h.js";import"./PageMenu-CxvVxmGM.js";const p=`<!-- @license CC0-1.0 -->
+
+# Footer
+
+The footer is the closing section on every web page.
+Its content provides service information.
+A part is prescribed, and a part is filled based on the website’s objectives.
+
+## Guidelines
+
+- The footer is mandatory, and there is one on every page.
+- The first column focuses on contact information.
+  The information is as specific as possible and tailored to the respective website or page (e.g., including a chat option if available).
+- The second column contains links to relevant (online) sites or sources.
+- The third column refers to newsletters, social media, etc.
+- The last part of the footer is intended for links to privacy policies, cookie statements, explanations about the website, etc.
+  It is not intended for links to contact information, for example.
+`;function n(o){return e.jsxs(e.Fragment,{children:[e.jsx(r,{of:m}),`
+`,e.jsx(s,{children:p}),`
+`,e.jsx(a,{})]})}function A(o={}){const{wrapper:t}={...i(),...o.components};return t?e.jsx(t,{...o,children:e.jsx(n,{...o})}):n()}export{A as default};
