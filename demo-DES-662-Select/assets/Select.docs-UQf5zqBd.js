db0506f39d89bd646946487b90417ce36d0d220f
dlnr <niels@dlnr.dev>
2024-04-04 08:53:45 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@d1a2221dc7c0f16acb3ae941467a19338325788b 🚀

---
+++ demo-DES-662-Select/assets/Select.docs-UQf5zqBd.js
@@ -0,0 +1,7 @@
+import{j as t}from"./jsx-runtime-CKrituN3.js";import{useMDXComponents as e}from"./index-DSz_1G2r.js";import{M as s,d as i,P as m,e as p}from"./index-DuLmt17r.js";import{S as a}from"./Select.stories-ce2HhtYO.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-CnbODYX4.js";import"../sb-preview/runtime.js";import"./index-BtM5VmRH.js";import"./index-Bw8VTzHM.js";import"./index-k_s-dmML.js";import"./index-DrFu-skq.js";import"./index.esm-UwBrYMaQ.js";const c=`<!-- @license CC0-1.0 -->
+
+# Select
+`;function n(o){return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:a}),`
+`,t.jsx(i,{children:c}),`
+`,t.jsx(m,{}),`
+`,t.jsx(p,{})]})}function w(o={}){const{wrapper:r}={...e(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(n,{...o})}):n()}export{w as default};
