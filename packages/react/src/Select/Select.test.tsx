e813e4feb0c54a81d7075d71e38399338d67a3a5
Niels Roozemond <n.roozemond@amsterdam.nl>
2024-04-04 15:16:13 +0200

    Adding tests

--- packages/react/src/Select/Select.test.tsx
+++ packages/react/src/Select/Select.test.tsx
@@ -1,30 +1,30 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import { createRef } from 'react'
 import { Select } from './Select'
 import '@testing-library/jest-dom'
 
 describe('Select', () => {
   it('renders', () => {
-    const { container } = render(<Select />)
+    render(<Select />)
 
-    const component = container.querySelector(':only-child')
+    const component = screen.getByRole('combobox')
 
     expect(component).toBeInTheDocument()
     expect(component).toBeVisible()
   })
 
   it('renders a design system BEM class name', () => {
-    const { container } = render(<Select />)
+    render(<Select />)
 
-    const component = container.querySelector(':only-child')
+    const component = screen.getByRole('combobox')
 
     expect(component).toHaveClass('ams-select')
   })
 
   it('renders an additional class name', () => {
-    const { container } = render(<Select className="extra" />)
+    render(<Select className="extra" />)
 
-    const component = container.querySelector(':only-child')
+    const component = screen.getByRole('combobox')
 
     expect(component).toHaveClass('ams-select extra')
   })
@@ -32,10 +32,59 @@ describe('Select', () => {
   it('supports ForwardRef in React', () => {
     const ref = createRef<HTMLSelectElement>()
 
-    const { container } = render(<Select ref={ref} />)
+    render(<Select ref={ref} />)
 
-    const component = container.querySelector(':only-child')
+    const component = screen.getByRole('combobox')
 
     expect(ref.current).toBe(component)
+  })
+
+  it('renders options', () => {
+    render(
+      <Select>
+        <Select.Option value="a">Option A</Select.Option>
+        <Select.Option value="b">Option B</Select.Option>
+      </Select>,
+    )
+
+    const select = screen.getByRole('combobox')
+
+    const option = screen.getByRole('option', {
+      name: 'Option B',
+    })
+
+    expect(select).toContain(option)
+  })
+
+  it('can be disabled', () => {
+    render(<Select disabled />)
+
+    const component = screen.getByRole('combobox')
+
+    expect(component).toBeDisabled()
+  })
+
+  it('can be invalid', () => {
+    render(<Select invalid />)
+
+    const component = screen.getByRole('combobox')
+
+    expect(component).toHaveClass('ams-select--invalid')
+  })
+
+  it('is not required by default', () => {
+    render(<Select />)
+
+    const component = screen.getByRole('combobox')
+
+    expect(component).not.toBeRequired()
+  })
+
+  it('omits the required attribute when not required', () => {
+    render(<Select required={false} />)
+
+    const component = screen.getByRole('combobox')
+
+    expect(component).not.toHaveAttribute('required')
   })
 })
