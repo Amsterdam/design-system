419ee7055e20c3ecd6563d5e1ead4fbc45653dc3
dlnr <niels@dlnr.dev>
2024-04-04 07:05:51 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@989046fb916b26f24a0c3e6b501ec3dc37b3d4b8 🚀

---
+++ demo-DES-662-Select/assets/Spotlight.stories-BWgDNKsN.js
@@ -0,0 +1,37 @@
+import{j as m}from"./jsx-runtime-CKrituN3.js";import{Z as d,G as g,e as H}from"./index.esm-UwBrYMaQ.js";import{a as I}from"./exampleContent-BoywlOQx.js";const J=I(),K={title:"Components/Containers/Spotlight",component:d,argTypes:{as:{control:{type:"radio"},options:["article","aside","div","footer","section"]},color:{control:{type:"select"},options:["blue","dark-blue","dark-green","green","magenta","orange","purple","yellow"]}},render:({as:F,color:u})=>m.jsx(d,{as:F,color:u,children:m.jsx(g,{paddingVertical:"medium",children:m.jsx(g.Cell,{span:"all",children:m.jsx(H,{inverseColor:!u||!["green","yellow"].includes(u),children:J})})})})},r={},e={args:{color:"blue"}},a={args:{color:"dark-blue"}},o={args:{color:"dark-green"}},s={args:{color:"green"}},n={args:{color:"magenta"}},c={args:{color:"orange"}},t={args:{color:"purple"}},l={args:{color:"yellow"}},p={args:{as:"section"}};var i,S,k;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:"{}",...(k=(S=r.parameters)==null?void 0:S.docs)==null?void 0:k.source}}};var b,f,x;e.parameters={...e.parameters,docs:{...(b=e.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    color: 'blue'
+  }
+}`,...(x=(f=e.parameters)==null?void 0:f.docs)==null?void 0:x.source}}};var y,j,h;a.parameters={...a.parameters,docs:{...(y=a.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    color: 'dark-blue'
+  }
+}`,...(h=(j=a.parameters)==null?void 0:j.docs)==null?void 0:h.source}}};var C,D,G;o.parameters={...o.parameters,docs:{...(C=o.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  args: {
+    color: 'dark-green'
+  }
+}`,...(G=(D=o.parameters)==null?void 0:D.docs)==null?void 0:G.source}}};var w,B,O;s.parameters={...s.parameters,docs:{...(w=s.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  args: {
+    color: 'green'
+  }
+}`,...(O=(B=s.parameters)==null?void 0:B.docs)==null?void 0:O.source}}};var _,M,P;n.parameters={...n.parameters,docs:{...(_=n.parameters)==null?void 0:_.docs,source:{originalSource:`{
+  args: {
+    color: 'magenta'
+  }
+}`,...(P=(M=n.parameters)==null?void 0:M.docs)==null?void 0:P.source}}};var T,v,Y;c.parameters={...c.parameters,docs:{...(T=c.parameters)==null?void 0:T.docs,source:{originalSource:`{
+  args: {
+    color: 'orange'
+  }
+}`,...(Y=(v=c.parameters)==null?void 0:v.docs)==null?void 0:Y.source}}};var q,E,N;t.parameters={...t.parameters,docs:{...(q=t.parameters)==null?void 0:q.docs,source:{originalSource:`{
+  args: {
+    color: 'purple'
+  }
+}`,...(N=(E=t.parameters)==null?void 0:E.docs)==null?void 0:N.source}}};var z,Q,R;l.parameters={...l.parameters,docs:{...(z=l.parameters)==null?void 0:z.docs,source:{originalSource:`{
+  args: {
+    color: 'yellow'
+  }
+}`,...(R=(Q=l.parameters)==null?void 0:Q.docs)==null?void 0:R.source}}};var V,Z,A;p.parameters={...p.parameters,docs:{...(V=p.parameters)==null?void 0:V.docs,source:{originalSource:`{
+  args: {
+    as: 'section'
+  }
+}`,...(A=(Z=p.parameters)==null?void 0:Z.docs)==null?void 0:A.source}}};const L=["Default","Blue","DarkBlue","DarkGreen","Green","Magenta","Orange","Purple","Yellow","CustomTagName"],$=Object.freeze(Object.defineProperty({__proto__:null,Blue:e,CustomTagName:p,DarkBlue:a,DarkGreen:o,Default:r,Green:s,Magenta:n,Orange:c,Purple:t,Yellow:l,__namedExportsOrder:L,default:K},Symbol.toStringTag,{value:"Module"}));export{e as B,p as C,a as D,s as G,n as M,c as O,t as P,$ as S,l as Y,o as a};
--- demo-DES-662-Select/assets/WithTooltip-Y7J54OF7-XJ84FYsM.js
+++
@@ -1,1 +0,0 @@
-import{W as h,W as l,b as s}from"./index-Re5p2fnn.js";import"./iframe-B8sRRcGs.js";import"../sb-preview/runtime.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-BtM5VmRH.js";import"./index-Bw8VTzHM.js";import"./index-k_s-dmML.js";import"./index-DrFu-skq.js";export{h as WithToolTipState,l as WithTooltip,s as WithTooltipPure};
