e606b98e811e334d64e4d902bbd411c3109ae8c1
dlnr <niels@dlnr.dev>
2024-04-05 07:19:29 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@f8e7a1050effdf82bb95ceafe853f69c7bd3568c 🚀

---
+++ demo-DES-662-Select/assets/Footer.stories-Cwguw6cl.js
@@ -0,0 +1,100 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{n as s,V as d,j as r,G as i,P as o,o as n,p as m,q as L,d as l}from"./index.esm-DWv_GkAh.js";const g={title:"Components/Containers/Footer",component:s},a={args:{children:[e.jsxs(s.Top,{children:[e.jsx(d,{children:e.jsx(r,{children:"Colofon"})}),e.jsxs(i,{gapVertical:"large",paddingVertical:"medium",children:[e.jsx(i.Cell,{span:3,children:e.jsxs("div",{style:{display:"grid",gap:"2.5rem"},children:[e.jsx(r,{level:2,size:"level-4",inverseColor:!0,children:"Contact"}),e.jsx(o,{size:"small",inverseColor:!0,children:"Heeft u een vraag en kunt u het antwoord niet vinden op deze site? Neem dan contact met ons op."}),e.jsxs(n,{children:[e.jsx(n.Link,{href:"mailto:redactie.os@amsterdam.nl",icon:m,onBackground:"dark",size:"small",children:"E-mail"}),e.jsx(n.Link,{href:"tel:+31202510333",icon:L,onBackground:"dark",size:"small",children:"020 251 0333"})]})]})}),e.jsx(i.Cell,{span:3,start:{narrow:1,medium:5,wide:5},children:e.jsxs("div",{style:{display:"grid",gap:"2.5rem"},children:[e.jsx(r,{level:2,size:"level-4",inverseColor:!0,children:"Panels en enquêtes"}),e.jsx(o,{size:"small",inverseColor:!0,children:"Bent u uitgenodigd om mee te doen aan onderzoek of heeft u vragen over het panel of stadspaspanel?"}),e.jsxs(n,{children:[e.jsx(n.Link,{href:"#",onBackground:"dark",rel:"external",size:"small",children:"Meedoen aan onderzoek"}),e.jsx(n.Link,{href:"#",onBackground:"dark",rel:"external",size:"small",children:"Panel Amsterdam"}),e.jsx(n.Link,{href:"#",onBackground:"dark",rel:"external",size:"small",children:"Stadspaspanel Amsterdam"})]})]})}),e.jsx(i.Cell,{span:3,start:{narrow:1,medium:1,wide:9},children:e.jsxs("div",{style:{display:"grid",gap:"2.5rem"},children:[e.jsx(r,{level:2,size:"level-4",inverseColor:!0,children:"Onderzoek en Statistiek"}),e.jsxs(n,{children:[e.jsx(n.Link,{href:"#",onBackground:"dark",size:"small",children:"Over Onderzoek en Statistiek"}),e.jsx(n.Link,{href:"#",onBackground:"dark",size:"small",children:"Veelgestelde vragen"}),e.jsx(n.Link,{href:"#",onBackground:"dark",rel:"external",size:"small",children:"Nieuwsbrief"}),e.jsx(n.Link,{href:"#",onBackground:"dark",size:"small",children:"Vacatures"})]})]})})]})]},"footer-top"),e.jsxs(s.Bottom,{children:[e.jsx(d,{children:e.jsx(r,{level:2,children:"Over deze website"})}),e.jsx(i,{paddingVertical:"small",children:e.jsx(i.Cell,{span:"all",children:e.jsxs(l,{children:[e.jsx(l.Link,{href:"#",children:"Privacy"}),e.jsx(l.Link,{href:"#",children:"Toegankelijkheid"})]})})})]},"footer-bottom")]}};var t,k,c;a.parameters={...a.parameters,docs:{...(t=a.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    children: [<Footer.Top key="footer-top">
+        <VisuallyHidden>
+          <Heading>Colofon</Heading>
+        </VisuallyHidden>
+        <Grid gapVertical="large" paddingVertical="medium">
+          <Grid.Cell span={3}>
+            <div style={{
+            display: 'grid',
+            gap: '2.5rem'
+          }}>
+              <Heading level={2} size="level-4" inverseColor>
+                Contact
+              </Heading>
+              <Paragraph size="small" inverseColor>
+                Heeft u een vraag en kunt u het antwoord niet vinden op deze site? Neem dan contact met ons op.
+              </Paragraph>
+              <LinkList>
+                <LinkList.Link href="mailto:redactie.os@amsterdam.nl" icon={EmailIcon} onBackground="dark" size="small">
+                  E-mail
+                </LinkList.Link>
+                <LinkList.Link href="tel:+31202510333" icon={PhoneIcon} onBackground="dark" size="small">
+                  020 251 0333
+                </LinkList.Link>
+              </LinkList>
+            </div>
+          </Grid.Cell>
+          <Grid.Cell span={3} start={{
+          narrow: 1,
+          medium: 5,
+          wide: 5
+        }}>
+            <div style={{
+            display: 'grid',
+            gap: '2.5rem'
+          }}>
+              <Heading level={2} size="level-4" inverseColor>
+                Panels en enquêtes
+              </Heading>
+              <Paragraph size="small" inverseColor>
+                Bent u uitgenodigd om mee te doen aan onderzoek of heeft u vragen over het panel of stadspaspanel?
+              </Paragraph>
+              <LinkList>
+                <LinkList.Link href="#" onBackground="dark" rel="external" size="small">
+                  Meedoen aan onderzoek
+                </LinkList.Link>
+                <LinkList.Link href="#" onBackground="dark" rel="external" size="small">
+                  Panel Amsterdam
+                </LinkList.Link>
+                <LinkList.Link href="#" onBackground="dark" rel="external" size="small">
+                  Stadspaspanel Amsterdam
+                </LinkList.Link>
+              </LinkList>
+            </div>
+          </Grid.Cell>
+          <Grid.Cell span={3} start={{
+          narrow: 1,
+          medium: 1,
+          wide: 9
+        }}>
+            <div style={{
+            display: 'grid',
+            gap: '2.5rem'
+          }}>
+              <Heading level={2} size="level-4" inverseColor>
+                Onderzoek en Statistiek
+              </Heading>
+              <LinkList>
+                <LinkList.Link href="#" onBackground="dark" size="small">
+                  Over Onderzoek en Statistiek
+                </LinkList.Link>
+                <LinkList.Link href="#" onBackground="dark" size="small">
+                  Veelgestelde vragen
+                </LinkList.Link>
+                <LinkList.Link href="#" onBackground="dark" rel="external" size="small">
+                  Nieuwsbrief
+                </LinkList.Link>
+                <LinkList.Link href="#" onBackground="dark" size="small">
+                  Vacatures
+                </LinkList.Link>
+              </LinkList>
+            </div>
+          </Grid.Cell>
+        </Grid>
+      </Footer.Top>, <Footer.Bottom key="footer-bottom">
+        <VisuallyHidden>
+          <Heading level={2}>Over deze website</Heading>
+        </VisuallyHidden>
+        <Grid paddingVertical="small">
+          <Grid.Cell span="all">
+            <PageMenu>
+              <PageMenu.Link href="#">Privacy</PageMenu.Link>
+              <PageMenu.Link href="#">Toegankelijkheid</PageMenu.Link>
+            </PageMenu>
+          </Grid.Cell>
+        </Grid>
+      </Footer.Bottom>]
+  }
+}`,...(c=(k=a.parameters)==null?void 0:k.docs)==null?void 0:c.source}}};const h=["Default"],v=Object.freeze(Object.defineProperty({__proto__:null,Default:a,__namedExportsOrder:h,default:g},Symbol.toStringTag,{value:"Module"}));export{v as F};
