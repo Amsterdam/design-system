8a112077822aa142954a51619718a7edeeb9d8a7
dlnr <niels@dlnr.dev>
2024-04-05 09:09:38 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@1c8f21fd257ef0ac918d608bdf88e8b1c2504f1c 🚀

---
+++ demo-DES-662-Select/assets/Select.stories-DnRWeOoM.js
@@ -0,0 +1,58 @@
+import{j as t}from"./jsx-runtime-CKrituN3.js";import{X as e}from"./index.esm-D3Qr1elF.js";const G={title:"Components/Forms/Select",component:e,args:{invalid:!1,required:!1,disabled:!1,multiple:!1,children:[t.jsx(e.Option,{disabled:!0,selected:!0,hidden:!0,children:"Selecteer een stadsdeel"},"0"),t.jsx(e.Option,{value:"1",children:"Centrum"},"1"),t.jsx(e.Option,{value:"2",children:"Noord"},"2"),t.jsx(e.Option,{value:"3",children:"West"},"3"),t.jsx(e.Option,{value:"4",disabled:!0,children:"Westpoort"},"4"),t.jsx(e.Option,{value:"5",children:"Nieuw-West"},"6"),t.jsx(e.Option,{value:"6",children:"Zuid"},"7"),t.jsx(e.Option,{value:"7",children:"Zuidoost"},"8")]}},n={},r={args:{multiple:!0}},i={args:{children:[t.jsx(e.Option,{disabled:!0,selected:!0,hidden:!0,children:"Selecteer een wijk"},"0"),t.jsxs(e.Group,{label:"Burgwallen-Oude Zijde",children:[t.jsx(e.Option,{children:"BG-terrein e.o."}),t.jsx(e.Option,{children:"Burgwallen oost"}),t.jsx(e.Option,{children:"Kop Zeedijk"}),t.jsx(e.Option,{children:"Nes e.o."}),t.jsx(e.Option,{children:"Oude Kerk e.o."})]},"1"),t.jsxs(e.Group,{label:"Burgwallen-Nieuwe Zijde",children:[t.jsx(e.Option,{children:"Begijnhofbuurt"}),t.jsx(e.Option,{children:"Hemelrijk"}),t.jsx(e.Option,{children:"Kalverdriehoek"}),t.jsx(e.Option,{children:"Nieuwe Kerk e.o."}),t.jsx(e.Option,{children:"Nieuwendijk Noord"}),t.jsx(e.Option,{children:"Spuistraat Noord"}),t.jsx(e.Option,{children:"Spuistraat Zuid"}),t.jsx(e.Option,{children:"Stationsplein e.o."})]},"2"),t.jsxs(e.Group,{label:"Grachtengordel-West",children:[t.jsx(e.Option,{children:"Felix Meritisbuurt"}),t.jsx(e.Option,{children:"Langestraat e.o."}),t.jsx(e.Option,{children:"Leidsegracht Noord"}),t.jsx(e.Option,{children:"Leliegracht e.o."})]},"3"),t.jsxs(e.Group,{label:"Haarlemmerbuurt",children:[t.jsx(e.Option,{children:"Haarlemmerbuurt Oost"}),t.jsx(e.Option,{children:"Haarlemmerbuurt West"}),t.jsx(e.Option,{children:"Planciusbuurt Noord"}),t.jsx(e.Option,{children:"Planciusbuurt Zuid"}),t.jsx(e.Option,{children:"Westelijke eilanden"}),t.jsx(e.Option,{children:"Westerdokseiland"})]},"4"),t.jsxs(e.Group,{label:"Jordaan",children:[t.jsx(e.Option,{children:"Anjeliersbuurt Noord"}),t.jsx(e.Option,{children:"Anjeliersbuurt Zuid"}),t.jsx(e.Option,{children:"Bloemgrachtbuurt"}),t.jsx(e.Option,{children:"Driehoekbuurt"}),t.jsx(e.Option,{children:"Elandsgrachtbuurt"}),t.jsx(e.Option,{children:"Groenmarktkadebuurt"}),t.jsx(e.Option,{children:"Marnixbuurt Midden"}),t.jsx(e.Option,{children:"Marnixbuurt Noord"}),t.jsx(e.Option,{children:"Marnixbuurt Zuid"}),t.jsx(e.Option,{children:"Passeerdersgrachtbuurt"}),t.jsx(e.Option,{children:"Zaagpoortbuurt"})]},"5")]}},o={args:{invalid:!0}},l={args:{disabled:!0}};var c,s,a;n.parameters={...n.parameters,docs:{...(c=n.parameters)==null?void 0:c.docs,source:{originalSource:"{}",...(a=(s=n.parameters)==null?void 0:s.docs)==null?void 0:a.source}}};var p,u,d;r.parameters={...r.parameters,docs:{...(p=r.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    multiple: true
+  }
+}`,...(d=(u=r.parameters)==null?void 0:u.docs)==null?void 0:d.source}}};var O,S,h;i.parameters={...i.parameters,docs:{...(O=i.parameters)==null?void 0:O.docs,source:{originalSource:`{
+  args: {
+    children: [<Select.Option key="0" disabled selected hidden>
+        Selecteer een wijk
+      </Select.Option>, <Select.Group label="Burgwallen-Oude Zijde" key="1">
+        <Select.Option>BG-terrein e.o.</Select.Option>
+        <Select.Option>Burgwallen oost</Select.Option>
+        <Select.Option>Kop Zeedijk</Select.Option>
+        <Select.Option>Nes e.o.</Select.Option>
+        <Select.Option>Oude Kerk e.o.</Select.Option>
+      </Select.Group>, <Select.Group label="Burgwallen-Nieuwe Zijde" key="2">
+        <Select.Option>Begijnhofbuurt</Select.Option>
+        <Select.Option>Hemelrijk</Select.Option>
+        <Select.Option>Kalverdriehoek</Select.Option>
+        <Select.Option>Nieuwe Kerk e.o.</Select.Option>
+        <Select.Option>Nieuwendijk Noord</Select.Option>
+        <Select.Option>Spuistraat Noord</Select.Option>
+        <Select.Option>Spuistraat Zuid</Select.Option>
+        <Select.Option>Stationsplein e.o.</Select.Option>
+      </Select.Group>, <Select.Group label="Grachtengordel-West" key="3">
+        <Select.Option>Felix Meritisbuurt</Select.Option>
+        <Select.Option>Langestraat e.o.</Select.Option>
+        <Select.Option>Leidsegracht Noord</Select.Option>
+        <Select.Option>Leliegracht e.o.</Select.Option>
+      </Select.Group>, <Select.Group label="Haarlemmerbuurt" key="4">
+        <Select.Option>Haarlemmerbuurt Oost</Select.Option>
+        <Select.Option>Haarlemmerbuurt West</Select.Option>
+        <Select.Option>Planciusbuurt Noord</Select.Option>
+        <Select.Option>Planciusbuurt Zuid</Select.Option>
+        <Select.Option>Westelijke eilanden</Select.Option>
+        <Select.Option>Westerdokseiland</Select.Option>
+      </Select.Group>, <Select.Group label="Jordaan" key="5">
+        <Select.Option>Anjeliersbuurt Noord</Select.Option>
+        <Select.Option>Anjeliersbuurt Zuid</Select.Option>
+        <Select.Option>Bloemgrachtbuurt</Select.Option>
+        <Select.Option>Driehoekbuurt</Select.Option>
+        <Select.Option>Elandsgrachtbuurt</Select.Option>
+        <Select.Option>Groenmarktkadebuurt</Select.Option>
+        <Select.Option>Marnixbuurt Midden</Select.Option>
+        <Select.Option>Marnixbuurt Noord</Select.Option>
+        <Select.Option>Marnixbuurt Zuid</Select.Option>
+        <Select.Option>Passeerdersgrachtbuurt</Select.Option>
+        <Select.Option>Zaagpoortbuurt</Select.Option>
+      </Select.Group>]
+  }
+}`,...(h=(S=i.parameters)==null?void 0:S.docs)==null?void 0:h.source}}};var j,x,b;o.parameters={...o.parameters,docs:{...(j=o.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  args: {
+    invalid: true
+  }
+}`,...(b=(x=o.parameters)==null?void 0:x.docs)==null?void 0:b.source}}};var m,g,k;l.parameters={...l.parameters,docs:{...(m=l.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    disabled: true
+  }
+}`,...(k=(g=l.parameters)==null?void 0:g.docs)==null?void 0:k.source}}};const N=["Default","Multiple","Grouped","Invalid","Disabled"],w=Object.freeze(Object.defineProperty({__proto__:null,Default:n,Disabled:l,Grouped:i,Invalid:o,Multiple:r,__namedExportsOrder:N,default:G},Symbol.toStringTag,{value:"Module"}));export{l as D,i as G,o as I,r as M,w as S};
