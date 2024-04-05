b691e2c6136e76a62daca766295b9298e2dccf1a
alimpens <37216945+alimpens@users.noreply.github.com>
2024-04-05 13:30:28 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@7a1e3102b754482811c828eb32cfd7839e6a2adb 🚀

--- demo-DES-666-add-date-input/assets/FormLabel.stories-DyQRITdL.js
+++
@@ -1,4 +0,0 @@
-import{t as a}from"./index.esm-BWQ6Y7Jy.js";/**
- * @license EUPL-1.2+
- * Copyright Gemeente Amsterdam
- */const s={title:"Components/Forms/Form Label",component:a,args:{children:"Form label"},argTypes:{children:{table:{disable:!1}}}},e={};var r,o,t;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:"{}",...(t=(o=e.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};const l=["Default"],c=Object.freeze(Object.defineProperty({__proto__:null,Default:e,__namedExportsOrder:l,default:s},Symbol.toStringTag,{value:"Module"}));export{c as F};
