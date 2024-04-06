7d5de7759ca2909a8b591a44a6d653c024a9444d
RubenSibon <mail@rubensibon.nl>
2024-04-06 10:44:23 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@3b066514a25e51c9724977f5f1f2a4a32eeb6544 🚀

---
+++ demo-web-manifest-app-icons-and-favicon/README.md
@@ -0,0 +1,9 @@
+<!-- @license CC0-1.0 -->
+
+# Assets
+
+Assets for the City of Amsterdam
+
+## Web app manifest, app icons and favicon
+
+See the Design System documentation for an [overview and examples of the manifest and these icons](https://amsterdam.github.io/design-system/?path=/docs/docs-assets-app-icons-favicon--docs).
---
+++ demo-web-manifest-app-icons-and-favicon/assets/notice.docs-C6nP5qX2.js
@@ -0,0 +1,23 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{useMDXComponents as r}from"./index-DSz_1G2r.js";import{M as s,d as a}from"./index-CyOHno8t.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-BfDRRBzJ.js";import"../sb-preview/runtime.js";import"./index-BtM5VmRH.js";import"./index-Bw8VTzHM.js";import"./index-k_s-dmML.js";import"./index-DrFu-skq.js";const i=`<!-- @license CC0-1.0 -->
+
+# Terms of Use
+
+## Branding, logo and authors’ rights
+
+The branding and logo are used exclusively by the City of Amsterdam.
+Authors’ rights and trademark rights apply to the logo.
+If you intend to use a modified version of the software for other purposes, you are not allowed to use the City of Amsterdam’s logo.
+Instead, you should design your own branding.
+
+## Fonts
+
+Not all fonts used in the branding are free and open source.
+Please note that you must arrange a (paid) license if you use the provided fonts.
+Alternatively, adjust the configuration to use fewer or different fonts.
+Amsterdam Sans is licensed exclusively to the City of Amsterdam and amsterdam&partners.
+
+## Permission
+
+For permission to use the logo or branding, please contact <designsystem@amsterdam.nl>.
+`;function o(n){return e.jsxs(e.Fragment,{children:[e.jsx(s,{title:"Docs/Terms of Use"}),`
+`,e.jsx(a,{children:i})]})}function C(n={}){const{wrapper:t}={...r(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{C as default};
