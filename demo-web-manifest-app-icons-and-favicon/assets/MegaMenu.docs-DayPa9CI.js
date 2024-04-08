ffd5ff4e80e7c5d6d334b52bbbc9026084d534ab
RubenSibon <mail@rubensibon.nl>
2024-04-08 15:18:31 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@c9db5ed715dc1e956546ae7fa228aed98e5cd83a 🚀

--- demo-web-manifest-app-icons-and-favicon/assets/MegaMenu.docs-DayPa9CI.js
+++
@@ -1,8 +0,0 @@
-import{j as t}from"./jsx-runtime-CKrituN3.js";import{useMDXComponents as i}from"./index-DSz_1G2r.js";import{M as e,d as m,P as s,f as a}from"./index-BsTBr-3h.js";import{M as p,a as c}from"./MegaMenu.stories-DP9k6s-e.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-k3LuM_GF.js";import"../sb-preview/runtime.js";import"./index-BtM5VmRH.js";import"./index-Bw8VTzHM.js";import"./index-k_s-dmML.js";import"./index-DrFu-skq.js";import"./clsx-B-dksMZM.js";import"./Screen-CBGDXr7L.js";import"./Grid-xhYoXT5x.js";import"./Heading-DlTzCZbz.js";import"./getHeadingElement-DiRwFEz7.js";import"./LinkList-BmpxO9J_.js";import"./index.esm-DjZ2yxjc.js";import"./Icon-DW52t4nJ.js";const M=`<!-- @license CC0-1.0 -->
-
-# Mega Menu
-`;function n(o){const r={h2:"h2",...i(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(e,{of:p}),`
-`,t.jsx(m,{children:M}),`
-`,t.jsx(s,{}),`
-`,t.jsx(r.h2,{id:"with-categories",children:"With Categories"}),`
-`,t.jsx(a,{of:c})]})}function F(o={}){const{wrapper:r}={...i(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(n,{...o})}):n(o)}export{F as default};
