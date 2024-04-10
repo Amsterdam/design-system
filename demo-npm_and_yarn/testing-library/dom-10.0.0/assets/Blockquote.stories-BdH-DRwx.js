900e0fb054a13077ee803e3118c56c2e7a00c9a0
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-04-10 11:39:00 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@185bb987ff3a46d0821764e13b6d7be3cc491f08 🚀

---
+++ demo-npm_and_yarn/testing-library/dom-10.0.0/assets/Blockquote.stories-BdH-DRwx.js
@@ -0,0 +1,12 @@
+import{j as o}from"./jsx-runtime-CKrituN3.js";import{e as m}from"./exampleContent-DcLVbCjv.js";import{B as u}from"./Blockquote-BGaZngnq.js";const p=m(),v={title:"Components/Text/Blockquote",component:u,args:{children:p,inverseColor:!1},argTypes:{children:{table:{disable:!1}}}},e={decorators:[(s,a)=>o.jsx("div",{className:a.args.inverseColor?"ams-docs-dark-background":void 0,children:o.jsx(s,{})})]},r={args:{inverseColor:!0},decorators:[(s,a)=>o.jsx("div",{className:a.args.inverseColor?"ams-docs-dark-background":void 0,children:o.jsx(s,{})})]};var t,n,c;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  decorators: [(Story, context) => <div className={context.args.inverseColor ? 'ams-docs-dark-background' : undefined}>
+        <Story />
+      </div>]
+}`,...(c=(n=e.parameters)==null?void 0:n.docs)==null?void 0:c.source}}};var d,l,i;r.parameters={...r.parameters,docs:{...(d=r.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    inverseColor: true
+  },
+  decorators: [(Story, context) => <div className={context.args.inverseColor ? 'ams-docs-dark-background' : undefined}>
+        <Story />
+      </div>]
+}`,...(i=(l=r.parameters)==null?void 0:l.docs)==null?void 0:i.source}}};const g=["Default","InvertedColor"],C=Object.freeze(Object.defineProperty({__proto__:null,Default:e,InvertedColor:r,__namedExportsOrder:g,default:v},Symbol.toStringTag,{value:"Module"}));export{C as B,r as I};
