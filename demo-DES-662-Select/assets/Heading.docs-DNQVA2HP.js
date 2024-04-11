86a70198cb8f7425378916f7eb83273bc08557c7
dlnr <niels@dlnr.dev>
2024-04-11 12:26:08 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@6d47507dccd84cc4e60ccebc796fdc97dccff299 🚀

---
+++ demo-DES-662-Select/assets/Heading.docs-DNQVA2HP.js
@@ -0,0 +1,45 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{useMDXComponents as i}from"./index-DSz_1G2r.js";import{M as r,d as a,P as h,e as d,f as s}from"./index-CxB5A_3J.js";import{H as c,a as g,b as l,c as p,d as u,I as m}from"./Heading.stories-DcpWnylf.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-CIPCL-CF.js";import"../sb-preview/runtime.js";import"./index-BtM5VmRH.js";import"./index-Bw8VTzHM.js";import"./index-k_s-dmML.js";import"./index-DrFu-skq.js";import"./exampleContent-CRK3_7Hv.js";import"./Heading-DlTzCZbz.js";import"./clsx-B-dksMZM.js";import"./getHeadingElement-DiRwFEz7.js";const w=`<!-- @license CC0-1.0 -->
+
+# Heading
+
+A heading conveys information about the content below it.
+Use headings to allow the user to grasp the structure of the page quickly.
+
+## Guidelines
+
+- A heading describes the content beneath it.
+- Do not use a heading for formatting and styling. Use it to represent the page’s structure.
+- Use headings hierarchically, and do not skip heading levels.
+  So, an \`h1\` heading should be followed by an \`h2\`, not an \`h3\`.
+
+## Relevant WCAG Requirements
+
+- [WCAG 1.3.1](https://www.w3.org/WAI/WCAG21/quickref/#qr-content-structure-separation-programmatic):
+  Text that looks like a heading is also recognized as a heading by a computer.
+  Using the correct heading level makes the page structure clear for all users.
+- [WCAG 2.4.1](https://www.w3.org/WAI/WCAG21/quickref/#qr-navigation-mechanisms-skip): Headings can be used to navigate to the next section with the keyboard.
+- [WCAG 2.4.6](https://www.w3.org/WAI/WCAG21/quickref/#headings-and-labels): Use headings to describe the purpose or subject of the underlying content.
+- [WCAG 2.4.10](https://www.w3.org/WAI/WCAG21/quickref/#qr-navigation-mechanisms-headings): Use headings to structure the content.
+
+## References
+
+- [W3C - Headings](https://www.w3.org/WAI/tutorials/page-structure/headings/)
+- [W3C - Headings & labels](https://www.w3.org/TR/WCAG21/#headings-and-labels)
+- [A11Y project - Heading structure](https://www.a11yproject.com/posts/how-to-accessible-heading-structure/)
+`;function o(t){const n={code:"code",h2:"h2",h3:"h3",p:"p",...i(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(r,{of:c}),`
+`,e.jsx(a,{children:w}),`
+`,e.jsx(h,{}),`
+`,e.jsx(d,{}),`
+`,e.jsx(n.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(n.h3,{id:"heading-1",children:"Heading 1"}),`
+`,e.jsx(s,{of:g}),`
+`,e.jsx(n.h3,{id:"heading-2",children:"Heading 2"}),`
+`,e.jsx(s,{of:l}),`
+`,e.jsx(n.h3,{id:"heading-3",children:"Heading 3"}),`
+`,e.jsx(s,{of:p}),`
+`,e.jsx(n.h3,{id:"heading-4",children:"Heading 4"}),`
+`,e.jsx(s,{of:u}),`
+`,e.jsx(n.h3,{id:"inverse-colour",children:"Inverse colour"}),`
+`,e.jsxs(n.p,{children:["Set the ",e.jsx(n.code,{children:"inverseColor"}),` prop if the Heading sits on a dark background.
+This ensures the colour of the text provides enough contrast.`]}),`
+`,e.jsx(s,{of:m})]})}function U(t={}){const{wrapper:n}={...i(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(o,{...t})}):o(t)}export{U as default};
