2dfc112cfb6b574c39faae287a33cbc115aef711
dlnr <niels@dlnr.dev>
2024-04-11 08:28:28 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@5876bc1f98237cf6501d158ae8b344f0a57c739e 🚀

---
+++ demo-DES-717-refactor-inverseColor-stories/assets/Heading.stories-S_DRi3pm.js
@@ -0,0 +1,17 @@
+import{j as t}from"./jsx-runtime-CKrituN3.js";import{a as O}from"./exampleContent-DcLVbCjv.js";import{H as T}from"./Heading-DlTzCZbz.js";const k=O(),D={title:"Components/Text/Heading",component:T,args:{children:k,inverseColor:!1},argTypes:{children:{table:{disable:!1}}},decorators:[(y,I)=>t.jsx("div",{className:I.args.inverseColor?"ams-docs-dark-background":void 0,children:t.jsx(y,{})})]},e={},r={},a={args:{level:2}},s={args:{level:3}},o={args:{level:4}},n={args:{inverseColor:!0}};var c,d,i;e.parameters={...e.parameters,docs:{...(c=e.parameters)==null?void 0:c.docs,source:{originalSource:"{}",...(i=(d=e.parameters)==null?void 0:d.docs)==null?void 0:i.source}}};var l,m,p;r.parameters={...r.parameters,docs:{...(l=r.parameters)==null?void 0:l.docs,source:{originalSource:"{}",...(p=(m=r.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var g,u,v;a.parameters={...a.parameters,docs:{...(g=a.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    level: 2
+  }
+}`,...(v=(u=a.parameters)==null?void 0:u.docs)==null?void 0:v.source}}};var H,f,S;s.parameters={...s.parameters,docs:{...(H=s.parameters)==null?void 0:H.docs,source:{originalSource:`{
+  args: {
+    level: 3
+  }
+}`,...(S=(f=s.parameters)==null?void 0:f.docs)==null?void 0:S.source}}};var x,b,C;o.parameters={...o.parameters,docs:{...(x=o.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  args: {
+    level: 4
+  }
+}`,...(C=(b=o.parameters)==null?void 0:b.docs)==null?void 0:C.source}}};var j,_,h;n.parameters={...n.parameters,docs:{...(j=n.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  args: {
+    inverseColor: true
+  }
+}`,...(h=(_=n.parameters)==null?void 0:_.docs)==null?void 0:h.source}}};const E=["Default","Heading1","Heading2","Heading3","Heading4","InvertedColor"],P=Object.freeze(Object.defineProperty({__proto__:null,Default:e,Heading1:r,Heading2:a,Heading3:s,Heading4:o,InvertedColor:n,__namedExportsOrder:E,default:D},Symbol.toStringTag,{value:"Module"}));export{P as H,n as I,r as a,a as b,s as c,o as d};
---
+++ demo-DES-717-refactor-inverseColor-stories/assets/Paragraph.stories-BEvtSh0s.js
@@ -0,0 +1,13 @@
+import{j as o}from"./jsx-runtime-CKrituN3.js";import{c as x}from"./exampleContent-DcLVbCjv.js";import{P as b}from"./Paragraph-Im3SnT_h.js";const C=x(),P={title:"Components/Text/Paragraph",component:b,args:{children:C,inverseColor:!1},argTypes:{children:{table:{disable:!1}},size:{control:{type:"radio",labels:{small:"small",undefined:"medium",large:"large"}},options:["small",void 0,"large"]}},decorators:[(S,h)=>o.jsx("div",{className:h.args.inverseColor?"ams-docs-dark-background":void 0,children:o.jsx(S,{})})]},r={},e={args:{size:"large"}},a={args:{size:"small"}},s={args:{inverseColor:!0}};var t,l,n;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:"{}",...(n=(l=r.parameters)==null?void 0:l.docs)==null?void 0:n.source}}};var c,m,i;e.parameters={...e.parameters,docs:{...(c=e.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    size: 'large'
+  }
+}`,...(i=(m=e.parameters)==null?void 0:m.docs)==null?void 0:i.source}}};var p,d,g;a.parameters={...a.parameters,docs:{...(p=a.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    size: 'small'
+  }
+}`,...(g=(d=a.parameters)==null?void 0:d.docs)==null?void 0:g.source}}};var u,f,v;s.parameters={...s.parameters,docs:{...(u=s.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    inverseColor: true
+  }
+}`,...(v=(f=s.parameters)==null?void 0:f.docs)==null?void 0:v.source}}};const j=["Default","Large","Small","InvertedColor"],I=Object.freeze(Object.defineProperty({__proto__:null,Default:r,InvertedColor:s,Large:e,Small:a,__namedExportsOrder:j,default:P},Symbol.toStringTag,{value:"Module"}));export{s as I,e as L,I as P,a as S};
