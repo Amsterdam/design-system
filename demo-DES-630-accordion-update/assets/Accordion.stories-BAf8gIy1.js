d9f10ed469681ca2b9b4cd4e3529b2f5935ade1a
dlnr <niels@dlnr.dev>
2024-04-12 09:47:33 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@ee90a41d58b45adb8ae03d6c625017927533821e 🚀

---
+++ demo-DES-630-accordion-update/assets/Accordion.stories-BAf8gIy1.js
@@ -0,0 +1,38 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{c as S}from"./exampleContent-DcLVbCjv.js";import{c as p}from"./clsx-B-dksMZM.js";import{r as n}from"./index-CBqU2yxZ.js";import{t as O}from"./index.esm-DjZ2yxjc.js";import{g as $}from"./getHeadingElement-DiRwFEz7.js";import{I as H}from"./Icon-DW52t4nJ.js";import{u as M}from"./useKeyboardFocus-CRUFsA_C.js";import{P as r}from"./Paragraph-Im3SnT_h.js";/**
+ * @license EUPL-1.2+
+ * Copyright Gemeente Amsterdam
+ */const z={headingLevel:1,section:!0},B=n.createContext(z),T=B;try{Context.displayName="Context",Context.__docgenInfo={description:`Context lets components pass information deep down without explicitly
+passing props.
+
+Created from {@link createContext}`,displayName:"Context",props:{}}}catch{}const I=n.forwardRef(({label:o,expanded:m=!1,children:d,className:h,...u},c)=>{const{headingLevel:g,section:R}=n.useContext(T),[l,V]=n.useState(m),q=$(g),A=n.useId(),x=`button-${A}`,_=`panel-${A}`;return e.jsxs("div",{className:p("ams-accordion__section",h),ref:c,...u,children:[e.jsx(q,{className:"ams-accordion__header",children:e.jsxs("button",{"aria-controls":_,"aria-expanded":l,className:"ams-accordion__button",id:x,onClick:()=>V(!l),type:"button",children:[e.jsx(H,{svg:O,size:"level-5",className:"ams-accordion__icon"}),o]})}),R?e.jsx("section",{id:_,"aria-labelledby":x,className:p("ams-accordion__panel",{"ams-accordion__panel--expanded":l}),children:d}):e.jsx("div",{id:_,"aria-labelledby":x,className:p("ams-accordion__panel",{"ams-accordion__panel--expanded":l}),children:d})]})});I.displayName="Accordion.Section";try{Accordion.Section.displayName="Accordion.Section",Accordion.Section.__docgenInfo={description:"",displayName:"Accordion.Section",props:{label:{defaultValue:null,description:"",name:"label",required:!0,type:{name:"string"}},expanded:{defaultValue:{value:"false"},description:"",name:"expanded",required:!1,type:{name:"boolean"}}}}}catch{}const L=n.forwardRef(({children:o,className:m,headingLevel:d,section:h=!0},u)=>{const c=n.useRef(null);n.useImperativeHandle(u,()=>c.current);const{keyDown:g}=M(c,{rotating:!0});return e.jsx(T.Provider,{value:{headingLevel:d,section:h},children:e.jsx("div",{className:p("ams-accordion",m),onKeyDown:g,ref:c,children:o})})});L.displayName="Accordion";const a=Object.assign(L,{Section:I});try{a.displayName="Accordion",a.__docgenInfo={description:"",displayName:"Accordion",props:{headingLevel:{defaultValue:null,description:"The hierarchical level of the accordion title within the document.",name:"headingLevel",required:!0,type:{name:"enum",value:[{value:"1"},{value:"2"},{value:"3"},{value:"4"}]}},section:{defaultValue:{value:"true"},description:"",name:"section",required:!1,type:{name:"boolean"}}}}}catch{}const f=S(),y=S(),b=S(),K={title:"Components/Containers/Accordion",component:a,args:{headingLevel:1}},t={args:{children:[e.jsx(a.Section,{label:"Eerste sectie",children:e.jsx(r,{children:f})},1),e.jsx(a.Section,{label:"Tweede sectie",children:e.jsx(r,{children:y})},2),e.jsx(a.Section,{label:"Derde sectie",children:e.jsx(r,{children:b})},3)]}},i={args:{children:[e.jsx(a.Section,{label:"Eerste sectie",children:e.jsx(r,{children:f})},1),e.jsx(a.Section,{label:"Tweede sectie",expanded:!0,children:e.jsx(r,{children:y})},2),e.jsx(a.Section,{label:"Derde sectie",children:e.jsx(r,{children:b})},3)]}},s={args:{section:!1,children:[e.jsx(a.Section,{label:"Eerste sectie",children:e.jsx(r,{children:f})},1),e.jsx(a.Section,{label:"Tweede sectie",children:e.jsx(r,{children:y})},2),e.jsx(a.Section,{label:"Derde sectie",children:e.jsx(r,{children:b})},3)]}};var j,v,P;t.parameters={...t.parameters,docs:{...(j=t.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  args: {
+    children: [<Accordion.Section key={1} label="Eerste sectie">
+        <Paragraph>{paragraph1}</Paragraph>
+      </Accordion.Section>, <Accordion.Section key={2} label="Tweede sectie">
+        <Paragraph>{paragraph2}</Paragraph>
+      </Accordion.Section>, <Accordion.Section key={3} label="Derde sectie">
+        <Paragraph>{paragraph3}</Paragraph>
+      </Accordion.Section>]
+  }
+}`,...(P=(v=t.parameters)==null?void 0:v.docs)==null?void 0:P.source}}};var C,E,N;i.parameters={...i.parameters,docs:{...(C=i.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  args: {
+    children: [<Accordion.Section key={1} label="Eerste sectie">
+        <Paragraph>{paragraph1}</Paragraph>
+      </Accordion.Section>, <Accordion.Section key={2} label="Tweede sectie" expanded>
+        <Paragraph>{paragraph2}</Paragraph>
+      </Accordion.Section>, <Accordion.Section key={3} label="Derde sectie">
+        <Paragraph>{paragraph3}</Paragraph>
+      </Accordion.Section>]
+  }
+}`,...(N=(E=i.parameters)==null?void 0:E.docs)==null?void 0:N.source}}};var k,w,D;s.parameters={...s.parameters,docs:{...(k=s.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  args: {
+    section: false,
+    children: [<Accordion.Section key={1} label="Eerste sectie">
+        <Paragraph>{paragraph1}</Paragraph>
+      </Accordion.Section>, <Accordion.Section key={2} label="Tweede sectie">
+        <Paragraph>{paragraph2}</Paragraph>
+      </Accordion.Section>, <Accordion.Section key={3} label="Derde sectie">
+        <Paragraph>{paragraph3}</Paragraph>
+      </Accordion.Section>]
+  }
+}`,...(D=(w=s.parameters)==null?void 0:w.docs)==null?void 0:D.source}}};const F=["Default","ExpandedByDefault","TooManyLandmarks"],ae=Object.freeze(Object.defineProperty({__proto__:null,Default:t,ExpandedByDefault:i,TooManyLandmarks:s,__namedExportsOrder:F,default:K},Symbol.toStringTag,{value:"Module"}));export{ae as A,i as E,s as T};
---
+++ demo-DES-630-accordion-update/assets/OrderedList.stories-FV8Xikx9.js
@@ -0,0 +1,72 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{f as T}from"./exampleContent-DcLVbCjv.js";import{c as D}from"./clsx-B-dksMZM.js";import{r as P}from"./index-CBqU2yxZ.js";import{H as p}from"./Heading-DlTzCZbz.js";import{P as d}from"./Paragraph-Im3SnT_h.js";const B=P.forwardRef(({children:s,className:t,...c},u)=>e.jsx("li",{ref:u,className:D("ams-ordered-list__item",t),...c,children:s}));B.displayName="OrderedList.Item";try{OrderedList.Item.displayName="OrderedList.Item",OrderedList.Item.__docgenInfo={description:"",displayName:"OrderedList.Item",props:{}}}catch{}const E=P.forwardRef(({children:s,className:t,inverseColor:c,markers:u=!0,...C},H)=>e.jsx("ol",{ref:H,className:D("ams-ordered-list",c&&"ams-ordered-list--inverse-color",!u&&"ams-ordered-list--no-markers",t),...C,children:s}));E.displayName="OrderedList";const r=Object.assign(E,{Item:B});try{r.displayName="OrderedList",r.__docgenInfo={description:"",displayName:"OrderedList",props:{markers:{defaultValue:{value:"true"},description:"",name:"markers",required:!1,type:{name:"boolean"}},inverseColor:{defaultValue:null,description:"",name:"inverseColor",required:!1,type:{name:"boolean"}}}}}catch{}const A=T().map((s,t)=>e.jsx(r.Item,{children:s},t)),G={title:"Components/Text/Ordered List",component:r,args:{children:A,inverseColor:!1,markers:void 0,reversed:void 0,start:void 0},argTypes:{reversed:{control:"boolean"},start:{control:"number"}},decorators:[(s,t)=>e.jsx("div",{className:t.args.inverseColor?"ams-docs-dark-background":void 0,children:e.jsx(s,{})})]},a={},n={render:s=>e.jsxs(r,{...s,children:[e.jsxs(r.Item,{children:["Stadsdeel West",e.jsxs(r,{...s,children:[e.jsx(r.Item,{children:"Bos en Lommer"}),e.jsx(r.Item,{children:"Oud West / De Baarsjes"}),e.jsx(r.Item,{children:"Westerpark"})]})]}),e.jsxs(r.Item,{children:["Stadsdeel Nieuw-West",e.jsxs(r,{...s,children:[e.jsx(r.Item,{children:"De Aker, Sloten en Nieuw Sloten"}),e.jsx(r.Item,{children:"Geuzenveld, Slotermeer en Sloterdijken"}),e.jsx(r.Item,{children:"Osdorp"}),e.jsx(r.Item,{children:"Slotervaart"})]})]})]})},i={args:{children:[e.jsx(r.Item,{children:"Zes"},6),e.jsx(r.Item,{children:"Zeven"},7),e.jsx(r.Item,{children:"Acht"},8)],start:6}},o={args:{children:[e.jsx(r.Item,{children:"Drie"},3),e.jsx(r.Item,{children:"Twee"},2),e.jsx(r.Item,{children:"Eén"},1)],reversed:!0,start:3}},l={args:{children:[e.jsxs(r.Item,{children:[e.jsx(p,{size:"level-4",children:"Weg met steen, hallo extra groen en koelte"}),e.jsx(d,{children:"Sinds 2021 kwamen er maar liefst 60 nieuwe groene plekken bij in de stad. Groen is fijn en het verkoelt de stad in de zomer. Een paar voorbeelden."}),e.jsx(d,{size:"small",children:"16 augustus 2023"})]},1),e.jsxs(r.Item,{children:[e.jsx(p,{size:"level-4",children:"Amsterdam bindt de strijd aan met lawaaierige voertuigen"}),e.jsx(d,{children:"Deze zomer testen we of digitale borden langs de weg kunnen helpen om geluidsoverlast van voertuigen zoals motoren en auto’s tegen te gaan."}),e.jsx(d,{size:"small",children:"10 augustus 2023"})]},2),e.jsxs(r.Item,{children:[e.jsx(p,{size:"level-4",children:"Een prachtroute door de wonderlijke Baarsjes"}),e.jsx(d,{children:"In de Baarsjes zijn kunst en cultuur met elkaar vervlochten. We zetten een prachtige wandelroute voor u uit en laten zien hoe het was en hoe het nu is."}),e.jsx(d,{size:"small",children:"8 augustus 2023"})]},3)],markers:!1}},m={args:{inverseColor:!0},render:s=>e.jsx(r,{...s,children:e.jsxs(r.Item,{children:["Stadsdeel West",e.jsxs(r,{...s,children:[e.jsx(r.Item,{children:"Bos en Lommer"}),e.jsx(r.Item,{children:"Oud West / De Baarsjes"}),e.jsx(r.Item,{children:"Westerpark"})]})]})})};var g,h,I;a.parameters={...a.parameters,docs:{...(g=a.parameters)==null?void 0:g.docs,source:{originalSource:"{}",...(I=(h=a.parameters)==null?void 0:h.docs)==null?void 0:I.source}}};var O,L,j;n.parameters={...n.parameters,docs:{...(O=n.parameters)==null?void 0:O.docs,source:{originalSource:`{
+  render: args => <OrderedList {...args}>
+      <OrderedList.Item>
+        Stadsdeel West
+        <OrderedList {...args}>
+          <OrderedList.Item>Bos en Lommer</OrderedList.Item>
+          <OrderedList.Item>Oud West / De Baarsjes</OrderedList.Item>
+          <OrderedList.Item>Westerpark</OrderedList.Item>
+        </OrderedList>
+      </OrderedList.Item>
+      <OrderedList.Item>
+        Stadsdeel Nieuw-West
+        <OrderedList {...args}>
+          <OrderedList.Item>De Aker, Sloten en Nieuw Sloten</OrderedList.Item>
+          <OrderedList.Item>Geuzenveld, Slotermeer en Sloterdijken</OrderedList.Item>
+          <OrderedList.Item>Osdorp</OrderedList.Item>
+          <OrderedList.Item>Slotervaart</OrderedList.Item>
+        </OrderedList>
+      </OrderedList.Item>
+    </OrderedList>
+}`,...(j=(L=n.parameters)==null?void 0:L.docs)==null?void 0:j.source}}};var v,x,k;i.parameters={...i.parameters,docs:{...(v=i.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    children: [<OrderedList.Item key={6}>Zes</OrderedList.Item>, <OrderedList.Item key={7}>Zeven</OrderedList.Item>, <OrderedList.Item key={8}>Acht</OrderedList.Item>],
+    start: 6
+  }
+}`,...(k=(x=i.parameters)==null?void 0:x.docs)==null?void 0:k.source}}};var f,S,z;o.parameters={...o.parameters,docs:{...(f=o.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    children: [<OrderedList.Item key={3}>Drie</OrderedList.Item>, <OrderedList.Item key={2}>Twee</OrderedList.Item>, <OrderedList.Item key={1}>Eén</OrderedList.Item>],
+    reversed: true,
+    start: 3
+  }
+}`,...(z=(S=o.parameters)==null?void 0:S.docs)==null?void 0:z.source}}};var w,y,_;l.parameters={...l.parameters,docs:{...(w=l.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  args: {
+    children: [<OrderedList.Item key={1}>
+        <Heading size="level-4">Weg met steen, hallo extra groen en koelte</Heading>
+        <Paragraph>
+          Sinds 2021 kwamen er maar liefst 60 nieuwe groene plekken bij in de stad. Groen is fijn en het verkoelt de
+          stad in de zomer. Een paar voorbeelden.
+        </Paragraph>
+        <Paragraph size="small">16 augustus 2023</Paragraph>
+      </OrderedList.Item>, <OrderedList.Item key={2}>
+        <Heading size="level-4">Amsterdam bindt de strijd aan met lawaaierige voertuigen</Heading>
+        <Paragraph>
+          Deze zomer testen we of digitale borden langs de weg kunnen helpen om geluidsoverlast van voertuigen zoals
+          motoren en auto’s tegen te gaan.
+        </Paragraph>
+        <Paragraph size="small">10 augustus 2023</Paragraph>
+      </OrderedList.Item>, <OrderedList.Item key={3}>
+        <Heading size="level-4">Een prachtroute door de wonderlijke Baarsjes</Heading>
+        <Paragraph>
+          In de Baarsjes zijn kunst en cultuur met elkaar vervlochten. We zetten een prachtige wandelroute voor u uit en
+          laten zien hoe het was en hoe het nu is.
+        </Paragraph>
+        <Paragraph size="small">8 augustus 2023</Paragraph>
+      </OrderedList.Item>],
+    markers: false
+  }
+}`,...(_=(y=l.parameters)==null?void 0:y.docs)==null?void 0:_.source}}};var b,W,N;m.parameters={...m.parameters,docs:{...(b=m.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    inverseColor: true
+  },
+  render: args => <OrderedList {...args}>
+      <OrderedList.Item>
+        Stadsdeel West
+        <OrderedList {...args}>
+          <OrderedList.Item>Bos en Lommer</OrderedList.Item>
+          <OrderedList.Item>Oud West / De Baarsjes</OrderedList.Item>
+          <OrderedList.Item>Westerpark</OrderedList.Item>
+        </OrderedList>
+      </OrderedList.Item>
+    </OrderedList>
+}`,...(N=(W=m.parameters)==null?void 0:W.docs)==null?void 0:N.source}}};const R=["Default","TwoLevels","StartingNumber","DescendingNumbers","WithoutMarkers","InverseColor"],J=Object.freeze(Object.defineProperty({__proto__:null,Default:a,DescendingNumbers:o,InverseColor:m,StartingNumber:i,TwoLevels:n,WithoutMarkers:l,__namedExportsOrder:R,default:G},Symbol.toStringTag,{value:"Module"}));export{o as D,m as I,J as O,i as S,n as T,l as W};
