1b16ff4e866fe2beedbb727abd0a4221a96aeb75
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-04-12 11:45:56 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@db596f4dcd6564d121b008f2aa6ec47555efe322 🚀

--- demo-DES-717-refactor-inverseColor-stories/assets/Blockquote.stories-BqYdd0ln.js
+++
@@ -1,5 +0,0 @@
-import{j as o}from"./jsx-runtime-CKrituN3.js";import{e as i}from"./exampleContent-DcLVbCjv.js";import{B as u}from"./Blockquote-BGaZngnq.js";const p=i(),f={title:"Components/Text/Blockquote",component:u,args:{children:p,inverseColor:!1},argTypes:{children:{table:{disable:!1}}},decorators:[(d,m)=>o.jsx("div",{className:m.args.inverseColor?"ams-docs-dark-background":void 0,children:o.jsx(d,{})})]},e={},r={args:{inverseColor:!0}};var s,t,a;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:"{}",...(a=(t=e.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};var c,n,l;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:`{
-  args: {
-    inverseColor: true
-  }
-}`,...(l=(n=r.parameters)==null?void 0:n.docs)==null?void 0:l.source}}};const g=["Default","InvertedColor"],b=Object.freeze(Object.defineProperty({__proto__:null,Default:e,InvertedColor:r,__namedExportsOrder:g,default:f},Symbol.toStringTag,{value:"Module"}));export{b as B,r as I};
