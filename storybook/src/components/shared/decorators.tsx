3b1e989c57b68644e0630b22cdc07c4c422f1ae3
Vincent Smedinga <v.smedinga@amsterdam.nl>
2024-04-12 14:11:07 +0200

    Add missing empty line

--- storybook/src/components/shared/decorators.tsx
+++ storybook/src/components/shared/decorators.tsx
@@ -2,6 +2,7 @@
  * @license EUPL-1.2+
  * Copyright Gemeente Amsterdam
  */
+
 import type { StoryContext, StoryFn } from '@storybook/react'
 
 /** Applies a dark background colour if the `inverseColor` arg is true. */
