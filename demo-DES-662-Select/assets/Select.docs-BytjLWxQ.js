d012cbaf505ce7dc8de70a1b05fe062065cf1dad
dlnr <niels@dlnr.dev>
2024-04-12 09:08:06 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@1922aa9a9e4f86feca5f3e5ff4a393b411449c37 🚀

---
+++ demo-DES-662-Select/assets/Select.docs-BytjLWxQ.js
@@ -0,0 +1,29 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{useMDXComponents as r}from"./index-DSz_1G2r.js";import{M as i,d,P as l,e as c,f as n}from"./index-BZMFo5Kr.js";import{S as a,M as h,G as m,I as p,D as u}from"./Select.stories-B0MWnyxW.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-AkxIXEEe.js";import"../sb-preview/runtime.js";import"./index-BtM5VmRH.js";import"./index-Bw8VTzHM.js";import"./index-k_s-dmML.js";import"./index-DrFu-skq.js";import"./index.esm-qaTfZl6E.js";import"./clsx-B-dksMZM.js";import"./index.esm-DjZ2yxjc.js";const x=`<!-- @license CC0-1.0 -->
+
+# Select
+
+A form control that allows users to select one or more options from a list.
+
+## References
+
+- [Combobox pattern](https://www.w3.org/WAI/ARIA/apg/patterns/combobox/)
+- [MDN Web Docs](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/select)
+`;function s(t){const o={code:"code",h1:"h1",h2:"h2",p:"p",...r(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(i,{of:a}),`
+`,e.jsx(d,{children:x}),`
+`,e.jsx(o.h1,{id:"default",children:"Default"}),`
+`,e.jsx(l,{}),`
+`,e.jsx(c,{}),`
+`,e.jsx(o.h2,{id:"multiple",children:"Multiple"}),`
+`,e.jsxs(o.p,{children:["With ",e.jsx(o.code,{children:"multiple"})," enabled the attribute ",e.jsx(o.code,{children:"size"}),` is allowed to set the number of rows. Multi select is harder to use
+on desktop as they require the user to hold down the `,e.jsx(o.code,{children:"Ctrl"})," or ",e.jsx(o.code,{children:"Cmd"}),` key while clicking on the options.
+You might need to describe this behavior to the user. It is recommended to use checkboxes instead.
+On the other hand, it is a good choice for mobile devices.`]}),`
+`,e.jsx(n,{of:h}),`
+`,e.jsx(o.h2,{id:"grouped",children:"Grouped"}),`
+`,e.jsxs(o.p,{children:["Use the ",e.jsx(o.code,{children:"Select.Group"}),` component to group options.
+The component requires a `,e.jsx(o.code,{children:"label"})," attribute."]}),`
+`,e.jsx(n,{of:m}),`
+`,e.jsx(o.h2,{id:"invalid",children:"Invalid"}),`
+`,e.jsx(n,{of:p}),`
+`,e.jsx(o.h2,{id:"disabled",children:"Disabled"}),`
+`,e.jsx(n,{of:u})]})}function G(t={}){const{wrapper:o}={...r(),...t.components};return o?e.jsx(o,{...t,children:e.jsx(s,{...t})}):s(t)}export{G as default};
