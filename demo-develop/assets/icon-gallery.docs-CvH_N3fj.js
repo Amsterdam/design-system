513d326f38fa8529a19dc4040b283121db19b06c
alimpens <37216945+alimpens@users.noreply.github.com>
2024-04-17 12:01:36 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@eb05fff8295af3c59ef584700e3c1bc06da0c924 🚀

---
+++ demo-develop/assets/icon-gallery.docs-CvH_N3fj.js
@@ -0,0 +1,8 @@
+import{j as n}from"./jsx-runtime-CKrituN3.js";import{useMDXComponents as r}from"./index-DSz_1G2r.js";import{I as i,g as c,M as m}from"./index-BYSI57Qj.js";import{I as s}from"./index.esm-DjZ2yxjc.js";import{I as l}from"./Icon-DW52t4nJ.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-DiLbbwbL.js";import"../sb-preview/runtime.js";import"./index-BtM5VmRH.js";import"./index-Bw8VTzHM.js";import"./index-DLle_QMf.js";import"./index-DrFu-skq.js";import"./clsx-B-dksMZM.js";const a=()=>{const t=Object.keys(s);return n.jsx(i,{children:t.map(o=>n.jsx(c,{name:o.substring(0,o.length-4),children:n.jsx(l,{svg:s[o]})},o))})};function e(t){const o={a:"a",h1:"h1",h2:"h2",li:"li",p:"p",ul:"ul",...r(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(m,{title:"Docs/Assets/Icons"}),`
+`,n.jsx(o.h1,{id:"icons",children:"Icons"}),`
+`,n.jsx(o.p,{children:"An overview of all currently available icons."}),`
+`,n.jsx(a,{}),`
+`,n.jsx(o.h2,{id:"related-components",children:"Related components"}),`
+`,n.jsxs(o.ul,{children:[`
+`,n.jsx(o.li,{children:n.jsx(o.a,{href:"/docs/components-media-icon--docs",children:"Icon"})}),`
+`]})]})}function C(t={}){const{wrapper:o}={...r(),...t.components};return o?n.jsx(o,{...t,children:n.jsx(e,{...t})}):e(t)}export{C as default};
