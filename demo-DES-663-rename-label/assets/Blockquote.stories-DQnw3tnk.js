db182d9d65d8d38e69369949d812821fc54e1337
GitHub <github-action@users.noreply.github.com>
2024-04-17 10:30:07 +0000

    Remove feature branch folder

--- demo-DES-663-rename-label/assets/Blockquote.stories-DQnw3tnk.js
+++
@@ -1,8 +0,0 @@
-import{i as l}from"./decorators-CJr_2w8M.js";import{e as m}from"./exampleContent-CRK3_7Hv.js";import{B as i}from"./Blockquote-BGaZngnq.js";/**
- * @license EUPL-1.2+
- * Copyright Gemeente Amsterdam
- */const p=m(),u={title:"Components/Text/Blockquote",component:i,args:{children:p,inverseColor:!1},argTypes:{children:{table:{disable:!1}}},decorators:[l]},e={},r={args:{inverseColor:!0}};var o,t,s;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:"{}",...(s=(t=e.parameters)==null?void 0:t.docs)==null?void 0:s.source}}};var a,c,n;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
-  args: {
-    inverseColor: true
-  }
-}`,...(n=(c=r.parameters)==null?void 0:c.docs)==null?void 0:n.source}}};const d=["Default","InvertedColor"],C=Object.freeze(Object.defineProperty({__proto__:null,Default:e,InvertedColor:r,__namedExportsOrder:d,default:u},Symbol.toStringTag,{value:"Module"}));export{C as B,r as I};
