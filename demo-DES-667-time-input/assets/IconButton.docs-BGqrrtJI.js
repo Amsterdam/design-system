6b546845641a1178e386af40bc30a83eff4cbf34
dlnr <niels@dlnr.dev>
2024-04-18 07:44:35 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@39897cfb259684a5a37806a533681caec37274a6 🚀

---
+++ demo-DES-667-time-input/assets/IconButton.docs-BGqrrtJI.js
@@ -0,0 +1,33 @@
+import{j as n}from"./jsx-runtime-CKrituN3.js";import{useMDXComponents as r}from"./index-DSz_1G2r.js";import{M as i,d as a,P as l,e as c,f as t}from"./index-C7P3F1CG.js";import{I as d,O as m,a as u,L as h,b as x,c as p,d as f}from"./IconButton.stories-CpfbOobX.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-Cd4Xx9Cr.js";import"../sb-preview/runtime.js";import"./index-BtM5VmRH.js";import"./index-Bw8VTzHM.js";import"./index-DLle_QMf.js";import"./index-DrFu-skq.js";import"./index.esm-DjZ2yxjc.js";import"./IconButton-DnKuviCZ.js";import"./clsx-B-dksMZM.js";import"./VisuallyHidden-DSeislZq.js";import"./Icon-DW52t4nJ.js";const j=`<!-- @license CC0-1.0 -->
+
+# Icon Button
+
+A button containing only an icon.
+
+## Guidelines
+
+- Use this button only if the icon is entirely clear and understandable.
+  Many icons are interpreted differently by different users.
+  Only use universally recognized icons, such as a close icon or a play icon.
+- Provide a label text that describes what the button does for screen reader users.
+
+## Relevant WCAG Requirements
+
+The Icon Button follows [the same requirements and guidelines as a regular button](/docs/components-buttons-button--docs).
+`;function s(o){const e={h2:"h2",h3:"h3",...r(),...o.components};return n.jsxs(n.Fragment,{children:[n.jsx(i,{of:d}),`
+`,n.jsx(a,{children:j}),`
+`,n.jsx(l,{}),`
+`,n.jsx(c,{}),`
+`,n.jsx(e.h2,{id:"examples",children:"Examples"}),`
+`,n.jsx(e.h3,{id:"on-light-background",children:"On Light Background"}),`
+`,n.jsx(t,{of:m}),`
+`,n.jsx(e.h3,{id:"on-dark-background",children:"On Dark Background"}),`
+`,n.jsx(t,{of:u}),`
+`,n.jsx(e.h3,{id:"level-3",children:"Level 3"}),`
+`,n.jsx(t,{of:h}),`
+`,n.jsx(e.h3,{id:"level-4",children:"Level 4"}),`
+`,n.jsx(t,{of:x}),`
+`,n.jsx(e.h3,{id:"level-5",children:"Level 5"}),`
+`,n.jsx(t,{of:p}),`
+`,n.jsx(e.h3,{id:"level-6",children:"Level 6"}),`
+`,n.jsx(t,{of:f})]})}function X(o={}){const{wrapper:e}={...r(),...o.components};return e?n.jsx(e,{...o,children:n.jsx(s,{...o})}):s(o)}export{X as default};
