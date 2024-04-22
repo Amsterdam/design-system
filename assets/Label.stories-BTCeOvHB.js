9b7361c11841dc9fd3976bb286e4e94f96873598
alimpens <alimpens@users.noreply.github.com>
2024-04-22 09:31:52 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@7679acc7542c2e3d1279b9eaa364031092d16016 🚀

---
+++ assets/Label.stories-BTCeOvHB.js
@@ -0,0 +1,4 @@
+import{L as t}from"./Label-DaAdNem9.js";/**
+ * @license EUPL-1.2+
+ * Copyright Gemeente Amsterdam
+ */const s={title:"Components/Forms/Label",component:t,args:{children:"Label"},argTypes:{children:{table:{disable:!1}}}},e={};var r,a,o;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:"{}",...(o=(a=e.parameters)==null?void 0:a.docs)==null?void 0:o.source}}};const l=["Default"],n=Object.freeze(Object.defineProperty({__proto__:null,Default:e,__namedExportsOrder:l,default:s},Symbol.toStringTag,{value:"Module"}));export{n as L};
