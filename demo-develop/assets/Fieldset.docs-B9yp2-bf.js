138185a1593d896686e5be7d593d442b684e609b
alimpens <37216945+alimpens@users.noreply.github.com>
2024-04-22 09:38:03 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@f12ea8d41211d4a0cb6bb1d27744df9794f70dec 🚀

---
+++ demo-develop/assets/Fieldset.docs-B9yp2-bf.js
@@ -0,0 +1,26 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{useMDXComponents as s}from"./index-DSz_1G2r.js";import{M as r,d as i,P as p,e as a,f as l}from"./index-BwOsFOtB.js";import{F as d,C as u}from"./Fieldset.stories-_6OMOOwW.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-COcPBBzc.js";import"../sb-preview/runtime.js";import"./index-BtM5VmRH.js";import"./index-Bw8VTzHM.js";import"./index-DLle_QMf.js";import"./index-DrFu-skq.js";import"./Fieldset-D0mGUuLU.js";import"./clsx-B-dksMZM.js";import"./Checkbox-DmcZqcAR.js";const m=`<!-- @license CC0-1.0 -->
+
+# Fieldset
+
+A component to group related form inputs.
+
+## Guidelines
+
+- Use Fieldset when you need to show a relationship between multiple form inputs. For example, you may need to group a set of text inputs into a single fieldset when asking for an address.
+- When grouping radio inputs, use \`role="radiogroup"\` on Fieldset to have this grouping explicitly announced as a radio group. Fieldset has a default role of \`group\`.
+
+## Relevant WCAG Requirements
+
+- [WCAG 1.3.5](https://www.w3.org/WAI/WCAG22/Understanding/identify-input-purpose.html): Fieldset labels the purpose of a group of inputs.
+
+## References
+
+- [Providing a description for groups of form controls using fieldset and legend elements](https://www.w3.org/WAI/WCAG22/Techniques/html/H71)
+`;function t(n){const o={h2:"h2",h3:"h3",p:"p",...s(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(r,{of:d}),`
+`,e.jsx(i,{children:m}),`
+`,e.jsx(p,{}),`
+`,e.jsx(a,{}),`
+`,e.jsx(o.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(o.h3,{id:"checkbox-group",children:"Checkbox group"}),`
+`,e.jsx(o.p,{children:"Fieldset is used to group related form inputs, like checkboxes."}),`
+`,e.jsx(l,{of:u})]})}function G(n={}){const{wrapper:o}={...s(),...n.components};return o?e.jsx(o,{...n,children:e.jsx(t,{...n})}):t(n)}export{G as default};
