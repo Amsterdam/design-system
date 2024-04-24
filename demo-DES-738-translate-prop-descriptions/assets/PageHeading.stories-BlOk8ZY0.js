ec04ba787b835f4ca1ecc6cb8f0bb1da90c35c85
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-04-24 21:17:43 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@1b4c416c04ff44fe4f52ed1f5ffec58c8159ed74 🚀

--- demo-DES-738-translate-prop-descriptions/assets/PageHeading.stories-BlOk8ZY0.js
+++
@@ -1,8 +0,0 @@
-import{i as u}from"./decorators-CJr_2w8M.js";import{j as f}from"./jsx-runtime-CKrituN3.js";import{c as _}from"./clsx-B-dksMZM.js";import{r as h}from"./index-CBqU2yxZ.js";const o=h.forwardRef(({children:a,className:d,inverseColor:p,...m},g)=>f.jsx("h1",{...m,ref:g,className:_("ams-page-heading",p&&"ams-page-heading--inverse-color",d),children:a}));o.displayName="PageHeading";try{o.displayName="PageHeading",o.__docgenInfo={description:"",displayName:"PageHeading",props:{inverseColor:{defaultValue:null,description:"Property to show the heading in the opposite color.",name:"inverseColor",required:!1,type:{name:"boolean"}}}}}catch{}/**
- * @license EUPL-1.2+
- * Copyright Gemeente Amsterdam
- */const v={title:"Components/Text/Page Heading",component:o,args:{children:"Gemeente Amsterdam",inverseColor:!1},argTypes:{children:{table:{disable:!1}}},decorators:[u]},e={},r={args:{inverseColor:!0}};var s,t,n;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:"{}",...(n=(t=e.parameters)==null?void 0:t.docs)==null?void 0:n.source}}};var i,c,l;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:`{
-  args: {
-    inverseColor: true
-  }
-}`,...(l=(c=r.parameters)==null?void 0:c.docs)==null?void 0:l.source}}};const y=["Default","InvertedColor"],H=Object.freeze(Object.defineProperty({__proto__:null,Default:e,InvertedColor:r,__namedExportsOrder:y,default:v},Symbol.toStringTag,{value:"Module"}));export{r as I,H as P};
