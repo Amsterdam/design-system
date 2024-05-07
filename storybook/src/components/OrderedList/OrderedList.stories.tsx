e360c1ed3941aa1a8219a3434d9be843a8752257
Niels Roozemond <n.roozemond@amsterdam.nl>
2024-05-07 10:49:56 +0200

    specificity issue no-marker option

--- storybook/src/components/OrderedList/OrderedList.stories.tsx
+++ storybook/src/components/OrderedList/OrderedList.stories.tsx
@@ -25,6 +25,13 @@ const meta = {
   argTypes: {
     reversed: { control: 'boolean' },
     start: { control: 'number' },
+    size: {
+      control: {
+        type: 'radio',
+        labels: { small: 'small', undefined: 'medium', large: 'large' },
+      },
+      options: ['small', undefined, 'large'],
+    },
   },
   decorators: [inverseColorDecorator],
 } satisfies Meta<typeof OrderedList>
