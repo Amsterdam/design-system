81a38f1cc530808c99b7f515d116a5667d80c7c2
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-05-07 19:10:38 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@1bb25a1e8e7bdd814bec133d0e64b060dabf52a6 🚀

---
+++ demo-flatten-medium-text-size-tokens/assets/TimeInput.stories-C8c6wViQ.js
@@ -0,0 +1,12 @@
+import{j as g}from"./jsx-runtime-CKrituN3.js";import{c as I}from"./clsx-B-dksMZM.js";import{r as b}from"./index-CBqU2yxZ.js";const a=b.forwardRef(({className:t,..._},f)=>g.jsx("input",{..._,ref:f,className:I("ams-time-input",t),type:"time"}));a.displayName="TimeInput";try{a.displayName="TimeInput",a.__docgenInfo={description:"",displayName:"TimeInput",props:{}}}catch{}/**
+ * @license EUPL-1.2+
+ * Copyright Gemeente Amsterdam
+ */const y={title:"Components/Forms/Time Input",component:a,args:{disabled:!1}},e={},r={args:{required:!0}},s={args:{disabled:!0}};var o,n,p;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:"{}",...(p=(n=e.parameters)==null?void 0:n.docs)==null?void 0:p.source}}};var c,i,m;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    required: true
+  }
+}`,...(m=(i=r.parameters)==null?void 0:i.docs)==null?void 0:m.source}}};var d,u,l;s.parameters={...s.parameters,docs:{...(d=s.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    disabled: true
+  }
+}`,...(l=(u=s.parameters)==null?void 0:u.docs)==null?void 0:l.source}}};const T=["Default","Invalid","Disabled"],D=Object.freeze(Object.defineProperty({__proto__:null,Default:e,Disabled:s,Invalid:r,__namedExportsOrder:T,default:y},Symbol.toStringTag,{value:"Module"}));export{s as D,r as I,D as T};
