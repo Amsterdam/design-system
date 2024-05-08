266503ce37ceaea7f5c71a1382e7f012329037f3
RubenSibon <mail@rubensibon.nl>
2024-05-08 16:14:40 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@7c5b156cb4f02677fca6da15876bb1d7153af5eb 🚀

---
+++ demo-DES-652-favicon-and-app-icons/assets/MegaMenu.docs-CrSslxL8.js
@@ -0,0 +1,8 @@
+import{j as t}from"./jsx-runtime-CKrituN3.js";import{useMDXComponents as i}from"./index-DSz_1G2r.js";import{M as e,d as m,P as s,f as a}from"./index-Cf8toUJ0.js";import{M as p,a as c}from"./MegaMenu.stories-DrCcpa75.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-C7MpLvQn.js";import"../sb-preview/runtime.js";import"./index-BtM5VmRH.js";import"./index-Dkj0J1ds.js";import"./index-BLcLG51Y.js";import"./index-DrFu-skq.js";import"./clsx-B-dksMZM.js";import"./Screen-CBGDXr7L.js";import"./Grid-xhYoXT5x.js";import"./Heading-DWMBrRCk.js";import"./getHeadingElement-DiRwFEz7.js";import"./LinkList-BmpxO9J_.js";import"./index.esm-DjZ2yxjc.js";import"./Icon-DW52t4nJ.js";const M=`<!-- @license CC0-1.0 -->
+
+# Mega Menu
+`;function n(o){const r={h2:"h2",...i(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(e,{of:p}),`
+`,t.jsx(m,{children:M}),`
+`,t.jsx(s,{}),`
+`,t.jsx(r.h2,{id:"with-categories",children:"With Categories"}),`
+`,t.jsx(a,{of:c})]})}function F(o={}){const{wrapper:r}={...i(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(n,{...o})}):n(o)}export{F as default};
---
+++ demo-DES-652-favicon-and-app-icons/assets/breakpoints.docs-Cr7ZeGsf.js
@@ -0,0 +1,15 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{useMDXComponents as o}from"./index-DSz_1G2r.js";import{M as r}from"./index-Cf8toUJ0.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-C7MpLvQn.js";import"../sb-preview/runtime.js";import"./index-BtM5VmRH.js";import"./index-Dkj0J1ds.js";import"./index-BLcLG51Y.js";import"./index-DrFu-skq.js";function t(s){const n={a:"a",code:"code",h1:"h1",p:"p",pre:"pre",...o(),...s.components};return e.jsxs(e.Fragment,{children:[e.jsx(r,{title:"Docs/Design Guidelines/Breakpoints"}),`
+`,e.jsx(n.h1,{id:"breakpoints",children:"Breakpoints"}),`
+`,e.jsx(n.p,{children:"We use 2 breakpoints where the design of a website changes to ensure a good user experience: at screen widths of 576px and 1088px."}),`
+`,e.jsx(n.p,{children:"We follow a mobile-first approach: first, we create the design below the breakpoint, and then we use a media query to create the design above the breakpoint."}),`
+`,e.jsx(n.p,{children:"For example:"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-css",children:`.class {
+  /* Your narrow styling */
+
+  @media screen and (min-width: $ams-breakpoint-wide) {
+    /* Your wide styling */
+  }
+}
+`})}),`
+`,e.jsxs(n.p,{children:[`Within this repository, you can import the breakpoint as a Sass variable.
+You can find this variable in `,e.jsx(n.a,{href:"https://github.com/Amsterdam/design-system/blob/main/packages/css/src/common/breakpoint.scss",rel:"nofollow",children:"packages/css/src/common/breakpoint.scss"}),"."]})]})}function g(s={}){const{wrapper:n}={...o(),...s.components};return n?e.jsx(n,{...s,children:e.jsx(t,{...s})}):t(s)}export{g as default};
