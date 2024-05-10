76a9219b7addda6a4f8f32b2dabc032dd8bece53
dlnr <niels@dlnr.dev>
2024-05-10 09:35:05 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@34ba789cc36653bc666c73cde8ad64099b2a170b 🚀

---
+++ demo-DES-670-file-input/assets/Screen.docs-Dh5gQjbU.js
@@ -0,0 +1,28 @@
+import{j as n}from"./jsx-runtime-CKrituN3.js";import{useMDXComponents as s}from"./index-DSz_1G2r.js";import{M as i,d as r,P as a,e as m,f as c}from"./index-D6rNdIhA.js";import{S as d,E as p}from"./Screen.stories-Bdgq-Ddc.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-DoThjUpB.js";import"../sb-preview/runtime.js";import"./index-BtM5VmRH.js";import"./index-Dkj0J1ds.js";import"./index-BLcLG51Y.js";import"./index-DrFu-skq.js";import"./Screen-CBGDXr7L.js";import"./clsx-B-dksMZM.js";const h=`<!-- @license CC0-1.0 -->
+
+# Screen
+
+Manages the maximum width and alignment of the entire website or application.
+
+## Usage
+
+This should be the outermost component of your website or application.
+Within it, combine components such as
+[Grid](/docs/components-layout-grid--docs),
+[Header](/docs/components-containers-header--docs),
+[Footer](/docs/components-containers-footer--docs),
+[Spotlight](/docs/components-containers-spotlight--docs),
+and Figure.
+
+## Design
+
+By default, this component expands to a width of 1600 pixels.
+This is the maximum width for a website of the City of Amsterdam.
+For applications, you can choose a larger maximum width of 2112 pixels.
+The component horizontally centres both itself and its content.
+`;function e(o){const t={h2:"h2",...s(),...o.components};return n.jsxs(n.Fragment,{children:[n.jsx(i,{of:d}),`
+`,n.jsx(r,{children:h}),`
+`,n.jsx(a,{}),`
+`,n.jsx(m,{}),`
+`,n.jsx(t.h2,{id:"extra-wide",children:"Extra wide"}),`
+`,n.jsx(c,{of:p})]})}function F(o={}){const{wrapper:t}={...s(),...o.components};return t?n.jsx(t,{...o,children:n.jsx(e,{...o})}):e(o)}export{F as default};
