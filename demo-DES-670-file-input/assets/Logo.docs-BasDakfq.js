3f61b5eadd1faff1cc146e74b18c5631cfb2fd11
dlnr <niels@dlnr.dev>
2024-05-10 09:18:44 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@3712d6c464f7928990e4065738e1784a0cc8d5ed 🚀

---
+++ demo-DES-670-file-input/assets/Logo.docs-BasDakfq.js
@@ -0,0 +1,56 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{useMDXComponents as a}from"./index-DSz_1G2r.js";import{M as i,d as r,P as h,e as l,f as o}from"./index-GCVeX5za.js";import{L as d,G as m,S as g,a as p,V as c,M as u}from"./Logo.stories-DYcR_Q_g.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-DvXGNDgg.js";import"../sb-preview/runtime.js";import"./index-BtM5VmRH.js";import"./index-Dkj0J1ds.js";import"./index-BLcLG51Y.js";import"./index-DrFu-skq.js";import"./Logo-DYZyNpme.js";import"./clsx-B-dksMZM.js";const f=`<!-- @license CC0-1.0 -->
+
+# Logo
+
+The City’s logo includes an icon (3 Saint Andrew’s crosses) and a wordmark (Gemeente Amsterdam).
+The logo has a fixed colour, size, and position and is present on all our websites.
+
+## Specifications
+
+The design system provides the correct logo for online use.
+Other logo variations exist, but they cannot be used on websites and in applications.
+The Dutch logo is for websites in the Dutch language and any language other than English.
+The English logo is used for websites in the English language.
+(We will add this logo to the design system later.)
+In the meantime, you can download an image of this logo from [Stijlweb](https://amsterdam.nl/stijlweb/basiselementen/logo-gemeente-amsterdam/#hba498e5c-2388-464f-b769-7622a5d32bd1).
+
+## Exception
+
+Use a sub-brand logo if the website is owned by one of the 5 sub-brands.
+They have a separate status because of their unique service provision not directly associated with the City.
+
+The sub-brands are:
+
+- GGD Amsterdam
+- Museum Weesp
+- Stadsarchief
+- Stadsbank van Lening
+- VGA Verzekeringen
+
+## Requirements for all logos
+
+- The logo is always displayed.
+- The logo is left-aligned in the header.
+- The logo links to the homepage of the website or application.
+- If the application is a form, application, or tool without a homepage, the logo links to the page where the form, application, or tool is referred to.
+
+The height of the logo is always 40 pixels.
+This also applies to sub-brand logos.
+
+## Download
+
+- [Logo set for websites and apps (ZIP, 10 kB)](https://assets.amsterdam.nl/publish/pages/1007650/logoset_voor_websites_en_apps.zip)
+`;function t(s){const n={h2:"h2",...a(),...s.components};return e.jsxs(e.Fragment,{children:[e.jsx(i,{of:d}),`
+`,e.jsx(r,{children:f}),`
+`,e.jsx(h,{}),`
+`,e.jsx(l,{}),`
+`,e.jsx(n.h2,{id:"ggd-amsterdam",children:"GGD Amsterdam"}),`
+`,e.jsx(o,{of:m}),`
+`,e.jsx(n.h2,{id:"stadsarchief",children:"Stadsarchief"}),`
+`,e.jsx(o,{of:g}),`
+`,e.jsx(n.h2,{id:"stadsbank-van-lening",children:"Stadsbank van Lening"}),`
+`,e.jsx(o,{of:p}),`
+`,e.jsx(n.h2,{id:"vga-verzekeringen",children:"VGA Verzekeringen"}),`
+`,e.jsx(o,{of:c}),`
+`,e.jsx(n.h2,{id:"museum-weesp",children:"Museum Weesp"}),`
+`,e.jsx(o,{of:u})]})}function L(s={}){const{wrapper:n}={...a(),...s.components};return n?e.jsx(n,{...s,children:e.jsx(t,{...s})}):t(s)}export{L as default};
---
+++ demo-DES-670-file-input/assets/PageHeading.docs-BACVNpFi.js
@@ -0,0 +1,29 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{useMDXComponents as i}from"./index-DSz_1G2r.js";import{M as s,d as r,P as a,e as h,f as p}from"./index-GCVeX5za.js";import{P as m,I as c}from"./PageHeading.stories-UMIFG9Yo.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-DvXGNDgg.js";import"../sb-preview/runtime.js";import"./index-BtM5VmRH.js";import"./index-Dkj0J1ds.js";import"./index-BLcLG51Y.js";import"./index-DrFu-skq.js";import"./decorators-CJr_2w8M.js";import"./clsx-B-dksMZM.js";const d=`<!-- @license CC0-1.0 -->
+
+# Page Heading
+
+The page heading component is a prominent text at the beginning of a page, immediately after the header.
+Use this component for a name, title, or motto.
+
+## Guidelines
+
+- Use this component to capture the user’s attention.
+- Only use this component once per page, typically only on the homepage.
+- Avoid using it if you need to display long pieces of text; the text should be short and precise.
+
+## Relevant WCAG Requirements
+
+The page heading component is a variant of a heading level 1 with a distinct style.
+When using this component, ensure that the heading hierarchy of the page remains logical.
+
+For this component, the same WCAG requirements apply as for [the heading component](/docs/components-text-heading--docs).
+`;function o(t){const n={code:"code",h2:"h2",h3:"h3",p:"p",...i(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(s,{of:m}),`
+`,e.jsx(r,{children:d}),`
+`,e.jsx(n.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(n.h3,{id:"default",children:"Default"}),`
+`,e.jsx(a,{}),`
+`,e.jsx(h,{}),`
+`,e.jsx(n.h3,{id:"inverse-colour",children:"Inverse colour"}),`
+`,e.jsxs(n.p,{children:["Set the ",e.jsx(n.code,{children:"inverseColor"}),` prop if the Page Heading sits on a dark background.
+This ensures the colour of the text provides enough contrast.`]}),`
+`,e.jsx(p,{of:c})]})}function R(t={}){const{wrapper:n}={...i(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(o,{...t})}):o(t)}export{R as default};
