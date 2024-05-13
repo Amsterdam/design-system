794570a4a4eb0d2b1fcaf7493f6a791a96079e99
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-05-13 05:06:58 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@7f3c89cd655708246ac8c8a15eebc529783852da 🚀

--- demo-DES-756-hardcoded-labels/assets/MegaMenu.docs-CQjE256B.js
+++
@@ -1,8 +0,0 @@
-import{j as t}from"./jsx-runtime-Nms4Y4qS.js";import{useMDXComponents as i}from"./index-DlpNa54Y.js";import{M as e,d as m,P as s,f as a}from"./index-2DD0Jk6U.js";import{M as p,a as c}from"./MegaMenu.stories-CB-l-HKk.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-BIU0p9K3.js";import"../sb-preview/runtime.js";import"./index-B8XB3FuZ.js";import"./index-COebWTXQ.js";import"./index-B8K4vdXH.js";import"./index-DrFu-skq.js";import"./clsx-B-dksMZM.js";import"./Screen-DD3ND5ci.js";import"./Grid-qe23dp1l.js";import"./Heading-mbayTkiK.js";import"./getHeadingElement-DiRwFEz7.js";import"./LinkList-Cj5_Zz5z.js";import"./index.esm-DQkeu4as.js";import"./Icon-tKAGJH_4.js";const M=`<!-- @license CC0-1.0 -->
-
-# Mega Menu
-`;function n(o){const r={h2:"h2",...i(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(e,{of:p}),`
-`,t.jsx(m,{children:M}),`
-`,t.jsx(s,{}),`
-`,t.jsx(r.h2,{id:"with-categories",children:"With Categories"}),`
-`,t.jsx(a,{of:c})]})}function F(o={}){const{wrapper:r}={...i(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(n,{...o})}):n(o)}export{F as default};
