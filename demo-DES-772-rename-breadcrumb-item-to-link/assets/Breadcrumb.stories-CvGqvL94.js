09821dcd4c057daad7acccda8156589a130202e0
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-05-15 22:40:35 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@6ce97c76d6a8e0303182637161b43df4f1875f0c 🚀

---
+++ demo-DES-772-rename-breadcrumb-item-to-link/assets/Breadcrumb.stories-CvGqvL94.js
@@ -0,0 +1,11 @@
+import{j as e}from"./jsx-runtime-Nms4Y4qS.js";import{c as t}from"./clsx-B-dksMZM.js";import{r as u}from"./index-BwDkhjyp.js";const l=u.forwardRef(({children:a,className:c,href:s,...i},p)=>e.jsx("li",{className:"ams-breadcrumb__item",children:e.jsx("a",{...i,className:t("ams-breadcrumb__link",c),href:s,ref:p,children:a})}));l.displayName="Breadcrumb.Link";try{Breadcrumb.Link.displayName="Breadcrumb.Link",Breadcrumb.Link.__docgenInfo={description:"",displayName:"Breadcrumb.Link",props:{}}}catch{}const b=u.forwardRef(({children:a,className:c,...s},i)=>e.jsx("nav",{...s,className:t("ams-breadcrumb",c),ref:i,children:e.jsx("ol",{className:"ams-breadcrumb__list",children:a})}));b.displayName="Breadcrumb";const r=Object.assign(b,{Link:l});try{r.displayName="Breadcrumb",r.__docgenInfo={description:"",displayName:"Breadcrumb",props:{}}}catch{}const _={title:"Components/Navigation/Breadcrumb",component:r},n={args:{children:[e.jsx(r.Link,{href:"#",children:"Home"},1),e.jsx(r.Link,{href:"#",children:"Nieuws"},2),e.jsx(r.Link,{href:"#",children:"Kennisgevingen en bekendmakingen"},3)]}};var m,o,d;n.parameters={...n.parameters,docs:{...(m=n.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    children: [<Breadcrumb.Link href="#" key={1}>
+        Home
+      </Breadcrumb.Link>, <Breadcrumb.Link href="#" key={2}>
+        Nieuws
+      </Breadcrumb.Link>, <Breadcrumb.Link href="#" key={3}>
+        Kennisgevingen en bekendmakingen
+      </Breadcrumb.Link>]
+  }
+}`,...(d=(o=n.parameters)==null?void 0:o.docs)==null?void 0:d.source}}};const k=["Default"],L=Object.freeze(Object.defineProperty({__proto__:null,Default:n,__namedExportsOrder:k,default:_},Symbol.toStringTag,{value:"Module"}));export{L as B};
---
+++ demo-DES-772-rename-breadcrumb-item-to-link/assets/DateInput.docs-B-EOV9Xn.js
@@ -0,0 +1,19 @@
+import{j as n}from"./jsx-runtime-Nms4Y4qS.js";import{useMDXComponents as o}from"./index-DlpNa54Y.js";import{M as r,d as a,P as d,e as m,f as s}from"./index-CBoRRuzR.js";import{D as c,I as p,a as l}from"./DateInput.stories-tt9bP7Yf.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-CR3sRgD6.js";import"../sb-preview/runtime.js";import"./index-B8XB3FuZ.js";import"./index-COebWTXQ.js";import"./index-B8K4vdXH.js";import"./index-DrFu-skq.js";import"./index.esm-D_ak_a6r.js";import"./clsx-B-dksMZM.js";import"./index.esm-DQkeu4as.js";const h=`<!-- @license CC0-1.0 -->
+
+# Date Input
+
+Helps users enter a date.
+
+## Visual considerations
+
+This component uses a native date input, which is styled differently in different browsers and operating systems.
+Recreating the native functionality is quite difficult and prone to accessibility errors, which is why we’ve chosen not to do that.
+This does mean that this component does not look identical on all browsers and operating systems.
+`;function i(t){const e={h2:"h2",...o(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(r,{of:c}),`
+`,n.jsx(a,{children:h}),`
+`,n.jsx(d,{}),`
+`,n.jsx(m,{}),`
+`,n.jsx(e.h2,{id:"invalid",children:"Invalid"}),`
+`,n.jsx(s,{of:p}),`
+`,n.jsx(e.h2,{id:"disabled",children:"Disabled"}),`
+`,n.jsx(s,{of:l})]})}function X(t={}){const{wrapper:e}={...o(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(i,{...t})}):i(t)}export{X as default};
