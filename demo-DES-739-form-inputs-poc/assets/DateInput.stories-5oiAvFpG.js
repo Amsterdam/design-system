945582878a4dcbe5e40341a54bc365357395dcb8
dlnr <niels@dlnr.dev>
2024-05-16 11:01:16 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@33231dba7912f7fbfbad249ae7cf9f06a97685ff 🚀

---
+++ demo-DES-739-form-inputs-poc/assets/DateInput.stories-5oiAvFpG.js
@@ -0,0 +1,24 @@
+import{D as M}from"./index.esm-Bwo_LtZC.js";/**
+ * @license EUPL-1.2+
+ * Copyright Gemeente Amsterdam
+ */const v={title:"Components/Forms/Date Input",component:M,args:{disabled:!1}},e={},r={args:{required:!0}},a={args:{disabled:!0}},s={args:{type:"datetime-local"}},t={args:{type:"month"}},o={args:{type:"week"}};var c,n,p;e.parameters={...e.parameters,docs:{...(c=e.parameters)==null?void 0:c.docs,source:{originalSource:"{}",...(p=(n=e.parameters)==null?void 0:n.docs)==null?void 0:p.source}}};var m,d,u;r.parameters={...r.parameters,docs:{...(m=r.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    required: true
+  }
+}`,...(u=(d=r.parameters)==null?void 0:d.docs)==null?void 0:u.source}}};var i,l,g;a.parameters={...a.parameters,docs:{...(i=a.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    disabled: true
+  }
+}`,...(g=(l=a.parameters)==null?void 0:l.docs)==null?void 0:g.source}}};var D,b,S;s.parameters={...s.parameters,docs:{...(D=s.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  args: {
+    type: 'datetime-local'
+  }
+}`,...(S=(b=s.parameters)==null?void 0:b.docs)==null?void 0:S.source}}};var y,f,I;t.parameters={...t.parameters,docs:{...(y=t.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    type: 'month'
+  }
+}`,...(I=(f=t.parameters)==null?void 0:f.docs)==null?void 0:I.source}}};var _,h,k;o.parameters={...o.parameters,docs:{...(_=o.parameters)==null?void 0:_.docs,source:{originalSource:`{
+  args: {
+    type: 'week'
+  }
+}`,...(k=(h=o.parameters)==null?void 0:h.docs)==null?void 0:k.source}}};const O=["Default","Invalid","Disabled","DateTimeLocal","Month","Week"],W=Object.freeze(Object.defineProperty({__proto__:null,DateTimeLocal:s,Default:e,Disabled:a,Invalid:r,Month:t,Week:o,__namedExportsOrder:O,default:v},Symbol.toStringTag,{value:"Module"}));export{W as D,r as I,t as M,o as W,a,s as b};
