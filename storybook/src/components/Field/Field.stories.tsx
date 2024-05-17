66832aaf14a209915b60498acecf90cecff27c23
Aram <37216945+alimpens@users.noreply.github.com>
2024-05-18 00:11:34 +0200

    feat: Add Field component (#1228)
    Co-authored-by: Vincent Smedinga <v.smedinga@amsterdam.nl>

---
+++ storybook/src/components/Field/Field.stories.tsx
@@ -0,0 +1,44 @@
+/**
+ * @license EUPL-1.2+
+ * Copyright Gemeente Amsterdam
+ */
+
+import { TextInput } from '@amsterdam/design-system-react'
+import { Field, Label, Paragraph } from '@amsterdam/design-system-react/src'
+import { Meta, StoryObj } from '@storybook/react'
+
+const meta = {
+  title: 'Components/Forms/Field',
+  component: Field,
+  args: {
+    invalid: false,
+  },
+  render: (args) => (
+    <Field invalid={args.invalid}>
+      <Label htmlFor="input1">Waar gaat het om?</Label>
+      <Paragraph id="description1" size="small">
+        Typ geen persoonsgegevens in deze omschrijving. We vragen dit later in dit formulier aan u.
+      </Paragraph>
+      <TextInput id="input1" aria-describedby="description1" aria-invalid={args.invalid ? true : undefined} />
+    </Field>
+  ),
+} satisfies Meta<typeof Field>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {}
+
+export const WithError: Story = {
+  args: { invalid: true },
+  render: (args) => (
+    <Field invalid={args.invalid}>
+      <Label htmlFor="input2">Waar gaat het om?</Label>
+      <Paragraph id="description2" size="small">
+        Typ geen persoonsgegevens in deze omschrijving. We vragen dit later in dit formulier aan u.
+      </Paragraph>
+      <TextInput id="input2" aria-describedby="description2" aria-invalid={args.invalid ? true : undefined} />
+    </Field>
+  ),
+}
