060d10753a7dcf3ca4e6c8e1e5bb00a258aa626b
RubenSibon <mail@rubensibon.nl>
2024-05-17 13:00:16 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@182f74324e3595de9b6cef0247c93a7fdea60a52 🚀

--- demo-DES-680-component-status/assets/icon-gallery.docs-ChSF5f8Y.js
+++
@@ -1,9 +0,0 @@
-import{j as o}from"./jsx-runtime-Nms4Y4qS.js";import{useMDXComponents as r}from"./index-DlpNa54Y.js";import{I as c,g as i,M as m}from"./index-Bqz70Bna.js";import{I as s}from"./index.esm-DQkeu4as.js";import{I as a}from"./Icon-tKAGJH_4.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-BYkBvFBJ.js";import"../sb-preview/runtime.js";import"./index-B8XB3FuZ.js";import"./index-COebWTXQ.js";import"./index-B8K4vdXH.js";import"./index-DrFu-skq.js";import"./clsx-B-dksMZM.js";const{deprecate:l}=__STORYBOOK_MODULE_CLIENT_LOGGER__;l("Import from '@storybook/addon-docs/blocks' is deprecated. Please import from '@storybook/blocks' instead.");const p=()=>{const t=Object.keys(s);return o.jsx(c,{children:t.map(n=>o.jsx(i,{name:n.substring(0,n.length-4),children:o.jsx(a,{svg:s[n]})},n))})};function e(t){const n={a:"a",h1:"h1",h2:"h2",li:"li",p:"p",ul:"ul",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
-`,o.jsx(m,{title:"Docs/Assets/Icons",parameters:{status:{type:"beta"}}}),`
-`,o.jsx(n.h1,{id:"icons",children:"Icons"}),`
-`,o.jsx(n.p,{children:"An overview of all currently available icons."}),`
-`,o.jsx(p,{}),`
-`,o.jsx(n.h2,{id:"related-components",children:"Related components"}),`
-`,o.jsxs(n.ul,{children:[`
-`,o.jsx(n.li,{children:o.jsx(n.a,{href:"/docs/components-media-icon--docs",children:"Icon"})}),`
-`]})]})}function v(t={}){const{wrapper:n}={...r(),...t.components};return n?o.jsx(n,{...t,children:o.jsx(e,{...t})}):e(t)}export{v as default};
