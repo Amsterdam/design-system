b58622fe4e1f90b62aa82e62f1fe3be6f90a0141
alimpens <37216945+alimpens@users.noreply.github.com>
2024-05-17 10:17:31 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@d2b9844e80a3a133f8aa84e785e0e585d90456ca 🚀

---
+++ demo-DES-774-add-field-component/assets/Footer.docs-C2UuRJKX.js
@@ -0,0 +1,20 @@
+import{j as o}from"./jsx-runtime-Nms4Y4qS.js";import{useMDXComponents as i}from"./index-DlpNa54Y.js";import{M as r,d as s,P as a}from"./index-D78YWkj8.js";import{F as m}from"./Footer.stories-BuY9xsbd.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-S2jg6kCc.js";import"../sb-preview/runtime.js";import"./index-B8XB3FuZ.js";import"./index-COebWTXQ.js";import"./index-B8K4vdXH.js";import"./index-DrFu-skq.js";import"./index.esm-DQkeu4as.js";import"./clsx-B-dksMZM.js";import"./Spotlight-BnYY0WBj.js";import"./VisuallyHidden-Db-04MJ9.js";import"./Grid-CZNbk3EV.js";import"./LinkList-Cj5_Zz5z.js";import"./Icon-tKAGJH_4.js";import"./Heading-mbayTkiK.js";import"./getHeadingElement-DiRwFEz7.js";import"./Paragraph-B6eM4Eli.js";import"./PageMenu-C8WrLt97.js";const p=`<!-- @license CC0-1.0 -->
+
+# Footer
+
+The footer is the closing section on every web page.
+Its content provides service information.
+A part is prescribed, and a part is filled based on the website’s objectives.
+
+## Guidelines
+
+- The footer is mandatory, and there is one on every page.
+- The first column focuses on contact information.
+  The information is as specific as possible and tailored to the respective website or page (e.g., including a chat option if available).
+- The second column contains links to relevant (online) sites or sources.
+- The third column refers to newsletters, social media, etc.
+- The last part of the footer is intended for links to privacy policies, cookie statements, explanations about the website, etc.
+  It is not intended for links to contact information, for example.
+`;function n(t){return o.jsxs(o.Fragment,{children:[o.jsx(r,{of:m}),`
+`,o.jsx(s,{children:p}),`
+`,o.jsx(a,{})]})}function P(t={}){const{wrapper:e}={...i(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n()}export{P as default};
