08e9fbefad91a28aeaea277af336967ae076a73d
alimpens <37216945+alimpens@users.noreply.github.com>
2024-05-17 10:15:17 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@d8eaa5719b39f9ef1f4878961a4e002b59db8b82 🚀

---
+++ demo-DES-774-add-field-component/assets/Field.docs-DBxM7H3T.js
@@ -0,0 +1,16 @@
+import{j as n}from"./jsx-runtime-Nms4Y4qS.js";import{useMDXComponents as r}from"./index-DlpNa54Y.js";import{M as e,d as s,P as a,e as p,f as m}from"./index-BFAlYXKI.js";import{F as d,W as c}from"./Field.stories-B2n5zIuD.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-B8SC6Ilt.js";import"../sb-preview/runtime.js";import"./index-B8XB3FuZ.js";import"./index-COebWTXQ.js";import"./index-B8K4vdXH.js";import"./index-DrFu-skq.js";import"./index.esm-D_7Y9awD.js";import"./clsx-B-dksMZM.js";import"./index.esm-DQkeu4as.js";import"./Label-7rbJKFpg.js";import"./Paragraph-B6eM4Eli.js";const l=`<!-- @license CC0-1.0 -->
+
+# Field
+
+Wraps around a single input and can indicate if that input has a validation error.
+
+## Guidelines
+
+Only use Field to wrap a single input. Use [Fieldset](/docs/components-forms-fieldset--docs) to wrap multiple inputs.
+`;function o(t){const i={h2:"h2",p:"p",...r(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(e,{of:d}),`
+`,n.jsx(s,{children:l}),`
+`,n.jsx(a,{}),`
+`,n.jsx(p,{}),`
+`,n.jsx(i.h2,{id:"with-error",children:"With Error"}),`
+`,n.jsx(i.p,{children:"A Field can indicate if the contained input has a validation error."}),`
+`,n.jsx(m,{of:c})]})}function P(t={}){const{wrapper:i}={...r(),...t.components};return i?n.jsx(i,{...t,children:n.jsx(o,{...t})}):o(t)}export{P as default};
