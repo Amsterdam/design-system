113274c3d1671f994c0e5e8feee7b49dc2edc912
RubenSibon <mail@rubensibon.nl>
2024-05-17 15:32:09 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@0816ed9ae5b8a36920ab853ff56f812669517d5a 🚀

---
+++ demo-DES-680-component-status/assets/Spotlight.docs-C4d1jbrK.js
@@ -0,0 +1,41 @@
+import{j as e}from"./jsx-runtime-Nms4Y4qS.js";import{useMDXComponents as s}from"./index-DlpNa54Y.js";import{M as a,d as i,P as l,e as c,f as t}from"./index-BcDQlgdc.js";import{S as m,B as d,D as h,a as p,G as u,M as x,O as j,P as f,Y as g,C as w}from"./Spotlight.stories-BqqMQCLA.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-DfSAK5o7.js";import"../sb-preview/runtime.js";import"./index-B8XB3FuZ.js";import"./index-COebWTXQ.js";import"./index-B8K4vdXH.js";import"./index-DrFu-skq.js";import"./exampleContent-CRK3_7Hv.js";import"./Spotlight-BnYY0WBj.js";import"./clsx-B-dksMZM.js";import"./Grid-qe23dp1l.js";import"./Blockquote-BYbejEe0.js";const C=`<!-- @license CC0-1.0 -->
+
+# Spotlight
+
+Emphasizes a section on a page through a distinctive background colour.
+
+## Guidelines
+
+Refer to [this overview on Stijlweb](https://amsterdam.nl/stijlweb/basiselementen/kleuren/#PagCls_15671872) to determine whether you can use black or white text on the background colour of your choice.
+
+## Relevant WCAG Requirements
+
+- [WCAG 1.4.3](https://www.w3.org/TR/WCAG21/#contrast-minimum): Large-scale text and images of large-scale text have a contrast ratio of at least 3:1;
+
+## References
+
+- [Color and contrast accessibility](https://web.dev/articles/color-and-contrast-accessibility)
+`;function r(o){const n={code:"code",h3:"h3",p:"p",...s(),...o.components};return e.jsxs(e.Fragment,{children:[e.jsx(a,{of:m}),`
+`,e.jsx(i,{children:C}),`
+`,e.jsx(l,{}),`
+`,e.jsx(c,{}),`
+`,e.jsx(n.h3,{id:"blue",children:"Blue"}),`
+`,e.jsx(t,{of:d}),`
+`,e.jsx(n.h3,{id:"dark-blue",children:"Dark Blue"}),`
+`,e.jsx(t,{of:h}),`
+`,e.jsx(n.h3,{id:"dark-green",children:"Dark Green"}),`
+`,e.jsx(t,{of:p}),`
+`,e.jsx(n.h3,{id:"green",children:"Green"}),`
+`,e.jsx(t,{of:u}),`
+`,e.jsx(n.h3,{id:"magenta",children:"Magenta"}),`
+`,e.jsx(t,{of:x}),`
+`,e.jsx(n.h3,{id:"orange",children:"Orange"}),`
+`,e.jsx(t,{of:j}),`
+`,e.jsx(n.h3,{id:"purple",children:"Purple"}),`
+`,e.jsx(t,{of:f}),`
+`,e.jsx(n.h3,{id:"yellow",children:"Yellow"}),`
+`,e.jsx(t,{of:g}),`
+`,e.jsx(n.h3,{id:"custom-html-element",children:"Custom HTML Element"}),`
+`,e.jsxs(n.p,{children:["By default, a Spotlight renders a ",e.jsx(n.code,{children:"<div>"}),`.
+Use the `,e.jsx(n.code,{children:"as"})," prop to make your markup more semantic."]}),`
+`,e.jsx(t,{of:w})]})}function X(o={}){const{wrapper:n}={...s(),...o.components};return n?e.jsx(n,{...o,children:e.jsx(r,{...o})}):r(o)}export{X as default};
---
+++ demo-DES-680-component-status/assets/icon-gallery.docs-B71o5INX.js
@@ -0,0 +1,9 @@
+import{j as n}from"./jsx-runtime-Nms4Y4qS.js";import{useMDXComponents as r}from"./index-DlpNa54Y.js";import{I as i,g as c,M as m}from"./index-BcDQlgdc.js";import{I as s}from"./index.esm-DQkeu4as.js";import{I as a}from"./Icon-tKAGJH_4.js";import{B as l}from"./Badge-D52ssMM1.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-DfSAK5o7.js";import"../sb-preview/runtime.js";import"./index-B8XB3FuZ.js";import"./index-COebWTXQ.js";import"./index-B8K4vdXH.js";import"./index-DrFu-skq.js";import"./clsx-B-dksMZM.js";const p=()=>{const t=Object.keys(s);return n.jsx(i,{children:t.map(o=>n.jsx(c,{name:o.substring(0,o.length-4),children:n.jsx(a,{svg:s[o]})},o))})};function e(t){const o={a:"a",h1:"h1",h2:"h2",li:"li",p:"p",ul:"ul",...r(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(m,{title:"Docs/Assets/Icons"}),`
+`,n.jsx(l,{label:"Beta",color:"orange"}),`
+`,n.jsx(o.h1,{id:"icons",children:"Icons"}),`
+`,n.jsx(o.p,{children:"An overview of all currently available icons."}),`
+`,n.jsx(p,{}),`
+`,n.jsx(o.h2,{id:"related-components",children:"Related components"}),`
+`,n.jsxs(o.ul,{children:[`
+`,n.jsx(o.li,{children:n.jsx(o.a,{href:"/docs/components-media-icon--docs",children:"Icon"})}),`
+`]})]})}function X(t={}){const{wrapper:o}={...r(),...t.components};return o?n.jsx(o,{...t,children:n.jsx(e,{...t})}):e(t)}export{X as default};
