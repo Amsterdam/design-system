7b6ba98530caaefafedada5b89a175ef0b1a8784
Niels Roozemond <n.roozemond@amsterdam.nl>
2024-05-24 17:37:11 +0200

    feat: File Input (#1218)
    Co-authored-by: Vincent Smedinga <v.smedinga@amsterdam.nl>
    Co-authored-by: Aram <37216945+alimpens@users.noreply.github.com>

---
+++ storybook/src/components/FileInput/FileInput.stories.tsx
@@ -0,0 +1,52 @@
+/**
+ * @license EUPL-1.2+
+ * Copyright Gemeente Amsterdam
+ */
+
+import { FileInput } from '@amsterdam/design-system-react/src'
+import { Meta, StoryObj } from '@storybook/react'
+
+const meta = {
+  title: 'Components/Forms/File Input',
+  component: FileInput,
+  args: {
+    accept: undefined,
+    multiple: false,
+    disabled: false,
+  },
+  argTypes: {
+    accept: {
+      control: {
+        type: 'text',
+      },
+    },
+    multiple: {
+      control: {
+        type: 'boolean',
+      },
+    },
+    disabled: {
+      control: {
+        type: 'boolean',
+      },
+    },
+  },
+} satisfies Meta<typeof FileInput>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {}
+
+export const Multiple: Story = {
+  args: { multiple: true },
+}
+
+export const Accept: Story = {
+  args: { accept: 'application/pdf' },
+}
+
+export const Disabled: Story = {
+  args: { disabled: true },
+}
