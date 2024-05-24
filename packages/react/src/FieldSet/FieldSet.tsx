d274d03889451615e3273fe4db0189637e0eca96
Aram Limpens <a.limpens@amsterdam.nl>
2024-05-24 09:10:40 +0200

    Add invalid prop

--- packages/react/src/FieldSet/FieldSet.tsx
+++ packages/react/src/FieldSet/FieldSet.tsx
@@ -8,13 +8,19 @@ import { forwardRef } from 'react'
 import type { ForwardedRef, HTMLAttributes, PropsWithChildren } from 'react'
 
 export type FieldSetProps = PropsWithChildren<HTMLAttributes<HTMLFieldSetElement>> & {
+  /** Whether the field set has an input with a validation error */
+  invalid?: boolean
   /** The text for the caption. */
   legend: string
 }
 
 export const FieldSet = forwardRef(
-  ({ children, className, legend, ...restProps }: FieldSetProps, ref: ForwardedRef<HTMLFieldSetElement>) => (
-    <fieldset {...restProps} ref={ref} className={clsx('ams-field-set', className)}>
+  ({ children, className, invalid, legend, ...restProps }: FieldSetProps, ref: ForwardedRef<HTMLFieldSetElement>) => (
+    <fieldset
+      {...restProps}
+      ref={ref}
+      className={clsx('ams-field-set', invalid && 'ams-field-set--invalid', className)}
+    >
       <legend className="ams-field-set__legend">{legend}</legend>
       {children}
     </fieldset>
