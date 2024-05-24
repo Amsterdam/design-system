27c571fd2d62c8089ffc8e8c507bae447d044b8f
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-05-24 18:29:23 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@6daf2c1b493f1ce8daf65a88ff03b98ad2031480 🚀

---
+++ demo-DES-781-reset-opacity/assets/Blockquote.docs-BZobp_pQ.js
@@ -0,0 +1,25 @@
+import{j as t}from"./jsx-runtime-Nms4Y4qS.js";import{useMDXComponents as r}from"./index-DlpNa54Y.js";import{M as s,d as i,P as a,e as m,f as c}from"./index-Cmqq6wSh.js";import{B as h,I as l}from"./Blockquote.stories-CMUg-udV.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-DuP7Axqj.js";import"../sb-preview/runtime.js";import"./index-B8XB3FuZ.js";import"./index-COebWTXQ.js";import"./index-B8K4vdXH.js";import"./index-DrFu-skq.js";import"./decorators-Dh4yEj-W.js";import"./exampleContent-CRK3_7Hv.js";import"./Blockquote-XJcoMbef.js";import"./clsx-B-dksMZM.js";const u=`<!-- @license CC0-1.0 -->
+
+# Blockquote
+
+We use blockquotes to emphasize citations.
+A quote appears differently from the main text to make it stand out.
+
+## Guidelines
+
+- Quote someone’s literal written or oral text.
+- A quote has a width of half or a whole column of text.
+- The component adds the correct quotation marks.
+
+## Relevant WCAG requirements
+
+- [WCAG 3.1.2](https://www.w3.org/WAI/WCAG21/Understanding/language-of-parts.html): if the citation is in a language other than that of the surrounding content, indicate so through the \`lang\` attribute.
+`;function n(e){const o={code:"code",h2:"h2",h3:"h3",p:"p",...r(),...e.components};return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:h}),`
+`,t.jsx(i,{children:u}),`
+`,t.jsx(a,{}),`
+`,t.jsx(m,{}),`
+`,t.jsx(o.h2,{id:"examples",children:"Examples"}),`
+`,t.jsx(o.h3,{id:"inverse-colour",children:"Inverse colour"}),`
+`,t.jsxs(o.p,{children:["Set the ",t.jsx(o.code,{children:"inverseColor"}),` prop if the Blockquote sits on a dark background.
+This ensures the colour of the text provides enough contrast.`]}),`
+`,t.jsx(c,{of:l})]})}function E(e={}){const{wrapper:o}={...r(),...e.components};return o?t.jsx(o,{...e,children:t.jsx(n,{...e})}):n(e)}export{E as default};
