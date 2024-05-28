0b1eaffb3fc44ba8ded37bda211110256a11bc90
dlnr <niels@dlnr.dev>
2024-05-28 08:49:58 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@9541483ac3b50b38cd584dc86b761d322be579f5 🚀

---
+++ demo-DES-654-table-of-content/assets/TableOfContents.docs-BA0kfOqQ.js
@@ -0,0 +1,7 @@
+import{j as t}from"./jsx-runtime-Nms4Y4qS.js";import{useMDXComponents as e}from"./index-DlpNa54Y.js";import{M as s,d as i,P as m,e as a}from"./index-Csc-vEIE.js";import{T as p}from"./TableOfContents.stories-DR3fjbVd.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-D4ifTuHK.js";import"../sb-preview/runtime.js";import"./index-B8XB3FuZ.js";import"./index-COebWTXQ.js";import"./index-B8K4vdXH.js";import"./index-DrFu-skq.js";import"./clsx-B-dksMZM.js";const x=`<!-- @license CC0-1.0 -->
+
+# Table of Contents
+`;function r(o){return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:p}),`
+`,t.jsx(i,{children:x}),`
+`,t.jsx(m,{}),`
+`,t.jsx(a,{})]})}function b(o={}){const{wrapper:n}={...e(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(r,{...o})}):r()}export{b as default};
