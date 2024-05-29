89f6fbd80fcccf4e5c1fd2b468e8a8db26651e7f
alimpens <37216945+alimpens@users.noreply.github.com>
2024-05-29 09:41:00 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@3c3348b2f30c69cd6771f82bef07151c751ef25b 🚀

---
+++ demo-DES-735-inputs-on-ios-bugs/assets/Logo.stories-d5TXccXX.js
@@ -0,0 +1,24 @@
+import{L as M}from"./Logo-C5ZbuiYN.js";/**
+ * @license EUPL-1.2+
+ * Copyright Gemeente Amsterdam
+ */const h={title:"Components/Media/Logo",component:M},e={},a={args:{brand:"ggd-amsterdam"}},r={args:{brand:"stadsarchief"}},s={args:{brand:"stadsbank-van-lening"}},n={args:{brand:"vga-verzekeringen"}},o={args:{brand:"museum-weesp"}};var t,c,d;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:"{}",...(d=(c=e.parameters)==null?void 0:c.docs)==null?void 0:d.source}}};var m,g,p;a.parameters={...a.parameters,docs:{...(m=a.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    brand: 'ggd-amsterdam'
+  }
+}`,...(p=(g=a.parameters)==null?void 0:g.docs)==null?void 0:p.source}}};var u,i,b;r.parameters={...r.parameters,docs:{...(u=r.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    brand: 'stadsarchief'
+  }
+}`,...(b=(i=r.parameters)==null?void 0:i.docs)==null?void 0:b.source}}};var l,S,f;s.parameters={...s.parameters,docs:{...(l=s.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    brand: 'stadsbank-van-lening'
+  }
+}`,...(f=(S=s.parameters)==null?void 0:S.docs)==null?void 0:f.source}}};var k,v,L;n.parameters={...n.parameters,docs:{...(k=n.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  args: {
+    brand: 'vga-verzekeringen'
+  }
+}`,...(L=(v=n.parameters)==null?void 0:v.docs)==null?void 0:L.source}}};var V,_,z;o.parameters={...o.parameters,docs:{...(V=o.parameters)==null?void 0:V.docs,source:{originalSource:`{
+  args: {
+    brand: 'museum-weesp'
+  }
+}`,...(z=(_=o.parameters)==null?void 0:_.docs)==null?void 0:z.source}}};const G=["Default","GgdAmsterdam","Stadsarchief","StadsbankVanLening","VgaVerzekeringen","MuseumWeesp"],j=Object.freeze(Object.defineProperty({__proto__:null,Default:e,GgdAmsterdam:a,MuseumWeesp:o,Stadsarchief:r,StadsbankVanLening:s,VgaVerzekeringen:n,__namedExportsOrder:G,default:h},Symbol.toStringTag,{value:"Module"}));export{a as G,j as L,o as M,r as S,n as V,s as a};
