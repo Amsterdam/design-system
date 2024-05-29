0cddf781af5da450dea9a35769f57e3c46063e2d
GitHub <github-action@users.noreply.github.com>
2024-05-29 09:10:41 +0000

    Remove feature branch folder

--- demo-DES-680-component-badges/assets/icon-gallery.docs-BqZRh3Tw.js
+++
@@ -1,9 +0,0 @@
-import{j as n}from"./jsx-runtime-Nms4Y4qS.js";import{useMDXComponents as r}from"./index-DlpNa54Y.js";import{e as i}from"./index-BhFxsZrh.js";import{I as c,g as m,M as a}from"./index-Ca_zOuQ5.js";import{I as s}from"./index.esm-DQkeu4as.js";import{I as l}from"./Icon-tKAGJH_4.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-Jc4qTc2x.js";import"../sb-preview/runtime.js";import"./index-B8XB3FuZ.js";import"./index-COebWTXQ.js";import"./index-B8K4vdXH.js";import"./index-DrFu-skq.js";import"./clsx-B-dksMZM.js";const p=()=>{const t=Object.keys(s);return n.jsx(c,{children:t.map(o=>n.jsx(m,{name:o.substring(0,o.length-4),children:n.jsx(l,{svg:s[o]})},o))})};function e(t){const o={a:"a",h1:"h1",h2:"h2",li:"li",p:"p",ul:"ul",...r(),...t.components};return n.jsxs(n.Fragment,{children:[`
-`,n.jsx(a,{title:"Docs/Assets/Icons",parameters:{badges:[i.BETA]}}),`
-`,n.jsx(o.h1,{id:"icons",children:"Icons"}),`
-`,n.jsx(o.p,{children:"An overview of all currently available icons."}),`
-`,n.jsx(p,{}),`
-`,n.jsx(o.h2,{id:"related-components",children:"Related components"}),`
-`,n.jsxs(o.ul,{children:[`
-`,n.jsx(o.li,{children:n.jsx(o.a,{href:"/docs/components-media-icon--docs",children:"Icon"})}),`
-`]})]})}function w(t={}){const{wrapper:o}={...r(),...t.components};return o?n.jsx(o,{...t,children:n.jsx(e,{...t})}):e(t)}export{w as default};
