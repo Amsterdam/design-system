f750a586da8008ea819f0a618629f53a9674b503
RubenSibon <mail@rubensibon.nl>
2024-05-31 11:43:31 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@5d3a78f0e11dccaab897280188f2c8d02d36cb50 🚀

---
+++ demo-DES-680-lifecycle-status/assets/Header.docs-CoKuHRW5.js
@@ -0,0 +1,37 @@
+import{j as n}from"./jsx-runtime-Nms4Y4qS.js";import{useMDXComponents as a}from"./index-DlpNa54Y.js";import{M as o,d as r,P as m,e as d,f as e}from"./index-SJQ_bW7h.js";import{H as h,W as l,a as u,b as p,c,d as x,e as g,f as j}from"./Header.stories-Dm3gthCL.js";import{S as f}from"./StatusBadge-tBCrwbgJ.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-Cnf0w6m6.js";import"../sb-preview/runtime.js";import"./index-B8XB3FuZ.js";import"./index-COebWTXQ.js";import"./index-B8K4vdXH.js";import"./index-DrFu-skq.js";import"./index.esm-DQkeu4as.js";import"./Header-DF3VOPHu.js";import"./clsx-B-dksMZM.js";import"./VisuallyHidden-Db-04MJ9.js";import"./Logo-C5ZbuiYN.js";import"./Heading-CWbN3elW.js";import"./getHeadingElement-DiRwFEz7.js";import"./PageMenu-DQ_snOzP.js";import"./Icon-CuaumHXX.js";import"./Badge-BRa1hh1O.js";const w=`<!-- @license CC0-1.0 -->
+
+# Header
+
+Arranges the City’s logo, the title of the website, and a page menu.
+
+## Guidelines
+
+- Use the Header when the site is hosted on amsterdam.nl or one of its subdomains.
+- The page menu can contain a maximum of 5 items, including menu and search.
+- When you have a long subsite title, use no or as few page menu items as possible.
+
+## References
+
+- A Header is a [landmark](https://www.w3.org/TR/wai-aria-practices-1.1/#aria_landmark_roles) and can be use to group navigation elements.
+- [WCAG 3.2.3](https://wcag.com/designers/3-2-3-consistent-navigation/) Consistent Navigation: Navigation menus that appear on multiple pages are consistent.
+`;function s(i){const t={a:"a",h2:"h2",p:"p",...a(),...i.components};return n.jsxs(n.Fragment,{children:[n.jsx(o,{of:h}),`
+`,n.jsx(f,{statusReason:"Excepteur magna deserunt id quis ullamco est occaecat velit laborum incididunt est sint nostrud."}),`
+`,n.jsx(r,{children:w}),`
+`,n.jsx(m,{}),`
+`,n.jsx(d,{}),`
+`,n.jsx(t.h2,{id:"with-logo-variant",children:"With logo variant"}),`
+`,n.jsx(e,{of:l}),`
+`,n.jsx(t.h2,{id:"with-subsite-title",children:"With subsite title"}),`
+`,n.jsx(e,{of:u}),`
+`,n.jsx(t.h2,{id:"with-menu",children:"With menu"}),`
+`,n.jsx(e,{of:p}),`
+`,n.jsx(t.h2,{id:"with-links",children:"With links"}),`
+`,n.jsxs(t.p,{children:["Use a ",n.jsx(t.a,{href:"?path=/docs/components-navigation-page-menu--docs",children:"Page Menu"}),` to add links.
+A Page Menu in the Header should not wrap.`]}),`
+`,n.jsx(e,{of:c}),`
+`,n.jsx(t.h2,{id:"with-links-and-menu",children:"With links and menu"}),`
+`,n.jsx(e,{of:x}),`
+`,n.jsx(t.h2,{id:"with-a-title-and-menu",children:"With a title and menu"}),`
+`,n.jsx(e,{of:g}),`
+`,n.jsx(t.h2,{id:"with-a-title-links-and-menu",children:"With a title, links and menu"}),`
+`,n.jsx(e,{of:j})]})}function F(i={}){const{wrapper:t}={...a(),...i.components};return t?n.jsx(t,{...i,children:n.jsx(s,{...i})}):s(i)}export{F as default};
