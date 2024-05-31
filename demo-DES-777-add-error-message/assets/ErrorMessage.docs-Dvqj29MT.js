e50469344a1af2951bc56188ab1ddfc1a096950b
alimpens <37216945+alimpens@users.noreply.github.com>
2024-05-31 15:50:28 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@b4196c224de569c18235aa36fa0fe1f244e8e9f8 🚀

---
+++ demo-DES-777-add-error-message/assets/ErrorMessage.docs-Dvqj29MT.js
@@ -0,0 +1,19 @@
+import{j as e}from"./jsx-runtime-Nms4Y4qS.js";import{useMDXComponents as t}from"./index-DlpNa54Y.js";import{M as s,d as i,P as a,e as m,f as c}from"./index-CD6_Z1H8.js";import{E as d,W as h}from"./ErrorMessage.stories-I9MvltWm.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-DH6MbhPE.js";import"../sb-preview/runtime.js";import"./index-B8XB3FuZ.js";import"./index-COebWTXQ.js";import"./index-B8K4vdXH.js";import"./index-DrFu-skq.js";import"./ErrorMessage-BIVZW8RN.js";import"./clsx-B-dksMZM.js";import"./VisuallyHidden-Db-04MJ9.js";const p=`<!-- @license CC0-1.0 -->
+
+# Error Message
+
+Show an error message when there is a form field validation error.
+In the error message explain what went wrong and how to fix it.
+
+See the [Field](/docs/components-forms-field--docs) and [Field Set](/docs/components-forms-field-set--docs) documentation for an explanation and examples of how to use error messages in a form.
+
+Read the documentation by [NL Design System](https://www.nldesignsystem.nl/richtlijnen/formulieren/foutmeldingen) and [Gov.uk](https://design-system.service.gov.uk/components/error-message/) for more information on the contents of error messages and when to show them.
+`;function r(o){const n={code:"code",h2:"h2",p:"p",...t(),...o.components};return e.jsxs(e.Fragment,{children:[e.jsx(s,{of:d}),`
+`,e.jsx(i,{children:p}),`
+`,e.jsx(a,{}),`
+`,e.jsx(m,{}),`
+`,e.jsx(n.h2,{id:"with-a-custom-prefix",children:"With a custom prefix"}),`
+`,e.jsxs(n.p,{children:[`Error messages are automatically prefixed with a visually hidden text, the Dutch phrase "Invoerfout: ".
+This makes the error message more clear for screen reader users.
+If you want to change this prefix, to support another language for example, you can use the `,e.jsx(n.code,{children:"prefix"})," prop."]}),`
+`,e.jsx(c,{of:h})]})}function F(o={}){const{wrapper:n}={...t(),...o.components};return n?e.jsx(n,{...o,children:e.jsx(r,{...o})}):r(o)}export{F as default};
--- demo-DES-777-add-error-message/assets/icon-gallery.docs-EPDfZJ0B.js
+++
@@ -1,8 +0,0 @@
-import{j as n}from"./jsx-runtime-Nms4Y4qS.js";import{useMDXComponents as r}from"./index-DlpNa54Y.js";import{I as i,g as c,M as m}from"./index-D_YJH48_.js";import{I as s}from"./index.esm-DQkeu4as.js";import{I as l}from"./Icon-CuaumHXX.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-CwXlHxFg.js";import"../sb-preview/runtime.js";import"./index-B8XB3FuZ.js";import"./index-COebWTXQ.js";import"./index-B8K4vdXH.js";import"./index-DrFu-skq.js";import"./clsx-B-dksMZM.js";const a=()=>{const t=Object.keys(s);return n.jsx(i,{children:t.map(o=>n.jsx(c,{name:o.substring(0,o.length-4),children:n.jsx(l,{svg:s[o]})},o))})};function e(t){const o={a:"a",h1:"h1",h2:"h2",li:"li",p:"p",ul:"ul",...r(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(m,{title:"Docs/Assets/Icons"}),`
-`,n.jsx(o.h1,{id:"icons",children:"Icons"}),`
-`,n.jsx(o.p,{children:"An overview of all currently available icons."}),`
-`,n.jsx(a,{}),`
-`,n.jsx(o.h2,{id:"related-components",children:"Related components"}),`
-`,n.jsxs(o.ul,{children:[`
-`,n.jsx(o.li,{children:n.jsx(o.a,{href:"/docs/components-media-icon--docs",children:"Icon"})}),`
-`]})]})}function C(t={}){const{wrapper:o}={...r(),...t.components};return o?n.jsx(o,{...t,children:n.jsx(e,{...t})}):e(t)}export{C as default};
