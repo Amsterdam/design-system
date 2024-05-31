f32af72e40f9b5671710c9f88ca3af1312a9b50e
RubenSibon <mail@rubensibon.nl>
2024-05-31 14:01:07 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@94ac6cd9c00fcba77835f89d1f14e8778d5969a2 🚀

---
+++ demo-DES-680-lifecycle-status/assets/icon-gallery.docs-CyD5hivR.js
@@ -0,0 +1,9 @@
+import{j as n}from"./jsx-runtime-Nms4Y4qS.js";import{useMDXComponents as e}from"./index-DlpNa54Y.js";import{I as i,g as c,M as m}from"./index-5Lgl_BbF.js";import{I as r}from"./index.esm-DQkeu4as.js";import{I as a}from"./Icon-CuaumHXX.js";import{S as l}from"./StatusBadge-DpMkLybt.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-MSI-J5hY.js";import"../sb-preview/runtime.js";import"./index-B8XB3FuZ.js";import"./index-COebWTXQ.js";import"./index-B8K4vdXH.js";import"./index-DrFu-skq.js";import"./clsx-B-dksMZM.js";import"./Badge-BRa1hh1O.js";const p=()=>{const t=Object.keys(r);return n.jsx(i,{children:t.map(o=>n.jsx(c,{name:o.substring(0,o.length-4),children:n.jsx(a,{svg:r[o]})},o))})};function s(t){const o={a:"a",h1:"h1",h2:"h2",li:"li",p:"p",ul:"ul",...e(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(m,{title:"Docs/Assets/Icons"}),`
+`,n.jsx(l,{statusReason:"Anim tempor nulla proident culpa aliquip culpa Lorem magna minim culpa culpa reprehenderit laboris dolor."}),`
+`,n.jsx(o.h1,{id:"icons",children:"Icons"}),`
+`,n.jsx(o.p,{children:"An overview of all currently available icons."}),`
+`,n.jsx(p,{}),`
+`,n.jsx(o.h2,{id:"related-components",children:"Related components"}),`
+`,n.jsxs(o.ul,{children:[`
+`,n.jsx(o.li,{children:n.jsx(o.a,{href:"/docs/components-media-icon--docs",children:"Icon"})}),`
+`]})]})}function w(t={}){const{wrapper:o}={...e(),...t.components};return o?n.jsx(o,{...t,children:n.jsx(s,{...t})}):s(t)}export{w as default};
