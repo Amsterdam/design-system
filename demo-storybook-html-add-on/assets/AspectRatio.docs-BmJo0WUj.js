b6fe74645fb560b929abb4e3692c84313762833b
dlnr <niels@dlnr.dev>
2024-05-31 11:26:58 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@5bf51bf01e769fb6d570005db4c115a2b48ef87b 🚀

---
+++ demo-storybook-html-add-on/assets/AspectRatio.docs-BmJo0WUj.js
@@ -0,0 +1,40 @@
+import{j as e}from"./jsx-runtime-Nms4Y4qS.js";import{useMDXComponents as i}from"./index-DlpNa54Y.js";import{M as a,d as r,P as l,e as d,f as o}from"./index-DmaH4xa8.js";import{A as x,D as h,E as m,W as p,S as c,T as f,a as j}from"./AspectRatio.stories-D__dCfe8.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-BD1Z6UaA.js";import"../sb-preview/runtime.js";import"./index-B8XB3FuZ.js";import"./index-COebWTXQ.js";import"./index-B8K4vdXH.js";import"./index-DrFu-skq.js";import"./AspectRatio-BxbFlkWk.js";import"./clsx-B-dksMZM.js";import"./Image-z3YSpimw.js";const u=`<!-- @license CC0-1.0 -->
+
+# Aspect Ratio
+
+Aspect Ratio updates the height of an element with its width.
+
+These are the proportions we use:
+
+| keyword   | ratio |
+| :-------- | ----: |
+| \`2x-wide\` |  32:9 |
+| \`x-wide\`  |  16:9 |
+| \`wide\`    |   5:4 |
+| \`square\`  |   1:1 |
+| \`tall\`    |   4:5 |
+| \`x-tall\`  |  9:16 |
+
+## Guidelines
+
+- Use this component to constrain images, videos and other elements to one of the available aspect ratios.
+`;function s(t){const n={h2:"h2",p:"p",...i(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(a,{of:x}),`
+`,e.jsx(r,{children:u}),`
+`,e.jsx(l,{}),`
+`,e.jsx(d,{}),`
+`,e.jsx(n.h2,{id:"double-extra-wide",children:"Double extra wide"}),`
+`,e.jsx(n.p,{children:"We only use this aspect ratio for a ‘hero’ image across the entire website width."}),`
+`,e.jsx(o,{of:h}),`
+`,e.jsx(n.h2,{id:"extra-wide",children:"Extra wide"}),`
+`,e.jsx(o,{of:m}),`
+`,e.jsx(n.h2,{id:"wide",children:"Wide"}),`
+`,e.jsx(o,{of:p}),`
+`,e.jsx(n.h2,{id:"square",children:"Square"}),`
+`,e.jsx(o,{of:c}),`
+`,e.jsx(n.h2,{id:"tall",children:"Tall"}),`
+`,e.jsx(o,{of:f}),`
+`,e.jsx(n.h2,{id:"extra-tall",children:"Extra Tall"}),`
+`,e.jsx(n.p,{children:`This variant may be suitable for telephones.
+Most devices nowadays have an aspect ratio of 9:19.5.
+Images of 9:16 fill approximately 80% of the screen.`}),`
+`,e.jsx(o,{of:j})]})}function X(t={}){const{wrapper:n}={...i(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(s,{...t})}):s(t)}export{X as default};
---
+++ demo-storybook-html-add-on/assets/TimeInput.stories-oGlYJ8Pj.js
@@ -0,0 +1,12 @@
+import{j as g}from"./jsx-runtime-Nms4Y4qS.js";import{c as b}from"./clsx-B-dksMZM.js";import{r as y}from"./index-BwDkhjyp.js";const s=y.forwardRef(({className:t,invalid:f,...v},_)=>g.jsx("input",{...v,"aria-invalid":f||void 0,className:b("ams-time-input",t),ref:_,type:"time"}));s.displayName="TimeInput";try{s.displayName="TimeInput",s.__docgenInfo={description:"",displayName:"TimeInput",props:{invalid:{defaultValue:null,description:"Whether the value fails a validation rule.",name:"invalid",required:!1,type:{name:"boolean"}}}}}catch{}/**
+ * @license EUPL-1.2+
+ * Copyright Gemeente Amsterdam
+ */const I={title:"Components/Forms/Time Input",component:s,args:{disabled:!1,invalid:!1},argTypes:{disabled:{description:"Prevents interaction. Avoid if possible."}}},e={},a={args:{invalid:!0}},r={args:{disabled:!0}};var o,i,n;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:"{}",...(n=(i=e.parameters)==null?void 0:i.docs)==null?void 0:n.source}}};var d,l,p;a.parameters={...a.parameters,docs:{...(d=a.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    invalid: true
+  }
+}`,...(p=(l=a.parameters)==null?void 0:l.docs)==null?void 0:p.source}}};var c,m,u;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    disabled: true
+  }
+}`,...(u=(m=r.parameters)==null?void 0:m.docs)==null?void 0:u.source}}};const T=["Default","Invalid","Disabled"],D=Object.freeze(Object.defineProperty({__proto__:null,Default:e,Disabled:r,Invalid:a,__namedExportsOrder:T,default:I},Symbol.toStringTag,{value:"Module"}));export{r as D,a as I,D as T};
