6916b16531e723962067ef632c17d1c6b8ca3101
Ruben Sibon <r.sibon@amsterdam.nl>
2024-05-31 12:25:58 +0200

    feat(status): :sparkles: component lifecycle badge for stories

---
+++ storybook/src/docs/components/StatusBadge.tsx
@@ -0,0 +1,19 @@
+import './status-badge.css'
+import { Badge } from '@amsterdam/design-system-react/src'
+
+export const StatusBadge = ({
+  status = 'beta',
+  statusReason = '',
+  documentTop = true,
+}: {
+  status: 'beta'
+  statusReason: string
+  documentTop: boolean
+}) => {
+  return (
+    <span className={`ams-storybook-status-badge${documentTop ? ` ams-storybook-status-badge--top` : undefined}`}>
+      <Badge label={status} color="orange" />
+      {statusReason && <span>{statusReason}</span>}
+    </span>
+  )
+}
