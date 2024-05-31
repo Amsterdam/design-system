4c289ab592fd41e23aa11af71e034b186281c5c3
alimpens <37216945+alimpens@users.noreply.github.com>
2024-05-31 12:45:17 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@eaab9046d67ea325a3575bab820eda83137cc361 🚀

---
+++ demo-DES-784-disallow-directional-style-rules/assets/Link.stories-DklfUgk2.js
@@ -0,0 +1,31 @@
+import{j as r}from"./jsx-runtime-Nms4Y4qS.js";import{L as x}from"./Link-DfaTqfXA.js";import{P as j}from"./Paragraph-C4jU4-Z4.js";const B={title:"Components/Navigation/Link",component:x,args:{children:"Link label",href:"#",onBackground:void 0},argTypes:{children:{description:"The link text.",table:{disable:!1}},onBackground:{control:{type:"radio",labels:{undefined:"default",light:"light",dark:"dark"}},options:[void 0,"light","dark"]},href:{description:"The url for the link.",name:"href",type:{name:"string",required:!1}}}},a={},n={},o={args:{children:"typograaf",variant:"inline"},decorators:[e=>r.jsxs(j,{children:["Jouw ",r.jsx(e,{})," biedt mij zulke exquise schreven!"]})]},s={args:{onBackground:"dark"},decorators:[e=>r.jsx("div",{style:{background:"#004699",display:"inline",padding:"1rem"},children:r.jsx(e,{})})]},t={args:{onBackground:"light"},decorators:[e=>r.jsx("div",{style:{background:"#FFE600",display:"inline",padding:"1rem"},children:r.jsx(e,{})})]};var i,d,c;a.parameters={...a.parameters,docs:{...(i=a.parameters)==null?void 0:i.docs,source:{originalSource:"{}",...(c=(d=a.parameters)==null?void 0:d.docs)==null?void 0:c.source}}};var l,p,g;n.parameters={...n.parameters,docs:{...(l=n.parameters)==null?void 0:l.docs,source:{originalSource:"{}",...(g=(p=n.parameters)==null?void 0:p.docs)==null?void 0:g.source}}};var u,m,k;o.parameters={...o.parameters,docs:{...(u=o.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    children: 'typograaf',
+    variant: 'inline'
+  },
+  decorators: [Story => <Paragraph>
+        Jouw <Story /> biedt mij zulke exquise schreven!
+      </Paragraph>]
+}`,...(k=(m=o.parameters)==null?void 0:m.docs)==null?void 0:k.source}}};var h,y,f;s.parameters={...s.parameters,docs:{...(h=s.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    onBackground: 'dark'
+  },
+  decorators: [Story => <div style={{
+    background: '#004699',
+    display: 'inline',
+    padding: '1rem'
+  }}>
+        <Story />
+      </div>]
+}`,...(f=(y=s.parameters)==null?void 0:y.docs)==null?void 0:f.source}}};var S,v,b;t.parameters={...t.parameters,docs:{...(S=t.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    onBackground: 'light'
+  },
+  decorators: [Story => <div style={{
+    background: '#FFE600',
+    display: 'inline',
+    padding: '1rem'
+  }}>
+        <Story />
+      </div>]
+}`,...(b=(v=t.parameters)==null?void 0:v.docs)==null?void 0:b.source}}};const L=["Default","Standalone","Inline","onDarkBackground","onLightBackground"],E=Object.freeze(Object.defineProperty({__proto__:null,Default:a,Inline:o,Standalone:n,__namedExportsOrder:L,default:B,onDarkBackground:s,onLightBackground:t},Symbol.toStringTag,{value:"Module"}));export{o as I,E as L,n as S,t as a,s as o};
