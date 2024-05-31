771c91dfb208ac6957fc9ab382aa36c781b07f65
dlnr <niels@dlnr.dev>
2024-05-31 11:21:20 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@8da0e72eedf622456a692e4836ea542088648c51 🚀

---
+++ demo-DES-654-table-of-content/assets/TableOfContents.docs-Cz9yjXr6.js
@@ -0,0 +1,14 @@
+import{j as t}from"./jsx-runtime-Nms4Y4qS.js";import{useMDXComponents as i}from"./index-DlpNa54Y.js";import{M as r,d as a,P as m,e as l,f as o}from"./index-CV7CaedL.js";import{T as c,M as p,A as h}from"./TableOfContents.stories-Du_ZrWXl.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-BRA3n6D9.js";import"../sb-preview/runtime.js";import"./index-B8XB3FuZ.js";import"./index-COebWTXQ.js";import"./index-B8K4vdXH.js";import"./index-DrFu-skq.js";import"./index.esm-DWEUIgEW.js";import"./clsx-B-dksMZM.js";import"./index.esm-DQkeu4as.js";import"./exampleContent-CRK3_7Hv.js";const f=`<!-- @license CC0-1.0 -->
+
+# Table of Contents
+
+A table of contents is a list of links to sections of a document.
+`;function s(n){const e={h2:"h2",p:"p",...i(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(r,{of:c}),`
+`,t.jsx(a,{children:f}),`
+`,t.jsx(m,{}),`
+`,t.jsx(l,{}),`
+`,t.jsx(e.h2,{id:"multilevel",children:"Multilevel"}),`
+`,t.jsx(o,{of:p}),`
+`,t.jsx(e.h2,{id:"active",children:"Active"}),`
+`,t.jsx(e.p,{children:"When the user scrolls the page, the active section in the table of contents will be highlighted."}),`
+`,t.jsx(o,{of:h})]})}function y(n={}){const{wrapper:e}={...i(),...n.components};return e?t.jsx(e,{...n,children:t.jsx(s,{...n})}):s(n)}export{y as default};
