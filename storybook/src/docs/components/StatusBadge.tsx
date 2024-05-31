d87939c1ecb925aea42f68b4f21695e92ed83475
Ruben Sibon <r.sibon@amsterdam.nl>
2024-05-31 15:30:41 +0200

    feat(status): :fire: remove class modifier for top-level

--- storybook/src/docs/components/StatusBadge.tsx
+++ storybook/src/docs/components/StatusBadge.tsx
@@ -1,17 +1,9 @@
 import './status-badge.css'
 import { Badge } from '@amsterdam/design-system-react/src'
 
-export const StatusBadge = ({
-  status = 'beta',
-  statusReason = '',
-  documentTop = true,
-}: {
-  status: 'beta'
-  statusReason: string
-  documentTop: boolean
-}) => {
+export const StatusBadge = ({ status = 'beta', statusReason = '' }: { status: 'beta'; statusReason: string }) => {
   return (
-    <span className={`ams-storybook-status-badge${documentTop ? ` ams-storybook-status-badge--top` : ''}`}>
+    <span className="ams-storybook-status-badge">
       <Badge label={status} color="orange" />
       {statusReason && <span>{statusReason}</span>}
     </span>
