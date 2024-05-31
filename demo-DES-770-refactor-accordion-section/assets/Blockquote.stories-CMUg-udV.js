416bccf854f8a0e2e3a27bc165c0373de16e8ac6
GitHub <github-action@users.noreply.github.com>
2024-05-31 13:25:57 +0000

    Remove feature branch folder

--- demo-DES-770-refactor-accordion-section/assets/Blockquote.stories-CMUg-udV.js
+++
@@ -1,8 +0,0 @@
-import{i as l}from"./decorators-Dh4yEj-W.js";import{e as i}from"./exampleContent-CRK3_7Hv.js";import{B as m}from"./Blockquote-XJcoMbef.js";/**
- * @license EUPL-1.2+
- * Copyright Gemeente Amsterdam
- */const p=i(),u={title:"Components/Text/Blockquote",component:m,args:{children:p,inverseColor:!1},argTypes:{children:{description:"The text for the quote.",table:{disable:!1}}},decorators:[l]},e={},o={args:{inverseColor:!0}};var r,t,s;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:"{}",...(s=(t=e.parameters)==null?void 0:t.docs)==null?void 0:s.source}}};var a,c,n;o.parameters={...o.parameters,docs:{...(a=o.parameters)==null?void 0:a.docs,source:{originalSource:`{
-  args: {
-    inverseColor: true
-  }
-}`,...(n=(c=o.parameters)==null?void 0:c.docs)==null?void 0:n.source}}};const d=["Default","InvertedColor"],C=Object.freeze(Object.defineProperty({__proto__:null,Default:e,InvertedColor:o,__namedExportsOrder:d,default:u},Symbol.toStringTag,{value:"Module"}));export{C as B,o as I};
