8489a87bb51eb7ea8e7eaa5b83b83775bcbe130c
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-06-04 19:11:04 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@b6d6a00893d41e694bc5916e3a36d54b22f0bd16 🚀

---
+++ demo-DES-680-lifecycle-status/assets/Mark.docs-CTP6UFr_.js
@@ -0,0 +1,24 @@
+import{j as n}from"./jsx-runtime-Nms4Y4qS.js";import{useMDXComponents as i}from"./index-DlpNa54Y.js";import{M as r,d as a,P as s,e as m}from"./index-8GfF2Dor.js";import{M as p}from"./Mark.stories-DG4s4ew7.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-DLvmx9hC.js";import"../sb-preview/runtime.js";import"./index-B8XB3FuZ.js";import"./index-COebWTXQ.js";import"./index-B8K4vdXH.js";import"./index-DrFu-skq.js";import"./clsx-B-dksMZM.js";import"./Paragraph-C4jU4-Z4.js";const d=`<!-- @license CC0-1.0 -->
+
+# Mark
+
+Highlights a text fragment using a yellow background.
+Provides strong emphasis on the text and draws attention to it.
+
+## Guidelines
+
+### How to Use
+
+- Use a maximum of 1 mark per page.
+- Limit the marked text to 1 sentence.
+
+### Avoid This
+
+- Do not use it in titles and subtitles.
+  Use a significant and engaging title if the entire paragraph is important.
+- Do not mark an entire paragraph.
+`;function o(t){const e={h2:"h2",...i(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(r,{of:p}),`
+`,n.jsx(a,{children:d}),`
+`,n.jsx(e.h2,{id:"example",children:"Example"}),`
+`,n.jsx(s,{}),`
+`,n.jsx(m,{})]})}function y(t={}){const{wrapper:e}={...i(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(o,{...t})}):o(t)}export{y as default};
