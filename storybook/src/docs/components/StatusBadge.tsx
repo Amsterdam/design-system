d0c7991cdee0df7833b64116398538d16cfddcc0
Vincent Smedinga <v.smedinga@amsterdam.nl>
2024-06-04 20:50:15 +0200

    Update StatusBadge API and descriptions

--- storybook/src/docs/components/StatusBadge.tsx
+++ storybook/src/docs/components/StatusBadge.tsx
@@ -1,11 +1,17 @@
 import './status-badge.css'
 import { Badge } from '@amsterdam/design-system-react/src'
 
-export const StatusBadge = ({ status = 'beta', statusReason = '' }: { status: 'beta'; statusReason: string }) => {
-  return (
-    <span className="ams-storybook-status-badge">
-      <Badge label={status} color="orange" />
-      {statusReason && <span>{statusReason}</span>}
-    </span>
-  )
+type StatusBadgeProps = {
+  /** The status of this component. */
+  label?: string
+  /* The explanation of what needs to change before transitioning into another status. */
+  reason: string
 }
+
+/** Indicates the status of a component. Use this to prepare implementers for API changes. */
+export const StatusBadge = ({ label = 'beta', reason }: StatusBadgeProps) => (
+  <span className="ams-storybook-status-badge">
+    <Badge label={label} color="orange" />
+    <span>{reason}</span>
+  </span>
+)
