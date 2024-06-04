30f50c6c78a8660618209f638f25f7070c763024
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-06-04 19:54:38 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@bca8b56654c13a2a04d145be6b30b381e56333c6 🚀

---
+++ demo-update-dependencies/assets/DateInput.stories-BGq0XCAv.js
@@ -0,0 +1,12 @@
+import{j as g}from"./jsx-runtime-X2b_N9AH.js";import{c as b}from"./clsx-B-dksMZM.js";import{r as D}from"./index-uCp2LrAq.js";const t=D.forwardRef(({className:s,invalid:f,...v},_)=>g.jsx("input",{...v,"aria-invalid":f||void 0,className:b("ams-date-input",s),ref:_,type:"date"}));t.displayName="DateInput";try{t.displayName="DateInput",t.__docgenInfo={description:"",displayName:"DateInput",props:{invalid:{defaultValue:null,description:"Whether the value fails a validation rule.",name:"invalid",required:!1,type:{name:"boolean"}}}}}catch{}/**
+ * @license EUPL-1.2+
+ * Copyright Gemeente Amsterdam
+ */const y={title:"Components/Forms/Date Input",component:t,args:{disabled:!1,invalid:!1},argTypes:{disabled:{description:"Prevents interaction. Avoid if possible."}}},e={},a={args:{invalid:!0}},r={args:{disabled:!0}};var o,i,n;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:"{}",...(n=(i=e.parameters)==null?void 0:i.docs)==null?void 0:n.source}}};var d,l,p;a.parameters={...a.parameters,docs:{...(d=a.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    invalid: true
+  }
+}`,...(p=(l=a.parameters)==null?void 0:l.docs)==null?void 0:p.source}}};var c,u,m;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    disabled: true
+  }
+}`,...(m=(u=r.parameters)==null?void 0:u.docs)==null?void 0:m.source}}};const I=["Default","Invalid","Disabled"],h=Object.freeze(Object.defineProperty({__proto__:null,Default:e,Disabled:r,Invalid:a,__namedExportsOrder:I,default:y},Symbol.toStringTag,{value:"Module"}));export{h as D,a as I,r as a};
