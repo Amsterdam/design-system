fcec0c2a6afc1370232b3b6fb72945da48542a1c
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-06-04 19:27:47 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@eaa3bfc9283ade6046f6f78ba63e28c5b3b54099 🚀

---
+++ demo-update-figma-docs/assets/UnorderedList.docs-BcK-S56T.js
@@ -0,0 +1,39 @@
+import{j as e}from"./jsx-runtime-Nms4Y4qS.js";import{useMDXComponents as o}from"./index-DlpNa54Y.js";import{M as r,d as a,P as l,e as h,f as n}from"./index-CkMC8THc.js";import{U as d,T as c,W as p,I as m,S as x}from"./UnorderedList.stories-BSnhVBk-.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-Bqc_hkNt.js";import"../sb-preview/runtime.js";import"./index-B8XB3FuZ.js";import"./index-COebWTXQ.js";import"./index-B8K4vdXH.js";import"./index-DrFu-skq.js";import"./index.esm-DQkeu4as.js";import"./decorators-Dh4yEj-W.js";import"./exampleContent-CRK3_7Hv.js";import"./UnorderedList-J9gSHTlV.js";import"./clsx-B-dksMZM.js";import"./Icon-CuaumHXX.js";import"./Paragraph-C4jU4-Z4.js";const f=`<!-- @license CC0-1.0 -->
+
+# Unordered List
+
+A list is a vertical group of related content.
+This list can have 2 levels.
+The first level consists of squares.
+The second level consists of en dashes (–).
+Text in the list items is indented by a fixed distance.
+`;function i(t){const s={code:"code",h2:"h2",h3:"h3",p:"p",...o(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(r,{of:d}),`
+`,e.jsx(a,{children:f}),`
+`,e.jsx(s.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(s.h3,{id:"default",children:"Default"}),`
+`,e.jsx(s.p,{children:`Items of an unordered list have square markers.
+There is extra white space between the items.
+This provides better distinction between the items, especially when they consist of multiple lines of text.`}),`
+`,e.jsx(l,{}),`
+`,e.jsx(h,{}),`
+`,e.jsx(s.h3,{id:"two-levels",children:"Two Levels"}),`
+`,e.jsx(s.p,{children:`A list can have one deeper level.
+In this case, an en dash is used as the list item marker.
+It is left-aligned with the text of the higher-level item.`}),`
+`,e.jsx(n,{of:c}),`
+`,e.jsx(s.h3,{id:"without-markers",children:"Without Markers"}),`
+`,e.jsx(s.p,{children:`An overview of equivalent elements is also an unordered list.
+In such cases, apply the list semantics while hiding markers.
+This helps screen readers understand and navigate the list.`}),`
+`,e.jsxs(s.p,{children:[`Note: In this variant, each item typically contains a container with titles and other text components.
+Therefore, this variant does not define typography for the items and the `,e.jsx(s.code,{children:"inverseColor"})," prop has no effect."]}),`
+`,e.jsx(s.p,{children:"This example is based on the top tasks on the homepage of the website:"}),`
+`,e.jsx(n,{of:p}),`
+`,e.jsx(s.h3,{id:"inverse-colour",children:"Inverse Colour"}),`
+`,e.jsxs(s.p,{children:["Set the ",e.jsx(s.code,{children:"inverseColor"}),` prop if the List sits on a dark background.
+This ensures the colour of the text provides enough contrast.
+When nesting lists, set the prop on all lists.`]}),`
+`,e.jsx(n,{of:m}),`
+`,e.jsx(s.h3,{id:"small",children:"Small"}),`
+`,e.jsx(s.p,{children:"Use a list with a smaller font size in form descriptions and captions and the like."}),`
+`,e.jsx(n,{of:x})]})}function q(t={}){const{wrapper:s}={...o(),...t.components};return s?e.jsx(s,{...t,children:e.jsx(i,{...t})}):i(t)}export{q as default};
