bc529836961a445897b629357295cfcb034f738b
dlnr <niels@dlnr.dev>
2024-06-04 14:20:20 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@4981d62cef480e327d7c121ea2d2578403ea7fb2 🚀

---
+++ demo-DES-654-table-of-content/assets/TableOfContents.docs-Hl9iOuBk.js
@@ -0,0 +1,11 @@
+import{j as t}from"./jsx-runtime-Nms4Y4qS.js";import{useMDXComponents as s}from"./index-DlpNa54Y.js";import{M as r,d as i,P as m,e as a,f as l}from"./index-D76FVNfN.js";import{T as c,M as f}from"./TableOfContents.stories-BIjXKn5c.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-tJeSE3SN.js";import"../sb-preview/runtime.js";import"./index-B8XB3FuZ.js";import"./index-COebWTXQ.js";import"./index-B8K4vdXH.js";import"./index-DrFu-skq.js";import"./clsx-B-dksMZM.js";const p=`<!-- @license CC0-1.0 -->
+
+# Table of Contents
+
+A table of contents is a list of links to sections of a document.
+`;function e(n){const o={h2:"h2",...s(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(r,{of:c}),`
+`,t.jsx(i,{children:p}),`
+`,t.jsx(m,{}),`
+`,t.jsx(a,{}),`
+`,t.jsx(o.h2,{id:"multilevel",children:"Multilevel"}),`
+`,t.jsx(l,{of:f})]})}function k(n={}){const{wrapper:o}={...s(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(e,{...n})}):e(n)}export{k as default};
