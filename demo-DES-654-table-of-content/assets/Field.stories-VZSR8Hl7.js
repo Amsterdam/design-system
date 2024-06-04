8c23042a7ce630f434e814a67459520f0f5194dc
dlnr <niels@dlnr.dev>
2024-06-04 07:47:43 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@06aaa4517aed315dd6d0267325a8cc24303304b9 🚀

---
+++ demo-DES-654-table-of-content/assets/Field.stories-VZSR8Hl7.js
@@ -0,0 +1,25 @@
+import{j as e}from"./jsx-runtime-Nms4Y4qS.js";import{T as t}from"./index.esm-DQhiZNhi.js";import{c as y}from"./clsx-B-dksMZM.js";import{r as W}from"./index-BwDkhjyp.js";import{L as d}from"./Label-7rbJKFpg.js";import{P as f}from"./Paragraph-C4jU4-Z4.js";const a=W.forwardRef(({children:i,className:x,invalid:j,...F},b)=>e.jsx("div",{...F,ref:b,className:y("ams-field",j&&"ams-field--invalid",x),children:i}));a.displayName="Field";try{a.displayName="Field",a.__docgenInfo={description:"",displayName:"Field",props:{invalid:{defaultValue:null,description:"Whether the field has an input with a validation error",name:"invalid",required:!1,type:{name:"boolean"}}}}}catch{}const _={title:"Components/Forms/Field",component:a,args:{invalid:!1}},r={render:i=>e.jsxs(a,{invalid:i.invalid,children:[e.jsx(d,{htmlFor:"input1",children:"Waar gaat het om?"}),e.jsx(t,{id:"input1",invalid:i.invalid})]})},n={render:i=>e.jsxs(a,{invalid:i.invalid,children:[e.jsx(d,{htmlFor:"input2",children:"Waar gaat het om?"}),e.jsx(f,{id:"description1",size:"small",children:"Typ geen persoonsgegevens in deze omschrijving. We vragen dit later in dit formulier aan u."}),e.jsx(t,{id:"input2","aria-describedby":"description1",invalid:i.invalid})]})},s={args:{invalid:!0},render:i=>e.jsxs(a,{invalid:i.invalid,children:[e.jsx(d,{htmlFor:"input3",children:"Waar gaat het om?"}),e.jsx(f,{id:"description2",size:"small",children:"Typ geen persoonsgegevens in deze omschrijving. We vragen dit later in dit formulier aan u."}),e.jsx(t,{id:"input3","aria-describedby":"description2",invalid:i.invalid})]})};var o,l,p;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: args => <Field invalid={args.invalid}>
+      <Label htmlFor="input1">Waar gaat het om?</Label>
+      <TextInput id="input1" invalid={args.invalid} />
+    </Field>
+}`,...(p=(l=r.parameters)==null?void 0:l.docs)==null?void 0:p.source}}};var c,m,g;n.parameters={...n.parameters,docs:{...(c=n.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  render: args => <Field invalid={args.invalid}>
+      <Label htmlFor="input2">Waar gaat het om?</Label>
+      <Paragraph id="description1" size="small">
+        Typ geen persoonsgegevens in deze omschrijving. We vragen dit later in dit formulier aan u.
+      </Paragraph>
+      <TextInput id="input2" aria-describedby="description1" invalid={args.invalid} />
+    </Field>
+}`,...(g=(m=n.parameters)==null?void 0:m.docs)==null?void 0:g.source}}};var u,v,h;s.parameters={...s.parameters,docs:{...(u=s.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    invalid: true
+  },
+  render: args => <Field invalid={args.invalid}>
+      <Label htmlFor="input3">Waar gaat het om?</Label>
+      <Paragraph id="description2" size="small">
+        Typ geen persoonsgegevens in deze omschrijving. We vragen dit later in dit formulier aan u.
+      </Paragraph>
+      <TextInput id="input3" aria-describedby="description2" invalid={args.invalid} />
+    </Field>
+}`,...(h=(v=s.parameters)==null?void 0:v.docs)==null?void 0:h.source}}};const T=["Default","WithDescription","WithError"],D=Object.freeze(Object.defineProperty({__proto__:null,Default:r,WithDescription:n,WithError:s,__namedExportsOrder:T,default:_},Symbol.toStringTag,{value:"Module"}));export{D as F,n as W,s as a};
---
+++ demo-DES-654-table-of-content/assets/TableOfContents.docs-D54lVFWS.js
@@ -0,0 +1,14 @@
+import{j as t}from"./jsx-runtime-Nms4Y4qS.js";import{useMDXComponents as i}from"./index-DlpNa54Y.js";import{M as r,d as a,P as m,e as l,f as o}from"./index-C1L6ssQp.js";import{T as c,M as p,A as h}from"./TableOfContents.stories-NEEiqmxZ.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-BVA7UK1g.js";import"../sb-preview/runtime.js";import"./index-B8XB3FuZ.js";import"./index-COebWTXQ.js";import"./index-B8K4vdXH.js";import"./index-DrFu-skq.js";import"./index.esm-DQhiZNhi.js";import"./clsx-B-dksMZM.js";import"./index.esm-DQkeu4as.js";import"./exampleContent-CRK3_7Hv.js";const f=`<!-- @license CC0-1.0 -->
+
+# Table of Contents
+
+A table of contents is a list of links to sections of a document.
+`;function s(n){const e={h2:"h2",p:"p",...i(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(r,{of:c}),`
+`,t.jsx(a,{children:f}),`
+`,t.jsx(m,{}),`
+`,t.jsx(l,{}),`
+`,t.jsx(e.h2,{id:"multilevel",children:"Multilevel"}),`
+`,t.jsx(o,{of:p}),`
+`,t.jsx(e.h2,{id:"active",children:"Active"}),`
+`,t.jsx(e.p,{children:"When the user scrolls the page, the active section in the table of contents will be highlighted."}),`
+`,t.jsx(o,{of:h})]})}function y(n={}){const{wrapper:e}={...i(),...n.components};return e?t.jsx(e,{...n,children:t.jsx(s,{...n})}):s(n)}export{y as default};
