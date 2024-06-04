3613dfcb87db0d7f6ffc08554022dbdabee7b66e
Vincent Smedinga <v.smedinga@amsterdam.nl>
2024-06-04 20:57:37 +0200

    Remove unused prop

--- storybook/src/docs/components/StatusBadge.tsx
+++ storybook/src/docs/components/StatusBadge.tsx
@@ -2,16 +2,14 @@ import './status-badge.css'
 import { Badge } from '@amsterdam/design-system-react/src'
 
 type StatusBadgeProps = {
-  /** The status of this component. */
-  label?: string
-  /* The explanation of what needs to change before transitioning into another status. */
+  /* Explains how the component must change to transition into another status. */
   reason: string
 }
 
 /** Indicates the status of a component. Use this to prepare implementers for API changes. */
-export const StatusBadge = ({ label = 'beta', reason }: StatusBadgeProps) => (
+export const StatusBadge = ({ reason }: StatusBadgeProps) => (
   <span className="ams-storybook-status-badge">
-    <Badge label={label} color="orange" />
+    <Badge color="orange" label="beta" />
     <span>{reason}</span>
   </span>
 )
