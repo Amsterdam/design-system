bc09071e17a2abc1f6c9fcd8f90c29f3eff58ba3
GitHub <github-action@users.noreply.github.com>
2024-06-04 19:20:04 +0000

    Remove feature branch folder

--- demo-DES-680-lifecycle-status/assets/Badge.stories-CbMoQ05A.js
+++
@@ -1,4 +0,0 @@
-import{B as a}from"./Badge-BRa1hh1O.js";/**
- * @license EUPL-1.2+
- * Copyright Gemeente Amsterdam
- */const s={title:"Components/Feedback/Badge",component:a,args:{label:"Tip"}},e={};var o,t,r;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:"{}",...(r=(t=e.parameters)==null?void 0:t.docs)==null?void 0:r.source}}};const c=["Default"],d=Object.freeze(Object.defineProperty({__proto__:null,Default:e,__namedExportsOrder:c,default:s},Symbol.toStringTag,{value:"Module"}));export{d as B};
