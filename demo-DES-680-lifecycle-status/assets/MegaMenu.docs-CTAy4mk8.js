066209dda09cfc635978e1662b20c8250ba3d25f
RubenSibon <mail@rubensibon.nl>
2024-06-04 09:40:06 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@1ce7dcff214006cca776867dd6bb14e51ce3f9e6 🚀

--- demo-DES-680-lifecycle-status/assets/MegaMenu.docs-CTAy4mk8.js
+++
@@ -1,9 +0,0 @@
-import{j as t}from"./jsx-runtime-Nms4Y4qS.js";import{useMDXComponents as n}from"./index-DlpNa54Y.js";import{M as i,d as s,P as m,f as a}from"./index-BRCXxs-6.js";import{M as p,a as c}from"./MegaMenu.stories-CxVbwOdn.js";import{S as M}from"./StatusBadge-DpMkLybt.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-CkQ8yoFj.js";import"../sb-preview/runtime.js";import"./index-B8XB3FuZ.js";import"./index-COebWTXQ.js";import"./index-B8K4vdXH.js";import"./index-DrFu-skq.js";import"./clsx-B-dksMZM.js";import"./Screen-DD3ND5ci.js";import"./Grid-CwB6HQaq.js";import"./Heading-CWbN3elW.js";import"./getHeadingElement-DiRwFEz7.js";import"./LinkList-BqKR28sO.js";import"./index.esm-DQkeu4as.js";import"./Icon-CuaumHXX.js";import"./Badge-BRa1hh1O.js";const d=`<!-- @license CC0-1.0 -->
-
-# Mega Menu
-`;function e(o){const r={h2:"h2",...n(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(i,{of:p}),`
-`,t.jsx(M,{statusReason:"Is being redeveloped together with Header."}),`
-`,t.jsx(s,{children:d}),`
-`,t.jsx(m,{}),`
-`,t.jsx(r.h2,{id:"with-categories",children:"With Categories"}),`
-`,t.jsx(a,{of:c})]})}function F(o={}){const{wrapper:r}={...n(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(e,{...o})}):e(o)}export{F as default};
