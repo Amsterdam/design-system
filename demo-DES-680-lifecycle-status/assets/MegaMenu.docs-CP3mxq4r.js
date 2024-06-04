5f4ecfb6cb7e57b1bb547a514c0a17ea31c085a9
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-06-04 19:17:57 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@621ccb69bb13e20bb931584b8c091d35ecd2e20b 🚀

---
+++ demo-DES-680-lifecycle-status/assets/MegaMenu.docs-CP3mxq4r.js
@@ -0,0 +1,9 @@
+import{j as t}from"./jsx-runtime-Nms4Y4qS.js";import{useMDXComponents as e}from"./index-DlpNa54Y.js";import{M as i,d as s,P as m,f as a}from"./index-Dr8ugrgp.js";import{M as p,a as c}from"./MegaMenu.stories-BOsbjb5h.js";import{S as M}from"./StatusBadge-sxc7y6Zc.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-ul4Ux1iq.js";import"../sb-preview/runtime.js";import"./index-B8XB3FuZ.js";import"./index-COebWTXQ.js";import"./index-B8K4vdXH.js";import"./index-DrFu-skq.js";import"./clsx-B-dksMZM.js";import"./Screen-DD3ND5ci.js";import"./Grid-CwB6HQaq.js";import"./Heading-CWbN3elW.js";import"./getHeadingElement-DiRwFEz7.js";import"./LinkList-DB6g-rx2.js";import"./index.esm-DQkeu4as.js";import"./Icon-CuaumHXX.js";import"./Badge-BRa1hh1O.js";const u=`<!-- @license CC0-1.0 -->
+
+# Mega Menu
+`;function n(o){const r={h2:"h2",...e(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(i,{of:p}),`
+`,t.jsx(M,{reason:"Needs work for narrow screens and integration with Mega Menu."}),`
+`,t.jsx(s,{children:u}),`
+`,t.jsx(m,{}),`
+`,t.jsx(r.h2,{id:"with-categories",children:"With Categories"}),`
+`,t.jsx(a,{of:c})]})}function L(o={}){const{wrapper:r}={...e(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(n,{...o})}):n(o)}export{L as default};
---
+++ demo-DES-680-lifecycle-status/assets/Radio.docs-CoKu0iDs.js
@@ -0,0 +1,25 @@
+import{j as o}from"./jsx-runtime-Nms4Y4qS.js";import{useMDXComponents as s}from"./index-DlpNa54Y.js";import{M as r,d as i,P as a,e as l,f as c}from"./index-Dr8ugrgp.js";import{R as d,a as m}from"./Radio.stories-WTkwdCM8.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-ul4Ux1iq.js";import"../sb-preview/runtime.js";import"./index-B8XB3FuZ.js";import"./index-COebWTXQ.js";import"./index-B8K4vdXH.js";import"./index-DrFu-skq.js";import"./FieldSet-OhsVVCgX.js";import"./clsx-B-dksMZM.js";const p=`<!-- @license CC0-1.0 -->
+
+# Radio
+
+Allows users to select one option from a list.
+
+## Guidelines
+
+- Do not assume that users will know how many options they can select based on the visual difference between radios and checkboxes alone.
+  If needed, add a hint explaining this, for example, ‘Select one option’.
+- Order radio options alphabetically by default.
+  In some cases, it can be helpful to order them from most-to-least common options.
+  For example, you could order options for ‘Where do you live?’ based on population size.
+  However you should do this with extreme caution as it can reinforce bias.
+  If in doubt, order alphabetically.
+- Labels should be concise. Try to keep labels shorter than 4 words.
+
+Use a [Checkbox](/docs/components-forms-checkbox--docs) when a user can select more than 1 option from a list.
+`;function t(e){const n={a:"a",p:"p",...s(),...e.components};return o.jsxs(o.Fragment,{children:[o.jsx(r,{of:d}),`
+`,o.jsx(i,{children:p}),`
+`,o.jsx(a,{}),`
+`,o.jsx(l,{}),`
+`,o.jsxs(n.p,{children:["Group radios together with a ",o.jsx(n.a,{href:"/docs/components-forms-field-set--docs",children:"Field Set"}),` that describes them.
+This is usually a question, like ‘Where do you live?’.`]}),`
+`,o.jsx(c,{of:m})]})}function g(e={}){const{wrapper:n}={...s(),...e.components};return n?o.jsx(n,{...e,children:o.jsx(t,{...e})}):t(e)}export{g as default};
