fc90d47c5deafc415296d18c158cfa7e8d2f5ba7
alimpens <37216945+alimpens@users.noreply.github.com>
2024-06-05 08:32:07 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@09ce0d3af4a98060e184246d2d09054d7fadbf36 🚀

---
+++ demo-DES-778-add-form-error-list/assets/DateInput.docs-B6rjPswG.js
@@ -0,0 +1,19 @@
+import{j as n}from"./jsx-runtime-Nms4Y4qS.js";import{useMDXComponents as o}from"./index-DlpNa54Y.js";import{M as a,d as r,P as d,e as m,f as s}from"./index-B_obEByM.js";import{D as c,I as l,a as p}from"./DateInput.stories-CQMu6sUe.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-Ds2ZfbDC.js";import"../sb-preview/runtime.js";import"./index-B8XB3FuZ.js";import"./index-COebWTXQ.js";import"./index-B8K4vdXH.js";import"./index-DrFu-skq.js";import"./clsx-B-dksMZM.js";const h=`<!-- @license CC0-1.0 -->
+
+# Date Input
+
+Helps users enter a date.
+
+## Visual considerations
+
+This component uses a native date input, which is styled differently in different browsers and operating systems.
+Recreating the native functionality is quite difficult and prone to accessibility errors, which is why we’ve chosen not to do that.
+This does mean that this component does not look identical on all browsers and operating systems.
+`;function i(t){const e={h2:"h2",...o(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(a,{of:c}),`
+`,n.jsx(r,{children:h}),`
+`,n.jsx(d,{}),`
+`,n.jsx(m,{}),`
+`,n.jsx(e.h2,{id:"invalid",children:"Invalid"}),`
+`,n.jsx(s,{of:l}),`
+`,n.jsx(e.h2,{id:"disabled",children:"Disabled"}),`
+`,n.jsx(s,{of:p})]})}function E(t={}){const{wrapper:e}={...o(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(i,{...t})}):i(t)}export{E as default};
