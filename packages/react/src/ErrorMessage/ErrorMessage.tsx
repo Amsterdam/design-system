397dcb8d1ab1b36a90ec66e5f154cab29eb0f257
Aram <37216945+alimpens@users.noreply.github.com>
2024-06-05 10:56:57 +0200

    Update packages/react/src/ErrorMessage/ErrorMessage.tsx
    Co-authored-by: Vincent Smedinga <v.smedinga@amsterdam.nl>

--- packages/react/src/ErrorMessage/ErrorMessage.tsx
+++ packages/react/src/ErrorMessage/ErrorMessage.tsx
@@ -9,6 +9,7 @@ import type { ForwardedRef, HTMLAttributes, PropsWithChildren } from 'react'
 import { VisuallyHidden } from '../VisuallyHidden'
 
 export type ErrorMessageProps = {
+  /** An accessible phrase that screen readers announce before the error message. Should translate to something like ‘input error’. */
   prefix?: string
 } & PropsWithChildren<HTMLAttributes<HTMLParagraphElement>>
 
