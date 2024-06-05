c72652b9dbd9f5f0408bac4ed58b10e8c2ef322b
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-06-05 09:42:25 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@1dec2ead41fba24e4128c2e1d60b72a7ccb29a92 🚀

---
+++ demo-develop/assets/Overlap.docs-BHRS7ViH.js
@@ -0,0 +1,20 @@
+import{j as e}from"./jsx-runtime-X2b_N9AH.js";import{useMDXComponents as r}from"./index-CRbi4mQH.js";import{M as t,d as s,P as a}from"./index-B7urFUJa.js";import{O as l}from"./Overlap.stories-D4Iu4a5S.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-BbLdLY9l.js";import"../sb-preview/runtime.js";import"./index-BOkhicXD.js";import"./index-DXimoRZY.js";import"./index-B8K4vdXH.js";import"./index-DrFu-skq.js";import"./clsx-B-dksMZM.js";import"./SearchField-DZDG777S.js";import"./index.esm-DTv5ro-2.js";import"./VisuallyHidden-CgdwkxFN.js";import"./Icon-j44OzEOs.js";import"./AspectRatio-DxP_kpbd.js";import"./Image-Bf5zYcta.js";import"./Grid-BRQJwiXa.js";const m=`<!-- @license CC0-1.0 -->
+
+# Overlap
+
+Displays two or more components on top of each other.
+`;function i(n){const o={a:"a",h2:"h2",h3:"h3",h4:"h4",li:"li",p:"p",ul:"ul",...r(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(t,{of:l}),`
+`,e.jsx(s,{children:m}),`
+`,e.jsx(o.h2,{id:"example",children:"Example"}),`
+`,e.jsx(o.h3,{id:"hero-image-with-search-field",children:"Hero Image with Search Field"}),`
+`,e.jsxs(o.p,{children:[`This allows for the use of a mood-setting image as a background.
+A `,e.jsx(o.a,{href:"/docs/components-layout-grid--docs",children:"Grid"}),` provides horizontal space on both sides and columns for layout.
+Note: the library does not yet offer an option to vertically center the form.`]}),`
+`,e.jsx(a,{}),`
+`,e.jsx(o.h4,{id:"guidelines",children:"Guidelines"}),`
+`,e.jsxs(o.ul,{children:[`
+`,e.jsx(o.li,{children:"First, implement the underlying component."}),`
+`,e.jsxs(o.li,{children:["Follow ",e.jsx(o.a,{href:"/docs/components-media-image--docs#guidelines",children:"the general guidelines for images"}),"."]}),`
+`,e.jsx(o.li,{children:`Use a sufficiently large image.
+The aspect ratio of 32:9 is most suitable.`}),`
+`]})]})}function k(n={}){const{wrapper:o}={...r(),...n.components};return o?e.jsx(o,{...n,children:e.jsx(i,{...n})}):i(n)}export{k as default};
