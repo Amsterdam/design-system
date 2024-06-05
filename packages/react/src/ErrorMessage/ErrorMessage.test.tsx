1dec2ead41fba24e4128c2e1d60b72a7ccb29a92
Aram <37216945+alimpens@users.noreply.github.com>
2024-06-05 11:40:49 +0200

    feat: Add Error Message (#1247)
    Co-authored-by: Vincent Smedinga <v.smedinga@amsterdam.nl>

---
+++ packages/react/src/ErrorMessage/ErrorMessage.test.tsx
@@ -0,0 +1,51 @@
+import { render, screen } from '@testing-library/react'
+import { createRef } from 'react'
+import { ErrorMessage } from './ErrorMessage'
+import '@testing-library/jest-dom'
+
+describe('Error message', () => {
+  it('renders', () => {
+    render(<ErrorMessage />)
+    const component = screen.getByRole('paragraph')
+
+    expect(component).toBeInTheDocument()
+    expect(component).toBeVisible()
+  })
+
+  it('renders a design system BEM class name', () => {
+    render(<ErrorMessage />)
+    const component = screen.getByRole('paragraph')
+
+    expect(component).toHaveClass('ams-error-message')
+  })
+
+  it('renders an additional class name', () => {
+    render(<ErrorMessage className="extra" />)
+    const component = screen.getByRole('paragraph')
+
+    expect(component).toHaveClass('ams-error-message extra')
+  })
+
+  it('renders a Dutch prefix by default', () => {
+    render(<ErrorMessage />)
+    const component = screen.getByText('Invoerfout', { exact: false })
+
+    expect(component).toBeInTheDocument()
+  })
+
+  it('renders a custom prefix', () => {
+    render(<ErrorMessage prefix="Error" />)
+    const component = screen.getByText('Error', { exact: false })
+
+    expect(component).toBeInTheDocument()
+  })
+
+  it('supports ForwardRef in React', () => {
+    const ref = createRef<HTMLParagraphElement>()
+
+    render(<ErrorMessage ref={ref} />)
+    const component = screen.getByRole('paragraph')
+
+    expect(ref.current).toBe(component)
+  })
+})
