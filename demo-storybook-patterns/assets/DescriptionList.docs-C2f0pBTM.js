a6ca9a62c6b0941a9d1dadb04c74b6e3a753753d
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-06-06 11:15:57 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@a4266b27fa9aa00ac57d04c1e9e2baad3526ce50 🚀

---
+++ demo-storybook-patterns/assets/DescriptionList.docs-C2f0pBTM.js
@@ -0,0 +1,29 @@
+import{j as e}from"./jsx-runtime-X2b_N9AH.js";import{useMDXComponents as i}from"./index-CRbi4mQH.js";import{M as s,d as a,P as l,e as m,f as o}from"./index-C1iK5tJs.js";import{D as p,M as c,I as d}from"./DescriptionList.stories-DnpA6rjz.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-B9aTXRSH.js";import"../sb-preview/runtime.js";import"./index-BOkhicXD.js";import"./index-DXimoRZY.js";import"./index-B8K4vdXH.js";import"./index-DrFu-skq.js";import"./index.esm-B8fMqKf-.js";import"./clsx-B-dksMZM.js";import"./index.esm-DTv5ro-2.js";import"./decorators-CMqafKrE.js";import"./exampleContent-CRK3_7Hv.js";const h=`<!-- @license CC0-1.0 -->
+
+# Description List
+
+A collection of terms and their details.
+
+## Design
+
+On a narrow screen, details appear indented below their term.
+From the medium breakpoint, terms and details appear next to each other.
+The column for the details is twice as wide as the one for the term.
+
+Details are set in bold text.
+
+## References
+
+- [MDN: \`<dl>\`: The Description List element](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/dl)
+- [WCAG: Using description lists](https://www.w3.org/WAI/WCAG22/Techniques/html/H40)
+`;function r(n){const t={code:"code",h2:"h2",h3:"h3",p:"p",...i(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(s,{of:p}),`
+`,e.jsx(a,{children:h}),`
+`,e.jsx(l,{}),`
+`,e.jsx(m,{}),`
+`,e.jsx(t.h2,{id:"multiple-details",children:"Multiple details"}),`
+`,e.jsx(t.p,{children:"A term may have multiple details."}),`
+`,e.jsx(o,{of:c}),`
+`,e.jsx(t.h3,{id:"inverse-colour",children:"Inverse colour"}),`
+`,e.jsxs(t.p,{children:["Set the ",e.jsx(t.code,{children:"inverseColor"}),` prop if the Description List sits on a dark background.
+This ensures the colour of the text provides enough contrast.`]}),`
+`,e.jsx(o,{of:d})]})}function W(n={}){const{wrapper:t}={...i(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(r,{...n})}):r(n)}export{W as default};
