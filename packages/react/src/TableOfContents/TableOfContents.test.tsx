1f4c09a08d2aa96d1c584eceb498d33c31aa52ad
Niels Roozemond <n.roozemond@amsterdam.nl>
2024-06-06 19:07:41 +0200

    feat: Add Table of Contents component (#1248)
    Co-authored-by: Vincent Smedinga <v.smedinga@amsterdam.nl>

---
+++ packages/react/src/TableOfContents/TableOfContents.test.tsx
@@ -0,0 +1,57 @@
+import { render, screen } from '@testing-library/react'
+import { createRef } from 'react'
+import { TableOfContents } from './TableOfContents'
+import '@testing-library/jest-dom'
+
+describe('Table of contents', () => {
+  it('renders', () => {
+    const { container } = render(<TableOfContents />)
+    const component = container.querySelector(':only-child')
+
+    expect(component).toBeInTheDocument()
+    expect(component).toBeVisible()
+  })
+
+  it('renders its children', () => {
+    render(
+      <TableOfContents>
+        <TableOfContents.List />
+      </TableOfContents>,
+    )
+
+    const testChild = screen.getByRole('list')
+
+    expect(testChild).toBeTruthy()
+  })
+
+  it('renders a heading', () => {
+    render(<TableOfContents heading="Test heading" />)
+
+    const heading = screen.getByRole('heading')
+
+    expect(heading).toBeInTheDocument()
+    expect(heading).toHaveTextContent('Test heading')
+  })
+
+  it('renders a design system BEM class name', () => {
+    const { container } = render(<TableOfContents />)
+    const component = container.querySelector(':only-child')
+
+    expect(component).toHaveClass('ams-table-of-contents')
+  })
+
+  it('renders an additional class name', () => {
+    const { container } = render(<TableOfContents className="extra" />)
+    const component = container.querySelector(':only-child')
+
+    expect(component).toHaveClass('ams-table-of-contents extra')
+  })
+
+  it('supports ForwardRef in React', () => {
+    const ref = createRef<HTMLElement>()
+    const { container } = render(<TableOfContents ref={ref} />)
+    const component = container.querySelector(':only-child')
+
+    expect(ref.current).toBe(component)
+  })
+})
