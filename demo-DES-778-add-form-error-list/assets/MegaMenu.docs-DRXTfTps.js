3a5f7b6382ac69f6c72c0bee72200c6aa86eebbf
GitHub <github-action@users.noreply.github.com>
2024-06-07 09:54:14 +0000

    Remove feature branch folder

--- demo-DES-778-add-form-error-list/assets/MegaMenu.docs-DRXTfTps.js
+++
@@ -1,9 +0,0 @@
-import{j as t}from"./jsx-runtime-X2b_N9AH.js";import{useMDXComponents as e}from"./index-CRbi4mQH.js";import{M as i,d as s,P as m,f as a}from"./index-CFnjd4Bm.js";import{M as p,a as c}from"./MegaMenu.stories-DZPsZdkp.js";import{S as M}from"./StatusBadge-Bhp8flS0.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-D8ysUv73.js";import"../sb-preview/runtime.js";import"./index-BOkhicXD.js";import"./index-DXimoRZY.js";import"./index-B8K4vdXH.js";import"./index-DrFu-skq.js";import"./clsx-B-dksMZM.js";import"./Screen-nsUNRErq.js";import"./Grid-BRQJwiXa.js";import"./Heading-CGpdaMNv.js";import"./getHeadingElement-DiRwFEz7.js";import"./LinkList-BJyxmPMi.js";import"./index.esm-CXIn5TrU.js";import"./Icon-j44OzEOs.js";import"./Badge-BaF8tuOI.js";const u=`<!-- @license CC0-1.0 -->
-
-# Mega Menu
-`;function n(o){const r={h2:"h2",...e(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(i,{of:p}),`
-`,t.jsx(M,{reason:"Needs work for narrow screens and integration with Mega Menu."}),`
-`,t.jsx(s,{children:u}),`
-`,t.jsx(m,{}),`
-`,t.jsx(r.h2,{id:"with-categories",children:"With Categories"}),`
-`,t.jsx(a,{of:c})]})}function L(o={}){const{wrapper:r}={...e(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(n,{...o})}):n(o)}export{L as default};
