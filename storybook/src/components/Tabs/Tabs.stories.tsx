d6fc9fe813d1544c4c5ba62397f97ecce9ec888b
Ruben Sibon <r.sibon@amsterdam.nl>
2024-06-12 14:53:04 +0200

    docs(tabs): :memo: add set initial active tab story

--- storybook/src/components/Tabs/Tabs.stories.tsx
+++ storybook/src/components/Tabs/Tabs.stories.tsx
@@ -40,41 +40,50 @@ const tabMeta = {
 type Story = StoryObj<typeof meta>
 type TabStory = StoryObj<typeof tabMeta>
 
+const defaultTabs = [
+  <Tabs.List key={0}>
+    <Tabs.Button tab={0}>Gegevens</Tabs.Button>
+    <Tabs.Button tab={1}>Aanslagen</Tabs.Button>
+    <Tabs.Button tab={2}>Documenten</Tabs.Button>
+    <Tabs.Button tab={3}>Acties</Tabs.Button>
+  </Tabs.List>,
+  <Tabs.Panel tab={0} key={1}>
+    <div style={{ paddingTop: '2rem' }}>
+      <Heading level={3}>Gegevens</Heading>
+      <Paragraph>{exampleParagraph()}</Paragraph>
+    </div>
+  </Tabs.Panel>,
+  <Tabs.Panel tab={1} key={2}>
+    <div style={{ paddingTop: '2rem' }}>
+      <Heading level={3}>Aanslagen</Heading>
+      <Paragraph>{exampleParagraph()}</Paragraph>
+    </div>
+  </Tabs.Panel>,
+  <Tabs.Panel tab={2} key={3}>
+    <div style={{ paddingTop: '2rem' }}>
+      <Heading level={3}>Documenten</Heading>
+      <Paragraph>(This tab panel simulates a load time of 500 milliseconds.)</Paragraph>
+      <SlowPanel />
+    </div>
+  </Tabs.Panel>,
+  <Tabs.Panel tab={3} key={4}>
+    <div style={{ paddingTop: '2rem' }}>
+      <Heading level={3}>Acties</Heading>
+      <Paragraph>{exampleParagraph()}</Paragraph>
+    </div>
+  </Tabs.Panel>,
+]
+
 export const Default: Story = {
   args: {
-    children: [
-      <Tabs.List key={0}>
-        <Tabs.Button tab={0}>Gegevens</Tabs.Button>
-        <Tabs.Button tab={1}>Aanslagen</Tabs.Button>
-        <Tabs.Button tab={2}>Documenten</Tabs.Button>
-        <Tabs.Button tab={3}>Acties</Tabs.Button>
-      </Tabs.List>,
-      <Tabs.Panel tab={0} key={1}>
-        <div style={{ paddingTop: '2rem' }}>
-          <Heading level={3}>Gegevens</Heading>
-          <Paragraph>{exampleParagraph()}</Paragraph>
-        </div>
-      </Tabs.Panel>,
-      <Tabs.Panel tab={1} key={2}>
-        <div style={{ paddingTop: '2rem' }}>
-          <Heading level={3}>Aanslagen</Heading>
-          <Paragraph>{exampleParagraph()}</Paragraph>
-        </div>
-      </Tabs.Panel>,
-      <Tabs.Panel tab={2} key={3}>
-        <div style={{ paddingTop: '2rem' }}>
-          <Heading level={3}>Documenten</Heading>
-          <Paragraph>(This tab panel simulates a load time of 500 milliseconds.)</Paragraph>
-          <SlowPanel />
-        </div>
-      </Tabs.Panel>,
-      <Tabs.Panel tab={3} key={4}>
-        <div style={{ paddingTop: '2rem' }}>
-          <Heading level={3}>Acties</Heading>
-          <Paragraph>{exampleParagraph()}</Paragraph>
-        </div>
-      </Tabs.Panel>,
-    ],
+    children: defaultTabs,
+  },
+}
+
+export const WithInitialTab: Story = {
+  args: {
+    activeTab: 3,
+    children: defaultTabs,
   },
 }
 
