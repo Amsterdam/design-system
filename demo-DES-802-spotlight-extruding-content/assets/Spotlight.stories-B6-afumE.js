5e7f82dacae66ac8bd74502f9bc7f0e4b47d9768
RubenSibon <mail@rubensibon.nl>
2024-06-13 08:45:10 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@e05d565f5a07828245d9ea5c03d701118acc7d9c 🚀

---
+++ demo-DES-802-spotlight-extruding-content/assets/Spotlight.stories-B6-afumE.js
@@ -0,0 +1,41 @@
+import{j as r}from"./jsx-runtime-X2b_N9AH.js";import{e as U}from"./exampleContent-CRK3_7Hv.js";import{S as i}from"./Spotlight-DRfAhOJX.js";import{I as X}from"./Image-Bf5zYcta.js";import{G as d}from"./Grid-BRQJwiXa.js";import{B as Z}from"./Blockquote-7VFIi5C-.js";const $=U(),rr={title:"Components/Containers/Spotlight",component:i,render:({as:K,color:g,PopoutMedia:L})=>r.jsx(i,{as:K,color:g,PopoutMedia:L,children:r.jsx(d,{paddingVertical:"medium",children:r.jsx(d.Cell,{span:"all",children:r.jsx(Z,{inverseColor:!g||!["green","yellow"].includes(g),children:$})})})})},e={},a={args:{PopoutMedia:r.jsx(X,{src:"https://picsum.photos/240/360",alt:""})}},o={args:{color:"blue"}},s={args:{color:"dark-blue"}},t={args:{color:"dark-green"}},c={args:{color:"green"}},n={args:{color:"magenta"}},p={args:{color:"orange"}},m={args:{color:"purple"}},l={args:{color:"yellow"}},u={args:{as:"section"}};var S,h,f;e.parameters={...e.parameters,docs:{...(S=e.parameters)==null?void 0:S.docs,source:{originalSource:"{}",...(f=(h=e.parameters)==null?void 0:h.docs)==null?void 0:f.source}}};var x,j,k;a.parameters={...a.parameters,docs:{...(x=a.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  args: {
+    PopoutMedia: <Image src="https://picsum.photos/240/360" alt="" />
+  }
+}`,...(k=(j=a.parameters)==null?void 0:j.docs)==null?void 0:k.source}}};var P,b,B;o.parameters={...o.parameters,docs:{...(P=o.parameters)==null?void 0:P.docs,source:{originalSource:`{
+  args: {
+    color: 'blue'
+  }
+}`,...(B=(b=o.parameters)==null?void 0:b.docs)==null?void 0:B.source}}};var C,D,G;s.parameters={...s.parameters,docs:{...(C=s.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  args: {
+    color: 'dark-blue'
+  }
+}`,...(G=(D=s.parameters)==null?void 0:D.docs)==null?void 0:G.source}}};var M,O,_;t.parameters={...t.parameters,docs:{...(M=t.parameters)==null?void 0:M.docs,source:{originalSource:`{
+  args: {
+    color: 'dark-green'
+  }
+}`,...(_=(O=t.parameters)==null?void 0:O.docs)==null?void 0:_.source}}};var w,y,I;c.parameters={...c.parameters,docs:{...(w=c.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  args: {
+    color: 'green'
+  }
+}`,...(I=(y=c.parameters)==null?void 0:y.docs)==null?void 0:I.source}}};var T,W,Y;n.parameters={...n.parameters,docs:{...(T=n.parameters)==null?void 0:T.docs,source:{originalSource:`{
+  args: {
+    color: 'magenta'
+  }
+}`,...(Y=(W=n.parameters)==null?void 0:W.docs)==null?void 0:Y.source}}};var q,v,E;p.parameters={...p.parameters,docs:{...(q=p.parameters)==null?void 0:q.docs,source:{originalSource:`{
+  args: {
+    color: 'orange'
+  }
+}`,...(E=(v=p.parameters)==null?void 0:v.docs)==null?void 0:E.source}}};var N,z,Q;m.parameters={...m.parameters,docs:{...(N=m.parameters)==null?void 0:N.docs,source:{originalSource:`{
+  args: {
+    color: 'purple'
+  }
+}`,...(Q=(z=m.parameters)==null?void 0:z.docs)==null?void 0:Q.source}}};var R,V,A;l.parameters={...l.parameters,docs:{...(R=l.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  args: {
+    color: 'yellow'
+  }
+}`,...(A=(V=l.parameters)==null?void 0:V.docs)==null?void 0:A.source}}};var F,H,J;u.parameters={...u.parameters,docs:{...(F=u.parameters)==null?void 0:F.docs,source:{originalSource:`{
+  args: {
+    as: 'section'
+  }
+}`,...(J=(H=u.parameters)==null?void 0:H.docs)==null?void 0:J.source}}};const er=["Default","WithPopoutImage","Blue","DarkBlue","DarkGreen","Green","Magenta","Orange","Purple","Yellow","CustomTagName"],pr=Object.freeze(Object.defineProperty({__proto__:null,Blue:o,CustomTagName:u,DarkBlue:s,DarkGreen:t,Default:e,Green:c,Magenta:n,Orange:p,Purple:m,WithPopoutImage:a,Yellow:l,__namedExportsOrder:er,default:rr},Symbol.toStringTag,{value:"Module"}));export{o as B,u as C,s as D,c as G,n as M,p as O,m as P,pr as S,a as W,l as Y,t as a};
