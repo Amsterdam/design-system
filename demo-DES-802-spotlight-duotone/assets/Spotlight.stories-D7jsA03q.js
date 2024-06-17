34a11d6a8073d6dfdb6cce92ba762cf42625f860
dlnr <niels@dlnr.dev>
2024-06-17 11:26:37 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@28e352d91685653b2abdbf635a8309a8beab9a7e 🚀

---
+++ demo-DES-802-spotlight-duotone/assets/Spotlight.stories-D7jsA03q.js
@@ -0,0 +1,193 @@
+import{j as e}from"./jsx-runtime-X2b_N9AH.js";import{e as fe,a as l}from"./exampleContent-CRK3_7Hv.js";import{S as o}from"./Spotlight-rLNvAYSL.js";import{G as n}from"./Grid-BRQJwiXa.js";import{B as ve}from"./Blockquote-7VFIi5C-.js";import{S as Ae}from"./Screen-nsUNRErq.js";import{C as t}from"./Column-BRryplre.js";import{A as i}from"./AspectRatio-DxP_kpbd.js";import{I as c}from"./Image-Bf5zYcta.js";import{P as d}from"./Paragraph-DpVlyIEJ.js";const Pe=fe(),b="https://picsum.photos/1600/1600",we={title:"Components/Containers/Spotlight",component:o,render:({as:a,color:r})=>e.jsx(o,{as:a,color:r,children:e.jsx(n,{paddingVertical:"medium",children:e.jsx(n.Cell,{span:"all",children:e.jsx(ve,{inverseColor:!r||!["green","yellow"].includes(r),children:Pe})})})}),decorators:[a=>e.jsx(Ae,{maxWidth:"wide",children:e.jsx(a,{cover:!0})})]},p={},m={args:{color:"blue"}},u={args:{color:"light-blue"}},g={args:{color:"dark-green"}},h={args:{color:"green"}},x={args:{color:"magenta"}},j={args:{color:"orange"}},C={args:{color:"purple"}},G={args:{color:"yellow"}},S={args:{as:"section"}},y={parameters:{layout:"fullscreen"},args:{color:"blue",gradient:!0},render:({as:a,color:r,gradient:s})=>e.jsx(o,{as:a,color:r,gradient:s,children:e.jsx(n,{paddingVertical:"medium",children:e.jsx(n.Cell,{span:"all",children:e.jsxs(t,{gap:"medium",children:[e.jsx(i,{ratio:"x-tall",children:e.jsx(c,{alt:"",src:"https://picsum.photos/1440/2560",cover:!0})}),e.jsx(d,{size:"small",inverseColor:!r||!["green","yellow"].includes(r),children:l()})]})})})})},f={parameters:{layout:"fullscreen"},args:{color:"blue",gradient:!0},render:({as:a,color:r,gradient:s})=>e.jsx(o,{as:a,color:r,gradient:s,children:e.jsx(n,{paddingVertical:"medium",children:e.jsx(n.Cell,{span:"all",children:e.jsxs(t,{gap:"medium",children:[e.jsx(i,{ratio:"tall",children:e.jsx(c,{alt:"",src:"https://picsum.photos/1200/1600",cover:!0})}),e.jsx(d,{size:"small",inverseColor:!r||!["green","yellow"].includes(r),children:l()})]})})})})},v={parameters:{layout:"fullscreen"},args:{color:"blue",gradient:!0},render:({as:a,color:r,gradient:s})=>e.jsx(o,{as:a,color:r,gradient:s,children:e.jsx(n,{paddingVertical:"medium",children:e.jsx(n.Cell,{span:"all",children:e.jsxs(t,{gap:"medium",children:[e.jsx(i,{ratio:"square",children:e.jsx(c,{alt:"",src:b,cover:!0})}),e.jsx(d,{size:"small",inverseColor:!r||!["green","yellow"].includes(r),children:l()})]})})})})},A={parameters:{layout:"fullscreen"},args:{color:"blue",gradient:!0},render:({as:a,color:r,gradient:s})=>e.jsx(o,{as:a,color:r,gradient:s,children:e.jsx(n,{paddingVertical:"medium",children:e.jsx(n.Cell,{span:"all",children:e.jsxs(t,{gap:"medium",children:[e.jsx(i,{ratio:"wide",children:e.jsx(c,{alt:"",src:b,cover:!0})}),e.jsx(d,{size:"small",inverseColor:!r||!["green","yellow"].includes(r),children:l()})]})})})})},P={parameters:{layout:"fullscreen"},args:{color:"blue",gradient:!0},render:({as:a,color:r,gradient:s})=>e.jsx(o,{as:a,color:r,gradient:s,children:e.jsx(n,{paddingVertical:"medium",children:e.jsx(n.Cell,{span:"all",children:e.jsxs(t,{gap:"medium",children:[e.jsx(i,{ratio:"x-wide",children:e.jsx(c,{alt:"",src:b,cover:!0})}),e.jsx(d,{size:"small",inverseColor:!r||!["green","yellow"].includes(r),children:l()})]})})})})},w={parameters:{layout:"fullscreen"},args:{color:"blue",gradient:!0},render:({as:a,color:r,gradient:s})=>e.jsx(o,{as:a,color:r,gradient:s,children:e.jsx(n,{paddingVertical:"medium",children:e.jsx(n.Cell,{span:"all",children:e.jsxs(t,{gap:"medium",children:[e.jsx(i,{ratio:"2x-wide",children:e.jsx(c,{alt:"",src:b,cover:!0})}),e.jsx(d,{size:"small",inverseColor:!r||!["green","yellow"].includes(r),children:l()})]})})})})};var R,z,V;p.parameters={...p.parameters,docs:{...(R=p.parameters)==null?void 0:R.docs,source:{originalSource:"{}",...(V=(z=p.parameters)==null?void 0:z.docs)==null?void 0:V.source}}};var I,B,T;m.parameters={...m.parameters,docs:{...(I=m.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  args: {
+    color: 'blue'
+  }
+}`,...(T=(B=m.parameters)==null?void 0:B.docs)==null?void 0:T.source}}};var W,q,O;u.parameters={...u.parameters,docs:{...(W=u.parameters)==null?void 0:W.docs,source:{originalSource:`{
+  args: {
+    color: 'light-blue'
+  }
+}`,...(O=(q=u.parameters)==null?void 0:q.docs)==null?void 0:O.source}}};var X,_,k;g.parameters={...g.parameters,docs:{...(X=g.parameters)==null?void 0:X.docs,source:{originalSource:`{
+  args: {
+    color: 'dark-green'
+  }
+}`,...(k=(_=g.parameters)==null?void 0:_.docs)==null?void 0:k.source}}};var D,M,L;h.parameters={...h.parameters,docs:{...(D=h.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  args: {
+    color: 'green'
+  }
+}`,...(L=(M=h.parameters)==null?void 0:M.docs)==null?void 0:L.source}}};var Y,E,N;x.parameters={...x.parameters,docs:{...(Y=x.parameters)==null?void 0:Y.docs,source:{originalSource:`{
+  args: {
+    color: 'magenta'
+  }
+}`,...(N=(E=x.parameters)==null?void 0:E.docs)==null?void 0:N.source}}};var Q,F,H;j.parameters={...j.parameters,docs:{...(Q=j.parameters)==null?void 0:Q.docs,source:{originalSource:`{
+  args: {
+    color: 'orange'
+  }
+}`,...(H=(F=j.parameters)==null?void 0:F.docs)==null?void 0:H.source}}};var J,K,U;C.parameters={...C.parameters,docs:{...(J=C.parameters)==null?void 0:J.docs,source:{originalSource:`{
+  args: {
+    color: 'purple'
+  }
+}`,...(U=(K=C.parameters)==null?void 0:K.docs)==null?void 0:U.source}}};var Z,$,ee;G.parameters={...G.parameters,docs:{...(Z=G.parameters)==null?void 0:Z.docs,source:{originalSource:`{
+  args: {
+    color: 'yellow'
+  }
+}`,...(ee=($=G.parameters)==null?void 0:$.docs)==null?void 0:ee.source}}};var re,ae,ne;S.parameters={...S.parameters,docs:{...(re=S.parameters)==null?void 0:re.docs,source:{originalSource:`{
+  args: {
+    as: 'section'
+  }
+}`,...(ne=(ae=S.parameters)==null?void 0:ae.docs)==null?void 0:ne.source}}};var se,oe,le;y.parameters={...y.parameters,docs:{...(se=y.parameters)==null?void 0:se.docs,source:{originalSource:`{
+  parameters: {
+    layout: 'fullscreen'
+  },
+  args: {
+    color: 'blue',
+    gradient: true
+  },
+  render: ({
+    as,
+    color,
+    gradient
+  }) => <Spotlight as={as} color={color} gradient={gradient}>
+      <Grid paddingVertical="medium">
+        <Grid.Cell span="all">
+          <Column gap="medium">
+            <AspectRatio ratio="x-tall">
+              <Image alt="" src="https://picsum.photos/1440/2560" cover />
+            </AspectRatio>
+            <Paragraph size="small" inverseColor={!color || !['green', 'yellow'].includes(color)}>
+              {exampleParagraph()}
+            </Paragraph>
+          </Column>
+        </Grid.Cell>
+      </Grid>
+    </Spotlight>
+}`,...(le=(oe=y.parameters)==null?void 0:oe.docs)==null?void 0:le.source}}};var te,ie,ce;f.parameters={...f.parameters,docs:{...(te=f.parameters)==null?void 0:te.docs,source:{originalSource:`{
+  parameters: {
+    layout: 'fullscreen'
+  },
+  args: {
+    color: 'blue',
+    gradient: true
+  },
+  render: ({
+    as,
+    color,
+    gradient
+  }) => <Spotlight as={as} color={color} gradient={gradient}>
+      <Grid paddingVertical="medium">
+        <Grid.Cell span="all">
+          <Column gap="medium">
+            <AspectRatio ratio="tall">
+              <Image alt="" src="https://picsum.photos/1200/1600" cover />
+            </AspectRatio>
+            <Paragraph size="small" inverseColor={!color || !['green', 'yellow'].includes(color)}>
+              {exampleParagraph()}
+            </Paragraph>
+          </Column>
+        </Grid.Cell>
+      </Grid>
+    </Spotlight>
+}`,...(ce=(ie=f.parameters)==null?void 0:ie.docs)==null?void 0:ce.source}}};var de,pe,me;v.parameters={...v.parameters,docs:{...(de=v.parameters)==null?void 0:de.docs,source:{originalSource:`{
+  parameters: {
+    layout: 'fullscreen'
+  },
+  args: {
+    color: 'blue',
+    gradient: true
+  },
+  render: ({
+    as,
+    color,
+    gradient
+  }) => <Spotlight as={as} color={color} gradient={gradient}>
+      <Grid paddingVertical="medium">
+        <Grid.Cell span="all">
+          <Column gap="medium">
+            <AspectRatio ratio="square">
+              <Image alt="" src={image} cover />
+            </AspectRatio>
+            <Paragraph size="small" inverseColor={!color || !['green', 'yellow'].includes(color)}>
+              {exampleParagraph()}
+            </Paragraph>
+          </Column>
+        </Grid.Cell>
+      </Grid>
+    </Spotlight>
+}`,...(me=(pe=v.parameters)==null?void 0:pe.docs)==null?void 0:me.source}}};var ue,ge,he;A.parameters={...A.parameters,docs:{...(ue=A.parameters)==null?void 0:ue.docs,source:{originalSource:`{
+  parameters: {
+    layout: 'fullscreen'
+  },
+  args: {
+    color: 'blue',
+    gradient: true
+  },
+  render: ({
+    as,
+    color,
+    gradient
+  }) => <Spotlight as={as} color={color} gradient={gradient}>
+      <Grid paddingVertical="medium">
+        <Grid.Cell span="all">
+          <Column gap="medium">
+            <AspectRatio ratio="wide">
+              <Image alt="" src={image} cover />
+            </AspectRatio>
+            <Paragraph size="small" inverseColor={!color || !['green', 'yellow'].includes(color)}>
+              {exampleParagraph()}
+            </Paragraph>
+          </Column>
+        </Grid.Cell>
+      </Grid>
+    </Spotlight>
+}`,...(he=(ge=A.parameters)==null?void 0:ge.docs)==null?void 0:he.source}}};var xe,je,Ce;P.parameters={...P.parameters,docs:{...(xe=P.parameters)==null?void 0:xe.docs,source:{originalSource:`{
+  parameters: {
+    layout: 'fullscreen'
+  },
+  args: {
+    color: 'blue',
+    gradient: true
+  },
+  render: ({
+    as,
+    color,
+    gradient
+  }) => <Spotlight as={as} color={color} gradient={gradient}>
+      <Grid paddingVertical="medium">
+        <Grid.Cell span="all">
+          <Column gap="medium">
+            <AspectRatio ratio="x-wide">
+              <Image alt="" src={image} cover />
+            </AspectRatio>
+            <Paragraph size="small" inverseColor={!color || !['green', 'yellow'].includes(color)}>
+              {exampleParagraph()}
+            </Paragraph>
+          </Column>
+        </Grid.Cell>
+      </Grid>
+    </Spotlight>
+}`,...(Ce=(je=P.parameters)==null?void 0:je.docs)==null?void 0:Ce.source}}};var Ge,Se,ye;w.parameters={...w.parameters,docs:{...(Ge=w.parameters)==null?void 0:Ge.docs,source:{originalSource:`{
+  parameters: {
+    layout: 'fullscreen'
+  },
+  args: {
+    color: 'blue',
+    gradient: true
+  },
+  render: ({
+    as,
+    color,
+    gradient
+  }) => <Spotlight as={as} color={color} gradient={gradient}>
+      <Grid paddingVertical="medium">
+        <Grid.Cell span="all">
+          <Column gap="medium">
+            <AspectRatio ratio="2x-wide">
+              <Image alt="" src={image} cover />
+            </AspectRatio>
+            <Paragraph size="small" inverseColor={!color || !['green', 'yellow'].includes(color)}>
+              {exampleParagraph()}
+            </Paragraph>
+          </Column>
+        </Grid.Cell>
+      </Grid>
+    </Spotlight>
+}`,...(ye=(Se=w.parameters)==null?void 0:Se.docs)==null?void 0:ye.source}}};const be=["Default","Blue","LightBlue","DarkGreen","Green","Magenta","Orange","Purple","Yellow","CustomTagName","GradientXTallAspect","GradientTallAspect","GradientSquareAspect","GradientWideAspect","GradientXWideAspect","Gradient2XWideAspect"],_e=Object.freeze(Object.defineProperty({__proto__:null,Blue:m,CustomTagName:S,DarkGreen:g,Default:p,Gradient2XWideAspect:w,GradientSquareAspect:v,GradientTallAspect:f,GradientWideAspect:A,GradientXTallAspect:y,GradientXWideAspect:P,Green:h,LightBlue:u,Magenta:x,Orange:j,Purple:C,Yellow:G,__namedExportsOrder:be,default:we},Symbol.toStringTag,{value:"Module"}));export{m as B,S as C,g as D,h as G,u as L,x as M,j as O,C as P,_e as S,G as Y};
---
+++ demo-DES-802-spotlight-duotone/assets/TextArea.docs-RB6hpLHc.js
@@ -0,0 +1,33 @@
+import{j as e}from"./jsx-runtime-X2b_N9AH.js";import{useMDXComponents as o}from"./index-CRbi4mQH.js";import{M as r,d as a,P as l,e as h,f as s}from"./index-DujBB0cK.js";import{T as m,V as d,H as c,N as x,I as p,D as f}from"./TextArea.stories-Crp4U3dj.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-lhKFBuhw.js";import"../sb-preview/runtime.js";import"./index-BOkhicXD.js";import"./index-DXimoRZY.js";import"./index-B8K4vdXH.js";import"./index-DrFu-skq.js";import"./exampleContent-CRK3_7Hv.js";import"./clsx-B-dksMZM.js";const u=`<!-- @license CC0-1.0 -->
+
+# TextArea
+
+A form field that allows the user to input text over multiple lines.
+
+## Guidelines
+
+- Use a TextArea when users need to enter more than 1 sentence of text, such as a comment or description.
+- The height of the TextArea should be appropriate for the information to be entered.
+- A TextArea must have a label, and in most cases, this label should be visible.
+- Use \`spellcheck="false"\` for fields that may contain sensitive information, such as passwords and personal data.
+  Some browser extensions for spell-checking send this information to external servers.
+
+## Accessibility
+
+- [WCAG 2.1](https://www.w3.org/WAI/WCAG21/Techniques/html/H91.html) requires that the TextArea has a label or title attribute.
+`;/*@license CC0-1.0*/function i(t){const n={h2:"h2",...o(),...t.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(r,{of:m}),`
+`,e.jsx(a,{children:u}),`
+`,e.jsx(l,{}),`
+`,e.jsx(h,{}),`
+`,e.jsx(n.h2,{id:"vertical-resize",children:"Vertical Resize"}),`
+`,e.jsx(s,{of:d}),`
+`,e.jsx(n.h2,{id:"horizontal-resize",children:"Horizontal Resize"}),`
+`,e.jsx(s,{of:c}),`
+`,e.jsx(n.h2,{id:"no-resize",children:"No Resize"}),`
+`,e.jsx(s,{of:x}),`
+`,e.jsx(n.h2,{id:"invalid",children:"Invalid"}),`
+`,e.jsx(s,{of:p}),`
+`,e.jsx(n.h2,{id:"disabled",children:"Disabled"}),`
+`,e.jsx(s,{of:f})]})}function I(t={}){const{wrapper:n}={...o(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(i,{...t})}):i(t)}export{I as default};
