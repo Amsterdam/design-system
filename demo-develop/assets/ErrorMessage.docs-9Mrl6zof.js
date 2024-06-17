d5366fedfd64eec415f5a9eba9fce71194acbac7
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-06-17 12:55:00 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@9d2f01d1db3cd2de8cc1364cc8a677b70a50a9f8 🚀

---
+++ demo-develop/assets/ErrorMessage.docs-9Mrl6zof.js
@@ -0,0 +1,22 @@
+import{j as e}from"./jsx-runtime-X2b_N9AH.js";import{useMDXComponents as t}from"./index-CRbi4mQH.js";import{M as s,d as i,P as a,e as m,f as d}from"./index-DnL6aKTa.js";import{E as c,W as h}from"./ErrorMessage.stories-MKsHR_8W.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-qO-rodjQ.js";import"../sb-preview/runtime.js";import"./index-BOkhicXD.js";import"./index-DXimoRZY.js";import"./index-B8K4vdXH.js";import"./index-DrFu-skq.js";import"./ErrorMessage-bGWcoafk.js";import"./clsx-B-dksMZM.js";import"./VisuallyHidden-CgdwkxFN.js";const p=`<!-- @license CC0-1.0 -->
+
+# Error Message
+
+Show an error message when there is a form field validation error.
+In the error message explain what went wrong and how to fix it.
+
+For guidance and examples on using error messages in a form,
+refer to the [Field](/docs/components-forms-field--docs) and [Field Set](/docs/components-forms-field-set--docs) documentation.
+
+Read the documentation by [NL Design System](https://www.nldesignsystem.nl/richtlijnen/formulieren/foutmeldingen) and [Gov.uk](https://design-system.service.gov.uk/components/error-message/) for more information on the contents of error messages and when to show them.
+`;/*@license CC0-1.0*/function r(o){const n={code:"code",h2:"h2",p:"p",...t(),...o.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(s,{of:c}),`
+`,e.jsx(i,{children:p}),`
+`,e.jsx(a,{}),`
+`,e.jsx(m,{}),`
+`,e.jsx(n.h2,{id:"with-a-custom-prefix",children:"With a custom prefix"}),`
+`,e.jsxs(n.p,{children:[`Error messages are automatically prefixed with a visually hidden text, the Dutch word "Invoerfout".
+This makes the error message more clear for screen reader users.
+If you want to change this prefix, to support another language for example, you can use the `,e.jsx(n.code,{children:"prefix"})," prop."]}),`
+`,e.jsx(d,{of:h})]})}function S(o={}){const{wrapper:n}={...t(),...o.components};return n?e.jsx(n,{...o,children:e.jsx(r,{...o})}):r(o)}export{S as default};
