d46b7010f58a2045500eaa345b9fe83f6aff88dc
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-06-18 11:32:15 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@88f89413e14ce27e0c869a5f7c473e5f4a28954b 🚀

---
+++ demo-accordion-button-text-align/assets/Avatar.docs-Cma3FfSk.js
@@ -0,0 +1,32 @@
+import{j as n}from"./jsx-runtime-X2b_N9AH.js";import{useMDXComponents as a}from"./index-CRbi4mQH.js";import{M as i,d as s,P as c,e as p,f as t}from"./index-eqxVJ3-m.js";import{A as l,W as m,F as d,I as h}from"./Avatar.stories-ChPk5Kwy.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-HG6aWbbZ.js";import"../sb-preview/runtime.js";import"./index-BOkhicXD.js";import"./index-DXimoRZY.js";import"./index-B8K4vdXH.js";import"./index-DrFu-skq.js";import"./index.esm-CXIn5TrU.js";import"./clsx-B-dksMZM.js";import"./VisuallyHidden-CgdwkxFN.js";import"./Image-Bf5zYcta.js";import"./Icon-j44OzEOs.js";import"./Header-tGtavnQj.js";import"./Logo-B6EXimfo.js";import"./Heading-CGpdaMNv.js";import"./getHeadingElement-DiRwFEz7.js";import"./PageMenu-C-9UIlX6.js";const u=`<!-- @license CC0-1.0 -->
+
+# Avatar
+
+A circular badge representing a person.
+
+## Design
+
+The avatar contains 1 or 2 initial letters from the person's full name, a picture, or a generic icon.
+The default background colour is dark blue.
+
+## Usage
+
+Display an avatar for the person currently using the application,
+or to associate a person with a content item.
+`;/*@license CC0-1.0*/function o(e){const r={h2:"h2",h3:"h3",p:"p",...a(),...e.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(i,{of:l}),`
+`,n.jsx(s,{children:u}),`
+`,n.jsx(r.h2,{id:"stories",children:"Stories"}),`
+`,n.jsx(r.h3,{id:"default",children:"Default"}),`
+`,n.jsx(c,{}),`
+`,n.jsx(p,{}),`
+`,n.jsx(r.h3,{id:"with-picture",children:"With Picture"}),`
+`,n.jsx(r.p,{children:`The Avatar can also display a photo or other image for the person.
+Make sure to scale the image down to around 100 pixels to prevent unnecessary data transfers.`}),`
+`,n.jsx(t,{of:m}),`
+`,n.jsx(r.h3,{id:"fallback-icon",children:"Fallback Icon"}),`
+`,n.jsx(r.p,{children:"A user icon displays if no label and image are provided."}),`
+`,n.jsx(t,{of:d}),`
+`,n.jsx(r.h3,{id:"in-a-header",children:"In a Header"}),`
+`,n.jsx(t,{of:h})]})}function S(e={}){const{wrapper:r}={...a(),...e.components};return r?n.jsx(r,{...e,children:n.jsx(o,{...e})}):o(e)}export{S as default};
---
+++ demo-accordion-button-text-align/assets/PageMenu.docs-CHjzBkc1.js
@@ -0,0 +1,36 @@
+import{j as e}from"./jsx-runtime-X2b_N9AH.js";import{useMDXComponents as s}from"./index-CRbi4mQH.js";import{M as a,d as r,P as m,e as d,f as o}from"./index-eqxVJ3-m.js";import{P as h,A as l,W as c}from"./PageMenu.stories-BYKyurCB.js";import{S as p}from"./StatusBadge-CybIRuV-.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-HG6aWbbZ.js";import"../sb-preview/runtime.js";import"./index-BOkhicXD.js";import"./index-DXimoRZY.js";import"./index-B8K4vdXH.js";import"./index-DrFu-skq.js";import"./index.esm-CXIn5TrU.js";import"./PageMenu-C-9UIlX6.js";import"./clsx-B-dksMZM.js";import"./Icon-j44OzEOs.js";import"./Badge-BnLGL1Dc.js";const u=`<!-- @license CC0-1.0 -->
+
+# Page Menu
+
+A small set of links for use in the Header and Footer.
+
+## Guidelines
+
+- Limit the amount of menu items in the Header to 5 items or less, including ‘Search’ and ‘Menu’.
+- The menu should fit on a single line and is right-aligned.
+- The menu in the footer is left-aligned.
+- Submenus are not allowed.
+- The ‘Menu’ button opens the Mega Menu.
+- On narrow screens, menu items other than ‘Search’ and ‘Menu’ move from the Page Menu to the Mega Menu.
+
+## Relevant WCAG Requirements
+
+- [Consistent Navigation (Level AA)](https://www.w3.org/WAI/WCAG21/Understanding/consistent-navigation.html)
+
+PageMenu is an interactive element, and the [general requirements and guidelines for interactive elements](/docs/docs-design-guidelines-interactive-elements--docs) apply.
+`;/*@license CC0-1.0*/function i(t){const n={a:"a",h2:"h2",h3:"h3",p:"p",...s(),...t.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(a,{of:h}),`
+`,e.jsx(p,{reason:"May be affected by changes to Header and/or have its name changed."}),`
+`,e.jsx(r,{children:u}),`
+`,e.jsx(m,{}),`
+`,e.jsx(d,{}),`
+`,e.jsx(n.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(n.h3,{id:"alignment",children:"Alignment"}),`
+`,e.jsxs(n.p,{children:["In the ",e.jsx(n.a,{href:"?path=/docs/components-containers-header--docs",children:"Header"}),", the menu aligns to the end of the line."]}),`
+`,e.jsx(o,{of:l}),`
+`,e.jsx(n.h3,{id:"wrapping",children:"Wrapping"}),`
+`,e.jsxs(n.p,{children:[`If all menu items do not fit on a single line, e.g. on a narrow screen or with zoomed-in text, they wrap to new lines.
+This is often useful in the `,e.jsx(n.a,{href:"?path=/docs/components-containers-footer--docs",children:"Footer"}),`.
+The Header has its own responsive behaviour; its Page Menu should be configured not to wrap.`]}),`
+`,e.jsx(o,{of:c})]})}function R(t={}){const{wrapper:n}={...s(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(i,{...t})}):i(t)}export{R as default};
