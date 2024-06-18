14d90ed63f3c691d7022127de005d2041c2b2136
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-06-18 11:44:02 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@77fd3ec6d9b247924dd82657b3689551028d0fdb 🚀

---
+++ demo-require-header-and-footer/assets/Header.docs-CD_z8p72.js
@@ -0,0 +1,44 @@
+import{j as n}from"./jsx-runtime-X2b_N9AH.js";import{useMDXComponents as s}from"./index-CRbi4mQH.js";import{M as o,d as r,P as p,e as h,f as t}from"./index-CgB7cxkr.js";import{H as m,W as d,a as l,b as c,c as u,d as g,e as f,f as x}from"./Header.stories-Qa1BDCda.js";import{S as j}from"./StatusBadge-CybIRuV-.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-BXO6Rta7.js";import"../sb-preview/runtime.js";import"./index-BOkhicXD.js";import"./index-DXimoRZY.js";import"./index-B8K4vdXH.js";import"./index-DrFu-skq.js";import"./index.esm-CXIn5TrU.js";import"./Header-tGtavnQj.js";import"./clsx-B-dksMZM.js";import"./VisuallyHidden-CgdwkxFN.js";import"./Logo-B6EXimfo.js";import"./Heading-CGpdaMNv.js";import"./getHeadingElement-DiRwFEz7.js";import"./PageMenu-C-9UIlX6.js";import"./Icon-j44OzEOs.js";import"./Badge-BnLGL1Dc.js";const w=`<!-- @license CC0-1.0 -->
+
+# Header
+
+Displays the City’s logo and a navigation menu at the top of every page.
+Includes the name of the application if it is not the general website.
+
+## Guidelines
+
+The Header must be used on all websites and applications for the City of Amsterdam.
+It includes the logo of the City or the organization, the site title (except for the general website), and a menu with links to commonly used pages.
+The Header is important because it conveys our corporate identity and is the first thing people see.
+Using it consistently helps users recognize and trust the website.
+It is the same on every page of the application.
+
+The Page Menu should contain no more than 5 items, including ‘Search’ and ‘Menu’.
+
+## References
+
+- A Header is a [landmark](https://www.w3.org/TR/wai-aria-practices-1.1/#aria_landmark_roles) and can be use to group navigation elements.
+- [WCAG 3.2.3](https://wcag.com/designers/3-2-3-consistent-navigation/) Consistent Navigation: Navigation menus that appear on multiple pages are consistent.
+`;/*@license CC0-1.0*/function a(i){const e={a:"a",h2:"h2",p:"p",...s(),...i.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(o,{of:m}),`
+`,n.jsx(j,{reason:"Needs work for narrow screens and integration with Mega Menu."}),`
+`,n.jsx(r,{children:w}),`
+`,n.jsx(p,{}),`
+`,n.jsx(h,{}),`
+`,n.jsx(e.h2,{id:"with-logo-variant",children:"With logo variant"}),`
+`,n.jsx(t,{of:d}),`
+`,n.jsx(e.h2,{id:"with-app-name",children:"With app name"}),`
+`,n.jsx(t,{of:l}),`
+`,n.jsx(e.h2,{id:"with-menu",children:"With menu"}),`
+`,n.jsx(t,{of:c}),`
+`,n.jsx(e.h2,{id:"with-links",children:"With links"}),`
+`,n.jsxs(e.p,{children:["Use a ",n.jsx(e.a,{href:"?path=/docs/components-navigation-page-menu--docs",children:"Page Menu"}),` to add links.
+A Page Menu in the Header should not wrap.`]}),`
+`,n.jsx(t,{of:u}),`
+`,n.jsx(e.h2,{id:"with-links-and-menu",children:"With links and menu"}),`
+`,n.jsx(t,{of:g}),`
+`,n.jsx(e.h2,{id:"with-app-name-and-menu",children:"With app name and menu"}),`
+`,n.jsx(t,{of:f}),`
+`,n.jsx(e.h2,{id:"with-app-name-links-and-menu",children:"With app name, links and menu"}),`
+`,n.jsx(t,{of:x})]})}function B(i={}){const{wrapper:e}={...s(),...i.components};return e?n.jsx(e,{...i,children:n.jsx(a,{...i})}):a(i)}export{B as default};
