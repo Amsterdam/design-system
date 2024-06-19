a64826b6c25a7765b4ea95856aa1ba6385f4b81e
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-06-19 13:23:52 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@7e543ad567d446b96120cf132fe07087587ab2cc 🚀

---
+++ demo-DES-739-text-input-types/assets/FileInput.docs-DZ02WpkH.js
@@ -0,0 +1,26 @@
+import{j as e}from"./jsx-runtime-X2b_N9AH.js";import{useMDXComponents as o}from"./index-CRbi4mQH.js";import{M as l,d as r,P as a,e as c,f as t}from"./index-C6ekbabN.js";import{F as d,M as p,A as m,D as h}from"./FileInput.stories-CfU1Me_7.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-D8xSsUEC.js";import"../sb-preview/runtime.js";import"./index-BOkhicXD.js";import"./index-DXimoRZY.js";import"./index-B8K4vdXH.js";import"./index-DrFu-skq.js";import"./clsx-B-dksMZM.js";const f=`<!-- @license CC0-1.0 -->
+
+# File Input
+
+Allows the user to upload one or more files from their device.
+
+## Visual considerations
+
+The filename label and button are displayed in the language of the browser and can vary between browsers and operating systems.
+`;/*@license CC0-1.0*/function s(i){const n={a:"a",code:"code",h2:"h2",li:"li",p:"p",ul:"ul",...o(),...i.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(l,{of:d}),`
+`,e.jsx(r,{children:f}),`
+`,e.jsx(a,{}),`
+`,e.jsx(c,{}),`
+`,e.jsx(n.h2,{id:"multiple-files",children:"Multiple Files"}),`
+`,e.jsx(n.p,{children:"Allow multiple files to be selected. The label will update to show the number of files selected."}),`
+`,e.jsx(t,{of:p}),`
+`,e.jsx(n.h2,{id:"accept",children:"Accept"}),`
+`,e.jsxs(n.p,{children:["Limit the types of files that can be selected. Some examples are ",e.jsx(n.code,{children:"image/*"}),", ",e.jsx(n.code,{children:"video/*"}),", or ",e.jsx(n.code,{children:"audio/*"}),". To limit to a specific file type, use the MIME type, such as ",e.jsx(n.code,{children:"application/pdf"}),"."]}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[e.jsx(n.a,{href:"https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/file#limiting_accepted_file_types",rel:"nofollow",children:"MDN File Input"}),": More examples"]}),`
+`]}),`
+`,e.jsx(t,{of:m}),`
+`,e.jsx(n.h2,{id:"disabled",children:"Disabled"}),`
+`,e.jsx(t,{of:h})]})}function E(i={}){const{wrapper:n}={...o(),...i.components};return n?e.jsx(n,{...i,children:e.jsx(s,{...i})}):s(i)}export{E as default};
