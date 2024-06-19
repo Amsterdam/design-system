1cbdf1b20746402dd288fa28257e808c29f70274
Vincent Smedinga <v.smedinga@amsterdam.nl>
2024-06-19 12:38:18 +0200

    Remove remnants of breadcrumbs prop

--- storybook/src/pages/amsterdam/article/Article.stories.tsx
+++ storybook/src/pages/amsterdam/article/Article.stories.tsx
@@ -3,11 +3,10 @@
  * Copyright Gemeente Amsterdam
  */
 
-import type { BreadcrumbProps, FooterProps } from '@amsterdam/design-system-react'
+import type { FooterProps } from '@amsterdam/design-system-react'
 import { Meta, StoryObj } from '@storybook/react'
 import type { ReactElement } from 'react'
 import { Article } from './Article'
-import * as BreadcrumbStories from '../../../components/Breadcrumb/Breadcrumb.stories'
 import * as FooterStories from '../../../components/Footer/Footer.stories'
 import * as HeadingStories from '../../../components/Heading/Heading.stories'
 import * as ImageStories from '../../../components/Image/Image.stories'
@@ -16,15 +15,11 @@ const meta = {
   title: 'Pages/Amsterdam.nl/Article',
   component: Article,
   args: {
-    breadcrumb: BreadcrumbStories.Default.args?.children as ReactElement<BreadcrumbProps>,
     footer: FooterStories.Default.args?.children as ReactElement<FooterProps>,
     heading: HeadingStories.Default.args?.children as string,
     imageSrc: ImageStories.LazyLoading.args?.src,
   },
   argTypes: {
-    breadcrumb: {
-      table: { disable: true },
-    },
     footer: {
       table: { disable: true },
     },
