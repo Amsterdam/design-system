dc1e5d5d0e1f7a1f9baa29581de11a3400d4d9da
Vincent Smedinga <v.smedinga@amsterdam.nl>
2024-06-21 14:17:48 +0200

    feat: Allow various types of text input (#1259)
    Co-authored-by: Ruben Sibon <r.sibon@amsterdam.nl>
    Co-authored-by: Aram <37216945+alimpens@users.noreply.github.com>

--- packages/react/src/TextInput/TextInput.tsx
+++ packages/react/src/TextInput/TextInput.tsx
@@ -7,20 +7,26 @@ import clsx from 'clsx'
 import { forwardRef } from 'react'
 import type { ForwardedRef, InputHTMLAttributes } from 'react'
 
+export const textInputTypes = ['email', 'password', 'tel', 'text', 'url'] as const
+
+type TextInputType = (typeof textInputTypes)[number]
+
 export type TextInputProps = {
   /** Whether the value fails a validation rule. */
   invalid?: boolean
+  /** The kind of data that the user should provide. */
+  type?: TextInputType
 } & Omit<InputHTMLAttributes<HTMLInputElement>, 'aria-invalid'>
 
 export const TextInput = forwardRef(
-  ({ className, dir, invalid, ...restProps }: TextInputProps, ref: ForwardedRef<HTMLInputElement>) => (
+  ({ className, dir, invalid, type = 'text', ...restProps }: TextInputProps, ref: ForwardedRef<HTMLInputElement>) => (
     <input
       {...restProps}
       aria-invalid={invalid || undefined}
       className={clsx('ams-text-input', className)}
       dir={dir ?? 'auto'}
       ref={ref}
-      type="text"
+      type={type}
     />
   ),
 )
