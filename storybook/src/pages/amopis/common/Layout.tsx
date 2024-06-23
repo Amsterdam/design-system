f6572ba3f58b3e9064fbc2d4bbd4431beca6ce5d
Vincent Smedinga <v.smedinga@amsterdam.nl>
2024-06-23 12:32:50 +0200

    Add example pages from Amopis

---
+++ storybook/src/pages/amopis/common/Layout.tsx
@@ -0,0 +1,36 @@
+import { Grid, PageMenu, Screen, SkipLink } from '@amsterdam/design-system-react'
+import { HTMLAttributes } from 'react'
+import type { PropsWithChildren } from 'react'
+import { Sidebar } from './SideBar'
+import { SiteHeader } from './SiteHeader'
+
+type LayoutProps = PropsWithChildren<HTMLAttributes<HTMLElement>>
+
+export const Layout = ({ children }: LayoutProps) => (
+  <>
+    <SkipLink href="#main">Direct naar inhoud</SkipLink>
+    <Screen className="ams-screen--amopis ams-theme ams-theme--compact" fullHeight maxWidth="x-wide">
+      <div className="ams-docs-amopis-app-grid">
+        <Sidebar />
+        <div className="ams-docs-amopis-app-content">
+          <SiteHeader />
+          <div id="main" className="ams-docs-amopis-main-background">
+            {children}
+          </div>
+          <Grid paddingVertical="small">
+            <Grid.Cell span="all">
+              <PageMenu>
+                <PageMenu.Link href="#">Kerngegevens</PageMenu.Link>
+                <PageMenu.Link href="#">Ramingen</PageMenu.Link>
+                <PageMenu.Link href="#">E-mail je vraag of feedback</PageMenu.Link>
+                <PageMenu.Link href="#">Bekijk veelgestelde vragen</PageMenu.Link>
+                <PageMenu.Link href="#">Bekijk releasebeschrijving</PageMenu.Link>
+                <PageMenu.Link href="#">Prototypes</PageMenu.Link>
+              </PageMenu>
+            </Grid.Cell>
+          </Grid>
+        </div>
+      </div>
+    </Screen>
+  </>
+)
