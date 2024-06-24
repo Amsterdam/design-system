6efb5aadadc043e02bc724458f4d276894887fdb
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-06-24 07:32:25 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@d9b536741b5aea2a8c0c8599d881dbdd51c188cf 🚀

---
+++ demo-DES-728-compact-space-tokens/assets/TablePage.docs-DYNi2Oky.js
@@ -0,0 +1,4 @@
+import{j as o}from"./jsx-runtime-X2b_N9AH.js";import{useMDXComponents as e}from"./index-CRbi4mQH.js";import{M as m}from"./index-BTXj84Ps.js";import{T as i}from"./TablePage.stories-DV_RbZYS.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-tk2I6kcV.js";import"../sb-preview/runtime.js";import"./index-BOkhicXD.js";import"./index-DXimoRZY.js";import"./index-B8K4vdXH.js";import"./index-DrFu-skq.js";import"./index.esm-BAQfnEsJ.js";import"./clsx-B-dksMZM.js";import"./index.esm-BmSTY5-j.js";import"./Layout-UZYxGpoU.js";/*@license CC0-1.0*/function n(t){const r={h1:"h1",...e(),...t.components};return o.jsxs(o.Fragment,{children:[`
+`,`
+`,o.jsx(m,{of:i}),`
+`,o.jsx(r.h1,{id:"table-page",children:"Table Page"})]})}function T(t={}){const{wrapper:r}={...e(),...t.components};return r?o.jsx(r,{...t,children:o.jsx(n,{...t})}):n(t)}export{T as default};
