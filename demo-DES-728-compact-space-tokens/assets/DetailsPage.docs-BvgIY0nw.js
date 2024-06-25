babda98f392b2eae93e936bec80dc12d50295795
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-06-25 11:48:34 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@2b99aaff47ede22585252976017f5acce85cc67f 🚀

---
+++ demo-DES-728-compact-space-tokens/assets/DetailsPage.docs-BvgIY0nw.js
@@ -0,0 +1,6 @@
+import{j as t}from"./jsx-runtime-X2b_N9AH.js";import{useMDXComponents as i}from"./index-CRbi4mQH.js";import{M as e,P as m}from"./index-CLlphFlY.js";import{D as s}from"./DetailsPage.stories-BBUDa9L3.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-DT2eUt5E.js";import"../sb-preview/runtime.js";import"./index-BOkhicXD.js";import"./index-DXimoRZY.js";import"./index-B8K4vdXH.js";import"./index-DrFu-skq.js";import"./index.esm-Dwf6iMva.js";import"./clsx-B-dksMZM.js";import"./index.esm-BmSTY5-j.js";import"./meta-DngW38tU.js";/*@license CC0-1.0*/function n(o){const r={h1:"h1",p:"p",...i(),...o.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(e,{of:s}),`
+`,t.jsx(r.h1,{id:"details-page",children:"Details Page"}),`
+`,t.jsx(r.p,{children:"Displays properties of an entity."}),`
+`,t.jsx(m,{})]})}function X(o={}){const{wrapper:r}={...i(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(n,{...o})}):n(o)}export{X as default};
---
+++ demo-DES-728-compact-space-tokens/assets/TablePage.docs-DgpjeM4Q.js
@@ -0,0 +1,6 @@
+import{j as t}from"./jsx-runtime-X2b_N9AH.js";import{useMDXComponents as e}from"./index-CRbi4mQH.js";import{M as i,P as m}from"./index-CLlphFlY.js";import{T as p}from"./TablePage.stories-CjrbsOVp.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-DT2eUt5E.js";import"../sb-preview/runtime.js";import"./index-BOkhicXD.js";import"./index-DXimoRZY.js";import"./index-B8K4vdXH.js";import"./index-DrFu-skq.js";import"./index.esm-Dwf6iMva.js";import"./clsx-B-dksMZM.js";import"./index.esm-BmSTY5-j.js";import"./meta-DngW38tU.js";/*@license CC0-1.0*/function n(o){const r={h1:"h1",p:"p",...e(),...o.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(i,{of:p}),`
+`,t.jsx(r.h1,{id:"table-page",children:"Table Page"}),`
+`,t.jsx(r.p,{children:"Displays items of a collection."}),`
+`,t.jsx(m,{})]})}function C(o={}){const{wrapper:r}={...e(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(n,{...o})}):n(o)}export{C as default};
