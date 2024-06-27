3d92b049275e8a6b91b148736d342b8aa7ab6635
alimpens <37216945+alimpens@users.noreply.github.com>
2024-06-27 14:13:52 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@8e4efcc65b83707f36c4085a606278b3d06ce2ac 🚀

---
+++ demo-add-focus-mgmt-docs/assets/PageHeading.docs-BCsAJe03.js
@@ -0,0 +1,31 @@
+import{j as e}from"./jsx-runtime-X2b_N9AH.js";import{useMDXComponents as i}from"./index-CRbi4mQH.js";import{M as s,d as r,P as a,e as h,f as p}from"./index-Fu3rI3HV.js";import{P as m,I as c}from"./PageHeading.stories-Dz976okV.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-T5Rbdxl4.js";import"../sb-preview/runtime.js";import"./index-BOkhicXD.js";import"./index-DXimoRZY.js";import"./index-B8K4vdXH.js";import"./index-DrFu-skq.js";import"./decorators-CMqafKrE.js";import"./clsx-B-dksMZM.js";const d=`<!-- @license CC0-1.0 -->
+
+# Page Heading
+
+The page heading component is a prominent text at the beginning of a page, immediately after the header.
+Use this component for a name, title, or motto.
+
+## Guidelines
+
+- Use this component to capture the user’s attention.
+- Only use this component once per page, typically only on the homepage.
+- Avoid using it if you need to display long pieces of text; the text should be short and precise.
+
+## Relevant WCAG Requirements
+
+The page heading component is a variant of a heading level 1 with a distinct style.
+When using this component, ensure that the heading hierarchy of the page remains logical.
+
+For this component, the same WCAG requirements apply as for [the heading component](/docs/components-text-heading--docs).
+`;/*@license CC0-1.0*/function o(t){const n={code:"code",h2:"h2",h3:"h3",p:"p",...i(),...t.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(s,{of:m}),`
+`,e.jsx(r,{children:d}),`
+`,e.jsx(n.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(n.h3,{id:"default",children:"Default"}),`
+`,e.jsx(a,{}),`
+`,e.jsx(h,{}),`
+`,e.jsx(n.h3,{id:"inverse-colour",children:"Inverse colour"}),`
+`,e.jsxs(n.p,{children:["Set the ",e.jsx(n.code,{children:"inverseColor"}),` prop if the Page Heading sits on a dark background.
+This ensures the colour of the text provides enough contrast.`]}),`
+`,e.jsx(p,{of:c})]})}function R(t={}){const{wrapper:n}={...i(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(o,{...t})}):o(t)}export{R as default};
