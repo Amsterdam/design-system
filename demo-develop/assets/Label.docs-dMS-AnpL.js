4a4ea2781cac66d20e0b204b7718615b1abf39e3
alimpens <37216945+alimpens@users.noreply.github.com>
2024-06-28 11:12:50 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@3072f322052b693ead71e37e1fdc9f08d4ade95c 🚀

---
+++ demo-develop/assets/Label.docs-dMS-AnpL.js
@@ -0,0 +1,15 @@
+import{j as n}from"./jsx-runtime-X2b_N9AH.js";import{useMDXComponents as e}from"./index-CRbi4mQH.js";import{M as s,d as i,P as a,e as m}from"./index-DuapEtBJ.js";import{L as p}from"./Label.stories-BasMlsJU.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-o-lfsfM3.js";import"../sb-preview/runtime.js";import"./index-BOkhicXD.js";import"./index-DXimoRZY.js";import"./index-B8K4vdXH.js";import"./index-DrFu-skq.js";import"./Label-BnSmP98q.js";import"./clsx-B-dksMZM.js";const c=`<!-- @license CC0-1.0 -->
+
+# Label
+
+Describes a form control.
+
+## Guidelines
+
+Always associate a form element (such as an \`input\` or \`textarea\`) with a label.
+`;/*@license CC0-1.0*/function r(t){return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(s,{of:p}),`
+`,n.jsx(i,{children:c}),`
+`,n.jsx(a,{}),`
+`,n.jsx(m,{})]})}function E(t={}){const{wrapper:o}={...e(),...t.components};return o?n.jsx(o,{...t,children:n.jsx(r,{...t})}):r()}export{E as default};
