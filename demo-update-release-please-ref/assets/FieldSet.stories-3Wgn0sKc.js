27dc70cb8b0e342a296602bb31529f7fe7c9c6f4
alimpens <37216945+alimpens@users.noreply.github.com>
2024-06-28 11:50:25 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@bdab90b68880037915440e82c033b1725b649ff1 🚀

---
+++ demo-update-release-please-ref/assets/FieldSet.stories-3Wgn0sKc.js
@@ -0,0 +1,149 @@
+import{j as a}from"./jsx-runtime-X2b_N9AH.js";import{F as d,R as i}from"./FieldSet-DkgxHA-T.js";import{L as s}from"./Label-BnSmP98q.js";import{T as o}from"./TextInput-DvUsoF4e.js";import{C as l}from"./Column-BRryplre.js";import{E as r}from"./ErrorMessage-BHYT2tqE.js";import{P as t}from"./Paragraph-DpVlyIEJ.js";import{C as n}from"./Checkbox-DRBoUL7U.js";const A={title:"Components/Forms/Field Set",component:d,args:{invalid:!1,legend:"Wat is uw naam?"},decorators:[e=>a.jsx("form",{children:a.jsx(e,{})})]},u={render:e=>a.jsx(d,{invalid:e.invalid,legend:e.legend,children:a.jsxs(l,{gap:"extra-small",children:[a.jsx(s,{htmlFor:"input-a1",children:"Voornaam"}),e.invalid&&a.jsx(r,{id:"error-a1",children:"Vul uw voornaam in."}),a.jsx(o,{"aria-describedby":e.invalid?"error-a1":void 0,"aria-required":"true",id:"input-a1",invalid:e.invalid}),a.jsx(s,{htmlFor:"input-a2",children:"Achternaam"}),e.invalid&&a.jsx(r,{id:"error-a2",children:"Vul uw achternaam in."}),a.jsx(o,{"aria-describedby":e.invalid?"error-a2":void 0,"aria-required":"true",id:"input-a2",invalid:e.invalid})]})})},m={render:e=>a.jsxs(d,{invalid:e.invalid,legend:e.legend,"aria-describedby":"description-b",children:[a.jsx(t,{id:"description-b",size:"small",className:"ams-mb--sm",children:"Vul uw naam in zoals in uw paspoort staat."}),a.jsxs(l,{gap:"extra-small",children:[a.jsx(s,{htmlFor:"input-b1",children:"Voornaam"}),e.invalid&&a.jsx(r,{id:"error-b1",children:"Vul uw voornaam in."}),a.jsx(o,{"aria-describedby":e.invalid?"error-b1":void 0,"aria-required":"true",id:"input-b1",invalid:e.invalid}),a.jsx(s,{htmlFor:"input-b2",children:"Achternaam"}),e.invalid&&a.jsx(r,{id:"error-b2",children:"Vul uw achternaam in."}),a.jsx(o,{"aria-describedby":e.invalid?"error-b2":void 0,"aria-required":"true",id:"input-b2",invalid:e.invalid})]})]})},c={args:{invalid:!0},render:e=>a.jsxs(d,{invalid:e.invalid,legend:e.legend,"aria-describedby":"description-c",children:[a.jsx(t,{id:"description-c",size:"small",className:"ams-mb--sm",children:"Vul uw naam in zoals in uw paspoort staat."}),a.jsxs(l,{gap:"extra-small",children:[a.jsx(s,{htmlFor:"input-c1",children:"Voornaam"}),e.invalid&&a.jsx(r,{id:"error-c1",children:"Vul uw voornaam in."}),a.jsx(o,{"aria-describedby":e.invalid?"error-c1":void 0,"aria-required":"true",id:"input-c1",invalid:e.invalid}),a.jsx(s,{htmlFor:"input-c2",children:"Achternaam"}),e.invalid&&a.jsx(r,{id:"error-c2",children:"Vul uw achternaam in."}),a.jsx(o,{"aria-describedby":e.invalid?"error-c2":void 0,"aria-required":"true",id:"input-c2",invalid:e.invalid})]})]})},v={args:{legend:"Waar gaat uw melding over?"},render:e=>a.jsxs(d,{"aria-describedby":`description-d${e.invalid?" error-d":""}`,"aria-required":"true",invalid:e.invalid,legend:e.legend,role:"radiogroup",children:[a.jsx(t,{className:"ams-mb--sm",id:"description-d",size:"small",children:"De laatstgenoemde melding."}),e.invalid&&a.jsx(r,{className:"ams-mb--sm",id:"error-d",children:"Geef aan waar uw laatstgenoemde melding over gaat."}),a.jsxs(l,{gap:"extra-small",children:[a.jsx(i,{"aria-required":"true",invalid:e.invalid,name:"about",value:"horeca",children:"Horecabedrijf"}),a.jsx(i,{"aria-required":"true",invalid:e.invalid,name:"about",value:"ander_bedrijf",children:"Ander soort bedrijf"}),a.jsx(i,{"aria-required":"true",invalid:e.invalid,name:"about",value:"evenement",children:"Evenement"}),a.jsx(i,{"aria-required":"true",invalid:e.invalid,name:"about",value:"anders",children:"Iets anders"})]})]})},g={args:{legend:"Waar gaat uw melding over?",invalid:!0},render:e=>a.jsxs(d,{"aria-describedby":`description-e${e.invalid?" error-e":""}`,"aria-required":"true",invalid:e.invalid,legend:e.legend,role:"radiogroup",children:[a.jsx(t,{className:"ams-mb--sm",id:"description-e",size:"small",children:"De laatstgenoemde melding."}),e.invalid&&a.jsx(r,{className:"ams-mb--sm",id:"error-e",children:"Geef aan waar uw laatstgenoemde melding over gaat."}),a.jsxs(l,{gap:"extra-small",children:[a.jsx(i,{"aria-required":"true",invalid:e.invalid,name:"about",value:"horeca",children:"Horecabedrijf"}),a.jsx(i,{"aria-required":"true",invalid:e.invalid,name:"about",value:"ander_bedrijf",children:"Ander soort bedrijf"}),a.jsx(i,{"aria-required":"true",invalid:e.invalid,name:"about",value:"evenement",children:"Evenement"}),a.jsx(i,{"aria-required":"true",invalid:e.invalid,name:"about",value:"anders",children:"Iets anders"})]})]})},b={args:{legend:"Waar gaat uw melding over?"},render:e=>a.jsxs(d,{"aria-labelledby":`fieldset-f description-f${e.invalid?" error-f":""}`,id:"fieldset-f",invalid:e.invalid,legend:e.legend,children:[a.jsx(t,{className:"ams-mb--sm",id:"description-f",size:"small",children:"De laatstgenoemde melding."}),e.invalid&&a.jsx(r,{className:"ams-mb--sm",id:"error-f",children:"Geef aan waar uw melding over gaat."}),a.jsxs(l,{gap:"extra-small",children:[a.jsx(n,{"aria-required":"true",invalid:e.invalid,name:"about",value:"horeca",children:"Horecabedrijf"}),a.jsx(n,{"aria-required":"true",invalid:e.invalid,name:"about",value:"ander_bedrijf",children:"Ander soort bedrijf"}),a.jsx(n,{"aria-required":"true",invalid:e.invalid,name:"about",value:"evenement",children:"Evenement"}),a.jsx(n,{"aria-required":"true",invalid:e.invalid,name:"about",value:"anders",children:"Iets anders"})]})]})},p={args:{invalid:!0,legend:"Waar gaat uw melding over?"},render:e=>a.jsxs(d,{"aria-labelledby":`fieldset-g description-g${e.invalid?" error-g":""}`,id:"fieldset-g",invalid:e.invalid,legend:e.legend,children:[a.jsx(t,{id:"description-g",size:"small",className:"ams-mb--sm",children:"De laatstgenoemde melding."}),e.invalid&&a.jsx(r,{className:"ams-mb--sm",id:"error-g",children:"Geef aan waar uw melding over gaat."}),a.jsxs(l,{gap:"extra-small",children:[a.jsx(n,{"aria-required":"true",invalid:e.invalid,name:"about",value:"horeca",children:"Horecabedrijf"}),a.jsx(n,{"aria-required":"true",invalid:e.invalid,name:"about",value:"ander_bedrijf",children:"Ander soort bedrijf"}),a.jsx(n,{"aria-required":"true",invalid:e.invalid,name:"about",value:"evenement",children:"Evenement"}),a.jsx(n,{"aria-required":"true",invalid:e.invalid,name:"about",value:"anders",children:"Iets anders"})]})]})};var h,x,j;u.parameters={...u.parameters,docs:{...(h=u.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  render: args => <FieldSet invalid={args.invalid} legend={args.legend}>
+      <Column gap="extra-small">
+        <Label htmlFor="input-a1">Voornaam</Label>
+        {args.invalid && <ErrorMessage id="error-a1">Vul uw voornaam in.</ErrorMessage>}
+        <TextInput aria-describedby={args.invalid ? 'error-a1' : undefined} aria-required="true" id="input-a1" invalid={args.invalid} />
+        <Label htmlFor="input-a2">Achternaam</Label>
+        {args.invalid && <ErrorMessage id="error-a2">Vul uw achternaam in.</ErrorMessage>}
+        <TextInput aria-describedby={args.invalid ? 'error-a2' : undefined} aria-required="true" id="input-a2" invalid={args.invalid} />
+      </Column>
+    </FieldSet>
+}`,...(j=(x=u.parameters)==null?void 0:x.docs)==null?void 0:j.source}}};var f,q,w;m.parameters={...m.parameters,docs:{...(f=m.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  render: args => <FieldSet invalid={args.invalid} legend={args.legend} aria-describedby="description-b">
+      <Paragraph id="description-b" size="small" className="ams-mb--sm">
+        Vul uw naam in zoals in uw paspoort staat.
+      </Paragraph>
+      <Column gap="extra-small">
+        <Label htmlFor="input-b1">Voornaam</Label>
+        {args.invalid && <ErrorMessage id="error-b1">Vul uw voornaam in.</ErrorMessage>}
+        <TextInput aria-describedby={args.invalid ? 'error-b1' : undefined} aria-required="true" id="input-b1" invalid={args.invalid} />
+        <Label htmlFor="input-b2">Achternaam</Label>
+        {args.invalid && <ErrorMessage id="error-b2">Vul uw achternaam in.</ErrorMessage>}
+        <TextInput aria-describedby={args.invalid ? 'error-b2' : undefined} aria-required="true" id="input-b2" invalid={args.invalid} />
+      </Column>
+    </FieldSet>
+}`,...(w=(q=m.parameters)==null?void 0:q.docs)==null?void 0:w.source}}};var C,E,F;c.parameters={...c.parameters,docs:{...(C=c.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  args: {
+    invalid: true
+  },
+  render: args => <FieldSet invalid={args.invalid} legend={args.legend} aria-describedby="description-c">
+      <Paragraph id="description-c" size="small" className="ams-mb--sm">
+        Vul uw naam in zoals in uw paspoort staat.
+      </Paragraph>
+      <Column gap="extra-small">
+        <Label htmlFor="input-c1">Voornaam</Label>
+        {args.invalid && <ErrorMessage id="error-c1">Vul uw voornaam in.</ErrorMessage>}
+        <TextInput aria-describedby={args.invalid ? 'error-c1' : undefined} aria-required="true" id="input-c1" invalid={args.invalid} />
+        <Label htmlFor="input-c2">Achternaam</Label>
+        {args.invalid && <ErrorMessage id="error-c2">Vul uw achternaam in.</ErrorMessage>}
+        <TextInput aria-describedby={args.invalid ? 'error-c2' : undefined} aria-required="true" id="input-c2" invalid={args.invalid} />
+      </Column>
+    </FieldSet>
+}`,...(F=(E=c.parameters)==null?void 0:E.docs)==null?void 0:F.source}}};var S,y,R;v.parameters={...v.parameters,docs:{...(S=v.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    legend: 'Waar gaat uw melding over?'
+  },
+  render: args => <FieldSet aria-describedby={\`description-d\${args.invalid ? ' error-d' : ''}\`} aria-required="true" invalid={args.invalid} legend={args.legend} role="radiogroup">
+      <Paragraph className="ams-mb--sm" id="description-d" size="small">
+        De laatstgenoemde melding.
+      </Paragraph>
+      {args.invalid && <ErrorMessage className="ams-mb--sm" id="error-d">
+          Geef aan waar uw laatstgenoemde melding over gaat.
+        </ErrorMessage>}
+      <Column gap="extra-small">
+        <Radio aria-required="true" invalid={args.invalid} name="about" value="horeca">
+          Horecabedrijf
+        </Radio>
+        <Radio aria-required="true" invalid={args.invalid} name="about" value="ander_bedrijf">
+          Ander soort bedrijf
+        </Radio>
+        <Radio aria-required="true" invalid={args.invalid} name="about" value="evenement">
+          Evenement
+        </Radio>
+        <Radio aria-required="true" invalid={args.invalid} name="about" value="anders">
+          Iets anders
+        </Radio>
+      </Column>
+    </FieldSet>
+}`,...(R=(y=v.parameters)==null?void 0:y.docs)==null?void 0:R.source}}};var M,V,k;g.parameters={...g.parameters,docs:{...(M=g.parameters)==null?void 0:M.docs,source:{originalSource:`{
+  args: {
+    legend: 'Waar gaat uw melding over?',
+    invalid: true
+  },
+  render: args => <FieldSet aria-describedby={\`description-e\${args.invalid ? ' error-e' : ''}\`} aria-required="true" invalid={args.invalid} legend={args.legend} role="radiogroup">
+      <Paragraph className="ams-mb--sm" id="description-e" size="small">
+        De laatstgenoemde melding.
+      </Paragraph>
+      {args.invalid && <ErrorMessage className="ams-mb--sm" id="error-e">
+          Geef aan waar uw laatstgenoemde melding over gaat.
+        </ErrorMessage>}
+      <Column gap="extra-small">
+        <Radio aria-required="true" invalid={args.invalid} name="about" value="horeca">
+          Horecabedrijf
+        </Radio>
+        <Radio aria-required="true" invalid={args.invalid} name="about" value="ander_bedrijf">
+          Ander soort bedrijf
+        </Radio>
+        <Radio aria-required="true" invalid={args.invalid} name="about" value="evenement">
+          Evenement
+        </Radio>
+        <Radio aria-required="true" invalid={args.invalid} name="about" value="anders">
+          Iets anders
+        </Radio>
+      </Column>
+    </FieldSet>
+}`,...(k=(V=g.parameters)==null?void 0:V.docs)==null?void 0:k.source}}};var N,W,z;b.parameters={...b.parameters,docs:{...(N=b.parameters)==null?void 0:N.docs,source:{originalSource:`{
+  args: {
+    legend: 'Waar gaat uw melding over?'
+  },
+  render: args => <FieldSet aria-labelledby={\`fieldset-f description-f\${args.invalid ? ' error-f' : ''}\`} id="fieldset-f" invalid={args.invalid} legend={args.legend}>
+      <Paragraph className="ams-mb--sm" id="description-f" size="small">
+        De laatstgenoemde melding.
+      </Paragraph>
+      {args.invalid && <ErrorMessage className="ams-mb--sm" id="error-f">
+          Geef aan waar uw melding over gaat.
+        </ErrorMessage>}
+      <Column gap="extra-small">
+        <Checkbox aria-required="true" invalid={args.invalid} name="about" value="horeca">
+          Horecabedrijf
+        </Checkbox>
+        <Checkbox aria-required="true" invalid={args.invalid} name="about" value="ander_bedrijf">
+          Ander soort bedrijf
+        </Checkbox>
+        <Checkbox aria-required="true" invalid={args.invalid} name="about" value="evenement">
+          Evenement
+        </Checkbox>
+        <Checkbox aria-required="true" invalid={args.invalid} name="about" value="anders">
+          Iets anders
+        </Checkbox>
+      </Column>
+    </FieldSet>
+}`,...(z=(W=b.parameters)==null?void 0:W.docs)==null?void 0:z.source}}};var G,I,P;p.parameters={...p.parameters,docs:{...(G=p.parameters)==null?void 0:G.docs,source:{originalSource:`{
+  args: {
+    invalid: true,
+    legend: 'Waar gaat uw melding over?'
+  },
+  render: args => <FieldSet aria-labelledby={\`fieldset-g description-g\${args.invalid ? ' error-g' : ''}\`} id="fieldset-g" invalid={args.invalid} legend={args.legend}>
+      <Paragraph id="description-g" size="small" className="ams-mb--sm">
+        De laatstgenoemde melding.
+      </Paragraph>
+      {args.invalid && <ErrorMessage className="ams-mb--sm" id="error-g">
+          Geef aan waar uw melding over gaat.
+        </ErrorMessage>}
+      <Column gap="extra-small">
+        <Checkbox aria-required="true" invalid={args.invalid} name="about" value="horeca">
+          Horecabedrijf
+        </Checkbox>
+        <Checkbox aria-required="true" invalid={args.invalid} name="about" value="ander_bedrijf">
+          Ander soort bedrijf
+        </Checkbox>
+        <Checkbox aria-required="true" invalid={args.invalid} name="about" value="evenement">
+          Evenement
+        </Checkbox>
+        <Checkbox aria-required="true" invalid={args.invalid} name="about" value="anders">
+          Iets anders
+        </Checkbox>
+      </Column>
+    </FieldSet>
+}`,...(P=(I=p.parameters)==null?void 0:I.docs)==null?void 0:P.source}}};const L=["Default","WithDescription","WithError","RadioGroup","RadioGroupWithError","CheckboxGroup","CheckboxGroupWithError"],K=Object.freeze(Object.defineProperty({__proto__:null,CheckboxGroup:b,CheckboxGroupWithError:p,Default:u,RadioGroup:v,RadioGroupWithError:g,WithDescription:m,WithError:c,__namedExportsOrder:L,default:A},Symbol.toStringTag,{value:"Module"}));export{b as C,K as F,v as R,m as W,c as a,g as b,p as c};
