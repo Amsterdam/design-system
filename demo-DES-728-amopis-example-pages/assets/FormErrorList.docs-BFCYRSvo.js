d26207d8e269620d4923edac90f1fbcf854c74fd
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-06-28 14:19:20 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@3ab5a6389e167b15fd6d1ab010e246d1180f95df 🚀

---
+++ demo-DES-728-amopis-example-pages/assets/FormErrorList.docs-BFCYRSvo.js
@@ -0,0 +1,41 @@
+import{j as e}from"./jsx-runtime-X2b_N9AH.js";import{useMDXComponents as o}from"./index-CRbi4mQH.js";import{M as i,d as s,P as a,e as m}from"./index-XSx6ljbH.js";import{F as h}from"./FormErrorList.stories-BHGVRQmz.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-BIfmbQvf.js";import"../sb-preview/runtime.js";import"./index-BOkhicXD.js";import"./index-DXimoRZY.js";import"./index-B8K4vdXH.js";import"./index-DrFu-skq.js";import"./clsx-B-dksMZM.js";import"./Alert-DpKdmoxK.js";import"./index.esm-BmSTY5-j.js";import"./Icon-j44OzEOs.js";import"./Heading-CGpdaMNv.js";import"./getHeadingElement-DiRwFEz7.js";import"./IconButton-Dq0zUB6U.js";import"./LinkList-B-fKfq07.js";const l=`<!-- @license CC0-1.0 -->
+
+# Form Error List
+
+Use this component at the top of a page to summarise any errors a user has made.
+When a user makes an error, you must show both a Form Error List and an Error Message above each answer that contains an error.
+
+## Guidelines
+
+- Always show a Form Error List when there is a validation error, even if there’s only one.
+- You must link the errors in the Form Error List to the answer they relate to (see below).
+
+## Linking from the Form Error List to each answer
+
+For questions that require a user to answer using a single field, like a file upload, select, textarea, text input or character count, link to the \`id\` of that field.
+
+When a user has to enter their answer into multiple fields, such as day, month and year fields, link to the \`id\` of the first field that contains an error.
+If you do not know which field contains an error, link to the \`id\` of the first field.
+
+For questions that require a user to select one or more options from a list using Radios or Checkboxes, link to the \`id\` of the first Radio or Checkbox.
+
+## Where to put the Form Error List
+
+Put the Form Error List directly above the first question on the page. Place it outside of the \`<form>\`-tag, [to make sure screen readers do not skip it](https://nldesignsystem.nl/richtlijnen/formulieren/meerdere-stappen/#plaats-de-informatie-over-waar-de-gebruiker-is-in-de-stappen-boven-het-formulier).
+
+## Relevant WCAG requirements
+
+Pay extra attention to these parts:
+
+- [WCAG requirement 1.3.1](https://www.w3.org/TR/WCAG21/#info-and-relationships): the heading level of the Form Error List depends on where in the page it is placed, this may differ per page.
+
+## References
+
+- [Show an error summary above the form - NL Design System](https://www.nldesignsystem.nl/richtlijnen/formulieren/foutmeldingen#zet-een-samenvatting-van-de-foutmeldingen-boven-het-formulier)
+- [Error Summary component - Gov.uk](https://design-system.service.gov.uk/components/error-summary/)
+`;/*@license CC0-1.0*/function n(r){return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(i,{of:h}),`
+`,e.jsx(s,{children:l}),`
+`,e.jsx(a,{}),`
+`,e.jsx(m,{})]})}function W(r={}){const{wrapper:t}={...o(),...r.components};return t?e.jsx(t,{...r,children:e.jsx(n,{...r})}):n()}export{W as default};
