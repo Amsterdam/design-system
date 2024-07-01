5391f7c1d86370e63a6cf4abd998d191552f796c
Vincent Smedinga <v.smedinga@amsterdam.nl>
2024-07-01 11:59:58 +0200

    Extract configuration object for Sidebar

--- storybook/src/pages/amopis/common/SideBar.tsx
+++ storybook/src/pages/amopis/common/SideBar.tsx
@@ -6,42 +6,32 @@ import {
   StopIcon,
   SuitcaseIcon,
 } from '@amsterdam/design-system-react-icons'
+import type { ElementType } from 'react'
 
-export function Sidebar() {
-  return (
-    <div className="ams-docs-amopis-sidebar">
-      <Column gap="large">
-        <Column className="ams-docs-amopis-column--halign-center" gap="extra-small">
-          <HousingIcon fill="white" width={24} />
-          <Paragraph inverseColor size="small">
-            Dashboard
-          </Paragraph>
-        </Column>
-        <Column className="ams-docs-amopis-column--halign-center" gap="extra-small">
-          <SuitcaseIcon fill="white" width={24} />
-          <Paragraph inverseColor size="small">
-            Projecten
-          </Paragraph>
-        </Column>
-        <Column className="ams-docs-amopis-column--halign-center" gap="extra-small">
-          <DocumentTextIcon fill="white" width={24} />
-          <Paragraph inverseColor size="small">
-            Rapportages
-          </Paragraph>
-        </Column>
-        <Column className="ams-docs-amopis-column--halign-center" gap="extra-small">
-          <ExternalLinkIcon fill="white" width={24} />
-          <Paragraph inverseColor size="small">
-            Analyses
-          </Paragraph>
-        </Column>
-        <Column className="ams-docs-amopis-column--halign-center" gap="extra-small">
-          <StopIcon fill="white" width={24} />
-          <Paragraph inverseColor size="small">
-            Instellingen
-          </Paragraph>
-        </Column>
-      </Column>
-    </div>
-  )
+type MenuItem = {
+  Icon: ElementType
+  text: string
 }
+
+const menuItems: MenuItem[] = [
+  { Icon: HousingIcon, text: 'Dashboard' },
+  { Icon: SuitcaseIcon, text: 'Projecten' },
+  { Icon: DocumentTextIcon, text: 'Rapportages' },
+  { Icon: ExternalLinkIcon, text: 'Analyses' },
+  { Icon: StopIcon, text: 'Instellingen' },
+]
+
+export const Sidebar = () => (
+  <div className="ams-docs-amopis-sidebar">
+    <Column gap="large">
+      {menuItems.map(({ Icon, text }, index) => (
+        <Column key={index} className="ams-docs-amopis-column--halign-center" gap="extra-small">
+          <Icon fill="white" width={24} />
+          <Paragraph inverseColor size="small">
+            {text}
+          </Paragraph>
+        </Column>
+      ))}
+    </Column>
+  </div>
+)
