6fe6e83e3fc5bdc84051977d9d1437db70511cbf
Vincent Smedinga <v.smedinga@amsterdam.nl>
2024-07-01 11:22:47 +0200

    Use Avatar component and randomize initials

--- storybook/src/pages/amopis/common/SiteHeader.tsx
+++ storybook/src/pages/amopis/common/SiteHeader.tsx
@@ -1,24 +1,17 @@
-import { Grid, Header, Paragraph } from '@amsterdam/design-system-react'
+import { Avatar, Grid, Header } from '@amsterdam/design-system-react'
 
-export function SiteHeader() {
-  return (
-    <Grid>
-      <Grid.Cell span="all">
-        <Header
-          appName="Amopis"
-          links={
-            <div style={{ display: 'flex', justifyContent: 'end' }}>
-              <div className="ams-docs-amopis-user-badge">
-                <Paragraph inverseColor size="small" title="Goedemorgen Kees Herder">
-                  KH
-                </Paragraph>
-              </div>
-            </div>
-          }
-          logoLink="amopis"
-          logoLinkTitle="Naar de homepage van Amopis"
-        />
-      </Grid.Cell>
-    </Grid>
-  )
-}
+const getRandomLetter = () => Math.floor(Math.random() * 26) + 65
+const generateInitials = (): string => String.fromCharCode(...[getRandomLetter(), getRandomLetter()])
+
+export const SiteHeader = () => (
+  <Grid>
+    <Grid.Cell span="all">
+      <Header
+        appName="Amopis"
+        links={<Avatar label={generateInitials()} />}
+        logoLink="amopis"
+        logoLinkTitle="Naar de homepage van Amopis"
+      />
+    </Grid.Cell>
+  </Grid>
+)
