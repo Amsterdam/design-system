827e24797aeda7986f36570883d3e1c44bc55ebb
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-07-01 09:52:09 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@ae114ea84e8f52660dbdce13a0723e2060a2c71c 🚀

---
+++ demo-DES-728-amopis-example-pages/assets/DetailsPage.docs-CmnUpCL0.js
@@ -0,0 +1,6 @@
+import{j as t}from"./jsx-runtime-X2b_N9AH.js";import{useMDXComponents as i}from"./index-CRbi4mQH.js";import{M as e,P as m}from"./index-CBRE639V.js";import{D as s}from"./DetailsPage.stories-B_BSZ-NZ.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-BUyDuf-p.js";import"../sb-preview/runtime.js";import"./index-BOkhicXD.js";import"./index-DXimoRZY.js";import"./index-B8K4vdXH.js";import"./index-DrFu-skq.js";import"./index.esm-yXQQXCDi.js";import"./clsx-B-dksMZM.js";import"./index.esm-BmSTY5-j.js";import"./meta-BmQMEcDc.js";/*@license CC0-1.0*/function n(o){const r={h1:"h1",p:"p",...i(),...o.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(e,{of:s}),`
+`,t.jsx(r.h1,{id:"details-page",children:"Details Page"}),`
+`,t.jsx(r.p,{children:"Displays properties of an entity."}),`
+`,t.jsx(m,{})]})}function X(o={}){const{wrapper:r}={...i(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(n,{...o})}):n(o)}export{X as default};
---
+++ demo-DES-728-amopis-example-pages/assets/Introduction.docs-ZDpAhvIm.js
@@ -0,0 +1,24 @@
+import{j as e}from"./jsx-runtime-X2b_N9AH.js";import{useMDXComponents as s}from"./index-CRbi4mQH.js";import{M as i}from"./index-CBRE639V.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-BUyDuf-p.js";import"../sb-preview/runtime.js";import"./index-BOkhicXD.js";import"./index-DXimoRZY.js";import"./index-B8K4vdXH.js";import"./index-DrFu-skq.js";/*@license CC0-1.0*/function o(n){const t={code:"code",h1:"h1",h2:"h2",h3:"h3",p:"p",...s(),...n.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(i,{title:"Pages/Introduction"}),`
+`,e.jsx(t.h1,{id:"introduction",children:"Introduction"}),`
+`,e.jsx(t.p,{children:"This section demonstrates how to build entire pages using the Design System’s components."}),`
+`,e.jsx(t.p,{children:"We offer a home page, an article page and a form for the upcoming Amsterdam.nl website."}),`
+`,e.jsx(t.p,{children:`As an example of an application created with our libraries, we have two pages for ‘Amopis’.
+These employ some custom class names and inline styles for features we don’t yet support.
+These are not intended for reuse.`}),`
+`,e.jsx(t.h2,{id:"documentation",children:"Documentation"}),`
+`,e.jsx(t.p,{children:"The ‘Docs’ page explains the design decisions for this type of page and how to build it."}),`
+`,e.jsx(t.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(t.p,{children:"Each example, of which the first is called ‘Default’, shows the actual page in its entirety."}),`
+`,e.jsx(t.h3,{id:"flexible-content",children:"Flexible content"}),`
+`,e.jsx(t.p,{children:`Reload an example to get new random texts and images.
+Use the Control tab to experiment with your own content.`}),`
+`,e.jsx(t.h3,{id:"test-for-accessibility-and-see-html-output",children:"Test for accessibility and see HTML output"}),`
+`,e.jsx(t.p,{children:`The ‘Accessibility’ tab offers a report of various automatic tests.
+The ‘HTML’ tab displays the entire HTML-document rendered by all components on the page.`}),`
+`,e.jsx(t.h3,{id:"check-how-the-page-would-look-on-various-devices",children:"Check how the page would look on various devices"}),`
+`,e.jsx(t.p,{children:"The buttons in the toolbar offer various features, of which the ‘three layered square’ button replicates the appearance and behaviour of the page on a phone, tablet or desktop. Press the button with the two arrows to rotate the screen."}),`
+`,e.jsx(t.h3,{id:"see-the-page-full-screen",children:"See the page full-screen"}),`
+`,e.jsxs(t.p,{children:["Use the ‘Go to full screen’ (or press ",e.jsx(t.code,{children:"alt+F"}),`) or ‘Open canvas in new tab’ buttons to the top right to see the entire page.
+Alternatively, press `,e.jsx(t.code,{children:"alt+S,T,A"})," to hide the three panels."]})]})}function j(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o(n)}export{j as default};
