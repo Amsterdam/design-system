bc87a01ee07c3b048ebf99fb6ca875190fb320e3
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-07-01 09:31:37 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@6fe6e83e3fc5bdc84051977d9d1437db70511cbf 🚀

---
+++ demo-DES-728-amopis-example-pages/assets/DetailsPage.docs-C0LLgDJ6.js
@@ -0,0 +1,6 @@
+import{j as t}from"./jsx-runtime-X2b_N9AH.js";import{useMDXComponents as i}from"./index-CRbi4mQH.js";import{M as e,P as m}from"./index-Dset_qpc.js";import{D as s}from"./DetailsPage.stories-CVRj6AoJ.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-CrRH9MJQ.js";import"../sb-preview/runtime.js";import"./index-BOkhicXD.js";import"./index-DXimoRZY.js";import"./index-B8K4vdXH.js";import"./index-DrFu-skq.js";import"./index.esm-yXQQXCDi.js";import"./clsx-B-dksMZM.js";import"./index.esm-BmSTY5-j.js";import"./meta-amq_5T4S.js";/*@license CC0-1.0*/function n(o){const r={h1:"h1",p:"p",...i(),...o.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(e,{of:s}),`
+`,t.jsx(r.h1,{id:"details-page",children:"Details Page"}),`
+`,t.jsx(r.p,{children:"Displays properties of an entity."}),`
+`,t.jsx(m,{})]})}function X(o={}){const{wrapper:r}={...i(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(n,{...o})}):n(o)}export{X as default};
