bd6a4a100f0d5d1a2219b25f907f0920ffcdb0a9
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-07-02 21:35:15 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@feee785d3786adb9cc7c824ba37e78a61ec965f9 🚀

---
+++ demo-DES-802-spotlight-duotone/assets/Spotlight.stories-D1RRyfKq.js
@@ -0,0 +1,260 @@
+import{j as e}from"./jsx-runtime-X2b_N9AH.js";import{H as Pe,a as be}from"./index.esm-ClG5nKvY.js";import{e as ze,b as d}from"./exampleContent-BEbrefRz.js";import{S as s}from"./Spotlight-Cn7b3okT.js";import{G as n}from"./Grid-BwoxiRyw.js";import{B as Re}from"./Blockquote-7VFIi5C-.js";import{S as Be}from"./Screen-nsUNRErq.js";import{C as l}from"./Column-BRryplre.js";import{A as t}from"./AspectRatio-DxP_kpbd.js";import{I as i}from"./Image-Bf5zYcta.js";import{P as c}from"./Paragraph-DpVlyIEJ.js";const Ve=ze(),b="https://picsum.photos/1600/1600",Te={title:"Components/Containers/Spotlight",component:s,render:({as:a,color:r})=>e.jsx(s,{as:a,color:r,children:e.jsx(n,{paddingVertical:"medium",children:e.jsx(n.Cell,{colspan:"all",children:e.jsx(Re,{inverseColor:!r||!["green","yellow"].includes(r),children:Ve})})})}),decorators:[a=>e.jsx(Be,{maxWidth:"wide",children:e.jsx(a,{cover:!0})})]},p={},m={args:{color:"blue"}},u={args:{color:"light-blue"}},g={args:{color:"dark-green"}},h={args:{color:"green"}},w={args:{color:"magenta"}},x={args:{color:"orange"}},j={args:{color:"purple"}},C={args:{color:"yellow"}},G={args:{as:"section"}},v={parameters:{layout:"fullscreen"},args:{color:"blue",gradient:!0},render:({as:a,color:r,gradient:o})=>e.jsx(s,{as:a,color:r,gradient:o,children:e.jsx(n,{paddingVertical:"medium",children:e.jsx(n.Cell,{colspan:"all",children:e.jsxs(l,{gap:"medium",children:[e.jsx(t,{ratio:"x-tall",children:e.jsx(i,{alt:"",src:"https://picsum.photos/1440/2560",cover:!0})}),e.jsx(c,{size:"small",inverseColor:!r||!["green","yellow"].includes(r),children:d()})]})})})})},S={parameters:{layout:"fullscreen"},args:{color:"blue",gradient:!0},render:({as:a,color:r,gradient:o})=>e.jsx(s,{as:a,color:r,gradient:o,children:e.jsx(n,{paddingVertical:"medium",children:e.jsx(n.Cell,{colspan:"all",children:e.jsxs(l,{gap:"medium",children:[e.jsx(t,{ratio:"tall",children:e.jsx(i,{alt:"",src:"https://picsum.photos/1200/1600",cover:!0})}),e.jsx(c,{size:"small",inverseColor:!r||!["green","yellow"].includes(r),children:d()})]})})})})},k={parameters:{layout:"fullscreen"},args:{color:"blue",gradient:!0},render:({as:a,color:r,gradient:o})=>e.jsx(s,{as:a,color:r,gradient:o,children:e.jsx(n,{paddingVertical:"medium",children:e.jsx(n.Cell,{colspan:"all",children:e.jsxs(l,{gap:"medium",children:[e.jsx(t,{ratio:"square",children:e.jsx(i,{alt:"",src:b,cover:!0})}),e.jsx(c,{size:"small",inverseColor:!r||!["green","yellow"].includes(r),children:d()})]})})})})},f={parameters:{layout:"fullscreen"},args:{color:"blue",gradient:!0},render:({as:a,color:r,gradient:o})=>e.jsx(s,{as:a,color:r,gradient:o,children:e.jsx(n,{paddingVertical:"medium",children:e.jsx(n.Cell,{colspan:"all",children:e.jsxs(l,{gap:"medium",children:[e.jsx(t,{ratio:"wide",children:e.jsx(i,{alt:"",src:b,cover:!0})}),e.jsx(c,{size:"small",inverseColor:!r||!["green","yellow"].includes(r),children:d()})]})})})})},y={parameters:{layout:"fullscreen"},args:{color:"blue",gradient:!0},render:({as:a,color:r,gradient:o})=>e.jsx(s,{as:a,color:r,gradient:o,children:e.jsx(n,{paddingVertical:"medium",children:e.jsx(n.Cell,{colspan:"all",children:e.jsxs(l,{gap:"medium",children:[e.jsx(t,{ratio:"x-wide",children:e.jsx(i,{alt:"",src:b,cover:!0})}),e.jsx(c,{size:"small",inverseColor:!r||!["green","yellow"].includes(r),children:d()})]})})})})},A={parameters:{layout:"fullscreen"},args:{color:"blue",gradient:!0},render:({as:a,color:r,gradient:o})=>e.jsx(s,{as:a,color:r,gradient:o,children:e.jsx(n,{paddingVertical:"medium",children:e.jsx(n.Cell,{colspan:"all",children:e.jsxs(l,{gap:"medium",children:[e.jsx(t,{ratio:"2x-wide",children:e.jsx(i,{alt:"",src:b,cover:!0})}),e.jsx(c,{size:"small",inverseColor:!r||!["green","yellow"].includes(r),children:d()})]})})})})},P={parameters:{layout:"fullscreen"},args:{color:"blue",gradient:!0},render:({as:a,color:r,gradient:o})=>e.jsx(s,{as:a,color:r,gradient:o,children:e.jsxs(n,{paddingBottom:"large",rows:2,children:[e.jsx(n.Cell,{start:{narrow:1,medium:1,wide:2},colspan:{narrow:4,medium:4,wide:5},row:{narrow:2,medium:2,wide:2},rowspan:{narrow:1,medium:1,wide:1},children:e.jsxs(l,{gap:"medium",children:[e.jsx(Pe,{level:2,inverseColor:!0,children:"Projecten op de kaart"}),e.jsx(c,{inverseColor:!0,children:"Overzicht van ruim 200 projecten in Amsterdam. Lees waar en wanneer we werken aan nieuwbouw, groot onderhoud, herinrichting van straten en wegen, aanpak van parken of ontwikkeling van hele gebieden. En hoe u kunt meedenken. Ga naar de kaart om een project in de stad te zien."}),e.jsx(be,{variant:"standalone",href:"#",onClick:Ae=>Ae.preventDefault,onBackground:"dark",children:"Bekijk de projectenkaart"})]})}),e.jsx(n.Cell,{colspan:{narrow:4,medium:4,wide:5},row:{narrow:1,medium:1,wide:1},rowspan:{narrow:1,medium:2,wide:2},children:e.jsx(t,{ratio:"tall",children:e.jsx(i,{alt:"",src:"https://picsum.photos/1200/1600",cover:!0,style:{width:"100%",height:"100%"}})})})]})})};var z,R,B;p.parameters={...p.parameters,docs:{...(z=p.parameters)==null?void 0:z.docs,source:{originalSource:"{}",...(B=(R=p.parameters)==null?void 0:R.docs)==null?void 0:B.source}}};var V,T,I;m.parameters={...m.parameters,docs:{...(V=m.parameters)==null?void 0:V.docs,source:{originalSource:`{
+  args: {
+    color: 'blue'
+  }
+}`,...(I=(T=m.parameters)==null?void 0:T.docs)==null?void 0:I.source}}};var W,L,O;u.parameters={...u.parameters,docs:{...(W=u.parameters)==null?void 0:W.docs,source:{originalSource:`{
+  args: {
+    color: 'light-blue'
+  }
+}`,...(O=(L=u.parameters)==null?void 0:L.docs)==null?void 0:O.source}}};var D,q,X;g.parameters={...g.parameters,docs:{...(D=g.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  args: {
+    color: 'dark-green'
+  }
+}`,...(X=(q=g.parameters)==null?void 0:q.docs)==null?void 0:X.source}}};var _,E,H;h.parameters={...h.parameters,docs:{...(_=h.parameters)==null?void 0:_.docs,source:{originalSource:`{
+  args: {
+    color: 'green'
+  }
+}`,...(H=(E=h.parameters)==null?void 0:E.docs)==null?void 0:H.source}}};var M,Y,N;w.parameters={...w.parameters,docs:{...(M=w.parameters)==null?void 0:M.docs,source:{originalSource:`{
+  args: {
+    color: 'magenta'
+  }
+}`,...(N=(Y=w.parameters)==null?void 0:Y.docs)==null?void 0:N.source}}};var Q,F,J;x.parameters={...x.parameters,docs:{...(Q=x.parameters)==null?void 0:Q.docs,source:{originalSource:`{
+  args: {
+    color: 'orange'
+  }
+}`,...(J=(F=x.parameters)==null?void 0:F.docs)==null?void 0:J.source}}};var K,U,Z;j.parameters={...j.parameters,docs:{...(K=j.parameters)==null?void 0:K.docs,source:{originalSource:`{
+  args: {
+    color: 'purple'
+  }
+}`,...(Z=(U=j.parameters)==null?void 0:U.docs)==null?void 0:Z.source}}};var $,ee,re;C.parameters={...C.parameters,docs:{...($=C.parameters)==null?void 0:$.docs,source:{originalSource:`{
+  args: {
+    color: 'yellow'
+  }
+}`,...(re=(ee=C.parameters)==null?void 0:ee.docs)==null?void 0:re.source}}};var ae,ne,oe;G.parameters={...G.parameters,docs:{...(ae=G.parameters)==null?void 0:ae.docs,source:{originalSource:`{
+  args: {
+    as: 'section'
+  }
+}`,...(oe=(ne=G.parameters)==null?void 0:ne.docs)==null?void 0:oe.source}}};var se,le,te;v.parameters={...v.parameters,docs:{...(se=v.parameters)==null?void 0:se.docs,source:{originalSource:`{
+  parameters: {
+    layout: 'fullscreen'
+  },
+  args: {
+    color: 'blue',
+    gradient: true
+  },
+  render: ({
+    as,
+    color,
+    gradient
+  }) => <Spotlight as={as} color={color} gradient={gradient}>
+      <Grid paddingVertical="medium">
+        <Grid.Cell colspan="all">
+          <Column gap="medium">
+            <AspectRatio ratio="x-tall">
+              <Image alt="" src="https://picsum.photos/1440/2560" cover />
+            </AspectRatio>
+            <Paragraph size="small" inverseColor={!color || !['green', 'yellow'].includes(color)}>
+              {exampleParagraph()}
+            </Paragraph>
+          </Column>
+        </Grid.Cell>
+      </Grid>
+    </Spotlight>
+}`,...(te=(le=v.parameters)==null?void 0:le.docs)==null?void 0:te.source}}};var ie,ce,de;S.parameters={...S.parameters,docs:{...(ie=S.parameters)==null?void 0:ie.docs,source:{originalSource:`{
+  parameters: {
+    layout: 'fullscreen'
+  },
+  args: {
+    color: 'blue',
+    gradient: true
+  },
+  render: ({
+    as,
+    color,
+    gradient
+  }) => <Spotlight as={as} color={color} gradient={gradient}>
+      <Grid paddingVertical="medium">
+        <Grid.Cell colspan="all">
+          <Column gap="medium">
+            <AspectRatio ratio="tall">
+              <Image alt="" src="https://picsum.photos/1200/1600" cover />
+            </AspectRatio>
+            <Paragraph size="small" inverseColor={!color || !['green', 'yellow'].includes(color)}>
+              {exampleParagraph()}
+            </Paragraph>
+          </Column>
+        </Grid.Cell>
+      </Grid>
+    </Spotlight>
+}`,...(de=(ce=S.parameters)==null?void 0:ce.docs)==null?void 0:de.source}}};var pe,me,ue;k.parameters={...k.parameters,docs:{...(pe=k.parameters)==null?void 0:pe.docs,source:{originalSource:`{
+  parameters: {
+    layout: 'fullscreen'
+  },
+  args: {
+    color: 'blue',
+    gradient: true
+  },
+  render: ({
+    as,
+    color,
+    gradient
+  }) => <Spotlight as={as} color={color} gradient={gradient}>
+      <Grid paddingVertical="medium">
+        <Grid.Cell colspan="all">
+          <Column gap="medium">
+            <AspectRatio ratio="square">
+              <Image alt="" src={image} cover />
+            </AspectRatio>
+            <Paragraph size="small" inverseColor={!color || !['green', 'yellow'].includes(color)}>
+              {exampleParagraph()}
+            </Paragraph>
+          </Column>
+        </Grid.Cell>
+      </Grid>
+    </Spotlight>
+}`,...(ue=(me=k.parameters)==null?void 0:me.docs)==null?void 0:ue.source}}};var ge,he,we;f.parameters={...f.parameters,docs:{...(ge=f.parameters)==null?void 0:ge.docs,source:{originalSource:`{
+  parameters: {
+    layout: 'fullscreen'
+  },
+  args: {
+    color: 'blue',
+    gradient: true
+  },
+  render: ({
+    as,
+    color,
+    gradient
+  }) => <Spotlight as={as} color={color} gradient={gradient}>
+      <Grid paddingVertical="medium">
+        <Grid.Cell colspan="all">
+          <Column gap="medium">
+            <AspectRatio ratio="wide">
+              <Image alt="" src={image} cover />
+            </AspectRatio>
+            <Paragraph size="small" inverseColor={!color || !['green', 'yellow'].includes(color)}>
+              {exampleParagraph()}
+            </Paragraph>
+          </Column>
+        </Grid.Cell>
+      </Grid>
+    </Spotlight>
+}`,...(we=(he=f.parameters)==null?void 0:he.docs)==null?void 0:we.source}}};var xe,je,Ce;y.parameters={...y.parameters,docs:{...(xe=y.parameters)==null?void 0:xe.docs,source:{originalSource:`{
+  parameters: {
+    layout: 'fullscreen'
+  },
+  args: {
+    color: 'blue',
+    gradient: true
+  },
+  render: ({
+    as,
+    color,
+    gradient
+  }) => <Spotlight as={as} color={color} gradient={gradient}>
+      <Grid paddingVertical="medium">
+        <Grid.Cell colspan="all">
+          <Column gap="medium">
+            <AspectRatio ratio="x-wide">
+              <Image alt="" src={image} cover />
+            </AspectRatio>
+            <Paragraph size="small" inverseColor={!color || !['green', 'yellow'].includes(color)}>
+              {exampleParagraph()}
+            </Paragraph>
+          </Column>
+        </Grid.Cell>
+      </Grid>
+    </Spotlight>
+}`,...(Ce=(je=y.parameters)==null?void 0:je.docs)==null?void 0:Ce.source}}};var Ge,ve,Se;A.parameters={...A.parameters,docs:{...(Ge=A.parameters)==null?void 0:Ge.docs,source:{originalSource:`{
+  parameters: {
+    layout: 'fullscreen'
+  },
+  args: {
+    color: 'blue',
+    gradient: true
+  },
+  render: ({
+    as,
+    color,
+    gradient
+  }) => <Spotlight as={as} color={color} gradient={gradient}>
+      <Grid paddingVertical="medium">
+        <Grid.Cell colspan="all">
+          <Column gap="medium">
+            <AspectRatio ratio="2x-wide">
+              <Image alt="" src={image} cover />
+            </AspectRatio>
+            <Paragraph size="small" inverseColor={!color || !['green', 'yellow'].includes(color)}>
+              {exampleParagraph()}
+            </Paragraph>
+          </Column>
+        </Grid.Cell>
+      </Grid>
+    </Spotlight>
+}`,...(Se=(ve=A.parameters)==null?void 0:ve.docs)==null?void 0:Se.source}}};var ke,fe,ye;P.parameters={...P.parameters,docs:{...(ke=P.parameters)==null?void 0:ke.docs,source:{originalSource:`{
+  parameters: {
+    layout: 'fullscreen'
+  },
+  args: {
+    color: 'blue',
+    gradient: true
+  },
+  render: ({
+    as,
+    color,
+    gradient
+  }) => <Spotlight as={as} color={color} gradient={gradient}>
+      <Grid paddingBottom="large" rows={2}>
+        <Grid.Cell start={{
+        narrow: 1,
+        medium: 1,
+        wide: 2
+      }} colspan={{
+        narrow: 4,
+        medium: 4,
+        wide: 5
+      }} row={{
+        narrow: 2,
+        medium: 2,
+        wide: 2
+      }} rowspan={{
+        narrow: 1,
+        medium: 1,
+        wide: 1
+      }}>
+          <Column gap="medium">
+            <Heading level={2} inverseColor>
+              Projecten op de kaart
+            </Heading>
+            <Paragraph inverseColor>
+              Overzicht van ruim 200 projecten in Amsterdam. Lees waar en wanneer we werken aan nieuwbouw, groot
+              onderhoud, herinrichting van straten en wegen, aanpak van parken of ontwikkeling van hele gebieden. En hoe
+              u kunt meedenken. Ga naar de kaart om een project in de stad te zien.
+            </Paragraph>
+            <Link variant="standalone" href="#" onClick={e => e.preventDefault} onBackground="dark">
+              Bekijk de projectenkaart
+            </Link>
+          </Column>
+        </Grid.Cell>
+        <Grid.Cell colspan={{
+        narrow: 4,
+        medium: 4,
+        wide: 5
+      }} row={{
+        narrow: 1,
+        medium: 1,
+        wide: 1
+      }} rowspan={{
+        narrow: 1,
+        medium: 2,
+        wide: 2
+      }}>
+          <AspectRatio ratio="tall">
+            <Image alt="" src="https://picsum.photos/1200/1600" cover style={{
+            width: '100%',
+            height: '100%'
+          }} />
+          </AspectRatio>
+        </Grid.Cell>
+      </Grid>
+    </Spotlight>
+}`,...(ye=(fe=P.parameters)==null?void 0:fe.docs)==null?void 0:ye.source}}};const Ie=["Default","Blue","LightBlue","DarkGreen","Green","Magenta","Orange","Purple","Yellow","CustomTagName","GradientXTallAspect","GradientTallAspect","GradientSquareAspect","GradientWideAspect","GradientXWideAspect","Gradient2XWideAspect","GradientTallAspectWithText"],Ne=Object.freeze(Object.defineProperty({__proto__:null,Blue:m,CustomTagName:G,DarkGreen:g,Default:p,Gradient2XWideAspect:A,GradientSquareAspect:k,GradientTallAspect:S,GradientTallAspectWithText:P,GradientWideAspect:f,GradientXTallAspect:v,GradientXWideAspect:y,Green:h,LightBlue:u,Magenta:w,Orange:x,Purple:j,Yellow:C,__namedExportsOrder:Ie,default:Te},Symbol.toStringTag,{value:"Module"}));export{m as B,G as C,g as D,h as G,u as L,w as M,x as O,j as P,Ne as S,C as Y};
