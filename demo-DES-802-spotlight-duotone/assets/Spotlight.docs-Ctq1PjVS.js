880f672d4b7cbea6a359eb68389cbb1ecb8354ca
dlnr <niels@dlnr.dev>
2024-07-03 09:37:38 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@f139ef0f892a01e3534c057f1674d79701192093 🚀

---
+++ demo-DES-802-spotlight-duotone/assets/Spotlight.docs-Ctq1PjVS.js
@@ -0,0 +1,43 @@
+import{j as e}from"./jsx-runtime-X2b_N9AH.js";import{useMDXComponents as s}from"./index-CRbi4mQH.js";import{M as i,d as a,P as l,e as m,f as t}from"./index-CWP9CAOk.js";import{S as c,B as h,L as d,D as p,G as u,M as x,O as j,P as g,Y as f,C as w}from"./Spotlight.stories-w6png5mg.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-fusLMWvT.js";import"../sb-preview/runtime.js";import"./index-BOkhicXD.js";import"./index-DXimoRZY.js";import"./index-B8K4vdXH.js";import"./index-DrFu-skq.js";import"./index.esm-ClG5nKvY.js";import"./clsx-B-dksMZM.js";import"./index.esm-Dr6r5gIo.js";import"./exampleContent-BEbrefRz.js";import"./Spotlight-Cn7b3okT.js";import"./Grid-BwoxiRyw.js";import"./Blockquote-7VFIi5C-.js";import"./Screen-nsUNRErq.js";import"./Column-BRryplre.js";import"./AspectRatio-CfnkH-uR.js";import"./Image-Bf5zYcta.js";import"./Paragraph-DpVlyIEJ.js";const C=`<!-- @license CC0-1.0 -->
+
+# Spotlight
+
+Emphasizes a section on a page through a distinctive background colour.
+
+## Guidelines
+
+Refer to [this overview on Stijlweb](https://amsterdam.nl/stijlweb/basiselementen/kleuren/#PagCls_15671872) to determine whether you can use black or white text on the background colour of your choice.
+
+## Relevant WCAG Requirements
+
+- [WCAG 1.4.3](https://www.w3.org/TR/WCAG21/#contrast-minimum): Large-scale text and images of large-scale text have a contrast ratio of at least 3:1;
+
+## References
+
+- [Color and contrast accessibility](https://web.dev/articles/color-and-contrast-accessibility)
+`;/*@license CC0-1.0*/function r(o){const n={code:"code",h3:"h3",p:"p",...s(),...o.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(i,{of:c}),`
+`,e.jsx(a,{children:C}),`
+`,e.jsx(l,{}),`
+`,e.jsx(m,{}),`
+`,e.jsx(n.h3,{id:"blue",children:"Blue"}),`
+`,e.jsx(t,{of:h}),`
+`,e.jsx(n.h3,{id:"light-blue",children:"Light Blue"}),`
+`,e.jsx(t,{of:d}),`
+`,e.jsx(n.h3,{id:"dark-green",children:"Dark Green"}),`
+`,e.jsx(t,{of:p}),`
+`,e.jsx(n.h3,{id:"green",children:"Green"}),`
+`,e.jsx(t,{of:u}),`
+`,e.jsx(n.h3,{id:"magenta",children:"Magenta"}),`
+`,e.jsx(t,{of:x}),`
+`,e.jsx(n.h3,{id:"orange",children:"Orange"}),`
+`,e.jsx(t,{of:j}),`
+`,e.jsx(n.h3,{id:"purple",children:"Purple"}),`
+`,e.jsx(t,{of:g}),`
+`,e.jsx(n.h3,{id:"yellow",children:"Yellow"}),`
+`,e.jsx(t,{of:f}),`
+`,e.jsx(n.h3,{id:"custom-html-element",children:"Custom HTML Element"}),`
+`,e.jsxs(n.p,{children:["By default, a Spotlight renders a ",e.jsx(n.code,{children:"<div>"}),`.
+Use the `,e.jsx(n.code,{children:"as"})," prop to make your markup more semantic."]}),`
+`,e.jsx(t,{of:w})]})}function N(o={}){const{wrapper:n}={...s(),...o.components};return n?e.jsx(n,{...o,children:e.jsx(r,{...o})}):r(o)}export{N as default};
