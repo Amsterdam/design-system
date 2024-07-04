8492370208df0267d15e38631d3370da52da20e0
Vincent Smedinga <v.smedinga@amsterdam.nl>
2024-07-04 14:49:21 +0200

    chore: Extract common meta configuration for pages (#1322)

---
+++ storybook/src/pages/amsterdam/common/config.ts
@@ -0,0 +1,11 @@
+export const commonMeta = {
+  argTypes: {
+    footer: {
+      table: { disable: true },
+    },
+  },
+  parameters: {
+    layout: 'fullscreen',
+    themes: { themeOverride: 'Spacious' },
+  },
+}
