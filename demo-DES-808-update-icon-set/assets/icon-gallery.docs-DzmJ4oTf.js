ccf0a759a56f5c2eb6e2fd711e6d06b29dde4585
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-07-04 12:06:23 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@d18d6baf9c40387569459738a2bac449e8b34fb6 🚀

---
+++ demo-DES-808-update-icon-set/assets/icon-gallery.docs-DzmJ4oTf.js
@@ -0,0 +1,11 @@
+import{j as n}from"./jsx-runtime-X2b_N9AH.js";import{useMDXComponents as r}from"./index-CRbi4mQH.js";import{M as i}from"./index-DasngWyu.js";import{I as t}from"./index.esm-BPfhnSlj.js";import{I as c}from"./Icon-j44OzEOs.js";import{S as a}from"./StatusBadge-CybIRuV-.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-LevxW-3W.js";import"../sb-preview/runtime.js";import"./index-BOkhicXD.js";import"./index-DXimoRZY.js";import"./index-B8K4vdXH.js";import"./index-DrFu-skq.js";import"./clsx-B-dksMZM.js";import"./Badge-BnLGL1Dc.js";const l=()=>{const s=Object.keys(t);return n.jsx("div",{className:"ams-storybook-icon-gallery",children:s.filter(o=>o.startsWith("New")).map(o=>n.jsxs("div",{className:"ams-storybook-icon-gallery__item",children:[n.jsx(c,{svg:t[o]}),n.jsx("span",{children:o.substring(3,o.length-8)})]},o))})};/*@license CC0-1.0*/function e(s){const o={a:"a",h1:"h1",h2:"h2",li:"li",p:"p",ul:"ul",...r(),...s.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(i,{title:"Docs/Assets/Icons"}),`
+`,n.jsx(a,{reason:"The set of available icons will change."}),`
+`,n.jsx(o.h1,{id:"icons",children:"Icons"}),`
+`,n.jsx(o.p,{children:"An overview of all currently available icons."}),`
+`,n.jsx(l,{}),`
+`,n.jsx(o.h2,{id:"related-components",children:"Related components"}),`
+`,n.jsxs(o.ul,{children:[`
+`,n.jsx(o.li,{children:n.jsx(o.a,{href:"/docs/components-media-icon--docs",children:"Icon"})}),`
+`]})]})}function _(s={}){const{wrapper:o}={...r(),...s.components};return o?n.jsx(o,{...s,children:n.jsx(e,{...s})}):e(s)}export{_ as default};
