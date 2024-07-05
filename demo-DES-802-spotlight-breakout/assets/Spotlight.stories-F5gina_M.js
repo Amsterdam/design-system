b9e0fc387e12eeb4feb47da5d90c74d8da73f413
dlnr <niels@dlnr.dev>
2024-07-05 12:36:22 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@b1cf0cf4c243b06315789b73288801e02d9a3586 🚀

---
+++ demo-DES-802-spotlight-breakout/assets/Spotlight.stories-F5gina_M.js
@@ -0,0 +1,71 @@
+import{j as r}from"./jsx-runtime-X2b_N9AH.js";import{a as W,C as X,A as Z,I as $,P as rr}from"./index.esm-Bj1eszD8.js";import{e as er,b as ar}from"./exampleContent-BEbrefRz.js";import{S as h}from"./Spotlight-tJBvWjkL.js";import{G as u}from"./Grid-BRQJwiXa.js";import{B as or}from"./Blockquote-7VFIi5C-.js";const sr=er(),nr={title:"Components/Containers/Spotlight",component:h,render:({as:g,color:e})=>r.jsx(h,{as:g,color:e,children:r.jsx(u,{paddingVertical:"medium",children:r.jsx(u.Cell,{span:"all",children:r.jsx(or,{inverseColor:!e||!["green","yellow"].includes(e),children:sr})})})})},a={},o={args:{color:"blue"}},s={args:{color:"dark-blue"}},n={args:{color:"dark-green"}},t={args:{color:"green"}},c={args:{color:"magenta"}},l={args:{color:"orange"}},m={args:{color:"purple"}},i={args:{color:"yellow"}},p={args:{as:"section"}},d={args:{color:"dark-blue",breakout:!0},render:({as:g,color:e,breakout:U})=>r.jsx(W,{children:r.jsx(h,{as:g,color:e,breakout:U,children:r.jsx(u,{paddingVertical:"medium",children:r.jsx(u.Cell,{start:{narrow:1,medium:2,wide:2},span:{narrow:4,medium:6,wide:10},children:r.jsxs(X,{gap:"medium",children:[r.jsx(Z,{ratio:"wide",children:r.jsx($,{src:"https://picsum.photos/960/720",alt:"",cover:!0,style:{width:"100%",height:"100%"}})}),r.jsx(rr,{inverseColor:!0,children:ar()})]})})})})})};var S,k,x;a.parameters={...a.parameters,docs:{...(S=a.parameters)==null?void 0:S.docs,source:{originalSource:"{}",...(x=(k=a.parameters)==null?void 0:k.docs)==null?void 0:x.source}}};var w,j,C;o.parameters={...o.parameters,docs:{...(w=o.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  args: {
+    color: 'blue'
+  }
+}`,...(C=(j=o.parameters)==null?void 0:j.docs)==null?void 0:C.source}}};var b,f,B;s.parameters={...s.parameters,docs:{...(b=s.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    color: 'dark-blue'
+  }
+}`,...(B=(f=s.parameters)==null?void 0:f.docs)==null?void 0:B.source}}};var G,P,D;n.parameters={...n.parameters,docs:{...(G=n.parameters)==null?void 0:G.docs,source:{originalSource:`{
+  args: {
+    color: 'dark-green'
+  }
+}`,...(D=(P=n.parameters)==null?void 0:P.docs)==null?void 0:D.source}}};var y,v,O;t.parameters={...t.parameters,docs:{...(y=t.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    color: 'green'
+  }
+}`,...(O=(v=t.parameters)==null?void 0:v.docs)==null?void 0:O.source}}};var _,A,M;c.parameters={...c.parameters,docs:{...(_=c.parameters)==null?void 0:_.docs,source:{originalSource:`{
+  args: {
+    color: 'magenta'
+  }
+}`,...(M=(A=c.parameters)==null?void 0:A.docs)==null?void 0:M.source}}};var R,I,T;l.parameters={...l.parameters,docs:{...(R=l.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  args: {
+    color: 'orange'
+  }
+}`,...(T=(I=l.parameters)==null?void 0:I.docs)==null?void 0:T.source}}};var V,Y,q;m.parameters={...m.parameters,docs:{...(V=m.parameters)==null?void 0:V.docs,source:{originalSource:`{
+  args: {
+    color: 'purple'
+  }
+}`,...(q=(Y=m.parameters)==null?void 0:Y.docs)==null?void 0:q.source}}};var E,N,z;i.parameters={...i.parameters,docs:{...(E=i.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  args: {
+    color: 'yellow'
+  }
+}`,...(z=(N=i.parameters)==null?void 0:N.docs)==null?void 0:z.source}}};var Q,F,H;p.parameters={...p.parameters,docs:{...(Q=p.parameters)==null?void 0:Q.docs,source:{originalSource:`{
+  args: {
+    as: 'section'
+  }
+}`,...(H=(F=p.parameters)==null?void 0:F.docs)==null?void 0:H.source}}};var J,K,L;d.parameters={...d.parameters,docs:{...(J=d.parameters)==null?void 0:J.docs,source:{originalSource:`{
+  args: {
+    color: 'dark-blue',
+    breakout: true
+  },
+  render: ({
+    as,
+    color,
+    breakout
+  }) => <Screen>
+      <Spotlight as={as} color={color} breakout={breakout}>
+        <Grid paddingVertical="medium">
+          <Grid.Cell start={{
+          narrow: 1,
+          medium: 2,
+          wide: 2
+        }} span={{
+          narrow: 4,
+          medium: 6,
+          wide: 10
+        }}>
+            <Column gap="medium">
+              <AspectRatio ratio="wide">
+                <Image src="https://picsum.photos/960/720" alt="" cover style={{
+                width: '100%',
+                height: '100%'
+              }} />
+              </AspectRatio>
+              <Paragraph inverseColor>{exampleParagraph()}</Paragraph>
+            </Column>
+          </Grid.Cell>
+        </Grid>
+      </Spotlight>
+    </Screen>
+}`,...(L=(K=d.parameters)==null?void 0:K.docs)==null?void 0:L.source}}};const tr=["Default","Blue","DarkBlue","DarkGreen","Green","Magenta","Orange","Purple","Yellow","CustomTagName","DarkBlueBreakout"],ur=Object.freeze(Object.defineProperty({__proto__:null,Blue:o,CustomTagName:p,DarkBlue:s,DarkBlueBreakout:d,DarkGreen:n,Default:a,Green:t,Magenta:c,Orange:l,Purple:m,Yellow:i,__namedExportsOrder:tr,default:nr},Symbol.toStringTag,{value:"Module"}));export{o as B,p as C,s as D,t as G,c as M,l as O,m as P,ur as S,i as Y,n as a};
---
+++ demo-DES-802-spotlight-breakout/assets/WithTooltip-KJL26V4Q-B-DTGD3R.js
@@ -0,0 +1,1 @@
+import{W as h,W as l,b as s}from"./index-C7p5P3iC.js";import"./iframe-Cu4WzCur.js";import"../sb-preview/runtime.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-BOkhicXD.js";import"./index-DXimoRZY.js";import"./index-B8K4vdXH.js";import"./index-DrFu-skq.js";export{h as WithToolTipState,l as WithTooltip,s as WithTooltipPure};
