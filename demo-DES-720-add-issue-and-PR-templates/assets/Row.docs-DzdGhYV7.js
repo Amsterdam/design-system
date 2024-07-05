0c1dd911e1c93efd2732ade9c15096b10adcb9fb
alimpens <37216945+alimpens@users.noreply.github.com>
2024-07-05 14:55:51 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@8b07a03a21716eb1e226769b361385dc3c36813b 🚀

---
+++ demo-DES-720-add-issue-and-PR-templates/assets/Row.docs-DzdGhYV7.js
@@ -0,0 +1,18 @@
+import{j as e}from"./jsx-runtime-X2b_N9AH.js";import{useMDXComponents as s}from"./index-CRbi4mQH.js";import{M as i,d as r,P as a,e as p,f as m}from"./index-CQzUsdl8.js";import{R as h,W as d}from"./Row.stories-2_jdmU1L.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-CbXJy8Sf.js";import"../sb-preview/runtime.js";import"./index-BOkhicXD.js";import"./index-DXimoRZY.js";import"./index-B8K4vdXH.js";import"./index-DrFu-skq.js";import"./clsx-B-dksMZM.js";import"./Button-B8AXuJtG.js";import"./Paragraph-DpVlyIEJ.js";const l=`<!-- @license CC0-1.0 -->
+
+# Row
+`;/*@license CC0-1.0*/function t(o){const n={a:"a",h2:"h2",h3:"h3",p:"p",...s(),...o.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(i,{of:h}),`
+`,e.jsx(r,{children:l}),`
+`,e.jsx(n.h2,{id:"design",children:"Design"}),`
+`,e.jsxs(n.p,{children:["The five ",e.jsx(n.a,{href:"/docs/docs-design-guidelines-space--docs",children:"space"})," sizes are available for the width of the gap."]}),`
+`,e.jsx(n.h2,{id:"how-to-use",children:"How to Use"}),`
+`,e.jsx(n.p,{children:"Wrap a Row around a set of components that need the same amount of white space between them."}),`
+`,e.jsx(n.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(n.h3,{id:"default",children:"Default"}),`
+`,e.jsx(a,{}),`
+`,e.jsx(p,{}),`
+`,e.jsx(n.h3,{id:"wrapping",children:"Wrapping"}),`
+`,e.jsx(n.p,{children:"Items that may not fit together on a single line should be allowed to wrap onto multiple lines."}),`
+`,e.jsx(m,{of:d})]})}function y(o={}){const{wrapper:n}={...s(),...o.components};return n?e.jsx(n,{...o,children:e.jsx(t,{...o})}):t(o)}export{y as default};
---
+++ demo-DES-720-add-issue-and-PR-templates/assets/SearchField.stories-CPZvypZq.js
@@ -0,0 +1,34 @@
+import{j as a}from"./jsx-runtime-X2b_N9AH.js";import{S as e}from"./SearchField-BywiADXF.js";const{useArgs:C}=__STORYBOOK_MODULE_PREVIEW_API__,j={title:"Components/Forms/Search Field",component:e,args:{invalid:!1,onSubmit:o=>{o.preventDefault()}},argTypes:{label:{description:"Describes the field for screen readers."},onSubmit:{table:{disable:!0}},placeholder:{description:"Displayed while the field is empty."}},render:({invalid:o,label:l,placeholder:c,...u})=>a.jsxs(e,{...u,children:[a.jsx(e.Input,{invalid:o,label:l,placeholder:c}),a.jsx(e.Button,{})]})},n={},r={args:{placeholder:"Wat kunnen we voor u vinden?"}},t={args:{value:""},render:function({invalid:l,label:c,placeholder:u}){const[x,D]=C(),_=s=>{D({value:s.target.value})};return a.jsxs(e,{onSubmit:s=>{s.preventDefault();const i=new FormData(s.currentTarget).get("search-box");i&&alert(`Gezocht op '${i}'`)},children:[a.jsx(e.Input,{name:"search-box",value:x.value,invalid:l,label:c,onChange:_,placeholder:u}),a.jsx(e.Button,{})]})}};var d,p,m;n.parameters={...n.parameters,docs:{...(d=n.parameters)==null?void 0:d.docs,source:{originalSource:"{}",...(m=(p=n.parameters)==null?void 0:p.docs)==null?void 0:m.source}}};var h,g,v;r.parameters={...r.parameters,docs:{...(h=r.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    placeholder: 'Wat kunnen we voor u vinden?'
+  }
+}`,...(v=(g=r.parameters)==null?void 0:g.docs)==null?void 0:v.source}}};var f,b,S;t.parameters={...t.parameters,docs:{...(f=t.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    value: ''
+  },
+  render: function Component({
+    invalid,
+    label,
+    placeholder
+  }: InputProps) {
+    const [args, setArgs] = useArgs();
+    const onValueChange = (event: any) => {
+      setArgs({
+        value: event.target.value
+      });
+    };
+    return <SearchField onSubmit={e => {
+      e.preventDefault();
+      const formData = new FormData(e.currentTarget);
+      const value = formData.get('search-box');
+      // search actions should not be triggered without a value
+      if (value) {
+        // eslint-disable-next-line no-alert
+        alert(\`Gezocht op '\${value}'\`);
+      }
+    }}>
+        <SearchField.Input name="search-box" value={args['value']} invalid={invalid} label={label} onChange={onValueChange} placeholder={placeholder} />
+        <SearchField.Button />
+      </SearchField>;
+  }
+}`,...(S=(b=t.parameters)==null?void 0:b.docs)==null?void 0:S.source}}};const F=["Default","WithPlaceholder","Controlled"],A=Object.freeze(Object.defineProperty({__proto__:null,Controlled:t,Default:n,WithPlaceholder:r,__namedExportsOrder:F,default:j},Symbol.toStringTag,{value:"Module"}));export{t as C,A as S,r as W};
