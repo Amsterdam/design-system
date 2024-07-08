f4f67463b264d010bf396707b40ec234348588b0
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-07-08 10:37:30 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@1ad22f114a4578c45305e3834e13d83765bda791 🚀

---
+++ demo-row-alignment/assets/OrderedList.docs-CJHLiqrl.js
@@ -0,0 +1,47 @@
+import{j as e}from"./jsx-runtime-X2b_N9AH.js";import{useMDXComponents as i}from"./index-CRbi4mQH.js";import{M as o,d as a,P as h,e as l,f as r}from"./index-C_KQBeoz.js";import{O as d,S as c,T as m,a as p,D as x,W as u,I as j}from"./OrderedList.stories-jRuQJtnR.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-CJa-9CP0.js";import"../sb-preview/runtime.js";import"./index-BOkhicXD.js";import"./index-DXimoRZY.js";import"./index-B8K4vdXH.js";import"./index-DrFu-skq.js";import"./decorators-CMqafKrE.js";import"./exampleContent-BEbrefRz.js";import"./clsx-B-dksMZM.js";import"./Heading-CGpdaMNv.js";import"./getHeadingElement-DiRwFEz7.js";import"./Paragraph-DpVlyIEJ.js";const f=`<!-- @license CC0-1.0 -->
+
+# Ordered List
+
+An ordered list is a vertical group of related content with an order.
+This list can have 2 levels.
+The first level has numbers as list markers.
+The second level uses letters in alphabetical order.
+List items indent their text by a fixed distance.
+`;/*@license CC0-1.0*/function t(n){const s={code:"code",h2:"h2",h3:"h3",p:"p",...i(),...n.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(o,{of:d}),`
+`,e.jsx(a,{children:f}),`
+`,e.jsx(s.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(s.h3,{id:"default",children:"Default"}),`
+`,e.jsx(s.p,{children:`The list uses ascending numbers as markers, providing enough space for numbers up to 99.
+Extra white space between items enhances the distinction, mainly when they consist of multiple lines of text.`}),`
+`,e.jsx(h,{}),`
+`,e.jsx(l,{}),`
+`,e.jsx(s.h2,{id:"small",children:"Small"}),`
+`,e.jsx(s.p,{children:"Use a list with a smaller font size in form descriptions and captions and the like."}),`
+`,e.jsx(r,{of:c}),`
+`,e.jsx(s.h3,{id:"two-levels",children:"Two Levels"}),`
+`,e.jsx(s.p,{children:`A list may have one nested level.
+In this case, lowercase letters are used as markers.`}),`
+`,e.jsx(r,{of:m}),`
+`,e.jsx(s.h3,{id:"starting-number",children:"Starting Number"}),`
+`,e.jsxs(s.p,{children:[`When necessary, the list can start with a higher number.
+Use the HTML attribute `,e.jsx(s.code,{children:"start"}),"."]}),`
+`,e.jsx(r,{of:p}),`
+`,e.jsx(s.h3,{id:"reverse-order",children:"Reverse Order"}),`
+`,e.jsxs(s.p,{children:[`The numbers can also be in reverse order.
+Use the HTML attribute `,e.jsx(s.code,{children:"reversed"}),"."]}),`
+`,e.jsx(r,{of:x}),`
+`,e.jsx(s.h3,{id:"without-markers",children:"Without Markers"}),`
+`,e.jsx(s.p,{children:`An overview of articles or search results can be presented as an ordered can be seen as an unordered list.
+In such cases, apply the list semantics while hiding markers.
+This helps screen readers understand and navigate the list.`}),`
+`,e.jsxs(s.p,{children:[`Note: each item typically contains a container with headings and other text components in this variant.
+Therefore, it does not define typography for the items and the `,e.jsx(s.code,{children:"inverseColor"})," prop has no effect."]}),`
+`,e.jsx(s.p,{children:"For example, here are three news articles:"}),`
+`,e.jsx(r,{of:u}),`
+`,e.jsx(s.h3,{id:"inverse-colour",children:"Inverse Colour"}),`
+`,e.jsxs(s.p,{children:["Set the ",e.jsx(s.code,{children:"inverseColor"}),` prop if the List sits on a dark background.
+This ensures the colour of the text provides enough contrast.
+When nesting lists, set the prop on all lists.`]}),`
+`,e.jsx(r,{of:j})]})}function R(n={}){const{wrapper:s}={...i(),...n.components};return s?e.jsx(s,{...n,children:e.jsx(t,{...n})}):t(n)}export{R as default};
---
+++ demo-row-alignment/assets/Row.stories-DBksfZe6.js
@@ -0,0 +1,18 @@
+import{j as r}from"./jsx-runtime-X2b_N9AH.js";import{c as z}from"./clsx-B-dksMZM.js";import{r as N}from"./index-uCp2LrAq.js";import{B as R}from"./Button-B8AXuJtG.js";import{P as t}from"./Paragraph-DpVlyIEJ.js";const O=["baseline","bottom","center","stretch","top"],P=["around","between","center","end","evenly","start"],i=N.forwardRef(({align:e,alignVertical:l,as:_="div",children:A,className:S,gap:T="medium",wrap:V,...j},k)=>r.jsx(_,{...j,ref:k,className:z("ams-row",`ams-row--${T}`,e&&`ams-row--align-${e}`,l&&`ams-row--align-vertical-${l}`,V&&"ams-row--wrap",S),children:A}));i.displayName="Row";try{i.displayName="Row",i.__docgenInfo={description:"",displayName:"Row",props:{align:{defaultValue:null,description:"The horizontal align of the items in the row.",name:"align",required:!1,type:{name:"string"}},alignVertical:{defaultValue:null,description:"The vertical align of the items in the row.",name:"alignVertical",required:!1,type:{name:"string"}},as:{defaultValue:null,description:"The HTML element to use.",name:"as",required:!1,type:{name:"enum",value:[{value:'"article"'},{value:'"div"'},{value:'"section"'}]}},gap:{defaultValue:{value:"medium"},description:"The amount of vertical space between items.",name:"gap",required:!1,type:{name:"string"}},wrap:{defaultValue:null,description:"Whether items of the row can wrap onto multiple lines.",name:"wrap",required:!1,type:{name:"boolean"}}}}}catch{}const q=Array.from(Array(3).keys()).map(e=>r.jsxs(R,{children:["Button ",e+1]},e)),v=[r.jsx(t,{className:"ams-docs-pink-box",style:{inlineSize:"8rem"},children:"One line"},1),r.jsxs(t,{className:"ams-docs-pink-box",style:{inlineSize:"8rem"},children:["Two",r.jsx("br",{}),"lines"]},2),r.jsx(t,{className:"ams-docs-pink-box",style:{inlineSize:"8rem"},children:"One line"},3)],B={title:"Components/Layout/Row",component:i,args:{children:q},argTypes:{align:{control:"radio",options:P},alignVertical:{control:"radio",options:O},gap:{control:"radio",options:["extra-small","small","medium","large","extra-large"]}}},a={},s={args:{align:"evenly",children:v}},n={args:{alignVertical:"center",children:v}},o={args:{children:Array.from(Array(4).keys()).map(e=>r.jsx(t,{className:"ams-docs-pink-box",style:{inlineSize:"16rem"}},e)),wrap:!0}};var c,m,p;a.parameters={...a.parameters,docs:{...(c=a.parameters)==null?void 0:c.docs,source:{originalSource:"{}",...(p=(m=a.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var d,u,g;s.parameters={...s.parameters,docs:{...(d=s.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    align: 'evenly',
+    children: ThreeParagraphs
+  }
+}`,...(g=(u=s.parameters)==null?void 0:u.docs)==null?void 0:g.source}}};var h,y,f;n.parameters={...n.parameters,docs:{...(h=n.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    alignVertical: 'center',
+    children: ThreeParagraphs
+  }
+}`,...(f=(y=n.parameters)==null?void 0:y.docs)==null?void 0:f.source}}};var w,x,b;o.parameters={...o.parameters,docs:{...(w=o.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  args: {
+    children: Array.from(Array(4).keys()).map(i => <Paragraph className="ams-docs-pink-box" key={i} style={{
+      inlineSize: '16rem'
+    }} />),
+    wrap: true
+  }
+}`,...(b=(x=o.parameters)==null?void 0:x.docs)==null?void 0:b.source}}};const H=["Default","HorizontalAlignment","VerticalAlignment","Wrapping"],M=Object.freeze(Object.defineProperty({__proto__:null,Default:a,HorizontalAlignment:s,VerticalAlignment:n,Wrapping:o,__namedExportsOrder:H,default:B},Symbol.toStringTag,{value:"Module"}));export{s as H,M as R,n as V,o as W};
