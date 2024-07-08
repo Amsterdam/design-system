11fd55bd8a5db87adbf7957befe09352b33af131
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-07-08 10:49:14 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@6c0bc41448072aafd565d1d2744845f0c7d8b207 🚀

---
+++ demo-row-alignment/assets/Row.docs-DEruQpTH.js
@@ -0,0 +1,27 @@
+import{j as e}from"./jsx-runtime-X2b_N9AH.js";import{useMDXComponents as r}from"./index-CRbi4mQH.js";import{M as s,d as a,P as l,e as h,f as o}from"./index-t3nDni2q.js";import{R as d,H as m,V as c,W as p}from"./Row.stories-D4OoTSqy.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-DYNHl3eQ.js";import"../sb-preview/runtime.js";import"./index-BOkhicXD.js";import"./index-DXimoRZY.js";import"./index-B8K4vdXH.js";import"./index-DrFu-skq.js";import"./clsx-B-dksMZM.js";import"./Button-B8AXuJtG.js";import"./Paragraph-DpVlyIEJ.js";const x=`<!-- @license CC0-1.0 -->
+
+# Row
+`;/*@license CC0-1.0*/function i(t){const n={a:"a",h2:"h2",h3:"h3",p:"p",...r(),...t.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(s,{of:d}),`
+`,e.jsx(a,{children:x}),`
+`,e.jsx(n.h2,{id:"design",children:"Design"}),`
+`,e.jsxs(n.p,{children:["The five ",e.jsx(n.a,{href:"/docs/docs-design-guidelines-space--docs",children:"space"})," sizes are available for the width of the gap."]}),`
+`,e.jsx(n.h2,{id:"how-to-use",children:"How to Use"}),`
+`,e.jsx(n.p,{children:"Wrap a Row around a set of components that need the same amount of white space between them."}),`
+`,e.jsx(n.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(n.h3,{id:"default",children:"Default"}),`
+`,e.jsx(l,{}),`
+`,e.jsx(h,{}),`
+`,e.jsx(n.h3,{id:"horizontal-alignment",children:"Horizontal Alignment"}),`
+`,e.jsx(n.p,{children:`Items can be aligned horizontally to the start, center or end of the row.
+Or whitespace can be distributed between, around or evenly around them.`}),`
+`,e.jsx(o,{of:m}),`
+`,e.jsx(n.h3,{id:"vertical-alignment",children:"Vertical Alignment"}),`
+`,e.jsx(n.p,{children:`Items can be aligned vertically to the top, middle, or bottom of the row.
+They can also align to their common text baseline.
+By default, they stretch to fill the height of the row.`}),`
+`,e.jsx(o,{of:c}),`
+`,e.jsx(n.h3,{id:"wrapping",children:"Wrapping"}),`
+`,e.jsx(n.p,{children:"Items that may not fit together on a single line should be allowed to wrap onto multiple lines."}),`
+`,e.jsx(o,{of:p})]})}function H(t={}){const{wrapper:n}={...r(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(i,{...t})}):i(t)}export{H as default};
