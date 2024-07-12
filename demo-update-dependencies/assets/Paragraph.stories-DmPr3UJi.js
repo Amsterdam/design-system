ea953d6515e3d73cfd3037b5ab1fa9c1ff38e271
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-07-12 19:46:29 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@9d1f604eb3ce68a309fba1d63596c2ce19686441 🚀

---
+++ demo-update-dependencies/assets/Paragraph.stories-DmPr3UJi.js
@@ -0,0 +1,16 @@
+import{i as S}from"./decorators-DLPD6TOJ.js";import{b as h}from"./exampleContent-BEbrefRz.js";import{P as v}from"./Paragraph-BWS5kDPY.js";/**
+ * @license EUPL-1.2+
+ * Copyright Gemeente Amsterdam
+ */const b=h(),C={title:"Components/Text/Paragraph",component:v,args:{children:b,inverseColor:!1},argTypes:{children:{description:"The paragraph text.",table:{disable:!1}},size:{control:{type:"radio",labels:{small:"small",undefined:"medium",large:"large"}},options:["small",void 0,"large"]}},decorators:[S]},r={},e={args:{size:"large"}},a={args:{size:"small"}},s={args:{inverseColor:!0}};var o,t,l;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:"{}",...(l=(t=r.parameters)==null?void 0:t.docs)==null?void 0:l.source}}};var n,c,p;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    size: 'large'
+  }
+}`,...(p=(c=e.parameters)==null?void 0:c.docs)==null?void 0:p.source}}};var m,i,d;a.parameters={...a.parameters,docs:{...(m=a.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    size: 'small'
+  }
+}`,...(d=(i=a.parameters)==null?void 0:i.docs)==null?void 0:d.source}}};var g,u,f;s.parameters={...s.parameters,docs:{...(g=s.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    inverseColor: true
+  }
+}`,...(f=(u=s.parameters)==null?void 0:u.docs)==null?void 0:f.source}}};const P=["Default","Large","Small","InvertedColor"],y=Object.freeze(Object.defineProperty({__proto__:null,Default:r,InvertedColor:s,Large:e,Small:a,__namedExportsOrder:P,default:C},Symbol.toStringTag,{value:"Module"}));export{s as I,e as L,y as P,a as S};
