3d29cece46744721f8a08d9a05cd5cce8c4c3e57
GitHub <github-action@users.noreply.github.com>
2024-07-12 10:21:07 +0000

    Remove feature branch folder

--- demo-DES-853-5-application-page-examples/assets/TablePage.docs-BhlX7Tyd.js
+++
@@ -1,5 +0,0 @@
-import{j as t}from"./jsx-runtime-X2b_N9AH.js";import{useMDXComponents as e}from"./index-CRbi4mQH.js";import{M as i}from"./index-B-hFLSCt.js";import{T as m}from"./TablePage.stories-NCA3CHGL.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-CjwyXUW7.js";import"../sb-preview/runtime.js";import"./index-BOkhicXD.js";import"./index-DXimoRZY.js";import"./index-B8K4vdXH.js";import"./index-DrFu-skq.js";import"./index.esm-C_eFfWAb.js";import"./clsx-B-dksMZM.js";import"./index.esm-BmSTY5-j.js";import"./Submenu-CHi8v8Fk.js";/*@license CC0-1.0*/function n(o){const r={h1:"h1",p:"p",...e(),...o.components};return t.jsxs(t.Fragment,{children:[`
-`,`
-`,t.jsx(i,{of:m}),`
-`,t.jsx(r.h1,{id:"table-page",children:"Table Page"}),`
-`,t.jsx(r.p,{children:"Displays items of a collection."})]})}function T(o={}){const{wrapper:r}={...e(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(n,{...o})}):n(o)}export{T as default};
