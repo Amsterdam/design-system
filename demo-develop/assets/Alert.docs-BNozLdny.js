294f05d0add23ccb753f02d56868081fdd36b230
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-07-12 19:41:20 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@a5299dbfb3659bada5565893be63f615b511c0ee 🚀

---
+++ demo-develop/assets/Alert.docs-BNozLdny.js
@@ -0,0 +1,55 @@
+import{j as n}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as s}from"./index-BI1Biiay.js";import{ae as o,ak as a,al as c,am as h,an as i}from"./index-BOw5E_a6.js";import{A as l,W as d,E as m,S as p,I as f,a as u,b as x,c as g}from"./Alert.stories--djTBZ7F.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-C62GhH7Q.js";import"../sb-preview/runtime.js";import"./index-BMdlDBjA.js";import"./index-D-8MO0q_.js";import"./index-DIJ7EKf7.js";import"./index-DrFu-skq.js";import"./Alert-PdZqOsBw.js";import"./index.esm-BdsPla5o.js";import"./clsx-B-dksMZM.js";import"./Icon-msbkstIT.js";import"./Heading-BgL7hu1i.js";import"./getHeadingElement-DiRwFEz7.js";import"./IconButton-DC-mU1yK.js";import"./UnorderedList-B7xFot2K.js";import"./Link-D5rDrOyf.js";import"./Paragraph-BWS5kDPY.js";const j=`<!-- @license CC0-1.0 -->
+
+# Alert
+
+An Alert informs the user of a significant or time-sensitive message without interrupting their task.
+
+## Specifications
+
+There are four types of notifications:
+
+- **Warning** (orange) when action is needed to prevent damage.
+- **Error** (red) indicates an error has occurred.
+- **Confirmation** (green) to reassure that a process is complete.
+- **Notification** (blue) to bring attention to a message.
+
+## Guidelines
+
+- Place an orange Alert directly below the Header for important and urgent information.
+  Examples: system outage or changes in the opening hours of a City Office.
+- Ensure sufficient white space around the Alert.
+  The grid’s white space is a good reference – place the Alert in its own cell.
+- By default, the Alert cannot be closed.
+  This functionality can be added optionally.
+- Optionally, the heading can be omitted.
+`;/*@license CC0-1.0*/function r(t){const e={h2:"h2",h3:"h3",p:"p",...s(),...t.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(o,{of:l}),`
+`,n.jsx(a,{children:j}),`
+`,n.jsx(e.h2,{id:"examples",children:"Examples"}),`
+`,n.jsx(e.p,{children:"The default Alert is a warning."}),`
+`,n.jsx(c,{}),`
+`,n.jsx(h,{}),`
+`,n.jsx(e.h3,{id:"warning",children:"Warning"}),`
+`,n.jsx(e.p,{children:"Display a warning when user action is required."}),`
+`,n.jsx(i,{of:d}),`
+`,n.jsx(e.h3,{id:"error",children:"Error"}),`
+`,n.jsx(e.p,{children:`In case of an error, use the red alert.
+Provide the user with suggestions on alternative ways to achieve the goal.`}),`
+`,n.jsx(i,{of:m}),`
+`,n.jsx(e.h3,{id:"success",children:"Success"}),`
+`,n.jsx(e.p,{children:"Announce the success of a significant action."}),`
+`,n.jsx(i,{of:p}),`
+`,n.jsx(e.h3,{id:"info",children:"Info"}),`
+`,n.jsx(e.p,{children:"An informative message can emphasize matters that are useful to follow."}),`
+`,n.jsx(i,{of:f}),`
+`,n.jsx(e.h3,{id:"with-inline-link",children:"With Inline Link"}),`
+`,n.jsx(e.p,{children:"Include an inline link in the text to guide the user."}),`
+`,n.jsx(i,{of:u}),`
+`,n.jsx(e.h3,{id:"with-list",children:"With List"}),`
+`,n.jsx(e.p,{children:"For clarification, formatted text can be included in the alert."}),`
+`,n.jsx(i,{of:x}),`
+`,n.jsx(e.h3,{id:"without-heading",children:"Without Heading"}),`
+`,n.jsx(e.p,{children:`Sometimes, a heading is unnecessary.
+The icon automatically becomes slightly smaller.`}),`
+`,n.jsx(i,{of:g})]})}function q(t={}){const{wrapper:e}={...s(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(r,{...t})}):r(t)}export{q as default};
