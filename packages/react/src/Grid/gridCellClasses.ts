9c109279e70bd48d8591d71150ad6d20a85ce6a9
Vincent Smedinga <v.smedinga@amsterdam.nl>
2024-07-15 02:04:27 +0200

    Add row span and start props to Grid Cell

--- packages/react/src/Grid/gridCellClasses.ts
+++ packages/react/src/Grid/gridCellClasses.ts
@@ -5,7 +5,12 @@
 
 import type { GridCellProps } from './GridCell'
 
-export const gridCellClasses = (span?: GridCellProps['span'], start?: GridCellProps['start']): string[] => {
+export const gridCellClasses = (
+  rowSpan?: GridCellProps['rowSpan'],
+  rowStart?: GridCellProps['rowStart'],
+  span?: GridCellProps['span'],
+  start?: GridCellProps['start'],
+): string[] => {
   if (!span && !start) {
     return []
   }
@@ -31,6 +36,28 @@ export const gridCellClasses = (span?: GridCellProps['span'], start?: GridCellPr
       `ams-grid__cell--start-${narrow}`,
       `ams-grid__cell--start-${medium}-medium`,
       `ams-grid__cell--start-${wide}-wide`,
+    )
+  }
+
+  if (typeof rowSpan === 'number') {
+    classes.push(`ams-grid__cell--row-span-${rowSpan}`)
+  } else if (rowSpan) {
+    const { narrow, medium, wide } = rowSpan
+    classes.push(
+      `ams-grid__cell--row-span-${narrow}`,
+      `ams-grid__cell--row-span-${medium}-medium`,
+      `ams-grid__cell--row-span-${wide}-wide`,
+    )
+  }
+
+  if (typeof rowStart === 'number') {
+    classes.push(`ams-grid__cell--row-start-${rowStart}`)
+  } else if (rowStart) {
+    const { narrow, medium, wide } = rowStart
+    classes.push(
+      `ams-grid__cell--row-start-${narrow}`,
+      `ams-grid__cell--row-start-${medium}-medium`,
+      `ams-grid__cell--row-start-${wide}-wide`,
     )
   }
 
