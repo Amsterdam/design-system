6eea74cecec49ec5b6b97fd6184220ecf7032cec
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-07-16 17:46:16 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@50b590186617025c00c62edadc9190e70e941724 🚀

---
+++ demo-update-dependencies/assets/Row.docs-Ci2Kbqsv.js
@@ -0,0 +1,18 @@
+import{j as e}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as s}from"./index-BI1Biiay.js";import{ae as i,ak as a,al as r,am as p,an as m}from"./index-DoauO1Zh.js";import{R as h,W as d}from"./Row.stories-BVIghZIy.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-saUknxlC.js";import"../sb-preview/runtime.js";import"./index-BMdlDBjA.js";import"./index-D-8MO0q_.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";import"./clsx-B-dksMZM.js";import"./Button-fZZcNY7w.js";import"./Paragraph-BWS5kDPY.js";const l=`<!-- @license CC0-1.0 -->
+
+# Row
+`;/*@license CC0-1.0*/function t(o){const n={a:"a",h2:"h2",h3:"h3",p:"p",...s(),...o.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(i,{of:h}),`
+`,e.jsx(a,{children:l}),`
+`,e.jsx(n.h2,{id:"design",children:"Design"}),`
+`,e.jsxs(n.p,{children:["The five ",e.jsx(n.a,{href:"/docs/docs-design-guidelines-space--docs",children:"space"})," sizes are available for the width of the gap."]}),`
+`,e.jsx(n.h2,{id:"how-to-use",children:"How to Use"}),`
+`,e.jsx(n.p,{children:"Wrap a Row around a set of components that need the same amount of white space between them."}),`
+`,e.jsx(n.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(n.h3,{id:"default",children:"Default"}),`
+`,e.jsx(r,{}),`
+`,e.jsx(p,{}),`
+`,e.jsx(n.h3,{id:"wrapping",children:"Wrapping"}),`
+`,e.jsx(n.p,{children:"Items that may not fit together on a single line should be allowed to wrap onto multiple lines."}),`
+`,e.jsx(m,{of:d})]})}function y(o={}){const{wrapper:n}={...s(),...o.components};return n?e.jsx(n,{...o,children:e.jsx(t,{...o})}):t(o)}export{y as default};
