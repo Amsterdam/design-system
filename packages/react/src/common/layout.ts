967092e6fb8ab266ee88b2ced49ca7db53be7486
Vincent Smedinga <v.smedinga@amsterdam.nl>
2024-07-17 22:04:48 +0200

    Replace ‘top’ and ‘bottom’ vertical alignment witth ‘start’ and ‘end’

--- packages/react/src/common/layout.ts
+++ packages/react/src/common/layout.ts
@@ -1,4 +1,4 @@
-export const crossAlignOptions: Array<string> = ['baseline', 'bottom', 'center', 'top']
+export const crossAlignOptions: Array<string> = ['baseline', 'center', 'end', 'start']
 export type CrossAlign = (typeof crossAlignOptions)[number]
 
 export const mainAlignOptions: Array<string> = ['around', 'between', 'center', 'end', 'evenly']
