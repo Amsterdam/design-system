0a790b29cbe664d977ca4cc0d1e4cb648b42c2a9
Vincent Smedinga <v.smedinga@amsterdam.nl>
2024-07-17 21:06:08 +0200

    Remove options settings initial values for CSS properties

--- packages/react/src/common/layout.ts
+++ packages/react/src/common/layout.ts
@@ -1,5 +1,5 @@
-export const crossAlignOptions: Array<string> = ['baseline', 'bottom', 'center', 'stretch', 'top']
+export const crossAlignOptions: Array<string> = ['baseline', 'bottom', 'center', 'top']
 export type CrossAlign = (typeof crossAlignOptions)[number]
 
-export const mainAlignOptions: Array<string> = ['around', 'between', 'center', 'end', 'evenly', 'start']
+export const mainAlignOptions: Array<string> = ['around', 'between', 'center', 'end', 'evenly']
 export type MainAlign = (typeof mainAlignOptions)[number]
