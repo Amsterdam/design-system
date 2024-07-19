b44d78a82cb91d89b2248aebff8bd6ada8f46978
GitHub <github-action@users.noreply.github.com>
2024-07-19 13:31:26 +0000

    Remove feature branch folder

--- demo-DES-804-reorganize-docs-section/assets/Label-DNXaCjsW.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./jsx-runtime-BlAj40OV.js";import{c as l}from"./clsx-B-dksMZM.js";import{r as p}from"./index-Cs7sjTYM.js";const a=p.forwardRef(({children:r,className:e,...s},o)=>t.jsx("label",{...s,ref:o,className:l("ams-label",e),children:r}));a.displayName="Label";try{a.displayName="Label",a.__docgenInfo={description:"",displayName:"Label",props:{}}}catch{}export{a as L};
