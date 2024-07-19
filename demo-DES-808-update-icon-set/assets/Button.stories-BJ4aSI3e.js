41374399bfa1bea986d8252b13d807996018669f
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-07-19 17:04:04 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@e6dfbd109b3109f5faec616d1fd5b88534a7e231 🚀

---
+++ demo-DES-808-update-icon-set/assets/Button.stories-BJ4aSI3e.js
@@ -0,0 +1,29 @@
+import{j as o}from"./jsx-runtime-BlAj40OV.js";import{a as T}from"./index.esm-Bwe0m6R0.js";import{B}from"./Button-fZZcNY7w.js";import{I as j}from"./Icon-msbkstIT.js";const W={title:"Components/Buttons/Button",component:B,args:{children:"Button label",variant:"primary",disabled:!1},argTypes:{children:{description:"The text for the label and/or an icon.",table:{disable:!1}},disabled:{description:"Prevents interaction. Avoid if possible."}}},r={},e={args:{variant:"secondary"}},a={args:{variant:"tertiary"}},s={args:{children:["Button label",o.jsx(j,{svg:T,size:"level-5"},"icon")]},argTypes:{children:{table:{disable:!0}}}},t={args:{children:"Vergunningsaanvraag verzenden"},decorators:[f=>o.jsx("div",{style:{maxWidth:"16rem"},children:o.jsx(f,{})})]};var n,i,c;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:"{}",...(c=(i=r.parameters)==null?void 0:i.docs)==null?void 0:c.source}}};var d,l,p;e.parameters={...e.parameters,docs:{...(d=e.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    variant: 'secondary'
+  }
+}`,...(p=(l=e.parameters)==null?void 0:l.docs)==null?void 0:p.source}}};var m,u,g;a.parameters={...a.parameters,docs:{...(m=a.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    variant: 'tertiary'
+  }
+}`,...(g=(u=a.parameters)==null?void 0:u.docs)==null?void 0:g.source}}};var y,v,h;s.parameters={...s.parameters,docs:{...(y=s.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    children: ['Button label', <Icon key="icon" svg={ShareIcon} size="level-5" />]
+  },
+  argTypes: {
+    children: {
+      table: {
+        disable: true
+      }
+    }
+  }
+}`,...(h=(v=s.parameters)==null?void 0:v.docs)==null?void 0:h.source}}};var b,S,x;t.parameters={...t.parameters,docs:{...(b=t.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    children: 'Vergunningsaanvraag verzenden'
+  },
+  decorators: [Story => <div style={{
+    maxWidth: '16rem'
+  }}>
+        <Story />
+      </div>]
+}`,...(x=(S=t.parameters)==null?void 0:S.docs)==null?void 0:x.source}}};const I=["Primary","Secondary","Tertiary","WithIcon","TextWrapping"],E=Object.freeze(Object.defineProperty({__proto__:null,Primary:r,Secondary:e,Tertiary:a,TextWrapping:t,WithIcon:s,__namedExportsOrder:I,default:W},Symbol.toStringTag,{value:"Module"}));export{E as B,r as P,e as S,a as T,s as W,t as a};
---
+++ demo-DES-808-update-icon-set/assets/MegaMenu.docs-Bxz1YmUx.js
@@ -0,0 +1,11 @@
+import{j as t}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as e}from"./index-BI1Biiay.js";import{ae as i,ak as s,al as a,an as m}from"./index-z88J44wA.js";import{M as p,a as c}from"./MegaMenu.stories-CVLgGgtO.js";import{S as M}from"./StatusBadge-C5O5aAhq.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-BZm7zxOa.js";import"../sb-preview/runtime.js";import"./index-BMdlDBjA.js";import"./index-D-8MO0q_.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";import"./clsx-B-dksMZM.js";import"./Screen-Bi3gjHLv.js";import"./Grid-LBiHLbDB.js";import"./Heading-BgL7hu1i.js";import"./getHeadingElement-DiRwFEz7.js";import"./LinkList-Chpl1euo.js";import"./index.esm-Bwe0m6R0.js";import"./Icon-msbkstIT.js";import"./Badge-DiZ9fuyS.js";const u=`<!-- @license CC0-1.0 -->
+
+# Mega Menu
+`;/*@license CC0-1.0*/function n(o){const r={h2:"h2",...e(),...o.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(i,{of:p}),`
+`,t.jsx(M,{reason:"Needs work for narrow screens and integration with Mega Menu."}),`
+`,t.jsx(s,{children:u}),`
+`,t.jsx(a,{}),`
+`,t.jsx(r.h2,{id:"with-categories",children:"With Categories"}),`
+`,t.jsx(m,{of:c})]})}function N(o={}){const{wrapper:r}={...e(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(n,{...o})}):n(o)}export{N as default};
