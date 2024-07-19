537c83c42748e4ca9764a7fc1f882ede1ce77bdf
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-07-19 10:15:08 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@f8b1bf4d1f95ad4a625f6955b54a29b63e423aa1 🚀

---
+++ demo-row-column-default-gap/assets/Dialog.stories-By32U6CB.js
@@ -0,0 +1,111 @@
+import{j as e}from"./jsx-runtime-BlAj40OV.js";import{c as B}from"./clsx-B-dksMZM.js";import{r as P}from"./index-Cs7sjTYM.js";import{H as d}from"./Heading-BgL7hu1i.js";import{I as C}from"./IconButton-DaVLp41G.js";import{B as t}from"./Button-fZZcNY7w.js";import{P as a}from"./Paragraph-BWS5kDPY.js";const l=P.forwardRef(({actions:n,children:r,className:D,closeButtonLabel:z="Sluiten",heading:S,..._},A)=>e.jsx("dialog",{..._,ref:A,className:B("ams-dialog",D),children:e.jsxs("form",{method:"dialog",className:"ams-dialog__form",children:[e.jsxs("header",{className:"ams-dialog__header",children:[e.jsx(d,{size:"level-4",children:S}),e.jsx(C,{formNoValidate:!0,label:z,size:"level-4"})]}),e.jsx("article",{className:"ams-dialog__article",children:r}),n&&e.jsx("footer",{className:"ams-dialog__footer",children:n})]})}));l.displayName="Dialog";try{l.displayName="Dialog",l.__docgenInfo={description:"",displayName:"Dialog",props:{actions:{defaultValue:null,description:"The button(s) in the footer. Start with a primary button.",name:"actions",required:!1,type:{name:"ReactNode"}},closeButtonLabel:{defaultValue:{value:"Sluiten"},description:"The label for the button that dismisses the Dialog.",name:"closeButtonLabel",required:!1,type:{name:"string"}},heading:{defaultValue:null,description:"The text for the Heading.",name:"heading",required:!0,type:{name:"string"}}}}}catch{}const c=n=>{var r;return(r=n.currentTarget.closest("dialog"))==null?void 0:r.close()},N={title:"Components/Containers/Dialog",component:l,args:{actions:e.jsxs(e.Fragment,{children:[e.jsx(t,{type:"submit",children:"Doorgaan"}),e.jsx(t,{onClick:c,variant:"tertiary",children:"Stoppen"})]}),children:e.jsx(a,{children:"Weet u zeker dat u door wilt gaan met het uitvoeren van deze actie? Dat verwijdert gegevens die nog niet opgeslagen zijn."}),heading:"Niet alle gegevens zijn opgeslagen"},argTypes:{actions:{table:{disable:!0}}}},o={args:{open:!0},argTypes:{heading:{description:"The text for the heading."},open:{description:"Whether the dialog box is active and available for interaction."}},decorators:[n=>e.jsx("div",{style:{backgroundColor:"#0006",position:"absolute",width:"100%",height:"100%"},children:e.jsx(n,{})})],parameters:{docs:{story:{height:"32em"}},layout:"fullscreen"}},i={args:{actions:e.jsx(t,{onClick:c,children:"Sluiten"}),children:[e.jsx(d,{level:2,size:"level-5",children:"Algemeen"},1),e.jsx(a,{children:"De gemeente Amsterdam verwerkt bij de uitvoering van haar taken en verplichtingen persoonsgegevens. De manier waarop de gemeente Amsterdam om gaat met persoonsgegevens is vastgelegd in het stedelijk kader verwerken persoonsgegevens."},2),e.jsx(a,{children:"Deze verklaring geeft aanvullende informatie over de omgang met persoonsgegevens door de gemeente Amsterdam en over uw mogelijkheden tot het uitoefenen van uw rechten met betrekking tot persoonsgegevens."},3),e.jsx(a,{children:"Meer specifieke informatie over privacy en de verwerking van persoonsgegevens door de gemeente Amsterdam kunt u op de hoofdpagina vinden."},4),e.jsx(a,{children:"Vanwege nieuwe wetgeving of andere ontwikkelingen, past de gemeente regelmatig haar processen aan. Dit kunnen ook wijzigingen zijn in de wijze van het verwerken van persoonsgegevens. Wij raden u daarom aan om regelmatig deze pagina te bekijken. Deze pagina wordt doorlopend geactualiseerd."},5),e.jsx(d,{level:2,size:"level-5",children:"Geldende wet- en regelgeving en reikwijdte"},6),e.jsx(a,{children:"Vanaf 25 mei 2018 is de Algemene verordening gegevensbescherming (Avg) van toepassing op alle verwerkingen van persoonsgegevens. Deze Europese wetgeving heeft directe werking in Nederland. Voor die zaken die nationaal geregeld moeten worden, is de Uitvoeringswet Avg in Nederland aanvullend van toepassing. Deze wetteksten kunt u vinden op de website van Autoriteit Persoonsgegevens."},7)],heading:"Privacyverklaring gemeente Amsterdam",open:!0},decorators:[n=>e.jsx("div",{style:{backgroundColor:"#0006",position:"absolute",width:"100%",height:"100%"},children:e.jsx(n,{})})],parameters:{docs:{story:{height:"100vh"}},layout:"fullscreen"}},s={args:{id:"openDialog"},decorators:[n=>e.jsxs("article",{children:[e.jsx(t,{onClick:()=>{var r;return(r=document.querySelector("#openDialog"))==null?void 0:r.showModal()},children:"Open Dialog"}),e.jsx(n,{})]})]},g={args:{actions:e.jsxs(e.Fragment,{children:[e.jsx(t,{type:"submit",children:"Lange teksten op deze knoppen"}),e.jsx(t,{onClick:c,variant:"tertiary",children:"Om verticaal stapelen te demonstreren"})]}),open:!0},decorators:[n=>e.jsx("div",{style:{backgroundColor:"#0006",position:"absolute",width:"100%",height:"100%"},children:e.jsx(n,{})})],parameters:{docs:{story:{height:"32em"}},layout:"fullscreen"}};var p,m,u;o.parameters={...o.parameters,docs:{...(p=o.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    open: true
+  },
+  argTypes: {
+    heading: {
+      description: 'The text for the heading.'
+    },
+    open: {
+      description: 'Whether the dialog box is active and available for interaction.'
+    }
+  },
+  decorators: [Story => <div style={{
+    backgroundColor: '#0006',
+    position: 'absolute',
+    width: '100%',
+    height: '100%'
+  }}>
+        <Story />
+      </div>],
+  parameters: {
+    docs: {
+      story: {
+        height: '32em'
+      }
+    },
+    layout: 'fullscreen'
+  }
+}`,...(u=(m=o.parameters)==null?void 0:m.docs)==null?void 0:u.source}}};var h,v,k;i.parameters={...i.parameters,docs:{...(h=i.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    actions: <Button onClick={closeDialog}>Sluiten</Button>,
+    children: [<Heading level={2} size="level-5" key={1}>
+        Algemeen
+      </Heading>, <Paragraph key={2}>
+        De gemeente Amsterdam verwerkt bij de uitvoering van haar taken en verplichtingen persoonsgegevens. De manier
+        waarop de gemeente Amsterdam om gaat met persoonsgegevens is vastgelegd in het stedelijk kader verwerken
+        persoonsgegevens.
+      </Paragraph>, <Paragraph key={3}>
+        Deze verklaring geeft aanvullende informatie over de omgang met persoonsgegevens door de gemeente Amsterdam en
+        over uw mogelijkheden tot het uitoefenen van uw rechten met betrekking tot persoonsgegevens.
+      </Paragraph>, <Paragraph key={4}>
+        Meer specifieke informatie over privacy en de verwerking van persoonsgegevens door de gemeente Amsterdam kunt u
+        op de hoofdpagina vinden.
+      </Paragraph>, <Paragraph key={5}>
+        Vanwege nieuwe wetgeving of andere ontwikkelingen, past de gemeente regelmatig haar processen aan. Dit kunnen
+        ook wijzigingen zijn in de wijze van het verwerken van persoonsgegevens. Wij raden u daarom aan om regelmatig
+        deze pagina te bekijken. Deze pagina wordt doorlopend geactualiseerd.
+      </Paragraph>, <Heading level={2} size="level-5" key={6}>
+        Geldende wet- en regelgeving en reikwijdte
+      </Heading>, <Paragraph key={7}>
+        Vanaf 25 mei 2018 is de Algemene verordening gegevensbescherming (Avg) van toepassing op alle verwerkingen van
+        persoonsgegevens. Deze Europese wetgeving heeft directe werking in Nederland. Voor die zaken die nationaal
+        geregeld moeten worden, is de Uitvoeringswet Avg in Nederland aanvullend van toepassing. Deze wetteksten kunt u
+        vinden op de website van Autoriteit Persoonsgegevens.
+      </Paragraph>],
+    heading: 'Privacyverklaring gemeente Amsterdam',
+    open: true
+  },
+  decorators: [Story => <div style={{
+    backgroundColor: '#0006',
+    position: 'absolute',
+    width: '100%',
+    height: '100%'
+  }}>
+        <Story />
+      </div>],
+  parameters: {
+    docs: {
+      story: {
+        height: '100vh'
+      }
+    },
+    layout: 'fullscreen'
+  }
+}`,...(k=(v=i.parameters)==null?void 0:v.docs)==null?void 0:k.source}}};var y,w,f;s.parameters={...s.parameters,docs:{...(y=s.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    id: 'openDialog'
+  },
+  decorators: [Story => <article>
+        <Button onClick={() => (document.querySelector('#openDialog') as HTMLDialogElement)?.showModal()}>
+          Open Dialog
+        </Button>
+        <Story />
+      </article>]
+}`,...(f=(w=s.parameters)==null?void 0:w.docs)==null?void 0:f.source}}};var j,b,x;g.parameters={...g.parameters,docs:{...(j=g.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  args: {
+    actions: <>
+        <Button type="submit">Lange teksten op deze knoppen</Button>
+        <Button onClick={closeDialog} variant="tertiary">
+          Om verticaal stapelen te demonstreren
+        </Button>
+      </>,
+    open: true
+  },
+  decorators: [Story => <div style={{
+    backgroundColor: '#0006',
+    position: 'absolute',
+    width: '100%',
+    height: '100%'
+  }}>
+        <Story />
+      </div>],
+  parameters: {
+    docs: {
+      story: {
+        height: '32em'
+      }
+    },
+    layout: 'fullscreen'
+  }
+}`,...(x=(b=g.parameters)==null?void 0:b.docs)==null?void 0:x.source}}};const T=["Default","WithScrollbar","TriggerButton","VerticalButtons"],L=Object.freeze(Object.defineProperty({__proto__:null,Default:o,TriggerButton:s,VerticalButtons:g,WithScrollbar:i,__namedExportsOrder:T,default:N},Symbol.toStringTag,{value:"Module"}));export{L as D,s as T,g as V,i as W};
---
+++ demo-row-column-default-gap/assets/Row.stories-BypWV7uY.js
@@ -0,0 +1,30 @@
+import{j as e}from"./jsx-runtime-BlAj40OV.js";import{c as P}from"./clsx-B-dksMZM.js";import{r as q}from"./index-Cs7sjTYM.js";import{B as W}from"./Button-fZZcNY7w.js";import{A as C}from"./Avatar-CRAAVM1p.js";import{H as $}from"./Heading-BgL7hu1i.js";import{L as D}from"./Link-CkpyUMtL.js";import{P as c}from"./Paragraph-BWS5kDPY.js";const l=q.forwardRef(({align:a,alignVertical:m,as:N="div",children:O,className:R,gap:H,wrap:B,...E},L)=>e.jsx(N,{...E,ref:L,className:P("ams-row",`ams-row--gap-${H}`,a&&`ams-row--align-${a}`,m&&`ams-row--align-vertical-${m}`,B&&"ams-row--wrap",R),children:O}));l.displayName="Row";try{l.displayName="Row",l.__docgenInfo={description:"",displayName:"Row",props:{align:{defaultValue:null,description:"The horizontal alignment of the items in the row.",name:"align",required:!1,type:{name:"string"}},alignVertical:{defaultValue:null,description:"The vertical alignment of the items in the row.",name:"alignVertical",required:!1,type:{name:"string"}},as:{defaultValue:null,description:"The HTML element to use.",name:"as",required:!1,type:{name:"enum",value:[{value:'"article"'},{value:'"div"'},{value:'"section"'}]}},gap:{defaultValue:null,description:"The amount of vertical space between items.",name:"gap",required:!1,type:{name:"string"}},wrap:{defaultValue:null,description:"Whether items of the row can wrap onto multiple lines.",name:"wrap",required:!1,type:{name:"boolean"}}}}}catch{}const M=["baseline","center","end","start"],I=["around","between","center","end","evenly"],F=Array.from(Array(3).keys()).map(a=>e.jsxs(W,{children:["Button ",a+1]},a)),z=[e.jsx(c,{className:"ams-docs-pink-box",style:{inlineSize:"8rem"},children:"One line"},1),e.jsxs(c,{className:"ams-docs-pink-box",style:{inlineSize:"8rem"},children:["Two",e.jsx("br",{}),"lines"]},2),e.jsx(c,{className:"ams-docs-pink-box",style:{inlineSize:"8rem"},children:"One line"},3)],G={title:"Components/Layout/Row",component:l,args:{children:F},argTypes:{align:{control:"radio",options:I},alignVertical:{control:"radio",options:M},gap:{control:"radio",options:["extra-small","small","medium","large","extra-large"]}}},r={},n={args:{align:"evenly",children:z}},s={args:{align:"end",children:e.jsx(C,{label:"AB"})}},i={args:{align:"between",alignVertical:"baseline",children:[e.jsx($,{level:3,children:"An example heading"}),e.jsx(D,{href:"#",children:"An example link"})],wrap:!0}},o={args:{alignVertical:"center",children:z}},t={args:{children:Array.from(Array(4).keys()).map(a=>e.jsx("span",{className:"ams-docs-pink-box",style:{inlineSize:"16rem"}},a)),wrap:!0}};var p,d,g;r.parameters={...r.parameters,docs:{...(p=r.parameters)==null?void 0:p.docs,source:{originalSource:"{}",...(g=(d=r.parameters)==null?void 0:d.docs)==null?void 0:g.source}}};var u,h,f;n.parameters={...n.parameters,docs:{...(u=n.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    align: 'evenly',
+    children: ThreeParagraphs
+  }
+}`,...(f=(h=n.parameters)==null?void 0:h.docs)==null?void 0:f.source}}};var y,x,w;s.parameters={...s.parameters,docs:{...(y=s.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    align: 'end',
+    children: <Avatar label="AB" />
+  }
+}`,...(w=(x=s.parameters)==null?void 0:x.docs)==null?void 0:w.source}}};var A,b,v;i.parameters={...i.parameters,docs:{...(A=i.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  args: {
+    align: 'between',
+    alignVertical: 'baseline',
+    children: [<Heading level={3}>An example heading</Heading>, <Link href="#">An example link</Link>],
+    wrap: true
+  }
+}`,...(v=(b=i.parameters)==null?void 0:b.docs)==null?void 0:v.source}}};var S,j,k;o.parameters={...o.parameters,docs:{...(S=o.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    alignVertical: 'center',
+    children: ThreeParagraphs
+  }
+}`,...(k=(j=o.parameters)==null?void 0:j.docs)==null?void 0:k.source}}};var T,V,_;t.parameters={...t.parameters,docs:{...(T=t.parameters)==null?void 0:T.docs,source:{originalSource:`{
+  args: {
+    children: Array.from(Array(4).keys()).map(i => <span className="ams-docs-pink-box" key={i} style={{
+      inlineSize: '16rem'
+    }} />),
+    wrap: true
+  }
+}`,...(_=(V=t.parameters)==null?void 0:V.docs)==null?void 0:_.source}}};const J=["Default","HorizontalAlignment","EndAlignASingleChild","AlignOpposingTexts","VerticalAlignment","Wrapping"],re=Object.freeze(Object.defineProperty({__proto__:null,AlignOpposingTexts:i,Default:r,EndAlignASingleChild:s,HorizontalAlignment:n,VerticalAlignment:o,Wrapping:t,__namedExportsOrder:J,default:G},Symbol.toStringTag,{value:"Module"}));export{i as A,s as E,n as H,re as R,o as V,t as W};
