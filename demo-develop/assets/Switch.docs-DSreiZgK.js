77b8005a2169629fd793d693c4b2bae98a9e41b2
alimpens <37216945+alimpens@users.noreply.github.com>
2024-07-19 15:06:15 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@84eb5127f974514effdab3d35ac292e38bdffbe3 🚀

---
+++ demo-develop/assets/Switch.docs-DSreiZgK.js
@@ -0,0 +1,36 @@
+import{j as e}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as s}from"./index-BI1Biiay.js";import{ae as a,ak as i,al as r,am as c,an as l}from"./index-BWgSdizi.js";import{S as h,W as m}from"./Switch.stories-B578q8_s.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-BoWntlGz.js";import"../sb-preview/runtime.js";import"./index-BMdlDBjA.js";import"./index-D-8MO0q_.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";import"./clsx-B-dksMZM.js";import"./Label-DNXaCjsW.js";const p=`<!-- @license CC0-1.0 -->
+
+# Switch
+
+A switch is a control element to toggle between two different states quickly.
+A switch applies to a page or the entire system, such as an on/off switch.
+
+## Guidelines
+
+- Switch only between two different states.
+  It is a binary action.
+- Use labels with a switch to make the action clear.
+- The action takes place immediately when the user operates the switch.
+
+## Relevant WCAG requirements
+
+- [WCAG 1.3.5](https://www.w3.org/WAI/WCAG21/Understanding/identify-input-purpose.html): it is both clear for a user and programmatically what the purpose of a form field is.
+
+Switch is an interactive element; general requirements and guidelines for interactivity apply [here](/docs/docs-developer-guide-interactivity--docs).
+
+## References
+
+- [Switch Pattern](https://www.w3.org/WAI/ARIA/apg/patterns/switch/)
+- [Role switch](https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/switch_role)
+`;/*@license CC0-1.0*/function o(n){const t={code:"code",h2:"h2",h3:"h3",p:"p",...s(),...n.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(a,{of:h}),`
+`,e.jsx(i,{children:p}),`
+`,e.jsxs(t.p,{children:[`It can be used as a standalone component or as a form control element.
+When used as a standalone component provide an `,e.jsx(t.code,{children:"aria-label"})," or ",e.jsx(t.code,{children:"aria-labelledby"}),` prop to keep it accessible.
+When used as a formcontrol, make sure to add the associated `,e.jsx(t.code,{children:"<Label>"})," component."]}),`
+`,e.jsx(r,{}),`
+`,e.jsx(c,{}),`
+`,e.jsx(t.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(t.h3,{id:"with-label",children:"With Label"}),`
+`,e.jsx(l,{of:m})]})}function S(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o(n)}export{S as default};
