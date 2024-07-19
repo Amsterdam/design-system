663cfd2f196c42c11519f9267be1405073e849e9
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-07-19 09:11:30 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@8c6072a8b2e83f6949145b755f6b855361f29aa5 🚀

---
+++ demo-DES-793-add-autofocus-to-form-error-list/assets/Card.docs-ajAdQ0PS.js
@@ -0,0 +1,40 @@
+import{j as e}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as o}from"./index-BI1Biiay.js";import{ae as r,ak as s,al as c,an as a}from"./index-B4jdddXv.js";import{C as d,W as h,a as l}from"./Card.stories-JVnVFM2p.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-e95zkA2-.js";import"../sb-preview/runtime.js";import"./index-BMdlDBjA.js";import"./index-D-8MO0q_.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";import"./exampleContent-BEbrefRz.js";import"./Card-BK8ntILx.js";import"./clsx-B-dksMZM.js";import"./Paragraph-BWS5kDPY.js";import"./Heading-BgL7hu1i.js";import"./getHeadingElement-DiRwFEz7.js";import"./AspectRatio-Cd0KgYDn.js";import"./Image-CVzrCme5.js";const p=`<!-- @license CC0-1.0 -->
+
+# Card
+
+Use a card on an overview page to link to an article page, such as a news article, a job vacancy, or a search result.
+The card typically includes the title, introduction, and an image of the content.
+The link is mandatory.
+
+## Guidelines
+
+- The title of a card is a link within a heading.
+  The same guidelines as for regular [links](/docs/components-navigation-link--docs) and [headings](/docs/components-text-heading--docs) apply here.
+  The link is active across the entire area of the card.
+- A card needs more content than just a title.
+  Supplement this with textual and/or visual content.
+- Place the text in a regular paragraph.
+  Only use the smallest text size for a tagline or date.
+- This component may not be the best option if the content does not represent a detailed page.
+  For referencing a thematic page, use a [top task link](/docs/components-navigation-top-task-link--docs).
+  You can also use a [regular link](/docs/components-navigation-link--docs).
+
+## Screen Readers
+
+With a screen reader, you can navigate using headings and links in a document.
+The title of a card is a link within a heading, allowing you to utilize both navigation methods.
+A screen reader reads the title first, followed by the rest of the content.
+`;/*@license CC0-1.0*/function i(t){const n={a:"a",code:"code",h2:"h2",p:"p",...o(),...t.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(r,{of:d}),`
+`,e.jsx(s,{children:p}),`
+`,e.jsx(c,{}),`
+`,e.jsx(n.h2,{id:"with-tagline",children:"With Tagline"}),`
+`,e.jsxs(n.p,{children:[`A card can display a tagline above the heading, a short term like a category or type of information.
+Wrap the tagline and the heading in a `,e.jsx(n.code,{children:"Card.HeadingGroup"}),`.
+This ensures screen readers first read the heading and then the tagline.`]}),`
+`,e.jsx(a,{of:h}),`
+`,e.jsx(n.h2,{id:"with-image",children:"With Image"}),`
+`,e.jsxs(n.p,{children:[`A card often displays the image of the detail page.
+Use `,e.jsx(n.a,{href:"/docs/layout-aspect-ratio--docs",children:"Aspect Ratio"})," for the correct aspect ratio."]}),`
+`,e.jsx(a,{of:l})]})}function X(t={}){const{wrapper:n}={...o(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(i,{...t})}):i(t)}export{X as default};
---
+++ demo-DES-793-add-autofocus-to-form-error-list/assets/DescriptionList.docs-BFQONjap.js
@@ -0,0 +1,31 @@
+import{j as e}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as i}from"./index-BI1Biiay.js";import{ae as s,ak as a,al as l,am as m,an as o}from"./index-B4jdddXv.js";import{D as p,M as c,I as d}from"./DescriptionList.stories-CPpsKN7M.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-e95zkA2-.js";import"../sb-preview/runtime.js";import"./index-BMdlDBjA.js";import"./index-D-8MO0q_.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";import"./index.esm-Oy2d-T_j.js";import"./clsx-B-dksMZM.js";import"./index.esm-DY3iRp4W.js";import"./decorators-DLPD6TOJ.js";import"./exampleContent-BEbrefRz.js";const h=`<!-- @license CC0-1.0 -->
+
+# Description List
+
+A collection of terms and their details.
+
+## Design
+
+On a narrow screen, details appear indented below their term.
+From the medium breakpoint, terms and details appear next to each other.
+The column for the details is twice as wide as the one for the term.
+
+Details are set in bold text.
+
+## References
+
+- [MDN: \`<dl>\`: The Description List element](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/dl)
+- [WCAG: Using description lists](https://www.w3.org/WAI/WCAG22/Techniques/html/H40)
+`;/*@license CC0-1.0*/function r(n){const t={code:"code",h2:"h2",h3:"h3",p:"p",...i(),...n.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(s,{of:p}),`
+`,e.jsx(a,{children:h}),`
+`,e.jsx(l,{}),`
+`,e.jsx(m,{}),`
+`,e.jsx(t.h2,{id:"multiple-details",children:"Multiple details"}),`
+`,e.jsx(t.p,{children:"A term may have multiple details."}),`
+`,e.jsx(o,{of:c}),`
+`,e.jsx(t.h3,{id:"inverse-colour",children:"Inverse colour"}),`
+`,e.jsxs(t.p,{children:["Set the ",e.jsx(t.code,{children:"inverseColor"}),` prop if the Description List sits on a dark background.
+This ensures the colour of the text provides enough contrast.`]}),`
+`,e.jsx(o,{of:d})]})}function W(n={}){const{wrapper:t}={...i(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(r,{...n})}):r(n)}export{W as default};
