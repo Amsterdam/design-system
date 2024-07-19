2576279030c82e050bd44699cbb8536e19a7e205
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-07-19 11:36:04 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@11aeab2c2f360a50d6f02b53cd78608273e245ed 🚀

---
+++ demo-DES-837-remove-visually-hidden-component/assets/VisuallyHidden.docs-9AXVO5kK.js
@@ -0,0 +1,11 @@
+import{j as n}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as o}from"./index-BI1Biiay.js";import{ak as r,al as a,am as i}from"./index-Dd849QUR.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-D6OKB9Uv.js";import"../sb-preview/runtime.js";import"./index-BMdlDBjA.js";import"./index-D-8MO0q_.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";const m=`<!-- @license CC0-1.0 -->
+
+# Visually Hidden
+
+Hides content from sighted users but keeps it accessible to non-visual user agents, such as screen readers.
+
+Note: in most cases, visually available content should be accessible to non-visual user agents and vice versa.
+Elaborate instructions or guidance read only by non-visual user agents can do more harm than good.
+`;function t(e){return n.jsxs(n.Fragment,{children:[n.jsx(r,{children:m}),`
+`,n.jsx(a,{}),`
+`,n.jsx(i,{})]})}function v(e={}){const{wrapper:s}={...o(),...e.components};return s?n.jsx(s,{...e,children:n.jsx(t,{...e})}):t()}export{v as default};
