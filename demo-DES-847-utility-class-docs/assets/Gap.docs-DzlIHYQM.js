2ce8943ad54d82f6bce68fa47e9ae22279057bce
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-07-22 15:59:27 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@52cbcab6f5e0d2bd86b5f652a59588b424189308 🚀

---
+++ demo-DES-847-utility-class-docs/assets/Gap.docs-DzlIHYQM.js
@@ -0,0 +1,22 @@
+import{j as e}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as o}from"./index-BI1Biiay.js";import{ae as s,ak as r,al as i,am as m}from"./index-BE5RIzBj.js";import{G as p}from"./Gap.stories-DH961LUk.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-2TA1gwRa.js";import"../sb-preview/runtime.js";import"./index-BMdlDBjA.js";import"./index-D-8MO0q_.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";import"./Paragraph-BWS5kDPY.js";import"./clsx-B-dksMZM.js";const d=`<!-- @license CC0-1.0 -->
+
+# Gap
+
+Adds white space between a set of elements.
+
+The five sizes of [Component Space](/docs/foundation-design-tokens-space--docs) are available for the length of the gap.
+
+## Guidelines
+
+- Use this utility class to vertically separate the children of a parent element from each other.
+- It can be used on any element and sets the \`gap\` CSS property.
+  It also makes the element a grid container; the gap would be ignored otherwise.
+  These declarations are marked with the \`!important\` flag to ensure they override any other gaps and display modes.
+- To add a gap between 2 siblings, the first one can be given a [Margin Bottom](/docs/utilities-css-margin--docs) instead.
+  This may be preferable because it doesn’t change the parent’s display mode.
+`;/*@license CC0-1.0*/function a(t){return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(s,{of:p}),`
+`,e.jsx(r,{children:d}),`
+`,e.jsx(i,{}),`
+`,e.jsx(m,{})]})}function v(t={}){const{wrapper:n}={...o(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(a,{...t})}):a()}export{v as default};
