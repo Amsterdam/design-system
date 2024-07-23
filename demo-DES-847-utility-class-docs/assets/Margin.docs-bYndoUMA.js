a7639e407a11fb5d764a99876459ed1016e7e16c
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-07-23 21:58:17 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@02da81960f2ad31f206872d4ec284c00c67b210c 🚀

---
+++ demo-DES-847-utility-class-docs/assets/Margin.docs-bYndoUMA.js
@@ -0,0 +1,31 @@
+import{j as e}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as a}from"./index-BI1Biiay.js";import{ae as o,ak as i,al as m,am as r}from"./index-D2KNNoEd.js";import{M as l}from"./Margin.stories-B4Jb88Az.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-CflW38GY.js";import"../sb-preview/runtime.js";import"./index-BMdlDBjA.js";import"./index-D-8MO0q_.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";import"./index.esm-BlOReKcg.js";import"./clsx-B-dksMZM.js";import"./index.esm-Bsc2owIZ.js";const p=`<!-- @license CC0-1.0 -->
+
+# Margin
+
+Adds white space below a single element.
+
+## Class names
+
+The five sizes of [Component Space](/docs/foundation-design-tokens-space--docs) are available for the length of the margin.
+
+| Class name    | Preview                                                                                  |
+| ------------- | ---------------------------------------------------------------------------------------- |
+| \`.ams-mb--xs\` | <div className="ams-docs-token-preview--space" style="inline-size: var(--ams-mb-xs);" /> |
+| \`.ams-mb--sm\` | <div className="ams-docs-token-preview--space" style="inline-size: var(--ams-mb-sm);" /> |
+| \`.ams-mb--md\` | <div className="ams-docs-token-preview--space" style="inline-size: var(--ams-mb-md);" /> |
+| \`.ams-mb--lg\` | <div className="ams-docs-token-preview--space" style="inline-size: var(--ams-mb-lg);" /> |
+| \`.ams-mb--xl\` | <div className="ams-docs-token-preview--space" style="inline-size: var(--ams-mb-xl);" /> |
+
+## Guidelines
+
+- Use this utility class to vertically separate one element from the next.
+- It can be used on any element and sets the \`margin-block-end\` CSS property. This declaration is marked with the \`!important\` flag to ensure it overrides any other margins.
+- Elements’ top and bottom margins are sometimes collapsed into a single margin. Consult [MDN](https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_box_model/Mastering_margin_collapsing) for details.
+- To add equal margins between elements, either use the [Gap](/docs/utilities-css-gap--docs) utility class on their common parent or wrap them in a [Column](/docs/components-layout-column--docs) component.
+  This helps in loops: it prevents having to treat the last element differently.
+`;/*@license CC0-1.0*/function t(s){return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(o,{of:l}),`
+`,e.jsx(i,{children:p}),`
+`,e.jsx(m,{}),`
+`,e.jsx(r,{})]})}function S(s={}){const{wrapper:n}={...a(),...s.components};return n?e.jsx(n,{...s,children:e.jsx(t,{...s})}):t()}export{S as default};
