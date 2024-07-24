a387273a61c98a206696e6eddc57a4463f94c19b
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-07-24 23:13:46 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@0e0fb09ff0fae2f4d16c3b6478ea56c55035884d 🚀

---
+++ demo-rename-foundation-to-brand/assets/DescriptionList.docs-Cj3B2-A8.js
@@ -0,0 +1,31 @@
+import{j as e}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as s}from"./index-BI1Biiay.js";import{ae as i,ak as a,al as l,am as m,an as o}from"./index-DPM_MTFd.js";import{D as p,M as c,I as d}from"./DescriptionList.stories-CCwDWEg5.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-Dk-ynP7z.js";import"../sb-preview/runtime.js";import"./index-BU4L-DQy.js";import"./index-D-8MO0q_.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";import"./decorators-DLPD6TOJ.js";import"./exampleContent-DN6_nyQl.js";import"./clsx-B-dksMZM.js";const h=`<!-- @license CC0-1.0 -->
+
+# Description List
+
+A collection of terms and their details.
+
+## Design
+
+On a narrow screen, details appear indented below their term.
+From the medium breakpoint, terms and details appear next to each other.
+The column for the details is twice as wide as the one for the term.
+
+Details are set in bold text.
+
+## References
+
+- [MDN: \`<dl>\`: The Description List element](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/dl)
+- [WCAG: Using description lists](https://www.w3.org/WAI/WCAG22/Techniques/html/H40)
+`;/*@license CC0-1.0*/function r(n){const t={code:"code",h2:"h2",h3:"h3",p:"p",...s(),...n.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(i,{of:p}),`
+`,e.jsx(a,{children:h}),`
+`,e.jsx(l,{}),`
+`,e.jsx(m,{}),`
+`,e.jsx(t.h2,{id:"multiple-details",children:"Multiple details"}),`
+`,e.jsx(t.p,{children:"A term may have multiple details."}),`
+`,e.jsx(o,{of:c}),`
+`,e.jsx(t.h3,{id:"inverse-colour",children:"Inverse colour"}),`
+`,e.jsxs(t.p,{children:["Set the ",e.jsx(t.code,{children:"inverseColor"}),` prop if the Description List sits on a dark background.
+This ensures the colour of the text provides enough contrast.`]}),`
+`,e.jsx(o,{of:d})]})}function E(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(r,{...n})}):r(n)}export{E as default};
