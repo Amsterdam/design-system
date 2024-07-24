8f65c68d35ea6f984cf3bd148c1cf2cfc82c3196
Vincent Smedinga <v.smedinga@amsterdam.nl>
2024-07-25 00:59:55 +0200

    Rename stories to match prop name and docs heading

--- storybook/src/components/Blockquote/Blockquote.stories.tsx
+++ storybook/src/components/Blockquote/Blockquote.stories.tsx
@@ -32,7 +32,7 @@ type Story = StoryObj<typeof meta>
 
 export const Default: Story = {}
 
-export const InvertedColor: Story = {
+export const InverseColour: Story = {
   args: {
     inverseColor: true,
   },
