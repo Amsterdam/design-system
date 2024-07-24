f266cb6e03bed0b779ecfff69780c15d3a8264c7
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-07-24 12:19:27 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@859eb7fac5cf50c2d675318f318108d9f86eadd5 🚀

--- demo-DES-847-utility-class-docs/assets/Gap.stories-FHyp1oZl.js
+++
@@ -1,6 +0,0 @@
-import{j as e}from"./jsx-runtime-BlAj40OV.js";import{P as s}from"./index.esm-BlOReKcg.js";/**
- * @license EUPL-1.2+
- * Copyright Gemeente Amsterdam
- */const t=({children:r,length:l})=>e.jsx("span",{className:`ams-gap--${l}`,children:r});try{t.displayName="Gap",t.__docgenInfo={description:"Renders examples in Storybook. Not for reuse.",displayName:"Gap",props:{length:{defaultValue:null,description:"The amount of vertical space between items.",name:"length",required:!0,type:{name:"enum",value:[{value:'"xs"'},{value:'"sm"'},{value:'"md"'},{value:'"lg"'},{value:'"xl"'}]}}}}}catch{}const c=({length:r})=>e.jsxs("div",{className:`ams-gap--${r}`,children:[e.jsx(s,{children:"These paragraphs are separated by a gap."}),e.jsx(s,{children:"These paragraphs are separated by a gap."}),e.jsx(s,{children:"These paragraphs are separated by a gap."})]}),d={title:"Utilities/CSS/Gap",component:t,args:{length:"xs"}},a={render:c};var o,p,n;a.parameters={...a.parameters,docs:{...(o=a.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  render
-}`,...(n=(p=a.parameters)==null?void 0:p.docs)==null?void 0:n.source}}};const i=["Default"],g=Object.freeze(Object.defineProperty({__proto__:null,Default:a,__namedExportsOrder:i,default:d},Symbol.toStringTag,{value:"Module"}));export{g as G};
