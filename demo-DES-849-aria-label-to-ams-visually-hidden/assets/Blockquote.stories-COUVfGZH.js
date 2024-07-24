157045b53cd34040b2da727978641686b75ee230
RubenSibon <mail@rubensibon.nl>
2024-07-24 14:24:56 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@1dbb2aaa5a808447a6c0dfcbfff3ed66ffab12a1 🚀

---
+++ demo-DES-849-aria-label-to-ams-visually-hidden/assets/Blockquote.stories-COUVfGZH.js
@@ -0,0 +1,8 @@
+import{i as f}from"./decorators-DLPD6TOJ.js";import{c as _}from"./exampleContent-DN6_nyQl.js";import{j as g}from"./jsx-runtime-BlAj40OV.js";import{c as q}from"./clsx-B-dksMZM.js";import{r as b}from"./index-Cs7sjTYM.js";const r=b.forwardRef(({children:t,className:p,inverseColor:m,...u},d)=>g.jsx("blockquote",{...u,ref:d,className:q("ams-blockquote",m&&"ams-blockquote--inverse-color",p),children:t}));r.displayName="Blockquote";try{r.displayName="Blockquote",r.__docgenInfo={description:"",displayName:"Blockquote",props:{inverseColor:{defaultValue:null,description:"Changes the text colour for readability on a dark background.",name:"inverseColor",required:!1,type:{name:"boolean"}}}}}catch{}/**
+ * @license EUPL-1.2+
+ * Copyright Gemeente Amsterdam
+ */const k=_(),x={title:"Components/Text/Blockquote",component:r,args:{children:k,inverseColor:!1},argTypes:{children:{description:"The text for the quote.",table:{disable:!1}}},decorators:[f]},e={},o={args:{inverseColor:!0}};var a,s,c;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:"{}",...(c=(s=e.parameters)==null?void 0:s.docs)==null?void 0:c.source}}};var l,n,i;o.parameters={...o.parameters,docs:{...(l=o.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    inverseColor: true
+  }
+}`,...(i=(n=o.parameters)==null?void 0:n.docs)==null?void 0:i.source}}};const v=["Default","InvertedColor"],S=Object.freeze(Object.defineProperty({__proto__:null,Default:e,InvertedColor:o,__namedExportsOrder:v,default:x},Symbol.toStringTag,{value:"Module"}));export{S as B,o as I};
