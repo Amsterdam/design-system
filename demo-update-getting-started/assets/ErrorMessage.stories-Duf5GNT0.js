bf8f3df72867acbd8b434621e4891afdecacea3a
GitHub <github-action@users.noreply.github.com>
2024-07-24 11:32:59 +0000

    Remove feature branch folder

--- demo-update-getting-started/assets/ErrorMessage.stories-Duf5GNT0.js
+++
@@ -1,9 +0,0 @@
-import{E as i}from"./ErrorMessage-BV40jE7R.js";/**
- * @license EUPL-1.2+
- * Copyright Gemeente Amsterdam
- */const l={title:"Components/Forms/Error Message",component:i,args:{children:"Vul een geldig e-mailadres in, bijvoorbeeld naam@voorbeeld.nl."},argTypes:{children:{table:{disable:!1}}}},e={},r={args:{children:"Enter an email address in the correct format, like name@example.com",prefix:"Error"}};var a,o,s;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:"{}",...(s=(o=e.parameters)==null?void 0:o.docs)==null?void 0:s.source}}};var t,n,c;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  args: {
-    children: 'Enter an email address in the correct format, like name@example.com',
-    prefix: 'Error'
-  }
-}`,...(c=(n=r.parameters)==null?void 0:n.docs)==null?void 0:c.source}}};const m=["Default","WithCustomPrefix"],p=Object.freeze(Object.defineProperty({__proto__:null,Default:e,WithCustomPrefix:r,__namedExportsOrder:m,default:l},Symbol.toStringTag,{value:"Module"}));export{p as E,r as W};
