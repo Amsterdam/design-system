06eae3e79e486dfede708f6b9f281e255def6154
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-07-24 10:28:11 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@9deeb96e3a03394442f7cd28ba2ef683561810e0 🚀

---
+++ demo-DES-847-utility-class-docs/assets/AspectRatio.docs-BukhojP_.js
@@ -0,0 +1,42 @@
+import{j as e}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as s}from"./index-BI1Biiay.js";import{ae as i,ak as r,al as l,am as d,an as a}from"./index-CY9MF2S6.js";import{A as x,D as h,E as m,W as p,S as c,T as j,a as f}from"./AspectRatio.stories-B0l0JCOI.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-Bs4QhRsQ.js";import"../sb-preview/runtime.js";import"./index-BMdlDBjA.js";import"./index-D-8MO0q_.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";import"./index.esm-BlOReKcg.js";import"./clsx-B-dksMZM.js";import"./index.esm-Bsc2owIZ.js";const u=`<!-- @license CC0-1.0 -->
+
+# Aspect Ratio
+
+Aspect Ratio updates the height of an element with its width.
+
+These are the proportions we use:
+
+| keyword   | ratio |
+| :-------- | ----: |
+| \`2x-wide\` |  16:5 |
+| \`x-wide\`  |  16:9 |
+| \`wide\`    |   4:3 |
+| \`square\`  |   1:1 |
+| \`tall\`    |   3:4 |
+| \`x-tall\`  |  9:16 |
+
+## Guidelines
+
+- Use this component to constrain images, videos and other elements to one of the available aspect ratios.
+`;/*@license CC0-1.0*/function o(t){const n={h2:"h2",p:"p",...s(),...t.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(i,{of:x}),`
+`,e.jsx(r,{children:u}),`
+`,e.jsx(l,{}),`
+`,e.jsx(d,{}),`
+`,e.jsx(n.h2,{id:"double-extra-wide",children:"Double extra wide"}),`
+`,e.jsx(n.p,{children:"We only use this aspect ratio for a ‘hero’ image across the entire website width."}),`
+`,e.jsx(a,{of:h}),`
+`,e.jsx(n.h2,{id:"extra-wide",children:"Extra wide"}),`
+`,e.jsx(a,{of:m}),`
+`,e.jsx(n.h2,{id:"wide",children:"Wide"}),`
+`,e.jsx(a,{of:p}),`
+`,e.jsx(n.h2,{id:"square",children:"Square"}),`
+`,e.jsx(a,{of:c}),`
+`,e.jsx(n.h2,{id:"tall",children:"Tall"}),`
+`,e.jsx(a,{of:j}),`
+`,e.jsx(n.h2,{id:"extra-tall",children:"Extra Tall"}),`
+`,e.jsx(n.p,{children:`This variant may be suitable for telephones.
+Most devices nowadays have an aspect ratio of 9:19.5.
+Images of 9:16 fill approximately 80% of the screen.`}),`
+`,e.jsx(a,{of:f})]})}function k(t={}){const{wrapper:n}={...s(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(o,{...t})}):o(t)}export{k as default};
