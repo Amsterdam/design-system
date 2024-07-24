0a72002937bcb8d6388d786c120ce91517db613d
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-07-24 11:48:18 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@7cfee6c4fed6355a25f3af92d7da6b273c877cd9 🚀

---
+++ demo-DES-847-utility-class-docs/assets/Margin.stories-C9B7W2o1.js
@@ -0,0 +1,6 @@
+import{j as r}from"./jsx-runtime-BlAj40OV.js";import{P as l}from"./index.esm-BlOReKcg.js";/**
+ * @license EUPL-1.2+
+ * Copyright Gemeente Amsterdam
+ */const t=({children:a,length:i})=>r.jsx("span",{className:`ams-mb--${i}`,children:a});try{t.displayName="Margin",t.__docgenInfo={description:"Renders examples in Storybook. Not for reuse.",displayName:"Margin",props:{length:{defaultValue:null,description:"The amount of white space to add below the element.",name:"length",required:!0,type:{name:"enum",value:[{value:'"xs"'},{value:'"sm"'},{value:'"md"'},{value:'"lg"'},{value:'"xl"'}]}}}}}catch{}const m=({length:a})=>[r.jsx("h1",{className:`ams-heading ams-heading--2 ams-mb--${a}`,children:"This heading has a bottom margin"},0),r.jsx(l,{children:"It introduces white space between the heading and this paragraph below."},1)],d={title:"Utilities/CSS/Margin",component:t,args:{length:"xs"},argTypes:{length:{control:"radio"}}},e={render:m};var s,o,n;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  render
+}`,...(n=(o=e.parameters)==null?void 0:o.docs)==null?void 0:n.source}}};const c=["Default"],g=Object.freeze(Object.defineProperty({__proto__:null,Default:e,__namedExportsOrder:c,default:d},Symbol.toStringTag,{value:"Module"}));export{g as M};
