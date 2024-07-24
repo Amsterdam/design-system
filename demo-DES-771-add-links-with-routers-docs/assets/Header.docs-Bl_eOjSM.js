7cf6b53937d7dd010272e72e2ec2dae7b879c5e8
alimpens <37216945+alimpens@users.noreply.github.com>
2024-07-24 12:38:45 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@2bcab88325a778adbbd672882324a11c87343219 🚀

---
+++ demo-DES-771-add-links-with-routers-docs/assets/Header.docs-Bl_eOjSM.js
@@ -0,0 +1,48 @@
+import{j as n}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as s}from"./index-BI1Biiay.js";import{ae as o,ak as r,al as m,am as h,an as t}from"./index-DnZ3g-H1.js";import{H as p,W as d,a as l,b as c,c as u,d as f,e as g,f as x}from"./Header.stories-BpmkU8bJ.js";import{S as w}from"./StatusBadge-C5O5aAhq.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-Bbix2QIh.js";import"../sb-preview/runtime.js";import"./index-BU4L-DQy.js";import"./index-D-8MO0q_.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";import"./index.esm-BlOReKcg.js";import"./clsx-B-dksMZM.js";import"./index.esm-Bsc2owIZ.js";import"./Logo-Co1O1cSe.js";import"./Heading-DUu-96hO.js";import"./getHeadingTag-Dv1eMqvl.js";import"./Badge-DiZ9fuyS.js";const j=`<!-- @license CC0-1.0 -->
+
+# Header
+
+Displays the City’s logo at the top of every page, and optionally a navigation menu.
+Includes the name of the application if it is not the general website.
+
+## Guidelines
+
+The Header must be used on all websites and applications for the City of Amsterdam.
+It includes the logo of the City or the organization, the site title (except for the general website), and a menu with links to commonly used pages.
+
+The Header is important because it conveys our corporate identity and is the first thing people see.
+Using it consistently helps users recognize and trust the website.
+It is the same on every page of the application.
+
+The page menu can contain a maximum of 5 items.
+The last two will often be ‘Search’ and ‘Menu’.
+Labels should be short to ensure the menu fits on one line, even on medium-wide screens.
+An icon can be added to the end of a link to make its function easier to find.
+
+## References
+
+- A Header is a [landmark](https://www.w3.org/TR/wai-aria-practices-1.1/#aria_landmark_roles) and can be use to group navigation elements.
+- [WCAG 3.2.3](https://wcag.com/designers/3-2-3-consistent-navigation/) Consistent Navigation: Navigation menus that appear on multiple pages are consistent.
+`;/*@license CC0-1.0*/function a(i){const e={a:"a",h2:"h2",p:"p",...s(),...i.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(o,{of:p}),`
+`,n.jsx(w,{reason:"Needs work for narrow screens and integration with Mega Menu."}),`
+`,n.jsx(r,{children:j}),`
+`,n.jsx(m,{}),`
+`,n.jsx(h,{}),`
+`,n.jsx(e.h2,{id:"with-logo-variant",children:"With logo variant"}),`
+`,n.jsx(t,{of:d}),`
+`,n.jsx(e.h2,{id:"with-app-name",children:"With app name"}),`
+`,n.jsx(t,{of:l}),`
+`,n.jsx(e.h2,{id:"with-menu",children:"With menu"}),`
+`,n.jsx(t,{of:c}),`
+`,n.jsx(e.h2,{id:"with-links",children:"With links"}),`
+`,n.jsxs(e.p,{children:["Use a ",n.jsx(e.a,{href:"/docs/components-navigation-page-menu--docs",children:"Page Menu"}),` to add links.
+A Page Menu in the Header should not wrap.`]}),`
+`,n.jsx(t,{of:u}),`
+`,n.jsx(e.h2,{id:"with-links-and-menu",children:"With links and menu"}),`
+`,n.jsx(t,{of:f}),`
+`,n.jsx(e.h2,{id:"with-app-name-and-menu",children:"With app name and menu"}),`
+`,n.jsx(t,{of:g}),`
+`,n.jsx(e.h2,{id:"with-app-name-links-and-menu",children:"With app name, links and menu"}),`
+`,n.jsx(t,{of:x})]})}function z(i={}){const{wrapper:e}={...s(),...i.components};return e?n.jsx(e,{...i,children:n.jsx(a,{...i})}):a(i)}export{z as default};
---
+++ demo-DES-771-add-links-with-routers-docs/assets/using-links-with-routers.docs-YXMR9qpJ.js
@@ -0,0 +1,44 @@
+import{j as e}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as r}from"./index-BI1Biiay.js";import{ae as i}from"./index-DnZ3g-H1.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-Bbix2QIh.js";import"../sb-preview/runtime.js";import"./index-BU4L-DQy.js";import"./index-D-8MO0q_.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";/*@license CC0-1.0*/function o(t){const n={a:"a",code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...r(),...t.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(i,{title:"Docs/Developer Guide/Using links with routers"}),`
+`,e.jsx(n.h1,{id:"using-links-with-routers",children:"Using links with routers"}),`
+`,e.jsx(n.p,{children:`The Amsterdam Design System provides various link components that render native anchor tags,
+typically without extra features.
+You may need to integrate these components into a routing solution for your application.
+The link components in the Amsterdam Design System are designed for easy extension.
+Below are some examples for common routing solutions:`}),`
+`,e.jsx(n.h2,{id:"nextjs",children:"Next.js"}),`
+`,e.jsxs(n.p,{children:["You can hook into the Next router using the ",e.jsx(n.code,{children:"legacyBehaviour"})," and ",e.jsx(n.code,{children:"passHref"})," props on the Next link component, like so:"]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-javascript",children:`import { Link } from "@amsterdam/design-system-react";
+import NextLink from "next/link";
+
+<NextLink legacyBehavior passHref href="/amsterdam">
+  <Link>Link label</Link>
+</NextLink>;
+`})}),`
+`,e.jsx(n.h2,{id:"react-router",children:"React Router"}),`
+`,e.jsxs(n.p,{children:["React Router allows you to hook into their routing functionality with ",e.jsx(n.a,{href:"https://reactrouter.com/en/main/hooks/use-link-click-handler",rel:"nofollow",children:"the useLinkClickHandler hook"}),`.
+You can use this hook directly on a link component, or you can write a component which adds this functionality to the link component you pass to it, like so:`]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-javascript",children:`import { Link } from "@amsterdam/design-system-react";
+import { useLinkClickHandler, useHref } from "react-router-dom";
+
+const ReactRouterLink = ({ component, to, ...restProps }) => {
+  const Tag = component;
+  let href = useHref(to);
+  let handleClick = useLinkClickHandler(to);
+
+  return (
+    <Tag
+      href={href}
+      onClick={(event) => {
+        handleClick(event);
+      }}
+      {...restProps}
+    />
+  );
+};
+
+<ReactRouterLink component={Link} to="/amsterdam">
+  Link label
+</ReactRouterLink>;
+`})})]})}function f(t={}){const{wrapper:n}={...r(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(o,{...t})}):o(t)}export{f as default};
