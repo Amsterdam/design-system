2c8f6cabceebfbfa6224fcfac310285237f16a4e
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-07-25 15:41:23 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@dac1b998e1ff99b65ca8e9fe14c18540173d7868 🚀

---
+++ demo-design-token-previews/assets/IconButton.docs-BKPSYFth.js
@@ -0,0 +1,37 @@
+import{j as n}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as r}from"./index-BI1Biiay.js";import{ae as i,ak as a,al as l,am as c,an as t}from"./index-BmcjmK_5.js";import{I as d,O as h,a as u,L as m,b as x,c as p,d as j}from"./IconButton.stories-DjQ8AGxq.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-BZD4ZuRn.js";import"../sb-preview/runtime.js";import"./index-BU4L-DQy.js";import"./index-D-8MO0q_.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";import"./index.esm-Bsc2owIZ.js";import"./IconButton-Dm87s2wh.js";import"./clsx-B-dksMZM.js";import"./Icon-msbkstIT.js";const f=`<!-- @license CC0-1.0 -->
+
+# Icon Button
+
+A button containing only an icon.
+
+## Guidelines
+
+- Use this button only if the icon is entirely clear and understandable.
+  Many icons are interpreted differently by different users.
+  Only use universally recognized icons, such as a close icon or a play icon.
+- Provide a label text that describes what the button does for screen reader users.
+
+## Relevant WCAG requirements
+
+The Icon Button follows [the same requirements and guidelines as a regular button](/docs/components-buttons-button--docs).
+`;/*@license CC0-1.0*/function s(o){const e={h2:"h2",h3:"h3",p:"p",...r(),...o.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(i,{of:d}),`
+`,n.jsx(a,{children:f}),`
+`,n.jsx(l,{}),`
+`,n.jsx(c,{}),`
+`,n.jsx(e.h2,{id:"examples",children:"Examples"}),`
+`,n.jsx(e.h3,{id:"on-light-background",children:"On Light Background"}),`
+`,n.jsx(e.p,{children:"On a light (not white) background, the icon is black."}),`
+`,n.jsx(t,{of:h}),`
+`,n.jsx(e.h3,{id:"on-dark-background",children:"On Dark Background"}),`
+`,n.jsx(e.p,{children:"On a dark background, the icon is white."}),`
+`,n.jsx(t,{of:u}),`
+`,n.jsx(e.h3,{id:"level-3",children:"Level 3"}),`
+`,n.jsx(t,{of:m}),`
+`,n.jsx(e.h3,{id:"level-4",children:"Level 4"}),`
+`,n.jsx(t,{of:x}),`
+`,n.jsx(e.h3,{id:"level-5",children:"Level 5"}),`
+`,n.jsx(t,{of:p}),`
+`,n.jsx(e.h3,{id:"level-6",children:"Level 6"}),`
+`,n.jsx(t,{of:j})]})}function X(o={}){const{wrapper:e}={...r(),...o.components};return e?n.jsx(e,{...o,children:n.jsx(s,{...o})}):s(o)}export{X as default};
