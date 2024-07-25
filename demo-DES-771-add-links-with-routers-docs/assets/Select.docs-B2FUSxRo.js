a84f19dce53d856fb378589467f4c9e5cbfa0db9
alimpens <37216945+alimpens@users.noreply.github.com>
2024-07-25 15:27:24 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@a8cb2812ed722b04eef98aa90f7d8ba793a2e49c 🚀

---
+++ demo-DES-771-add-links-with-routers-docs/assets/Select.docs-B2FUSxRo.js
@@ -0,0 +1,28 @@
+import{j as e}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as i}from"./index-BI1Biiay.js";import{ae as r,ak as l,al as d,am as c,an as t}from"./index-x8uzgOe3.js";import{S as a,G as m,I as p,D as h}from"./Select.stories-CzWpM5MR.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-DKkNRsFz.js";import"../sb-preview/runtime.js";import"./index-BU4L-DQy.js";import"./index-D-8MO0q_.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";import"./clsx-B-dksMZM.js";const u=`<!-- @license CC0-1.0 -->
+
+# Select
+
+A form control that allows users to select one or more options from a list.
+
+## References
+
+- [MDN Web Docs](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/select)
+`;/*@license CC0-1.0*/function s(o){const n={code:"code",h1:"h1",h2:"h2",p:"p",...i(),...o.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(r,{of:a}),`
+`,e.jsx(l,{children:u}),`
+`,e.jsx(n.h1,{id:"default",children:"Default"}),`
+`,e.jsx(d,{}),`
+`,e.jsx(c,{}),`
+`,e.jsx(n.h2,{id:"multiple",children:"Multiple"}),`
+`,e.jsxs(n.p,{children:[`Avoid adding functionality to allow selecting multiple options. Multi select is harder to use
+on desktop as they require the user to hold down the `,e.jsx(n.code,{children:"Ctrl"})," or ",e.jsx(n.code,{children:"Cmd"}),` key while clicking on the options.
+It is recommended to use checkboxes instead.`]}),`
+`,e.jsx(n.h2,{id:"grouped",children:"Grouped"}),`
+`,e.jsxs(n.p,{children:["Use the ",e.jsx(n.code,{children:"Select.Group"}),` component to group options.
+The component requires a `,e.jsx(n.code,{children:"label"})," attribute."]}),`
+`,e.jsx(t,{of:m}),`
+`,e.jsx(n.h2,{id:"invalid",children:"Invalid"}),`
+`,e.jsx(t,{of:p}),`
+`,e.jsx(n.h2,{id:"disabled",children:"Disabled"}),`
+`,e.jsx(t,{of:h})]})}function E(o={}){const{wrapper:n}={...i(),...o.components};return n?e.jsx(n,{...o,children:e.jsx(s,{...o})}):s(o)}export{E as default};
