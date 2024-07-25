fc5b8a785b9f4975a5f25c2e628228e5002b6686
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-07-25 15:59:08 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@03ba3609115871e901463c56373f3863283c514b 🚀

---
+++ demo-design-token-previews/assets/Spotlight.docs-3VZwZyTd.js
@@ -0,0 +1,43 @@
+import{j as e}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as s}from"./index-BI1Biiay.js";import{ae as a,ak as i,al as l,am as c,an as t}from"./index-BD8_3uzK.js";import{S as m,B as d,D as h,a as p,G as u,M as x,O as j,P as f,Y as g,C as w}from"./Spotlight.stories-CCSD7o1w.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-BRhBXeT_.js";import"../sb-preview/runtime.js";import"./index-BU4L-DQy.js";import"./index-D-8MO0q_.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";import"./index.esm-DLRIDccq.js";import"./clsx-B-dksMZM.js";import"./index.esm-Bsc2owIZ.js";import"./exampleContent-DN6_nyQl.js";import"./Spotlight-IssvtslZ.js";const C=`<!-- @license CC0-1.0 -->
+
+# Spotlight
+
+Emphasizes a section on a page through a distinctive background colour.
+
+## Guidelines
+
+Refer to [this overview on Stijlweb](https://amsterdam.nl/stijlweb/basiselementen/kleuren/#PagCls_15671872) to determine whether you can use black or white text on the background colour of your choice.
+
+## Relevant WCAG requirements
+
+- [WCAG 1.4.3](https://www.w3.org/TR/WCAG21/#contrast-minimum): Large-scale text and images of large-scale text have a contrast ratio of at least 3:1;
+
+## References
+
+- [Color and contrast accessibility](https://web.dev/articles/color-and-contrast-accessibility)
+`;/*@license CC0-1.0*/function r(o){const n={code:"code",h3:"h3",p:"p",...s(),...o.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(a,{of:m}),`
+`,e.jsx(i,{children:C}),`
+`,e.jsx(l,{}),`
+`,e.jsx(c,{}),`
+`,e.jsx(n.h3,{id:"blue",children:"Blue"}),`
+`,e.jsx(t,{of:d}),`
+`,e.jsx(n.h3,{id:"dark-blue",children:"Dark Blue"}),`
+`,e.jsx(t,{of:h}),`
+`,e.jsx(n.h3,{id:"dark-green",children:"Dark Green"}),`
+`,e.jsx(t,{of:p}),`
+`,e.jsx(n.h3,{id:"green",children:"Green"}),`
+`,e.jsx(t,{of:u}),`
+`,e.jsx(n.h3,{id:"magenta",children:"Magenta"}),`
+`,e.jsx(t,{of:x}),`
+`,e.jsx(n.h3,{id:"orange",children:"Orange"}),`
+`,e.jsx(t,{of:j}),`
+`,e.jsx(n.h3,{id:"purple",children:"Purple"}),`
+`,e.jsx(t,{of:f}),`
+`,e.jsx(n.h3,{id:"yellow",children:"Yellow"}),`
+`,e.jsx(t,{of:g}),`
+`,e.jsx(n.h3,{id:"custom-html-element",children:"Custom HTML Element"}),`
+`,e.jsxs(n.p,{children:["By default, a Spotlight renders a ",e.jsx(n.code,{children:"<div>"}),`.
+Use the `,e.jsx(n.code,{children:"as"})," prop to make your markup more semantic."]}),`
+`,e.jsx(t,{of:w})]})}function X(o={}){const{wrapper:n}={...s(),...o.components};return n?e.jsx(n,{...o,children:e.jsx(r,{...o})}):r(o)}export{X as default};
