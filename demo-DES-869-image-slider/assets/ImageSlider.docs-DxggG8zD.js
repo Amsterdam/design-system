8c73094226941034d1714954558f13b58f2a0a49
dlnr <niels@dlnr.dev>
2024-07-26 12:24:41 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@d15603505382868da92fe320ac9574c39ac783b7 🚀

--- demo-DES-869-image-slider/assets/ImageSlider.docs-DxggG8zD.js
+++
@@ -1,9 +0,0 @@
-import{j as r}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as m}from"./index-BI1Biiay.js";import{ae as i,ak as e,al as s,am as p}from"./index-CO7ZUClF.js";import{I as a}from"./ImageSlider.stories-CnX0ju35.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-DImvER-l.js";import"../sb-preview/runtime.js";import"./index-BMdlDBjA.js";import"./index-D-8MO0q_.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";import"./index.esm-DY3iRp4W.js";import"./clsx-B-dksMZM.js";import"./IconButton-DaVLp41G.js";import"./Icon-msbkstIT.js";import"./AspectRatio-Btqti7q-.js";import"./Image-CVzrCme5.js";const x=`<!-- @license CC0-1.0 -->
-
-# Image Slider
-`;/*@license CC0-1.0*/function n(t){return r.jsxs(r.Fragment,{children:[`
-`,`
-`,r.jsx(i,{of:a}),`
-`,r.jsx(e,{children:x}),`
-`,r.jsx(s,{}),`
-`,r.jsx(p,{})]})}function R(t={}){const{wrapper:o}={...m(),...t.components};return o?r.jsx(o,{...t,children:r.jsx(n,{...t})}):n()}export{R as default};
---
+++ demo-DES-869-image-slider/assets/TableOfContents.docs-DTojvPbX.js
@@ -0,0 +1,14 @@
+import{j as t}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as s}from"./index-BI1Biiay.js";import{ae as r,ak as i,al as a,am as m,an as l}from"./index-MbvKOBBs.js";import{T as p,M as c}from"./TableOfContents.stories-DlJAF1z-.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-TE892xBv.js";import"../sb-preview/runtime.js";import"./index-BMdlDBjA.js";import"./index-D-8MO0q_.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";import"./clsx-B-dksMZM.js";import"./Heading-DUu-96hO.js";import"./getHeadingTag-Dv1eMqvl.js";const f=`<!-- @license CC0-1.0 -->
+
+# Table of Contents
+
+A list of links corresponding to the content sections on the page.
+It helps users to easily navigate to different sections on the same page.
+`;/*@license CC0-1.0*/function e(n){const o={h2:"h2",...s(),...n.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(r,{of:p}),`
+`,t.jsx(i,{children:f}),`
+`,t.jsx(a,{}),`
+`,t.jsx(m,{}),`
+`,t.jsx(o.h2,{id:"multilevel",children:"Multilevel"}),`
+`,t.jsx(l,{of:c})]})}function b(n={}){const{wrapper:o}={...s(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(e,{...n})}):e(n)}export{b as default};
