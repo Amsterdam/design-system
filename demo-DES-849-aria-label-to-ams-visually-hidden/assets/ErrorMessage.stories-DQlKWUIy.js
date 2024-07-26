641c2030e466bb6f8d0409aeb93d95bd687704de
GitHub <github-action@users.noreply.github.com>
2024-07-26 08:11:53 +0000

    Remove feature branch folder

--- demo-DES-849-aria-label-to-ams-visually-hidden/assets/ErrorMessage.stories-DQlKWUIy.js
+++
@@ -1,9 +0,0 @@
-import{j as o}from"./jsx-runtime-BlAj40OV.js";import{c as g}from"./clsx-B-dksMZM.js";import{r as h}from"./index-Cs7sjTYM.js";const s=h.forwardRef(({children:a,className:d,prefix:p="Invoerfout",...u},f)=>o.jsxs("p",{...u,ref:f,className:g("ams-error-message",d),children:[o.jsxs("span",{className:"ams-visually-hidden",children:[p,": "]}),a]}));s.displayName="ErrorMessage";try{s.displayName="ErrorMessage",s.__docgenInfo={description:"",displayName:"ErrorMessage",props:{prefix:{defaultValue:{value:"Invoerfout"},description:"An accessible phrase that screen readers announce before the error message. Should translate to something like ‘input error’.",name:"prefix",required:!1,type:{name:"string"}}}}}catch{}/**
- * @license EUPL-1.2+
- * Copyright Gemeente Amsterdam
- */const x={title:"Components/Forms/Error Message",component:s,args:{children:"Vul een geldig e-mailadres in, bijvoorbeeld naam@voorbeeld.nl."},argTypes:{children:{table:{disable:!1}}}},e={},r={args:{children:"Enter an email address in the correct format, like name@example.com",prefix:"Error"}};var t,n,i;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:"{}",...(i=(n=e.parameters)==null?void 0:n.docs)==null?void 0:i.source}}};var c,l,m;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:`{
-  args: {
-    children: 'Enter an email address in the correct format, like name@example.com',
-    prefix: 'Error'
-  }
-}`,...(m=(l=r.parameters)==null?void 0:l.docs)==null?void 0:m.source}}};const E=["Default","WithCustomPrefix"],j=Object.freeze(Object.defineProperty({__proto__:null,Default:e,WithCustomPrefix:r,__namedExportsOrder:E,default:x},Symbol.toStringTag,{value:"Module"}));export{j as E,r as W};
