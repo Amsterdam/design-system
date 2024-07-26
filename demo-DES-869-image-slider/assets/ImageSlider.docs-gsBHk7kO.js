e353ff87f39847670d8a663248454feb43e52f82
dlnr <niels@dlnr.dev>
2024-07-26 10:11:02 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@15f4794d1896cfca31aa01b8c6cb34477c4a3cb0 🚀

--- demo-DES-869-image-slider/assets/ImageSlider.docs-gsBHk7kO.js
+++
@@ -1,9 +0,0 @@
-import{j as r}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as m}from"./index-BI1Biiay.js";import{ae as i,ak as e,al as s,am as p}from"./index-CXye8nW0.js";import{I as a}from"./ImageSlider.stories-BcGn0t_b.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-BK6sAcAF.js";import"../sb-preview/runtime.js";import"./index-BMdlDBjA.js";import"./index-D-8MO0q_.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";import"./index.esm-DY3iRp4W.js";import"./clsx-B-dksMZM.js";import"./IconButton-DaVLp41G.js";import"./Icon-msbkstIT.js";import"./AspectRatio-Cd0KgYDn.js";import"./Image-CVzrCme5.js";const x=`<!-- @license CC0-1.0 -->
-
-# Image Slider
-`;/*@license CC0-1.0*/function n(t){return r.jsxs(r.Fragment,{children:[`
-`,`
-`,r.jsx(i,{of:a}),`
-`,r.jsx(e,{children:x}),`
-`,r.jsx(s,{}),`
-`,r.jsx(p,{})]})}function R(t={}){const{wrapper:o}={...m(),...t.components};return o?r.jsx(o,{...t,children:r.jsx(n,{...t})}):n()}export{R as default};
