d776a24a3eb178952b3df0bd5c11f0145b39132e
Ruben Sibon <r.sibon@amsterdam.nl>
2024-08-07 14:29:12 +0200

    feat(label): 💄 apply not-required label design

--- packages/react/src/FieldSet/FieldSet.tsx
+++ packages/react/src/FieldSet/FieldSet.tsx
@@ -6,16 +6,17 @@
 import clsx from 'clsx'
 import { forwardRef } from 'react'
 import type { ForwardedRef, HTMLAttributes, PropsWithChildren } from 'react'
+import { LabelOptionalFlag } from '../Label/Label'
 
 export type FieldSetProps = PropsWithChildren<HTMLAttributes<HTMLFieldSetElement>> & {
   /** Whether the field set has an input with a validation error */
   invalid?: boolean
   /** The text for the caption. */
   legend: string
-  /** Whether the field set is not required. */
-  notRequired?: boolean
   /** Text to inform the user that the field is explicitely not required. */
   notRequiredLabel?: string
+  /** Whether the field set is required. */
+  required?: boolean
 }
 
 export const FieldSet = forwardRef(
@@ -25,8 +26,8 @@ export const FieldSet = forwardRef(
       className,
       invalid,
       legend,
+      required,
       notRequiredLabel = 'niet verplicht',
-      notRequired,
       ...restProps
     }: FieldSetProps,
     ref: ForwardedRef<HTMLFieldSetElement>,
@@ -38,11 +39,7 @@ export const FieldSet = forwardRef(
     >
       <legend className="ams-field-set__legend">
         {legend}
-        {notRequired === true && (
-          <em className="ams-field-set__legend--not-required">
-            <span className="ams-visually-hidden">,</span> ({notRequiredLabel})
-          </em>
-        )}
+        {!required && notRequiredLabel && <LabelOptionalFlag notRequiredLabel={notRequiredLabel} />}
       </legend>
       {children}
     </fieldset>
--- packages/react/src/Label/Label.tsx
+++ packages/react/src/Label/Label.tsx
@@ -8,13 +8,27 @@ import clsx from 'clsx'
 import { forwardRef } from 'react'
 import type { ForwardedRef, LabelHTMLAttributes, PropsWithChildren } from 'react'
 
+export type LabelProps = PropsWithChildren<LabelHTMLAttributes<HTMLLabelElement>> & {
+  /** Text to inform the user that the field is explicitely not required. */
+  notRequiredLabel?: string
+  /** Whether the field set is required. */
+  required?: boolean
+}
+
+export const LabelOptionalFlag = ({ notRequiredLabel }: LabelProps) => (
+  <span className="ams-label__not-required">
+    <span className="ams-visually-hidden">,</span> ({notRequiredLabel})
+  </span>
+)
+
 export const Label = forwardRef(
   (
-    { children, className, ...restProps }: PropsWithChildren<LabelHTMLAttributes<HTMLLabelElement>>,
+    { children, className, notRequiredLabel = 'niet verplicht', required, ...restProps }: LabelProps,
     ref: ForwardedRef<HTMLLabelElement>,
   ) => (
     <label {...restProps} ref={ref} className={clsx('ams-label', className)}>
       {children}
+      {!required && notRequiredLabel && <LabelOptionalFlag notRequiredLabel={notRequiredLabel} />}
     </label>
   ),
 )
