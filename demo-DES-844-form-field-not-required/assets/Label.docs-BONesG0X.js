ba45a98c559b0d9302b0fab7ab2b71fba5e36262
RubenSibon <mail@rubensibon.nl>
2024-08-07 13:56:49 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@f2bd8563f4da5ec208f10eaee47faf667dfbf51a 🚀

---
+++ demo-DES-844-form-field-not-required/assets/Label.docs-BONesG0X.js
@@ -0,0 +1,16 @@
+import{j as t}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as o}from"./index-BI1Biiay.js";import{ae as a,ak as s,al as i,am as m}from"./index-CG8kh7ft.js";import{L as l}from"./Label.stories-D3Ayw1Yw.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-BM588qxZ.js";import"../sb-preview/runtime.js";import"./index-BU4L-DQy.js";import"./index-D-8MO0q_.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";import"./Label-nD_f5r9k.js";import"./clsx-B-dksMZM.js";const p=`<!-- @license CC0-1.0 -->
+
+# Label
+
+Describes a form control.
+
+## Guidelines
+
+- Always associate a form element (such as an \`input\` or \`textarea\`) with a label.
+- If the associated form element gets a \`required\` attribute, add it to the label with the same value as well.
+`;/*@license CC0-1.0*/function r(e){return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(a,{of:l}),`
+`,t.jsx(s,{children:p}),`
+`,t.jsx(i,{}),`
+`,t.jsx(m,{})]})}function E(e={}){const{wrapper:n}={...o(),...e.components};return n?t.jsx(n,{...e,children:t.jsx(r,{...e})}):r()}export{E as default};
