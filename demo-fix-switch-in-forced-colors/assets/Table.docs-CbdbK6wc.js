d0dbf4fb7fc205ebad580e89b758ac9de1629187
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-08-24 08:29:00 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@c779e99931962e3e9e8b136ac407ac4be9db3321 🚀

---
+++ demo-fix-switch-in-forced-colors/assets/Table.docs-CbdbK6wc.js
@@ -0,0 +1,24 @@
+import{j as e}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as s}from"./index-BI1Biiay.js";import{ae as a,ak as r,al as i,an as d}from"./index-Chk_JphO.js";import{T as m,W as l}from"./Table.stories-BUauZ9r1.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-D5VwsADQ.js";import"../sb-preview/runtime.js";import"./index-BU4L-DQy.js";import"./index-D-8MO0q_.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";import"./clsx-B-dksMZM.js";const c=`<!-- @license CC0-1.0 -->
+
+# Table
+
+Use the table component to let users compare information in rows and columns.
+Table is used to display simple information that does not need to be filtered or edited.
+
+## Guidelines
+
+- Use \`<Table.Caption>\` to describe a table in the same way you would use a heading.
+  A caption helps users find, navigate and understand tables.
+- Never use the table component to layout content on a page.
+  Instead, use [Grid](/docs/components-layout-grid--docs).
+`;/*@license CC0-1.0*/function t(o){const n={code:"code",h2:"h2",h3:"h3",p:"p",...s(),...o.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(a,{of:m}),`
+`,e.jsx(r,{children:c}),`
+`,e.jsx(i,{}),`
+`,e.jsx(n.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(n.h3,{id:"with-footer-and-row-headers",children:"With footer and row headers"}),`
+`,e.jsxs(n.p,{children:[`Use table headers to tell users what the rows and columns represent.
+Use the scope attribute to help users of assistive technology distinguish between row and column headers.
+Use `,e.jsx(n.code,{children:"Table.Footer"})," for a summary row."]}),`
+`,e.jsx(d,{of:l})]})}function U(o={}){const{wrapper:n}={...s(),...o.components};return n?e.jsx(n,{...o,children:e.jsx(t,{...o})}):t(o)}export{U as default};
