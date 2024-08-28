9ccb77b53cbc605c348a8ce4e3c06022cf54ba80
alimpens <a.limpens@amsterdam.nl>
2024-08-28 13:32:11 +0200

    Add Field stories to Text Area, fix ids

--- storybook/src/components/TextInput/TextInput.stories.tsx
+++ storybook/src/components/TextInput/TextInput.stories.tsx
@@ -93,12 +93,12 @@ export const InAnInvalidField: Story = {
   },
   render: (args) => (
     <Field invalid={args.invalid}>
-      <Label htmlFor="input1">Label</Label>
-      <Paragraph id="description1" size="small">
+      <Label htmlFor="input2">Label</Label>
+      <Paragraph id="description2" size="small">
         Omschrijving.
       </Paragraph>
-      {args.invalid && <ErrorMessage id="error1">Foutmelding.</ErrorMessage>}
-      <TextInput aria-describedby={`description1${args.invalid ? ' error1' : ''}`} id="input1" {...args} />
+      {args.invalid && <ErrorMessage id="error2">Foutmelding.</ErrorMessage>}
+      <TextInput aria-describedby={`description2${args.invalid ? ' error2' : ''}`} id="input2" {...args} />
     </Field>
   ),
 }
