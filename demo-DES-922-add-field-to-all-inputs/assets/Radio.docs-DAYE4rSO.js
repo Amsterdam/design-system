99cd938221743fbd517a8612f5274fcdbbc7310a
alimpens <37216945+alimpens@users.noreply.github.com>
2024-08-30 10:56:58 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@671709d166b0ff6eca3257fdb0ce922a6bcc15e1 🚀

---
+++ demo-DES-922-add-field-to-all-inputs/assets/Radio.docs-DAYE4rSO.js
@@ -0,0 +1,37 @@
+import{j as e}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as s}from"./index-BI1Biiay.js";import{ae as r,ak as a,al as d,am as l,an as i}from"./index-B6bc5iYf.js";import{R as c,I as h,a as p}from"./Radio.stories-DZpLEmrC.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-CKtbkZql.js";import"../sb-preview/runtime.js";import"./index-BU4L-DQy.js";import"./index-D-8MO0q_.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";import"./index.esm-BEwyunnw.js";import"./clsx-B-dksMZM.js";import"./index.esm-Bsc2owIZ.js";const m=`<!-- @license CC0-1.0 -->
+
+# Radio
+
+Allows users to select one option from a list.
+
+## Guidelines
+
+- Do not assume that users will know how many options they can select based on the visual difference between Radios and Checkboxes alone.
+  If needed, add a hint explaining this, for example, ‘Select one option’.
+- Order Radios alphabetically by default.
+  In some cases, it can be helpful to order them from most-to-least common options.
+  For example, you could order options for ‘Where do you live?’ based on population size.
+  However you should do this with extreme caution as it can reinforce bias.
+  If in doubt, order alphabetically.
+- Labels should be concise. Try to keep labels shorter than 4 words.
+
+Use a [Checkbox](/docs/components-forms-checkbox--docs) when a user can select more than 1 option from a list.
+`;/*@license CC0-1.0*/function t(n){const o={a:"a",code:"code",h2:"h2",h3:"h3",p:"p",...s(),...n.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(r,{of:c}),`
+`,e.jsx(a,{children:m}),`
+`,e.jsx(d,{}),`
+`,e.jsx(l,{}),`
+`,e.jsx(o.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(o.h3,{id:"in-a-field-set",children:"In a Field Set"}),`
+`,e.jsx(o.p,{children:"Use a Field Set to group several Radios with a legend, description and / or an error message."}),`
+`,e.jsxs(o.p,{children:["When grouping Radios, add ",e.jsx(o.code,{children:'role="radiogroup"'}),` to the Field Set to have it explicitly announced as a radio group (the default role is just ‘group’).
+The ‘radio group’ role also allows using `,e.jsx(o.code,{children:"aria-required"}),` on Field Set; that isn’t allowed for the ‘group’ role.
+Always also set `,e.jsx(o.code,{children:"aria-required"})," on the individual Radios though, to make sure it’s read by screen readers."]}),`
+`,e.jsxs(o.p,{children:["If you don’t need the description, just take out the description Paragraph and the ",e.jsx(o.code,{children:"aria-describedby"})," from the Field Set."]}),`
+`,e.jsxs(o.p,{children:["Check ",e.jsx(o.a,{href:"/docs/components-forms-field-set--docs",children:"Field Set"})," for more information on configuring it."]}),`
+`,e.jsx(i,{of:h}),`
+`,e.jsx(o.h3,{id:"in-a-field-set-with-validation",children:"In a Field Set With Validation"}),`
+`,e.jsxs(o.p,{children:["If the Radio can become invalid, add an Error Message component and its ",e.jsx(o.code,{children:"id"})," to the ",e.jsx(o.code,{children:"aria-describedby"})," prop of the Field Set."]}),`
+`,e.jsxs(o.p,{children:["For more information, check ",e.jsx(o.a,{href:"/docs/components-forms-field-set--docs",children:"the Field Set component documentation"}),"."]}),`
+`,e.jsx(i,{of:p})]})}function M(n={}){const{wrapper:o}={...s(),...n.components};return o?e.jsx(o,{...n,children:e.jsx(t,{...n})}):t(n)}export{M as default};
