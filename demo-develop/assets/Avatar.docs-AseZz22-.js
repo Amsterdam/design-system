2bb4b2be68cfe8de189d1970b4ea5bfe92bf3c70
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-09-03 08:51:00 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@96524b0e2f9122e43e6d9d73160ca50cd0c68633 🚀

---
+++ demo-develop/assets/Avatar.docs-AseZz22-.js
@@ -0,0 +1,32 @@
+import{j as n}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as o}from"./index-BI1Biiay.js";import{ae as i,ak as s,al as c,am as l,an as a}from"./index-CsfDCjl3.js";import{A as p,W as m,F as d,I as h}from"./Avatar.stories-CmxAyg0l.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-Bcq9_DtR.js";import"../sb-preview/runtime.js";import"./index-BU4L-DQy.js";import"./index-D-8MO0q_.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";import"./index.esm-BWPyxIsd.js";import"./clsx-B-dksMZM.js";import"./index.esm-Bsc2owIZ.js";import"./Image-CVzrCme5.js";import"./Icon-msbkstIT.js";const u=`<!-- @license CC0-1.0 -->
+
+# Avatar
+
+A circular badge representing a person.
+
+## Design
+
+The avatar contains 1 or 2 initial letters from the person's full name, a picture, or a generic icon.
+The default background colour is dark blue.
+
+## Usage
+
+Display an avatar for the person currently using the application,
+or to associate a person with a content item.
+`;/*@license CC0-1.0*/function t(r){const e={h2:"h2",h3:"h3",p:"p",...o(),...r.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(i,{of:p}),`
+`,n.jsx(s,{children:u}),`
+`,n.jsx(e.h2,{id:"stories",children:"Stories"}),`
+`,n.jsx(e.h3,{id:"default",children:"Default"}),`
+`,n.jsx(c,{}),`
+`,n.jsx(l,{}),`
+`,n.jsx(e.h3,{id:"with-picture",children:"With Picture"}),`
+`,n.jsx(e.p,{children:`The Avatar can also display a photo or other image for the person.
+Make sure to scale the image down to around 100 pixels to prevent unnecessary data transfers.`}),`
+`,n.jsx(a,{of:m}),`
+`,n.jsx(e.h3,{id:"fallback-icon",children:"Fallback Icon"}),`
+`,n.jsx(e.p,{children:"A user icon displays if no label and image are provided."}),`
+`,n.jsx(a,{of:d}),`
+`,n.jsx(e.h3,{id:"in-a-header",children:"In a Header"}),`
+`,n.jsx(a,{of:h})]})}function T(r={}){const{wrapper:e}={...o(),...r.components};return e?n.jsx(e,{...r,children:n.jsx(t,{...r})}):t(r)}export{T as default};
