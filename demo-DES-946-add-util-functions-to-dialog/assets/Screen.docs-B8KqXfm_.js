83cff8ed976b335416a9acbb53f382a5f052e5ea
alimpens <37216945+alimpens@users.noreply.github.com>
2024-09-10 13:39:31 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@ffd67d427fb07ae836b706ca31445cba824a1615 🚀

---
+++ demo-DES-946-add-util-functions-to-dialog/assets/Screen.docs-B8KqXfm_.js
@@ -0,0 +1,31 @@
+import{j as n}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as s}from"./index-BI1Biiay.js";import{ae as i,ak as r,al as a,am as m,an as c}from"./index-BBXGf4Cd.js";import{S as d,E as p}from"./Screen.stories-DN8CPv8z.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-CZQMnvyq.js";import"../sb-preview/runtime.js";import"./index-BU4L-DQy.js";import"./index-D-8MO0q_.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";import"./clsx-B-dksMZM.js";const h=`<!-- @license CC0-1.0 -->
+
+# Screen
+
+Manages the maximum width and alignment of the entire website or application.
+
+## Guidelines
+
+- This should be the outermost component of your website or application.
+- Within it, combine components such as
+  [Grid](/docs/components-layout-grid--docs),
+  [Header](/docs/components-containers-header--docs),
+  [Footer](/docs/components-containers-footer--docs),
+  [Spotlight](/docs/components-containers-spotlight--docs),
+  and Figure.
+
+## Design
+
+By default, this component expands to a width of 1600 pixels.
+This is the maximum width for a website of the City of Amsterdam.
+For applications, you can choose a larger maximum width of 2112 pixels.
+The component horizontally centres both itself and its content.
+`;/*@license CC0-1.0*/function e(o){const t={h2:"h2",h3:"h3",...s(),...o.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(i,{of:d}),`
+`,n.jsx(r,{children:h}),`
+`,n.jsx(a,{}),`
+`,n.jsx(m,{}),`
+`,n.jsx(t.h2,{id:"examples",children:"Examples"}),`
+`,n.jsx(t.h3,{id:"extra-wide",children:"Extra wide"}),`
+`,n.jsx(c,{of:p})]})}function S(o={}){const{wrapper:t}={...s(),...o.components};return t?n.jsx(t,{...o,children:n.jsx(e,{...o})}):e(o)}export{S as default};
---
+++ demo-DES-946-add-util-functions-to-dialog/assets/TextArea.docs-gXdYWwJd.js
@@ -0,0 +1,43 @@
+import{j as e}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as t}from"./index-BI1Biiay.js";import{ae as r,ak as a,al as d,am as l,an as i}from"./index-BBXGf4Cd.js";import{T as h,V as c,H as m,N as x,I as p,D as f,a as j,b as u}from"./TextArea.stories-BfuRCj6d.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-CZQMnvyq.js";import"../sb-preview/runtime.js";import"./index-BU4L-DQy.js";import"./index-D-8MO0q_.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";import"./index.esm-kq7Krebn.js";import"./clsx-B-dksMZM.js";import"./index.esm-Bsc2owIZ.js";import"./exampleContent-hIIPvEhU.js";const b=`<!-- @license CC0-1.0 -->
+
+# TextArea
+
+A form field that allows the user to input text over multiple lines.
+
+## Guidelines
+
+- Use a TextArea when users need to enter more than 1 sentence of text, such as a comment or description.
+- The height of the TextArea should be appropriate for the information to be entered.
+- A TextArea must have a label, and in most cases, this label should be visible.
+- Use \`spellcheck="false"\` for fields that may contain sensitive information, such as passwords and personal data.
+  Some browser extensions for spell-checking send this information to external servers.
+
+## Accessibility
+
+- [WCAG 2.1](https://www.w3.org/WAI/WCAG21/Techniques/html/H91.html) requires that the TextArea has a label or title attribute.
+`;/*@license CC0-1.0*/function o(s){const n={a:"a",code:"code",h2:"h2",h3:"h3",h4:"h4",p:"p",...t(),...s.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(r,{of:h}),`
+`,e.jsx(a,{children:b}),`
+`,e.jsx(d,{}),`
+`,e.jsx(l,{}),`
+`,e.jsx(n.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(n.h3,{id:"vertical-resize",children:"Vertical resize"}),`
+`,e.jsx(i,{of:c}),`
+`,e.jsx(n.h3,{id:"horizontal-resize",children:"Horizontal resize"}),`
+`,e.jsx(i,{of:m}),`
+`,e.jsx(n.h3,{id:"no-resize",children:"No resize"}),`
+`,e.jsx(i,{of:x}),`
+`,e.jsx(n.h3,{id:"invalid",children:"Invalid"}),`
+`,e.jsx(i,{of:p}),`
+`,e.jsx(n.h3,{id:"disabled",children:"Disabled"}),`
+`,e.jsx(i,{of:f}),`
+`,e.jsx(n.h4,{id:"in-a-field",children:"In a Field"}),`
+`,e.jsx(n.p,{children:"Use a Field to group a Text Area with a Label, description and / or an Error Message."}),`
+`,e.jsxs(n.p,{children:["If you don’t need the description, remove its Paragraph and the ",e.jsx(n.code,{children:"aria-describedby"})," from the Text Input."]}),`
+`,e.jsxs(n.p,{children:["Check ",e.jsx(n.a,{href:"/docs/components-forms-field--docs",children:"the Field docs"})," for more information on configuring it."]}),`
+`,e.jsx(i,{of:j}),`
+`,e.jsx(n.h4,{id:"in-a-field-with-validation",children:"In a Field with validation"}),`
+`,e.jsxs(n.p,{children:["If the Text Area can become invalid, add an Error Message and its ",e.jsx(n.code,{children:"id"})," to the ",e.jsx(n.code,{children:"aria-describedby"})," prop of the Text Area."]}),`
+`,e.jsxs(n.p,{children:["Check ",e.jsx(n.a,{href:"/docs/components-forms-field--docs",children:"the Field docs"})," for more information on configuring it."]}),`
+`,e.jsx(i,{of:u})]})}function V(s={}){const{wrapper:n}={...t(),...s.components};return n?e.jsx(n,{...s,children:e.jsx(o,{...s})}):o(s)}export{V as default};
