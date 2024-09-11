f25bc016f1cea8c81e4b341af515fcad2f00a416
Aram <37216945+alimpens@users.noreply.github.com>
2024-09-11 10:05:34 +0200

    feat: Add utility functions as methods to Dialog (#1576)

--- packages/react/src/Dialog/Dialog.tsx
+++ packages/react/src/Dialog/Dialog.tsx
@@ -18,11 +18,10 @@ export type DialogProps = {
   heading: string
 } & PropsWithChildren<DialogHTMLAttributes<HTMLDialogElement>>
 
-export const closeDialog = (event: MouseEvent<HTMLButtonElement>) => event.currentTarget.closest('dialog')?.close()
+const closeDialog = (event: MouseEvent<HTMLButtonElement>) => event.currentTarget.closest('dialog')?.close()
+const openDialog = (id: string) => (document.querySelector(id) as HTMLDialogElement)?.showModal()
 
-export const openDialog = (id: string) => (document.querySelector(id) as HTMLDialogElement)?.showModal()
-
-export const Dialog = forwardRef(
+const DialogRoot = forwardRef(
   (
     { actions, children, className, closeButtonLabel = 'Sluiten', heading, ...restProps }: DialogProps,
     ref: ForwardedRef<HTMLDialogElement>,
@@ -40,4 +39,9 @@ export const Dialog = forwardRef(
   ),
 )
 
-Dialog.displayName = 'Dialog'
+DialogRoot.displayName = 'Dialog'
+
+export const Dialog = Object.assign(DialogRoot, {
+  close: closeDialog,
+  open: openDialog,
+})
