f28e804ea09d780fc863dec7d69015c75429a0da
Vincent Smedinga <v.smedinga@amsterdam.nl>
2024-09-11 17:20:37 +0200

    Describe and move the example

--- storybook/src/components/Spotlight/Spotlight.stories.tsx
+++ storybook/src/components/Spotlight/Spotlight.stories.tsx
@@ -37,30 +37,6 @@ type Story = StoryObj<typeof meta>
 
 export const Default: Story = {}
 
-export const StickOut: Story = {
-  render: ({ as, color }) => (
-    <Grid paddingBottom="medium">
-      <Grid.Cell coverGap rowSpan={{ narrow: 2, medium: 2, wide: 1 }} rowStart={2} span="all">
-        <Spotlight as={as} color={color} style={{ height: '100%' }} />
-      </Grid.Cell>
-      <Grid.Cell rowStart={{ narrow: 3, medium: 3, wide: 2 }} span={{ narrow: 4, medium: 8, wide: 6 }} start={1}>
-        <Paragraph inverseColor={!color || !['green', 'yellow'].includes(color)}>{exampleParagraph()}</Paragraph>
-      </Grid.Cell>
-      <Grid.Cell
-        rowSpan={2}
-        rowStart={1}
-        span={{ narrow: 4, medium: 8, wide: 6 }}
-        start={{ narrow: 1, medium: 1, wide: 7 }}
-        style={{ alignSelf: 'end' }}
-      >
-        <AspectRatio ratio="wide">
-          <Image alt="" src="https://picsum.photos/960/720" />
-        </AspectRatio>
-      </Grid.Cell>
-    </Grid>
-  ),
-}
-
 export const Blue: Story = {
   args: {
     color: 'blue',
@@ -107,6 +83,30 @@ export const Yellow: Story = {
   args: {
     color: 'yellow',
   },
+}
+
+export const StickOutContent: Story = {
+  render: ({ as, color }) => (
+    <Grid paddingBottom="medium">
+      <Grid.Cell coverGap rowSpan={{ narrow: 2, medium: 2, wide: 1 }} rowStart={2} span="all">
+        <Spotlight as={as} color={color} style={{ height: '100%' }} />
+      </Grid.Cell>
+      <Grid.Cell rowStart={{ narrow: 3, medium: 3, wide: 2 }} span={{ narrow: 4, medium: 8, wide: 6 }} start={1}>
+        <Paragraph inverseColor={!color || !['green', 'yellow'].includes(color)}>{exampleParagraph()}</Paragraph>
+      </Grid.Cell>
+      <Grid.Cell
+        rowSpan={2}
+        rowStart={1}
+        span={{ narrow: 4, medium: 8, wide: 6 }}
+        start={{ narrow: 1, medium: 1, wide: 7 }}
+        style={{ alignSelf: 'end' }}
+      >
+        <AspectRatio ratio="square">
+          <Image alt="" src="https://picsum.photos/960/960" />
+        </AspectRatio>
+      </Grid.Cell>
+    </Grid>
+  ),
 }
 
 export const ImproveSemantics: Story = {
