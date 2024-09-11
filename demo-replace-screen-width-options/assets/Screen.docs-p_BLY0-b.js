268aa0c34d02064d4fe3b569719021dba2412657
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-09-11 16:28:07 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@953beab0500c416913afdb2c20d06417db36e9e1 🚀

---
+++ demo-replace-screen-width-options/assets/Screen.docs-p_BLY0-b.js
@@ -0,0 +1,30 @@
+import{j as n}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as s}from"./index-BI1Biiay.js";import{ae as i,ak as r,al as a,am as m,an as c}from"./index-wzvzuYbI.js";import{S as d,F as p}from"./Screen.stories-VS9zA1O5.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-B1v1jGqS.js";import"../sb-preview/runtime.js";import"./index-BU4L-DQy.js";import"./index-D-8MO0q_.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";import"./clsx-B-dksMZM.js";const h=`<!-- @license CC0-1.0 -->
+
+# Screen
+
+Manages the maximum width and alignment of the entire website or application.
+
+## Guidelines
+
+- This should be the outermost component of your website or application.
+- Within it, combine components such as
+  [Grid](/docs/components-layout-grid--docs),
+  [Header](/docs/components-containers-header--docs),
+  [Footer](/docs/components-containers-footer--docs),
+  [Spotlight](/docs/components-containers-spotlight--docs),
+  and Figure.
+
+## Design
+
+The Screen component expands to 1600 pixels wide.
+This is the maximum width for a website of the City of Amsterdam.
+The component horizontally centres itself.
+`;/*@license CC0-1.0*/function t(o){const e={h2:"h2",h3:"h3",...s(),...o.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(i,{of:d}),`
+`,n.jsx(r,{children:h}),`
+`,n.jsx(a,{}),`
+`,n.jsx(m,{}),`
+`,n.jsx(e.h2,{id:"examples",children:"Examples"}),`
+`,n.jsx(e.h3,{id:"full-width",children:"Full width"}),`
+`,n.jsx(c,{of:p})]})}function D(o={}){const{wrapper:e}={...s(),...o.components};return e?n.jsx(e,{...o,children:n.jsx(t,{...o})}):t(o)}export{D as default};
---
+++ demo-replace-screen-width-options/assets/Screen.stories-VS9zA1O5.js
@@ -0,0 +1,10 @@
+import{j as s}from"./jsx-runtime-BlAj40OV.js";import{c as g}from"./clsx-B-dksMZM.js";import{r as v}from"./index-Cs7sjTYM.js";const t=v.forwardRef(({children:a,className:m,fullHeight:u,fullWidth:h,...p},f)=>s.jsx("div",{...p,ref:f,className:g("ams-screen",u&&"ams-screen--full-height",h&&"ams-screen--full-width",m),children:a}));t.displayName="Screen";try{t.displayName="Screen",t.__docgenInfo={description:"",displayName:"Screen",props:{fullHeight:{defaultValue:null,description:"Whether the screen should stretch to the full height of the viewport.",name:"fullHeight",required:!1,type:{name:"boolean"}},fullWidth:{defaultValue:null,description:"Whether the screen should stretch to the full width of the viewport.",name:"fullWidth",required:!1,type:{name:"boolean"}}}}}catch{}const _={title:"Components/Layout/Screen",component:t,parameters:{layout:"fullscreen"}},e={args:{children:s.jsx("div",{className:"ams-docs-item",children:"Ik dwing een maximale breedte af."})}},r={args:{children:s.jsx("div",{className:"ams-docs-item",children:"Ik bezet de volledige breedte van het venster."}),fullWidth:!0}};var l,o,n;e.parameters={...e.parameters,docs:{...(l=e.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    children: <div className="ams-docs-item">Ik dwing een maximale breedte af.</div>
+  }
+}`,...(n=(o=e.parameters)==null?void 0:o.docs)==null?void 0:n.source}}};var c,d,i;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    children: <div className="ams-docs-item">Ik bezet de volledige breedte van het venster.</div>,
+    fullWidth: true
+  }
+}`,...(i=(d=r.parameters)==null?void 0:d.docs)==null?void 0:i.source}}};const S=["Default","FullWidth"],N=Object.freeze(Object.defineProperty({__proto__:null,Default:e,FullWidth:r,__namedExportsOrder:S,default:_},Symbol.toStringTag,{value:"Module"}));export{r as F,N as S};
