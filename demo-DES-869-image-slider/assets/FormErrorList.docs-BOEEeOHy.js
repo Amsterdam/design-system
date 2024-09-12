5a5bdc48daaacd3e089c80f9e7210ba63828dfa7
dlnr <niels@dlnr.dev>
2024-09-12 14:45:26 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@d063e4aaa846dc6a760b641d3c96dc05e6ee5127 🚀

---
+++ demo-DES-869-image-slider/assets/FormErrorList.docs-BOEEeOHy.js
@@ -0,0 +1,53 @@
+import{j as e}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as o}from"./index-BI1Biiay.js";import{ae as i,ak as s,al as a,am as m}from"./index-Cplyd0ax.js";import{F as h}from"./FormErrorList.stories-Cx6PFlaE.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-DOfUAbpa.js";import"../sb-preview/runtime.js";import"./index-Cf-03bMR.js";import"./index-D-8MO0q_.js";import"./index-BVoBHvaS.js";import"./index-DrFu-skq.js";import"./clsx-B-dksMZM.js";import"./Alert-Dt6KCQuZ.js";import"./index.esm-Bsc2owIZ.js";import"./Icon-msbkstIT.js";import"./Heading-DUu-96hO.js";import"./getHeadingTag-Dv1eMqvl.js";import"./IconButton-BgcGyAbu.js";import"./LinkList-mVZ7vby0.js";const l=`<!-- @license CC0-1.0 -->
+
+# Form Error List
+
+Use this component at the top of a page to summarise any errors a user has made.
+When a user makes an error, you must show both a Form Error List and an Error Message above each answer that contains an error.
+
+## Guidelines
+
+- Always show a Form Error List when there is a validation error, even if there’s only one.
+- You must link the errors in the Form Error List to the answer they relate to (see below).
+
+## Linking from the Form Error List to each answer
+
+For questions that require a user to answer using a single field, like a file upload, select, textarea, text input or character count, link to the \`id\` of that field.
+
+When a user has to enter their answer into multiple fields, such as day, month and year fields, link to the \`id\` of the first field that contains an error.
+If you do not know which field contains an error, link to the \`id\` of the first field.
+
+For questions that require a user to select one or more options from a list using Radios or Checkboxes, link to the \`id\` of the first Radio or Checkbox.
+
+## Where to put the Form Error List
+
+Put the Form Error List directly above the first question on the page. Place it outside of the \`<form>\`-tag, [to make sure screen readers do not skip it](https://nldesignsystem.nl/richtlijnen/formulieren/meerdere-stappen/#plaats-de-informatie-over-waar-de-gebruiker-is-in-de-stappen-boven-het-formulier).
+
+## Error count in document title
+
+This component adds the error count to the document title,
+in line with [GOV.UK guidelines for informing users about validation errors](https://design-system.service.gov.uk/patterns/validation/#how-to-tell-the-user-about-validation-errors).
+
+## Focus on initial render
+
+This component receives focus the first time it gets displayed on a page.
+This allows keyboard users to quickly navigate to the errors in the form.
+It also scrolls the component into view if it isn't already.
+Note: this functionality has been disabled on this page, to prevent unexpected focus behaviour.
+
+## Relevant WCAG requirements
+
+Pay extra attention to these parts:
+
+- [WCAG requirement 1.3.1](https://www.w3.org/TR/WCAG21/#info-and-relationships): the heading level of the Form Error List depends on where in the page it is placed, this may differ per page.
+
+## References
+
+- [Show an error summary above the form - NL Design System](https://www.nldesignsystem.nl/richtlijnen/formulieren/foutmeldingen#zet-een-samenvatting-van-de-foutmeldingen-boven-het-formulier)
+- [Error Summary component - Gov.uk](https://design-system.service.gov.uk/components/error-summary/)
+`;/*@license CC0-1.0*/function n(t){return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(i,{of:h}),`
+`,e.jsx(s,{children:l}),`
+`,e.jsx(a,{}),`
+`,e.jsx(m,{})]})}function G(t={}){const{wrapper:r}={...o(),...t.components};return r?e.jsx(r,{...t,children:e.jsx(n,{...t})}):n()}export{G as default};
---
+++ demo-DES-869-image-slider/assets/ImageSlider.docs-xi4PxrMZ.js
@@ -0,0 +1,25 @@
+import{j as n}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as t}from"./index-BI1Biiay.js";import{ae as r,ak as a,al as l,am as m,an as e}from"./index-Cplyd0ax.js";import{I as c,N as h,a as d,b as p,c as x,V as j}from"./ImageSlider.stories-CJH1Ey31.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-DOfUAbpa.js";import"../sb-preview/runtime.js";import"./index-Cf-03bMR.js";import"./index-D-8MO0q_.js";import"./index-BVoBHvaS.js";import"./index-DrFu-skq.js";import"./index.esm-Bsc2owIZ.js";import"./clsx-B-dksMZM.js";import"./IconButton-BgcGyAbu.js";import"./Icon-msbkstIT.js";import"./AspectRatio-Cd0KgYDn.js";import"./Image-CVzrCme5.js";const u=`<!-- @license CC0-1.0 -->
+
+# Image Slider
+
+An image slider is a carousel of images that can be navigated by clicking on the navigation buttons or by swiping on touch devices.
+`;/*@license CC0-1.0*/function i(s){const o={code:"code",h2:"h2",h3:"h3",p:"p",...t(),...s.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(r,{of:c}),`
+`,n.jsx(a,{children:u}),`
+`,n.jsx(l,{}),`
+`,n.jsx(m,{}),`
+`,n.jsx(o.h2,{id:"examples",children:"Examples"}),`
+`,n.jsx(o.h3,{id:"no-controls",children:"No Controls"}),`
+`,n.jsx(e,{of:h}),`
+`,n.jsx(o.h3,{id:"no-thumbnails",children:"No Thumbnails"}),`
+`,n.jsx(e,{of:d}),`
+`,n.jsx(o.h3,{id:"snapstop",children:"Snapstop"}),`
+`,n.jsxs(o.p,{children:["Setting ",n.jsx(o.code,{children:"snapStop"})," to false will make the slider not stop at each image."]}),`
+`,n.jsx(e,{of:p}),`
+`,n.jsx(o.h3,{id:"native-scrollbar",children:"Native scrollbar"}),`
+`,n.jsxs(o.p,{children:["Turning on ",n.jsx(o.code,{children:"nativeScrollbar"})," will make the slider use the native scrollbar. You might need this if you disable to other controls."]}),`
+`,n.jsx(e,{of:x}),`
+`,n.jsx(o.h3,{id:"various-sizes",children:"Various Sizes"}),`
+`,n.jsx(o.p,{children:"When you mix images of different sizes, the slider will adjust to the size of the largest image."}),`
+`,n.jsx(e,{of:j})]})}function A(s={}){const{wrapper:o}={...t(),...s.components};return o?n.jsx(o,{...s,children:n.jsx(i,{...s})}):i(s)}export{A as default};
