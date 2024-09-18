0b680be2c9da02207cb8d10e4a2846649080c77d
RubenSibon <mail@rubensibon.nl>
2024-09-18 06:53:45 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@8d2701f84b06523d07d208449a0caac51fb85bef 🚀

---
+++ demo-DES-844-form-field-not-required/assets/Hint.docs-Ce6HbDBc.js
@@ -0,0 +1,19 @@
+import{j as n}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as i}from"./index-BI1Biiay.js";import{ae as s,ak as r,al as a,am as m,an as p}from"./index-qQeCPNxW.js";import{H as c,I as h}from"./Hint.stories-C328r3GF.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-C_GJcUs8.js";import"../sb-preview/runtime.js";import"./index-Cf-03bMR.js";import"./index-D-8MO0q_.js";import"./index-BVoBHvaS.js";import"./index-DrFu-skq.js";import"./index.esm-C0OPxfeR.js";import"./clsx-B-dksMZM.js";import"./index.esm-Bsc2owIZ.js";import"./Hint-D4Fy8YuO.js";const l=`<!-- @license CC0-1.0 -->
+
+# Hint
+
+Shows a hint text. Only used internally by Label and Field Set. 
+
+## Class name
+
+\`ams-hint\`
+`;/*@license CC0-1.0*/function e(o){const t={code:"code",h2:"h2",h3:"h3",p:"p",...i(),...o.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(s,{of:c}),`
+`,n.jsx(r,{children:l}),`
+`,n.jsx(a,{}),`
+`,n.jsx(m,{}),`
+`,n.jsx(t.h2,{id:"examples",children:"Examples"}),`
+`,n.jsx(t.h3,{id:"in-copy",children:"In copy"}),`
+`,n.jsxs(t.p,{children:["Hints inherit the CSS styling, such as ",n.jsx(t.code,{children:"font-size"}),", of their parent element, with the exception of ",n.jsx(t.code,{children:"color"}),"."]}),`
+`,n.jsx(p,{of:h})]})}function g(o={}){const{wrapper:t}={...i(),...o.components};return t?n.jsx(t,{...o,children:n.jsx(e,{...o})}):e(o)}export{g as default};
