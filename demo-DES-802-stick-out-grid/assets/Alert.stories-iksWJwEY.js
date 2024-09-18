78ae9d581e004c663d03abb409395a23673cb45d
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-09-18 13:33:46 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@113d4a09bd936abeaf60bed5e3e1589c72383345 🚀

---
+++ demo-DES-802-stick-out-grid/assets/Alert.stories-iksWJwEY.js
@@ -0,0 +1,64 @@
+import{j as e}from"./jsx-runtime-BlAj40OV.js";import{P as n,U as g,L as A}from"./index.esm-gfS4xpxM.js";import{A as E}from"./Alert-Dt6KCQuZ.js";const H={title:"Components/Feedback/Alert",component:E,args:{closeable:!1,heading:"Let op"}},r={args:{children:e.jsx(n,{children:"Tijdens Koningsdag zijn alle Stadsloketten gesloten. Ook 14 020 en alle andere telefoonnummers van de gemeente zijn niet bereikbaar."})}},a={args:{children:e.jsx(n,{children:"U bent vergeten verplichte velden in te vullen."}),heading:"Vul de gegevens aan"}},s={args:{children:e.jsx(n,{children:"Wegens een technische fout kon het formulier niet worden verzonden. We hebben uw gegevens niet ontvangen. Probeer het over een paar minuten opnieuw."}),heading:"Niet gelukt",severity:"error"}},t={args:{children:e.jsx(n,{children:"Het formulier is verzonden. We hebben uw gegevens goed ontvangen."}),closeable:!0,heading:"Gelukt",severity:"success"}},o={args:{children:e.jsx(n,{children:"Tijdens de hijswerkzaamheden geldt er een korte verkeersstop. We zetten verkeersregelaars in, volg hun aanwijzingen op. De verkeersstop duurt ongeveer 10 minuten."}),closeable:!0,severity:"info"}},i={args:{children:[e.jsx(n,{children:"U bent vergeten de volgende verplichte velden in te vullen:"},1),e.jsxs(g,{children:[e.jsx(g.Item,{children:"Naam"}),e.jsx(g.Item,{children:"Telefoonnummer"})]},2)],heading:"Vul de gegevens aan"}},d={args:{children:e.jsxs(n,{children:["Tijdens Koningsdag zijn"," ",e.jsx(A,{variant:"inline",href:"#",children:"alle Stadsloketten"})," ","gesloten. Ook 14 020 en alle andere telefoonnummers van de gemeente zijn niet bereikbaar."]}),severity:"info"}},l={args:{children:e.jsx(n,{children:"De geschatte wachttijd in de adreszoeker klopt momenteel niet altijd. We werken aan een oplossing."}),heading:void 0}};var c,h,m;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    children: <Paragraph>
+        Tijdens Koningsdag zijn alle Stadsloketten gesloten. Ook 14 020 en alle andere telefoonnummers van de gemeente
+        zijn niet bereikbaar.
+      </Paragraph>
+  }
+}`,...(m=(h=r.parameters)==null?void 0:h.docs)==null?void 0:m.source}}};var u,p,v;a.parameters={...a.parameters,docs:{...(u=a.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    children: <Paragraph>U bent vergeten verplichte velden in te vullen.</Paragraph>,
+    heading: 'Vul de gegevens aan'
+  }
+}`,...(v=(p=a.parameters)==null?void 0:p.docs)==null?void 0:v.source}}};var k,j,b;s.parameters={...s.parameters,docs:{...(k=s.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  args: {
+    children: <Paragraph>
+        Wegens een technische fout kon het formulier niet worden verzonden. We hebben uw gegevens niet ontvangen.
+        Probeer het over een paar minuten opnieuw.
+      </Paragraph>,
+    heading: 'Niet gelukt',
+    severity: 'error'
+  }
+}`,...(b=(j=s.parameters)==null?void 0:j.docs)==null?void 0:b.source}}};var f,z,P;t.parameters={...t.parameters,docs:{...(f=t.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    children: <Paragraph>Het formulier is verzonden. We hebben uw gegevens goed ontvangen.</Paragraph>,
+    closeable: true,
+    heading: 'Gelukt',
+    severity: 'success'
+  }
+}`,...(P=(z=t.parameters)==null?void 0:z.docs)==null?void 0:P.source}}};var W,S,w;o.parameters={...o.parameters,docs:{...(W=o.parameters)==null?void 0:W.docs,source:{originalSource:`{
+  args: {
+    children: <Paragraph>
+        Tijdens de hijswerkzaamheden geldt er een korte verkeersstop. We zetten verkeersregelaars in, volg hun
+        aanwijzingen op. De verkeersstop duurt ongeveer 10 minuten.
+      </Paragraph>,
+    closeable: true,
+    severity: 'info'
+  }
+}`,...(w=(S=o.parameters)==null?void 0:S.docs)==null?void 0:w.source}}};var x,L,y;i.parameters={...i.parameters,docs:{...(x=i.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  args: {
+    children: [<Paragraph key={1}>U bent vergeten de volgende verplichte velden in te vullen:</Paragraph>, <UnorderedList key={2}>
+        <UnorderedList.Item>Naam</UnorderedList.Item>
+        <UnorderedList.Item>Telefoonnummer</UnorderedList.Item>
+      </UnorderedList>],
+    heading: 'Vul de gegevens aan'
+  }
+}`,...(y=(L=i.parameters)==null?void 0:L.docs)==null?void 0:y.source}}};var U,I,T;d.parameters={...d.parameters,docs:{...(U=d.parameters)==null?void 0:U.docs,source:{originalSource:`{
+  args: {
+    children: <Paragraph>
+        Tijdens Koningsdag zijn{' '}
+        <Link variant="inline" href="#">
+          alle Stadsloketten
+        </Link>{' '}
+        gesloten. Ook 14 020 en alle andere telefoonnummers van de gemeente zijn niet bereikbaar.
+      </Paragraph>,
+    severity: 'info'
+  }
+}`,...(T=(I=d.parameters)==null?void 0:I.docs)==null?void 0:T.source}}};var O,D,_;l.parameters={...l.parameters,docs:{...(O=l.parameters)==null?void 0:O.docs,source:{originalSource:`{
+  args: {
+    children: <Paragraph>
+        De geschatte wachttijd in de adreszoeker klopt momenteel niet altijd. We werken aan een oplossing.
+      </Paragraph>,
+    heading: undefined
+  }
+}`,...(_=(D=l.parameters)==null?void 0:D.docs)==null?void 0:_.source}}};const K=["Default","Warning","Error","Success","Info","WithList","WithInlineLink","WithoutHeading"],C=Object.freeze(Object.defineProperty({__proto__:null,Default:r,Error:s,Info:o,Success:t,Warning:a,WithInlineLink:d,WithList:i,WithoutHeading:l,__namedExportsOrder:K,default:H},Symbol.toStringTag,{value:"Module"}));export{C as A,s as E,o as I,t as S,a as W,d as a,i as b,l as c};
