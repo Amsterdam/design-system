3d86882f22e4629a17a98cbbf96f03ad56620be9
Ruben Sibon <r.sibon@amsterdam.nl>
2024-09-20 11:34:14 +0200

    feat: Add Hint as component and Label and FieldSet props (#1499)
    Co-authored-by: Vincent Smedinga <v.smedinga@amsterdam.nl>
    Co-authored-by: Aram <37216945+alimpens@users.noreply.github.com>

---
+++ packages/css/src/components/hint/README.md
@@ -0,0 +1,9 @@
+<!-- @license CC0-1.0 -->
+
+# Hint
+
+Shows a hint text. Only used internally by Label and Field Set. 
+
+## Class name
+
+`ams-hint`
--- packages/react/src/FieldSet/FieldSet.tsx
+++ packages/react/src/FieldSet/FieldSet.tsx
@@ -6,25 +6,33 @@
 import clsx from 'clsx'
 import { forwardRef } from 'react'
 import type { ForwardedRef, HTMLAttributes, PropsWithChildren } from 'react'
+import { Hint, HintProps } from '../Hint'
 
 export type FieldSetProps = PropsWithChildren<HTMLAttributes<HTMLFieldSetElement>> & {
   /** Whether the field set has an input with a validation error */
   invalid?: boolean
   /** The text for the caption. */
   legend: string
-}
+} & HintProps
 
 export const FieldSet = forwardRef(
-  ({ children, className, invalid, legend, ...restProps }: FieldSetProps, ref: ForwardedRef<HTMLFieldSetElement>) => (
-    <fieldset
-      {...restProps}
-      ref={ref}
-      className={clsx('ams-field-set', invalid && 'ams-field-set--invalid', className)}
-    >
-      <legend className="ams-field-set__legend">{legend}</legend>
-      {children}
-    </fieldset>
-  ),
+  (
+    { children, className, hint, invalid, legend, optional, ...restProps }: FieldSetProps,
+    ref: ForwardedRef<HTMLFieldSetElement>,
+  ) => {
+    return (
+      <fieldset
+        {...restProps}
+        ref={ref}
+        className={clsx('ams-field-set', invalid && 'ams-field-set--invalid', className)}
+      >
+        <legend className="ams-field-set__legend">
+          {legend} <Hint hint={hint} optional={optional} />
+        </legend>
+        {children}
+      </fieldset>
+    )
+  },
 )
 
 FieldSet.displayName = 'FieldSet'
