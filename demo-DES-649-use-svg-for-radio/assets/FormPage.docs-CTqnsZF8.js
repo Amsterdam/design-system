73a6f5f25336478432fb2858d10fbd06e229d0c9
alimpens <37216945+alimpens@users.noreply.github.com>
2024-09-20 09:01:37 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@208cdbbfaefab1a81fc522a544f9a8ada573a65f 🚀

---
+++ demo-DES-649-use-svg-for-radio/assets/FormPage.docs-CTqnsZF8.js
@@ -0,0 +1,5 @@
+import{j as o}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as i}from"./index-BI1Biiay.js";import{ae as e}from"./index-BQlv95Xy.js";import{F as n}from"./FormPage.stories--ju5dQfd.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-DsgRMRlH.js";import"../sb-preview/runtime.js";import"./index-Cf-03bMR.js";import"./index-D-8MO0q_.js";import"./index-BVoBHvaS.js";import"./index-DrFu-skq.js";import"./index.esm-DQHNxj5y.js";import"./clsx-B-dksMZM.js";import"./index.esm-CIU4HIuz.js";import"./config-C8dkNkZt.js";import"./PageMenu.stories-bNVSJFvt.js";import"./Icon-msbkstIT.js";import"./Footer.stories-CSzR4TDQ.js";import"./Spotlight-IssvtslZ.js";/*@license CC0-1.0*/function m(t){const r={h1:"h1",p:"p",...i(),...t.components};return o.jsxs(o.Fragment,{children:[`
+`,`
+`,o.jsx(e,{of:n}),`
+`,o.jsx(r.h1,{id:"form-page",children:"Form page"}),`
+`,o.jsx(r.p,{children:"This simple example presents various types of fields."})]})}function w(t={}){const{wrapper:r}={...i(),...t.components};return r?o.jsx(r,{...t,children:o.jsx(m,{...t})}):m(t)}export{w as default};
