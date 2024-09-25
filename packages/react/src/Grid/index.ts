bc3ac3a6214c063160cf2af398468a3ff5af0798
Vincent Smedinga <v.smedinga@amsterdam.nl>
2024-09-25 15:23:55 +0200

    Revert breaking change for now

--- packages/react/src/Grid/index.ts
+++ packages/react/src/Grid/index.ts
@@ -1,3 +1,3 @@
 export { Grid } from './Grid'
-export type { GridProps } from './Grid'
+export type { GridProps, GridColumnNumber, GridColumnNumbers } from './Grid'
 export type { GridCellProps } from './GridCell'
