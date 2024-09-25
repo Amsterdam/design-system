cbcefd5d2415e9cb87baa76ead9f40b3dd632afd
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-09-25 13:25:41 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@bc3ac3a6214c063160cf2af398468a3ff5af0798 🚀

---
+++ demo-DES-802-stick-out-grid/assets/Breadcrumb.docs-BgRxIQ6s.js
@@ -0,0 +1,27 @@
+import{j as e}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as i}from"./index-BI1Biiay.js";import{ae as s,ak as a,al as o}from"./index-D-IAiPVE.js";import{B as c}from"./Breadcrumb.stories-Cujp0Yrk.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-DWkWHRwS.js";import"../sb-preview/runtime.js";import"./index-Cf-03bMR.js";import"./index-D-8MO0q_.js";import"./index-DF3CPj03.js";import"./index-DrFu-skq.js";import"./clsx-B-dksMZM.js";const u=`<!-- @license CC0-1.0 -->
+
+# Breadcrumb
+
+The breadcrumb is a secondary navigation pattern that helps a user understand the structure between levels in the website up to the current page.
+Users can use it to navigate.
+
+## Guidelines
+
+- Only use the breadcrumb trail if it adds something functional for the user and the structure is ‘static’.
+- Do not display the breadcrumb trail on a form page.
+  It distracts the user from their task, or one can accidentally interpret it as a Progress Indicator.
+- This is a secondary navigation pattern.
+  It can’t replace the main navigation.
+
+### Using links with routing libraries
+
+Check out the [developer guide](/docs/docs-developer-guide-routing-libraries--docs) on how to integrate links with external routing libraries.
+`;/*@license CC0-1.0*/function r(n){const t={h2:"h2",li:"li",ul:"ul",...i(),...n.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(s,{of:c}),`
+`,e.jsx(a,{children:u}),`
+`,e.jsx(o,{}),`
+`,e.jsx(t.h2,{id:"guidelines",children:"Guidelines"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsx(t.li,{children:"A breadcrumb should not display more than 8 items."}),`
+`]})]})}function M(n={}){const{wrapper:t}={...i(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(r,{...n})}):r(n)}export{M as default};
