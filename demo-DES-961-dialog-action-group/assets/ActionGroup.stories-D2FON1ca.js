2843c3f48199d0ef79485bd19d51c8fea145d19e
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-09-26 13:20:06 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@678eb8fbcefa0dbbec594507771da2e2dbc61728 🚀

---
+++ demo-DES-961-dialog-action-group/assets/ActionGroup.stories-D2FON1ca.js
@@ -0,0 +1,9 @@
+import{j as t}from"./jsx-runtime-BlAj40OV.js";import{a as o}from"./index.esm-skUWpdVt.js";import{c as g}from"./clsx-B-dksMZM.js";import{r as j}from"./index-Cs7sjTYM.js";const a=j.forwardRef(({children:s,className:l,...u},_)=>t.jsx("div",{...u,ref:_,className:g("ams-action-group",l),children:s}));a.displayName="ActionGroup";try{a.displayName="ActionGroup",a.__docgenInfo={description:"",displayName:"ActionGroup",props:{}}}catch{}const f={title:"Components/Buttons/Action Group",component:a,args:{children:[t.jsx(o,{children:"Doorgaan"}),t.jsx(o,{variant:"tertiary",children:"Stoppen"})]}},r={},e={args:{children:[t.jsx(o,{children:"Adres wijzigen"}),t.jsx(o,{variant:"tertiary",children:"Adres verwijderen"})],className:"ams-resize-horizontal",style:{inlineSize:"16rem"}}};var i,n,c;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:"{}",...(c=(n=r.parameters)==null?void 0:n.docs)==null?void 0:c.source}}};var p,d,m;e.parameters={...e.parameters,docs:{...(p=e.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    children: [<Button>Adres wijzigen</Button>, <Button variant="tertiary">Adres verwijderen</Button>],
+    className: 'ams-resize-horizontal',
+    style: {
+      inlineSize: '16rem'
+    }
+  }
+}`,...(m=(d=e.parameters)==null?void 0:d.docs)==null?void 0:m.source}}};const y=["Default","Stacked"],z=Object.freeze(Object.defineProperty({__proto__:null,Default:r,Stacked:e,__namedExportsOrder:y,default:f},Symbol.toStringTag,{value:"Module"}));export{z as A,e as S};
