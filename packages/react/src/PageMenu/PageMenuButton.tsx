29bb49f55ea909995e7734a990ea661ee69270b8
Vincent Smedinga <v.smedinga@amsterdam.nl>
2024-09-30 13:34:56 +0200

    Allow icon in Page Menu Button

--- packages/react/src/PageMenu/PageMenuButton.tsx
+++ packages/react/src/PageMenu/PageMenuButton.tsx
@@ -6,13 +6,17 @@
 import clsx from 'clsx'
 import type { ButtonHTMLAttributes, PropsWithChildren } from 'react'
 import { forwardRef } from 'react'
+import { Icon } from '../Icon'
 
-export type PageMenuButtonProps = PropsWithChildren<ButtonHTMLAttributes<Omit<HTMLButtonElement, 'disabled' | 'type'>>>
+export type PageMenuButtonProps = { icon?: Function } & PropsWithChildren<
+  ButtonHTMLAttributes<Omit<HTMLButtonElement, 'disabled' | 'type'>>
+>
 
 export const PageMenuButton = forwardRef<HTMLButtonElement, PageMenuButtonProps>(
-  ({ children, className, ...restProps }, ref) => (
+  ({ children, className, icon, ...restProps }, ref) => (
     <button {...restProps} className={clsx('ams-page-menu__button', className)} ref={ref} type="button">
       {children}
+      {icon && <Icon svg={icon} size="level-6" />}
     </button>
   ),
 )
