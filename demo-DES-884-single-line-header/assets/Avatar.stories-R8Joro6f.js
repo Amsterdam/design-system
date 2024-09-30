ff569e677caa39e9be1585cb1b6a3b9ad1130e2a
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-09-30 08:34:13 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@359edc6567c21c63dc0b6fb7ceaddf09f0e724b0 🚀

---
+++ demo-DES-884-single-line-header/assets/Avatar.stories-R8Joro6f.js
@@ -0,0 +1,24 @@
+import{j as e}from"./jsx-runtime-BlAj40OV.js";import{b as P,c as i}from"./index.esm-D2p-Q9mV.js";import{j as L,k as M}from"./index.esm-CsAWJJ2G.js";import{c as w}from"./clsx-B-dksMZM.js";import{r as C}from"./index-Cs7sjTYM.js";import{I as D}from"./Image-CVzrCme5.js";import{I as E}from"./Icon-msbkstIT.js";const N=({imageSrc:a,initials:r})=>a?e.jsx(D,{src:a,alt:""}):r.length?e.jsx("span",{"aria-hidden":!0,children:r}):e.jsx(E,{svg:L,size:"level-6"}),o=C.forwardRef(({label:a,imageSrc:r,className:y,color:j="blue",...I},_)=>{const c=a.slice(0,2).toUpperCase(),A=c.length===0?"Gebruiker":`Initialen gebruiker: ${c}`;return e.jsxs("span",{...I,ref:_,className:w("ams-avatar",`ams-avatar--${j}`,r&&"ams-avatar--has-image",y),children:[e.jsx("span",{className:"ams-visually-hidden",children:A}),e.jsx(N,{imageSrc:r,initials:c})]})});o.displayName="Avatar";try{o.displayName="Avatar",o.__docgenInfo={description:"",displayName:"Avatar",props:{color:{defaultValue:{value:"blue"},description:"The background colour.",name:"color",required:!1,type:{name:"enum",value:[{value:'"black"'},{value:'"blue"'},{value:'"dark-green"'},{value:'"green"'},{value:'"grey-1"'},{value:'"grey-2"'},{value:'"grey-3"'},{value:'"light-blue"'},{value:'"magenta"'},{value:'"orange"'},{value:'"purple"'},{value:'"red"'},{value:'"white"'},{value:'"yellow"'}]}},imageSrc:{defaultValue:null,description:"The url for the user’s image. Its center will be displayed. Should be square and scaled down.",name:"imageSrc",required:!1,type:{name:"string"}},label:{defaultValue:null,description:"The text content. Should be the user’s initials. The first two characters will be displayed.",name:"label",required:!0,type:{name:"string"}}}}}catch{}const T={title:"Components/Feedback/Avatar",component:o,args:{label:"DS",imageSrc:""}},s={},t={args:{label:"PS",imageSrc:"https://i.pravatar.cc/128"}},n={args:{imageSrc:void 0,label:""}},l={args:{label:"DS"},render:a=>e.jsx(P,{links:e.jsxs(i,{alignEnd:!0,children:[e.jsx(i.Link,{href:"#",children:"Contact"}),e.jsx(i.Link,{href:"#",icon:M,children:"Zoeken"}),e.jsx("li",{children:e.jsx(o,{...a})})]})})};var u,d,m;s.parameters={...s.parameters,docs:{...(u=s.parameters)==null?void 0:u.docs,source:{originalSource:"{}",...(m=(d=s.parameters)==null?void 0:d.docs)==null?void 0:m.source}}};var p,g,v;t.parameters={...t.parameters,docs:{...(p=t.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    label: 'PS',
+    imageSrc: 'https://i.pravatar.cc/128'
+  }
+}`,...(v=(g=t.parameters)==null?void 0:g.docs)==null?void 0:v.source}}};var h,b,f;n.parameters={...n.parameters,docs:{...(h=n.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    imageSrc: undefined,
+    label: ''
+  }
+}`,...(f=(b=n.parameters)==null?void 0:b.docs)==null?void 0:f.source}}};var S,k,x;l.parameters={...l.parameters,docs:{...(S=l.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    label: 'DS'
+  },
+  render: args => <Header links={<PageMenu alignEnd>
+          <PageMenu.Link href="#">Contact</PageMenu.Link>
+          <PageMenu.Link href="#" icon={SearchIcon}>
+            Zoeken
+          </PageMenu.Link>
+          <li>
+            <Avatar {...args} />
+          </li>
+        </PageMenu>} />
+}`,...(x=(k=l.parameters)==null?void 0:k.docs)==null?void 0:x.source}}};const q=["Default","WithImage","FallbackIcon","InAHeader"],Z=Object.freeze(Object.defineProperty({__proto__:null,Default:s,FallbackIcon:n,InAHeader:l,WithImage:t,__namedExportsOrder:q,default:T},Symbol.toStringTag,{value:"Module"}));export{Z as A,n as F,l as I,t as W};
