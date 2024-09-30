0615ff6718caac5426193da33979003fb2520f06
Vincent Smedinga <v.smedinga@amsterdam.nl>
2024-09-30 14:04:01 +0200

    Update homepage example

--- storybook/src/pages/amsterdam/common/AppHeader.tsx
+++ storybook/src/pages/amsterdam/common/AppHeader.tsx
@@ -1,10 +1,42 @@
-import { Grid, Header, PageMenu } from '@amsterdam/design-system-react'
-import { Alignment as PageMenuStory } from '../../../components/PageMenu/PageMenu.stories'
+import { Grid, Header, PageMenu, SearchField } from '@amsterdam/design-system-react'
+import { MenuIcon, SearchIcon } from '@amsterdam/design-system-react-icons'
+import { useState } from 'react'
+import { Default as MegaMenuStory } from '../../../components/MegaMenu/MegaMenu.stories'
 
-export const AppHeader = () => (
-  <Grid>
-    <Grid.Cell span="all">
-      <Header links={<PageMenu alignEnd>{PageMenuStory.args?.children}</PageMenu>} onClickMenu={() => {}} />
-    </Grid.Cell>
-  </Grid>
-)
+export const AppHeader = () => {
+  const [showMenu, setShowMenu] = useState(false)
+  const [showSearch, setShowSearch] = useState(false)
+
+  return (
+    <Header
+      menu={
+        <PageMenu alignEnd>
+          <PageMenu.Link href="#">Contact</PageMenu.Link>
+          <PageMenu.Link href="#">Mijn Amsterdam</PageMenu.Link>
+          <PageMenu.Button icon={SearchIcon} onClick={() => setShowSearch(!showSearch)}>
+            Zoeken
+          </PageMenu.Button>
+          <PageMenu.Button icon={MenuIcon} onClick={() => setShowMenu(!showMenu)}>
+            Menu
+          </PageMenu.Button>
+        </PageMenu>
+      }
+    >
+      {showMenu && (
+        <Grid.Cell span="all">
+          <div style={{ paddingBlockEnd: 'var(--ams-space-grid-md)' }}>{MegaMenuStory.args?.children}</div>
+        </Grid.Cell>
+      )}
+      {showSearch && (
+        <Grid.Cell span={{ narrow: 4, medium: 6, wide: 8 }} start={{ narrow: 1, medium: 2, wide: 3 }}>
+          <div style={{ paddingBlockEnd: 'var(--ams-space-grid-md)', paddingBlockStart: 'var(--ams-space-grid-sm)' }}>
+            <SearchField onSubmit={(e) => e.preventDefault()}>
+              <SearchField.Input label="Zoeken" placeholder="Wat kunnen we voor u vinden?" />
+              <SearchField.Button />
+            </SearchField>
+          </div>
+        </Grid.Cell>
+      )}
+    </Header>
+  )
+}
