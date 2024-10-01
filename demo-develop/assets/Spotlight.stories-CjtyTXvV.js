baecd69842a8fb2ef99a2fbfcaea5d78e50800f9
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-10-01 11:27:20 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@d0ea054b98a7f2ee41bd9a8434b76a85e9b8c9f2 🚀

---
+++ demo-develop/assets/Spotlight.stories-CjtyTXvV.js
@@ -0,0 +1,37 @@
+import{j as r}from"./jsx-runtime-BlAj40OV.js";import{S as J,G as i,B as K}from"./index.esm-CB7foHyD.js";import{c as L}from"./exampleContent-hIIPvEhU.js";import{S as g}from"./Spotlight-i1uKJTzV.js";const N=L(),U={title:"Components/Containers/Spotlight",component:g,decorators:[u=>r.jsx(J,{children:r.jsx(u,{})})],render:({as:u,color:d})=>r.jsx(g,{as:u,color:d,children:r.jsx(i,{paddingVertical:"medium",children:r.jsx(i.Cell,{span:"all",children:r.jsx(K,{inverseColor:!d||!["green","yellow"].includes(d),children:N})})})})},e={},a={args:{color:"blue"}},s={args:{color:"dark-blue"}},o={args:{color:"dark-green"}},c={args:{color:"green"}},n={args:{color:"magenta"}},t={args:{color:"orange"}},l={args:{color:"purple"}},p={args:{color:"yellow"}},m={args:{as:"section"}};var S,x,j;e.parameters={...e.parameters,docs:{...(S=e.parameters)==null?void 0:S.docs,source:{originalSource:"{}",...(j=(x=e.parameters)==null?void 0:x.docs)==null?void 0:j.source}}};var f,k,h;a.parameters={...a.parameters,docs:{...(f=a.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    color: 'blue'
+  }
+}`,...(h=(k=a.parameters)==null?void 0:k.docs)==null?void 0:h.source}}};var b,B,D;s.parameters={...s.parameters,docs:{...(b=s.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    color: 'dark-blue'
+  }
+}`,...(D=(B=s.parameters)==null?void 0:B.docs)==null?void 0:D.source}}};var G,O,_;o.parameters={...o.parameters,docs:{...(G=o.parameters)==null?void 0:G.docs,source:{originalSource:`{
+  args: {
+    color: 'dark-green'
+  }
+}`,...(_=(O=o.parameters)==null?void 0:O.docs)==null?void 0:_.source}}};var w,y,v;c.parameters={...c.parameters,docs:{...(w=c.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  args: {
+    color: 'green'
+  }
+}`,...(v=(y=c.parameters)==null?void 0:y.docs)==null?void 0:v.source}}};var C,M,P;n.parameters={...n.parameters,docs:{...(C=n.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  args: {
+    color: 'magenta'
+  }
+}`,...(P=(M=n.parameters)==null?void 0:M.docs)==null?void 0:P.source}}};var I,Y,q;t.parameters={...t.parameters,docs:{...(I=t.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  args: {
+    color: 'orange'
+  }
+}`,...(q=(Y=t.parameters)==null?void 0:Y.docs)==null?void 0:q.source}}};var E,z,Q;l.parameters={...l.parameters,docs:{...(E=l.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  args: {
+    color: 'purple'
+  }
+}`,...(Q=(z=l.parameters)==null?void 0:z.docs)==null?void 0:Q.source}}};var R,T,V;p.parameters={...p.parameters,docs:{...(R=p.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  args: {
+    color: 'yellow'
+  }
+}`,...(V=(T=p.parameters)==null?void 0:T.docs)==null?void 0:V.source}}};var A,F,H;m.parameters={...m.parameters,docs:{...(A=m.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  args: {
+    as: 'section'
+  }
+}`,...(H=(F=m.parameters)==null?void 0:F.docs)==null?void 0:H.source}}};const W=["Default","Blue","DarkBlue","DarkGreen","Green","Magenta","Orange","Purple","Yellow","ImproveSemantics"],er=Object.freeze(Object.defineProperty({__proto__:null,Blue:a,DarkBlue:s,DarkGreen:o,Default:e,Green:c,ImproveSemantics:m,Magenta:n,Orange:t,Purple:l,Yellow:p,__namedExportsOrder:W,default:U},Symbol.toStringTag,{value:"Module"}));export{a as B,s as D,c as G,m as I,n as M,t as O,l as P,er as S,p as Y,o as a};
