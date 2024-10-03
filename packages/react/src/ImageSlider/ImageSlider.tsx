216ee9fb1d343043c9065521d2fbef7a8fe5f733
Vincent Smedinga <v.smedinga@amsterdam.nl>
2024-10-03 16:19:12 +0200

    Update scroll position after resize or orientation change

--- packages/react/src/ImageSlider/ImageSlider.tsx
+++ packages/react/src/ImageSlider/ImageSlider.tsx
@@ -105,7 +105,10 @@ export const ImageSliderRoot = forwardRef(
     const goToSlide = useCallback((element: HTMLElement) => {
       const sliderScrollerElement = targetRef.current
       if (!sliderScrollerElement || !element) return
-      sliderScrollerElement.scrollTo({ left: Math.abs(sliderScrollerElement.offsetLeft - element.offsetLeft) })
+
+      sliderScrollerElement.scrollTo({
+        left: sliderScrollerElement.scrollLeft + element.offsetLeft - sliderScrollerElement.scrollLeft,
+      })
     }, [])
 
     const goToSlideId = useCallback(
@@ -126,6 +129,17 @@ export const ImageSliderRoot = forwardRef(
       const element = targetRef.current?.children[currentSlideId]
       const previousElement = element?.previousElementSibling as HTMLElement | null
       if (previousElement) goToSlide(previousElement)
+    }, [currentSlideId, goToSlide])
+
+    useEffect(() => {
+      const handleResize = () => {
+        if (targetRef.current && targetRef.current.children[currentSlideId]) {
+          const element = targetRef.current.children[currentSlideId] as HTMLElement
+          goToSlide(element)
+        }
+      }
+      window.addEventListener('resize', handleResize)
+      return () => window.removeEventListener('resize', handleResize)
     }, [currentSlideId, goToSlide])
 
     return (
