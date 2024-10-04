e841f427fcdc3981721685c1ee6003ed517dcd08
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-10-04 12:00:29 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@84b81fece17defab28ab9ec578fbb30964b5d8fb 🚀

---
+++ demo-develop/assets/PasswordInput.docs-CcSeWrbH.js
@@ -0,0 +1,45 @@
+import{j as e}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as o}from"./index-BI1Biiay.js";import{ae as i,ak as a,al as d,am as h,an as t}from"./index-DQqGBEWw.js";import{P as l,I as p,a as c}from"./PasswordInput.stories-DDF3k6hy.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-C-hfKlWl.js";import"../sb-preview/runtime.js";import"./index-Cf-03bMR.js";import"./index-D-8MO0q_.js";import"./index-DF3CPj03.js";import"./index-DrFu-skq.js";import"./index.esm-HFyfo-D9.js";import"./clsx-B-dksMZM.js";import"./index.esm-BBV4ZYo5.js";const m=`<!-- @license CC0-1.0 -->
+
+# Password Input
+
+Helps users enter a password.
+
+## Guidelines
+
+- Use this component when the input requires sensitive information, like passwords or PINs.
+  It ensures that the input is not readable by others who might be looking at the screen.
+- The characters entered are hidden, represented by squares.
+
+This component sets \`autocorrect="off"\` and \`spellcheck="false"\` to stop browsers automatically changing user input.
+Passwords shouldn’t be checked for spelling or grammar.
+This may also prevent posting the password to third-party plugins.
+These props cannot be overridden.
+
+Consider setting the following attributes:
+
+1. Allow the user’s password manager to automatically fill the password through \`autocomplete="current-password"\`.
+   When asking for a new password, use \`autocomplete="new-password"\` instead.
+2. Do not add a \`minlength\` attribute to ensure passwords meet a minimum length requirement.
+   This would prematurely indicate an error to the user – while they are still typing.
+3. Do not add a \`maxlength\` attribute either.
+   Users will not get any feedback when their text input has been truncated, e.g. after pasting from a password manager.
+4. If the password is a numeric PIN, add \`inputmode="numeric"\`.
+   Devices with virtual keyboards then switch to a numeric keypad layout which makes entering the password easier.
+
+Follow the [guidelines for asking for passwords](https://design-system.service.gov.uk/patterns/passwords/) of the GOV.UK Design System.
+`;/*@license CC0-1.0*/function r(s){const n={a:"a",code:"code",h2:"h2",h3:"h3",p:"p",...o(),...s.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(i,{of:l}),`
+`,e.jsx(a,{children:m}),`
+`,e.jsx(d,{}),`
+`,e.jsx(h,{}),`
+`,e.jsx(n.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(n.h3,{id:"in-a-field",children:"In a Field"}),`
+`,e.jsx(n.p,{children:"Use a Field to group a Password Input with a Label, description and / or an Error Message."}),`
+`,e.jsxs(n.p,{children:["If you don’t need the description, remove its Paragraph and the ",e.jsx(n.code,{children:"aria-describedby"})," from the Password Input."]}),`
+`,e.jsxs(n.p,{children:["Check ",e.jsx(n.a,{href:"/docs/components-forms-field--docs",children:"the Field docs"})," for more information on configuring it."]}),`
+`,e.jsx(t,{of:p}),`
+`,e.jsx(n.h3,{id:"in-a-field-with-validation",children:"In a Field with validation"}),`
+`,e.jsxs(n.p,{children:["If the Password Input can become invalid, add an Error Message and its ",e.jsx(n.code,{children:"id"})," to the ",e.jsx(n.code,{children:"aria-describedby"})," attribute of the Password Input."]}),`
+`,e.jsxs(n.p,{children:["Check ",e.jsx(n.a,{href:"/docs/components-forms-field--docs",children:"the Field docs"})," for more information on configuring it."]}),`
+`,e.jsx(t,{of:c})]})}function D(s={}){const{wrapper:n}={...o(),...s.components};return n?e.jsx(n,{...s,children:e.jsx(r,{...s})}):r(s)}export{D as default};
