bda41113c06acca9c922dd3e8b6eeeced995ab8c
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-10-14 12:11:44 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@42a56b8cb722300d435bf2e42734d9225846f743 🚀

---
+++ demo-audit-vulnerabilities/assets/Screen.stories-Cmhj7t71.js
@@ -0,0 +1,10 @@
+import{j as m}from"./jsx-runtime-BlAj40OV.js";import{S as n}from"./Screen-Bi3gjHLv.js";const d={title:"Components/Layout/Screen",component:n,parameters:{layout:"fullscreen"}},e={args:{children:m.jsx("div",{className:"ams-docs-item",children:"Ik pas in het scherm."})}},r={args:{children:m.jsx("div",{className:"ams-docs-item",children:"Ik pas in een extra breed scherm."}),maxWidth:"x-wide"}};var s,a,t;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    children: <div className="ams-docs-item">Ik pas in het scherm.</div>
+  }
+}`,...(t=(a=e.parameters)==null?void 0:a.docs)==null?void 0:t.source}}};var o,c,i;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    children: <div className="ams-docs-item">Ik pas in een extra breed scherm.</div>,
+    maxWidth: 'x-wide'
+  }
+}`,...(i=(c=r.parameters)==null?void 0:c.docs)==null?void 0:i.source}}};const l=["Default","ExtraWide"],h=Object.freeze(Object.defineProperty({__proto__:null,Default:e,ExtraWide:r,__namedExportsOrder:l,default:d},Symbol.toStringTag,{value:"Module"}));export{r as E,h as S};
