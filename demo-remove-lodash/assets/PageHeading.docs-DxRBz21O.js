88e701477b1ffe5ff4baa67ef42b9f759e8ffd19
dlnr <niels@dlnr.dev>
2024-10-16 07:53:53 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@a31a9bfccc923c50f02af43af49135e70b25f801 🚀

---
+++ demo-remove-lodash/assets/PageHeading.docs-DxRBz21O.js
@@ -0,0 +1,30 @@
+import{j as e}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as i}from"./index-BI1Biiay.js";import{ae as s,ak as a,al as r,am as h,an as p}from"./index-DTI3EXbC.js";import{P as m,I as c}from"./PageHeading.stories-CGDfgDck.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-Cs8FVbK8.js";import"../sb-preview/runtime.js";import"./index-Cf-03bMR.js";import"./index-D-8MO0q_.js";import"./index-DF3CPj03.js";import"./index-DrFu-skq.js";import"./decorators-CweBkPI6.js";import"./clsx-B-dksMZM.js";const d=`<!-- @license CC0-1.0 -->
+
+# Page Heading
+
+The page heading component is a prominent text at the beginning of a page, immediately after the header.
+Use this component for a name, title, or motto.
+
+## Guidelines
+
+- Use this component to capture the user’s attention.
+- Only use this component once per page, typically only on the homepage.
+- Avoid using it if you need to display long pieces of text; the text should be short and precise.
+
+## Relevant WCAG requirements
+
+The page heading component is a variant of a heading level 1 with a distinct style.
+When using this component, ensure that the heading hierarchy of the page remains logical.
+
+For this component, the same WCAG requirements apply as for [the heading component](/docs/components-text-heading--docs).
+`;/*@license CC0-1.0*/function o(t){const n={code:"code",h2:"h2",h3:"h3",p:"p",...i(),...t.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(s,{of:m}),`
+`,e.jsx(a,{children:d}),`
+`,e.jsx(r,{}),`
+`,e.jsx(h,{}),`
+`,e.jsx(n.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(n.h3,{id:"inverse-colour",children:"Inverse colour"}),`
+`,e.jsxs(n.p,{children:["Set the ",e.jsx(n.code,{children:"inverseColor"}),` prop if the Page Heading sits on a dark background.
+This ensures the colour of the text provides enough contrast.`]}),`
+`,e.jsx(p,{of:c})]})}function E(t={}){const{wrapper:n}={...i(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(o,{...t})}):o(t)}export{E as default};
