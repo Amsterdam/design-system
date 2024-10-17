9fa9ac0cad19e6fd54029b5ceb155af36fe716bc
dlnr <niels@dlnr.dev>
2024-10-17 14:15:27 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@b0f8c27f178e609e07dd396e720f0f268a096440 🚀

---
+++ demo-DES-970-label-optional-story/assets/Label.docs-BAyf9llT.js
@@ -0,0 +1,24 @@
+import{j as t}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as r}from"./index-BI1Biiay.js";import{ae as s,ak as a,al as h,am as p,an as o}from"./index-BOKpCNQp.js";import{L as m,O as l,W as c}from"./Label.stories-CfUWZz4C.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-DRIyz6ZT.js";import"../sb-preview/runtime.js";import"./index-Cf-03bMR.js";import"./index-D-8MO0q_.js";import"./index-DF3CPj03.js";import"./index-DrFu-skq.js";import"./clsx-B-dksMZM.js";import"./Hint-D4Fy8YuO.js";const d=`<!-- @license CC0-1.0 -->
+
+# Label
+
+Describes a form control.
+
+## Guidelines
+
+- Always associate a form element (such as an \`input\` or \`textarea\`) with a label.
+- Set \`optional\` to \`true\` if the input is not required.
+- Use \`hint\` to show a custom hint text.
+`;/*@license CC0-1.0*/function i(e){const n={code:"code",h2:"h2",h3:"h3",p:"p",...r(),...e.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(s,{of:m}),`
+`,t.jsx(a,{children:d}),`
+`,t.jsx(h,{}),`
+`,t.jsx(p,{}),`
+`,t.jsx(n.h2,{id:"examples",children:"Examples"}),`
+`,t.jsx(n.h3,{id:"optional",children:"Optional"}),`
+`,t.jsxs(n.p,{children:["Use the ",t.jsx(n.code,{children:"optional"})," prop to indicate that the user doesn’t need to fill in the field to submit the form."]}),`
+`,t.jsx(o,{of:l}),`
+`,t.jsx(n.h3,{id:"with-hint",children:"With hint"}),`
+`,t.jsxs(n.p,{children:["Use the ",t.jsx(n.code,{children:"hint"})," prop to either customize the text for the optional property or to privide another type of remark."]}),`
+`,t.jsx(o,{of:c})]})}function U(e={}){const{wrapper:n}={...r(),...e.components};return n?t.jsx(n,{...e,children:t.jsx(i,{...e})}):i(e)}export{U as default};
