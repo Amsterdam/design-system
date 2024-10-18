c902d2dd58e5aa9ff3956299c5d5fe57ccfea2b3
GitHub <github-action@users.noreply.github.com>
2024-10-18 07:38:31 +0000

    Remove feature branch folder

--- demo-DES-943-iconset-update/assets/FormPage.docs-rsaekIyD.js
+++
@@ -1,5 +0,0 @@
-import{j as o}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as i}from"./index-BI1Biiay.js";import{ae as e}from"./index-DKLFK83T.js";import{F as n}from"./FormPage.stories-EGB5AkAD.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-soMNBcqL.js";import"../sb-preview/runtime.js";import"./index-Cf-03bMR.js";import"./index-D-8MO0q_.js";import"./index-DF3CPj03.js";import"./index-DrFu-skq.js";import"./index.esm-Da1qTZC9.js";import"./clsx-B-dksMZM.js";import"./index.esm-C_mrrS85.js";import"./config-DG_eDYMw.js";import"./PageMenu.stories-C-MWSGL1.js";import"./Icon-msbkstIT.js";import"./Footer.stories-ByX4rlxU.js";import"./Spotlight-i1uKJTzV.js";/*@license CC0-1.0*/function m(t){const r={h1:"h1",p:"p",...i(),...t.components};return o.jsxs(o.Fragment,{children:[`
-`,`
-`,o.jsx(e,{of:n}),`
-`,o.jsx(r.h1,{id:"form-page",children:"Form page"}),`
-`,o.jsx(r.p,{children:"This simple example presents various types of fields."})]})}function w(t={}){const{wrapper:r}={...i(),...t.components};return r?o.jsx(r,{...t,children:o.jsx(m,{...t})}):m(t)}export{w as default};
