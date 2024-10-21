6ec3a70f501c8e329dd1eb700e93f01886ec06d4
dlnr <niels@dlnr.dev>
2024-10-21 09:41:01 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@e972a601267458cc8417268f1e084e3b3b250d3d 🚀

---
+++ demo-DES-859-icon-only-button/assets/Pagination.docs-CsJLd8lR.js
@@ -0,0 +1,28 @@
+import{j as e}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as i}from"./index-BI1Biiay.js";import{ae as o,ak as s,al as r,am as l}from"./index-CRtl8jiB.js";import{P as p}from"./Pagination.stories-CVHuDEtK.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-DNsH70zQ.js";import"../sb-preview/runtime.js";import"./index-Cf-03bMR.js";import"./index-D-8MO0q_.js";import"./index-DF3CPj03.js";import"./index-DrFu-skq.js";import"./index.esm-DQgjFsBh.js";import"./clsx-B-dksMZM.js";import"./Icon-msbkstIT.js";const g=`<!-- @license CC0-1.0 -->
+
+# Pagination
+
+Pagination is a navigation element used to navigate between multiple pages of overview lists.
+This is commonly used for search results or a list of news articles.
+In cases where overview lists are too long to display on a single page, pagination can provide a clearer and more functional way to divide the content across multiple pages.
+Pagination indicates the current page and allows users to navigate to other pages.
+
+## Guidelines
+
+- Use pagination only when an overview list is too long to display on a single page.
+- Place pagination below the overview list.
+- Pagination should not be displayed if there is only one page.
+- Redirect users to the first page if they enter a URL with a page number that doesn’t exist.
+- Pagination can be combined with a count at the top of the page indicating “Page # of ##.”
+- Start a page with an overview list at the top after changing the page.
+- Make sure that the visible and accessible labels for the ‘previous’ and ‘next’ buttons convey the same meaning at all times – e.g. don’t update one and forget the other.
+
+## Relevant WCAG rules
+
+Pagination is an interactive element, and the [general requirements and guidelines for interactivity](/docs/docs-developer-guide-interactivity--docs) apply.
+`;/*@license CC0-1.0*/function a(n){return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(o,{of:p}),`
+`,e.jsx(s,{children:g}),`
+`,e.jsx(r,{}),`
+`,e.jsx(l,{})]})}function R(n={}){const{wrapper:t}={...i(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(a,{...n})}):a()}export{R as default};
