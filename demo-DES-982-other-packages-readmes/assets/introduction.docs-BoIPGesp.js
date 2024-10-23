41cfeceb639a2cbf9aaabafde53789d0d173ffa9
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-10-23 09:06:20 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@72d77e82aed135600329514388cf10bf3f1bee13 🚀

---
+++ demo-DES-982-other-packages-readmes/assets/introduction.docs-BoIPGesp.js
@@ -0,0 +1,73 @@
+import{j as e}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as o}from"./index-BI1Biiay.js";import{ae as i}from"./index-Dbif429a.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-CFYS4qo2.js";import"../sb-preview/runtime.js";import"./index-Cf-03bMR.js";import"./index-D-8MO0q_.js";import"./index-DF3CPj03.js";import"./index-DrFu-skq.js";/*@license CC0-1.0*/function s(t){const n={a:"a",h1:"h1",h2:"h2",h3:"h3",p:"p",...o(),...t.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(i,{title:"Docs/Introduction"}),`
+`,e.jsx(n.h1,{id:"amsterdam-design-system",children:"Amsterdam Design System"}),`
+`,e.jsx(n.p,{children:`This is the library for the user interfaces (UI) of all digital products offered by the City of Amsterdam.
+It provides reusable components, patterns, and guidelines for teams working on websites and applications.`}),`
+`,e.jsx(n.p,{children:`It helps everyone work together faster and better, with more time to create actual value.
+It makes it easier to align all software we create, purchase and use with our corporate identity.
+By using it in all our channels, they appear the same, work similarly, are widely accessible, and inspire trust.`}),`
+`,e.jsx(n.h2,{id:"how-does-it-work",children:"How does it work"}),`
+`,e.jsx(n.p,{children:`The most concrete products of the Amsterdam Design System are the reusable user interface components, such as buttons, text components, menus, and form fields.
+They adhere to the City’s branding: colour, typography and dimensions.
+They adapt responsively to different screens and devices and are accessible to everyone.`}),`
+`,e.jsx(n.p,{children:"We make these components available in various libraries so that designers and developers can use them in their work:"}),`
+`,e.jsx(n.h3,{id:"figma",children:"Figma"}),`
+`,e.jsxs(n.p,{children:[e.jsx(n.a,{href:"https://www.figma.com/file/9IGm6IdPUYizBNGsUnueBd/Amsterdam-Design-System?type=design&node-id=2927%3A29177&mode=design&t=6KlrHnKkHU2uZ9s9-1",rel:"nofollow",children:"Find our Figma Library here"}),`.
+Various components, text styles and grid styles are ready to use in your designs.
+Several others are in progress or being reviewed.
+Refer to the introduction and legend on the cover page.`]}),`
+`,e.jsx(n.h3,{id:"react",children:"React"}),`
+`,e.jsxs(n.p,{children:[`On this website, you will find the React Library and documentation – use the table of contents on the left (on wide windows) or under the ‘Sidebar’ tab (on narrow ones).
+These components can be installed and used in your application `,e.jsx(n.a,{href:"https://www.npmjs.com/search?q=%40amsterdam%2Fdesign-system",rel:"nofollow",children:"via npm"}),"."]}),`
+`,e.jsx(n.h3,{id:"react-native",children:"React Native"}),`
+`,e.jsxs(n.p,{children:[`While developing the Amsterdam App, many UI components have been built according to the updated corporate identity.
+They are still `,e.jsx(n.a,{href:"https://github.com/Amsterdam/amsterdam-app-frontend",rel:"nofollow",children:"in that repository"})," but will eventually be transferred to the design system for other apps to use."]}),`
+`,e.jsx(n.h3,{id:"css",children:"CSS"}),`
+`,e.jsx(n.p,{children:`The components are usable without React by applying the correct classes.
+If you would like to follow this approach, please let us know.`}),`
+`,e.jsx(n.h3,{id:"mendix",children:"Mendix"}),`
+`,e.jsx(n.p,{children:"We help applications implemented in this low-code platform match the corporate identity."}),`
+`,e.jsx(n.h2,{id:"we-build-on-the-nl-design-system",children:"We build on the NL Design System"}),`
+`,e.jsxs(n.p,{children:["Amsterdam Design System uses the architecture of ",e.jsx(n.a,{href:"https://nldesignsystem.nl/",rel:"nofollow",children:"NL Design System"}),"."]}),`
+`,e.jsx(n.p,{children:`Thanks to the NL Design System, the entire government in the Netherlands can work together to provide understandable, user-friendly and accessible online services.
+A service that is logical and coherent but not necessarily uniform, because it allows room for the identity of government organizations.`}),`
+`,e.jsx(n.p,{children:"The choice to connect to NL Design System also makes it easier for us to create different libraries."}),`
+`,e.jsx(n.h2,{id:"contact-us",children:"Contact us"}),`
+`,e.jsx(n.h3,{id:"join-teams",children:"Join Teams"}),`
+`,e.jsxs(n.p,{children:["If you work for the City of Amsterdam, ",e.jsx(n.a,{href:"https://teams.microsoft.com/l/team/19%3afYKS_RD2n1q4UhguA9jwEJk0A_VjYPO4TiLQjYlG_bo1%40thread.tacv2/conversations?groupId=381b5f11-b342-4a3a-8a78-8b371a90457d&tenantId=72fca1b1-2c2e-4376-a445-294d80196804",rel:"nofollow",children:"register now for the Design System team"}),` on MS Teams.
+This way, you can easily stay informed, and we can keep in touch.
+The channels have a lot of information you can read, and of course you are more than welcome to participate in threads.`]}),`
+`,e.jsx(n.h3,{id:"watch-the-demo",children:"Watch the demo"}),`
+`,e.jsx(n.p,{children:`For an overview of what’s new, you are welcome to our four-weekly Tuesday demo at 11:00 AM.
+You can also ask questions and provide feedback here.
+You can find the invitation in the ‘Sprint review’ channel in MS Teams.
+We record the sessions for later viewing.`}),`
+`,e.jsx(n.h3,{id:"contribute-via-git",children:"Contribute via Git"}),`
+`,e.jsxs(n.p,{children:[e.jsx(n.a,{href:"http://github.com/Amsterdam/design-system",rel:"nofollow",children:"The code repository is on GitHub"}),` and is open source.
+Your pull requests and issues are welcome.`]}),`
+`,e.jsx(n.h3,{id:"send-a-message",children:"Send a message"}),`
+`,e.jsx(n.p,{children:`Do you want to know how the design system can help you? Questions about our working method or suggestions for the roadmap?
+Let us know; we are happy to work with you.`}),`
+`,e.jsxs(n.p,{children:["Don’t hesitate to get in touch with us by email at ",e.jsx(n.a,{href:"designsystem@amsterdam.nl",children:"designsystem@amsterdam.nl"}),"."]}),`
+`,e.jsx(n.p,{children:`The current core team:
+Aram Limpens,
+Bas Kroese,
+Inge de Bondt (lead),
+Niels Roozemond,
+Ruben Sibon,
+and
+Vincent Smedinga (lead).`}),`
+`,e.jsx(n.p,{children:`Contributions by:
+Bettina Slee,
+Hee Chan van der Haar,
+Jesper Duinker,
+Justin Straver,
+Laurens van den Akker,
+Mike Alders,
+Nico Druif,
+Renate Schwarzler,
+Rick Zweers,
+Soesanto Arp,
+and
+Tanja van der Heide.`})]})}function w(t={}){const{wrapper:n}={...o(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(s,{...t})}):s(t)}export{w as default};
