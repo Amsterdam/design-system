01f09adcfdf34354edad299201e3674417444911
alimpens <37216945+alimpens@users.noreply.github.com>
2024-10-25 12:35:34 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@96c876e3e15243eb7e28487a02363504d679c37d 🚀

---
+++ demo-DES-859-button-icon-api/assets/Heading.docs-Czws1PfS.js
@@ -0,0 +1,56 @@
+import{j as e}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as r}from"./index-BI1Biiay.js";import{ae as i,ak as a,al as l,am as h,an as s}from"./index-5LVIxDft.js";import{H as c,L as d,a as p,b as u,c as g,S as m,I as v}from"./Heading.stories-Ddq10I3B.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-BSKw1TFI.js";import"../sb-preview/runtime.js";import"./index-DJFdew98.js";import"./index-D-8MO0q_.js";import"./index-DF3CPj03.js";import"./index-DrFu-skq.js";import"./index.esm-BQio6ZyM.js";import"./clsx-B-dksMZM.js";import"./index.esm-DQgjFsBh.js";import"./decorators-CweBkPI6.js";import"./exampleContent-hIIPvEhU.js";import"./Heading-DUu-96hO.js";import"./getHeadingTag-Dv1eMqvl.js";const x=`<!-- @license CC0-1.0 -->
+
+# Heading
+
+Describes the content that follows it.
+Use Headings to allow the user to grasp the structure of the page quickly.
+
+## Guidelines
+
+- A heading describes the content beneath it.
+- Every page should contain one Heading with level 1.
+- Do not skip heading levels, e.g. a level 2 Heading should be followed by one with level 3, not level 4.
+- Do not use a Heading for formatting and styling, but to represent the page’s structure.
+  Users of screen readers use headings to navigate the page – an incorrect hierarchy can confuse them.
+
+## Relevant WCAG requirements
+
+- [WCAG 1.3.1](https://www.w3.org/WAI/WCAG21/quickref/#qr-content-structure-separation-programmatic):
+  Text that looks like a heading is also recognized as a heading by a computer.
+  Using the correct heading level makes the page structure clear for all users.
+- [WCAG 2.4.1](https://www.w3.org/WAI/WCAG21/quickref/#qr-navigation-mechanisms-skip): Headings can be used to navigate to the next section with the keyboard.
+- [WCAG 2.4.6](https://www.w3.org/WAI/WCAG21/quickref/#headings-and-labels): Use headings to describe the purpose or subject of the underlying content.
+- [WCAG 2.4.10](https://www.w3.org/WAI/WCAG21/quickref/#qr-navigation-mechanisms-headings): Use headings to structure the content.
+
+## References
+
+- [W3C - Headings](https://www.w3.org/WAI/tutorials/page-structure/headings/)
+- [W3C - Headings & labels](https://www.w3.org/TR/WCAG21/#headings-and-labels)
+- [A11Y project - Heading structure](https://www.a11yproject.com/posts/how-to-accessible-heading-structure/)
+`;/*@license CC0-1.0*/function o(t){const n={a:"a",code:"code",h2:"h2",h3:"h3",h4:"h4",p:"p",...r(),...t.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(i,{of:c}),`
+`,e.jsx(a,{children:x}),`
+`,e.jsx(l,{}),`
+`,e.jsx(h,{}),`
+`,e.jsx(n.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(n.h3,{id:"levels",children:"Levels"}),`
+`,e.jsx(n.p,{children:`We’ve limited the number of heading levels to four.
+More detailed content should probably be split into multiple pages or have some sections merged.`}),`
+`,e.jsx(n.h4,{id:"level-1",children:"Level 1"}),`
+`,e.jsx(s,{of:d}),`
+`,e.jsx(n.h4,{id:"level-2",children:"Level 2"}),`
+`,e.jsx(s,{of:p}),`
+`,e.jsx(n.h4,{id:"level-3",children:"Level 3"}),`
+`,e.jsx(s,{of:u}),`
+`,e.jsx(n.h4,{id:"level-4",children:"Level 4"}),`
+`,e.jsx(s,{of:g}),`
+`,e.jsx(n.h3,{id:"sizes",children:"Sizes"}),`
+`,e.jsxs(n.p,{children:[`It may be visually more suitable to display a heading in a smaller font size.
+Six sizes from the set of `,e.jsx(n.a,{href:"/docs/brand-design-tokens-text--docs",children:"Text Levels"}),` can be used for this purpose.
+Ensure that you still select a level that accurately represents the section’s hierarchy.`]}),`
+`,e.jsx(s,{of:m}),`
+`,e.jsx(n.h3,{id:"inverse-colour",children:"Inverse colour"}),`
+`,e.jsxs(n.p,{children:["Set the ",e.jsx(n.code,{children:"inverseColor"}),` prop if the Heading sits on a dark background.
+This ensures the colour of the text provides enough contrast.`]}),`
+`,e.jsx(s,{of:v})]})}function R(t={}){const{wrapper:n}={...r(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(o,{...t})}):o(t)}export{R as default};
