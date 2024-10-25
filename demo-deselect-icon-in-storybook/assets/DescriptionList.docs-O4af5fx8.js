7de24c6af3028efb682f45749e8b10fddc6de726
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-10-25 09:44:28 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@f804141f8742f2658d1cc73c0ca926119a602807 🚀

---
+++ demo-deselect-icon-in-storybook/assets/DescriptionList.docs-O4af5fx8.js
@@ -0,0 +1,37 @@
+import{j as e}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as r}from"./index-BI1Biiay.js";import{ae as o,ak as c,al as p,am as a,an as t}from"./index-DU4CZFzi.js";import{D as l,M as m,R as d,a as h,I as x}from"./DescriptionList.stories-4z2Tu_lO.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-D74WWJ7m.js";import"../sb-preview/runtime.js";import"./index-DJFdew98.js";import"./index-D-8MO0q_.js";import"./index-DF3CPj03.js";import"./index-DrFu-skq.js";import"./index.esm-BqBze3dC.js";import"./clsx-B-dksMZM.js";import"./index.esm-DQgjFsBh.js";import"./decorators-CweBkPI6.js";const u=`<!-- @license CC0-1.0 -->
+
+# Description List
+
+A collection of terms and their descriptions.
+
+## Design
+
+On a narrow screen, descriptions appear indented below their term.
+From the medium breakpoint, terms and descriptions appear next to each other.
+The column for the descriptions is twice as wide as the one for the term.
+The term is set in bold text.
+
+## References
+
+- [MDN: \`<dl>\`: The Description List element](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/dl)
+- [WCAG: Using description lists](https://www.w3.org/WAI/WCAG22/Techniques/html/H40)
+`;/*@license CC0-1.0*/function s(i){const n={code:"code",h2:"h2",h3:"h3",p:"p",...r(),...i.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(o,{of:l}),`
+`,e.jsx(c,{children:u}),`
+`,e.jsx(p,{}),`
+`,e.jsx(a,{}),`
+`,e.jsx(n.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(n.h3,{id:"multiple-descriptions",children:"Multiple descriptions"}),`
+`,e.jsx(n.p,{children:"A term may have multiple descriptions."}),`
+`,e.jsx(t,{of:m}),`
+`,e.jsx(n.h3,{id:"rich-description",children:"Rich description"}),`
+`,e.jsx(n.p,{children:"A description can include rich content such as inline formatting, links, paragraphs, and even lists."}),`
+`,e.jsx(t,{of:d}),`
+`,e.jsx(n.h3,{id:"multiple-terms",children:"Multiple terms"}),`
+`,e.jsx(n.p,{children:"Multiple terms may share one description."}),`
+`,e.jsx(t,{of:h}),`
+`,e.jsx(n.h3,{id:"inverse-colour",children:"Inverse colour"}),`
+`,e.jsxs(n.p,{children:["Set the ",e.jsx(n.code,{children:"inverseColor"}),` prop if the Description List sits on a dark background.
+This ensures the colour of the text provides enough contrast.`]}),`
+`,e.jsx(t,{of:x})]})}function I(i={}){const{wrapper:n}={...r(),...i.components};return n?e.jsx(n,{...i,children:e.jsx(s,{...i})}):s(i)}export{I as default};
