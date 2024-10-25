e678c3ce21420c1797d028ad7564ea57968e83f7
dlnr <niels@dlnr.dev>
2024-10-25 09:42:44 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@01f4bf9e251c53580de8a9412af3621426b02bc6 🚀

---
+++ demo-file-uploader-poc/assets/Switch.stories-CMIkQ8T_.js
@@ -0,0 +1,21 @@
+import{j as e}from"./jsx-runtime-BlAj40OV.js";import{g as b}from"./index.esm-DRzvx_01.js";import{c as w}from"./clsx-B-dksMZM.js";import{r as n}from"./index-Cs7sjTYM.js";const s=n.forwardRef(({className:t,id:r,...c},a)=>{const l=r||n.useId();return e.jsxs("div",{className:w("ams-switch",t),children:[e.jsx("input",{...c,className:"ams-switch__input",id:l,ref:a,type:"checkbox",role:"switch"}),e.jsx("label",{className:"ams-switch__label",htmlFor:l})]})});s.displayName="Switch";try{s.displayName="Switch",s.__docgenInfo={description:"",displayName:"Switch",props:{}}}catch{}const{useArgs:u}=__STORYBOOK_MODULE_PREVIEW_API__,k={title:"Components/Forms/Switch",component:s,args:{checked:!1,disabled:!1},argTypes:{checked:{description:"Whether the control is initially checked."},disabled:{description:"Prevents interaction. Avoid if possible."},onChange:{action:"clicked",table:{disable:!0}}},render:t=>{const[,r]=u(),c=a=>{r({checked:a.target.checked})};return e.jsx(s,{onClick:c,...t})}},i={},o={decorators:[t=>e.jsx("div",{style:{display:"flex",alignItems:"center",gap:"5rem"},children:e.jsx(t,{})})],render:t=>{const[,r]=u(),c=a=>{r({checked:a.target.checked})};return e.jsxs(e.Fragment,{children:[e.jsx(b,{htmlFor:"switch-with-label",children:"Label"}),e.jsx(s,{onClick:c,...t,id:"switch-with-label"})]})}};var d,h,m;i.parameters={...i.parameters,docs:{...(d=i.parameters)==null?void 0:d.docs,source:{originalSource:"{}",...(m=(h=i.parameters)==null?void 0:h.docs)==null?void 0:m.source}}};var p,g,_;o.parameters={...o.parameters,docs:{...(p=o.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  decorators: [Story => <div style={{
+    display: 'flex',
+    alignItems: 'center',
+    gap: '5rem'
+  }}>
+        <Story />
+      </div>],
+  render: args => {
+    const [, setArgs] = useArgs();
+    const handleClick = (event: any) => {
+      setArgs({
+        checked: event.target.checked
+      });
+    };
+    return <>
+        <Label htmlFor="switch-with-label">Label</Label>
+        <Switch onClick={handleClick} {...args} id="switch-with-label" />
+      </>;
+  }
+}`,...(_=(g=o.parameters)==null?void 0:g.docs)==null?void 0:_.source}}};const x=["Default","WithLabel"],v=Object.freeze(Object.defineProperty({__proto__:null,Default:i,WithLabel:o,__namedExportsOrder:x,default:k},Symbol.toStringTag,{value:"Module"}));export{v as S,o as W};
