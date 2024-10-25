e6abd3dcca265bce46507d7c32cf63318ed0a0de
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-10-25 13:42:20 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@cd6e6d0e2bba31ffefc7fbddd7219a5253b39e63 🚀

---
+++ demo-release-please--branches--main/assets/Footer.docs-yvoW7B0R.js
@@ -0,0 +1,32 @@
+import{j as e}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as i}from"./index-BI1Biiay.js";import{ae as s,ak as r,al as a,an as c}from"./index-EVVlnb_x.js";import{F as m,O as p}from"./Footer.stories-DvEAll6d.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-BXGZkleP.js";import"../sb-preview/runtime.js";import"./index-DJFdew98.js";import"./index-D-8MO0q_.js";import"./index-DF3CPj03.js";import"./index-DrFu-skq.js";import"./index.esm-B5L6_zpt.js";import"./clsx-B-dksMZM.js";import"./index.esm-DQgjFsBh.js";import"./PageMenu.stories-DFa96G62.js";import"./Icon-msbkstIT.js";import"./Spotlight-IssvtslZ.js";const l=`<!-- @license CC0-1.0 -->
+
+# Footer
+
+Provides service information at the bottom of every page.
+
+## Guidelines
+
+- The Footer consists of a dark blue [Spotlight](/docs/components-containers-spotlight--docs) and a [Page Menu](/docs/components-navigation-page-menu--docs).
+  It must be used on all websites for the City of Amsterdam.
+- For applications, only the Page Menu can be sufficient.
+- The Footer is the same on every page of the application.
+
+The Spotlight offers space for various practical links:
+
+- The first column focuses on contact information.
+  The information is as specific as possible and tailored to the respective website or page (e.g., including a chat option if available).
+- The second column contains links to relevant (online) sites or sources.
+- The third column refers to newsletters, social media, etc.
+
+The menu at the very bottom is intended for links to privacy policies, cookie statements, information about the website itself, etc.
+Contact details should not go here.
+`;/*@license CC0-1.0*/function o(t){const n={h2:"h2",h3:"h3",p:"p",...i(),...t.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(s,{of:m}),`
+`,e.jsx(r,{children:l}),`
+`,e.jsx(a,{}),`
+`,e.jsx(n.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(n.h3,{id:"onderzoek-en-statistiek",children:"Onderzoek en Statistiek"}),`
+`,e.jsx(n.p,{children:`The footer for a specific website can be a bit different.
+Here’s an example for the ‘Onderzoek en Statistiek’ department.`}),`
+`,e.jsx(c,{of:p})]})}function z(t={}){const{wrapper:n}={...i(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(o,{...t})}):o(t)}export{z as default};
