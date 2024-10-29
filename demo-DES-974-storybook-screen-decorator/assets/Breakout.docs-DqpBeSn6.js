df9d55c84a1123ea4834c7013bdefb4425786889
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-10-29 11:16:01 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@271bf3b1f188beeb41bd501c34798116560d513b 🚀

---
+++ demo-DES-974-storybook-screen-decorator/assets/Breakout.docs-DqpBeSn6.js
@@ -0,0 +1,25 @@
+import{j as e}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as s}from"./index-BI1Biiay.js";import{ae as r,ak as i,al as a,am as l,an as h}from"./index-CKG4Hd89.js";import{B as c,V as d}from"./Breakout.stories-T2nkgZ4A.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-BtsfkDwl.js";import"../sb-preview/runtime.js";import"./index-DJFdew98.js";import"./index-D-8MO0q_.js";import"./index-DF3CPj03.js";import"./index-DrFu-skq.js";import"./index.esm-CXPFsmgB.js";import"./clsx-B-dksMZM.js";import"./index.esm-DQgjFsBh.js";import"./paddingClasses-C5gheHLv.js";const p=`<!-- @license CC0-1.0 -->
+
+# Breakout
+
+A composition that lets a figure – e.g. an image, video or map – break out of a Spotlight.
+`;/*@license CC0-1.0*/function o(n){const t={code:"code",h2:"h2",h3:"h3",p:"p",...s(),...n.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(r,{of:c}),`
+`,e.jsx(i,{children:p}),`
+`,e.jsx(t.h3,{id:"how-to-use",children:"How to use"}),`
+`,e.jsxs(t.p,{children:["This component is an extension of ",e.jsx(t.code,{children:"Grid"}),`.
+It offers the same props, although `,e.jsx(t.code,{children:"span"})," and ",e.jsx(t.code,{children:"start"})," have been renamed to ",e.jsx(t.code,{children:"colSpan"})," and ",e.jsx(t.code,{children:"colStart"}),`.
+Additionally, `,e.jsx(t.code,{children:"Breakout"})," allows cells to span multiple rows."]}),`
+`,e.jsxs(t.p,{children:["The Cell containing the Spotlight must have ",e.jsx(t.code,{children:'has="spotlight"'}),` to extend to the gaps and margins around it.
+This prop can be used only on Cells that span all columns of the grid, so `,e.jsx(t.code,{children:'span="all"'})," is required as well."]}),`
+`,e.jsxs(t.p,{children:["The Cell containing the figure must have ",e.jsx(t.code,{children:'has="figure"'})," to ensure it aligns to the bottom."]}),`
+`,e.jsx(t.p,{children:`The content that doesn’t break out defines the height of the Spotlight.
+Ensure that the text is short, or the image tall, to prevent losing the break-out effect.`}),`
+`,e.jsx(t.p,{children:"On narrower screens, let the text move below the image."}),`
+`,e.jsx(a,{}),`
+`,e.jsx(l,{}),`
+`,e.jsx(t.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(t.h3,{id:"vertical-layout",children:"Vertical layout"}),`
+`,e.jsx(t.p,{children:"A large figure can be placed at the top of the Spotlight, with related text positioned underneath."}),`
+`,e.jsx(h,{of:d})]})}function A(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o(n)}export{A as default};
---
+++ demo-DES-974-storybook-screen-decorator/assets/Screen.docs-BK6vHlAb.js
@@ -0,0 +1,32 @@
+import{j as n}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as s}from"./index-BI1Biiay.js";import{ae as i,ak as a,al as r,am as m,an as c}from"./index-CKG4Hd89.js";import{S as d,E as p}from"./Screen.stories-DN8CPv8z.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-BtsfkDwl.js";import"../sb-preview/runtime.js";import"./index-DJFdew98.js";import"./index-D-8MO0q_.js";import"./index-DF3CPj03.js";import"./index-DrFu-skq.js";import"./clsx-B-dksMZM.js";const h=`<!-- @license CC0-1.0 -->
+
+# Screen
+
+Manages the maximum width and alignment of the entire website or application.
+
+## Guidelines
+
+- This should be the outermost component of your website or application.
+- Within it, combine components such as
+  [Grid](/docs/components-layout-grid--docs),
+  [Header](/docs/components-containers-header--docs),
+  [Footer](/docs/components-containers-footer--docs),
+  [Spotlight](/docs/components-containers-spotlight--docs),
+  [Image Slider](/docs/components-media-image-slider--docs),
+  and Figure.
+
+## Design
+
+By default, this component expands to a width of 1600 pixels.
+This is the maximum width for a website of the City of Amsterdam.
+For applications, you can choose a larger maximum width of 2112 pixels.
+The component horizontally centres both itself and its content.
+`;/*@license CC0-1.0*/function t(o){const e={h2:"h2",h3:"h3",...s(),...o.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(i,{of:d}),`
+`,n.jsx(a,{children:h}),`
+`,n.jsx(r,{}),`
+`,n.jsx(m,{}),`
+`,n.jsx(e.h2,{id:"examples",children:"Examples"}),`
+`,n.jsx(e.h3,{id:"extra-wide",children:"Extra wide"}),`
+`,n.jsx(c,{of:p})]})}function D(o={}){const{wrapper:e}={...s(),...o.components};return e?n.jsx(e,{...o,children:n.jsx(t,{...o})}):t(o)}export{D as default};
