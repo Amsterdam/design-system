3bdc73c699f2d33692938961644f3e77cb69f8e0
alimpens <37216945+alimpens@users.noreply.github.com>
2024-10-30 08:55:11 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@4a6f8558cb6e6a59299d81a155b797b0cf2c1ca8 🚀

---
+++ demo-github_actions/mydea/action-wait-for-api-2/assets/Blockquote.stories-0ySVjV59.js
@@ -0,0 +1,8 @@
+import{i as f}from"./decorators-CweBkPI6.js";import{c as _}from"./exampleContent-hIIPvEhU.js";import{j as g}from"./jsx-runtime-BlAj40OV.js";import{c as q}from"./clsx-B-dksMZM.js";import{r as b}from"./index-Cs7sjTYM.js";const r=b.forwardRef(({children:t,className:u,inverseColor:p,...m},d)=>g.jsx("blockquote",{...m,ref:d,className:q("ams-blockquote",p&&"ams-blockquote--inverse-color",u),children:t}));r.displayName="Blockquote";try{r.displayName="Blockquote",r.__docgenInfo={description:"",displayName:"Blockquote",props:{inverseColor:{defaultValue:null,description:"Changes the text colour for readability on a dark background.",name:"inverseColor",required:!1,type:{name:"boolean"}}}}}catch{}/**
+ * @license EUPL-1.2+
+ * Copyright Gemeente Amsterdam
+ */const k=_(),x={title:"Components/Text/Blockquote",component:r,args:{children:k,inverseColor:!1},argTypes:{children:{description:"The text for the quote.",table:{disable:!1}}},decorators:[f]},e={},o={args:{inverseColor:!0}};var s,a,c;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:"{}",...(c=(a=e.parameters)==null?void 0:a.docs)==null?void 0:c.source}}};var l,n,i;o.parameters={...o.parameters,docs:{...(l=o.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    inverseColor: true
+  }
+}`,...(i=(n=o.parameters)==null?void 0:n.docs)==null?void 0:i.source}}};const v=["Default","InverseColour"],S=Object.freeze(Object.defineProperty({__proto__:null,Default:e,InverseColour:o,__namedExportsOrder:v,default:x},Symbol.toStringTag,{value:"Module"}));export{S as B,o as I};
---
+++ demo-github_actions/mydea/action-wait-for-api-2/assets/layout-B5YJ6HHW.js
@@ -0,0 +1,1 @@
+const e=["start","center","baseline","end"],s=e.filter(n=>n!=="baseline"),t=["center","end","between","around","evenly"];export{e as a,s as c,t as m};
