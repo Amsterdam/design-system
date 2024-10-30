b91b4b160928353888b8245ffa272324f80bb312
Aram <37216945+alimpens@users.noreply.github.com>
2024-10-30 17:31:22 +0100

    Delete demo-DES-728-amopis-example-pages directory

--- demo-DES-728-amopis-example-pages/assets/DetailsPage.docs-baqGWmSk.js
+++
@@ -1,6 +0,0 @@
-import{j as t}from"./jsx-runtime-X2b_N9AH.js";import{useMDXComponents as i}from"./index-CRbi4mQH.js";import{M as e,P as m}from"./index-BOC8kysb.js";import{D as s}from"./DetailsPage.stories-DI5n9scL.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-yJpXivWj.js";import"../sb-preview/runtime.js";import"./index-BOkhicXD.js";import"./index-DXimoRZY.js";import"./index-B8K4vdXH.js";import"./index-DrFu-skq.js";import"./index.esm-yXQQXCDi.js";import"./clsx-B-dksMZM.js";import"./index.esm-BmSTY5-j.js";import"./meta-pWdP192p.js";/*@license CC0-1.0*/function n(o){const r={h1:"h1",p:"p",...i(),...o.components};return t.jsxs(t.Fragment,{children:[`
-`,`
-`,t.jsx(e,{of:s}),`
-`,t.jsx(r.h1,{id:"details-page",children:"Details Page"}),`
-`,t.jsx(r.p,{children:"Displays properties of an entity."}),`
-`,t.jsx(m,{})]})}function X(o={}){const{wrapper:r}={...i(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(n,{...o})}):n(o)}export{X as default};
