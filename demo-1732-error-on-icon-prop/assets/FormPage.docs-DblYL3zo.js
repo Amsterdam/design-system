4729b0098936d2a7e8899e4094b9c7c807a450e1
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-11-04 14:52:35 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@711b1be6ecc7951253898160328b25a03a7cfbbf 🚀

---
+++ demo-1732-error-on-icon-prop/assets/FormPage.docs-DblYL3zo.js
@@ -0,0 +1,5 @@
+import{j as o}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as i}from"./index-BI1Biiay.js";import{ae as e}from"./index-DNXVmPlZ.js";import{F as n}from"./FormPage.stories-DYZJkRLZ.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-Chn7UdQi.js";import"../sb-preview/runtime.js";import"./index-DJFdew98.js";import"./index-D-8MO0q_.js";import"./index-DF3CPj03.js";import"./index-DrFu-skq.js";import"./index.esm-edP8GOgy.js";import"./clsx-B-dksMZM.js";import"./index.esm-DQgjFsBh.js";import"./config-DFibQQL4.js";import"./PageMenu.stories-DFa96G62.js";import"./Icon-msbkstIT.js";import"./Footer.stories-BaJ-LM4v.js";import"./Spotlight-IssvtslZ.js";/*@license CC0-1.0*/function m(t){const r={h1:"h1",p:"p",...i(),...t.components};return o.jsxs(o.Fragment,{children:[`
+`,`
+`,o.jsx(e,{of:n}),`
+`,o.jsx(r.h1,{id:"form-page",children:"Form page"}),`
+`,o.jsx(r.p,{children:"This simple example presents various types of fields."})]})}function w(t={}){const{wrapper:r}={...i(),...t.components};return r?o.jsx(r,{...t,children:o.jsx(m,{...t})}):m(t)}export{w as default};
