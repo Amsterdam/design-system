89cd4b5cf9d7b2c3c932dc1131dd01e779cf081a
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-11-04 14:28:04 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@73a7fb2930cc392460b5c90e027063c85edd0fdf 🚀

---
+++ demo-develop/assets/ErrorMessage.docs-ghqxes26.js
@@ -0,0 +1,23 @@
+import{j as e}from"./jsx-runtime-j_jdvEMj.js";import{useMDXComponents as s}from"./index-BSj771as.js";import{ae as t,aq as a,ar as i,as as m,at as d}from"./index-B8heP7oi.js";import{E as c,C as h}from"./ErrorMessage.stories-B3cj9aqo.js";import"./index-B-o1Wr-g.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-BaCQmPWc.js";import"../sb-preview/runtime.js";import"./index-DolzVqEf.js";import"./index-D-8MO0q_.js";import"./index-CJyPbrN5.js";import"./index-DrFu-skq.js";import"./clsx-B-dksMZM.js";const p=`<!-- @license CC0-1.0 -->
+
+# Error Message
+
+Show an error message when there is a form field validation error.
+In the error message explain what went wrong and how to fix it.
+
+For guidance and examples on using error messages in a form,
+refer to the [Field](/docs/components-forms-field--docs) and [Field Set](/docs/components-forms-field-set--docs) documentation.
+
+Read the documentation by [NL Design System](https://www.nldesignsystem.nl/richtlijnen/formulieren/foutmeldingen) and [Gov.uk](https://design-system.service.gov.uk/components/error-message/) for more information on the contents of error messages and when to show them.
+`;/*@license CC0-1.0*/function r(n){const o={code:"code",h2:"h2",h3:"h3",p:"p",...s(),...n.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(t,{of:c}),`
+`,e.jsx(a,{children:p}),`
+`,e.jsx(i,{}),`
+`,e.jsx(m,{}),`
+`,e.jsx(o.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(o.h3,{id:"custom-prefix",children:"Custom prefix"}),`
+`,e.jsxs(o.p,{children:[`Error messages are automatically prefixed with a visually hidden text, the Dutch word "Invoerfout".
+This makes the error message more clear for screen reader users.
+If you want to change this prefix, to support another language for example, you can use the `,e.jsx(o.code,{children:"prefix"})," prop."]}),`
+`,e.jsx(d,{of:h})]})}function k(n={}){const{wrapper:o}={...s(),...n.components};return o?e.jsx(o,{...n,children:e.jsx(r,{...n})}):r(n)}export{k as default};
---
+++ demo-develop/assets/IconButton.stories-B91M3ZRI.js
@@ -0,0 +1,28 @@
+import{I as c}from"./index.esm-DoGgRjwu.js";import{i as x,c as P}from"./decorators-DYZ3zH5Z.js";import{I as T}from"./IconButton-D2q6GTZc.js";/**
+ * @license EUPL-1.2+
+ * Copyright Gemeente Amsterdam
+ */const k={title:"Components/Buttons/Icon Button",component:T,args:{contrastColor:!1,disabled:!1,inverseColor:!1,label:"Sluiten",size:void 0},argTypes:{disabled:{description:"Prevents interaction. Avoid if possible."},size:{control:{type:"radio",labels:{undefined:"level-5"}},options:["level-3","level-4",void 0,"level-6"]},svg:{control:{type:"select"},options:Object.keys(c),mapping:c}},decorators:[x,P]},e={},r={args:{size:"level-3"}},s={args:{size:"level-4"}},o={args:{size:"level-5"}},a={args:{size:"level-6"}},t={args:{contrastColor:!0}},n={args:{inverseColor:!0}};var l,i,p;e.parameters={...e.parameters,docs:{...(l=e.parameters)==null?void 0:l.docs,source:{originalSource:"{}",...(p=(i=e.parameters)==null?void 0:i.docs)==null?void 0:p.source}}};var u,m,d;r.parameters={...r.parameters,docs:{...(u=r.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    size: 'level-3'
+  }
+}`,...(d=(m=r.parameters)==null?void 0:m.docs)==null?void 0:d.source}}};var v,g,C;s.parameters={...s.parameters,docs:{...(v=s.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    size: 'level-4'
+  }
+}`,...(C=(g=s.parameters)==null?void 0:g.docs)==null?void 0:C.source}}};var f,z,S;o.parameters={...o.parameters,docs:{...(f=o.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    size: 'level-5'
+  }
+}`,...(S=(z=o.parameters)==null?void 0:z.docs)==null?void 0:S.source}}};var b,I,L;a.parameters={...a.parameters,docs:{...(b=a.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    size: 'level-6'
+  }
+}`,...(L=(I=a.parameters)==null?void 0:I.docs)==null?void 0:L.source}}};var y,_,B;t.parameters={...t.parameters,docs:{...(y=t.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    contrastColor: true
+  }
+}`,...(B=(_=t.parameters)==null?void 0:_.docs)==null?void 0:B.source}}};var D,O,j;n.parameters={...n.parameters,docs:{...(D=n.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  args: {
+    inverseColor: true
+  }
+}`,...(j=(O=n.parameters)==null?void 0:O.docs)==null?void 0:j.source}}};const A=["Default","Level3","Level4","Level5","Level6","ContrastColour","InverseColour"],q=Object.freeze(Object.defineProperty({__proto__:null,ContrastColour:t,Default:e,InverseColour:n,Level3:r,Level4:s,Level5:o,Level6:a,__namedExportsOrder:A,default:k},Symbol.toStringTag,{value:"Module"}));export{t as C,q as I,r as L,s as a,o as b,a as c,n as d};
