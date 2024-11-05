300978ce80aeda291f8a93a900ce3ef4e7c79505
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-11-05 19:59:14 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@d6724a7ed6e8c2b81572e36515926dda00b8d743 🚀

---
+++ demo-DES-1005-refactor-search-field-button/assets/Heading.stories-BMyiytH3.js
@@ -0,0 +1,26 @@
+import{j as D}from"./jsx-runtime-j_jdvEMj.js";import{r as s}from"./index-B-o1Wr-g.js";import{C as I}from"./index.esm-BqzW-N8n.js";import{i as O}from"./decorators-DYZ3zH5Z.js";import{e as M}from"./exampleContent-hIIPvEhU.js";import{H as e}from"./Heading-BMR-3CH3.js";const P=M(),R={title:"Components/Text/Heading",component:e,args:{children:P,inverseColor:!1},argTypes:{children:{description:"The heading text.",table:{disable:!1}}},decorators:[O]},a={},l={},o={args:{level:2}},t={args:{level:3}},n={args:{level:4}},c={render:r=>D.jsxs(I,{gap:"small",children:[s.createElement(e,{...r,key:"level-1",size:"level-1"}),s.createElement(e,{...r,key:"level-2",size:"level-2"}),s.createElement(e,{...r,key:"level-3",size:"level-3"}),s.createElement(e,{...r,key:"level-4",size:"level-4"}),s.createElement(e,{...r,key:"level-5",size:"level-5"}),s.createElement(e,{...r,key:"level-6",size:"level-6"})]})},i={args:{inverseColor:!0}};var m,p,d;a.parameters={...a.parameters,docs:{...(m=a.parameters)==null?void 0:m.docs,source:{originalSource:"{}",...(d=(p=a.parameters)==null?void 0:p.docs)==null?void 0:d.source}}};var v,g,u;l.parameters={...l.parameters,docs:{...(v=l.parameters)==null?void 0:v.docs,source:{originalSource:"{}",...(u=(g=l.parameters)==null?void 0:g.docs)==null?void 0:u.source}}};var y,z,f;o.parameters={...o.parameters,docs:{...(y=o.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    level: 2
+  }
+}`,...(f=(z=o.parameters)==null?void 0:z.docs)==null?void 0:f.source}}};var S,k,H;t.parameters={...t.parameters,docs:{...(S=t.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    level: 3
+  }
+}`,...(H=(k=t.parameters)==null?void 0:k.docs)==null?void 0:H.source}}};var C,x,E;n.parameters={...n.parameters,docs:{...(C=n.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  args: {
+    level: 4
+  }
+}`,...(E=(x=n.parameters)==null?void 0:x.docs)==null?void 0:E.source}}};var L,b,h;c.parameters={...c.parameters,docs:{...(L=c.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  render: args => <Column gap="small">
+      <Heading {...args} key="level-1" size="level-1" />
+      <Heading {...args} key="level-2" size="level-2" />
+      <Heading {...args} key="level-3" size="level-3" />
+      <Heading {...args} key="level-4" size="level-4" />
+      <Heading {...args} key="level-5" size="level-5" />
+      <Heading {...args} key="level-6" size="level-6" />
+    </Column>
+}`,...(h=(b=c.parameters)==null?void 0:b.docs)==null?void 0:h.source}}};var _,j,T;i.parameters={...i.parameters,docs:{...(_=i.parameters)==null?void 0:_.docs,source:{originalSource:`{
+  args: {
+    inverseColor: true
+  }
+}`,...(T=(j=i.parameters)==null?void 0:j.docs)==null?void 0:T.source}}};const q=["Default","Level1","Level2","Level3","Level4","Sizes","InverseColour"],K=Object.freeze(Object.defineProperty({__proto__:null,Default:a,InverseColour:i,Level1:l,Level2:o,Level3:t,Level4:n,Sizes:c,__namedExportsOrder:q,default:R},Symbol.toStringTag,{value:"Module"}));export{K as H,i as I,l as L,c as S,o as a,t as b,n as c};
