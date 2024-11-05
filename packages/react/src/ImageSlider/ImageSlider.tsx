a4d93061e52deb40522d646c1bdc8ad7344f8ee4
Vincent Smedinga <v.smedinga@amsterdam.nl>
2024-11-05 20:27:21 +0100

    Restore support for tall images

--- packages/react/src/ImageSlider/ImageSlider.tsx
+++ packages/react/src/ImageSlider/ImageSlider.tsx
@@ -16,7 +16,7 @@ import { Image, ImageProps } from '../Image/Image'
 
 export type ImageSliderImageProps = Omit<ImageProps, 'aspectRatio'> & {
   /** Specify the aspect ratio to use for the images. */
-  aspectRatio: Pick<Ratio, 'square' | 'wide' | 'x-wide'>
+  aspectRatio: Pick<Ratio, 'tall' | 'square' | 'wide' | 'x-wide'>
 }
 
 export type ImageSliderProps = {
