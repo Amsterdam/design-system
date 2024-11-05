747ef9e50cf8733ae4d1f4c570479e6fe8d3695d
Vincent Smedinga <v.smedinga@amsterdam.nl>
2024-11-05 16:56:11 +0100

    Remove unused class name

--- packages/react/src/SearchField/SearchFieldButton.tsx
+++ packages/react/src/SearchField/SearchFieldButton.tsx
@@ -4,7 +4,6 @@
  */
 
 import { SearchIcon } from '@amsterdam/design-system-react-icons'
-import clsx from 'clsx'
 import { forwardRef } from 'react'
 import type { ForwardedRef } from 'react'
 import { Button } from '../Button'
@@ -13,14 +12,8 @@ import type { ButtonProps } from '../Button'
 type SearchFieldButtonProps = Omit<ButtonProps, 'icon' | 'iconPosition' | 'variant'>
 
 export const SearchFieldButton = forwardRef(
-  ({ className, children = 'Zoeken', ...restProps }: SearchFieldButtonProps, ref: ForwardedRef<HTMLButtonElement>) => (
-    <Button
-      {...restProps}
-      className={clsx('ams-search-field__button', className)}
-      icon={SearchIcon}
-      iconBefore
-      ref={ref}
-    >
+  ({ children = 'Zoeken', ...restProps }: SearchFieldButtonProps, ref: ForwardedRef<HTMLButtonElement>) => (
+    <Button {...restProps} icon={SearchIcon} iconBefore ref={ref}>
       {children}
     </Button>
   ),
