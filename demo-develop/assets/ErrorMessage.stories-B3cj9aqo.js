738225f60e2a811246ed531053c84b55ebf491f3
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-11-06 16:48:46 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@eca5fe4ed0325b01860f816c70247c26e2158f9c 🚀

--- demo-develop/assets/ErrorMessage.stories-B3cj9aqo.js
+++
@@ -1,9 +0,0 @@
-import{j as o}from"./jsx-runtime-j_jdvEMj.js";import{c as g}from"./clsx-B-dksMZM.js";import{r as x}from"./index-B-o1Wr-g.js";const s=x.forwardRef(({children:a,className:d,prefix:p="Invoerfout",...u},f)=>o.jsxs("p",{...u,ref:f,className:g("ams-error-message",d),children:[o.jsxs("span",{className:"ams-visually-hidden",children:[p,": "]}),a]}));s.displayName="ErrorMessage";try{s.displayName="ErrorMessage",s.__docgenInfo={description:"",displayName:"ErrorMessage",props:{prefix:{defaultValue:{value:"Invoerfout"},description:"An accessible phrase that screen readers announce before the error message. Should translate to something like ‘input error’.",name:"prefix",required:!1,type:{name:"string"}}}}}catch{}/**
- * @license EUPL-1.2+
- * Copyright Gemeente Amsterdam
- */const h={title:"Components/Forms/Error Message",component:s,args:{children:"Vul een geldig e-mailadres in, bijvoorbeeld naam@voorbeeld.nl."},argTypes:{children:{table:{disable:!1}}}},e={},r={args:{children:"Enter an email address in the correct format, like name@example.com",prefix:"Error"}};var t,n,i;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:"{}",...(i=(n=e.parameters)==null?void 0:n.docs)==null?void 0:i.source}}};var c,l,m;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:`{
-  args: {
-    children: 'Enter an email address in the correct format, like name@example.com',
-    prefix: 'Error'
-  }
-}`,...(m=(l=r.parameters)==null?void 0:l.docs)==null?void 0:m.source}}};const E=["Default","CustomPrefix"],j=Object.freeze(Object.defineProperty({__proto__:null,CustomPrefix:r,Default:e,__namedExportsOrder:E,default:h},Symbol.toStringTag,{value:"Module"}));export{r as C,j as E};
