000e53b40332b011223bc1fc34d337f893a55b4b
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-11-07 10:57:14 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@e9936910b907b483fd83b8c9d78283a19f473840 🚀

---
+++ demo-DES-834-aspect-ratio-comp-to-prop/assets/AspectRatio.docs-DDKDWrFh.js
@@ -0,0 +1,26 @@
+import{j as t}from"./jsx-runtime-j_jdvEMj.js";import{useMDXComponents as r}from"./index-BSj771as.js";import{ae as i,aq as s,ar as a,as as m}from"./index-Du87LWSv.js";import{A as p}from"./AspectRatio.stories-C57oFleZ.js";import"./index-B-o1Wr-g.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-B308A611.js";import"../sb-preview/runtime.js";import"./index-DolzVqEf.js";import"./index-D-8MO0q_.js";import"./index-CJyPbrN5.js";import"./index-DrFu-skq.js";import"./index.esm-C2_JCJAQ.js";import"./clsx-B-dksMZM.js";import"./index.esm-CEuZchO4.js";import"./index-CAgONygT.js";const c=`<!-- @license CC0-1.0 -->
+
+# Aspect Ratio
+
+Aspect Ratio updates the height of an element with its width.
+
+These are the proportions we use:
+
+| keyword   | ratio |
+| :-------- | ----: |
+| \`2x-wide\` |  16:5 |
+| \`x-wide\`  |  16:9 |
+| \`wide\`    |   4:3 |
+| \`square\`  |   1:1 |
+| \`tall\`    |   3:4 |
+| \`x-tall\`  |  9:16 |
+
+## Guidelines
+
+- Use this CSS class to constrain images, videos and other elements to one of the available aspect ratios.
+`;/*@license CC0-1.0*/function o(n){return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(i,{of:p}),`
+`,t.jsx(s,{children:c}),`
+`,t.jsx(a,{}),`
+`,t.jsx(m,{})]})}function S(n={}){const{wrapper:e}={...r(),...n.components};return e?t.jsx(e,{...n,children:t.jsx(o,{...n})}):o()}export{S as default};
