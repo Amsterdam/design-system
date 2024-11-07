f349157715d575f8234246c0651b6aab1df65442
Vincent Smedinga <v.smedinga@amsterdam.nl>
2024-11-07 14:11:33 +0100

    Remove intermediate type

--- packages/react/src/common/aspectRatio.ts
+++ packages/react/src/common/aspectRatio.ts
@@ -1,6 +1,4 @@
-type Ratio = 'x-tall' | 'tall' | 'square' | 'wide' | 'x-wide' | '2x-wide'
-
 export type AspectRatioProps = {
-  /** The ratio of the horizontal and vertical dimensions. */
-  aspectRatio?: Ratio
+  /** The aspect ratio to display media content in. */
+  aspectRatio?: 'x-tall' | 'tall' | 'square' | 'wide' | 'x-wide' | '2x-wide'
 }
