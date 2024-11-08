56b5aeaa290f948aca340dc5e96feb533f4fb445
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-11-08 10:29:55 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@14893b988bedcb69066de6b4cacff44a3dd4d5d9 🚀

---
+++ demo-DES-834-aspect-ratio-comp-to-prop/assets/AspectRatio.docs-CJbUnIy9.js
@@ -0,0 +1,30 @@
+import{j as a}from"./jsx-runtime-j_jdvEMj.js";import{useMDXComponents as o}from"./index-BSj771as.js";import{ae as n,aq as i,ar as m,as as c}from"./index-Dh5V1uqO.js";import{A as r}from"./AspectRatio.stories-1YKRPR3L.js";import"./index-B-o1Wr-g.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-B1pIrsXu.js";import"../sb-preview/runtime.js";import"./index-DolzVqEf.js";import"./index-D-8MO0q_.js";import"./index-CJyPbrN5.js";import"./index-DrFu-skq.js";const p=`<!-- @license CC0-1.0 -->
+
+# Aspect Ratio
+
+Aspect Ratio updates the height of an element with its width.
+
+## Class names
+
+Each available aspect ratio has an associated class name.
+The class can be applied to any component or element.
+
+| Class name                  | Example                                                                     |
+| :-------------------------- | :-------------------------------------------------------------------------- |
+| \`ams-aspect-ratio--x-tall\`  | <div className="ams-docs-token-example--space ams-aspect-ratio--x-tall" />  |
+| \`ams-aspect-ratio--tall\`    | <div className="ams-docs-token-example--space ams-aspect-ratio--tall" />    |
+| \`ams-aspect-ratio--square\`  | <div className="ams-docs-token-example--space ams-aspect-ratio--square" />  |
+| \`ams-aspect-ratio--wide\`    | <div className="ams-docs-token-example--space ams-aspect-ratio--wide" />    |
+| \`ams-aspect-ratio--x-wide\`  | <div className="ams-docs-token-example--space ams-aspect-ratio--x-wide" />  |
+| \`ams-aspect-ratio--2x-wide\` | <div className="ams-docs-token-example--space ams-aspect-ratio--2x-wide" /> |
+
+## Guidelines
+
+- Apply one of these classes to an image, video or other media content to constrain its dimensions to one of the available aspect ratios.
+- The default aspect ratio is 16:9.
+`;/*@license CC0-1.0*/function t(e){return a.jsxs(a.Fragment,{children:[`
+`,`
+`,a.jsx(n,{of:r}),`
+`,a.jsx(i,{children:p}),`
+`,a.jsx(m,{}),`
+`,a.jsx(c,{})]})}function A(e={}){const{wrapper:s}={...o(),...e.components};return s?a.jsx(s,{...e,children:a.jsx(t,{...e})}):t()}export{A as default};
