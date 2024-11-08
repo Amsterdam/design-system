46f317d65f745d53720b1ecd61c6ea918887b4d0
alimpens <37216945+alimpens@users.noreply.github.com>
2024-11-08 13:11:40 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@92312faf845fbb8d28494aa46a57bdc8f378f402 🚀

---
+++ demo-develop/assets/DescriptionList.docs-BgB3k6bH.js
@@ -0,0 +1,36 @@
+import{j as e}from"./jsx-runtime-j_jdvEMj.js";import{useMDXComponents as o}from"./index-BSj771as.js";import{ae as r,aq as a,ar as c,as as p,at as i}from"./index-BmggfTif.js";import{D as l,M as d,R as h,I as m}from"./DescriptionList.stories-BvdJYO0T.js";import"./index-B-o1Wr-g.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-BvT0cfqx.js";import"../sb-preview/runtime.js";import"./index-DolzVqEf.js";import"./index-D-8MO0q_.js";import"./index-CJyPbrN5.js";import"./index-DrFu-skq.js";import"./index.esm-CCdWh44F.js";import"./clsx-B-dksMZM.js";import"./index.esm-CEuZchO4.js";import"./decorators-DYZ3zH5Z.js";const u=`<!-- @license CC0-1.0 -->
+
+# Description List
+
+A collection of terms and their descriptions.
+
+## Design
+
+In a narrow window, descriptions appear indented below their term.
+After that, they display in a two-column layout.
+The column for the terms is as wide as the longest term, without wrapping.
+Its width can be adjusted to be ‘large’ (50%), ‘medium’ (33%), or ‘small’ (20%), which also allows the terms to wrap.
+
+The term is set in bold text.
+
+## References
+
+- [MDN: \`<dl>\`: The Description List element](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/dl)
+- [WCAG: Using description lists](https://www.w3.org/WAI/WCAG22/Techniques/html/H40)
+`;/*@license CC0-1.0*/function s(t){const n={code:"code",h2:"h2",h3:"h3",p:"p",...o(),...t.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(r,{of:l}),`
+`,e.jsx(a,{children:u}),`
+`,e.jsx(c,{}),`
+`,e.jsx(p,{}),`
+`,e.jsx(n.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(n.h3,{id:"multiple-descriptions",children:"Multiple descriptions"}),`
+`,e.jsx(n.p,{children:"A term may have multiple descriptions."}),`
+`,e.jsx(i,{of:d}),`
+`,e.jsx(n.h3,{id:"rich-description",children:"Rich description"}),`
+`,e.jsx(n.p,{children:"A description can include rich content such as inline formatting, links, paragraphs, and even lists."}),`
+`,e.jsx(i,{of:h}),`
+`,e.jsx(n.h3,{id:"inverse-colour",children:"Inverse colour"}),`
+`,e.jsxs(n.p,{children:["Set the ",e.jsx(n.code,{children:"inverseColor"}),` prop if the Description List sits on a dark background.
+This ensures the colour of the text provides enough contrast.`]}),`
+`,e.jsx(i,{of:m})]})}function E(t={}){const{wrapper:n}={...o(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(s,{...t})}):s(t)}export{E as default};
