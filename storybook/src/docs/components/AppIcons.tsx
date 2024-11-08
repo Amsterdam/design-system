3967d8befca76749761f1ed5744ee42b463c2d42
Vincent Smedinga <v.smedinga@amsterdam.nl>
2024-11-08 11:59:52 +0100

    Remove alternate text for decorative images

--- storybook/src/docs/components/AppIcons.tsx
+++ storybook/src/docs/components/AppIcons.tsx
@@ -3,7 +3,7 @@ import { Image } from '@amsterdam/design-system-react'
 export const AppleTouchIcon = () => (
   <div className="ams-docs-gallery">
     <figure className="ams-docs-figure">
-      <Image alt="apple-touch-icon.png (180px)" src="favicon/apple-touch-icon.png" width={180} height={180} />
+      <Image alt="" src="favicon/apple-touch-icon.png" width={180} height={180} />
       <figcaption>apple-touch-icon.png (180px)</figcaption>
     </figure>
   </div>
@@ -12,19 +12,19 @@ export const AppleTouchIcon = () => (
 export const Favicon = () => (
   <div className="ams-docs-gallery">
     <figure className="ams-docs-figure">
-      <Image alt="favicon.ico (16px)" src="favicon/favicon.ico" width={16} height={16} />
+      <Image alt="" src="favicon/favicon.ico" width={16} height={16} />
       <figcaption>favicon.ico (16px)</figcaption>
     </figure>
     <figure className="ams-docs-figure">
-      <Image alt="favicon.ico (32px)" src="favicon/favicon.ico" width={32} height={32} />
+      <Image alt="" src="favicon/favicon.ico" width={32} height={32} />
       <figcaption>favicon.ico (32px)</figcaption>
     </figure>
     <figure className="ams-docs-figure">
-      <Image alt="favicon.ico (48px)" src="favicon/favicon.ico" width={48} height={48} />
+      <Image alt="" src="favicon/favicon.ico" width={48} height={48} />
       <figcaption>favicon.ico (48px)</figcaption>
     </figure>
     <figure className="ams-docs-figure">
-      <Image alt="favicon.ico (64px)" src="favicon/favicon.ico" width={64} height={64} />
+      <Image alt="" src="favicon/favicon.ico" width={64} height={64} />
       <figcaption>favicon.ico (64px)</figcaption>
     </figure>
   </div>
@@ -33,7 +33,7 @@ export const Favicon = () => (
 export const SvgIcon = () => (
   <div className="ams-docs-gallery">
     <figure className="ams-docs-figure">
-      <Image alt="icon.svg (64px)" src="favicon/icon.svg" width={64} height={64} />
+      <Image alt="" src="favicon/icon.svg" width={64} height={64} />
       <figcaption>icon.svg (64px)</figcaption>
     </figure>
   </div>
@@ -42,11 +42,11 @@ export const SvgIcon = () => (
 export const WebAppIcons = () => (
   <div className="ams-docs-gallery">
     <figure className="ams-docs-figure">
-      <Image alt="icon-192.png (192px)" src="app-icons/icon-192.png" width={192} height={192} />
+      <Image alt="" src="app-icons/icon-192.png" width={192} height={192} />
       <figcaption>icon-192.png (192px)</figcaption>
     </figure>
     <figure className="ams-docs-figure">
-      <Image alt="icon-512.png (512px)" src="app-icons/icon-512.png" width={512} height={512} />
+      <Image alt="" src="app-icons/icon-512.png" width={512} height={512} />
       <figcaption>icon-512.png (512px)</figcaption>
     </figure>
   </div>
