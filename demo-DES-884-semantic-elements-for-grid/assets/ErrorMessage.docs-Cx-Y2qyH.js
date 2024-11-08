3b022236c0e996ed404f7a2893e7b12ab36c4510
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-11-08 15:08:13 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@69c02007ec638857f5bd6939de78eefd3f2c84ed 🚀

---
+++ demo-DES-884-semantic-elements-for-grid/assets/ErrorMessage.docs-Cx-Y2qyH.js
@@ -0,0 +1,27 @@
+import{j as e}from"./jsx-runtime-j_jdvEMj.js";import{useMDXComponents as s}from"./index-BSj771as.js";import{ae as i,aq as a,ar as m,as as c,at as r}from"./index-BROEAuVh.js";import{E as h,C as d,W as p}from"./ErrorMessage.stories-BemBB-3W.js";import"./index-B-o1Wr-g.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-BIe2snBo.js";import"../sb-preview/runtime.js";import"./index-DolzVqEf.js";import"./index-D-8MO0q_.js";import"./index-CJyPbrN5.js";import"./index-DrFu-skq.js";import"./index.esm-CEuZchO4.js";import"./clsx-B-dksMZM.js";import"./Icon-D1obyjGa.js";const f=`<!-- @license CC0-1.0 -->
+
+# Error Message
+
+Show an error message when there is a form field validation error.
+In the error message explain what went wrong and how to fix it.
+
+For guidance and examples on using error messages in a form,
+refer to the [Field](/docs/components-forms-field--docs) and [Field Set](/docs/components-forms-field-set--docs) documentation.
+
+Read the documentation by [NL Design System](https://www.nldesignsystem.nl/richtlijnen/formulieren/foutmeldingen) and [Gov.uk](https://design-system.service.gov.uk/components/error-message/) for more information on the contents of error messages and when to show them.
+`;/*@license CC0-1.0*/function t(n){const o={code:"code",h2:"h2",h3:"h3",p:"p",...s(),...n.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(i,{of:h}),`
+`,e.jsx(a,{children:f}),`
+`,e.jsx(m,{}),`
+`,e.jsx(c,{}),`
+`,e.jsx(o.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(o.h3,{id:"custom-prefix",children:"Custom prefix"}),`
+`,e.jsxs(o.p,{children:[`Error messages are automatically prefixed with a visually hidden text, the Dutch word "Invoerfout".
+This makes the error message more clear for screen reader users.
+If you want to change this prefix, to support another language for example, you can use the `,e.jsx(o.code,{children:"prefix"})," prop."]}),`
+`,e.jsx(r,{of:d}),`
+`,e.jsx(o.h3,{id:"with-custom-icon",children:"With custom icon"}),`
+`,e.jsx(o.p,{children:`Replace the icon with another to use Error Message in a different theme or visual identity.
+Applications for the City of Amsterdam must use the default icon.`}),`
+`,e.jsx(r,{of:p})]})}function R(n={}){const{wrapper:o}={...s(),...n.components};return o?e.jsx(o,{...n,children:e.jsx(t,{...n})}):t(n)}export{R as default};
---
+++ demo-DES-884-semantic-elements-for-grid/assets/decorators-DYZ3zH5Z.js
@@ -0,0 +1,4 @@
+import{j as o}from"./jsx-runtime-j_jdvEMj.js";/**
+ * @license EUPL-1.2+
+ * Copyright Gemeente Amsterdam
+ */const e=(r,a)=>o.jsx("div",{className:a.args.contrastColor?"ams-docs-light-background":void 0,children:o.jsx(r,{})}),s=(r,a)=>o.jsx("div",{className:a.args.inverseColor?"ams-docs-dark-background":void 0,children:o.jsx(r,{})});try{e.displayName="contrastColorDecorator",e.__docgenInfo={description:"Applies a light background colour if the `contrastColor` arg is true.",displayName:"contrastColorDecorator",props:{}}}catch{}try{s.displayName="inverseColorDecorator",s.__docgenInfo={description:"Applies a dark background colour if the `inverseColor` arg is true.",displayName:"inverseColorDecorator",props:{}}}catch{}export{e as c,s as i};
