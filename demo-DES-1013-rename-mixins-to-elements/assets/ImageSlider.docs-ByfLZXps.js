7550331f6ac3ce55dfd1c91f25b5e31634979229
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-11-08 11:25:29 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@8a78e03c45576654ee9debaf2bbff852abff5163 🚀

---
+++ demo-DES-1013-rename-mixins-to-elements/assets/ImageSlider.docs-ByfLZXps.js
@@ -0,0 +1,37 @@
+import{j as e}from"./jsx-runtime-j_jdvEMj.js";import{useMDXComponents as o}from"./index-BSj771as.js";import{ae as i,aq as a,ar as r,as as l,at as m}from"./index-CUPmeG5-.js";import{I as d,R as h}from"./ImageSlider.stories-D_jpmWP3.js";import"./index-B-o1Wr-g.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-Chfg6wO6.js";import"../sb-preview/runtime.js";import"./index-DolzVqEf.js";import"./index-D-8MO0q_.js";import"./index-CJyPbrN5.js";import"./index-DrFu-skq.js";import"./clsx-B-dksMZM.js";import"./index.esm-CEuZchO4.js";import"./IconButton-CjnVutoo.js";import"./Icon-D1obyjGa.js";import"./Image-FUeHJ70v.js";import"./Screen-CvK1H_-v.js";const c=`<!-- @license CC0-1.0 -->
+
+# Image Slider
+
+Displays a small set of images in a limited space.
+
+## Design
+
+The first or selected image shows at its maximum size.
+Every image displays a thumbnail at about 20% of its width.
+Users can use buttons, thumbnails or swiping to navigate between the images.
+The buttons re not displayed on a narrow touch device.
+The images do not slide automatically.
+
+## How to use
+
+- Use this for a series of images that belong together.
+- Provide at least 2 images and at most 5.
+- Feature the most essential image first.
+- Assume that some or many users will not navigate between the slides and only see the first image.
+  Display all images separately if you want each of them to receive attention.
+- A full-width Image Slider should run from one edge of the Screen to the other;
+  position it next to a Grid, not in a Cell spanning all columns.
+  Smaller sliders are fine in a Grid Cell.
+- Consult the [Image](/docs/components-media-image--docs) docs for guidelines on the individual images.
+`;/*@license CC0-1.0*/function s(n){const t={a:"a",code:"code",h2:"h2",h3:"h3",p:"p",...o(),...n.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(i,{of:d}),`
+`,e.jsx(a,{children:c}),`
+`,e.jsx(r,{}),`
+`,e.jsx(l,{}),`
+`,e.jsx(t.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(t.h3,{id:"responsive-images",children:"Responsive images"}),`
+`,e.jsxs(t.p,{children:["Provide a set of smaller and larger instances for every image (through ",e.jsx(t.code,{children:"srcSet"}),", see ",e.jsx(t.a,{href:"https://developer.mozilla.org/en-US/docs/Web/HTML/Element/img#srcset",rel:"nofollow",children:"MDN"}),`) to let the browser select the optimal source file.
+A mobile device can often download a smaller file, saving the user’s bandwidth and time.
+Don’t forget to still include the required `,e.jsx(t.code,{children:"src"})," attribute."]}),`
+`,e.jsx(m,{of:h})]})}function T(n={}){const{wrapper:t}={...o(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(s,{...n})}):s(n)}export{T as default};
