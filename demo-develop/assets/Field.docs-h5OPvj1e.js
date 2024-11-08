664829889a95e47c0cb042f59e9028f3a82aa719
alimpens <37216945+alimpens@users.noreply.github.com>
2024-11-08 11:18:04 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@6a401828af96291ebce241009a019496bbe6a9c2 🚀

---
+++ demo-develop/assets/Field.docs-h5OPvj1e.js
@@ -0,0 +1,29 @@
+import{j as e}from"./jsx-runtime-j_jdvEMj.js";import{useMDXComponents as s}from"./index-BSj771as.js";import{ae as o,aq as a,ar as d,as as c,at as i}from"./index-CEVuhxgr.js";import{F as h,W as p,a as l}from"./Field.stories-Cg3olwc4.js";import"./index-B-o1Wr-g.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-Cx_sc6HZ.js";import"../sb-preview/runtime.js";import"./index-DolzVqEf.js";import"./index-D-8MO0q_.js";import"./index-CJyPbrN5.js";import"./index-DrFu-skq.js";import"./index.esm-Bdr_Cxzh.js";import"./clsx-B-dksMZM.js";import"./index.esm-CEuZchO4.js";const m=`<!-- @license CC0-1.0 -->
+
+# Field
+
+Wraps a single input and its related elements. May indicate that the input has a validation error.
+
+## Guidelines
+
+- Only use Field to wrap a single input. Use [Field Set](/docs/components-forms-field-set--docs) to wrap multiple inputs.
+`;/*@license CC0-1.0*/function r(t){const n={a:"a",code:"code",h2:"h2",h3:"h3",p:"p",...s(),...t.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(o,{of:h}),`
+`,e.jsx(a,{children:m}),`
+`,e.jsx(d,{}),`
+`,e.jsx(c,{}),`
+`,e.jsx(n.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(n.h3,{id:"with-description",children:"With description"}),`
+`,e.jsxs(n.p,{children:[`A Field can have a description.
+Make sure to connect this description to the input in the Field,
+otherwise it won’t be read by a screen reader.
+Add an `,e.jsx(n.code,{children:"aria-describedby"})," attribute to the input and provide the ",e.jsx(n.code,{children:"id"})," of the describing element as its value."]}),`
+`,e.jsx(i,{of:p}),`
+`,e.jsx(n.h3,{id:"with-validation",children:"With validation"}),`
+`,e.jsxs(n.p,{children:[`A Field can indicate if the contained input has a validation error.
+Use an `,e.jsx(n.a,{href:"/docs/components-forms-error-message--docs",children:"Error Message"}),` to describe the error.
+Make sure to connect the Error Message to the input in the Field,
+otherwise it won’t be read by a screen reader.
+Add an `,e.jsx(n.code,{children:"aria-describedby"})," attribute to the input and provide the ",e.jsx(n.code,{children:"id"})," of Error Message as its value."]}),`
+`,e.jsx(i,{of:l})]})}function D(t={}){const{wrapper:n}={...s(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(r,{...t})}):r(t)}export{D as default};
