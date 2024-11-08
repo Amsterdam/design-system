bfa6dfb66ccd4a58b9ba0ff21627ccaffb39423f
Vincent Smedinga <v.smedinga@amsterdam.nl>
2024-11-08 11:25:03 +0100

    Inline type to prevent confusing rename

--- storybook/src/utils/AspectRatio/AspectRatio.tsx
+++ storybook/src/utils/AspectRatio/AspectRatio.tsx
@@ -3,12 +3,13 @@
  * Copyright Gemeente Amsterdam
  */
 
-import { AspectRatioProps as IAspectRatioProps } from '@amsterdam/design-system-react/src/common/types'
+import { AspectRatioProps } from '@amsterdam/design-system-react/src/common/types'
 import { HTMLAttributes, PropsWithChildren } from 'react'
 
-export type AspectRatioProps = IAspectRatioProps & PropsWithChildren<HTMLAttributes<HTMLSpanElement>>
-
 /** Renders examples in Storybook. Not for reuse. */
-export const AspectRatio = ({ children, aspectRatio }: AspectRatioProps) => (
+export const AspectRatio = ({
+  children,
+  aspectRatio,
+}: AspectRatioProps & PropsWithChildren<HTMLAttributes<HTMLSpanElement>>) => (
   <span className={`ams-aspect-ratio--${aspectRatio}`}>{children}</span>
 )
