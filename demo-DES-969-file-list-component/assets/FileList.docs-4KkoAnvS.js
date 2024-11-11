d67c013d4dbecbf480590a36de32ec43ac6e84f1
dlnr <niels@dlnr.dev>
2024-11-11 14:42:47 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@3706713993ad73f6475b6c63c869a6e80767dd78 🚀

--- demo-DES-969-file-list-component/assets/FileList.docs-4KkoAnvS.js
+++
@@ -1,9 +0,0 @@
-import{j as t}from"./jsx-runtime-j_jdvEMj.js";import{useMDXComponents as s}from"./index-BSj771as.js";import{ae as i,aq as e,ar as m,as as a}from"./index-BUBgChon.js";import{F as p}from"./FileList.stories-Cfi9Swun.js";import"./index-B-o1Wr-g.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-Q5NHX3yD.js";import"../sb-preview/runtime.js";import"./index-DolzVqEf.js";import"./index-D-8MO0q_.js";import"./index-CJyPbrN5.js";import"./index-DrFu-skq.js";import"./clsx-B-dksMZM.js";const x=`<!-- @license CC0-1.0 -->
-
-# FileList
-`;/*@license CC0-1.0*/function n(r){return t.jsxs(t.Fragment,{children:[`
-`,`
-`,t.jsx(i,{of:p}),`
-`,t.jsx(e,{children:x}),`
-`,t.jsx(m,{}),`
-`,t.jsx(a,{})]})}function X(r={}){const{wrapper:o}={...s(),...r.components};return o?t.jsx(o,{...r,children:t.jsx(n,{...r})}):n()}export{X as default};
---
+++ demo-DES-969-file-list-component/assets/FormPage.docs-DhqqRHiS.js
@@ -0,0 +1,5 @@
+import{j as o}from"./jsx-runtime-j_jdvEMj.js";import{useMDXComponents as i}from"./index-BSj771as.js";import{ae as e}from"./index-CQ0fM7nx.js";import{F as n}from"./FormPage.stories-DjUlpYvB.js";import"./index-B-o1Wr-g.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-D31QcmIu.js";import"../sb-preview/runtime.js";import"./index-DolzVqEf.js";import"./index-D-8MO0q_.js";import"./index-CJyPbrN5.js";import"./index-DrFu-skq.js";import"./index.esm-BrzuHIqe.js";import"./index.esm-GlSJ_tyX.js";import"./clsx-B-dksMZM.js";import"./config-LoqN_vUL.js";import"./PageMenu.stories-CYi_8Euo.js";import"./Icon-D1obyjGa.js";import"./Footer.stories-Brmflhcf.js";import"./Spotlight-iwYL-F79.js";/*@license CC0-1.0*/function m(t){const r={h1:"h1",p:"p",...i(),...t.components};return o.jsxs(o.Fragment,{children:[`
+`,`
+`,o.jsx(e,{of:n}),`
+`,o.jsx(r.h1,{id:"form-page",children:"Form page"}),`
+`,o.jsx(r.p,{children:"This simple example presents various types of fields."})]})}function w(t={}){const{wrapper:r}={...i(),...t.components};return r?o.jsx(r,{...t,children:o.jsx(m,{...t})}):m(t)}export{w as default};
