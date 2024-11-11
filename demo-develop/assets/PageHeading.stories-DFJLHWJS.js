79e8c06138cc3512b2e410f96b0fd49469ebaebf
alimpens <37216945+alimpens@users.noreply.github.com>
2024-11-11 08:54:48 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@e5725a76ad40f8f0756ebb177fde6ae650d42309 🚀

--- demo-develop/assets/PageHeading.stories-DFJLHWJS.js
+++
@@ -1,8 +0,0 @@
-import{i as u}from"./decorators-DYZ3zH5Z.js";import{j as f}from"./jsx-runtime-j_jdvEMj.js";import{c as _}from"./clsx-B-dksMZM.js";import{r as h}from"./index-B-o1Wr-g.js";const a=h.forwardRef(({children:o,className:l,inverseColor:p,...m},g)=>f.jsx("h1",{...m,ref:g,className:_("ams-page-heading",p&&"ams-page-heading--inverse-color",l),children:o}));a.displayName="PageHeading";try{a.displayName="PageHeading",a.__docgenInfo={description:"",displayName:"PageHeading",props:{inverseColor:{defaultValue:null,description:"Changes the text colour for readability on a dark background.",name:"inverseColor",required:!1,type:{name:"boolean"}}}}}catch{}/**
- * @license EUPL-1.2+
- * Copyright Gemeente Amsterdam
- */const v={title:"Components/Text/Page Heading",component:a,args:{children:"Gemeente Amsterdam",inverseColor:!1},argTypes:{children:{description:"The heading text.",table:{disable:!1}}},decorators:[u]},e={},r={args:{inverseColor:!0}};var s,t,n;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:"{}",...(n=(t=e.parameters)==null?void 0:t.docs)==null?void 0:n.source}}};var i,c,d;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:`{
-  args: {
-    inverseColor: true
-  }
-}`,...(d=(c=r.parameters)==null?void 0:c.docs)==null?void 0:d.source}}};const x=["Default","InverseColour"],H=Object.freeze(Object.defineProperty({__proto__:null,Default:e,InverseColour:r,__namedExportsOrder:x,default:v},Symbol.toStringTag,{value:"Module"}));export{r as I,H as P};
---
+++ demo-develop/assets/TimeInput.docs-DHosPgXK.js
@@ -0,0 +1,31 @@
+import{j as n}from"./jsx-runtime-j_jdvEMj.js";import{useMDXComponents as t}from"./index-BSj771as.js";import{ae as r,aq as a,ar as d,as as c,at as o}from"./index-Cp1k0SM3.js";import{T as h,I as l,D as m,a as p,b as f}from"./TimeInput.stories-BefrKExg.js";import"./index-B-o1Wr-g.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-k0_W_Lz8.js";import"../sb-preview/runtime.js";import"./index-DolzVqEf.js";import"./index-D-8MO0q_.js";import"./index-CJyPbrN5.js";import"./index-DrFu-skq.js";import"./index.esm-DBblPCWP.js";import"./clsx-B-dksMZM.js";import"./index.esm-CEuZchO4.js";const x=`<!-- @license CC0-1.0 -->
+
+# Time Input
+
+Helps users enter time.
+
+## Visual considerations
+
+This component uses a native time input, which is styled differently in different browsers and operating systems.
+Recreating the native functionality is quite difficult and prone to accessibility errors, which is why we’ve chosen not to do that.
+This does mean that this component does not look identical on all browsers and operating systems.
+`;/*@license CC0-1.0*/function s(i){const e={a:"a",code:"code",h2:"h2",h3:"h3",p:"p",...t(),...i.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(r,{of:h}),`
+`,n.jsx(a,{children:x}),`
+`,n.jsx(d,{}),`
+`,n.jsx(c,{}),`
+`,n.jsx(e.h2,{id:"examples",children:"Examples"}),`
+`,n.jsx(e.h3,{id:"invalid",children:"Invalid"}),`
+`,n.jsx(o,{of:l}),`
+`,n.jsx(e.h3,{id:"disabled",children:"Disabled"}),`
+`,n.jsx(o,{of:m}),`
+`,n.jsx(e.h3,{id:"in-a-field",children:"In a Field"}),`
+`,n.jsx(e.p,{children:"Use a Field to group a Time Input with a Label, description and / or an Error Message."}),`
+`,n.jsxs(e.p,{children:["If you don’t need the description, remove its Paragraph and the ",n.jsx(e.code,{children:"aria-describedby"})," from the Time Input."]}),`
+`,n.jsxs(e.p,{children:["Check ",n.jsx(e.a,{href:"/docs/components-forms-field--docs",children:"the Field docs"})," for more information on configuring it."]}),`
+`,n.jsx(o,{of:p}),`
+`,n.jsx(e.h3,{id:"in-a-field-with-validation",children:"In a Field with validation"}),`
+`,n.jsxs(e.p,{children:["If the Time Input can become invalid, add an Error Message and its ",n.jsx(e.code,{children:"id"})," to the ",n.jsx(e.code,{children:"aria-describedby"})," attribute of the Time Input."]}),`
+`,n.jsxs(e.p,{children:["Check ",n.jsx(e.a,{href:"/docs/components-forms-field--docs",children:"the Field docs"})," for more information on configuring it."]}),`
+`,n.jsx(o,{of:f})]})}function A(i={}){const{wrapper:e}={...t(),...i.components};return e?n.jsx(e,{...i,children:n.jsx(s,{...i})}):s(i)}export{A as default};
