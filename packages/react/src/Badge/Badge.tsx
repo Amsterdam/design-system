6b22f36f50fe5a124383561a64e3ca6d5dc520bf
Vincent Smedinga <v.smedinga@amsterdam.nl>
2024-11-11 16:17:22 +0100

    Try a background colours on Badge

--- packages/react/src/Badge/Badge.tsx
+++ packages/react/src/Badge/Badge.tsx
@@ -24,7 +24,7 @@ export const badgeColors = [
   'yellow',
 ] as const
 
-type BadgeColor = (typeof badgeColors)[number]
+export type BadgeColor = (typeof badgeColors)[number]
 
 export type BadgeProps = {
   /** The background colour. */
--- storybook/src/components/Badge/Badge.stories.tsx
+++ storybook/src/components/Badge/Badge.stories.tsx
@@ -3,7 +3,9 @@
  * Copyright Gemeente Amsterdam
  */
 
-import { Badge } from '@amsterdam/design-system-react/src'
+import { Badge, Column } from '@amsterdam/design-system-react/src'
+import { badgeColors } from '@amsterdam/design-system-react/src/Badge/Badge'
+import type { BadgeColor } from '@amsterdam/design-system-react/src/Badge/Badge'
 import { Meta, StoryObj } from '@storybook/react'
 
 const meta = {
@@ -19,3 +21,28 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const Default: Story = {}
+
+const updateColorName = (color: BadgeColor) => {
+  const mapping: Partial<Record<BadgeColor, string>> = {
+    'dark-green': 'green',
+    green: 'lime',
+    'light-blue': 'azure',
+  }
+
+  return mapping[color] ?? color
+}
+
+export const AllColours: Story = {
+  args: {
+    label: 'Badge label',
+  },
+  render: () => (
+    <Column alignHorizontal="start">
+      {badgeColors
+        .filter((color) => !color.startsWith('grey-'))
+        .map((color) => (
+          <Badge key={color} color={color} label={updateColorName(color)} />
+        ))}
+    </Column>
+  ),
+}
