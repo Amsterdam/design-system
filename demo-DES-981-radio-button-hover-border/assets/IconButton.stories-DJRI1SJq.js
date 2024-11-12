802f8569c26172872ea530436a3734605d8e0ee8
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-11-12 10:26:21 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@3bd0d58bba88699d07577c7579b4e9b0b65cf37d 🚀

---
+++ demo-DES-981-radio-button-hover-border/assets/IconButton.stories-DJRI1SJq.js
@@ -0,0 +1,28 @@
+import{I as l}from"./index.esm-BJ5jhZwW.js";import{I as D}from"./IconButton-CN4yNogz.js";/**
+ * @license EUPL-1.2+
+ * Copyright Gemeente Amsterdam
+ */const P={title:"Components/Buttons/Icon Button",component:D,args:{contrastColor:!1,disabled:!1,inverseColor:!1,label:"Sluiten",size:void 0},argTypes:{disabled:{description:"Prevents interaction. Avoid if possible."},size:{control:{type:"radio",labels:{undefined:"level-5"}},options:["level-3","level-4",void 0,"level-6"]},svg:{control:{type:"select"},options:Object.keys(l),mapping:l}}},e={},r={args:{size:"level-3"}},s={args:{size:"level-4"}},o={args:{size:"level-5"}},a={args:{size:"level-6"}},t={args:{contrastColor:!0}},n={args:{inverseColor:!0}};var c,i,p;e.parameters={...e.parameters,docs:{...(c=e.parameters)==null?void 0:c.docs,source:{originalSource:"{}",...(p=(i=e.parameters)==null?void 0:i.docs)==null?void 0:p.source}}};var u,d,m;r.parameters={...r.parameters,docs:{...(u=r.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    size: 'level-3'
+  }
+}`,...(m=(d=r.parameters)==null?void 0:d.docs)==null?void 0:m.source}}};var v,g,C;s.parameters={...s.parameters,docs:{...(v=s.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    size: 'level-4'
+  }
+}`,...(C=(g=s.parameters)==null?void 0:g.docs)==null?void 0:C.source}}};var f,z,S;o.parameters={...o.parameters,docs:{...(f=o.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    size: 'level-5'
+  }
+}`,...(S=(z=o.parameters)==null?void 0:z.docs)==null?void 0:S.source}}};var b,I,L;a.parameters={...a.parameters,docs:{...(b=a.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    size: 'level-6'
+  }
+}`,...(L=(I=a.parameters)==null?void 0:I.docs)==null?void 0:L.source}}};var y,_,B;t.parameters={...t.parameters,docs:{...(y=t.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    contrastColor: true
+  }
+}`,...(B=(_=t.parameters)==null?void 0:_.docs)==null?void 0:B.source}}};var O,j,x;n.parameters={...n.parameters,docs:{...(O=n.parameters)==null?void 0:O.docs,source:{originalSource:`{
+  args: {
+    inverseColor: true
+  }
+}`,...(x=(j=n.parameters)==null?void 0:j.docs)==null?void 0:x.source}}};const T=["Default","Level3","Level4","Level5","Level6","ContrastColour","InverseColour"],E=Object.freeze(Object.defineProperty({__proto__:null,ContrastColour:t,Default:e,InverseColour:n,Level3:r,Level4:s,Level5:o,Level6:a,__namedExportsOrder:T,default:P},Symbol.toStringTag,{value:"Module"}));export{t as C,E as I,r as L,s as a,o as b,a as c,n as d};
