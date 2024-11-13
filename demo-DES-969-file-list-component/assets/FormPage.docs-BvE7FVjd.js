89ad5473fc93235afff561d308cb23f60eaa753a
dlnr <niels@dlnr.dev>
2024-11-13 08:53:03 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@5a7a32c2457f09e0928245916ca2677e35ebf1bb 🚀

---
+++ demo-DES-969-file-list-component/assets/FormPage.docs-BvE7FVjd.js
@@ -0,0 +1,5 @@
+import{j as o}from"./jsx-runtime-j_jdvEMj.js";import{useMDXComponents as i}from"./index-BSj771as.js";import{ae as e}from"./index-C3D_C3Tr.js";import{F as n}from"./FormPage.stories-BP1mD3PN.js";import"./index-B-o1Wr-g.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-Dvl-JrEP.js";import"../sb-preview/runtime.js";import"./index-DolzVqEf.js";import"./index-D-8MO0q_.js";import"./index-CJyPbrN5.js";import"./index-DrFu-skq.js";import"./index.esm-nImK0sTL.js";import"./index.esm-5uBNoYvV.js";import"./clsx-B-dksMZM.js";import"./config-CsYPBQUr.js";import"./PageMenu.stories-vMtB8qir.js";import"./Icon-D1obyjGa.js";import"./Footer.stories-CWgo3Cuv.js";import"./Spotlight-iwYL-F79.js";/*@license CC0-1.0*/function m(t){const r={h1:"h1",p:"p",...i(),...t.components};return o.jsxs(o.Fragment,{children:[`
+`,`
+`,o.jsx(e,{of:n}),`
+`,o.jsx(r.h1,{id:"form-page",children:"Form page"}),`
+`,o.jsx(r.p,{children:"This simple example presents various types of fields."})]})}function w(t={}){const{wrapper:r}={...i(),...t.components};return r?o.jsx(r,{...t,children:o.jsx(m,{...t})}):m(t)}export{w as default};
---
+++ demo-DES-969-file-list-component/assets/Spotlight.docs-CUGVffqf.js
@@ -0,0 +1,45 @@
+import{j as e}from"./jsx-runtime-j_jdvEMj.js";import{useMDXComponents as r}from"./index-BSj771as.js";import{ae as a,aq as i,ar as c,as as l,at as t}from"./index-C3D_C3Tr.js";import{S as d,B as m,D as h,a as p,G as x,M as u,O as j,P as f,Y as g,I as w}from"./Spotlight.stories-Dp_gEdlm.js";import"./index-B-o1Wr-g.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-Dvl-JrEP.js";import"../sb-preview/runtime.js";import"./index-DolzVqEf.js";import"./index-D-8MO0q_.js";import"./index-CJyPbrN5.js";import"./index-DrFu-skq.js";import"./index.esm-nImK0sTL.js";import"./index.esm-5uBNoYvV.js";import"./clsx-B-dksMZM.js";import"./exampleContent-hIIPvEhU.js";import"./Spotlight-iwYL-F79.js";const k=`<!-- @license CC0-1.0 -->
+
+# Spotlight
+
+Emphasizes a section on a page through a distinctive background colour.
+
+## Guidelines
+
+- Display the Spotlight at the entire width of the [Screen](/docs/components-layout-screen--docs); do not position it on the [Grid](/docs/components-layout-grid--docs).
+- Refer to [this overview on Stijlweb](https://amsterdam.nl/stijlweb/basiselementen/kleuren/#PagCls_15671872) to determine whether you can use black or white text on the background colour of your choice.
+
+## Relevant WCAG requirements
+
+- [WCAG 1.4.3](https://www.w3.org/TR/WCAG21/#contrast-minimum): Large-scale text and images of large-scale text have a contrast ratio of at least 3:1;
+
+## References
+
+- [Color and contrast accessibility](https://web.dev/articles/color-and-contrast-accessibility)
+`;/*@license CC0-1.0*/function s(o){const n={code:"code",h2:"h2",h3:"h3",p:"p",...r(),...o.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(a,{of:d}),`
+`,e.jsx(i,{children:k}),`
+`,e.jsx(c,{}),`
+`,e.jsx(l,{}),`
+`,e.jsx(n.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(n.h3,{id:"blue",children:"Blue"}),`
+`,e.jsx(t,{of:m}),`
+`,e.jsx(n.h3,{id:"dark-blue",children:"Dark Blue"}),`
+`,e.jsx(t,{of:h}),`
+`,e.jsx(n.h3,{id:"dark-green",children:"Dark Green"}),`
+`,e.jsx(t,{of:p}),`
+`,e.jsx(n.h3,{id:"green",children:"Green"}),`
+`,e.jsx(t,{of:x}),`
+`,e.jsx(n.h3,{id:"magenta",children:"Magenta"}),`
+`,e.jsx(t,{of:u}),`
+`,e.jsx(n.h3,{id:"orange",children:"Orange"}),`
+`,e.jsx(t,{of:j}),`
+`,e.jsx(n.h3,{id:"purple",children:"Purple"}),`
+`,e.jsx(t,{of:f}),`
+`,e.jsx(n.h3,{id:"yellow",children:"Yellow"}),`
+`,e.jsx(t,{of:g}),`
+`,e.jsx(n.h3,{id:"improve-semantics",children:"Improve semantics"}),`
+`,e.jsxs(n.p,{children:["By default, a Spotlight renders a ",e.jsx(n.code,{children:"<div>"}),` element in HTML.
+Use the `,e.jsx(n.code,{children:"as"})," prop to make your markup more semantic."]}),`
+`,e.jsx(t,{of:w})]})}function X(o={}){const{wrapper:n}={...r(),...o.components};return n?e.jsx(n,{...o,children:e.jsx(s,{...o})}):s(o)}export{X as default};
