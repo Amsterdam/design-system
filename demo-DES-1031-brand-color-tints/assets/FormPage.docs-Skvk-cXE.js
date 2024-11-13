8b72a8a2bf54655da217f79868a58732b84566d4
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-11-13 14:48:53 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@a56943ef904936d3ef588e40b16ca29bcefad7fd 🚀

---
+++ demo-DES-1031-brand-color-tints/assets/FormPage.docs-Skvk-cXE.js
@@ -0,0 +1,5 @@
+import{j as o}from"./jsx-runtime-j_jdvEMj.js";import{useMDXComponents as i}from"./index-BSj771as.js";import{ae as e}from"./index-Du87TAwT.js";import{F as n}from"./FormPage.stories-B7C8deSm.js";import"./index-B-o1Wr-g.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-CZtYl4tT.js";import"../sb-preview/runtime.js";import"./index-DolzVqEf.js";import"./index-D-8MO0q_.js";import"./index-CJyPbrN5.js";import"./index-DrFu-skq.js";import"./index.esm-DoBu7FM9.js";import"./clsx-B-dksMZM.js";import"./index.esm-CEuZchO4.js";import"./config-DcUc9_u2.js";import"./PageMenu.stories-CmM4zv6g.js";import"./Icon-D1obyjGa.js";import"./Footer.stories-DjvTD4NX.js";import"./Spotlight-CTNH19z8.js";/*@license CC0-1.0*/function m(t){const r={h1:"h1",p:"p",...i(),...t.components};return o.jsxs(o.Fragment,{children:[`
+`,`
+`,o.jsx(e,{of:n}),`
+`,o.jsx(r.h1,{id:"form-page",children:"Form page"}),`
+`,o.jsx(r.p,{children:"This simple example presents various types of fields."})]})}function w(t={}){const{wrapper:r}={...i(),...t.components};return r?o.jsx(r,{...t,children:o.jsx(m,{...t})}):m(t)}export{w as default};
