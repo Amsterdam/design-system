0caac7708056cf7bfbc655aa873a33c807206897
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-11-19 21:23:52 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@f948dd84d56539c57f153e5f9a663167cc9b91b0 🚀

---
+++ demo-DES-981-radio-button-hover-border/assets/Screen.docs-lPqYvAJD.js
@@ -0,0 +1,32 @@
+import{j as n}from"./jsx-runtime-j_jdvEMj.js";import{useMDXComponents as s}from"./index-BSj771as.js";import{ae as i,aq as r,ar as a,as as m,at as c}from"./index-DUNeqPbu.js";import{S as d,E as p}from"./Screen.stories-DBafdEz8.js";import"./index-B-o1Wr-g.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-v2yReoh6.js";import"../sb-preview/runtime.js";import"./index-DolzVqEf.js";import"./index-D-8MO0q_.js";import"./index-CBft-ReK.js";import"./index-DrFu-skq.js";import"./clsx-B-dksMZM.js";const h=`<!-- @license CC0-1.0 -->
+
+# Screen
+
+Manages the maximum width and alignment of the entire website or application.
+
+## Guidelines
+
+- This should be the outermost component of your website or application.
+- Within it, combine components such as
+  [Grid](/docs/components-layout-grid--docs),
+  [Header](/docs/components-containers-header--docs),
+  [Footer](/docs/components-containers-footer--docs),
+  [Spotlight](/docs/components-containers-spotlight--docs),
+  [Image Slider](/docs/components-media-image-slider--docs),
+  and Figure.
+
+## Design
+
+By default, this component expands to a width of 1600 pixels.
+This is the maximum width for a website of the City of Amsterdam.
+For applications, you can choose a larger maximum width of 2112 pixels.
+The component horizontally centres both itself and its content.
+`;/*@license CC0-1.0*/function t(o){const e={h2:"h2",h3:"h3",...s(),...o.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(i,{of:d}),`
+`,n.jsx(r,{children:h}),`
+`,n.jsx(a,{}),`
+`,n.jsx(m,{}),`
+`,n.jsx(e.h2,{id:"examples",children:"Examples"}),`
+`,n.jsx(e.h3,{id:"extra-wide",children:"Extra wide"}),`
+`,n.jsx(c,{of:p})]})}function D(o={}){const{wrapper:e}={...s(),...o.components};return e?n.jsx(e,{...o,children:n.jsx(t,{...o})}):t(o)}export{D as default};
