bc73433f8274c692da26198bfac20faea24ef21e
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-11-25 07:43:38 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@60a677a770d12f9fe9e8f1dab96028959efb0973 🚀

---
+++ demo-DES-969-file-list-component/assets/ActionGroup.stories-BQCIMGBm.js
@@ -0,0 +1,15 @@
+import{j as e}from"./jsx-runtime-j_jdvEMj.js";import{a as t,L as _}from"./index.esm-B1nkekBi.js";import{c as x}from"./clsx-B-dksMZM.js";import{r as S}from"./index-B-o1Wr-g.js";const n=S.forwardRef(({children:s,className:j,...y},f)=>e.jsx("div",{...y,ref:f,className:x("ams-action-group",j),role:"group",children:s}));n.displayName="ActionGroup";try{n.displayName="ActionGroup",n.__docgenInfo={description:"",displayName:"ActionGroup",props:{}}}catch{}const k={title:"Components/Layout/Action Group",component:n,args:{children:[e.jsx(t,{children:"Doorgaan"}),e.jsx(t,{variant:"tertiary",children:"Stoppen"})]}},r={},o={args:{children:[e.jsx(t,{children:"Adres wijzigen"}),e.jsx(t,{variant:"secondary",children:"Adres verwijderen"})],className:"ams-resize-horizontal",style:{inlineSize:"16rem"}}},a={args:{children:[e.jsx(t,{children:"Bewerken"},1),e.jsx(_,{download:!0,href:"#",variant:"standalone",children:"Downloaden"},2)]}};var i,c,d;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:"{}",...(d=(c=r.parameters)==null?void 0:c.docs)==null?void 0:d.source}}};var l,p,m;o.parameters={...o.parameters,docs:{...(l=o.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    children: [<Button>Adres wijzigen</Button>, <Button variant="secondary">Adres verwijderen</Button>],
+    className: 'ams-resize-horizontal',
+    style: {
+      inlineSize: '16rem'
+    }
+  }
+}`,...(m=(p=o.parameters)==null?void 0:p.docs)==null?void 0:m.source}}};var u,h,g;a.parameters={...a.parameters,docs:{...(u=a.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    children: [<Button key={1}>Bewerken</Button>, <Link download href="#" key={2} variant="standalone">
+        Downloaden
+      </Link>]
+  }
+}`,...(g=(h=a.parameters)==null?void 0:h.docs)==null?void 0:g.source}}};const w=["Default","Stacked","WithLink"],L=Object.freeze(Object.defineProperty({__proto__:null,Default:r,Stacked:o,WithLink:a,__namedExportsOrder:w,default:k},Symbol.toStringTag,{value:"Module"}));export{L as A,o as S,a as W};
