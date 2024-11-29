a4c5fe1470dfeab27c0f7bb3800efef23add9f73
Niels Roozemond <n.roozemond@amsterdam.nl>
2024-11-29 15:22:11 +0100

    refactor: update formatFileSize test fixes

--- packages/react/src/common/formatFileSize.test.tsx
+++ packages/react/src/common/formatFileSize.test.tsx
@@ -2,26 +2,21 @@ import { formatFileSize } from './formatFileSize'
 
 describe('formatFileSize', () => {
   it('formats bytes correctly', () => {
-    expect(formatFileSize(500)).toBe('500B')
+    expect(formatFileSize(500)).toBe('500 bytes')
   })
 
   it('formats kilobytes correctly', () => {
-    expect(formatFileSize(1024, 1)).toBe('1KB')
-    expect(formatFileSize(2048, 1)).toBe('2KB')
+    expect(formatFileSize(1024, 1)).toBe('1 kB')
+    expect(formatFileSize(2048, 1)).toBe('2 kB')
   })
 
   it('formats megabytes correctly', () => {
-    expect(formatFileSize(1048576, 1)).toBe('1MB')
-    expect(formatFileSize(2097152, 1)).toBe('2MB')
+    expect(formatFileSize(1048576, 1)).toBe('1 MB')
+    expect(formatFileSize(2097152, 1)).toBe('2 MB')
   })
 
   it('formats gigabytes correctly', () => {
-    expect(formatFileSize(1073741824, 1)).toBe('1GB')
-    expect(formatFileSize(2147483648, 1)).toBe('2GB')
-  })
-
-  it('formats terabytes correctly', () => {
-    expect(formatFileSize(1099511627776, 1)).toBe('1TB')
-    expect(formatFileSize(2199023255552, 1)).toBe('2TB')
+    expect(formatFileSize(1073741824, 1)).toBe('1 GB')
+    expect(formatFileSize(2147483648, 1)).toBe('2 GB')
   })
 })
--- packages/react/src/common/formatFileSize.tsx
+++ packages/react/src/common/formatFileSize.tsx
@@ -11,7 +11,7 @@
 export const formatFileSize = (fileSize: number, precision = 3) => {
   const UNITS = ['bytes', 'kB', 'MB', 'GB']
 
-  if (fileSize === 0) return '0 B'
+  if (fileSize === 0) return '0 bytes'
 
   const exponent = Math.floor(Math.log10(fileSize) / 3)
   const size = (fileSize / Math.pow(1000, exponent)).toPrecision(precision)
