730affb58fcb8a1b52dedfb45d5693c6f1b6138e
alimpens <a.limpens@amsterdam.nl>
2024-11-29 16:37:12 +0100

    Setup initial component

--- packages/react/src/Header/Header.tsx
+++ packages/react/src/Header/Header.tsx
@@ -3,14 +3,30 @@
  * Copyright Gemeente Amsterdam
  */
 
+import { MenuIcon } from '@amsterdam/design-system-react-icons'
 import clsx from 'clsx'
-import { forwardRef } from 'react'
-import type { ForwardedRef, HTMLAttributes, ReactNode } from 'react'
+import { forwardRef, useState } from 'react'
+import type { AnchorHTMLAttributes, ForwardedRef, HTMLAttributes, PropsWithChildren, ReactNode } from 'react'
+import { Grid } from '../Grid'
 import { Heading } from '../Heading'
+import { Icon } from '../Icon'
 import { Logo } from '../Logo'
 import type { LogoBrand } from '../Logo'
 
+type HeaderMenuLinkProps = {
+  secondary?: boolean
+} & PropsWithChildren<AnchorHTMLAttributes<HTMLAnchorElement>>
+
+const HeaderMenuLink = ({ children, secondary, ...restProps }: HeaderMenuLinkProps) => (
+  <li className={clsx(secondary && 'ams-header__menu-item--secondary')}>
+    <a {...restProps} className="ams-header__menu-link">
+      {children}
+    </a>
+  </li>
+)
+
 export type HeaderProps = {
+  alwaysShowMenuButton?: boolean
   /** The name of the application. */
   appName?: string
   /** The list of menu links. Use a Page Menu here. */
@@ -23,42 +39,94 @@ export type HeaderProps = {
   logoLinkTitle?: string
   /** A button to toggle the visibility of a Mega Menu. */
   menu?: ReactNode
+  menuButtonText?: string
+  navigationLabel?: string
 } & HTMLAttributes<HTMLElement>
 
-export const Header = forwardRef(
+// TODO: change state als je over grens media query gaat
+// TODO: gov.uk leest elke keer als je menu opent en sluit 'Show / close navigation menu' voor.
+// Da's wel handig, alleen als je dat met een vis hidden span doet leest 'ie 'm niet elke keer voor.
+
+export const HeaderRoot = forwardRef(
   (
     {
+      alwaysShowMenuButton,
       appName,
       className,
-      links,
+      children,
       logoBrand = 'amsterdam',
       logoLink = '/',
       logoLinkTitle = 'Ga naar de homepage',
       menu,
+      menuButtonText = 'Menu',
+      navigationLabel = 'Hoofdnavigatie',
       ...restProps
     }: HeaderProps,
     ref: ForwardedRef<HTMLElement>,
   ) => {
+    const [open, setOpen] = useState(false)
+
     return (
-      <>
-        <header {...restProps} ref={ref} className={clsx('ams-header', className)}>
-          <a href={logoLink} className="ams-header__logo-link">
+      <header {...restProps} ref={ref} className={clsx('ams-header', className)}>
+        <div className="ams-header__brand-section">
+          <a className="ams-header__logo-link" href={logoLink}>
             <span className="ams-visually-hidden">{logoLinkTitle}</span>
             <Logo brand={logoBrand} />
           </a>
-          {links && <div className="ams-header__links">{links}</div>}
-          {menu && <div className="ams-header__menu">{menu}</div>}
           {appName && (
-            <div className="ams-header__app-name">
-              <Heading level={1} size="level-3" className="ams-header__app-name-heading">
-                {appName}
-              </Heading>
-            </div>
+            <Heading level={1} size="level-5">
+              {appName}
+            </Heading>
           )}
-        </header>
-      </>
+        </div>
+        <nav className="ams-header__navigation" aria-labelledby="primary-navigation">
+          <h2 id="primary-navigation" className="ams-visually-hidden">
+            {navigationLabel}
+          </h2>
+
+          {/* The logo and app name section is recreated here, to make sure the page menu breaks at the right spot */}
+          <div className="ams-header__brand-section" aria-hidden style={{ opacity: 0 }}>
+            <div className="ams-header__logo-link">
+              <Logo brand={logoBrand} />
+            </div>
+            {appName && <span className="ams-heading ams-heading--level-5">{appName}</span>}
+          </div>
+
+          <ul className="ams-header__menu">
+            {menu}
+            <li
+              className={clsx(
+                'ams-header__mega-menu-button-item',
+                alwaysShowMenuButton && 'ams-header__mega-menu-button-item--always-show',
+              )}
+            >
+              <button
+                {...restProps}
+                aria-controls="ams-mega-menu"
+                aria-expanded={open}
+                className="ams-header__mega-menu-button"
+                onClick={() => setOpen(!open)}
+                type="button"
+              >
+                {menuButtonText}
+                <Icon svg={MenuIcon} size="level-6" />
+              </button>
+            </li>
+          </ul>
+
+          <Grid
+            className={clsx('ams-header__mega-menu', !open && 'ams-header__mega-menu--closed')}
+            id="ams-mega-menu"
+            paddingBottom="large"
+          >
+            {children}
+          </Grid>
+        </nav>
+      </header>
     )
   },
 )
 
-Header.displayName = 'Header'
+HeaderRoot.displayName = 'Header'
+
+export const Header = Object.assign(HeaderRoot, { MenuLink: HeaderMenuLink })
--- storybook/src/components/Header/Header.stories.tsx
+++ storybook/src/components/Header/Header.stories.tsx
@@ -3,9 +3,10 @@
  * Copyright Gemeente Amsterdam
  */
 
-import { PageMenu } from '@amsterdam/design-system-react'
+// import { PageMenu } from '@amsterdam/design-system-react'
+import { Grid, Heading, LinkList } from '@amsterdam/design-system-react'
 import { Header } from '@amsterdam/design-system-react/src'
-import { SearchIcon } from '@amsterdam/design-system-react-icons'
+// import { SearchIcon } from '@amsterdam/design-system-react-icons'
 import { Meta, StoryObj } from '@storybook/react'
 
 const meta = {
@@ -17,76 +18,129 @@ export default meta
 
 type Story = StoryObj<typeof meta>
 
-export const Default: Story = {}
-
-export const ForSubBrand: Story = {
+export const Default: Story = {
   args: {
-    logoBrand: 'ggd-amsterdam',
-    logoLink: 'https://www.ggd.amsterdam.nl/',
-    logoLinkTitle: 'Naar de homepage van de GGD Amsterdam',
-  },
-}
-
-export const WithAppName: Story = {
-  args: {
+    alwaysShowMenuButton: true,
     appName: 'Aan de Amsterdamse grachten',
-  },
-}
-
-export const WithLinks: Story = {
-  args: {
-    links: (
-      <PageMenu alignEnd wrap={false}>
-        <PageMenu.Link href="#">Contact</PageMenu.Link>
-        <PageMenu.Link href="#">Mijn Amsterdam</PageMenu.Link>
-        <PageMenu.Link href="#" icon={SearchIcon}>
-          Zoeken
-        </PageMenu.Link>
-      </PageMenu>
+    children: (
+      <Grid.Cell span="all">
+        <Heading size="level-2">Alle onderwerpen</Heading>
+        {/* <Header.ListCategory> */}
+        <LinkList>
+          <LinkList.Link href="#">Afval</LinkList.Link>
+          <LinkList.Link href="#">Bestuur en organisatie</LinkList.Link>
+          <LinkList.Link href="#">Bouw- en verkeersprojecten</LinkList.Link>
+        </LinkList>
+        {/* </Header.ListCategory> */}
+      </Grid.Cell>
+    ),
+    menu: (
+      <>
+        <Header.MenuLink href="#" lang="en" secondary>
+          English
+        </Header.MenuLink>
+        <Header.MenuLink href="#" secondary>
+          Contact
+        </Header.MenuLink>
+      </>
     ),
   },
 }
 
-export const WithMenuButton: Story = {
-  args: {
-    menu: <button className="ams-header__menu-button">Menu</button>,
-  },
-}
-
-export const WithLinksAndMenuButton: Story = {
-  args: {
-    links: (
-      <PageMenu alignEnd wrap={false}>
-        <PageMenu.Link href="#">Contact</PageMenu.Link>
-        <PageMenu.Link href="#">Mijn Amsterdam</PageMenu.Link>
-        <PageMenu.Link href="#" icon={SearchIcon}>
-          Zoeken
-        </PageMenu.Link>
-      </PageMenu>
-    ),
-    menu: <button className="ams-header__menu-button">Menu</button>,
-  },
-}
-
-export const WithAppNameAndMenuButton: Story = {
+export const Default2: Story = {
   args: {
     appName: 'Aan de Amsterdamse grachten',
-    menu: <button className="ams-header__menu-button">Menu</button>,
+    children: (
+      <Grid.Cell span="all">
+        {/* <Header.ListCategory> */}
+        <LinkList>
+          <LinkList.Link href="#">English</LinkList.Link>
+          <LinkList.Link href="#">Contact</LinkList.Link>
+        </LinkList>
+        {/* </Header.ListCategory> */}
+      </Grid.Cell>
+    ),
+    menu: (
+      <>
+        <Header.MenuLink href="#" lang="en" secondary>
+          English
+        </Header.MenuLink>
+        <Header.MenuLink href="#" secondary>
+          Contact
+        </Header.MenuLink>
+      </>
+    ),
   },
 }
 
-export const WithAppNameLinksAndMenuButton: Story = {
-  args: {
-    appName: 'Aan de Amsterdamse grachten',
-    links: (
-      <PageMenu alignEnd wrap={false}>
-        <PageMenu.Link href="#">Contact</PageMenu.Link>
-        <PageMenu.Link href="#">Mijn Amsterdam</PageMenu.Link>
-        <PageMenu.Link href="#" icon={SearchIcon}>
-          Zoeken
-        </PageMenu.Link>
-      </PageMenu>
-    ),
-    menu: <button className="ams-header__menu-button">Menu</button>,
-  },
-}
+// export const ForSubBrand: Story = {
+//   args: {
+//     logoBrand: 'ggd-amsterdam',
+//     logoLink: 'https://www.ggd.amsterdam.nl/',
+//     logoLinkTitle: 'Naar de homepage van de GGD Amsterdam',
+//   },
+// }
+
+// export const WithAppName: Story = {
+//   args: {
+//     appName: 'Aan de Amsterdamse grachten',
+//   },
+// }
+
+// export const WithLinks: Story = {
+//   args: {
+//     links: (
+//       <PageMenu alignEnd wrap={false}>
+//         <PageMenu.Link href="#">Contact</PageMenu.Link>
+//         <PageMenu.Link href="#">Mijn Amsterdam</PageMenu.Link>
+//         <PageMenu.Link href="#" icon={SearchIcon}>
+//           Zoeken
+//         </PageMenu.Link>
+//       </PageMenu>
+//     ),
+//   },
+// }
+
+// export const WithMenuButton: Story = {
+//   args: {
+//     menu: <button className="ams-header__menu-button">Menu</button>,
+//   },
+// }
+
+// export const WithLinksAndMenuButton: Story = {
+//   args: {
+//     links: (
+//       <PageMenu alignEnd wrap={false}>
+//         <PageMenu.Link href="#">Contact</PageMenu.Link>
+//         <PageMenu.Link href="#">Mijn Amsterdam</PageMenu.Link>
+//         <PageMenu.Link href="#" icon={SearchIcon}>
+//           Zoeken
+//         </PageMenu.Link>
+//       </PageMenu>
+//     ),
+//     menu: <button className="ams-header__menu-button">Menu</button>,
+//   },
+// }
+
+// export const WithAppNameAndMenuButton: Story = {
+//   args: {
+//     appName: 'Aan de Amsterdamse grachten',
+//     menu: <button className="ams-header__menu-button">Menu</button>,
+//   },
+// }
+
+// export const WithAppNameLinksAndMenuButton: Story = {
+//   args: {
+//     appName: 'Aan de Amsterdamse grachten',
+//     links: (
+//       <PageMenu alignEnd wrap={false}>
+//         <PageMenu.Link href="#">Contact</PageMenu.Link>
+//         <PageMenu.Link href="#">Mijn Amsterdam</PageMenu.Link>
+//         <PageMenu.Link href="#" icon={SearchIcon}>
+//           Zoeken
+//         </PageMenu.Link>
+//       </PageMenu>
+//     ),
+//     menu: <button className="ams-header__menu-button">Menu</button>,
+//   },
+// }
