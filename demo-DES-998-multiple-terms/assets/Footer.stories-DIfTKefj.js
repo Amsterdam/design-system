3e2a815c8b0ec5cf4bc021e501424c16200bf6ec
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-12-04 11:20:40 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@ae5e76dd51714542dcbd80df6a4a4243036c75ad 🚀

---
+++ demo-DES-998-multiple-terms/assets/Footer.stories-DIfTKefj.js
@@ -0,0 +1,163 @@
+import{j as e}from"./jsx-runtime-j_jdvEMj.js";import{H as r,G as i,o as n,P as c,c as j}from"./index.esm-Cka35VEH.js";import{d as z,h as b,s as F,t as H,u as G,v as P,w as S}from"./index.esm-DE9QoVc-.js";import{D as N}from"./PageMenu.stories-DryfQZuq.js";import{c as L}from"./clsx-B-dksMZM.js";import{r as v}from"./index-B-o1Wr-g.js";import{S as V}from"./Spotlight-DZdLn00J.js";const w=v.forwardRef(({children:s,className:l,...o},t)=>e.jsx("div",{...o,ref:t,className:L("ams-footer__bottom",l),children:s}));w.displayName="Footer.Bottom";try{Footer.Bottom.displayName="Footer.Bottom",Footer.Bottom.__docgenInfo={description:"",displayName:"Footer.Bottom",props:{}}}catch{}const _=v.forwardRef(({children:s,className:l,...o},t)=>e.jsx(V,{...o,color:"blue",ref:t,className:L("ams-footer__top",l),children:s}));_.displayName="Footer.Top";try{Footer.Top.displayName="Footer.Top",Footer.Top.__docgenInfo={description:"",displayName:"Footer.Top",props:{}}}catch{}const y=v.forwardRef(({children:s,className:l,...o},t)=>e.jsx("footer",{...o,ref:t,className:L("ams-footer",l),children:s}));y.displayName="Footer";const a=Object.assign(y,{Bottom:w,Top:_});try{a.displayName="Footer",a.__docgenInfo={description:"",displayName:"Footer",props:{}}}catch{}const O={title:"Components/Containers/Footer",component:a},B=[{icon:H,name:"Twitter"},{icon:G,name:"Facebook"},{icon:P,name:"Linkedin"},{icon:S,name:"Instagram"}];var h;const d={args:{children:[e.jsxs(a.Top,{children:[e.jsx(r,{className:"ams-visually-hidden",inverseColor:!0,children:"Colofon"}),e.jsxs(i,{gapVertical:"large",paddingVertical:"medium",children:[e.jsxs(i.Cell,{span:4,children:[e.jsx(r,{className:"ams-mb--xs",level:2,size:"level-4",inverseColor:!0,children:"Meer weten"}),e.jsx(n,{children:["Veel gestelde vragen","Over ons","Werken bij","Kalender","Uit in Amsterdam","Bronnen"].map((s,l)=>e.jsx(n.Link,{href:"#",inverseColor:!0,size:"small",children:s},l))})]}),e.jsxs(i.Cell,{span:4,start:{narrow:1,medium:5,wide:5},children:[e.jsx(r,{className:"ams-mb--xs",level:2,size:"level-4",inverseColor:!0,children:"Contact"}),e.jsx(c,{className:"ams-mb--xs",inverseColor:!0,size:"small",children:"Hebt u een vraag en kunt u het antwoord niet vinden op deze website? Neem dan contact met ons op."}),e.jsxs(n,{children:[e.jsx(n.Link,{href:"mailto:redactie@amsterdam.nl",icon:z,inverseColor:!0,size:"small",children:"E-mail"}),e.jsx(n.Link,{href:"tel:+3114020",icon:b,inverseColor:!0,size:"small",children:"14 020"}),e.jsx(n.Link,{href:"#",icon:F,inverseColor:!0,size:"small",children:"Contactgegevens en openingstijden"})]})]}),e.jsxs(i.Cell,{span:4,start:{narrow:1,medium:1,wide:9},children:[e.jsxs("section",{className:"ams-mb--md",children:[e.jsx(r,{className:"ams-mb--xs",level:2,size:"level-4",inverseColor:!0,children:"Nieuwsbrief"}),e.jsx(n,{children:e.jsx(n.Link,{href:"#",inverseColor:!0,size:"small",children:"Inschrijven"})})]}),e.jsxs("section",{children:[e.jsx(r,{className:"ams-mb--xs",level:2,size:"level-4",inverseColor:!0,children:"Volg ons"}),e.jsx(n,{children:B.map(({name:s,icon:l})=>e.jsx(n.Link,{href:"#",icon:l,inverseColor:!0,size:"small",children:s},s))})]})]})]})]},"footer-top"),e.jsxs(a.Bottom,{children:[e.jsx(r,{className:"ams-visually-hidden",level:2,children:"Over deze website"}),e.jsx(i,{paddingVertical:"small",children:e.jsx(i.Cell,{span:"all",children:e.jsx(j,{children:(h=N.args)==null?void 0:h.children})})})]},"footer-bottom")]}};var p;const m={args:{children:[e.jsxs(a.Top,{children:[e.jsx(r,{className:"ams-visually-hidden",inverseColor:!0,children:"Colofon"}),e.jsxs(i,{gapVertical:"large",paddingVertical:"medium",children:[e.jsxs(i.Cell,{span:3,children:[e.jsx(r,{className:"ams-mb--xs",level:2,size:"level-4",inverseColor:!0,children:"Contact"}),e.jsx(c,{className:"ams-mb--xs",size:"small",inverseColor:!0,children:"Heeft u een vraag en kunt u het antwoord niet vinden op deze site? Neem dan contact met ons op."}),e.jsxs(n,{children:[e.jsx(n.Link,{href:"mailto:redactie.os@amsterdam.nl",icon:z,inverseColor:!0,size:"small",children:"E-mail"}),e.jsx(n.Link,{href:"tel:+31202510333",icon:b,inverseColor:!0,size:"small",children:"020 251 0333"})]})]}),e.jsxs(i.Cell,{span:3,start:{narrow:1,medium:5,wide:5},children:[e.jsx(r,{className:"ams-mb--xs",level:2,size:"level-4",inverseColor:!0,children:"Panels en enquêtes"}),e.jsx(c,{className:"ams-mb--xs",inverseColor:!0,size:"small",children:"Bent u uitgenodigd om mee te doen aan onderzoek of heeft u vragen over het panel of stadspaspanel?"}),e.jsxs(n,{children:[e.jsx(n.Link,{href:"#",inverseColor:!0,rel:"external",size:"small",children:"Meedoen aan onderzoek"}),e.jsx(n.Link,{href:"#",inverseColor:!0,rel:"external",size:"small",children:"Panel Amsterdam"}),e.jsx(n.Link,{href:"#",inverseColor:!0,rel:"external",size:"small",children:"Stadspaspanel Amsterdam"})]})]}),e.jsxs(i.Cell,{span:3,start:{narrow:1,medium:1,wide:9},children:[e.jsx(r,{className:"ams-mb--xs",level:2,size:"level-4",inverseColor:!0,children:"Onderzoek en Statistiek"}),e.jsxs(n,{children:[e.jsx(n.Link,{href:"#",inverseColor:!0,size:"small",children:"Over Onderzoek en Statistiek"}),e.jsx(n.Link,{href:"#",inverseColor:!0,size:"small",children:"Veelgestelde vragen"}),e.jsx(n.Link,{href:"#",inverseColor:!0,rel:"external",size:"small",children:"Nieuwsbrief"}),e.jsx(n.Link,{href:"#",inverseColor:!0,size:"small",children:"Vacatures"})]})]})]})]},"footer-top"),e.jsxs(a.Bottom,{children:[e.jsx(r,{className:"ams-visually-hidden",level:2,children:"Over deze website"}),e.jsx(i,{paddingVertical:"small",children:e.jsx(i.Cell,{span:"all",children:e.jsx(j,{children:(p=N.args)==null?void 0:p.children})})})]},"footer-bottom")]}};var k,g,u;d.parameters={...d.parameters,docs:{...(k=d.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  args: {
+    children: [<Footer.Top key="footer-top">
+        <Heading className="ams-visually-hidden" inverseColor>
+          Colofon
+        </Heading>
+        <Grid gapVertical="large" paddingVertical="medium">
+          <Grid.Cell span={4}>
+            <Heading className="ams-mb--xs" level={2} size="level-4" inverseColor>
+              Meer weten
+            </Heading>
+            <LinkList>
+              {['Veel gestelde vragen', 'Over ons', 'Werken bij', 'Kalender', 'Uit in Amsterdam', 'Bronnen'].map((label, index) => <LinkList.Link href="#" key={index} inverseColor size="small">
+                    {label}
+                  </LinkList.Link>)}
+            </LinkList>
+          </Grid.Cell>
+          <Grid.Cell span={4} start={{
+          narrow: 1,
+          medium: 5,
+          wide: 5
+        }}>
+            <Heading className="ams-mb--xs" level={2} size="level-4" inverseColor>
+              Contact
+            </Heading>
+            <Paragraph className="ams-mb--xs" inverseColor size="small">
+              Hebt u een vraag en kunt u het antwoord niet vinden op deze website? Neem dan contact met ons op.
+            </Paragraph>
+            <LinkList>
+              <LinkList.Link href="mailto:redactie@amsterdam.nl" icon={EmailIcon} inverseColor size="small">
+                E-mail
+              </LinkList.Link>
+              <LinkList.Link href="tel:+3114020" icon={PhoneIcon} inverseColor size="small">
+                14 020
+              </LinkList.Link>
+              <LinkList.Link href="#" icon={ClockIcon} inverseColor size="small">
+                Contactgegevens en openingstijden
+              </LinkList.Link>
+            </LinkList>
+          </Grid.Cell>
+          <Grid.Cell span={4} start={{
+          narrow: 1,
+          medium: 1,
+          wide: 9
+        }}>
+            <section className="ams-mb--md">
+              <Heading className="ams-mb--xs" level={2} size="level-4" inverseColor>
+                Nieuwsbrief
+              </Heading>
+              <LinkList>
+                <LinkList.Link href="#" inverseColor size="small">
+                  Inschrijven
+                </LinkList.Link>
+              </LinkList>
+            </section>
+            <section>
+              <Heading className="ams-mb--xs" level={2} size="level-4" inverseColor>
+                Volg ons
+              </Heading>
+              <LinkList>
+                {socialPlatforms.map(({
+                name,
+                icon
+              }) => <LinkList.Link href="#" icon={icon} key={name} inverseColor size="small">
+                    {name}
+                  </LinkList.Link>)}
+              </LinkList>
+            </section>
+          </Grid.Cell>
+        </Grid>
+      </Footer.Top>, <Footer.Bottom key="footer-bottom">
+        <Heading className="ams-visually-hidden" level={2}>
+          Over deze website
+        </Heading>
+        <Grid paddingVertical="small">
+          <Grid.Cell span="all">
+            <PageMenu>{PageMenuStory.args?.children}</PageMenu>
+          </Grid.Cell>
+        </Grid>
+      </Footer.Bottom>]
+  }
+}`,...(u=(g=d.parameters)==null?void 0:g.docs)==null?void 0:u.source}}};var x,C,f;m.parameters={...m.parameters,docs:{...(x=m.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  args: {
+    children: [<Footer.Top key="footer-top">
+        <Heading className="ams-visually-hidden" inverseColor>
+          Colofon
+        </Heading>
+        <Grid gapVertical="large" paddingVertical="medium">
+          <Grid.Cell span={3}>
+            <Heading className="ams-mb--xs" level={2} size="level-4" inverseColor>
+              Contact
+            </Heading>
+            <Paragraph className="ams-mb--xs" size="small" inverseColor>
+              Heeft u een vraag en kunt u het antwoord niet vinden op deze site? Neem dan contact met ons op.
+            </Paragraph>
+            <LinkList>
+              <LinkList.Link href="mailto:redactie.os@amsterdam.nl" icon={EmailIcon} inverseColor size="small">
+                E-mail
+              </LinkList.Link>
+              <LinkList.Link href="tel:+31202510333" icon={PhoneIcon} inverseColor size="small">
+                020 251 0333
+              </LinkList.Link>
+            </LinkList>
+          </Grid.Cell>
+          <Grid.Cell span={3} start={{
+          narrow: 1,
+          medium: 5,
+          wide: 5
+        }}>
+            <Heading className="ams-mb--xs" level={2} size="level-4" inverseColor>
+              Panels en enquêtes
+            </Heading>
+            <Paragraph className="ams-mb--xs" inverseColor size="small">
+              Bent u uitgenodigd om mee te doen aan onderzoek of heeft u vragen over het panel of stadspaspanel?
+            </Paragraph>
+            <LinkList>
+              <LinkList.Link href="#" inverseColor rel="external" size="small">
+                Meedoen aan onderzoek
+              </LinkList.Link>
+              <LinkList.Link href="#" inverseColor rel="external" size="small">
+                Panel Amsterdam
+              </LinkList.Link>
+              <LinkList.Link href="#" inverseColor rel="external" size="small">
+                Stadspaspanel Amsterdam
+              </LinkList.Link>
+            </LinkList>
+          </Grid.Cell>
+          <Grid.Cell span={3} start={{
+          narrow: 1,
+          medium: 1,
+          wide: 9
+        }}>
+            <Heading className="ams-mb--xs" level={2} size="level-4" inverseColor>
+              Onderzoek en Statistiek
+            </Heading>
+            <LinkList>
+              <LinkList.Link href="#" inverseColor size="small">
+                Over Onderzoek en Statistiek
+              </LinkList.Link>
+              <LinkList.Link href="#" inverseColor size="small">
+                Veelgestelde vragen
+              </LinkList.Link>
+              <LinkList.Link href="#" inverseColor rel="external" size="small">
+                Nieuwsbrief
+              </LinkList.Link>
+              <LinkList.Link href="#" inverseColor size="small">
+                Vacatures
+              </LinkList.Link>
+            </LinkList>
+          </Grid.Cell>
+        </Grid>
+      </Footer.Top>, <Footer.Bottom key="footer-bottom">
+        <Heading className="ams-visually-hidden" level={2}>
+          Over deze website
+        </Heading>
+        <Grid paddingVertical="small">
+          <Grid.Cell span="all">
+            <PageMenu>{PageMenuStory.args?.children}</PageMenu>
+          </Grid.Cell>
+        </Grid>
+      </Footer.Bottom>]
+  }
+}`,...(f=(C=m.parameters)==null?void 0:C.docs)==null?void 0:f.source}}};const T=["Default","OnderzoekEnStatistiek"],K=Object.freeze(Object.defineProperty({__proto__:null,Default:d,OnderzoekEnStatistiek:m,__namedExportsOrder:T,default:O},Symbol.toStringTag,{value:"Module"}));export{d as D,K as F,m as O};
