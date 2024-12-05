ae6bf71430e64556ad89b6820ee5d81541e39baa
dlnr <niels@dlnr.dev>
2024-12-05 10:58:01 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@6bab918b656476b02c978952ee08a05ace9d13ae 🚀

---
+++ demo-DES-998-multiple-terms/assets/Breadcrumb.docs-DNhwm69w.js
@@ -0,0 +1,27 @@
+import{j as e}from"./jsx-runtime-j_jdvEMj.js";import{useMDXComponents as i}from"./index-BSj771as.js";import{ae as s,aq as a,ar as o}from"./index-VJpAeK4P.js";import{B as c}from"./Breadcrumb.stories-EowaNKYC.js";import"./index-B-o1Wr-g.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-NBfZUwQQ.js";import"../sb-preview/runtime.js";import"./index-DolzVqEf.js";import"./index-D-8MO0q_.js";import"./index-CJyPbrN5.js";import"./index-DrFu-skq.js";import"./clsx-B-dksMZM.js";const u=`<!-- @license CC0-1.0 -->
+
+# Breadcrumb
+
+The breadcrumb is a secondary navigation pattern that helps a user understand the structure between levels in the website up to the current page.
+Users can use it to navigate.
+
+## Guidelines
+
+- Only use the breadcrumb trail if it adds something functional for the user and the structure is ‘static’.
+- Do not display the breadcrumb trail on a form page.
+  It distracts the user from their task, or one can accidentally interpret it as a Progress Indicator.
+- This is a secondary navigation pattern.
+  It can’t replace the main navigation.
+
+### Using links with routing libraries
+
+Check out the [developer guide](/docs/docs-developer-guide-routing-libraries--docs) on how to integrate links with external routing libraries.
+`;/*@license CC0-1.0*/function r(n){const t={h2:"h2",li:"li",ul:"ul",...i(),...n.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(s,{of:c}),`
+`,e.jsx(a,{children:u}),`
+`,e.jsx(o,{}),`
+`,e.jsx(t.h2,{id:"guidelines",children:"Guidelines"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsx(t.li,{children:"A breadcrumb should not display more than 8 items."}),`
+`]})]})}function M(n={}){const{wrapper:t}={...i(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(r,{...n})}):r(n)}export{M as default};
---
+++ demo-DES-998-multiple-terms/assets/CharacterCount.docs-CqH474xq.js
@@ -0,0 +1,29 @@
+import{j as e}from"./jsx-runtime-j_jdvEMj.js";import{useMDXComponents as a}from"./index-BSj771as.js";import{ae as o,aq as s,ar as h,as as i,at as c}from"./index-VJpAeK4P.js";import{C as m,E as l}from"./CharacterCount.stories-BuZ7Uh_6.js";import"./index-B-o1Wr-g.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-NBfZUwQQ.js";import"../sb-preview/runtime.js";import"./index-DolzVqEf.js";import"./index-D-8MO0q_.js";import"./index-CJyPbrN5.js";import"./index-DrFu-skq.js";import"./clsx-B-dksMZM.js";const u=`<!-- @license CC0-1.0 -->
+
+# Character Count
+
+Help users know how much text they can enter when there is a limit on the number of characters.
+
+Users will get updates at a pace that works best for the way they interact with the textarea. This means:
+
+- sighted users will see a count message that updates as they type;
+- screen reader users will hear the count announcement when they stop typing.
+
+This component does not restrict the user from entering information. The user can enter more than the character limit, but are told they’ve entered too many characters. This lets them type or copy and paste their full answer, then edit it down.
+
+## Guidelines
+
+- Only use a character count when there is a good reason for limiting the number of characters users can enter.
+  For example, if there is an indication that users are likely to enter more information than they need to.
+  Or when there is a legal or technical reason that means an entry must be no more than a certain number of characters.
+- If your users keep hitting the character limit imposed by the backend of your service then try to increase the limit rather than use a character count.
+`;/*@license CC0-1.0*/function r(t){const n={h2:"h2",h3:"h3",p:"p",...a(),...t.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(o,{of:m}),`
+`,e.jsx(s,{children:u}),`
+`,e.jsx(h,{}),`
+`,e.jsx(i,{}),`
+`,e.jsx(n.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(n.h3,{id:"error",children:"Error"}),`
+`,e.jsx(n.p,{children:"When the length exceeds the maximum length, the color of the text changes to red."}),`
+`,e.jsx(c,{of:l})]})}function D(t={}){const{wrapper:n}={...a(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(r,{...t})}):r(t)}export{D as default};
