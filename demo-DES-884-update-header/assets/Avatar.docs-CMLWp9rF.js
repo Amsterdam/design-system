f44b1f557d3ad9cdc9a80e285e0155d4b244d02f
alimpens <37216945+alimpens@users.noreply.github.com>
2024-12-11 12:36:07 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@b4bd94beac0c81c6402902d734eb9f9ec13e51c4 🚀

---
+++ demo-DES-884-update-header/assets/Avatar.docs-CMLWp9rF.js
@@ -0,0 +1,29 @@
+import{j as n}from"./jsx-runtime-j_jdvEMj.js";import{useMDXComponents as t}from"./index-BSj771as.js";import{ae as i,aq as s,ar as c,as as p,at as r}from"./index-C0nODLaq.js";import{A as l,W as m,F as h}from"./Avatar.stories-CENQFAxm.js";import"./index-B-o1Wr-g.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-Cz1DasK4.js";import"../sb-preview/runtime.js";import"./index-DolzVqEf.js";import"./index-D-8MO0q_.js";import"./index-CJyPbrN5.js";import"./index-DrFu-skq.js";import"./index.esm-Dydeoktt.js";import"./clsx-B-dksMZM.js";import"./Image-BeIE0HQj.js";import"./Icon-B6-t9cKk.js";const d=`<!-- @license CC0-1.0 -->
+
+# Avatar
+
+A circular badge representing a person.
+
+## Design
+
+The avatar contains 1 or 2 initial letters from the person's full name, a picture, or a generic icon.
+The default background colour is dark blue.
+
+## Guidelines
+
+- Display an avatar for the person currently using the application,
+  or to associate a person with a content item.
+`;/*@license CC0-1.0*/function o(e){const a={h2:"h2",h3:"h3",p:"p",...t(),...e.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(i,{of:l}),`
+`,n.jsx(s,{children:d}),`
+`,n.jsx(c,{}),`
+`,n.jsx(p,{}),`
+`,n.jsx(a.h2,{id:"examples",children:"Examples"}),`
+`,n.jsx(a.h3,{id:"with-image",children:"With image"}),`
+`,n.jsx(a.p,{children:`The Avatar can also display a photo or other image for the person.
+Make sure to scale the image down to around 100 pixels to prevent unnecessary data transfers.`}),`
+`,n.jsx(r,{of:m}),`
+`,n.jsx(a.h3,{id:"fallback-icon",children:"Fallback icon"}),`
+`,n.jsx(a.p,{children:"A user icon displays if no label and image are provided."}),`
+`,n.jsx(r,{of:h})]})}function T(e={}){const{wrapper:a}={...t(),...e.components};return a?n.jsx(a,{...e,children:n.jsx(o,{...e})}):o(e)}export{T as default};
