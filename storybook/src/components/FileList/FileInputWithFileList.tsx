01924048c53805eb1536d2aa9b5454bcd44375b8
Niels Roozemond <n.roozemond@amsterdam.nl>
2024-12-13 14:01:23 +0100

    feat: Add File List component to display below a File Input (#1751)
    Co-authored-by: Vincent Smedinga <v.smedinga@amsterdam.nl>

---
+++ storybook/src/components/FileList/FileInputWithFileList.tsx
@@ -0,0 +1,39 @@
+import { FileInput, FileList } from '@amsterdam/design-system-react'
+import { useRef, useState } from 'react'
+
+export const FileInputWithFileList = () => {
+  const inputRef = useRef<HTMLInputElement>(null)
+  const [files, setFiles] = useState<FileList | null>(null)
+
+  const changeFiles = () => {
+    if (inputRef.current) {
+      setFiles(inputRef.current.files)
+    }
+  }
+
+  const removeFile = (index: number) => {
+    if (files) {
+      const newFiles = new DataTransfer()
+      Array.from(files).forEach((file, i) => {
+        if (i !== index) newFiles.items.add(file)
+      })
+      if (inputRef.current) {
+        inputRef.current.files = newFiles.files
+      }
+      setFiles(newFiles.files)
+    }
+  }
+
+  return (
+    <>
+      <FileInput multiple ref={inputRef} onChange={changeFiles} />
+      {files && (
+        <FileList>
+          {Array.from(files).map((file, index) => (
+            <FileList.Item key={index} file={file} onDelete={() => removeFile(index)} />
+          ))}
+        </FileList>
+      )}
+    </>
+  )
+}
