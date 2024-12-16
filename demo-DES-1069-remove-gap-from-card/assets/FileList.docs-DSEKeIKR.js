ed9ae6a8748cd8041f4450ba01205733a7ec075c
RubenSibon <mail@rubensibon.nl>
2024-12-16 09:00:06 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@9360496d1fa2958a5d1555f7f8a694c50719a76d 🚀

---
+++ demo-DES-1069-remove-gap-from-card/assets/FileList.docs-DSEKeIKR.js
@@ -0,0 +1,55 @@
+import{j as e}from"./jsx-runtime-BjgbQsUx.js";import{useMDXComponents as s}from"./index-3rgQkYFR.js";import{ae as r,aq as o,ar as l,at as a,aw as m}from"./index-C7tQKI4S.js";import{F as f,W as p}from"./FileList.stories-BDs9eqVG.js";import"./index-D2MAbzvX.js";import"./index-RigO-4kf.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-C4K6NK-4.js";import"../sb-preview/runtime.js";import"./index-BIm0odtm.js";import"./index-D-8MO0q_.js";import"./index-CHGET4sZ.js";import"./index-DrFu-skq.js";import"./index.esm-CEzKrmam.js";import"./clsx-B-dksMZM.js";import"./index.esm-Bz9pa_xK.js";import"./Icon-B4wlDyQ3.js";import"./Button-nAoFhtc2.js";const u=`import { FileInput, FileList } from '@amsterdam/design-system-react'
+import { useRef, useState } from 'react'
+
+export const FileInputWithFileList = () => {
+  const inputRef = useRef<HTMLInputElement>(null)
+  const [files, setFiles] = useState<FileList | null>(null)
+
+  const changeFiles = () => {
+    if (inputRef.current) {
+      setFiles(inputRef.current.files)
+    }
+  }
+
+  const removeFile = (index: number) => {
+    if (files) {
+      const newFiles = new DataTransfer()
+      Array.from(files).forEach((file, i) => {
+        if (i !== index) newFiles.items.add(file)
+      })
+      if (inputRef.current) {
+        inputRef.current.files = newFiles.files
+      }
+      setFiles(newFiles.files)
+    }
+  }
+
+  return (
+    <>
+      <FileInput multiple ref={inputRef} onChange={changeFiles} />
+      {files && (
+        <FileList>
+          {Array.from(files).map((file, index) => (
+            <FileList.Item key={index} file={file} onDelete={() => removeFile(index)} />
+          ))}
+        </FileList>
+      )}
+    </>
+  )
+}
+`,c=`<!-- @license CC0-1.0 -->
+
+# File List
+
+An overview of files, showing their name, type, size, and a preview.
+`;/*@license CC0-1.0*/function t(i){const n={code:"code",h2:"h2",h3:"h3",p:"p",...s(),...i.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(r,{of:f}),`
+`,e.jsx(o,{children:c}),`
+`,e.jsx(l,{}),`
+`,e.jsx(n.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(n.h3,{id:"using-a-file-input",children:"Using a file input"}),`
+`,e.jsxs(n.p,{children:["To connect a File Input to a File List, use the ",e.jsx(n.code,{children:"onChange"}),` event to update the
+list of files and use `,e.jsx(n.code,{children:"onDelete"})," when removing a file from the list."]}),`
+`,e.jsx(a,{of:p}),`
+`,e.jsx(m,{code:u,language:"jsx"})]})}function W(i={}){const{wrapper:n}={...s(),...i.components};return n?e.jsx(n,{...i,children:e.jsx(t,{...i})}):t(i)}export{W as default};
---
+++ demo-DES-1069-remove-gap-from-card/assets/Spotlight.stories-Bf9mkOpd.js
@@ -0,0 +1,37 @@
+import{j as p}from"./jsx-runtime-BjgbQsUx.js";import{G as i,B as H}from"./index.esm-CEzKrmam.js";import{c as J}from"./exampleContent-hIIPvEhU.js";import{S as d}from"./Spotlight-CVxUBei2.js";const K=J(),N={title:"Components/Containers/Spotlight",component:d,render:({as:F,color:u})=>p.jsx(d,{as:F,color:u,children:p.jsx(i,{paddingVertical:"medium",children:p.jsx(i.Cell,{span:"all",children:p.jsx(H,{inverseColor:!u||!["lime","yellow"].includes(u),children:K})})})})},r={},e={args:{color:"azure"}},s={args:{color:"blue"}},o={args:{color:"green"}},a={args:{color:"lime"}},c={args:{color:"magenta"}},n={args:{color:"orange"}},t={args:{color:"purple"}},l={args:{color:"yellow"}},m={args:{as:"section"}};var g,S,f;r.parameters={...r.parameters,docs:{...(g=r.parameters)==null?void 0:g.docs,source:{originalSource:"{}",...(f=(S=r.parameters)==null?void 0:S.docs)==null?void 0:f.source}}};var x,j,h;e.parameters={...e.parameters,docs:{...(x=e.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  args: {
+    color: 'azure'
+  }
+}`,...(h=(j=e.parameters)==null?void 0:j.docs)==null?void 0:h.source}}};var O,_,b;s.parameters={...s.parameters,docs:{...(O=s.parameters)==null?void 0:O.docs,source:{originalSource:`{
+  args: {
+    color: 'blue'
+  }
+}`,...(b=(_=s.parameters)==null?void 0:_.docs)==null?void 0:b.source}}};var w,y,z;o.parameters={...o.parameters,docs:{...(w=o.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  args: {
+    color: 'green'
+  }
+}`,...(z=(y=o.parameters)==null?void 0:y.docs)==null?void 0:z.source}}};var B,G,v;a.parameters={...a.parameters,docs:{...(B=a.parameters)==null?void 0:B.docs,source:{originalSource:`{
+  args: {
+    color: 'lime'
+  }
+}`,...(v=(G=a.parameters)==null?void 0:G.docs)==null?void 0:v.source}}};var C,M,P;c.parameters={...c.parameters,docs:{...(C=c.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  args: {
+    color: 'magenta'
+  }
+}`,...(P=(M=c.parameters)==null?void 0:M.docs)==null?void 0:P.source}}};var A,I,L;n.parameters={...n.parameters,docs:{...(A=n.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  args: {
+    color: 'orange'
+  }
+}`,...(L=(I=n.parameters)==null?void 0:I.docs)==null?void 0:L.source}}};var Y,q,D;t.parameters={...t.parameters,docs:{...(Y=t.parameters)==null?void 0:Y.docs,source:{originalSource:`{
+  args: {
+    color: 'purple'
+  }
+}`,...(D=(q=t.parameters)==null?void 0:q.docs)==null?void 0:D.source}}};var E,k,Q;l.parameters={...l.parameters,docs:{...(E=l.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  args: {
+    color: 'yellow'
+  }
+}`,...(Q=(k=l.parameters)==null?void 0:k.docs)==null?void 0:Q.source}}};var R,T,V;m.parameters={...m.parameters,docs:{...(R=m.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  args: {
+    as: 'section'
+  }
+}`,...(V=(T=m.parameters)==null?void 0:T.docs)==null?void 0:V.source}}};const U=["Default","Azure","Blue","Green","Lime","Magenta","Orange","Purple","Yellow","ImproveSemantics"],rr=Object.freeze(Object.defineProperty({__proto__:null,Azure:e,Blue:s,Default:r,Green:o,ImproveSemantics:m,Lime:a,Magenta:c,Orange:n,Purple:t,Yellow:l,__namedExportsOrder:U,default:N},Symbol.toStringTag,{value:"Module"}));export{e as A,s as B,o as G,m as I,a as L,c as M,n as O,t as P,rr as S,l as Y};
