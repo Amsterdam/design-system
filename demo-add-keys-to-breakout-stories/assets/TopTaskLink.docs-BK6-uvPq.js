da0b67bea218b2c4eef168564ea3346eee68fa0e
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-12-17 23:40:22 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@7c64445579fc34161c755211427a4fc1ff3358d5 🚀

---
+++ demo-add-keys-to-breakout-stories/assets/TopTaskLink.docs-BK6-uvPq.js
@@ -0,0 +1,38 @@
+import{j as n}from"./jsx-runtime-BjgbQsUx.js";import{useMDXComponents as i}from"./index-3rgQkYFR.js";import{ae as s,aq as r,ar as a,as as p,at as l}from"./index-C7odwxgp.js";import{T as c,M as d}from"./TopTaskLink.stories-DSqfQ_gZ.js";import{S as h}from"./StatusBadge-DiE14TfE.js";import"./index-D2MAbzvX.js";import"./index-RigO-4kf.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-Bomu998i.js";import"../sb-preview/runtime.js";import"./index-BIm0odtm.js";import"./index-D-8MO0q_.js";import"./index-CHGET4sZ.js";import"./index-DrFu-skq.js";import"./index.esm-CfNUYwVi.js";import"./clsx-B-dksMZM.js";import"./index.esm-Bz9pa_xK.js";import"./Badge-BvbF-Lgn.js";const m=`<!-- @license CC0-1.0 -->
+
+# Top Task Link
+
+A prominent navigation element to quickly start a task.
+
+## Guidelines
+
+- A Top Task Link has both a title and a description.
+- The title describes the essence of the page being referred to, attracting attention when scanning the page.
+- The description provides more context, helping the user confirm that they can indeed achieve their goal on the linked page.
+- Keep both texts concise.
+  A title consists of a few words, and the description consists of 1 or 2 short sentences.
+  The description should end with a period or another appropriate punctuation mark.
+- For wide windows, place 3 or 4 Top Task Links side by side on the [Grid](/docs/components-layout-grid--docs).
+  If you have more Top Task Links, place them in the next row.
+  Try to distribute the amount of links on wide windows over rows of 3 or 4 links.
+  If this is not possible, leave the last cells of the grid empty.
+- If you have a group of links for which a description is unnecessary or illogical, use a [Link List](/docs/components-navigation-link--docs).
+
+### Using links with routing libraries
+
+Check out the [developer guide](/docs/docs-developer-guide-routing-libraries--docs) on how to integrate links with external routing libraries.
+
+## Relevant WCAG requirements
+
+The Top Task Link is subject to the same requirements as [regular links](/docs/components-navigation-link--docs).
+`;/*@license CC0-1.0*/function t(e){const o={h3:"h3",p:"p",...i(),...e.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(s,{of:c}),`
+`,n.jsx(h,{reason:"Will probably be removed in favour of Card."}),`
+`,n.jsx(r,{children:m}),`
+`,n.jsx(a,{}),`
+`,n.jsx(p,{}),`
+`,n.jsx(o.h3,{id:"multiple-links",children:"Multiple links"}),`
+`,n.jsx(o.p,{children:`Top tasks usually come in groups.
+Present them in a grid.`}),`
+`,n.jsx(l,{of:d})]})}function G(e={}){const{wrapper:o}={...i(),...e.components};return o?n.jsx(o,{...e,children:n.jsx(t,{...e})}):t(e)}export{G as default};
