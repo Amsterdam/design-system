86ef879eec7d14237a80149ac76443654bc10a1e
Vincent Smedinga <v.smedinga@amsterdam.nl>
2024-12-18 22:30:56 +0100

    Add more examples of captions

--- storybook/src/components/Figure/Figure.stories.tsx
+++ storybook/src/components/Figure/Figure.stories.tsx
@@ -6,9 +6,9 @@
 import { Image } from '@amsterdam/design-system-react'
 import { Figure } from '@amsterdam/design-system-react/src'
 import { Meta, StoryObj } from '@storybook/react'
+import { exampleCaption } from '../shared/exampleContent'
 
-const exampleCaption =
-  'Een rustige Amsterdamse gracht met eeuwenoude gevels die weerspiegelen in het water, terwijl fietsen nonchalant tegen de brugleuning rusten – een alledaags tafereel vol historie en charme.'
+const caption = exampleCaption()
 
 const meta = {
   title: 'Components/Media/Figure',
@@ -35,14 +35,14 @@ export const Default: Story = {
         src="https://picsum.photos/1600/500"
         srcSet="https://picsum.photos/640/200 640w, https://picsum.photos/1280/400 1280w, https://picsum.photos/1600/500 1600w"
       />,
-      <Figure.Caption>{exampleCaption}</Figure.Caption>,
+      <Figure.Caption>{caption}</Figure.Caption>,
     ],
   },
 }
 
 export const InverseColour: CaptionStory = {
   args: {
-    children: exampleCaption,
+    children: caption,
     inverseColor: true,
   },
   render: ({ children, ...args }) => (
