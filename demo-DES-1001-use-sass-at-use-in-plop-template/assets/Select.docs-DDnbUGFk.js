37fce21b476d1ecf7fdb706400fc711f678917ad
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-12-18 16:01:27 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@bb1d0b88594e22ce7fc80a5edb71e7fe5bd8ed16 🚀

---
+++ demo-DES-1001-use-sass-at-use-in-plop-template/assets/Select.docs-DDnbUGFk.js
@@ -0,0 +1,37 @@
+import{j as e}from"./jsx-runtime-BjgbQsUx.js";import{useMDXComponents as s}from"./index-3rgQkYFR.js";import{ae as r,aq as d,ar as a,as as l,at as i}from"./index-CE-HPoT0.js";import{S as c,G as h,I as m,D as p,a as x,b as j}from"./Select.stories-Cjwjd_6B.js";import"./index-D2MAbzvX.js";import"./index-RigO-4kf.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-DtbZ1Njx.js";import"../sb-preview/runtime.js";import"./index-BIm0odtm.js";import"./index-D-8MO0q_.js";import"./index-CHGET4sZ.js";import"./index-DrFu-skq.js";import"./index.esm-CfNUYwVi.js";import"./clsx-B-dksMZM.js";import"./index.esm-Bz9pa_xK.js";const f=`<!-- @license CC0-1.0 -->
+
+# Select
+
+A form control that allows users to select one or more options from a list.
+
+## References
+
+- [MDN Web Docs](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/select)
+`;/*@license CC0-1.0*/function t(o){const n={a:"a",code:"code",h2:"h2",h3:"h3",p:"p",...s(),...o.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(r,{of:c}),`
+`,e.jsx(d,{children:f}),`
+`,e.jsx(a,{}),`
+`,e.jsx(l,{}),`
+`,e.jsx(n.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(n.h3,{id:"multiple",children:"Multiple"}),`
+`,e.jsxs(n.p,{children:[`Avoid adding functionality to allow selecting multiple options. Multi select is harder to use
+on desktop as they require the user to hold down the `,e.jsx(n.code,{children:"Ctrl"})," or ",e.jsx(n.code,{children:"Cmd"}),` key while clicking on the options.
+It is recommended to use checkboxes instead.`]}),`
+`,e.jsx(n.h3,{id:"grouped",children:"Grouped"}),`
+`,e.jsxs(n.p,{children:["Use the ",e.jsx(n.code,{children:"Select.Group"}),` component to group options.
+The component requires a `,e.jsx(n.code,{children:"label"})," attribute."]}),`
+`,e.jsx(i,{of:h}),`
+`,e.jsx(n.h3,{id:"invalid",children:"Invalid"}),`
+`,e.jsx(i,{of:m}),`
+`,e.jsx(n.h3,{id:"disabled",children:"Disabled"}),`
+`,e.jsx(i,{of:p}),`
+`,e.jsx(n.h3,{id:"in-a-field",children:"In a Field"}),`
+`,e.jsx(n.p,{children:"Use a Field to group a Select with a Label, description and / or an Error Message."}),`
+`,e.jsxs(n.p,{children:["If you don’t need the description, remove its Paragraph and the ",e.jsx(n.code,{children:"aria-describedby"})," from the Select."]}),`
+`,e.jsxs(n.p,{children:["Check ",e.jsx(n.a,{href:"/docs/components-forms-field--docs",children:"the Field docs"})," for more information on configuring it."]}),`
+`,e.jsx(i,{of:x}),`
+`,e.jsx(n.h3,{id:"in-a-field-with-validation",children:"In a Field with validation"}),`
+`,e.jsxs(n.p,{children:["If the Select can become invalid, add an Error Message and its ",e.jsx(n.code,{children:"id"})," to the ",e.jsx(n.code,{children:"aria-describedby"})," attribute of the Select."]}),`
+`,e.jsxs(n.p,{children:["Check ",e.jsx(n.a,{href:"/docs/components-forms-field--docs",children:"the Field docs"})," for more information on configuring it."]}),`
+`,e.jsx(i,{of:j})]})}function q(o={}){const{wrapper:n}={...s(),...o.components};return n?e.jsx(n,{...o,children:e.jsx(t,{...o})}):t(o)}export{q as default};
---
+++ demo-DES-1001-use-sass-at-use-in-plop-template/assets/Switch.docs-Bdg1KSD0.js
@@ -0,0 +1,33 @@
+import{j as e}from"./jsx-runtime-BjgbQsUx.js";import{useMDXComponents as s}from"./index-3rgQkYFR.js";import{ae as o,aq as a,ar as r,as as c,at as l}from"./index-CE-HPoT0.js";import{S as h,W as m}from"./Switch.stories-B7FjjUkn.js";import"./index-D2MAbzvX.js";import"./index-RigO-4kf.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-DtbZ1Njx.js";import"../sb-preview/runtime.js";import"./index-BIm0odtm.js";import"./index-D-8MO0q_.js";import"./index-CHGET4sZ.js";import"./index-DrFu-skq.js";import"./index.esm-CfNUYwVi.js";import"./clsx-B-dksMZM.js";import"./index.esm-Bz9pa_xK.js";const p=`<!-- @license CC0-1.0 -->
+
+# Switch
+
+A switch is a control element to toggle between two different states quickly.
+A switch applies to a page or the entire system, such as an on/off switch.
+
+## Guidelines
+
+- A Switch must have a label, and in most cases, this label should be visible.
+- Switch only between two different states.
+  It is a binary action.
+- The action takes place immediately when the user operates the switch.
+
+## Relevant WCAG requirements
+
+- [WCAG 1.3.5](https://www.w3.org/WAI/WCAG21/Understanding/identify-input-purpose.html): it is both clear for a user and programmatically what the purpose of a form field is.
+
+Switch is an interactive element, and the [general requirements and guidelines for interactivity](/docs/docs-developer-guide-interactivity--docs) apply.
+
+## References
+
+- [Switch Pattern](https://www.w3.org/WAI/ARIA/apg/patterns/switch/)
+- [Role switch](https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/switch_role)
+`;/*@license CC0-1.0*/function i(t){const n={h2:"h2",h3:"h3",...s(),...t.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(o,{of:h}),`
+`,e.jsx(a,{children:p}),`
+`,e.jsx(r,{}),`
+`,e.jsx(c,{}),`
+`,e.jsx(n.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(n.h3,{id:"with-label",children:"With Label"}),`
+`,e.jsx(l,{of:m})]})}function I(t={}){const{wrapper:n}={...s(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(i,{...t})}):i(t)}export{I as default};
