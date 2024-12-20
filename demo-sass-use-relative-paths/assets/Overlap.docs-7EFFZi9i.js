8758950aa17abcbc0af9c28f121ff7f2d730996d
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-12-20 10:25:20 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@4cd70f1fad8b1deee9a91cc8923547a50913509d 🚀

---
+++ demo-sass-use-relative-paths/assets/Overlap.docs-7EFFZi9i.js
@@ -0,0 +1,21 @@
+import{j as e}from"./jsx-runtime-BjgbQsUx.js";import{useMDXComponents as r}from"./index-3rgQkYFR.js";import{ae as t,aq as s,ar as a}from"./index-CWKCY_Bl.js";import{O as l}from"./Overlap.stories-CAlAVuyg.js";import"./index-D2MAbzvX.js";import"./index-RigO-4kf.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-DBQNO3fk.js";import"../sb-preview/runtime.js";import"./index-BIm0odtm.js";import"./index-D-8MO0q_.js";import"./index-CHGET4sZ.js";import"./index-DrFu-skq.js";import"./index.esm-4tYMzfzc.js";import"./clsx-B-dksMZM.js";import"./index.esm-Bz9pa_xK.js";const m=`<!-- @license CC0-1.0 -->
+
+# Overlap
+
+Displays two or more components on top of each other.
+`;/*@license CC0-1.0*/function i(n){const o={a:"a",h2:"h2",h3:"h3",li:"li",p:"p",ul:"ul",...r(),...n.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(t,{of:l}),`
+`,e.jsx(s,{children:m}),`
+`,e.jsx(o.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(o.h3,{id:"hero-image-with-search-field",children:"Hero image with Search Field"}),`
+`,e.jsxs(o.p,{children:[`This allows for the use of a mood-setting image as a background.
+A `,e.jsx(o.a,{href:"/docs/components-layout-grid--docs",children:"Grid"}),` provides horizontal space on both sides and columns for layout.
+Note: the library does not yet offer an option to vertically center the form.`]}),`
+`,e.jsx(a,{}),`
+`,e.jsx(o.h2,{id:"guidelines",children:"Guidelines"}),`
+`,e.jsxs(o.ul,{children:[`
+`,e.jsxs(o.li,{children:["Follow ",e.jsx(o.a,{href:"/docs/components-media-image--docs#guidelines",children:"the general guidelines for images"}),"."]}),`
+`,e.jsx(o.li,{children:`Use a sufficiently large image.
+The aspect ratio of 32:9 is most suitable.`}),`
+`]})]})}function E(n={}){const{wrapper:o}={...r(),...n.components};return o?e.jsx(o,{...n,children:e.jsx(i,{...n})}):i(n)}export{E as default};
