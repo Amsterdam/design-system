e5bdbbf9719b98e098ae2ae8e3a925188836c57e
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-12-20 10:53:27 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@693529533b8724979844e13eb4149f4c41bc12f5 🚀

---
+++ demo-DES-1113-remove-spotlight-from-footer/assets/Pagination.docs-H7mGY1aP.js
@@ -0,0 +1,28 @@
+import{j as e}from"./jsx-runtime-BjgbQsUx.js";import{useMDXComponents as i}from"./index-3rgQkYFR.js";import{ae as o,aq as s,ar as r,as as l}from"./index-DIVch7fU.js";import{P as p}from"./Pagination.stories-DMveVvuE.js";import"./index-D2MAbzvX.js";import"./index-RigO-4kf.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-LTB1Lao6.js";import"../sb-preview/runtime.js";import"./index-BIm0odtm.js";import"./index-D-8MO0q_.js";import"./index-CHGET4sZ.js";import"./index-DrFu-skq.js";import"./index.esm-Bz9pa_xK.js";import"./clsx-B-dksMZM.js";import"./Icon-CPe807nE.js";const g=`<!-- @license CC0-1.0 -->
+
+# Pagination
+
+Pagination is a navigation element used to navigate between multiple pages of overview lists.
+This is commonly used for search results or a list of news articles.
+In cases where overview lists are too long to display on a single page, pagination can provide a clearer and more functional way to divide the content across multiple pages.
+Pagination indicates the current page and allows users to navigate to other pages.
+
+## Guidelines
+
+- Use pagination only when an overview list is too long to display on a single page.
+- Place pagination below the overview list.
+- Pagination should not be displayed if there is only one page.
+- Redirect users to the first page if they enter a URL with a page number that doesn’t exist.
+- Pagination can be combined with a count at the top of the page indicating “Page # of ##.”
+- Start a page with an overview list at the top after changing the page.
+- Make sure that the visible and accessible labels for the ‘previous’ and ‘next’ buttons convey the same meaning at all times – e.g. don’t update one and forget the other.
+
+## Relevant WCAG rules
+
+Pagination is an interactive element, and the [general requirements and guidelines for interactivity](/docs/docs-developer-guide-interactivity--docs) apply.
+`;/*@license CC0-1.0*/function a(t){return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(o,{of:p}),`
+`,e.jsx(s,{children:g}),`
+`,e.jsx(r,{}),`
+`,e.jsx(l,{})]})}function D(t={}){const{wrapper:n}={...i(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(a,{...t})}):a()}export{D as default};
