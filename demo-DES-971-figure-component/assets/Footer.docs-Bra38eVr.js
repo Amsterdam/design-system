719ff11255e5f717d614a482f88e0f4b1222e0fb
VincentSmedinga <v.smedinga@amsterdam.nl>
2024-12-20 15:43:08 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@038e8d7c3185478cda08e9e98852493074dee08a 🚀

---
+++ demo-DES-971-figure-component/assets/Footer.docs-Bra38eVr.js
@@ -0,0 +1,32 @@
+import{j as e}from"./jsx-runtime-BjgbQsUx.js";import{useMDXComponents as i}from"./index-3rgQkYFR.js";import{ae as s,aq as r,ar as a,at as c}from"./index-CVjkP0S9.js";import{F as m,O as p}from"./Footer.stories-CIRnT_Mb.js";import"./index-D2MAbzvX.js";import"./index-RigO-4kf.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-Dp2VuOBv.js";import"../sb-preview/runtime.js";import"./index-BIm0odtm.js";import"./index-D-8MO0q_.js";import"./index-CHGET4sZ.js";import"./index-DrFu-skq.js";import"./index.esm-C0OPbFsf.js";import"./clsx-B-dksMZM.js";import"./index.esm-Bz9pa_xK.js";import"./PageMenu.stories-DMSuHN48.js";import"./Icon-CPe807nE.js";const l=`<!-- @license CC0-1.0 -->
+
+# Footer
+
+Provides service information at the bottom of every page.
+
+## Guidelines
+
+- The Footer usually consists of two sections: a full-width blue area and a [Page Menu](/docs/components-navigation-page-menu--docs) below it.
+- A Footer containing only a Page Menu can be sufficient for applications.
+- The Footer must be used on all websites for the City of Amsterdam.
+- It must be the same on every page.
+
+The top section offers space for various practical links:
+
+- The first column focuses on contact information.
+  The information is as specific as possible and tailored to the respective website or page (e.g., including a chat option if available).
+- The second column contains links to relevant (online) sites or sources.
+- The third column refers to newsletters, social media, etc.
+
+The menu in the bottom section is for links to privacy policies, cookie statements, information about the website itself, etc.
+Contact details should not go here.
+`;/*@license CC0-1.0*/function o(t){const n={h2:"h2",h3:"h3",p:"p",...i(),...t.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(s,{of:m}),`
+`,e.jsx(r,{children:l}),`
+`,e.jsx(a,{}),`
+`,e.jsx(n.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(n.h3,{id:"onderzoek-en-statistiek",children:"Onderzoek en Statistiek"}),`
+`,e.jsx(n.p,{children:`The footer for a specific website can be a bit different.
+Here’s an example for the ‘Onderzoek en Statistiek’ department.`}),`
+`,e.jsx(c,{of:p})]})}function D(t={}){const{wrapper:n}={...i(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(o,{...t})}):o(t)}export{D as default};
---
+++ demo-DES-971-figure-component/assets/Icon.stories-LbYIhcfJ.js
@@ -0,0 +1,36 @@
+import{j as e}from"./jsx-runtime-BjgbQsUx.js";import{H as k}from"./index.esm-C0OPbFsf.js";import{d as F,I as p}from"./index.esm-Bz9pa_xK.js";import{I as M}from"./Icon-CPe807nE.js";const P={title:"Components/Media/Icon",component:M,args:{inverseColor:!1,svg:F},argTypes:{svg:{control:{type:"select"},options:Object.keys(p),mapping:p}}},r={},s={decorators:[i=>e.jsx("span",{style:{display:"flex",gap:"1rem"},children:e.jsx(i,{})})],render:i=>e.jsxs(e.Fragment,{children:[e.jsx(M,{...i,size:"level-3"}),e.jsx(k,{size:"level-3",children:"Inline text"})]})},a={args:{square:!0}},o={args:{size:"level-3"}},t={args:{size:"level-4"}},n={args:{size:"level-5"}},c={args:{size:"level-6"}},l={args:{inverseColor:!0}};var m,d,u;r.parameters={...r.parameters,docs:{...(m=r.parameters)==null?void 0:m.docs,source:{originalSource:"{}",...(u=(d=r.parameters)==null?void 0:d.docs)==null?void 0:u.source}}};var g,v,S;s.parameters={...s.parameters,docs:{...(g=s.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  decorators: [Story => <span style={{
+    display: 'flex',
+    gap: '1rem'
+  }}>
+        <Story />
+      </span>],
+  render: args => <>
+      <Icon {...args} size="level-3" />
+      <Heading size="level-3">Inline text</Heading>
+    </>
+}`,...(S=(v=s.parameters)==null?void 0:v.docs)==null?void 0:S.source}}};var x,z,f;a.parameters={...a.parameters,docs:{...(x=a.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  args: {
+    square: true
+  }
+}`,...(f=(z=a.parameters)==null?void 0:z.docs)==null?void 0:f.source}}};var I,y,j;o.parameters={...o.parameters,docs:{...(I=o.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  args: {
+    size: 'level-3'
+  }
+}`,...(j=(y=o.parameters)==null?void 0:y.docs)==null?void 0:j.source}}};var L,C,_;t.parameters={...t.parameters,docs:{...(L=t.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  args: {
+    size: 'level-4'
+  }
+}`,...(_=(C=t.parameters)==null?void 0:C.docs)==null?void 0:_.source}}};var b,h,q;n.parameters={...n.parameters,docs:{...(b=n.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    size: 'level-5'
+  }
+}`,...(q=(h=n.parameters)==null?void 0:h.docs)==null?void 0:q.source}}};var H,O,T;c.parameters={...c.parameters,docs:{...(H=c.parameters)==null?void 0:H.docs,source:{originalSource:`{
+  args: {
+    size: 'level-6'
+  }
+}`,...(T=(O=c.parameters)==null?void 0:O.docs)==null?void 0:T.source}}};var E,W,D;l.parameters={...l.parameters,docs:{...(E=l.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  args: {
+    inverseColor: true
+  }
+}`,...(D=(W=l.parameters)==null?void 0:W.docs)==null?void 0:D.source}}};const R=["Default","WithText","Square","Level3","Level4","Level5","Level6","InverseColour"],J=Object.freeze(Object.defineProperty({__proto__:null,Default:r,InverseColour:l,Level3:o,Level4:t,Level5:n,Level6:c,Square:a,WithText:s,__namedExportsOrder:R,default:P},Symbol.toStringTag,{value:"Module"}));export{J as I,o as L,a as S,s as W,t as a,n as b,c,l as d};
