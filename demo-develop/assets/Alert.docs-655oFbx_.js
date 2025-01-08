741f47857c8044e530fdd4c84d46f033b8bd1d98
alimpens <37216945+alimpens@users.noreply.github.com>
2025-01-08 09:07:31 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@cd4eca5e99f68e50f2a76488fa839c638ed88931 🚀

---
+++ demo-develop/assets/Alert.docs-655oFbx_.js
@@ -0,0 +1,55 @@
+import{j as n}from"./jsx-runtime-z8MfsBtr.js";import{useMDXComponents as s}from"./index-BAvqEBP-.js";import{ae as o,aq as a,ar as c,as as h,at as i}from"./index-Cl4EX_7k.js";import{A as l,W as d,E as m,S as p,I as f,a as u,b as x,c as g}from"./Alert.stories-vGUO18Hz.js";import"./index-C9rmetsa.js";import"./index-D7uoVdV3.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-AueDPKPw.js";import"../sb-preview/runtime.js";import"./index-D-OUEn-9.js";import"./index-Cu4lwwaE.js";import"./index-NOh9rqHv.js";import"./index-ogSvIofg.js";import"./index.esm-DEOhk3Tb.js";import"./clsx-B-dksMZM.js";import"./index.esm-X3U7dYaT.js";import"./Alert-BLyPtb9i.js";import"./Icon-BoBhPfLP.js";import"./Heading-DTBaC5sD.js";import"./getHeadingTag-Dv1eMqvl.js";import"./IconButton-1Nl16OUv.js";const j=`<!-- @license CC0-1.0 -->
+
+# Alert
+
+An Alert informs the user of a significant or time-sensitive message without interrupting their task.
+
+## Specifications
+
+There are four types of notifications:
+
+- **Warning** (orange) when action is needed to prevent damage.
+- **Error** (red) indicates an error has occurred.
+- **Confirmation** (green) to reassure that a process is complete.
+- **Notification** (blue) to bring attention to a message.
+
+## Guidelines
+
+- Place an orange Alert directly below the Header for important and urgent information.
+  Examples: system outage or changes in the opening hours of a City Office.
+- Ensure sufficient white space around the Alert.
+  The grid’s white space is a good reference – place the Alert in its own cell.
+- By default, the Alert cannot be closed.
+  This functionality can be added optionally.
+- Optionally, the heading can be omitted.
+`;/*@license CC0-1.0*/function r(t){const e={h2:"h2",h3:"h3",p:"p",...s(),...t.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(o,{of:l}),`
+`,n.jsx(a,{children:j}),`
+`,n.jsx(c,{}),`
+`,n.jsx(h,{}),`
+`,n.jsx(e.h2,{id:"examples",children:"Examples"}),`
+`,n.jsx(e.h3,{id:"warning",children:"Warning"}),`
+`,n.jsx(e.p,{children:`Display a warning when user action is required.
+This is the default severity.`}),`
+`,n.jsx(i,{of:d}),`
+`,n.jsx(e.h3,{id:"error",children:"Error"}),`
+`,n.jsx(e.p,{children:`In case of an error, use the red alert.
+Provide the user with suggestions on alternative ways to achieve the goal.`}),`
+`,n.jsx(i,{of:m}),`
+`,n.jsx(e.h3,{id:"success",children:"Success"}),`
+`,n.jsx(e.p,{children:"Announce the success of a significant action."}),`
+`,n.jsx(i,{of:p}),`
+`,n.jsx(e.h3,{id:"info",children:"Info"}),`
+`,n.jsx(e.p,{children:"An informative message can emphasize matters that are useful to follow."}),`
+`,n.jsx(i,{of:f}),`
+`,n.jsx(e.h3,{id:"with-inline-link",children:"With inline link"}),`
+`,n.jsx(e.p,{children:"Include an inline link in the text to guide the user."}),`
+`,n.jsx(i,{of:u}),`
+`,n.jsx(e.h3,{id:"with-list",children:"With list"}),`
+`,n.jsx(e.p,{children:"For clarification, formatted text can be included in the alert."}),`
+`,n.jsx(i,{of:x}),`
+`,n.jsx(e.h3,{id:"without-heading",children:"Without heading"}),`
+`,n.jsx(e.p,{children:`Sometimes, a heading is unnecessary.
+The icon automatically becomes slightly smaller.`}),`
+`,n.jsx(i,{of:g})]})}function R(t={}){const{wrapper:e}={...s(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(r,{...t})}):r(t)}export{R as default};
---
+++ demo-develop/assets/Spotlight.docs-7D1ur8Rc.js
@@ -0,0 +1,43 @@
+import{j as e}from"./jsx-runtime-z8MfsBtr.js";import{useMDXComponents as i}from"./index-BAvqEBP-.js";import{ae as r,aq as a,ar as c,as as l,at as t}from"./index-Cl4EX_7k.js";import{S as d,A as m,G as h,L as p,M as u,O as x,Y as j,I as g}from"./Spotlight.stories-DiEtk3wj.js";import"./index-C9rmetsa.js";import"./index-D7uoVdV3.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-AueDPKPw.js";import"../sb-preview/runtime.js";import"./index-D-OUEn-9.js";import"./index-Cu4lwwaE.js";import"./index-NOh9rqHv.js";import"./index-ogSvIofg.js";import"./index.esm-DEOhk3Tb.js";import"./clsx-B-dksMZM.js";import"./index.esm-X3U7dYaT.js";import"./exampleContent-CpGgN1Hc.js";const f=`<!-- @license CC0-1.0 -->
+
+# Spotlight
+
+Emphasizes a section on a page through a distinctive background colour.
+
+## Guidelines
+
+- Display the Spotlight at the entire width of the [Screen](/docs/components-layout-screen--docs); do not position it on the [Grid](/docs/components-layout-grid--docs).
+- Add a Grid with medium vertical padding inside the Spotlight to add whitespace around the content, even for a single element.
+- The default background is purple, but the colours can be chosen freely – they do not convey a meaning or theme in itself.
+- Refer to [this overview on Stijlweb](https://amsterdam.nl/stijlweb/basiselementen/kleuren/#PagCls_15671872) to determine whether you can use black or white text on the background colour of your choice.
+
+## Relevant WCAG requirements
+
+- [WCAG 1.4.3](https://www.w3.org/TR/WCAG21/#contrast-minimum): Large-scale text and images of large-scale text have a contrast ratio of at least 3:1;
+
+## References
+
+- [Color and contrast accessibility](https://web.dev/articles/color-and-contrast-accessibility)
+`;/*@license CC0-1.0*/function s(o){const n={code:"code",h2:"h2",h3:"h3",p:"p",...i(),...o.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(r,{of:d}),`
+`,e.jsx(a,{children:f}),`
+`,e.jsx(c,{}),`
+`,e.jsx(l,{}),`
+`,e.jsx(n.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(n.h3,{id:"azure",children:"Azure"}),`
+`,e.jsx(t,{of:m}),`
+`,e.jsx(n.h3,{id:"green",children:"Green"}),`
+`,e.jsx(t,{of:h}),`
+`,e.jsx(n.h3,{id:"lime",children:"Lime"}),`
+`,e.jsx(t,{of:p}),`
+`,e.jsx(n.h3,{id:"magenta",children:"Magenta"}),`
+`,e.jsx(t,{of:u}),`
+`,e.jsx(n.h3,{id:"orange",children:"Orange"}),`
+`,e.jsx(t,{of:x}),`
+`,e.jsx(n.h3,{id:"yellow",children:"Yellow"}),`
+`,e.jsx(t,{of:j}),`
+`,e.jsx(n.h3,{id:"improve-semantics",children:"Improve semantics"}),`
+`,e.jsxs(n.p,{children:["By default, a Spotlight renders a ",e.jsx(n.code,{children:"<div>"}),` element in HTML.
+Use the `,e.jsx(n.code,{children:"as"})," prop to make your markup more semantic."]}),`
+`,e.jsx(t,{of:g})]})}function T(o={}){const{wrapper:n}={...i(),...o.components};return n?e.jsx(n,{...o,children:e.jsx(s,{...o})}):s(o)}export{T as default};
