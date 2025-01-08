15c6f84ba703b46fb79eeb10452bd8f0f784bb2a
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-01-08 09:27:18 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@bbbbc5e021fcefa5a88051ac6fc6a2e42c8ba131 🚀

---
+++ demo-accordion-examples-heading-level/assets/ActionGroup.docs-Crs6Eh__.js
@@ -0,0 +1,28 @@
+import{j as t}from"./jsx-runtime-BjgbQsUx.js";import{useMDXComponents as r}from"./index-3rgQkYFR.js";import{ae as a,aq as s,ar as h,at as e}from"./index-BxVUwR6l.js";import{A as c,S as l,W as p}from"./ActionGroup.stories-DM0fZE3i.js";import"./index-D2MAbzvX.js";import"./index-RigO-4kf.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-qdilbe_i.js";import"../sb-preview/runtime.js";import"./index-BIm0odtm.js";import"./index-D-8MO0q_.js";import"./index-CHGET4sZ.js";import"./index-DrFu-skq.js";import"./index.esm-CfNUYwVi.js";import"./clsx-B-dksMZM.js";import"./index.esm-Bz9pa_xK.js";const d=`<!-- @license CC0-1.0 -->
+
+# Action Group
+
+Groups one or more related actions and manages their layout.
+
+## How to use
+
+- Both a [Button](?path=/docs/components-buttons-button--docs) and a [Link](?path=/docs/components-navigation-link--docs) can provide an ‘action’ in this context.
+- If two or more buttons or links are in a row, put the one for the primary action first and the other buttons behind it.
+- Sighted users will read the primary action first, in line with the natural reading order.
+  The same goes for users of screen readers, who will hear the primary action first, and users of a keyboard, who will focus the primary action first.
+- Also, this approach keeps the order of buttons consistent on both narrow and wide screens: if the buttons do not fit next to each other, they get stacked vertically with the primary action on top.
+- Replace the default ’group’ role with \`role="toolbar"\` for button toolbars.
+`;/*@license CC0-1.0*/function i(o){const n={h2:"h2",h3:"h3",p:"p",...r(),...o.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(a,{of:c}),`
+`,t.jsx(s,{children:d}),`
+`,t.jsx(h,{}),`
+`,t.jsx(n.h2,{id:"examples",children:"Examples"}),`
+`,t.jsx(n.h3,{id:"stacked",children:"Stacked"}),`
+`,t.jsx(n.p,{children:`If the Buttons don’t fit next to each other, they will automatically stack vertically and stretch to the full width.
+This can occur in a narrow Dialog, with long labels, a large text size, or when zooming in.
+Resize the pink rectangle to see this in action.`}),`
+`,t.jsx(e,{of:l}),`
+`,t.jsx(n.h3,{id:"with-link",children:"With Link"}),`
+`,t.jsx(n.p,{children:"An action that involves navigation should be a link."}),`
+`,t.jsx(e,{of:p})]})}function E(o={}){const{wrapper:n}={...r(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(i,{...o})}):i(o)}export{E as default};
