5479829313f20195603fa7ac537c4e3442824459
alimpens <37216945+alimpens@users.noreply.github.com>
2025-01-10 16:02:09 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@c9f9a4d5230fd46a4fb25468a61a3913cec77d89 🚀

---
+++ demo-update-publishing-md/assets/copyright.docs-BfI4B7hT.js
@@ -0,0 +1,26 @@
+import{j as e}from"./jsx-runtime-z8MfsBtr.js";import{useMDXComponents as r}from"./index-BAvqEBP-.js";import{ae as s,aq as a}from"./index-Ck-peV38.js";import"./index-C9rmetsa.js";import"./index-D7uoVdV3.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-Doadv7ph.js";import"../sb-preview/runtime.js";import"./index-D-OUEn-9.js";import"./index-Cu4lwwaE.js";import"./index-NOh9rqHv.js";import"./index-ogSvIofg.js";const i=`<!-- @license CC0-1.0 -->
+
+# Copyright
+
+## Branding, logo and authors’ rights
+
+The branding and logo are used exclusively by the City of Amsterdam.
+Authors’ rights and trademark rights apply to the logo.
+If you intend to use a modified version of the software for other purposes, you are not allowed to use the City of Amsterdam’s logo.
+Instead, you should design your own branding.
+
+## Typefaces
+
+Not all typefaces used in the branding are free and open source.
+Please note that you must arrange a (paid) license if you use the provided typefaces.
+Alternatively, adjust the configuration to use fewer or different typefaces.
+
+Amsterdam Sans is licensed exclusively to the City of Amsterdam and amsterdam&partners.
+
+## Permission
+
+For permission to use the logo or branding, please contact <designsystem@amsterdam.nl>.
+`;/*@license CC0-1.0*/function o(t){return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(s,{title:"Docs/Terms of Use/Copyright"}),`
+`,e.jsx(a,{children:i})]})}function j(t={}){const{wrapper:n}={...r(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(o,{...t})}):o()}export{j as default};
