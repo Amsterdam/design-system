2a0b6a9f36ba1e27e9a372594aef117d9b6d8e0c
dlnr <niels@dlnr.dev>
2025-01-10 15:12:03 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@b3ce6e4255c0d8f56cd937dfe7a3a83f9beb38dc 🚀

---
+++ demo-DES-943-new-icons/assets/HomePage.docs-A3U8zrYz.js
@@ -0,0 +1,40 @@
+import{j as n}from"./jsx-runtime-z8MfsBtr.js";import{useMDXComponents as o}from"./index-BAvqEBP-.js";import{ae as t}from"./index-DAhAwhbF.js";import{H as a}from"./HomePage.stories-MKKqxcj8.js";import"./index-C9rmetsa.js";import"./index-D7uoVdV3.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-xEZnUEq5.js";import"../sb-preview/runtime.js";import"./index-D-OUEn-9.js";import"./index-Cu4lwwaE.js";import"./index-NOh9rqHv.js";import"./index-ogSvIofg.js";import"./index.esm-DAG4DwtI.js";import"./clsx-B-dksMZM.js";import"./index.esm-CKYHBquS.js";import"./Overlap.stories-C3m1PCWi.js";import"./config-BhELMbda.js";import"./Footer.stories-C-iHI-0B.js";import"./PageMenu.stories-0NlkiIdK.js";import"./Icon-BoBhPfLP.js";/*@license CC0-1.0*/function r(i){const e={code:"code",h1:"h1",h2:"h2",li:"li",ol:"ol",p:"p",pre:"pre",...o(),...i.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(t,{of:a}),`
+`,n.jsx(e.h1,{id:"home-page",children:"Home page"}),`
+`,n.jsx(e.p,{children:"The starting page of an application generally provides a broad overview of subjects, common tasks, and recent articles."}),`
+`,n.jsx(e.h2,{id:"layout",children:"Layout"}),`
+`,n.jsx(e.p,{children:"A typical layout for a homepage:"}),`
+`,n.jsx(e.pre,{children:n.jsx(e.code,{className:"language-tsx",children:`<Screen>
+  <SkipLink href="#main">Direct naar inhoud</SkipLink>
+  <Grid>
+    <Header />
+  </Grid>
+  <main id="main">
+    <Overlap>
+      <Search />
+    </Overlap>
+    <Grid>
+      <AppComponent1 />
+      <AppComponent2 />
+      <AppComponent3 />
+    </Grid>
+  </main>
+  <Footer>
+    <Grid />
+  </Footer>
+</Screen>
+`})}),`
+`,n.jsxs(e.ol,{children:[`
+`,n.jsx(e.li,{children:"The Skip Link prevents users from having to pass all preceding navigation links."}),`
+`,n.jsx(e.li,{children:"The Screen component sets a maximum width for the entire page."}),`
+`,n.jsx(e.li,{children:"The Grid divides the available width into 4, 8, or 12 columns."}),`
+`,n.jsx(e.li,{children:"The Header shows the logo and menus for the application."}),`
+`,n.jsx(e.li,{children:`The main element wraps everything between Header and Footer.
+It carries an id for the skip link to target.`}),`
+`,n.jsx(e.li,{children:`An Overlap stacks a Search Field on a background Image.
+It is as wide as the Grid, hence it is a sibling of it.`}),`
+`,n.jsx(e.li,{children:"Other components in the main area are laid out on a Grid again."}),`
+`,n.jsx(e.li,{children:`The Footer contains its own Grid.
+This allows its background colour to stretch to the Screen’s width.`}),`
+`]})]})}function F(i={}){const{wrapper:e}={...o(),...i.components};return e?n.jsx(e,{...i,children:n.jsx(r,{...i})}):r(i)}export{F as default};
