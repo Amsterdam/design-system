8cc6308eeafd2d73d56861bff7b7f481b58fb693
alimpens <37216945+alimpens@users.noreply.github.com>
2025-01-10 13:23:43 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@d0c0a2ddf7d39060596aa8eeebb9ae233df4a131 🚀

---
+++ demo-release-branch/assets/Footer.docs-hPhPsU_C.js
@@ -0,0 +1,32 @@
+import{j as e}from"./jsx-runtime-z8MfsBtr.js";import{useMDXComponents as i}from"./index-BAvqEBP-.js";import{ae as s,aq as r,ar as a,at as c}from"./index-DUkTAklZ.js";import{F as m,O as p}from"./Footer.stories-BpvrOOOl.js";import"./index-C9rmetsa.js";import"./index-D7uoVdV3.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-BGlMs-HE.js";import"../sb-preview/runtime.js";import"./index-D-OUEn-9.js";import"./index-Cu4lwwaE.js";import"./index-NOh9rqHv.js";import"./index-ogSvIofg.js";import"./index.esm-DSD1WKm8.js";import"./clsx-B-dksMZM.js";import"./index.esm-X3U7dYaT.js";import"./PageMenu.stories-ibpWa2qd.js";import"./Icon-BoBhPfLP.js";const l=`<!-- @license CC0-1.0 -->
+
+# Footer
+
+Provides service information at the bottom of every page.
+
+## Guidelines
+
+- The Footer usually consists of two sections: a full-width blue area and a [Page Menu](/docs/components-navigation-page-menu--docs) below it.
+- A Footer containing only a Page Menu can be sufficient for applications.
+- The Footer must be used on all websites for the City of Amsterdam.
+- It must be the same on every page.
+
+The top section offers space for various practical links:
+
+- The first column focuses on contact information.
+  The information is as specific as possible and tailored to the respective website or page (e.g., including a chat option if available).
+- The second column contains links to relevant (online) sites or sources.
+- The third column refers to newsletters, social media, etc.
+
+The menu in the bottom section is for links to privacy policies, cookie statements, information about the website itself, etc.
+Contact details should not go here.
+`;/*@license CC0-1.0*/function o(t){const n={h2:"h2",h3:"h3",p:"p",...i(),...t.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(s,{of:m}),`
+`,e.jsx(r,{children:l}),`
+`,e.jsx(a,{}),`
+`,e.jsx(n.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(n.h3,{id:"onderzoek-en-statistiek",children:"Onderzoek en Statistiek"}),`
+`,e.jsx(n.p,{children:`The footer for a specific website can be a bit different.
+Here’s an example for the ‘Onderzoek en Statistiek’ department.`}),`
+`,e.jsx(c,{of:p})]})}function D(t={}){const{wrapper:n}={...i(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(o,{...t})}):o(t)}export{D as default};
