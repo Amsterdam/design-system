e5ba40eaa0d98e0d12645744678a08e724877cba
GitHub <github-action@users.noreply.github.com>
2025-01-13 09:44:45 +0000

    Remove feature branch folder

--- demo-aspect-ratio-link/assets/PageHeading.stories-DrE-JtHr.js
+++
@@ -1,8 +0,0 @@
-import{j as u}from"./jsx-runtime-z8MfsBtr.js";import{c as f}from"./clsx-B-dksMZM.js";import{r as _}from"./index-D7uoVdV3.js";const a=_.forwardRef(({children:o,className:l,inverseColor:p,...m},g)=>u.jsx("h1",{...m,ref:g,className:f("ams-page-heading",p&&"ams-page-heading--inverse-color",l),children:o}));a.displayName="PageHeading";try{a.displayName="PageHeading",a.__docgenInfo={description:"",displayName:"PageHeading",props:{inverseColor:{defaultValue:null,description:"Changes the text colour for readability on a dark background.",name:"inverseColor",required:!1,type:{name:"boolean"}}}}}catch{}/**
- * @license EUPL-1.2+
- * Copyright Gemeente Amsterdam
- */const h={title:"Components/Text/Page Heading",component:a,args:{children:"Gemeente Amsterdam",inverseColor:!1},argTypes:{children:{description:"The heading text.",table:{disable:!1}}}},e={},r={args:{inverseColor:!0}};var s,t,n;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:"{}",...(n=(t=e.parameters)==null?void 0:t.docs)==null?void 0:n.source}}};var i,c,d;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:`{
-  args: {
-    inverseColor: true
-  }
-}`,...(d=(c=r.parameters)==null?void 0:c.docs)==null?void 0:d.source}}};const x=["Default","InverseColour"],b=Object.freeze(Object.defineProperty({__proto__:null,Default:e,InverseColour:r,__namedExportsOrder:x,default:h},Symbol.toStringTag,{value:"Module"}));export{r as I,b as P};
