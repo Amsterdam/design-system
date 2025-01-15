00c57bd8fd953053b83c8c0c06749025a180899f
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-01-15 15:36:01 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@c9d8faf378e1dacc7c0c49028a3d38d417022d98 🚀

---
+++ demo-flex-direction-initial-value/assets/Badge.docs-CMrrHMWD.js
@@ -0,0 +1,18 @@
+import{j as n}from"./jsx-runtime-z8MfsBtr.js";import{useMDXComponents as e}from"./index-BAvqEBP-.js";import{ae as i,aq as s,ar as a,as as m}from"./index-DhzkNPoq.js";import{B as c}from"./Badge.stories-D7X3uoLb.js";import"./index-C9rmetsa.js";import"./index-D7uoVdV3.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-DoS5XMnd.js";import"../sb-preview/runtime.js";import"./index-D-OUEn-9.js";import"./index-Cu4lwwaE.js";import"./index-NOh9rqHv.js";import"./index-ogSvIofg.js";import"./Badge-HfNEZZM2.js";import"./clsx-B-dksMZM.js";const p=`<!-- @license CC0-1.0 -->
+
+# Badge
+
+A prominently coloured box containing 1 or 2 words.
+Guides the user in taking a specific action or describes its surrounding content.
+
+## Design
+
+The badge can contain a short text or a number.
+The default background colour is dark green.
+Suggestions on when to use the other colours will follow soon.
+`;/*@license CC0-1.0*/function t(o){return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(i,{of:c}),`
+`,n.jsx(s,{children:p}),`
+`,n.jsx(a,{}),`
+`,n.jsx(m,{})]})}function B(o={}){const{wrapper:r}={...e(),...o.components};return r?n.jsx(r,{...o,children:n.jsx(t,{...o})}):t()}export{B as default};
---
+++ demo-flex-direction-initial-value/assets/Label.docs-BEzNFBGV.js
@@ -0,0 +1,26 @@
+import{j as t}from"./jsx-runtime-z8MfsBtr.js";import{useMDXComponents as r}from"./index-BAvqEBP-.js";import{ae as s,aq as a,ar as h,as as l,at as o}from"./index-DhzkNPoq.js";import{L as p,O as m,W as c}from"./Label.stories-DqtjGFwO.js";import"./index-C9rmetsa.js";import"./index-D7uoVdV3.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-DoS5XMnd.js";import"../sb-preview/runtime.js";import"./index-D-OUEn-9.js";import"./index-Cu4lwwaE.js";import"./index-NOh9rqHv.js";import"./index-ogSvIofg.js";import"./clsx-B-dksMZM.js";import"./Hint-pM00LTCu.js";const d=`<!-- @license CC0-1.0 -->
+
+# Label
+
+Describes a form control.
+
+## Guidelines
+
+- Always associate a form element (such as an \`input\` or \`textarea\`) with a label.
+- Set \`optional\` to \`true\` if the input is not required.
+- Use \`hint\` to show a custom hint text.
+`;/*@license CC0-1.0*/function i(n){const e={a:"a",code:"code",h2:"h2",h3:"h3",p:"p",...r(),...n.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(s,{of:p}),`
+`,t.jsx(a,{children:d}),`
+`,t.jsx(h,{}),`
+`,t.jsx(l,{}),`
+`,t.jsx(e.h2,{id:"examples",children:"Examples"}),`
+`,t.jsx(e.h3,{id:"optional",children:"Optional"}),`
+`,t.jsxs(e.p,{children:["Use the ",t.jsx(e.code,{children:"optional"})," prop to indicate that the user doesn’t need to fill in the field to submit the form."]}),`
+`,t.jsx(o,{of:m}),`
+`,t.jsx(e.h3,{id:"with-hint",children:"With hint"}),`
+`,t.jsxs(e.p,{children:["Use the ",t.jsx(e.code,{children:"hint"})," prop to either customize the text for the optional property or provide another type of remark."]}),`
+`,t.jsxs(e.p,{children:["Note: ensure the hint meets ",t.jsx(e.a,{href:"https://www.amsterdam.nl/schrijfwijzer/heldere-taal-basis-onze-huisstijl/",rel:"nofollow",children:"language level B1"}),`.
+For the same reason, the default hint text in Dutch is ‘niet verplicht’, not ‘optioneel’.`]}),`
+`,t.jsx(o,{of:c})]})}function O(n={}){const{wrapper:e}={...r(),...n.components};return e?t.jsx(e,{...n,children:t.jsx(i,{...n})}):i(n)}export{O as default};
