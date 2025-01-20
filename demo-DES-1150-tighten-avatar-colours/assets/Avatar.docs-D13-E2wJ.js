0f91752363271b04ba77e149e009fb2af3e9cab5
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-01-20 22:33:46 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@55772ccff990fa80e679fd5fb74c5924ef4a6610 🚀

---
+++ demo-DES-1150-tighten-avatar-colours/assets/Avatar.docs-D13-E2wJ.js
@@ -0,0 +1,30 @@
+import{j as n}from"./jsx-runtime-z8MfsBtr.js";import{useMDXComponents as r}from"./index-BAvqEBP-.js";import{M as i,e as s,P as p,f as c,h as a}from"./index-6ZTWmBGf.js";import{A as l,W as h,F as m}from"./Avatar.stories-B_MbJq7l.js";import"./index-C9rmetsa.js";import"./index-D7uoVdV3.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-D6EUK33F.js";import"./index-D-OUEn-9.js";import"./index-Cu4lwwaE.js";import"./index-NOh9rqHv.js";import"./index-ogSvIofg.js";import"./index.esm-X3U7dYaT.js";import"./clsx-B-dksMZM.js";import"./Image-wjPwPRFj.js";import"./Icon-BoBhPfLP.js";const d=`<!-- @license CC0-1.0 -->
+
+# Avatar
+
+A circular badge representing a person.
+
+## Design
+
+The avatar contains 1 or 2 initial letters from the person’s full name, a picture, or a generic icon.
+To personalize the avatar, the user can be allowd to choose one of the highlight colours.
+The default background colour is purple.
+
+## Guidelines
+
+- Display an avatar for the person who is using the application,
+  or to associate a person with a content item.
+`;/*@license CC0-1.0*/function t(o){const e={h2:"h2",h3:"h3",p:"p",...r(),...o.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(i,{of:l}),`
+`,n.jsx(s,{children:d}),`
+`,n.jsx(p,{}),`
+`,n.jsx(c,{}),`
+`,n.jsx(e.h2,{id:"examples",children:"Examples"}),`
+`,n.jsx(e.h3,{id:"with-image",children:"With image"}),`
+`,n.jsx(e.p,{children:`The Avatar can also display a photo or other image for the person.
+Make sure to scale the image down to around 100 pixels to prevent unnecessary data transfers.`}),`
+`,n.jsx(a,{of:h}),`
+`,n.jsx(e.h3,{id:"fallback-icon",children:"Fallback icon"}),`
+`,n.jsx(e.p,{children:"A user icon displays if no label and image are provided."}),`
+`,n.jsx(a,{of:m})]})}function T(o={}){const{wrapper:e}={...r(),...o.components};return e?n.jsx(e,{...o,children:n.jsx(t,{...o})}):t(o)}export{T as default};
---
+++ demo-DES-1150-tighten-avatar-colours/assets/VisuallyHidden.docs-fdM992O-.js
@@ -0,0 +1,20 @@
+import{j as n}from"./jsx-runtime-z8MfsBtr.js";import{useMDXComponents as t}from"./index-BAvqEBP-.js";import{M as r,e as i,P as a,f as m}from"./index-6ZTWmBGf.js";import{V as l}from"./VisuallyHidden.stories-Cpw80tP2.js";import"./index-C9rmetsa.js";import"./index-D7uoVdV3.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-D6EUK33F.js";import"./index-D-OUEn-9.js";import"./index-Cu4lwwaE.js";import"./index-NOh9rqHv.js";import"./index-ogSvIofg.js";import"./index.esm-7ulgzkiE.js";import"./clsx-B-dksMZM.js";import"./index.esm-X3U7dYaT.js";const u=`<!-- @license CC0-1.0 -->
+
+# Visually Hidden
+
+Hides content from sighted users but keeps it accessible to non-visual user agents, such as screen readers.
+
+## Class name
+
+\`ams-visually-hidden\`
+
+## Guidelines
+
+- In most cases, visually available content should be accessible to non-visual user agents and vice versa.
+  Elaborate instructions or guidance read only by non-visual user agents can do more harm than good.
+`;/*@license CC0-1.0*/function o(s){return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(r,{of:l}),`
+`,n.jsx(i,{children:u}),`
+`,n.jsx(a,{}),`
+`,n.jsx(m,{})]})}function H(s={}){const{wrapper:e}={...t(),...s.components};return e?n.jsx(e,{...s,children:n.jsx(o,{...s})}):o()}export{H as default};
