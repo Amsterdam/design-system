7ca03884b8643d4745d8c3ffb332b547bdc51036
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-01-21 10:31:50 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@a9607d070c941e132e740932042d5773b73f6fc1 🚀

---
+++ demo-DES-1101-update-alert-border-design/assets/Avatar.docs-Cn_C2Ody.js
@@ -0,0 +1,29 @@
+import{j as n}from"./jsx-runtime-z8MfsBtr.js";import{useMDXComponents as t}from"./index-BAvqEBP-.js";import{M as i,e as s,P as c,f as p,h as r}from"./index-C-RWvOPr.js";import{A as l,W as m,F as h}from"./Avatar.stories-ym4Ebewd.js";import"./index-C9rmetsa.js";import"./index-D7uoVdV3.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-BAylfXrj.js";import"./index-D-OUEn-9.js";import"./index-Cu4lwwaE.js";import"./index-NOh9rqHv.js";import"./index-ogSvIofg.js";import"./index.esm-DfRxjY0e.js";import"./clsx-B-dksMZM.js";import"./Image-wjPwPRFj.js";import"./Icon-BoBhPfLP.js";const d=`<!-- @license CC0-1.0 -->
+
+# Avatar
+
+A circular badge representing a person.
+
+## Design
+
+The avatar contains 1 or 2 initial letters from the person's full name, a picture, or a generic icon.
+The default background colour is dark blue.
+
+## Guidelines
+
+- Display an avatar for the person currently using the application,
+  or to associate a person with a content item.
+`;/*@license CC0-1.0*/function o(a){const e={h2:"h2",h3:"h3",p:"p",...t(),...a.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(i,{of:l}),`
+`,n.jsx(s,{children:d}),`
+`,n.jsx(c,{}),`
+`,n.jsx(p,{}),`
+`,n.jsx(e.h2,{id:"examples",children:"Examples"}),`
+`,n.jsx(e.h3,{id:"with-image",children:"With image"}),`
+`,n.jsx(e.p,{children:`The Avatar can also display a photo or other image for the person.
+Make sure to scale the image down to around 100 pixels to prevent unnecessary data transfers.`}),`
+`,n.jsx(r,{of:m}),`
+`,n.jsx(e.h3,{id:"fallback-icon",children:"Fallback icon"}),`
+`,n.jsx(e.p,{children:"A user icon displays if no label and image are provided."}),`
+`,n.jsx(r,{of:h})]})}function T(a={}){const{wrapper:e}={...t(),...a.components};return e?n.jsx(e,{...a,children:n.jsx(o,{...a})}):o(a)}export{T as default};
