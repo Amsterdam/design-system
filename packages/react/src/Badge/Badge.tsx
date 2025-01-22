8d8cc952be19de6bf92f3e40a682b0fd69b04968
Vincent Smedinga <v.smedinga@amsterdam.nl>
2025-01-22 15:38:28 +0100

    Remove explicit default color value

--- packages/react/src/Badge/Badge.tsx
+++ packages/react/src/Badge/Badge.tsx
@@ -7,10 +7,9 @@ import clsx from 'clsx'
 import { forwardRef } from 'react'
 import type { ForwardedRef, HTMLAttributes } from 'react'
 
-const defaultColor = 'dark-green'
 export const badgeColors = ['green', 'light-blue', 'magenta', 'orange', 'purple', 'red', 'yellow'] as const
 
-type BadgeColor = (typeof badgeColors)[number] | typeof defaultColor
+type BadgeColor = (typeof badgeColors)[number]
 
 export type BadgeProps = {
   /** The background colour. */
@@ -20,8 +19,8 @@ export type BadgeProps = {
 } & HTMLAttributes<HTMLElement>
 
 export const Badge = forwardRef(
-  ({ label, className, color = defaultColor, ...restProps }: BadgeProps, ref: ForwardedRef<HTMLElement>) => (
-    <span {...restProps} ref={ref} className={clsx('ams-badge', `ams-badge--${color}`, className)}>
+  ({ label, className, color, ...restProps }: BadgeProps, ref: ForwardedRef<HTMLElement>) => (
+    <span {...restProps} ref={ref} className={clsx('ams-badge', color && `ams-badge--${color}`, className)}>
       {label}
     </span>
   ),
