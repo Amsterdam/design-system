b79ab01f8a92ffa466f1b4c4ad73402896156a32
alimpens <37216945+alimpens@users.noreply.github.com>
2025-01-24 12:32:46 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@9f2579111287fb18ec4dea20e9d1ba3a7c6e0429 🚀

---
+++ demo-DES-1150-tighten-avatar-colours/assets/Alert.stories-BNOSTfUt.js
@@ -0,0 +1,51 @@
+import{j as e}from"./jsx-runtime-z8MfsBtr.js";import{P as r,L as y}from"./index.esm-Db8jqwR6.js";import{A as x}from"./Alert-C6vAmOjA.js";const L={title:"Components/Feedback/Alert",component:x,args:{closeable:!1,heading:"Let op",headingLevel:2},argTypes:{severity:{control:{labels:{undefined:"information"}},options:[void 0,"success","warning","error"]}}},n={args:{children:e.jsx(r,{children:"Tijdens de hijswerkzaamheden geldt er een korte verkeersstop. We zetten verkeersregelaars in, volg hun aanwijzingen op. De verkeersstop duurt ongeveer 10 minuten."}),heading:"Tijdelijk geen verkeer mogelijk"}},a={args:{children:e.jsx(r,{children:"Sommige verplichte velden zijn nog niet ingevuld."}),heading:"Vul de gegevens aan",severity:"warning"}},s={args:{children:e.jsx(r,{children:"Wegens een technische fout kon het formulier niet worden verzonden. We hebben uw gegevens niet ontvangen. Probeer het over een paar minuten opnieuw."}),heading:"Niet gelukt",severity:"error"}},i={args:{children:e.jsx(r,{children:"Het formulier is verzonden. We hebben uw gegevens goed ontvangen."}),closeable:!0,heading:"Gelukt",severity:"success"}},t={args:{children:e.jsx(r,{children:"Tijdens de hijswerkzaamheden geldt er een korte verkeersstop. We zetten verkeersregelaars in, volg hun aanwijzingen op. De verkeersstop duurt ongeveer 10 minuten."}),closeable:!0}},o={args:{children:e.jsxs(r,{children:["Aangepast op 30 april: de werkzaamheden lopen uit tot ten met 31 juli 2025."," ",e.jsx(y,{variant:"inline",href:"#",children:"Lees het nieuwsbericht"})," ","voor meer informatie."]}),heading:"De werkzaamheden duren langer",severity:"warning"}};var g,d,l;n.parameters={...n.parameters,docs:{...(g=n.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    children: <Paragraph>
+        Tijdens de hijswerkzaamheden geldt er een korte verkeersstop. We zetten verkeersregelaars in, volg hun
+        aanwijzingen op. De verkeersstop duurt ongeveer 10 minuten.
+      </Paragraph>,
+    heading: 'Tijdelijk geen verkeer mogelijk'
+  }
+}`,...(l=(d=n.parameters)==null?void 0:d.docs)==null?void 0:l.source}}};var c,h,u;a.parameters={...a.parameters,docs:{...(c=a.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    children: <Paragraph>Sommige verplichte velden zijn nog niet ingevuld.</Paragraph>,
+    heading: 'Vul de gegevens aan',
+    severity: 'warning'
+  }
+}`,...(u=(h=a.parameters)==null?void 0:h.docs)==null?void 0:u.source}}};var p,m,v;s.parameters={...s.parameters,docs:{...(p=s.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    children: <Paragraph>
+        Wegens een technische fout kon het formulier niet worden verzonden. We hebben uw gegevens niet ontvangen.
+        Probeer het over een paar minuten opnieuw.
+      </Paragraph>,
+    heading: 'Niet gelukt',
+    severity: 'error'
+  }
+}`,...(v=(m=s.parameters)==null?void 0:m.docs)==null?void 0:v.source}}};var k,j,w;i.parameters={...i.parameters,docs:{...(k=i.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  args: {
+    children: <Paragraph>Het formulier is verzonden. We hebben uw gegevens goed ontvangen.</Paragraph>,
+    closeable: true,
+    heading: 'Gelukt',
+    severity: 'success'
+  }
+}`,...(w=(j=i.parameters)==null?void 0:j.docs)==null?void 0:w.source}}};var f,z,b;t.parameters={...t.parameters,docs:{...(f=t.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    children: <Paragraph>
+        Tijdens de hijswerkzaamheden geldt er een korte verkeersstop. We zetten verkeersregelaars in, volg hun
+        aanwijzingen op. De verkeersstop duurt ongeveer 10 minuten.
+      </Paragraph>,
+    closeable: true
+  }
+}`,...(b=(z=t.parameters)==null?void 0:z.docs)==null?void 0:b.source}}};var P,W,S;o.parameters={...o.parameters,docs:{...(P=o.parameters)==null?void 0:P.docs,source:{originalSource:`{
+  args: {
+    children: <Paragraph>
+        Aangepast op 30 april: de werkzaamheden lopen uit tot ten met 31 juli 2025.{' '}
+        <Link variant="inline" href="#">
+          Lees het nieuwsbericht
+        </Link>{' '}
+        voor meer informatie.
+      </Paragraph>,
+    heading: 'De werkzaamheden duren langer',
+    severity: 'warning'
+  }
+}`,...(S=(W=o.parameters)==null?void 0:W.docs)==null?void 0:S.source}}};const D=["Default","Warning","Error","Success","Info","WithInlineLink"],E=Object.freeze(Object.defineProperty({__proto__:null,Default:n,Error:s,Info:t,Success:i,Warning:a,WithInlineLink:o,__namedExportsOrder:D,default:L},Symbol.toStringTag,{value:"Module"}));export{E as A,s as E,t as I,i as S,a as W,o as a};
