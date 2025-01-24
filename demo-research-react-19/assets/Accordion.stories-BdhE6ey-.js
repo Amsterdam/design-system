e949e00e576fba39c5a3acdc5e206000d76dd1c9
RubenSibon <mail@rubensibon.nl>
2025-01-24 15:33:32 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@ef849ed459d0c7a8293b96e3e06574c7eca85acb 🚀

---
+++ demo-research-react-19/assets/Accordion.stories-BdhE6ey-.js
@@ -0,0 +1,40 @@
+import{j as e}from"./jsx-runtime-z8MfsBtr.js";import{P as n}from"./index.esm-DGPae32s.js";import{b as x,h as S}from"./exampleContent-CpGgN1Hc.js";import{c as u}from"./clsx-B-dksMZM.js";import{r}from"./index-D7uoVdV3.js";import{r as K}from"./index.esm-DfRxjY0e.js";import{g as M}from"./getHeadingTag-Dv1eMqvl.js";import{I as F}from"./Icon-BoBhPfLP.js";import{u as W}from"./useKeyboardFocus-CRUFsA_C.js";/**
+ * @license EUPL-1.2+
+ * Copyright Gemeente Amsterdam
+ */const G={headingLevel:null,sectionAs:"section"},H=r.createContext(G);try{Context.displayName="Context",Context.__docgenInfo={description:`Context lets components pass information deep down without explicitly
+passing props.
+
+Created from {@link createContext}`,displayName:"Context",props:{}}}catch{}const V=r.forwardRef(({label:o,expanded:d=!1,children:l,className:p,...h},c)=>{const{headingLevel:g,sectionAs:O}=r.useContext(H),[m,z]=r.useState(d),$=M(g),B=O||"section",j=r.useId(),P=`button-${j}`,N=`panel-${j}`;return e.jsxs("div",{className:u("ams-accordion__section",p),ref:c,...h,children:[e.jsx($,{className:"ams-accordion__header",children:e.jsxs("button",{"aria-controls":N,"aria-expanded":m,className:"ams-accordion__button",id:P,onClick:()=>z(!m),type:"button",children:[e.jsx(F,{svg:K,size:"level-5",className:"ams-accordion__icon"}),o]})}),e.jsx(B,{"aria-labelledby":P,className:u("ams-accordion__panel",{"ams-accordion__panel--expanded":m}),id:N,children:l})]})});V.displayName="Accordion.Section";try{Accordion.Section.displayName="Accordion.Section",Accordion.Section.__docgenInfo={description:"",displayName:"Accordion.Section",props:{label:{defaultValue:null,description:"The heading text.",name:"label",required:!0,type:{name:"string"}},expanded:{defaultValue:{value:"false"},description:"Whether the content is displayed initially.",name:"expanded",required:!1,type:{name:"boolean"}}}}}catch{}const q=r.forwardRef(({children:o,className:d,headingLevel:l,sectionAs:p="section"},h)=>{const c=r.useRef(null);r.useImperativeHandle(h,()=>c.current);const{keyDown:g}=W(c,{rotating:!0});return e.jsx(H.Provider,{value:{headingLevel:l,sectionAs:p},children:e.jsx("div",{className:u("ams-accordion",d),onKeyDown:g,ref:c,children:o})})});q.displayName="Accordion";const a=Object.assign(q,{Section:V});try{a.displayName="Accordion",a.__docgenInfo={description:"",displayName:"Accordion",props:{headingLevel:{defaultValue:null,description:`The hierarchical level of this Accordion’s Section Headings within the document.
+There is no default value; determine the correct level for each instance.
+Note: this intentionally does not change the font size.`,name:"headingLevel",required:!0,type:{name:"enum",value:[{value:"1"},{value:"2"},{value:"3"},{value:"4"}]}},sectionAs:{defaultValue:{value:"section"},description:"The HTML element to use for each Accordion Section.",name:"sectionAs",required:!1,type:{name:"enum",value:[{value:'"div"'},{value:'"section"'}]}}}}}catch{}const f=S(),A=S(),_=S(),y=x(),b=x(),v=x(),J={title:"Components/Containers/Accordion",component:a,args:{headingLevel:3}},i={args:{children:[e.jsx(a.Section,{label:f,children:e.jsx(n,{children:y})},1),e.jsx(a.Section,{label:A,children:e.jsx(n,{children:b})},2),e.jsx(a.Section,{label:_,children:e.jsx(n,{children:v})},3)]}},t={args:{children:[e.jsx(a.Section,{label:f,children:e.jsx(n,{children:y})},1),e.jsx(a.Section,{label:A,expanded:!0,children:e.jsx(n,{children:b})},2),e.jsx(a.Section,{label:_,children:e.jsx(n,{children:v})},3)]}},s={args:{sectionAs:"div",children:[e.jsx(a.Section,{label:f,children:e.jsx(n,{children:y})},1),e.jsx(a.Section,{label:A,children:e.jsx(n,{children:b})},2),e.jsx(a.Section,{label:_,children:e.jsx(n,{children:v})},3)]}};var k,C,I;i.parameters={...i.parameters,docs:{...(k=i.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  args: {
+    children: [<Accordion.Section key={1} label={heading1}>
+        <Paragraph>{paragraph1}</Paragraph>
+      </Accordion.Section>, <Accordion.Section key={2} label={heading2}>
+        <Paragraph>{paragraph2}</Paragraph>
+      </Accordion.Section>, <Accordion.Section key={3} label={heading3}>
+        <Paragraph>{paragraph3}</Paragraph>
+      </Accordion.Section>]
+  }
+}`,...(I=(C=i.parameters)==null?void 0:C.docs)==null?void 0:I.source}}};var R,T,w;t.parameters={...t.parameters,docs:{...(R=t.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  args: {
+    children: [<Accordion.Section key={1} label={heading1}>
+        <Paragraph>{paragraph1}</Paragraph>
+      </Accordion.Section>, <Accordion.Section key={2} label={heading2} expanded>
+        <Paragraph>{paragraph2}</Paragraph>
+      </Accordion.Section>, <Accordion.Section key={3} label={heading3}>
+        <Paragraph>{paragraph3}</Paragraph>
+      </Accordion.Section>]
+  }
+}`,...(w=(T=t.parameters)==null?void 0:T.docs)==null?void 0:w.source}}};var E,L,D;s.parameters={...s.parameters,docs:{...(E=s.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  args: {
+    sectionAs: 'div',
+    children: [<Accordion.Section key={1} label={heading1}>
+        <Paragraph>{paragraph1}</Paragraph>
+      </Accordion.Section>, <Accordion.Section key={2} label={heading2}>
+        <Paragraph>{paragraph2}</Paragraph>
+      </Accordion.Section>, <Accordion.Section key={3} label={heading3}>
+        <Paragraph>{paragraph3}</Paragraph>
+      </Accordion.Section>]
+  }
+}`,...(D=(L=s.parameters)==null?void 0:L.docs)==null?void 0:D.source}}};const Q=["Default","ExpandedByDefault","ReduceLandmarks"],ce=Object.freeze(Object.defineProperty({__proto__:null,Default:i,ExpandedByDefault:t,ReduceLandmarks:s,__namedExportsOrder:Q,default:J},Symbol.toStringTag,{value:"Module"}));export{ce as A,t as E,s as R};
