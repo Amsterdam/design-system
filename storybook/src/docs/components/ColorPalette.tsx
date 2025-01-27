b1e740d600b52edb987f60925fcc81f32e4e421e
Vincent Smedinga <v.smedinga@amsterdam.nl>
2025-01-27 11:22:46 +0100

    Remove temporary brand tokens

--- storybook/src/docs/components/ColorPalette.tsx
+++ storybook/src/docs/components/ColorPalette.tsx
@@ -13,34 +13,21 @@ const ColorPaletteRoot = forwardRef(({ children, ...restProps }: DivProps, ref: 
 
 ColorPaletteRoot.displayName = 'ColorPalette'
 
-type ColorPaletteSwatchProps = {
-  color: Record<'0' | '20' | '60' | '80' | '100', string>
+type ColorPaletteSwatchProps = PropsWithChildren<{
   name: string
-}
+}>
 
-const ColorPaletteSwatch = ({ color, name }: ColorPaletteSwatchProps) => (
+const ColorPaletteSwatch = ({ children, name }: ColorPaletteSwatchProps) => (
   <div className={clsx('ams-docs-color-swatch', `ams-docs-color-swatch--${name}`)}>
     <span className="ams-docs-color-name">{name}</span>
-    {name === 'neutral' ? (
-      <>
-        <ColorPaletteTile color={color['0']} level="0" />
-        <ColorPaletteTile color={color['20']} level="20" />
-        <ColorPaletteTile color={color['60']} level="60" />
-        <ColorPaletteTile color={color['100']} level="100" />
-      </>
-    ) : (
-      <>
-        <ColorPaletteTile color={color['60']} level="60" />
-        <ColorPaletteTile color={color['80']} level="80" />
-      </>
-    )}
+    {children}
   </div>
 )
 
 ColorPaletteSwatch.displayName = 'ColorPalette.Swatch'
 
 type ColorPaletteTileProps = {
-  color: string | undefined
+  color: string
   level: string
 }
 
