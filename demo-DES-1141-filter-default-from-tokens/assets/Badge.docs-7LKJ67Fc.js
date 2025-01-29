3e8d1d3c68b1a7e4b700860279d22aed0a804682
alimpens <37216945+alimpens@users.noreply.github.com>
2025-01-29 10:32:24 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@6d45b242cc58cee8a5063edb0bd30397ba42598b 🚀

---
+++ demo-DES-1141-filter-default-from-tokens/assets/Badge.docs-7LKJ67Fc.js
@@ -0,0 +1,18 @@
+import{j as n}from"./jsx-runtime-z8MfsBtr.js";import{useMDXComponents as e}from"./index-BAvqEBP-.js";import{M as i,e as s,P as a,f as m}from"./index-BtHMldEv.js";import{B as c}from"./Badge.stories-Dx1EH4OQ.js";import"./index-C9rmetsa.js";import"./index-D7uoVdV3.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-CuEqzU_8.js";import"./index-D-OUEn-9.js";import"./index-Cu4lwwaE.js";import"./index-NOh9rqHv.js";import"./index-ogSvIofg.js";import"./Badge-D8pd7uC1.js";import"./clsx-B-dksMZM.js";const p=`<!-- @license CC0-1.0 -->
+
+# Badge
+
+A prominently coloured box containing 1 or 2 words.
+Guides the user in taking a specific action or describes its surrounding content.
+
+## Design
+
+The badge can contain a short text or a number.
+Any colour from the ‘highlight’ and ‘feedback’ groups can be used as a background colour.
+The default is dark green.
+`;/*@license CC0-1.0*/function t(o){return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(i,{of:c}),`
+`,n.jsx(s,{children:p}),`
+`,n.jsx(a,{}),`
+`,n.jsx(m,{})]})}function w(o={}){const{wrapper:r}={...e(),...o.components};return r?n.jsx(r,{...o,children:n.jsx(t,{...o})}):t()}export{w as default};
