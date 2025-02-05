d77ac796b5df4197e1cd35d60bc61618a5eed715
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-02-05 14:59:20 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@94d8a0dbe5032aa1a48da21e771d7e397e60316d 🚀

---
+++ demo-DES-1169-tab-panel-overflow/assets/Tabs.docs-DMkKsiHs.js
@@ -0,0 +1,47 @@
+import{j as t}from"./jsx-runtime-z8MfsBtr.js";import{useMDXComponents as i}from"./index-BAvqEBP-.js";import{ae as s,aq as h,ar as r,at as a}from"./index-Idg3IMAj.js";import{T as c,W as l,a as p}from"./Tabs.stories-BkcwSpvA.js";import"./index-C9rmetsa.js";import"./index-D7uoVdV3.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-CNwJ6MtK.js";import"../sb-preview/runtime.js";import"./index-D-OUEn-9.js";import"./index-Cu4lwwaE.js";import"./index-NOh9rqHv.js";import"./index-ogSvIofg.js";import"./index.esm-BH242R5B.js";import"./clsx-B-dksMZM.js";import"./index.esm-X3U7dYaT.js";import"./exampleContent-B9IK-krr.js";import"./useKeyboardFocus-CRUFsA_C.js";const d=`<!-- @license CC0-1.0 -->
+
+# Tabs
+
+Tabs are used to bundle related content in a compact overview within a page. Each tab has a short name, and these names indicate the relationship between the information displayed in each tab.
+
+## Guidelines
+
+- The content of each tab is viewable independently, not in the context of one another.
+- The content within each tab should have a similar structure.
+- Use when there is limited visual space and content needs to be divided into sections.
+- Each tab consists of a button and a panel.
+  A \`tab\` prop with a corresponding value connects them.
+
+You can navigate tabs with your keyboard:
+
+| key            | element                                        |
+| :------------- | :--------------------------------------------- |
+| Enter or Space | Open or close the tab that has the focus       |
+| Tab            | Go to the next element that can have focus     |
+| Shift + Tab    | Go to the next element that can have focus     |
+| Left arrow     | If the tabs have focus: go to the previous tab |
+| Right arrow    | If the tabs have focus: go to the next tab     |
+| Home           | If the tabs have focus: go to the first tab    |
+| End            | If the tabs have focus, go to the last tab     |
+
+### Caution
+
+Do not use tabs if the content in each tab functions just as well on separate pages.
+
+## References
+
+- [W3C - Tabs Pattern](https://www.w3.org/WAI/ARIA/apg/patterns/tabs/)
+- [MDN - Tab Aria Roles](https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/tab_role)
+`;/*@license CC0-1.0*/function o(n){const e={h2:"h2",h3:"h3",p:"p",...i(),...n.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(s,{of:c}),`
+`,t.jsx(h,{children:d}),`
+`,t.jsx(r,{}),`
+`,t.jsx(e.h2,{id:"examples",children:"Examples"}),`
+`,t.jsx(e.h3,{id:"with-initial-tab",children:"With initial tab"}),`
+`,t.jsx(e.p,{children:`The first tab is active by default.
+Another tab’s panel can be displayed initially as well.`}),`
+`,t.jsx(a,{of:l}),`
+`,t.jsx(e.h3,{id:"with-wide-content",children:"With wide content"}),`
+`,t.jsx(e.p,{children:"The Tab Buttons list and the content within the active Tab Panel will scroll horizontally when their width exceeds the available space."}),`
+`,t.jsx(a,{of:p})]})}function D(n={}){const{wrapper:e}={...i(),...n.components};return e?t.jsx(e,{...n,children:t.jsx(o,{...n})}):o(n)}export{D as default};
