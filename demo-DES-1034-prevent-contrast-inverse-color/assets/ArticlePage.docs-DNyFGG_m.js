9708e342f74c7b27e2214d90be1fdf4ebca27fe9
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-02-07 12:18:56 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@14795bc2ceaa81487fc78a23d5f242653431485c 🚀

---
+++ demo-DES-1034-prevent-contrast-inverse-color/assets/ArticlePage.docs-DNyFGG_m.js
@@ -0,0 +1,5 @@
+import{j as t}from"./jsx-runtime-z8MfsBtr.js";import{useMDXComponents as m}from"./index-BAvqEBP-.js";import{M as n}from"./index-BM237G9L.js";import{A as e}from"./ArticlePage.stories-BOYeoaXs.js";import"./index-C9rmetsa.js";import"./index-D7uoVdV3.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-Cub9BXUq.js";import"./index-D-OUEn-9.js";import"./index-Cu4lwwaE.js";import"./index-NOh9rqHv.js";import"./index-ogSvIofg.js";import"./index.esm-IMHYXOZR.js";import"./clsx-B-dksMZM.js";import"./index.esm-CQm4RKjO.js";import"./Image.stories-TfbbU36E.js";import"./Image-CogrYt7V.js";import"./config-aSfbz_Mq.js";import"./Footer.stories-CX0Z0aKp.js";import"./PageMenu.stories-DfyCg7u2.js";import"./Icon-BPwTIPQC.js";/*@license CC0-1.0*/function i(o){const r={h1:"h1",p:"p",...m(),...o.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(n,{of:e}),`
+`,t.jsx(r.h1,{id:"article-page",children:"Article page"}),`
+`,t.jsx(r.p,{children:"This page type is for news or similar kinds of articles."})]})}function E(o={}){const{wrapper:r}={...m(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(i,{...o})}):i(o)}export{E as default};
--- demo-DES-1034-prevent-contrast-inverse-color/assets/FormPage.docs-Hr6q6IzA.js
+++
@@ -1,5 +0,0 @@
-import{j as o}from"./jsx-runtime-z8MfsBtr.js";import{useMDXComponents as i}from"./index-BAvqEBP-.js";import{M as e}from"./index-wroGQ5tU.js";import{F as n}from"./FormPage.stories-C6Fde5wc.js";import"./index-C9rmetsa.js";import"./index-D7uoVdV3.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-Cn6aGhu7.js";import"./index-D-OUEn-9.js";import"./index-Cu4lwwaE.js";import"./index-NOh9rqHv.js";import"./index-ogSvIofg.js";import"./index.esm-IMHYXOZR.js";import"./clsx-B-dksMZM.js";import"./index.esm-CQm4RKjO.js";import"./config-C7Xh1cj8.js";import"./Footer.stories-DtM4OCOo.js";import"./PageMenu.stories-qZD2O8Wg.js";import"./Icon-BcunPd3u.js";/*@license CC0-1.0*/function m(t){const r={h1:"h1",p:"p",...i(),...t.components};return o.jsxs(o.Fragment,{children:[`
-`,`
-`,o.jsx(e,{of:n}),`
-`,o.jsx(r.h1,{id:"form-page",children:"Form page"}),`
-`,o.jsx(r.p,{children:"This simple example presents various types of fields."})]})}function v(t={}){const{wrapper:r}={...i(),...t.components};return r?o.jsx(r,{...t,children:o.jsx(m,{...t})}):m(t)}export{v as default};
