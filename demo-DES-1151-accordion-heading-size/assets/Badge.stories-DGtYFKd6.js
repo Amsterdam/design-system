1328d2ea68efb6f68fa3f737076dda3a51bdae6e
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-02-07 09:51:46 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@81aa7045f07df5eee1201dc9fad494b21cbdc19c 🚀

---
+++ demo-DES-1151-accordion-heading-size/assets/Badge.stories-DGtYFKd6.js
@@ -0,0 +1,4 @@
+import{b as t,B as s}from"./Badge-D8pd7uC1.js";/**
+ * @license EUPL-1.2+
+ * Copyright Gemeente Amsterdam
+ */const n={title:"Components/Feedback/Badge",component:s,args:{label:"Tip"},argTypes:{color:{control:{labels:{undefined:"dark-green (default)"}},options:[void 0,...t]}}},e={};var o,r,a;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:"{}",...(a=(r=e.parameters)==null?void 0:r.docs)==null?void 0:a.source}}};const d=["Default"],c=Object.freeze(Object.defineProperty({__proto__:null,Default:e,__namedExportsOrder:d,default:n},Symbol.toStringTag,{value:"Module"}));export{c as B};
