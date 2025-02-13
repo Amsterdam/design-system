ac423fcf035a5304c9fbc5e0cd6da903974f8582
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-02-13 20:02:24 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@9dec8a31731247e842bed1afe51ffbaf5546ed5b 🚀

---
+++ demo-DES-873-badge-color-names/assets/Badge.docs-gTPZ0NhF.js
@@ -0,0 +1,22 @@
+import{j as e}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as r}from"./index-1Ha5aU2j.js";import{M as i,e as s,P as a,f as d}from"./index-Dq_7M8zD.js";import{B as c}from"./Badge.stories-B_7A3Xv6.js";import"./index-yBjzXJbu.js";import"./index-C0MU9AHG.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-D_Xm3wUP.js";import"./index-IE9ObSwG.js";import"./index-Cu4lwwaE.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./Badge-CLv5sYRh.js";import"./clsx-B-dksMZM.js";const u=`<!-- @license CC0-1.0 -->
+
+# Badge
+
+A prominently coloured box containing 1 or 2 words.
+Guides the user in taking a specific action or describes its surrounding content.
+
+## Design
+
+- The badge can contain a short text or a number.
+- Any [colour](/docs/brand-design-tokens-colour--docs) from the ‘highlight’ and ‘feedback’ groups can be used for the background.
+  The default is green.
+  The design system does not define a meaning to any of these background colours, although it makes sense to use green and red for positive and negative feedback.
+- The default text colour (black) is used on an azure, lime, orange or yellow background.
+  The inverse text colour (white) is used on a green, magenta, red or purple background.
+- Blue is not an option because it is used exclusively to indicate interactivity.
+`;/*@license CC0-1.0*/function t(n){return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(i,{of:c}),`
+`,e.jsx(s,{children:u}),`
+`,e.jsx(a,{}),`
+`,e.jsx(d,{})]})}function w(n={}){const{wrapper:o}={...r(),...n.components};return o?e.jsx(o,{...n,children:e.jsx(t,{...n})}):t()}export{w as default};
