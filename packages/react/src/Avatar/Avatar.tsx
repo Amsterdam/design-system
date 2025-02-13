795c9baebee69998f49e60f6c480240080af8858
Vincent Smedinga <v.smedinga@amsterdam.nl>
2025-02-13 18:20:54 +0100

    Rename ‘light blue’ to ‘azure’ for Avatar

--- packages/react/src/Avatar/Avatar.tsx
+++ packages/react/src/Avatar/Avatar.tsx
@@ -9,7 +9,7 @@ import { forwardRef } from 'react'
 import type { ForwardedRef, HTMLAttributes } from 'react'
 import { Icon } from '../Icon'
 
-export const avatarColors = ['green', 'light-blue', 'lime', 'magenta', 'orange', 'yellow'] as const
+export const avatarColors = ['azure', 'green', 'lime', 'magenta', 'orange', 'yellow'] as const
 
 type AvatarColor = (typeof avatarColors)[number]
 
