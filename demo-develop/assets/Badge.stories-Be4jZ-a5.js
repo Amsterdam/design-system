9b38c68a4628c9ec007e847b6c32ac40c3299c63
alimpens <37216945+alimpens@users.noreply.github.com>
2025-02-14 14:17:40 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@96777889fe805ca23e65ff689ccb89c147f0668b 🚀

---
+++ demo-develop/assets/Badge.stories-Be4jZ-a5.js
@@ -0,0 +1,4 @@
+import{b as t,B as s}from"./Badge-CXuM8o0u.js";/**
+ * @license EUPL-1.2+
+ * Copyright Gemeente Amsterdam
+ */const l={title:"Components/Feedback/Badge",component:s,args:{label:"Tip"},argTypes:{color:{control:{labels:{undefined:"green (default)"}},options:[void 0,...t]}}},e={};var o,r,a;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:"{}",...(a=(r=e.parameters)==null?void 0:r.docs)==null?void 0:a.source}}};const n=["Default"],c=Object.freeze(Object.defineProperty({__proto__:null,Default:e,__namedExportsOrder:n,default:l},Symbol.toStringTag,{value:"Module"}));export{c as B};
