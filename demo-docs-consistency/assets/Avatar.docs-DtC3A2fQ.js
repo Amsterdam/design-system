3504321bbb5f71b42f52d6773bd8951941d210a3
alimpens <37216945+alimpens@users.noreply.github.com>
2025-02-14 12:46:48 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@bc1ea876bfd69de12813201d230bb724c4e4118c 🚀

---
+++ demo-docs-consistency/assets/Avatar.docs-DtC3A2fQ.js
@@ -0,0 +1,31 @@
+import{j as e}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as t}from"./index-1Ha5aU2j.js";import{M as i,e as s,P as l,f as c,h as a}from"./index-BTBCaJxj.js";import{A as p,W as h,F as m}from"./Avatar.stories-BTXD3kE5.js";import"./index-yBjzXJbu.js";import"./index-C0MU9AHG.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-31BLB8_u.js";import"./index-IE9ObSwG.js";import"./index-Cu4lwwaE.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index.esm-DvoLfDtE.js";import"./clsx-B-dksMZM.js";import"./Icon-plsGHcLz.js";const d=`<!-- @license CC0-1.0 -->
+
+# Avatar
+
+A circular badge representing a person.
+
+## Design
+
+The avatar contains 1 or 2 initial letters from the person’s full name, a picture, or a generic icon.
+To personalize the avatar, the user can be allowed to choose one of the highlight colours.
+The default background colour is purple.
+
+## Guidelines
+
+- Show an avatar for the user of the application,
+  or to associate a person with a content item.
+`;/*@license CC0-1.0*/function r(o){const n={h2:"h2",h3:"h3",p:"p",...t(),...o.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(i,{of:p}),`
+`,e.jsx(s,{children:d}),`
+`,e.jsx(l,{}),`
+`,e.jsx(c,{}),`
+`,e.jsx(n.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(n.h3,{id:"with-image",children:"With image"}),`
+`,e.jsx(n.p,{children:`The Avatar can also display a photo or other image for the person.
+Make sure to scale the image down to around 100 pixels to prevent unnecessary data transfers.
+The center of the image will be visible; provide a square image if possible.`}),`
+`,e.jsx(a,{of:h}),`
+`,e.jsx(n.h3,{id:"fallback-icon",children:"Fallback icon"}),`
+`,e.jsx(n.p,{children:"A user icon displays if no label and image are provided."}),`
+`,e.jsx(a,{of:m})]})}function E(o={}){const{wrapper:n}={...t(),...o.components};return n?e.jsx(n,{...o,children:e.jsx(r,{...o})}):r(o)}export{E as default};
