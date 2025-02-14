9f2c93d64881a61deee3c0d1ae6dc78eb2514fbb
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-02-14 10:39:19 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@06e43403d764b620c9c2f66c89759e12b9f8c4bc 🚀

---
+++ demo-DES-873-badge-color-names/assets/MegaMenu.docs-DGzwDQkk.js
@@ -0,0 +1,12 @@
+import{j as t}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as r}from"./index-1Ha5aU2j.js";import{M as s,e as i,P as a,h as m}from"./index-BfNwl7TB.js";import{M as p,a as c}from"./MegaMenu.stories-qnsvoVzE.js";import{S as d}from"./StatusBadge-BqUNxUUi.js";import"./index-yBjzXJbu.js";import"./index-C0MU9AHG.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-BnRaektM.js";import"./index-IE9ObSwG.js";import"./index-Cu4lwwaE.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index.esm-C56IaLsI.js";import"./clsx-B-dksMZM.js";import"./index.esm-DvoLfDtE.js";import"./Badge-CXuM8o0u.js";const x=`<!-- @license CC0-1.0 -->
+
+# Mega Menu
+`;/*@license CC0-1.0*/function o(e){const n={h2:"h2",h3:"h3",...r(),...e.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(s,{of:p}),`
+`,t.jsx(d,{status:"deprecated",description:"Use child components in Header instead."}),`
+`,t.jsx(i,{children:x}),`
+`,t.jsx(a,{}),`
+`,t.jsx(n.h2,{id:"examples",children:"Examples"}),`
+`,t.jsx(n.h3,{id:"with-categories",children:"With categories"}),`
+`,t.jsx(m,{of:c})]})}function k(e={}){const{wrapper:n}={...r(),...e.components};return n?t.jsx(n,{...e,children:t.jsx(o,{...e})}):o(e)}export{k as default};
