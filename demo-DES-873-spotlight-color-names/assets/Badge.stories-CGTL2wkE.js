b95faa298c3986d1e98d4b6a147bbf9767755e93
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-02-14 12:14:00 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@3b0708f25440875e6f3dc1f7f104d811190e06a4 🚀

---
+++ demo-DES-873-spotlight-color-names/assets/Badge.stories-CGTL2wkE.js
@@ -0,0 +1,4 @@
+import{b as t,B as s}from"./Badge-DZaiFXLX.js";/**
+ * @license EUPL-1.2+
+ * Copyright Gemeente Amsterdam
+ */const d={title:"Components/Feedback/Badge",component:s,args:{label:"Tip"},argTypes:{color:{control:{labels:{undefined:"dark-green (default)"}},options:[void 0,...t]}}},e={};var o,r,a;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:"{}",...(a=(r=e.parameters)==null?void 0:r.docs)==null?void 0:a.source}}};const l=["Default"],c=Object.freeze(Object.defineProperty({__proto__:null,Default:e,__namedExportsOrder:l,default:d},Symbol.toStringTag,{value:"Module"}));export{c as B};
