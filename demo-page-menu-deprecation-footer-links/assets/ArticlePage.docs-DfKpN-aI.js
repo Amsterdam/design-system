5fce0914afc0f1ff746feaa949725e88e80bbcfc
dlnr <niels@dlnr.dev>
2025-02-14 10:02:18 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@a3bda64f1575b6beac7baf088afda87798aa6b7d 🚀

---
+++ demo-page-menu-deprecation-footer-links/assets/ArticlePage.docs-DfKpN-aI.js
@@ -0,0 +1,5 @@
+import{j as t}from"./jsx-runtime-z8MfsBtr.js";import{useMDXComponents as n}from"./index-BAvqEBP-.js";import{M as e}from"./index-f_tFj7CU.js";import{A as m}from"./ArticlePage.stories-DXxWN9N1.js";import"./index-C9rmetsa.js";import"./index-D7uoVdV3.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-2s1fQP0l.js";import"./index-wgz3LQUQ.js";import"./index-Cu4lwwaE.js";import"./index-Brs1icLk.js";import"./index-ogSvIofg.js";import"./index.esm-C0N9iXLH.js";import"./clsx-B-dksMZM.js";import"./index.esm-P1wEHiuM.js";import"./Image.stories-TfbbU36E.js";import"./Image-CogrYt7V.js";import"./config-B6tF_9kx.js";import"./Footer.stories-CueFn-7H.js";import"./Icon-BPwTIPQC.js";/*@license CC0-1.0*/function i(o){const r={h1:"h1",p:"p",...n(),...o.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(e,{of:m}),`
+`,t.jsx(r.h1,{id:"article-page",children:"Article page"}),`
+`,t.jsx(r.p,{children:"This page type is for news or similar kinds of articles."})]})}function k(o={}){const{wrapper:r}={...n(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(i,{...o})}):i(o)}export{k as default};
---
+++ demo-page-menu-deprecation-footer-links/assets/Paragraph.stories-Bt25H0-y.js
@@ -0,0 +1,16 @@
+import{b as S}from"./exampleContent-fW-Djhm4.js";import{P as h}from"./Paragraph-CaR3Zb7F.js";/**
+ * @license EUPL-1.2+
+ * Copyright Gemeente Amsterdam
+ */const x=S(),b={title:"Components/Text/Paragraph",component:h,args:{children:x},argTypes:{children:{description:"The paragraph text.",table:{disable:!1}},color:{control:{labels:{undefined:"default"},type:"radio"},options:[void 0,"inverse"]},size:{control:{labels:{large:"large",small:"small",undefined:"medium"},type:"radio"},options:["small",void 0,"large"]}}},e={},r={args:{size:"large"}},a={args:{size:"small"}},s={args:{color:"inverse"}};var o,t,n;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:"{}",...(n=(t=e.parameters)==null?void 0:t.docs)==null?void 0:n.source}}};var l,c,p;r.parameters={...r.parameters,docs:{...(l=r.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    size: 'large'
+  }
+}`,...(p=(c=r.parameters)==null?void 0:c.docs)==null?void 0:p.source}}};var i,m,d;a.parameters={...a.parameters,docs:{...(i=a.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    size: 'small'
+  }
+}`,...(d=(m=a.parameters)==null?void 0:m.docs)==null?void 0:d.source}}};var g,u,f;s.parameters={...s.parameters,docs:{...(g=s.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    color: 'inverse'
+  }
+}`,...(f=(u=s.parameters)==null?void 0:u.docs)==null?void 0:f.source}}};const T=["Default","LargeText","SmallText","InverseColour"],z=Object.freeze(Object.defineProperty({__proto__:null,Default:e,InverseColour:s,LargeText:r,SmallText:a,__namedExportsOrder:T,default:b},Symbol.toStringTag,{value:"Module"}));export{s as I,r as L,z as P,a as S};
