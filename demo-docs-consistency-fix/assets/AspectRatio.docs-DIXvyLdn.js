ca4686b196ebb27da390c1a270d4dfce638e9053
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-02-14 19:28:02 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@250c906552849aa89f6b2a21332b79e5692234a8 🚀

---
+++ demo-docs-consistency-fix/assets/AspectRatio.docs-DIXvyLdn.js
@@ -0,0 +1,30 @@
+import{j as a}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as o}from"./index-1Ha5aU2j.js";import{M as n,e as i,P as m,f as r}from"./index-D4KnIFYl.js";import{A as c}from"./AspectRatio.stories-CgttgCaT.js";import"./index-yBjzXJbu.js";import"./index-C0MU9AHG.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-DH89M2Kc.js";import"./index-IE9ObSwG.js";import"./index-Cu4lwwaE.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";const p=`<!-- @license CC0-1.0 -->
+
+# Aspect Ratio
+
+Constrains media content to a supported aspect ratio.
+
+## Class names
+
+Each available aspect ratio has an associated class name.
+The class can be applied to any component or element.
+
+| Class name                  | Example                                                                     |
+| :-------------------------- | :-------------------------------------------------------------------------- |
+| \`ams-aspect-ratio--x-tall\`  | <div className="ams-docs-token-example--space ams-aspect-ratio--x-tall" />  |
+| \`ams-aspect-ratio--tall\`    | <div className="ams-docs-token-example--space ams-aspect-ratio--tall" />    |
+| \`ams-aspect-ratio--square\`  | <div className="ams-docs-token-example--space ams-aspect-ratio--square" />  |
+| \`ams-aspect-ratio--wide\`    | <div className="ams-docs-token-example--space ams-aspect-ratio--wide" />    |
+| \`ams-aspect-ratio--x-wide\`  | <div className="ams-docs-token-example--space ams-aspect-ratio--x-wide" />  |
+| \`ams-aspect-ratio--2x-wide\` | <div className="ams-docs-token-example--space ams-aspect-ratio--2x-wide" /> |
+
+## Guidelines
+
+- Apply one of these classes to an image, video or other media content to constrain its dimensions to one of the available aspect ratios.
+- The default aspect ratio is 16:9.
+`;/*@license CC0-1.0*/function t(s){return a.jsxs(a.Fragment,{children:[`
+`,`
+`,a.jsx(n,{of:c}),`
+`,a.jsx(i,{children:p}),`
+`,a.jsx(m,{}),`
+`,a.jsx(r,{})]})}function N(s={}){const{wrapper:e}={...o(),...s.components};return e?a.jsx(e,{...s,children:a.jsx(t,{...s})}):t()}export{N as default};
