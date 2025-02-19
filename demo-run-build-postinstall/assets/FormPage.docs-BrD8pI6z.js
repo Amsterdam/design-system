5345f4aa0879decc49adbe83f85394e1ff18e083
GitHub <github-action@users.noreply.github.com>
2025-02-19 12:22:49 +0000

    Remove feature branch folder

--- demo-run-build-postinstall/assets/FormPage.docs-BrD8pI6z.js
+++
@@ -1,5 +0,0 @@
-import{j as o}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as e}from"./index-1Ha5aU2j.js";import{M as n}from"./index-CO8CyW6t.js";import{F as i}from"./FormPage.stories-Dsk2TmGy.js";import"./index-yBjzXJbu.js";import"./index-C0MU9AHG.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-JUNjKWRL.js";import"./index-IE9ObSwG.js";import"./index-Cu4lwwaE.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index.esm-HS628HCY.js";import"./clsx-B-dksMZM.js";import"./index.esm-DvoLfDtE.js";import"./config-PHXFFSOB.js";import"./Footer.stories-DrNH64pr.js";/*@license CC0-1.0*/function m(t){const r={h1:"h1",p:"p",...e(),...t.components};return o.jsxs(o.Fragment,{children:[`
-`,`
-`,o.jsx(n,{of:i}),`
-`,o.jsx(r.h1,{id:"form-page",children:"Form page"}),`
-`,o.jsx(r.p,{children:"This simple example presents various types of fields."})]})}function y(t={}){const{wrapper:r}={...e(),...t.components};return r?o.jsx(r,{...t,children:o.jsx(m,{...t})}):m(t)}export{y as default};
