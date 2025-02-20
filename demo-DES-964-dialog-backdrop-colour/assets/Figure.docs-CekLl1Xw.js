2525d8ec1b7b82ad147e089e06afc75ed53e720a
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-02-20 15:09:11 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@ff01477b3b943e5e13146915a5cf5bc146a9c381 🚀

---
+++ demo-DES-964-dialog-backdrop-colour/assets/Figure.docs-CekLl1Xw.js
@@ -0,0 +1,15 @@
+import{j as o}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as t}from"./index-1Ha5aU2j.js";import{M as s,e as i,P as c,h as m}from"./index-CODj3wa2.js";import{F as a,I as p}from"./Figure.stories-BgL8N6J2.js";import"./index-yBjzXJbu.js";import"./index-C0MU9AHG.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-9FVDydvU.js";import"./index-IE9ObSwG.js";import"./index-Cu4lwwaE.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index.esm-Dx8ItK6R.js";import"./clsx-B-dksMZM.js";import"./index.esm-DvoLfDtE.js";import"./exampleContent-PGFPgakh.js";const h=`<!-- @license CC0-1.0 -->
+
+# Figure
+
+Groups media content with a caption that describes it.
+`;/*@license CC0-1.0*/function r(n){const e={code:"code",h2:"h2",h3:"h3",p:"p",...t(),...n.components};return o.jsxs(o.Fragment,{children:[`
+`,`
+`,o.jsx(s,{of:a}),`
+`,o.jsx(i,{children:h}),`
+`,o.jsx(c,{}),`
+`,o.jsx(e.h2,{id:"examples",children:"Examples"}),`
+`,o.jsx(e.h3,{id:"inverse-colour",children:"Inverse colour"}),`
+`,o.jsxs(e.p,{children:["Set the ",o.jsx(e.code,{children:"color"})," prop to ",o.jsx(e.code,{children:"inverse"}),` if the Figure Caption sits on a dark background.
+This ensures the colour of the text provides enough contrast.`]}),`
+`,o.jsx(m,{of:p})]})}function X(n={}){const{wrapper:e}={...t(),...n.components};return e?o.jsx(e,{...n,children:o.jsx(r,{...n})}):r(n)}export{X as default};
