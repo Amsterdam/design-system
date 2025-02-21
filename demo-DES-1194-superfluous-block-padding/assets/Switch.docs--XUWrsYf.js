283e5e821ac49a00b45a719a75509d56a88376d6
RubenSibon <mail@rubensibon.nl>
2025-02-21 13:38:22 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@41ac498e87593e8a9d30913edc5c315226bd185e 🚀

---
+++ demo-DES-1194-superfluous-block-padding/assets/Switch.docs--XUWrsYf.js
@@ -0,0 +1,33 @@
+import{j as e}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as s}from"./index-1Ha5aU2j.js";import{M as o,e as r,P as a,f as c,h as l}from"./index-BjqQ2Tvv.js";import{S as h,W as m}from"./Switch.stories-BEp1TxGv.js";import"./index-yBjzXJbu.js";import"./index-C0MU9AHG.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-u81qHW9Y.js";import"./index-IE9ObSwG.js";import"./index-Cu4lwwaE.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index.esm-HS628HCY.js";import"./clsx-B-dksMZM.js";import"./index.esm-DvoLfDtE.js";const p=`<!-- @license CC0-1.0 -->
+
+# Switch
+
+A switch is a control element to toggle between two different states quickly.
+A switch applies to a page or the entire system, such as an on/off switch.
+
+## Guidelines
+
+- A Switch must have a label, and in most cases, this label should be visible.
+- Switch only between two different states.
+  It is a binary action.
+- The action takes place immediately when the user operates the switch.
+
+## Relevant WCAG requirements
+
+- [WCAG 1.3.5](https://www.w3.org/WAI/WCAG21/Understanding/identify-input-purpose.html): it is both clear for a user and programmatically what the purpose of a form field is.
+
+Switch is an interactive element, and the [general requirements and guidelines for interactivity](/docs/docs-developer-guide-interactivity--docs) apply.
+
+## References
+
+- [Switch Pattern](https://www.w3.org/WAI/ARIA/apg/patterns/switch/)
+- [Role switch](https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/switch_role)
+`;/*@license CC0-1.0*/function i(t){const n={h2:"h2",h3:"h3",...s(),...t.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(o,{of:h}),`
+`,e.jsx(r,{children:p}),`
+`,e.jsx(a,{}),`
+`,e.jsx(c,{}),`
+`,e.jsx(n.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(n.h3,{id:"with-label",children:"With Label"}),`
+`,e.jsx(l,{of:m})]})}function R(t={}){const{wrapper:n}={...s(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(i,{...t})}):i(t)}export{R as default};
