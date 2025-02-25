3512db5fe78d3c6db46352939b47feba355d847a
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-02-25 09:54:12 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@fb363cb8db031a2bd5e389d03693d1ab45629575 🚀

---
+++ demo-github_actions/JamesIves/github-pages-deploy-action-4.7.3/assets/Paragraph.stories-BAAOtTlf.js
@@ -0,0 +1,16 @@
+import{b as S}from"./exampleContent-PGFPgakh.js";import{P as h}from"./Paragraph-Dg4TdYBs.js";/**
+ * @license EUPL-1.2+
+ * Copyright Gemeente Amsterdam
+ */const x=S(),b={title:"Components/Text/Paragraph",component:h,args:{children:x},argTypes:{children:{description:"The paragraph text.",table:{disable:!1}},color:{control:{labels:{undefined:"default"},type:"radio"},options:[void 0,"inverse"]},size:{control:{labels:{large:"large",small:"small",undefined:"medium"},type:"radio"},options:["small",void 0,"large"]}}},e={},r={args:{size:"large"}},a={args:{size:"small"}},s={args:{color:"inverse"}};var o,t,l;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:"{}",...(l=(t=e.parameters)==null?void 0:t.docs)==null?void 0:l.source}}};var n,c,p;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    size: 'large'
+  }
+}`,...(p=(c=r.parameters)==null?void 0:c.docs)==null?void 0:p.source}}};var i,m,d;a.parameters={...a.parameters,docs:{...(i=a.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    size: 'small'
+  }
+}`,...(d=(m=a.parameters)==null?void 0:m.docs)==null?void 0:d.source}}};var g,u,f;s.parameters={...s.parameters,docs:{...(g=s.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    color: 'inverse'
+  }
+}`,...(f=(u=s.parameters)==null?void 0:u.docs)==null?void 0:f.source}}};const v=["Default","LargeText","SmallText","InverseColour"],z=Object.freeze(Object.defineProperty({__proto__:null,Default:e,InverseColour:s,LargeText:r,SmallText:a,__namedExportsOrder:v,default:b},Symbol.toStringTag,{value:"Module"}));export{s as I,r as L,z as P,a as S};
