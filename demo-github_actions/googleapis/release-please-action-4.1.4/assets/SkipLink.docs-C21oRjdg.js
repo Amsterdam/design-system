0da382f08bd71e28df545f4837ec43e9cc207064
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-03-03 13:57:10 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@87e012785e9758889de6e7519e8fc034d18a3cd5 🚀

---
+++ demo-github_actions/googleapis/release-please-action-4.1.4/assets/SkipLink.docs-C21oRjdg.js
@@ -0,0 +1,34 @@
+import{j as n}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as s}from"./index-1Ha5aU2j.js";import{M as a,e as r,P as p,f as l,h as t}from"./index-DtC6oq0t.js";import{S as h,O as m,M as c}from"./SkipLink.stories-kntd5eZo.js";import"./index-yBjzXJbu.js";import"./index-C0MU9AHG.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-B99SM7FC.js";import"./index-fNjTmf9T.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index.esm-B-KcSkYB.js";import"./clsx-B-dksMZM.js";import"./index.esm-DvoLfDtE.js";const d=`<!-- @license CC0-1.0 -->
+
+# Skip Link
+
+Allows skipping past recurring navigation blocks at the top of a page.
+
+## Design
+
+The Skip Link sits above the header and is as wide as the Screen container.
+It remains hidden until activated with the ‘Tab’ key.
+After appearing, it pushes the entire page down.
+
+## Guidelines
+
+- Place the Skip Link as the first element in the Screen.
+- Target the Skip Link to the main content.
+  On an article page, for example, it could be the title of the article.
+  On a search page, it could be the search field.
+- Set \`id="example-id"\` on the container of that element and then use \`href="#example-id"\` on the Skip Link.
+- Complex pages with multiple sections may benefit from more than one Skip Link.
+- Skip Links are unnecessary on a simple page with only text and minimal navigation.
+`;/*@license CC0-1.0*/function o(i){const e={h2:"h2",h3:"h3",p:"p",...s(),...i.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(a,{of:h}),`
+`,n.jsx(r,{children:d}),`
+`,n.jsx(p,{}),`
+`,n.jsx(l,{}),`
+`,n.jsx(e.h2,{id:"examples",children:"Examples"}),`
+`,n.jsx(e.h3,{id:"display-on-focus",children:"Display on focus"}),`
+`,n.jsx(e.p,{children:"A Skip Link is only displayed when it receives focus."}),`
+`,n.jsx(t,{of:m}),`
+`,n.jsx(e.h3,{id:"multiple-links",children:"Multiple links"}),`
+`,n.jsx(e.p,{children:"You can use more than one Skip Link if you have a complex page with multiple sections."}),`
+`,n.jsx(t,{of:c})]})}function D(i={}){const{wrapper:e}={...s(),...i.components};return e?n.jsx(e,{...i,children:n.jsx(o,{...i})}):o(i)}export{D as default};
---
+++ demo-github_actions/googleapis/release-please-action-4.1.4/assets/types-BUn1ClL5.js
@@ -0,0 +1,1 @@
+const e=["start","center","baseline","end"],s=e.filter(n=>n!=="baseline"),t=["center","end","between","around","evenly"];export{s as a,e as c,t as m};
