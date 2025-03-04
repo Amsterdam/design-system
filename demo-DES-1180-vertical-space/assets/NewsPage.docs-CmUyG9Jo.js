db539abdee9aed8c5a7ae72b24f5966715aef304
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-03-04 12:44:56 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@30a5c33667a78f2d1010a2ccc688bf50cfb59b26 🚀

---
+++ demo-DES-1180-vertical-space/assets/NewsPage.docs-CmUyG9Jo.js
@@ -0,0 +1,5 @@
+import{j as t}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as n}from"./index-1Ha5aU2j.js";import{M as i}from"./index-ByVZXHeW.js";import{N as m}from"./NewsPage.stories-CmNcuOu7.js";import"./index-yBjzXJbu.js";import"./index-C0MU9AHG.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-C3RgTgYz.js";import"./index-fNjTmf9T.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index.esm-Bbl-Ydi0.js";import"./clsx-B-dksMZM.js";import"./index.esm-DvoLfDtE.js";import"./config-yBWnR1u4.js";import"./Footer.stories-PmGKTnx5.js";/*@license CC0-1.0*/function e(o){const r={h1:"h1",p:"p",...n(),...o.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(i,{of:m}),`
+`,t.jsx(r.h1,{id:"news-page",children:"News page"}),`
+`,t.jsx(r.p,{children:"Testing vertical space here."})]})}function N(o={}){const{wrapper:r}={...n(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(e,{...o})}):e(o)}export{N as default};
