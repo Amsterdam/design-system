39122d1bdf9851579ebbc49230c93ce4ca219413
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-03-04 22:38:09 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@8a8d87c80b4c7c9fe7dcb2dc586b8ad3ccedbcc5 🚀

---
+++ demo-DES-1183-icon-alignment/assets/FileList.docs-rK6pzxRy.js
@@ -0,0 +1,55 @@
+import{j as e}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as s}from"./index-1Ha5aU2j.js";import{M as o,e as r,P as l,h as a,S as f}from"./index-sRgmtw2z.js";import{F as m,W as p}from"./FileList.stories-BeSywEIu.js";import"./index-yBjzXJbu.js";import"./index-C0MU9AHG.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-DPrmKV_K.js";import"./index-fNjTmf9T.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index.esm-B-KcSkYB.js";import"./clsx-B-dksMZM.js";import"./index.esm-DvoLfDtE.js";import"./Icon-CJVFfJZC.js";import"./Button-CjSAP85z.js";const u=`import { FileInput, FileList } from '@amsterdam/design-system-react'
+import { useRef, useState } from 'react'
+
+export const FileInputWithFileList = () => {
+  const inputRef = useRef<HTMLInputElement>(null)
+  const [files, setFiles] = useState<FileList | null>(null)
+
+  const changeFiles = () => {
+    if (inputRef.current) {
+      setFiles(inputRef.current.files)
+    }
+  }
+
+  const removeFile = (index: number) => {
+    if (files) {
+      const newFiles = new DataTransfer()
+      Array.from(files).forEach((file, i) => {
+        if (i !== index) newFiles.items.add(file)
+      })
+      if (inputRef.current) {
+        inputRef.current.files = newFiles.files
+      }
+      setFiles(newFiles.files)
+    }
+  }
+
+  return (
+    <>
+      <FileInput multiple onChange={changeFiles} ref={inputRef} />
+      {files && (
+        <FileList>
+          {Array.from(files).map((file, index) => (
+            <FileList.Item file={file} key={index} onDelete={() => removeFile(index)} />
+          ))}
+        </FileList>
+      )}
+    </>
+  )
+}
+`,c=`<!-- @license CC0-1.0 -->
+
+# File List
+
+An overview of files, showing their name, type, size, and a preview.
+`;/*@license CC0-1.0*/function t(i){const n={code:"code",h2:"h2",h3:"h3",p:"p",...s(),...i.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(o,{of:m}),`
+`,e.jsx(r,{children:c}),`
+`,e.jsx(l,{}),`
+`,e.jsx(n.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(n.h3,{id:"using-a-file-input",children:"Using a file input"}),`
+`,e.jsxs(n.p,{children:["To connect a File Input to a File List, use the ",e.jsx(n.code,{children:"onChange"}),` event to update the
+list of files and use `,e.jsx(n.code,{children:"onDelete"})," when removing a file from the list."]}),`
+`,e.jsx(a,{of:p}),`
+`,e.jsx(f,{code:u,language:"jsx"})]})}function S(i={}){const{wrapper:n}={...s(),...i.components};return n?e.jsx(n,{...i,children:e.jsx(t,{...i})}):t(i)}export{S as default};
