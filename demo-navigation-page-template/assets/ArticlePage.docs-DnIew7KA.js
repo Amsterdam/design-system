72f7853e7105528e99dbcaab03cd951af35b13f8
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-03-05 23:00:02 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@f9582b43068f172a748bd7110a040c694b9fbc57 🚀

--- demo-navigation-page-template/assets/ArticlePage.docs-DnIew7KA.js
+++
@@ -1,5 +0,0 @@
-import{j as t}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as n}from"./index-1Ha5aU2j.js";import{M as e}from"./index-BklzyIVq.js";import{A as m}from"./ArticlePage.stories-BJO7L_Lz.js";import"./index-yBjzXJbu.js";import"./index-C0MU9AHG.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-DGSsuWC2.js";import"./index-fNjTmf9T.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index.esm-B7Zto0Yz.js";import"./clsx-B-dksMZM.js";import"./index.esm-9B8hlNX4.js";import"./Image.stories-1nLEKApt.js";import"./Image-n37fLCx2.js";import"./config-B3a1_fOj.js";import"./Footer.stories-B-k21Cto.js";/*@license CC0-1.0*/function i(o){const r={h1:"h1",p:"p",...n(),...o.components};return t.jsxs(t.Fragment,{children:[`
-`,`
-`,t.jsx(e,{of:m}),`
-`,t.jsx(r.h1,{id:"article-page",children:"Article page"}),`
-`,t.jsx(r.p,{children:"This page type is for news or similar kinds of articles."})]})}function y(o={}){const{wrapper:r}={...n(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(i,{...o})}):i(o)}export{y as default};
--- demo-navigation-page-template/assets/FormPage.docs-DS5pJBc1.js
+++
@@ -1,5 +0,0 @@
-import{j as o}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as m}from"./index-1Ha5aU2j.js";import{M as n}from"./index-BklzyIVq.js";import{F as i}from"./FormPage.stories-Uk3jM25o.js";import"./index-yBjzXJbu.js";import"./index-C0MU9AHG.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-DGSsuWC2.js";import"./index-fNjTmf9T.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index.esm-B7Zto0Yz.js";import"./clsx-B-dksMZM.js";import"./index.esm-9B8hlNX4.js";import"./config-B3a1_fOj.js";import"./Footer.stories-B-k21Cto.js";/*@license CC0-1.0*/function e(t){const r={h1:"h1",p:"p",...m(),...t.components};return o.jsxs(o.Fragment,{children:[`
-`,`
-`,o.jsx(n,{of:i}),`
-`,o.jsx(r.h1,{id:"form-page",children:"Form page"}),`
-`,o.jsx(r.p,{children:"This simple example presents various types of fields."})]})}function X(t={}){const{wrapper:r}={...m(),...t.components};return r?o.jsx(r,{...t,children:o.jsx(e,{...t})}):e(t)}export{X as default};
