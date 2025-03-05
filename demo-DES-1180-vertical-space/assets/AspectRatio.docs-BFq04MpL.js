15e77d9e0e75097f329be47f7cb2e424ac8909f8
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-03-05 08:22:37 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@386062560c271c1a78019ffd644fbb70013c721b 🚀

---
+++ demo-DES-1180-vertical-space/assets/AspectRatio.docs-BFq04MpL.js
@@ -0,0 +1,30 @@
+import{j as a}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as o}from"./index-1Ha5aU2j.js";import{M as n,e as i,P as m,f as c}from"./index-DbcqPt37.js";import{A as r}from"./AspectRatio.stories-CgttgCaT.js";import"./index-yBjzXJbu.js";import"./index-C0MU9AHG.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-ADWt8sAS.js";import"./index-fNjTmf9T.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";const p=`<!-- @license CC0-1.0 -->
+
+# Aspect Ratio
+
+Constrains media content to a supported aspect ratio.
+
+## Class names
+
+Each available aspect ratio has an associated class name.
+The class can be applied to any component or element.
+
+| Class name                  | Example                                                                     |
+| :-------------------------- | :-------------------------------------------------------------------------- |
+| \`ams-aspect-ratio--x-tall\`  | <div className="ams-docs-token-example--space ams-aspect-ratio--x-tall" />  |
+| \`ams-aspect-ratio--tall\`    | <div className="ams-docs-token-example--space ams-aspect-ratio--tall" />    |
+| \`ams-aspect-ratio--square\`  | <div className="ams-docs-token-example--space ams-aspect-ratio--square" />  |
+| \`ams-aspect-ratio--wide\`    | <div className="ams-docs-token-example--space ams-aspect-ratio--wide" />    |
+| \`ams-aspect-ratio--x-wide\`  | <div className="ams-docs-token-example--space ams-aspect-ratio--x-wide" />  |
+| \`ams-aspect-ratio--2x-wide\` | <div className="ams-docs-token-example--space ams-aspect-ratio--2x-wide" /> |
+
+## Guidelines
+
+- Apply one of these classes to an image, video or other media content to constrain its dimensions to one of the available aspect ratios.
+- The default aspect ratio is 16:9.
+`;/*@license CC0-1.0*/function t(s){return a.jsxs(a.Fragment,{children:[`
+`,`
+`,a.jsx(n,{of:r}),`
+`,a.jsx(i,{children:p}),`
+`,a.jsx(m,{}),`
+`,a.jsx(c,{})]})}function k(s={}){const{wrapper:e}={...o(),...s.components};return e?a.jsx(e,{...s,children:a.jsx(t,{...s})}):t()}export{k as default};
---
+++ demo-DES-1180-vertical-space/assets/FormPage.docs-DpQBmdHf.js
@@ -0,0 +1,5 @@
+import{j as o}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as m}from"./index-1Ha5aU2j.js";import{M as n}from"./index-DbcqPt37.js";import{F as i}from"./FormPage.stories-OxeLZc9r.js";import"./index-yBjzXJbu.js";import"./index-C0MU9AHG.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-ADWt8sAS.js";import"./index-fNjTmf9T.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index.esm-Bbl-Ydi0.js";import"./clsx-B-dksMZM.js";import"./index.esm-DvoLfDtE.js";import"./config-yBWnR1u4.js";import"./Footer.stories-PmGKTnx5.js";/*@license CC0-1.0*/function e(t){const r={h1:"h1",p:"p",...m(),...t.components};return o.jsxs(o.Fragment,{children:[`
+`,`
+`,o.jsx(n,{of:i}),`
+`,o.jsx(r.h1,{id:"form-page",children:"Form page"}),`
+`,o.jsx(r.p,{children:"This simple example presents various types of fields."})]})}function X(t={}){const{wrapper:r}={...m(),...t.components};return r?o.jsx(r,{...t,children:o.jsx(e,{...t})}):e(t)}export{X as default};
