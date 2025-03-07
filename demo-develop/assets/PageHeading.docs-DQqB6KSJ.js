59805b6d9f0808c4d7325b2a8c62374b613b61df
alimpens <37216945+alimpens@users.noreply.github.com>
2025-03-07 13:42:55 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@190b58865dee4552cf2c304cbc4d193cf7e14036 🚀

---
+++ demo-develop/assets/PageHeading.docs-DQqB6KSJ.js
@@ -0,0 +1,30 @@
+import{j as e}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as s}from"./index-1Ha5aU2j.js";import{M as i,e as r,P as a,f as h,h as c}from"./index-DwGSVYAc.js";import{P as p,I as m}from"./PageHeading.stories-7C68HgG7.js";import"./index-yBjzXJbu.js";import"./index-C0MU9AHG.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-C3Da1O4s.js";import"./index-fNjTmf9T.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./clsx-B-dksMZM.js";const d=`<!-- @license CC0-1.0 -->
+
+# Page Heading
+
+The page heading component is a prominent text at the beginning of a page, immediately after the header.
+Use this component for a name, title, or motto.
+
+## Guidelines
+
+- Use this component to capture the user’s attention.
+- Only use this component once per page, typically only on the homepage.
+- Avoid using it if you need to display long pieces of text; the text should be short and precise.
+
+## Relevant WCAG requirements
+
+The page heading component is a variant of a heading level 1 with a distinct style.
+When using this component, ensure that the heading hierarchy of the page remains logical.
+
+For this component, the same WCAG requirements apply as for [the heading component](/docs/components-text-heading--docs).
+`;/*@license CC0-1.0*/function o(t){const n={code:"code",h2:"h2",h3:"h3",p:"p",...s(),...t.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(i,{of:p}),`
+`,e.jsx(r,{children:d}),`
+`,e.jsx(a,{}),`
+`,e.jsx(h,{}),`
+`,e.jsx(n.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(n.h3,{id:"inverse-colour",children:"Inverse colour"}),`
+`,e.jsxs(n.p,{children:["Set the ",e.jsx(n.code,{children:"color"})," prop to ",e.jsx(n.code,{children:"inverse"}),` if the Page Heading sits on a dark background.
+This ensures the colour of the text provides enough contrast.`]}),`
+`,e.jsx(c,{of:m})]})}function D(t={}){const{wrapper:n}={...s(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(o,{...t})}):o(t)}export{D as default};
