9a892a2a96885d2d162d5c66c47b124dc0053a3a
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-03-11 16:53:53 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@fa9de3b432817a661de8e956167c9ba93cece812 🚀

---
+++ demo-DES-1004-local-close-button/assets/ArticlePage.docs-BEm2fjKv.js
@@ -0,0 +1,5 @@
+import{j as t}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as n}from"./index-1Ha5aU2j.js";import{M as e}from"./index-CKgM-fli.js";import{A as m}from"./ArticlePage.stories-pnN2BeYv.js";import"./index-yBjzXJbu.js";import"./index-C0MU9AHG.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-DS5nkbCM.js";import"./index-fNjTmf9T.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index.esm-Cxx0GL-_.js";import"./clsx-B-dksMZM.js";import"./index.esm-DvoLfDtE.js";import"./Image.stories-1nLEKApt.js";import"./Image-n37fLCx2.js";import"./config-BYYW6bEL.js";import"./Footer.stories-T65CvSc_.js";/*@license CC0-1.0*/function i(o){const r={h1:"h1",p:"p",...n(),...o.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(e,{of:m}),`
+`,t.jsx(r.h1,{id:"article-page",children:"Article page"}),`
+`,t.jsx(r.p,{children:"This page type is for news or similar kinds of articles."})]})}function y(o={}){const{wrapper:r}={...n(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(i,{...o})}):i(o)}export{y as default};
---
+++ demo-DES-1004-local-close-button/assets/Blockquote.docs-DztEutCD.js
@@ -0,0 +1,27 @@
+import{j as t}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as r}from"./index-1Ha5aU2j.js";import{M as s,e as i,P as a,f as c,h}from"./index-CKgM-fli.js";import{B as m,I as l}from"./Blockquote.stories-WQ94p8ZM.js";import"./index-yBjzXJbu.js";import"./index-C0MU9AHG.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-DS5nkbCM.js";import"./index-fNjTmf9T.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./exampleContent-PGFPgakh.js";import"./clsx-B-dksMZM.js";const u=`<!-- @license CC0-1.0 -->
+
+# Blockquote
+
+We use blockquotes to emphasize citations.
+A quote appears differently from the main text to make it stand out.
+
+## Guidelines
+
+- Quote someone’s literal written or oral text.
+- A quote has a width of half or a whole column of text.
+- The component adds the correct quotation marks.
+
+## Relevant WCAG requirements
+
+- [WCAG 3.1.2](https://www.w3.org/WAI/WCAG21/Understanding/language-of-parts.html): if the citation is in a language other than that of the surrounding content, indicate so through the \`lang\` attribute.
+`;/*@license CC0-1.0*/function n(o){const e={code:"code",h2:"h2",h3:"h3",p:"p",...r(),...o.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(s,{of:m}),`
+`,t.jsx(i,{children:u}),`
+`,t.jsx(a,{}),`
+`,t.jsx(c,{}),`
+`,t.jsx(e.h2,{id:"examples",children:"Examples"}),`
+`,t.jsx(e.h3,{id:"inverse-colour",children:"Inverse colour"}),`
+`,t.jsxs(e.p,{children:["Set the ",t.jsx(e.code,{children:"color"})," prop to ",t.jsx(e.code,{children:"inverse"}),` if the Blockquote sits on a dark background.
+This ensures the colour of the text provides enough contrast.`]}),`
+`,t.jsx(h,{of:l})]})}function W(o={}){const{wrapper:e}={...r(),...o.components};return e?t.jsx(e,{...o,children:t.jsx(n,{...o})}):n(o)}export{W as default};
