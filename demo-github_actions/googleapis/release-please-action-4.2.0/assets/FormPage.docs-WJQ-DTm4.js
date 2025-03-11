3d6a9de62652f1f71fa05b14f65a7ef82ae4e2f9
GitHub <github-action@users.noreply.github.com>
2025-03-11 13:25:45 +0000

    Remove feature branch folder

--- demo-github_actions/googleapis/release-please-action-4.2.0/assets/FormPage.docs-WJQ-DTm4.js
+++
@@ -1,5 +0,0 @@
-import{j as o}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as m}from"./index-1Ha5aU2j.js";import{M as n}from"./index-Dc4QmojN.js";import{F as i}from"./FormPage.stories-DfLbISYk.js";import"./index-yBjzXJbu.js";import"./index-C0MU9AHG.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-CRjV9EOf.js";import"./index-fNjTmf9T.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index.esm-hirjXOjg.js";import"./clsx-B-dksMZM.js";import"./index.esm-DvoLfDtE.js";import"./config-CfXbPjDr.js";import"./Footer.stories-CG0hrDCJ.js";/*@license CC0-1.0*/function e(t){const r={h1:"h1",p:"p",...m(),...t.components};return o.jsxs(o.Fragment,{children:[`
-`,`
-`,o.jsx(n,{of:i}),`
-`,o.jsx(r.h1,{id:"form-page",children:"Form page"}),`
-`,o.jsx(r.p,{children:"This simple example presents various types of fields."})]})}function X(t={}){const{wrapper:r}={...m(),...t.components};return r?o.jsx(r,{...t,children:o.jsx(e,{...t})}):e(t)}export{X as default};
