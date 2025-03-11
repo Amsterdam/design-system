c594defad9fecf22831bd27608d22cab0e4b5b0d
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-03-11 11:03:04 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@67bef28e23f91a530b1ce91d602b53b5e1a0533e 🚀

--- demo-DES-1078-space-tokens-with-amopis/assets/DetailsPage.docs-CuUPZ58F.js
+++
@@ -1,5 +0,0 @@
-import{j as t}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as e}from"./index-1Ha5aU2j.js";import{M as i}from"./index-lbZSk6fx.js";import{D as m}from"./DetailsPage.stories-CIMXM0in.js";import"./index-yBjzXJbu.js";import"./index-C0MU9AHG.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-CPRoIh9h.js";import"./index-fNjTmf9T.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index.esm-FxfsT9X9.js";import"./clsx-B-dksMZM.js";import"./index.esm-Dgjlx7qu.js";import"./Submenu-D7iWWcbU.js";/*@license CC0-1.0*/function n(o){const r={h1:"h1",p:"p",...e(),...o.components};return t.jsxs(t.Fragment,{children:[`
-`,`
-`,t.jsx(i,{of:m}),`
-`,t.jsx(r.h1,{id:"details-page",children:"Details Page"}),`
-`,t.jsx(r.p,{children:"Displays properties of an entity."})]})}function C(o={}){const{wrapper:r}={...e(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(n,{...o})}):n(o)}export{C as default};
---
+++ demo-DES-1078-space-tokens-with-amopis/assets/DetailsPage.docs-DNl37mWo.js
@@ -0,0 +1,5 @@
+import{j as t}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as e}from"./index-1Ha5aU2j.js";import{M as i}from"./index-BwgHN0sj.js";import{D as m}from"./DetailsPage.stories-B4gidHzC.js";import"./index-yBjzXJbu.js";import"./index-C0MU9AHG.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-B1UpfQ-n.js";import"./index-fNjTmf9T.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index.esm-FxfsT9X9.js";import"./clsx-B-dksMZM.js";import"./index.esm-Dgjlx7qu.js";import"./Submenu-Dl1XAH-o.js";/*@license CC0-1.0*/function n(o){const r={h1:"h1",p:"p",...e(),...o.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(i,{of:m}),`
+`,t.jsx(r.h1,{id:"details-page",children:"Details Page"}),`
+`,t.jsx(r.p,{children:"Displays properties of an entity."})]})}function C(o={}){const{wrapper:r}={...e(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(n,{...o})}):n(o)}export{C as default};
