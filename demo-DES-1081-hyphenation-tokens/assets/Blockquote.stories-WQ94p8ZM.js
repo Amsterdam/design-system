39b3bb133d559df069a5e0161c72b884c408b56b
GitHub <github-action@users.noreply.github.com>
2025-03-12 08:29:34 +0000

    Remove feature branch folder

--- demo-DES-1081-hyphenation-tokens/assets/Blockquote.stories-WQ94p8ZM.js
+++
@@ -1,8 +0,0 @@
-import{c as f}from"./exampleContent-PGFPgakh.js";import{j as _}from"./jsx-runtime-Cf8x2fCZ.js";import{c as g}from"./clsx-B-dksMZM.js";import{r as q}from"./index-C0MU9AHG.js";const r=q.forwardRef(({children:t,className:p,color:a,...d},m)=>_.jsx("blockquote",{...d,className:g("ams-blockquote",a&&`ams-blockquote--${a}`,p),ref:m,children:t}));r.displayName="Blockquote";try{r.displayName="Blockquote",r.__docgenInfo={description:"",displayName:"Blockquote",props:{color:{defaultValue:null,description:"Changes the text colour for readability on a dark background.",name:"color",required:!1,type:{name:"enum",value:[{value:'"inverse"'}]}}}}}catch{}/**
- * @license EUPL-1.2+
- * Copyright Gemeente Amsterdam
- */const b=f(),k={title:"Components/Text/Blockquote",component:r,args:{children:b},argTypes:{children:{description:"The text for the quote.",table:{disable:!1}},color:{control:{labels:{undefined:"default"},type:"radio"},options:[void 0,"inverse"]}}},e={},o={args:{color:"inverse"}};var s,c,l;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:"{}",...(l=(c=e.parameters)==null?void 0:c.docs)==null?void 0:l.source}}};var n,u,i;o.parameters={...o.parameters,docs:{...(n=o.parameters)==null?void 0:n.docs,source:{originalSource:`{
-  args: {
-    color: 'inverse'
-  }
-}`,...(i=(u=o.parameters)==null?void 0:u.docs)==null?void 0:i.source}}};const x=["Default","InverseColour"],j=Object.freeze(Object.defineProperty({__proto__:null,Default:e,InverseColour:o,__namedExportsOrder:x,default:k},Symbol.toStringTag,{value:"Module"}));export{j as B,o as I};
