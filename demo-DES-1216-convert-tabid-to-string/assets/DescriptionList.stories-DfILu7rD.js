dc36808d6e132e2d5d4969215aadb500d2348600
alimpens <37216945+alimpens@users.noreply.github.com>
2025-03-14 13:14:52 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@c006d85d61090ece7db4a3a7c56f9499a5adb34d 🚀

---
+++ demo-DES-1216-convert-tabid-to-string/assets/DescriptionList.stories-DfILu7rD.js
@@ -0,0 +1,43 @@
+import{j as e}from"./jsx-runtime-Cf8x2fCZ.js";import{P as E,U as l,L as M}from"./index.esm-D141QxfQ.js";import{c as m}from"./clsx-B-dksMZM.js";import{r as D}from"./index-C0MU9AHG.js";const S=D.forwardRef(({children:r,className:n,...s},t)=>e.jsx("dd",{...s,className:m("ams-description-list__description",n),ref:t,children:r}));S.displayName="DescriptionList.Description";try{DescriptionList.Description.displayName="DescriptionList.Description",DescriptionList.Description.__docgenInfo={description:"",displayName:"DescriptionList.Description",props:{}}}catch{}const z=D.forwardRef(({children:r,className:n,...s},t)=>e.jsx("div",{...s,className:m("ams-description-list__section",n),ref:t,children:r}));z.displayName="DescriptionList.Section";try{DescriptionList.Section.displayName="DescriptionList.Section",DescriptionList.Section.__docgenInfo={description:"",displayName:"DescriptionList.Section",props:{}}}catch{}const I=D.forwardRef(({children:r,className:n,...s},t)=>e.jsx("dt",{...s,className:m("ams-description-list__term",n),ref:t,children:r}));I.displayName="DescriptionList.Term";try{DescriptionList.Term.displayName="DescriptionList.Term",DescriptionList.Term.__docgenInfo={description:"",displayName:"DescriptionList.Term",props:{}}}catch{}const P=D.forwardRef(({children:r,className:n,color:s,termsWidth:t,...R},U)=>e.jsx("dl",{...R,className:m("ams-description-list",s&&`ams-description-list--${s}`,t&&`ams-description-list--terms-width-${t}`,n),ref:U,children:r}));P.displayName="DescriptionList";const i=Object.assign(P,{Description:S,Section:z,Term:I});try{i.displayName="DescriptionList",i.__docgenInfo={description:"",displayName:"DescriptionList",props:{color:{defaultValue:null,description:"Changes the text colour for readability on a dark background.",name:"color",required:!1,type:{name:"enum",value:[{value:'"inverse"'}]}},termsWidth:{defaultValue:null,description:"",name:"termsWidth",required:!1,type:{name:"enum",value:[{value:'"sm"'},{value:'"md"'},{value:'"lg"'}]}}}}}catch{}const O={title:"Components/Text/Description List",component:i,args:{children:[e.jsx(i.Term,{children:"Het hoger onderwijs"},1),e.jsx(i.Description,{children:"Het hbo en wo"},2),e.jsx(i.Term,{children:"Het mbo en hoger onderwijs"},3),e.jsx(i.Description,{children:"Het vervolgonderwijs"},4),e.jsx(i.Term,{children:"Laagopgeleid"},5),e.jsx(i.Description,{children:"Praktisch opgeleid"},6),e.jsx(i.Term,{children:"Hoogopgeleid"},7),e.jsx(i.Description,{children:"Theoretisch opgeleid"},8),e.jsx(i.Term,{children:"Opleidingsniveau"},9),e.jsx(i.Description,{children:"Onderwijsrichting"},10)]},argTypes:{color:{control:{labels:{undefined:"default"},type:"radio"},options:[void 0,"inverse"]},termsWidth:{control:{labels:{lg:"large",md:"medium",sm:"small",undefined:"auto"},type:"radio"},options:[void 0,"sm","md","lg"]}}},o={},a={args:{children:[e.jsx(i.Term,{children:"Blinde, slechtziende"},1),e.jsx(i.Description,{children:"Persoon met een visuele beperking"},2),e.jsx(i.Description,{children:"Persoon met een visuele handicap"},3),e.jsx(i.Description,{children:"Persoon die blind is"},4),e.jsx(i.Description,{children:"Persoon die slechtziend is"},5)]}},c={args:{children:[e.jsx(i.Term,{children:"Achternaam"},1),e.jsx(i.Description,{children:"De naam die een persoon van zijn of haar ouders krijgt"},2),e.jsxs(i.Section,{children:[e.jsx(i.Term,{children:"Voornaam"}),e.jsx(i.Term,{children:"Roepnaam"}),e.jsx(i.Term,{children:"Bijnaam"}),e.jsx(i.Description,{children:"De naam waarmee een persoon wordt aangesproken"})]},3),e.jsx(i.Term,{children:"Geboortedatum"},4),e.jsx(i.Description,{children:"De datum waarop een persoon is geboren"},5)],termsWidth:"md"}},d={render:r=>e.jsxs(i,{...r,children:[e.jsx(i.Term,{children:"Amsterdam Light Festival"},1),e.jsxs(i.Description,{children:[e.jsxs(E,{className:"ams-mb--sm",color:r.color,children:["Een jaarlijks evenement waarbij kunstenaars van over de hele wereld hun"," ",e.jsx("strong",{children:"creatieve lichtinstallaties"})," tonen. De kunstwerken zijn verspreid over de stad en zijn zowel vanaf het water als vanaf de kant te bewonderen."]}),e.jsxs(l,{color:r.color,children:[e.jsx(l.Item,{children:"Kleed je warm aan, want de meeste exposities zijn buiten."}),e.jsx(l.Item,{children:"Er zijn begeleide boottochten en wandelroutes beschikbaar."}),e.jsxs(l.Item,{children:["Voor de volledige lijst met exposities kun je"," ",e.jsx(M,{color:r.color,href:"#",children:"de festivalbrochure downloaden"}),"."]})]})]},2)]})},p={args:{color:"inverse"}};var h,u,L;o.parameters={...o.parameters,docs:{...(h=o.parameters)==null?void 0:h.docs,source:{originalSource:"{}",...(L=(u=o.parameters)==null?void 0:u.docs)==null?void 0:L.source}}};var j,g,x;a.parameters={...a.parameters,docs:{...(j=a.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  args: {
+    children: [<DescriptionList.Term key={1}>Blinde, slechtziende</DescriptionList.Term>, <DescriptionList.Description key={2}>Persoon met een visuele beperking</DescriptionList.Description>, <DescriptionList.Description key={3}>Persoon met een visuele handicap</DescriptionList.Description>, <DescriptionList.Description key={4}>Persoon die blind is</DescriptionList.Description>, <DescriptionList.Description key={5}>Persoon die slechtziend is</DescriptionList.Description>]
+  }
+}`,...(x=(g=a.parameters)==null?void 0:g.docs)==null?void 0:x.source}}};var v,_,y;c.parameters={...c.parameters,docs:{...(v=c.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    children: [<DescriptionList.Term key={1}>Achternaam</DescriptionList.Term>, <DescriptionList.Description key={2}>
+        De naam die een persoon van zijn of haar ouders krijgt
+      </DescriptionList.Description>, <DescriptionList.Section key={3}>
+        <DescriptionList.Term>Voornaam</DescriptionList.Term>
+        <DescriptionList.Term>Roepnaam</DescriptionList.Term>
+        <DescriptionList.Term>Bijnaam</DescriptionList.Term>
+        <DescriptionList.Description>De naam waarmee een persoon wordt aangesproken</DescriptionList.Description>
+      </DescriptionList.Section>, <DescriptionList.Term key={4}>Geboortedatum</DescriptionList.Term>, <DescriptionList.Description key={5}>De datum waarop een persoon is geboren</DescriptionList.Description>],
+    termsWidth: 'md'
+  }
+}`,...(y=(_=c.parameters)==null?void 0:_.docs)==null?void 0:y.source}}};var f,T,k;d.parameters={...d.parameters,docs:{...(f=d.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  render: args => <DescriptionList {...args}>
+      <DescriptionList.Term key={1}>Amsterdam Light Festival</DescriptionList.Term>
+      <DescriptionList.Description key={2}>
+        <Paragraph className="ams-mb--sm" color={args.color}>
+          Een jaarlijks evenement waarbij kunstenaars van over de hele wereld hun{' '}
+          <strong>creatieve lichtinstallaties</strong> tonen. De kunstwerken zijn verspreid over de stad en zijn zowel
+          vanaf het water als vanaf de kant te bewonderen.
+        </Paragraph>
+        <UnorderedList color={args.color}>
+          <UnorderedList.Item>Kleed je warm aan, want de meeste exposities zijn buiten.</UnorderedList.Item>
+          <UnorderedList.Item>Er zijn begeleide boottochten en wandelroutes beschikbaar.</UnorderedList.Item>
+          <UnorderedList.Item>
+            Voor de volledige lijst met exposities kun je{' '}
+            <Link color={args.color} href="#">
+              de festivalbrochure downloaden
+            </Link>
+            .
+          </UnorderedList.Item>
+        </UnorderedList>
+      </DescriptionList.Description>
+    </DescriptionList>
+}`,...(k=(T=d.parameters)==null?void 0:T.docs)==null?void 0:k.source}}};var b,w,N;p.parameters={...p.parameters,docs:{...(b=p.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    color: 'inverse'
+  }
+}`,...(N=(w=p.parameters)==null?void 0:w.docs)==null?void 0:N.source}}};const V=["Default","MultipleDescriptions","MultipleTerms","RichDescription","InverseColour"],W=Object.freeze(Object.defineProperty({__proto__:null,Default:o,InverseColour:p,MultipleDescriptions:a,MultipleTerms:c,RichDescription:d,__namedExportsOrder:V,default:O},Symbol.toStringTag,{value:"Module"}));export{W as D,p as I,a as M,d as R,c as a};
