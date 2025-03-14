ff8523304ccb5cef056a6247d6a161f498fae9be
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-03-14 14:19:50 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@8e0f28b1885cbd28fd01aee26b77941b01721ca3 🚀

--- demo-DES-1216-convert-tabid-to-string/assets/FormPage.docs-CDW2cNDD.js
+++
@@ -1,5 +0,0 @@
-import{j as o}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as m}from"./index-1Ha5aU2j.js";import{M as n}from"./index-BQhGYX6a.js";import{F as i}from"./FormPage.stories-DUbvv87s.js";import"./index-yBjzXJbu.js";import"./index-C0MU9AHG.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-DgoT5ToQ.js";import"./index-fNjTmf9T.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index.esm-D141QxfQ.js";import"./clsx-B-dksMZM.js";import"./index.esm-DvoLfDtE.js";import"./config-Cw0kBGIf.js";import"./Footer.stories-DtsgyJ2N.js";/*@license CC0-1.0*/function e(t){const r={h1:"h1",p:"p",...m(),...t.components};return o.jsxs(o.Fragment,{children:[`
-`,`
-`,o.jsx(n,{of:i}),`
-`,o.jsx(r.h1,{id:"form-page",children:"Form page"}),`
-`,o.jsx(r.p,{children:"This simple example presents various types of fields."})]})}function X(t={}){const{wrapper:r}={...m(),...t.components};return r?o.jsx(r,{...t,children:o.jsx(e,{...t})}):e(t)}export{X as default};
