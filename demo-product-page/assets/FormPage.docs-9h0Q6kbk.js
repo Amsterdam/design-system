30ae0d398c89f01cc90daedb7708d8cc598f61c4
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-03-14 15:49:50 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@b59da3a8fb16e0c7a745b4b68e37596db5172d4c 🚀

---
+++ demo-product-page/assets/FormPage.docs-9h0Q6kbk.js
@@ -0,0 +1,5 @@
+import{j as o}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as m}from"./index-1Ha5aU2j.js";import{M as n}from"./index-Dt-h5wr8.js";import{F as i}from"./FormPage.stories-D_9qh_fP.js";import"./index-yBjzXJbu.js";import"./index-C0MU9AHG.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-BpIRPrVR.js";import"./index-fNjTmf9T.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index.esm-7DgB5sAx.js";import"./clsx-B-dksMZM.js";import"./index.esm-DvoLfDtE.js";import"./config-XEs_S07Y.js";import"./Footer.stories-A-1n0VNe.js";/*@license CC0-1.0*/function e(t){const r={h1:"h1",p:"p",...m(),...t.components};return o.jsxs(o.Fragment,{children:[`
+`,`
+`,o.jsx(n,{of:i}),`
+`,o.jsx(r.h1,{id:"form-page",children:"Form page"}),`
+`,o.jsx(r.p,{children:"This simple example presents various types of fields."})]})}function X(t={}){const{wrapper:r}={...m(),...t.components};return r?o.jsx(r,{...t,children:o.jsx(e,{...t})}):e(t)}export{X as default};
---
+++ demo-product-page/assets/ProductPage.docs-CL6FrBFl.js
@@ -0,0 +1,5 @@
+import{j as t}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as i}from"./index-1Ha5aU2j.js";import{M as m}from"./index-Dt-h5wr8.js";import{P as e}from"./ProductPage.stories-B2F8_P3Q.js";import"./index-yBjzXJbu.js";import"./index-C0MU9AHG.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-BpIRPrVR.js";import"./index-fNjTmf9T.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index.esm-7DgB5sAx.js";import"./clsx-B-dksMZM.js";import"./index.esm-DvoLfDtE.js";import"./config-XEs_S07Y.js";import"./Footer.stories-A-1n0VNe.js";/*@license CC0-1.0*/function n(o){const r={h1:"h1",p:"p",...i(),...o.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(m,{of:e}),`
+`,t.jsx(r.h1,{id:"product-page",children:"Product page"}),`
+`,t.jsx(r.p,{children:"TODO Description"})]})}function X(o={}){const{wrapper:r}={...i(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(n,{...o})}):n(o)}export{X as default};
