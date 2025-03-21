099b9a17255e9b1ffcd000615eb6825ffcff5b47
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-03-21 12:53:34 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@00f8b5b8be1cd710305e167dd8bfec8fcd80ee55 🚀

--- demo-DES-1078-space-tokens/assets/ArticlePage.docs-DuT_o2JJ.js
+++
@@ -1,5 +0,0 @@
-import{j as t}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as n}from"./index-1Ha5aU2j.js";import{M as e}from"./index-DHtkf3qg.js";import{A as m}from"./ArticlePage.stories-BtQ4AhsE.js";import"./index-yBjzXJbu.js";import"./index-C0MU9AHG.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-D4Ucl1en.js";import"./index-fNjTmf9T.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index.esm-_Py4KnVy.js";import"./clsx-B-dksMZM.js";import"./index.esm-DvoLfDtE.js";import"./Image.stories-Crwpu-VJ.js";import"./Image-B_CIMnUX.js";import"./generateAspectRatioClass-wb7sZDsw.js";import"./config-BApTmh_A.js";import"./Footer.stories-DYG4G8tS.js";/*@license CC0-1.0*/function i(o){const r={h1:"h1",p:"p",...n(),...o.components};return t.jsxs(t.Fragment,{children:[`
-`,`
-`,t.jsx(e,{of:m}),`
-`,t.jsx(r.h1,{id:"article-page",children:"Article page"}),`
-`,t.jsx(r.p,{children:"This page type is for news or similar kinds of articles."})]})}function _(o={}){const{wrapper:r}={...n(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(i,{...o})}):i(o)}export{_ as default};
---
+++ demo-DES-1078-space-tokens/assets/FormPage.docs-ZwIkPddS.js
@@ -0,0 +1,5 @@
+import{j as o}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as m}from"./index-1Ha5aU2j.js";import{M as n}from"./index-BR0K5v_Q.js";import{F as i}from"./FormPage.stories-BBMqjlGQ.js";import"./index-yBjzXJbu.js";import"./index-C0MU9AHG.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-CVH6TyeC.js";import"./index-fNjTmf9T.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index.esm-_Py4KnVy.js";import"./clsx-B-dksMZM.js";import"./index.esm-DvoLfDtE.js";import"./config-BApTmh_A.js";import"./Footer.stories-DYG4G8tS.js";/*@license CC0-1.0*/function e(t){const r={h1:"h1",p:"p",...m(),...t.components};return o.jsxs(o.Fragment,{children:[`
+`,`
+`,o.jsx(n,{of:i}),`
+`,o.jsx(r.h1,{id:"form-page",children:"Form page"}),`
+`,o.jsx(r.p,{children:"This simple example presents various types of fields."})]})}function X(t={}){const{wrapper:r}={...m(),...t.components};return r?o.jsx(r,{...t,children:o.jsx(e,{...t})}):e(t)}export{X as default};
