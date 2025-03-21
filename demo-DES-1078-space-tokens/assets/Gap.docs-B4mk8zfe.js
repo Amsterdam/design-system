d10b2499dcffef2e54936d7e557f5faf49847f43
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-03-21 13:56:59 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@6d9afc849013b5bc4eb6253042a8c16074ccae15 🚀

---
+++ demo-DES-1078-space-tokens/assets/Gap.docs-B4mk8zfe.js
@@ -0,0 +1,35 @@
+import{j as e}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as t}from"./index-1Ha5aU2j.js";import{M as i,h as o,P as r,i as m}from"./index-CIYAUlqc.js";import{G as p}from"./Gap.stories-DlrSqaJT.js";import"./index-yBjzXJbu.js";import"./index-C0MU9AHG.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-BMWBUnJS.js";import"./index-fNjTmf9T.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index.esm-_Py4KnVy.js";import"./clsx-B-dksMZM.js";import"./index.esm-DvoLfDtE.js";const l=`<!-- @license CC0-1.0 -->
+
+# Gap
+
+Adds white space between a set of elements.
+
+## Class names
+
+All six sizes of [Space](/docs/brand-design-tokens-space--docs) are available for the width or height of the gap.
+
+| Class name     | Example                                                                                    |
+| :------------- | :----------------------------------------------------------------------------------------- |
+| \`ams-gap--xs\`  | <div className="ams-docs-token-example--space" style="inline-size: var(--ams-gap-xs);" />  |
+| \`ams-gap--sm\`  | <div className="ams-docs-token-example--space" style="inline-size: var(--ams-gap-sm);" />  |
+| \`ams-gap--md\`  | <div className="ams-docs-token-example--space" style="inline-size: var(--ams-gap-md);" />  |
+| \`ams-gap--lg\`  | <div className="ams-docs-token-example--space" style="inline-size: var(--ams-gap-lg);" />  |
+| \`ams-gap--xl\`  | <div className="ams-docs-token-example--space" style="inline-size: var(--ams-gap-xl);" />  |
+| \`ams-gap--2xl\` | <div className="ams-docs-token-example--space" style="inline-size: var(--ams-gap-2xl);" /> |
+
+## Guidelines
+
+- Use this utility class to vertically separate the children of a parent element from each other.
+- Do not use it on the Grid component.
+  Grid is responsible for its own gaps.
+- It can be used on any other element and sets the \`gap\` CSS property.
+  It also makes the element a grid container; the gap would be ignored otherwise.
+  These declarations are marked with the \`!important\` flag to ensure they override any other gaps and display modes.
+- To add a gap between 2 siblings, the first one can be given a [Margin Bottom](/docs/utilities-css-margin--docs) instead.
+  This may be preferable because it doesn’t change the parent’s display mode.
+`;/*@license CC0-1.0*/function n(s){return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(i,{of:p}),`
+`,e.jsx(o,{children:l}),`
+`,e.jsx(r,{}),`
+`,e.jsx(m,{})]})}function M(s={}){const{wrapper:a}={...t(),...s.components};return a?e.jsx(a,{...s,children:e.jsx(n,{...s})}):n()}export{M as default};
