b9c7e16c51a72062670b44aad77e07e92ac93af6
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-03-21 22:49:21 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@981e54891a11d05b1b63d9a81433a6ef94c8509b 🚀

---
+++ demo-DES-1117-utility-class-naming/assets/TableOfContents.docs-CPXt2ziC.js
@@ -0,0 +1,15 @@
+import{j as n}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as s}from"./index-1Ha5aU2j.js";import{M as i,h as r,P as a,i as m,j as l}from"./index-BrrQ4Abg.js";import{T as p,M as c}from"./TableOfContents.stories-x1WeFG3V.js";import"./index-yBjzXJbu.js";import"./index-C0MU9AHG.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-C5Sg03Tf.js";import"./index-fNjTmf9T.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./clsx-B-dksMZM.js";import"./Heading-BaZwNtbo.js";import"./getHeadingTag-Dv1eMqvl.js";const h=`<!-- @license CC0-1.0 -->
+
+# Table of Contents
+
+A list of links corresponding to the content sections on the page.
+It helps users to easily navigate to different sections on the same page.
+`;/*@license CC0-1.0*/function o(t){const e={h2:"h2",h3:"h3",...s(),...t.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(i,{of:p}),`
+`,n.jsx(r,{children:h}),`
+`,n.jsx(a,{}),`
+`,n.jsx(m,{}),`
+`,n.jsx(e.h2,{id:"examples",children:"Examples"}),`
+`,n.jsx(e.h3,{id:"multiple-levels",children:"Multiple levels"}),`
+`,n.jsx(l,{of:c})]})}function b(t={}){const{wrapper:e}={...s(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(o,{...t})}):o(t)}export{b as default};
