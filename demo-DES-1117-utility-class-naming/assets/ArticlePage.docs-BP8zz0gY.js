d7c6c87918b7576db67d8ac9ce25f931b8c1fc49
GitHub <github-action@users.noreply.github.com>
2025-03-24 08:49:39 +0000

    Remove feature branch folder

--- demo-DES-1117-utility-class-naming/assets/ArticlePage.docs-BP8zz0gY.js
+++
@@ -1,5 +0,0 @@
-import{j as t}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as n}from"./index-1Ha5aU2j.js";import{M as e}from"./index-BrrQ4Abg.js";import{A as m}from"./ArticlePage.stories-C82OCRE4.js";import"./index-yBjzXJbu.js";import"./index-C0MU9AHG.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-C5Sg03Tf.js";import"./index-fNjTmf9T.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index.esm-CQRAfVlW.js";import"./clsx-B-dksMZM.js";import"./index.esm-DvoLfDtE.js";import"./Image.stories-0CwN5eO-.js";import"./Image-DeZSCedU.js";import"./generateAspectRatioClass-jt2KOtJX.js";import"./config-CS3AeGpC.js";import"./Footer.stories-wYpy6ZEK.js";/*@license CC0-1.0*/function i(o){const r={h1:"h1",p:"p",...n(),...o.components};return t.jsxs(t.Fragment,{children:[`
-`,`
-`,t.jsx(e,{of:m}),`
-`,t.jsx(r.h1,{id:"article-page",children:"Article page"}),`
-`,t.jsx(r.p,{children:"This page type is for news or similar kinds of articles."})]})}function _(o={}){const{wrapper:r}={...n(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(i,{...o})}):i(o)}export{_ as default};
