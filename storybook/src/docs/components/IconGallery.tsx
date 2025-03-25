6176fafaf8e3c188df3ffe5c95ebe0e7e46ddc09
Niels Roozemond <n.roozemond@amsterdam.nl>
2025-03-25 09:45:32 +0100

    Some style changes for the gallery

--- storybook/src/docs/components/IconGallery.tsx
+++ storybook/src/docs/components/IconGallery.tsx
@@ -1,5 +1,4 @@
-import { Heading } from '@amsterdam/design-system-react'
-import { Icon } from '@amsterdam/design-system-react/src'
+import { Icon, Paragraph } from '@amsterdam/design-system-react'
 import * as Icons from '@amsterdam/design-system-react-icons/src'
 
 import './icon-gallery.css'
@@ -27,21 +26,16 @@ export const IconGallery = () => {
   const groupedIcons = groupIcons(icons)
 
   return (
-    <>
-      <Heading className="ams-mb--sm" level={2}>
-        Icons
-      </Heading>
-      <div className="ams-storybook-icon-gallery">
-        {Object.keys(groupedIcons).map((baseName) => (
-          <div className="ams-storybook-icon-gallery__item" key={baseName}>
-            <div>
-              {groupedIcons[baseName].outline && <Icon size="large" svg={Icons[groupedIcons[baseName].outline!]} />}
-              {groupedIcons[baseName].filled && <Icon size="large" svg={Icons[groupedIcons[baseName].filled!]} />}
-            </div>
-            <p className="ams-paragraph ams-paragraph--small">{baseName.substring(0, baseName.length - 4)}</p>
+    <div className="ams-storybook-icon-gallery">
+      {Object.keys(groupedIcons).map((baseName) => (
+        <div className="ams-storybook-icon-gallery__item" key={baseName}>
+          <div>
+            {groupedIcons[baseName].outline && <Icon size="large" svg={Icons[groupedIcons[baseName].outline!]} />}
+            {groupedIcons[baseName].filled && <Icon size="large" svg={Icons[groupedIcons[baseName].filled!]} />}
           </div>
-        ))}
-      </div>
-    </>
+          <Paragraph size="small">{baseName.substring(0, baseName.length - 4)}</Paragraph>
+        </div>
+      ))}
+    </div>
   )
 }
