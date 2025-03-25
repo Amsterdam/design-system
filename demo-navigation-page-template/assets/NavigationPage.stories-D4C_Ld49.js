e38537d63197ff798a27ce7d23e38e0acb8133a8
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-03-25 12:43:08 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@75a579bc3f88b84e2ca46abc01e5fa11809e21c5 🚀

---
+++ demo-navigation-page-template/assets/NavigationPage.stories-D4C_Ld49.js
@@ -0,0 +1,286 @@
+import{j as e}from"./jsx-runtime-Cf8x2fCZ.js";import{G as n,C as _,H as r,n as s,i as f,t as x,P as a,p as j,L as d,U as h,S as u,I as R}from"./index.esm-Bwts-hQ3.js";import{r as A,s as y,t as O,u as F,v as W}from"./index.esm-DlBD7MQf.js";import{b as i,e as l}from"./exampleContent-PGFPgakh.js";import{c as T}from"./config-CthLQ-VZ.js";const m=({linkGroups:o})=>o.map(({heading:p,links:V},b)=>e.jsx(n.Cell,{span:{narrow:4,medium:4,wide:5},start:{narrow:1,medium:b%2===0?1:5,wide:b%2===0?2:7},children:e.jsxs(_,{gap:"extra-small",children:[e.jsx(r,{className:"ams-mb-sm",level:2,size:"level-3",children:p}),e.jsx(s,{children:V.map(L=>e.jsx(s.Link,{href:"#",children:L},L))})]},p)},p));try{m.displayName="LinkGroupGridCells",m.__docgenInfo={description:"",displayName:"LinkGroupGridCells",props:{linkGroups:{defaultValue:null,description:"",name:"linkGroups",required:!0,type:{name:"LinkGroup[]"}}}}}catch{}const k=({children:o})=>e.jsxs(e.Fragment,{children:[e.jsx(n,{paddingTop:"small",children:e.jsx(n.Cell,{span:{narrow:4,medium:6,wide:7},children:e.jsx(f,{children:e.jsx(f.Link,{href:"#",children:"Home"})})})}),e.jsx("main",{id:"main",children:o})]});try{k.displayName="NavigationPage",k.__docgenInfo={description:"",displayName:"NavigationPage",props:{}}}catch{}/**
+ * @license EUPL-1.2+
+ * Copyright Gemeente Amsterdam
+ */const w=[{heading:"Paspoort en identiteitskaart",links:["Paspoort of identiteitskaart aanvragen","Zakenpaspoort aanvragen","Tweede paspoort aanvragen","Paspoort kwijt of gestolen"]},{heading:"Rijbewijs",links:["Rijbewijs aanvragen","Rijbewijs verlengen of categorie toevoegen","Rijbewijs kwijt of gestolen","Buitenlands rijbewijs omwisselen"]},{heading:"Trouwen en Partnerschap",links:["Gratis, budget en standaard ceremonie","Kosten","Meer"]},{heading:"Verhuizen",links:["Verhuizing doorgeven"]},{heading:"Akten, uittreksels en verklaringen",links:["Uittreksel BRP aanvragen","Geboorteakte aanvragen","Meer"]},{heading:"Geboorte en erkenning kinderen",links:["Geboorteaangifte doen","Kind erkennen","Meer"]},{heading:"Adres en inschrijving",links:["Opnieuw inschrijven bij de gemeente","Onjuiste inschrijving melden","Onderzoek naar inschrijving Basisregistratie Personen (BRP)","Briefadres aanvragen en wijzigen"]},{heading:"Persoonsgegevens",links:["Inzien of wijzigen van uw gegevens","Wie mogen mijn persoonlijke gegevens opvragen?","Geheimhouding persoonsgegevens","Voor- of achternaam wijzigen","Wat te doen bij identiteitsfraude"]},{heading:"Immigratie en naturalisatie",links:["Verhuizen naar Amsterdam vanuit het buitenland","Naturalisatie – Wat u moet doen om Nederlander te worden"]},{heading:"Overlijden",links:["Aangifte van overlijden","Aangifte van overlijden door uitvaartondernemers","Aangifte van overlijden in het buitenland","Begraafplaatsen en crematoria","Uitvaarten door de gemeente"]}],U=[{heading:"Bewoners",links:["Paspoort of identiteitskaart aanvragen","Zakenpaspoort aanvragen","Tweede paspoort aanvragen","Paspoort kwijt of gestolen"]},{heading:"Waar u kunt parkeren",links:["Rijbewijs aanvragen","Rijbewijs verlengen of categorie toevoegen","Rijbewijs kwijt of gestolen"]},{heading:"Bedrijven",links:["Paspoort of identiteitskaart aanvragen","Zakenpaspoort aanvragen","Tweede paspoort aanvragen","Paspoort kwijt of gestolen"]},{heading:"Parkeerboetes",links:["Rijbewijs aanvragen"]}],M={...T,title:"Pages/Amsterdam.nl/Navigation Page",component:k},t={args:{children:e.jsxs(n,{gapVertical:"large",paddingBottom:"large",children:[e.jsxs(n.Cell,{span:{narrow:4,medium:6,wide:7},children:[e.jsx(x,{className:"ams-mb-md",children:"Burgerzaken"}),e.jsx(a,{size:"large",children:"Heeft u een paspoort, rijbewijs of uittreksel nodig? Of wilt u een verhuizing doorgeven of een geboorte aangeven? Op deze pagina vindt u alle informatie en regelzaken rondom Burgerzaken."})]}),e.jsx(m,{linkGroups:w})]})}},v="Aenean lorem elit, commodo eget elit sit amet, auctor venenatis ligula. Phasellus faucibus fringilla viverra. Aliquam vestibulum vehicula enim, ac consequat nisl mattis id.",g={args:{children:e.jsxs(e.Fragment,{children:[e.jsxs(n,{gapVertical:"large",paddingBottom:"large",children:[e.jsxs(n.Cell,{span:{narrow:4,medium:6,wide:7},children:[e.jsx(x,{className:"ams-mb-md",children:"Parkeren"}),e.jsx(a,{className:"ams-mb-md",size:"large",children:v}),e.jsx(a,{className:"ams-mb-md",children:v}),e.jsx(a,{children:v})]}),e.jsx(m,{linkGroups:U})]}),e.jsx(j,{children:e.jsx(n,{paddingVertical:"medium",children:e.jsxs(n.Cell,{span:{narrow:4,medium:4,wide:5},start:{narrow:1,medium:1,wide:2},children:[e.jsx(r,{className:"ams-mb-md",color:"inverse",level:2,children:"Parkeertarieven"}),e.jsxs(a,{className:"ams-mb-md",color:"inverse",children:["Voor Weesp kijkt u op"," ",e.jsx(d,{color:"inverse",href:"#",variant:"inline",children:"Parkeertarieven stadsgebied Weesp"}),"."]}),e.jsxs(h,{className:"ams-mb-lg",color:"inverse",children:[e.jsx(h.Item,{children:"Vul een adres in om de mogelijkheden te bekijken"}),e.jsxs(h.Item,{children:["Als u op een feestdag wilt parkeren, bekijk dan ook"," ",e.jsx(d,{color:"inverse",href:"#",variant:"inline",children:"Parkeren op feestdagen"}),"."]})]}),e.jsxs(u,{children:[e.jsx(u.Input,{}),e.jsx(u.Button,{})]})]})})}),e.jsx(R,{alt:"",aspectRatio:"16:9",className:"ams-mb-xl",src:"http://www.parkerenindestad.nl/images/amsterdam/parkeren-in-amsterdam.jpg",style:{inlineSize:"100%"}},1)]})}},c={args:{children:e.jsxs(e.Fragment,{children:[e.jsx(n,{paddingBottom:"medium",children:e.jsxs(n.Cell,{span:{narrow:4,medium:6,wide:7},children:[e.jsx(x,{className:"ams-mb-md",children:"Overzichtspagina content / Subnavigatiepagina"}),e.jsx(a,{size:"large",children:"Amsterdam wil een nieuwe traditie starten om met oud en nieuw naar een centrale nieuwjaarsviering te gaan in plaats van zelf vuurwerk af te steken."})]})}),e.jsx(R,{alt:"",aspectRatio:"16:5",src:"https://picsum.photos/1600/500"}),e.jsxs(n,{paddingVertical:"medium",children:[e.jsxs(n.Cell,{span:{narrow:4,medium:6,wide:7},start:{narrow:1,medium:1,wide:2},children:[e.jsx(r,{className:"ams-mb-sm",level:2,children:"L2 Paragraaf titel"}),e.jsx(a,{children:i()})]}),e.jsx(m,{linkGroups:w.slice(0,6)}),e.jsxs(n.Cell,{span:{narrow:4,medium:4,wide:5},start:{narrow:1,medium:1,wide:2},children:[e.jsx(r,{className:"ams-mb-sm",level:2,size:"level-4",children:l()}),e.jsx(a,{className:"ams-mb-sm",children:i()}),e.jsx(d,{href:"#",variant:"standalone",children:"Rijbewijs aanvragen"})]}),e.jsxs(n.Cell,{span:{narrow:4,medium:4,wide:5},children:[e.jsx(r,{className:"ams-mb-sm",level:2,size:"level-4",children:l()}),e.jsx(a,{className:"ams-mb-sm",children:i()}),e.jsx(d,{href:"#",variant:"standalone",children:"Rijbewijs aanvragen"})]}),e.jsxs(n.Cell,{span:{narrow:4,medium:4,wide:5},start:{narrow:1,medium:1,wide:2},children:[e.jsx(r,{className:"ams-mb-sm",level:2,size:"level-4",children:l()}),e.jsx(a,{className:"ams-mb-sm",children:i()}),e.jsx(d,{href:"#",variant:"standalone",children:"Rijbewijs aanvragen"})]}),e.jsxs(n.Cell,{span:{narrow:4,medium:4,wide:5},children:[e.jsx(r,{className:"ams-mb-sm",level:2,size:"level-4",children:l()}),e.jsx(a,{className:"ams-mb-sm",children:i()}),e.jsx(d,{href:"#",variant:"standalone",children:"Rijbewijs aanvragen"})]})]}),e.jsx(j,{color:"magenta",children:e.jsxs(n,{paddingVertical:"medium",children:[e.jsx(n.Cell,{span:{narrow:4,medium:8,wide:10},start:{narrow:1,medium:1,wide:2},children:e.jsx(r,{color:"inverse",level:3,children:l()})}),e.jsxs(n.Cell,{span:{narrow:4,medium:4,wide:5},start:{narrow:1,medium:1,wide:2},children:[e.jsx(r,{className:"ams-mb-sm",color:"inverse",level:4,children:l()}),e.jsx(a,{color:"inverse",children:i()})]}),e.jsxs(n.Cell,{span:{narrow:4,medium:4,wide:5},children:[e.jsx(r,{className:"ams-mb-sm",color:"inverse",level:4,children:l()}),e.jsx(a,{color:"inverse",children:i()})]})]})}),e.jsxs(n,{paddingVertical:"medium",children:[e.jsxs(n.Cell,{span:{narrow:4,medium:6,wide:7},start:{narrow:1,medium:1,wide:2},children:[e.jsx(r,{className:"ams-mb-sm",level:2,children:"L2 Paragraaf titel"}),e.jsx(a,{children:i()})]}),e.jsx(m,{linkGroups:w.slice(4,8)})]}),e.jsx(j,{className:"ams-mb-xl",color:"green",children:e.jsxs(n,{paddingVertical:"medium",children:[e.jsxs(n.Cell,{span:{narrow:4,medium:5,wide:7},start:{narrow:1,medium:1,wide:2},children:[e.jsx(r,{className:"ams-mb-sm",color:"inverse",level:4,children:"Blijf op de hoogte"}),e.jsx(a,{className:"ams-mb-md",color:"inverse",children:"Schrijf u nu in voor de Nieuwsbrief Amsterdam en ontvang wekelijks nieuws, tips en mooie verhalen over de stad en uw stadsdeel."}),e.jsx(d,{color:"inverse",href:"#",variant:"standalone",children:"Ik wil de nieuwsbrief"})]}),e.jsxs(n.Cell,{span:{narrow:4,medium:3,wide:4},children:[e.jsx(r,{className:"ams-mb-sm",color:"inverse",level:4,children:"Deel deze pagina"}),e.jsxs(s,{children:[e.jsx(s.Link,{color:"inverse",href:"#",icon:A,children:"Facebook"}),e.jsx(s.Link,{color:"inverse",href:"#",icon:y,children:"Instagram"}),e.jsx(s.Link,{color:"inverse",href:"#",icon:O,children:"LinkedIn"}),e.jsx(s.Link,{color:"inverse",href:"#",icon:F,children:"Whatsapp"}),e.jsx(s.Link,{color:"inverse",href:"#",icon:W,children:"X (Twitter)"})]})]})]})})]})}};var P,G,N;t.parameters={...t.parameters,docs:{...(P=t.parameters)==null?void 0:P.docs,source:{originalSource:`{
+  args: {
+    children: <Grid gapVertical="large" paddingBottom="large">
+        <Grid.Cell span={{
+        narrow: 4,
+        medium: 6,
+        wide: 7
+      }}>
+          <PageHeading className="ams-mb-md">Burgerzaken</PageHeading>
+          <Paragraph size="large">
+            Heeft u een paspoort, rijbewijs of uittreksel nodig? Of wilt u een verhuizing doorgeven of een geboorte
+            aangeven? Op deze pagina vindt u alle informatie en regelzaken rondom Burgerzaken.
+          </Paragraph>
+        </Grid.Cell>
+        <LinkGroupGridCells linkGroups={burgerzakenLinks} />
+      </Grid>
+  }
+}`,...(N=(G=t.parameters)==null?void 0:G.docs)==null?void 0:N.source}}};var C,z,H;g.parameters={...g.parameters,docs:{...(C=g.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  args: {
+    children: <>
+        <Grid gapVertical="large" paddingBottom="large">
+          <Grid.Cell span={{
+          narrow: 4,
+          medium: 6,
+          wide: 7
+        }}>
+            <PageHeading className="ams-mb-md">Parkeren</PageHeading>
+            <Paragraph className="ams-mb-md" size="large">
+              {parkerenLorem}
+            </Paragraph>
+            <Paragraph className="ams-mb-md">{parkerenLorem}</Paragraph>
+            <Paragraph>{parkerenLorem}</Paragraph>
+          </Grid.Cell>
+          <LinkGroupGridCells linkGroups={parkerenLinks}></LinkGroupGridCells>
+        </Grid>
+        <Spotlight>
+          <Grid paddingVertical="medium">
+            <Grid.Cell span={{
+            narrow: 4,
+            medium: 4,
+            wide: 5
+          }} start={{
+            narrow: 1,
+            medium: 1,
+            wide: 2
+          }}>
+              <Heading className="ams-mb-md" color="inverse" level={2}>
+                Parkeertarieven
+              </Heading>
+              <Paragraph className="ams-mb-md" color="inverse">
+                Voor Weesp kijkt u op{' '}
+                <Link color="inverse" href="#" variant="inline">
+                  Parkeertarieven stadsgebied Weesp
+                </Link>
+                .
+              </Paragraph>
+              <UnorderedList className="ams-mb-lg" color="inverse">
+                <UnorderedList.Item>Vul een adres in om de mogelijkheden te bekijken</UnorderedList.Item>
+                <UnorderedList.Item>
+                  Als u op een feestdag wilt parkeren, bekijk dan ook{' '}
+                  <Link color="inverse" href="#" variant="inline">
+                    Parkeren op feestdagen
+                  </Link>
+                  .
+                </UnorderedList.Item>
+              </UnorderedList>
+              <SearchField>
+                <SearchField.Input />
+                <SearchField.Button />
+              </SearchField>
+            </Grid.Cell>
+          </Grid>
+        </Spotlight>
+        <Image alt="" aspectRatio="16:9" className="ams-mb-xl" key={1} src="http://www.parkerenindestad.nl/images/amsterdam/parkeren-in-amsterdam.jpg" style={{
+        inlineSize: '100%'
+      }} />
+      </>
+  }
+}`,...(H=(z=g.parameters)==null?void 0:z.docs)==null?void 0:H.source}}};var S,I,B;c.parameters={...c.parameters,docs:{...(S=c.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    children: <>
+        <Grid paddingBottom="medium">
+          <Grid.Cell span={{
+          narrow: 4,
+          medium: 6,
+          wide: 7
+        }}>
+            <PageHeading className="ams-mb-md">Overzichtspagina content / Subnavigatiepagina</PageHeading>
+            <Paragraph size="large">
+              Amsterdam wil een nieuwe traditie starten om met oud en nieuw naar een centrale nieuwjaarsviering te gaan
+              in plaats van zelf vuurwerk af te steken.
+            </Paragraph>
+          </Grid.Cell>
+        </Grid>
+        <Image alt="" aspectRatio="16:5" src="https://picsum.photos/1600/500" />
+        <Grid paddingVertical="medium">
+          <Grid.Cell span={{
+          narrow: 4,
+          medium: 6,
+          wide: 7
+        }} start={{
+          narrow: 1,
+          medium: 1,
+          wide: 2
+        }}>
+            <Heading className="ams-mb-sm" level={2}>
+              L2 Paragraaf titel
+            </Heading>
+            <Paragraph>{exampleParagraph()}</Paragraph>
+          </Grid.Cell>
+          <LinkGroupGridCells linkGroups={burgerzakenLinks.slice(0, 6)} />
+          <Grid.Cell span={{
+          narrow: 4,
+          medium: 4,
+          wide: 5
+        }} start={{
+          narrow: 1,
+          medium: 1,
+          wide: 2
+        }}>
+            <Heading className="ams-mb-sm" level={2} size="level-4">
+              {exampleHeading()}
+            </Heading>
+            <Paragraph className="ams-mb-sm">{exampleParagraph()}</Paragraph>
+            <Link href="#" variant="standalone">
+              Rijbewijs aanvragen
+            </Link>
+          </Grid.Cell>
+          <Grid.Cell span={{
+          narrow: 4,
+          medium: 4,
+          wide: 5
+        }}>
+            <Heading className="ams-mb-sm" level={2} size="level-4">
+              {exampleHeading()}
+            </Heading>
+            <Paragraph className="ams-mb-sm">{exampleParagraph()}</Paragraph>
+            <Link href="#" variant="standalone">
+              Rijbewijs aanvragen
+            </Link>
+          </Grid.Cell>
+          <Grid.Cell span={{
+          narrow: 4,
+          medium: 4,
+          wide: 5
+        }} start={{
+          narrow: 1,
+          medium: 1,
+          wide: 2
+        }}>
+            <Heading className="ams-mb-sm" level={2} size="level-4">
+              {exampleHeading()}
+            </Heading>
+            <Paragraph className="ams-mb-sm">{exampleParagraph()}</Paragraph>
+            <Link href="#" variant="standalone">
+              Rijbewijs aanvragen
+            </Link>
+          </Grid.Cell>
+          <Grid.Cell span={{
+          narrow: 4,
+          medium: 4,
+          wide: 5
+        }}>
+            <Heading className="ams-mb-sm" level={2} size="level-4">
+              {exampleHeading()}
+            </Heading>
+            <Paragraph className="ams-mb-sm">{exampleParagraph()}</Paragraph>
+            <Link href="#" variant="standalone">
+              Rijbewijs aanvragen
+            </Link>
+          </Grid.Cell>
+        </Grid>
+        <Spotlight color="magenta">
+          <Grid paddingVertical="medium">
+            <Grid.Cell span={{
+            narrow: 4,
+            medium: 8,
+            wide: 10
+          }} start={{
+            narrow: 1,
+            medium: 1,
+            wide: 2
+          }}>
+              <Heading color="inverse" level={3}>
+                {exampleHeading()}
+              </Heading>
+            </Grid.Cell>
+            <Grid.Cell span={{
+            narrow: 4,
+            medium: 4,
+            wide: 5
+          }} start={{
+            narrow: 1,
+            medium: 1,
+            wide: 2
+          }}>
+              <Heading className="ams-mb-sm" color="inverse" level={4}>
+                {exampleHeading()}
+              </Heading>
+              <Paragraph color="inverse">{exampleParagraph()}</Paragraph>
+            </Grid.Cell>
+            <Grid.Cell span={{
+            narrow: 4,
+            medium: 4,
+            wide: 5
+          }}>
+              <Heading className="ams-mb-sm" color="inverse" level={4}>
+                {exampleHeading()}
+              </Heading>
+              <Paragraph color="inverse">{exampleParagraph()}</Paragraph>
+            </Grid.Cell>
+          </Grid>
+        </Spotlight>
+        <Grid paddingVertical="medium">
+          <Grid.Cell span={{
+          narrow: 4,
+          medium: 6,
+          wide: 7
+        }} start={{
+          narrow: 1,
+          medium: 1,
+          wide: 2
+        }}>
+            <Heading className="ams-mb-sm" level={2}>
+              L2 Paragraaf titel
+            </Heading>
+            <Paragraph>{exampleParagraph()}</Paragraph>
+          </Grid.Cell>
+          <LinkGroupGridCells linkGroups={burgerzakenLinks.slice(4, 8)} />
+        </Grid>
+        <Spotlight className="ams-mb-xl" color="green">
+          <Grid paddingVertical="medium">
+            <Grid.Cell span={{
+            narrow: 4,
+            medium: 5,
+            wide: 7
+          }} start={{
+            narrow: 1,
+            medium: 1,
+            wide: 2
+          }}>
+              <Heading className="ams-mb-sm" color="inverse" level={4}>
+                Blijf op de hoogte
+              </Heading>
+              <Paragraph className="ams-mb-md" color="inverse">
+                Schrijf u nu in voor de Nieuwsbrief Amsterdam en ontvang wekelijks nieuws, tips en mooie verhalen over
+                de stad en uw stadsdeel.
+              </Paragraph>
+              <Link color="inverse" href="#" variant="standalone">
+                Ik wil de nieuwsbrief
+              </Link>
+            </Grid.Cell>
+            <Grid.Cell span={{
+            narrow: 4,
+            medium: 3,
+            wide: 4
+          }}>
+              <Heading className="ams-mb-sm" color="inverse" level={4}>
+                Deel deze pagina
+              </Heading>
+              <LinkList>
+                <LinkList.Link color="inverse" href="#" icon={FacebookIcon}>
+                  Facebook
+                </LinkList.Link>
+                <LinkList.Link color="inverse" href="#" icon={CameraIcon}>
+                  Instagram
+                </LinkList.Link>
+                <LinkList.Link color="inverse" href="#" icon={LinkedinIcon}>
+                  LinkedIn
+                </LinkList.Link>
+                <LinkList.Link color="inverse" href="#" icon={ChatBubbleIcon}>
+                  Whatsapp
+                </LinkList.Link>
+                <LinkList.Link color="inverse" href="#" icon={TwitterIcon}>
+                  X (Twitter)
+                </LinkList.Link>
+              </LinkList>
+            </Grid.Cell>
+          </Grid>
+        </Spotlight>
+      </>
+  }
+}`,...(B=(I=c.parameters)==null?void 0:I.docs)==null?void 0:B.source}}};const D=["Default","WithMap","SubnavigationPage"],J=Object.freeze(Object.defineProperty({__proto__:null,Default:t,SubnavigationPage:c,WithMap:g,__namedExportsOrder:D,default:M},Symbol.toStringTag,{value:"Module"}));export{J as N};
