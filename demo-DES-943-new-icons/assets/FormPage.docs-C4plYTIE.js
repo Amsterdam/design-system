4e1969e03605273a1123995efc2917b2d8dc6041
dlnr <niels@dlnr.dev>
2025-03-25 13:09:50 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@c0abc6a14ebf8e7385d4389de37cf6246d126df6 🚀

--- demo-DES-943-new-icons/assets/FormPage.docs-C4plYTIE.js
+++
@@ -1,5 +0,0 @@
-import{j as o}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as m}from"./index-CxbY7dAs.js";import{M as n}from"./index-B04--DbN.js";import{F as i}from"./FormPage.stories-BaVfkoFM.js";import"./index-yBjzXJbu.js";import"./index-G8LIXM5I.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-DDF_x5WF.js";import"./index-fNjTmf9T.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index.esm-DMwB2Kqj.js";import"./clsx-B-dksMZM.js";import"./index.esm-CxVKIgoj.js";import"./config-DI2-he4D.js";import"./Footer.stories-VnAU2qeG.js";/*@license CC0-1.0*/function e(t){const r={h1:"h1",p:"p",...m(),...t.components};return o.jsxs(o.Fragment,{children:[`
-`,`
-`,o.jsx(n,{of:i}),`
-`,o.jsx(r.h1,{id:"form-page",children:"Form page"}),`
-`,o.jsx(r.p,{children:"This simple example presents various types of fields."})]})}function X(t={}){const{wrapper:r}={...m(),...t.components};return r?o.jsx(r,{...t,children:o.jsx(e,{...t})}):e(t)}export{X as default};
---
+++ demo-DES-943-new-icons/assets/FormPage.docs-kB-qczv7.js
@@ -0,0 +1,5 @@
+import{j as o}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as m}from"./index-CxbY7dAs.js";import{M as n}from"./index-B21x_a69.js";import{F as i}from"./FormPage.stories-KRtEc8ar.js";import"./index-yBjzXJbu.js";import"./index-G8LIXM5I.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-rvxvc_Ek.js";import"./index-fNjTmf9T.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index.esm-Cmo-tIJj.js";import"./clsx-B-dksMZM.js";import"./index.esm-CR44Ykp0.js";import"./config-DZ2TZfgI.js";import"./Footer.stories-OuaN9czX.js";/*@license CC0-1.0*/function e(t){const r={h1:"h1",p:"p",...m(),...t.components};return o.jsxs(o.Fragment,{children:[`
+`,`
+`,o.jsx(n,{of:i}),`
+`,o.jsx(r.h1,{id:"form-page",children:"Form page"}),`
+`,o.jsx(r.p,{children:"This simple example presents various types of fields."})]})}function X(t={}){const{wrapper:r}={...m(),...t.components};return r?o.jsx(r,{...t,children:o.jsx(e,{...t})}):e(t)}export{X as default};
