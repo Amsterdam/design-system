c0fc472cb7f82c62b2f4740a8c356855d6e77985
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-03-26 13:26:55 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@7893081c8b1a2f52824d53cad30cc966420e984f 🚀

---
+++ demo-navigation-page-template/assets/MegaMenu.docs-BbH9KPTY.js
@@ -0,0 +1,12 @@
+import{j as t}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as r}from"./index-CxbY7dAs.js";import{M as s,h as i,P as a,j as m}from"./index-C31GION_.js";import{M as p,a as c}from"./MegaMenu.stories-CuFTHZjP.js";import{S as d}from"./StatusBadge-BqbyZIoz.js";import"./index-yBjzXJbu.js";import"./index-G8LIXM5I.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-CIG2eOaY.js";import"./index-fNjTmf9T.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index.esm-BW9UsdlY.js";import"./clsx-B-dksMZM.js";import"./index.esm-DlBD7MQf.js";import"./Badge-B-pFi2ST.js";const x=`<!-- @license CC0-1.0 -->
+
+# Mega Menu
+`;/*@license CC0-1.0*/function o(e){const n={h2:"h2",h3:"h3",...r(),...e.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(s,{of:p}),`
+`,t.jsx(d,{status:"deprecated",description:"Use child components in Header instead."}),`
+`,t.jsx(i,{children:x}),`
+`,t.jsx(a,{}),`
+`,t.jsx(n.h2,{id:"examples",children:"Examples"}),`
+`,t.jsx(n.h3,{id:"with-categories",children:"With categories"}),`
+`,t.jsx(m,{of:c})]})}function _(e={}){const{wrapper:n}={...r(),...e.components};return n?t.jsx(n,{...e,children:t.jsx(o,{...e})}):o(e)}export{_ as default};
---
+++ demo-navigation-page-template/assets/shortSize-YHt-sfIr.js
@@ -0,0 +1,4 @@
+/**
+ * @license EUPL-1.2+
+ * Copyright Gemeente Amsterdam
+ */const l={large:"l",medium:"m",none:"none",small:"s","x-large":"xl","x-small":"xs"};export{l as s};
