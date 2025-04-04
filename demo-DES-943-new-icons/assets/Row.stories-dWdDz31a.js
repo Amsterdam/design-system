e42e0cc1245721915eee9a2e64ff3d41d35c990c
dlnr <niels@dlnr.dev>
2025-04-04 10:55:49 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@0924b830ea2b88ba80fbdca5088b4d150b3d2089 🚀

---
+++ demo-DES-943-new-icons/assets/Row.stories-dWdDz31a.js
@@ -0,0 +1,46 @@
+import{j as e}from"./jsx-runtime-Cf8x2fCZ.js";import{A as O,H as T,L as _}from"./index.esm-qcg3he_U.js";import{r as C,R as L}from"./Row-DPyRQ-WY.js";import{c as B,m as H}from"./types-CjGn4A4T.js";const W=[e.jsx("div",{className:"ams-docs-item"},0),e.jsx("div",{className:"ams-docs-item"},1),e.jsx("div",{className:"ams-docs-item"},2)],z={title:"Components/Layout/Row",component:L,args:{children:W,className:"ams-docs-row"},argTypes:{align:{control:{labels:{undefined:"start"},type:"radio"},options:[void 0,...H]},alignVertical:{control:{labels:{undefined:"stretch"},type:"radio"},options:[void 0,...B]},className:{table:{disable:!0}},gap:{control:{labels:{undefined:"medium"},type:"radio"},options:[void 0,...C]}}},a={},r={args:{align:"evenly"}},s={args:{align:"end",alignVertical:"center",children:e.jsx(O,{label:"AB"})}},n={args:{align:"end",dir:"rtl"},argTypes:{dir:{table:{disable:!0}}}},i={args:{align:"between",alignVertical:"baseline",children:[e.jsx(T,{level:3,children:"An example heading"},1),e.jsx(_,{href:"#",children:"An example link"},2)],className:void 0,wrap:!0}},t={args:{alignVertical:"center"}},l={args:{children:Array.from(Array(4).keys()).map(E=>e.jsx("span",{className:"ams-docs-item",style:{flexBasis:"16rem"}},E)),wrap:!0}};var o,c,d;a.parameters={...a.parameters,docs:{...(o=a.parameters)==null?void 0:o.docs,source:{originalSource:"{}",...(d=(c=a.parameters)==null?void 0:c.docs)==null?void 0:d.source}}};var m,p,g;r.parameters={...r.parameters,docs:{...(m=r.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    align: 'evenly'
+  }
+}`,...(g=(p=r.parameters)==null?void 0:p.docs)==null?void 0:g.source}}};var u,A,y;s.parameters={...s.parameters,docs:{...(u=s.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    align: 'end',
+    alignVertical: 'center',
+    children: <Avatar label="AB" />
+  }
+}`,...(y=(A=s.parameters)==null?void 0:A.docs)==null?void 0:y.source}}};var b,f,h;n.parameters={...n.parameters,docs:{...(b=n.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    align: 'end',
+    dir: 'rtl'
+  },
+  argTypes: {
+    dir: {
+      table: {
+        disable: true
+      }
+    }
+  }
+}`,...(h=(f=n.parameters)==null?void 0:f.docs)==null?void 0:h.source}}};var x,S,v;i.parameters={...i.parameters,docs:{...(x=i.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  args: {
+    align: 'between',
+    alignVertical: 'baseline',
+    children: [<Heading key={1} level={3}>
+        An example heading
+      </Heading>, <Link href="#" key={2}>
+        An example link
+      </Link>],
+    className: undefined,
+    wrap: true
+  }
+}`,...(v=(S=i.parameters)==null?void 0:S.docs)==null?void 0:v.source}}};var j,w,k;t.parameters={...t.parameters,docs:{...(j=t.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  args: {
+    alignVertical: 'center'
+  }
+}`,...(k=(w=t.parameters)==null?void 0:w.docs)==null?void 0:k.source}}};var V,N,R;l.parameters={...l.parameters,docs:{...(V=l.parameters)==null?void 0:V.docs,source:{originalSource:`{
+  args: {
+    children: Array.from(Array(4).keys()).map(i => <span className="ams-docs-item" key={i} style={{
+      flexBasis: '16rem'
+    }} />),
+    wrap: true
+  }
+}`,...(R=(N=l.parameters)==null?void 0:N.docs)==null?void 0:R.source}}};const D=["Default","Alignment","EndAlignASingleChild","EndAlignASingleChildRtl","AlignOpposingTexts","VerticalAlignment","Wrapping"],q=Object.freeze(Object.defineProperty({__proto__:null,AlignOpposingTexts:i,Alignment:r,Default:a,EndAlignASingleChild:s,EndAlignASingleChildRtl:n,VerticalAlignment:t,Wrapping:l,__namedExportsOrder:D,default:z},Symbol.toStringTag,{value:"Module"}));export{r as A,s as E,q as R,t as V,l as W,n as a,i as b};
