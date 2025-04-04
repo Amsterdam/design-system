34ab0083a9089cef7d0b651f2049222454abc949
alimpens <37216945+alimpens@users.noreply.github.com>
2025-04-04 10:59:27 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@d0f7edc1bd3dcbba0973d4494fd6c9e90a158cd2 🚀

---
+++ demo-develop/assets/IconButton.docs-D-Cv_Wzj.js
@@ -0,0 +1,34 @@
+import{j as n}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as s}from"./index-CxbY7dAs.js";import{M as a,h as i,P as c,i as l,j as r}from"./index-DSncj1Og.js";import{I as u,C as d,a as h}from"./IconButton.stories-R5DM-pRq.js";import"./index-yBjzXJbu.js";import"./index-G8LIXM5I.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-BqdmNoGE.js";import"./index-fNjTmf9T.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./Icon-BiLXnzv6.js";import"./clsx-B-dksMZM.js";import"./index.esm-Bv5DmutW.js";import"./IconButton-B1qq4j6H.js";const m=`<!-- @license CC0-1.0 -->
+
+# Icon Button
+
+A button containing only an icon.
+
+## Guidelines
+
+- Use this button only if the icon is entirely clear and understandable.
+  Many icons are interpreted differently by different users.
+  Only use universally recognized icons, such as a close icon or a play icon.
+- Provide a label text that describes what the button does for screen reader users.
+
+## Relevant WCAG requirements
+
+The Icon Button follows [the same requirements and guidelines as a regular button](/docs/components-buttons-button--docs).
+`;/*@license CC0-1.0*/function t(e){const o={a:"a",h2:"h2",h3:"h3",h4:"h4",p:"p",...s(),...e.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(a,{of:u}),`
+`,n.jsx(i,{children:m}),`
+`,n.jsx(c,{}),`
+`,n.jsx(l,{}),`
+`,n.jsx(o.h2,{id:"examples",children:"Examples"}),`
+`,n.jsx(o.h3,{id:"on-a-coloured-background",children:"On a coloured background"}),`
+`,n.jsxs(o.p,{children:["An Icon Button on a coloured background must set ",n.jsx(o.a,{href:"?path=/docs/brand-design-tokens-colour--docs#pairing-foreground-with-background-colours",children:"the correct text colour"}),` to provide enough contrast.
+We have light and dark background colours, and Icon Buttons behave differently on each.`]}),`
+`,n.jsx(o.h4,{id:"contrast-colour",children:"Contrast colour"}),`
+`,n.jsx(o.p,{children:`Use the contrast colour on a lime, orange or yellow background.
+It will make the icon black.`}),`
+`,n.jsx(r,{of:d}),`
+`,n.jsx(o.h4,{id:"inverse-colour",children:"Inverse colour"}),`
+`,n.jsx(o.p,{children:`Use the inverse colour on an azure, blue, green, magenta or purple background.
+It will make the icon white.`}),`
+`,n.jsx(r,{of:h})]})}function D(e={}){const{wrapper:o}={...s(),...e.components};return o?n.jsx(o,{...e,children:n.jsx(t,{...e})}):t(e)}export{D as default};
