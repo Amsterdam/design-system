97b8498c041aeb619aa9e04520d1d4eeafb580a8
alimpens <a.limpens@amsterdam.nl>
2025-04-11 16:43:27 +0200

    Update link tests

--- packages/react/src/Link/Link.test.tsx
+++ packages/react/src/Link/Link.test.tsx
@@ -1,63 +1,57 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import { createRef } from 'react'
 import { Link } from './Link'
 import '@testing-library/jest-dom'
 
 describe('Link', () => {
-  const linktext = 'Linktext'
-
   it('renders with href attribute', () => {
-    const { container } = render(<Link href="#">{linktext}</Link>)
+    render(<Link href="#" />)
 
-    const link = container.querySelector('a:only-child')
+    const component = screen.getByRole('link')
 
-    expect(link).toBeInTheDocument()
-    expect(link).toHaveAttribute('href', '#')
+    expect(component).toBeInTheDocument()
+    expect(component).toHaveAttribute('href', '#')
+  })
+
+  it('renders a design system BEM class name', () => {
+    render(<Link href="#" />)
+
+    const component = screen.getByRole('link')
+
+    expect(component).toHaveClass('ams-link')
   })
 
   it('renders an extra class name', () => {
-    const { container } = render(<Link className="extra" />)
+    render(<Link className="extra" href="#" />)
 
-    const link = container.querySelector(':only-child')
+    const component = screen.getByRole('link')
 
-    expect(link).toHaveClass('ams-link extra')
+    expect(component).toHaveClass('ams-link extra')
   })
 
   it('renders the class name for contrast color', () => {
-    const { container } = render(
-      <Link color="contrast" href="#">
-        {linktext}
-      </Link>,
-    )
+    render(<Link color="contrast" href="#" />)
 
-    const link = container.querySelector('a:only-child')
+    const component = screen.getByRole('link')
 
-    expect(link).toHaveClass('ams-link ams-link--contrast')
+    expect(component).toHaveClass('ams-link ams-link--contrast')
   })
 
   it('renders the class name for inverse color', () => {
-    const { container } = render(
-      <Link color="inverse" href="#">
-        {linktext}
-      </Link>,
-    )
+    render(<Link color="inverse" href="#" />)
 
-    const link = container.querySelector('a:only-child')
+    const component = screen.getByRole('link')
 
-    expect(link).toHaveClass('ams-link ams-link--inverse')
+    expect(component).toHaveClass('ams-link ams-link--inverse')
   })
 
   it('supports ForwardRef in React', () => {
     const ref = createRef<HTMLAnchorElement>()
 
-    const { container } = render(
-      <Link href="https://example.com/" ref={ref}>
-        {'https://example.com/'}
-      </Link>,
-    )
+    render(<Link href="#" ref={ref} />)
 
-    const link = container.querySelector(':only-child')
+    const component = screen.getByRole('link')
 
-    expect(ref.current).toBe(link)
+    expect(ref.current).toBe(component)
   })
 })
