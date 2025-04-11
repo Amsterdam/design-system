4458a2b346619c9ba0c139bdcc313764322af0f6
Vincent Smedinga <v.smedinga@amsterdam.nl>
2025-04-11 12:55:50 +0200

    Reuse Image props for Card Image

--- packages/react/src/Card/CardImage.tsx
+++ packages/react/src/Card/CardImage.tsx
@@ -5,13 +5,12 @@
 
 import clsx from 'clsx'
 import { forwardRef } from 'react'
-import type { ForwardedRef, HTMLAttributes, PropsWithChildren } from 'react'
-import { Image, ImageProps } from '../Image'
-
-export type CardImageProps = ImageProps & PropsWithChildren<HTMLAttributes<HTMLImageElement>>
+import type { ForwardedRef } from 'react'
+import { Image } from '../Image'
+import type { ImageProps } from '../Image'
 
 export const CardImage = forwardRef(
-  ({ children, className, ...restProps }: CardImageProps, ref: ForwardedRef<HTMLImageElement>) => (
+  ({ children, className, ...restProps }: ImageProps, ref: ForwardedRef<HTMLImageElement>) => (
     <Image {...restProps} className={clsx('ams-card__image', className)} ref={ref}>
       {children}
     </Image>
