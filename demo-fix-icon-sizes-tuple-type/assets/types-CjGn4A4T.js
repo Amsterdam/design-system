c8e8c2ddc27e9bb73720b9606c9784fae686fae3
GitHub <github-action@users.noreply.github.com>
2025-04-11 13:18:17 +0000

    Remove feature branch folder

--- demo-fix-icon-sizes-tuple-type/assets/types-CjGn4A4T.js
+++
@@ -1,1 +0,0 @@
-const e=["9:16","3:4","1:1","4:3","16:9","16:5"],s=["start","center","baseline","end"],t=s.filter(n=>n!=="baseline"),o=["center","end","between","around","evenly"];export{t as a,e as b,s as c,o as m};
