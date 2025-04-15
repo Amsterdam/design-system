4f967a2973d7ea2ea3996d572af0ba3a3b8227cd
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-04-15 07:24:14 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@c42212a3c6efae2d96e1a473371840c263ddcd44 🚀

--- demo-news-index-page/assets/NewsIndexPage.docs-Cq4q2qsb.js
+++
@@ -1,5 +0,0 @@
-import{j as t}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as e}from"./index-CxbY7dAs.js";import{M as i}from"./index-CjIxQM1v.js";import{N as m}from"./NewsIndexPage.stories-CjHD9mHZ.js";import"./index-yBjzXJbu.js";import"./index-G8LIXM5I.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-hr2i43KT.js";import"./index-fNjTmf9T.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index.esm-BznwN7-N.js";import"./clsx-B-dksMZM.js";import"./index.esm-Bv5DmutW.js";import"./config-DTwjYblp.js";import"./Footer.stories-DDL4HEOZ.js";/*@license CC0-1.0*/function r(o){const n={h1:"h1",p:"p",...e(),...o.components};return t.jsxs(t.Fragment,{children:[`
-`,`
-`,t.jsx(i,{of:m}),`
-`,t.jsx(n.h1,{id:"news-index-page",children:"News Index page"}),`
-`,t.jsx(n.p,{children:"A list of news articles with a search form."})]})}function N(o={}){const{wrapper:n}={...e(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(r,{...o})}):r(o)}export{N as default};
---
+++ demo-news-index-page/assets/NewsIndexPage.docs-ttmK0pWw.js
@@ -0,0 +1,5 @@
+import{j as t}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as e}from"./index-CxbY7dAs.js";import{M as i}from"./index-DnfF9BIf.js";import{N as m}from"./NewsIndexPage.stories-C3VTaccR.js";import"./index-yBjzXJbu.js";import"./index-G8LIXM5I.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-5uwOpzrP.js";import"./index-fNjTmf9T.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index.esm-DfxnaTPX.js";import"./clsx-B-dksMZM.js";import"./index.esm-Bv5DmutW.js";import"./config-CCV7vMvp.js";import"./PageFooter.stories-Bl4fB6bb.js";/*@license CC0-1.0*/function r(o){const n={h1:"h1",p:"p",...e(),...o.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(i,{of:m}),`
+`,t.jsx(n.h1,{id:"news-index-page",children:"News Index page"}),`
+`,t.jsx(n.p,{children:"A list of news articles with a search form."})]})}function N(o={}){const{wrapper:n}={...e(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(r,{...o})}):r(o)}export{N as default};
