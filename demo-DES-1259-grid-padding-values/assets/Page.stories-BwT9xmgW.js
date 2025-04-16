509d779f3ae8a2f46f2652ec8b4a0de4ad024f87
GitHub <github-action@users.noreply.github.com>
2025-04-16 12:02:13 +0000

    Remove feature branch folder

--- demo-DES-1259-grid-padding-values/assets/Page.stories-BwT9xmgW.js
+++
@@ -1,5 +0,0 @@
-import{j as c}from"./jsx-runtime-Cf8x2fCZ.js";import{c as p}from"./clsx-B-dksMZM.js";import{r as d}from"./index-G8LIXM5I.js";const a=d.forwardRef(({children:s,className:m,...n},l)=>c.jsx("div",{...n,className:p("ams-page",m),ref:l,children:s}));a.displayName="Page";try{a.displayName="Page",a.__docgenInfo={description:"",displayName:"Page",props:{}}}catch{}const i={title:"Components/Containers/Page",component:a,args:{className:"ams-docs-page"},argTypes:{className:{table:{disable:!0}}},parameters:{layout:"fullscreen"}},e={args:{children:c.jsx("div",{className:"ams-docs-item"})}};var r,o,t;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  args: {
-    children: <div className="ams-docs-item" />
-  }
-}`,...(t=(o=e.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};const g=["Default"],y=Object.freeze(Object.defineProperty({__proto__:null,Default:e,__namedExportsOrder:g,default:i},Symbol.toStringTag,{value:"Module"}));export{y as P};
