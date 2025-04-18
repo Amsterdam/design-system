b6afc0d9401b5bffd2ea49189d60175d540ce0e3
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-04-18 12:01:46 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@a016c3aa969e4d61a7873bab9862cdd1201ba157 🚀

---
+++ demo-fix-breakout-text-colour/assets/Icon.docs-3YBeD3D5.js
@@ -0,0 +1,73 @@
+import{j as e}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as a}from"./index-CxbY7dAs.js";import{M as i,h as r,P as c,i as h,j as t}from"./index-DFxI4Rv6.js";import{I as d,W as l,a as m,b as p,c as x,d as u,e as g,f,S as j,g as y}from"./Icon.stories-DNe01gYk.js";import{S as b}from"./StatusBadge-DIRNee3V.js";import"./index-yBjzXJbu.js";import"./index-G8LIXM5I.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-5VAfVzzh.js";import"./index-fNjTmf9T.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index.esm-BPOQ1nhe.js";import"./clsx-B-dksMZM.js";import"./index.esm-Bv5DmutW.js";import"./Icon-BBW_x4VP.js";import"./Row-BdNYwl-m.js";import"./Paragraph-BDLeQ91t.js";import"./Badge-nq-uHbpZ.js";const w=`<!-- @license CC0-1.0 -->
+
+# Icon
+
+Icons are visual symbols used to represent ideas, themes, or actions.
+They communicate the message at a glance and draw attention to important (interactive) information.
+Always use an \`Icon\` component to encapsulate an SVG icon for proper styling and sizing.
+
+## Interactive elements
+
+Use an icon to support textual interactive elements like buttons and links.
+Always try to provide accompanying text for an icon.
+Only use icons without accompanying text if they adhere to international standards, such as menu (hamburger icon), search (magnifying glass), next, previous, play, pause, etc.
+
+## Colour
+
+Icons are black or white.
+When used as part of an interactive element, they are blue.
+When disabled, they are grey.
+They must adhere to the same contrast ratio for accessibility as typography against a background colour.
+The icon defaults to the colour of the container it is in.
+
+## Alignment
+
+Icons are aligned to the left of the text by default and vertically centred to the middle of the first line of text.
+
+## Size
+
+Icons have extra whitespace at their top and bottom to match the line height of text components.
+Specify the appropriate size to pair an icon with small or large body text or with a heading.
+[Read more in the typography documentation](/docs/brand-design-tokens-typography--docs).
+
+## Overview
+
+[You can find an overview of the available icons here](/docs/brand-assets-icons--docs).
+`;/*@license CC0-1.0*/function s(o){const n={a:"a",code:"code",h2:"h2",h3:"h3",p:"p",pre:"pre",...a(),...o.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(i,{of:d}),`
+`,e.jsx(b,{description:"The set of available icon names and the component API will change."}),`
+`,e.jsx(r,{children:w}),`
+`,e.jsx(n.h2,{id:"how-to-use",children:"How to use"}),`
+`,e.jsxs(n.p,{children:["Use the React Icon component together with a React SVG component from ",e.jsx(n.code,{children:"@amsterdam/design-system-react-icons"}),`.
+Import this SVG as follows:`]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-tsx",children:`import { EmailIcon } from "@amsterdam/design-system-react-icons";
+`})}),`
+`,e.jsx(n.p,{children:"Then, you can use it in the component like this:"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-tsx",children:`<Icon svg={EmailIcon} />
+`})}),`
+`,e.jsx(c,{}),`
+`,e.jsx(h,{}),`
+`,e.jsx(n.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(n.h3,{id:"with-body-text",children:"With body text"}),`
+`,e.jsx(n.p,{children:"An icon is as large as regular body text, e.g. paragraphs and lists, including its line height."}),`
+`,e.jsx(t,{of:l}),`
+`,e.jsxs(n.p,{children:["Use the ",e.jsx(n.code,{children:"size"})," prop to pair an icon with small or large body text."]}),`
+`,e.jsx(t,{of:m}),`
+`,e.jsx(t,{of:p}),`
+`,e.jsx(n.h3,{id:"with-a-heading",children:"With a heading"}),`
+`,e.jsxs(n.p,{children:["Use a ",e.jsx(n.code,{children:"size"})," of ",e.jsx(n.code,{children:"heading-3"})," – ",e.jsx(n.code,{children:"heading-6"})," to pair an icon with a ",e.jsx(n.a,{href:"/docs/components-text-heading--docs",children:"Heading"}),"."]}),`
+`,e.jsx(t,{of:x}),`
+`,e.jsx(t,{of:u}),`
+`,e.jsx(t,{of:g}),`
+`,e.jsx(t,{of:f}),`
+`,e.jsx(n.h3,{id:"square",children:"Square"}),`
+`,e.jsxs(n.p,{children:[`An icon is placed inside a container that has the same line height as text.
+This helps align the icon with the text.
+It also makes the container rectangular.
+Sometimes, it's better for the icon to be square, such as with the `,e.jsx(n.a,{href:"/docs/components-buttons-icon-button--docs",children:"Icon Button"}),"."]}),`
+`,e.jsx(t,{of:j}),`
+`,e.jsx(n.h3,{id:"inverse-colour",children:"Inverse colour"}),`
+`,e.jsx(n.p,{children:`Use the inverse colour if the Icon sits on a dark background.
+This ensures the colour of the icon provides enough contrast.`}),`
+`,e.jsx(t,{of:y})]})}function V(o={}){const{wrapper:n}={...a(),...o.components};return n?e.jsx(n,{...o,children:e.jsx(s,{...o})}):s(o)}export{V as default};
---
+++ demo-fix-breakout-text-colour/assets/Tabs.docs-BQha6tqX.js
@@ -0,0 +1,48 @@
+import{j as t}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as i}from"./index-CxbY7dAs.js";import{M as s,h,P as r,i as c,j as a}from"./index-DFxI4Rv6.js";import{T as l,W as p,a as d}from"./Tabs.stories-Bh1wMkMP.js";import"./index-yBjzXJbu.js";import"./index-G8LIXM5I.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-5VAfVzzh.js";import"./index-fNjTmf9T.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index.esm-BPOQ1nhe.js";import"./clsx-B-dksMZM.js";import"./index.esm-Bv5DmutW.js";import"./exampleContent-PGFPgakh.js";import"./useKeyboardFocus-BsBpr6qG.js";const b=`<!-- @license CC0-1.0 -->
+
+# Tabs
+
+Tabs are used to bundle related content in a compact overview within a page. Each tab has a short name, and these names indicate the relationship between the information displayed in each tab.
+
+## Guidelines
+
+- The content of each tab is viewable independently, not in the context of one another.
+- The content within each tab should have a similar structure.
+- Use when there is limited visual space and content needs to be divided into sections.
+- Each tab consists of a button and a panel.
+  A \`tab\` prop with a corresponding value connects them.
+
+You can navigate tabs with your keyboard:
+
+| key            | element                                        |
+| :------------- | :--------------------------------------------- |
+| Enter or Space | Open or close the tab that has the focus       |
+| Tab            | Go to the next element that can have focus     |
+| Shift + Tab    | Go to the next element that can have focus     |
+| Left arrow     | If the tabs have focus: go to the previous tab |
+| Right arrow    | If the tabs have focus: go to the next tab     |
+| Home           | If the tabs have focus: go to the first tab    |
+| End            | If the tabs have focus, go to the last tab     |
+
+### Caution
+
+Do not use tabs if the content in each tab functions just as well on separate pages.
+
+## References
+
+- [W3C - Tabs Pattern](https://www.w3.org/WAI/ARIA/apg/patterns/tabs/)
+- [MDN - Tab Aria Roles](https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/tab_role)
+`;/*@license CC0-1.0*/function o(n){const e={h2:"h2",h3:"h3",p:"p",...i(),...n.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(s,{of:l}),`
+`,t.jsx(h,{children:b}),`
+`,t.jsx(r,{}),`
+`,t.jsx(c,{}),`
+`,t.jsx(e.h2,{id:"examples",children:"Examples"}),`
+`,t.jsx(e.h3,{id:"with-initial-tab",children:"With initial tab"}),`
+`,t.jsx(e.p,{children:`The first tab is active by default.
+Another tab’s panel can be displayed initially as well.`}),`
+`,t.jsx(a,{of:p}),`
+`,t.jsx(e.h3,{id:"with-wide-content",children:"With wide content"}),`
+`,t.jsx(e.p,{children:"The Tab Buttons list and the content within the active Tab Panel will scroll horizontally when their width exceeds the available space."}),`
+`,t.jsx(a,{of:d})]})}function R(n={}){const{wrapper:e}={...i(),...n.components};return e?t.jsx(e,{...n,children:t.jsx(o,{...n})}):o(n)}export{R as default};
