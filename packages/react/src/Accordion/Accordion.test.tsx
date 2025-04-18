597d9b85b6d0b4d6e0132f07ae7f589778255a95
alimpens <a.limpens@amsterdam.nl>
2025-04-18 14:13:20 +0200

    Add test for Accordion keyboard focus

--- packages/react/src/Accordion/Accordion.test.tsx
+++ packages/react/src/Accordion/Accordion.test.tsx
@@ -1,7 +1,8 @@
-import { render } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import { createRef } from 'react'
 import { Accordion } from './Accordion'
 import '@testing-library/jest-dom'
+import { KeyboardKeys } from '../common/useKeyboardFocus'
 
 describe('Accordion', () => {
   it('renders an accordion', () => {
@@ -19,6 +20,35 @@ describe('Accordion', () => {
     const accordion = container.querySelector('.ams-accordion')
 
     expect(accordion).toHaveClass('test')
+  })
+
+  it('sets focus on Accordion buttons when using arrow keys', () => {
+    const { container } = render(
+      <Accordion headingLevel={1}>
+        <Accordion.Section label="one" />
+        <Accordion.Section label="two" />
+        <Accordion.Section label="three" />
+      </Accordion>,
+    )
+
+    const firstChild = container.firstChild as HTMLElement
+    const firstButton = screen.getByRole('button', { name: 'one' })
+    const thirdButton = screen.getByRole('button', { name: 'three' })
+
+    fireEvent.keyDown(firstChild, {
+      key: KeyboardKeys.ArrowDown,
+    })
+
+    expect(firstButton).toHaveFocus()
+
+    Array.from(Array(2).keys()).forEach(() => {
+      fireEvent.keyDown(firstChild, {
+        key: KeyboardKeys.ArrowDown,
+      })
+    })
+
+    expect(thirdButton).toHaveFocus()
+    expect(firstButton).not.toHaveFocus()
   })
 
   it('supports ForwardRef in React', () => {
