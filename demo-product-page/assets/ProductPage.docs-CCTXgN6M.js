7f8ca745dd2fc912ad673dfe4fde0a237f4c3055
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-04-23 05:57:21 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@29ff50171348cfe1b3b64e918babf28fcc7cb148 🚀

---
+++ demo-product-page/assets/ProductPage.docs-CCTXgN6M.js
@@ -0,0 +1,5 @@
+import{j as t}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as i}from"./index-CxbY7dAs.js";import{M as m}from"./index-DOgfQBhd.js";import{P as e}from"./ProductPage.stories-By1mhF0A.js";import"./index-yBjzXJbu.js";import"./index-G8LIXM5I.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-D2-DfqDz.js";import"./index-fNjTmf9T.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index.esm-CcowrvHw.js";import"./clsx-B-dksMZM.js";import"./index.esm-Bv5DmutW.js";import"./config-DqhbBgPK.js";import"./PageFooter.stories-DbZ8nl5T.js";/*@license CC0-1.0*/function n(o){const r={h1:"h1",p:"p",...i(),...o.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(m,{of:e}),`
+`,t.jsx(r.h1,{id:"product-page",children:"Product page"}),`
+`,t.jsx(r.p,{children:"TODO Description"})]})}function X(o={}){const{wrapper:r}={...i(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(n,{...o})}):n(o)}export{X as default};
