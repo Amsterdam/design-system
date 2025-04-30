b7b57ca2cb758de58aa75ec4eae22f8e4b4a8a4f
alimpens <37216945+alimpens@users.noreply.github.com>
2025-04-30 15:09:39 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@fad36df7b6681b213d9918cb36194e27b40889e9 🚀

---
+++ demo-DES-955-header-logo-forced-colors/assets/VisuallyHidden.docs-BtwuGzsx.js
@@ -0,0 +1,20 @@
+import{j as n}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as t}from"./index-CxbY7dAs.js";import{M as r,h as i,P as a,i as m}from"./index-Dt0k15us.js";import{V as l}from"./VisuallyHidden.stories-BM8qlgvn.js";import"./index-yBjzXJbu.js";import"./index-G8LIXM5I.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-BEIyBC56.js";import"./index-fNjTmf9T.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index.esm-CcoUaelU.js";import"./clsx-B-dksMZM.js";import"./index.esm-CblzCDI2.js";const u=`<!-- @license CC0-1.0 -->
+
+# Visually Hidden
+
+Hides content from sighted users but keeps it accessible to non-visual user agents, such as screen readers.
+
+## Class name
+
+\`ams-visually-hidden\`
+
+## Guidelines
+
+- In most cases, visually available content should be accessible to non-visual user agents and vice versa.
+  Elaborate instructions or guidance read only by non-visual user agents can do more harm than good.
+`;/*@license CC0-1.0*/function o(s){return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(r,{of:l}),`
+`,n.jsx(i,{children:u}),`
+`,n.jsx(a,{}),`
+`,n.jsx(m,{})]})}function E(s={}){const{wrapper:e}={...t(),...s.components};return e?n.jsx(e,{...s,children:n.jsx(o,{...s})}):o()}export{E as default};
