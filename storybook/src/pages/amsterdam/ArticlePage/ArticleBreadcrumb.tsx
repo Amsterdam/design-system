b2db6188d22c8b5f1c731cf7cce3d61f61521d79
Niels Roozemond <n.roozemond@amsterdam.nl>
2025-05-01 17:03:29 +0200

    Before breadcrumbs and medium util instead of grid prop?

--- storybook/src/pages/amsterdam/ArticlePage/ArticleBreadcrumb.tsx
+++ storybook/src/pages/amsterdam/ArticlePage/ArticleBreadcrumb.tsx
@@ -6,9 +6,9 @@
 import { Breadcrumb, Grid } from '@amsterdam/design-system-react'
 
 export const ArticleBreadcrumb = () => (
-  <Grid paddingTop="large">
+  <Grid>
     <Grid.Cell span={{ narrow: 4, medium: 6, wide: 8 }} start={{ narrow: 1, medium: 1, wide: 2 }}>
-      <Breadcrumb>
+      <Breadcrumb className="ams-mb-m">
         <Breadcrumb.Link href="#">Home</Breadcrumb.Link>
         <Breadcrumb.Link href="#">Nieuws</Breadcrumb.Link>
       </Breadcrumb>
