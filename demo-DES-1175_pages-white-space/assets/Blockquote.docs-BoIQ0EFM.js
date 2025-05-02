451fd7693023c665355dcf0fb3f07a5d6b8e7b86
dlnr <niels@dlnr.dev>
2025-05-02 12:05:33 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@2a45a6c613a1a5f0ef048da0ffaa380ca824a132 🚀

---
+++ demo-DES-1175_pages-white-space/assets/Blockquote.docs-BoIQ0EFM.js
@@ -0,0 +1,27 @@
+import{j as t}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as r}from"./index-CxbY7dAs.js";import{M as s,h as i,P as a,i as h,j as m}from"./index-CZ-yTYrH.js";import{B as u,I as c}from"./Blockquote.stories-CTmH7a1Q.js";import"./index-yBjzXJbu.js";import"./index-G8LIXM5I.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-1XXdL0oA.js";import"./index-fNjTmf9T.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./exampleContent-r-zRGsZY.js";import"./clsx-B-dksMZM.js";const l=`<!-- @license CC0-1.0 -->
+
+# Blockquote
+
+We use blockquotes to emphasize citations.
+A quote appears differently from the main text to make it stand out.
+
+## Guidelines
+
+- Quote someone’s literal written or oral text.
+- A quote has a width of half or a whole column of text.
+- The component adds the correct quotation marks.
+
+## Relevant WCAG requirements
+
+- [WCAG 3.1.2](https://www.w3.org/WAI/WCAG21/Understanding/language-of-parts.html): if the citation is in a language other than that of the surrounding content, indicate so through the \`lang\` attribute.
+`;/*@license CC0-1.0*/function e(n){const o={h2:"h2",h3:"h3",p:"p",...r(),...n.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(s,{of:u}),`
+`,t.jsx(i,{children:l}),`
+`,t.jsx(a,{}),`
+`,t.jsx(h,{}),`
+`,t.jsx(o.h2,{id:"examples",children:"Examples"}),`
+`,t.jsx(o.h3,{id:"inverse-colour",children:"Inverse colour"}),`
+`,t.jsx(o.p,{children:`Use the inverse colour if the Blockquote sits on a dark background.
+This ensures the colour of the text provides enough contrast.`}),`
+`,t.jsx(m,{of:c})]})}function W(n={}){const{wrapper:o}={...r(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(e,{...n})}):e(n)}export{W as default};
