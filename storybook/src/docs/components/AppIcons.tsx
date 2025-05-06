c96e536f98fc035a349f717028cd7f289824d7db
Niels Roozemond <n.roozemond@amsterdam.nl>
2025-05-06 14:53:40 +0200

    fix(AspectRatio): Update README and AppIcons to enforce aspect ratio class on images

--- storybook/src/docs/components/AppIcons.tsx
+++ storybook/src/docs/components/AppIcons.tsx
@@ -8,7 +8,7 @@ import { Image } from '@amsterdam/design-system-react'
 export const AppleTouchIcon = () => (
   <div className="ams-docs-gallery">
     <figure className="ams-docs-figure">
-      <Image alt="" height={180} src="favicon/apple-touch-icon.png" width={180} />
+      <Image alt="" className="ams-aspect-ratio-1-1" height={180} src="favicon/apple-touch-icon.png" width={180} />
       <figcaption>apple-touch-icon.png (180px)</figcaption>
     </figure>
   </div>
@@ -17,19 +17,19 @@ export const AppleTouchIcon = () => (
 export const Favicon = () => (
   <div className="ams-docs-gallery">
     <figure className="ams-docs-figure">
-      <Image alt="" height={16} src="favicon/favicon.ico" width={16} />
+      <Image alt="" className="ams-aspect-ratio-1-1" height={16} src="favicon/favicon.ico" width={16} />
       <figcaption>favicon.ico (16px)</figcaption>
     </figure>
     <figure className="ams-docs-figure">
-      <Image alt="" height={32} src="favicon/favicon.ico" width={32} />
+      <Image alt="" className="ams-aspect-ratio-1-1" height={32} src="favicon/favicon.ico" width={32} />
       <figcaption>favicon.ico (32px)</figcaption>
     </figure>
     <figure className="ams-docs-figure">
-      <Image alt="" height={48} src="favicon/favicon.ico" width={48} />
+      <Image alt="" className="ams-aspect-ratio-1-1" height={48} src="favicon/favicon.ico" width={48} />
       <figcaption>favicon.ico (48px)</figcaption>
     </figure>
     <figure className="ams-docs-figure">
-      <Image alt="" height={64} src="favicon/favicon.ico" width={64} />
+      <Image alt="" className="ams-aspect-ratio-1-1" height={64} src="favicon/favicon.ico" width={64} />
       <figcaption>favicon.ico (64px)</figcaption>
     </figure>
   </div>
@@ -38,7 +38,7 @@ export const Favicon = () => (
 export const SvgIcon = () => (
   <div className="ams-docs-gallery">
     <figure className="ams-docs-figure">
-      <Image alt="" height={64} src="favicon/icon.svg" width={64} />
+      <Image alt="" className="ams-aspect-ratio-1-1" height={64} src="favicon/icon.svg" width={64} />
       <figcaption>icon.svg (64px)</figcaption>
     </figure>
   </div>
@@ -47,11 +47,11 @@ export const SvgIcon = () => (
 export const WebAppIcons = () => (
   <div className="ams-docs-gallery">
     <figure className="ams-docs-figure">
-      <Image alt="" height={192} src="app-icons/icon-192.png" width={192} />
+      <Image alt="" className="ams-aspect-ratio-1-1" height={192} src="app-icons/icon-192.png" width={192} />
       <figcaption>icon-192.png (192px)</figcaption>
     </figure>
     <figure className="ams-docs-figure">
-      <Image alt="" height={512} src="app-icons/icon-512.png" width={512} />
+      <Image alt="" className="ams-aspect-ratio-1-1" height={512} src="app-icons/icon-512.png" width={512} />
       <figcaption>icon-512.png (512px)</figcaption>
     </figure>
   </div>
