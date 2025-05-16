1345298d62fe5e3de0dd0222e6ab5d34d87b942a
GitHub <github-action@users.noreply.github.com>
2025-05-16 21:05:45 +0000

    Remove feature branch folder

--- demo-DES-1107-add-logo-link-component-prop/assets/FormPage.docs-CpbH6lzs.js
+++
@@ -1,5 +0,0 @@
-import{j as o}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as m}from"./index-CxbY7dAs.js";import{M as n}from"./index-CQEdx3oS.js";import{F as i}from"./FormPage.stories-BKvdQ354.js";import"./index-yBjzXJbu.js";import"./index-G8LIXM5I.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-BFxyy_52.js";import"./index-fNjTmf9T.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index.esm-CeMsXqEC.js";import"./clsx-B-dksMZM.js";import"./index.esm-CblzCDI2.js";import"./config-CQN81RXu.js";import"./PageFooter.stories-BQe3eZ1g.js";/*@license CC0-1.0*/function e(t){const r={h1:"h1",p:"p",...m(),...t.components};return o.jsxs(o.Fragment,{children:[`
-`,`
-`,o.jsx(n,{of:i}),`
-`,o.jsx(r.h1,{id:"form-page",children:"Form page"}),`
-`,o.jsx(r.p,{children:"This simple example presents various types of fields."})]})}function X(t={}){const{wrapper:r}={...m(),...t.components};return r?o.jsx(r,{...t,children:o.jsx(e,{...t})}):e(t)}export{X as default};
