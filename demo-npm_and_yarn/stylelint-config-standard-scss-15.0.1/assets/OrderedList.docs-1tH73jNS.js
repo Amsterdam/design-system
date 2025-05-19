0193ccebf5b60ced88f09f6f035abeb5a585e392
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-05-19 12:37:11 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@b4e240f3f0cca0d5cf5f4336b46f9df4adeda735 🚀

---
+++ demo-npm_and_yarn/stylelint-config-standard-scss-15.0.1/assets/OrderedList.docs-1tH73jNS.js
@@ -0,0 +1,49 @@
+import{j as e}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as i}from"./index-CxbY7dAs.js";import{M as o,f as a,P as h,h as l,i as t}from"./index-_wUqkvc9.js";import{O as d,T as c,S as m,D as p,W as x,I as u,a as j}from"./OrderedList.stories-B0WnuaN7.js";import"./index-yBjzXJbu.js";import"./index-G8LIXM5I.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-DI6xiKrc.js";import"./index-fNjTmf9T.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index.esm-CeMsXqEC.js";import"./clsx-B-dksMZM.js";import"./index.esm-CblzCDI2.js";import"./exampleContent-r-zRGsZY.js";const f=`<!-- @license CC0-1.0 -->
+
+# Ordered List
+
+An ordered list is a vertical group of related content with an order.
+This list can have 2 levels.
+The first level has numbers as list markers.
+The second level uses letters in alphabetical order.
+List items indent their text by a fixed distance.
+
+## Design
+
+The list uses ascending numbers as markers, providing enough space for numbers up to 99.
+Extra white space between items enhances the distinction, mainly when they consist of multiple lines of text.
+`;/*@license CC0-1.0*/function r(s){const n={code:"code",h2:"h2",h3:"h3",p:"p",...i(),...s.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(o,{of:d}),`
+`,e.jsx(a,{children:f}),`
+`,e.jsx(h,{}),`
+`,e.jsx(l,{}),`
+`,e.jsx(n.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(n.h3,{id:"two-levels",children:"Two levels"}),`
+`,e.jsx(n.p,{children:`A list may have one nested level.
+In this case, lowercase letters are used as markers.`}),`
+`,e.jsx(t,{of:c}),`
+`,e.jsx(n.h3,{id:"starting-number",children:"Starting number"}),`
+`,e.jsxs(n.p,{children:[`When necessary, the list can start with a higher number.
+Use the HTML attribute `,e.jsx(n.code,{children:"start"}),"."]}),`
+`,e.jsx(t,{of:m}),`
+`,e.jsx(n.h3,{id:"reverse-order",children:"Reverse order"}),`
+`,e.jsxs(n.p,{children:[`The numbers can also be in reverse order.
+Use the HTML attribute `,e.jsx(n.code,{children:"reversed"}),"."]}),`
+`,e.jsx(t,{of:p}),`
+`,e.jsx(n.h3,{id:"without-markers",children:"Without markers"}),`
+`,e.jsx(n.p,{children:`An overview of articles or search results can be presented as an ordered can be seen as an unordered list.
+In such cases, apply the list semantics while hiding markers.
+This helps screen readers understand and navigate the list.`}),`
+`,e.jsxs(n.p,{children:[`Note: each item typically contains a container with headings and other text components in this variant.
+Therefore, it does not define typography for the items and the `,e.jsx(n.code,{children:"color"})," prop has no effect."]}),`
+`,e.jsx(n.p,{children:"For example, here are three news articles:"}),`
+`,e.jsx(t,{of:x}),`
+`,e.jsx(n.h3,{id:"inverse-colour",children:"Inverse colour"}),`
+`,e.jsx(n.p,{children:`Use the inverse colour if the List sits on a dark background.
+This ensures the colour of the text provides enough contrast.
+When nesting lists, set the prop on all lists.`}),`
+`,e.jsx(t,{of:u}),`
+`,e.jsx(n.h3,{id:"small-text",children:"Small text"}),`
+`,e.jsx(n.p,{children:"Use a list with a smaller font size in form descriptions and captions and the like."}),`
+`,e.jsx(t,{of:j})]})}function A(s={}){const{wrapper:n}={...i(),...s.components};return n?e.jsx(n,{...s,children:e.jsx(r,{...s})}):r(s)}export{A as default};
---
+++ demo-npm_and_yarn/stylelint-config-standard-scss-15.0.1/assets/Tabs.docs-CF6CUhvt.js
@@ -0,0 +1,61 @@
+import{j as e}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as i}from"./index-CxbY7dAs.js";import{M as s,f as h,P as r,h as c,i as a}from"./index-_wUqkvc9.js";import{T as l,W as d,a as p,P as b}from"./Tabs.stories-D4J1CGeU.js";import"./index-yBjzXJbu.js";import"./index-G8LIXM5I.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-DI6xiKrc.js";import"./index-fNjTmf9T.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index.esm-CeMsXqEC.js";import"./clsx-B-dksMZM.js";import"./index.esm-CblzCDI2.js";import"./exampleContent-r-zRGsZY.js";import"./useKeyboardFocus-BsBpr6qG.js";const m=`<!-- @license CC0-1.0 -->
+
+# Tabs
+
+Tabs are used to bundle related content in a compact overview within a page. Each tab has a short name, and these names indicate the relationship between the information displayed in each tab.
+
+## Guidelines
+
+- The content of each tab is viewable independently, not in the context of one another.
+- The content within each tab should have a similar structure.
+- Use when there is limited visual space and content needs to be divided into sections.
+- Each tab consists of a button and a panel.
+  The button has an \`aria-controls\` attribute that matches the \`id\` of the associated panel.
+
+You can navigate tabs with your keyboard:
+
+| key            | element                                        |
+| :------------- | :--------------------------------------------- |
+| Enter or Space | Open or close the tab that has the focus       |
+| Tab            | Go to the next element that can have focus     |
+| Shift + Tab    | Go to the next element that can have focus     |
+| Left arrow     | If the tabs have focus: go to the previous tab |
+| Right arrow    | If the tabs have focus: go to the next tab     |
+| Home           | If the tabs have focus: go to the first tab    |
+| End            | If the tabs have focus, go to the last tab     |
+
+### Caution
+
+Do not use tabs if the content in each tab functions just as well on separate pages.
+
+## References
+
+- [W3C - Tabs Pattern](https://www.w3.org/WAI/ARIA/apg/patterns/tabs/)
+- [MDN - Tab Aria Roles](https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/tab_role)
+`;/*@license CC0-1.0*/function o(t){const n={code:"code",h2:"h2",h3:"h3",p:"p",pre:"pre",...i(),...t.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(s,{of:l}),`
+`,e.jsx(h,{children:m}),`
+`,e.jsx(r,{}),`
+`,e.jsx(c,{}),`
+`,e.jsx(n.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(n.h3,{id:"with-initial-tab",children:"With initial tab"}),`
+`,e.jsx(n.p,{children:`The first tab is active by default.
+Another tab’s panel can be displayed initially as well.`}),`
+`,e.jsx(a,{of:d}),`
+`,e.jsx(n.h3,{id:"with-wide-content",children:"With wide content"}),`
+`,e.jsx(n.p,{children:"The Tab Buttons list and the content within the active Tab Panel will scroll horizontally when their width exceeds the available space."}),`
+`,e.jsx(a,{of:p}),`
+`,e.jsx(n.h3,{id:"prevent-tabs-from-changing",children:"Prevent tabs from changing"}),`
+`,e.jsxs(n.p,{children:["The Tab Button ",e.jsx(n.code,{children:"onChange"})," prop can be used with ",e.jsx(n.code,{children:"event.preventDefault"}),` to stop tabs from changing when clicked.
+This is helpful to keep users from switching tabs until specific conditions are met.`]}),`
+`,e.jsx(n.p,{children:"You can use this click handler for example:"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-jsx",children:`const handleClick = (event) => {
+  // eslint-disable-next-line no-alert
+  const confirmLeave = window.confirm("You have unsaved changes. Do you want to leave?");
+  if (!confirmLeave) {
+    event.preventDefault();
+  }
+};
+`})}),`
+`,e.jsx(a,{of:b})]})}function R(t={}){const{wrapper:n}={...i(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(o,{...t})}):o(t)}export{R as default};
