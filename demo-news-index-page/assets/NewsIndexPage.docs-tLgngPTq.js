7d9f7781f9516b74654fe627c1d17da989e0db12
GitHub <github-action@users.noreply.github.com>
2025-05-22 07:32:51 +0000

    Remove feature branch folder

--- demo-news-index-page/assets/NewsIndexPage.docs-tLgngPTq.js
+++
@@ -1,5 +0,0 @@
-import{j as t}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as e}from"./index-CxbY7dAs.js";import{M as i}from"./index-CTTRhuP0.js";import{N as m}from"./NewsIndexPage.stories-45BJ2KhF.js";import"./index-yBjzXJbu.js";import"./index-G8LIXM5I.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-Cc2nTNPc.js";import"./index-fNjTmf9T.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index.esm-DaWnEfaL.js";import"./clsx-B-dksMZM.js";import"./index.esm-CblzCDI2.js";import"./config-CohcKicg.js";import"./PageFooter.stories-CPuAeIx2.js";/*@license CC0-1.0*/function r(o){const n={h1:"h1",p:"p",...e(),...o.components};return t.jsxs(t.Fragment,{children:[`
-`,`
-`,t.jsx(i,{of:m}),`
-`,t.jsx(n.h1,{id:"news-index-page",children:"News Index page"}),`
-`,t.jsx(n.p,{children:"A list of news articles with a search form."})]})}function N(o={}){const{wrapper:n}={...e(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(r,{...o})}):r(o)}export{N as default};
