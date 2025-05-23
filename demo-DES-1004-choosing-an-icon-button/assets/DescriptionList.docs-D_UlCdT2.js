dde0a28d2eeb056c99ce5097ebdaa770769d5447
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-05-23 11:28:17 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@2187f81efe74a339669030b8b6c553a55e7884b7 🚀

---
+++ demo-DES-1004-choosing-an-icon-button/assets/DescriptionList.docs-D_UlCdT2.js
@@ -0,0 +1,39 @@
+import{j as e}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as o}from"./index-CxbY7dAs.js";import{M as r,f as l,P as c,h as p,i}from"./index-DvYTNEGE.js";import{D as a,M as h,a as d,R as m,I as u}from"./DescriptionList.stories-Bm8WTIKe.js";import"./index-yBjzXJbu.js";import"./index-G8LIXM5I.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-CwYkDMZx.js";import"./index-fNjTmf9T.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index.esm-CeMsXqEC.js";import"./clsx-B-dksMZM.js";import"./index.esm-CblzCDI2.js";const x=`<!-- @license CC0-1.0 -->
+
+# Description List
+
+A collection of terms and their descriptions.
+
+## Design
+
+In a narrow window, descriptions appear indented below their term.
+After that, they display in a two-column layout.
+The column for the terms is as wide as the longest term, without wrapping.
+Its width can be adjusted to be ‘large’ (50%), ‘medium’ (33%), or ‘small’ (20%), which also allows the terms to wrap.
+
+The term is set in bold text.
+
+## References
+
+- [MDN: \`<dl>\`: The Description List element](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/dl)
+- [WCAG: Using description lists](https://www.w3.org/WAI/WCAG22/Techniques/html/H40)
+`;/*@license CC0-1.0*/function s(t){const n={code:"code",h2:"h2",h3:"h3",p:"p",...o(),...t.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(r,{of:a}),`
+`,e.jsx(l,{children:x}),`
+`,e.jsx(c,{}),`
+`,e.jsx(p,{}),`
+`,e.jsx(n.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(n.h3,{id:"multiple-descriptions",children:"Multiple descriptions"}),`
+`,e.jsx(n.p,{children:"A term may have multiple descriptions."}),`
+`,e.jsx(i,{of:h}),`
+`,e.jsx(n.h3,{id:"multiple-terms",children:"Multiple terms"}),`
+`,e.jsxs(n.p,{children:["If multiple terms share a single description, group them in a ",e.jsx(n.code,{children:"DescriptionList.Section"})," component to ensure proper grid layout."]}),`
+`,e.jsx(i,{of:d}),`
+`,e.jsx(n.h3,{id:"rich-description",children:"Rich description"}),`
+`,e.jsx(n.p,{children:"A description can include rich content such as inline formatting, links, paragraphs, and even lists."}),`
+`,e.jsx(i,{of:m}),`
+`,e.jsx(n.h3,{id:"inverse-colour",children:"Inverse colour"}),`
+`,e.jsx(n.p,{children:`Use the inverse colour if the Description List sits on a dark background.
+This ensures the colour of the text provides enough contrast.`}),`
+`,e.jsx(i,{of:u})]})}function R(t={}){const{wrapper:n}={...o(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(s,{...t})}):s(t)}export{R as default};
