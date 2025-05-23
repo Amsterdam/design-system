1eb04e74859ded459ac99713e95f2a36b43b4a34
dlnr <niels@dlnr.dev>
2025-05-23 13:44:51 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@38c5057b04e9b70afa1e715cffc7a0c3ab4b0590 🚀

--- demo-DES-943-new-icons/assets/FormPage.docs-DNxzShbo.js
+++
@@ -1,5 +0,0 @@
-import{j as o}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as m}from"./index-CxbY7dAs.js";import{M as n}from"./index-C9SGyaHm.js";import{F as i}from"./FormPage.stories-B9E9B3nT.js";import"./index-yBjzXJbu.js";import"./index-G8LIXM5I.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-DE0ClFSO.js";import"./index-fNjTmf9T.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index.esm-vK_gfqzP.js";import"./clsx-B-dksMZM.js";import"./index.esm-D4WVF_4K.js";import"./config-D08Z5aLw.js";import"./PageFooter.stories-BC6j564U.js";/*@license CC0-1.0*/function e(t){const r={h1:"h1",p:"p",...m(),...t.components};return o.jsxs(o.Fragment,{children:[`
-`,`
-`,o.jsx(n,{of:i}),`
-`,o.jsx(r.h1,{id:"form-page",children:"Form page"}),`
-`,o.jsx(r.p,{children:"This simple example presents various types of fields."})]})}function X(t={}){const{wrapper:r}={...m(),...t.components};return r?o.jsx(r,{...t,children:o.jsx(e,{...t})}):e(t)}export{X as default};
--- demo-DES-943-new-icons/assets/NavigationPage.docs-Csyv6bJn.js
+++
@@ -1,6 +0,0 @@
-import{j as t}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as r}from"./index-CxbY7dAs.js";import{M as e}from"./index-C9SGyaHm.js";import{N as p}from"./NavigationPage.stories-BGY9pk5k.js";import"./index-yBjzXJbu.js";import"./index-G8LIXM5I.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-DE0ClFSO.js";import"./index-fNjTmf9T.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index.esm-vK_gfqzP.js";import"./clsx-B-dksMZM.js";import"./index.esm-D4WVF_4K.js";import"./exampleContent-r-zRGsZY.js";import"./config-D08Z5aLw.js";import"./PageFooter.stories-BC6j564U.js";/*@license CC0-1.0*/function i(o){const n={h1:"h1",p:"p",...r(),...o.components};return t.jsxs(t.Fragment,{children:[`
-`,`
-`,t.jsx(e,{of:p}),`
-`,t.jsx(n.h1,{id:"navigation-page",children:"Navigation page"}),`
-`,t.jsx(n.p,{children:`A navigation page does not have much content of its own.
-Instead, it presents groups of links to various pages related to a specific subject.`})]})}function X(o={}){const{wrapper:n}={...r(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(i,{...o})}):i(o)}export{X as default};
