39959e127c3f56f5c75be6821227a55584f03b31
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-06-06 12:09:31 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@50366b331274d90b187890cf3ae7d68d69481fe1 🚀

--- demo-DES-1299_icon-groups-galleries/assets/NavigationPage.docs-B7Kz71Tz.js
+++
@@ -1,6 +0,0 @@
-import{j as t}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as r}from"./index-CxbY7dAs.js";import{M as e}from"./index-fJROJn8R.js";import{N as p}from"./NavigationPage.stories-BNmTIF_p.js";import"./index-yBjzXJbu.js";import"./index-G8LIXM5I.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-CYpvou0F.js";import"./index-fNjTmf9T.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index.esm-D1IbH_61.js";import"./clsx-B-dksMZM.js";import"./index.esm-jhuYF6FN.js";import"./exampleContent-r-zRGsZY.js";import"./config-DXKQMFOA.js";import"./PageFooter.stories-CnAG0okQ.js";/*@license CC0-1.0*/function i(o){const n={h1:"h1",p:"p",...r(),...o.components};return t.jsxs(t.Fragment,{children:[`
-`,`
-`,t.jsx(e,{of:p}),`
-`,t.jsx(n.h1,{id:"navigation-page",children:"Navigation page"}),`
-`,t.jsx(n.p,{children:`A navigation page does not have much content of its own.
-Instead, it presents groups of links to various pages related to a specific subject.`})]})}function X(o={}){const{wrapper:n}={...r(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(i,{...o})}):i(o)}export{X as default};
