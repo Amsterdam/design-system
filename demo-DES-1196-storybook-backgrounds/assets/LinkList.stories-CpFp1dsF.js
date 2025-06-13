fdd8eee328806cd5d543bffdb0b99eb68b5285bd
RubenSibon <mail@rubensibon.nl>
2025-06-13 12:43:06 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@ea55a460e51fcae5883a3ef04519989e1441c1e1 🚀

---
+++ demo-DES-1196-storybook-backgrounds/assets/LinkList.stories-CpFp1dsF.js
@@ -0,0 +1,82 @@
+import{j as n}from"./jsx-runtime-Cf8x2fCZ.js";import{r as D}from"./index-G8LIXM5I.js";import{I as k,f as P,g as F,h as H,i as W}from"./index.esm-jhuYF6FN.js";import{backgroundColourOptions as M,backgroundColourOptionsLight as N,backgroundColourOptionsDark as R,getForegroundColor as q}from"./preview-DFUMt3w9.js";import{a as G}from"./exampleContent-r-zRGsZY.js";import{L as e}from"./LinkList-CY5A71mH.js";const E=G(),J={title:"Components/Navigation/Link List",component:e};e.Link;const p={args:{children:E.map((r,o)=>n.jsx(e.Link,{href:"#",children:r},o))}},d={args:{children:E[0],href:"#",icon:P},argTypes:{color:{control:{labels:{undefined:"default"},type:"radio"},options:[void 0,"contrast","inverse"]},icon:{control:{labels:{undefined:"none"},type:"select"},mapping:k,options:[void 0,...Object.keys(k)]},size:{control:{labels:{undefined:"medium"},type:"radio"},options:["small",void 0,"large"]}},decorators:[(r,o)=>(console.log(o),n.jsx(e,{children:n.jsx(r,{})}))],render:({children:r,...o})=>n.jsx(e.Link,{...o,children:r})},s={...p,parameters:{backgrounds:{options:M}},render:(r,{globals:{backgrounds:o}})=>n.jsx(e,{...r,children:Array.isArray(r.children)&&r.children.map((u,_)=>{const B=q(o.value);return D.createElement(e.Link,{...u.props,color:B,key:_},u.props.children)})})},a={...p,args:{children:[n.jsx(e.Link,{href:"#",icon:F,children:"Contactformulier"},"form"),n.jsx(e.Link,{href:"#",icon:H,children:"Adressen en openingstijden"},"address"),n.jsx(e.Link,{href:"#",icon:W,children:"Bel 14 020"},"phone")]}},i={...p,args:{children:[n.jsx(e.Link,{href:"#",size:"small",children:"Over deze website"},"about"),n.jsx(e.Link,{href:"#",size:"small",children:"Abonneer u op de nieuwsbrief"},"newsletter"),n.jsx(e.Link,{href:"#",size:"small",children:"Werken bij de gemeente Amsterdam"},"jobs")]}},t={...d},l={...d,args:{...d.args,color:"contrast"},globals:{backgrounds:{value:"yellow"}},parameters:{backgrounds:{options:{yellow:N.yellow}}}},c={...d,args:{...d.args,color:"inverse"},globals:{backgrounds:{value:"blue"}},parameters:{backgrounds:{options:{blue:R.blue}}}};var m,L,g;s.parameters={...s.parameters,docs:{...(m=s.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  ...StoryTemplate,
+  parameters: {
+    backgrounds: {
+      options: backgroundColourOptions
+    }
+  },
+  render: (args, {
+    globals: {
+      backgrounds
+    }
+  }) => {
+    return <LinkList {...args}>
+        {Array.isArray(args.children) && args.children.map((link, index) => {
+        const color = getForegroundColor(backgrounds.value) as LinkListLinkProps['color'];
+        return <LinkList.Link {...link.props} color={color} key={index}>
+                {link.props.children}
+              </LinkList.Link>;
+      })}
+      </LinkList>;
+  }
+}`,...(g=(L=s.parameters)==null?void 0:L.docs)==null?void 0:g.source}}};var b,h,f;a.parameters={...a.parameters,docs:{...(b=a.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  ...StoryTemplate,
+  args: {
+    children: [<LinkList.Link href="#" icon={Icons.SpeechBalloonEllipsisIcon} key="form">
+        Contactformulier
+      </LinkList.Link>, <LinkList.Link href="#" icon={Icons.HouseIcon} key="address">
+        Adressen en openingstijden
+      </LinkList.Link>, <LinkList.Link href="#" icon={Icons.PhoneIcon} key="phone">
+        Bel 14 020
+      </LinkList.Link>]
+  }
+}`,...(f=(h=a.parameters)==null?void 0:h.docs)==null?void 0:f.source}}};var y,S,v;i.parameters={...i.parameters,docs:{...(y=i.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  ...StoryTemplate,
+  args: {
+    children: [<LinkList.Link href="#" key="about" size="small">
+        Over deze website
+      </LinkList.Link>, <LinkList.Link href="#" key="newsletter" size="small">
+        Abonneer u op de nieuwsbrief
+      </LinkList.Link>, <LinkList.Link href="#" key="jobs" size="small">
+        Werken bij de gemeente Amsterdam
+      </LinkList.Link>]
+  }
+}`,...(v=(S=i.parameters)==null?void 0:S.docs)==null?void 0:v.source}}};var j,x,C;t.parameters={...t.parameters,docs:{...(j=t.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  ...LinkStoryTemplate
+}`,...(C=(x=t.parameters)==null?void 0:x.docs)==null?void 0:C.source}}};var T,w,I;l.parameters={...l.parameters,docs:{...(T=l.parameters)==null?void 0:T.docs,source:{originalSource:`{
+  ...LinkStoryTemplate,
+  args: {
+    ...LinkStoryTemplate.args,
+    color: 'contrast'
+  },
+  globals: {
+    backgrounds: {
+      value: 'yellow'
+    }
+  },
+  parameters: {
+    backgrounds: {
+      options: {
+        yellow: backgroundColourOptionsLight.yellow
+      }
+    }
+  }
+}`,...(I=(w=l.parameters)==null?void 0:w.docs)==null?void 0:I.source}}};var O,z,A;c.parameters={...c.parameters,docs:{...(O=c.parameters)==null?void 0:O.docs,source:{originalSource:`{
+  ...LinkStoryTemplate,
+  args: {
+    ...LinkStoryTemplate.args,
+    color: 'inverse'
+  },
+  globals: {
+    backgrounds: {
+      value: 'blue'
+    }
+  },
+  parameters: {
+    backgrounds: {
+      options: {
+        blue: backgroundColourOptionsDark.blue
+      }
+    }
+  }
+}`,...(A=(z=c.parameters)==null?void 0:z.docs)==null?void 0:A.source}}};const K=["Default","CustomIcons","SmallText","Link","ContrastColour","InverseColour"],$=Object.freeze(Object.defineProperty({__proto__:null,ContrastColour:l,CustomIcons:a,Default:s,InverseColour:c,Link:t,SmallText:i,__namedExportsOrder:K,default:J},Symbol.toStringTag,{value:"Module"}));export{a as C,c as I,$ as L,i as S,t as a,l as b};
---
+++ demo-DES-1196-storybook-backgrounds/assets/Paragraph.stories-B7nz1lZa.js
@@ -0,0 +1,50 @@
+import{j as t}from"./jsx-runtime-Cf8x2fCZ.js";import{e as C}from"./index.esm-jhuYF6FN.js";import{backgroundColourOptions as I,backgroundColourOptionsDark as j,getForegroundColor as T}from"./preview-DFUMt3w9.js";import{b as O}from"./exampleContent-r-zRGsZY.js";import{P as c}from"./Paragraph-BDLeQ91t.js";import{R as _}from"./Row-BbMuA978.js";import{I as y}from"./Icon-BBW_x4VP.js";const R=O(),D={title:"Components/Text/Paragraph",component:c,args:{children:R},argTypes:{children:{description:"The paragraph text.",table:{disable:!1}},color:{control:{labels:{undefined:"default"},type:"radio"},options:[void 0,"inverse"]},size:{control:{labels:{large:"large",small:"small",undefined:"medium"},type:"radio"},options:["small",void 0,"large"]}}},a={parameters:{backgrounds:{options:I}},render:(r,{globals:{backgrounds:e}})=>(r.color=T(e.value),t.jsx(c,{...r,children:r.children}))},o={args:{size:"large"}},s={args:{size:"small"}},n={args:{color:"inverse"},globals:{backgrounds:{value:"blue"}},parameters:{backgrounds:{options:{blue:j.blue}}}},l={args:{children:"Heading text"},render:({children:r,...e})=>t.jsxs(_,{gap:"small",children:[t.jsx(y,{color:e.color,size:e.size,svg:C}),t.jsx(c,{...e,children:r})]})};var i,p,g;a.parameters={...a.parameters,docs:{...(i=a.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  parameters: {
+    backgrounds: {
+      options: backgroundColourOptions
+    }
+  },
+  render: (args, {
+    globals: {
+      backgrounds
+    }
+  }) => {
+    args['color'] = getForegroundColor(backgrounds.value) as ParagraphProps['color'];
+    return <Paragraph {...args}>{args.children}</Paragraph>;
+  }
+}`,...(g=(p=a.parameters)==null?void 0:p.docs)==null?void 0:g.source}}};var d,u,m;o.parameters={...o.parameters,docs:{...(d=o.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    size: 'large'
+  }
+}`,...(m=(u=o.parameters)==null?void 0:u.docs)==null?void 0:m.source}}};var b,h,x;s.parameters={...s.parameters,docs:{...(b=s.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    size: 'small'
+  }
+}`,...(x=(h=s.parameters)==null?void 0:h.docs)==null?void 0:x.source}}};var f,v,k;n.parameters={...n.parameters,docs:{...(f=n.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    color: 'inverse'
+  },
+  globals: {
+    backgrounds: {
+      value: 'blue'
+    }
+  },
+  parameters: {
+    backgrounds: {
+      options: {
+        blue: backgroundColourOptionsDark.blue
+      }
+    }
+  }
+}`,...(k=(v=n.parameters)==null?void 0:v.docs)==null?void 0:k.source}}};var P,S,z;l.parameters={...l.parameters,docs:{...(P=l.parameters)==null?void 0:P.docs,source:{originalSource:`{
+  args: {
+    children: 'Heading text'
+  },
+  render: ({
+    children,
+    ...args
+  }) => <Row gap="small">
+      <Icon color={args.color} size={args.size} svg={MailIcon} />
+      <Paragraph {...args}>{children}</Paragraph>
+    </Row>
+}`,...(z=(S=l.parameters)==null?void 0:S.docs)==null?void 0:z.source}}};const w=["Default","LargeText","SmallText","InverseColour","WithIcon"],A=Object.freeze(Object.defineProperty({__proto__:null,Default:a,InverseColour:n,LargeText:o,SmallText:s,WithIcon:l,__namedExportsOrder:w,default:D},Symbol.toStringTag,{value:"Module"}));export{n as I,o as L,A as P,s as S,l as W};
