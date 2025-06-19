492e02a701b8b56cd2028a2673fd27c90446d9d6
Vincent Smedinga <v.smedinga@amsterdam.nl>
2025-06-19 12:35:00 +0200

    Add application variant to Page

--- packages/react/src/Page/Page.tsx
+++ packages/react/src/Page/Page.tsx
@@ -7,14 +7,21 @@ import clsx from 'clsx'
 import { forwardRef } from 'react'
 import type { ForwardedRef, HTMLAttributes, PropsWithChildren } from 'react'
 
-export type PageProps = PropsWithChildren<HTMLAttributes<HTMLDivElement>>
+export type PageProps = {
+  /* Whether the page is used for an application. This changes the layout. */
+  purpose?: 'application'
+} & PropsWithChildren<HTMLAttributes<HTMLDivElement>>
 
 /**
  * @see {@link https://designsystem.amsterdam/?path=/docs/components-containers-page--docs Page docs at Amsterdam Design System}
  */
 export const Page = forwardRef(
-  ({ children, className, ...restProps }: PageProps, ref: ForwardedRef<HTMLDivElement>) => (
-    <div {...restProps} className={clsx('ams-page', className)} ref={ref}>
+  ({ children, className, purpose, ...restProps }: PageProps, ref: ForwardedRef<HTMLDivElement>) => (
+    <div
+      {...restProps}
+      className={clsx('ams-page', purpose === 'application' && 'ams-page--application', className)}
+      ref={ref}
+    >
       {children}
     </div>
   ),
