e97b496b543be224b7ab14faf482463834d93ce0
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-06-19 10:58:14 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@f0d5eaf332c61e8717524a8d184eb12c3d2ce359 🚀

---
+++ demo-DES-1319-app-layout/assets/Breadcrumb.stories-DOfRx5tx.js
@@ -0,0 +1,13 @@
+import{j as n}from"./jsx-runtime-Cf8x2fCZ.js";import{B as e}from"./Breadcrumb-CuDaK0zb.js";const i={title:"Components/Navigation/Breadcrumb",component:e},r={args:{children:[n.jsx(e.Link,{href:"#",children:"Home"},1),n.jsx(e.Link,{href:"#",children:"Afval"},2),n.jsx(e.Link,{href:"#",children:"Bedrijfsafval"},3),n.jsx(e.Link,{href:"#",children:"Recyclepunten voor bedrijven"},4)]}};var a,o,c;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    children: [<Breadcrumb.Link href="#" key={1}>
+        Home
+      </Breadcrumb.Link>, <Breadcrumb.Link href="#" key={2}>
+        Afval
+      </Breadcrumb.Link>, <Breadcrumb.Link href="#" key={3}>
+        Bedrijfsafval
+      </Breadcrumb.Link>, <Breadcrumb.Link href="#" key={4}>
+        Recyclepunten voor bedrijven
+      </Breadcrumb.Link>]
+  }
+}`,...(c=(o=r.parameters)==null?void 0:o.docs)==null?void 0:c.source}}};const t=["Default"],m=Object.freeze(Object.defineProperty({__proto__:null,Default:r,__namedExportsOrder:t,default:i},Symbol.toStringTag,{value:"Module"}));export{m as B};
