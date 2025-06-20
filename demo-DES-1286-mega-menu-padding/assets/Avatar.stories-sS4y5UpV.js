de3c78ce8597cc29559006e514036727c0fc0a47
dlnr <niels@dlnr.dev>
2025-06-20 15:00:51 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@c9b495283edf3915070c7e16a30e485108260e6a 🚀

---
+++ demo-DES-1286-mega-menu-padding/assets/Avatar.stories-sS4y5UpV.js
@@ -0,0 +1,14 @@
+import{j as e}from"./jsx-runtime-Cf8x2fCZ.js";import{j as x}from"./index.esm-jhuYF6FN.js";import{c as j}from"./clsx-B-dksMZM.js";import{r as I}from"./index-G8LIXM5I.js";import{I as A}from"./Icon-BBW_x4VP.js";const w=["azure","green","lime","magenta","orange","yellow"],k=({imageSrc:a,initials:r})=>a?e.jsx("img",{alt:"",src:a}):r.length?e.jsx("span",{"aria-hidden":!0,children:r}):e.jsx(A,{size:"small",svg:x}),o=I.forwardRef(({className:a,color:r,imageSrc:c,label:h,...y},S)=>{const n=h.slice(0,2).toUpperCase(),_=n.length===0?"Gebruiker":`Initialen gebruiker: ${n}`;return e.jsxs("span",{...y,className:j("ams-avatar",r&&`ams-avatar--${r}`,c&&"ams-avatar--has-image",a),ref:S,children:[e.jsx("span",{className:"ams-visually-hidden",children:_}),e.jsx(k,{imageSrc:c,initials:n})]})});o.displayName="Avatar";try{o.displayName="Avatar",o.__docgenInfo={description:"",displayName:"Avatar",props:{color:{defaultValue:null,description:"The background colour.",name:"color",required:!1,type:{name:"enum",value:[{value:'"azure"'},{value:'"green"'},{value:'"lime"'},{value:'"magenta"'},{value:'"orange"'},{value:'"yellow"'}]}},imageSrc:{defaultValue:null,description:"The url for the user’s image. Its center will be displayed. Should be square and scaled down.",name:"imageSrc",required:!1,type:{name:"string"}},label:{defaultValue:null,description:"The text content. Should be the user’s initials. The first two characters will be displayed.",name:"label",required:!0,type:{name:"string"}}}}}catch{}/**
+ * @license EUPL-1.2+
+ * Copyright Gemeente Amsterdam
+ */const T={title:"Components/Feedback/Avatar",component:o,args:{imageSrc:"",label:"DS"},argTypes:{color:{control:{labels:{undefined:"purple (default)"}},options:[void 0,...w]}}},s={},t={args:{imageSrc:"https://i.pravatar.cc/128",label:"PS"}},l={args:{imageSrc:void 0,label:""}};var i,u,m;s.parameters={...s.parameters,docs:{...(i=s.parameters)==null?void 0:i.docs,source:{originalSource:"{}",...(m=(u=s.parameters)==null?void 0:u.docs)==null?void 0:m.source}}};var d,p,g;t.parameters={...t.parameters,docs:{...(d=t.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    imageSrc: 'https://i.pravatar.cc/128',
+    label: 'PS'
+  }
+}`,...(g=(p=t.parameters)==null?void 0:p.docs)==null?void 0:g.source}}};var v,f,b;l.parameters={...l.parameters,docs:{...(v=l.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    imageSrc: undefined,
+    label: ''
+  }
+}`,...(b=(f=l.parameters)==null?void 0:f.docs)==null?void 0:b.source}}};const F=["Default","WithImage","FallbackIcon"],D=Object.freeze(Object.defineProperty({__proto__:null,Default:s,FallbackIcon:l,WithImage:t,__namedExportsOrder:F,default:T},Symbol.toStringTag,{value:"Module"}));export{D as A,l as F,t as W};
---
+++ demo-DES-1286-mega-menu-padding/assets/Column.docs-CDyO5DRP.js
@@ -0,0 +1,47 @@
+import{j as e}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as r}from"./index-CxbY7dAs.js";import{M as l,e as o,P as a,f as h,h as i}from"./index-DLMDwKoW.js";import{C as c,A as d,H as m,I as x}from"./Column.stories-C8xSRgRd.js";import"./index-yBjzXJbu.js";import"./index-G8LIXM5I.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-DVj5BKMn.js";import"./index-fNjTmf9T.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index.esm-C4Aaqgc8.js";import"./clsx-B-dksMZM.js";import"./index.esm-jhuYF6FN.js";import"./types-yiKVj219.js";const p=`<!-- @license CC0-1.0 -->
+
+# Column
+
+Stacks its children vertically with gaps between them and offers alignment options.
+`;/*@license CC0-1.0*/function s(t){const n={a:"a",code:"code",h2:"h2",h3:"h3",li:"li",p:"p",strong:"strong",ul:"ul",...r(),...t.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(l,{of:c}),`
+`,e.jsx(o,{children:p}),`
+`,e.jsx(n.h2,{id:"guidelines",children:"Guidelines"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Wrap a Column around a set of elements that need the same amount of white space between them."}),`
+`,e.jsxs(n.li,{children:["Five sizes of ",e.jsx(n.a,{href:"/docs/brand-design-tokens-space--docs",children:"Space"}),` are available for the width or height of the gap.
+The medium gap is the default.`]}),`
+`,e.jsxs(n.li,{children:["To add white space below a single element, you can add a ",e.jsx(n.a,{href:"/docs/utilities-css-margin--docs",children:"Margin Bottom"})," instead."]}),`
+`,e.jsx(n.li,{children:"Align the elements horizontally or vertically through the alignment props."}),`
+`]}),`
+`,e.jsx(a,{}),`
+`,e.jsx(h,{}),`
+`,e.jsx(n.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(n.h3,{id:"alignment",children:"Alignment"}),`
+`,e.jsx(n.p,{children:"Items in the column can be aligned vertically in several ways:"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"Center"}),": center items within the column."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"End"}),": align items to the bottom of the column – to the bottom."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"Space Between"}),": distribute whitespace between items."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"Space Around"}),": distribute whitespace around items."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"Space Evenly"}),": distribute whitespace evenly around items."]}),`
+`]}),`
+`,e.jsxs(n.p,{children:["By default, items align to the ",e.jsx(n.strong,{children:"start"}),` of the column – at the top.
+This option has no class name or prop value.`]}),`
+`,e.jsx(n.p,{children:"This example centers three items vertically."}),`
+`,e.jsx(i,{of:d}),`
+`,e.jsx(n.h3,{id:"horizontal-alignment",children:"Horizontal alignment"}),`
+`,e.jsx(n.p,{children:"Items in the column can be aligned horizontally in several ways:"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"Start"}),": align items to the start of the column. This is the left side in left-to-right languages."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"Center"}),": center items within the column."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"End"}),": align items to the end of the column. This is the right side in left-to-right languages."]}),`
+`]}),`
+`,e.jsx(n.p,{children:"This example centers three items horizontally."}),`
+`,e.jsx(i,{of:m}),`
+`,e.jsx(n.h3,{id:"improve-semantics",children:"Improve semantics"}),`
+`,e.jsxs(n.p,{children:["By default, a Column renders a ",e.jsx(n.code,{children:"<div>"}),` element in HTML.
+Use the `,e.jsx(n.code,{children:"as"}),` prop to make your markup more semantic.
+In this example, the Column uses a `,e.jsx(n.code,{children:"<section>"})," element to group the heading and the cards."]}),`
+`,e.jsx(i,{of:x})]})}function I(t={}){const{wrapper:n}={...r(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(s,{...t})}):s(t)}export{I as default};
