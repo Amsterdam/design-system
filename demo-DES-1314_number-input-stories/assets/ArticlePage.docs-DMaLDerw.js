a17b4afc3733c7f3d79f21e0faca8bdad6a07685
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-06-20 12:46:17 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@aba41c1e7ab84460435433a4c5d0f99bc7940ab5 🚀

---
+++ demo-DES-1314_number-input-stories/assets/ArticlePage.docs-DMaLDerw.js
@@ -0,0 +1,5 @@
+import{j as t}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as n}from"./index-CxbY7dAs.js";import{M as e}from"./index-DMjyuNSj.js";import{A as m}from"./ArticlePage.stories-mpaHDN86.js";import"./index-yBjzXJbu.js";import"./index-G8LIXM5I.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-Gu4sTDL-.js";import"./index-fNjTmf9T.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index.esm-C4Aaqgc8.js";import"./clsx-B-dksMZM.js";import"./index.esm-jhuYF6FN.js";import"./Image.stories-KcovXxmi.js";import"./Image-Ci7950RD.js";import"./generateAspectRatioClass-CefTNpDa.js";import"./config-CoCqHKkh.js";import"./PageFooter.stories-eyrsv3-q.js";/*@license CC0-1.0*/function i(o){const r={h1:"h1",p:"p",...n(),...o.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(e,{of:m}),`
+`,t.jsx(r.h1,{id:"article-page",children:"Article page"}),`
+`,t.jsx(r.p,{children:"This page type is for news or similar kinds of articles."})]})}function _(o={}){const{wrapper:r}={...n(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(i,{...o})}):i(o)}export{_ as default};
--- demo-DES-1314_number-input-stories/assets/FormPage.docs-LpSudQ-O.js
+++
@@ -1,5 +0,0 @@
-import{j as o}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as m}from"./index-CxbY7dAs.js";import{M as n}from"./index-ujtPAxop.js";import{F as i}from"./FormPage.stories-OYlo8Uwh.js";import"./index-yBjzXJbu.js";import"./index-G8LIXM5I.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-DpWp3Guw.js";import"./index-fNjTmf9T.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index.esm-C4Aaqgc8.js";import"./clsx-B-dksMZM.js";import"./index.esm-jhuYF6FN.js";import"./config-CoCqHKkh.js";import"./PageFooter.stories-eyrsv3-q.js";/*@license CC0-1.0*/function e(t){const r={h1:"h1",p:"p",...m(),...t.components};return o.jsxs(o.Fragment,{children:[`
-`,`
-`,o.jsx(n,{of:i}),`
-`,o.jsx(r.h1,{id:"form-page",children:"Form page"}),`
-`,o.jsx(r.p,{children:"This simple example presents various types of fields."})]})}function X(t={}){const{wrapper:r}={...m(),...t.components};return r?o.jsx(r,{...t,children:o.jsx(e,{...t})}):e(t)}export{X as default};
