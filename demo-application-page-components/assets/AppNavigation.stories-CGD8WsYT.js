e32819c995eaeae657791c8f39df7868177bdab9
dlnr <niels@dlnr.dev>
2025-06-23 22:48:07 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@a39c8e30625f14ac8ab6035f15ea5d448b2aafd6 🚀

--- demo-application-page-components/assets/AppNavigation.stories-CGD8WsYT.js
+++
@@ -1,4 +0,0 @@
-import{A as a}from"./AppNavigation-BGUk2X0i.js";/**
- * @license EUPL-1.2+
- * Copyright Gemeente Amsterdam
- */const s={title:"App Navigation",component:a,args:{children:"Nieuw component"}},e={};var o,t,r;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:"{}",...(r=(t=e.parameters)==null?void 0:t.docs)==null?void 0:r.source}}};const n=["Default"],c=Object.freeze(Object.defineProperty({__proto__:null,Default:e,__namedExportsOrder:n,default:s},Symbol.toStringTag,{value:"Module"}));export{c as A};
---
+++ demo-application-page-components/assets/NavigationPage.docs-BzaiCCeQ.js
@@ -0,0 +1,6 @@
+import{j as t}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as r}from"./index-CxbY7dAs.js";import{M as e}from"./index-BqbS6u8a.js";import{N as p}from"./NavigationPage.stories-DyiFWxMB.js";import"./index-yBjzXJbu.js";import"./index-G8LIXM5I.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-25l5vNz2.js";import"./index-fNjTmf9T.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index.esm-B3eFeUl2.js";import"./clsx-B-dksMZM.js";import"./index.esm-DqkpIHnU.js";import"./exampleContent-r-zRGsZY.js";import"./config-B3fjLZeQ.js";import"./PageFooter.stories-D5tRZSGI.js";/*@license CC0-1.0*/function i(o){const n={h1:"h1",p:"p",...r(),...o.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(e,{of:p}),`
+`,t.jsx(n.h1,{id:"navigation-page",children:"Navigation page"}),`
+`,t.jsx(n.p,{children:`A navigation page does not have much content of its own.
+Instead, it presents groups of links to various pages related to a specific subject.`})]})}function X(o={}){const{wrapper:n}={...r(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(i,{...o})}):i(o)}export{X as default};
