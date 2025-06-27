dd43578dba9f8e1b44796b14df287ed7376af4b5
dlnr <niels@dlnr.dev>
2025-06-27 10:56:01 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@32d8dddfe2b70074a60d6167103e4140c6c1c74f 🚀

---
+++ demo-DES-1325-login-icons-rename/assets/FormPage.docs-BqGQmAzI.js
@@ -0,0 +1,5 @@
+import{j as o}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as m}from"./index-CxbY7dAs.js";import{M as n}from"./index-BEuKwfnF.js";import{F as i}from"./FormPage.stories-C7MA-Z7H.js";import"./index-yBjzXJbu.js";import"./index-G8LIXM5I.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-DhcT0pgj.js";import"./index-fNjTmf9T.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index.esm-izLc_sUP.js";import"./clsx-B-dksMZM.js";import"./index.esm-BRDUce6n.js";import"./config-BK5eAAFI.js";import"./PageFooter.stories-BSkLJory.js";/*@license CC0-1.0*/function e(t){const r={h1:"h1",p:"p",...m(),...t.components};return o.jsxs(o.Fragment,{children:[`
+`,`
+`,o.jsx(n,{of:i}),`
+`,o.jsx(r.h1,{id:"form-page",children:"Form page"}),`
+`,o.jsx(r.p,{children:"This simple example presents various types of fields."})]})}function X(t={}){const{wrapper:r}={...m(),...t.components};return r?o.jsx(r,{...t,children:o.jsx(e,{...t})}):e(t)}export{X as default};
---
+++ demo-DES-1325-login-icons-rename/assets/Paragraph.stories-uzxjSBl4.js
@@ -0,0 +1,24 @@
+import{j as l}from"./jsx-runtime-Cf8x2fCZ.js";import{e as T}from"./index.esm-BRDUce6n.js";import{b as j}from"./exampleContent-r-zRGsZY.js";import{P}from"./Paragraph-BDLeQ91t.js";import{R as _}from"./Row-JeNeYdOs.js";import{I as y}from"./Icon-BBW_x4VP.js";const R=j(),w={title:"Components/Text/Paragraph",component:P,args:{children:R},argTypes:{children:{description:"The paragraph text.",table:{disable:!1}},color:{control:{labels:{undefined:"default"},type:"radio"},options:[void 0,"inverse"]},size:{control:{labels:{large:"large",small:"small",undefined:"medium"},type:"radio"},options:["small",void 0,"large"]}}},r={},e={args:{size:"large"}},a={args:{size:"small"}},s={args:{color:"inverse"}},o={args:{children:"Heading text"},render:({children:b,...t})=>l.jsxs(_,{gap:"small",children:[l.jsx(y,{color:t.color,size:t.size,svg:T}),l.jsx(P,{...t,children:b})]})};var n,c,i;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:"{}",...(i=(c=r.parameters)==null?void 0:c.docs)==null?void 0:i.source}}};var p,m,d;e.parameters={...e.parameters,docs:{...(p=e.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    size: 'large'
+  }
+}`,...(d=(m=e.parameters)==null?void 0:m.docs)==null?void 0:d.source}}};var g,u,h;a.parameters={...a.parameters,docs:{...(g=a.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    size: 'small'
+  }
+}`,...(h=(u=a.parameters)==null?void 0:u.docs)==null?void 0:h.source}}};var x,f,S;s.parameters={...s.parameters,docs:{...(x=s.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  args: {
+    color: 'inverse'
+  }
+}`,...(S=(f=s.parameters)==null?void 0:f.docs)==null?void 0:S.source}}};var v,z,I;o.parameters={...o.parameters,docs:{...(v=o.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    children: 'Heading text'
+  },
+  render: ({
+    children,
+    ...args
+  }) => <Row gap="small">
+      <Icon color={args.color} size={args.size} svg={MailIcon} />
+      <Paragraph {...args}>{children}</Paragraph>
+    </Row>
+}`,...(I=(z=o.parameters)==null?void 0:z.docs)==null?void 0:I.source}}};const C=["Default","LargeText","SmallText","InverseColour","WithIcon"],H=Object.freeze(Object.defineProperty({__proto__:null,Default:r,InverseColour:s,LargeText:e,SmallText:a,WithIcon:o,__namedExportsOrder:C,default:w},Symbol.toStringTag,{value:"Module"}));export{s as I,e as L,H as P,a as S,o as W};
