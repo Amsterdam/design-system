5d1ee7ee76bbeb40ebc4b5459d1d84cae62aca2d
dlnr <niels@dlnr.dev>
2025-06-27 14:37:09 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@f5c66c287bd6a4e976ad7c801be334cdea3714bd 🚀

---
+++ demo-application-page-components/assets/DashboardPage.stories-BNvjO70s.js
@@ -0,0 +1,67 @@
+import{j as e}from"./jsx-runtime-Cf8x2fCZ.js";import{D as P,E as O,F as R,p as j,e as g,f as b,j as p,k as h,l as d,m as z,n as _,o as H,g as u,h as v,i as f}from"./index.esm-CN1rmi5g.js";import{G as x,c as o,b as c}from"./index.esm-BGLgu2rm.js";import{c as E,L as k}from"./config-gtXCoBve.js";import{A as n}from"./AppNavigation-CZ469oYI.js";const w="_tile_1f88z_3",T="_tiles_1f88z_10",W="_icon_1f88z_16",Q="_link_1f88z_22",a={tile:w,tiles:T,icon:W,link:Q},G=()=>e.jsx(e.Fragment,{children:e.jsx(x,{paddingBottom:"x-large",paddingTop:"large",children:e.jsx(x.Cell,{span:"all",children:e.jsxs("div",{className:a.tiles,children:[e.jsx(o,{className:a.tile,children:e.jsx(o.Heading,{level:2,children:e.jsxs(o.Link,{className:a.link,href:"#",children:[e.jsx(c,{className:a.icon,svg:P}),"Projecten"]})})}),e.jsx(o,{className:a.tile,children:e.jsx(o.Heading,{level:2,children:e.jsxs(o.Link,{className:a.link,href:"#",children:[e.jsx(c,{className:a.icon,svg:O}),"Rapportages"]})})}),e.jsx(o,{className:a.tile,children:e.jsx(o.Heading,{level:2,children:e.jsxs(o.Link,{className:a.link,href:"#",children:[e.jsx(c,{className:a.icon,svg:R}),"Analyses"]})})}),e.jsx(o,{className:a.tile,children:e.jsx(o.Heading,{level:2,children:e.jsxs(o.Link,{className:a.link,href:"#",children:[e.jsx(c,{className:a.icon,svg:j}),"Instellingen"]})})})]})})})}),q={...E,title:"Pages/Application/Dashboard",component:G},r={decorators:[i=>e.jsx(k,{children:e.jsx(i,{})})]},l={decorators:[i=>e.jsx(k,{navItems:e.jsxs(e.Fragment,{children:[e.jsx(n.Link,{href:"#dashboard",icon:g,label:"Dashboard"},1),e.jsx(n.Button,{icon:b,label:"Projecten",popoverTarget:"projecten-menu",children:e.jsxs(n.Menu,{id:"projecten-menu",popover:"auto",children:[e.jsx(n.Link,{href:"#projecten/objecten",icon:p,label:"Objecten"},"1"),e.jsx(n.Link,{href:"#projecten/inspecties",icon:h,label:"Inspecties"},"2"),e.jsx(n.Link,{href:"#projecten/nader-onderzoek",icon:d,label:"Nader onderzoek"},"3"),e.jsx(n.Link,{href:"#projecten/maatregelen",icon:z,label:"Maatregelen"},"4"),e.jsx(n.Link,{href:"#projecten/batches",icon:_,label:"Batches"},"5"),e.jsx(n.Link,{href:"#projecten/hulp",icon:H,label:"Hulp"},"6"),e.jsx(n.Link,{href:"#projecten/beheer",icon:j,label:"Beheer"},"7")]})},2),e.jsx(n.Button,{icon:u,label:"Rapportages",popoverTarget:"rapportages-menu",children:e.jsxs(n.Menu,{id:"rapportages-menu",popover:"auto",children:[e.jsx(n.Link,{href:"#projecten/objecten",icon:p,label:"Objecten"},"1"),e.jsx(n.Link,{href:"#projecten/inspecties",icon:h,label:"Inspecties"},"2"),e.jsx(n.Link,{href:"#projecten/nader-onderzoek",icon:d,label:"Nader onderzoek"},"3")]})},3),e.jsx(n.Link,{href:"#analyses",icon:v,label:"Analyses"},4),e.jsx(n.Link,{href:"#instellingen",icon:f,label:"Instellingen"},5)]}),children:e.jsx(i,{})})]},s={decorators:[i=>e.jsx(k,{expandable:!0,navItems:e.jsxs(e.Fragment,{children:[e.jsx(n.Link,{href:"#dashboard",icon:g,label:"Dashboard"},1),e.jsx(n.Link,{href:"#projecten",icon:b,label:"Projecten"},2),e.jsx(n.Link,{href:"#rapportages",icon:u,label:"Rapportages"},3),e.jsx(n.Link,{href:"#analyses",icon:v,label:"Analyses"},4),e.jsx(n.Link,{href:"#instellingen",icon:f,label:"Instellingen"},5)]}),children:e.jsx(i,{})})]},t={decorators:[i=>e.jsx(k,{navExpanded:!0,navItems:e.jsxs(e.Fragment,{children:[e.jsx(n.Link,{href:"#dashboard",icon:g,label:"Dashboard"},1),e.jsx(n.Link,{href:"#projecten",icon:b,label:"Projecten",children:e.jsxs(n.Menu,{children:[e.jsx(n.Link,{href:"#projecten/objecten",icon:p,label:"Objecten"},"1"),e.jsx(n.Link,{href:"#projecten/inspecties",icon:h,label:"Inspecties"},"2"),e.jsx(n.Link,{href:"#projecten/nader-onderzoek",icon:d,label:"Nader onderzoek"},"3"),e.jsx(n.Link,{href:"#projecten/maatregelen",icon:z,label:"Maatregelen"},"4"),e.jsx(n.Link,{href:"#projecten/batches",icon:_,label:"Batches"},"5"),e.jsx(n.Link,{href:"#projecten/hulp",icon:H,label:"Hulp"},"6"),e.jsx(n.Link,{href:"#projecten/beheer",icon:j,label:"Beheer"},"7")]})},2),e.jsx(n.Link,{href:"#rapportages",icon:u,label:"Rapportages",children:e.jsxs(n.Menu,{children:[e.jsx(n.Link,{href:"#projecten/objecten",icon:p,label:"Objecten"},"1"),e.jsx(n.Link,{href:"#projecten/inspecties",icon:h,label:"Inspecties"},"2"),e.jsx(n.Link,{href:"#projecten/nader-onderzoek",icon:d,label:"Nader onderzoek"},"3")]})},3),e.jsx(n.Link,{href:"#analyses",icon:v,label:"Analyses"},4),e.jsx(n.Link,{href:"#instellingen",icon:f,label:"Instellingen"},5)]}),children:e.jsx(i,{})})]};var L,m,N;r.parameters={...r.parameters,docs:{...(L=r.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  decorators: [Story => <Layout>
+        <Story />
+      </Layout>]
+}`,...(N=(m=r.parameters)==null?void 0:m.docs)==null?void 0:N.source}}};var y,A,I;l.parameters={...l.parameters,docs:{...(y=l.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  decorators: [Story => <Layout navItems={<>
+            <AppNavigation.Link href="#dashboard" icon={HouseCanalFillIcon} key={1} label="Dashboard" />
+            <AppNavigation.Button icon={FolderFillIcon} key={2} label="Projecten" popoverTarget="projecten-menu">
+              <AppNavigation.Menu id="projecten-menu" popover="auto">
+                <AppNavigation.Link href="#projecten/objecten" icon={BuildingIcon} key="1" label="Objecten" />
+                <AppNavigation.Link href="#projecten/inspecties" icon={SearchIcon} key="2" label="Inspecties" />
+                <AppNavigation.Link href="#projecten/nader-onderzoek" icon={DocumentCheckMarkIcon} key="3" label="Nader onderzoek" />
+                <AppNavigation.Link href="#projecten/maatregelen" icon={RulerIcon} key="4" label="Maatregelen" />
+                <AppNavigation.Link href="#projecten/batches" icon={HandshakeIcon} key="5" label="Batches" />
+                <AppNavigation.Link href="#projecten/hulp" icon={QuestionMarkCircleIcon} key="6" label="Hulp" />
+                <AppNavigation.Link href="#projecten/beheer" icon={CogwheelIcon} key="7" label="Beheer" />
+              </AppNavigation.Menu>
+            </AppNavigation.Button>
+            <AppNavigation.Button icon={DocumentsFillIcon} key={3} label="Rapportages" popoverTarget="rapportages-menu">
+              <AppNavigation.Menu id="rapportages-menu" popover="auto">
+                <AppNavigation.Link href="#projecten/objecten" icon={BuildingIcon} key="1" label="Objecten" />
+                <AppNavigation.Link href="#projecten/inspecties" icon={SearchIcon} key="2" label="Inspecties" />
+                <AppNavigation.Link href="#projecten/nader-onderzoek" icon={DocumentCheckMarkIcon} key="3" label="Nader onderzoek" />
+              </AppNavigation.Menu>
+            </AppNavigation.Button>
+            <AppNavigation.Link href="#analyses" icon={BarChartFillIcon} key={4} label="Analyses" />
+            <AppNavigation.Link href="#instellingen" icon={CogwheelFillIcon} key={5} label="Instellingen" />
+          </>}>
+        <Story />
+      </Layout>]
+}`,...(I=(A=l.parameters)==null?void 0:A.docs)==null?void 0:I.source}}};var S,F,C;s.parameters={...s.parameters,docs:{...(S=s.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  decorators: [Story => <Layout expandable navItems={<>
+            <AppNavigation.Link href="#dashboard" icon={HouseCanalFillIcon} key={1} label="Dashboard" />
+            <AppNavigation.Link href="#projecten" icon={FolderFillIcon} key={2} label="Projecten" />
+            <AppNavigation.Link href="#rapportages" icon={DocumentsFillIcon} key={3} label="Rapportages" />
+            <AppNavigation.Link href="#analyses" icon={BarChartFillIcon} key={4} label="Analyses" />
+            <AppNavigation.Link href="#instellingen" icon={CogwheelFillIcon} key={5} label="Instellingen" />
+          </>}>
+        <Story />
+      </Layout>]
+}`,...(C=(F=s.parameters)==null?void 0:F.docs)==null?void 0:C.source}}};var M,B,D;t.parameters={...t.parameters,docs:{...(M=t.parameters)==null?void 0:M.docs,source:{originalSource:`{
+  decorators: [Story => <Layout navExpanded navItems={<>
+            <AppNavigation.Link href="#dashboard" icon={HouseCanalFillIcon} key={1} label="Dashboard" />
+            <AppNavigation.Link href="#projecten" icon={FolderFillIcon} key={2} label="Projecten">
+              <AppNavigation.Menu>
+                <AppNavigation.Link href="#projecten/objecten" icon={BuildingIcon} key="1" label="Objecten" />
+                <AppNavigation.Link href="#projecten/inspecties" icon={SearchIcon} key="2" label="Inspecties" />
+                <AppNavigation.Link href="#projecten/nader-onderzoek" icon={DocumentCheckMarkIcon} key="3" label="Nader onderzoek" />
+                <AppNavigation.Link href="#projecten/maatregelen" icon={RulerIcon} key="4" label="Maatregelen" />
+                <AppNavigation.Link href="#projecten/batches" icon={HandshakeIcon} key="5" label="Batches" />
+                <AppNavigation.Link href="#projecten/hulp" icon={QuestionMarkCircleIcon} key="6" label="Hulp" />
+                <AppNavigation.Link href="#projecten/beheer" icon={CogwheelIcon} key="7" label="Beheer" />
+              </AppNavigation.Menu>
+            </AppNavigation.Link>
+            <AppNavigation.Link href="#rapportages" icon={DocumentsFillIcon} key={3} label="Rapportages">
+              <AppNavigation.Menu>
+                <AppNavigation.Link href="#projecten/objecten" icon={BuildingIcon} key="1" label="Objecten" />
+                <AppNavigation.Link href="#projecten/inspecties" icon={SearchIcon} key="2" label="Inspecties" />
+                <AppNavigation.Link href="#projecten/nader-onderzoek" icon={DocumentCheckMarkIcon} key="3" label="Nader onderzoek" />
+              </AppNavigation.Menu>
+            </AppNavigation.Link>
+            <AppNavigation.Link href="#analyses" icon={BarChartFillIcon} key={4} label="Analyses" />
+            <AppNavigation.Link href="#instellingen" icon={CogwheelFillIcon} key={5} label="Instellingen" />
+          </>}>
+        <Story />
+      </Layout>]
+}`,...(D=(B=t.parameters)==null?void 0:B.docs)==null?void 0:D.source}}};const J=["Default","WithSubNavigationPopover","Expandable","ExpandedWithSubNavigation"],Z=Object.freeze(Object.defineProperty({__proto__:null,Default:r,Expandable:s,ExpandedWithSubNavigation:t,WithSubNavigationPopover:l,__namedExportsOrder:J,default:q},Symbol.toStringTag,{value:"Module"}));export{Z as D};
---
+++ demo-application-page-components/assets/PageHeader.stories-D6vrNapT.js
@@ -0,0 +1,129 @@
+import{j as e}from"./jsx-runtime-Cf8x2fCZ.js";import{G as s,a as n,H as h}from"./index.esm-BGLgu2rm.js";import{P as r}from"./PageHeader-CihQ526n.js";const B={title:"Components/Containers/Page Header",component:r},g=[[{href:"#",label:"Kaart"},{href:"#",label:"Panoramabeelden"},{href:"#",label:"Tabellen"},{href:"#",label:"Catalogus (Beta)"},{href:"#",label:"Kaarten"},{href:"#",label:"Datacatalogus"}],[{href:"#",label:"Over de organisatie"},{href:"#",label:"Over het dataplatform"}],[{href:"#",label:"Help"},{href:"#",label:"Contact"}]],l={args:{brandName:"Data Amsterdam",children:e.jsxs(s,{paddingBottom:"2x-large",children:[e.jsx(r.GridCellNarrowWindowOnly,{span:"all",children:e.jsx(n,{children:e.jsx(n.Link,{href:"#",lang:"en",children:"English"})})}),e.jsxs(s.Cell,{span:4,children:[e.jsx(h,{className:"ams-mb-s",level:3,children:"Onderdelen"}),e.jsx(n,{children:g[0].map(({href:i,label:a})=>e.jsx(n.Link,{href:i,children:a},a))})]}),e.jsxs(s.Cell,{span:4,children:[e.jsx(h,{className:"ams-mb-s",level:3,children:"Over ons"}),e.jsx(n,{children:g[1].map(({href:i,label:a})=>e.jsx(n.Link,{href:i,children:a},a))})]}),e.jsxs(s.Cell,{span:4,children:[e.jsx(h,{className:"ams-mb-s",level:3,children:"Help"}),e.jsx(n,{children:g[2].map(({href:i,label:a})=>e.jsx(n.Link,{href:i,children:a},a))})]})]}),menuItems:[e.jsx(r.MenuLink,{href:"#",lang:"en",children:"English"},1),e.jsx(r.MenuLink,{fixed:!0,href:"#",children:"Zoeken"},2)]}},t={args:{children:e.jsxs(s,{gapVertical:"large",paddingBottom:"2x-large",children:[e.jsx(r.GridCellNarrowWindowOnly,{span:"all",children:e.jsx(n,{children:e.jsx(n.Link,{href:"#",lang:"en",children:"English"})})}),e.jsx(s.Cell,{span:"all",children:e.jsx(n,{children:e.jsx(n.Link,{href:"#",children:"Regular collapsible menu link"})})})]}),menuItems:[e.jsx(r.MenuLink,{href:"#",lang:"en",children:"English"},1),e.jsx(r.MenuLink,{fixed:!0,href:"#",children:"Zoeken"},2)]}},c=[{href:"#",label:"Stad"},{href:"#",label:"Techniek"},{href:"#",label:"Historie"},{href:"#",label:"Duurzaamheid"}],d={args:{brandName:"Aan de Amsterdamse grachten",children:e.jsx(n,{className:"ams-mb-l",children:c.map(({href:i,label:a})=>e.jsx(n.Link,{href:i,children:a},a))}),menuItems:[...c.map(({href:i,label:a})=>e.jsx(r.MenuLink,{href:i,children:a},a)),e.jsx(r.MenuLink,{fixed:!0,href:"#",children:"Zoeken"},"Zoeken")],noMenuButtonOnWideWindow:!0}},o={args:{brandName:"Mijn Amsterdam",menuItems:e.jsx(r.MenuLink,{fixed:!0,href:"#",children:"Inloggen"})}},m={args:{logoAccessibleName:"Gemeentelijke Gezondheidsdienst Amsterdam logo",logoBrand:"ggd-amsterdam",logoLink:"https://www.ggd.amsterdam.nl/",logoLinkTitle:"Naar de voorpagina van de GGD Amsterdam"}},L={args:{children:e.jsx(s,{children:e.jsx(s.Cell,{span:"all",children:e.jsx(n,{children:e.jsx(n.Link,{href:"#",lang:"en",children:"English"})})})}),menuButtonText:"Alle onderwerpen",navigationLabel:"Navigatie"}};var k,u,p;l.parameters={...l.parameters,docs:{...(k=l.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  args: {
+    brandName: 'Data Amsterdam',
+    children: <Grid paddingBottom="2x-large">
+        <PageHeader.GridCellNarrowWindowOnly span="all">
+          <LinkList>
+            <LinkList.Link href="#" lang="en">
+              English
+            </LinkList.Link>
+          </LinkList>
+        </PageHeader.GridCellNarrowWindowOnly>
+        <Grid.Cell span={4}>
+          <Heading className="ams-mb-s" level={3}>
+            Onderdelen
+          </Heading>
+          <LinkList>
+            {defaultStoryLinks[0].map(({
+            href,
+            label
+          }) => <LinkList.Link href={href} key={label}>
+                {label}
+              </LinkList.Link>)}
+          </LinkList>
+        </Grid.Cell>
+        <Grid.Cell span={4}>
+          <Heading className="ams-mb-s" level={3}>
+            Over ons
+          </Heading>
+          <LinkList>
+            {defaultStoryLinks[1].map(({
+            href,
+            label
+          }) => <LinkList.Link href={href} key={label}>
+                {label}
+              </LinkList.Link>)}
+          </LinkList>
+        </Grid.Cell>
+        <Grid.Cell span={4}>
+          <Heading className="ams-mb-s" level={3}>
+            Help
+          </Heading>
+          <LinkList>
+            {defaultStoryLinks[2].map(({
+            href,
+            label
+          }) => <LinkList.Link href={href} key={label}>
+                {label}
+              </LinkList.Link>)}
+          </LinkList>
+        </Grid.Cell>
+      </Grid>,
+    menuItems: [<PageHeader.MenuLink href="#" key={1} lang="en">
+        English
+      </PageHeader.MenuLink>, <PageHeader.MenuLink fixed href="#" key={2}>
+        Zoeken
+      </PageHeader.MenuLink>]
+  }
+}`,...(p=(u=l.parameters)==null?void 0:u.docs)==null?void 0:p.source}}};var f,x,b;t.parameters={...t.parameters,docs:{...(f=t.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    children: <Grid gapVertical="large" paddingBottom="2x-large">
+        <PageHeader.GridCellNarrowWindowOnly span="all">
+          <LinkList>
+            <LinkList.Link href="#" lang="en">
+              English
+            </LinkList.Link>
+          </LinkList>
+        </PageHeader.GridCellNarrowWindowOnly>
+        <Grid.Cell span="all">
+          <LinkList>
+            <LinkList.Link href="#">Regular collapsible menu link</LinkList.Link>
+          </LinkList>
+        </Grid.Cell>
+      </Grid>,
+    menuItems: [<PageHeader.MenuLink href="#" key={1} lang="en">
+        English
+      </PageHeader.MenuLink>, <PageHeader.MenuLink fixed href="#" key={2}>
+        Zoeken
+      </PageHeader.MenuLink>]
+  }
+}`,...(b=(x=t.parameters)==null?void 0:x.docs)==null?void 0:b.source}}};var j,M,W;d.parameters={...d.parameters,docs:{...(j=d.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  args: {
+    brandName: 'Aan de Amsterdamse grachten',
+    children: <LinkList className="ams-mb-l">
+        {WithoutMenuButtonOnWideWindowStoryLinks.map(({
+        href,
+        label
+      }) => <LinkList.Link href={href} key={label}>
+            {label}
+          </LinkList.Link>)}
+      </LinkList>,
+    menuItems: [...WithoutMenuButtonOnWideWindowStoryLinks.map(({
+      href,
+      label
+    }) => <PageHeader.MenuLink href={href} key={label}>
+          {label}
+        </PageHeader.MenuLink>), <PageHeader.MenuLink fixed href="#" key="Zoeken">
+        Zoeken
+      </PageHeader.MenuLink>],
+    noMenuButtonOnWideWindow: true
+  }
+}`,...(W=(M=d.parameters)==null?void 0:M.docs)==null?void 0:W.source}}};var H,G,C;o.parameters={...o.parameters,docs:{...(H=o.parameters)==null?void 0:H.docs,source:{originalSource:`{
+  args: {
+    brandName: 'Mijn Amsterdam',
+    menuItems: <PageHeader.MenuLink fixed href="#">
+        Inloggen
+      </PageHeader.MenuLink>
+  }
+}`,...(C=(G=o.parameters)==null?void 0:G.docs)==null?void 0:C.source}}};var w,N,y;m.parameters={...m.parameters,docs:{...(w=m.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  args: {
+    logoAccessibleName: 'Gemeentelijke Gezondheidsdienst Amsterdam logo',
+    logoBrand: 'ggd-amsterdam',
+    logoLink: 'https://www.ggd.amsterdam.nl/',
+    logoLinkTitle: 'Naar de voorpagina van de GGD Amsterdam'
+  }
+}`,...(y=(N=m.parameters)==null?void 0:N.docs)==null?void 0:y.source}}};var P,O,v;L.parameters={...L.parameters,docs:{...(P=L.parameters)==null?void 0:P.docs,source:{originalSource:`{
+  args: {
+    children: <Grid>
+        <Grid.Cell span="all">
+          <LinkList>
+            <LinkList.Link href="#" lang="en">
+              English
+            </LinkList.Link>
+          </LinkList>
+        </Grid.Cell>
+      </Grid>,
+    menuButtonText: 'Alle onderwerpen',
+    navigationLabel: 'Navigatie'
+  }
+}`,...(v=(O=L.parameters)==null?void 0:O.docs)==null?void 0:v.source}}};const S=["Default","WithMovingLinks","WithoutMenuButtonOnWideWindow","WithoutMenuButton","WithCustomLogoLink","WithCustomTexts"],T=Object.freeze(Object.defineProperty({__proto__:null,Default:l,WithCustomLogoLink:m,WithCustomTexts:L,WithMovingLinks:t,WithoutMenuButton:o,WithoutMenuButtonOnWideWindow:d,__namedExportsOrder:S,default:B},Symbol.toStringTag,{value:"Module"}));export{T as P,t as W,d as a,o as b,m as c,L as d};
