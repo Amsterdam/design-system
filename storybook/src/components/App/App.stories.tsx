974f7fb6566b3f47db5c41a72517104d82ab073b
Niels Roozemond <n.roozemond@amsterdam.nl>
2025-06-27 16:43:09 +0200

    Header story

--- storybook/src/components/App/App.stories.tsx
+++ storybook/src/components/App/App.stories.tsx
@@ -9,6 +9,21 @@ import { Meta, StoryObj } from '@storybook/react'
 const meta = {
   title: 'Components/Containers/App',
   component: App,
+  args: {
+    children: (
+      <>
+        <header className="ams-page-header" style={{ background: 'rgba(0, 149, 10, 0.4)' }}></header>
+        <div className="ams-app-navigation"></div>
+        <main className="ams-app-content" style={{ background: 'rgba(177, 0, 0, 0.4)' }}></main>
+        <footer className="ams-page-footer" style={{ background: 'rgba(91, 0, 71, 0.4)' }}>
+          footer
+        </footer>
+      </>
+    ),
+  },
+  parameters: {
+    layout: 'fullscreen',
+  },
 } satisfies Meta<typeof App>
 
 export default meta
