0c44d85d34814ceb88ba53dde7398e5b4c865301
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-07-08 19:59:11 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@1b239299889f4cb882291f9938ae54342d65b3e5 🚀

---
+++ demo-DES-1336-page-footer-text-size/assets/Link.stories-K1Tk3bhg.js
@@ -0,0 +1,9 @@
+import{j as n}from"./jsx-runtime-Cf8x2fCZ.js";import{P as d}from"./index.esm-BQ0Ra-KO.js";import{c as p}from"./clsx-B-dksMZM.js";import{r as m}from"./index-G8LIXM5I.js";const s=m.forwardRef(({children:a,className:t,color:i,...c},l)=>n.jsx("a",{...c,className:p("ams-link",i&&`ams-link--${i}`,t),ref:l,children:a}));s.displayName="Link";try{s.displayName="Link",s.__docgenInfo={description:"",displayName:"Link",props:{color:{defaultValue:null,description:"Changes the text colour for readability on a light or dark background.",name:"color",required:!1,type:{name:"enum",value:[{value:'"inverse"'},{value:'"contrast"'}]}}}}}catch{}const u={title:"Components/Navigation/Link",component:s,args:{children:"typograaf",href:"#"},argTypes:{children:{description:"The link text.",table:{disable:!1}},color:{control:{labels:{undefined:"default"},type:"radio"},options:[void 0,"contrast","inverse"]},href:{description:"The url for the link.",name:"href",type:{name:"string",required:!1}}},decorators:[(a,{args:t})=>n.jsxs(d,{color:t.color==="inverse"?"inverse":void 0,children:["Jouw ",n.jsx(a,{})," biedt mij zulke exquise schreven!"]})]},e={},r={args:{color:"contrast"}},o={args:{color:"inverse"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:"{}",...e.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    color: 'contrast'
+  }
+}`,...r.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    color: 'inverse'
+  }
+}`,...o.parameters?.docs?.source}}};const f=["Default","ContrastColour","InverseColour"],k=Object.freeze(Object.defineProperty({__proto__:null,ContrastColour:r,Default:e,InverseColour:o,__namedExportsOrder:f,default:u},Symbol.toStringTag,{value:"Module"}));export{r as C,o as I,k as L};
