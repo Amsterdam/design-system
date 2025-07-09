770b3a58a0612526228cb4a467d1a50df31d31af
dlnr <niels@dlnr.dev>
2025-07-09 08:20:48 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@ffd0ff2cfa351121cd3ebaee501a0cdae43334b4 🚀

---
+++ demo-DES-1321-application-layout-navigation/assets/Mark.stories-DbFvGqwE.js
@@ -0,0 +1,12 @@
+import{j as n}from"./jsx-runtime-Cf8x2fCZ.js";import{P as i}from"./index.esm--1yU8nqs.js";import{c as m}from"./clsx-B-dksMZM.js";import{r as d}from"./index-G8LIXM5I.js";const r=d.forwardRef(({children:a,className:t,...s},o)=>n.jsx("mark",{...s,className:m("ams-mark",t),ref:o,children:a}));r.displayName="Mark";try{r.displayName="Mark",r.__docgenInfo={description:"",displayName:"Mark",props:{}}}catch{}const l={title:"Components/Text/Mark",component:r,argTypes:{children:{description:"The text to mark.",table:{disable:!1}}}},e={args:{children:"Wat vinden Amsterdammers belangrijk?"},render:({children:a})=>n.jsxs(i,{children:["Daarom organiseren we in 2024 het burgerberaad schone stad, waarin 150 Amsterdammers in gesprek gaan over hoe we de stad beter schoonhouden. ",n.jsx(r,{children:a})," Welke oplossingen zien zij? Hier zijn we benieuwd naar. Want elke Amsterdammer heeft afval en moet het kwijt. Wij kunnen als gemeente veel van deze afvalexperts leren."]})};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Wat vinden Amsterdammers belangrijk?'
+  },
+  render: ({
+    children
+  }) => <Paragraph>
+      Daarom organiseren we in 2024 het burgerberaad schone stad, waarin 150 Amsterdammers in gesprek gaan over hoe we
+      de stad beter schoonhouden. <Mark>{children}</Mark> Welke oplossingen zien zij? Hier zijn we benieuwd naar. Want
+      elke Amsterdammer heeft afval en moet het kwijt. Wij kunnen als gemeente veel van deze afvalexperts leren.
+    </Paragraph>
+}`,...e.parameters?.docs?.source}}};const c=["Default"],f=Object.freeze(Object.defineProperty({__proto__:null,Default:e,__namedExportsOrder:c,default:l},Symbol.toStringTag,{value:"Module"}));export{f as M};
