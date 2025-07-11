10cd688f18961e4a2a4d241c04ca251205438fc3
dlnr <niels@dlnr.dev>
2025-07-11 14:20:29 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@17fd47bc6daf9ba0ad5b8b6bfc931624dbde6581 🚀

---
+++ demo-DES-1354-application-menu/assets/Dialog.docs-B7amQDrl.js
@@ -0,0 +1,44 @@
+import{j as o}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as i}from"./index-CxbY7dAs.js";import{M as r,e as s,P as a,f as l,h as d}from"./index-D324hIcv.js";import{D as c,C as m}from"./Dialog.stories-CV6Wx7hs.js";import"./index-yBjzXJbu.js";import"./index-G8LIXM5I.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-CTDsazDI.js";import"./index-CJ88nXQk.js";import"./index-Cf9b6H0j.js";import"./index-DrFu-skq.js";import"./index.esm-Bxisna0a.js";import"./clsx-B-dksMZM.js";import"./index.esm-CiTCpp5W.js";import"./v4-CtRu48qb.js";import"./Heading-7IbEntpa.js";import"./getHeadingTag-C4Rztoc8.js";import"./IconButton-BfyVj8GR.js";import"./Icon-BBW_x4VP.js";const h=`<!-- @license CC0-1.0 -->
+
+# Dialog
+
+A popup window in which the user must perform an action to proceed.
+
+## Guidelines
+
+- Use dialogs sparingly because they interrupt the user’s workflow.
+- Use a dialog for short and non-frequent tasks.
+  Consider using the main flow for regular tasks.
+- Wrap multiple buttons in an [Action Group](https://designsystem.amsterdam/?path=/docs/components-layout-action-group--docs).
+- To open the Dialog, use \`Dialog.open(dialogId)\` from the React package.
+- To close it, either call the \`Dialog.close\` function or add a \`<form>\` as in the ‘confirmation’ example.
+
+## Keyboard support
+
+| key         | function                                                         |
+| :---------- | :--------------------------------------------------------------- |
+| Tab         | Moves focus to the next focusable element inside the dialog.     |
+| Shift + Tab | Moves focus to the previous focusable element inside the dialog. |
+| Escape      | Closes the dialog.                                               |
+
+## References
+
+- [HTMLDialogElement](https://developer.mozilla.org/en-US/docs/Web/API/HTMLDialogElement)
+- [Return value](https://developer.mozilla.org/en-US/docs/Web/API/HTMLDialogElement/returnValue)
+- [Modal & Nonmodal Dialogs: When (& When Not) to Use Them](https://www.nngroup.com/articles/modal-nonmodal-dialog/)
+- [Patterns](https://www.w3.org/WAI/ARIA/apg/patterns/dialog-modal/)
+`;/*@license CC0-1.0*/function t(n){const e={a:"a",code:"code",h2:"h2",h3:"h3",p:"p",...i(),...n.components};return o.jsxs(o.Fragment,{children:[`
+`,`
+`,o.jsx(r,{of:c}),`
+`,o.jsx(s,{children:h}),`
+`,o.jsx(a,{}),`
+`,o.jsx(l,{}),`
+`,o.jsx(e.h2,{id:"examples",children:"Examples"}),`
+`,o.jsx(e.h3,{id:"asking-to-confirm",children:"Asking to confirm"}),`
+`,o.jsxs(e.p,{children:["Use a ",o.jsx(e.code,{children:"form"}),` when asking the user to confirm an action, e.g. through ‘OK’ and ‘Cancel’ buttons.
+Add `,o.jsx(e.code,{children:'method="dialog"'}),` to make the browser close the Dialog when the form is submitted.
+Wrap multiple Buttons in an `,o.jsx(e.a,{href:"/docs/components-layout-action-group--docs",children:"Action Group"})," and place it in the ",o.jsx(e.code,{children:"footer"}),`.
+Add an `,o.jsx(e.code,{children:"id"})," to the form and use it in the ",o.jsx(e.code,{children:"form"}),` attribute of the buttons.
+Set a `,o.jsx(e.code,{children:"value"}),` on each button to identify which one was clicked.
+Consult `,o.jsx(e.a,{href:"https://developer.mozilla.org/en-US/docs/Web/HTML/Element/dialog#handling_the_return_value_from_the_dialog",rel:"nofollow",children:"MDN"})," for more information."]}),`
+`,o.jsx(d,{of:m})]})}function U(n={}){const{wrapper:e}={...i(),...n.components};return e?o.jsx(e,{...n,children:o.jsx(t,{...n})}):t(n)}export{U as default};
---
+++ demo-DES-1354-application-menu/assets/Field.stories-CR77JLTS.js
@@ -0,0 +1,26 @@
+import{j as e}from"./jsx-runtime-Cf8x2fCZ.js";import{c as d,T as t,P as o,E as g}from"./index.esm-Bxisna0a.js";import{c as u}from"./clsx-B-dksMZM.js";import{r as v}from"./index-G8LIXM5I.js";const a=v.forwardRef(({children:i,className:l,invalid:p,...c},m)=>e.jsx("div",{...c,className:u("ams-field",p&&"ams-field--invalid",l),ref:m,children:i}));a.displayName="Field";try{a.displayName="Field",a.__docgenInfo={description:"",displayName:"Field",props:{invalid:{defaultValue:null,description:"Whether the field has an input with a validation error",name:"invalid",required:!1,type:{name:"boolean"}}}}}catch{}const h={title:"Components/Forms/Field",component:a,args:{invalid:!1}},r={render:i=>e.jsxs(a,{invalid:i.invalid,children:[e.jsx(d,{htmlFor:"input1",children:"Waar gaat het om?"}),e.jsx(t,{id:"input1",invalid:i.invalid})]})},n={render:i=>e.jsxs(a,{invalid:i.invalid,children:[e.jsx(d,{htmlFor:"input2",children:"Waar gaat het om?"}),e.jsx(o,{id:"description1",size:"small",children:"Typ geen persoonsgegevens in deze omschrijving. We vragen dit later in dit formulier aan u."}),e.jsx(t,{"aria-describedby":"description1",id:"input2",invalid:i.invalid})]})},s={args:{invalid:!0},render:i=>e.jsxs(a,{invalid:i.invalid,children:[e.jsx(d,{htmlFor:"input3",children:"Waar gaat het om?"}),e.jsx(o,{id:"description2",size:"small",children:"Typ geen persoonsgegevens in deze omschrijving. We vragen dit later in dit formulier aan u."}),e.jsx(g,{id:"error",children:"Geef aan waar het om gaat."}),e.jsx(t,{"aria-describedby":"description2 error","aria-required":!0,id:"input3",invalid:i.invalid})]})};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  render: args => <Field invalid={args.invalid}>
+      <Label htmlFor="input1">Waar gaat het om?</Label>
+      <TextInput id="input1" invalid={args.invalid} />
+    </Field>
+}`,...r.parameters?.docs?.source}}};n.parameters={...n.parameters,docs:{...n.parameters?.docs,source:{originalSource:`{
+  render: args => <Field invalid={args.invalid}>
+      <Label htmlFor="input2">Waar gaat het om?</Label>
+      <Paragraph id="description1" size="small">
+        Typ geen persoonsgegevens in deze omschrijving. We vragen dit later in dit formulier aan u.
+      </Paragraph>
+      <TextInput aria-describedby="description1" id="input2" invalid={args.invalid} />
+    </Field>
+}`,...n.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    invalid: true
+  },
+  render: args => <Field invalid={args.invalid}>
+      <Label htmlFor="input3">Waar gaat het om?</Label>
+      <Paragraph id="description2" size="small">
+        Typ geen persoonsgegevens in deze omschrijving. We vragen dit later in dit formulier aan u.
+      </Paragraph>
+      <ErrorMessage id="error">Geef aan waar het om gaat.</ErrorMessage>
+      <TextInput aria-describedby="description2 error" aria-required id="input3" invalid={args.invalid} />
+    </Field>
+}`,...s.parameters?.docs?.source}}};const f=["Default","WithDescription","WithValidation"],y=Object.freeze(Object.defineProperty({__proto__:null,Default:r,WithDescription:n,WithValidation:s,__namedExportsOrder:f,default:h},Symbol.toStringTag,{value:"Module"}));export{y as F,n as W,s as a};
