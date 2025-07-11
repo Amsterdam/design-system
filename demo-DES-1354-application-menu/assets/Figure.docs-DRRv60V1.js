f7480dd7ae067ee0906cd9234dbd4a1db8a756d9
dlnr <niels@dlnr.dev>
2025-07-11 20:42:00 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@5574cb352cef9dac658f1786a62b2bcba0a2c778 🚀

---
+++ demo-DES-1354-application-menu/assets/Figure.docs-DRRv60V1.js
@@ -0,0 +1,15 @@
+import{j as n}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as r}from"./index-CxbY7dAs.js";import{M as s,e as i,P as a,h as m}from"./index-CdJBja9t.js";import{F as p,I as c}from"./Figure.stories-vaICCFpG.js";import"./index-yBjzXJbu.js";import"./index-G8LIXM5I.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-SZcZ0wzI.js";import"./index-CJ88nXQk.js";import"./index-Cf9b6H0j.js";import"./index-DrFu-skq.js";import"./index.esm-Bxisna0a.js";import"./clsx-B-dksMZM.js";import"./index.esm-CiTCpp5W.js";import"./exampleContent-r-zRGsZY.js";const h=`<!-- @license CC0-1.0 -->
+
+# Figure
+
+Groups media content with a caption that describes it.
+`;/*@license CC0-1.0*/function e(o){const t={h2:"h2",h3:"h3",p:"p",...r(),...o.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(s,{of:p}),`
+`,n.jsx(i,{children:h}),`
+`,n.jsx(a,{}),`
+`,n.jsx(t.h2,{id:"examples",children:"Examples"}),`
+`,n.jsx(t.h3,{id:"inverse-colour",children:"Inverse colour"}),`
+`,n.jsx(t.p,{children:`Use the inverse colour if the Figure Caption sits on a dark background.
+This ensures the colour of the text provides enough contrast.`}),`
+`,n.jsx(m,{of:c})]})}function I(o={}){const{wrapper:t}={...r(),...o.components};return t?n.jsx(t,{...o,children:n.jsx(e,{...o})}):e(o)}export{I as default};
