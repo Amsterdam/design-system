cfa57dd5f5401e5de4ef260a499143e65b84e91a
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-07-14 13:06:50 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@4130f5a1ecce406b2230e8cc67900e06df03a601 🚀

---
+++ demo-develop/assets/Blockquote.docs-DwAQB7MG.js
@@ -0,0 +1,27 @@
+import{j as t}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as r}from"./index-CxbY7dAs.js";import{M as s,e as i,P as a,f as h,h as m}from"./index-Y5mERu4V.js";import{B as u,I as c}from"./Blockquote.stories-BbkOYAiW.js";import"./index-yBjzXJbu.js";import"./index-G8LIXM5I.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-D43fA0ff.js";import"./index-CJ88nXQk.js";import"./index-Cf9b6H0j.js";import"./index-DrFu-skq.js";import"./exampleContent-r-zRGsZY.js";import"./clsx-B-dksMZM.js";const l=`<!-- @license CC0-1.0 -->
+
+# Blockquote
+
+We use blockquotes to emphasize citations.
+A quote appears differently from the main text to make it stand out.
+
+## Guidelines
+
+- Quote someone’s literal written or oral text.
+- A quote has a width of half or a whole column of text.
+- The component adds the correct quotation marks.
+
+## Relevant WCAG requirements
+
+- [WCAG 3.1.2](https://www.w3.org/WAI/WCAG21/Understanding/language-of-parts.html): if the citation is in a language other than that of the surrounding content, indicate so through the \`lang\` attribute.
+`;/*@license CC0-1.0*/function o(n){const e={h2:"h2",h3:"h3",p:"p",...r(),...n.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(s,{of:u}),`
+`,t.jsx(i,{children:l}),`
+`,t.jsx(a,{}),`
+`,t.jsx(h,{}),`
+`,t.jsx(e.h2,{id:"examples",children:"Examples"}),`
+`,t.jsx(e.h3,{id:"inverse-colour",children:"Inverse colour"}),`
+`,t.jsx(e.p,{children:`Use the inverse colour if the Blockquote sits on a dark background.
+This ensures the colour of the text provides enough contrast.`}),`
+`,t.jsx(m,{of:c})]})}function W(n={}){const{wrapper:e}={...r(),...n.components};return e?t.jsx(e,{...n,children:t.jsx(o,{...n})}):o(n)}export{W as default};
---
+++ demo-develop/assets/StandaloneLink.stories-DBjiy5eG.js
@@ -0,0 +1,26 @@
+import{j as e}from"./jsx-runtime-Cf8x2fCZ.js";import{H as u,P as h}from"./index.esm-BpefUuPx.js";import{I as c,f as g}from"./index.esm-CVKOIKGq.js";import{c as k}from"./clsx-B-dksMZM.js";import{r as f}from"./index-G8LIXM5I.js";import{I as v}from"./Icon-BBW_x4VP.js";const t=f.forwardRef(({children:s,className:d,color:l,icon:i,...m},p)=>e.jsxs("a",{...m,className:k("ams-standalone-link",l&&`ams-standalone-link--${l}`,i&&"ams-standalone-link--with-icon",d),ref:p,children:[i&&e.jsx(v,{svg:i}),s]}));t.displayName="StandaloneLink";try{t.displayName="StandaloneLink",t.__docgenInfo={description:"",displayName:"StandaloneLink",props:{color:{defaultValue:null,description:"Changes the text colour for readability on a light or dark background.",name:"color",required:!1,type:{name:"enum",value:[{value:'"inverse"'},{value:'"contrast"'}]}},icon:{defaultValue:null,description:"Adds an icon to the link, showing it before the link text.",name:"icon",required:!1,type:{name:"Function"}}}}}catch{}const w={title:"Components/Navigation/Standalone Link",component:t,args:{children:"Link label",href:"#"},argTypes:{children:{description:"The link text.",table:{disable:!1}},color:{control:{labels:{undefined:"default"},type:"radio"},options:[void 0,"contrast","inverse"]},href:{description:"The url for the link.",name:"href",type:{name:"string",required:!1}},icon:{control:{labels:{undefined:"none"},type:"select"},mapping:c,options:[void 0,...Object.keys(c)]}}},a={},r={args:{color:"contrast"}},n={args:{color:"inverse"}},o={args:{children:"Bekijk alle werkzaamheden",icon:g},decorators:[s=>e.jsxs("article",{style:{maxWidth:"32rem"},children:[e.jsx(u,{className:"ams-mb-xs",level:2,children:"Werkzaamheden"}),e.jsx(h,{className:"ams-mb-s",children:"Lees waar en wanneer we werken aan nieuwbouw, groot onderhoud, herinrichting van straten en wegen, aanpak van parken of ontwikkeling van hele gebieden."}),e.jsx(s,{})]})]};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:"{}",...a.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    color: 'contrast'
+  }
+}`,...r.parameters?.docs?.source}}};n.parameters={...n.parameters,docs:{...n.parameters?.docs,source:{originalSource:`{
+  args: {
+    color: 'inverse'
+  }
+}`,...n.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Bekijk alle werkzaamheden',
+    icon: ChevronForwardIcon
+  },
+  decorators: [Story => <article style={{
+    maxWidth: '32rem'
+  }}>
+        <Heading className="ams-mb-xs" level={2}>
+          Werkzaamheden
+        </Heading>
+        <Paragraph className="ams-mb-s">
+          Lees waar en wanneer we werken aan nieuwbouw, groot onderhoud, herinrichting van straten en wegen, aanpak van
+          parken of ontwikkeling van hele gebieden.
+        </Paragraph>
+        <Story />
+      </article>]
+}`,...o.parameters?.docs?.source}}};const b=["Default","ContrastColour","InverseColour","WithChevronIcon"],I=Object.freeze(Object.defineProperty({__proto__:null,ContrastColour:r,Default:a,InverseColour:n,WithChevronIcon:o,__namedExportsOrder:b,default:w},Symbol.toStringTag,{value:"Module"}));export{r as C,n as I,I as S,o as W};
