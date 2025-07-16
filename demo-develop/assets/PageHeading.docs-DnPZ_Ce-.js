05e5dac9ebcf144b45b0f8a57ce242bd83a737d8
alimpens <37216945+alimpens@users.noreply.github.com>
2025-07-16 08:37:13 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@e25a5ea76d4d7a88097dfeb95e1aa51caa550e4e 🚀

---
+++ demo-develop/assets/PageHeading.docs-DnPZ_Ce-.js
@@ -0,0 +1,30 @@
+import{j as e}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as s}from"./index-CxbY7dAs.js";import{M as i,e as a,P as r,f as h,h as m}from"./index-fmTc2Ra-.js";import{P as p,I as c}from"./PageHeading.stories-kXvA5Y7B.js";import"./index-yBjzXJbu.js";import"./index-G8LIXM5I.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-CTLIAD5I.js";import"./index-CJ88nXQk.js";import"./index-Cf9b6H0j.js";import"./index-DrFu-skq.js";import"./clsx-B-dksMZM.js";const d=`<!-- @license CC0-1.0 -->
+
+# Page Heading
+
+The page heading component is a prominent text at the beginning of a page, immediately after the Page Header.
+Use this component for a name, title, or motto.
+
+## Guidelines
+
+- Use this component to capture the user’s attention.
+- Only use this component once per page, typically only on the homepage.
+- Avoid using it if you need to display long pieces of text; the text should be short and precise.
+
+## Relevant WCAG requirements
+
+The page heading component is a variant of a heading level 1 with a distinct style.
+When using this component, ensure that the heading hierarchy of the page remains logical.
+
+For this component, the same WCAG requirements apply as for [the heading component](/docs/components-text-heading--docs).
+`;/*@license CC0-1.0*/function o(n){const t={h2:"h2",h3:"h3",p:"p",...s(),...n.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(i,{of:p}),`
+`,e.jsx(a,{children:d}),`
+`,e.jsx(r,{}),`
+`,e.jsx(h,{}),`
+`,e.jsx(t.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(t.h3,{id:"inverse-colour",children:"Inverse colour"}),`
+`,e.jsx(t.p,{children:`Use the inverse colour if the Page Heading sits on a dark background.
+This ensures the colour of the text provides enough contrast.`}),`
+`,e.jsx(m,{of:c})]})}function D(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o(n)}export{D as default};
