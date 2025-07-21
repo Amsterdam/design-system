fe4fce4972992c1290f78f724132dd0e84d5af5f
alimpens <37216945+alimpens@users.noreply.github.com>
2025-07-21 15:28:32 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@5da623090fa47a93e30f754b8f46503403df960f 🚀

---
+++ demo-DES-1184-remove-title-and-img-from-nav-page-header/assets/ArticlePage.docs-BJY3ZiRh.js
@@ -0,0 +1,5 @@
+import{j as t}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as n}from"./index-CxbY7dAs.js";import{M as e}from"./index-DpdqzK7L.js";import{A as m}from"./ArticlePage.stories-DJWO_K39.js";import"./index-yBjzXJbu.js";import"./index-G8LIXM5I.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-j6WkQK6C.js";import"./index-CJ88nXQk.js";import"./index-Cf9b6H0j.js";import"./index-DrFu-skq.js";import"./index.esm-CsOOCRIC.js";import"./clsx-B-dksMZM.js";import"./index.esm-CVKOIKGq.js";import"./Image.stories-f9T87IiS.js";import"./Image-Ci7950RD.js";import"./generateAspectRatioClass-CefTNpDa.js";import"./config-CmwxjGch.js";import"./PageFooter.stories-bJnWWqqy.js";/*@license CC0-1.0*/function i(o){const r={h1:"h1",p:"p",...n(),...o.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(e,{of:m}),`
+`,t.jsx(r.h1,{id:"article-page",children:"Article page"}),`
+`,t.jsx(r.p,{children:"This page type is for news or similar kinds of articles."})]})}function _(o={}){const{wrapper:r}={...n(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(i,{...o})}):i(o)}export{_ as default};
