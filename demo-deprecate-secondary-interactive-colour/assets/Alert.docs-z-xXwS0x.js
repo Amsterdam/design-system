95c4de09ac419c5338a0241b9b30ddbd5fa6c8ec
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-07-23 13:35:31 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@f2781811eb224df6948c5d8bfd1a426b102d9a87 🚀

---
+++ demo-deprecate-secondary-interactive-colour/assets/Alert.docs-z-xXwS0x.js
@@ -0,0 +1,45 @@
+import{j as n}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as s}from"./index-CxbY7dAs.js";import{M as o,e as a,P as h,f as c,h as t}from"./index-0H6eWAka.js";import{A as l,W as m,E as p,S as d,I as u,a as f}from"./Alert.stories-yQviXdlN.js";import"./index-yBjzXJbu.js";import"./index-G8LIXM5I.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-Bi8hTzCp.js";import"./index-CJ88nXQk.js";import"./index-Cf9b6H0j.js";import"./index-DrFu-skq.js";import"./index.esm-r0Mi5W58.js";import"./clsx-B-dksMZM.js";import"./index.esm-CVKOIKGq.js";import"./Alert-DnKWT2C0.js";import"./Icon-Pril9caF.js";import"./Row-JeNeYdOs.js";import"./Heading-7IbEntpa.js";import"./getHeadingTag-C4Rztoc8.js";import"./IconButton-Dz7ZQ_1j.js";const x=`<!-- @license CC0-1.0 -->
+
+# Alert
+
+An Alert informs the user of a significant or time-sensitive message without interrupting their task.
+
+## Guidelines
+
+- Use an Alert if the message’s purpose matches one of the following:
+  - **Information** to bring attention to a message.
+    This variation is blue and it is the default.
+  - **Success** to reassure that a process is complete.
+    This variation is green.
+  - **Warning** when action is needed to prevent damage.
+    This variation is orange.
+  - **Error** indicates an error has occurred.
+    This variation is red.
+- Place the Alert in a Grid Cell to create enough white space around it.
+- Important and urgent information is to be placed directly below the Page Header.
+  Examples: system outage or changes in the opening hours of a City Office.
+  This can be applied to the entire application or to a part of it.
+- Add a close button if the user should be able to dismiss the Alert.
+`;/*@license CC0-1.0*/function r(i){const e={h2:"h2",h3:"h3",p:"p",...s(),...i.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(o,{of:l}),`
+`,n.jsx(a,{children:x}),`
+`,n.jsx(h,{}),`
+`,n.jsx(c,{}),`
+`,n.jsx(e.h2,{id:"examples",children:"Examples"}),`
+`,n.jsx(e.h3,{id:"warning",children:"Warning"}),`
+`,n.jsx(e.p,{children:"Display a warning when user action is required."}),`
+`,n.jsx(t,{of:m}),`
+`,n.jsx(e.h3,{id:"error",children:"Error"}),`
+`,n.jsx(e.p,{children:`In case of an error, use the red alert.
+Provide the user with suggestions on alternative ways to achieve the goal.`}),`
+`,n.jsx(t,{of:p}),`
+`,n.jsx(e.h3,{id:"success",children:"Success"}),`
+`,n.jsx(e.p,{children:"Announce the success of a significant action."}),`
+`,n.jsx(t,{of:d}),`
+`,n.jsx(e.h3,{id:"information",children:"Information"}),`
+`,n.jsx(e.p,{children:"An informative message can emphasize matters that are useful to follow."}),`
+`,n.jsx(t,{of:u}),`
+`,n.jsx(e.h3,{id:"with-inline-link",children:"With inline link"}),`
+`,n.jsx(e.p,{children:"Include an inline link in the text to guide the user."}),`
+`,n.jsx(t,{of:f})]})}function R(i={}){const{wrapper:e}={...s(),...i.components};return e?n.jsx(e,{...i,children:n.jsx(r,{...i})}):r(i)}export{R as default};
---
+++ demo-deprecate-secondary-interactive-colour/assets/generateAspectRatioClass-CefTNpDa.js
@@ -0,0 +1,4 @@
+/**
+ * @license EUPL-1.2+
+ * Copyright Gemeente Amsterdam
+ */const a=e=>e&&`ams-aspect-ratio-${e.replace(":","-")}`;export{a as g};
