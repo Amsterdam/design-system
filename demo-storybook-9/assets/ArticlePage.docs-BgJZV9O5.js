3c9105653ccc6dc5a7a54bc56693fb36aa509267
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-07-25 14:52:33 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@de410b959b062ed308105f9b46d9dbc6705c3a22 🚀

---
+++ demo-storybook-9/assets/ArticlePage.docs-BgJZV9O5.js
@@ -0,0 +1,5 @@
+import{j as t}from"./iframe-Bwj7fvgb.js";import{useMDXComponents as e}from"./index-CljgXjKA.js";import{M as i}from"./blocks-P6KaOaYQ.js";import{A as s}from"./ArticlePage.stories-K8htvWpD.js";import"./index-BolGF98R.js";import"./Image.stories-C8mURwj0.js";import"./Image-CR3JbzNc.js";import"./generateAspectRatioClass-CefTNpDa.js";import"./config-C3vc9uKr.js";import"./PageFooter.stories-CsMX4Owb.js";/*@license CC0-1.0*/function n(o){const r={h1:"h1",p:"p",...e(),...o.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(i,{of:s}),`
+`,t.jsx(r.h1,{id:"article-page",children:"Article page"}),`
+`,t.jsx(r.p,{children:"This page type is for news or similar kinds of articles."})]})}function u(o={}){const{wrapper:r}={...e(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(n,{...o})}):n(o)}export{u as default};
