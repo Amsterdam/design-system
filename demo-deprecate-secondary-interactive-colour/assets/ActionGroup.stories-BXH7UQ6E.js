ea70be2ae6858676f65b49d3d1168159652bac18
alimpens <37216945+alimpens@users.noreply.github.com>
2025-07-25 09:36:47 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@d8ea4cd9be4574aabc378edf3e2af0c735779ded 🚀

---
+++ demo-deprecate-secondary-interactive-colour/assets/ActionGroup.stories-BXH7UQ6E.js
@@ -0,0 +1,15 @@
+import{j as e}from"./jsx-runtime-Cf8x2fCZ.js";import{B as n,S as l}from"./index.esm-D-ZCYk5B.js";import{j as p}from"./index.esm-fTUnZvIH.js";import{c as m}from"./clsx-B-dksMZM.js";import{r as u}from"./index-G8LIXM5I.js";const t=u.forwardRef(({children:s,className:i,...c},d)=>e.jsx("div",{...c,className:m("ams-action-group",i),ref:d,role:"group",children:s}));t.displayName="ActionGroup";try{t.displayName="ActionGroup",t.__docgenInfo={description:"",displayName:"ActionGroup",props:{}}}catch{}const g={title:"Components/Layout/Action Group",component:t,args:{children:[e.jsx(n,{children:"Doorgaan"}),e.jsx(n,{variant:"secondary",children:"Stoppen"})]}},r={},o={args:{children:[e.jsx(n,{children:"Adres wijzigen"}),e.jsx(n,{variant:"secondary",children:"Adres verwijderen"})],className:"ams-resize-horizontal",style:{inlineSize:"16rem"}}},a={args:{children:[e.jsx(n,{children:"Bewerken"},1),e.jsx(l,{download:!0,href:"#",icon:p,children:"Downloaden"},2)]}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:"{}",...r.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<Button>Adres wijzigen</Button>, <Button variant="secondary">Adres verwijderen</Button>],
+    className: 'ams-resize-horizontal',
+    style: {
+      inlineSize: '16rem'
+    }
+  }
+}`,...o.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<Button key={1}>Bewerken</Button>, <StandaloneLink download href="#" icon={DownloadIcon} key={2}>
+        Downloaden
+      </StandaloneLink>]
+  }
+}`,...a.parameters?.docs?.source}}};const h=["Default","Stacked","WithLink"],w=Object.freeze(Object.defineProperty({__proto__:null,Default:r,Stacked:o,WithLink:a,__namedExportsOrder:h,default:g},Symbol.toStringTag,{value:"Module"}));export{w as A,o as S,a as W};
---
+++ demo-deprecate-secondary-interactive-colour/assets/Blockquote.stories-n8yT5EDT.js
@@ -0,0 +1,8 @@
+import{c as n}from"./exampleContent-B9pMnolB.js";import{j as u}from"./jsx-runtime-Cf8x2fCZ.js";import{c as i}from"./clsx-B-dksMZM.js";import{r as p}from"./index-G8LIXM5I.js";const r=p.forwardRef(({children:t,className:s,color:a,...c},l)=>u.jsx("blockquote",{...c,className:i("ams-blockquote",a&&`ams-blockquote--${a}`,s),ref:l,children:t}));r.displayName="Blockquote";try{r.displayName="Blockquote",r.__docgenInfo={description:"",displayName:"Blockquote",props:{color:{defaultValue:null,description:"Changes the text colour for readability on a dark background.",name:"color",required:!1,type:{name:"enum",value:[{value:'"inverse"'}]}}}}}catch{}/**
+ * @license EUPL-1.2+
+ * Copyright Gemeente Amsterdam
+ */const d=n(),m={title:"Components/Text/Blockquote",component:r,args:{children:d},argTypes:{children:{description:"The text for the quote.",table:{disable:!1}},color:{control:{labels:{undefined:"default"},type:"radio"},options:[void 0,"inverse"]}}},e={},o={args:{color:"inverse"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:"{}",...e.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    color: 'inverse'
+  }
+}`,...o.parameters?.docs?.source}}};const f=["Default","InverseColour"],k=Object.freeze(Object.defineProperty({__proto__:null,Default:e,InverseColour:o,__namedExportsOrder:f,default:m},Symbol.toStringTag,{value:"Module"}));export{k as B,o as I};
