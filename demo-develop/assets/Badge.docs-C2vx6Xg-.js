c7862f163ebf9aabe6c519536b908c844b215927
alimpens <37216945+alimpens@users.noreply.github.com>
2025-07-25 09:23:34 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@908fc5d107ded55c344d9d8c40feeb8c2071a1c7 🚀

---
+++ demo-develop/assets/Badge.docs-C2vx6Xg-.js
@@ -0,0 +1,19 @@
+import{j as n}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as t}from"./index-CxbY7dAs.js";import{M as s,e as a,P as i,f as c}from"./index-B6f73r1P.js";import{B as m}from"./Badge.stories-CW8lnkKX.js";import"./index-yBjzXJbu.js";import"./index-G8LIXM5I.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-xEtu5QWr.js";import"./index-CJ88nXQk.js";import"./index-Cf9b6H0j.js";import"./index-DrFu-skq.js";import"./clsx-B-dksMZM.js";const d=`<!-- @license CC0-1.0 -->
+
+# Badge
+
+A prominent label that indicates a category, property or instruction.
+
+## Guidelines
+
+- Let the badge contain a short text or a number.
+- Use any of the feedback or highlight [colours](/docs/brand-design-tokens-colour--docs) for the background.
+  The default is green.
+  Red, orange and green are especially useful for showing an error, warning, or success.
+  However, they can also be combined with the other colours without carrying these meanings.
+`;/*@license CC0-1.0*/function r(e){return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(s,{of:m}),`
+`,n.jsx(a,{children:d}),`
+`,n.jsx(i,{}),`
+`,n.jsx(c,{})]})}function C(e={}){const{wrapper:o}={...t(),...e.components};return o?n.jsx(o,{...e,children:n.jsx(r,{...e})}):r()}export{C as default};
