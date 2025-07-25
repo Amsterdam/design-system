10e78ad2b1fcf0ff0ba311603519a9d06e429ff4
dlnr <niels@dlnr.dev>
2025-07-25 10:41:28 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@4e2893b5512f57a97760dd70c238116f1ce204b4 🚀

---
+++ demo-DES-1354-application-menu/assets/NavigationPage.docs-B56oXJKL.js
@@ -0,0 +1,6 @@
+import{j as t}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as r}from"./index-CxbY7dAs.js";import{M as e}from"./index-DQ1dNYPZ.js";import{N as p}from"./NavigationPage.stories-CaVDUHz5.js";import"./index-yBjzXJbu.js";import"./index-G8LIXM5I.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-DayKLWd8.js";import"./index-CJ88nXQk.js";import"./index-Cf9b6H0j.js";import"./index-DrFu-skq.js";import"./index.esm-4FzYp2wj.js";import"./clsx-B-dksMZM.js";import"./index.esm-D3RaMjqb.js";import"./exampleContent-B9pMnolB.js";import"./config-BNJgXzx1.js";import"./PageFooter.stories-BwcRaoMG.js";/*@license CC0-1.0*/function i(o){const n={h1:"h1",p:"p",...r(),...o.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(e,{of:p}),`
+`,t.jsx(n.h1,{id:"navigation-page",children:"Navigation page"}),`
+`,t.jsx(n.p,{children:`A navigation page does not have much content of its own.
+Instead, it presents groups of links to various pages related to a specific subject.`})]})}function X(o={}){const{wrapper:n}={...r(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(i,{...o})}):i(o)}export{X as default};
---
+++ demo-DES-1354-application-menu/assets/Overlap.docs-DnhtCAb_.js
@@ -0,0 +1,21 @@
+import{j as e}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as r}from"./index-CxbY7dAs.js";import{M as s,e as t,P as a}from"./index-DQ1dNYPZ.js";import{O as l}from"./Overlap.stories-xuCU-HPW.js";import"./index-yBjzXJbu.js";import"./index-G8LIXM5I.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-DayKLWd8.js";import"./index-CJ88nXQk.js";import"./index-Cf9b6H0j.js";import"./index-DrFu-skq.js";import"./index.esm-4FzYp2wj.js";import"./clsx-B-dksMZM.js";import"./index.esm-D3RaMjqb.js";const m=`<!-- @license CC0-1.0 -->
+
+# Overlap
+
+Displays two or more components on top of each other.
+`;/*@license CC0-1.0*/function i(n){const o={a:"a",h2:"h2",h3:"h3",li:"li",p:"p",ul:"ul",...r(),...n.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(s,{of:l}),`
+`,e.jsx(t,{children:m}),`
+`,e.jsx(o.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(o.h3,{id:"hero-image-with-search-field",children:"Hero image with Search Field"}),`
+`,e.jsxs(o.p,{children:[`This allows for the use of a mood-setting image as a background.
+A `,e.jsx(o.a,{href:"/docs/components-layout-grid--docs",children:"Grid"}),` provides horizontal space on both sides and columns for layout.
+Note: the library does not yet offer an option to vertically center the form.`]}),`
+`,e.jsx(a,{}),`
+`,e.jsx(o.h2,{id:"guidelines",children:"Guidelines"}),`
+`,e.jsxs(o.ul,{children:[`
+`,e.jsxs(o.li,{children:["Follow ",e.jsx(o.a,{href:"/docs/components-media-image--docs#guidelines",children:"the general guidelines for images"}),"."]}),`
+`,e.jsx(o.li,{children:`Use a sufficiently large image.
+The aspect ratio of 32:9 is most suitable.`}),`
+`]})]})}function b(n={}){const{wrapper:o}={...r(),...n.components};return o?e.jsx(o,{...n,children:e.jsx(i,{...n})}):i(n)}export{b as default};
