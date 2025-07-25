979cf7443bf8ba3c0cedfe59a03ef51ba4db82cb
Aram <37216945+alimpens@users.noreply.github.com>
2025-07-25 13:30:59 +0200

    chore: Simplify update scripts (#2146)

--- .ncurc.major.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable no-undef */
-const minorConfig = require('./.ncurc.minor')
-
-module.exports = {
-  reject: [...minorConfig.reject],
-}
--- .ncurc.minor.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable no-undef */
-const patchConfig = require('./.ncurc.patch')
-
-module.exports = {
-  reject: [...patchConfig.reject],
-}
