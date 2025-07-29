653f20af5c994873928dd4fd3537308e23129402
Vincent Smedinga <v.smedinga@amsterdam.nl>
2025-07-30 00:04:25 +0200

    Add example of search results with Mark

--- storybook/src/components/Mark/Mark.stories.tsx
+++ storybook/src/components/Mark/Mark.stories.tsx
@@ -3,7 +3,7 @@
  * Copyright Gemeente Amsterdam
  */
 
-import { Paragraph } from '@amsterdam/design-system-react'
+import { Card, Column, Grid, Heading, Paragraph, SearchField } from '@amsterdam/design-system-react'
 import { Mark } from '@amsterdam/design-system-react/src'
 import { Meta, StoryObj } from '@storybook/react'
 
@@ -32,5 +32,86 @@ export const Default: Story = {
       de stad beter schoonhouden. <Mark>{children}</Mark> Welke oplossingen zien zij? Hier zijn we benieuwd naar. Want
       elke Amsterdammer heeft afval en moet het kwijt. Wij kunnen als gemeente veel van deze afvalexperts leren.
     </Paragraph>
+  ),
+}
+
+type SearchResult = {
+  category: string
+  date: Date
+  fragment: string
+  heading: string
+}
+
+const searchResults: SearchResult[] = [
+  {
+    category: 'Kansspelen',
+    date: new Date('2025-10-27'),
+    fragment:
+      'Op dit moment zijn alle vergunning voor speelautomatenhallen verleend. Als u kansspelautomaten wilt plaatsen in uw horecabedrijf dan heeft u de …',
+    heading: 'Vergunning speelautomatenhal of kansspelautomaten aanvragen',
+  },
+  {
+    category: 'Veelgevraagd',
+    date: new Date('2024-09-15'),
+    fragment:
+      'U heeft een ontheffing nodig als u de geldende geluidsgrenzen van uw horecagelegenheid wil overschrijden. Kijk hier hoe het werkt en hoe u de …',
+    heading: 'Ontheffing geluidsvoorschriften',
+  },
+  {
+    category: 'Vergunningen',
+    date: new Date('2023-08-03'),
+    fragment: `Voor de organisatie van grootschalige vechtsportgala’s in Amsterdam moet u een vergunning aanvragen bij de gemeente. Vooraf moet u een Bibobformulier …`,
+    heading: 'Vergunning vechtsportevenementen',
+  },
+]
+
+const dateFormat = new Intl.DateTimeFormat('nl', {
+  day: 'numeric',
+  month: 'long',
+  year: 'numeric',
+})
+
+const query = 'horeca vergunning'
+
+const mark = (text: string, terms: string) => {
+  const regex = new RegExp(`(${terms.replace(' ', '|')})`, 'gi')
+  return text
+    .split(regex)
+    .map((part, index) => (terms.split(' ').includes(part.toLowerCase()) ? <Mark key={index}>{part}</Mark> : part))
+}
+
+export const SearchResults: Story = {
+  argTypes: {
+    children: {
+      table: { disable: true },
+    },
+  },
+  render: () => (
+    <Grid>
+      <Grid.Cell span={{ narrow: 4, medium: 6, wide: 8 }}>
+        <SearchField className="ams-mb-m">
+          <SearchField.Input label="Zoeken" value={query} />
+          <SearchField.Button />
+        </SearchField>
+        <Paragraph className="ams-mb-xl">
+          <strong>{searchResults.length}</strong> artikelen gaan over ‘{query}’.
+        </Paragraph>
+        <Column gap="x-large">
+          {searchResults.map(({ category, date, fragment, heading }) => (
+            <Card key={heading}>
+              <Card.HeadingGroup tagline={category}>
+                <Heading level={2} size="level-4">
+                  <Card.Link href="#">{mark(heading, query)}</Card.Link>
+                </Heading>
+              </Card.HeadingGroup>
+              <Paragraph className="ams-mb-xs">{mark(fragment, query)}</Paragraph>
+              <Paragraph size="small">
+                <time dateTime={date.toISOString()}>{dateFormat.format(date)}</time>
+              </Paragraph>
+            </Card>
+          ))}
+        </Column>
+      </Grid.Cell>
+    </Grid>
   ),
 }
