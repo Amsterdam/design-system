c814e968406f29176ac02ac12c9fda9aa566e36c
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-07-30 14:00:56 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@13a46710a15e400fdb7e812b0fef3d5e6e3374bb 🚀

---
+++ demo-mark-example/assets/ProductPage.docs-CdZKg2MQ.js
@@ -0,0 +1,6 @@
+import{j as t}from"./jsx-runtime-Cf8x2fCZ.js";import{useMDXComponents as i}from"./index-CxbY7dAs.js";import{M as e}from"./index-8fPhpTT1.js";import{P as m}from"./ProductPage.stories-BFHEPOZU.js";import"./index-yBjzXJbu.js";import"./index-G8LIXM5I.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-KE_Cq8Mq.js";import"./index-CJ88nXQk.js";import"./index-Cf9b6H0j.js";import"./index-DrFu-skq.js";import"./index.esm-CMapxPh_.js";import"./clsx-B-dksMZM.js";import"./index.esm-D3RaMjqb.js";import"./config-eu0ci80M.js";import"./PageFooter.stories-Fr0ieBmN.js";/*@license CC0-1.0*/function n(o){const r={h1:"h1",p:"p",...i(),...o.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(e,{of:m}),`
+`,t.jsx(r.h1,{id:"product-page",children:"Product page"}),`
+`,t.jsx(r.p,{children:`Lists all information about a product or service of the City.
+Often includes a Call to Action Link that directs the user to a form.`})]})}function P(o={}){const{wrapper:r}={...i(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(n,{...o})}):n(o)}export{P as default};
