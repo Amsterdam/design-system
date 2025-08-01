8cfe9a1940ffa53f36977aa89fa346bbf6ed3712
GitHub <github-action@users.noreply.github.com>
2025-08-01 13:01:40 +0000

    Remove feature branch folder

--- demo-storybook-9/assets/ArticlePage.docs-B9GHDU-T.js
+++
@@ -1,5 +0,0 @@
-import{j as t}from"./iframe-B8vkLnOn.js";import{useMDXComponents as e}from"./index-BPsBOmHA.js";import{M as i}from"./blocks-4bzF2r4V.js";import{A as s}from"./ArticlePage.stories-7KAh2kuL.js";import"./index-CH3xkpyo.js";import"./Image.stories-ClWj09vP.js";import"./Image-CdNfzZ96.js";import"./generateAspectRatioClass-CefTNpDa.js";import"./config-cctdvh7o.js";import"./PageFooter.stories-p4mqYag7.js";/*@license CC0-1.0*/function n(o){const r={h1:"h1",p:"p",...e(),...o.components};return t.jsxs(t.Fragment,{children:[`
-`,`
-`,t.jsx(i,{of:s}),`
-`,t.jsx(r.h1,{id:"article-page",children:"Article page"}),`
-`,t.jsx(r.p,{children:"This page type is for news or similar kinds of articles."})]})}function u(o={}){const{wrapper:r}={...e(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(n,{...o})}):n(o)}export{u as default};
