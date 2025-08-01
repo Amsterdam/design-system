50ae4061c988cf512f4d2551e9e57c18eb5b581e
GitHub <github-action@users.noreply.github.com>
2025-08-01 19:07:50 +0000

    Remove feature branch folder

--- demo-DES-1387-fix-appearance-without-css/assets/ProductPage.docs-CYGwVILv.js
+++
@@ -1,6 +0,0 @@
-import{j as t}from"./iframe-CWlW8gBR.js";import{useMDXComponents as e}from"./index-B86ELECc.js";import{M as i}from"./blocks-DiJT5_0r.js";import{P as s}from"./ProductPage.stories-77lv1FJz.js";import"./index-XtaaeNzJ.js";import"./config-DV-B7HrL.js";import"./PageFooter.stories-BqIcNAuT.js";/*@license CC0-1.0*/function r(o){const n={h1:"h1",p:"p",...e(),...o.components};return t.jsxs(t.Fragment,{children:[`
-`,`
-`,t.jsx(i,{of:s}),`
-`,t.jsx(n.h1,{id:"product-page",children:"Product page"}),`
-`,t.jsx(n.p,{children:`Lists all information about a product or service of the City.
-Often includes a Call to Action Link that directs the user to a form.`})]})}function h(o={}){const{wrapper:n}={...e(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(r,{...o})}):r(o)}export{h as default};
