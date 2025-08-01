0961902164a19e56538abdbe19259d6e09a907f9
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-08-01 22:17:46 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@b74586d652d9594cac1a87a04df2cfa9bf30c744 🚀

---
+++ demo-DES-1351-heading-sizes/assets/ArticlePage.docs-D29qHGlN.js
@@ -0,0 +1,5 @@
+import{j as t}from"./iframe-C0t6SRZB.js";import{useMDXComponents as e}from"./index-DfALwJ82.js";import{M as i}from"./blocks-PGJ0pn3X.js";import{A as s}from"./ArticlePage.stories-BWFglb5H.js";import"./index-DACVHTtH.js";import"./common-DrnyCu3t.js";import"./HomeNews-HH53O1Yp.js";import"./config-Dem5Ra68.js";import"./PageFooter.stories-BdvHE4eE.js";/*@license CC0-1.0*/function r(n){const o={h1:"h1",p:"p",...e(),...n.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(i,{of:s}),`
+`,t.jsx(o.h1,{id:"article-page",children:"Article page"}),`
+`,t.jsx(o.p,{children:"This page type is for news or similar kinds of articles."})]})}function j(n={}){const{wrapper:o}={...e(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(r,{...n})}):r(n)}export{j as default};
--- demo-DES-1351-heading-sizes/assets/NavigationPage.docs-eQpzdJ4O.js
+++
@@ -1,6 +0,0 @@
-import{j as t}from"./iframe-C9wbfKto.js";import{useMDXComponents as i}from"./index-BVKh2AfG.js";import{M as s}from"./blocks-ZQqwzJ7p.js";import{N as r}from"./NavigationPage.stories-BEhIDeWf.js";import"./index-D6CT6hk-.js";import"./exampleContent-B9pMnolB.js";import"./config-iQLDaG8G.js";import"./PageFooter.stories-3mPGzWTB.js";/*@license CC0-1.0*/function e(n){const o={h1:"h1",p:"p",...i(),...n.components};return t.jsxs(t.Fragment,{children:[`
-`,`
-`,t.jsx(s,{of:r}),`
-`,t.jsx(o.h1,{id:"navigation-page",children:"Navigation page"}),`
-`,t.jsx(o.p,{children:`A navigation page does not have much content of its own.
-Instead, it presents groups of links to various pages related to a specific subject.`})]})}function x(n={}){const{wrapper:o}={...i(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(e,{...n})}):e(n)}export{x as default};
