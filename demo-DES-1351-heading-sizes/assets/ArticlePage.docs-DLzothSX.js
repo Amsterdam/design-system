e64f969ccc980fe895b5fce6f5b1fd2c62fe1ce6
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-08-01 22:01:21 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@b31331c8f9f6fc3b42ffad30523677dcf583c679 🚀

---
+++ demo-DES-1351-heading-sizes/assets/ArticlePage.docs-DLzothSX.js
@@ -0,0 +1,5 @@
+import{j as t}from"./iframe-C9wbfKto.js";import{useMDXComponents as e}from"./index-BVKh2AfG.js";import{M as i}from"./blocks-ZQqwzJ7p.js";import{A as s}from"./ArticlePage.stories-BZgbRyjR.js";import"./index-D6CT6hk-.js";import"./common-DrnyCu3t.js";import"./HomeNews-D69jJLbt.js";import"./config-iQLDaG8G.js";import"./PageFooter.stories-3mPGzWTB.js";/*@license CC0-1.0*/function r(n){const o={h1:"h1",p:"p",...e(),...n.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(i,{of:s}),`
+`,t.jsx(o.h1,{id:"article-page",children:"Article page"}),`
+`,t.jsx(o.p,{children:"This page type is for news or similar kinds of articles."})]})}function j(n={}){const{wrapper:o}={...e(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(r,{...n})}):r(n)}export{j as default};
---
+++ demo-DES-1351-heading-sizes/assets/FormPage.docs-C6cD78wK.js
@@ -0,0 +1,5 @@
+import{j as o}from"./iframe-C9wbfKto.js";import{useMDXComponents as r}from"./index-BVKh2AfG.js";import{M as s}from"./blocks-ZQqwzJ7p.js";import{F as m}from"./FormPage.stories-DpivIw1U.js";import"./index-D6CT6hk-.js";import"./config-iQLDaG8G.js";import"./PageFooter.stories-3mPGzWTB.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
+`,`
+`,o.jsx(s,{of:m}),`
+`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
+`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function d(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{d as default};
