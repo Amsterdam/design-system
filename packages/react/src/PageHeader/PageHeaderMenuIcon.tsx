fbe23f9e6fd2059e6a4167e8c77187ed6e1e9c8c
alimpens <a.limpens@amsterdam.nl>
2025-08-01 12:57:03 +0200

    Revert changes to SVGs

--- packages/react/src/PageHeader/PageHeaderMenuIcon.tsx
+++ packages/react/src/PageHeader/PageHeaderMenuIcon.tsx
@@ -6,14 +6,7 @@
 import type { SVGProps } from 'react'
 
 export const PageHeaderMenuIcon = (props: SVGProps<SVGSVGElement>) => (
-  <svg
-    aria-hidden="true"
-    focusable="false"
-    height="0"
-    viewBox="0 0 24 24"
-    xmlns="http://www.w3.org/2000/svg"
-    {...props}
-  >
+  <svg aria-hidden="true" focusable="false" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...props}>
     <line className="ams-page-header__menu-icon-top" x1="0" x2="100%" y1="50%" y2="50%" />
     <line className="ams-page-header__menu-icon-middle" x1="0" x2="100%" y1="50%" y2="50%" />
     <line className="ams-page-header__menu-icon-bottom" x1="0" x2="100%" y1="50%" y2="50%" />
