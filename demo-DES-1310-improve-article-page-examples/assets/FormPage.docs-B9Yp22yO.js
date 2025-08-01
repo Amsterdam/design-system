a12c6f7b432f56c42415264f7a1da1bfb38d20f3
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-08-01 13:15:49 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@654d5228ea52a17553ad98914c73736fe62d20b0 🚀

---
+++ demo-DES-1310-improve-article-page-examples/assets/FormPage.docs-B9Yp22yO.js
@@ -0,0 +1,5 @@
+import{j as o}from"./iframe-CUA88lU5.js";import{useMDXComponents as r}from"./index-Ch--ueVC.js";import{M as s}from"./blocks-BkwnH5kH.js";import{F as m}from"./FormPage.stories-BQZYeEYr.js";import"./index-DlQQ0dwT.js";import"./config-BSblaxRE.js";import"./PageFooter.stories-BXHbM8aT.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
+`,`
+`,o.jsx(s,{of:m}),`
+`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
+`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function d(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{d as default};
---
+++ demo-DES-1310-improve-article-page-examples/assets/NavigationPage.docs-DuZcHFL_.js
@@ -0,0 +1,6 @@
+import{j as t}from"./iframe-CUA88lU5.js";import{useMDXComponents as i}from"./index-Ch--ueVC.js";import{M as s}from"./blocks-BkwnH5kH.js";import{N as r}from"./NavigationPage.stories-Ct-pchQC.js";import"./index-DlQQ0dwT.js";import"./exampleContent-B9pMnolB.js";import"./config-BSblaxRE.js";import"./PageFooter.stories-BXHbM8aT.js";/*@license CC0-1.0*/function e(n){const o={h1:"h1",p:"p",...i(),...n.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(s,{of:r}),`
+`,t.jsx(o.h1,{id:"navigation-page",children:"Navigation page"}),`
+`,t.jsx(o.p,{children:`A navigation page does not have much content of its own.
+Instead, it presents groups of links to various pages related to a specific subject.`})]})}function x(n={}){const{wrapper:o}={...i(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(e,{...n})}):e(n)}export{x as default};
