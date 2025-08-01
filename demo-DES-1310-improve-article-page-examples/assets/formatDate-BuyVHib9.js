575b0434d35ab2530aad586ffee35c905220c6e8
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-08-01 10:30:06 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@7713e645dcc5193caedbab6f14f9dd8deb6e7261 🚀

---
+++ demo-DES-1310-improve-article-page-examples/assets/formatDate-BuyVHib9.js
@@ -0,0 +1,4 @@
+/**
+ * @license EUPL-1.2+
+ * Copyright Gemeente Amsterdam
+ */const t={day:"numeric",month:"long",year:"numeric"},n=new Intl.DateTimeFormat("nl",t),a=o=>n.format(o);export{a as f};
