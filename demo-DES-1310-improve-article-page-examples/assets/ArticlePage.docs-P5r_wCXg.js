667e9d4cd55a4955d2fb3cb0b576360714c4a9e9
GitHub <github-action@users.noreply.github.com>
2025-08-01 13:32:41 +0000

    Remove feature branch folder

--- demo-DES-1310-improve-article-page-examples/assets/ArticlePage.docs-P5r_wCXg.js
+++
@@ -1,5 +0,0 @@
-import{j as t}from"./iframe-_olaG_4U.js";import{useMDXComponents as e}from"./index-lW7CEHaP.js";import{M as i}from"./blocks-DG8eTlyU.js";import{A as s}from"./ArticlePage.stories-D2_wcDdj.js";import"./index-CyZqUWL_.js";import"./common-DrnyCu3t.js";import"./HomeNews-TI9bNftj.js";import"./config-Byj3uxlf.js";import"./PageFooter.stories-BM4ybM5I.js";/*@license CC0-1.0*/function r(n){const o={h1:"h1",p:"p",...e(),...n.components};return t.jsxs(t.Fragment,{children:[`
-`,`
-`,t.jsx(i,{of:s}),`
-`,t.jsx(o.h1,{id:"article-page",children:"Article page"}),`
-`,t.jsx(o.p,{children:"This page type is for news or similar kinds of articles."})]})}function j(n={}){const{wrapper:o}={...e(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(r,{...n})}):r(n)}export{j as default};
--- demo-DES-1310-improve-article-page-examples/assets/FormPage.docs-BW2PkQ4R.js
+++
@@ -1,5 +0,0 @@
-import{j as o}from"./iframe-_olaG_4U.js";import{useMDXComponents as r}from"./index-lW7CEHaP.js";import{M as s}from"./blocks-DG8eTlyU.js";import{F as m}from"./FormPage.stories-BViZjrxB.js";import"./index-CyZqUWL_.js";import"./config-Byj3uxlf.js";import"./PageFooter.stories-BM4ybM5I.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
-`,`
-`,o.jsx(s,{of:m}),`
-`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
-`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function d(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{d as default};
