ef4046cad18d9c6c1ebc2bf6c80d57d090609bb3
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-08-04 15:50:38 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@d2264aef2de9f61c2ec3d0e54cf7c8eecaad0464 🚀

---
+++ demo-DES-1396-text-size-error-message/assets/ArticlePage.docs-BaevLYl4.js
@@ -0,0 +1,5 @@
+import{j as t}from"./iframe-ChiMXUta.js";import{useMDXComponents as e}from"./index-DwS9B8vx.js";import{M as i}from"./blocks-79i35Yxp.js";import{A as s}from"./ArticlePage.stories-43Qw2pT8.js";import"./index-BzZhSD_5.js";import"./common-DrnyCu3t.js";import"./HomeNews-_Z0ECKB7.js";import"./config-DVHz64WB.js";import"./PageFooter.stories-P_V51q_L.js";/*@license CC0-1.0*/function r(n){const o={h1:"h1",p:"p",...e(),...n.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(i,{of:s}),`
+`,t.jsx(o.h1,{id:"article-page",children:"Article page"}),`
+`,t.jsx(o.p,{children:"This page type is for news or similar kinds of articles."})]})}function j(n={}){const{wrapper:o}={...e(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(r,{...n})}):r(n)}export{j as default};
