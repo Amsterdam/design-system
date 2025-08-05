221ec3c5d5942bc8b1e62a68a0ab85def9b230b9
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-08-05 12:11:38 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@7cf9f5360c8fe99d7bdbeaf37e624019d39556b8 🚀

---
+++ demo-navigation-page-with-image-gallery/assets/NavigationPage.docs-w4zNZwrN.js
@@ -0,0 +1,6 @@
+import{j as t}from"./iframe-C6h7txgd.js";import{useMDXComponents as i}from"./index-CiYBr0q2.js";import{M as s}from"./blocks-Cyhx7iH-.js";import{N as r}from"./NavigationPage.stories-D7c2PV-R.js";import"./index-BddrqZPh.js";import"./exampleContent-B9pMnolB.js";import"./config-Bt9EyUst.js";import"./PageFooter.stories-dDg5eZac.js";/*@license CC0-1.0*/function e(n){const o={h1:"h1",p:"p",...i(),...n.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(s,{of:r}),`
+`,t.jsx(o.h1,{id:"navigation-page",children:"Navigation page"}),`
+`,t.jsx(o.p,{children:`A navigation page does not have much content of its own.
+Instead, it presents groups of links to various pages related to a specific subject.`})]})}function x(n={}){const{wrapper:o}={...i(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(e,{...n})}):e(n)}export{x as default};
