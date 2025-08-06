5c5315c3b5abc19305d15e8ab6d768af11a064c7
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-08-06 08:15:48 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@b7ff7c12197d7332a2479484acdcbb2798803f38 🚀

--- demo-DES-1393-buttons-and-inputs-line-height/assets/ArticlePage.docs-CJVGnffL.js
+++
@@ -1,5 +0,0 @@
-import{j as t}from"./iframe-XliGWV11.js";import{useMDXComponents as e}from"./index--VykTRLj.js";import{M as i}from"./blocks-_xwKvlUz.js";import{A as s}from"./ArticlePage.stories-CPkLxhIN.js";import"./index-C7rLp2Xz.js";import"./common-DrnyCu3t.js";import"./HomeNews-CebqY62B.js";import"./config-bua6GBqs.js";import"./PageFooter.stories-GCd89gAE.js";/*@license CC0-1.0*/function r(n){const o={h1:"h1",p:"p",...e(),...n.components};return t.jsxs(t.Fragment,{children:[`
-`,`
-`,t.jsx(i,{of:s}),`
-`,t.jsx(o.h1,{id:"article-page",children:"Article page"}),`
-`,t.jsx(o.p,{children:"This page type is for news or similar kinds of articles."})]})}function j(n={}){const{wrapper:o}={...e(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(r,{...n})}):r(n)}export{j as default};
---
+++ demo-DES-1393-buttons-and-inputs-line-height/assets/FormPage.docs-Ax3YiH23.js
@@ -0,0 +1,5 @@
+import{j as o}from"./iframe-9EmNbxf3.js";import{useMDXComponents as r}from"./index-_ZTiYeco.js";import{M as s}from"./blocks-0wDt4Gvz.js";import{F as m}from"./FormPage.stories-vWumIvXE.js";import"./index-hULDQWh8.js";import"./config-DbExUrgh.js";import"./PageFooter.stories-DzRNvblc.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
+`,`
+`,o.jsx(s,{of:m}),`
+`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
+`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function d(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{d as default};
