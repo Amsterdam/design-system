4464384a276f6872df6efb46302b62be7063d56b
GitHub <github-action@users.noreply.github.com>
2025-08-06 09:01:36 +0000

    Remove feature branch folder

--- demo-DES-1396-text-size-error-message/assets/ProductPage.docs-CoI-c87o.js
+++
@@ -1,6 +0,0 @@
-import{j as t}from"./iframe-CCMocYvc.js";import{useMDXComponents as e}from"./index-B5e3jfbJ.js";import{M as i}from"./blocks-DzG8BP53.js";import{P as s}from"./ProductPage.stories-BadWeB1G.js";import"./index-l9vD08W5.js";import"./config-DozWeTjH.js";import"./PageFooter.stories-Dc4O0GLj.js";/*@license CC0-1.0*/function r(o){const n={h1:"h1",p:"p",...e(),...o.components};return t.jsxs(t.Fragment,{children:[`
-`,`
-`,t.jsx(i,{of:s}),`
-`,t.jsx(n.h1,{id:"product-page",children:"Product page"}),`
-`,t.jsx(n.p,{children:`Lists all information about a product or service of the City.
-Often includes a Call to Action Link that directs the user to a form.`})]})}function h(o={}){const{wrapper:n}={...e(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(r,{...o})}):r(o)}export{h as default};
