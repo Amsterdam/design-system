b57e64867c132ba9177267c539dbd3ee5772573c
alimpens <37216945+alimpens@users.noreply.github.com>
2025-08-06 09:03:13 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@c5045a068980a1e4614ba5bc627b05e1fa4e7813 🚀

---
+++ demo-develop/assets/ArticlePage.docs-CG9y6FF_.js
@@ -0,0 +1,5 @@
+import{j as t}from"./iframe-BmyvJtm_.js";import{useMDXComponents as e}from"./index-Dm_i3UEn.js";import{M as i}from"./blocks-BmtkGqyW.js";import{A as s}from"./ArticlePage.stories-7g-Ivwbe.js";import"./index-Cffte31o.js";import"./common-DrnyCu3t.js";import"./HomeNews-BqkYE2Dz.js";import"./config-fWD7bVbz.js";import"./PageFooter.stories-CUYTs_PL.js";/*@license CC0-1.0*/function r(n){const o={h1:"h1",p:"p",...e(),...n.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(i,{of:s}),`
+`,t.jsx(o.h1,{id:"article-page",children:"Article page"}),`
+`,t.jsx(o.p,{children:"This page type is for news or similar kinds of articles."})]})}function j(n={}){const{wrapper:o}={...e(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(r,{...n})}):r(n)}export{j as default};
---
+++ demo-develop/assets/NavigationPage.docs-BcmeU886.js
@@ -0,0 +1,6 @@
+import{j as t}from"./iframe-BmyvJtm_.js";import{useMDXComponents as i}from"./index-Dm_i3UEn.js";import{M as s}from"./blocks-BmtkGqyW.js";import{N as r}from"./NavigationPage.stories-D5Uy5BUP.js";import"./index-Cffte31o.js";import"./exampleContent-B9pMnolB.js";import"./config-fWD7bVbz.js";import"./PageFooter.stories-CUYTs_PL.js";/*@license CC0-1.0*/function e(n){const o={h1:"h1",p:"p",...i(),...n.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(s,{of:r}),`
+`,t.jsx(o.h1,{id:"navigation-page",children:"Navigation page"}),`
+`,t.jsx(o.p,{children:`A navigation page does not have much content of its own.
+Instead, it presents groups of links to various pages related to a specific subject.`})]})}function x(n={}){const{wrapper:o}={...i(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(e,{...n})}):e(n)}export{x as default};
