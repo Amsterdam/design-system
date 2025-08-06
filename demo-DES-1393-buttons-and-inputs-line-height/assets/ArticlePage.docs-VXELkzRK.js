501549d5f7389501451d132fd10f9dddde3c200f
GitHub <github-action@users.noreply.github.com>
2025-08-06 08:25:38 +0000

    Remove feature branch folder

--- demo-DES-1393-buttons-and-inputs-line-height/assets/ArticlePage.docs-VXELkzRK.js
+++
@@ -1,5 +0,0 @@
-import{j as t}from"./iframe-9EmNbxf3.js";import{useMDXComponents as e}from"./index-_ZTiYeco.js";import{M as i}from"./blocks-0wDt4Gvz.js";import{A as s}from"./ArticlePage.stories-C5MgOzei.js";import"./index-hULDQWh8.js";import"./common-DrnyCu3t.js";import"./HomeNews-cb73k0nP.js";import"./config-DbExUrgh.js";import"./PageFooter.stories-DzRNvblc.js";/*@license CC0-1.0*/function r(n){const o={h1:"h1",p:"p",...e(),...n.components};return t.jsxs(t.Fragment,{children:[`
-`,`
-`,t.jsx(i,{of:s}),`
-`,t.jsx(o.h1,{id:"article-page",children:"Article page"}),`
-`,t.jsx(o.p,{children:"This page type is for news or similar kinds of articles."})]})}function j(n={}){const{wrapper:o}={...e(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(r,{...n})}):r(n)}export{j as default};
