0c0b5f7877aa01b80b21482aa29ed2e117c94974
alimpens <37216945+alimpens@users.noreply.github.com>
2025-08-06 08:27:18 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@18d9a7d5010a54a6e08893dfbf72f1e7b9434e69 🚀

---
+++ demo-develop/assets/Blockquote.docs-DWqLSHgF.js
@@ -0,0 +1,27 @@
+import{j as n}from"./iframe-9EmNbxf3.js";import{useMDXComponents as s}from"./index-_ZTiYeco.js";import{M as r,c as a,P as i,e as h,f as c}from"./blocks-0wDt4Gvz.js";import{B as u,I as l}from"./Blockquote.stories-DLKanFXS.js";import"./index-hULDQWh8.js";import"./exampleContent-B9pMnolB.js";const m=`<!-- @license CC0-1.0 -->
+
+# Blockquote
+
+We use blockquotes to emphasize citations.
+A quote appears differently from the main text to make it stand out.
+
+## Guidelines
+
+- Quote someone’s literal written or oral text.
+- A quote has a width of half or a whole column of text.
+- The component adds the correct quotation marks.
+
+## Relevant WCAG requirements
+
+- [WCAG 3.1.2](https://www.w3.org/WAI/WCAG21/Understanding/language-of-parts.html): if the citation is in a language other than that of the surrounding content, indicate so through the \`lang\` attribute.
+`;/*@license CC0-1.0*/function o(t){const e={h2:"h2",h3:"h3",p:"p",...s(),...t.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(r,{of:u}),`
+`,n.jsx(a,{children:m}),`
+`,n.jsx(i,{}),`
+`,n.jsx(h,{}),`
+`,n.jsx(e.h2,{id:"examples",children:"Examples"}),`
+`,n.jsx(e.h3,{id:"inverse-colour",children:"Inverse colour"}),`
+`,n.jsx(e.p,{children:`Use the inverse colour if the Blockquote sits on a dark background.
+This ensures the colour of the text provides enough contrast.`}),`
+`,n.jsx(c,{of:l})]})}function C(t={}){const{wrapper:e}={...s(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(o,{...t})}):o(t)}export{C as default};
--- demo-develop/assets/ProductPage.docs-DvfP5PeS.js
+++
@@ -1,6 +0,0 @@
-import{j as t}from"./iframe-CtsmQdSA.js";import{useMDXComponents as e}from"./index-BVqBOYma.js";import{M as i}from"./blocks-BcAWfOAR.js";import{P as s}from"./ProductPage.stories-BaBTA8R3.js";import"./index-DqR6fIV7.js";import"./config-CNMo36LC.js";import"./PageFooter.stories-BJ57uhHN.js";/*@license CC0-1.0*/function r(o){const n={h1:"h1",p:"p",...e(),...o.components};return t.jsxs(t.Fragment,{children:[`
-`,`
-`,t.jsx(i,{of:s}),`
-`,t.jsx(n.h1,{id:"product-page",children:"Product page"}),`
-`,t.jsx(n.p,{children:`Lists all information about a product or service of the City.
-Often includes a Call to Action Link that directs the user to a form.`})]})}function h(o={}){const{wrapper:n}={...e(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(r,{...o})}):r(o)}export{h as default};
