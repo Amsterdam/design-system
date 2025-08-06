4ebd8da5b4539046d666c979ee445253789be563
Vincent Smedinga <v.smedinga@amsterdam.nl>
2025-08-06 11:10:39 +0200

    chore: Use regular text size for field (set) descriptions (#2162)
    Co-authored-by: Aram <37216945+alimpens@users.noreply.github.com>

--- storybook/src/components/TextInput/TextInput.stories.tsx
+++ storybook/src/components/TextInput/TextInput.stories.tsx
@@ -107,9 +107,7 @@ export const InAField: Story = {
   render: (args) => (
     <Field invalid={args.invalid}>
       <Label htmlFor="input1">Label</Label>
-      <Paragraph id="description1" size="small">
-        Omschrijving.
-      </Paragraph>
+      <Paragraph id="description1">Omschrijving.</Paragraph>
       {args.invalid && <ErrorMessage id="error1">Foutmelding.</ErrorMessage>}
       <TextInput aria-describedby={`description1${args.invalid ? ' error1' : ''}`} id="input1" {...args} />
     </Field>
@@ -123,9 +121,7 @@ export const InAFieldWithValidation: Story = {
   render: (args) => (
     <Field invalid={args.invalid}>
       <Label htmlFor="input2">Label</Label>
-      <Paragraph id="description2" size="small">
-        Omschrijving.
-      </Paragraph>
+      <Paragraph id="description2">Omschrijving.</Paragraph>
       {args.invalid && <ErrorMessage id="error2">Foutmelding.</ErrorMessage>}
       <TextInput aria-describedby={`description2${args.invalid ? ' error2' : ''}`} id="input2" {...args} />
     </Field>
