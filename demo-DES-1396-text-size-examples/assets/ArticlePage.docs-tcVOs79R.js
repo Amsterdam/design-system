685a9d4a95900448e5e91242eef14cc56333f681
GitHub <github-action@users.noreply.github.com>
2025-08-06 09:11:00 +0000

    Remove feature branch folder

--- demo-DES-1396-text-size-examples/assets/ArticlePage.docs-tcVOs79R.js
+++
@@ -1,5 +0,0 @@
-import{j as t}from"./iframe-D847AKYq.js";import{useMDXComponents as e}from"./index-jVWsKXxZ.js";import{M as i}from"./blocks-BnQR2q1Y.js";import{A as s}from"./ArticlePage.stories-BXQBe0kR.js";import"./index-CJGzbZle.js";import"./common-DrnyCu3t.js";import"./HomeNews-Cvep3aMk.js";import"./config-U86GNLVK.js";import"./PageFooter.stories-DUc8UHbb.js";/*@license CC0-1.0*/function r(n){const o={h1:"h1",p:"p",...e(),...n.components};return t.jsxs(t.Fragment,{children:[`
-`,`
-`,t.jsx(i,{of:s}),`
-`,t.jsx(o.h1,{id:"article-page",children:"Article page"}),`
-`,t.jsx(o.p,{children:"This page type is for news or similar kinds of articles."})]})}function j(n={}){const{wrapper:o}={...e(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(r,{...n})}):r(n)}export{j as default};
