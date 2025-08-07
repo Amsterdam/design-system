f2ce30513ca3b536a9d5bfc0a265b3e7c518dcd5
Vincent Smedinga <v.smedinga@amsterdam.nl>
2025-08-07 14:43:43 +0200

    Add component to display font design tokens

---
+++ storybook/src/utils/design-tokens/FontDesignToken.tsx
@@ -0,0 +1,43 @@
+/**
+ * @license EUPL-1.2+
+ * Copyright Gemeente Amsterdam
+ */
+
+import clsx from 'clsx'
+import './font-design-token.css'
+
+type FontDesignTokenProps = {
+  /** The HTML tag to use. */
+  as?: 'p' | 'h1' | 'h2' | 'h3' | 'h4'
+  /** Whether to use the tokens for Compact Mode. */
+  compact?: boolean
+  /**
+   * The font size to use in the example.
+   * In pixels for easy reference; will be converted to rems.
+   * No fluidity here: we want to display the given size on any viewport.
+   */
+  fontSize: number
+  /* The font weight to use in the example. Provide the distinctive part of its design token’s name. */
+  fontWeight?: string
+  /* The line height to use in the example. Provide the distinctive part of its design token’s name. */
+  lineHeight: string
+}
+
+export const FontDesignToken = ({
+  as: Tag = 'p',
+  compact,
+  fontSize,
+  fontWeight = 'body-text',
+  lineHeight,
+}: FontDesignTokenProps) => (
+  <Tag
+    className={clsx('ams-docs-font-design-token', compact && 'ams-theme--compact', 'sb-unstyled')}
+    style={{
+      fontSize: `${fontSize / 16}rem`,
+      fontWeight: `var(--ams-typography-${fontWeight}-font-weight)`,
+      lineHeight: `var(--ams-typography-${lineHeight}-line-height)`,
+    }}
+  >
+    Abc
+  </Tag>
+)
