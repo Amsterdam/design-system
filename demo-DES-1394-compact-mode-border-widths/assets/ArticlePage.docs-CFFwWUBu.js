077e9def3a85428c7ca89358ad2b650c484857d6
GitHub <github-action@users.noreply.github.com>
2025-08-08 13:03:40 +0000

    Remove feature branch folder

--- demo-DES-1394-compact-mode-border-widths/assets/ArticlePage.docs-CFFwWUBu.js
+++
@@ -1,5 +0,0 @@
-import{j as t}from"./iframe-CdQdN5Uu.js";import{useMDXComponents as e}from"./index-C2GBGR0m.js";import{M as i}from"./blocks-BhPjcMoE.js";import{A as s}from"./ArticlePage.stories-Dv5PG_7i.js";import"./index-aCSGuycC.js";import"./common-DrnyCu3t.js";import"./HomeNews-BFr5lFbT.js";import"./config-C79D8JCw.js";import"./PageFooter.stories-6p9S-x9G.js";/*@license CC0-1.0*/function r(n){const o={h1:"h1",p:"p",...e(),...n.components};return t.jsxs(t.Fragment,{children:[`
-`,`
-`,t.jsx(i,{of:s}),`
-`,t.jsx(o.h1,{id:"article-page",children:"Article page"}),`
-`,t.jsx(o.p,{children:"This page type is for news or similar kinds of articles."})]})}function j(n={}){const{wrapper:o}={...e(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(r,{...n})}):r(n)}export{j as default};
