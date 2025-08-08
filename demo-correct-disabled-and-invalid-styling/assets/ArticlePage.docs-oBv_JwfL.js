d7f5e25f362c79170f2753778093abcb1f9eec8c
GitHub <github-action@users.noreply.github.com>
2025-08-08 08:46:33 +0000

    Remove feature branch folder

--- demo-correct-disabled-and-invalid-styling/assets/ArticlePage.docs-oBv_JwfL.js
+++
@@ -1,5 +0,0 @@
-import{j as t}from"./iframe-CL47HroO.js";import{useMDXComponents as e}from"./index-CPG_RZyY.js";import{M as i}from"./blocks-CnrCRolk.js";import{A as s}from"./ArticlePage.stories-DlryRtEj.js";import"./index-4gy-vPua.js";import"./common-DrnyCu3t.js";import"./HomeNews-Cogv1eQC.js";import"./config-D1RxPg2s.js";import"./PageFooter.stories-XsCxAJ0o.js";/*@license CC0-1.0*/function r(n){const o={h1:"h1",p:"p",...e(),...n.components};return t.jsxs(t.Fragment,{children:[`
-`,`
-`,t.jsx(i,{of:s}),`
-`,t.jsx(o.h1,{id:"article-page",children:"Article page"}),`
-`,t.jsx(o.p,{children:"This page type is for news or similar kinds of articles."})]})}function j(n={}){const{wrapper:o}={...e(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(r,{...n})}):r(n)}export{j as default};
--- demo-correct-disabled-and-invalid-styling/assets/NavigationPage.docs-BPYncboS.js
+++
@@ -1,6 +0,0 @@
-import{j as t}from"./iframe-CL47HroO.js";import{useMDXComponents as i}from"./index-CPG_RZyY.js";import{M as s}from"./blocks-CnrCRolk.js";import{N as r}from"./NavigationPage.stories-BRMEcrkE.js";import"./index-4gy-vPua.js";import"./exampleContent-B7cDFRSs.js";import"./config-D1RxPg2s.js";import"./PageFooter.stories-XsCxAJ0o.js";/*@license CC0-1.0*/function e(n){const o={h1:"h1",p:"p",...i(),...n.components};return t.jsxs(t.Fragment,{children:[`
-`,`
-`,t.jsx(s,{of:r}),`
-`,t.jsx(o.h1,{id:"navigation-page",children:"Navigation page"}),`
-`,t.jsx(o.p,{children:`A navigation page does not have much content of its own.
-Instead, it presents groups of links to various pages related to a specific subject.`})]})}function x(n={}){const{wrapper:o}={...i(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(e,{...n})}):e(n)}export{x as default};
