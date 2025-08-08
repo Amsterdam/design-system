f0cc9486a1b6298c7eca4ae8f6524edd9475f17d
alimpens <37216945+alimpens@users.noreply.github.com>
2025-08-08 12:35:05 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@82184bfc4b317cb2f219ba5975250b23c7dc8fa9 🚀

---
+++ demo-develop/assets/FileList.docs-W0hvhIbl.js
@@ -0,0 +1,60 @@
+import{j as e}from"./iframe-CaYTSbAF.js";import{useMDXComponents as s}from"./index-Bl4ohTOn.js";import{M as l,c as r,P as o,f as a,S as f}from"./blocks-DCJSRmiD.js";import{F as c,W as m}from"./FileList.stories-DobL56A-.js";import"./index-BbSPVmjL.js";import"./Icon-DvlFHr4a.js";import"./Button-CYnTg5Iu.js";const u=`/**
+ * @license EUPL-1.2+
+ * Copyright Gemeente Amsterdam
+ */
+
+import { FileInput, FileList } from '@amsterdam/design-system-react'
+import { useRef, useState } from 'react'
+
+export const FileInputWithFileList = () => {
+  const inputRef = useRef<HTMLInputElement>(null)
+  const [files, setFiles] = useState<FileList | null>(null)
+
+  const changeFiles = () => {
+    if (inputRef.current) {
+      setFiles(inputRef.current.files)
+    }
+  }
+
+  const removeFile = (index: number) => {
+    if (files) {
+      const newFiles = new DataTransfer()
+      Array.from(files).forEach((file, i) => {
+        if (i !== index) newFiles.items.add(file)
+      })
+      if (inputRef.current) {
+        inputRef.current.files = newFiles.files
+      }
+      setFiles(newFiles.files)
+    }
+  }
+
+  return (
+    <>
+      <FileInput multiple onChange={changeFiles} ref={inputRef} />
+      {files && (
+        <FileList>
+          {Array.from(files).map((file, index) => (
+            <FileList.Item file={file} key={file.name} onDelete={() => removeFile(index)} />
+          ))}
+        </FileList>
+      )}
+    </>
+  )
+}
+`,p=`<!-- @license CC0-1.0 -->
+
+# File List
+
+An overview of files, showing their name, type, size, and a preview.
+`;/*@license CC0-1.0*/function t(i){const n={code:"code",h2:"h2",h3:"h3",p:"p",...s(),...i.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(l,{of:c}),`
+`,e.jsx(r,{children:p}),`
+`,e.jsx(o,{}),`
+`,e.jsx(n.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(n.h3,{id:"using-a-file-input",children:"Using a file input"}),`
+`,e.jsxs(n.p,{children:["To connect a File Input to a File List, use the ",e.jsx(n.code,{children:"onChange"}),` event to update the
+list of files and use `,e.jsx(n.code,{children:"onDelete"})," when removing a file from the list."]}),`
+`,e.jsx(a,{of:m}),`
+`,e.jsx(f,{code:u,language:"jsx"})]})}function w(i={}){const{wrapper:n}={...s(),...i.components};return n?e.jsx(n,{...i,children:e.jsx(t,{...i})}):t(i)}export{w as default};
--- demo-develop/assets/FormPage.docs-DaJTDDE4.js
+++
@@ -1,5 +0,0 @@
-import{j as o}from"./iframe-C3xmyKUp.js";import{useMDXComponents as r}from"./index-Bge7qgV-.js";import{M as s}from"./blocks-BbvIrOGO.js";import{F as m}from"./FormPage.stories-BETyeD4V.js";import"./index-Cr4wb2M3.js";import"./config-D0TjWu3v.js";import"./PageFooter.stories-CqHz-XQ_.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
-`,`
-`,o.jsx(s,{of:m}),`
-`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
-`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function d(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{d as default};
