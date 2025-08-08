b640455c86b517d57ea1bdcabe1875f5c7554cda
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-08-08 12:09:26 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@d048ee872d214271fb5eac93f6cf44f8b564c932 🚀

--- demo-DES-1351-heading-sizes/assets/ArticlePage.docs-C-EDehUZ.js
+++
@@ -1,5 +0,0 @@
-import{j as t}from"./iframe-CQnP-6l6.js";import{useMDXComponents as e}from"./index-BDbfYb2T.js";import{M as i}from"./blocks-BBGA27RZ.js";import{A as s}from"./ArticlePage.stories-DzT0PA_C.js";import"./index-Bbm1wl5k.js";import"./common-DrnyCu3t.js";import"./HomeNews-nvVQqUb6.js";import"./config-4osjDg_i.js";import"./PageFooter.stories-CrzC0roU.js";/*@license CC0-1.0*/function r(n){const o={h1:"h1",p:"p",...e(),...n.components};return t.jsxs(t.Fragment,{children:[`
-`,`
-`,t.jsx(i,{of:s}),`
-`,t.jsx(o.h1,{id:"article-page",children:"Article page"}),`
-`,t.jsx(o.p,{children:"This page type is for news or similar kinds of articles."})]})}function j(n={}){const{wrapper:o}={...e(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(r,{...n})}):r(n)}export{j as default};
---
+++ demo-DES-1351-heading-sizes/assets/ProductPage.docs-Xk0_tyTs.js
@@ -0,0 +1,6 @@
+import{j as t}from"./iframe-DB9uHCAC.js";import{useMDXComponents as e}from"./index-xGbGnN7w.js";import{M as i}from"./blocks-5axxWPS7.js";import{P as s}from"./ProductPage.stories-Htp_QPdB.js";import"./index-C7KG5poE.js";import"./config-CUCLXczJ.js";import"./PageFooter.stories-BLlmNSCo.js";/*@license CC0-1.0*/function r(o){const n={h1:"h1",p:"p",...e(),...o.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(i,{of:s}),`
+`,t.jsx(n.h1,{id:"product-page",children:"Product page"}),`
+`,t.jsx(n.p,{children:`Lists all information about a product or service of the City.
+Often includes a Call to Action Link that directs the user to a form.`})]})}function h(o={}){const{wrapper:n}={...e(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(r,{...o})}):r(o)}export{h as default};
