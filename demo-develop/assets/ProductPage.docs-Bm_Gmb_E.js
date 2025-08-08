7895a01632649f725ebe368c10c935306e3ca5b7
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-08-08 08:48:21 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@af427237eb54cf805eead4925b2e98a49d21c652 🚀

---
+++ demo-develop/assets/ProductPage.docs-Bm_Gmb_E.js
@@ -0,0 +1,6 @@
+import{j as t}from"./iframe-ChVvhDGS.js";import{useMDXComponents as e}from"./index-BweGiijk.js";import{M as i}from"./blocks-BkWWsR-M.js";import{P as s}from"./ProductPage.stories-C_6lPCyR.js";import"./index-LdWmbL-M.js";import"./config-2ofPISsE.js";import"./PageFooter.stories-BvbfBqLH.js";/*@license CC0-1.0*/function r(o){const n={h1:"h1",p:"p",...e(),...o.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(i,{of:s}),`
+`,t.jsx(n.h1,{id:"product-page",children:"Product page"}),`
+`,t.jsx(n.p,{children:`Lists all information about a product or service of the City.
+Often includes a Call to Action Link that directs the user to a form.`})]})}function h(o={}){const{wrapper:n}={...e(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(r,{...o})}):r(o)}export{h as default};
