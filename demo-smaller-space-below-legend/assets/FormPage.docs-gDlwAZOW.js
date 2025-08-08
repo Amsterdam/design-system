867a9baa3c8bb60b928b859fba99a6d227bbd134
GitHub <github-action@users.noreply.github.com>
2025-08-08 15:29:41 +0000

    Remove feature branch folder

--- demo-smaller-space-below-legend/assets/FormPage.docs-gDlwAZOW.js
+++
@@ -1,5 +0,0 @@
-import{j as o}from"./iframe-DACOrW76.js";import{useMDXComponents as r}from"./index-l9o6YxBR.js";import{M as s}from"./blocks-DmgDLJJ0.js";import{F as m}from"./FormPage.stories-DjqudUpr.js";import"./index-CbvOz38e.js";import"./config-DFxx-s2h.js";import"./PageFooter.stories-BnD7iyoP.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
-`,`
-`,o.jsx(s,{of:m}),`
-`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
-`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function d(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{d as default};
