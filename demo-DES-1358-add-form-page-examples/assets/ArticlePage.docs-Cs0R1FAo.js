b1b62c5a46af6c8aedea9a3f18fb91baeadbd942
alimpens <37216945+alimpens@users.noreply.github.com>
2025-08-08 15:37:43 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@283cf6087c5b6b5cd2be12b964744954b395686b 🚀

---
+++ demo-DES-1358-add-form-page-examples/assets/ArticlePage.docs-Cs0R1FAo.js
@@ -0,0 +1,5 @@
+import{j as t}from"./iframe-CaxThz9I.js";import{useMDXComponents as e}from"./index-DtbYCDbV.js";import{M as i}from"./blocks-wgWQAOyw.js";import{A as s}from"./ArticlePage.stories-BpZPf8tl.js";import"./index-Bcko-exY.js";import"./common-DrnyCu3t.js";import"./HomeNews-BPZ2axTI.js";import"./config-jjueYb7A.js";import"./Layout-B8Z_ATvh.js";import"./PageFooter.stories-BuEQgVaD.js";/*@license CC0-1.0*/function n(o){const r={h1:"h1",p:"p",...e(),...o.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(i,{of:s}),`
+`,t.jsx(r.h1,{id:"article-page",children:"Article page"}),`
+`,t.jsx(r.p,{children:"This page type is for news or similar kinds of articles."})]})}function u(o={}){const{wrapper:r}={...e(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(n,{...o})}):n(o)}export{u as default};
--- demo-DES-1358-add-form-page-examples/assets/config-cCGCcg4h.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./iframe-w_afJdZc.js";import{L as r}from"./Layout-DZMko2v0.js";const a={decorators:[o=>e.jsx(r,{children:e.jsx(o,{})})],parameters:{layout:"fullscreen",themes:{themeOverride:"Spacious"}}};export{a as c};
