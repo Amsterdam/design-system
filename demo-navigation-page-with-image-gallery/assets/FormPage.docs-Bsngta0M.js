87865c98a153f6dde9c14e37bc2834c67d5f4a2b
GitHub <github-action@users.noreply.github.com>
2025-08-08 15:26:13 +0000

    Remove feature branch folder

--- demo-navigation-page-with-image-gallery/assets/FormPage.docs-Bsngta0M.js
+++
@@ -1,5 +0,0 @@
-import{j as o}from"./iframe-BuCM2YO8.js";import{useMDXComponents as r}from"./index-BqsvkB3H.js";import{M as s}from"./blocks-CKuUIMaW.js";import{F as m}from"./FormPage.stories-C6-n7d5W.js";import"./index-xAWPjtI-.js";import"./config-DRhpUlZw.js";import"./PageFooter.stories-CfnOcUiv.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
-`,`
-`,o.jsx(s,{of:m}),`
-`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
-`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function d(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{d as default};
--- demo-navigation-page-with-image-gallery/assets/NavigationPage.docs-D1GUO22v.js
+++
@@ -1,6 +0,0 @@
-import{j as t}from"./iframe-BuCM2YO8.js";import{useMDXComponents as i}from"./index-BqsvkB3H.js";import{M as s}from"./blocks-CKuUIMaW.js";import{N as r}from"./NavigationPage.stories-CNbDzSBg.js";import"./index-xAWPjtI-.js";import"./exampleContent-B7cDFRSs.js";import"./config-DRhpUlZw.js";import"./PageFooter.stories-CfnOcUiv.js";/*@license CC0-1.0*/function e(n){const o={h1:"h1",p:"p",...i(),...n.components};return t.jsxs(t.Fragment,{children:[`
-`,`
-`,t.jsx(s,{of:r}),`
-`,t.jsx(o.h1,{id:"navigation-page",children:"Navigation page"}),`
-`,t.jsx(o.p,{children:`A navigation page does not have much content of its own.
-Instead, it presents groups of links to various pages related to a specific subject.`})]})}function x(n={}){const{wrapper:o}={...i(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(e,{...n})}):e(n)}export{x as default};
