a03ebeb4f34f27c2631c049a4696608f3eb7f25e
GitHub <github-action@users.noreply.github.com>
2025-08-08 10:21:19 +0000

    Remove feature branch folder

--- demo-DES-1396-text-size-skip-link/assets/FormPage.docs-BCYVMMDA.js
+++
@@ -1,5 +0,0 @@
-import{j as o}from"./iframe-BJ_Ctx3d.js";import{useMDXComponents as r}from"./index-1Go8Ip3V.js";import{M as s}from"./blocks-BpmbR3D5.js";import{F as m}from"./FormPage.stories-DVR60BFl.js";import"./index-CO-ww0x-.js";import"./config-RzvuKIUB.js";import"./PageFooter.stories-BVY_1rKp.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
-`,`
-`,o.jsx(s,{of:m}),`
-`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
-`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function d(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{d as default};
--- demo-DES-1396-text-size-skip-link/assets/ProductPage.docs-CQs7jpvp.js
+++
@@ -1,6 +0,0 @@
-import{j as t}from"./iframe-BJ_Ctx3d.js";import{useMDXComponents as e}from"./index-1Go8Ip3V.js";import{M as i}from"./blocks-BpmbR3D5.js";import{P as s}from"./ProductPage.stories-DHScyWaM.js";import"./index-CO-ww0x-.js";import"./config-RzvuKIUB.js";import"./PageFooter.stories-BVY_1rKp.js";/*@license CC0-1.0*/function r(o){const n={h1:"h1",p:"p",...e(),...o.components};return t.jsxs(t.Fragment,{children:[`
-`,`
-`,t.jsx(i,{of:s}),`
-`,t.jsx(n.h1,{id:"product-page",children:"Product page"}),`
-`,t.jsx(n.p,{children:`Lists all information about a product or service of the City.
-Often includes a Call to Action Link that directs the user to a form.`})]})}function h(o={}){const{wrapper:n}={...e(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(r,{...o})}):r(o)}export{h as default};
