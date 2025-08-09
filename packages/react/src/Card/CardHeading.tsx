b49c3b706211c5d75a370c4560895808bf0eff47
Vincent Smedinga <v.smedinga@amsterdam.nl>
2025-08-09 08:21:32 +0200

    Change default Card Heading size to level 3

--- packages/react/src/Card/CardHeading.tsx
+++ packages/react/src/Card/CardHeading.tsx
@@ -10,7 +10,7 @@ import { Heading } from '../Heading'
 import type { HeadingProps } from '../Heading'
 
 export const CardHeading = forwardRef(
-  ({ children, className, size = 'level-4', ...restProps }: HeadingProps, ref: ForwardedRef<HTMLHeadingElement>) => (
+  ({ children, className, size = 'level-3', ...restProps }: HeadingProps, ref: ForwardedRef<HTMLHeadingElement>) => (
     <Heading {...restProps} className={clsx('ams-card__heading', className)} ref={ref} size={size}>
       {children}
     </Heading>
