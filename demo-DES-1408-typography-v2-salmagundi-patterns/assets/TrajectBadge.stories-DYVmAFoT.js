c26a1f465d55bb341b8b32d83509cfcd7392895e
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-08-10 21:00:17 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@171d15c7cf13591aaf3ab97298ec58e6503191ce 🚀

---
+++ demo-DES-1408-typography-v2-salmagundi-patterns/assets/TrajectBadge.stories-DYVmAFoT.js
@@ -0,0 +1,10 @@
+import{T as r}from"./TrajectBadge-B9HAgVs_.js";import"./iframe-CODresTW.js";const o={title:"Components/TrajectBadge",component:r,tags:["autodocs"]},e={args:{label:"Traject 1"}},a={args:{active:!0,label:"Traject 1"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Traject 1'
+  }
+}`,...e.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    active: true,
+    label: 'Traject 1'
+  }
+}`,...a.parameters?.docs?.source}}};const c=["Default","Active"];export{a as Active,e as Default,c as __namedExportsOrder,o as default};
---
+++ demo-DES-1408-typography-v2-salmagundi-patterns/assets/styles.module-DNCcQUJ9.js
@@ -0,0 +1,1 @@
+const a="_cards_lzh9p_3",r="_card_lzh9p_3",s="_relative_lzh9p_16",_="_nowrap_lzh9p_20",c={cards:a,card:r,relative:s,nowrap:_};export{c as s};
