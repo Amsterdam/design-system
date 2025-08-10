f1f7d008a5543417acfa0aac303910a842113d0a
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-08-10 20:29:12 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@b532b1de1f3a6c1d835ea18c692b9269e26cac32 🚀

---
+++ demo-DES-1408-typography-v2/assets/FormPage.docs-DGpWIhKy.js
@@ -0,0 +1,5 @@
+import{j as o}from"./iframe-DL29Hifb.js";import{useMDXComponents as r}from"./index-Ck9CJtY5.js";import{M as s}from"./blocks-DVrjR7D6.js";import{F as m}from"./FormPage.stories-VPMYHrQa.js";import"./index-Dwi8mGvo.js";import"./config-BgQZpzBt.js";import"./PageFooter.stories-rdJTFSnP.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
+`,`
+`,o.jsx(s,{of:m}),`
+`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
+`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function d(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{d as default};
---
+++ demo-DES-1408-typography-v2/assets/NavigationPage.docs-BC-CNimd.js
@@ -0,0 +1,16 @@
+import{j as e}from"./iframe-DL29Hifb.js";import{useMDXComponents as a}from"./index-Ck9CJtY5.js";import{M as s}from"./blocks-DVrjR7D6.js";import{N as o}from"./NavigationPage.stories-BslG8amV.js";import"./index-Dwi8mGvo.js";import"./exampleContent-B7cDFRSs.js";import"./config-BgQZpzBt.js";import"./PageFooter.stories-rdJTFSnP.js";/*@license CC0-1.0*/function t(i){const n={h1:"h1",h2:"h2",li:"li",p:"p",ul:"ul",...a(),...i.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(s,{of:o}),`
+`,e.jsx(n.h1,{id:"navigation-page",children:"Navigation page"}),`
+`,e.jsx(n.p,{children:`A navigation page does not have much content of its own.
+Instead, it presents groups of links to various pages related to a specific subject.`}),`
+`,e.jsx(n.h2,{id:"headings",children:"Headings"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Use a level 1 Heading for the page title."}),`
+`,e.jsx(n.li,{children:"Use the default size of 4 for Card Headings and give them level 2 if they follow the page heading directly, or 3 if there is a section heading in between."}),`
+`,e.jsx(n.li,{children:"Headings in compositions with a Paragraph and a Standalone Link or a Link List should get size 4 as well, along with the appropriate semantic level."}),`
+`]}),`
+`,e.jsx(n.h2,{id:"spacing",children:"Spacing"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Grids on this page type use the default x-large vertical gap."}),`
+`]})]})}function u(i={}){const{wrapper:n}={...a(),...i.components};return n?e.jsx(n,{...i,children:e.jsx(t,{...i})}):t(i)}export{u as default};
