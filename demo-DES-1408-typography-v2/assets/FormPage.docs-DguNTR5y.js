5a1a2182f03cd290f68013159cb2c1c77bfbef42
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-08-10 20:20:16 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@625112603b9e81674a0e9c5f84d58b4680ba498d 🚀

--- demo-DES-1408-typography-v2/assets/FormPage.docs-DguNTR5y.js
+++
@@ -1,5 +0,0 @@
-import{j as o}from"./iframe-B1NqtpYC.js";import{useMDXComponents as r}from"./index-Dm2kRdpT.js";import{M as s}from"./blocks-BE9wjfvW.js";import{F as m}from"./FormPage.stories-Bkybgl65.js";import"./index-Kuhbfl65.js";import"./config-U2gme9bW.js";import"./PageFooter.stories-Db0ANkH_.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
-`,`
-`,o.jsx(s,{of:m}),`
-`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
-`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function d(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{d as default};
