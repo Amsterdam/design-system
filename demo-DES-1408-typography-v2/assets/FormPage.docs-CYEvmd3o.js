92e60b7dd4d7f919a24b2c38c3774f7d02a6b4cd
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-08-10 19:47:24 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@42db8d6532e47d7489a64d785561b100ac768172 🚀

---
+++ demo-DES-1408-typography-v2/assets/FormPage.docs-CYEvmd3o.js
@@ -0,0 +1,5 @@
+import{j as o}from"./iframe-Bxj7p9XG.js";import{useMDXComponents as r}from"./index-BVahEskc.js";import{M as s}from"./blocks-CHuMr1lC.js";import{F as m}from"./FormPage.stories-DadQnEnv.js";import"./index-Gjq7lhRv.js";import"./config-DvH99Xhx.js";import"./PageFooter.stories-BhLsZPGN.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
+`,`
+`,o.jsx(s,{of:m}),`
+`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
+`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function d(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{d as default};
---
+++ demo-DES-1408-typography-v2/assets/copyright.docs-CtT_m6Ys.js
@@ -0,0 +1,30 @@
+import{j as e}from"./iframe-Bxj7p9XG.js";import{useMDXComponents as s}from"./index-BVahEskc.js";import{M as r,c as a}from"./blocks-CHuMr1lC.js";import"./index-Gjq7lhRv.js";const i=`<!-- @license CC0-1.0 -->
+
+# Copyright
+
+## Branding, logo and authors’ rights
+
+The branding and logo are used exclusively by the City of Amsterdam.
+Authors’ rights and trademark rights apply to the logo.
+If you intend to use a modified version of the software for other purposes, you are not allowed to use the City of Amsterdam’s logo.
+Instead, you should design your own branding.
+
+## Typefaces
+
+Not all typefaces used in the branding are free and open source.
+Please note that you must arrange a (paid) licence if you use the provided typefaces.
+Alternatively, adjust the configuration to use fewer or different typefaces.
+
+Amsterdam Sans is licenced exclusively to the City of Amsterdam and amsterdam&partners.
+
+## Icons
+
+Our icons can only be used on websites and applications where the City of Amsterdam is the main communicator.
+
+## Permission
+
+For permission to use the logo or branding, please contact <designsystem@amsterdam.nl>.
+`;/*@license CC0-1.0*/function o(n){return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(r,{title:"Docs/Terms of Use/Copyright"}),`
+`,e.jsx(a,{children:i})]})}function l(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{l as default};
