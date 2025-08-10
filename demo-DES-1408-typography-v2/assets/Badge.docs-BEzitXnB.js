f37a70faabbe5498c6f113d1fc85cd9594b49795
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-08-10 20:23:25 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@c56506f84e5caa13355a8a92f783667f3aed4360 🚀

---
+++ demo-DES-1408-typography-v2/assets/Badge.docs-BEzitXnB.js
@@ -0,0 +1,19 @@
+import{j as n}from"./iframe-DlbZ6vgj.js";import{useMDXComponents as t}from"./index-pOLI6dNR.js";import{M as s,c as a,P as i,e as c}from"./blocks-pA5xRIQl.js";import{B as d}from"./Badge.stories-Ccs6vyjn.js";import"./index-CpTpK7P-.js";import"./Badge-BlFHfVmZ.js";const u=`<!-- @license CC0-1.0 -->
+
+# Badge
+
+A prominent label that indicates a category, property or instruction.
+
+## Guidelines
+
+- Let the badge contain a short text or a number.
+- Use any of the feedback or highlight [colours](/docs/brand-design-tokens-colour--docs) for the background.
+  The default is green.
+  Red, orange and green are especially useful for showing an error, warning, or success.
+  However, they can also be combined with the other colours without carrying these meanings.
+`;/*@license CC0-1.0*/function r(e){return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(s,{of:d}),`
+`,n.jsx(a,{children:u}),`
+`,n.jsx(i,{}),`
+`,n.jsx(c,{})]})}function x(e={}){const{wrapper:o}={...t(),...e.components};return o?n.jsx(o,{...e,children:n.jsx(r,{...e})}):r()}export{x as default};
---
+++ demo-DES-1408-typography-v2/assets/FormPage.docs-Bx1Wwvk5.js
@@ -0,0 +1,5 @@
+import{j as o}from"./iframe-DlbZ6vgj.js";import{useMDXComponents as r}from"./index-pOLI6dNR.js";import{M as s}from"./blocks-pA5xRIQl.js";import{F as m}from"./FormPage.stories-B2WUS3K3.js";import"./index-CpTpK7P-.js";import"./config-CMeOY-r2.js";import"./PageFooter.stories-tAkUcH4H.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
+`,`
+`,o.jsx(s,{of:m}),`
+`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
+`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function d(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{d as default};
