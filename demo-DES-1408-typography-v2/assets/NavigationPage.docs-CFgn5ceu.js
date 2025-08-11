399963f33375d513ac8e98a87c0bc858d134b45b
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-08-11 08:50:44 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@5b4bab58d67fc54b60b96558edb65fdb08a3149f 🚀

---
+++ demo-DES-1408-typography-v2/assets/NavigationPage.docs-CFgn5ceu.js
@@ -0,0 +1,17 @@
+import{j as n}from"./iframe-va82AIAY.js";import{useMDXComponents as a}from"./index-GFmhW0sS.js";import{M as s}from"./blocks-BRgGllah.js";import{N as o}from"./NavigationPage.stories-D-NelfhM.js";import"./index-B4JOikTA.js";import"./exampleContent-B7cDFRSs.js";import"./config-1N-u9-zO.js";import"./PageFooter.stories-EtLfGv9Y.js";/*@license CC0-1.0*/function t(i){const e={h1:"h1",h2:"h2",li:"li",p:"p",ul:"ul",...a(),...i.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(s,{of:o}),`
+`,n.jsx(e.h1,{id:"navigation-page",children:"Navigation page"}),`
+`,n.jsx(e.p,{children:`A navigation page does not have much content of its own.
+Instead, it presents groups of links to various pages related to a specific subject.`}),`
+`,n.jsx(e.h2,{id:"headings",children:"Headings"}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsx(e.li,{children:"Use a level 1 Heading for the page title."}),`
+`,n.jsx(e.li,{children:`All Card Headings and Headings in compositions with a Paragraph and a Standalone Link or a Link List get size 3.
+This is already the default for Card Headings.
+Give them level 2 and size 3 if they follow the page heading directly.`}),`
+`]}),`
+`,n.jsx(e.h2,{id:"spacing",children:"Spacing"}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsx(e.li,{children:"Grids on this page type use the default x-large vertical gap."}),`
+`]})]})}function u(i={}){const{wrapper:e}={...a(),...i.components};return e?n.jsx(e,{...i,children:n.jsx(t,{...i})}):t(i)}export{u as default};
