4d3f6c98f60d077c910da9b23658ef4767dfd9c2
GitHub <github-action@users.noreply.github.com>
2025-08-13 11:04:45 +0000

    Remove feature branch folder

--- demo-npm_and_yarn/patch-and-minor-dev-dependencies-7071dec27d/assets/NavigationPage.docs-BVINjTwZ.js
+++
@@ -1,6 +0,0 @@
-import{j as t}from"./iframe-DACOrW76.js";import{useMDXComponents as i}from"./index-l9o6YxBR.js";import{M as s}from"./blocks-DmgDLJJ0.js";import{N as r}from"./NavigationPage.stories-B5uq7B7I.js";import"./index-CbvOz38e.js";import"./exampleContent-B7cDFRSs.js";import"./config-DFxx-s2h.js";import"./PageFooter.stories-BnD7iyoP.js";/*@license CC0-1.0*/function e(n){const o={h1:"h1",p:"p",...i(),...n.components};return t.jsxs(t.Fragment,{children:[`
-`,`
-`,t.jsx(s,{of:r}),`
-`,t.jsx(o.h1,{id:"navigation-page",children:"Navigation page"}),`
-`,t.jsx(o.p,{children:`A navigation page does not have much content of its own.
-Instead, it presents groups of links to various pages related to a specific subject.`})]})}function x(n={}){const{wrapper:o}={...i(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(e,{...n})}):e(n)}export{x as default};
