64a6db4832c37e9bb8f6d90ca7988ff011a51b29
GitHub <github-action@users.noreply.github.com>
2025-08-13 11:13:37 +0000

    Remove feature branch folder

--- demo-npm_and_yarn/html-validate-10.0.0/assets/ArticlePage.docs-QYAiOUYn.js
+++
@@ -1,5 +0,0 @@
-import{j as t}from"./iframe-Drn-OJAB.js";import{useMDXComponents as e}from"./index-BqJGaaxM.js";import{M as i}from"./blocks-DwO5xMOb.js";import{A as s}from"./ArticlePage.stories-B0zGzk1J.js";import"./index-BZl9dTu_.js";import"./common-DrnyCu3t.js";import"./HomeNews-LdQcShib.js";import"./config-DB4d4BVL.js";import"./PageFooter.stories-DCFwFZqY.js";/*@license CC0-1.0*/function r(n){const o={h1:"h1",p:"p",...e(),...n.components};return t.jsxs(t.Fragment,{children:[`
-`,`
-`,t.jsx(i,{of:s}),`
-`,t.jsx(o.h1,{id:"article-page",children:"Article page"}),`
-`,t.jsx(o.p,{children:"This page type is for news or similar kinds of articles."})]})}function j(n={}){const{wrapper:o}={...e(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(r,{...n})}):r(n)}export{j as default};
--- demo-npm_and_yarn/html-validate-10.0.0/assets/FormPage.docs-C1BpP8Sy.js
+++
@@ -1,5 +0,0 @@
-import{j as o}from"./iframe-Drn-OJAB.js";import{useMDXComponents as r}from"./index-BqJGaaxM.js";import{M as s}from"./blocks-DwO5xMOb.js";import{F as m}from"./FormPage.stories-DzUk6MMm.js";import"./index-BZl9dTu_.js";import"./config-DB4d4BVL.js";import"./PageFooter.stories-DCFwFZqY.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
-`,`
-`,o.jsx(s,{of:m}),`
-`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
-`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function d(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{d as default};
