20e6b0e2135133dc6611c4c494d4c7aa8f81cb11
alimpens <37216945+alimpens@users.noreply.github.com>
2025-08-13 10:12:03 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@410246b992223d0a9cd67dd73c3465490f8f1afa 🚀

--- demo-DES-1358-add-form-page-examples/assets/NavigationPage.docs-DEvWK08Z.js
+++
@@ -1,6 +0,0 @@
-import{j as t}from"./iframe-OW4oVQ-T.js";import{useMDXComponents as i}from"./index-C7hp3sF6.js";import{M as r}from"./blocks-QfDEKDwc.js";import{N as s}from"./NavigationPage.stories-BncKIOG8.js";import"./index-H_NnBPq2.js";import"./exampleContent-B7cDFRSs.js";import"./config-ChaO431f.js";import"./Layout-Cvc3KM3X.js";import"./PageFooter.stories-Q7M1uD31.js";/*@license CC0-1.0*/function e(n){const o={h1:"h1",p:"p",...i(),...n.components};return t.jsxs(t.Fragment,{children:[`
-`,`
-`,t.jsx(r,{of:s}),`
-`,t.jsx(o.h1,{id:"navigation-page",children:"Navigation page"}),`
-`,t.jsx(o.p,{children:`A navigation page does not have much content of its own.
-Instead, it presents groups of links to various pages related to a specific subject.`})]})}function h(n={}){const{wrapper:o}={...i(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(e,{...n})}):e(n)}export{h as default};
---
+++ demo-DES-1358-add-form-page-examples/assets/ProductPage.docs-DPK9Jx_z.js
@@ -0,0 +1,6 @@
+import{j as t}from"./iframe-CHufOM-6.js";import{useMDXComponents as e}from"./index-D1fh9JEC.js";import{M as i}from"./blocks-B0XYcX-v.js";import{P as s}from"./ProductPage.stories-Dh0AxEOw.js";import"./index-BHrxS8Js.js";import"./config-BrprAsI-.js";import"./Layout-CLqatJui.js";import"./PageFooter.stories-C5Jc5kJt.js";/*@license CC0-1.0*/function r(o){const n={h1:"h1",p:"p",...e(),...o.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(i,{of:s}),`
+`,t.jsx(n.h1,{id:"product-page",children:"Product page"}),`
+`,t.jsx(n.p,{children:`Lists all information about a product or service of the City.
+Often includes a Call to Action Link that directs the user to a form.`})]})}function l(o={}){const{wrapper:n}={...e(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(r,{...o})}):r(o)}export{l as default};
