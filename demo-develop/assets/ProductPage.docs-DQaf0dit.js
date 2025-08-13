b72506d63fc726c419a2e897dbfc81680f160d43
alimpens <37216945+alimpens@users.noreply.github.com>
2025-08-13 11:06:22 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@853865baf5c599d26716b1a3e52ed2cb7aad2a26 🚀

--- demo-develop/assets/ProductPage.docs-DQaf0dit.js
+++
@@ -1,6 +0,0 @@
-import{j as t}from"./iframe-Drn-OJAB.js";import{useMDXComponents as e}from"./index-BqJGaaxM.js";import{M as i}from"./blocks-DwO5xMOb.js";import{P as s}from"./ProductPage.stories-D2huvRi4.js";import"./index-BZl9dTu_.js";import"./config-DB4d4BVL.js";import"./PageFooter.stories-DCFwFZqY.js";/*@license CC0-1.0*/function r(o){const n={h1:"h1",p:"p",...e(),...o.components};return t.jsxs(t.Fragment,{children:[`
-`,`
-`,t.jsx(i,{of:s}),`
-`,t.jsx(n.h1,{id:"product-page",children:"Product page"}),`
-`,t.jsx(n.p,{children:`Lists all information about a product or service of the City.
-Often includes a Call to Action Link that directs the user to a form.`})]})}function h(o={}){const{wrapper:n}={...e(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(r,{...o})}):r(o)}export{h as default};
