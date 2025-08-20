739836f391ac21e242e4c6cda5881e6b1a6ea256
alimpens <37216945+alimpens@users.noreply.github.com>
2025-08-20 08:00:48 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@f0f3211ebf98f189fb307ca44fa1a93a25bb4506 🚀

---
+++ demo-develop/assets/FormPage.docs-PlxA8Epp.js
@@ -0,0 +1,5 @@
+import{j as o}from"./iframe-Bja2d3i7.js";import{useMDXComponents as r}from"./index-DRVAFPo-.js";import{M as s}from"./blocks-Ds0c63Sh.js";import{F as m}from"./FormPage.stories-B5PWeX9U.js";import"./index-BPsxZe6X.js";import"./config-BXwYF-zF.js";import"./PageFooter.stories-Bp1tv1Qv.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
+`,`
+`,o.jsx(s,{of:m}),`
+`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
+`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function d(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{d as default};
--- demo-develop/assets/NavigationPage.docs-DcV1YnMM.js
+++
@@ -1,6 +0,0 @@
-import{j as t}from"./iframe-BlTSEvLE.js";import{useMDXComponents as i}from"./index-C8fBMEu3.js";import{M as s}from"./blocks-Db3mX6pe.js";import{N as r}from"./NavigationPage.stories-D8PpNl8t.js";import"./index-BwmE4Tmo.js";import"./exampleContent-B7cDFRSs.js";import"./config-CVp5yyyZ.js";import"./PageFooter.stories-RE_xLfTm.js";/*@license CC0-1.0*/function e(n){const o={h1:"h1",p:"p",...i(),...n.components};return t.jsxs(t.Fragment,{children:[`
-`,`
-`,t.jsx(s,{of:r}),`
-`,t.jsx(o.h1,{id:"navigation-page",children:"Navigation page"}),`
-`,t.jsx(o.p,{children:`A navigation page does not have much content of its own.
-Instead, it presents groups of links to various pages related to a specific subject.`})]})}function x(n={}){const{wrapper:o}={...i(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(e,{...n})}):e(n)}export{x as default};
