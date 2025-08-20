23e5da029d2f035ee27b6c7141dd83be8ea683b8
alimpens <37216945+alimpens@users.noreply.github.com>
2025-08-20 08:22:36 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@c06a40885750964432cd68360574f0ff9c0b47b5 🚀

---
+++ demo-npm_and_yarn/patch-and-minor-dependencies-b56097c19e/assets/Badge.docs-sdHNXyFB.js
@@ -0,0 +1,19 @@
+import{j as n}from"./iframe-AQWlAU3l.js";import{useMDXComponents as t}from"./index-BJGpXqFS.js";import{M as s,c as a,P as i,e as c}from"./blocks-DwS6rTpG.js";import{B as d}from"./Badge.stories-B02CbujL.js";import"./index-CjkNjoAe.js";import"./Badge-BK-1PE08.js";const u=`<!-- @license CC0-1.0 -->
+
+# Badge
+
+A prominent label that indicates a category, property or instruction.
+
+## Guidelines
+
+- Let the badge contain a short text or a number.
+- Use any of the feedback or highlight [colours](/docs/brand-design-tokens-colour--docs) for the background.
+  The default is green.
+  Red, orange and green are especially useful for showing an error, warning, or success.
+  However, they can also be combined with the other colours without carrying these meanings.
+`;/*@license CC0-1.0*/function r(e){return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(s,{of:d}),`
+`,n.jsx(a,{children:u}),`
+`,n.jsx(i,{}),`
+`,n.jsx(c,{})]})}function x(e={}){const{wrapper:o}={...t(),...e.components};return o?n.jsx(o,{...e,children:n.jsx(r,{...e})}):r()}export{x as default};
---
+++ demo-npm_and_yarn/patch-and-minor-dependencies-b56097c19e/assets/ProductPage.docs-Ty_HHO78.js
@@ -0,0 +1,6 @@
+import{j as t}from"./iframe-AQWlAU3l.js";import{useMDXComponents as e}from"./index-BJGpXqFS.js";import{M as i}from"./blocks-DwS6rTpG.js";import{P as s}from"./ProductPage.stories-DSSVAEYu.js";import"./index-CjkNjoAe.js";import"./config-CS5h0-li.js";import"./PageFooter.stories-BlN07Ikm.js";/*@license CC0-1.0*/function r(o){const n={h1:"h1",p:"p",...e(),...o.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(i,{of:s}),`
+`,t.jsx(n.h1,{id:"product-page",children:"Product page"}),`
+`,t.jsx(n.p,{children:`Lists all information about a product or service of the City.
+Often includes a Call to Action Link that directs the user to a form.`})]})}function h(o={}){const{wrapper:n}={...e(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(r,{...o})}):r(o)}export{h as default};
