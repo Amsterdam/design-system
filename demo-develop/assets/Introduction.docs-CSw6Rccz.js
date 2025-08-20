8f4e0ea5be083124b9381866f2ae00f57a819efb
alimpens <37216945+alimpens@users.noreply.github.com>
2025-08-20 08:04:31 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@6f7e33253087bd0a2e6e19828eedcb50ea4e8779 🚀

---
+++ demo-develop/assets/Introduction.docs-CSw6Rccz.js
@@ -0,0 +1,24 @@
+import{j as e}from"./iframe-AQWlAU3l.js";import{useMDXComponents as s}from"./index-BJGpXqFS.js";import{M as i}from"./blocks-DwS6rTpG.js";import"./index-CjkNjoAe.js";/*@license CC0-1.0*/function o(n){const t={code:"code",h1:"h1",h2:"h2",h3:"h3",p:"p",...s(),...n.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(i,{title:"Pages/Introduction"}),`
+`,e.jsx(t.h1,{id:"pages",children:"Pages"}),`
+`,e.jsx(t.p,{children:`This section demonstrates how to compose pages with the Design System’s components.
+It uses the latest versions of the libraries, without any customizations.
+This gives an honest representation of what they can achieve and how they work in the browser.`}),`
+`,e.jsx(t.h2,{id:"documentation",children:"Documentation"}),`
+`,e.jsx(t.p,{children:"We offer examples for various page types; see the list in the sidebar."}),`
+`,e.jsx(t.p,{children:"The ‘Docs’ page for each page explains its design decisions and how to build it."}),`
+`,e.jsx(t.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(t.p,{children:`The ‘Default’ example shows the actual page in its entirety.
+Some page types have more than one example.`}),`
+`,e.jsx(t.h3,{id:"flexible-content",children:"Flexible content"}),`
+`,e.jsx(t.p,{children:`Reload an example to get new random texts and images.
+Use the ‘Control’ tab to see how it looks with your own content.`}),`
+`,e.jsx(t.h3,{id:"test-for-accessibility-and-see-html-output",children:"Test for accessibility and see HTML output"}),`
+`,e.jsx(t.p,{children:`The ‘Accessibility’ tab offers a report of various automatic tests.
+The ‘HTML’ tab displays the entire document as rendered by all components on the page.`}),`
+`,e.jsx(t.h3,{id:"check-how-the-page-would-look-on-various-devices",children:"Check how the page would look on various devices"}),`
+`,e.jsx(t.p,{children:`The buttons in the toolbar offer various features, of which the ‘three-layered square’ button replicates the appearance and behaviour of the page on a phone, tablet or desktop.
+Press the button with the two arrows to rotate the screen.`}),`
+`,e.jsx(t.h3,{id:"see-the-page-full-screen",children:"See the page full-screen"}),`
+`,e.jsxs(t.p,{children:["Use the ‘Go to full screen’ (or press ",e.jsx(t.code,{children:"alt+F"}),") or ‘Open canvas in new tab’ buttons to the top right to see the entire page."]})]})}function l(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o(n)}export{l as default};
---
+++ demo-develop/assets/NavigationPage.docs-DtcZ_TdX.js
@@ -0,0 +1,6 @@
+import{j as t}from"./iframe-AQWlAU3l.js";import{useMDXComponents as i}from"./index-BJGpXqFS.js";import{M as s}from"./blocks-DwS6rTpG.js";import{N as r}from"./NavigationPage.stories-8w7RRlGu.js";import"./index-CjkNjoAe.js";import"./exampleContent-B7cDFRSs.js";import"./config-CS5h0-li.js";import"./PageFooter.stories-BlN07Ikm.js";/*@license CC0-1.0*/function e(n){const o={h1:"h1",p:"p",...i(),...n.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(s,{of:r}),`
+`,t.jsx(o.h1,{id:"navigation-page",children:"Navigation page"}),`
+`,t.jsx(o.p,{children:`A navigation page does not have much content of its own.
+Instead, it presents groups of links to various pages related to a specific subject.`})]})}function x(n={}){const{wrapper:o}={...i(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(e,{...n})}):e(n)}export{x as default};
