f5080775e2e5ebd7382b170da5108ba1545cf968
alimpens <37216945+alimpens@users.noreply.github.com>
2025-08-27 07:51:12 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@9795121e1e6020c05a183fb15b93b4558b588803 🚀

---
+++ demo-DES-1392-fix-product-page-markup/assets/FormPage.docs-nzRCvVv5.js
@@ -0,0 +1,5 @@
+import{j as o}from"./iframe-COln_Wya.js";import{useMDXComponents as r}from"./index-BgHmsA34.js";import{M as s}from"./blocks-DYX8m74r.js";import{F as m}from"./FormPage.stories-C2nf5WBM.js";import"./index-C-zhRIJx.js";import"./config-BK0xLfpu.js";import"./PageFooter.stories-B0hu5wnD.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
+`,`
+`,o.jsx(s,{of:m}),`
+`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
+`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function d(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{d as default};
