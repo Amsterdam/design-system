a56c00f658e80a952c0b3eab1a3abe825ff95314
alimpens <37216945+alimpens@users.noreply.github.com>
2025-08-27 07:22:14 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@da8fd078f4311761cf7f24a4062cf157c73edaad 🚀

--- demo-DES-1273-use-lint-rules/assets/NavigationPage.docs-D8Y9Gzb4.js
+++
@@ -1,6 +0,0 @@
-import{j as t}from"./iframe-BbZEBrLd.js";import{useMDXComponents as i}from"./index-C9LTarRE.js";import{M as s}from"./blocks-D8Gu8GUJ.js";import{N as r}from"./NavigationPage.stories-CkLFROSG.js";import"./index-r9OV9uQ3.js";import"./exampleContent-B7cDFRSs.js";import"./config-BaWVhRN9.js";import"./PageFooter.stories-BQJaEx2n.js";/*@license CC0-1.0*/function e(n){const o={h1:"h1",p:"p",...i(),...n.components};return t.jsxs(t.Fragment,{children:[`
-`,`
-`,t.jsx(s,{of:r}),`
-`,t.jsx(o.h1,{id:"navigation-page",children:"Navigation page"}),`
-`,t.jsx(o.p,{children:`A navigation page does not have much content of its own.
-Instead, it presents groups of links to various pages related to a specific subject.`})]})}function x(n={}){const{wrapper:o}={...i(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(e,{...n})}):e(n)}export{x as default};
--- demo-DES-1273-use-lint-rules/assets/PageHeading.stories-B2Tuh7lY.js
+++
@@ -1,8 +0,0 @@
-import{r as d,j as i,c as p}from"./iframe-BbZEBrLd.js";const r=d.forwardRef(({children:s,className:t,color:o,...n},c)=>i.jsx("h1",{...n,className:p("ams-page-heading",o&&`ams-page-heading--${o}`,t),ref:c,children:s}));r.displayName="PageHeading";try{r.displayName="PageHeading",r.__docgenInfo={description:"",displayName:"PageHeading",props:{}}}catch{}/**
- * @license EUPL-1.2+
- * Copyright Gemeente Amsterdam
- */const l={title:"Components/Text/Page Heading",component:r,args:{children:"Gemeente Amsterdam"},argTypes:{children:{description:"The heading text.",table:{disable:!1}},color:{control:{labels:{undefined:"default"},type:"radio"},options:[void 0,"inverse"]}}},e={},a={args:{color:"inverse"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:"{}",...e.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
-  args: {
-    color: 'inverse'
-  }
-}`,...a.parameters?.docs?.source}}};const g=["Default","InverseColour"],u=Object.freeze(Object.defineProperty({__proto__:null,Default:e,InverseColour:a,__namedExportsOrder:g,default:l},Symbol.toStringTag,{value:"Module"}));export{a as I,u as P};
