f7506ef05e9d93e144384e6b979edff6b9a1ab2c
alimpens <37216945+alimpens@users.noreply.github.com>
2025-08-27 09:38:04 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@fce56ff3dceb5e73d46a9712d4595e37f4f955c9 🚀

---
+++ demo-deprecate-unused-tokens/assets/ArticlePage.docs-jgaf0h7z.js
@@ -0,0 +1,5 @@
+import{j as t}from"./iframe-BOmxlOCJ.js";import{useMDXComponents as e}from"./index-CFM1kcc2.js";import{M as i}from"./blocks-BSrdrN-k.js";import{A as s}from"./ArticlePage.stories-DX5WT3f9.js";import"./index-xOyp-lFS.js";import"./common-DrnyCu3t.js";import"./HomeNews-DfMQTO-h.js";import"./config-CkCSdr68.js";import"./PageFooter.stories-YHEaaOnO.js";/*@license CC0-1.0*/function r(n){const o={h1:"h1",p:"p",...e(),...n.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(i,{of:s}),`
+`,t.jsx(o.h1,{id:"article-page",children:"Article page"}),`
+`,t.jsx(o.p,{children:"This page type is for news or similar kinds of articles."})]})}function j(n={}){const{wrapper:o}={...e(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(r,{...n})}):r(n)}export{j as default};
---
+++ demo-deprecate-unused-tokens/assets/FormPage.docs-CBgq2keD.js
@@ -0,0 +1,5 @@
+import{j as o}from"./iframe-BOmxlOCJ.js";import{useMDXComponents as r}from"./index-CFM1kcc2.js";import{M as s}from"./blocks-BSrdrN-k.js";import{F as m}from"./FormPage.stories-As0PqsrQ.js";import"./index-xOyp-lFS.js";import"./config-CkCSdr68.js";import"./PageFooter.stories-YHEaaOnO.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
+`,`
+`,o.jsx(s,{of:m}),`
+`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
+`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function d(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{d as default};
