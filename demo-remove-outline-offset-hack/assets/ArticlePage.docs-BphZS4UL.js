6492175a2f910c5c07731eea9762836ffe67c568
alimpens <37216945+alimpens@users.noreply.github.com>
2025-08-27 08:39:41 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@83b4f5c7e42e4f6d2ff0195fd7d305480ac18cd1 🚀

---
+++ demo-remove-outline-offset-hack/assets/ArticlePage.docs-BphZS4UL.js
@@ -0,0 +1,5 @@
+import{j as t}from"./iframe-ClfhyRHb.js";import{useMDXComponents as e}from"./index-Bm_pGp7C.js";import{M as i}from"./blocks-DUn3QP1n.js";import{A as s}from"./ArticlePage.stories-BkfUxasr.js";import"./index-D7eLwXoN.js";import"./common-DrnyCu3t.js";import"./HomeNews-B2FHclw7.js";import"./config-WgByoY-Q.js";import"./PageFooter.stories-Bej-ApBG.js";/*@license CC0-1.0*/function r(n){const o={h1:"h1",p:"p",...e(),...n.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(i,{of:s}),`
+`,t.jsx(o.h1,{id:"article-page",children:"Article page"}),`
+`,t.jsx(o.p,{children:"This page type is for news or similar kinds of articles."})]})}function j(n={}){const{wrapper:o}={...e(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(r,{...n})}):r(n)}export{j as default};
