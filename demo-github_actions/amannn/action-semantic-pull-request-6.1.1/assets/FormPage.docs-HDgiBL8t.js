b38c6ea7c402f2571314b2da888345b97ed8859c
alimpens <37216945+alimpens@users.noreply.github.com>
2025-08-27 09:25:31 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@19c24afca0281c86496a2cf7f7e8b7619cee8b32 🚀

---
+++ demo-github_actions/amannn/action-semantic-pull-request-6.1.1/assets/FormPage.docs-HDgiBL8t.js
@@ -0,0 +1,5 @@
+import{j as o}from"./iframe-DBIc7Dfn.js";import{useMDXComponents as r}from"./index-DVV9xH4c.js";import{M as s}from"./blocks-cN5yN0DV.js";import{F as m}from"./FormPage.stories-CfG8hyjG.js";import"./index-DXAXgmP6.js";import"./config-Dgt-leY_.js";import"./PageFooter.stories-U2WeDV_d.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
+`,`
+`,o.jsx(s,{of:m}),`
+`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
+`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function d(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{d as default};
