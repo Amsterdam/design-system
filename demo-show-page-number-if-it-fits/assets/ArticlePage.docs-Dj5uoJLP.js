9687c2078875431eb560eeb95770e5ae7adc267f
alimpens <37216945+alimpens@users.noreply.github.com>
2025-08-27 11:06:56 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@76df23297646df6050c46c23c66ce6b9e9053bfd 🚀

---
+++ demo-show-page-number-if-it-fits/assets/ArticlePage.docs-Dj5uoJLP.js
@@ -0,0 +1,5 @@
+import{j as t}from"./iframe-Ds1yFmSB.js";import{useMDXComponents as e}from"./index-A2SVB7uH.js";import{M as i}from"./blocks-CghVeyYf.js";import{A as s}from"./ArticlePage.stories-CENy4lop.js";import"./index-dSA4kVvb.js";import"./common-DrnyCu3t.js";import"./HomeNews-CrNFHFd5.js";import"./config-Dt4IX3fH.js";import"./PageFooter.stories-CY5QutYN.js";/*@license CC0-1.0*/function r(n){const o={h1:"h1",p:"p",...e(),...n.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(i,{of:s}),`
+`,t.jsx(o.h1,{id:"article-page",children:"Article page"}),`
+`,t.jsx(o.p,{children:"This page type is for news or similar kinds of articles."})]})}function j(n={}){const{wrapper:o}={...e(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(r,{...n})}):r(n)}export{j as default};
