ba513e12d0239ea377611660b2e6c6eb8a241622
alimpens <37216945+alimpens@users.noreply.github.com>
2025-08-29 13:46:46 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@def68e72be57d79ca78b73725bf973d05e54db0f 🚀

---
+++ demo-DES-1413-allow-different-size-accordion-headings/assets/FormPage.docs-CcAgGv0e.js
@@ -0,0 +1,5 @@
+import{j as o}from"./iframe-CUwhsAM3.js";import{useMDXComponents as r}from"./index-DG-01lpn.js";import{M as s}from"./blocks-DSSoEXWu.js";import{F as m}from"./FormPage.stories-Bu3qmTNQ.js";import"./index-CaUeOlSe.js";import"./config-wWVtq1ih.js";import"./PageFooter.stories-mwy5Ulgg.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
+`,`
+`,o.jsx(s,{of:m}),`
+`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
+`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function d(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{d as default};
---
+++ demo-DES-1413-allow-different-size-accordion-headings/assets/ProductPage.docs-CIvO3HLt.js
@@ -0,0 +1,6 @@
+import{j as t}from"./iframe-CUwhsAM3.js";import{useMDXComponents as e}from"./index-DG-01lpn.js";import{M as i}from"./blocks-DSSoEXWu.js";import{P as s}from"./ProductPage.stories-BvAibUBN.js";import"./index-CaUeOlSe.js";import"./config-wWVtq1ih.js";import"./PageFooter.stories-mwy5Ulgg.js";/*@license CC0-1.0*/function r(o){const n={h1:"h1",p:"p",...e(),...o.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(i,{of:s}),`
+`,t.jsx(n.h1,{id:"product-page",children:"Product page"}),`
+`,t.jsx(n.p,{children:`Lists all information about a product or service of the City.
+Often includes a Call to Action Link that directs the user to a form.`})]})}function h(o={}){const{wrapper:n}={...e(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(r,{...o})}):r(o)}export{h as default};
