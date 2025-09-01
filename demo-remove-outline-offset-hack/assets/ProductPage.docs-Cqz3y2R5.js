7ddace70d0cfb8cdbda11c509c70e7c7b51e357a
GitHub <github-action@users.noreply.github.com>
2025-09-01 15:07:48 +0000

    Remove feature branch folder

--- demo-remove-outline-offset-hack/assets/ProductPage.docs-Cqz3y2R5.js
+++
@@ -1,6 +0,0 @@
-import{j as t}from"./iframe-DLbv482-.js";import{useMDXComponents as e}from"./index-CH2P-wdP.js";import{M as i}from"./blocks-BJklC-fq.js";import{P as s}from"./ProductPage.stories-abrTLCy8.js";import"./index-BB1RK1G2.js";import"./config-CmLzddTj.js";import"./PageFooter.stories-Du9Lmuan.js";/*@license CC0-1.0*/function r(o){const n={h1:"h1",p:"p",...e(),...o.components};return t.jsxs(t.Fragment,{children:[`
-`,`
-`,t.jsx(i,{of:s}),`
-`,t.jsx(n.h1,{id:"product-page",children:"Product page"}),`
-`,t.jsx(n.p,{children:`Lists all information about a product or service of the City.
-Often includes a Call to Action Link that directs the user to a form.`})]})}function h(o={}){const{wrapper:n}={...e(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(r,{...o})}):r(o)}export{h as default};
