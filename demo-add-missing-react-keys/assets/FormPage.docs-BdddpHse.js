7b6afaf4722ec40cf5c533468aa4ae3613e376c9
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-09-01 19:36:38 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@068f85edc5d327915a55e44be32bb9c487b396b9 🚀

---
+++ demo-add-missing-react-keys/assets/FormPage.docs-BdddpHse.js
@@ -0,0 +1,5 @@
+import{j as o}from"./iframe-YSEUpZp-.js";import{useMDXComponents as r}from"./index-CmOYNngx.js";import{M as s}from"./blocks-BmoFb8Ts.js";import{F as m}from"./FormPage.stories-kZ2SsSO6.js";import"./index-DH-RjABI.js";import"./config-V9IK09xO.js";import"./PageFooter.stories-ZDchbU85.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
+`,`
+`,o.jsx(s,{of:m}),`
+`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
+`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function d(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{d as default};
