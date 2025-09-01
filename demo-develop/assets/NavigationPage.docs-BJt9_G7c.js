88a90225bda5d2c10060916f2a1d794950d95a8c
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-09-01 14:44:55 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@a69cc4d99e9720d9c9ff0555c2c592d7a89e7e14 🚀

---
+++ demo-develop/assets/NavigationPage.docs-BJt9_G7c.js
@@ -0,0 +1,6 @@
+import{j as t}from"./iframe-DeS9DVLq.js";import{useMDXComponents as i}from"./index--zNYjCNg.js";import{M as s}from"./blocks-rrDanw-v.js";import{N as r}from"./NavigationPage.stories-BsHEMHwH.js";import"./index-B_cWDGnk.js";import"./exampleContent-B7cDFRSs.js";import"./config-LYhtmd8s.js";import"./PageFooter.stories-Ca1Kv7BW.js";/*@license CC0-1.0*/function e(n){const o={h1:"h1",p:"p",...i(),...n.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(s,{of:r}),`
+`,t.jsx(o.h1,{id:"navigation-page",children:"Navigation page"}),`
+`,t.jsx(o.p,{children:`A navigation page does not have much content of its own.
+Instead, it presents groups of links to various pages related to a specific subject.`})]})}function x(n={}){const{wrapper:o}={...i(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(e,{...n})}):e(n)}export{x as default};
---
+++ demo-develop/assets/TextInput.stories-Bje26jWS.js
@@ -0,0 +1,66 @@
+import{r as I,j as a,c as S,F as h,u as v,P as b,E as f}from"./iframe-DeS9DVLq.js";const r=I.forwardRef(({className:e,dir:x,invalid:y,type:j="text",...F},V)=>a.jsx("input",{...F,"aria-invalid":y||void 0,className:S("ams-text-input",e),dir:x??"auto",ref:V,type:j}));r.displayName="TextInput";try{r.displayName="TextInput",r.__docgenInfo={description:"",displayName:"TextInput",props:{invalid:{defaultValue:null,description:"Whether the value fails a validation rule.",name:"invalid",required:!1,type:{name:"boolean"}},type:{defaultValue:{value:"text"},description:"The kind of data that the user should provide.",name:"type",required:!1,type:{name:"enum",value:[{value:'"text"'},{value:'"tel"'},{value:'"url"'},{value:'"email"'}]}}}}}catch{}const P={title:"Components/Forms/Text Input",component:r,args:{disabled:!1,invalid:!1},argTypes:{defaultValue:{table:{disable:!0}},disabled:{description:"Prevents interaction. Avoid if possible."},invalid:{description:"Whether the value fails a validation rule."},size:{control:{min:0,type:"number"},description:"The width, expressed in the average number of characters."}}},s={},i={args:{defaultValue:"designsystem@amsterdam.nl",type:"email"}},t={args:{defaultValue:"https://designsystem.amsterdam/",type:"url"}},n={args:{defaultValue:"14020",type:"tel"}},d={args:{defaultValue:"1",inputMode:"numeric",pattern:"[0-9]*",size:5,spellCheck:!1}},l={args:{defaultValue:"1.99",pattern:"[0-9.,]*",size:5,spellCheck:!1}},o={args:{size:10}},c={args:{placeholder:"Placeholder text"}},u={args:{defaultValue:"Invalid value",invalid:!0}},p={args:{defaultValue:"Disabled input",disabled:!0}},m={render:e=>a.jsxs(h,{invalid:e.invalid,children:[a.jsx(v,{htmlFor:"input1",children:"Label"}),a.jsx(b,{id:"description1",children:"Omschrijving."}),e.invalid&&a.jsx(f,{id:"error1",children:"Foutmelding."}),a.jsx(r,{"aria-describedby":`description1${e.invalid?" error1":""}`,id:"input1",...e})]})},g={args:{invalid:!0},render:e=>a.jsxs(h,{invalid:e.invalid,children:[a.jsx(v,{htmlFor:"input2",children:"Label"}),a.jsx(b,{id:"description2",children:"Omschrijving."}),e.invalid&&a.jsx(f,{id:"error2",children:"Foutmelding."}),a.jsx(r,{"aria-describedby":`description2${e.invalid?" error2":""}`,id:"input2",...e})]})};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:"{}",...s.parameters?.docs?.source}}};i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
+  args: {
+    defaultValue: 'designsystem@amsterdam.nl',
+    type: 'email'
+  }
+}`,...i.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    defaultValue: 'https://designsystem.amsterdam/',
+    type: 'url'
+  }
+}`,...t.parameters?.docs?.source}}};n.parameters={...n.parameters,docs:{...n.parameters?.docs,source:{originalSource:`{
+  args: {
+    defaultValue: '14020',
+    type: 'tel'
+  }
+}`,...n.parameters?.docs?.source}}};d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  args: {
+    defaultValue: '1',
+    inputMode: 'numeric',
+    pattern: '[0-9]*',
+    size: 5,
+    spellCheck: false
+  }
+}`,...d.parameters?.docs?.source}}};l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  args: {
+    defaultValue: '1.99',
+    pattern: '[0-9.,]*',
+    size: 5,
+    spellCheck: false
+  }
+}`,...l.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    size: 10
+  }
+}`,...o.parameters?.docs?.source}}};c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    placeholder: 'Placeholder text'
+  }
+}`,...c.parameters?.docs?.source}}};u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    defaultValue: 'Invalid value',
+    invalid: true
+  }
+}`,...u.parameters?.docs?.source}}};p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
+  args: {
+    defaultValue: 'Disabled input',
+    disabled: true
+  }
+}`,...p.parameters?.docs?.source}}};m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`{
+  render: args => <Field invalid={args.invalid}>
+      <Label htmlFor="input1">Label</Label>
+      <Paragraph id="description1">Omschrijving.</Paragraph>
+      {args.invalid && <ErrorMessage id="error1">Foutmelding.</ErrorMessage>}
+      <TextInput aria-describedby={\`description1\${args.invalid ? ' error1' : ''}\`} id="input1" {...args} />
+    </Field>
+}`,...m.parameters?.docs?.source}}};g.parameters={...g.parameters,docs:{...g.parameters?.docs,source:{originalSource:`{
+  args: {
+    invalid: true
+  },
+  render: args => <Field invalid={args.invalid}>
+      <Label htmlFor="input2">Label</Label>
+      <Paragraph id="description2">Omschrijving.</Paragraph>
+      {args.invalid && <ErrorMessage id="error2">Foutmelding.</ErrorMessage>}
+      <TextInput aria-describedby={\`description2\${args.invalid ? ' error2' : ''}\`} id="input2" {...args} />
+    </Field>
+}`,...g.parameters?.docs?.source}}};const _=["Default","EmailAddress","WebAddress","PhoneNumber","WholeNumber","DecimalNumber","Size","Placeholder","Invalid","Disabled","InAField","InAFieldWithValidation"],E=Object.freeze(Object.defineProperty({__proto__:null,DecimalNumber:l,Default:s,Disabled:p,EmailAddress:i,InAField:m,InAFieldWithValidation:g,Invalid:u,PhoneNumber:n,Placeholder:c,Size:o,WebAddress:t,WholeNumber:d,__namedExportsOrder:_,default:P},Symbol.toStringTag,{value:"Module"}));export{l as D,i as E,u as I,n as P,o as S,E as T,t as W,d as a,c as b,p as c,m as d,g as e};
