da381c7b29bb03054c9f0b9d07e53eac3501a912
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-09-03 15:37:32 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@40caee3e46561783ee68146ae3922b4b77b67e15 🚀

---
+++ demo-DES-1408-typography-v2/assets/SkipLink.docs-zfAz4gNZ.js
@@ -0,0 +1,34 @@
+import{j as n}from"./iframe-C9XODWs0.js";import{useMDXComponents as s}from"./index-B3pvoSoF.js";import{M as o,c as r,P as p,e as l,f as t}from"./blocks-DHs6m7wH.js";import{S as c,O as h,M as m}from"./SkipLink.stories-Crd1oYE0.js";import"./index-9f0w4Ulw.js";const d=`<!-- @license CC0-1.0 -->
+
+# Skip Link
+
+Allows skipping past recurring navigation blocks at the top of a page.
+
+## Design
+
+The Skip Link sits above the Page Header and is as wide as the Page container.
+It remains hidden until activated with the ‘Tab’ key.
+After appearing, it pushes the rest of the page down.
+
+## Guidelines
+
+- Place the Skip Link as the first element in the [Page](/docs/components-containers-page--docs) container.
+- Target the Skip Link to the main content.
+  On an article page, for example, it could be the title of the article.
+  On a search page, it could be the search field.
+- Set \`id="example-id"\` on the container of that element and then use \`href="#example-id"\` on the Skip Link.
+- Complex pages with multiple sections may benefit from more than one Skip Link.
+- Skip Links are unnecessary on a simple page with only text and minimal navigation.
+`;/*@license CC0-1.0*/function a(i){const e={h2:"h2",h3:"h3",p:"p",...s(),...i.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(o,{of:c}),`
+`,n.jsx(r,{children:d}),`
+`,n.jsx(p,{}),`
+`,n.jsx(l,{}),`
+`,n.jsx(e.h2,{id:"examples",children:"Examples"}),`
+`,n.jsx(e.h3,{id:"display-on-focus",children:"Display on focus"}),`
+`,n.jsx(e.p,{children:"A Skip Link is only displayed when it receives focus."}),`
+`,n.jsx(t,{of:h}),`
+`,n.jsx(e.h3,{id:"multiple-links",children:"Multiple links"}),`
+`,n.jsx(e.p,{children:"You can use more than one Skip Link if you have a complex page with multiple sections."}),`
+`,n.jsx(t,{of:m})]})}function j(i={}){const{wrapper:e}={...s(),...i.components};return e?n.jsx(e,{...i,children:n.jsx(a,{...i})}):a(i)}export{j as default};
