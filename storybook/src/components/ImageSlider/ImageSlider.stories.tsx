2e5785e9f67256166881b63a597f4af413c4e52a
Vincent Smedinga <v.smedinga@amsterdam.nl>
2025-09-04 17:22:40 +0200

    poc: Add example of Image Slider with various aspect ratios

--- storybook/src/components/ImageSlider/ImageSlider.stories.tsx
+++ storybook/src/components/ImageSlider/ImageSlider.stories.tsx
@@ -66,3 +66,42 @@ export const ResponsiveImages: Story = {
     ],
   },
 }
+
+export const VariousAspectRatios: Story = {
+  args: {
+    images: [
+      {
+        alt: 'Landscape 16:9',
+        aspectRatio: '16:9',
+        src: 'https://picsum.photos/id/870/768/432',
+      },
+      {
+        alt: 'Landscape 4:3',
+        aspectRatio: '4:3',
+        src: 'https://picsum.photos/id/870/576/432',
+      },
+      {
+        alt: 'Square 1:1',
+        aspectRatio: '1:1',
+        src: 'https://picsum.photos/id/870/432/432',
+      },
+      {
+        alt: 'Portrait 3:4',
+        aspectRatio: '3:4',
+        src: 'https://picsum.photos/id/870/324/432',
+      },
+      {
+        alt: 'Portrait 9:16',
+        aspectRatio: '9:16',
+        src: 'https://picsum.photos/id/870/243/432',
+      },
+    ],
+  },
+  decorators: [
+    (Story) => (
+      <div style={{ maxInlineSize: '15.1875rem' }}>
+        <Story />
+      </div>
+    ),
+  ],
+}
