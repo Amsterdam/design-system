3d1884cbec563bab9d728328a99b01d693ec2040
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-09-09 12:03:26 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@9830854bcd0075f537c1a0901cb64f58bd8f4bae 🚀

---
+++ demo-npm_and_yarn/patch-and-minor-dev-dependencies-617ac47e75/assets/ProductPage.docs-Dlottgwo.js
@@ -0,0 +1,6 @@
+import{j as t}from"./iframe-BJHrSXXD.js";import{useMDXComponents as e}from"./index-CKGBfpuL.js";import{M as i}from"./blocks-BNNhPOGk.js";import{P as s}from"./ProductPage.stories-XT7UOuL4.js";import"./index-CcB_86BV.js";import"./config-BAigelw-.js";import"./PageFooter.stories-CuKNXGkk.js";/*@license CC0-1.0*/function r(o){const n={h1:"h1",p:"p",...e(),...o.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(i,{of:s}),`
+`,t.jsx(n.h1,{id:"product-page",children:"Product page"}),`
+`,t.jsx(n.p,{children:`Lists all information about a product or service of the City.
+Often includes a Call to Action Link that directs the user to a form.`})]})}function h(o={}){const{wrapper:n}={...e(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(r,{...o})}):r(o)}export{h as default};
