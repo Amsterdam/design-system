cf6b758aa58cfe6831a2d9444818fd40fd1db992
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-09-10 11:25:10 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@b23c9de495a907a04d63491dbe6265b40a15f138 🚀

---
+++ demo-DES-1408-typography-v2/assets/FormPage.docs-D397TizO.js
@@ -0,0 +1,5 @@
+import{j as o}from"./iframe-CId4-51g.js";import{useMDXComponents as r}from"./index-Q7u4bX0P.js";import{M as s}from"./blocks-K0MEtH21.js";import{F as m}from"./FormPage.stories-uD4UjIuN.js";import"./index-d6dE_J9B.js";import"./config-B9rch9TE.js";import"./PageFooter.stories-DFrdwj35.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
+`,`
+`,o.jsx(s,{of:m}),`
+`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
+`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function d(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{d as default};
---
+++ demo-DES-1408-typography-v2/assets/ProductPage.docs-BSuu_Bnp.js
@@ -0,0 +1,16 @@
+import{j as n}from"./iframe-CId4-51g.js";import{useMDXComponents as o}from"./index-Q7u4bX0P.js";import{M as r}from"./blocks-K0MEtH21.js";import{P as s}from"./ProductPage.stories-D6GVr2mC.js";import"./index-d6dE_J9B.js";import"./config-B9rch9TE.js";import"./PageFooter.stories-DFrdwj35.js";/*@license CC0-1.0*/function i(t){const e={h1:"h1",h2:"h2",li:"li",p:"p",ul:"ul",...o(),...t.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(r,{of:s}),`
+`,n.jsx(e.h1,{id:"product-page",children:"Product page"}),`
+`,n.jsx(e.p,{children:`Lists all information about a product or service of the City.
+Often includes a Call to Action Link that directs the user to a form.`}),`
+`,n.jsx(e.h2,{id:"headings",children:"Headings"}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsx(e.li,{children:"Use a level 1 Heading for the title of the page."}),`
+`,n.jsx(e.li,{children:"Use level 2 Headings for the section titles."}),`
+`]}),`
+`,n.jsx(e.h2,{id:"spacing",children:"Spacing"}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsx(e.li,{children:"Add a small margin bottom to a Heading 2 with following body text."}),`
+`,n.jsx(e.li,{children:"Use an extra large margin between sections and before the Call to Action Link."}),`
+`]})]})}function p(t={}){const{wrapper:e}={...o(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(i,{...t})}):i(t)}export{p as default};
