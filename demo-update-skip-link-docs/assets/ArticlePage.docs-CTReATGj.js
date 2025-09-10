e9ef2cd94b118c16bd93e642496c85aaa8662b33
alimpens <37216945+alimpens@users.noreply.github.com>
2025-09-10 11:21:38 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@d0e170cc0d7d5a7a5b102dba565706a3bbfa0caf 🚀

--- demo-update-skip-link-docs/assets/ArticlePage.docs-CTReATGj.js
+++
@@ -1,5 +0,0 @@
-import{j as t}from"./iframe-CBZE33UZ.js";import{useMDXComponents as e}from"./index-Cn7i2XGF.js";import{M as i}from"./blocks-spb3D45S.js";import{A as s}from"./ArticlePage.stories-DjJjHgDz.js";import"./index-CyKLgi7G.js";import"./common-DrnyCu3t.js";import"./HomeNews-DETOqVpF.js";import"./config-BnfbchBl.js";import"./PageFooter.stories-8sYZK9oE.js";/*@license CC0-1.0*/function r(n){const o={h1:"h1",p:"p",...e(),...n.components};return t.jsxs(t.Fragment,{children:[`
-`,`
-`,t.jsx(i,{of:s}),`
-`,t.jsx(o.h1,{id:"article-page",children:"Article page"}),`
-`,t.jsx(o.p,{children:"This page type is for news or similar kinds of articles."})]})}function j(n={}){const{wrapper:o}={...e(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(r,{...n})}):r(n)}export{j as default};
--- demo-update-skip-link-docs/assets/ProductPage.docs-B6k-N5QB.js
+++
@@ -1,6 +0,0 @@
-import{j as t}from"./iframe-CBZE33UZ.js";import{useMDXComponents as e}from"./index-Cn7i2XGF.js";import{M as i}from"./blocks-spb3D45S.js";import{P as s}from"./ProductPage.stories-BphX6uSv.js";import"./index-CyKLgi7G.js";import"./config-BnfbchBl.js";import"./PageFooter.stories-8sYZK9oE.js";/*@license CC0-1.0*/function r(o){const n={h1:"h1",p:"p",...e(),...o.components};return t.jsxs(t.Fragment,{children:[`
-`,`
-`,t.jsx(i,{of:s}),`
-`,t.jsx(n.h1,{id:"product-page",children:"Product page"}),`
-`,t.jsx(n.p,{children:`Lists all information about a product or service of the City.
-Often includes a Call to Action Link that directs the user to a form.`})]})}function h(o={}){const{wrapper:n}={...e(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(r,{...o})}):r(o)}export{h as default};
