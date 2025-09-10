a34e5fee8faa1a3b68279120a3f1902ec8014a6d
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-09-10 12:00:25 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@126926c73674ad69152513638cdff3eded337321 🚀

---
+++ demo-develop/assets/NavigationPage.docs-DPQ5aezt.js
@@ -0,0 +1,6 @@
+import{j as t}from"./iframe-BKNTAGVH.js";import{useMDXComponents as i}from"./index-BN8x8W-j.js";import{M as s}from"./blocks-DKW30nUE.js";import{N as r}from"./NavigationPage.stories-ertzm0M_.js";import"./index-CPcPHaG4.js";import"./exampleContent-B7cDFRSs.js";import"./config-CsD4IvkG.js";import"./PageFooter.stories-CcM2duY7.js";/*@license CC0-1.0*/function e(n){const o={h1:"h1",p:"p",...i(),...n.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(s,{of:r}),`
+`,t.jsx(o.h1,{id:"navigation-page",children:"Navigation page"}),`
+`,t.jsx(o.p,{children:`A navigation page does not have much content of its own.
+Instead, it presents groups of links to various pages related to a specific subject.`})]})}function x(n={}){const{wrapper:o}={...i(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(e,{...n})}):e(n)}export{x as default};
