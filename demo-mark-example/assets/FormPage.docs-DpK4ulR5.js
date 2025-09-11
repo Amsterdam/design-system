8f76d9ce43ede3abd8d47eecf71b8b93d65138e5
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-09-11 09:14:31 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@658a73c3a08fe304f0f25c5b327c8aee54000a3c 🚀

---
+++ demo-mark-example/assets/FormPage.docs-DpK4ulR5.js
@@ -0,0 +1,5 @@
+import{j as o}from"./iframe-CjDxN6tp.js";import{useMDXComponents as r}from"./index-B-itF01Y.js";import{M as s}from"./blocks-D_NE7UXk.js";import{F as m}from"./FormPage.stories-LEBymJTe.js";import"./index-DPSQ4Wwa.js";import"./config-BAglYIov.js";import"./PageFooter.stories-Cc9MwKUP.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
+`,`
+`,o.jsx(s,{of:m}),`
+`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
+`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function d(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{d as default};
---
+++ demo-mark-example/assets/ProductPage.docs-CcM3DNXK.js
@@ -0,0 +1,6 @@
+import{j as t}from"./iframe-CjDxN6tp.js";import{useMDXComponents as e}from"./index-B-itF01Y.js";import{M as i}from"./blocks-D_NE7UXk.js";import{P as s}from"./ProductPage.stories-F_YIY55g.js";import"./index-DPSQ4Wwa.js";import"./config-BAglYIov.js";import"./PageFooter.stories-Cc9MwKUP.js";/*@license CC0-1.0*/function r(o){const n={h1:"h1",p:"p",...e(),...o.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(i,{of:s}),`
+`,t.jsx(n.h1,{id:"product-page",children:"Product page"}),`
+`,t.jsx(n.p,{children:`Lists all information about a product or service of the City.
+Often includes a Call to Action Link that directs the user to a form.`})]})}function h(o={}){const{wrapper:n}={...e(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(r,{...o})}):r(o)}export{h as default};
