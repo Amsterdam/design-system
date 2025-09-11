6f3cd1d11d76c2af69471febd60b4590c00f1121
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-09-11 08:14:42 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@f7e0211e311e63c50d6f8e7ad16736c75f7e8a7a 🚀

--- demo-DES-1408-typography-v2/assets/FormPage.docs-Dc5aZYsZ.js
+++
@@ -1,5 +0,0 @@
-import{j as o}from"./iframe-DEttnp1B.js";import{useMDXComponents as r}from"./index-XtEN9_v2.js";import{M as s}from"./blocks-DpzhrXx7.js";import{F as m}from"./FormPage.stories-CzxWygdA.js";import"./index-FALf7j_h.js";import"./config-1TS3Xi9o.js";import"./PageFooter.stories-DCQBVR0O.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
-`,`
-`,o.jsx(s,{of:m}),`
-`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
-`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function d(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{d as default};
---
+++ demo-DES-1408-typography-v2/assets/NavigationPage.docs-Bgs2cM6I.js
@@ -0,0 +1,17 @@
+import{j as n}from"./iframe-Cn3BpJHX.js";import{useMDXComponents as a}from"./index-Su2q6Dtm.js";import{M as s}from"./blocks-7tSu9Yjs.js";import{N as o}from"./NavigationPage.stories-XjXKgLZQ.js";import"./index-Cc2ZD2pC.js";import"./exampleContent-DRope23M.js";import"./config-D_ffedEn.js";import"./PageFooter.stories-CqFtOJ6O.js";/*@license CC0-1.0*/function t(i){const e={h1:"h1",h2:"h2",li:"li",p:"p",ul:"ul",...a(),...i.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(s,{of:o}),`
+`,n.jsx(e.h1,{id:"navigation-page",children:"Navigation page"}),`
+`,n.jsx(e.p,{children:`A navigation page does not have much content of its own.
+Instead, it presents groups of links to various pages related to a specific subject.`}),`
+`,n.jsx(e.h2,{id:"headings",children:"Headings"}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsx(e.li,{children:"Use a level 1 Heading for the page title."}),`
+`,n.jsx(e.li,{children:`All Card Headings and Headings in compositions with a Paragraph and a Standalone Link or a Link List get size 3.
+This is already the default for Card Headings.
+Give them level 2 and size 3 if they follow the page title directly.`}),`
+`]}),`
+`,n.jsx(e.h2,{id:"spacing",children:"Spacing"}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsx(e.li,{children:"Grids on this page type use the default x-large vertical gap."}),`
+`]})]})}function u(i={}){const{wrapper:e}={...a(),...i.components};return e?n.jsx(e,{...i,children:n.jsx(t,{...i})}):t(i)}export{u as default};
