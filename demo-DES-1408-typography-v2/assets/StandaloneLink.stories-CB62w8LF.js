8e3c41cbb5eb015bef41537b493ea7f9c24ff8fd
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-09-12 10:12:42 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@8688ee579a9949d811837eb23e9f22aac4b5c96a 🚀

---
+++ demo-DES-1408-typography-v2/assets/StandaloneLink.stories-CB62w8LF.js
@@ -0,0 +1,36 @@
+import{r as h,j as e,c as g,D as k,I as d,H as v,P as w,q as f}from"./iframe-BmAP_liR.js";import{I as S}from"./Icon-L6drKG-N.js";const i=h.forwardRef(({children:a,className:l,color:c,icon:m=k,...p},u)=>e.jsxs("a",{...p,className:g("ams-standalone-link ams-standalone-link--with-icon",c&&`ams-standalone-link--${c}`,l),ref:u,children:[e.jsx(S,{svg:m}),a]}));i.displayName="StandaloneLink";try{i.displayName="StandaloneLink",i.__docgenInfo={description:"",displayName:"StandaloneLink",props:{color:{defaultValue:null,description:"Changes the text colour for readability on a light or dark background.",name:"color",required:!1,type:{name:"enum",value:[{value:'"inverse"'},{value:'"contrast"'}]}},icon:{defaultValue:{value:"ChevronForwardIcon"},description:"The icon to show before the link text.",name:"icon",required:!1,type:{name:"Function | ReactNode"}}}}}catch{}const b={title:"Components/Navigation/Standalone Link",component:i,args:{children:"Bekijk alle onderwerpen",href:"#"},argTypes:{children:{description:"The link text.",table:{disable:!1}},color:{control:{labels:{undefined:"default"},type:"radio"},options:[void 0,"contrast","inverse"]},href:{description:"The url for the link.",name:"href",type:{name:"string",required:!1}},icon:{control:{labels:{undefined:"none"},type:"select"},mapping:d,options:[void 0,...Object.keys(d)],table:{defaultValue:{summary:"ChevronForwardIcon"}}}}},r={},n={args:{color:"contrast"}},o={args:{color:"inverse"}},s={args:{children:"Bekijk alle werkzaamheden"},decorators:[a=>e.jsx("article",{style:{maxWidth:"32rem"},children:e.jsx(a,{})})],render:({children:a,...l})=>e.jsxs("article",{children:[e.jsx(v,{className:"ams-mb-s",level:2,size:"level-3",children:"Werkzaamheden"}),e.jsx(w,{className:"ams-mb-m",children:"Lees waar en wanneer we werken aan nieuwbouw, groot onderhoud, herinrichting van straten en wegen, aanpak van parken of ontwikkeling van hele gebieden."}),e.jsx(i,{...l,children:a})]})},t={args:{children:"Downloaden",icon:e.jsx(f,{})}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:"{}",...r.parameters?.docs?.source}}};n.parameters={...n.parameters,docs:{...n.parameters?.docs,source:{originalSource:`{
+  args: {
+    color: 'contrast'
+  }
+}`,...n.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    color: 'inverse'
+  }
+}`,...o.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Bekijk alle werkzaamheden'
+  },
+  decorators: [Story => <article style={{
+    maxWidth: '32rem'
+  }}>
+        <Story />
+      </article>],
+  render: ({
+    children,
+    ...args
+  }) => <article>
+      <Heading className="ams-mb-s" level={2} size="level-3">
+        Werkzaamheden
+      </Heading>
+      <Paragraph className="ams-mb-m">
+        Lees waar en wanneer we werken aan nieuwbouw, groot onderhoud, herinrichting van straten en wegen, aanpak van
+        parken of ontwikkeling van hele gebieden.
+      </Paragraph>
+      <StandaloneLink {...args}>{children}</StandaloneLink>
+    </article>
+}`,...s.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Downloaden',
+    icon: <DownloadIcon />
+  }
+}`,...t.parameters?.docs?.source}}};const x=["Default","ContrastColour","InverseColour","WithHeadingAndParagraph","WithCustomIcon"],C=Object.freeze(Object.defineProperty({__proto__:null,ContrastColour:n,Default:r,InverseColour:o,WithCustomIcon:t,WithHeadingAndParagraph:s,__namedExportsOrder:x,default:b},Symbol.toStringTag,{value:"Module"}));export{n as C,o as I,C as S,s as W,t as a};
---
+++ demo-DES-1408-typography-v2/assets/TableOfContents.docs-BvDLq0U-.js
@@ -0,0 +1,15 @@
+import{j as n}from"./iframe-BmAP_liR.js";import{useMDXComponents as o}from"./index-HYf8-jAT.js";import{M as r,c as i,P as a,e as l,f as m}from"./blocks-BJPrNGmz.js";import{T as c,M as p}from"./TableOfContents.stories-CGd22RaS.js";import"./index-DPIOVFoV.js";import"./Heading-8Z3l2B9t.js";import"./getHeadingTag-C4Rztoc8.js";const x=`<!-- @license CC0-1.0 -->
+
+# Table of Contents
+
+A list of links corresponding to the content sections on the page.
+It helps users to easily navigate to different sections on the same page.
+`;/*@license CC0-1.0*/function s(e){const t={h2:"h2",h3:"h3",...o(),...e.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(r,{of:c}),`
+`,n.jsx(i,{children:x}),`
+`,n.jsx(a,{}),`
+`,n.jsx(l,{}),`
+`,n.jsx(t.h2,{id:"examples",children:"Examples"}),`
+`,n.jsx(t.h3,{id:"multiple-levels",children:"Multiple levels"}),`
+`,n.jsx(m,{of:p})]})}function g(e={}){const{wrapper:t}={...o(),...e.components};return t?n.jsx(t,{...e,children:n.jsx(s,{...e})}):s(e)}export{g as default};
