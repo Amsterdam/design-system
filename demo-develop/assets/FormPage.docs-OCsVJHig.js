a76745f6614f97c75536efacb239660db2b62e4a
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-09-12 15:20:10 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@73022649371608d39c5c8e95ecc90e09d3f0f71d 🚀

---
+++ demo-develop/assets/FormPage.docs-OCsVJHig.js
@@ -0,0 +1,5 @@
+import{j as o}from"./iframe-DJddtUqu.js";import{useMDXComponents as r}from"./index-CpIlGrVi.js";import{M as s}from"./blocks-BzroRw0p.js";import{F as m}from"./FormPage.stories-2c8dUY28.js";import"./index-9MMVeL9n.js";import"./config-BQ06s-6u.js";import"./PageFooter.stories-CR6aaNqB.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
+`,`
+`,o.jsx(s,{of:m}),`
+`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
+`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function d(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{d as default};
---
+++ demo-develop/assets/PageHeading.docs-BWSxJZ45.js
@@ -0,0 +1,9 @@
+import{j as e}from"./iframe-DJddtUqu.js";import{useMDXComponents as s}from"./index-CpIlGrVi.js";import{M as r,c as a,P as i}from"./blocks-BzroRw0p.js";import{P as m}from"./PageHeading.stories-DswRUSWr.js";import{S as d}from"./StatusBadge-DvjsAsV5.js";import"./index-9MMVeL9n.js";import"./Badge-BMziluc8.js";const c=`<!-- @license CC0-1.0 -->
+
+# Page Heading
+`;/*@license CC0-1.0*/function o(n){return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(d,{description:"We no longer use this size of headings. Use the Heading component with level 1 instead.",status:"deprecated"}),`
+`,e.jsx(r,{of:m}),`
+`,e.jsx(a,{children:c}),`
+`,e.jsx(i,{})]})}function M(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{M as default};
