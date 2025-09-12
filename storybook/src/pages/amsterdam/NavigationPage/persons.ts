4032baa4a33488f14fa4ca43d4498396d99290d3
Vincent Smedinga <v.smedinga@amsterdam.nl>
2025-09-12 14:04:57 +0200

    Show the same mock image for all B&W persons

--- storybook/src/pages/amsterdam/NavigationPage/persons.ts
+++ storybook/src/pages/amsterdam/NavigationPage/persons.ts
@@ -7,57 +7,57 @@ type Person = {
 
 export const persons: Person[] = [
   {
-    imageSource: 'https://picsum.photos/480/270?random=1',
+    imageSource: 'https://picsum.photos/480/270',
     name: 'Femke Halsema',
     role: 'Burgemeester',
   },
   {
-    imageSource: 'https://picsum.photos/480/270?random=2',
+    imageSource: 'https://picsum.photos/480/270',
     name: 'Marjolein Moorman',
     role: 'Wethouder',
   },
   {
-    imageSource: 'https://picsum.photos/480/270?random=1',
+    imageSource: 'https://picsum.photos/480/270',
     name: 'Rutger Groot Wassink',
     role: 'Wethouder',
   },
   {
-    imageSource: 'https://picsum.photos/480/270?random=2',
+    imageSource: 'https://picsum.photos/480/270',
     name: 'Hester van Buren',
     role: 'Wethouder',
   },
   {
-    imageSource: 'https://picsum.photos/480/270?random=1',
+    imageSource: 'https://picsum.photos/480/270',
     name: 'Sofyan Mbarki',
     role: 'Wethouder',
   },
   {
-    imageSource: 'https://picsum.photos/480/270?random=2',
+    imageSource: 'https://picsum.photos/480/270',
     name: 'Touria Meliani',
     role: 'Wethouder',
   },
   {
-    imageSource: 'https://picsum.photos/480/270?random=1',
+    imageSource: 'https://picsum.photos/480/270',
     name: 'Melanie van der Horst',
     role: 'Wethouder',
   },
   {
-    imageSource: 'https://picsum.photos/480/270?random=2',
+    imageSource: 'https://picsum.photos/480/270',
     name: 'Alexander Scholtes',
     role: 'Wethouder',
   },
   {
-    imageSource: 'https://picsum.photos/480/270?random=1',
+    imageSource: 'https://picsum.photos/480/270',
     name: 'Zita Pels',
     role: 'Wethouder',
   },
   {
-    imageSource: 'https://picsum.photos/480/270?random=2',
+    imageSource: 'https://picsum.photos/480/270',
     name: 'Steven van Weyenberg',
     role: 'Wethouder',
   },
   {
-    imageSource: 'https://picsum.photos/480/270?random=1',
+    imageSource: 'https://picsum.photos/480/270',
     name: 'Thea de Vries',
     role: 'Gemeentesecretaris',
     suffix: 'waarnemend',
