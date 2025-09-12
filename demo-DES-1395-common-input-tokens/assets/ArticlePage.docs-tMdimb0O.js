e41526a48c84a3684f2fc0854da658db9b7e519d
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-09-12 13:28:32 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@c47bac4f1fea0f38f7b08eeb02d49bc8eeab7745 🚀

---
+++ demo-DES-1395-common-input-tokens/assets/ArticlePage.docs-tMdimb0O.js
@@ -0,0 +1,5 @@
+import{j as t}from"./iframe-QAyKjEFB.js";import{useMDXComponents as e}from"./index-D8z7et2C.js";import{M as i}from"./blocks-rD9biC5E.js";import{A as s}from"./ArticlePage.stories-CxDUJQtB.js";import"./index-BlbhRtRB.js";import"./common-DrnyCu3t.js";import"./HomeNews-RdksJ2DH.js";import"./config-D7zW0c1m.js";import"./PageFooter.stories-DWDALFb6.js";/*@license CC0-1.0*/function r(n){const o={h1:"h1",p:"p",...e(),...n.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(i,{of:s}),`
+`,t.jsx(o.h1,{id:"article-page",children:"Article page"}),`
+`,t.jsx(o.p,{children:"This page type is for news or similar kinds of articles."})]})}function j(n={}){const{wrapper:o}={...e(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(r,{...n})}):r(n)}export{j as default};
--- demo-DES-1395-common-input-tokens/assets/NavigationPage.docs-HwferWCS.js
+++
@@ -1,6 +0,0 @@
-import{j as t}from"./iframe-DFBRHsYq.js";import{useMDXComponents as i}from"./index-DTioAP7t.js";import{M as s}from"./blocks-Dt8KGD3k.js";import{N as r}from"./NavigationPage.stories-DA2VDtPe.js";import"./index-UDFK2474.js";import"./exampleContent-B7cDFRSs.js";import"./config-DL2tmA4j.js";import"./PageFooter.stories-Bi2pJ-y-.js";/*@license CC0-1.0*/function e(n){const o={h1:"h1",p:"p",...i(),...n.components};return t.jsxs(t.Fragment,{children:[`
-`,`
-`,t.jsx(s,{of:r}),`
-`,t.jsx(o.h1,{id:"navigation-page",children:"Navigation page"}),`
-`,t.jsx(o.p,{children:`A navigation page does not have much content of its own.
-Instead, it presents groups of links to various pages related to a specific subject.`})]})}function x(n={}){const{wrapper:o}={...i(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(e,{...n})}):e(n)}export{x as default};
