975e67befdb1c696b2d10026f73b60bf7b651bfb
alimpens <37216945+alimpens@users.noreply.github.com>
2025-09-12 15:12:19 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@4f2e5deee33ee5165f021446712b008b2a86a07e 🚀

---
+++ demo-DES-1273-use-lint-rules-1/assets/PageFooter.docs-B-rDuxb7.js
@@ -0,0 +1,36 @@
+import{j as e}from"./iframe-DJddtUqu.js";import{useMDXComponents as s}from"./index-CpIlGrVi.js";import{M as i,c as a,P as r,f as c}from"./blocks-BzroRw0p.js";import{P as l,O as d}from"./PageFooter.stories-CR6aaNqB.js";import"./index-9MMVeL9n.js";const h=`<!-- @license CC0-1.0 -->
+
+# Page Footer
+
+Provides service information at the bottom of every page.
+
+## Guidelines
+
+- The Page Footer must be used on all websites and applications for the City of Amsterdam.
+- It must be the same on every page.
+- It usually consists of two sections: a full-width blue \`PageFooter.Spotlight\` and a \`PageFooter.Menu\` below it.
+- A Page Footer containing only a \`PageFooter.Menu\` can be sufficient for applications.
+- Set [the inverse colour](?path=/docs/brand-design-tokens-colour--docs#pairing-foreground-with-background-colours) on all text and links in the Spotlight area.
+- If the Page Footer contains [Headings](https://designsystem.amsterdam/?path=/docs/components-text-heading--docs), give them level 2 and a size of ‘level-3’.
+  A visually hidden heading to group them is not necessary – the footer’s landmark provides enough guidance.
+
+## Content
+
+The top section offers space for various practical links:
+
+1. The first column focuses on contact information.
+   The information is as specific as possible and tailored to the respective website or page (e.g., including a chat option if available).
+1. The second column contains links to relevant (online) sites or sources.
+1. The third column refers to newsletters, events, social media, etc.
+
+The menu is for links to privacy policies, cookie statements, and other information about the website itself.
+`;/*@license CC0-1.0*/function o(n){const t={h2:"h2",h3:"h3",p:"p",...s(),...n.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(i,{of:l}),`
+`,e.jsx(a,{children:h}),`
+`,e.jsx(r,{}),`
+`,e.jsx(t.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(t.h3,{id:"onderzoek-en-statistiek",children:"Onderzoek en Statistiek"}),`
+`,e.jsx(t.p,{children:`The Page Footer for a specific website can be a bit different.
+Here’s an example for the ‘Onderzoek en Statistiek’ department.`}),`
+`,e.jsx(c,{of:d})]})}function x(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o(n)}export{x as default};
