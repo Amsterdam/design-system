e39b1dda4e93c7f18745630f55c558d46117d427
dlnr <niels@dlnr.dev>
2025-09-12 13:15:05 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@beb28fe257c1577e391131696661449e9de81042 🚀

---
+++ demo-DES-1441-new-icons/assets/ProductPage.docs-BND-O6Dr.js
@@ -0,0 +1,6 @@
+import{j as t}from"./iframe-BMkNnuEk.js";import{useMDXComponents as e}from"./index-YoKFotoX.js";import{M as i}from"./blocks-CqBKKePF.js";import{P as s}from"./ProductPage.stories-0h35i1kn.js";import"./index-DQoG71aT.js";import"./config-CXngyIs8.js";import"./PageFooter.stories-BlLXgE4M.js";/*@license CC0-1.0*/function r(o){const n={h1:"h1",p:"p",...e(),...o.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(i,{of:s}),`
+`,t.jsx(n.h1,{id:"product-page",children:"Product page"}),`
+`,t.jsx(n.p,{children:`Lists all information about a product or service of the City.
+Often includes a Call to Action Link that directs the user to a form.`})]})}function h(o={}){const{wrapper:n}={...e(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(r,{...o})}):r(o)}export{h as default};
