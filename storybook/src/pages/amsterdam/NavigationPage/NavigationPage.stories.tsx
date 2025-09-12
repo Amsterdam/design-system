47b50579a06c14152c61918fcf11d75c21d09451
Vincent Smedinga <v.smedinga@amsterdam.nl>
2025-09-12 14:05:13 +0200

    Add space before suffix

--- storybook/src/pages/amsterdam/NavigationPage/NavigationPage.stories.tsx
+++ storybook/src/pages/amsterdam/NavigationPage/NavigationPage.stories.tsx
@@ -162,8 +162,7 @@ export const WithImageGallery: StoryObj = {
                 <Card.Image alt="" src={imageSource} />
                 <Card.Heading level={3}>
                   <Card.Link href="#">
-                    {role} {name}
-                    {suffix && `(${suffix})`}
+                    {role} {name} {suffix && ` (${suffix})`}
                   </Card.Link>
                 </Card.Heading>
               </Card>
