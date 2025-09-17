35fa05fa590dff8877b8710a04446b3c4a1f18dd
alimpens <37216945+alimpens@users.noreply.github.com>
2025-09-17 07:58:53 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@10346793f0c5891cc080ade4433939937a08edfb 🚀

---
+++ demo-develop/assets/AspectRatio.docs-D6g-bJ-V.js
@@ -0,0 +1,31 @@
+import{j as a}from"./iframe-CdZ3I5Tz.js";import{useMDXComponents as n}from"./index-LNHA16LN.js";import{M as o,c as i,P as c,e as m}from"./blocks-CV1XZCqI.js";import{A as r}from"./AspectRatio.stories-B_P4zatj.js";import"./index-9AYw3VsT.js";import"./types-yiKVj219.js";import"./generateAspectRatioClass-CefTNpDa.js";const p=`<!-- @license CC0-1.0 -->
+
+# Aspect Ratio
+
+Constrains media content to a supported aspect ratio.
+
+## Class names
+
+Each available aspect ratio has an associated class name.
+The class can be applied to any component or element.
+
+| Class name              | Example                                                                 |
+| :---------------------- | :---------------------------------------------------------------------- |
+| \`ams-aspect-ratio-9-16\` | <div className="ams-docs-token-example--space ams-aspect-ratio-9-16" /> |
+| \`ams-aspect-ratio-3-4\`  | <div className="ams-docs-token-example--space ams-aspect-ratio-3-4" />  |
+| \`ams-aspect-ratio-1-1\`  | <div className="ams-docs-token-example--space ams-aspect-ratio-1-1" />  |
+| \`ams-aspect-ratio-4-3\`  | <div className="ams-docs-token-example--space ams-aspect-ratio-4-3" />  |
+| \`ams-aspect-ratio-16-9\` | <div className="ams-docs-token-example--space ams-aspect-ratio-16-9" /> |
+| \`ams-aspect-ratio-16-5\` | <div className="ams-docs-token-example--space ams-aspect-ratio-16-5" /> |
+
+## Guidelines
+
+- Apply one of these classes to an image, video or other media content to constrain its dimensions to one of the available aspect ratios.
+- The default aspect ratio is 16:9.
+- When using \`width\` and \`height\` attributes on an image (for example, when using the [Next Image component](https://nextjs.org/docs/app/api-reference/components/image)), the aspect ratio is determined by these values and will take precedence over the class. To correct this, use the class \`ams-image\`.
+`;/*@license CC0-1.0*/function t(e){return a.jsxs(a.Fragment,{children:[`
+`,`
+`,a.jsx(o,{of:r}),`
+`,a.jsx(i,{children:p}),`
+`,a.jsx(c,{}),`
+`,a.jsx(m,{})]})}function g(e={}){const{wrapper:s}={...n(),...e.components};return s?a.jsx(s,{...e,children:a.jsx(t,{...e})}):t()}export{g as default};
