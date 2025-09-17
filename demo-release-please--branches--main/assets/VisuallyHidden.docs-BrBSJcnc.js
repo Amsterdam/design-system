aebbcd2502ad7b7e418e9c700e093169e3381292
alimpens <37216945+alimpens@users.noreply.github.com>
2025-09-17 10:51:29 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@764859bf3612ff772effafc42f4e0b9fc34b2fb4 🚀

---
+++ demo-release-please--branches--main/assets/VisuallyHidden.docs-BrBSJcnc.js
@@ -0,0 +1,20 @@
+import{j as n}from"./iframe-B-TrPROC.js";import{useMDXComponents as o}from"./index-CUzwrI5V.js";import{M as t,c as r,P as i,e as l}from"./blocks-sUguUKvS.js";import{V as u}from"./VisuallyHidden.stories-DMaXLxhg.js";import"./index-CKKH14bR.js";const c=`<!-- @license CC0-1.0 -->
+
+# Visually Hidden
+
+Hides content from sighted users but keeps it accessible to non-visual user agents, such as screen readers.
+
+## Class name
+
+\`ams-visually-hidden\`
+
+## Guidelines
+
+- In most cases, visually available content should be accessible to non-visual user agents and vice versa.
+  Elaborate instructions or guidance read only by non-visual user agents can do more harm than good.
+`;/*@license CC0-1.0*/function a(s){return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(t,{of:u}),`
+`,n.jsx(r,{children:c}),`
+`,n.jsx(i,{}),`
+`,n.jsx(l,{})]})}function h(s={}){const{wrapper:e}={...o(),...s.components};return e?n.jsx(e,{...s,children:n.jsx(a,{...s})}):a()}export{h as default};
