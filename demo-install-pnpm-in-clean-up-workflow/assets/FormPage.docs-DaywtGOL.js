a81ac7fd4cde55d862a6f944fcfc128e93607818
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-09-17 08:18:53 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@3436da9bce3b357edaab9361f9821a730d1040ec 🚀

--- demo-install-pnpm-in-clean-up-workflow/assets/FormPage.docs-DaywtGOL.js
+++
@@ -1,5 +0,0 @@
-import{j as o}from"./iframe-2suj_liG.js";import{useMDXComponents as r}from"./index-BxV2Pb9W.js";import{M as s}from"./blocks-ckiYAgD3.js";import{F as m}from"./FormPage.stories-DbYaczya.js";import"./index-CnzSwb1T.js";import"./config-BJFZvpyu.js";import"./PageFooter.stories-B-4TWcQp.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
-`,`
-`,o.jsx(s,{of:m}),`
-`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
-`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function d(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{d as default};
---
+++ demo-install-pnpm-in-clean-up-workflow/assets/PageHeading.docs-DtZloHy7.js
@@ -0,0 +1,9 @@
+import{j as e}from"./iframe-CgzEtzEu.js";import{useMDXComponents as s}from"./index-C6jcAEx8.js";import{M as r,c as a,P as i}from"./blocks-o7jYaz_B.js";import{P as m}from"./PageHeading.stories-CLO-CaWB.js";import{S as d}from"./StatusBadge-Cs23ptNc.js";import"./index-Dx3dm0Ok.js";import"./Badge-DWytC_WY.js";const c=`<!-- @license CC0-1.0 -->
+
+# Page Heading
+`;/*@license CC0-1.0*/function o(n){return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(d,{description:"We no longer use this size of headings. Use the Heading component with level 1 instead.",status:"deprecated"}),`
+`,e.jsx(r,{of:m}),`
+`,e.jsx(a,{children:c}),`
+`,e.jsx(i,{})]})}function M(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{M as default};
