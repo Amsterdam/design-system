a8e5d087c97f98280cace76164065c86405b3180
alimpens <37216945+alimpens@users.noreply.github.com>
2025-09-19 13:52:20 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@3923b135a28ec520607509a4586f2b378412a032 🚀

---
+++ demo-DES-1358-add-form-page-examples/assets/config-smWMm1TE.js
@@ -0,0 +1,1 @@
+import{j as e}from"./iframe-C20t_Pl0.js";import{L as r}from"./Layout-Bc982z24.js";const a={decorators:[o=>e.jsx(r,{children:e.jsx(o,{})})],parameters:{layout:"fullscreen",themes:{themeOverride:"Spacious"}}};export{a as c};
