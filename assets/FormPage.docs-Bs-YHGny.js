c406734e9d65ab2cf4deb7dafdb91a2467be3994
VincentSmedinga <VincentSmedinga@users.noreply.github.com>
2025-09-19 12:01:18 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@acdd7d5dddad8aefb3943b25a2b4a855449a8d71 🚀

---
+++ assets/FormPage.docs-Bs-YHGny.js
@@ -0,0 +1,5 @@
+import{j as o}from"./iframe-DGJV8Deg.js";import{useMDXComponents as r}from"./index-BKSY53VT.js";import{M as s}from"./blocks-BmaE-R7T.js";import{F as m}from"./FormPage.stories-DaXt3Lzo.js";import"./index-CQMEboiA.js";import"./config-g4yTuezq.js";import"./PageFooter.stories-DNpFlgbA.js";import"./index.esm-C63mO3Hf.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
+`,`
+`,o.jsx(s,{of:m}),`
+`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
+`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function h(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{h as default};
