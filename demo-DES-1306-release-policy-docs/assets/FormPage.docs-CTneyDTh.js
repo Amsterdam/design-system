409025bdd99b86aae7996a2e139a27b262ed22a8
alimpens <37216945+alimpens@users.noreply.github.com>
2025-09-19 13:48:29 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@0fda29e5d66a1465561e1041377215ae6b6310f9 🚀

--- demo-DES-1306-release-policy-docs/assets/FormPage.docs-CTneyDTh.js
+++
@@ -1,5 +0,0 @@
-import{j as o}from"./iframe-pdkgNiUz.js";import{useMDXComponents as r}from"./index-CjzelccA.js";import{M as s}from"./blocks-DxaK3LB0.js";import{F as m}from"./FormPage.stories-BbNl6dKd.js";import"./index-Byn_89gb.js";import"./config-CD4kcJcw.js";import"./PageFooter.stories-Dcu1DYBI.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
-`,`
-`,o.jsx(s,{of:m}),`
-`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
-`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function d(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{d as default};
