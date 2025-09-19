8b4dd128760d62f899c64da0acd230a8c98570f9
alimpens <37216945+alimpens@users.noreply.github.com>
2025-09-19 11:22:57 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@479c39edcf6b5a0709899a362b37090d1a5ad188 🚀

---
+++ demo-update-react-icons-dependency/assets/PageHeading.docs-BDgEnnmI.js
@@ -0,0 +1,9 @@
+import{j as e}from"./iframe-bwsJbePA.js";import{useMDXComponents as s}from"./index-DHQBGGhP.js";import{M as r,c as a,P as i}from"./blocks-D9EFOGDH.js";import{P as m}from"./PageHeading.stories-DtS9Mdr2.js";import{S as d}from"./StatusBadge-CF9YYe7L.js";import"./index-EAOXC7md.js";import"./Badge-BRExYfYA.js";const c=`<!-- @license CC0-1.0 -->
+
+# Page Heading
+`;/*@license CC0-1.0*/function o(n){return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(d,{description:"We no longer use this size of headings. Use the Heading component with level 1 instead.",status:"deprecated"}),`
+`,e.jsx(r,{of:m}),`
+`,e.jsx(a,{children:c}),`
+`,e.jsx(i,{})]})}function M(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{M as default};
---
+++ demo-update-react-icons-dependency/assets/ProductPage.docs-C_5vzuo0.js
@@ -0,0 +1,16 @@
+import{j as n}from"./iframe-bwsJbePA.js";import{useMDXComponents as o}from"./index-DHQBGGhP.js";import{M as r}from"./blocks-D9EFOGDH.js";import{P as s}from"./ProductPage.stories-ClN9UTxs.js";import"./index-EAOXC7md.js";import"./config-Bmhmhcoc.js";import"./PageFooter.stories-BZPI_6UZ.js";import"./index.esm-WIT4qREJ.js";/*@license CC0-1.0*/function i(t){const e={h1:"h1",h2:"h2",li:"li",p:"p",ul:"ul",...o(),...t.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(r,{of:s}),`
+`,n.jsx(e.h1,{id:"product-page",children:"Product page"}),`
+`,n.jsx(e.p,{children:`Lists all information about a product or service of the City.
+Often includes a Call to Action Link that directs the user to a form.`}),`
+`,n.jsx(e.h2,{id:"headings",children:"Headings"}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsx(e.li,{children:"Use a level 1 Heading for the title of the page."}),`
+`,n.jsx(e.li,{children:"Use level 2 Headings for the section titles."}),`
+`]}),`
+`,n.jsx(e.h2,{id:"spacing",children:"Spacing"}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsx(e.li,{children:"Add a small margin bottom to a Heading 2 with following body text."}),`
+`,n.jsx(e.li,{children:"Use an extra large margin between sections and before the Call to Action Link."}),`
+`]})]})}function u(t={}){const{wrapper:e}={...o(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(i,{...t})}):i(t)}export{u as default};
