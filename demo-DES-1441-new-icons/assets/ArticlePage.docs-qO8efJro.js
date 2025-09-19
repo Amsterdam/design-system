5e36b663597c569ad55997ce96ee6b78b5ba82ff
dlnr <niels@dlnr.dev>
2025-09-19 11:35:55 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@a3cfadabdcfe0aef2edfaeaba2534c7f78949f7c 🚀

--- demo-DES-1441-new-icons/assets/ArticlePage.docs-qO8efJro.js
+++
@@ -1,5 +0,0 @@
-import{j as t}from"./iframe-C9tiQ6hI.js";import{useMDXComponents as e}from"./index-DKnaFWg_.js";import{M as i}from"./blocks-QmKkhKYE.js";import{A as s}from"./ArticlePage.stories-B_nOv5f0.js";import"./index-Dot_OiqT.js";import"./common-DrnyCu3t.js";import"./HomeNews-DvMsWeI1.js";import"./config-DhEFtjTK.js";import"./PageFooter.stories-zBVEPuBZ.js";/*@license CC0-1.0*/function r(n){const o={h1:"h1",p:"p",...e(),...n.components};return t.jsxs(t.Fragment,{children:[`
-`,`
-`,t.jsx(i,{of:s}),`
-`,t.jsx(o.h1,{id:"article-page",children:"Article page"}),`
-`,t.jsx(o.p,{children:"This page type is for news or similar kinds of articles."})]})}function j(n={}){const{wrapper:o}={...e(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(r,{...n})}):r(n)}export{j as default};
