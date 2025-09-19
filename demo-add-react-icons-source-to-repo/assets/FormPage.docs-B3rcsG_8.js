136fbb93b09f777d2999156cb9d424417b630f27
GitHub <github-action@users.noreply.github.com>
2025-09-19 09:20:00 +0000

    Remove feature branch folder

--- demo-add-react-icons-source-to-repo/assets/FormPage.docs-B3rcsG_8.js
+++
@@ -1,5 +0,0 @@
-import{j as o}from"./iframe-BXYWkmjt.js";import{useMDXComponents as r}from"./index-BxPWnUn0.js";import{M as s}from"./blocks-CgJqP_cm.js";import{F as m}from"./FormPage.stories-C_hqOsVg.js";import"./index-C9F-vcJ2.js";import"./config-CnXkDcKR.js";import"./PageFooter.stories-DuWSR74y.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
-`,`
-`,o.jsx(s,{of:m}),`
-`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
-`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function d(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{d as default};
