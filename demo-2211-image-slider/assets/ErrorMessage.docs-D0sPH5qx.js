c6dbb4ea387244da52c9bc6d4fdd373e3dc802c0
Evi-2003 <69012052+Evi-2003@users.noreply.github.com>
2025-09-22 08:15:11 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@9ec5f91ac9bac51be1a0457c719ef41e9f507bdd 🚀

---
+++ demo-2211-image-slider/assets/ErrorMessage.docs-D0sPH5qx.js
@@ -0,0 +1,27 @@
+import{j as e}from"./iframe-vks0q6f1.js";import{useMDXComponents as t}from"./index-BD_0oPSv.js";import{M as i,c as a,P as m,e as c,f as s}from"./blocks-BnGBEJol.js";import{E as h,C as d,W as f}from"./ErrorMessage.stories-d84yFExu.js";import"./index-Ba2uvMyI.js";import"./Icon-I67WAvSh.js";const l=`<!-- @license CC0-1.0 -->
+
+# Error Message
+
+Show an error message when there is a form field validation error.
+In the error message explain what went wrong and how to fix it.
+
+For guidance and examples on using error messages in a form,
+refer to the [Field](/docs/components-forms-field--docs) and [Field Set](/docs/components-forms-field-set--docs) documentation.
+
+Read the documentation by [NL Design System](https://www.nldesignsystem.nl/richtlijnen/formulieren/foutmeldingen) and [Gov.uk](https://design-system.service.gov.uk/components/error-message/) for more information on the contents of error messages and when to show them.
+`;/*@license CC0-1.0*/function r(o){const n={code:"code",h2:"h2",h3:"h3",p:"p",...t(),...o.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(i,{of:h}),`
+`,e.jsx(a,{children:l}),`
+`,e.jsx(m,{}),`
+`,e.jsx(c,{}),`
+`,e.jsx(n.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(n.h3,{id:"custom-prefix",children:"Custom prefix"}),`
+`,e.jsxs(n.p,{children:[`Error messages are automatically prefixed with a visually hidden text, the Dutch word "Invoerfout".
+This makes the error message more clear for screen reader users.
+If you want to change this prefix, to support another language for example, you can use the `,e.jsx(n.code,{children:"prefix"})," prop."]}),`
+`,e.jsx(s,{of:d}),`
+`,e.jsx(n.h3,{id:"with-custom-icon",children:"With custom icon"}),`
+`,e.jsx(n.p,{children:`Replace the icon with another to use Error Message in a different theme or visual identity.
+Applications for the City of Amsterdam must use the default icon.`}),`
+`,e.jsx(s,{of:f})]})}function y(o={}){const{wrapper:n}={...t(),...o.components};return n?e.jsx(n,{...o,children:e.jsx(r,{...o})}):r(o)}export{y as default};
---
+++ demo-2211-image-slider/assets/PageHeading.docs-DwGFa7Vs.js
@@ -0,0 +1,9 @@
+import{j as e}from"./iframe-vks0q6f1.js";import{useMDXComponents as s}from"./index-BD_0oPSv.js";import{M as r,c as a,P as i}from"./blocks-BnGBEJol.js";import{P as m}from"./PageHeading.stories-CP1zNKkj.js";import{S as d}from"./StatusBadge-DcAHpVwN.js";import"./index-Ba2uvMyI.js";import"./Badge-B2gwPAqK.js";const c=`<!-- @license CC0-1.0 -->
+
+# Page Heading
+`;/*@license CC0-1.0*/function o(n){return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(d,{description:"We no longer use this size of headings. Use the Heading component with level 1 instead.",status:"deprecated"}),`
+`,e.jsx(r,{of:m}),`
+`,e.jsx(a,{children:c}),`
+`,e.jsx(i,{})]})}function M(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{M as default};
