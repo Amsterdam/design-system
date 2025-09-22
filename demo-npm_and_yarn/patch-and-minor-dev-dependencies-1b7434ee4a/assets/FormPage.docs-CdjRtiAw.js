52bd605629be1add9a90d50250eada3a21898e47
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-09-22 14:58:47 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@94cc2456efc9c8cf9f4a6ec57604b106c00c4b33 🚀

---
+++ demo-npm_and_yarn/patch-and-minor-dev-dependencies-1b7434ee4a/assets/FormPage.docs-CdjRtiAw.js
@@ -0,0 +1,5 @@
+import{j as o}from"./iframe-CceTd_q1.js";import{useMDXComponents as r}from"./index-Dnubb7rG.js";import{M as s}from"./blocks-Cp9tcmQQ.js";import{F as m}from"./FormPage.stories-CVARHq-h.js";import"./index-bknteKwE.js";import"./config-Dfk35urK.js";import"./PageFooter.stories-Cdc3Crsf.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
+`,`
+`,o.jsx(s,{of:m}),`
+`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
+`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function d(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{d as default};
---
+++ demo-npm_and_yarn/patch-and-minor-dev-dependencies-1b7434ee4a/assets/PageHeading.docs-Ck-OZIbV.js
@@ -0,0 +1,9 @@
+import{j as e}from"./iframe-CceTd_q1.js";import{useMDXComponents as s}from"./index-Dnubb7rG.js";import{M as r,c as a,P as i}from"./blocks-Cp9tcmQQ.js";import{P as m}from"./PageHeading.stories-Df3iqOpg.js";import{S as d}from"./StatusBadge-Cyy7horJ.js";import"./index-bknteKwE.js";import"./Badge-BDM0lkHH.js";const c=`<!-- @license CC0-1.0 -->
+
+# Page Heading
+`;/*@license CC0-1.0*/function o(n){return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(d,{description:"We no longer use this size of headings. Use the Heading component with level 1 instead.",status:"deprecated"}),`
+`,e.jsx(r,{of:m}),`
+`,e.jsx(a,{children:c}),`
+`,e.jsx(i,{})]})}function M(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{M as default};
