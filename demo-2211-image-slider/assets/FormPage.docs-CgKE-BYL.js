67de149bc6402ce66c40ab3aadbc99c1d69831d6
Evi-2003 <69012052+Evi-2003@users.noreply.github.com>
2025-09-23 13:59:01 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@17e4d3b59bd066d3fec45f72c3af82d011e40018 🚀

---
+++ demo-2211-image-slider/assets/FormPage.docs-CgKE-BYL.js
@@ -0,0 +1,5 @@
+import{j as o}from"./iframe-C0UA78yp.js";import{useMDXComponents as r}from"./index-BXJuLOm6.js";import{M as s}from"./blocks-QozN66u-.js";import{F as m}from"./FormPage.stories-0fcIQxhi.js";import"./index-mgc5Pvjc.js";import"./config-DoEt2RCR.js";import"./PageFooter.stories-CTqfzTJT.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
+`,`
+`,o.jsx(s,{of:m}),`
+`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
+`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function d(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{d as default};
--- demo-2211-image-slider/assets/FormPage.docs-DURdqOmJ.js
+++
@@ -1,5 +0,0 @@
-import{j as o}from"./iframe-vks0q6f1.js";import{useMDXComponents as r}from"./index-BD_0oPSv.js";import{M as s}from"./blocks-BnGBEJol.js";import{F as m}from"./FormPage.stories-DVBJois4.js";import"./index-Ba2uvMyI.js";import"./config-Bm9wj7pu.js";import"./PageFooter.stories-CsFV64aG.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
-`,`
-`,o.jsx(s,{of:m}),`
-`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
-`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function d(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{d as default};
