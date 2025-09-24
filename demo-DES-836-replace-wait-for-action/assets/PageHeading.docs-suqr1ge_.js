2e356e7e00243b4ff1837b0b3e1c3d494966e535
GitHub <github-action@users.noreply.github.com>
2025-09-24 11:36:28 +0000

    Remove feature branch folder

--- demo-DES-836-replace-wait-for-action/assets/PageHeading.docs-suqr1ge_.js
+++
@@ -1,9 +0,0 @@
-import{j as e}from"./iframe-CBBSwc7O.js";import{useMDXComponents as s}from"./index-CpR_vG1W.js";import{M as r,c as a,P as i}from"./blocks-DGpi17YZ.js";import{P as m}from"./PageHeading.stories-B2R7OovE.js";import{S as d}from"./StatusBadge-k081ANR9.js";import"./index-YvRCjaCL.js";import"./Badge-5BTnWK-Y.js";const c=`<!-- @license CC0-1.0 -->
-
-# Page Heading
-`;/*@license CC0-1.0*/function o(n){return e.jsxs(e.Fragment,{children:[`
-`,`
-`,e.jsx(d,{description:"We no longer use this size of headings. Use the Heading component with level 1 instead.",status:"deprecated"}),`
-`,e.jsx(r,{of:m}),`
-`,e.jsx(a,{children:c}),`
-`,e.jsx(i,{})]})}function M(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{M as default};
