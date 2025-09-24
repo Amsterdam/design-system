fe2e40748ec84632df7145e2f31ade37d197bbf9
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-09-24 11:35:01 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@05a665dcaaca983affed76871f6dddf7606cc1e7 🚀

---
+++ demo-DES-836-replace-wait-for-action/assets/FormPage.docs-CARX1H3H.js
@@ -0,0 +1,5 @@
+import{j as o}from"./iframe-CBBSwc7O.js";import{useMDXComponents as r}from"./index-CpR_vG1W.js";import{M as s}from"./blocks-DGpi17YZ.js";import{F as m}from"./FormPage.stories-yx1a7r-z.js";import"./index-YvRCjaCL.js";import"./config-ClSmYrun.js";import"./PageFooter.stories-Cv4cb-W2.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
+`,`
+`,o.jsx(s,{of:m}),`
+`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
+`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function d(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{d as default};
--- demo-DES-836-replace-wait-for-action/assets/FormPage.docs-CpgJSBfh.js
+++
@@ -1,5 +0,0 @@
-import{j as o}from"./iframe-DL7VmLw1.js";import{useMDXComponents as r}from"./index-P4f_t07s.js";import{M as s}from"./blocks-ycoSOceU.js";import{F as m}from"./FormPage.stories-CDWcXYLa.js";import"./index-Cbu0psZb.js";import"./config-BQAcIsz2.js";import"./PageFooter.stories-DHsymLUn.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
-`,`
-`,o.jsx(s,{of:m}),`
-`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
-`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function d(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{d as default};
