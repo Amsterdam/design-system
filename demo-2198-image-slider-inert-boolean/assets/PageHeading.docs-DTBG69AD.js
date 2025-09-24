5fe1b5bd8c677e5beaa12ae3554afca0e3b4ae12
Evi-2003 <69012052+Evi-2003@users.noreply.github.com>
2025-09-24 11:56:58 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@ebdff5b3e45440b31d3aef084d455b155ed9abdd 🚀

--- demo-2198-image-slider-inert-boolean/assets/PageHeading.docs-DTBG69AD.js
+++
@@ -1,9 +0,0 @@
-import{j as e}from"./iframe-HZg55zGy.js";import{useMDXComponents as s}from"./index-6E1U6gIZ.js";import{M as r,c as a,P as i}from"./blocks-Coeop07v.js";import{P as m}from"./PageHeading.stories-De1X8Bdq.js";import{S as d}from"./StatusBadge-LOmqU5dY.js";import"./index-Bp0c9cva.js";import"./Badge-B0KYfztO.js";const c=`<!-- @license CC0-1.0 -->
-
-# Page Heading
-`;/*@license CC0-1.0*/function o(n){return e.jsxs(e.Fragment,{children:[`
-`,`
-`,e.jsx(d,{description:"We no longer use this size of headings. Use the Heading component with level 1 instead.",status:"deprecated"}),`
-`,e.jsx(r,{of:m}),`
-`,e.jsx(a,{children:c}),`
-`,e.jsx(i,{})]})}function M(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{M as default};
