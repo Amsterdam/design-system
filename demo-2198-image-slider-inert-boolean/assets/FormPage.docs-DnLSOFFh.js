84c4b889416f5e1447b94f3791be934c6432ab16
Evi-2003 <69012052+Evi-2003@users.noreply.github.com>
2025-09-24 11:46:42 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@2a5f7a7a34524cc3f5354013ae923bbdce212daf 🚀

---
+++ demo-2198-image-slider-inert-boolean/assets/FormPage.docs-DnLSOFFh.js
@@ -0,0 +1,5 @@
+import{j as o}from"./iframe-HZg55zGy.js";import{useMDXComponents as r}from"./index-6E1U6gIZ.js";import{M as s}from"./blocks-Coeop07v.js";import{F as m}from"./FormPage.stories-CGQw1uqo.js";import"./index-Bp0c9cva.js";import"./config-BeCQtYa2.js";import"./PageFooter.stories-D03uLx14.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
+`,`
+`,o.jsx(s,{of:m}),`
+`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
+`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function d(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{d as default};
