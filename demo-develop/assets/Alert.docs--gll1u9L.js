bff0a8d835a8aac42106edbae309c686fe001eb6
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-09-25 13:45:37 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@0e16f554d3a58acce4d8b8617b7de7ce60cd8a0f 🚀

---
+++ demo-develop/assets/Alert.docs--gll1u9L.js
@@ -0,0 +1,45 @@
+import{j as n}from"./iframe-B7V_QBhb.js";import{useMDXComponents as r}from"./index-B5VzCSsy.js";import{M as o,c as a,P as h,e as c,f as t}from"./blocks-BIggcrUg.js";import{A as l,W as d,E as m,S as u,I as p,a as f}from"./Alert.stories-a0jKwO_c.js";import"./index-_kv9gDH8.js";import"./Alert-D7E0r1wI.js";import"./Icon-D9GSrsO0.js";import"./Row-BJFSt8a7.js";import"./Heading-BVKBsAJy.js";import"./IconButton-e0aTI_Ds.js";const x=`<!-- @license CC0-1.0 -->
+
+# Alert
+
+An Alert informs the user of a significant or time-sensitive message without interrupting their task.
+
+## Guidelines
+
+- Use an Alert if the message’s purpose matches one of the following:
+  - **Information** to bring attention to a message.
+    This variation is blue and it is the default.
+  - **Success** to reassure that a process is complete.
+    This variation is green.
+  - **Warning** when action is needed to prevent damage.
+    This variation is orange.
+  - **Error** indicates an error has occurred.
+    This variation is red.
+- Place the Alert in a Grid Cell to create enough white space around it.
+- Important and urgent information is to be placed directly below the Page Header.
+  Examples: system outage or changes in the opening hours of a City Office.
+  This can be applied to the entire application or to a part of it.
+- Add a close button if the user should be able to dismiss the Alert.
+`;/*@license CC0-1.0*/function s(i){const e={h2:"h2",h3:"h3",p:"p",...r(),...i.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(o,{of:l}),`
+`,n.jsx(a,{children:x}),`
+`,n.jsx(h,{}),`
+`,n.jsx(c,{}),`
+`,n.jsx(e.h2,{id:"examples",children:"Examples"}),`
+`,n.jsx(e.h3,{id:"warning",children:"Warning"}),`
+`,n.jsx(e.p,{children:"Display a warning when user action is required."}),`
+`,n.jsx(t,{of:d}),`
+`,n.jsx(e.h3,{id:"error",children:"Error"}),`
+`,n.jsx(e.p,{children:`In case of an error, use the red alert.
+Provide the user with suggestions on alternative ways to achieve the goal.`}),`
+`,n.jsx(t,{of:m}),`
+`,n.jsx(e.h3,{id:"success",children:"Success"}),`
+`,n.jsx(e.p,{children:"Announce the success of a significant action."}),`
+`,n.jsx(t,{of:u}),`
+`,n.jsx(e.h3,{id:"information",children:"Information"}),`
+`,n.jsx(e.p,{children:"An informative message can emphasize matters that are useful to follow."}),`
+`,n.jsx(t,{of:p}),`
+`,n.jsx(e.h3,{id:"with-inline-link",children:"With inline link"}),`
+`,n.jsx(e.p,{children:"Include an inline link in the text to guide the user."}),`
+`,n.jsx(t,{of:f})]})}function y(i={}){const{wrapper:e}={...r(),...i.components};return e?n.jsx(e,{...i,children:n.jsx(s,{...i})}):s(i)}export{y as default};
--- demo-develop/assets/FormPage.docs-3plp-CEJ.js
+++
@@ -1,5 +0,0 @@
-import{j as o}from"./iframe-D_rHBnuG.js";import{useMDXComponents as r}from"./index-QCQi8k6Q.js";import{M as s}from"./blocks-DFf-lLBM.js";import{F as m}from"./FormPage.stories-BDvy94AZ.js";import"./index-xJUqU5QK.js";import"./config-4WUJ8uGk.js";import"./PageFooter.stories-yzbtp8jF.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
-`,`
-`,o.jsx(s,{of:m}),`
-`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
-`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function d(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{d as default};
