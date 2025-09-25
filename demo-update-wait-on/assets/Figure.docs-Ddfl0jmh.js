50e03d8ae98eed71473c6f9e9a4d8af43e695448
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-09-25 09:01:37 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@28dc27f9d4c803ad9ba5559388824a98fe40f8f1 🚀

---
+++ demo-update-wait-on/assets/Figure.docs-Ddfl0jmh.js
@@ -0,0 +1,15 @@
+import{j as n}from"./iframe-CN98T1FG.js";import{useMDXComponents as r}from"./index-CVL_Bfwg.js";import{M as s,c as i,P as a,f as c}from"./blocks-Dxc01xQn.js";import{F as h,I as m}from"./Figure.stories-BOreqNnx.js";import"./index-B-7O6APp.js";import"./exampleContent-DRope23M.js";const u=`<!-- @license CC0-1.0 -->
+
+# Figure
+
+Groups media content with a caption that describes it.
+`;/*@license CC0-1.0*/function t(e){const o={h2:"h2",h3:"h3",p:"p",...r(),...e.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(s,{of:h}),`
+`,n.jsx(i,{children:u}),`
+`,n.jsx(a,{}),`
+`,n.jsx(o.h2,{id:"examples",children:"Examples"}),`
+`,n.jsx(o.h3,{id:"inverse-colour",children:"Inverse colour"}),`
+`,n.jsx(o.p,{children:`Use the inverse colour if the Figure Caption sits on a dark background.
+This ensures the colour of the text provides enough contrast.`}),`
+`,n.jsx(c,{of:m})]})}function C(e={}){const{wrapper:o}={...r(),...e.components};return o?n.jsx(o,{...e,children:n.jsx(t,{...e})}):t(e)}export{C as default};
