3c75effbc8859bdc318607a7620808505b060412
Evi-2003 <69012052+Evi-2003@users.noreply.github.com>
2025-09-25 11:46:59 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@f6ee685563cf834292bc5922b4a55dd81a59cc8e 🚀

--- demo-2198-image-slider-inert-boolean/assets/FormPage.docs-rp3TMF52.js
+++
@@ -1,5 +0,0 @@
-import{j as o}from"./iframe-DInni0Eq.js";import{useMDXComponents as r}from"./index-CmHCmS1I.js";import{M as s}from"./blocks-WmzWqJcU.js";import{F as m}from"./FormPage.stories-BRSAhRYs.js";import"./index-DrmwS6lE.js";import"./config-B9zEmXyh.js";import"./PageFooter.stories-C0HS70LB.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
-`,`
-`,o.jsx(s,{of:m}),`
-`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
-`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function d(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{d as default};
--- demo-2198-image-slider-inert-boolean/assets/PageHeading.docs-BoDW9Q_O.js
+++
@@ -1,9 +0,0 @@
-import{j as e}from"./iframe-DInni0Eq.js";import{useMDXComponents as s}from"./index-CmHCmS1I.js";import{M as r,c as a,P as i}from"./blocks-WmzWqJcU.js";import{P as m}from"./PageHeading.stories-D_p0DVnT.js";import{S as d}from"./StatusBadge-BNg00Stb.js";import"./index-DrmwS6lE.js";import"./Badge-BtH8hwoj.js";const c=`<!-- @license CC0-1.0 -->
-
-# Page Heading
-`;/*@license CC0-1.0*/function o(n){return e.jsxs(e.Fragment,{children:[`
-`,`
-`,e.jsx(d,{description:"We no longer use this size of headings. Use the Heading component with level 1 instead.",status:"deprecated"}),`
-`,e.jsx(r,{of:m}),`
-`,e.jsx(a,{children:c}),`
-`,e.jsx(i,{})]})}function M(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{M as default};
