40ec0dbc88e9bfce0274c0e2bbcb06fe38f6e6cd
GitHub <github-action@users.noreply.github.com>
2025-09-25 13:44:05 +0000

    Remove feature branch folder

--- demo-2198-image-slider-inert-boolean/assets/PageHeading.docs-B_A2--oe.js
+++
@@ -1,9 +0,0 @@
-import{j as e}from"./iframe-B7V_QBhb.js";import{useMDXComponents as s}from"./index-B5VzCSsy.js";import{M as r,c as a,P as i}from"./blocks-BIggcrUg.js";import{P as m}from"./PageHeading.stories-CQFFOQOl.js";import{S as d}from"./StatusBadge-BPLhDCTl.js";import"./index-_kv9gDH8.js";import"./Badge-D0PznBNS.js";const c=`<!-- @license CC0-1.0 -->
-
-# Page Heading
-`;/*@license CC0-1.0*/function o(n){return e.jsxs(e.Fragment,{children:[`
-`,`
-`,e.jsx(d,{description:"We no longer use this size of headings. Use the Heading component with level 1 instead.",status:"deprecated"}),`
-`,e.jsx(r,{of:m}),`
-`,e.jsx(a,{children:c}),`
-`,e.jsx(i,{})]})}function M(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{M as default};
