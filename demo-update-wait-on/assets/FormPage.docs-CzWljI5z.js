d1ac8ebf7c0f4ac43e4d3fa55c2c43d2c4727be9
GitHub <github-action@users.noreply.github.com>
2025-09-25 09:04:52 +0000

    Remove feature branch folder

--- demo-update-wait-on/assets/FormPage.docs-CzWljI5z.js
+++
@@ -1,5 +0,0 @@
-import{j as o}from"./iframe-CN98T1FG.js";import{useMDXComponents as r}from"./index-CVL_Bfwg.js";import{M as s}from"./blocks-Dxc01xQn.js";import{F as m}from"./FormPage.stories-DCmtHnXR.js";import"./index-B-7O6APp.js";import"./config-Bxg3AFtD.js";import"./PageFooter.stories-BGqVhKmH.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
-`,`
-`,o.jsx(s,{of:m}),`
-`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
-`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function d(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{d as default};
--- demo-update-wait-on/assets/PageHeading.docs-C9E06SuP.js
+++
@@ -1,9 +0,0 @@
-import{j as e}from"./iframe-CN98T1FG.js";import{useMDXComponents as s}from"./index-CVL_Bfwg.js";import{M as r,c as a,P as i}from"./blocks-Dxc01xQn.js";import{P as m}from"./PageHeading.stories-fkJaLr01.js";import{S as d}from"./StatusBadge-D9U7mVDB.js";import"./index-B-7O6APp.js";import"./Badge-DwD0IBrE.js";const c=`<!-- @license CC0-1.0 -->
-
-# Page Heading
-`;/*@license CC0-1.0*/function o(n){return e.jsxs(e.Fragment,{children:[`
-`,`
-`,e.jsx(d,{description:"We no longer use this size of headings. Use the Heading component with level 1 instead.",status:"deprecated"}),`
-`,e.jsx(r,{of:m}),`
-`,e.jsx(a,{children:c}),`
-`,e.jsx(i,{})]})}function M(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{M as default};
