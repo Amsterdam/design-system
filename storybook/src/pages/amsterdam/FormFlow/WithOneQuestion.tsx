72e31ee1f5ff3e071d948be922a0419a5a70f45d
alimpens <a.limpens@amsterdam.nl>
2025-09-26 11:41:54 +0200

    Add comments

--- storybook/src/pages/amsterdam/FormFlow/WithOneQuestion.tsx
+++ storybook/src/pages/amsterdam/FormFlow/WithOneQuestion.tsx
@@ -21,9 +21,19 @@ import { FormFooter } from './components/FormFooter'
 
 export const WithOneQuestion = () => (
   <Page>
+    {/* Keep the Page Header as simple as possible, to avoid distractions and to prevent users from accidentally navigating away from the form flow. */}
     <PageHeader className="ams-mb-xl" />
+    {/* The back link is in its own Grid, because it should be outside of the main section. */}
     <Grid className="ams-mb-s">
       <Grid.Cell span={{ narrow: 4, medium: 6, wide: 6 }} start={{ narrow: 1, medium: 2, wide: 3 }}>
+        {/*
+         * We add a back link to allow users to navigate between form pages, without having to worry about losing their progress.
+         * Using the browser back button should also work without losing progress, but users do not always trust it (for some applications, rightfully so).
+         * For more info, see: https://design-system.service.gov.uk/components/back-link/
+         *
+         * We use a link here instead of a button, because multiple submit buttons in a form can cause unexpected behavior.
+         * For more info, see: https://adamsilver.io/blog/forms-with-multiple-submit-buttons-are-problematic/
+         */}
         <StandaloneLink href="#" icon={ChevronBackwardIcon}>
           Vorige vraag
         </StandaloneLink>
@@ -31,13 +41,31 @@ export const WithOneQuestion = () => (
     </Grid>
     <Grid as="main" paddingBottom="2x-large">
       <Grid.Cell span={{ narrow: 4, medium: 6, wide: 6 }} start={{ narrow: 1, medium: 2, wide: 3 }}>
+        {/*
+         * We use a header here that is labelled by an aria-hidden heading, so that we communicate a labelled
+         * section to screen readers, without adding an unnecessary heading to the heading hierarchy.
+         * Furthermore, if CSS fails to load, we still have a clearly labelled section.
+         */}
         <header aria-labelledby="form-header" className="ams-mb-m ams-gap-xs">
           <Heading aria-hidden id="form-header" level={2} size="level-4">
             Afspraak maken
           </Heading>
+          {/*
+           * Start by testing your form without a progress indicator to see if it’s simple enough that users do not need one.
+           * If you do, use a simple one like this one.
+           * For more info, see: https://design-system.service.gov.uk/patterns/question-pages/#using-progress-indicators
+           */}
           <Paragraph>Stap 1 van 3: Afspraak</Paragraph>
         </header>
+        {/*
+         * Do not use HTML5 form validation, because it is not consistent across browsers and devices,
+         * and often gives the user too little information.
+         * Preferably validate user input on the server and return it to the client.
+         * If that is not possible, use client-side validation.
+         * For more info, see: https://nldesignsystem.nl/richtlijnen/formulieren/foutmeldingen/html-formuliervalidatie/#gebruik-geen-html-formuliervalidatie
+         */}
         <form noValidate onSubmit={(e) => e.preventDefault()}>
+          {/* See the docs on specific form components on https://designsystem.amsterdam for more information on how to use them */}
           <FieldSetWithHeading
             aria-required="true"
             className="ams-mb-l"
--- storybook/src/pages/amsterdam/FormFlow/components/FormFooter.tsx
+++ storybook/src/pages/amsterdam/FormFlow/components/FormFooter.tsx
@@ -1,36 +1,36 @@
 import { Column, Grid, Heading, Link, PageFooter, Paragraph } from '@amsterdam/design-system-react'
 
-export const FormFooter = () => {
-  return (
-    <PageFooter>
-      <PageFooter.Spotlight>
-        <Grid paddingVertical="x-large">
-          <Grid.Cell span={4}>
-            <Column gap="small">
-              <Heading color="inverse" level={2} size="level-3">
-                Contact
-              </Heading>
-              <Paragraph color="inverse">Lukt het niet om een afspraak te maken?</Paragraph>
-              <Paragraph color="inverse">
-                Bel het telefoonnummer{' '}
-                <Link color="inverse" href="tel:14020">
-                  14 020
-                </Link>
-                .
-              </Paragraph>
-              <Paragraph color="inverse">Maandag tot en met vrijdag van 08.00 tot 18.00 uur.</Paragraph>
-            </Column>
-          </Grid.Cell>
-        </Grid>
-      </PageFooter.Spotlight>
-      <Heading className="ams-visually-hidden" level={2}>
-        Over deze website
-      </Heading>
-      <PageFooter.Menu>
-        <PageFooter.MenuLink href="#">Over deze site</PageFooter.MenuLink>
-        <PageFooter.MenuLink href="#">Privacy</PageFooter.MenuLink>
-        <PageFooter.MenuLink href="#">Cookies op deze site</PageFooter.MenuLink>
-      </PageFooter.Menu>
-    </PageFooter>
-  )
-}
+export const FormFooter = () => (
+  <PageFooter>
+    {/* Keep the Page Footer as simple as possible, to avoid distractions and to prevent users from accidentally navigating away from the form flow. */}
+    <PageFooter.Spotlight>
+      <Grid paddingVertical="x-large">
+        <Grid.Cell span={4}>
+          <Column gap="small">
+            {/* Add contact information to the footer in a form, so users will know who to contact when they have trouble filling out the form.   */}
+            <Heading color="inverse" level={2} size="level-3">
+              Contact
+            </Heading>
+            <Paragraph color="inverse">Lukt het niet om een afspraak te maken?</Paragraph>
+            <Paragraph color="inverse">
+              Bel het telefoonnummer{' '}
+              <Link color="inverse" href="tel:14020">
+                14 020
+              </Link>
+              .
+            </Paragraph>
+            <Paragraph color="inverse">Maandag tot en met vrijdag van 08.00 tot 18.00 uur.</Paragraph>
+          </Column>
+        </Grid.Cell>
+      </Grid>
+    </PageFooter.Spotlight>
+    <Heading className="ams-visually-hidden" level={2}>
+      Over deze website
+    </Heading>
+    <PageFooter.Menu>
+      <PageFooter.MenuLink href="#">Over deze site</PageFooter.MenuLink>
+      <PageFooter.MenuLink href="#">Privacy</PageFooter.MenuLink>
+      <PageFooter.MenuLink href="#">Cookies op deze site</PageFooter.MenuLink>
+    </PageFooter.Menu>
+  </PageFooter>
+)
