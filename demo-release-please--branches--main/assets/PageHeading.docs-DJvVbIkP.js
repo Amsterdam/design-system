717fda229312c7e9a2a8c79bcf8dfc8118b16197
alimpens <37216945+alimpens@users.noreply.github.com>
2025-09-26 13:47:52 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@2c13665b0ed6e2c7a69d18bc58d22713790027ca 🚀

--- demo-release-please--branches--main/assets/PageHeading.docs-DJvVbIkP.js
+++
@@ -1,9 +0,0 @@
-import{j as e}from"./iframe-BDG42H02.js";import{useMDXComponents as s}from"./index-CPjJi14f.js";import{M as r,c as a,P as i}from"./blocks-BT-GLnco.js";import{P as m}from"./PageHeading.stories-wAsvcqmi.js";import{S as d}from"./StatusBadge-D89X5Def.js";import"./index-8zwsSObW.js";import"./Badge-HonsTanq.js";const c=`<!-- @license CC0-1.0 -->
-
-# Page Heading
-`;/*@license CC0-1.0*/function o(n){return e.jsxs(e.Fragment,{children:[`
-`,`
-`,e.jsx(d,{description:"We no longer use this size of headings. Use the Heading component with level 1 instead.",status:"deprecated"}),`
-`,e.jsx(r,{of:m}),`
-`,e.jsx(a,{children:c}),`
-`,e.jsx(i,{})]})}function M(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{M as default};
