c9ffdb7660c38bc190f05dd98a13230998fd6e96
alimpens <a.limpens@amsterdam.nl>
2025-09-26 10:09:50 +0200

    Use prop for spacing, use correct heading level

--- storybook/src/pages/amsterdam/FormFlow/LandingPage.tsx
+++ storybook/src/pages/amsterdam/FormFlow/LandingPage.tsx
@@ -9,7 +9,7 @@ import { Layout } from '../common/Layout'
 
 export const LandingPage = () => (
   <Layout>
-    <Grid as="main" className="ams-mb-2xl" id="main" paddingTop="large">
+    <Grid as="main" id="main" paddingBottom="2x-large" paddingTop="large">
       <Grid.Cell span={{ narrow: 4, medium: 5, wide: 7 }} start={{ narrow: 1, medium: 2, wide: 3 }}>
         <Heading className="ams-mb-xl" level={1}>
           Waar u dit formulier voor gebruikt
@@ -18,7 +18,7 @@ export const LandingPage = () => (
           Met dit formulier maakt u een afspraak bij een Stadsloket in Amsterdam of Weesp.
         </Paragraph>
 
-        <Heading className="ams-mb-s" level={2} size="level-3">
+        <Heading className="ams-mb-s" level={2}>
           De stappen in dit formulier
         </Heading>
         <OrderedList className="ams-mb-xl">
