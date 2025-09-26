e02ec362138f9e18dc9b105fd737c1b5a9b15b20
GitHub <github-action@users.noreply.github.com>
2025-09-26 14:01:13 +0000

    Remove feature branch folder

--- demo-DES-1446-exclude-test-files-from-dist/assets/PageHeading.docs-CZhmWQu2.js
+++
@@ -1,9 +0,0 @@
-import{j as e}from"./iframe-Y-NdWpYF.js";import{useMDXComponents as s}from"./index-o9GjO0fP.js";import{M as r,c as a,P as i}from"./blocks-DFhz0jMD.js";import{P as m}from"./PageHeading.stories-D8QT2yIL.js";import{S as d}from"./StatusBadge-D43QPU-8.js";import"./index-DsG8FjfJ.js";import"./Badge-CCx5_X4C.js";const c=`<!-- @license CC0-1.0 -->
-
-# Page Heading
-`;/*@license CC0-1.0*/function o(n){return e.jsxs(e.Fragment,{children:[`
-`,`
-`,e.jsx(d,{description:"We no longer use this size of headings. Use the Heading component with level 1 instead.",status:"deprecated"}),`
-`,e.jsx(r,{of:m}),`
-`,e.jsx(a,{children:c}),`
-`,e.jsx(i,{})]})}function M(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{M as default};
