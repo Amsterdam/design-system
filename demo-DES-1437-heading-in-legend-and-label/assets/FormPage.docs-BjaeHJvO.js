8dee4f79795371c76a089aa8d18eac9233b8f178
dlnr <niels@dlnr.dev>
2025-09-26 13:24:58 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@7a077d44c1e9f9121af3f10323ed3a71ddf75bf6 🚀

---
+++ demo-DES-1437-heading-in-legend-and-label/assets/FormPage.docs-BjaeHJvO.js
@@ -0,0 +1,5 @@
+import{j as o}from"./iframe-BsrJde5e.js";import{useMDXComponents as r}from"./index-CCIcJwuF.js";import{M as s}from"./blocks-DpBaNzdI.js";import{F as m}from"./FormPage.stories-CCMz4P88.js";import"./index-Bl3nPB15.js";import"./config-D-UAGTFQ.js";import"./PageFooter.stories-Cgmx9Aui.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
+`,`
+`,o.jsx(s,{of:m}),`
+`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
+`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function d(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{d as default};
---
+++ demo-DES-1437-heading-in-legend-and-label/assets/PageHeading.docs-erU6mQb2.js
@@ -0,0 +1,9 @@
+import{j as e}from"./iframe-BsrJde5e.js";import{useMDXComponents as s}from"./index-CCIcJwuF.js";import{M as r,c as a,P as i}from"./blocks-DpBaNzdI.js";import{P as m}from"./PageHeading.stories-P822WRMA.js";import{S as d}from"./StatusBadge-C-Q5kWTX.js";import"./index-Bl3nPB15.js";import"./Badge-DK5pj05n.js";const c=`<!-- @license CC0-1.0 -->
+
+# Page Heading
+`;/*@license CC0-1.0*/function o(n){return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(d,{description:"We no longer use this size of headings. Use the Heading component with level 1 instead.",status:"deprecated"}),`
+`,e.jsx(r,{of:m}),`
+`,e.jsx(a,{children:c}),`
+`,e.jsx(i,{})]})}function M(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{M as default};
