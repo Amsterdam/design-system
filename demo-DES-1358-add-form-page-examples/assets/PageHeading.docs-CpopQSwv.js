37084917284317b9da85f637466af5d609f0f0d7
alimpens <37216945+alimpens@users.noreply.github.com>
2025-09-26 09:43:56 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@72e31ee1f5ff3e071d948be922a0419a5a70f45d 🚀

---
+++ demo-DES-1358-add-form-page-examples/assets/PageHeading.docs-CpopQSwv.js
@@ -0,0 +1,9 @@
+import{j as e}from"./iframe-BOgk8nSC.js";import{useMDXComponents as s}from"./index-D4OUNgmg.js";import{M as r,c as a,P as i}from"./blocks-QgmnxTEl.js";import{P as m}from"./PageHeading.stories-DALcGGpS.js";import{S as d}from"./StatusBadge-C-Rq7IZq.js";import"./index-TbZyIWky.js";import"./Badge-Cz_a0GqU.js";const c=`<!-- @license CC0-1.0 -->
+
+# Page Heading
+`;/*@license CC0-1.0*/function o(n){return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(d,{description:"We no longer use this size of headings. Use the Heading component with level 1 instead.",status:"deprecated"}),`
+`,e.jsx(r,{of:m}),`
+`,e.jsx(a,{children:c}),`
+`,e.jsx(i,{})]})}function M(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{M as default};
---
+++ demo-DES-1358-add-form-page-examples/assets/config-DXQc77Ot.js
@@ -0,0 +1,1 @@
+import{j as e}from"./iframe-BOgk8nSC.js";import{L as r}from"./Layout-CpuJuzyS.js";const a={decorators:[o=>e.jsx(r,{children:e.jsx(o,{})})],parameters:{layout:"fullscreen",themes:{themeOverride:"Spacious"}}};export{a as c};
