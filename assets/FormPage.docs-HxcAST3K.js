936a621db99d3055b4bbd3bba03cd7b4f66b151c
alimpens <alimpens@users.noreply.github.com>
2025-09-26 13:56:32 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@0e16f554d3a58acce4d8b8617b7de7ce60cd8a0f 🚀

--- assets/FormPage.docs-HxcAST3K.js
+++
@@ -1,5 +0,0 @@
-import{j as o}from"./iframe-oXXjXg-9.js";import{useMDXComponents as r}from"./index-qY0aGC54.js";import{M as s}from"./blocks-C0Y8i57Q.js";import{F as m}from"./FormPage.stories-Cxqf3x1R.js";import"./index-Qqilwb3K.js";import"./config-BuEI8kl8.js";import"./PageFooter.stories-Dm9t8kI-.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
-`,`
-`,o.jsx(s,{of:m}),`
-`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
-`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function d(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{d as default};
---
+++ assets/PageHeading.docs-MB_Y0aPb.js
@@ -0,0 +1,9 @@
+import{j as e}from"./iframe-Bx07oV_M.js";import{useMDXComponents as s}from"./index-UoYK0nNC.js";import{M as r,c as a,P as i}from"./blocks-i81zobFG.js";import{P as m}from"./PageHeading.stories-Ch-0odxl.js";import{S as d}from"./StatusBadge-BVfi8GQ-.js";import"./index-BetIzucH.js";import"./Badge-D1zEUedA.js";const c=`<!-- @license CC0-1.0 -->
+
+# Page Heading
+`;/*@license CC0-1.0*/function o(n){return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(d,{description:"We no longer use this size of headings. Use the Heading component with level 1 instead.",status:"deprecated"}),`
+`,e.jsx(r,{of:m}),`
+`,e.jsx(a,{children:c}),`
+`,e.jsx(i,{})]})}function M(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{M as default};
