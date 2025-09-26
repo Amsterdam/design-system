7d7e74cf1adaf449b0d67b932adf328470a765f2
alimpens <37216945+alimpens@users.noreply.github.com>
2025-09-26 09:04:51 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@0e5b29204d563791e8263906ff15c38a009b6b40 🚀

---
+++ demo-DES-1358-add-form-page-examples/assets/NavigationPage.docs-Bru_2CpG.js
@@ -0,0 +1,17 @@
+import{j as n}from"./iframe-Blmda_yP.js";import{useMDXComponents as a}from"./index-BSaAh6JC.js";import{M as s}from"./blocks-Cd7GOAUk.js";import{N as o}from"./NavigationPage.stories-CML0t0wy.js";import"./index-CIku4gB7.js";import"./exampleContent-DRope23M.js";import"./config-NmiWVFQD.js";import"./Layout-DRnW5UXc.js";import"./PageFooter.stories-CIptx0Dc.js";/*@license CC0-1.0*/function t(i){const e={h1:"h1",h2:"h2",li:"li",p:"p",ul:"ul",...a(),...i.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(s,{of:o}),`
+`,n.jsx(e.h1,{id:"navigation-page",children:"Navigation page"}),`
+`,n.jsx(e.p,{children:`A navigation page does not have much content of its own.
+Instead, it presents groups of links to various pages related to a specific subject.`}),`
+`,n.jsx(e.h2,{id:"headings",children:"Headings"}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsx(e.li,{children:"Use a level 1 Heading for the page title."}),`
+`,n.jsx(e.li,{children:`Set a size of ‘level-3’ to the headings in Link Blocks (those with a Paragraph, Standalone Link and/or a Link List).
+Card Headings already set this by default.`}),`
+`,n.jsx(e.li,{children:"If multiple Cards and Link Blocks are grouped into a section, update their Heading levels accordingly."}),`
+`]}),`
+`,n.jsx(e.h2,{id:"spacing",children:"Spacing"}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsx(e.li,{children:"Grids on this page type use the default x-large vertical gap."}),`
+`]})]})}function x(i={}){const{wrapper:e}={...a(),...i.components};return e?n.jsx(e,{...i,children:n.jsx(t,{...i})}):t(i)}export{x as default};
