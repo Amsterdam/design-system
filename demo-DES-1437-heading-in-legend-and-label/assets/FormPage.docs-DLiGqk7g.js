5c827f3c1374691ac44c186ca11fbc929fec35cb
dlnr <niels@dlnr.dev>
2025-09-29 08:32:16 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@ead95294b1e6ba0c4ec97ca76d86796347f952cd 🚀

---
+++ demo-DES-1437-heading-in-legend-and-label/assets/FormPage.docs-DLiGqk7g.js
@@ -0,0 +1,5 @@
+import{j as o}from"./iframe-BhU_bAR4.js";import{useMDXComponents as r}from"./index-CqwXa-F-.js";import{M as s}from"./blocks-TkS9SNaT.js";import{F as m}from"./FormPage.stories-CX-gaekN.js";import"./index-qv6hR6cl.js";import"./config-ygJrUUyx.js";import"./PageFooter.stories-B6pjt-Xg.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
+`,`
+`,o.jsx(s,{of:m}),`
+`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
+`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function d(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{d as default};
