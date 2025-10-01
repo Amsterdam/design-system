805cc0906df1abe2394c8dfbe30d18b0535e92f0
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-10-01 09:01:20 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@b9cfd3c27b7fd729f66333c0ebd0ee3333ea1462 🚀

---
+++ demo-develop/assets/PageHeading.docs-D4hurC6z.js
@@ -0,0 +1,9 @@
+import{j as e}from"./iframe-DXkrHqY0.js";import{useMDXComponents as s}from"./index-CHfI2lnV.js";import{M as r,c as i,P as a}from"./blocks-2cF2MwxJ.js";import{P as m}from"./PageHeading.stories-CxROhdTQ.js";import{S as d}from"./StatusBadge-BpAtKFBJ.js";import"./preload-helper-PPVm8Dsz.js";import"./index-D4szAa__.js";import"./Badge-CG0qoSff.js";const p=`<!-- @license CC0-1.0 -->
+
+# Page Heading
+`;/*@license CC0-1.0*/function o(n){return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(d,{description:"We no longer use this size of headings. Use the Heading component with level 1 instead.",status:"deprecated"}),`
+`,e.jsx(r,{of:m}),`
+`,e.jsx(i,{children:p}),`
+`,e.jsx(a,{})]})}function h(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{h as default};
