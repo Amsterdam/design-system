29e4dca288b3d550a8c04f22232464d599150a21
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-10-01 09:08:32 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@b6ad60507d496b91a43befad608e3028edbf79c0 🚀

---
+++ demo-npm_and_yarn/stylelint-config-standard-scss-16.0.0/assets/ArticlePage.docs-BsW_EmVO.js
@@ -0,0 +1,15 @@
+import{j as e}from"./iframe-D2I0gOiD.js";import{useMDXComponents as r}from"./index-DXV2piaw.js";import{M as s}from"./blocks-2eF0utsr.js";import{A as o}from"./ArticlePage.stories-BQfk4c1N.js";import"./preload-helper-PPVm8Dsz.js";import"./index-DV4ugoJ7.js";import"./config-BRrfW-eL.js";import"./PageFooter.stories-BTF9YD2K.js";import"./common-DrnyCu3t.js";import"./HomeNews-BAmhgHPf.js";/*@license CC0-1.0*/function t(i){const n={h1:"h1",h2:"h2",li:"li",p:"p",ul:"ul",...r(),...i.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(s,{of:o}),`
+`,e.jsx(n.h1,{id:"article-page",children:"Article page"}),`
+`,e.jsx(n.p,{children:"This page type is for news or similar kinds of articles."}),`
+`,e.jsx(n.h2,{id:"headings",children:"Headings"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Use a level 1 Heading for the title of the article."}),`
+`,e.jsx(n.li,{children:"Use level 2 Headings for the section titles."}),`
+`]}),`
+`,e.jsx(n.h2,{id:"spacing",children:"Spacing"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Add a small margin bottom to a Heading 2 with following body text."}),`
+`,e.jsx(n.li,{children:"Use an extra large margin between sections."}),`
+`]})]})}function f(i={}){const{wrapper:n}={...r(),...i.components};return n?e.jsx(n,{...i,children:e.jsx(t,{...i})}):t(i)}export{f as default};
--- demo-npm_and_yarn/stylelint-config-standard-scss-16.0.0/assets/PageHeading.docs-BOwAGS_K.js
+++
@@ -1,9 +0,0 @@
-import{j as e}from"./iframe-9ppKRtb3.js";import{useMDXComponents as s}from"./index-CqlY5Mh9.js";import{M as r,c as a,P as i}from"./blocks-a0-GLXq8.js";import{P as m}from"./PageHeading.stories-u4Z0rwTY.js";import{S as d}from"./StatusBadge-CoVic68w.js";import"./index-BX0tD-u9.js";import"./Badge-A_FGCBUZ.js";const c=`<!-- @license CC0-1.0 -->
-
-# Page Heading
-`;/*@license CC0-1.0*/function o(n){return e.jsxs(e.Fragment,{children:[`
-`,`
-`,e.jsx(d,{description:"We no longer use this size of headings. Use the Heading component with level 1 instead.",status:"deprecated"}),`
-`,e.jsx(r,{of:m}),`
-`,e.jsx(a,{children:c}),`
-`,e.jsx(i,{})]})}function M(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{M as default};
