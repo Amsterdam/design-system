465f42951d4e0eb5c57f06dc6ce2a6761eda154d
GitHub <github-action@users.noreply.github.com>
2025-10-01 08:59:31 +0000

    Remove feature branch folder

--- demo-npm_and_yarn/patch-and-minor-dev-dependencies-83882df4e8/assets/gridCellClasses-BgQvftim.js
+++
@@ -1,7 +0,0 @@
-/**
- * @license EUPL-1.2+
- * Copyright Gemeente Amsterdam
- */const i=(s,r,a,t)=>{const $=[];return t?[`ams-${s}--padding-vertical--${t}`]:(r&&$.push(`ams-${s}--padding-bottom--${r}`),a&&$.push(`ams-${s}--padding-top--${a}`),$)};/**
- * @license EUPL-1.2+
- * Copyright Gemeente Amsterdam
- */const d=(s,r)=>{if(r==="all"||typeof r=="number")return[`${s}${r}`];if(r){const{narrow:a,medium:t,wide:$}=r;return[`${s}${a}`,`${s}${t}-medium`,`${s}${$}-wide`]}return[]},n=(s,r)=>[...d("ams-grid__cell--span-",s),...d("ams-grid__cell--start-",r)];export{d as a,n as g,i as p};
