06ca2c07d2a584d38647104c68a4aff11a5a09af
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-10-01 10:25:22 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@4cf39ffff9845e1abf2e95c92316c10d6b8b9fa0 🚀

---
+++ demo-DES-1358-add-form-page-examples/assets/ArticlePage.docs-CCQVxdnI.js
@@ -0,0 +1,15 @@
+import{j as e}from"./iframe-Dp9U-ZKx.js";import{useMDXComponents as r}from"./index-DufExIfv.js";import{M as s}from"./blocks-BqJZxUJo.js";import{A as o}from"./ArticlePage.stories-BDX_HT2r.js";import"./index-BNm45-WA.js";import"./config-DBaWUXNb.js";import"./Layout-Z0-cHYUy.js";import"./PageFooter.stories-CxiK_L1U.js";import"./common-DrnyCu3t.js";import"./HomeNews-DMtQ2ICR.js";/*@license CC0-1.0*/function t(i){const n={h1:"h1",h2:"h2",li:"li",p:"p",ul:"ul",...r(),...i.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(s,{of:o}),`
+`,e.jsx(n.h1,{id:"article-page",children:"Article page"}),`
+`,e.jsx(n.p,{children:"This page type is for news or similar kinds of articles."}),`
+`,e.jsx(n.h2,{id:"headings",children:"Headings"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Use a level 1 Heading for the title of the article."}),`
+`,e.jsx(n.li,{children:"Use level 2 Headings for the section titles."}),`
+`]}),`
+`,e.jsx(n.h2,{id:"spacing",children:"Spacing"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Add a small margin bottom to a Heading 2 with following body text."}),`
+`,e.jsx(n.li,{children:"Use an extra large margin between sections."}),`
+`]})]})}function f(i={}){const{wrapper:n}={...r(),...i.components};return n?e.jsx(n,{...i,children:e.jsx(t,{...i})}):t(i)}export{f as default};
--- demo-DES-1358-add-form-page-examples/assets/PageHeading.docs--zbeJI3O.js
+++
@@ -1,9 +0,0 @@
-import{j as e}from"./iframe-awC-ZjjZ.js";import{useMDXComponents as s}from"./index-DDZm16Cd.js";import{M as r,c as a,P as i}from"./blocks-C7BVl-kR.js";import{P as m}from"./PageHeading.stories-BXowFfCk.js";import{S as d}from"./StatusBadge-CZAwcSTW.js";import"./index-D_vm94Bl.js";import"./Badge-Dwi8bPfr.js";const c=`<!-- @license CC0-1.0 -->
-
-# Page Heading
-`;/*@license CC0-1.0*/function o(n){return e.jsxs(e.Fragment,{children:[`
-`,`
-`,e.jsx(d,{description:"We no longer use this size of headings. Use the Heading component with level 1 instead.",status:"deprecated"}),`
-`,e.jsx(r,{of:m}),`
-`,e.jsx(a,{children:c}),`
-`,e.jsx(i,{})]})}function M(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{M as default};
