138deecc2af1feaf772c2e0b5004714bea7b7526
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-10-01 08:55:38 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@a52907a014e5c54965081bdc9dcdf79ba5dcc611 🚀

---
+++ demo-npm_and_yarn/patch-and-minor-dev-dependencies-83882df4e8/assets/FormPage.docs-B1dUgEMC.js
@@ -0,0 +1,5 @@
+import{j as o}from"./iframe-BpZRu8Ci.js";import{useMDXComponents as r}from"./index-uituDxlI.js";import{M as s}from"./blocks-d34Sa1c-.js";import{F as m}from"./FormPage.stories-DK0PaP9P.js";import"./preload-helper-PPVm8Dsz.js";import"./index-8GSw8Gkk.js";import"./config-BqhOHBPD.js";import"./PageFooter.stories-BmZYRkfk.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
+`,`
+`,o.jsx(s,{of:m}),`
+`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
+`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function h(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{h as default};
---
+++ demo-npm_and_yarn/patch-and-minor-dev-dependencies-83882df4e8/assets/PageHeading.docs-CkYsPD5G.js
@@ -0,0 +1,9 @@
+import{j as e}from"./iframe-BpZRu8Ci.js";import{useMDXComponents as s}from"./index-uituDxlI.js";import{M as r,c as i,P as a}from"./blocks-d34Sa1c-.js";import{P as m}from"./PageHeading.stories-CuJTX_KA.js";import{S as d}from"./StatusBadge-RSfM4IKH.js";import"./preload-helper-PPVm8Dsz.js";import"./index-8GSw8Gkk.js";import"./Badge-qAUNlxFS.js";const p=`<!-- @license CC0-1.0 -->
+
+# Page Heading
+`;/*@license CC0-1.0*/function o(n){return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(d,{description:"We no longer use this size of headings. Use the Heading component with level 1 instead.",status:"deprecated"}),`
+`,e.jsx(r,{of:m}),`
+`,e.jsx(i,{children:p}),`
+`,e.jsx(a,{})]})}function h(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{h as default};
