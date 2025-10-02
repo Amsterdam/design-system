2c6d5c42e9857b66e1cf72f2c5595e475bda0762
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-10-02 14:10:36 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@630ae01ea3480a2ba20a5cec52218f148c180b35 🚀

---
+++ demo-DES-1443-new-common-input-tokens-docs/assets/PageHeading.docs-DhvtDDep.js
@@ -0,0 +1,9 @@
+import{j as e}from"./iframe-DaYoX8d6.js";import{useMDXComponents as s}from"./index-B1-ULjb4.js";import{M as r,c as i,P as a}from"./blocks-Czwy_5B3.js";import{P as m}from"./PageHeading.stories-DWC4M8bg.js";import{S as d}from"./StatusBadge-DNhqhDE1.js";import"./preload-helper-PPVm8Dsz.js";import"./index-C8_2oqpI.js";import"./Badge-D34x_f35.js";const p=`<!-- @license CC0-1.0 -->
+
+# Page Heading
+`;/*@license CC0-1.0*/function o(n){return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(d,{description:"We no longer use this size of headings. Use the Heading component with level 1 instead.",status:"deprecated"}),`
+`,e.jsx(r,{of:m}),`
+`,e.jsx(i,{children:p}),`
+`,e.jsx(a,{})]})}function h(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{h as default};
