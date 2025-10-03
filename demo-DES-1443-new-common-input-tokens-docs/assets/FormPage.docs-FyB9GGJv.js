1a38b6d446709dd7f4cdac676c6e0e91e9ce3771
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-10-03 13:33:33 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@59408410ba0dc6f31d04260c64f9f1e29d33d5fd 🚀

--- demo-DES-1443-new-common-input-tokens-docs/assets/FormPage.docs-FyB9GGJv.js
+++
@@ -1,5 +0,0 @@
-import{j as o}from"./iframe-DaYoX8d6.js";import{useMDXComponents as r}from"./index-B1-ULjb4.js";import{M as s}from"./blocks-Czwy_5B3.js";import{F as m}from"./FormPage.stories-DFteehoj.js";import"./preload-helper-PPVm8Dsz.js";import"./index-C8_2oqpI.js";import"./config-Dhhz_2fi.js";import"./PageFooter.stories-Bp_WF-bM.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
-`,`
-`,o.jsx(s,{of:m}),`
-`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
-`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function h(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{h as default};
