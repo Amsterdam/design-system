d64aefd9a5b2cdb93accadbd7bdac146e91c8bc9
Evi-2003 <69012052+Evi-2003@users.noreply.github.com>
2025-10-03 09:59:20 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@765c1bd1a50b922b0110bb7c788b6eebac9a538b 🚀

---
+++ demo-chromatic-tests/assets/Mark.docs-BEOHO5O0.js
@@ -0,0 +1,25 @@
+import{j as n}from"./iframe-C4FfhB-2.js";import{useMDXComponents as s}from"./index-B0dgKtj4.js";import{M as r,c as i,P as o,e as c,f as m}from"./blocks-D5NgZjb9.js";import{M as l,S as h}from"./Mark.stories-BytaUkZ0.js";import"./index-BVWlh1jt.js";const p=`<!-- @license CC0-1.0 -->
+
+# Mark
+
+Highlights a text fragment using a yellow background.
+Provides strong emphasis on the text and draws attention to it.
+
+## Guidelines
+
+- Use a maximum of 1 Mark per page.
+- Limit the marked text to 1 sentence.
+- Do not use it in titles and subtitles.
+  Use a significant and engaging title if the entire paragraph is important.
+- Do not mark an entire paragraph.
+`;/*@license CC0-1.0*/function a(e){const t={h2:"h2",h3:"h3",p:"p",...s(),...e.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(r,{of:l}),`
+`,n.jsx(i,{children:p}),`
+`,n.jsx(o,{}),`
+`,n.jsx(c,{}),`
+`,n.jsx(t.h2,{id:"examples",children:"Examples"}),`
+`,n.jsx(t.h3,{id:"search-results",children:"Search results"}),`
+`,n.jsx(t.p,{children:`For a basic search, mark all appearances of every keyword.
+Ignore capitalization and include partial matches.`}),`
+`,n.jsx(m,{of:h})]})}function j(e={}){const{wrapper:t}={...s(),...e.components};return t?n.jsx(t,{...e,children:n.jsx(a,{...e})}):a(e)}export{j as default};
---
+++ demo-chromatic-tests/assets/PageHeading.docs-khFf5-a0.js
@@ -0,0 +1,9 @@
+import{j as e}from"./iframe-C4FfhB-2.js";import{useMDXComponents as s}from"./index-B0dgKtj4.js";import{M as r,c as a,P as i}from"./blocks-D5NgZjb9.js";import{P as m}from"./PageHeading.stories-rKAhykMP.js";import{S as d}from"./StatusBadge-DbvPvEX5.js";import"./index-BVWlh1jt.js";import"./Badge-B4S6eMgc.js";const c=`<!-- @license CC0-1.0 -->
+
+# Page Heading
+`;/*@license CC0-1.0*/function o(n){return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(d,{description:"We no longer use this size of headings. Use the Heading component with level 1 instead.",status:"deprecated"}),`
+`,e.jsx(r,{of:m}),`
+`,e.jsx(a,{children:c}),`
+`,e.jsx(i,{})]})}function M(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{M as default};
