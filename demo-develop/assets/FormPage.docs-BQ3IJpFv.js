959777bb86663a8ad3ef188d19692ef352041c4a
alimpens <37216945+alimpens@users.noreply.github.com>
2025-10-03 09:29:01 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@064284f41e0b21b33907915e316003eef64c7933 🚀

--- demo-develop/assets/FormPage.docs-BQ3IJpFv.js
+++
@@ -1,5 +0,0 @@
-import{j as o}from"./iframe-DXkrHqY0.js";import{useMDXComponents as r}from"./index-CHfI2lnV.js";import{M as s}from"./blocks-2cF2MwxJ.js";import{F as m}from"./FormPage.stories-D6cw_D_q.js";import"./preload-helper-PPVm8Dsz.js";import"./index-D4szAa__.js";import"./config-DuLxV3ox.js";import"./PageFooter.stories-Z6Zz52DK.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
-`,`
-`,o.jsx(s,{of:m}),`
-`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
-`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function h(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{h as default};
---
+++ demo-develop/assets/FormPage.docs-DK2Dk3Ld.js
@@ -0,0 +1,5 @@
+import{j as o}from"./iframe-CWgnlQ-E.js";import{useMDXComponents as r}from"./index-CQew7Wds.js";import{M as s}from"./blocks-BrHoXN6F.js";import{F as m}from"./FormPage.stories-CSvOtfWb.js";import"./preload-helper-PPVm8Dsz.js";import"./index-JRFktWPl.js";import"./config-CMDTJDrn.js";import"./PageFooter.stories-DJhBiPxn.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
+`,`
+`,o.jsx(s,{of:m}),`
+`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
+`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function h(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{h as default};
