3d038e9ad7119067114c69dff16f8797ed493814
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-10-03 08:46:51 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@e9ab3fe80f6dec468e450698fe0af85ae6d6a9d0 🚀

--- demo-text-input-examples/assets/FormPage.docs-B7uuXVxK.js
+++
@@ -1,5 +0,0 @@
-import{j as o}from"./iframe-BpjFw7YE.js";import{useMDXComponents as r}from"./index-Cdd02lpG.js";import{M as s}from"./blocks-KbZu1Dfl.js";import{F as m}from"./FormPage.stories-EJ31ORr9.js";import"./index-DIMrqI0F.js";import"./config-CdWHA12t.js";import"./PageFooter.stories-DaUA74sJ.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
-`,`
-`,o.jsx(s,{of:m}),`
-`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
-`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function d(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{d as default};
---
+++ demo-text-input-examples/assets/VisuallyHidden.docs-ZFBiVe6n.js
@@ -0,0 +1,20 @@
+import{j as n}from"./iframe-BNuRO1Ex.js";import{useMDXComponents as o}from"./index-CCS45RU-.js";import{M as t,c as r,P as i,e as l}from"./blocks-CGVdgP8P.js";import{V as u}from"./VisuallyHidden.stories-m2q5QkKO.js";import"./preload-helper-PPVm8Dsz.js";import"./index-DTdIdc8Z.js";const c=`<!-- @license CC0-1.0 -->
+
+# Visually Hidden
+
+Hides content from sighted users but keeps it accessible to non-visual user agents, such as screen readers.
+
+## Class name
+
+\`ams-visually-hidden\`
+
+## Guidelines
+
+- In most cases, visually available content should be accessible to non-visual user agents and vice versa.
+  Elaborate instructions or guidance read only by non-visual user agents can do more harm than good.
+`;/*@license CC0-1.0*/function a(s){return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(t,{of:u}),`
+`,n.jsx(r,{children:c}),`
+`,n.jsx(i,{}),`
+`,n.jsx(l,{})]})}function j(s={}){const{wrapper:e}={...o(),...s.components};return e?n.jsx(e,{...s,children:n.jsx(a,{...s})}):a()}export{j as default};
