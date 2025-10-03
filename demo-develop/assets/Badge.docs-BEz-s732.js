87b6917417d357f8b4fb48823e2bffe5aa2f4d8b
alimpens <37216945+alimpens@users.noreply.github.com>
2025-10-03 10:49:29 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@9a1d432a0f2e726093a3cafabbf525f4734cc403 🚀

---
+++ demo-develop/assets/Badge.docs-BEz-s732.js
@@ -0,0 +1,19 @@
+import{j as n}from"./iframe-Cu1hIwRp.js";import{useMDXComponents as t}from"./index-C1evqs3q.js";import{M as s,c as a,P as i,e as c}from"./blocks-BIojT4Ra.js";import{B as d}from"./Badge.stories-CeTKOhtM.js";import"./preload-helper-PPVm8Dsz.js";import"./index-DkiaSnWL.js";import"./Badge-Db8fagZJ.js";const m=`<!-- @license CC0-1.0 -->
+
+# Badge
+
+A prominent label that indicates a category, property or instruction.
+
+## Guidelines
+
+- Let the badge contain a short text or a number.
+- Use any of the feedback or highlight [colours](/docs/brand-design-tokens-colour--docs) for the background.
+  The default is green.
+  Red, orange and green are especially useful for showing an error, warning, or success.
+  However, they can also be combined with the other colours without carrying these meanings.
+`;/*@license CC0-1.0*/function r(e){return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(s,{of:d}),`
+`,n.jsx(a,{children:m}),`
+`,n.jsx(i,{}),`
+`,n.jsx(c,{})]})}function j(e={}){const{wrapper:o}={...t(),...e.components};return o?n.jsx(o,{...e,children:n.jsx(r,{...e})}):r()}export{j as default};
--- demo-develop/assets/FormPage.docs-DO1oKwm8.js
+++
@@ -1,5 +0,0 @@
-import{j as o}from"./iframe-DUmmg3Oj.js";import{useMDXComponents as r}from"./index-DWH5b-iu.js";import{M as s}from"./blocks-Boj289tE.js";import{F as m}from"./FormPage.stories-C4ew3crt.js";import"./preload-helper-PPVm8Dsz.js";import"./index-BqaLdf5E.js";import"./config-lKM3WVkb.js";import"./PageFooter.stories-C9uA6HwE.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
-`,`
-`,o.jsx(s,{of:m}),`
-`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
-`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function h(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{h as default};
