8c8eaf5e9b6621fd96f5d8f69cf37ed9ab80c767
GitHub <github-action@users.noreply.github.com>
2025-10-03 09:27:10 +0000

    Remove feature branch folder

--- demo-npm_and_yarn/stylelint-config-standard-scss-16.0.0/assets/FormPage.docs-DNgS0ZO6.js
+++
@@ -1,5 +0,0 @@
-import{j as o}from"./iframe-D2I0gOiD.js";import{useMDXComponents as r}from"./index-DXV2piaw.js";import{M as s}from"./blocks-2eF0utsr.js";import{F as m}from"./FormPage.stories-Hee0IZ4O.js";import"./preload-helper-PPVm8Dsz.js";import"./index-DV4ugoJ7.js";import"./config-BRrfW-eL.js";import"./PageFooter.stories-BTF9YD2K.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
-`,`
-`,o.jsx(s,{of:m}),`
-`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
-`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function h(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{h as default};
