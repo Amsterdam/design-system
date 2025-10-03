c9d914b556f25999bdfad2a6d2e9c2b19a6e5fe7
GitHub <github-action@users.noreply.github.com>
2025-10-03 10:47:54 +0000

    Remove feature branch folder

--- demo-DES-1461-calendar-picker-indicator-token-names/assets/FormPage.docs-CvJq4ZHA.js
+++
@@ -1,5 +0,0 @@
-import{j as o}from"./iframe-Dvb3X2i5.js";import{useMDXComponents as r}from"./index-C-O3COSY.js";import{M as s}from"./blocks-CE6odf84.js";import{F as m}from"./FormPage.stories-DZLizY6b.js";import"./preload-helper-PPVm8Dsz.js";import"./index-C27y6qb-.js";import"./config-eSGcX3RZ.js";import"./PageFooter.stories-BRg4XR0j.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
-`,`
-`,o.jsx(s,{of:m}),`
-`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
-`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function h(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{h as default};
