d4198d3089c3468124b019ab0320bee2e32097c4
Evi-2003 <69012052+Evi-2003@users.noreply.github.com>
2025-10-03 11:54:42 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@1f9aba29ab951f1b54247a761b354ce472a8de85 🚀

---
+++ demo-chromatic-tests/assets/ArticlePage.docs-CpUHO8m4.js
@@ -0,0 +1,15 @@
+import{j as e}from"./iframe-BlUYFdm-.js";import{useMDXComponents as r}from"./index-BXYGuo-i.js";import{M as s}from"./blocks-CEWk4Wo8.js";import{A as o}from"./ArticlePage.stories-3lu9Pwv0.js";import"./preload-helper-PPVm8Dsz.js";import"./index-7ODgAcO2.js";import"./config-qfgn0UQA.js";import"./PageFooter.stories-DRjL5brs.js";import"./common-DrnyCu3t.js";import"./HomeNews-CrwA1lK7.js";/*@license CC0-1.0*/function t(i){const n={h1:"h1",h2:"h2",li:"li",p:"p",ul:"ul",...r(),...i.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(s,{of:o}),`
+`,e.jsx(n.h1,{id:"article-page",children:"Article page"}),`
+`,e.jsx(n.p,{children:"This page type is for news or similar kinds of articles."}),`
+`,e.jsx(n.h2,{id:"headings",children:"Headings"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Use a level 1 Heading for the title of the article."}),`
+`,e.jsx(n.li,{children:"Use level 2 Headings for the section titles."}),`
+`]}),`
+`,e.jsx(n.h2,{id:"spacing",children:"Spacing"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Add a small margin bottom to a Heading 2 with following body text."}),`
+`,e.jsx(n.li,{children:"Use an extra large margin between sections."}),`
+`]})]})}function f(i={}){const{wrapper:n}={...r(),...i.components};return n?e.jsx(n,{...i,children:e.jsx(t,{...i})}):t(i)}export{f as default};
---
+++ demo-chromatic-tests/assets/FileList.docs-dBEWU8sl.js
@@ -0,0 +1,60 @@
+import{j as e}from"./iframe-BlUYFdm-.js";import{useMDXComponents as s}from"./index-BXYGuo-i.js";import{M as l,c as r,P as o,f as a,S as f}from"./blocks-CEWk4Wo8.js";import{F as m,W as c}from"./FileList.stories-Cc4k1O35.js";import"./preload-helper-PPVm8Dsz.js";import"./index-7ODgAcO2.js";import"./Icon-BzXaKO6l.js";import"./Button-DjHidHcU.js";const u=`/**
+ * @license EUPL-1.2+
+ * Copyright Gemeente Amsterdam
+ */
+
+import { FileInput, FileList } from '@amsterdam/design-system-react'
+import { useRef, useState } from 'react'
+
+export const FileInputWithFileList = () => {
+  const inputRef = useRef<HTMLInputElement>(null)
+  const [files, setFiles] = useState<FileList | null>(null)
+
+  const changeFiles = () => {
+    if (inputRef.current) {
+      setFiles(inputRef.current.files)
+    }
+  }
+
+  const removeFile = (index: number) => {
+    if (files) {
+      const newFiles = new DataTransfer()
+      Array.from(files).forEach((file, i) => {
+        if (i !== index) newFiles.items.add(file)
+      })
+      if (inputRef.current) {
+        inputRef.current.files = newFiles.files
+      }
+      setFiles(newFiles.files)
+    }
+  }
+
+  return (
+    <>
+      <FileInput multiple onChange={changeFiles} ref={inputRef} />
+      {files && (
+        <FileList>
+          {Array.from(files).map((file, index) => (
+            <FileList.Item file={file} key={file.name} onDelete={() => removeFile(index)} />
+          ))}
+        </FileList>
+      )}
+    </>
+  )
+}
+`,p=`<!-- @license CC0-1.0 -->
+
+# File List
+
+An overview of files, showing their name, type, size, and a preview.
+`;/*@license CC0-1.0*/function t(i){const n={code:"code",h2:"h2",h3:"h3",p:"p",...s(),...i.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(l,{of:m}),`
+`,e.jsx(r,{children:p}),`
+`,e.jsx(o,{}),`
+`,e.jsx(n.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(n.h3,{id:"using-a-file-input",children:"Using a file input"}),`
+`,e.jsxs(n.p,{children:["To connect a File Input to a File List, use the ",e.jsx(n.code,{children:"onChange"}),` event to update the
+list of files and use `,e.jsx(n.code,{children:"onDelete"})," when removing a file from the list."]}),`
+`,e.jsx(a,{of:c}),`
+`,e.jsx(f,{code:u,language:"jsx"})]})}function R(i={}){const{wrapper:n}={...s(),...i.components};return n?e.jsx(n,{...i,children:e.jsx(t,{...i})}):t(i)}export{R as default};
