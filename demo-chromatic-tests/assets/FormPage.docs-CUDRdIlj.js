8e1947ab00413c96661d78745d62786fc1f177e2
Evi-2003 <69012052+Evi-2003@users.noreply.github.com>
2025-10-03 11:57:41 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@b370d73f41b9e77e776f5491ef1c87a465cd1a9f 🚀

---
+++ demo-chromatic-tests/assets/FormPage.docs-CUDRdIlj.js
@@ -0,0 +1,5 @@
+import{j as o}from"./iframe-Qja5T4n3.js";import{useMDXComponents as r}from"./index-DvtkyrAh.js";import{M as s}from"./blocks-DggIYBH8.js";import{F as m}from"./FormPage.stories-BLhkuT3P.js";import"./preload-helper-PPVm8Dsz.js";import"./index-CYfCmgWy.js";import"./config-DTKhrnJ8.js";import"./PageFooter.stories-B9LicN6U.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
+`,`
+`,o.jsx(s,{of:m}),`
+`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
+`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function h(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{h as default};
--- demo-chromatic-tests/assets/Link.stories-BVEtRXVq.js
+++
@@ -1,9 +0,0 @@
-import{r as d,j as n,c as p,P as m}from"./iframe-BlUYFdm-.js";const a=d.forwardRef(({children:s,className:t,color:c,...i},l)=>n.jsx("a",{...i,className:p("ams-link",c&&`ams-link--${c}`,t),ref:l,children:s}));a.displayName="Link";try{a.displayName="Link",a.__docgenInfo={description:"",displayName:"Link",props:{}}}catch{}const u={title:"Components/Navigation/Link",component:a,args:{children:"typograaf",href:"#"},argTypes:{children:{description:"The link text.",table:{disable:!1}},color:{control:{labels:{undefined:"default"},type:"radio"},options:[void 0,"contrast","inverse"]},href:{description:"The url for the link.",name:"href",type:{name:"string",required:!1}}},decorators:[(s,{args:t})=>n.jsxs(m,{color:t.color==="inverse"?"inverse":void 0,children:["Jouw ",n.jsx(s,{})," biedt mij zulke exquise schreven!"]})]},e={},r={args:{color:"contrast"}},o={args:{color:"inverse"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:"{}",...e.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    color: 'contrast'
-  }
-}`,...r.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
-  args: {
-    color: 'inverse'
-  }
-}`,...o.parameters?.docs?.source}}};const f=["Default","ContrastColour","InverseColour"],_=Object.freeze(Object.defineProperty({__proto__:null,ContrastColour:r,Default:e,InverseColour:o,__namedExportsOrder:f,default:u},Symbol.toStringTag,{value:"Module"}));export{r as C,o as I,_ as L};
