de49f8b0e8435f0650f8f2a130efe57e4d6b6ba9
alimpens <37216945+alimpens@users.noreply.github.com>
2025-10-03 13:28:11 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@b7d010ebc9c71b02ddaaa553fdb57c9f817fea55 🚀

---
+++ demo-remove-double-copyright-notices/assets/PageHeading.docs-DRMvNDRH.js
@@ -0,0 +1,9 @@
+import{j as e}from"./iframe-2It8nAa0.js";import{useMDXComponents as s}from"./index-D3kVmk7o.js";import{M as r,c as i,P as a}from"./blocks-kKuXP1ax.js";import{P as m}from"./PageHeading.stories-CfG6oRW1.js";import{S as d}from"./StatusBadge-DehtbV7e.js";import"./preload-helper-PPVm8Dsz.js";import"./index-Dkwreldp.js";import"./Badge-B5CtYy3j.js";const p=`<!-- @license CC0-1.0 -->
+
+# Page Heading
+`;/*@license CC0-1.0*/function o(n){return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(d,{description:"We no longer use this size of headings. Use the Heading component with level 1 instead.",status:"deprecated"}),`
+`,e.jsx(r,{of:m}),`
+`,e.jsx(i,{children:p}),`
+`,e.jsx(a,{})]})}function h(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{h as default};
---
+++ demo-remove-double-copyright-notices/assets/TableOfContents.docs-C5Laz7JG.js
@@ -0,0 +1,15 @@
+import{j as n}from"./iframe-2It8nAa0.js";import{useMDXComponents as o}from"./index-D3kVmk7o.js";import{M as r,c as i,P as a,e as l,f as m}from"./blocks-kKuXP1ax.js";import{T as c,M as p}from"./TableOfContents.stories-CKoBVktC.js";import"./preload-helper-PPVm8Dsz.js";import"./index-Dkwreldp.js";import"./Heading-CBDhDbv5.js";const x=`<!-- @license CC0-1.0 -->
+
+# Table of Contents
+
+A list of links corresponding to the content sections on the page.
+It helps users to easily navigate to different sections on the same page.
+`;/*@license CC0-1.0*/function s(e){const t={h2:"h2",h3:"h3",...o(),...e.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(r,{of:c}),`
+`,n.jsx(i,{children:x}),`
+`,n.jsx(a,{}),`
+`,n.jsx(l,{}),`
+`,n.jsx(t.h2,{id:"examples",children:"Examples"}),`
+`,n.jsx(t.h3,{id:"multiple-levels",children:"Multiple levels"}),`
+`,n.jsx(m,{of:p})]})}function g(e={}){const{wrapper:t}={...o(),...e.components};return t?n.jsx(t,{...e,children:n.jsx(s,{...e})}):s(e)}export{g as default};
