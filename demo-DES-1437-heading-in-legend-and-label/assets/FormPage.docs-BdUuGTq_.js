60eb3763f7f783441965e100aeff0cc1f2ac25b3
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-10-03 09:40:56 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@dd08165723ccc89ab0f0f83c25b3c8fca29a1b6f 🚀

--- demo-DES-1437-heading-in-legend-and-label/assets/FormPage.docs-BdUuGTq_.js
+++
@@ -1,5 +0,0 @@
-import{j as o}from"./iframe-B-Zjib92.js";import{useMDXComponents as r}from"./index-CKA24YZw.js";import{M as s}from"./blocks-C4IDW09a.js";import{F as m}from"./FormPage.stories-DetqFnTJ.js";import"./preload-helper-PPVm8Dsz.js";import"./index-B9nTDhR5.js";import"./config-DfqbZ1SL.js";import"./PageFooter.stories-DhaadnQ6.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
-`,`
-`,o.jsx(s,{of:m}),`
-`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
-`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function h(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{h as default};
---
+++ demo-DES-1437-heading-in-legend-and-label/assets/Page.stories-CbIneKFK.js
@@ -0,0 +1,5 @@
+import{r as n,j as r,c as l}from"./iframe-B9MmUP0L.js";const a=n.forwardRef(({children:s,className:t,...o},c)=>r.jsx("div",{...o,className:l("ams-page",t),ref:c,children:s}));a.displayName="Page";try{a.displayName="Page",a.__docgenInfo={description:"",displayName:"Page",props:{}}}catch{}const m={title:"Components/Containers/Page",component:a,args:{className:"ams-docs-page"},argTypes:{className:{table:{disable:!0}}},parameters:{layout:"fullscreen"}},e={args:{children:r.jsx("div",{className:"ams-docs-item"})}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: <div className="ams-docs-item" />
+  }
+}`,...e.parameters?.docs?.source}}};const d=["Default"],i=Object.freeze(Object.defineProperty({__proto__:null,Default:e,__namedExportsOrder:d,default:m},Symbol.toStringTag,{value:"Module"}));export{i as P};
