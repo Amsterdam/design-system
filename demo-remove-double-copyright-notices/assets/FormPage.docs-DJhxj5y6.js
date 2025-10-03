fb4365439b040b4f93c5e278d63a167bc3cb97ea
GitHub <github-action@users.noreply.github.com>
2025-10-03 13:32:52 +0000

    Remove feature branch folder

--- demo-remove-double-copyright-notices/assets/FormPage.docs-DJhxj5y6.js
+++
@@ -1,5 +0,0 @@
-import{j as o}from"./iframe-2It8nAa0.js";import{useMDXComponents as r}from"./index-D3kVmk7o.js";import{M as s}from"./blocks-kKuXP1ax.js";import{F as m}from"./FormPage.stories-Bangy7_x.js";import"./preload-helper-PPVm8Dsz.js";import"./index-Dkwreldp.js";import"./config-B6cx8-9h.js";import"./PageFooter.stories-Ch_R4bSj.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
-`,`
-`,o.jsx(s,{of:m}),`
-`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
-`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function h(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{h as default};
