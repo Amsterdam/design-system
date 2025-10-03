22a0ab754bbbba30699180153f421ccc0b05e9bf
Evi-2003 <69012052+Evi-2003@users.noreply.github.com>
2025-10-03 11:42:55 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@1e16b998e9dff4b5984c2dfc6e96e87c0d460efb 🚀

--- demo-chromatic-tests/assets/FormPage.docs-lOJSsoLK.js
+++
@@ -1,5 +0,0 @@
-import{j as o}from"./iframe-BlUYFdm-.js";import{useMDXComponents as r}from"./index-BXYGuo-i.js";import{M as s}from"./blocks-CEWk4Wo8.js";import{F as m}from"./FormPage.stories-CPMune1Z.js";import"./preload-helper-PPVm8Dsz.js";import"./index-7ODgAcO2.js";import"./config-qfgn0UQA.js";import"./PageFooter.stories-DRjL5brs.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
-`,`
-`,o.jsx(s,{of:m}),`
-`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
-`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function h(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{h as default};
---
+++ demo-chromatic-tests/assets/FormPage.docs-w8vWTLtk.js
@@ -0,0 +1,5 @@
+import{j as o}from"./iframe-BudgzWKU.js";import{useMDXComponents as r}from"./index-BDx_Mtzz.js";import{M as s}from"./blocks-BKsYt51a.js";import{F as m}from"./FormPage.stories-InzQK_li.js";import"./preload-helper-PPVm8Dsz.js";import"./index-BI-JoaE8.js";import"./config-BvwyluGY.js";import"./PageFooter.stories-DeLpFpBg.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
+`,`
+`,o.jsx(s,{of:m}),`
+`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
+`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function h(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{h as default};
