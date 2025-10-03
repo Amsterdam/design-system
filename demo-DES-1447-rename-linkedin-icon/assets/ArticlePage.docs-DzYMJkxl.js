346909aef645f0de596ced63c55e01497d5868f3
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-10-03 12:03:29 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@a34d79fe8aaeeaf5584ffa2b0a1fd0a439014736 🚀

---
+++ demo-DES-1447-rename-linkedin-icon/assets/ArticlePage.docs-DzYMJkxl.js
@@ -0,0 +1,15 @@
+import{j as e}from"./iframe-iQpU-ICJ.js";import{useMDXComponents as r}from"./index-C2aD8xaQ.js";import{M as s}from"./blocks-BJb-owCf.js";import{A as o}from"./ArticlePage.stories-BHzs_puY.js";import"./preload-helper-PPVm8Dsz.js";import"./index-WhIEGvFH.js";import"./config-CeKooNdl.js";import"./PageFooter.stories-CRVj8c1D.js";import"./common-DrnyCu3t.js";import"./HomeNews-s7rsR7aL.js";/*@license CC0-1.0*/function t(i){const n={h1:"h1",h2:"h2",li:"li",p:"p",ul:"ul",...r(),...i.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(s,{of:o}),`
+`,e.jsx(n.h1,{id:"article-page",children:"Article page"}),`
+`,e.jsx(n.p,{children:"This page type is for news or similar kinds of articles."}),`
+`,e.jsx(n.h2,{id:"headings",children:"Headings"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Use a level 1 Heading for the title of the article."}),`
+`,e.jsx(n.li,{children:"Use level 2 Headings for the section titles."}),`
+`]}),`
+`,e.jsx(n.h2,{id:"spacing",children:"Spacing"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Add a small margin bottom to a Heading 2 with following body text."}),`
+`,e.jsx(n.li,{children:"Use an extra large margin between sections."}),`
+`]})]})}function f(i={}){const{wrapper:n}={...r(),...i.components};return n?e.jsx(n,{...i,children:e.jsx(t,{...i})}):t(i)}export{f as default};
--- demo-DES-1447-rename-linkedin-icon/assets/inputs.docs-2P_tiwC5.js
+++
@@ -1,8 +0,0 @@
-import{j as t}from"./iframe-7_sY_uV3.js";import{useMDXComponents as l}from"./index-pBq93f4T.js";import{M as i}from"./blocks-BTizL4RJ.js";import"./preload-helper-PPVm8Dsz.js";import"./index-VrGtEVfz.js";/*@license CC0-1.0*/function s(n){const e={code:"code",h1:"h1",p:"p",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr",...l(),...n.components};return t.jsxs(t.Fragment,{children:[`
-`,`
-`,t.jsx(i,{title:"Brand/Design Tokens/Inputs"}),`
-`,t.jsx(e.h1,{id:"links",children:"Links"}),`
-`,t.jsx(e.p,{children:`Several input components share the same styles.
-This set of common tokens shows that connection.`}),`
-`,t.jsx(e.p,{children:"For now, only one shared token exists."}),`
-`,t.jsxs(e.table,{children:[t.jsx(e.thead,{children:t.jsxs(e.tr,{children:[t.jsx(e.th,{style:{textAlign:"left"},children:"CSS variable"}),t.jsx(e.th,{style:{textAlign:"left"},children:"Value"}),t.jsx(e.th,{style:{textAlign:"left"},children:"Description"})]})}),t.jsx(e.tbody,{children:t.jsxs(e.tr,{children:[t.jsx(e.td,{style:{textAlign:"left"},children:t.jsx(e.code,{children:"var(--ams-inputs-line-height)"})}),t.jsx(e.td,{style:{textAlign:"left"},children:t.jsx(e.code,{children:"1.4"})}),t.jsx(e.td,{style:{textAlign:"left"},children:"Inputs and Text Area have a smaller line height than body text. Buttons also use this, to match the height of Inputs and align them easily."})]})})]})]})}function c(n={}){const{wrapper:e}={...l(),...n.components};return e?t.jsx(e,{...n,children:t.jsx(s,{...n})}):s(n)}export{c as default};
