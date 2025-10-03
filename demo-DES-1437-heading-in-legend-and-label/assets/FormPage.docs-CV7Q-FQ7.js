41e07a075f845956886dc4011113c095e1212e16
GitHub <github-action@users.noreply.github.com>
2025-10-03 09:42:14 +0000

    Remove feature branch folder

--- demo-DES-1437-heading-in-legend-and-label/assets/FormPage.docs-CV7Q-FQ7.js
+++
@@ -1,5 +0,0 @@
-import{j as o}from"./iframe-B9MmUP0L.js";import{useMDXComponents as r}from"./index-Cm6ehAw6.js";import{M as s}from"./blocks-CovIhrtd.js";import{F as m}from"./FormPage.stories-DV0Zq1ST.js";import"./preload-helper-PPVm8Dsz.js";import"./index-j1zVpbho.js";import"./config-9rzBUcgR.js";import"./PageFooter.stories-BOh2TZQL.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
-`,`
-`,o.jsx(s,{of:m}),`
-`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
-`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function h(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{h as default};
