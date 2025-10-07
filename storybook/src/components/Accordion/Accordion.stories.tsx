8f083e11bb4a76ea25483207adee5555218c1d8d
Wammes <mail@eviwammes.nl>
2025-10-07 16:01:44 +0200

    feat(Accordion): add integration tests

--- storybook/src/components/Accordion/Accordion.stories.tsx
+++ storybook/src/components/Accordion/Accordion.stories.tsx
@@ -3,9 +3,10 @@
  * Copyright Gemeente Amsterdam
  */
 
-import { Paragraph } from '@amsterdam/design-system-react'
+import { AccordionProps, Paragraph } from '@amsterdam/design-system-react'
 import { Accordion } from '@amsterdam/design-system-react/src'
 import { Meta, StoryObj } from '@storybook/react-vite'
+import { expect } from 'storybook/test'
 
 import { exampleAccordionHeading, exampleParagraph } from '../shared/exampleContent'
 
@@ -91,5 +92,69 @@ export const ReduceLandmarks: Story = {
       </Accordion.Section>,
     ],
     sectionAs: 'div',
+  },
+}
+
+const exampleAccordions = [
+  {
+    label: 'Dit grof afval halen we niet op',
+    paragraph:
+      'Uw eigen buurt schoonhouden? U kunt meehelpen door alleen of samen met uw buren een afvalcontainer te adopteren. Bekijk hier hoe het werkt en hoe u zich aanmeldt als containeradoptant.',
+  },
+  {
+    label: 'Verhuizing doorgeven bij stadsloket',
+    paragraph:
+      'U kunt met uw auto goedkoop parkeren bij een P+R-locatie aan de rand van de stad, wanneer u van daar met het openbaar vervoer (ov) naar het centrumgebied van Amsterdam reist. Anders betaalt u het duurdere standaard parkeertarief, dat per P+R-locatie verschilt.',
+  },
+]
+
+const headingLevels = [2, 3, 4]
+const variants = ['expanded', 'closed']
+
+export const TestCases: Story = {
+  play: async ({ canvas, userEvent }) => {
+    for (const headingLevel of headingLevels) {
+      for (const variant of variants) {
+        for (const { label, paragraph } of exampleAccordions) {
+          const accordionLabel = canvas.getByTestId(`${headingLevel}-${variant}-${label}`)
+          const accordionParagraph = canvas.getByTestId(`${headingLevel}-${variant}-${paragraph}`)
+          const accordionButton = accordionLabel.querySelector('.ams-accordion__button')
+
+          if (!accordionButton) break
+
+          if (variant === 'expanded') {
+            await expect(accordionParagraph).toBeVisible()
+            await userEvent.click(accordionButton)
+            await expect(accordionParagraph).not.toBeVisible()
+          } else {
+            await expect(accordionParagraph).not.toBeVisible()
+            await userEvent.click(accordionButton)
+            await expect(accordionParagraph).toBeVisible()
+          }
+        }
+      }
+    }
+  },
+  render: () => {
+    return (
+      <div style={{ display: 'flex', flexDirection: 'column', gap: '100px' }}>
+        {headingLevels.map((headingLevel) =>
+          variants.flatMap((variant) => (
+            <Accordion headingLevel={headingLevel as AccordionProps['headingLevel']} key={`${headingLevel}-${variant}`}>
+              {exampleAccordions.map(({ label, paragraph }) => (
+                <Accordion.Section
+                  data-testid={`${headingLevel}-${variant}-${label}`}
+                  expanded={variant === 'expanded' ? true : undefined}
+                  key={`${label}-${variant}-${headingLevel}`}
+                  label={label}
+                >
+                  <Paragraph data-testid={`${headingLevel}-${variant}-${paragraph}`}>{paragraph}</Paragraph>
+                </Accordion.Section>
+              ))}
+            </Accordion>
+          )),
+        )}
+      </div>
+    )
   },
 }
