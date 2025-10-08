4a8906d3b430f9291af35cddd826859a37a9c194
Evi-2003 <69012052+Evi-2003@users.noreply.github.com>
2025-10-08 12:27:36 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@d16d42d5ce81fa408ab4fbcefcd3052d5cc8a9df 🚀

--- demo-chromatic-tests/assets/FormPage.docs-BGdvmMTB.js
+++
@@ -1,5 +0,0 @@
-import{j as o}from"./iframe-k_XWJsh-.js";import{useMDXComponents as r}from"./index-ebd1G5cu.js";import{M as s}from"./blocks-CSo8NAJf.js";import{F as m}from"./FormPage.stories-BJOLJs3N.js";import"./preload-helper-PPVm8Dsz.js";import"./index-Cv7uoS_W.js";import"./config-B8TfOSQx.js";import"./PageFooter.stories-BqdzUjXc.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
-`,`
-`,o.jsx(s,{of:m}),`
-`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
-`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function h(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{h as default};
