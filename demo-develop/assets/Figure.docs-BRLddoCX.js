8139cc3b55a5adcdc33f181a2c4b111152749878
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-10-08 12:52:20 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@ff73126e4f0c7c1580da5c8956d3f06e76ebb0e7 🚀

---
+++ demo-develop/assets/Figure.docs-BRLddoCX.js
@@ -0,0 +1,15 @@
+import{j as n}from"./iframe-BBinq_FP.js";import{useMDXComponents as r}from"./index-BKyrAvMe.js";import{M as s,c as i,P as a,f as c}from"./blocks-CHWZImDn.js";import{F as m,I as h}from"./Figure.stories-BCwcruD8.js";import"./preload-helper-PPVm8Dsz.js";import"./index-D57j7LeA.js";import"./exampleContent-DRope23M.js";const u=`<!-- @license CC0-1.0 -->
+
+# Figure
+
+Groups media content with a caption that describes it.
+`;/*@license CC0-1.0*/function t(e){const o={h2:"h2",h3:"h3",p:"p",...r(),...e.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(s,{of:m}),`
+`,n.jsx(i,{children:u}),`
+`,n.jsx(a,{}),`
+`,n.jsx(o.h2,{id:"examples",children:"Examples"}),`
+`,n.jsx(o.h3,{id:"inverse-colour",children:"Inverse colour"}),`
+`,n.jsx(o.p,{children:`Use the inverse colour if the Figure Caption sits on a dark background.
+This ensures the colour of the text provides enough contrast.`}),`
+`,n.jsx(c,{of:h})]})}function M(e={}){const{wrapper:o}={...r(),...e.components};return o?n.jsx(o,{...e,children:n.jsx(t,{...e})}):t(e)}export{M as default};
---
+++ demo-develop/assets/config-HAKw9bfB.js
@@ -0,0 +1,1 @@
+import{j as e}from"./iframe-BBinq_FP.js";import{L as r}from"./Layout-CnwfK14b.js";const a={decorators:[o=>e.jsx(r,{children:e.jsx(o,{})})],parameters:{layout:"fullscreen",themes:{themeOverride:"Spacious"}}};export{a as c};
