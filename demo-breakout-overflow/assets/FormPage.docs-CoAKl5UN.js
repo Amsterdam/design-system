d2a98d74a6a3412ae921fa1f609d0f608a094c7b
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-10-08 12:18:41 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@e986520f020020de2f3b9f687cc5e7ee2543b7d3 🚀

--- demo-breakout-overflow/assets/FormPage.docs-CoAKl5UN.js
+++
@@ -1,5 +0,0 @@
-import{j as o}from"./iframe-_Q3uXTZD.js";import{useMDXComponents as r}from"./index-Cr-4rDkw.js";import{M as s}from"./blocks-BHes3Vma.js";import{F as m}from"./FormPage.stories-m6kSIQrD.js";import"./preload-helper-PPVm8Dsz.js";import"./index-fKvQV4Vk.js";import"./config-BMhyJ7OC.js";import"./PageFooter.stories-DvFnYlMK.js";/*@license CC0-1.0*/function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[`
-`,`
-`,o.jsx(s,{of:m}),`
-`,o.jsx(e.h1,{id:"form-page",children:"Form page"}),`
-`,o.jsx(e.p,{children:"This simple example presents various types of fields."})]})}function h(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{h as default};
