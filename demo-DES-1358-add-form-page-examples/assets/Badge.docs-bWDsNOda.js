f3c9ab89884d0cf25df1f27f0e3709c06d879971
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-10-08 11:58:34 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@a40c68733d6fccf7526574ecf359f3356785b71f 🚀

---
+++ demo-DES-1358-add-form-page-examples/assets/Badge.docs-bWDsNOda.js
@@ -0,0 +1,19 @@
+import{j as n}from"./iframe-Cw142W4C.js";import{useMDXComponents as t}from"./index-BbPQzUg-.js";import{M as s,c as a,P as i,e as c}from"./blocks-BNfQ1e_S.js";import{B as d}from"./Badge.stories-D4-ck_cO.js";import"./preload-helper-PPVm8Dsz.js";import"./index-C5Zrf72m.js";import"./Badge-DzVxPJE2.js";const m=`<!-- @license CC0-1.0 -->
+
+# Badge
+
+A prominent label that indicates a category, property or instruction.
+
+## Guidelines
+
+- Let the badge contain a short text or a number.
+- Use any of the feedback or highlight [colours](/docs/brand-design-tokens-colour--docs) for the background.
+  The default is green.
+  Red, orange and green are especially useful for showing an error, warning, or success.
+  However, they can also be combined with the other colours without carrying these meanings.
+`;/*@license CC0-1.0*/function r(e){return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(s,{of:d}),`
+`,n.jsx(a,{children:m}),`
+`,n.jsx(i,{}),`
+`,n.jsx(c,{})]})}function j(e={}){const{wrapper:o}={...t(),...e.components};return o?n.jsx(o,{...e,children:n.jsx(r,{...e})}):r()}export{j as default};
---
+++ demo-DES-1358-add-form-page-examples/assets/PageHeading.docs-D_g-IZLo.js
@@ -0,0 +1,9 @@
+import{j as e}from"./iframe-Cw142W4C.js";import{useMDXComponents as s}from"./index-BbPQzUg-.js";import{M as r,c as i,P as a}from"./blocks-BNfQ1e_S.js";import{P as m}from"./PageHeading.stories-Dw9PQ4nl.js";import{S as d}from"./StatusBadge-BGUN5SLQ.js";import"./preload-helper-PPVm8Dsz.js";import"./index-C5Zrf72m.js";import"./Badge-DzVxPJE2.js";const p=`<!-- @license CC0-1.0 -->
+
+# Page Heading
+`;/*@license CC0-1.0*/function o(n){return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(d,{description:"We no longer use this size of headings. Use the Heading component with level 1 instead.",status:"deprecated"}),`
+`,e.jsx(r,{of:m}),`
+`,e.jsx(i,{children:p}),`
+`,e.jsx(a,{})]})}function h(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{h as default};
