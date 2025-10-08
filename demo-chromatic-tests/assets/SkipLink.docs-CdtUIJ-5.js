7182aa9906d0c271412d25b63030f4c443c8bab6
Evi-2003 <69012052+Evi-2003@users.noreply.github.com>
2025-10-08 12:22:19 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@ab9ba7131b9ac8caa8536412826006b15a9c9378 🚀

---
+++ demo-chromatic-tests/assets/SkipLink.docs-CdtUIJ-5.js
@@ -0,0 +1,35 @@
+import{j as n}from"./iframe-k_XWJsh-.js";import{useMDXComponents as a}from"./index-ebd1G5cu.js";import{M as o,c as r,P as p,e as c,f as t}from"./blocks-CSo8NAJf.js";import{S as l,O as h,M as m}from"./SkipLink.stories-3m_3ymks.js";import"./preload-helper-PPVm8Dsz.js";import"./index-Cv7uoS_W.js";const d=`<!-- @license CC0-1.0 -->
+
+# Skip Link
+
+Allows skipping past recurring navigation blocks at the top of a page.
+
+## Design
+
+The Skip Link sits above the Page Header and is as wide as the Page container.
+It remains hidden until activated with the ‘Tab’ key.
+After appearing, it pushes the rest of the page down.
+
+## Guidelines
+
+- Place the Skip Link as the first element in the [Page](/docs/components-containers-page--docs) container.
+- Direct the Skip Link to the main content area on the Page.
+  Typically, this is the \`main\` HTML element.
+  Repeated navigation blocks such as Breadcrumbs and back Links do not belong to the main content area.
+- Set \`id="inhoud"\` on the \`main\` HTML element and then use \`href="#inhoud"\` on the Skip Link.
+- Complex pages with multiple sections may benefit from more than one Skip Link.
+  If you need multiple words to describe a Skip Link target, use kebab case (\`id="op-deze-manier"\`).
+- Skip Links are unnecessary on a simple page with only text and minimal navigation.
+`;/*@license CC0-1.0*/function s(i){const e={h2:"h2",h3:"h3",p:"p",...a(),...i.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(o,{of:l}),`
+`,n.jsx(r,{children:d}),`
+`,n.jsx(p,{}),`
+`,n.jsx(c,{}),`
+`,n.jsx(e.h2,{id:"examples",children:"Examples"}),`
+`,n.jsx(e.h3,{id:"display-on-focus",children:"Display on focus"}),`
+`,n.jsx(e.p,{children:"A Skip Link is only displayed when it receives focus."}),`
+`,n.jsx(t,{of:h}),`
+`,n.jsx(e.h3,{id:"multiple-links",children:"Multiple links"}),`
+`,n.jsx(e.p,{children:"You can use more than one Skip Link if you have a complex page with multiple sections."}),`
+`,n.jsx(t,{of:m})]})}function L(i={}){const{wrapper:e}={...a(),...i.components};return e?n.jsx(e,{...i,children:n.jsx(s,{...i})}):s(i)}export{L as default};
