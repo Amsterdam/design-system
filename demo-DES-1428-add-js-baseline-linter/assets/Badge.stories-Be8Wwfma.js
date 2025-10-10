2f33dd3fc53c1a9e4348495e19aa77b3cc89f6d4
GitHub <github-action@users.noreply.github.com>
2025-10-10 17:36:20 +0000

    Remove feature branch folder

--- demo-DES-1428-add-js-baseline-linter/assets/Badge.stories-Be8Wwfma.js
+++
@@ -1,4 +0,0 @@
-import{b as o,B as r}from"./Badge-C_kfc7y7.js";/**
- * @license EUPL-1.2+
- * Copyright Gemeente Amsterdam
- */const a={title:"Components/Feedback/Badge",component:r,args:{label:"Tip"},argTypes:{color:{control:{labels:{undefined:"green (default)"}},options:[void 0,...o]}}},e={};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:"{}",...e.parameters?.docs?.source}}};const t=["Default"],l=Object.freeze(Object.defineProperty({__proto__:null,Default:e,__namedExportsOrder:t,default:a},Symbol.toStringTag,{value:"Module"}));export{l as B};
--- demo-DES-1428-add-js-baseline-linter/assets/config-DzxqTJ2f.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./iframe-YwxVZ8Zw.js";import{L as r}from"./Layout-fwLFchcF.js";const a={decorators:[o=>e.jsx(r,{children:e.jsx(o,{})})],parameters:{layout:"fullscreen",themes:{themeOverride:"Spacious"}}};export{a as c};
