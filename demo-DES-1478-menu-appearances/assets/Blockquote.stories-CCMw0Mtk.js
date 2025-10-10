a2a94303d85827eee1fc8e12040f3d0e5b519723
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-10-10 14:17:47 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@ef1bf1e1b7c5ef068fdf86ff0b48049da58a09d1 🚀

---
+++ demo-DES-1478-menu-appearances/assets/Blockquote.stories-CCMw0Mtk.js
@@ -0,0 +1,8 @@
+import{c as n}from"./exampleContent-DRope23M.js";import{r as p,j as i,c as u}from"./iframe-BqRUDbT-.js";const r=p.forwardRef(({children:t,className:a,color:s,...c},l)=>i.jsx("blockquote",{...c,className:u("ams-blockquote",s&&`ams-blockquote--${s}`,a),ref:l,children:t}));r.displayName="Blockquote";try{r.displayName="Blockquote",r.__docgenInfo={description:"",displayName:"Blockquote",props:{}}}catch{}/**
+ * @license EUPL-1.2+
+ * Copyright Gemeente Amsterdam
+ */const d=n(),m={title:"Components/Text/Blockquote",component:r,args:{children:d},argTypes:{children:{description:"The text for the quote.",table:{disable:!1}},color:{control:{labels:{undefined:"default"},type:"radio"},options:[void 0,"inverse"]}}},e={},o={args:{color:"inverse"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:"{}",...e.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    color: 'inverse'
+  }
+}`,...o.parameters?.docs?.source}}};const f=["Default","InverseColour"],g=Object.freeze(Object.defineProperty({__proto__:null,Default:e,InverseColour:o,__namedExportsOrder:f,default:m},Symbol.toStringTag,{value:"Module"}));export{g as B,o as I};
---
+++ demo-DES-1478-menu-appearances/assets/config-BoCe1kvV.js
@@ -0,0 +1,1 @@
+import{j as e}from"./iframe-BqRUDbT-.js";import{L as r}from"./Layout-CTVUihUD.js";const a={decorators:[o=>e.jsx(r,{children:e.jsx(o,{})})],parameters:{layout:"fullscreen",themes:{themeOverride:"Spacious"}}};export{a as c};
