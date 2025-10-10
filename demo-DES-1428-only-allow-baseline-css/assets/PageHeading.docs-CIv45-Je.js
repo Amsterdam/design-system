fda0c3c523ddd153138f0386fd31390a460d4bfe
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-10-10 17:38:33 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@064c65416710c8c2ae6ccaf10db8bfbad19e9aad 🚀

---
+++ demo-DES-1428-only-allow-baseline-css/assets/PageHeading.docs-CIv45-Je.js
@@ -0,0 +1,9 @@
+import{j as e}from"./iframe-C2k6py1P.js";import{useMDXComponents as s}from"./index-BD1rKJ4A.js";import{M as r,c as i,P as a}from"./blocks-DR5mYyxG.js";import{P as m}from"./PageHeading.stories-88MZydg8.js";import{S as d}from"./StatusBadge-D60we5aq.js";import"./preload-helper-PPVm8Dsz.js";import"./index-BVqR3x5g.js";import"./Badge-CYqAw2bm.js";const p=`<!-- @license CC0-1.0 -->
+
+# Page Heading
+`;/*@license CC0-1.0*/function o(n){return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(d,{description:"We no longer use this size of headings. Use the Heading component with level 1 instead.",status:"deprecated"}),`
+`,e.jsx(r,{of:m}),`
+`,e.jsx(i,{children:p}),`
+`,e.jsx(a,{})]})}function h(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{h as default};
---
+++ demo-DES-1428-only-allow-baseline-css/assets/VisuallyHidden.docs-BCbbnyFg.js
@@ -0,0 +1,20 @@
+import{j as n}from"./iframe-C2k6py1P.js";import{useMDXComponents as o}from"./index-BD1rKJ4A.js";import{M as t,c as r,P as i,e as l}from"./blocks-DR5mYyxG.js";import{V as u}from"./VisuallyHidden.stories-DTSVJO-h.js";import"./preload-helper-PPVm8Dsz.js";import"./index-BVqR3x5g.js";const c=`<!-- @license CC0-1.0 -->
+
+# Visually Hidden
+
+Hides content from sighted users but keeps it accessible to non-visual user agents, such as screen readers.
+
+## Class name
+
+\`ams-visually-hidden\`
+
+## Guidelines
+
+- In most cases, visually available content should be accessible to non-visual user agents and vice versa.
+  Elaborate instructions or guidance read only by non-visual user agents can do more harm than good.
+`;/*@license CC0-1.0*/function a(s){return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(t,{of:u}),`
+`,n.jsx(r,{children:c}),`
+`,n.jsx(i,{}),`
+`,n.jsx(l,{})]})}function j(s={}){const{wrapper:e}={...o(),...s.components};return e?n.jsx(e,{...s,children:n.jsx(a,{...s})}):a()}export{j as default};
