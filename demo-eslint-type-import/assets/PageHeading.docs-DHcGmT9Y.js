ad7ab259b2f8d40439b737215014e1f57ee63c2a
Evi-2003 <69012052+Evi-2003@users.noreply.github.com>
2025-10-10 10:01:50 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@4991c211e9fa30c51f80810534a87ca71c73926a 🚀

--- demo-eslint-type-import/assets/PageHeading.docs-DHcGmT9Y.js
+++
@@ -1,9 +0,0 @@
-import{j as e}from"./iframe-CNvg2dH7.js";import{useMDXComponents as s}from"./index-DuE5WXla.js";import{M as r,c as i,P as a}from"./blocks-Cl36Pg1D.js";import{P as m}from"./PageHeading.stories-DFbOC4vb.js";import{S as d}from"./StatusBadge-DHyT-ON7.js";import"./preload-helper-PPVm8Dsz.js";import"./index-C98Oe4p_.js";import"./Badge-DxPQIWEi.js";const p=`<!-- @license CC0-1.0 -->
-
-# Page Heading
-`;/*@license CC0-1.0*/function o(n){return e.jsxs(e.Fragment,{children:[`
-`,`
-`,e.jsx(d,{description:"We no longer use this size of headings. Use the Heading component with level 1 instead.",status:"deprecated"}),`
-`,e.jsx(r,{of:m}),`
-`,e.jsx(i,{children:p}),`
-`,e.jsx(a,{})]})}function h(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{h as default};
