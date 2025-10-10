a8262d3c6cbd7874410eba30b4dbe8b6097a5dc5
Vincent Smedinga <v.smedinga@amsterdam.nl>
2025-10-10 22:00:32 +0200

    Add application layout option to Page

--- storybook/src/components/Page/Page.stories.tsx
+++ storybook/src/components/Page/Page.stories.tsx
@@ -5,18 +5,27 @@
 
 import type { Meta, StoryObj } from '@storybook/react-vite'
 
+import { Grid, Heading, Menu, PageFooter, PageHeader } from '@amsterdam/design-system-react'
+import { SettingsFillIcon } from '@amsterdam/design-system-react-icons'
 import { Page } from '@amsterdam/design-system-react/src'
 
 const meta = {
   title: 'Components/Containers/Page',
   component: Page,
   args: {
-    className: 'ams-docs-page',
     lang: 'nl',
   },
   argTypes: {
     className: {
       table: { disable: true },
+    },
+    layout: {
+      control: {
+        labels: { undefined: 'website (default)' },
+      },
+      options: [undefined, 'application'],
+      // Changing this property can result in an invalid appearance.
+      table: { readonly: true },
     },
   },
   parameters: {
@@ -28,8 +37,62 @@ export default meta
 
 type Story = StoryObj<typeof meta>
 
+const PageBody = () => (
+  <Grid paddingBottom="x-large">
+    <Grid.Cell span="all">
+      <Heading level={1}>Page Body</Heading>
+    </Grid.Cell>
+  </Grid>
+)
+
 export const Default: Story = {
   args: {
-    children: <div className="ams-docs-item" />,
+    children: [
+      <PageHeader brandName="Page Header" key="header" noMenuButtonOnWideWindow />,
+      <main id="inhoud" key="main">
+        <PageBody />
+      </main>,
+      <PageFooter key="footer">
+        <PageFooter.Menu>
+          <PageFooter.MenuLink href="/">Page Footer</PageFooter.MenuLink>
+        </PageFooter.Menu>
+      </PageFooter>,
+    ],
   },
+}
+
+export const WithSideMenu: Story = {
+  args: {
+    children: <PageBody />,
+    lang: 'nl',
+    layout: 'application',
+  },
+  globals: {
+    theme: 'Compact',
+  },
+  render: ({ children, ...args }) => (
+    <Page {...args}>
+      <Menu inWideWindow>
+        <Menu.Link color="inverse" href="#" icon={<SettingsFillIcon />}>
+          Menu item
+        </Menu.Link>
+      </Menu>
+      <PageHeader brandName="Page Header" noMenuButtonOnWideWindow>
+        <Menu>
+          <Menu.Link color="inverse" href="#" icon={<SettingsFillIcon />}>
+            Menu item
+          </Menu.Link>
+        </Menu>
+      </PageHeader>
+      <main id="inhoud">{children}</main>
+      <PageFooter>
+        <Heading className="ams-visually-hidden" level={2}>
+          Over deze website
+        </Heading>
+        <PageFooter.Menu>
+          <PageFooter.MenuLink href="/">Page Footer</PageFooter.MenuLink>
+        </PageFooter.Menu>
+      </PageFooter>
+    </Page>
+  ),
 }
