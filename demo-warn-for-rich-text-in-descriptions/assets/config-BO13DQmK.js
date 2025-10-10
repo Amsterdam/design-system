e7dedebb8b3abc56ba58b5c33024127a25cedea5
alimpens <37216945+alimpens@users.noreply.github.com>
2025-10-10 09:15:22 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@ce725f97e8ce8c0eb80abd2365cc75dd9011be0a 🚀

--- demo-warn-for-rich-text-in-descriptions/assets/config-BO13DQmK.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./iframe-ARQpVmVY.js";import{L as r}from"./Layout-eIKoSGMi.js";const a={decorators:[o=>e.jsx(r,{children:e.jsx(o,{})})],parameters:{layout:"fullscreen",themes:{themeOverride:"Spacious"}}};export{a as c};
