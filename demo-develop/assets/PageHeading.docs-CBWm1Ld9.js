1907eff6521b3297187d1035a5a5c6c59f932bac
alimpens <37216945+alimpens@users.noreply.github.com>
2025-10-10 10:30:15 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@6cf5001f9cbba9cce648d61cb5989a9cd7bbed80 🚀

---
+++ demo-develop/assets/PageHeading.docs-CBWm1Ld9.js
@@ -0,0 +1,9 @@
+import{j as e}from"./iframe-THRD_0Xr.js";import{useMDXComponents as s}from"./index-C1OiZOxS.js";import{M as r,c as i,P as a}from"./blocks-z1rDmCjg.js";import{P as m}from"./PageHeading.stories-DH7kUWKF.js";import{S as d}from"./StatusBadge-B_zsHN7l.js";import"./preload-helper-PPVm8Dsz.js";import"./index-CEcyqVMl.js";import"./Badge-Cg3uuNC9.js";const p=`<!-- @license CC0-1.0 -->
+
+# Page Heading
+`;/*@license CC0-1.0*/function o(n){return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(d,{description:"We no longer use this size of headings. Use the Heading component with level 1 instead.",status:"deprecated"}),`
+`,e.jsx(r,{of:m}),`
+`,e.jsx(i,{children:p}),`
+`,e.jsx(a,{})]})}function h(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{h as default};
---
+++ demo-develop/assets/config-D1GmC8GQ.js
@@ -0,0 +1,1 @@
+import{j as e}from"./iframe-THRD_0Xr.js";import{L as r}from"./Layout-BH7dtPnA.js";const a={decorators:[o=>e.jsx(r,{children:e.jsx(o,{})})],parameters:{layout:"fullscreen",themes:{themeOverride:"Spacious"}}};export{a as c};
