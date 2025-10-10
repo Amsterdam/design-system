3cfdb4fe3ac8ae19b1b2e417c9e865f13d560c66
GitHub <github-action@users.noreply.github.com>
2025-10-10 14:05:21 +0000

    Remove feature branch folder

--- demo-warn-for-rich-text-in-descriptions/assets/PageHeading.docs-oTO45bJ7.js
+++
@@ -1,9 +0,0 @@
-import{j as e}from"./iframe-TTA3ZgLo.js";import{useMDXComponents as s}from"./index-AORhgz0E.js";import{M as r,c as i,P as a}from"./blocks-C5gFCr0j.js";import{P as m}from"./PageHeading.stories-Cq7aWUB-.js";import{S as d}from"./StatusBadge-xEmJIqHn.js";import"./preload-helper-PPVm8Dsz.js";import"./index-CuH22NVM.js";import"./Badge-JK3MI4WK.js";const p=`<!-- @license CC0-1.0 -->
-
-# Page Heading
-`;/*@license CC0-1.0*/function o(n){return e.jsxs(e.Fragment,{children:[`
-`,`
-`,e.jsx(d,{description:"We no longer use this size of headings. Use the Heading component with level 1 instead.",status:"deprecated"}),`
-`,e.jsx(r,{of:m}),`
-`,e.jsx(i,{children:p}),`
-`,e.jsx(a,{})]})}function h(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{h as default};
