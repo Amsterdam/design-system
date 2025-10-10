7c163cc76e4744ccf7a7bea06cbccd562d7345d9
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-10-10 14:00:06 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@33f42f1e3d6031093fc9dbbb724ffca93ac168e6 🚀

--- demo-DES-1478-no-page-in-page/assets/PageHeading.docs-DWkwgMKe.js
+++
@@ -1,9 +0,0 @@
-import{j as e}from"./iframe-iwTNEnQf.js";import{useMDXComponents as s}from"./index-B4YQBYuC.js";import{M as r,c as i,P as a}from"./blocks-BRkGOEqg.js";import{P as m}from"./PageHeading.stories-DtM0PiCd.js";import{S as d}from"./StatusBadge-Ctg6XQfo.js";import"./preload-helper-PPVm8Dsz.js";import"./index-iEab56j6.js";import"./Badge-D2OZ-rb8.js";const p=`<!-- @license CC0-1.0 -->
-
-# Page Heading
-`;/*@license CC0-1.0*/function o(n){return e.jsxs(e.Fragment,{children:[`
-`,`
-`,e.jsx(d,{description:"We no longer use this size of headings. Use the Heading component with level 1 instead.",status:"deprecated"}),`
-`,e.jsx(r,{of:m}),`
-`,e.jsx(i,{children:p}),`
-`,e.jsx(a,{})]})}function h(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{h as default};
---
+++ demo-DES-1478-no-page-in-page/assets/config-CJ5eBMhK.js
@@ -0,0 +1,1 @@
+import{j as e}from"./iframe-Dj6r61k8.js";import{L as r}from"./Layout-pigJJl5G.js";const a={decorators:[o=>e.jsx(r,{children:e.jsx(o,{})})],parameters:{layout:"fullscreen",themes:{themeOverride:"Spacious"}}};export{a as c};
