20c68d535a1409ed1ba7fb13edeb9e5c4e852394
GitHub <github-action@users.noreply.github.com>
2025-10-10 17:39:57 +0000

    Remove feature branch folder

--- demo-DES-1428-only-allow-baseline-css/assets/config-DbLvSv5_.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./iframe-C2k6py1P.js";import{L as r}from"./Layout-C7kYrfk5.js";const a={decorators:[o=>e.jsx(r,{children:e.jsx(o,{})})],parameters:{layout:"fullscreen",themes:{themeOverride:"Spacious"}}};export{a as c};
