76acb22dec932cfafff1e8f13337aff80c85d186
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-10-10 18:00:27 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@8fa1996952cf2c8565cb37e387fb3302afa3a837 🚀

---
+++ demo-hide-some-linter-warnings/assets/config-DXu5xAbv.js
@@ -0,0 +1,1 @@
+import{j as e}from"./iframe-DfciR-qF.js";import{L as r}from"./Layout-C_sWyjaa.js";const a={decorators:[o=>e.jsx(r,{children:e.jsx(o,{})})],parameters:{layout:"fullscreen",themes:{themeOverride:"Spacious"}}};export{a as c};
