2ffdff54f0c187161f9b000e17c64820d12277a0
Evi-2003 <69012052+Evi-2003@users.noreply.github.com>
2025-10-10 14:28:59 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@3b6ea34c4a803db27a848408f3ee66fa1e8bac11 🚀

---
+++ demo-chromatic-tests/assets/PageHeading.docs-Oca3UyFY.js
@@ -0,0 +1,9 @@
+import{j as e}from"./iframe-DtKGlMJd.js";import{useMDXComponents as s}from"./index-EnS2OQJD.js";import{M as r,c as i,P as a}from"./blocks-DL643QrC.js";import{P as m}from"./PageHeading.stories-DdDLtU5q.js";import{S as d}from"./StatusBadge-Cuj22ZhW.js";import"./preload-helper-PPVm8Dsz.js";import"./index-bK6Gc0MB.js";import"./Badge-C0Y7e0pm.js";const p=`<!-- @license CC0-1.0 -->
+
+# Page Heading
+`;/*@license CC0-1.0*/function o(n){return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(d,{description:"We no longer use this size of headings. Use the Heading component with level 1 instead.",status:"deprecated"}),`
+`,e.jsx(r,{of:m}),`
+`,e.jsx(i,{children:p}),`
+`,e.jsx(a,{})]})}function h(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{h as default};
