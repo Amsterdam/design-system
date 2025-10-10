411b8f445b39dc5c86e4521bc18a6648fbd6baca
Evi-2003 <69012052+Evi-2003@users.noreply.github.com>
2025-10-10 14:48:53 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@8ea7fa837d5af069f4a18cc431ea27d1d49b973b 🚀

---
+++ demo-chromatic-tests/assets/PageHeading.docs-BjlNbvP_.js
@@ -0,0 +1,9 @@
+import{j as e}from"./iframe-CGPXWMMA.js";import{useMDXComponents as s}from"./index-I2Yz8fwR.js";import{M as r,c as i,P as a}from"./blocks-DODgyO2H.js";import{P as m}from"./PageHeading.stories-D2I3Fjmq.js";import{S as d}from"./StatusBadge-BvldpTL8.js";import"./preload-helper-PPVm8Dsz.js";import"./index-dPssJX1E.js";import"./Badge-CuibQXtQ.js";const p=`<!-- @license CC0-1.0 -->
+
+# Page Heading
+`;/*@license CC0-1.0*/function o(n){return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(d,{description:"We no longer use this size of headings. Use the Heading component with level 1 instead.",status:"deprecated"}),`
+`,e.jsx(r,{of:m}),`
+`,e.jsx(i,{children:p}),`
+`,e.jsx(a,{})]})}function h(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{h as default};
--- demo-chromatic-tests/assets/PageHeading.docs-DaafueOe.js
+++
@@ -1,9 +0,0 @@
-import{j as e}from"./iframe-cpnLOAqp.js";import{useMDXComponents as s}from"./index-D6H31vBQ.js";import{M as r,c as i,P as a}from"./blocks-CVhr-4kS.js";import{P as m}from"./PageHeading.stories-DuWRLKdz.js";import{S as d}from"./StatusBadge-decoKDV3.js";import"./preload-helper-PPVm8Dsz.js";import"./index-C6tcZkzE.js";import"./Badge-CenMoujS.js";const p=`<!-- @license CC0-1.0 -->
-
-# Page Heading
-`;/*@license CC0-1.0*/function o(n){return e.jsxs(e.Fragment,{children:[`
-`,`
-`,e.jsx(d,{description:"We no longer use this size of headings. Use the Heading component with level 1 instead.",status:"deprecated"}),`
-`,e.jsx(r,{of:m}),`
-`,e.jsx(i,{children:p}),`
-`,e.jsx(a,{})]})}function h(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{h as default};
