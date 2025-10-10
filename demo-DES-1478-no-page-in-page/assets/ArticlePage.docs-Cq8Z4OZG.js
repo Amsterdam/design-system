84b8873284bca2ab1d42a44231f040e85bf94824
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-10-10 13:56:24 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@bc30e27148f15381a7b28ae8c9915ecc85ce5b33 🚀

---
+++ demo-DES-1478-no-page-in-page/assets/ArticlePage.docs-Cq8Z4OZG.js
@@ -0,0 +1,15 @@
+import{j as e}from"./iframe-iwTNEnQf.js";import{useMDXComponents as r}from"./index-B4YQBYuC.js";import{M as s}from"./blocks-BRkGOEqg.js";import{A as o}from"./ArticlePage.stories-DNTQJI6M.js";import"./preload-helper-PPVm8Dsz.js";import"./index-iEab56j6.js";import"./config-ChOxRo_Y.js";import"./Layout-kUAPiPtB.js";import"./PageFooter.stories-7laje2Oz.js";import"./common-DrnyCu3t.js";import"./HomeNews-Dd9U_LGW.js";/*@license CC0-1.0*/function t(i){const n={h1:"h1",h2:"h2",li:"li",p:"p",ul:"ul",...r(),...i.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(s,{of:o}),`
+`,e.jsx(n.h1,{id:"article-page",children:"Article page"}),`
+`,e.jsx(n.p,{children:"This page type is for news or similar kinds of articles."}),`
+`,e.jsx(n.h2,{id:"headings",children:"Headings"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Use a level 1 Heading for the title of the article."}),`
+`,e.jsx(n.li,{children:"Use level 2 Headings for the section titles."}),`
+`]}),`
+`,e.jsx(n.h2,{id:"spacing",children:"Spacing"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Add a small margin bottom to a Heading 2 with following body text."}),`
+`,e.jsx(n.li,{children:"Use an extra large margin between sections."}),`
+`]})]})}function u(i={}){const{wrapper:n}={...r(),...i.components};return n?e.jsx(n,{...i,children:e.jsx(t,{...i})}):t(i)}export{u as default};
---
+++ demo-DES-1478-no-page-in-page/assets/TableOfContents.docs-BU7XzJ74.js
@@ -0,0 +1,15 @@
+import{j as n}from"./iframe-iwTNEnQf.js";import{useMDXComponents as o}from"./index-B4YQBYuC.js";import{M as r,c as i,P as a,e as l,f as m}from"./blocks-BRkGOEqg.js";import{T as c,M as p}from"./TableOfContents.stories-DcCW7XuO.js";import"./preload-helper-PPVm8Dsz.js";import"./index-iEab56j6.js";import"./Heading-CQrjvCgy.js";const x=`<!-- @license CC0-1.0 -->
+
+# Table of Contents
+
+A list of links corresponding to the content sections on the page.
+It helps users to easily navigate to different sections on the same page.
+`;/*@license CC0-1.0*/function s(e){const t={h2:"h2",h3:"h3",...o(),...e.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(r,{of:c}),`
+`,n.jsx(i,{children:x}),`
+`,n.jsx(a,{}),`
+`,n.jsx(l,{}),`
+`,n.jsx(t.h2,{id:"examples",children:"Examples"}),`
+`,n.jsx(t.h3,{id:"multiple-levels",children:"Multiple levels"}),`
+`,n.jsx(m,{of:p})]})}function g(e={}){const{wrapper:t}={...o(),...e.components};return t?n.jsx(t,{...e,children:n.jsx(s,{...e})}):s(e)}export{g as default};
