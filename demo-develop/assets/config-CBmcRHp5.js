acce1318d6b439d8e1cf208c85cccab9e54785bf
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-10-13 12:50:23 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@638c4bf413debf7c210eb3ac8363e072f6d3d989 🚀

--- demo-develop/assets/config-CBmcRHp5.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./iframe-DvrD6MAE.js";import{L as r}from"./Layout-B2JN3NCJ.js";const a={decorators:[o=>e.jsx(r,{children:e.jsx(o,{})})],parameters:{layout:"fullscreen",themes:{themeOverride:"Spacious"}}};export{a as c};
