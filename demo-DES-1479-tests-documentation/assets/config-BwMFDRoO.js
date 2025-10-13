793bbdf2a40860c66c0eb9989820501e52bbcf64
Evi-2003 <69012052+Evi-2003@users.noreply.github.com>
2025-10-13 12:58:57 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@f4466c81dd7798f180262d9f7ce165cc41335952 🚀

---
+++ demo-DES-1479-tests-documentation/assets/config-BwMFDRoO.js
@@ -0,0 +1,1 @@
+import{j as e}from"./iframe-DozIysMD.js";import{L as r}from"./Layout-BR-N6ZsX.js";const a={decorators:[o=>e.jsx(r,{children:e.jsx(o,{})})],parameters:{layout:"fullscreen",themes:{themeOverride:"Spacious"}}};export{a as c};
--- demo-DES-1479-tests-documentation/assets/config-CkGPcT_U.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./iframe-DxgsTXqA.js";import{L as r}from"./Layout-D7bg95ZS.js";const a={decorators:[o=>e.jsx(r,{children:e.jsx(o,{})})],parameters:{layout:"fullscreen",themes:{themeOverride:"Spacious"}}};export{a as c};
