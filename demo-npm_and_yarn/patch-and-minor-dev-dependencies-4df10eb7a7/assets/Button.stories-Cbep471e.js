1941d6ec34c45772a2dd5e69f5cf5d95efd6e8e2
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-10-13 12:47:34 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@18e06af030e170e9d18a9d3f3a4844cffae17189 🚀

---
+++ demo-npm_and_yarn/patch-and-minor-dev-dependencies-4df10eb7a7/assets/Button.stories-Cbep471e.js
@@ -0,0 +1,38 @@
+import{f as d,j as c,e as i}from"./iframe-DxgsTXqA.js";import{B as p}from"./Button-BYTvWzlI.js";const u={title:"Components/Buttons/Button",component:p,args:{children:"Versturen",disabled:!1,icon:void 0,iconBefore:!1,iconOnly:void 0,variant:"primary"},argTypes:{disabled:{description:"Prevents interaction. Avoid if possible."},icon:{control:{labels:{undefined:"none"},type:"select"},mapping:d,options:[void 0,...Object.keys(d)]},iconBefore:{control:{type:"boolean"},if:{arg:"icon"}},iconOnly:{control:{type:"boolean"},if:{arg:"icon"}}}},r={},e={args:{children:"Annuleren",variant:"secondary"}},n={args:{children:"Terug",variant:"tertiary"}},o={args:{children:"Sluiten",icon:c.jsx(i,{})}},a={args:{children:"Sluiten",icon:c.jsx(i,{}),iconBefore:!0}},s={args:{children:"Sluiten",icon:c.jsx(i,{}),iconOnly:!0,variant:"tertiary"}},t={args:{children:"Vergunningsaanvraag verzenden"},decorators:[l=>c.jsx("div",{style:{maxWidth:"16rem"},children:c.jsx(l,{})})]};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:"{}",...r.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Annuleren',
+    variant: 'secondary'
+  }
+}`,...e.parameters?.docs?.source}}};n.parameters={...n.parameters,docs:{...n.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Terug',
+    variant: 'tertiary'
+  }
+}`,...n.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Sluiten',
+    icon: <CloseIcon />
+  }
+}`,...o.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Sluiten',
+    icon: <CloseIcon />,
+    iconBefore: true
+  }
+}`,...a.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Sluiten',
+    icon: <CloseIcon />,
+    iconOnly: true,
+    variant: 'tertiary'
+  }
+}`,...s.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Vergunningsaanvraag verzenden'
+  },
+  decorators: [Story => <div style={{
+    maxWidth: '16rem'
+  }}>
+        <Story />
+      </div>]
+}`,...t.parameters?.docs?.source}}};const m=["Primary","Secondary","Tertiary","WithIcon","WithIconBefore","WithIconOnly","TextWrapping"],h=Object.freeze(Object.defineProperty({__proto__:null,Primary:r,Secondary:e,Tertiary:n,TextWrapping:t,WithIcon:o,WithIconBefore:a,WithIconOnly:s,__namedExportsOrder:m,default:u},Symbol.toStringTag,{value:"Module"}));export{h as B,r as P,e as S,n as T,o as W,a,s as b,t as c};
---
+++ demo-npm_and_yarn/patch-and-minor-dev-dependencies-4df10eb7a7/assets/PageHeading.docs-BM6dvgOo.js
@@ -0,0 +1,9 @@
+import{j as e}from"./iframe-DxgsTXqA.js";import{useMDXComponents as s}from"./index-CvUPfs8z.js";import{M as r,c as i,P as a}from"./blocks-C76MHHGv.js";import{P as m}from"./PageHeading.stories-B2AJCX-1.js";import{S as d}from"./StatusBadge-BvD2UYGg.js";import"./preload-helper-PPVm8Dsz.js";import"./index-BNkgKHju.js";import"./Badge-Wt-LIX4i.js";const p=`<!-- @license CC0-1.0 -->
+
+# Page Heading
+`;/*@license CC0-1.0*/function o(n){return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(d,{description:"We no longer use this size of headings. Use the Heading component with level 1 instead.",status:"deprecated"}),`
+`,e.jsx(r,{of:m}),`
+`,e.jsx(i,{children:p}),`
+`,e.jsx(a,{})]})}function h(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{h as default};
