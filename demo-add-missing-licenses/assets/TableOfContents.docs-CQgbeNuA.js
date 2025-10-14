6c69c93e459b0d93e7d39fd62cd7200c8da648b1
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-10-14 12:27:45 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@9b601ee4879f2ba7cdd53c2a8938befc708fde5c 🚀

---
+++ demo-add-missing-licenses/assets/TableOfContents.docs-CQgbeNuA.js
@@ -0,0 +1,15 @@
+import{j as n}from"./iframe-Cgqer_r5.js";import{useMDXComponents as o}from"./index-CzSmftRn.js";import{M as r,c as i,P as a,e as l,f as m}from"./blocks-ANILr-dE.js";import{T as c,M as p}from"./TableOfContents.stories-DQaXW_Os.js";import"./preload-helper-PPVm8Dsz.js";import"./index-CHgTgA_6.js";import"./Heading-Bjoz_C-K.js";const x=`<!-- @license CC0-1.0 -->
+
+# Table of Contents
+
+A list of links corresponding to the content sections on the page.
+It helps users to easily navigate to different sections on the same page.
+`;/*@license CC0-1.0*/function s(e){const t={h2:"h2",h3:"h3",...o(),...e.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(r,{of:c}),`
+`,n.jsx(i,{children:x}),`
+`,n.jsx(a,{}),`
+`,n.jsx(l,{}),`
+`,n.jsx(t.h2,{id:"examples",children:"Examples"}),`
+`,n.jsx(t.h3,{id:"multiple-levels",children:"Multiple levels"}),`
+`,n.jsx(m,{of:p})]})}function g(e={}){const{wrapper:t}={...o(),...e.components};return t?n.jsx(t,{...e,children:n.jsx(s,{...e})}):s(e)}export{g as default};
