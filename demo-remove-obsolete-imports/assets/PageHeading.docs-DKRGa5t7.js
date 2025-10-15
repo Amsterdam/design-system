cff4934ec37789908587bb87d0400cfd560af168
GitHub <github-action@users.noreply.github.com>
2025-10-15 08:08:41 +0000

    Remove feature branch folder

--- demo-remove-obsolete-imports/assets/PageHeading.docs-DKRGa5t7.js
+++
@@ -1,9 +0,0 @@
-import{j as e}from"./iframe-ChxcTCzm.js";import{useMDXComponents as s}from"./index-Bfk5D7iG.js";import{M as r,c as i,P as a}from"./blocks-BeIBnmao.js";import{P as m}from"./PageHeading.stories-Bu29NhrB.js";import{S as d}from"./StatusBadge-C4iLYc01.js";import"./preload-helper-PPVm8Dsz.js";import"./index-CDOTJ_82.js";import"./Badge-XD7wc1Vn.js";const p=`<!-- @license CC0-1.0 -->
-
-# Page Heading
-`;/*@license CC0-1.0*/function o(n){return e.jsxs(e.Fragment,{children:[`
-`,`
-`,e.jsx(d,{description:"We no longer use this size of headings. Use the Heading component with level 1 instead.",status:"deprecated"}),`
-`,e.jsx(r,{of:m}),`
-`,e.jsx(i,{children:p}),`
-`,e.jsx(a,{})]})}function h(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{h as default};
