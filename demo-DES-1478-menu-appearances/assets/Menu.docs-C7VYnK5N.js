817b8449afcfe2ef2e84f564edabc8e00ed8a7da
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-10-17 14:45:56 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@e42d7c065105dfb4bc648e9231149d25cc2ab4a5 🚀

---
+++ demo-DES-1478-menu-appearances/assets/Menu.docs-C7VYnK5N.js
@@ -0,0 +1,40 @@
+import{j as e}from"./iframe-BMyLMXlG.js";import{useMDXComponents as s}from"./index-CwnlgVhS.js";import{M as r,c as h,P as c,e as o,f as l}from"./blocks-CVbGDFtQ.js";import{M as d,L as i}from"./Menu.stories-BzEN7Bvr.js";import{S as p}from"./StatusBadge-COIPTEk5.js";import"./preload-helper-PPVm8Dsz.js";import"./index-DX9cii1J.js";import"./Icon-DOR7gjtE.js";import"./Badge-Dun1qz_D.js";const m=`<!-- @license CC0-1.0 -->
+
+# Menu
+
+A primary navigation leading to key areas of an application or website.
+
+## Design
+
+- The menu has a blue background.
+  Links have a white icon and a label.
+- In a narrow window, the menu occupies the full width.
+  The icon and label of the link sit next to each other.
+- In a wide window, the menu has a maximum width of 8rem.
+  It adds whitespace at the top to align with the baseline of Page Header.
+  The icon is larger and positioned above the label.
+
+## Guidelines
+
+- Include this component twice: one in the Page Header and one to the side of the Page.
+  Do not use it anywhere else.
+- Set the \`inWideWindow\` prop to the latter to ensure only one of them is visible, and to get the correct appearance.
+- Use filled icons for the links.
+`;/*@license CC0-1.0*/function a(t){const n={blockquote:"blockquote",h2:"h2",h3:"h3",p:"p",strong:"strong",...s(),...t.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,`
+`,e.jsx(p,{description:"We’re experimenting with this component; feedback is welcome. Expect changes in future iterations.",status:"beta"}),`
+`,e.jsx(r,{of:d}),`
+`,e.jsx(h,{children:m}),`
+`,e.jsxs(n.blockquote,{children:[`
+`,e.jsxs(n.p,{children:[e.jsx(n.strong,{children:"Note"}),`
+This component is responsive.
+The appearance of the examples depends on the width of your screen.
+On the page for the Story, you can change the size of the preview with the related button in the toolbar.`]}),`
+`]}),`
+`,e.jsx(c,{}),`
+`,e.jsx(o,{}),`
+`,e.jsx(n.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(n.h3,{id:"link",children:"Link"}),`
+`,e.jsx(l,{of:i}),`
+`,e.jsx(o,{of:i})]})}function y(t={}){const{wrapper:n}={...s(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(a,{...t})}):a(t)}export{y as default};
--- demo-DES-1478-menu-appearances/assets/PageHeading.docs-CClCUazt.js
+++
@@ -1,9 +0,0 @@
-import{j as e}from"./iframe-zc3QNYYF.js";import{useMDXComponents as s}from"./index-DMFiBhYz.js";import{M as r,c as i,P as a}from"./blocks-BSK3EnyX.js";import{P as m}from"./PageHeading.stories-Dwpsq0UZ.js";import{S as d}from"./StatusBadge-Bf8VBD5b.js";import"./preload-helper-PPVm8Dsz.js";import"./index-BK25MkhL.js";import"./Badge-CD_nUTJN.js";const p=`<!-- @license CC0-1.0 -->
-
-# Page Heading
-`;/*@license CC0-1.0*/function o(n){return e.jsxs(e.Fragment,{children:[`
-`,`
-`,e.jsx(d,{description:"We no longer use this size of headings. Use the Heading component with level 1 instead.",status:"deprecated"}),`
-`,e.jsx(r,{of:m}),`
-`,e.jsx(i,{children:p}),`
-`,e.jsx(a,{})]})}function h(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{h as default};
