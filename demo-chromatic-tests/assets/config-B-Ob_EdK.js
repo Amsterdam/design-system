f8707dae089a533038bb8d8fc8c82b02951f3360
Evi-2003 <69012052+Evi-2003@users.noreply.github.com>
2025-10-21 07:20:15 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@e226e88d15f3e672044e1de9e19c14c2ed767dcc 🚀

---
+++ demo-chromatic-tests/assets/config-B-Ob_EdK.js
@@ -0,0 +1,1 @@
+import{j as e}from"./iframe-BCdq_638.js";import{L as r}from"./Layout-BOJV2WM_.js";const a={decorators:[o=>e.jsx(r,{children:e.jsx(o,{})})],parameters:{layout:"fullscreen",themes:{themeOverride:"Spacious"}}};export{a as c};
