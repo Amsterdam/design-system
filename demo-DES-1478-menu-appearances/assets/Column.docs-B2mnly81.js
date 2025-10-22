9efb043f93450308b9e06c328abe90d711f027c4
alimpens <37216945+alimpens@users.noreply.github.com>
2025-10-22 15:25:54 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@42e498cb4170ab06acb72211f3f8892d8dab503d 🚀

---
+++ demo-DES-1478-menu-appearances/assets/Column.docs-B2mnly81.js
@@ -0,0 +1,47 @@
+import{j as e}from"./iframe-CAAVQ-0I.js";import{useMDXComponents as l}from"./index-D2at_Oqy.js";import{M as r,c as o,P as a,e as h,f as s}from"./blocks-CzcjltYR.js";import{C as c,A as d,H as m,I as x}from"./Column.stories-DVaYMQd7.js";import"./preload-helper-PPVm8Dsz.js";import"./index-B3mhh7C1.js";import"./types-yiKVj219.js";const j=`<!-- @license CC0-1.0 -->
+
+# Column
+
+Stacks its children vertically with gaps between them and offers alignment options.
+`;/*@license CC0-1.0*/function i(t){const n={a:"a",code:"code",h2:"h2",h3:"h3",li:"li",p:"p",strong:"strong",ul:"ul",...l(),...t.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(r,{of:c}),`
+`,e.jsx(o,{children:j}),`
+`,e.jsx(n.h2,{id:"guidelines",children:"Guidelines"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Wrap a Column around a set of elements that need the same amount of white space between them."}),`
+`,e.jsxs(n.li,{children:["Five sizes of ",e.jsx(n.a,{href:"/docs/brand-design-tokens-space--docs",children:"Space"}),` are available for the width or height of the gap.
+The medium gap is the default.`]}),`
+`,e.jsxs(n.li,{children:["To add white space below a single element, you can add a ",e.jsx(n.a,{href:"/docs/utilities-css-margin--docs",children:"Margin Bottom"})," instead."]}),`
+`,e.jsx(n.li,{children:"Align the elements horizontally or vertically through the alignment props."}),`
+`]}),`
+`,e.jsx(a,{}),`
+`,e.jsx(h,{}),`
+`,e.jsx(n.h2,{id:"examples",children:"Examples"}),`
+`,e.jsx(n.h3,{id:"alignment",children:"Alignment"}),`
+`,e.jsx(n.p,{children:"Items in the column can be aligned vertically in several ways:"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"Center"}),": center items within the column."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"End"}),": align items to the bottom of the column – to the bottom."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"Space Between"}),": distribute whitespace between items."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"Space Around"}),": distribute whitespace around items."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"Space Evenly"}),": distribute whitespace evenly around items."]}),`
+`]}),`
+`,e.jsxs(n.p,{children:["By default, items align to the ",e.jsx(n.strong,{children:"start"}),` of the column – at the top.
+This option has no class name or prop value.`]}),`
+`,e.jsx(n.p,{children:"This example centers three items vertically."}),`
+`,e.jsx(s,{of:d}),`
+`,e.jsx(n.h3,{id:"horizontal-alignment",children:"Horizontal alignment"}),`
+`,e.jsx(n.p,{children:"Items in the column can be aligned horizontally in several ways:"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"Start"}),": align items to the start of the column. This is the left side in left-to-right languages."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"Center"}),": center items within the column."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"End"}),": align items to the end of the column. This is the right side in left-to-right languages."]}),`
+`]}),`
+`,e.jsx(n.p,{children:"This example centers three items horizontally."}),`
+`,e.jsx(s,{of:m}),`
+`,e.jsx(n.h3,{id:"improve-semantics",children:"Improve semantics"}),`
+`,e.jsxs(n.p,{children:["By default, a Column renders a ",e.jsx(n.code,{children:"<div>"}),` element in HTML.
+Use the `,e.jsx(n.code,{children:"as"}),` prop to make your markup more semantic.
+In this example, the Column uses a `,e.jsx(n.code,{children:"<section>"})," element to group the heading and the cards."]}),`
+`,e.jsx(s,{of:x})]})}function C(t={}){const{wrapper:n}={...l(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(i,{...t})}):i(t)}export{C as default};
--- demo-DES-1478-menu-appearances/assets/config-1N9ORpl8.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./iframe-Cp1Xfnde.js";import{L as r}from"./Layout-E3Z8FzUb.js";const a={decorators:[o=>e.jsx(r,{children:e.jsx(o,{})})],parameters:{layout:"fullscreen",themes:{themeOverride:"Spacious"}}};export{a as c};
