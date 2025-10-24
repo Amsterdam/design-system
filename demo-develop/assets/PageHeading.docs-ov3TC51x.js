330308196ee0a84682145ea2a1ddb898f66aae5c
alimpens <37216945+alimpens@users.noreply.github.com>
2025-10-24 13:47:32 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@929c6bf825ddb73b0d35e1497ad74cef46f9f256 🚀

---
+++ demo-develop/assets/PageHeading.docs-ov3TC51x.js
@@ -0,0 +1,9 @@
+import{j as e}from"./iframe-bKfKBIrv.js";import{useMDXComponents as s}from"./index-D88bxNa4.js";import{M as r,c as i,P as a}from"./blocks-DsyMALAN.js";import{P as m}from"./PageHeading.stories-Bk4btNG0.js";import{S as d}from"./StatusBadge-BKln-kmR.js";import"./preload-helper-PPVm8Dsz.js";import"./index-mwyZgxGw.js";import"./Badge-DJz3SsCw.js";const p=`<!-- @license CC0-1.0 -->
+
+# Page Heading
+`;/*@license CC0-1.0*/function o(n){return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(d,{description:"We no longer use this size of headings. Use the Heading component with level 1 instead.",status:"deprecated"}),`
+`,e.jsx(r,{of:m}),`
+`,e.jsx(i,{children:p}),`
+`,e.jsx(a,{})]})}function h(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{h as default};
---
+++ demo-develop/assets/TableOfContents.docs-CprcP50P.js
@@ -0,0 +1,15 @@
+import{j as n}from"./iframe-bKfKBIrv.js";import{useMDXComponents as o}from"./index-D88bxNa4.js";import{M as r,c as i,P as a,e as l,f as m}from"./blocks-DsyMALAN.js";import{T as c,M as p}from"./TableOfContents.stories-CX5qGjAE.js";import"./preload-helper-PPVm8Dsz.js";import"./index-mwyZgxGw.js";import"./Heading-YKjP2ek9.js";const x=`<!-- @license CC0-1.0 -->
+
+# Table of Contents
+
+A list of links corresponding to the content sections on the page.
+It helps users to easily navigate to different sections on the same page.
+`;/*@license CC0-1.0*/function s(e){const t={h2:"h2",h3:"h3",...o(),...e.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(r,{of:c}),`
+`,n.jsx(i,{children:x}),`
+`,n.jsx(a,{}),`
+`,n.jsx(l,{}),`
+`,n.jsx(t.h2,{id:"examples",children:"Examples"}),`
+`,n.jsx(t.h3,{id:"multiple-levels",children:"Multiple levels"}),`
+`,n.jsx(m,{of:p})]})}function g(e={}){const{wrapper:t}={...o(),...e.components};return t?n.jsx(t,{...e,children:n.jsx(s,{...e})}):s(e)}export{g as default};
