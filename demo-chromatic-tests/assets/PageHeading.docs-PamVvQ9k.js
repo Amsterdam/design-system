c2b09cb34bfe6c6d180f36f63448feac10958b86
GitHub <github-action@users.noreply.github.com>
2025-10-24 13:45:55 +0000

    Remove feature branch folder

--- demo-chromatic-tests/assets/PageHeading.docs-PamVvQ9k.js
+++
@@ -1,9 +0,0 @@
-import{j as e}from"./iframe-C79H1jfW.js";import{useMDXComponents as s}from"./index-Dx7Vb-oj.js";import{M as r,c as i,P as a}from"./blocks-Wc-Rw3DV.js";import{P as m}from"./PageHeading.stories-B7F0uQXc.js";import{S as d}from"./StatusBadge-B-Rqjm8O.js";import"./preload-helper-PPVm8Dsz.js";import"./index-1BKAExy4.js";import"./Badge-Bm6MqC15.js";const p=`<!-- @license CC0-1.0 -->
-
-# Page Heading
-`;/*@license CC0-1.0*/function o(n){return e.jsxs(e.Fragment,{children:[`
-`,`
-`,e.jsx(d,{description:"We no longer use this size of headings. Use the Heading component with level 1 instead.",status:"deprecated"}),`
-`,e.jsx(r,{of:m}),`
-`,e.jsx(i,{children:p}),`
-`,e.jsx(a,{})]})}function h(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{h as default};
