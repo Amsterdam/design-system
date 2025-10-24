129b4a629b3f7b7b224ff5d172ccac3eddd66f88
alimpens <37216945+alimpens@users.noreply.github.com>
2025-10-24 13:37:16 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@72ed3dbe35a609eef4d45d6835afe97399b00b60 🚀

---
+++ demo-DES-1478-menu-appearances/assets/PageHeading.docs-4ElhqkNW.js
@@ -0,0 +1,9 @@
+import{j as e}from"./iframe-D-rRwUzv.js";import{useMDXComponents as s}from"./index-CX8qPWFj.js";import{M as r,c as i,P as a}from"./blocks-B2ozf5Uy.js";import{P as m}from"./PageHeading.stories-CG62VDBO.js";import{S as d}from"./StatusBadge-BWBKnN1u.js";import"./preload-helper-PPVm8Dsz.js";import"./index-CqAZnfMJ.js";import"./Badge-G8djdEAk.js";const p=`<!-- @license CC0-1.0 -->
+
+# Page Heading
+`;/*@license CC0-1.0*/function o(n){return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(d,{description:"We no longer use this size of headings. Use the Heading component with level 1 instead.",status:"deprecated"}),`
+`,e.jsx(r,{of:m}),`
+`,e.jsx(i,{children:p}),`
+`,e.jsx(a,{})]})}function h(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{h as default};
--- demo-DES-1478-menu-appearances/assets/PageHeading.docs-BVHs_hyE.js
+++
@@ -1,9 +0,0 @@
-import{j as e}from"./iframe-CAAVQ-0I.js";import{useMDXComponents as s}from"./index-D2at_Oqy.js";import{M as r,c as i,P as a}from"./blocks-CzcjltYR.js";import{P as m}from"./PageHeading.stories-C9I5EEFS.js";import{S as d}from"./StatusBadge-CbSUOGYX.js";import"./preload-helper-PPVm8Dsz.js";import"./index-B3mhh7C1.js";import"./Badge-CLLDtEei.js";const p=`<!-- @license CC0-1.0 -->
-
-# Page Heading
-`;/*@license CC0-1.0*/function o(n){return e.jsxs(e.Fragment,{children:[`
-`,`
-`,e.jsx(d,{description:"We no longer use this size of headings. Use the Heading component with level 1 instead.",status:"deprecated"}),`
-`,e.jsx(r,{of:m}),`
-`,e.jsx(i,{children:p}),`
-`,e.jsx(a,{})]})}function h(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{h as default};
