5e15d56373f732d725c6fed2ef5c234cb41d48ca
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-10-24 13:56:39 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@e15f61cd1c41a55b00626297fcfa877a1e131c49 🚀

---
+++ demo-DES-1478-menu-appearances/assets/ProductPage.docs-CaCqJbeo.js
@@ -0,0 +1,16 @@
+import{j as n}from"./iframe-Db5h8sJL.js";import{useMDXComponents as o}from"./index-O1M5odKb.js";import{M as r}from"./blocks-DsxtjDsN.js";import{P as s}from"./ProductPage.stories-CyUaClRr.js";import"./preload-helper-PPVm8Dsz.js";import"./index-Blhk8mSQ.js";import"./config-C9y6-cbc.js";import"./Layout-Bz8-doRs.js";import"./PageFooter.stories-DkPoaE6H.js";/*@license CC0-1.0*/function i(t){const e={h1:"h1",h2:"h2",li:"li",p:"p",ul:"ul",...o(),...t.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(r,{of:s}),`
+`,n.jsx(e.h1,{id:"product-page",children:"Product page"}),`
+`,n.jsx(e.p,{children:`Lists all information about a product or service of the City.
+Often includes a Call to Action Link that directs the user to a form.`}),`
+`,n.jsx(e.h2,{id:"headings",children:"Headings"}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsx(e.li,{children:"Use a level 1 Heading for the title of the page."}),`
+`,n.jsx(e.li,{children:"Use level 2 Headings for the section titles."}),`
+`]}),`
+`,n.jsx(e.h2,{id:"spacing",children:"Spacing"}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsx(e.li,{children:"Add a small margin bottom to a Heading 2 with following body text."}),`
+`,n.jsx(e.li,{children:"Use an extra large margin between sections and before the Call to Action Link."}),`
+`]})]})}function f(t={}){const{wrapper:e}={...o(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(i,{...t})}):i(t)}export{f as default};
--- demo-DES-1478-menu-appearances/assets/config-CXJldnGs.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./iframe-D-rRwUzv.js";import{L as r}from"./Layout-BMTp6heK.js";const a={decorators:[o=>e.jsx(r,{children:e.jsx(o,{})})],parameters:{layout:"fullscreen",themes:{themeOverride:"Spacious"}}};export{a as c};
