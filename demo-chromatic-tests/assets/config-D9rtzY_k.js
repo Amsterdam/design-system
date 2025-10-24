4d26d1af4a715f5e8eeb3d0c937b39b4b627c4d5
Evi-2003 <69012052+Evi-2003@users.noreply.github.com>
2025-10-24 11:29:01 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@9352e4f631e28f85e4f0ef91ae1030ef5e920b85 🚀

---
+++ demo-chromatic-tests/assets/config-D9rtzY_k.js
@@ -0,0 +1,1 @@
+import{j as e}from"./iframe-C79H1jfW.js";import{L as r}from"./Layout-BKwA-SvR.js";const a={decorators:[o=>e.jsx(r,{children:e.jsx(o,{})})],parameters:{layout:"fullscreen",themes:{themeOverride:"Spacious"}}};export{a as c};
