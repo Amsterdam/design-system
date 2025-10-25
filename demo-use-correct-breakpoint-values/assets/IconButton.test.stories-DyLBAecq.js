45f91c8b131aa1e1d2da7c406f7e4b413c4623e4
alimpens <37216945+alimpens@users.noreply.github.com>
2025-10-25 13:56:59 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@f2a9b718c445d482c0bd843bcee8b9593c8f88dd 🚀

---
+++ demo-use-correct-breakpoint-values/assets/IconButton.test.stories-DyLBAecq.js
@@ -0,0 +1,22 @@
+import{j as e,e as d}from"./iframe-CEJNHUw3.js";import{i as c}from"./Icon-Dv6MZFHq.js";import{i as r,I as l}from"./IconButton-BYiRecKP.js";import{m as u}from"./IconButton.stories-DDjR20qg.js";import"./preload-helper-PPVm8Dsz.js";const I={...u,title:"Components/Buttons/Icon Button"},m=["default","disabled","hovered",...r],n={parameters:{pseudo:{hover:".hover"}},render:s=>e.jsx("table",{children:e.jsx("tbody",{children:c.map(t=>e.jsx("tr",{children:m.map(o=>{const a=i=>r.includes(i);return e.jsx("td",{children:e.jsx(l,{...s,className:o==="hovered"?"hover":void 0,color:a(o)?o:void 0,disabled:o==="disabled",size:t,svg:d})},`${o}-${t}`)})},t))})}),tags:["!dev","!autodocs"]};n.parameters={...n.parameters,docs:{...n.parameters?.docs,source:{originalSource:`{
+  parameters: {
+    pseudo: {
+      hover: '.hover'
+    }
+  },
+  render: args => {
+    return <table>
+        <tbody>
+          {iconSizes.map(size => <tr key={size}>
+              {variants.map(variant => {
+            const isIconButtonColor = (variant: string): variant is IconButtonColor => iconButtonColors.includes(variant as IconButtonColor);
+            return <td key={\`\${variant}-\${size}\`}>
+                    <IconButton {...args} className={variant === 'hovered' ? 'hover' : undefined} color={isIconButtonColor(variant) ? variant : undefined} disabled={variant === 'disabled'} size={size} svg={Icons.CloseIcon} />
+                  </td>;
+          })}
+            </tr>)}
+        </tbody>
+      </table>;
+  },
+  tags: ['!dev', '!autodocs']
+}`,...n.parameters?.docs?.source}}};const C=["Test"];export{n as Test,C as __namedExportsOrder,I as default};
--- demo-use-correct-breakpoint-values/assets/PageHeading.docs-4sHMelHs.js
+++
@@ -1,9 +0,0 @@
-import{j as e}from"./iframe-CLEORlkb.js";import{useMDXComponents as s}from"./index-CReb-x9x.js";import{M as r,c as i,P as a}from"./blocks-Crivmfdf.js";import{P as m}from"./PageHeading.stories-CBv7SXAb.js";import{S as d}from"./StatusBadge-C6zsrESy.js";import"./preload-helper-PPVm8Dsz.js";import"./index-BM9-jwxe.js";import"./Badge-BPOMApiK.js";const p=`<!-- @license CC0-1.0 -->
-
-# Page Heading
-`;/*@license CC0-1.0*/function o(n){return e.jsxs(e.Fragment,{children:[`
-`,`
-`,e.jsx(d,{description:"We no longer use this size of headings. Use the Heading component with level 1 instead.",status:"deprecated"}),`
-`,e.jsx(r,{of:m}),`
-`,e.jsx(i,{children:p}),`
-`,e.jsx(a,{})]})}function h(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{h as default};
