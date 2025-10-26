591203f5102334ad63ff0ae166c6c010bd906d20
E Wammes <e.wammes@amsterdam.nl>
2025-10-26 13:41:28 +0100

    Add visual tests for Select

---
+++ storybook/src/components/Select/Select.test.stories.tsx
@@ -0,0 +1,30 @@
+/**
+ * @license EUPL-1.2+
+ * Copyright Gemeente Amsterdam
+ */
+
+import type { Meta, StoryObj } from '@storybook/react-vite'
+
+import { Select } from '@amsterdam/design-system-react/src'
+
+import { default as selectMeta } from './Select.stories'
+
+const meta = {
+  ...selectMeta,
+  title: 'Components/Forms/Select',
+} satisfies Meta<typeof Select>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Test: Story = {
+  render: (args) => (
+    <div style={{ display: 'flex', flexDirection: 'column', gap: 'var(--ams-space-s)' }}>
+      <Select {...args} />
+      <Select {...args} invalid />
+      <Select {...args} disabled />
+    </div>
+  ),
+  tags: ['!dev', '!autodocs'],
+}
