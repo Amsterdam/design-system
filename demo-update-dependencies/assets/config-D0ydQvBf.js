6f559a32b6abf6c640cdaf837595caee2544a42d
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-10-27 13:34:06 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@e74873a21c3267601bbbbc82245b1f7f9d0c7a59 🚀

--- demo-update-dependencies/assets/config-D0ydQvBf.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./iframe-CEJNHUw3.js";import{L as r}from"./Layout-qU1hiij7.js";const a={decorators:[o=>e.jsx(r,{children:e.jsx(o,{})})],parameters:{layout:"fullscreen",themes:{themeOverride:"Spacious"}}};export{a as c};
