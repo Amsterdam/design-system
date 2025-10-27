bb2375dadc5d6293a1b54bc3b970884754076b7e
E Wammes <e.wammes@amsterdam.nl>
2025-10-27 12:04:50 +0100

    Add visual tests for Image component

---
+++ storybook/src/components/Image/Image.test.stories.tsx
@@ -0,0 +1,32 @@
+/**
+ * @license EUPL-1.2+
+ * Copyright Gemeente Amsterdam
+ */
+
+import type { Meta, StoryObj } from '@storybook/react-vite'
+
+import { Image } from '@amsterdam/design-system-react/src'
+
+import { getVariants } from '../../utils/getVariants'
+import { default as imageMeta } from './Image.stories'
+
+const meta = {
+  ...imageMeta,
+  title: 'Components/Media/Image',
+} satisfies Meta<typeof Image>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Test: Story = {
+  render: () =>
+    getVariants({
+      component: Image,
+      args: {
+        alt: '',
+        src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Concertgebouw3x2.jpg/330px-Concertgebouw3x2.jpg',
+      },
+    }),
+  tags: ['!dev', '!autodocs'],
+}
