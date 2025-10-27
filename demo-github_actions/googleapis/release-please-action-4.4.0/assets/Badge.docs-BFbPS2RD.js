dc3b0cf920aa1a91e5667935ed439cc0620404fa
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-10-27 16:00:05 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@b026fbfc5a6f2e8ebf6748d8842f96d6828e8a84 🚀

---
+++ demo-github_actions/googleapis/release-please-action-4.4.0/assets/Badge.docs-BFbPS2RD.js
@@ -0,0 +1,19 @@
+import{j as n}from"./iframe-D6MvL6MB.js";import{useMDXComponents as t}from"./index-C44UWCcd.js";import{M as s,f as a,P as i,h as c}from"./blocks-Bp2t6mub.js";import{B as d}from"./Badge.stories-C5zieqw4.js";import"./preload-helper-PPVm8Dsz.js";import"./index-5TCV-ppL.js";import"./Badge-De9gnaJ3.js";const h=`<!-- @license CC0-1.0 -->
+
+# Badge
+
+A prominent label that indicates a category, property or instruction.
+
+## Guidelines
+
+- Let the badge contain a short text or a number.
+- Use any of the feedback or highlight [colours](/docs/brand-design-tokens-colour--docs) for the background.
+  The default is green.
+  Red, orange and green are especially useful for showing an error, warning, or success.
+  However, they can also be combined with the other colours without carrying these meanings.
+`;/*@license CC0-1.0*/function r(e){return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(s,{of:d}),`
+`,n.jsx(a,{children:h}),`
+`,n.jsx(i,{}),`
+`,n.jsx(c,{})]})}function j(e={}){const{wrapper:o}={...t(),...e.components};return o?n.jsx(o,{...e,children:n.jsx(r,{...e})}):r()}export{j as default};
---
+++ demo-github_actions/googleapis/release-please-action-4.4.0/assets/PageHeading.docs-DkDd8DMz.js
@@ -0,0 +1,9 @@
+import{j as e}from"./iframe-D6MvL6MB.js";import{useMDXComponents as s}from"./index-C44UWCcd.js";import{M as r,f as i,P as a}from"./blocks-Bp2t6mub.js";import{P as m}from"./PageHeading.stories-CF2i7QHm.js";import{S as d}from"./StatusBadge-DIl1bYX0.js";import"./preload-helper-PPVm8Dsz.js";import"./index-5TCV-ppL.js";import"./Badge-De9gnaJ3.js";const p=`<!-- @license CC0-1.0 -->
+
+# Page Heading
+`;/*@license CC0-1.0*/function o(n){return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(d,{description:"We no longer use this size of headings. Use the Heading component with level 1 instead.",status:"deprecated"}),`
+`,e.jsx(r,{of:m}),`
+`,e.jsx(i,{children:p}),`
+`,e.jsx(a,{})]})}function h(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{h as default};
