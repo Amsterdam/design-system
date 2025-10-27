841d855b01d5cc799cac0c7209e9ba5d13a69fff
E Wammes <e.wammes@amsterdam.nl>
2025-10-27 15:34:13 +0100

    Add visual tests for ActionGroup and Overlap

---
+++ storybook/src/components/ActionGroup/ActionGroup.test.stories.tsx
@@ -0,0 +1,35 @@
+/**
+ * @license EUPL-1.2+
+ * Copyright Gemeente Amsterdam
+ */
+
+import type { Meta, StoryObj } from '@storybook/react-vite'
+
+import { ActionGroup } from '@amsterdam/design-system-react/src'
+
+import { getVariants } from '../../utils/getVariants'
+import { default as buttonMeta } from './ActionGroup.stories'
+
+const meta = {
+  ...buttonMeta,
+  title: 'Components/Layout/Action Group',
+} satisfies Meta<typeof ActionGroup>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Test: Story = {
+  render: (args) =>
+    getVariants({
+      component: ActionGroup,
+      args,
+      children: (
+        <>
+          <button>Stoppen</button>
+          <button>Verder</button>
+        </>
+      ),
+    }),
+  tags: ['!dev', '!autodocs'],
+}
