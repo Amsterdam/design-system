f6f6b4820ddad5ea23fbe447739776df9e4ed21c
GitHub <github-action@users.noreply.github.com>
2025-10-27 15:41:28 +0000

    Remove feature branch folder

--- demo-DES-1489-manual-visual-tests/assets/PageHeading.docs-Ddflg-hF.js
+++
@@ -1,9 +0,0 @@
-import{j as e}from"./iframe-CN5tPLMR.js";import{useMDXComponents as s}from"./index-C6b71dYW.js";import{M as r,f as i,P as a}from"./blocks-DwWv72vq.js";import{P as m}from"./PageHeading.stories-DVhtM9F9.js";import{S as d}from"./StatusBadge-D-ykY8Gp.js";import"./preload-helper-PPVm8Dsz.js";import"./index-DxTDKUIq.js";import"./Badge-DwVGh_Eh.js";const p=`<!-- @license CC0-1.0 -->
-
-# Page Heading
-`;/*@license CC0-1.0*/function o(n){return e.jsxs(e.Fragment,{children:[`
-`,`
-`,e.jsx(d,{description:"We no longer use this size of headings. Use the Heading component with level 1 instead.",status:"deprecated"}),`
-`,e.jsx(r,{of:m}),`
-`,e.jsx(i,{children:p}),`
-`,e.jsx(a,{})]})}function h(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{h as default};
--- demo-DES-1489-manual-visual-tests/assets/config-BBlLdp7K.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./iframe-CN5tPLMR.js";import{L as r}from"./Layout-BHc0w5db.js";const a={decorators:[o=>e.jsx(r,{children:e.jsx(o,{})})],parameters:{layout:"fullscreen",themes:{themeOverride:"Spacious"}}};export{a as c};
