0992a46548442a6563b80148f526daf91cc37b76
E Wammes <e.wammes@amsterdam.nl>
2025-10-27 10:30:46 +0100

    Add visual tests for the Card component

---
+++ storybook/src/components/Card/Card.test.stories.tsx
@@ -0,0 +1,48 @@
+/**
+ * @license EUPL-1.2+
+ * Copyright Gemeente Amsterdam
+ */
+
+import type { Meta, StoryObj } from '@storybook/react-vite'
+
+import { Card } from '@amsterdam/design-system-react/src'
+
+import { default as cardMeta } from './Card.stories'
+
+const meta = {
+  ...cardMeta,
+  title: 'Components/Navigation/Card',
+} satisfies Meta<typeof Card>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Test: Story = {
+  render: (args) => (
+    <Card
+      {...args}
+      style={{
+        maxWidth: '24rem',
+      }}
+    >
+      <Card.Image
+        alt=""
+        aspectRatio="4:3"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Concertgebouw3x2.jpg/330px-Concertgebouw3x2.jpg"
+      />
+      <Card.HeadingGroup tagline="Nieuws">
+        <Card.Heading level={3}>
+          <Card.Link href="/">Nederlands eerste houten woonwijk komt in Zuidoost</Card.Link>
+          <Card.Link className="hover" href="/">
+            Nederlands eerste houten woonwijk komt in Zuidoost
+          </Card.Link>
+        </Card.Heading>
+      </Card.HeadingGroup>
+      <div>
+        <p>We bouwen een levendige, groene en duurzame woonbuurt tussen de Gooiseweg en het Nelson Mandelapark.</p>
+      </div>
+    </Card>
+  ),
+  tags: ['!dev', '!autodocs'],
+}
