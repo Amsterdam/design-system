bf72349e7a6cdad1f2388106f0c139d61af0aa26
E Wammes <e.wammes@amsterdam.nl>
2025-10-27 12:15:20 +0100

    Add visual tests for OrderedList component

---
+++ storybook/src/components/OrderedList/OrderedList.test.stories.tsx
@@ -0,0 +1,43 @@
+/**
+ * @license EUPL-1.2+
+ * Copyright Gemeente Amsterdam
+ */
+
+import type { Meta, StoryObj } from '@storybook/react-vite'
+
+import { OrderedList } from '@amsterdam/design-system-react/src'
+
+import { getVariants } from '../../utils/getVariants'
+import { default as orderedListMeta } from './OrderedList.stories'
+
+const meta = {
+  ...orderedListMeta,
+  title: 'Components/Text/Ordered List',
+} satisfies Meta<typeof OrderedList>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Test: Story = {
+  render: (args) =>
+    getVariants({
+      component: OrderedList,
+      args,
+      children: (
+        <>
+          <OrderedList.Item>Ervaring is niet noodzakelijk.</OrderedList.Item>
+          <OrderedList.Item>
+            Er is veel werk in diverse functies, zoals monteur of timmerman. Er zijn ook banen op kantoor, zoals
+            werkvoorbereider of engineer.
+          </OrderedList.Item>
+          <OrderedList.Item>Deze banen bieden stabiliteit en zekerheid.</OrderedList.Item>
+          <OrderedList.Item>Er blijft altijd vraag naar werknemers in de bouw en techniek.</OrderedList.Item>
+          <OrderedList.Item>
+            Er zijn veel mogelijkheden in de bouw, infrastructuur, haven en groenvoorziening.
+          </OrderedList.Item>
+        </>
+      ),
+    }),
+  tags: ['!dev', '!autodocs'],
+}
