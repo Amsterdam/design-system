d2c96510cfcad00fd2413d624fbb27dbefd56db3
E Wammes <e.wammes@amsterdam.nl>
2025-10-27 11:30:05 +0100

    Add visual tests for Breakout component

---
+++ storybook/src/components/Breakout/Breakout.test.stories.tsx
@@ -0,0 +1,55 @@
+/**
+ * @license EUPL-1.2+
+ * Copyright Gemeente Amsterdam
+ */
+
+import type { Meta, StoryObj } from '@storybook/react-vite'
+
+import { Spotlight } from '@amsterdam/design-system-react'
+import { Breakout } from '@amsterdam/design-system-react/src'
+
+import { getVariants } from '../../utils/getVariants'
+import { default as breakoutMeta } from './Breakout.stories'
+
+const meta = {
+  ...breakoutMeta,
+  title: 'Components/Layout/Breakout',
+} satisfies Meta<typeof Breakout>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Test: Story = {
+  render: (args) =>
+    getVariants({
+      component: Breakout,
+      args,
+      children: (
+        <>
+          <Breakout.Cell colSpan="all" has="spotlight" rowSpan={{ narrow: 2, medium: 2, wide: 1 }} rowStart={2}>
+            <Spotlight color="green" />
+          </Breakout.Cell>
+          <Breakout.Cell
+            colSpan={{ narrow: 4, medium: 8, wide: 6 }}
+            colStart={1}
+            rowStart={{ narrow: 3, medium: 3, wide: 2 }}
+          >
+            <p>Het doel van deze club is om ervoor te zorgen dat de Zuidas steeds duurzamer wordt.</p>
+          </Breakout.Cell>
+          <Breakout.Cell
+            colSpan={{ narrow: 4, medium: 8, wide: 6 }}
+            colStart={{ narrow: 1, medium: 1, wide: 7 }}
+            rowSpan={2}
+            rowStart={1}
+          >
+            <img
+              alt=""
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Concertgebouw3x2.jpg/330px-Concertgebouw3x2.jpg"
+            />
+          </Breakout.Cell>
+        </>
+      ),
+    }),
+  tags: ['!dev', '!autodocs'],
+}
