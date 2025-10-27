488ea82d8963c444647163c54605c971c286f791
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-10-27 16:02:58 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@09ec8c461408773c5fa953398b9f6dc985e82b31 🚀

---
+++ demo-develop/assets/Badge.docs-BecJWiOw.js
@@ -0,0 +1,19 @@
+import{j as n}from"./iframe-YwOhrd5l.js";import{useMDXComponents as t}from"./index-VJn8pNxV.js";import{M as s,f as a,P as i,h as c}from"./blocks-rWJn47e0.js";import{B as d}from"./Badge.stories-DMGqG-iT.js";import"./preload-helper-PPVm8Dsz.js";import"./index-PZmR7ncx.js";import"./Badge-Y8nF0lSW.js";const h=`<!-- @license CC0-1.0 -->
+
+# Badge
+
+A prominent label that indicates a category, property or instruction.
+
+## Guidelines
+
+- Let the badge contain a short text or a number.
+- Use any of the feedback or highlight [colours](/docs/brand-design-tokens-colour--docs) for the background.
+  The default is green.
+  Red, orange and green are especially useful for showing an error, warning, or success.
+  However, they can also be combined with the other colours without carrying these meanings.
+`;/*@license CC0-1.0*/function r(e){return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(s,{of:d}),`
+`,n.jsx(a,{children:h}),`
+`,n.jsx(i,{}),`
+`,n.jsx(c,{})]})}function j(e={}){const{wrapper:o}={...t(),...e.components};return o?n.jsx(o,{...e,children:n.jsx(r,{...e})}):r()}export{j as default};
---
+++ demo-develop/assets/PageHeading.docs-Cb23QJyk.js
@@ -0,0 +1,9 @@
+import{j as e}from"./iframe-YwOhrd5l.js";import{useMDXComponents as s}from"./index-VJn8pNxV.js";import{M as r,f as i,P as a}from"./blocks-rWJn47e0.js";import{P as m}from"./PageHeading.stories-FHLIDI5m.js";import{S as d}from"./StatusBadge-DsVbm7eK.js";import"./preload-helper-PPVm8Dsz.js";import"./index-PZmR7ncx.js";import"./Badge-Y8nF0lSW.js";const p=`<!-- @license CC0-1.0 -->
+
+# Page Heading
+`;/*@license CC0-1.0*/function o(n){return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(d,{description:"We no longer use this size of headings. Use the Heading component with level 1 instead.",status:"deprecated"}),`
+`,e.jsx(r,{of:m}),`
+`,e.jsx(i,{children:p}),`
+`,e.jsx(a,{})]})}function h(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{h as default};
