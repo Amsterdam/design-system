b8598042ec810645c188e306e5a6e1c83677f44c
GitHub <github-action@users.noreply.github.com>
2025-10-27 13:57:19 +0000

    Remove feature branch folder

--- demo-update-dependencies/assets/config-DngrfEjG.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./iframe-D6MvL6MB.js";import{L as r}from"./Layout-BTwlZWXb.js";const a={decorators:[o=>e.jsx(r,{children:e.jsx(o,{})})],parameters:{layout:"fullscreen",themes:{themeOverride:"Spacious"}}};export{a as c};
