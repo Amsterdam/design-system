c86e425ff01cb066a8dc1b98f6f2f3b3e70747ae
GitHub <github-action@users.noreply.github.com>
2025-10-27 15:38:24 +0000

    Remove feature branch folder

--- demo-DES-1489-manual-tests-stories-media/assets/PageHeading.docs-CUki6Aye.js
+++
@@ -1,9 +0,0 @@
-import{j as e}from"./iframe-D8DOjtjC.js";import{useMDXComponents as s}from"./index-CRsDhmS5.js";import{M as r,f as i,P as a}from"./blocks-_wx-VNRU.js";import{P as m}from"./PageHeading.stories-7uLJtmgW.js";import{S as d}from"./StatusBadge-rp__qVCk.js";import"./preload-helper-PPVm8Dsz.js";import"./index-D6AlvG42.js";import"./Badge-xZuY5x-0.js";const p=`<!-- @license CC0-1.0 -->
-
-# Page Heading
-`;/*@license CC0-1.0*/function o(n){return e.jsxs(e.Fragment,{children:[`
-`,`
-`,e.jsx(d,{description:"We no longer use this size of headings. Use the Heading component with level 1 instead.",status:"deprecated"}),`
-`,e.jsx(r,{of:m}),`
-`,e.jsx(i,{children:p}),`
-`,e.jsx(a,{})]})}function h(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{h as default};
