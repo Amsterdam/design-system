764ea49b85167303a3564dc575c7ba026ae3e317
Evi-2003 <69012052+Evi-2003@users.noreply.github.com>
2025-10-27 07:24:18 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@00343cdedf41f283c9eb61ea048c5a5f7f742528 🚀

---
+++ demo-DES-1489-manual-tests-stories-media/assets/Figure.test.stories-2pf2osFX.js
@@ -0,0 +1,23 @@
+import{j as e}from"./iframe-Dg8OWIgz.js";import{m as i,F as t}from"./Figure.stories-Drf0Km4U.js";import"./preload-helper-PPVm8Dsz.js";import"./exampleContent-DRope23M.js";const g={...i,title:"Components/Media/Figure"},a={render:r=>e.jsxs("div",{style:{display:"flex",flexDirection:"column",gap:"var(--ams-space-s)"},children:[e.jsxs(t,{...r,children:[e.jsx("img",{alt:"",sizes:"(max-width: 36rem) 640px, (max-width: 68rem) 1280px, 1440px",src:"https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Concertgebouw3x2.jpg/330px-Concertgebouw3x2.jpg"}),e.jsx(t.Caption,{children:"Een schuin geplaatste fiets tegen een lantaarnpaal, met op de achtergrond een karakteristiek houten bruggetje."})]}),e.jsxs(t,{...r,style:{backgroundColor:"var(--ams-color-highlight-azure)"},children:[e.jsx("img",{alt:"",sizes:"(max-width: 36rem) 640px, (max-width: 68rem) 1280px, 1440px",src:"https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Concertgebouw3x2.jpg/330px-Concertgebouw3x2.jpg"}),e.jsx(t.Caption,{color:"inverse",children:"Een schuin geplaatste fiets tegen een lantaarnpaal, met op de achtergrond een karakteristiek houten bruggetje."})]})]}),tags:["!dev","!autodocs"]};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  render: args => <div style={{
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 'var(--ams-space-s)'
+  }}>
+      <Figure {...args}>
+        <img alt="" sizes="(max-width: 36rem) 640px, (max-width: 68rem) 1280px, 1440px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Concertgebouw3x2.jpg/330px-Concertgebouw3x2.jpg" />
+        <Figure.Caption>
+          Een schuin geplaatste fiets tegen een lantaarnpaal, met op de achtergrond een karakteristiek houten bruggetje.
+        </Figure.Caption>
+      </Figure>
+      <Figure {...args} style={{
+      backgroundColor: 'var(--ams-color-highlight-azure)'
+    }}>
+        <img alt="" sizes="(max-width: 36rem) 640px, (max-width: 68rem) 1280px, 1440px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Concertgebouw3x2.jpg/330px-Concertgebouw3x2.jpg" />
+        <Figure.Caption color="inverse">
+          Een schuin geplaatste fiets tegen een lantaarnpaal, met op de achtergrond een karakteristiek houten bruggetje.
+        </Figure.Caption>
+      </Figure>
+    </div>,
+  tags: ['!dev', '!autodocs']
+}`,...a.parameters?.docs?.source}}};const m=["Test"];export{a as Test,m as __namedExportsOrder,g as default};
