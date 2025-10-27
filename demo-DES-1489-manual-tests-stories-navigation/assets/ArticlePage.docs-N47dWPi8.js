b17d93f71268e63523574b3bd54712b393cd7110
Evi-2003 <69012052+Evi-2003@users.noreply.github.com>
2025-10-27 15:43:34 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@018f64077db564a62ecb8abcd67f5b83968b46a7 🚀

---
+++ demo-DES-1489-manual-tests-stories-navigation/assets/ArticlePage.docs-N47dWPi8.js
@@ -0,0 +1,15 @@
+import{j as e}from"./iframe-CLbqsn3O.js";import{useMDXComponents as r}from"./index-CHIwYrp4.js";import{M as s}from"./blocks-eXVmsYqr.js";import{A as o}from"./ArticlePage.stories-DwFIKLXA.js";import"./preload-helper-PPVm8Dsz.js";import"./index-UDlgVk9k.js";import"./config-CEInTIpA.js";import"./Layout-tjcFcjRS.js";import"./PageFooter.stories-vwsCmhgY.js";import"./common-DrnyCu3t.js";import"./HomeNews-Bqmo_Dt0.js";/*@license CC0-1.0*/function t(i){const n={h1:"h1",h2:"h2",li:"li",p:"p",ul:"ul",...r(),...i.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(s,{of:o}),`
+`,e.jsx(n.h1,{id:"article-page",children:"Article page"}),`
+`,e.jsx(n.p,{children:"This page type is for news or similar kinds of articles."}),`
+`,e.jsx(n.h2,{id:"headings",children:"Headings"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Use a level 1 Heading for the title of the article."}),`
+`,e.jsx(n.li,{children:"Use level 2 Headings for the section titles."}),`
+`]}),`
+`,e.jsx(n.h2,{id:"spacing",children:"Spacing"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Add a small margin bottom to a Heading 2 with following body text."}),`
+`,e.jsx(n.li,{children:"Use an extra large margin between sections."}),`
+`]})]})}function u(i={}){const{wrapper:n}={...r(),...i.components};return n?e.jsx(n,{...i,children:e.jsx(t,{...i})}):t(i)}export{u as default};
