530fb833b29a6f542c4a1344032094c0822bc078
Aram <37216945+alimpens@users.noreply.github.com>
2025-10-27 13:55:01 +0100

    fix: Use correct breakpoint values (#2264)

--- storybook/src/components/Image/Image.stories.tsx
+++ storybook/src/components/Image/Image.stories.tsx
@@ -37,7 +37,7 @@ export const Default: Story = {
 export const ResponsiveImages: Story = {
   args: {
     alt: '',
-    sizes: '(max-width: 36rem) 640px, 50vw',
+    sizes: '(max-width: 37.5rem) 640px, 50vw',
     src: 'https://picsum.photos/1280/720',
     srcSet: 'https://picsum.photos/640/360 640w, https://picsum.photos/1280/720 1280w',
   },
