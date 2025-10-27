aad8f55fd466a7f82e5836aa1353f0c6c8f83a1f
Evi-2003 <69012052+Evi-2003@users.noreply.github.com>
2025-10-27 07:21:07 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@c3cea6dd859ac04ba20b45737e02f4221a792773 🚀

---
+++ demo-DES-1489-manual-tests-stories-layout/assets/ActionGroup.test.stories-CopiKbZG.js
@@ -0,0 +1,19 @@
+import{j as e}from"./iframe-DEyg65t5.js";import{m as s,A as o}from"./ActionGroup.stories-BJkxV0o5.js";import"./preload-helper-PPVm8Dsz.js";const c={...s,title:"Components/Layout/Action Group"},t={render:r=>e.jsxs("div",{style:{display:"flex",flexDirection:"column",gap:"var(--ams-space-s)"},children:[e.jsxs(o,{children:[e.jsx("button",{children:"Stoppen"}),e.jsx("button",{children:"Verder"})]}),e.jsxs(o,{...r,className:"ams-resize-horizontal",style:{inlineSize:"16rem"},children:[e.jsx("button",{children:"Stoppen"}),e.jsx("button",{children:"Verder"})]})]}),tags:["!dev","!autodocs"]};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  render: args => <div style={{
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 'var(--ams-space-s)'
+  }}>
+      <ActionGroup>
+        <button>Stoppen</button>
+        <button>Verder</button>
+      </ActionGroup>
+      <ActionGroup {...args} className="ams-resize-horizontal" style={{
+      inlineSize: '16rem'
+    }}>
+        <button>Stoppen</button>
+        <button>Verder</button>
+      </ActionGroup>
+    </div>,
+  tags: ['!dev', '!autodocs']
+}`,...t.parameters?.docs?.source}}};const p=["Test"];export{t as Test,p as __namedExportsOrder,c as default};
---
+++ demo-DES-1489-manual-tests-stories-layout/assets/ProductPage.docs-ClEtbi3m.js
@@ -0,0 +1,16 @@
+import{j as n}from"./iframe-DEyg65t5.js";import{useMDXComponents as o}from"./index-CEYv7lUT.js";import{M as r}from"./blocks-Dlt3sXfw.js";import{P as s}from"./ProductPage.stories-C1m-Muj6.js";import"./preload-helper-PPVm8Dsz.js";import"./index-pUqLDp4s.js";import"./config-D89eMfxR.js";import"./Layout-Ccr0iiO0.js";import"./PageFooter.stories-D6fc_sFX.js";/*@license CC0-1.0*/function i(t){const e={h1:"h1",h2:"h2",li:"li",p:"p",ul:"ul",...o(),...t.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(r,{of:s}),`
+`,n.jsx(e.h1,{id:"product-page",children:"Product page"}),`
+`,n.jsx(e.p,{children:`Lists all information about a product or service of the City.
+Often includes a Call to Action Link that directs the user to a form.`}),`
+`,n.jsx(e.h2,{id:"headings",children:"Headings"}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsx(e.li,{children:"Use a level 1 Heading for the title of the page."}),`
+`,n.jsx(e.li,{children:"Use level 2 Headings for the section titles."}),`
+`]}),`
+`,n.jsx(e.h2,{id:"spacing",children:"Spacing"}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsx(e.li,{children:"Add a small margin bottom to a Heading 2 with following body text."}),`
+`,n.jsx(e.li,{children:"Use an extra large margin between sections and before the Call to Action Link."}),`
+`]})]})}function f(t={}){const{wrapper:e}={...o(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(i,{...t})}):i(t)}export{f as default};
