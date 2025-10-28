4fd882cd55ab24d1220dfbc8b8a080cb3d0f345c
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-10-28 11:07:56 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@9894ab7301a536df89831a46188df82a91b596b1 🚀

---
+++ demo-DES-1498-search-field-submit-button/assets/config-6IwXzvAf.js
@@ -0,0 +1,1 @@
+import{j as e}from"./iframe-DWuGvec4.js";import{L as r}from"./Layout-CNpR_1GY.js";const a={decorators:[o=>e.jsx(r,{children:e.jsx(o,{})})],parameters:{layout:"fullscreen",themes:{themeOverride:"Spacious"}}};export{a as c};
