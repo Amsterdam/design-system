12195ccd7a5f669d5b2c0bb82ae5e2513ca209ef
E Wammes <e.wammes@amsterdam.nl>
2025-10-28 10:32:08 +0100

    Add interaction tests for Alert component

---
+++ storybook/src/components/Alert/Alert.test.stories.tsx
@@ -0,0 +1,32 @@
+/**
+ * @license EUPL-1.2+
+ * Copyright Gemeente Amsterdam
+ */
+
+import type { Alert } from '@amsterdam/design-system-react/src'
+import type { Meta, StoryObj } from '@storybook/react-vite'
+
+import { expect, fn } from 'storybook/test'
+
+import { default as tabsMeta } from './Alert.stories'
+
+const meta = {
+  ...tabsMeta,
+  title: 'Components/Feedback/Alert',
+} satisfies Meta<typeof Alert>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Test: Story = {
+  args: {
+    closeable: true,
+    onClose: fn(),
+  },
+  play: async ({ args, canvas, userEvent }) => {
+    await userEvent.click(canvas.getByRole('button'))
+    expect(args.onClose).toHaveBeenCalled()
+  },
+  tags: ['!dev', '!autodocs'],
+}
