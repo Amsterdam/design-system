e35ae3c32783bbc08ac6208f75daa2b3f2f00671
GitHub <github-action@users.noreply.github.com>
2025-10-28 13:33:24 +0000

    Remove feature branch folder

--- demo-DES-1498-search-field-submit-button/assets/config-BE9iayWR.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./iframe-CiRS31Sb.js";import{L as r}from"./Layout-ClWv_BF2.js";const a={decorators:[o=>e.jsx(r,{children:e.jsx(o,{})})],parameters:{layout:"fullscreen",themes:{themeOverride:"Spacious"}}};export{a as c};
