0ea08d9440c6f9dbc2382b1232d096c202e71b68
GitHub <github-action@users.noreply.github.com>
2025-10-29 11:02:23 +0000

    Remove feature branch folder

--- demo-github_actions/actions/setup-node-6.0.0/assets/PageHeading.docs-Cmf_QHAZ.js
+++
@@ -1,9 +0,0 @@
-import{j as e}from"./iframe-BA-jntpJ.js";import{useMDXComponents as s}from"./index-BqJjmL-T.js";import{M as r,f as i,P as a}from"./blocks-DXuRlhVy.js";import{P as m}from"./PageHeading.stories-DYQjXreD.js";import{S as d}from"./StatusBadge-ccPVLkts.js";import"./preload-helper-PPVm8Dsz.js";import"./index-hvdArZPh.js";import"./Badge-U-cvWA9s.js";const p=`<!-- @license CC0-1.0 -->
-
-# Page Heading
-`;/*@license CC0-1.0*/function o(n){return e.jsxs(e.Fragment,{children:[`
-`,`
-`,e.jsx(d,{description:"We no longer use this size of headings. Use the Heading component with level 1 instead.",status:"deprecated"}),`
-`,e.jsx(r,{of:m}),`
-`,e.jsx(i,{children:p}),`
-`,e.jsx(a,{})]})}function h(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{h as default};
