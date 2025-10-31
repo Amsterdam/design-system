dce34b4a972416f7b65a6aa54e35ef11b67801a8
alimpens <37216945+alimpens@users.noreply.github.com>
2025-10-31 13:14:17 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@fd28fcabae9a4d7c0e6c41961fbba470721f2119 🚀

--- demo-release-please--branches--main/assets/Page.test.stories-CJWl1ha3.js
+++
@@ -1,9 +0,0 @@
-import{r as a}from"./renderComponentVariants-qsBlUmDo.js";import{m as t,P as o}from"./Page.stories-CgeRWzH0.js";import"./iframe-j8i8Y4ns.js";import"./preload-helper-PPVm8Dsz.js";import"./ChevronDown-CFNK5Aqe.js";/**
- * @license EUPL-1.2+
- * Copyright Gemeente Amsterdam
- */const c={...t,title:"Components/Containers/Page"},e={render:r=>a(o,{args:r}),tags:["!dev","!autodocs"]};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(Page, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...e.parameters?.docs?.source}}};const i=["Test"];export{e as Test,i as __namedExportsOrder,c as default};
