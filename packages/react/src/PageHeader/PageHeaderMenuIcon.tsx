bfbe8a8452aa51e6d17150e8f3c34166d864af78
Vincent Smedinga <v.smedinga@amsterdam.nl>
2025-11-13 09:21:02 +0100

    Draw each path from left to right

--- packages/react/src/PageHeader/PageHeaderMenuIcon.tsx
+++ packages/react/src/PageHeader/PageHeaderMenuIcon.tsx
@@ -7,8 +7,8 @@ import type { SVGProps } from 'react'
 
 export const PageHeaderMenuIcon = (props: SVGProps<SVGSVGElement>) => (
   <svg aria-hidden="true" focusable="false" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...props}>
-    <path className="ams-page-header__menu-icon-top" d="M23 5H1v2h22z" />
-    <path className="ams-page-header__menu-icon-middle" d="M23 11H1v2h22z" />
+    <path className="ams-page-header__menu-icon-top" d="M1 5h22v2H1z" />
+    <path className="ams-page-header__menu-icon-middle" d="M1 11h22v2H1z" />
     <path className="ams-page-header__menu-icon-bottom" d="M1 17h22v2H1z" />
   </svg>
 )
