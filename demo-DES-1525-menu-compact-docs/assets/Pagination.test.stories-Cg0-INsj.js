c0ffc73e36b6705c2bc911e4c401d793baa3fd70
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-11-17 13:09:54 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@0d4a526a7a6d0b5f94f4a0494a26c6b3d8188abb 🚀

---
+++ demo-DES-1525-menu-compact-docs/assets/Pagination.test.stories-Cg0-INsj.js
@@ -0,0 +1,9 @@
+import{r as t}from"./renderComponentVariants-OTCY9vhG.js";import{m as e,P as o}from"./Pagination.stories-B2U1FzP2.js";import"./iframe-2LjmGD-j.js";import"./preload-helper-PPVm8Dsz.js";import"./Icon-DmpJwWia.js";/**
+ * @license EUPL-1.2+
+ * Copyright Gemeente Amsterdam
+ */const p={...e,title:"Components/Navigation/Pagination"},r={render:a=>t(o,{args:a}),tags:["!dev","!autodocs"]};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(Pagination, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...r.parameters?.docs?.source}}};const c=["Test"];export{r as Test,c as __namedExportsOrder,p as default};
