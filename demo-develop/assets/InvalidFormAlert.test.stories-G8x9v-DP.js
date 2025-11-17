0c4fd69abc0a248154342260d6e4264913c173b7
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-11-17 08:43:38 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@42bccd478f11995cf98f9115badfb66ecc7c8c08 🚀

---
+++ demo-develop/assets/InvalidFormAlert.test.stories-G8x9v-DP.js
@@ -0,0 +1,9 @@
+import{r as t}from"./renderComponentVariants-DS0LLVeb.js";import{m as e,I as a}from"./InvalidFormAlert.stories-1T91Cnn7.js";import"./iframe-BKkY6V2P.js";import"./preload-helper-PPVm8Dsz.js";import"./Alert-CFFA4ttS.js";import"./Icon-DgXyJ-rF.js";import"./Row-CVENdeoE.js";import"./Heading-Yms6R2U4.js";import"./IconButton-DQTSsqK3.js";import"./LinkList-p5okQWZn.js";/**
+ * @license EUPL-1.2+
+ * Copyright Gemeente Amsterdam
+ */const v={...e,title:"Components/Forms/Invalid Form Alert"},r={render:o=>t(a,{args:o}),tags:["!dev","!autodocs"]};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(InvalidFormAlert, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...r.parameters?.docs?.source}}};const F=["Test"];export{r as Test,F as __namedExportsOrder,v as default};
