a3943243b7090fec26312ecbb22b56d61a930f7c
alimpens <37216945+alimpens@users.noreply.github.com>
2025-11-19 10:56:26 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@45f102fa23bbdc55100eff60e926c064bb9c32a2 🚀

---
+++ demo-DES-1486-header-menu-link-icon/assets/Badge.test.stories-DqujZQqv.js
@@ -0,0 +1,6 @@
+import{r as a}from"./renderComponentVariants-DEDvsTbm.js";import{m as o}from"./Badge.stories-BL1ggIOK.js";import{B as t}from"./Badge-BbQbzn1Q.js";import"./iframe-CL-7-L5u.js";import"./preload-helper-PPVm8Dsz.js";const c={...o,title:"Components/Feedback/Badge"},e={render:r=>a(t,{args:r}),tags:["!dev","!autodocs"]};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(Badge, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...e.parameters?.docs?.source}}};const i=["Test"];export{e as Test,i as __namedExportsOrder,c as default};
---
+++ demo-DES-1486-header-menu-link-icon/assets/Paragraph.test.stories-B7rOVhiM.js
@@ -0,0 +1,9 @@
+import{r}from"./renderComponentVariants-DEDvsTbm.js";import{m as n}from"./Paragraph.stories-ClsSj5I-.js";import{P as s}from"./Paragraph-0dXP0mGS.js";import"./iframe-CL-7-L5u.js";import"./preload-helper-PPVm8Dsz.js";import"./exampleContent-Du2yH2Fl.js";import"./Row-Bgyj96kJ.js";import"./Icon-CDM7wKSK.js";const v={...n,title:"Components/Text/Paragraph"},e={args:{children:"Het waterrijke achterland van de provincie Holland was een paradijs voor vissers. Maar terwijl de visserij floreerde in Holland, was Amsterdam eigenlijk helemaal geen visserijstad. Toch maakten Amsterdammers naam in de vishandel. Zij speelden namelijk een cruciale rol bij het klaarmaken en vervoeren van haring."},render:a=>r(s,{args:a}),tags:["!dev","!autodocs"]};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Het waterrijke achterland van de provincie Holland was een paradijs voor vissers. Maar terwijl de visserij floreerde in Holland, was Amsterdam eigenlijk helemaal geen visserijstad. Toch maakten Amsterdammers naam in de vishandel. Zij speelden namelijk een cruciale rol bij het klaarmaken en vervoeren van haring.'
+  },
+  render: args => renderComponentVariants(Paragraph, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...e.parameters?.docs?.source}}};const j=["Test"];export{e as Test,j as __namedExportsOrder,v as default};
