f749bdf0265a31d75e35378a0a6a4d72c7f6d9e4
GitHub <github-action@users.noreply.github.com>
2025-11-20 11:11:41 +0000

    Remove feature branch folder

--- demo-DES-1520-page-header-menu-icon/assets/Icon.test.stories-C51HNhBO.js
+++
@@ -1,6 +0,0 @@
-import{r as e}from"./renderComponentVariants-DcEhKtME.js";import{m as t}from"./Icon.stories-BuU3-vjg.js";import{I as a}from"./Icon-DIXhXMis.js";import"./iframe-Dv5rzbay.js";import"./preload-helper-PPVm8Dsz.js";import"./Row-CIA9zsIi.js";import"./Paragraph-BtLm20aK.js";const u={...t,title:"Components/Media/Icon"},r={render:o=>e(a,{args:o}),tags:["!dev","!autodocs"]};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(Icon, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...r.parameters?.docs?.source}}};const g=["Test"];export{r as Test,g as __namedExportsOrder,u as default};
--- demo-DES-1520-page-header-menu-icon/assets/config-BOTCNq3u.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./iframe-Dv5rzbay.js";import{L as r}from"./Layout-C7EfM6HZ.js";const a={decorators:[o=>e.jsx(r,{children:e.jsx(o,{})})],parameters:{layout:"fullscreen",themes:{themeOverride:"Spacious"}}};export{a as c};
