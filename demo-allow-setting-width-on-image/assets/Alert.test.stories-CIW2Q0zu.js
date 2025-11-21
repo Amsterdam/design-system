bafc88d6ca620e7cb31608acc6964bd0dbb2a33d
GitHub <github-action@users.noreply.github.com>
2025-11-21 10:41:02 +0000

    Remove feature branch folder

--- demo-allow-setting-width-on-image/assets/Alert.test.stories-CIW2Q0zu.js
+++
@@ -1,6 +0,0 @@
-import{r as t}from"./renderComponentVariants-jPSsoD7F.js";import{m as o}from"./Alert.stories-BZmDEEQ7.js";import{A as a}from"./Alert-B0y1XKeL.js";import"./iframe-Ca94aFC-.js";import"./preload-helper-PPVm8Dsz.js";import"./Icon-Birb56IQ.js";import"./Row-Dk5G59pr.js";import"./Heading-Cc8-Cw8M.js";import"./IconButton-DMyQs3uH.js";const g={...o,title:"Components/Feedback/Alert"},r={render:e=>t(a,{args:e}),tags:["!dev","!autodocs"]};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(Alert, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...r.parameters?.docs?.source}}};const f=["Test"];export{r as Test,f as __namedExportsOrder,g as default};
--- demo-allow-setting-width-on-image/assets/Icon.test.stories-gWK6N4_d.js
+++
@@ -1,6 +0,0 @@
-import{r as e}from"./renderComponentVariants-jPSsoD7F.js";import{m as t}from"./Icon.stories-DRdSrS92.js";import{I as a}from"./Icon-Birb56IQ.js";import"./iframe-Ca94aFC-.js";import"./preload-helper-PPVm8Dsz.js";import"./Row-Dk5G59pr.js";import"./Paragraph-BqCDx7Qo.js";const u={...t,title:"Components/Media/Icon"},r={render:o=>e(a,{args:o}),tags:["!dev","!autodocs"]};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(Icon, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...r.parameters?.docs?.source}}};const g=["Test"];export{r as Test,g as __namedExportsOrder,u as default};
