4c9e9f47c438357cdf925dc4d99c63dd7b1d5713
Evi-2003 <69012052+Evi-2003@users.noreply.github.com>
2025-11-26 08:36:29 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@dac4fb3a2444b015777a6ce4dc470cfee7789d2c 🚀

---
+++ demo-DES-1550-progress-list-status/assets/ProgressList.docs-CAE7ePXZ.js
@@ -0,0 +1,9 @@
+import{j as r}from"./iframe-Uc4BvFMg.js";import{useMDXComponents as s}from"./index-Co-wDqSp.js";import{M as e,f as i,P as m,h as a}from"./blocks-n5MkAepG.js";import{a as p}from"./ProgressList.stories-CTTYypAh.js";import"./preload-helper-PPVm8Dsz.js";import"./index-gKc3weFv.js";import"./Heading-BjUnjy6Y.js";import"./UnorderedList-CcHMgv7g.js";const x=`<!-- @license CC0-1.0 -->
+
+# Progress List
+`;/*@license CC0-1.0*/function o(n){return r.jsxs(r.Fragment,{children:[`
+`,`
+`,r.jsx(e,{of:p}),`
+`,r.jsx(i,{children:x}),`
+`,r.jsx(m,{}),`
+`,r.jsx(a,{})]})}function h(n={}){const{wrapper:t}={...s(),...n.components};return t?r.jsx(t,{...n,children:r.jsx(o,{...n})}):o()}export{h as default};
---
+++ demo-DES-1550-progress-list-status/assets/VisuallyHidden.docs-eLnApB9N.js
@@ -0,0 +1,20 @@
+import{j as n}from"./iframe-Uc4BvFMg.js";import{useMDXComponents as o}from"./index-Co-wDqSp.js";import{M as t,f as r,P as i,h as l}from"./blocks-n5MkAepG.js";import{V as u}from"./VisuallyHidden.stories-DKjNl59H.js";import"./preload-helper-PPVm8Dsz.js";import"./index-gKc3weFv.js";const c=`<!-- @license CC0-1.0 -->
+
+# Visually Hidden
+
+Hides content from sighted users but keeps it accessible to non-visual user agents, such as screen readers.
+
+## Class name
+
+\`ams-visually-hidden\`
+
+## Guidelines
+
+- In most cases, visually available content should be accessible to non-visual user agents and vice versa.
+  Elaborate instructions or guidance read only by non-visual user agents can do more harm than good.
+`;/*@license CC0-1.0*/function a(s){return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(t,{of:u}),`
+`,n.jsx(r,{children:c}),`
+`,n.jsx(i,{}),`
+`,n.jsx(l,{})]})}function j(s={}){const{wrapper:e}={...o(),...s.components};return e?n.jsx(e,{...s,children:n.jsx(a,{...s})}):a()}export{j as default};
