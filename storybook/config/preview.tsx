a3907b0653d6c49043ae92ca15dc8a4864b237d3
Vincent Smedinga <v.smedinga@amsterdam.nl>
2025-11-27 12:15:49 +0100

    chore(docs): Rename pages group for websites and consolidate docs (#2290)

--- storybook/config/preview.tsx
+++ storybook/config/preview.tsx
@@ -86,7 +86,7 @@ export const parameters = {
         ['Buttons', 'Containers', 'Feedback', 'Forms', 'Layout', 'Media', 'Navigation', 'Text'],
         'Utilities',
         'Pages',
-        ['Introduction', 'Guidelines', 'Amsterdam.nl', ['Home Page'], 'Internal', ['Introduction']],
+        ['Introduction', 'Guidelines', 'Public', ['Introduction'], ['Home Page'], 'Internal', ['Introduction']],
       ],
     },
   },
