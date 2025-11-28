aba6f681c7a924edac28df9a0ca739b4afdf17ee
GitHub <github-action@users.noreply.github.com>
2025-11-28 10:44:49 +0000

    Remove feature branch folder

--- demo-update-dependencies/assets/Label.test.stories-DuD-ZFZa.js
+++
@@ -1,6 +0,0 @@
-import{r as a}from"./renderComponentVariants-BtYncNej.js";import{m as o,L as t}from"./Label.stories-8yvhorK5.js";import"./iframe-CUGQqtKr.js";import"./preload-helper-PPVm8Dsz.js";import"./Hint-BEl47Le7.js";const c={...o,title:"Components/Forms/Label"},r={render:e=>a(t,{args:e}),tags:["!dev","!autodocs"]};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(Label, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...r.parameters?.docs?.source}}};const i=["Test"];export{r as Test,i as __namedExportsOrder,c as default};
