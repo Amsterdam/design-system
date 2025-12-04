6fa9498a34bf96d07760be05efc3fae3d0ef8622
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-12-04 15:17:58 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@171c9d1c00782be11b325afd56af8391e5aa577e 🚀

---
+++ demo-refine-ref-types/assets/Alert.test.stories-DpGU-rhn.js
@@ -0,0 +1,6 @@
+import{r as t}from"./renderComponentVariants-DswKZaBi.js";import{m as o}from"./Alert.stories-DnbLQquN.js";import{A as a}from"./Alert-FTEm8AxS.js";import"./iframe-B7nMCqm-.js";import"./preload-helper-PPVm8Dsz.js";import"./Icon-BMAEmRmd.js";import"./Row-BLC8A-MQ.js";import"./Heading-7aIYbzwV.js";import"./IconButton-CPgYRVE4.js";const g={...o,title:"Components/Feedback/Alert"},r={render:e=>t(a,{args:e}),tags:["!dev","!autodocs"]};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(Alert, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...r.parameters?.docs?.source}}};const f=["Test"];export{r as Test,f as __namedExportsOrder,g as default};
---
+++ demo-refine-ref-types/assets/InvalidFormAlert.test.stories-DTMWfq84.js
@@ -0,0 +1,6 @@
+import{r as t}from"./renderComponentVariants-DswKZaBi.js";import{m as e,I as a}from"./InvalidFormAlert.stories-T9rqlAgj.js";import"./iframe-B7nMCqm-.js";import"./preload-helper-PPVm8Dsz.js";import"./Alert-FTEm8AxS.js";import"./Icon-BMAEmRmd.js";import"./Row-BLC8A-MQ.js";import"./Heading-7aIYbzwV.js";import"./IconButton-CPgYRVE4.js";import"./LinkList-BqtIjDit.js";const v={...e,title:"Components/Forms/Invalid Form Alert"},r={render:o=>t(a,{args:o}),tags:["!dev","!autodocs"]};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(InvalidFormAlert, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...r.parameters?.docs?.source}}};const F=["Test"];export{r as Test,F as __namedExportsOrder,v as default};
