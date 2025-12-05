4f4843283725ba2f05e9408f7d188f51091eef9f
Vincent Smedinga <v.smedinga@amsterdam.nl>
2025-12-05 15:06:11 +0100

    Include visually hidden Heading in Page Footer Menu

--- packages/react/src/PageFooter/PageFooterMenu.tsx
+++ packages/react/src/PageFooter/PageFooterMenu.tsx
@@ -8,13 +8,37 @@ import type { ForwardedRef, HTMLAttributes, PropsWithChildren } from 'react'
 import { clsx } from 'clsx'
 import { forwardRef } from 'react'
 
-export type PageFooterMenuProps = PropsWithChildren<HTMLAttributes<HTMLUListElement>>
+import type { HeadingProps } from '../Heading'
+
+import { Heading } from '../Heading'
+
+export type PageFooterMenuProps = {
+  /**
+   * Describes the menu for users of assistive technology.
+   * The heading gets visually hidden – sighted users can infer the meaning of the menu from its appearance.
+   * @default Over deze website
+   */
+  heading?: string
+  /**
+   * The hierarchical level of the Footer Menu’s Heading within the document.
+   * The default value is 2. This will almost always be correct – but verify this yourself.
+   */
+  headingLevel?: HeadingProps['level']
+} & PropsWithChildren<HTMLAttributes<HTMLUListElement>>
 
 export const PageFooterMenu = forwardRef(
-  ({ children, className, ...restProps }: PageFooterMenuProps, ref: ForwardedRef<HTMLUListElement>) => (
-    <ul {...restProps} className={clsx('ams-page-footer__menu', className)} ref={ref}>
-      {children}
-    </ul>
+  (
+    { children, className, heading = 'Over deze website', headingLevel = 2, ...restProps }: PageFooterMenuProps,
+    ref: ForwardedRef<HTMLUListElement>,
+  ) => (
+    <>
+      <Heading className="ams-visually-hidden" level={headingLevel}>
+        {heading}
+      </Heading>
+      <ul {...restProps} className={clsx('ams-page-footer__menu', className)} ref={ref}>
+        {children}
+      </ul>
+    </>
   ),
 )
 
--- storybook/src/components/PageFooter/PageFooter.stories.tsx
+++ storybook/src/components/PageFooter/PageFooter.stories.tsx
@@ -98,9 +98,6 @@ export const Default: Story = {
           </Grid.Cell>
         </Grid>
       </PageFooter.Spotlight>,
-      <Heading className="ams-visually-hidden" key={2} level={2}>
-        Over deze website
-      </Heading>,
       <PageFooter.Menu key={3}>
         <PageFooter.MenuLink href="#">Over deze site</PageFooter.MenuLink>
         <PageFooter.MenuLink href="#">Privacy</PageFooter.MenuLink>
@@ -175,9 +172,6 @@ export const OnderzoekEnStatistiek: Story = {
           </Grid.Cell>
         </Grid>
       </PageFooter.Spotlight>,
-      <Heading className="ams-visually-hidden" key={2} level={2}>
-        Over deze website
-      </Heading>,
       <PageFooter.Menu key={3}>
         <PageFooter.MenuLink href="#">Privacy</PageFooter.MenuLink>
         <PageFooter.MenuLink href="#">Toegankelijkheid</PageFooter.MenuLink>
