5ea27d96d6fbeaf8d1857d28d995b9ac93596c80
E Wammes <e.wammes@amsterdam.nl>
2025-12-10 14:26:02 +0100

    Add sub step component

---
+++ packages/react/src/ProgressList/ProgressListSubStep.tsx
@@ -0,0 +1,35 @@
+/**
+ * @license EUPL-1.2+
+ * Copyright Gemeente Amsterdam
+ */
+import type { ForwardedRef, HTMLAttributes, PropsWithChildren } from 'react'
+
+import { clsx } from 'clsx'
+import { forwardRef } from 'react'
+
+export type ProgressListSubStepProps = {
+  status?: 'current' | 'completed'
+} & PropsWithChildren<HTMLAttributes<HTMLElement>>
+
+export const ProgressListSubStep = forwardRef(
+  ({ children, className, status, ...restProps }: ProgressListSubStepProps, ref: ForwardedRef<HTMLLIElement>) => {
+    return (
+      <li
+        aria-current={status === 'current' ? 'step' : undefined}
+        className={clsx('ams-progress-list__sub-step', status && `ams-progress-list__sub-step--${status}`, className)}
+        ref={ref}
+        {...restProps}
+      >
+        <div className="ams-progress-list-sub-step__indicator">
+          <div className="ams-progress-list-sub-step__marker">
+            <span className="ams-progress-list-sub-step__marker-shape" />
+          </div>
+          <span className="ams-progress-list-sub-step__connector" />
+        </div>
+        <div>{children}</div>
+      </li>
+    )
+  },
+)
+
+ProgressListSubStep.displayName = 'ProgressList.SubStep'
