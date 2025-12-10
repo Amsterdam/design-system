ee118325c2f143beaff3c35ea384176870829059
E Wammes <e.wammes@amsterdam.nl>
2025-12-10 14:24:12 +0100

    Add sub steps list component

---
+++ packages/react/src/ProgressList/ProgressListSubSteps.tsx
@@ -0,0 +1,21 @@
+/**
+ * @license EUPL-1.2+
+ * Copyright Gemeente Amsterdam
+ */
+
+import type { ForwardedRef, HTMLAttributes, PropsWithChildren } from 'react'
+
+import { clsx } from 'clsx'
+import { forwardRef } from 'react'
+
+export type ProgressListSubStepsProps = {} & PropsWithChildren<HTMLAttributes<HTMLOListElement>>
+
+export const ProgressListSubSteps = forwardRef(
+  ({ children, className, ...restProps }: ProgressListSubStepsProps, ref: ForwardedRef<HTMLOListElement>) => (
+    <ol {...restProps} className={clsx('ams-progress-list__sub-steps', className)} ref={ref}>
+      {children}
+    </ol>
+  ),
+)
+
+ProgressListSubSteps.displayName = 'ProgressList.SubSteps'
