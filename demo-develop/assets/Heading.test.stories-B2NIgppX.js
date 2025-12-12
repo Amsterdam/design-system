3582c57d0e2428c755684273a90e659b741179eb
alimpens <37216945+alimpens@users.noreply.github.com>
2025-12-12 13:06:57 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@c286213c5ef409090aaa71fc013698943aeac6e2 🚀

--- demo-develop/assets/Heading.test.stories-B2NIgppX.js
+++
@@ -1,9 +0,0 @@
-import{r as t}from"./renderComponentVariants-Mmj6MNQQ.js";import{m as a}from"./Heading.stories-BgkI3tle.js";import{H as o}from"./Heading-C-44l3hU.js";import"./iframe-BCkMM_Ad.js";import"./preload-helper-PPVm8Dsz.js";import"./exampleContent-Du2yH2Fl.js";const c={...a,title:"Components/Text/Heading"},e={args:{children:"Amsterdam Light Festival maakt de stad ook deze winter een beetje mooier"},render:r=>t(o,{args:r}),tags:["!dev","!autodocs"]};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: 'Amsterdam Light Festival maakt de stad ook deze winter een beetje mooier'
-  },
-  render: args => renderComponentVariants(Heading, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...e.parameters?.docs?.source}}};const g=["Test"];export{e as Test,g as __namedExportsOrder,c as default};
---
+++ demo-develop/assets/StandaloneLink.test.stories-ZIkMHTqa.js
@@ -0,0 +1,6 @@
+import{r as a}from"./renderComponentVariants-BiXd5Jrr.js";import{m as t,S as o}from"./StandaloneLink.stories-CXbShpbs.js";import"./iframe-DQVh-r_x.js";import"./preload-helper-PPVm8Dsz.js";import"./Icon-BZR1y9jw.js";const i={...t,title:"Components/StandaloneLink"},e={render:r=>a(o,{args:r}),tags:["!dev","!autodocs"]};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(StandaloneLink, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...e.parameters?.docs?.source}}};const c=["Test"];export{e as Test,c as __namedExportsOrder,i as default};
