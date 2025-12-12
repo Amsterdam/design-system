1effb0300303ed76c1599b025aa077a35e0d5eb4
GitHub <github-action@users.noreply.github.com>
2025-12-12 13:05:33 +0000

    Remove feature branch folder

--- demo-refine-ref-types/assets/PageHeading.docs-DyTHdUm9.js
+++
@@ -1,9 +0,0 @@
-import{j as e}from"./iframe-BFUn_NSv.js";import{useMDXComponents as s}from"./index-BEoQcyt1.js";import{M as r,f as i,P as a}from"./blocks-BeHJiWyZ.js";import{P as m}from"./PageHeading.stories-BWTcnZ1E.js";import{S as d}from"./StatusBadge-BCLPZ1sd.js";import"./preload-helper-PPVm8Dsz.js";import"./index-BtkdUvHa.js";import"./Badge-e-WynN21.js";const p=`<!-- @license CC0-1.0 -->
-
-# Page Heading
-`;function o(n){return e.jsxs(e.Fragment,{children:[`
-`,`
-`,e.jsx(d,{description:"We no longer use this size of headings. Use the Heading component with level 1 instead.",status:"deprecated"}),`
-`,e.jsx(r,{of:m}),`
-`,e.jsx(i,{children:p}),`
-`,e.jsx(a,{})]})}function h(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{h as default};
