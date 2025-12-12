28a1fd641b69bf56e5ee19d1e9cdbeb2e11a4970
GitHub <github-action@users.noreply.github.com>
2025-12-12 11:50:18 +0000

    Remove feature branch folder

--- demo-DES-1215-balance-large-paragraph/assets/Icon.test.stories-Bk71twVy.js
+++
@@ -1,6 +0,0 @@
-import{r as e}from"./renderComponentVariants-Mmj6MNQQ.js";import{m as t}from"./Icon.stories-BkJ5XmtV.js";import{I as a}from"./Icon-BK1JoFyH.js";import"./iframe-BCkMM_Ad.js";import"./preload-helper-PPVm8Dsz.js";import"./Row-C2rXvc_W.js";import"./Paragraph-BHKMPjmP.js";const u={...t,title:"Components/Media/Icon"},r={render:o=>e(a,{args:o}),tags:["!dev","!autodocs"]};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(Icon, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...r.parameters?.docs?.source}}};const g=["Test"];export{r as Test,g as __namedExportsOrder,u as default};
