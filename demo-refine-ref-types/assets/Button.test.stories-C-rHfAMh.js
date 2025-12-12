8aed87a57abee6b55a14acce433793f6ac7433a8
alimpens <37216945+alimpens@users.noreply.github.com>
2025-12-12 13:03:33 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@dd4ddbde494ebc7fbe19ee5f454b3ebbbf4ec284 🚀

---
+++ demo-refine-ref-types/assets/Button.test.stories-C-rHfAMh.js
@@ -0,0 +1,7 @@
+import{r as e}from"./renderComponentVariants-0rKuGcQ_.js";import{m as o}from"./Button.stories-CV5nAF_t.js";import{B as a}from"./Button-BJMU6CBN.js";import"./iframe-BFUn_NSv.js";import"./preload-helper-PPVm8Dsz.js";import"./Icon-2qXwQPLx.js";const c={...o,title:"Components/Buttons/Button"},r={render:t=>e(a,{args:t,variants:["disabled","hovered"]}),tags:["!dev","!autodocs"]};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(Button, {
+    args,
+    variants: ['disabled', 'hovered']
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...r.parameters?.docs?.source}}};const u=["Test"];export{r as Test,u as __namedExportsOrder,c as default};
--- demo-refine-ref-types/assets/Column.test.stories-GcTXshZq.js
+++
@@ -1,7 +0,0 @@
-import{r as e}from"./renderComponentVariants-DswKZaBi.js";import{m as t,C as a}from"./Column.stories-Bw_wn6W9.js";import"./iframe-B7nMCqm-.js";import"./preload-helper-PPVm8Dsz.js";import"./types-CjGn4A4T.js";const i={...t,title:"Components/Layout/Column"},r={render:o=>e(a,{args:o,layout:"grid"}),tags:["!dev","!autodocs"]};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(Column, {
-    args,
-    layout: 'grid'
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...r.parameters?.docs?.source}}};const u=["Test"];export{r as Test,u as __namedExportsOrder,i as default};
