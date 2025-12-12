ab7d7e7a9dc36948ea1e2b27c5b6b6aca4ad156b
GitHub <github-action@users.noreply.github.com>
2025-12-12 09:20:43 +0000

    Remove feature branch folder

--- demo-update-dependencies/assets/Button.test.stories-DlahD2Hk.js
+++
@@ -1,7 +0,0 @@
-import{r as e}from"./renderComponentVariants-0L2jViOx.js";import{m as o}from"./Button.stories-CB5AmUb7.js";import{B as a}from"./Button-qtbinCWq.js";import"./iframe-ByOg5_M4.js";import"./preload-helper-PPVm8Dsz.js";import"./Icon-Bdt6c229.js";const c={...o,title:"Components/Buttons/Button"},r={render:t=>e(a,{args:t,variants:["disabled","hovered"]}),tags:["!dev","!autodocs"]};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(Button, {
-    args,
-    variants: ['disabled', 'hovered']
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...r.parameters?.docs?.source}}};const u=["Test"];export{r as Test,u as __namedExportsOrder,c as default};
--- demo-update-dependencies/assets/TableOfContents.test.stories-KOqsnPjQ.js
+++
@@ -1,6 +0,0 @@
-import{r as t}from"./renderComponentVariants-0L2jViOx.js";import{m as o,T as s}from"./TableOfContents.stories-BgPBYU4m.js";import"./iframe-ByOg5_M4.js";import"./preload-helper-PPVm8Dsz.js";import"./Heading-mQGaSsCm.js";const c={...o,title:"Components/TableOfContents"},e={render:r=>t(s,{args:r}),tags:["!dev","!autodocs"]};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(TableOfContents, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...e.parameters?.docs?.source}}};const i=["Test"];export{e as Test,i as __namedExportsOrder,c as default};
