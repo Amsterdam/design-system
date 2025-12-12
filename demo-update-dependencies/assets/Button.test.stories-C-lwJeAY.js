705306ae1484232e8b20ba2426600d9941cea863
GitHub <github-action@users.noreply.github.com>
2025-12-12 21:31:23 +0000

    Remove feature branch folder

--- demo-update-dependencies/assets/Button.test.stories-C-lwJeAY.js
+++
@@ -1,7 +0,0 @@
-import{r as e}from"./renderComponentVariants-Mmj6MNQQ.js";import{m as o}from"./Button.stories-BPAG2f0t.js";import{B as a}from"./Button-Dpq5cHus.js";import"./iframe-BCkMM_Ad.js";import"./preload-helper-PPVm8Dsz.js";import"./Icon-BK1JoFyH.js";const c={...o,title:"Components/Buttons/Button"},r={render:t=>e(a,{args:t,variants:["disabled","hovered"]}),tags:["!dev","!autodocs"]};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(Button, {
-    args,
-    variants: ['disabled', 'hovered']
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...r.parameters?.docs?.source}}};const u=["Test"];export{r as Test,u as __namedExportsOrder,c as default};
--- demo-update-dependencies/assets/PageHeading.docs-D38u_Vpa.js
+++
@@ -1,9 +0,0 @@
-import{j as e}from"./iframe-BCkMM_Ad.js";import{useMDXComponents as s}from"./index-CzptQOtJ.js";import{M as r,f as i,P as a}from"./blocks-Csb9IsCU.js";import{P as m}from"./PageHeading.stories-zqcAvMWQ.js";import{S as d}from"./StatusBadge-B5mdG1iE.js";import"./preload-helper-PPVm8Dsz.js";import"./index-DZBL6IHL.js";import"./Badge-DD4HRfXr.js";const p=`<!-- @license CC0-1.0 -->
-
-# Page Heading
-`;function o(n){return e.jsxs(e.Fragment,{children:[`
-`,`
-`,e.jsx(d,{description:"We no longer use this size of headings. Use the Heading component with level 1 instead.",status:"deprecated"}),`
-`,e.jsx(r,{of:m}),`
-`,e.jsx(i,{children:p}),`
-`,e.jsx(a,{})]})}function h(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{h as default};
