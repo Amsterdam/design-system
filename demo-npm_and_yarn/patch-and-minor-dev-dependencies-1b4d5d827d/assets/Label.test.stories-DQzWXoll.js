c983f0a71cc74856f5875840a04ff30a38e20367
GitHub <github-action@users.noreply.github.com>
2025-12-15 12:12:45 +0000

    Remove feature branch folder

--- demo-npm_and_yarn/patch-and-minor-dev-dependencies-1b4d5d827d/assets/Label.test.stories-DQzWXoll.js
+++
@@ -1,6 +0,0 @@
-import{r as a}from"./renderComponentVariants-0rKuGcQ_.js";import{m as o,L as t}from"./Label.stories-ChfX38OS.js";import"./iframe-BFUn_NSv.js";import"./preload-helper-PPVm8Dsz.js";import"./Hint-Ctc2u01N.js";const c={...o,title:"Components/Forms/Label"},r={render:e=>a(t,{args:e}),tags:["!dev","!autodocs"]};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(Label, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...r.parameters?.docs?.source}}};const i=["Test"];export{r as Test,i as __namedExportsOrder,c as default};
