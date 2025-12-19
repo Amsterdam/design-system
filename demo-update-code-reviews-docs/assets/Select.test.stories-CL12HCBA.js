528fb70c5e7b738b2bdaed3ba0448e39f629e2da
alimpens <37216945+alimpens@users.noreply.github.com>
2025-12-19 16:22:23 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@ac6bc11676c94f88848489849bcff7b9c53bff42 🚀

---
+++ demo-update-code-reviews-docs/assets/Select.test.stories-CL12HCBA.js
@@ -0,0 +1,7 @@
+import{r as t}from"./renderComponentVariants-Cqgsyy8L.js";import{m as a,S as s}from"./Select.stories-BKDxh6Kd.js";import"./iframe-B26LIiQA.js";import"./preload-helper-PPVm8Dsz.js";const c={...a,title:"Components/Forms/Select"},e={render:r=>t(s,{args:r,variants:["hovered","disabled"]}),tags:["!dev","!autodocs"]};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(Select, {
+    args,
+    variants: ['hovered', 'disabled']
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...e.parameters?.docs?.source}}};const i=["Test"];export{e as Test,i as __namedExportsOrder,c as default};
