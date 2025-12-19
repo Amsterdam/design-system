cf208ca061ad31fe1befd47a40c15ff5f4d96e1d
Vincent Smedinga <v.smedinga@amsterdam.nl>
2025-12-19 11:00:34 +0100

    Use specific image for Overlap example

--- storybook/src/components/Overlap/Overlap.stories.tsx
+++ storybook/src/components/Overlap/Overlap.stories.tsx
@@ -8,6 +8,10 @@ import type { Meta, StoryObj } from '@storybook/react-vite'
 import { Grid, Image, SearchField } from '@amsterdam/design-system-react'
 import { Overlap } from '@amsterdam/design-system-react/src'
 
+import spiegelgracht640 from './assets/spiegelgracht.640.jpg'
+import spiegelgracht1280 from './assets/spiegelgracht.1280.jpg'
+import spiegelgracht1440 from './assets/spiegelgracht.1440.jpg'
+
 const meta = {
   title: 'Components/Layout/Overlap',
   component: Overlap,
@@ -25,8 +29,8 @@ export const Default: Story = {
         aspectRatio="16:5"
         key={1}
         sizes="(max-width: 37.5rem) 640px, (max-width: 72.5rem) 1280px, 1440px"
-        src="https://picsum.photos/1440/450"
-        srcSet="https://picsum.photos/640/200 640w, https://picsum.photos/1280/400 1280w, https://picsum.photos/1440/450 1440w"
+        src={spiegelgracht1440}
+        srcSet={`${spiegelgracht640} 640w, ${spiegelgracht1280} 1280w, ${spiegelgracht1440} 1440w`}
       />,
       <Grid key={2} style={{ alignSelf: 'center' }}>
         <Grid.Cell span={{ narrow: 4, medium: 6, wide: 8 }} start={{ narrow: 1, medium: 2, wide: 3 }}>
