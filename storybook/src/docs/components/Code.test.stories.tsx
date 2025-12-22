ae06e433b4427bb22635baeb283e3dcfba4daeb7
E Wammes <e.wammes@amsterdam.nl>
2025-12-22 15:27:21 +0100

    Adding a test story for the Code component for visual regression tests

---
+++ storybook/src/docs/components/Code.test.stories.tsx
@@ -0,0 +1,28 @@
+/**
+ * @license EUPL-1.2+
+ * Copyright Gemeente Amsterdam
+ */
+
+import type { Meta, StoryObj } from '@storybook/react-vite'
+
+import { renderComponentVariants } from '../../_common/renderComponentVariants'
+import { Code } from './Code'
+
+const meta = {
+  title: 'Components/Text/Code',
+  args: {
+    children: ['var(--ams-color-text)'],
+  },
+} satisfies Meta<typeof Code>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Test: Story = {
+  render: (args) =>
+    renderComponentVariants(Code, {
+      args,
+    }),
+  tags: ['!dev', '!autodocs'],
+}
