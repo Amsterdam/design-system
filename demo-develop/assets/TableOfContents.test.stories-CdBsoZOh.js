9823dcbb7d2fa3491939157c2a9627e218b24226
VincentSmedinga <v.smedinga@amsterdam.nl>
2025-12-22 13:42:34 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@ddd6e1b53ab7e2a4dfd77814540e935651208a68 🚀

---
+++ demo-develop/assets/TableOfContents.test.stories-CdBsoZOh.js
@@ -0,0 +1,6 @@
+import{r as t}from"./renderComponentVariants-Cqgsyy8L.js";import{m as o,T as s}from"./TableOfContents.stories-RS7_7WeG.js";import"./iframe-B26LIiQA.js";import"./preload-helper-PPVm8Dsz.js";import"./Heading-COTonpN2.js";const c={...o,title:"Components/TableOfContents"},e={render:r=>t(s,{args:r}),tags:["!dev","!autodocs"]};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(TableOfContents, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...e.parameters?.docs?.source}}};const i=["Test"];export{e as Test,i as __namedExportsOrder,c as default};
