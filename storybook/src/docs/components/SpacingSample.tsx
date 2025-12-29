39a8ef5e803011108af537d1e4faf546259aa8ec
E Wammes <e.wammes@amsterdam.nl>
2025-12-29 15:41:02 +0100

    Add spacing preview component

---
+++ storybook/src/docs/components/SpacingSample.tsx
@@ -0,0 +1,26 @@
+/**
+ * @license EUPL-1.2+
+ * Copyright Gemeente Amsterdam
+ */
+
+import type { HTMLAttributes, PropsWithChildren } from 'react'
+
+import { clsx } from 'clsx'
+
+import './spacing-sample.css'
+import { formatTokenValue } from '../../_common/formatTokenValue'
+
+export type DivProps = PropsWithChildren<HTMLAttributes<HTMLDivElement>>
+
+type SpacingSampleProps = {
+  spacing?: string
+} & HTMLAttributes<HTMLDivElement>
+
+export const SpacingSample = ({ className, spacing }: SpacingSampleProps) => (
+  <div
+    className={clsx('ams-docs-spacing-sample', className)}
+    style={{
+      ...(spacing && { inlineSize: formatTokenValue(spacing) }),
+    }}
+  />
+)
