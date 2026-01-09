2241e819b966a86c57ba9cd3186f2a1cf8976dc7
alimpens <37216945+alimpens@users.noreply.github.com>
2026-01-09 14:09:34 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@006bed24daf7471cb4ec15e702f65d7abf8988e3 🚀

--- demo-release-please--branches--main/assets/Badge.test.stories-CBvkgxoH.js
+++
@@ -1,6 +0,0 @@
-import{r as a}from"./renderComponentVariants-tdrmpYaE.js";import{m as o}from"./Badge.stories-BeRIjpzn.js";import{B as t}from"./Badge-AdxP0T_q.js";import"./iframe-CUW3pAwY.js";import"./preload-helper-PPVm8Dsz.js";const c={...o,title:"Components/Feedback/Badge"},e={render:r=>a(t,{args:r}),tags:["!dev","!autodocs"]};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(Badge, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...e.parameters?.docs?.source}}};const i=["Test"];export{e as Test,i as __namedExportsOrder,c as default};
