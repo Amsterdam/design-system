a8e22613ea56ef69c07104e2b7c211adc43290e3
GitHub <github-action@users.noreply.github.com>
2026-01-12 23:20:36 +0000

    Remove feature branch folder

--- demo-npm_and_yarn/patch-and-minor-dev-dependencies-214e990a51/assets/Alert.test.stories-DOClt83L.js
+++
@@ -1,6 +0,0 @@
-import{r as t}from"./renderComponentVariants-CnEixjDE.js";import{m as o}from"./Alert.stories-CZOW59Bo.js";import{A as a}from"./Alert-Dp1-y-4K.js";import"./iframe-Doavr_St.js";import"./preload-helper-PPVm8Dsz.js";import"./Icon-iylZKtyZ.js";import"./Row-jbDbNj8l.js";import"./Heading-BBAOL3Jv.js";import"./getHeadingTag-Dv1eMqvl.js";import"./IconButton-Iw2sIe4A.js";const f={...o,title:"Components/Feedback/Alert"},r={render:e=>t(a,{args:e}),tags:["!dev","!autodocs"]};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(Alert, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...r.parameters?.docs?.source}}};const A=["Test"];export{r as Test,A as __namedExportsOrder,f as default};
--- demo-npm_and_yarn/patch-and-minor-dev-dependencies-214e990a51/assets/Pagination.test.stories-DeqvJS4I.js
+++
@@ -1,6 +0,0 @@
-import{r as t}from"./renderComponentVariants-CnEixjDE.js";import{m as e,P as o}from"./Pagination.stories-D1i-fKBc.js";import"./iframe-Doavr_St.js";import"./preload-helper-PPVm8Dsz.js";import"./Icon-iylZKtyZ.js";const p={...e,title:"Components/Navigation/Pagination"},r={render:a=>t(o,{args:a}),tags:["!dev","!autodocs"]};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(Pagination, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...r.parameters?.docs?.source}}};const c=["Test"];export{r as Test,c as __namedExportsOrder,p as default};
