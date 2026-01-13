a61c6466b1a7b71c8cc4a1f952ad4734f41241f3
E Wammes <e.wammes@amsterdam.nl>
2026-01-13 14:23:34 +0100

    Convert usage of variables in test story to hard coded value without renderComponentVariants

--- storybook/src/docs/components/SpacingSample.test.stories.tsx
+++ storybook/src/docs/components/SpacingSample.test.stories.tsx
@@ -5,14 +5,10 @@
 
 import type { Meta, StoryObj } from '@storybook/react-vite'
 
-import { renderComponentVariants } from '../../_common/renderComponentVariants'
 import { SpacingSample } from './SpacingSample'
 
 const meta = {
   title: 'Components/Storybook/Spacing Sample',
-  args: {
-    spacing: 'var(--ams-space-m)',
-  },
 } satisfies Meta<typeof SpacingSample>
 
 export default meta
@@ -20,9 +16,5 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const Test: Story = {
-  render: (args) =>
-    renderComponentVariants(SpacingSample, {
-      args,
-    }),
-  tags: ['!dev', '!autodocs'],
+  render: () => <SpacingSample spacing="1em" />,
 }
