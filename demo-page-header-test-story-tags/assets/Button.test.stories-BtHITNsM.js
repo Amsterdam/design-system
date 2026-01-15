91e6f167bdaa695ffcda770e3672301786305af1
Evi-2003 <69012052+Evi-2003@users.noreply.github.com>
2026-01-15 09:57:58 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@ce2822c14cb9acf4bea5397d32a46cb6aa6247fc 🚀

---
+++ demo-page-header-test-story-tags/assets/Button.test.stories-BtHITNsM.js
@@ -0,0 +1,7 @@
+import{r as e}from"./renderComponentVariants-CWM6MZ2a.js";import{m as o}from"./Button.stories-9Rj3c0Pg.js";import{B as a}from"./Button-Czrg49dk.js";import"./iframe-D9FUOIZz.js";import"./preload-helper-PPVm8Dsz.js";import"./Icon-C0c2VWxh.js";const c={...o,title:"Components/Buttons/Button"},r={render:t=>e(a,{args:t,variants:["disabled","hovered"]}),tags:["!dev","!autodocs"]};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(Button, {
+    args,
+    variants: ['disabled', 'hovered']
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...r.parameters?.docs?.source}}};const u=["Test"];export{r as Test,u as __namedExportsOrder,c as default};
---
+++ demo-page-header-test-story-tags/assets/Pagination.test.stories-CKKTqP-Y.js
@@ -0,0 +1,6 @@
+import{r as t}from"./renderComponentVariants-CWM6MZ2a.js";import{m as e,P as o}from"./Pagination.stories-CdqssP0B.js";import"./iframe-D9FUOIZz.js";import"./preload-helper-PPVm8Dsz.js";import"./Icon-C0c2VWxh.js";const p={...e,title:"Components/Navigation/Pagination"},r={render:a=>t(o,{args:a}),tags:["!dev","!autodocs"]};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(Pagination, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...r.parameters?.docs?.source}}};const c=["Test"];export{r as Test,c as __namedExportsOrder,p as default};
