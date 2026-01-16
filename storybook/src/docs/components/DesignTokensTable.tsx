107137dac28a2de3ef37fecb89b4f8fc5cad4173
Wammes <69012052+Evi-2003@users.noreply.github.com>
2026-01-16 15:53:52 +0100

    chore: Add typography preview to Design Tokens Table  (#2374)
    Co-authored-by: Vincent Smedinga <v.smedinga@amsterdam.nl>

--- storybook/src/docs/components/DesignTokensTable.tsx
+++ storybook/src/docs/components/DesignTokensTable.tsx
@@ -2,6 +2,7 @@ import { BorderSample } from './BorderSample'
 import { Code } from './Code'
 import { ColorSample } from './ColorSample'
 import { SpaceSample } from './SpaceSample'
+import { TypographySample } from './TypographySample'
 
 type Token = {
   $extensions?: {
@@ -79,6 +80,10 @@ const DesignTokensTableRow = ({ name, type, value }: DesignTokensTableRowProps) 
       {type === 'borderStyle' && <BorderSample style={value} />}
       {type === 'borderWidth' && <BorderSample width={value} />}
       {type === 'color' && value !== 'currentColor' && <ColorSample value={value} />}
+      {type === 'fontFamily' && <TypographySample fontFamily={value} />}
+      {type === 'fontSize' && <TypographySample fontSize={value} />}
+      {type === 'fontWeight' && <TypographySample fontWeight={value} />}
+      {type === 'lineHeight' && <TypographySample lineHeight={value} />}
       {type === 'space' && <SpaceSample value={value} />}
     </td>
   </tr>
