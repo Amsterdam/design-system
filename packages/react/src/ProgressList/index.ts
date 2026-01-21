8fd3111d7b1cb656c8364846d154889e15b43482
Vincent Smedinga <v.smedinga@amsterdam.nl>
2026-01-21 14:04:53 +0100

    Export types for subcomponents

--- packages/react/src/ProgressList/index.ts
+++ packages/react/src/ProgressList/index.ts
@@ -5,3 +5,6 @@
 
 export { ProgressList } from './ProgressList'
 export type { ProgressListProps } from './ProgressList'
+export type { ProgressListStepProps } from './ProgressListStep'
+export type { ProgressListSubstepProps } from './ProgressListSubstep'
+export type { ProgressListSubstepsProps } from './ProgressListSubsteps'
