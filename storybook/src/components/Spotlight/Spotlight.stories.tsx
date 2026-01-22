f131e66995d2eb651ca19b4e8bdfcd48de2d3f4f
Vincent Smedinga <v.smedinga@amsterdam.nl>
2026-01-22 13:54:57 +0100

    Add example of highlighting content

--- storybook/src/components/Spotlight/Spotlight.stories.tsx
+++ storybook/src/components/Spotlight/Spotlight.stories.tsx
@@ -5,7 +5,7 @@
 
 import type { Meta, StoryObj } from '@storybook/react-vite'
 
-import { Blockquote, Grid } from '@amsterdam/design-system-react'
+import { Blockquote, Grid, Heading, Paragraph, StandaloneLink } from '@amsterdam/design-system-react'
 import { Spotlight } from '@amsterdam/design-system-react/src'
 import { spotlightColors } from '@amsterdam/design-system-react/src/Spotlight/Spotlight'
 
@@ -42,4 +42,26 @@ export const Default: Story = {
       </Grid>
     </Spotlight>
   ),
+}
+
+export const HighlightContent: Story = {
+  args: {
+    children: (
+      <Grid paddingVertical="x-large">
+        <Grid.Cell span={{ narrow: 4, medium: 5, wide: 7 }}>
+          <Heading className="ams-mb-s" color="inverse" level={2} size="level-3">
+            Steun geven aan een partij
+          </Heading>
+          <Paragraph className="ams-mb-m" color="inverse">
+            Van 19 januari tot en met 2 februari kunt u uw steun geven aan een partij voor de gemeenteraad of een
+            stadsdeel- of bestuurscommissie. Dit doet u met een ondersteuningsverklaring.
+          </Paragraph>
+          <StandaloneLink color="inverse" href="#">
+            Ondersteuningsverklaring inleveren
+          </StandaloneLink>
+        </Grid.Cell>
+      </Grid>
+    ),
+    color: 'green',
+  },
 }
