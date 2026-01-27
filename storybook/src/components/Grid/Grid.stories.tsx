db7782f729f2092d559938382a4f190e5866dd08
Vincent Smedinga <v.smedinga@amsterdam.nl>
2026-01-27 15:21:48 +0100

    Prefix classes of Grid component

--- storybook/src/components/Grid/Grid.stories.tsx
+++ storybook/src/components/Grid/Grid.stories.tsx
@@ -41,9 +41,9 @@ type Story = StoryObj<typeof meta>
 type CellStory = StoryObj<typeof cellMeta>
 
 const BackgroundGrid = () => (
-  <Grid className="ams-docs-grid">
+  <Grid className="_ams-grid-columns-guide">
     {Array.from(Array(12).keys()).map((i) => (
-      <Grid.Cell className="ams-docs-grid__cell" key={i} />
+      <Grid.Cell className="_ams-grid-columns-guide__cell" key={i} />
     ))}
   </Grid>
 )
