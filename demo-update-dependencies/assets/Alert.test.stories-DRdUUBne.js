b348c5f0147dda267a3e5dfe1de16aa6e04191b0
GitHub <github-action@users.noreply.github.com>
2026-01-28 16:09:47 +0000

    Remove feature branch folder

--- demo-update-dependencies/assets/Alert.test.stories-DRdUUBne.js
+++
@@ -1,6 +0,0 @@
-import{r as t}from"./renderComponentVariants-Bb0p44Ln.js";import{m as o}from"./Alert.stories-DX3UAY1l.js";import{A as a}from"./Alert-DF9cJSzk.js";import"./iframe-CdxcyWdy.js";import"./preload-helper-PPVm8Dsz.js";import"./Icon-DGCCnR4f.js";import"./Row-BMedlPf4.js";import"./Heading-5SLplAP5.js";import"./getHeadingTag-Dv1eMqvl.js";import"./IconButton-BIMldyTF.js";const f={...o,title:"Components/Feedback/Alert"},r={render:e=>t(a,{args:e}),tags:["!dev","!autodocs"]};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(Alert, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...r.parameters?.docs?.source}}};const A=["Test"];export{r as Test,A as __namedExportsOrder,f as default};
--- demo-update-dependencies/assets/TableOfContents.test.stories-Bm_6aaXW.js
+++
@@ -1,6 +0,0 @@
-import{r as t}from"./renderComponentVariants-Bb0p44Ln.js";import{m as o,T as s}from"./TableOfContents.stories-p36tDIHA.js";import"./iframe-CdxcyWdy.js";import"./preload-helper-PPVm8Dsz.js";import"./Heading-5SLplAP5.js";import"./getHeadingTag-Dv1eMqvl.js";const i={...o,title:"Components/TableOfContents"},e={render:r=>t(s,{args:r}),tags:["!dev","!autodocs"]};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(TableOfContents, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...e.parameters?.docs?.source}}};const f=["Test"];export{e as Test,f as __namedExportsOrder,i as default};
