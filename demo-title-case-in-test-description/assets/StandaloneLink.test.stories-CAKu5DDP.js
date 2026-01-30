2b4a4983eeeb761e040d5fe38a6c461479b91abe
alimpens <37216945+alimpens@users.noreply.github.com>
2026-01-30 14:55:16 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@1536d217ef8bc084b1ad06b12c332f55a623fc22 🚀

--- demo-title-case-in-test-description/assets/StandaloneLink.test.stories-CAKu5DDP.js
+++
@@ -1,6 +0,0 @@
-import{r as a}from"./renderComponentVariants-DBOCg0O1.js";import{m as t,S as o}from"./StandaloneLink.stories-C0gYjh-B.js";import"./iframe-B75N0tKX.js";import"./preload-helper-PPVm8Dsz.js";import"./Icon-rCaOdMEG.js";const i={...t,title:"Components/StandaloneLink"},e={render:r=>a(o,{args:r}),tags:["!dev","!autodocs"]};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(StandaloneLink, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...e.parameters?.docs?.source}}};const c=["Test"];export{e as Test,c as __namedExportsOrder,i as default};
