e6e65f8a7167043df522882b86a8e9e8cec3e067
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-02-01 22:45:18 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@42d6c09dd051b9a638f4f1508f976474e4c4c939 🚀

---
+++ demo-DES-1523-internal-websites-page-background/assets/TimeInput.test.stories-ZD4NcMTd.js
@@ -0,0 +1,7 @@
+import{r as t}from"./renderComponentVariants-t0Ds3zhu.js";import{m as a,T as s}from"./TimeInput.stories-DjF-SWz3.js";import"./iframe-Cv0AjmKM.js";import"./preload-helper-PPVm8Dsz.js";const i={...a,title:"Components/Forms/Time Input"},e={render:r=>t(s,{args:r,variants:["disabled","hovered"]}),tags:["!dev","!autodocs"]};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(TimeInput, {
+    args,
+    variants: ['disabled', 'hovered']
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...e.parameters?.docs?.source}}};const p=["Test"];export{e as Test,p as __namedExportsOrder,i as default};
