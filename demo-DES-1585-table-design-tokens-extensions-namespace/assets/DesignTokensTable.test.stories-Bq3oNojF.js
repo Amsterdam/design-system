c774e30eaf9672cd6a4c1660558e27dd37fa8fdc
GitHub <github-action@users.noreply.github.com>
2026-02-04 10:46:32 +0000

    Remove feature branch folder

--- demo-DES-1585-table-design-tokens-extensions-namespace/assets/DesignTokensTable.test.stories-Bq3oNojF.js
+++
@@ -1,8 +0,0 @@
-import{j as o}from"./iframe-DZBTdkmA.js";import{D as s}from"./DesignTokensTable-D3xpJFmW.js";import"./preload-helper-PPVm8Dsz.js";import"./BorderSample-Ca_mvWkr.js";import"./formatTokenValue-BYjgZrXg.js";import"./Code-DQZ9Sd5S.js";import"./ColorSample-BRDj_SU0.js";import"./SpaceSample-CYdxg5gi.js";import"./TypographySample-CEjJ2H2Y.js";const u={title:"Components/Docs/Design Tokens Table"},e={render:()=>o.jsx(s,{tokens:{name:{$value:"value"}}}),tags:["!dev","!autodocs"]};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  render: () => <DesignTokensTable tokens={{
-    name: {
-      $value: 'value'
-    }
-  }} />,
-  tags: ['!dev', '!autodocs']
-}`,...e.parameters?.docs?.source}}};const l=["Test"];export{e as Test,l as __namedExportsOrder,u as default};
--- demo-DES-1585-table-design-tokens-extensions-namespace/assets/HomePage.docs-CuR7fjxl.js
+++
@@ -1,6 +0,0 @@
-import{j as e}from"./iframe-DZBTdkmA.js";import{useMDXComponents as r}from"./index-C5ujInhi.js";import{M as s}from"./blocks-Dx5YYChx.js";import{H as a}from"./HomePage.stories-DOJoFwDr.js";import"./preload-helper-PPVm8Dsz.js";import"./index-q6kvUtaC.js";function o(n){const t={h1:"h1",p:"p",...r(),...n.components};return e.jsxs(e.Fragment,{children:[`
-`,`
-`,e.jsx(s,{of:a}),`
-`,e.jsx(t.h1,{id:"home-page",children:"Home page"}),`
-`,e.jsx(t.p,{children:"The home page of an internal website can have varying types of content."}),`
-`,e.jsx(t.p,{children:"Our single example in this category currently focuses on the overall layout."})]})}function u(n={}){const{wrapper:t}={...r(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o(n)}export{u as default};
