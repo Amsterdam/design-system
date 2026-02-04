06379a49022e591247eb03c8a8591f328c81c4ef
GitHub <github-action@users.noreply.github.com>
2026-02-04 12:42:42 +0000

    Remove feature branch folder

--- demo-spotlight-examples/assets/HomePage.docs-DFXmasgR.js
+++
@@ -1,6 +0,0 @@
-import{j as e}from"./iframe-Dz9cbi2t.js";import{useMDXComponents as r}from"./index-BFug2lb6.js";import{M as s}from"./blocks-iyODGV6v.js";import{H as a}from"./HomePage.stories-B6woQXNk.js";import"./preload-helper-PPVm8Dsz.js";import"./index-D9pwJnUc.js";function o(n){const t={h1:"h1",p:"p",...r(),...n.components};return e.jsxs(e.Fragment,{children:[`
-`,`
-`,e.jsx(s,{of:a}),`
-`,e.jsx(t.h1,{id:"home-page",children:"Home page"}),`
-`,e.jsx(t.p,{children:"The home page of an internal website can have varying types of content."}),`
-`,e.jsx(t.p,{children:"Our single example in this category currently focuses on the overall layout."})]})}function u(n={}){const{wrapper:t}={...r(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o(n)}export{u as default};
