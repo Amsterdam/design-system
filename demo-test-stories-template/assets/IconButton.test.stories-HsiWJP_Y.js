b426661ef936110a3869ddba7c7e27d00c9447a5
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-02-05 10:49:12 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@e3478658f0268e34f7e81db87df5ab929e5365f1 🚀

---
+++ demo-test-stories-template/assets/IconButton.test.stories-HsiWJP_Y.js
@@ -0,0 +1,7 @@
+import{r as o}from"./renderComponentVariants-CGGhGGQK.js";import{m as e}from"./IconButton.stories-nIW-1uhH.js";import{I as a}from"./IconButton-BuUG2Nu9.js";import"./iframe-Dz9cbi2t.js";import"./preload-helper-PPVm8Dsz.js";import"./Icon-B7mkEM3d.js";const c={...e,title:"Components/Buttons/Icon Button"},r={render:t=>o(a,{args:t,variants:["hovered","disabled"]}),tags:["!dev","!autodocs"]};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(IconButton, {
+    args,
+    variants: ['hovered', 'disabled']
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...r.parameters?.docs?.source}}};const u=["Test"];export{r as Test,u as __namedExportsOrder,c as default};
---
+++ demo-test-stories-template/assets/Radio.test.stories-CBNs5we2.js
@@ -0,0 +1,6 @@
+import{r as o}from"./renderComponentVariants-CGGhGGQK.js";import{m as a,R as s}from"./Radio.stories-BvC-Xuqe.js";import"./iframe-Dz9cbi2t.js";import"./preload-helper-PPVm8Dsz.js";const p={...a,title:"Components/Forms/Radio"},r={render:e=>o(s,{args:e}),tags:["!dev","!autodocs"]};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(Radio, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...r.parameters?.docs?.source}}};const i=["Test"];export{r as Test,i as __namedExportsOrder,p as default};
