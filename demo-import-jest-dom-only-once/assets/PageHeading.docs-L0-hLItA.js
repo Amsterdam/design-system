6134ca64fa7c55cafe7b265781b911d804e4b014
GitHub <github-action@users.noreply.github.com>
2026-02-13 14:26:22 +0000

    Remove feature branch folder

--- demo-import-jest-dom-only-once/assets/PageHeading.docs-L0-hLItA.js
+++
@@ -1,9 +0,0 @@
-import{j as e}from"./iframe-RcDKJEnZ.js";import{useMDXComponents as r}from"./index-CCOKwhsR.js";import{M as s,e as i,f as a}from"./blocks-zUSNToBk.js";import{P as m}from"./PageHeading.stories-DFXqY4NG.js";import{S as p}from"./StatusBadge-BLGPWQ5k.js";import"./preload-helper-PPVm8Dsz.js";import"./index-B_f1KmGT.js";import"./Badge-CLZWdrBc.js";import"./Icon-7h63n85b.js";const d=`<!-- @license CC0-1.0 -->
-
-# Page Heading
-`;function o(n){return e.jsxs(e.Fragment,{children:[`
-`,`
-`,e.jsx(p,{description:"We no longer use this size of headings. Use the Heading component with level 1 instead.",status:"deprecated"}),`
-`,e.jsx(s,{of:m}),`
-`,e.jsx(i,{children:d}),`
-`,e.jsx(a,{})]})}function C(n={}){const{wrapper:t}={...r(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{C as default};
--- demo-import-jest-dom-only-once/assets/Pagination.test.stories-6EYs2xez.js
+++
@@ -1,6 +0,0 @@
-import{r as t}from"./renderComponentVariants-C5yGn9z8.js";import{m as e,P as o}from"./Pagination.stories-BDW0vD-X.js";import"./iframe-RcDKJEnZ.js";import"./preload-helper-PPVm8Dsz.js";import"./Icon-7h63n85b.js";const p={...e,title:"Components/Navigation/Pagination"},r={render:a=>t(o,{args:a}),tags:["!dev","!autodocs"]};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(Pagination, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...r.parameters?.docs?.source}}};const c=["Test"];export{r as Test,c as __namedExportsOrder,p as default};
