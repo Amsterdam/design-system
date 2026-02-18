21cde07adde110529cfdb917c6d3ca97d06ed7ae
alimpens <a.limpens@amsterdam.nl>
2026-02-18 14:44:54 +0100

    Do not extract Home and Article pages to separate components

--- storybook/src/pages/public/ArticlePage/ArticlePage.stories.tsx
+++ storybook/src/pages/public/ArticlePage/ArticlePage.stories.tsx
@@ -5,17 +5,121 @@
 
 import type { Meta, StoryObj } from '@storybook/react-vite'
 
+import {
+  Breadcrumb,
+  Card,
+  Grid,
+  Heading,
+  Image,
+  LinkList,
+  Paragraph,
+  Spotlight,
+  StandaloneLink,
+} from '@amsterdam/design-system-react'
+import parse from 'html-react-parser'
+import { Fragment } from 'react'
+
 import type { NewsArticle } from './news-articles'
 
+import { formatDate } from '../../../common'
 import { commonMeta } from '../common/config'
-import { ArticlePage } from './ArticlePage'
+import { newsSection } from '../HomePage/data'
 import { newsArticles } from './news-articles'
+import { parserOptions } from './parser.config'
 
 const meta = {
   ...commonMeta,
   title: 'Pages/Public/Article Page',
-  component: ArticlePage,
-} satisfies Meta<typeof ArticlePage>
+
+  render: (args) => (
+    <>
+      <Grid paddingTop="large">
+        <Grid.Cell span={{ narrow: 4, medium: 7, wide: 9 }} start={{ narrow: 1, medium: 1, wide: 2 }}>
+          <Breadcrumb>
+            <Breadcrumb.Link href="#">Home</Breadcrumb.Link>
+            <Breadcrumb.Link href="#">Nieuws</Breadcrumb.Link>
+          </Breadcrumb>
+        </Grid.Cell>
+      </Grid>
+      <Grid as="main" id="inhoud" paddingBottom="x-large">
+        <Grid.Cell span={{ narrow: 4, medium: 7, wide: 9 }} start={{ narrow: 1, medium: 1, wide: 2 }}>
+          <Heading className="ams-mb-s" level={1}>
+            {args.heading}
+          </Heading>
+          <Paragraph className="ams-mb-xl">{formatDate(args.published)}</Paragraph>
+          <Paragraph size="large">{args.lead}</Paragraph>
+        </Grid.Cell>
+      </Grid>
+      <Image
+        alt={args.image.alt ?? ''}
+        aspectRatio="16:5"
+        loading="lazy"
+        src={args.image.src}
+        srcSet={args.image.srcSet}
+      />
+      <Grid paddingVertical="x-large">
+        <Grid.Cell span={{ narrow: 4, medium: 6, wide: 7 }} start={{ narrow: 1, medium: 2, wide: 3 }}>
+          {args.sections.map(({ body, heading, id }) => (
+            <Fragment key={id}>
+              <Heading className="ams-mb-s" level={2}>
+                {heading}
+              </Heading>
+              {parse(body, parserOptions)}
+            </Fragment>
+          ))}
+          <Heading className="ams-mb-s" level={2}>
+            Lees ook
+          </Heading>
+          <LinkList>
+            {args.relatedArticles?.map(({ heading, url }) => (
+              <LinkList.Link href={url} key={url}>
+                {heading}
+              </LinkList.Link>
+            ))}
+          </LinkList>
+        </Grid.Cell>
+      </Grid>
+      <Spotlight as="aside" color="green">
+        <Grid paddingVertical="x-large">
+          <Grid.Cell span={{ narrow: 4, medium: 6, wide: 7 }} start={{ narrow: 1, medium: 2, wide: 3 }}>
+            <Heading className="ams-mb-s" color="inverse" level={2} size="level-3">
+              Blijf op de hoogte!
+            </Heading>
+            <Paragraph className="ams-mb-m" color="inverse">
+              Schrijf u nu in voor de Nieuwsbrief Amsterdam en ontvang wekelijks nieuws, tips en mooie verhalen over de
+              stad en uw stadsdeel.
+            </Paragraph>
+            <StandaloneLink color="inverse" href="#">
+              Ik wil de nieuwsbrief
+            </StandaloneLink>
+          </Grid.Cell>
+        </Grid>
+      </Spotlight>
+      <aside>
+        <Grid gapVertical="large" paddingVertical="x-large">
+          <Grid.Cell span="all">
+            <Heading level={2} size="level-1">
+              Meer nieuws
+            </Heading>
+          </Grid.Cell>
+          {newsSection.items.map((item, index) => (
+            <Grid.Cell key={index} span={4}>
+              <Card>
+                <Card.Image alt="" src={item.image} />
+                <Card.HeadingGroup tagline="Nieuws">
+                  <Card.Heading level={3}>
+                    <Card.Link href="#">{item.title}</Card.Link>
+                  </Card.Heading>
+                </Card.HeadingGroup>
+                <Paragraph>{item.description}</Paragraph>
+              </Card>
+            </Grid.Cell>
+          ))}
+        </Grid>
+      </aside>
+    </>
+  ),
+} satisfies Meta<NewsArticle>
 
 export default meta
 
--- storybook/src/pages/public/HomePage/HomePage.stories.tsx
+++ storybook/src/pages/public/HomePage/HomePage.stories.tsx
@@ -3,16 +3,79 @@
  * Copyright Gemeente Amsterdam
  */
 
-import type { Meta, StoryObj } from '@storybook/react-vite'
+import type { StoryObj } from '@storybook/react-vite'
 
+import { Card, Grid, Heading, Overlap, Paragraph, Spotlight, StandaloneLink } from '@amsterdam/design-system-react'
+
+import { Default as OverlapStory } from '../../../components/Overlap/Overlap.stories'
 import { commonMeta } from '../common/config'
-import { HomePage } from './HomePage'
+import { newsSection, spotlightBlocks, topTaskSection } from './data'
 
 const meta = {
   ...commonMeta,
   title: 'Pages/Public/Home Page',
-  component: HomePage,
-} satisfies Meta<typeof HomePage>
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  render: (args: unknown) => (
+    <main id="inhoud">
+      <h1 className="ams-visually-hidden">Homepage van de gemeente Amsterdam</h1>
+      <Overlap>{OverlapStory.args?.children}</Overlap>
+      <Grid gapVertical="large" paddingVertical="x-large">
+        <Grid.Cell span="all">
+          <Heading level={2} size="level-1">
+            {topTaskSection.title}
+          </Heading>
+        </Grid.Cell>
+        {topTaskSection.tasks.map((task) => (
+          <Grid.Cell key={task.title} span={{ narrow: 4, medium: 4, wide: 3 }}>
+            <Card>
+              <Card.Heading level={3}>
+                <Card.Link href="#">{task.title}</Card.Link>
+              </Card.Heading>
+              <Paragraph>{task.description}</Paragraph>
+            </Card>
+          </Grid.Cell>
+        ))}
+      </Grid>
+      <Spotlight>
+        <Grid paddingVertical="x-large">
+          {spotlightBlocks.map((section) => (
+            <Grid.Cell key={section.title} span={{ narrow: 4, medium: 4, wide: 6 }}>
+              <Heading className="ams-mb-s" color="inverse" level={2} size="level-3">
+                {section.title}
+              </Heading>
+              <Paragraph className="ams-mb-m" color="inverse">
+                {section.description}
+              </Paragraph>
+              <StandaloneLink color="inverse" href="#">
+                {section.link}
+              </StandaloneLink>
+            </Grid.Cell>
+          ))}
+        </Grid>
+      </Spotlight>
+      <Grid gapVertical="large" paddingVertical="x-large">
+        <Grid.Cell span="all">
+          <Heading level={2} size="level-1">
+            {newsSection.title}
+          </Heading>
+        </Grid.Cell>
+        {newsSection.items.map((item, index) => (
+          <Grid.Cell key={index} span={4}>
+            <Card>
+              <Card.Image alt="" src={item.image} />
+              <Card.HeadingGroup tagline="Nieuws">
+                <Card.Heading level={3}>
+                  <Card.Link href="#">{item.title}</Card.Link>
+                </Card.Heading>
+              </Card.HeadingGroup>
+              <Paragraph>{item.description}</Paragraph>
+            </Card>
+          </Grid.Cell>
+        ))}
+      </Grid>
+    </main>
+  ),
+}
 
 export default meta
 
