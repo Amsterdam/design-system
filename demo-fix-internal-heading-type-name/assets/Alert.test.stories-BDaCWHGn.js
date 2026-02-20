20d7f507e8545e2083f186078ec14896664c0199
GitHub <github-action@users.noreply.github.com>
2026-02-20 12:50:34 +0000

    Remove feature branch folder

--- demo-fix-internal-heading-type-name/assets/Alert.test.stories-BDaCWHGn.js
+++
@@ -1,6 +0,0 @@
-import{r as t}from"./renderComponentVariants-CDDeAikF.js";import{m as o}from"./Alert.stories-Ib3sws45.js";import{A as a}from"./Alert-CTOVO30V.js";import"./iframe-moilhtc2.js";import"./preload-helper-PPVm8Dsz.js";import"./Icon-iyWua8OR.js";import"./Row-D3OvsRFX.js";import"./Heading-B-CHtN23.js";import"./getHeadingTag-Dv1eMqvl.js";import"./IconButton-CFAJbBCe.js";const f={...o,title:"Components/Feedback/Alert"},r={render:e=>t(a,{args:e}),tags:["!dev","!autodocs"]};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(Alert, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...r.parameters?.docs?.source}}};const A=["Test"];export{r as Test,A as __namedExportsOrder,f as default};
--- demo-fix-internal-heading-type-name/assets/StandaloneLink.test.stories-BFW0mVGU.js
+++
@@ -1,6 +0,0 @@
-import{r as a}from"./renderComponentVariants-CDDeAikF.js";import{m as t,S as o}from"./StandaloneLink.stories-Chm8RTK5.js";import"./iframe-moilhtc2.js";import"./preload-helper-PPVm8Dsz.js";import"./Icon-iyWua8OR.js";const i={...t,title:"Components/StandaloneLink"},e={render:r=>a(o,{args:r}),tags:["!dev","!autodocs"]};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(StandaloneLink, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...e.parameters?.docs?.source}}};const c=["Test"];export{e as Test,c as __namedExportsOrder,i as default};
