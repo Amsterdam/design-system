69c4c733c4176155460f0be5d52ead2984286db9
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-02-22 12:11:07 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@5a05c96d08e59f49fc0bff002895209e48da930f 🚀

---
+++ demo-DES-1616-extra-dialog-tokens/assets/IconButton.test.stories-CLCbGUuY.js
@@ -0,0 +1,7 @@
+import{r as o}from"./renderComponentVariants-GpwEDHA2.js";import{m as e}from"./IconButton.stories-sE_3_OS_.js";import{I as a}from"./IconButton-BwzBL_P0.js";import"./iframe-CoWMxKf4.js";import"./preload-helper-PPVm8Dsz.js";import"./Icon-qiISpTaD.js";const c={...e,title:"Components/Buttons/Icon Button"},r={render:t=>o(a,{args:t,variants:["hovered","disabled"]}),tags:["!dev","!autodocs"]};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(IconButton, {
+    args,
+    variants: ['hovered', 'disabled']
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...r.parameters?.docs?.source}}};const u=["Test"];export{r as Test,u as __namedExportsOrder,c as default};
---
+++ demo-DES-1616-extra-dialog-tokens/assets/formatter-EIJCOSYU-Bq6JIWAv.js
@@ -0,0 +1,1 @@
+import{d as m,r as e}from"./blocks-BvB5spCY.js";import{w as o}from"./iframe-CoWMxKf4.js";import"./preload-helper-PPVm8Dsz.js";import"./index-C4d0SyW2.js";var t=o(e(),1),s=(0,t.default)(2)(async(i,r)=>i===!1?r:m(r));export{s as formatter};
