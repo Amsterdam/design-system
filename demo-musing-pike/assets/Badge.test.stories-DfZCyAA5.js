02ee8f85b70cee78ff50deaf22e1d74aad6dd76f
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-02-26 14:18:06 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@1fd84cff2857fd4fde4a661512bd6c58b37400a0 🚀

---
+++ demo-musing-pike/assets/Badge.test.stories-DfZCyAA5.js
@@ -0,0 +1,6 @@
+import{r as a}from"./renderComponentVariants-CCnF8eIC.js";import{m as o}from"./Badge.stories-C6dKv35z.js";import{B as t}from"./Badge-CIvuzOCi.js";import"./iframe-Cd6waTMF.js";import"./preload-helper-PPVm8Dsz.js";import"./Icon-p-GC9MQv.js";const i={...o,title:"Components/Feedback/Badge"},e={render:r=>a(t,{args:r}),tags:["!dev","!autodocs"]};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(Badge, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...e.parameters?.docs?.source}}};const g=["Test"];export{e as Test,g as __namedExportsOrder,i as default};
---
+++ demo-musing-pike/assets/InvalidFormAlert.test.stories-CM5x7c2c.js
@@ -0,0 +1,6 @@
+import{r as t}from"./renderComponentVariants-CCnF8eIC.js";import{m as e,I as a}from"./InvalidFormAlert.stories-DDes1bLs.js";import"./iframe-Cd6waTMF.js";import"./preload-helper-PPVm8Dsz.js";import"./Alert-BI6ANtDz.js";import"./Icon-p-GC9MQv.js";import"./Row-okA3QeSv.js";import"./Heading-_99CGLuP.js";import"./getHeadingTag-Dv1eMqvl.js";import"./IconButton-DUtpQOh9.js";import"./LinkList-ehwK0YCK.js";const F={...e,title:"Components/Forms/Invalid Form Alert"},r={render:o=>t(a,{args:o}),tags:["!dev","!autodocs"]};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(InvalidFormAlert, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...r.parameters?.docs?.source}}};const I=["Test"];export{r as Test,I as __namedExportsOrder,F as default};
