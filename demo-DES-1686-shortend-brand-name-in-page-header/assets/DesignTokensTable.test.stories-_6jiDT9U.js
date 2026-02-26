7c6122b539c00168dbfc13241f9eb7d00927d22c
RubenSibon <mail@rubensibon.nl>
2026-02-26 08:48:44 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@ac43b0a1ed17e171eadd30ec783814b5494a413e 🚀

---
+++ demo-DES-1686-shortend-brand-name-in-page-header/assets/DesignTokensTable.test.stories-_6jiDT9U.js
@@ -0,0 +1,8 @@
+import{j as o}from"./iframe-DLzPVvzy.js";import{D as s}from"./DesignTokensTable-CnYa2Q3z.js";import"./preload-helper-PPVm8Dsz.js";import"./BorderSample-DT0i2v4S.js";import"./formatTokenValue-BYjgZrXg.js";import"./Code-cv9KwNdx.js";import"./ColorSample-DpiYTHlJ.js";import"./SpaceSample-EJc_QvTD.js";import"./TypographySample-BzvAa-m2.js";const u={title:"Components/Docs/Design Tokens Table"},e={render:()=>o.jsx(s,{tokens:{name:{$value:"value"}}}),tags:["!dev","!autodocs"]};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  render: () => <DesignTokensTable tokens={{
+    name: {
+      $value: 'value'
+    }
+  }} />,
+  tags: ['!dev', '!autodocs']
+}`,...e.parameters?.docs?.source}}};const l=["Test"];export{e as Test,l as __namedExportsOrder,u as default};
---
+++ demo-DES-1686-shortend-brand-name-in-page-header/assets/Mark.docs-DSrJAAiu.js
@@ -0,0 +1,29 @@
+import{j as n}from"./iframe-DLzPVvzy.js";import{useMDXComponents as s}from"./index-CV_WEPyN.js";import{M as o,e as r,f as i,h as m,i as c}from"./blocks-YwgmgD8e.js";import{M as l,S as p}from"./Mark.stories-W1yGRU9b.js";import{D as h}from"./DesignTokensTable-CnYa2Q3z.js";import"./preload-helper-PPVm8Dsz.js";import"./index-8DXw7sXw.js";import"./BorderSample-DT0i2v4S.js";import"./formatTokenValue-BYjgZrXg.js";import"./Code-cv9KwNdx.js";import"./ColorSample-DpiYTHlJ.js";import"./SpaceSample-EJc_QvTD.js";import"./TypographySample-BzvAa-m2.js";const d=`<!-- @license CC0-1.0 -->
+
+# Mark
+
+Highlights a text fragment using a yellow background.
+Provides strong emphasis on the text and draws attention to it.
+
+## Guidelines
+
+- Use a maximum of 1 Mark per page.
+- Limit the marked text to 1 sentence.
+- Do not use it in titles and subtitles.
+  Use a significant and engaging title if the entire paragraph is important.
+- Do not mark an entire paragraph.
+`,x={mark:{"background-color":{$value:"{ams.color.highlight.yellow}",$type:"color"}}},u={ams:x};function a(t){const e={h2:"h2",h3:"h3",p:"p",...s(),...t.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,`
+`,`
+`,n.jsx(o,{of:l}),`
+`,n.jsx(r,{children:d}),`
+`,n.jsx(i,{}),`
+`,n.jsx(m,{}),`
+`,n.jsx(e.h2,{id:"examples",children:"Examples"}),`
+`,n.jsx(e.h3,{id:"search-results",children:"Search results"}),`
+`,n.jsx(e.p,{children:`For a basic search, mark all appearances of every keyword.
+Ignore capitalization and include partial matches.`}),`
+`,n.jsx(c,{of:p}),`
+`,n.jsx(e.h2,{id:"tokens",children:"Tokens"}),`
+`,n.jsx(h,{tokens:u})]})}function R(t={}){const{wrapper:e}={...s(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(a,{...t})}):a(t)}export{R as default};
