ee20ce3953dbfdd6a9e8cd48b39d483d96ac7d1d
Evi-2003 <69012052+Evi-2003@users.noreply.github.com>
2026-03-11 11:53:33 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@a1c4617e8346b34c1222f7599dd3f4d93d9aa039 🚀

---
+++ demo-DES-1652-add-tests-for-props/assets/InvalidFormAlert.test.stories-CIe7_Uob.js
@@ -0,0 +1,6 @@
+import{r as t}from"./renderComponentVariants-DhPptRQn.js";import{m as e,I as a}from"./InvalidFormAlert.stories-leSb_ZOF.js";import"./iframe-Pj6X5Xd2.js";import"./preload-helper-PPVm8Dsz.js";import"./Alert-CeVQiMc-.js";import"./Icon-BquSYbTy.js";import"./Row-CGOqcy6z.js";import"./Heading-BCOIFVMI.js";import"./getHeadingTag-Dv1eMqvl.js";import"./IconButton-D3pwG479.js";import"./LinkList-CSZeQitp.js";const F={...e,title:"Components/Forms/Invalid Form Alert"},r={render:o=>t(a,{args:o}),tags:["!dev","!autodocs"]};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(InvalidFormAlert, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...r.parameters?.docs?.source}}};const I=["Test"];export{r as Test,I as __namedExportsOrder,F as default};
---
+++ demo-DES-1652-add-tests-for-props/assets/Mark.docs-BO8pOjeA.js
@@ -0,0 +1,29 @@
+import{j as n}from"./iframe-Pj6X5Xd2.js";import{useMDXComponents as s}from"./index-cke0GlvE.js";import{M as o,e as r,f as i,h as m,i as c}from"./blocks-CctasR3o.js";import{M as l,S as p}from"./Mark.stories-B9mfz-h3.js";import{D as h}from"./DesignTokensTable-BnJj4w-3.js";import"./preload-helper-PPVm8Dsz.js";import"./index-B0rxKYzH.js";import"./BorderSample-Cr96SEmz.js";import"./formatTokenValue-BYjgZrXg.js";import"./Code-CfOxkvuP.js";import"./ColorSample-DwS5UJaE.js";import"./SpaceSample-BkSdeLwA.js";import"./TypographySample-BT5_7M25.js";const d=`<!-- @license CC0-1.0 -->
+
+# Mark
+
+Highlights a text fragment using a yellow background.
+Provides strong emphasis on the text and draws attention to it.
+
+## Guidelines
+
+- Use a maximum of 1 Mark per page.
+- Limit the marked text to 1 sentence.
+- Do not use it in titles and subtitles.
+  Use a significant and engaging title if the entire paragraph is important.
+- Do not mark an entire paragraph.
+`,x={mark:{"background-color":{$value:"{ams.color.highlight.yellow}",$type:"color"}}},u={ams:x};function a(t){const e={h2:"h2",h3:"h3",p:"p",...s(),...t.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,`
+`,`
+`,n.jsx(o,{of:l}),`
+`,n.jsx(r,{children:d}),`
+`,n.jsx(i,{}),`
+`,n.jsx(m,{}),`
+`,n.jsx(e.h2,{id:"examples",children:"Examples"}),`
+`,n.jsx(e.h3,{id:"search-results",children:"Search results"}),`
+`,n.jsx(e.p,{children:`For a basic search, mark all appearances of every keyword.
+Ignore capitalization and include partial matches.`}),`
+`,n.jsx(c,{of:p}),`
+`,n.jsx(e.h2,{id:"tokens",children:"Tokens"}),`
+`,n.jsx(h,{tokens:u})]})}function R(t={}){const{wrapper:e}={...s(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(a,{...t})}):a(t)}export{R as default};
