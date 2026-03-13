d943ca7fbd2e7403323aa48573117614b802969f
GitHub <github-action@users.noreply.github.com>
2026-03-13 13:27:25 +0000

    Remove feature branch folder

--- demo-DES-1694-unique-landmarks-in-header/assets/Icon.test.stories-fB-a7AmE.js
+++
@@ -1,6 +0,0 @@
-import{r as e}from"./renderComponentVariants-D3iHUXmA.js";import{m as t}from"./Icon.stories-A1kmpJkl.js";import{I as a}from"./Icon-B1YxoY8l.js";import"./iframe-DxJrxckW.js";import"./preload-helper-PPVm8Dsz.js";import"./Paragraph-CQ9SWbLZ.js";import"./Row-D06qnanh.js";const u={...t,title:"Components/Media/Icon"},r={render:o=>e(a,{args:o}),tags:["!dev","!autodocs"]};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(Icon, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...r.parameters?.docs?.source}}};const g=["Test"];export{r as Test,g as __namedExportsOrder,u as default};
--- demo-DES-1694-unique-landmarks-in-header/assets/config-SKXB22wd.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./iframe-DxJrxckW.js";import{L as r}from"./Layout-pb7I4mp6.js";const a={decorators:[o=>e.jsx(r,{children:e.jsx(o,{})})],parameters:{layout:"fullscreen",themes:{themeOverride:"Spacious"}}};export{a as c};
