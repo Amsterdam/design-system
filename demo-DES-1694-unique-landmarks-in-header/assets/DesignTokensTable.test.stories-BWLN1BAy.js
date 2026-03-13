20e99dc5826b65711d9db53f86fab400dbe9642a
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-03-13 12:03:40 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@da23f6d32b558792c3b1e6dcfe940434b5b451ae 🚀

---
+++ demo-DES-1694-unique-landmarks-in-header/assets/DesignTokensTable.test.stories-BWLN1BAy.js
@@ -0,0 +1,8 @@
+import{j as o}from"./iframe-DFYCrGlq.js";import{D as s}from"./DesignTokensTable-txgEnd3N.js";import"./preload-helper-PPVm8Dsz.js";import"./BorderSample-_m1xgqxS.js";import"./formatTokenValue-BYjgZrXg.js";import"./Code-Cj8K2APd.js";import"./ColorSample-DDt98GtM.js";import"./SpaceSample-urXeCWEf.js";import"./TypographySample-CcFCiP06.js";const u={title:"Components/Docs/Design Tokens Table"},e={render:()=>o.jsx(s,{tokens:{name:{$value:"value"}}}),tags:["!dev","!autodocs"]};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  render: () => <DesignTokensTable tokens={{
+    name: {
+      $value: 'value'
+    }
+  }} />,
+  tags: ['!dev', '!autodocs']
+}`,...e.parameters?.docs?.source}}};const l=["Test"];export{e as Test,l as __namedExportsOrder,u as default};
---
+++ demo-DES-1694-unique-landmarks-in-header/assets/InvalidFormAlert.test.stories-B8K8XZkA.js
@@ -0,0 +1,6 @@
+import{r as t}from"./renderComponentVariants-D-tcItnx.js";import{m as e,I as a}from"./InvalidFormAlert.stories-CVdLN_ZK.js";import"./iframe-DFYCrGlq.js";import"./preload-helper-PPVm8Dsz.js";import"./Alert-gfi9YCaT.js";import"./Icon-BuZSgsPJ.js";import"./Row-BoAO7d11.js";import"./Heading-DgMbRiwl.js";import"./getHeadingTag-Dv1eMqvl.js";import"./IconButton-qkzauedh.js";import"./LinkList-DSMm1Yrk.js";const F={...e,title:"Components/Forms/Invalid Form Alert"},r={render:o=>t(a,{args:o}),tags:["!dev","!autodocs"]};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(InvalidFormAlert, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...r.parameters?.docs?.source}}};const I=["Test"];export{r as Test,I as __namedExportsOrder,F as default};
