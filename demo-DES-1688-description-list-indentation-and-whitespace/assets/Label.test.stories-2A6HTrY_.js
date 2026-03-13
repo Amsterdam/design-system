e922171e2fb71c1607afd808c4fe9bf40630cdc7
GitHub <github-action@users.noreply.github.com>
2026-03-13 10:56:04 +0000

    Remove feature branch folder

--- demo-DES-1688-description-list-indentation-and-whitespace/assets/Label.test.stories-2A6HTrY_.js
+++
@@ -1,6 +0,0 @@
-import{r as a}from"./renderComponentVariants-DFgPtPjx.js";import{m as o,L as t}from"./Label.stories-Bn45e71Z.js";import"./iframe-RnYfnkDQ.js";import"./preload-helper-PPVm8Dsz.js";import"./Hint-Cu94OjOQ.js";const c={...o,title:"Components/Forms/Label"},r={render:e=>a(t,{args:e}),tags:["!dev","!autodocs"]};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(Label, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...r.parameters?.docs?.source}}};const i=["Test"];export{r as Test,i as __namedExportsOrder,c as default};
