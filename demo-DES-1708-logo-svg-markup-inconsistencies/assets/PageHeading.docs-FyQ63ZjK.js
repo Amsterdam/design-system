9bb9cd35d8975de6c35d66abcfd313718fe0ba6a
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-03-17 22:48:01 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@f09fc248a6f8efcd8c0570defc95fb7ae1ba7b3e 🚀

---
+++ demo-DES-1708-logo-svg-markup-inconsistencies/assets/PageHeading.docs-FyQ63ZjK.js
@@ -0,0 +1,9 @@
+import{j as e}from"./iframe-lDDCKbVm.js";import{useMDXComponents as r}from"./index-DwfaEEzW.js";import{M as s,e as i,f as a}from"./blocks-BwItGWBX.js";import{P as m}from"./PageHeading.stories-WqODAAz0.js";import{S as p}from"./StatusBadge-DdResV44.js";import"./preload-helper-PPVm8Dsz.js";import"./index-h88lOs_u.js";import"./Badge-j4b-51Xm.js";import"./Icon-B3HNaz1L.js";const d=`<!-- @license CC0-1.0 -->
+
+# Page Heading
+`;function o(n){return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(p,{description:"We no longer use this size of headings. Use the Heading component with level 1 instead.",status:"deprecated"}),`
+`,e.jsx(s,{of:m}),`
+`,e.jsx(i,{children:d}),`
+`,e.jsx(a,{})]})}function C(n={}){const{wrapper:t}={...r(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{C as default};
