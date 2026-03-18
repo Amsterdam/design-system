1d626529835c80f686c71b211e31af37aec5583b
GitHub <github-action@users.noreply.github.com>
2026-03-18 08:26:28 +0000

    Remove feature branch folder

--- demo-DES-1708-logo-svg-markup-inconsistencies/assets/Alert.test.stories-CpOEYy5w.js
+++
@@ -1,6 +0,0 @@
-import{r as t}from"./renderComponentVariants-CS97ZZ4d.js";import{m as o}from"./Alert.stories-DiP7ON5c.js";import{A as a}from"./Alert-sGMjxG62.js";import"./iframe-lDDCKbVm.js";import"./preload-helper-PPVm8Dsz.js";import"./Icon-B3HNaz1L.js";import"./Row-Bgj0qXn2.js";import"./Heading-wKtZzh7p.js";import"./getHeadingTag-Dv1eMqvl.js";import"./IconButton-MfDRsjfo.js";const f={...o,title:"Components/Feedback/Alert"},r={render:e=>t(a,{args:e}),tags:["!dev","!autodocs"]};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(Alert, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...r.parameters?.docs?.source}}};const A=["Test"];export{r as Test,A as __namedExportsOrder,f as default};
--- demo-DES-1708-logo-svg-markup-inconsistencies/assets/Icon.test.stories-D3hKsTCB.js
+++
@@ -1,6 +0,0 @@
-import{r as e}from"./renderComponentVariants-CS97ZZ4d.js";import{m as t}from"./Icon.stories-CnB4A2q2.js";import{I as a}from"./Icon-B3HNaz1L.js";import"./iframe-lDDCKbVm.js";import"./preload-helper-PPVm8Dsz.js";import"./Paragraph-7mOR1vFt.js";import"./Row-Bgj0qXn2.js";const u={...t,title:"Components/Media/Icon"},r={render:o=>e(a,{args:o}),tags:["!dev","!autodocs"]};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(Icon, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...r.parameters?.docs?.source}}};const g=["Test"];export{r as Test,g as __namedExportsOrder,u as default};
