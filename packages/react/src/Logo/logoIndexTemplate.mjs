58a85a77ee19d69ae640fe75d27f18ab578b52f3
Vincent Smedinga <v.smedinga@amsterdam.nl>
2026-03-18 09:49:46 +0100

    feat(react): Replace generate.mjs with SVGR CLI and config files

---
+++ packages/react/src/Logo/logoIndexTemplate.mjs
@@ -0,0 +1,17 @@
+import path from 'path'
+
+export function logoIndexTemplate(filePaths) {
+  const exports = filePaths.map(({ path: filePath }) => {
+    const name = path.basename(filePath, path.extname(filePath))
+    return `export { default as ${name} } from './${name}'`
+  })
+  return [
+    '/**',
+    ' * @license EUPL-1.2+',
+    ' * Copyright Gemeente Amsterdam',
+    ' */',
+    '',
+    ...exports,
+    '',
+  ].join('\n')
+}
