f7f798ac9e43f59069710cccd930b21e8ad8c782
Vincent Smedinga <v.smedinga@amsterdam.nl>
2026-03-18 08:34:28 +0100

    feat(react): Generate Logo components from SVG assets

---
+++ packages/react/src/Logo/brands/index.ts
@@ -0,0 +1,12 @@
+/**
+ * @license EUPL-1.2+
+ * Copyright Gemeente Amsterdam
+ */
+
+export { default as AmsterdamEnglishLogo } from './AmsterdamEnglishLogo'
+export { default as AmsterdamLogo } from './AmsterdamLogo'
+export { default as GgdAmsterdamLogo } from './GgdAmsterdamLogo'
+export { default as MuseumWeespLogo } from './MuseumWeespLogo'
+export { default as StadsarchiefLogo } from './StadsarchiefLogo'
+export { default as StadsbankVanLeningLogo } from './StadsbankVanLeningLogo'
+export { default as VgaVerzekeringenLogo } from './VgaVerzekeringenLogo'
