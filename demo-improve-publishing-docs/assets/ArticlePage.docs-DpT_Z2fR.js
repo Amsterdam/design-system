db464e7c99a7c9b96ab356ff20c2a1db9c1c5371
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-03-19 22:10:25 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@e3ede7540ee4b656c62c29b8d6ca3896fba2518a 🚀

---
+++ demo-improve-publishing-docs/assets/ArticlePage.docs-DpT_Z2fR.js
@@ -0,0 +1,15 @@
+import{j as e}from"./iframe-apCSuah_.js";import{useMDXComponents as r}from"./index-B8V7GXhC.js";import{M as o}from"./blocks-D39N9vvb.js";import{A as s}from"./ArticlePage.stories-BNdYSH4x.js";import"./preload-helper-PPVm8Dsz.js";import"./index-Bhst0DyW.js";import"./formatDate-HeD_AkCg.js";import"./config-CGKdRpA1.js";import"./Layout-WPBj72g3.js";import"./PageFooter.stories-BClP4rqy.js";import"./getHeadingTag-Dv1eMqvl.js";import"./data-Bx-tH58n.js";function t(i){const n={h1:"h1",h2:"h2",li:"li",p:"p",ul:"ul",...r(),...i.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(o,{of:s}),`
+`,e.jsx(n.h1,{id:"article-page",children:"Article page"}),`
+`,e.jsx(n.p,{children:"This page type is for news or similar kinds of articles."}),`
+`,e.jsx(n.h2,{id:"headings",children:"Headings"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Use a level 1 Heading for the title of the article."}),`
+`,e.jsx(n.li,{children:"Use level 2 Headings for the section titles."}),`
+`]}),`
+`,e.jsx(n.h2,{id:"spacing",children:"Spacing"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Add a small margin bottom to a Heading 2 with following body text."}),`
+`,e.jsx(n.li,{children:"Use an extra large margin between sections."}),`
+`]})]})}function M(i={}){const{wrapper:n}={...r(),...i.components};return n?e.jsx(n,{...i,children:e.jsx(t,{...i})}):t(i)}export{M as default};
---
+++ demo-improve-publishing-docs/assets/FieldSet.test.stories-D3VxX6LG.js
@@ -0,0 +1,14 @@
+import{j as r}from"./iframe-apCSuah_.js";import{r as t}from"./renderComponentVariants-DRxfrI-O.js";import{m as s,F as o}from"./FieldSet.stories-DV3CVA-K.js";import"./preload-helper-PPVm8Dsz.js";import"./exampleContent-D2tKGi5u.js";import"./Hint-Br6ZCPKK.js";const c={...s,title:"Components/Forms/Field Set"},e={args:{children:r.jsxs("div",{children:[r.jsx("label",{htmlFor:"input-a1",children:"Voornaam"}),r.jsx("input",{id:"input-a1",value:"Yassine"})]})},render:a=>t(o,{args:a}),tags:["!dev","!autodocs"]};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    children:
+    // We add a label to prevent Chromatic from raising an accessibility error.
+    <div>
+        <label htmlFor="input-a1">Voornaam</label>
+        <input id="input-a1" value="Yassine" />
+      </div>
+  },
+  render: args => renderComponentVariants(FieldSet, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...e.parameters?.docs?.source}}};const u=["Test"];export{e as Test,u as __namedExportsOrder,c as default};
