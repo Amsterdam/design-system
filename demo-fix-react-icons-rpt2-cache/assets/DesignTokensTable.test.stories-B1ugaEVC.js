1e1768752d7de4161ae7ddf7f09df658cf11cf0f
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-03-19 22:16:20 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@b33aea139ddf44ac7c51ce05d807131d5d36c738 🚀

---
+++ demo-fix-react-icons-rpt2-cache/assets/DesignTokensTable.test.stories-B1ugaEVC.js
@@ -0,0 +1,8 @@
+import{j as o}from"./iframe-BFra502A.js";import{D as s}from"./DesignTokensTable-DD9Xw53K.js";import"./preload-helper-PPVm8Dsz.js";import"./BorderSample-D7Js_FCQ.js";import"./formatTokenValue-BYjgZrXg.js";import"./Code-BQPaRYpI.js";import"./ColorSample-D6ejHRGn.js";import"./SpaceSample-B8geO-Hf.js";import"./TypographySample-JYeadQQJ.js";const u={title:"Components/Docs/Design Tokens Table"},e={render:()=>o.jsx(s,{tokens:{name:{$value:"value"}}}),tags:["!dev","!autodocs"]};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  render: () => <DesignTokensTable tokens={{
+    name: {
+      $value: 'value'
+    }
+  }} />,
+  tags: ['!dev', '!autodocs']
+}`,...e.parameters?.docs?.source}}};const l=["Test"];export{e as Test,l as __namedExportsOrder,u as default};
---
+++ demo-fix-react-icons-rpt2-cache/assets/PageHeading.docs-CqLh_woi.js
@@ -0,0 +1,9 @@
+import{j as e}from"./iframe-BFra502A.js";import{useMDXComponents as r}from"./index-BaKGty-n.js";import{M as s,e as i,f as a}from"./blocks-DlczG8y-.js";import{P as m}from"./PageHeading.stories-FkXrnGZ1.js";import{S as p}from"./StatusBadge-CQ_TxoYB.js";import"./preload-helper-PPVm8Dsz.js";import"./index-CfVqYYH4.js";import"./Badge-CE5b2N-Z.js";import"./Icon-DqoFV_fo.js";const d=`<!-- @license CC0-1.0 -->
+
+# Page Heading
+`;function o(n){return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(p,{description:"We no longer use this size of headings. Use the Heading component with level 1 instead.",status:"deprecated"}),`
+`,e.jsx(s,{of:m}),`
+`,e.jsx(i,{children:d}),`
+`,e.jsx(a,{})]})}function C(n={}){const{wrapper:t}={...r(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{C as default};
