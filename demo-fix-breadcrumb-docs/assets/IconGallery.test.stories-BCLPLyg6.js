80e198b90a895cbafab6698fc08893cac52e4d4d
GitHub <github-action@users.noreply.github.com>
2026-03-20 15:19:52 +0000

    Remove feature branch folder

--- demo-fix-breadcrumb-docs/assets/IconGallery.test.stories-BCLPLyg6.js
+++
@@ -1,4 +0,0 @@
-import{j as r}from"./iframe-BKVuPMFs.js";import{I as e}from"./IconGallery-Dt9IVQBk.js";import"./preload-helper-PPVm8Dsz.js";import"./PieChartFill-k2eM1Pf0.js";const a={title:"Components/Docs/Icon Gallery"},o={render:()=>r.jsx(e,{icons:["CloseIcon","SearchIcon","ArrowDownIcon","ArrowUpIcon","ErrorFillIcon","ErrorIcon"]}),tags:["!dev","!autodocs"]};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
-  render: () => <IconGallery icons={['CloseIcon', 'SearchIcon', 'ArrowDownIcon', 'ArrowUpIcon', 'ErrorFillIcon', 'ErrorIcon']} />,
-  tags: ['!dev', '!autodocs']
-}`,...o.parameters?.docs?.source}}};const I=["Test"];export{o as Test,I as __namedExportsOrder,a as default};
