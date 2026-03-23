be2b7c259992fc41beebd41788c85ed7ff6a163e
GitHub <github-action@users.noreply.github.com>
2026-03-23 11:18:56 +0000

    Remove feature branch folder

--- demo-DES-1710-add-logo-inspection/assets/PageHeading.docs-CqYMbcsb.js
+++
@@ -1,9 +0,0 @@
-import{j as e}from"./iframe-BIsp1dfx.js";import{useMDXComponents as r}from"./index-Vu6prpLT.js";import{M as s,e as i,f as a}from"./blocks-ChJG_Hid.js";import{P as m}from"./PageHeading.stories-C_F7Vmfm.js";import{S as p}from"./StatusBadge-BasRbydS.js";import"./preload-helper-PPVm8Dsz.js";import"./index-DwQJVCjq.js";import"./Badge-Boy4tMD2.js";import"./Icon-C21-mcrw.js";const d=`<!-- @license CC0-1.0 -->
-
-# Page Heading
-`;function o(n){return e.jsxs(e.Fragment,{children:[`
-`,`
-`,e.jsx(p,{description:"We no longer use this size of headings. Use the Heading component with level 1 instead.",status:"deprecated"}),`
-`,e.jsx(s,{of:m}),`
-`,e.jsx(i,{children:d}),`
-`,e.jsx(a,{})]})}function C(n={}){const{wrapper:t}={...r(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{C as default};
