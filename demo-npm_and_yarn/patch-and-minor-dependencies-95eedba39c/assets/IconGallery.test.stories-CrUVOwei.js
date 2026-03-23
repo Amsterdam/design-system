5bce64e62ceedcf9cb7f9da53e07342e60d01af3
GitHub <github-action@users.noreply.github.com>
2026-03-23 12:38:01 +0000

    Remove feature branch folder

--- demo-npm_and_yarn/patch-and-minor-dependencies-95eedba39c/assets/IconGallery.test.stories-CrUVOwei.js
+++
@@ -1,4 +0,0 @@
-import{j as r}from"./iframe-CAugK9N0.js";import{I as e}from"./IconGallery-B-fkjx2e.js";import"./preload-helper-PPVm8Dsz.js";import"./PieChartFill-_KVNQdY8.js";const a={title:"Components/Docs/Icon Gallery"},o={render:()=>r.jsx(e,{icons:["CloseIcon","SearchIcon","ArrowDownIcon","ArrowUpIcon","ErrorFillIcon","ErrorIcon"]}),tags:["!dev","!autodocs"]};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
-  render: () => <IconGallery icons={['CloseIcon', 'SearchIcon', 'ArrowDownIcon', 'ArrowUpIcon', 'ErrorFillIcon', 'ErrorIcon']} />,
-  tags: ['!dev', '!autodocs']
-}`,...o.parameters?.docs?.source}}};const I=["Test"];export{o as Test,I as __namedExportsOrder,a as default};
