d9467931948e088c7c180a8d456009bc092e82e0
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-03-23 15:50:17 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@15cd06e0fc79f62a204975e84c0b15f6073ff544 🚀

---
+++ demo-npm_and_yarn/patch-and-minor-dev-dependencies-04a034fd9d/assets/Grid.test.stories-CPQNP15k.js
@@ -0,0 +1,9 @@
+import{j as a}from"./iframe-BIsp1dfx.js";import{r as o}from"./renderComponentVariants-DkveLdXE.js";import{m as t}from"./Grid.stories-D0u08K4X.js";import{G as e}from"./Grid-C_Vm-3TD.js";import"./preload-helper-PPVm8Dsz.js";import"./GridColumnsGuide-Cmvbyj5K.js";import"./Grid.argTypes-CfqFMx79.js";import"./gridCellClasses-DKw8XbSK.js";const g={...t,title:"Components/Layout/Grid"},r={args:{children:[a.jsx(e.Cell,{span:"all"},1),a.jsx(e.Cell,{span:"all"},2)]},render:s=>o(e,{args:s}),tags:["!dev","!autodocs"]};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<Grid.Cell key={1} span="all" />, <Grid.Cell key={2} span="all" />]
+  },
+  render: args => renderComponentVariants(Grid, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...r.parameters?.docs?.source}}};const C=["Test"];export{r as Test,C as __namedExportsOrder,g as default};
---
+++ demo-npm_and_yarn/patch-and-minor-dev-dependencies-04a034fd9d/assets/InvalidFormAlert.test.stories-BNGPLF-M.js
@@ -0,0 +1,6 @@
+import{r as t}from"./renderComponentVariants-DkveLdXE.js";import{m as e,I as a}from"./InvalidFormAlert.stories-CkdPBMhs.js";import"./iframe-BIsp1dfx.js";import"./preload-helper-PPVm8Dsz.js";import"./Alert-Bc-Z8jka.js";import"./Icon-C21-mcrw.js";import"./Row-mzxUbfsw.js";import"./Heading-DpfgdLFj.js";import"./getHeadingTag-Dv1eMqvl.js";import"./IconButton-D4XwT9Rx.js";import"./LinkList-mpACAkGk.js";const F={...e,title:"Components/Forms/Invalid Form Alert"},r={render:o=>t(a,{args:o}),tags:["!dev","!autodocs"]};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(InvalidFormAlert, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...r.parameters?.docs?.source}}};const I=["Test"];export{r as Test,I as __namedExportsOrder,F as default};
