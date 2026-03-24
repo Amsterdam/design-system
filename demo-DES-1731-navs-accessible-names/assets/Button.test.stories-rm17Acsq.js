cf8ebd282be423b43a59d7df0fa5cd2ca3cde959
GitHub <github-action@users.noreply.github.com>
2026-03-24 14:44:22 +0000

    Remove feature branch folder

--- demo-DES-1731-navs-accessible-names/assets/Button.test.stories-rm17Acsq.js
+++
@@ -1,7 +0,0 @@
-import{r as e}from"./renderComponentVariants-CiUJrcQv.js";import{m as o}from"./Button.stories-C2Z-uX1B.js";import{B as a}from"./Button-7PNjvAjO.js";import"./iframe-BHvEQ1YI.js";import"./preload-helper-PPVm8Dsz.js";import"./Icon-ByBE4jnz.js";const c={...o,title:"Components/Buttons/Button"},r={render:t=>e(a,{args:t,variants:["disabled","hovered"]}),tags:["!dev","!autodocs"]};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(Button, {
-    args,
-    variants: ['disabled', 'hovered']
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...r.parameters?.docs?.source}}};const u=["Test"];export{r as Test,u as __namedExportsOrder,c as default};
--- demo-DES-1731-navs-accessible-names/assets/Icon.test.stories-Da36zs8q.js
+++
@@ -1,6 +0,0 @@
-import{r as e}from"./renderComponentVariants-CiUJrcQv.js";import{m as t}from"./Icon.stories-CLzQ-CtL.js";import{I as a}from"./Icon-ByBE4jnz.js";import"./iframe-BHvEQ1YI.js";import"./preload-helper-PPVm8Dsz.js";import"./Paragraph-CWAAU2Xw.js";import"./Row-CiOMW5gA.js";const u={...t,title:"Components/Media/Icon"},r={render:o=>e(a,{args:o}),tags:["!dev","!autodocs"]};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(Icon, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...r.parameters?.docs?.source}}};const g=["Test"];export{r as Test,g as __namedExportsOrder,u as default};
