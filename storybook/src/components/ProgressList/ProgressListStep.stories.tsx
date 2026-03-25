e49933cf8176439d82c901f843bec246ce3dffce
Vincent Smedinga <v.smedinga@amsterdam.nl>
2026-03-25 16:23:15 +0100

    Add example and controls for Step subcomponent

---
+++ storybook/src/components/ProgressList/ProgressListStep.stories.tsx
@@ -0,0 +1,45 @@
+/**
+ * @license EUPL-1.2+
+ * Copyright Gemeente Amsterdam
+ */
+
+import type { Meta, StoryObj } from '@storybook/react-vite'
+
+import { Paragraph } from '@amsterdam/design-system-react'
+import { ProgressList } from '@amsterdam/design-system-react/src'
+
+import { exampleParagraph } from '../../_common/exampleContent'
+
+const meta = {
+  title: 'Components/Containers/Progress List',
+  component: ProgressList.Step,
+  args: {
+    children: <Paragraph>{exampleParagraph()}</Paragraph>,
+    defaultCollapsed: true,
+    heading: 'Aanpassing ontwerp fietspad Entreegebied',
+    status: 'current',
+  },
+  argTypes: {
+    status: {
+      control: {
+        labels: { undefined: 'default' },
+        type: 'radio',
+      },
+      options: [undefined, 'current', 'completed'],
+    },
+  },
+  decorators: [
+    (Story) => (
+      <ProgressList headingLevel={3}>
+        <Story />
+      </ProgressList>
+    ),
+  ],
+  render: ({ children, ...args }) => <ProgressList.Step {...args}>{children}</ProgressList.Step>,
+} satisfies Meta<typeof ProgressList.Step>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Step: Story = {}
