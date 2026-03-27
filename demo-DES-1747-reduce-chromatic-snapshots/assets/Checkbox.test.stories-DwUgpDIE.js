bf2285af27d3458ad025f41bc7a566e7e4487482
Evi-2003 <69012052+Evi-2003@users.noreply.github.com>
2026-03-27 14:42:04 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@48458ad68594107fd69ae336fcd6397538447ebd 🚀

---
+++ demo-DES-1747-reduce-chromatic-snapshots/assets/Checkbox.test.stories-DwUgpDIE.js
@@ -0,0 +1,6 @@
+import{n as e}from"./chunk-BneVvdWh.js";import{n as t,t as n}from"./renderComponentVariants-CTtnL2R2.js";import{t as r,tt as i}from"./src-B46CSdeH.js";import a,{n as o}from"./Checkbox.stories-BjskeCvv.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/Forms/Checkbox`},c={render:e=>t(i,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(Checkbox, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
