80cfd61af2408962a23426e3f27577f8f8d6a97b
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-03-27 14:40:30 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@a71d1b5ded8eda7dcef116fa65e55017754aabd2 🚀

---
+++ demo-develop/assets/IconButton.test.stories-Bh_yPYuS.js
@@ -0,0 +1,7 @@
+import{n as e}from"./chunk-BneVvdWh.js";import{n as t,t as n}from"./renderComponentVariants-CTtnL2R2.js";import{St as r,t as i}from"./src-D9hPSS_O.js";import a,{n as o}from"./IconButton.stories-DtG6C_qG.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Buttons/Icon Button`},c={render:e=>t(r,{args:e,variants:[`hovered`,`disabled`]}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(IconButton, {
+    args,
+    variants: ['hovered', 'disabled']
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
---
+++ demo-develop/assets/Page.docs-DNgkpXH-.js
@@ -0,0 +1,28 @@
+import{n as e}from"./chunk-BneVvdWh.js";import{t}from"./jsx-runtime-BrayXyei.js";import{r as n}from"./react-oCAR5XSR.js";import{c as r,f as i,i as a,l as o,n as s,s as c}from"./blocks-DcvYv5nB.js";import{n as l,t as u}from"./DesignTokensTable-CJgajNVN.js";import{t as d}from"./mdx-react-shim-9Tyt82po.js";import{WithMenu as f,n as p,t as m}from"./Page.stories-D5II6qx8.js";var h,g=e((()=>{h=`<!-- @license CC0-1.0 -->
+
+# Page
+
+Contains the entire website.
+
+The Page component wraps the [Page Header](https://designsystem.amsterdam/?path=/docs/components-containers-page-header--docs),
+[Page Footer](https://designsystem.amsterdam/?path=/docs/components-containers-page-footer--docs), and the main content in between.
+As a root layout component, it must be used for all websites for the City of Amsterdam.
+It is centered horizontally and sets a maximum width of 90 rems (usually 1.440 pixels).
+With a Menu, that becomes 120 rems (1.920 pixels).
+`})),_,v,y=e((()=>{_={page:{"background-color":{$value:`{ams.color.background}`,$type:`color`},"max-inline-size":{$value:{value:90,unit:`rem`},$type:`dimension`},"with-menu":{"max-inline-size":{$value:{value:120,unit:`rem`},$type:`dimension`},"area-skip-link":{"margin-inline-end":{$value:`{ams.grid.padding-inline}`,$type:`dimension`,$extensions:{"nl.amsterdam.hint":`Must be the Grid inline padding, to make sure Skip Link and Grid line up.`,"nl.amsterdam.subtype":`space`}}}}}},v={ams:_}}));function b(e){let t={a:`a`,h2:`h2`,h3:`h3`,p:`p`,...n(),...e.components};return(0,S.jsxs)(S.Fragment,{children:[`
+`,`
+`,`
+`,`
+`,(0,S.jsx)(r,{of:m}),`
+`,(0,S.jsx)(c,{children:h}),`
+`,(0,S.jsxs)(t.p,{children:[`See the `,(0,S.jsx)(t.a,{href:`/story/pages-public-home-page--default`,children:`Home Page`}),` template for public websites for a full-page example.`]}),`
+`,(0,S.jsx)(o,{}),`
+`,(0,S.jsx)(a,{}),`
+`,(0,S.jsx)(t.h2,{id:`examples`,children:`Examples`}),`
+`,(0,S.jsx)(t.h3,{id:`with-menu`,children:`With menu`}),`
+`,(0,S.jsxs)(t.p,{children:[`Internal websites can add a `,(0,S.jsx)(t.a,{href:`/docs/components-navigation-menu--docs`,children:`Menu`}),` for navigating between its modules and pages.
+This is an alternative to the approach using Link Lists, which is appropriate for public, content-oriented websites.`]}),`
+`,(0,S.jsx)(s,{of:f}),`
+`,(0,S.jsxs)(t.p,{children:[`See the `,(0,S.jsx)(t.a,{href:`/story/pages-internal-home-page--default`,children:`Home Page`}),` template for internal websites for a full-page example.`]}),`
+`,(0,S.jsx)(t.h2,{id:`design-tokens`,children:`Design tokens`}),`
+`,(0,S.jsx)(u,{tokens:v})]})}function x(e={}){let{wrapper:t}={...n(),...e.components};return t?(0,S.jsx)(t,{...e,children:(0,S.jsx)(b,{...e})}):b(e)}var S;e((()=>{S=t(),d(),i(),p(),g(),y(),l()}))();export{x as default};
