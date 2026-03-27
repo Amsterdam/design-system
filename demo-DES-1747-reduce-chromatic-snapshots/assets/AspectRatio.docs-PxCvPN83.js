40ec4ca899e40d59782d2577a59d166e0c8bcdc2
Evi-2003 <69012052+Evi-2003@users.noreply.github.com>
2026-03-27 14:44:49 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@6e60a76efbb7965c9945fb29d9156faa206b05c4 🚀

---
+++ demo-DES-1747-reduce-chromatic-snapshots/assets/AspectRatio.docs-PxCvPN83.js
@@ -0,0 +1,33 @@
+import{n as e}from"./chunk-BneVvdWh.js";import{t}from"./jsx-runtime-BrayXyei.js";import{r as n}from"./react-oCAR5XSR.js";import{c as r,f as i,i as a,l as o,s}from"./blocks-e3Ps4d1u.js";import{t as c}from"./mdx-react-shim-9Tyt82po.js";import{n as l,t as u}from"./AspectRatio.stories-Bu3R0tpU.js";var d,f=e((()=>{d=`<!-- @license CC0-1.0 -->
+
+import { SpaceSample } from "../../docs/components/SpaceSample";
+
+# Aspect Ratio
+
+Constrains media content to a supported aspect ratio.
+
+## Class names
+
+Each available aspect ratio has an associated class name.
+The class can be applied to any component or element.
+
+| Class name              | Example                                           |
+| :---------------------- | :------------------------------------------------ |
+| \`ams-aspect-ratio-9-16\` | <SpaceSample className="ams-aspect-ratio-9-16" /> |
+| \`ams-aspect-ratio-3-4\`  | <SpaceSample className="ams-aspect-ratio-3-4" />  |
+| \`ams-aspect-ratio-1-1\`  | <SpaceSample className="ams-aspect-ratio-1-1" />  |
+| \`ams-aspect-ratio-4-3\`  | <SpaceSample className="ams-aspect-ratio-4-3" />  |
+| \`ams-aspect-ratio-16-9\` | <SpaceSample className="ams-aspect-ratio-16-9" /> |
+| \`ams-aspect-ratio-16-5\` | <SpaceSample className="ams-aspect-ratio-16-5" /> |
+
+## Guidelines
+
+- Apply one of these classes to an image, video or other media content to constrain its dimensions to one of the available aspect ratios.
+- The default aspect ratio is 16:9.
+- When using \`width\` and \`height\` attributes on an image (for example, when using the [Next Image component](https://nextjs.org/docs/app/api-reference/components/image)), the aspect ratio is determined by these values and will take precedence over the class. To correct this, use the class \`ams-image\`.
+`}));function p(e){return(0,h.jsxs)(h.Fragment,{children:[`
+`,`
+`,(0,h.jsx)(r,{of:u}),`
+`,(0,h.jsx)(s,{children:d}),`
+`,(0,h.jsx)(o,{}),`
+`,(0,h.jsx)(a,{})]})}function m(e={}){let{wrapper:t}={...n(),...e.components};return t?(0,h.jsx)(t,{...e,children:(0,h.jsx)(p,{...e})}):p(e)}var h;e((()=>{h=t(),c(),i(),l(),f()}))();export{m as default};
--- demo-DES-1747-reduce-chromatic-snapshots/assets/ErrorMessage.test.stories-C2Rhrmny.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-BneVvdWh.js";import{n as t,t as n}from"./renderComponentVariants-CTtnL2R2.js";import{J as r,t as i}from"./src-B46CSdeH.js";import a,{n as o}from"./ErrorMessage.stories-CJyLguDD.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Error Message`},c={render:e=>t(r,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(ErrorMessage, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
