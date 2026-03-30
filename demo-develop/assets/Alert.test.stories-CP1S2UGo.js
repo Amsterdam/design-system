402971bdbdf87b0e19402a5323a30ca6d9dd1bcd
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-03-30 14:32:01 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@7ae919dd26631be067112f47b9b8ae3a6b26dd3c 🚀

--- demo-develop/assets/Alert.test.stories-CP1S2UGo.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-BneVvdWh.js";import{n as t,t as n}from"./renderComponentVariants-CTtnL2R2.js";import{t as r,vt as i}from"./src-Ctn-lx_K.js";import a,{n as o}from"./Alert.stories-4P7K2q94.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/Feedback/Alert`},c={render:e=>t(i,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(Alert, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
--- demo-develop/assets/FileInput.test.stories-pe2lhexS.js
+++
@@ -1,7 +0,0 @@
-import{n as e}from"./chunk-BneVvdWh.js";import{n as t,t as n}from"./renderComponentVariants-CTtnL2R2.js";import{W as r,t as i}from"./src-Ctn-lx_K.js";import a,{n as o}from"./FileInput.stories-Ds0E2Pyf.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/File Input`},c={render:e=>t(r,{args:e,variants:[`disabled`,`multiple`]}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(FileInput, {
-    args,
-    variants: ['disabled', 'multiple']
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
