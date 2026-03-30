287a77ced6e1291eb1d5d63fb76d17a1e2d88294
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-03-30 14:25:33 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@9eb952a87503dc681b388c965cdc119456262438 🚀

--- demo-develop/assets/Pagination.test.stories-ixxN5j3J.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-BneVvdWh.js";import{n as t,t as n}from"./renderComponentVariants-CTtnL2R2.js";import{b as r,t as i}from"./src-CZgQ7Co2.js";import a,{n as o}from"./Pagination.stories-j0FwNR2Z.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Navigation/Pagination`},c={render:e=>t(r,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(Pagination, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
