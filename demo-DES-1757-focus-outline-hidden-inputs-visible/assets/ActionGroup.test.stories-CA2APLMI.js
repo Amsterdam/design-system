f48a4d6cc3d86d81b80227e8e928f63c242510f3
Evi-2003 <69012052+Evi-2003@users.noreply.github.com>
2026-03-31 13:16:26 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@31aa12e4f7d08b89815c200afef54e47ce1d0668 🚀

---
+++ demo-DES-1757-focus-outline-hidden-inputs-visible/assets/ActionGroup.test.stories-CA2APLMI.js
@@ -0,0 +1,9 @@
+import{n as e}from"./chunk-BneVvdWh.js";import{t}from"./jsx-runtime-BrayXyei.js";import{n,t as r}from"./renderComponentVariants-CTtnL2R2.js";import{Ct as i,t as a}from"./src-Du3u7qEE.js";import o,{n as s}from"./ActionGroup.stories-aLmGBLec.js";var c,l,u,d;e((()=>{a(),r(),s(),c=t(),l={...o,title:`Components/Layout/Action Group`},u={args:{children:[(0,c.jsx)(`button`,{children:`Stoppen`},1),(0,c.jsx)(`button`,{children:`Verder`},2)]},render:e=>n(i,{args:e}),tags:[`!dev`,`!autodocs`]},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<button key={1}>Stoppen</button>, <button key={2}>Verder</button>]
+  },
+  render: args => renderComponentVariants(ActionGroup, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...u.parameters?.docs?.source}}},d=[`Test`]}))();export{u as Test,d as __namedExportsOrder,l as default};
