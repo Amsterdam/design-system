7bd4848a41694d6b6543a1076d59f9898167253c
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-03-31 07:58:48 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@891ba2ce4f5da5880585c56e78b1fdaae82c8488 🚀

---
+++ demo-DES-1613-collapse-progress-list-step/assets/modes.docs-DGCLL1KP.js
@@ -0,0 +1,20 @@
+import{n as e}from"./chunk-BneVvdWh.js";import{t}from"./jsx-runtime-BrayXyei.js";import{r as n}from"./react-oCAR5XSR.js";import{c as r,f as i}from"./blocks-CYwaI6xo.js";import{t as a}from"./mdx-react-shim-9Tyt82po.js";function o(e){let t={h1:`h1`,h2:`h2`,li:`li`,ol:`ol`,p:`p`,strong:`strong`,table:`table`,tbody:`tbody`,td:`td`,th:`th`,thead:`thead`,tr:`tr`,...n(),...e.components};return(0,c.jsxs)(c.Fragment,{children:[`
+`,`
+`,(0,c.jsx)(r,{title:`Docs/Developer Guide/Modes`}),`
+`,(0,c.jsx)(t.h1,{id:`modes`,children:`Modes`}),`
+`,(0,c.jsx)(t.p,{children:`The design system offers several modes to meet different needs.`}),`
+`,(0,c.jsx)(t.h2,{id:`density`,children:`Density`}),`
+`,(0,c.jsx)(t.p,{children:`The design system supports various kinds of websites.
+The optimal layout can differ significantly between them.`}),`
+`,(0,c.jsx)(t.p,{children:`Our visual style is quite spacious by default.
+This works fine for content-oriented websites, but not so much for websites with a high information density.
+We distinguish two levels of spaciousness to cover both needs.`}),`
+`,(0,c.jsxs)(t.ol,{children:[`
+`,(0,c.jsxs)(t.li,{children:[(0,c.jsx)(t.strong,{children:`Spacious Mode`}),` for public websites.
+Large type and generous spacing make reading comfortable.`]}),`
+`,(0,c.jsxs)(t.li,{children:[(0,c.jsx)(t.strong,{children:`Compact Mode`}),` for internal websites.
+Smaller text and tighter spacing allow showing more data.`]}),`
+`]}),`
+`,(0,c.jsx)(t.p,{children:`Key characteristics of each mode:`}),`
+`,(0,c.jsxs)(t.table,{children:[(0,c.jsx)(t.thead,{children:(0,c.jsxs)(t.tr,{children:[(0,c.jsx)(t.th,{style:{textAlign:`left`}}),(0,c.jsx)(t.th,{style:{textAlign:`center`},children:`Spacious Mode`}),(0,c.jsx)(t.th,{style:{textAlign:`center`},children:`Compact Mode`})]})}),(0,c.jsxs)(t.tbody,{children:[(0,c.jsxs)(t.tr,{children:[(0,c.jsx)(t.td,{style:{textAlign:`left`},children:(0,c.jsx)(t.strong,{children:`Audience`})}),(0,c.jsx)(t.td,{style:{textAlign:`center`},children:`Public`}),(0,c.jsx)(t.td,{style:{textAlign:`center`},children:`Internal`})]}),(0,c.jsxs)(t.tr,{children:[(0,c.jsx)(t.td,{style:{textAlign:`left`},children:(0,c.jsx)(t.strong,{children:`Size of body text`})}),(0,c.jsx)(t.td,{style:{textAlign:`center`},children:`20`}),(0,c.jsx)(t.td,{style:{textAlign:`center`},children:`16`})]}),(0,c.jsxs)(t.tr,{children:[(0,c.jsx)(t.td,{style:{textAlign:`left`},children:(0,c.jsx)(t.strong,{children:`Largest heading size`})}),(0,c.jsx)(t.td,{style:{textAlign:`center`},children:`60`}),(0,c.jsx)(t.td,{style:{textAlign:`center`},children:`28`})]}),(0,c.jsxs)(t.tr,{children:[(0,c.jsx)(t.td,{style:{textAlign:`left`},children:(0,c.jsx)(t.strong,{children:`Medium spacing length`})}),(0,c.jsx)(t.td,{style:{textAlign:`center`},children:`24`}),(0,c.jsx)(t.td,{style:{textAlign:`center`},children:`16`})]})]})]}),`
+`,(0,c.jsx)(t.p,{children:`(Values in pixels and for desktop.)`})]})}function s(e={}){let{wrapper:t}={...n(),...e.components};return t?(0,c.jsx)(t,{...e,children:(0,c.jsx)(o,{...e})}):o(e)}var c;e((()=>{c=t(),a(),i()}))();export{s as default};
