5547158f97404d572dd43c69568fb0653b1e3dd8
Evi-2003 <69012052+Evi-2003@users.noreply.github.com>
2026-03-31 13:26:46 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@f5f8cc9471fb9afd750b76b31da860149ee70bad 🚀

---
+++ demo-DES-1757-focus-outline-hidden-inputs-visible/assets/Label.test.stories-B_zMnyVC.js
@@ -0,0 +1,6 @@
+import{n as e}from"./chunk-BneVvdWh.js";import{n as t,t as n}from"./renderComponentVariants-CTtnL2R2.js";import{F as r,t as i}from"./src-FSCapU7X.js";import a,{n as o}from"./Label.stories-CQMW1Ee0.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Label`},c={render:e=>t(r,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(Label, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
--- demo-DES-1757-focus-outline-hidden-inputs-visible/assets/Link.test.stories-I3q5S0e8.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-BneVvdWh.js";import{n as t,t as n}from"./renderComponentVariants-CTtnL2R2.js";import{P as r,t as i}from"./src-Du3u7qEE.js";import a,{n as o}from"./Link.stories-SMKSM5-f.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Navigation/Link`},c={render:e=>t(r,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(Link, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
