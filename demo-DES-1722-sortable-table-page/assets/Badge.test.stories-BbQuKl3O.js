21b4933943d562e78caf8f4b216bcb21f8e13420
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-04-01 12:02:27 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@649e59cc8be17e8b54f0dd20dcecb0cf626d97ba 🚀

--- demo-DES-1722-sortable-table-page/assets/Badge.test.stories-BbQuKl3O.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-BneVvdWh.js";import{n as t,t as n}from"./renderComponentVariants-CTtnL2R2.js";import{ft as r,t as i}from"./src-GAMnU78P.js";import a,{n as o}from"./Badge.stories-DuHmh4Fk.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Feedback/Badge`},c={render:e=>t(r,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(Badge, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
--- demo-DES-1722-sortable-table-page/assets/Row.test.stories-BruHx9jo.js
+++
@@ -1,9 +0,0 @@
-import{n as e}from"./chunk-BneVvdWh.js";import{t}from"./jsx-runtime-BrayXyei.js";import{n,t as r}from"./renderComponentVariants-CTtnL2R2.js";import{t as i,yt as a}from"./src-GAMnU78P.js";import o,{n as s}from"./Row.stories-Cpx-jjzh.js";var c,l,u,d;e((()=>{i(),r(),s(),c=t(),l={...o,title:`Components/Layout/Row`},u={args:{children:[(0,c.jsx)(`div`,{className:`_ams-item`},1),(0,c.jsx)(`div`,{className:`_ams-item`},2),(0,c.jsx)(`div`,{className:`_ams-item`},3)]},render:e=>n(a,{args:e}),tags:[`!dev`,`!autodocs`]},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: [<div className="_ams-item" key={1} />, <div className="_ams-item" key={2} />, <div className="_ams-item" key={3} />]
-  },
-  render: args => renderComponentVariants(Row, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...u.parameters?.docs?.source}}},d=[`Test`]}))();export{u as Test,d as __namedExportsOrder,l as default};
