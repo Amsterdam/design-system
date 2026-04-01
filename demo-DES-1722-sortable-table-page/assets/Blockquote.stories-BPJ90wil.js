ba6225f8533b8a5953d83239858c01f05baf8695
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-04-01 12:05:26 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@13b80057c7826980d8fa023b99a2602d9182b62d 🚀

--- demo-DES-1722-sortable-table-page/assets/Blockquote.stories-BPJ90wil.js
+++
@@ -1,5 +0,0 @@
-import{n as e,r as t}from"./chunk-BneVvdWh.js";import{dt as n,t as r}from"./src-YYfMdocS.js";import{m as i,u as a}from"./exampleContent-ChMoTaqN.js";var o=t({Default:()=>c,InverseColour:()=>l,__namedExportsOrder:()=>u,default:()=>s}),s,c,l,u,d=e((()=>{r(),i(),s={title:`Components/Text/Blockquote`,component:n,args:{children:a()},argTypes:{children:{description:`The text for the quote.`,table:{disable:!1}},color:{control:{labels:{undefined:`default`},type:`radio`},options:[void 0,`inverse`]}}},c={},l={args:{color:`inverse`}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{}`,...c.parameters?.docs?.source}}},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
-  args: {
-    color: 'inverse'
-  }
-}`,...l.parameters?.docs?.source}}},u=[`Default`,`InverseColour`]}));d();export{c as Default,l as InverseColour,u as __namedExportsOrder,s as default,d as n,o as t};
