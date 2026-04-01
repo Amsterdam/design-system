d1d831a6a373cba0e9c9d49dab0cc76c989fe0bc
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-04-01 16:24:33 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@252df5f695d36ff019100f3b193b90b27262136b 🚀

---
+++ demo-DES-1722-table-caption-side/assets/Table.test.stories-wQGkfMPW.js
@@ -0,0 +1,34 @@
+import{n as e}from"./chunk-BneVvdWh.js";import{t}from"./jsx-runtime-BrayXyei.js";import{n,t as r}from"./renderComponentVariants-CTtnL2R2.js";import{c as i,t as a}from"./src-Du3u7qEE.js";import o,{n as s}from"./Table.stories-CXDGNJw2.js";var c,l,u,d;e((()=>{a(),r(),s(),c=t(),l={...o,title:`Components/Containers/Table`},u={args:{children:[(0,c.jsx)(i.Caption,{children:(0,c.jsx)(`h3`,{children:`Kosten voor de aanvraag`})},1),(0,c.jsx)(i.Header,{children:(0,c.jsxs)(i.Row,{children:[(0,c.jsx)(i.HeaderCell,{children:`Type`}),(0,c.jsxs)(i.HeaderCell,{children:[`Normale levertijd`,(0,c.jsx)(`br`,{}),`1 week`]}),(0,c.jsxs)(i.HeaderCell,{children:[`Spoed`,(0,c.jsx)(`br`,{}),`2 werkdagen`]})]})},2),(0,c.jsxs)(i.Body,{children:[(0,c.jsxs)(i.Row,{children:[(0,c.jsx)(i.Cell,{children:`Paspoort`}),(0,c.jsx)(i.Cell,{children:`€ 77,85`}),(0,c.jsx)(i.Cell,{children:`€ 130,80`})]}),(0,c.jsxs)(i.Row,{children:[(0,c.jsx)(i.Cell,{children:`ID-kaart`}),(0,c.jsx)(i.Cell,{children:`€ 70,35`}),(0,c.jsx)(i.Cell,{children:`€ 123,30`})]})]},3)]},render:e=>n(i,{args:e}),tags:[`!dev`,`!autodocs`]},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<Table.Caption key={1}>
+        <h3>Kosten voor de aanvraag</h3>
+      </Table.Caption>, <Table.Header key={2}>
+        <Table.Row>
+          <Table.HeaderCell>Type</Table.HeaderCell>
+          <Table.HeaderCell>
+            Normale levertijd
+            <br />1 week
+          </Table.HeaderCell>
+          <Table.HeaderCell>
+            Spoed
+            <br />2 werkdagen
+          </Table.HeaderCell>
+        </Table.Row>
+      </Table.Header>, <Table.Body key={3}>
+        <Table.Row>
+          <Table.Cell>Paspoort</Table.Cell>
+          <Table.Cell>€ 77,85</Table.Cell>
+          <Table.Cell>€ 130,80</Table.Cell>
+        </Table.Row>
+        <Table.Row>
+          <Table.Cell>ID-kaart</Table.Cell>
+          <Table.Cell>€ 70,35</Table.Cell>
+          <Table.Cell>€ 123,30</Table.Cell>
+        </Table.Row>
+      </Table.Body>]
+  },
+  render: args => renderComponentVariants(Table, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...u.parameters?.docs?.source}}},d=[`Test`]}))();export{u as Test,d as __namedExportsOrder,l as default};
