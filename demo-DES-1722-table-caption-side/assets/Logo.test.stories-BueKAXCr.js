cc5cbc43b74325ae783caae0da4b1db06c3169f1
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-04-01 14:05:08 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@1c58abf8e727463f593d56909927837f968610ee 🚀

---
+++ demo-DES-1722-table-caption-side/assets/Logo.test.stories-BueKAXCr.js
@@ -0,0 +1,10 @@
+import{a as e,n as t}from"./chunk-BneVvdWh.js";import{t as n}from"./react-XedgCGTy.js";import{t as r}from"./jsx-runtime-BrayXyei.js";import{M as i,N as a,j as o,t as s}from"./src-DR2buuCf.js";import c,{n as l}from"./Logo.stories-B_8HuR8C.js";var u,d,f,p,m;t((()=>{s(),i(),l(),u=e(n()),d=r(),f={...c,title:`Components/Media/Logo`},p={render:e=>(0,d.jsx)(`div`,{style:{display:`flex`,flexWrap:`wrap`,gap:`var(--ams-space-l)`},children:[void 0,...a].map(t=>(0,u.createElement)(o,{...e,brand:t,key:t}))}),tags:[`!dev`,`!autodocs`]},p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
+  render: args => <div style={{
+    display: 'flex',
+    flexWrap: 'wrap',
+    gap: 'var(--ams-space-l)'
+  }}>
+      {[undefined, ...logoBrands].map(brand => <Logo {...args} brand={brand} key={brand} />)}
+    </div>,
+  tags: ['!dev', '!autodocs']
+}`,...p.parameters?.docs?.source}}},m=[`Test`]}))();export{p as Test,m as __namedExportsOrder,f as default};
