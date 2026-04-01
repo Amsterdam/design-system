91c61d2bc293dd5475c7a3eef561d75772f4c78c
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-04-01 12:49:17 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@c8f97ad583771a1e0822859103c8e2bd3734abe7 🚀

---
+++ demo-DES-1503-space-between-docs/assets/border.docs-Dc3eiw9X.js
@@ -0,0 +1,16 @@
+import{n as e}from"./chunk-BneVvdWh.js";import{t}from"./jsx-runtime-BrayXyei.js";import{r as n}from"./react-oCAR5XSR.js";import{c as r,f as i}from"./blocks-C1HJxkAN.js";import{n as a,t as o}from"./DesignTokensTable-CJgajNVN.js";import{t as s}from"./mdx-react-shim-9Tyt82po.js";var c,l,u=e((()=>{c={border:{width:{s:{$value:{value:.0625,unit:`rem`},$extensions:{"nl.amsterdam.type":`borderWidth`}},m:{$value:{value:.0625,unit:`rem`},$extensions:{"nl.amsterdam.type":`borderWidth`}},l:{$value:{value:.125,unit:`rem`},$extensions:{"nl.amsterdam.type":`borderWidth`}},xl:{$value:{value:.1875,unit:`rem`},$extensions:{"nl.amsterdam.type":`borderWidth`}}}}},l={ams:c}})),d,f,p=e((()=>{d={border:{width:{s:{$value:{value:.0625,unit:`rem`},$extensions:{"nl.amsterdam.type":`borderWidth`}},m:{$value:{value:.125,unit:`rem`},$extensions:{"nl.amsterdam.type":`borderWidth`}},l:{$value:{value:.1875,unit:`rem`},$extensions:{"nl.amsterdam.type":`borderWidth`}},xl:{$value:{value:.25,unit:`rem`},$extensions:{"nl.amsterdam.type":`borderWidth`}}}}},f={ams:d}}));function m(e){let t={a:`a`,h1:`h1`,h2:`h2`,h3:`h3`,p:`p`,...n(),...e.components};return(0,g.jsxs)(g.Fragment,{children:[`
+`,`
+`,`
+`,(0,g.jsx)(r,{title:`Brand/Design Tokens/Border`}),`
+`,(0,g.jsx)(t.h1,{id:`border`,children:`Border`}),`
+`,(0,g.jsx)(t.p,{children:`Elements that have a border, outline or underline use one of these widths.`}),`
+`,(0,g.jsx)(t.h2,{id:`design`,children:`Design`}),`
+`,(0,g.jsx)(t.p,{children:`We use a limited set of border widths for consistency.`}),`
+`,(0,g.jsx)(t.h2,{id:`design-tokens`,children:`Design tokens`}),`
+`,(0,g.jsx)(t.h3,{id:`spacious-mode`,children:`Spacious Mode`}),`
+`,(0,g.jsx)(t.p,{children:`The default border is 2 pixels thick.
+One thinner and two thicker options are available.`}),`
+`,(0,g.jsx)(o,{tokens:f}),`
+`,(0,g.jsx)(t.h3,{id:`compact-mode`,children:`Compact Mode`}),`
+`,(0,g.jsxs)(t.p,{children:[`All borders are a pixel thinner in `,(0,g.jsx)(t.a,{href:`/docs/docs-developer-guide-modes--docs`,children:`Compact Mode`}),`, except for the small width.`]}),`
+`,(0,g.jsx)(o,{tokens:l})]})}function h(e={}){let{wrapper:t}={...n(),...e.components};return t?(0,g.jsx)(t,{...e,children:(0,g.jsx)(m,{...e})}):m(e)}var g;e((()=>{g=t(),s(),i(),a(),u(),p()}))();export{h as default};
---
+++ demo-DES-1503-space-between-docs/assets/mdx-react-shim-9Tyt82po.js
@@ -0,0 +1,1 @@
+import{n as e}from"./chunk-BneVvdWh.js";import{t}from"./react-oCAR5XSR.js";var n=e((()=>{t()}));export{n as t};
