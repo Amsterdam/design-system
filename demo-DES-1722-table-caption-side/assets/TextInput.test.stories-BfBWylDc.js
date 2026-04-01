c9f358812108521432e9aefe229c66f95a16eb80
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-04-01 16:41:02 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@9d6b3c0d404fe8153e0ad26f098ef42d4b96bd93 🚀

---
+++ demo-DES-1722-table-caption-side/assets/TextInput.test.stories-BfBWylDc.js
@@ -0,0 +1,7 @@
+import{n as e}from"./chunk-BneVvdWh.js";import{n as t,t as n}from"./renderComponentVariants-CTtnL2R2.js";import{i as r,t as i}from"./src-IF4cVlGA.js";import a,{n as o}from"./TextInput.stories-DVj7VWZx.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Text Input`},c={render:e=>t(r,{args:e,variants:[`disabled`,`hovered`]}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(TextInput, {
+    args,
+    variants: ['disabled', 'hovered']
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
