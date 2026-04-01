c02748efc05007ede5517a8d79df564be96c652c
RubenSibon <mail+github@rubensibon.nl>
2026-04-01 12:34:24 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@f7cbc331ad5a104457dbf18ccb61ffb74f667798 🚀

--- demo-DES-1715-logical-size-breakpoints-in-tokens/assets/Page.test.stories-BfCdukHT.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-BneVvdWh.js";import{n as t,t as n}from"./renderComponentVariants-CTtnL2R2.js";import{E as r,t as i}from"./src-B46CSdeH.js";import a,{n as o}from"./Page.stories-B1YbhQnw.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Containers/Page`},c={render:e=>t(r,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(Page, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
