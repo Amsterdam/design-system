96013917bbe38a7b1b69383854b2bfb531a24aca
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-04-02 09:34:33 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@622c361f80b8ca4d81c520be2fff82047ea7fc96 🚀

--- demo-DES-1613-collapse-progress-list-step/assets/StandaloneLink.test.stories-DXm49Psk.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-BneVvdWh.js";import{n as t,t as n}from"./renderComponentVariants-CTtnL2R2.js";import{t as r,u as i}from"./src-ClA4WzsD.js";import a,{n as o}from"./StandaloneLink.stories-Cs5mEAZt.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/StandaloneLink`},c={render:e=>t(i,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(StandaloneLink, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
---
+++ demo-DES-1613-collapse-progress-list-step/assets/Switch.docs-tjjoKGQ5.js
@@ -0,0 +1,37 @@
+import{n as e}from"./chunk-BneVvdWh.js";import{t}from"./jsx-runtime-BrayXyei.js";import{r as n}from"./react-oCAR5XSR.js";import{c as r,f as i,i as a,l as o,n as s,s as c}from"./blocks-CtYfeZSt.js";import{n as l,t as u}from"./DesignTokensTable-CJgajNVN.js";import{t as d}from"./mdx-react-shim-9Tyt82po.js";import{WithLabel as f,n as p,t as m}from"./Switch.stories-D3MoAtAW.js";var h,g=e((()=>{h=`<!-- @license CC0-1.0 -->
+
+# Switch
+
+A switch is a control element to toggle between two different states quickly.
+A switch applies to a page or the entire system, such as an on/off switch.
+
+## Guidelines
+
+- A Switch must have a label, and in most cases, this label should be visible.
+- Switch only between two different states.
+  It is a binary action.
+- The action takes place immediately when the user operates the switch.
+
+## Relevant WCAG requirements
+
+- [WCAG 1.3.5](https://www.w3.org/WAI/WCAG21/Understanding/identify-input-purpose.html): it is both clear for a user and programmatically what the purpose of a form field is.
+
+Switch is an interactive element, and the [general requirements and guidelines for interactivity](/docs/docs-developer-guide-interactivity--docs) apply.
+
+## References
+
+- [Switch Pattern](https://www.w3.org/WAI/ARIA/apg/patterns/switch/)
+- [Role switch](https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/switch_role)
+`})),_,v,y=e((()=>{_={switch:{"background-color":{$value:`#767676`,$type:`color`},cursor:{$value:`{ams.cursor.interactive}`},"outline-offset":{$value:`{ams.focus.outline-offset}`,$type:`dimension`,$extensions:{"nl.amsterdam.subtype":`space`}},"inline-size":{$value:{value:3.5,unit:`rem`},$type:`dimension`},thumb:{"background-color":{$value:`{ams.color.background}`,$type:`color`},"block-size":{$value:{value:1.75,unit:`rem`},$type:`dimension`},"inline-size":{$value:{value:1.75,unit:`rem`},$type:`dimension`},hover:{"box-shadow":{$value:`0 0 0 {ams.border.width.m} {ams.switch.thumb.hover.color}`},color:{$value:`{ams.color.interactive.hover}`,$type:`color`}}},checked:{"background-color":{$value:`{ams.color.interactive.default}`,$type:`color`}},disabled:{"background-color":{$value:`{ams.color.interactive.disabled}`,$type:`color`},cursor:{$value:`{ams.cursor.disabled}`}}}},v={ams:_}}));function b(e){let t={h2:`h2`,h3:`h3`,...n(),...e.components};return(0,S.jsxs)(S.Fragment,{children:[`
+`,`
+`,`
+`,`
+`,(0,S.jsx)(r,{of:m}),`
+`,(0,S.jsx)(c,{children:h}),`
+`,(0,S.jsx)(o,{}),`
+`,(0,S.jsx)(a,{}),`
+`,(0,S.jsx)(t.h2,{id:`examples`,children:`Examples`}),`
+`,(0,S.jsx)(t.h3,{id:`with-label`,children:`With Label`}),`
+`,(0,S.jsx)(s,{of:f}),`
+`,(0,S.jsx)(t.h2,{id:`design-tokens`,children:`Design tokens`}),`
+`,(0,S.jsx)(u,{tokens:v})]})}function x(e={}){let{wrapper:t}={...n(),...e.components};return t?(0,S.jsx)(t,{...e,children:(0,S.jsx)(b,{...e})}):b(e)}var S;e((()=>{S=t(),d(),i(),p(),g(),y(),l()}))();export{x as default};
