12c3682c4accd84bd63750daae3cb93c1ed56dad
GitHub <github-action@users.noreply.github.com>
2026-04-02 13:36:48 +0000

    Remove feature branch folder

--- demo-npm_and_yarn/patch-and-minor-dev-dependencies-e1c0b9e47a/assets/IconButton.test.stories-BVRYRKkq.js
+++
@@ -1,7 +0,0 @@
-import{n as e}from"./chunk-BneVvdWh.js";import{n as t,t as n}from"./renderComponentVariants-CTtnL2R2.js";import{St as r,t as i}from"./src-Ddak2An-.js";import a,{n as o}from"./IconButton.stories-Bk1ZTwKk.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Buttons/Icon Button`},c={render:e=>t(r,{args:e,variants:[`hovered`,`disabled`]}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(IconButton, {
-    args,
-    variants: ['hovered', 'disabled']
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
