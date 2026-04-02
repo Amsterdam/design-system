6e19b7ce877468c5139e7269ab4e3debfa3a96ea
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-04-02 13:28:13 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@0e2176e692a9233f4f4882939b283fc12096df87 🚀

---
+++ demo-npm_and_yarn/patch-and-minor-dev-dependencies-e1c0b9e47a/assets/OrderedList.stories-DShkNtgU.js
@@ -0,0 +1,92 @@
+import{n as e,r as t}from"./chunk-BneVvdWh.js";import{t as n}from"./jsx-runtime-BrayXyei.js";import{G as r,N as i,l as a}from"./iframe-Bpauyx0Y.js";import{O as o,t as s}from"./src-Ddak2An-.js";import{c,m as l}from"./exampleContent-Bw9nQbFM.js";var u=t({Default:()=>p,DescendingNumbers:()=>g,InverseColor:()=>v,SmallText:()=>y,StartingNumber:()=>h,TwoLevels:()=>m,WithoutMarkers:()=>_,__namedExportsOrder:()=>b,default:()=>f}),d,f,p,m,h,g,_,v,y,b,x=e((()=>{r(),s(),l(),d=n(),f={title:`Components/Text/Ordered List`,component:o,args:{children:c().map(e=>(0,d.jsx)(o.Item,{children:e},e)),markers:void 0,reversed:void 0,start:void 0},argTypes:{color:{control:{labels:{undefined:`default`},type:`radio`},options:[void 0,`inverse`]},reversed:{control:`boolean`,description:`Numbers the items from the highest value down.`},size:{control:{labels:{small:`small`,undefined:`medium`},type:`radio`},options:[`small`,void 0]},start:{control:`number`,description:`The value for the first list item’s marker.`}}},p={},m={render:e=>(0,d.jsxs)(o,{...e,children:[(0,d.jsxs)(o.Item,{children:[`Stadsdeel West`,(0,d.jsxs)(o,{...e,children:[(0,d.jsx)(o.Item,{children:`Bos en Lommer`}),(0,d.jsx)(o.Item,{children:`Oud West / De Baarsjes`}),(0,d.jsx)(o.Item,{children:`Westerpark`})]})]}),(0,d.jsxs)(o.Item,{children:[`Stadsdeel Nieuw-West`,(0,d.jsxs)(o,{...e,children:[(0,d.jsx)(o.Item,{children:`De Aker, Sloten en Nieuw Sloten`}),(0,d.jsx)(o.Item,{children:`Geuzenveld, Slotermeer en Sloterdijken`}),(0,d.jsx)(o.Item,{children:`Osdorp`}),(0,d.jsx)(o.Item,{children:`Slotervaart`})]})]})]})},h={args:{children:[(0,d.jsx)(o.Item,{children:`Zes`},6),(0,d.jsx)(o.Item,{children:`Zeven`},7),(0,d.jsx)(o.Item,{children:`Acht`},8)],start:6}},g={args:{children:[(0,d.jsx)(o.Item,{children:`Drie`},3),(0,d.jsx)(o.Item,{children:`Twee`},2),(0,d.jsx)(o.Item,{children:`Eén`},1)],reversed:!0,start:3}},_={args:{children:[(0,d.jsx)(o.Item,{children:(0,d.jsxs)(a,{children:[(0,d.jsx)(a.Heading,{level:2,children:(0,d.jsx)(a.Link,{href:`#`,children:`Weg met steen, hallo extra groen en koelte`})}),(0,d.jsx)(i,{children:`Sinds 2021 kwamen er maar liefst 60 nieuwe groene plekken bij in de stad. Groen is fijn en het verkoelt de stad in de zomer. Een paar voorbeelden.`}),(0,d.jsx)(i,{size:`small`,children:`16 augustus 2023`})]})},1),(0,d.jsx)(o.Item,{children:(0,d.jsxs)(a,{children:[(0,d.jsx)(a.Heading,{level:2,children:(0,d.jsx)(a.Link,{href:`#`,children:`Amsterdam bindt de strijd aan met lawaaierige voertuigen`})}),(0,d.jsx)(i,{children:`Deze zomer testen we of digitale borden langs de weg kunnen helpen om geluidsoverlast van voertuigen zoals motoren en auto’s tegen te gaan.`}),(0,d.jsx)(i,{size:`small`,children:`10 augustus 2023`})]})},2),(0,d.jsx)(o.Item,{children:(0,d.jsxs)(a,{children:[(0,d.jsx)(a.Heading,{level:2,children:(0,d.jsx)(a.Link,{href:`#`,children:`Een prachtroute door de wonderlijke Baarsjes`})}),(0,d.jsx)(i,{children:`In de Baarsjes zijn kunst en cultuur met elkaar vervlochten. We zetten een prachtige wandelroute voor u uit en laten zien hoe het was en hoe het nu is.`}),(0,d.jsx)(i,{size:`small`,children:`8 augustus 2023`})]})},3)],className:`ams-gap-xl`,markers:!1,style:{maxInlineSize:`32rem`}}},v={args:{color:`inverse`},render:e=>(0,d.jsx)(o,{...e,children:(0,d.jsxs)(o.Item,{children:[`Stadsdeel West`,(0,d.jsxs)(o,{...e,children:[(0,d.jsx)(o.Item,{children:`Bos en Lommer`}),(0,d.jsx)(o.Item,{children:`Oud West / De Baarsjes`}),(0,d.jsx)(o.Item,{children:`Westerpark`})]})]})})},y={args:{size:`small`}},p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{}`,...p.parameters?.docs?.source}}},m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`{
+  render: args => <OrderedList {...args}>
+      <OrderedList.Item>
+        Stadsdeel West
+        <OrderedList {...args}>
+          <OrderedList.Item>Bos en Lommer</OrderedList.Item>
+          <OrderedList.Item>Oud West / De Baarsjes</OrderedList.Item>
+          <OrderedList.Item>Westerpark</OrderedList.Item>
+        </OrderedList>
+      </OrderedList.Item>
+      <OrderedList.Item>
+        Stadsdeel Nieuw-West
+        <OrderedList {...args}>
+          <OrderedList.Item>De Aker, Sloten en Nieuw Sloten</OrderedList.Item>
+          <OrderedList.Item>Geuzenveld, Slotermeer en Sloterdijken</OrderedList.Item>
+          <OrderedList.Item>Osdorp</OrderedList.Item>
+          <OrderedList.Item>Slotervaart</OrderedList.Item>
+        </OrderedList>
+      </OrderedList.Item>
+    </OrderedList>
+}`,...m.parameters?.docs?.source}}},h.parameters={...h.parameters,docs:{...h.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<OrderedList.Item key={6}>Zes</OrderedList.Item>, <OrderedList.Item key={7}>Zeven</OrderedList.Item>, <OrderedList.Item key={8}>Acht</OrderedList.Item>],
+    start: 6
+  }
+}`,...h.parameters?.docs?.source}}},g.parameters={...g.parameters,docs:{...g.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<OrderedList.Item key={3}>Drie</OrderedList.Item>, <OrderedList.Item key={2}>Twee</OrderedList.Item>, <OrderedList.Item key={1}>Eén</OrderedList.Item>],
+    reversed: true,
+    start: 3
+  }
+}`,...g.parameters?.docs?.source}}},_.parameters={..._.parameters,docs:{..._.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<OrderedList.Item key={1}>
+        <Card>
+          <Card.Heading level={2}>
+            <Card.Link href="#">Weg met steen, hallo extra groen en koelte</Card.Link>
+          </Card.Heading>
+          <Paragraph>
+            Sinds 2021 kwamen er maar liefst 60 nieuwe groene plekken bij in de stad. Groen is fijn en het verkoelt de
+            stad in de zomer. Een paar voorbeelden.
+          </Paragraph>
+          <Paragraph size="small">16 augustus 2023</Paragraph>
+        </Card>
+      </OrderedList.Item>, <OrderedList.Item key={2}>
+        <Card>
+          <Card.Heading level={2}>
+            <Card.Link href="#">Amsterdam bindt de strijd aan met lawaaierige voertuigen</Card.Link>
+          </Card.Heading>
+          <Paragraph>
+            Deze zomer testen we of digitale borden langs de weg kunnen helpen om geluidsoverlast van voertuigen zoals
+            motoren en auto’s tegen te gaan.
+          </Paragraph>
+          <Paragraph size="small">10 augustus 2023</Paragraph>
+        </Card>
+      </OrderedList.Item>, <OrderedList.Item key={3}>
+        <Card>
+          <Card.Heading level={2}>
+            <Card.Link href="#">Een prachtroute door de wonderlijke Baarsjes</Card.Link>
+          </Card.Heading>
+          <Paragraph>
+            In de Baarsjes zijn kunst en cultuur met elkaar vervlochten. We zetten een prachtige wandelroute voor u uit
+            en laten zien hoe het was en hoe het nu is.
+          </Paragraph>
+          <Paragraph size="small">8 augustus 2023</Paragraph>
+        </Card>
+      </OrderedList.Item>],
+    className: 'ams-gap-xl',
+    markers: false,
+    style: {
+      maxInlineSize: '32rem'
+    }
+  }
+}`,..._.parameters?.docs?.source}}},v.parameters={...v.parameters,docs:{...v.parameters?.docs,source:{originalSource:`{
+  args: {
+    color: 'inverse'
+  },
+  render: args => <OrderedList {...args}>
+      <OrderedList.Item>
+        Stadsdeel West
+        <OrderedList {...args}>
+          <OrderedList.Item>Bos en Lommer</OrderedList.Item>
+          <OrderedList.Item>Oud West / De Baarsjes</OrderedList.Item>
+          <OrderedList.Item>Westerpark</OrderedList.Item>
+        </OrderedList>
+      </OrderedList.Item>
+    </OrderedList>
+}`,...v.parameters?.docs?.source}}},y.parameters={...y.parameters,docs:{...y.parameters?.docs,source:{originalSource:`{
+  args: {
+    size: 'small'
+  }
+}`,...y.parameters?.docs?.source}}},b=[`Default`,`TwoLevels`,`StartingNumber`,`DescendingNumbers`,`WithoutMarkers`,`InverseColor`,`SmallText`]}));x();export{p as Default,g as DescendingNumbers,v as InverseColor,y as SmallText,h as StartingNumber,m as TwoLevels,_ as WithoutMarkers,b as __namedExportsOrder,f as default,x as n,u as t};
