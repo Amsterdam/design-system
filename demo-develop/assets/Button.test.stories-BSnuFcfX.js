eac7ed70a9855930eda278aa6fa6216537de1ff6
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-04-02 13:39:18 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@aff91f521ca64471047ed2d86246ea6b2e9d2b4b 🚀

--- demo-develop/assets/Button.test.stories-BSnuFcfX.js
+++
@@ -1,7 +0,0 @@
-import{n as e}from"./chunk-BneVvdWh.js";import{n as t,t as n}from"./renderComponentVariants-CTtnL2R2.js";import{ct as r,t as i}from"./src-fUL9p-W2.js";import a,{n as o}from"./Button.stories-G0S1waU3.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Buttons/Button`},c={render:e=>t(r,{args:e,variants:[`disabled`,`hovered`]}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(Button, {
-    args,
-    variants: ['disabled', 'hovered']
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
