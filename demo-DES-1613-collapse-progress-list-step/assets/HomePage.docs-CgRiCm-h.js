e932c86aeeb1836146f2cb859e4a63beafc6ac0c
GitHub <github-action@users.noreply.github.com>
2026-04-02 13:25:39 +0000

    Remove feature branch folder

--- demo-DES-1613-collapse-progress-list-step/assets/HomePage.docs-CgRiCm-h.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-BneVvdWh.js";import{t}from"./jsx-runtime-BrayXyei.js";import{r as n}from"./react-oCAR5XSR.js";import{c as r,f as i}from"./blocks-CtYfeZSt.js";import{t as a}from"./mdx-react-shim-9Tyt82po.js";import{n as o,t as s}from"./HomePage.stories-BE2Wz8_A.js";function c(e){let t={h1:`h1`,p:`p`,...n(),...e.components};return(0,u.jsxs)(u.Fragment,{children:[`
-`,`
-`,(0,u.jsx)(r,{of:s}),`
-`,(0,u.jsx)(t.h1,{id:`home-page`,children:`Home page`}),`
-`,(0,u.jsx)(t.p,{children:`The home page of an internal website can have varying types of content.`}),`
-`,(0,u.jsx)(t.p,{children:`Our single example in this category currently focuses on the overall layout.`})]})}function l(e={}){let{wrapper:t}={...n(),...e.components};return t?(0,u.jsx)(t,{...e,children:(0,u.jsx)(c,{...e})}):c(e)}var u;e((()=>{u=t(),a(),i(),o()}))();export{l as default};
