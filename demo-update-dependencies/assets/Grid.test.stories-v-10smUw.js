d4037c59fc6dfa441c5a5849857218e89f52781f
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-04-03 13:53:20 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@95b0cd3a32f112efaab1f8840ebf18e490bab3b2 🚀

---
+++ demo-update-dependencies/assets/Grid.test.stories-v-10smUw.js
@@ -0,0 +1,9 @@
+import{n as e}from"./chunk-BneVvdWh.js";import{At as t}from"./iframe-DlHI38XH.js";import{n,t as r}from"./renderComponentVariants-DCFQN_lQ.js";import{H as i,t as a}from"./src-Df0VItQp.js";import o,{n as s}from"./Grid.stories-4q8fFmDW.js";var c,l,u,d;e((()=>{a(),r(),s(),c=t(),l={...o,title:`Components/Layout/Grid`},u={args:{children:[(0,c.jsx)(i.Cell,{span:`all`},1),(0,c.jsx)(i.Cell,{span:`all`},2)]},render:e=>n(i,{args:e}),tags:[`!dev`,`!autodocs`]},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<Grid.Cell key={1} span="all" />, <Grid.Cell key={2} span="all" />]
+  },
+  render: args => renderComponentVariants(Grid, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...u.parameters?.docs?.source}}},d=[`Test`]}))();export{u as Test,d as __namedExportsOrder,l as default};
