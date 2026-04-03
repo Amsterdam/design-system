9235b2e7f1d4614e68ea5213648cf1e993e82481
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-04-03 14:39:23 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@8af33e44eab8f7b8a7bb843ef12f956aa80cbcb6 🚀

---
+++ demo-release-please--branches--main/assets/DateInput.test.stories-DHwiRzZL.js
@@ -0,0 +1,6 @@
+import{n as e}from"./chunk-BneVvdWh.js";import{n as t,t as n}from"./renderComponentVariants-DCFQN_lQ.js";import{Z as r,t as i}from"./src-Df0VItQp.js";import a,{n as o}from"./DateInput.stories-BXqayX31.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Date Input`},c={render:e=>t(r,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(DateInput, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
---
+++ demo-release-please--branches--main/assets/Logo.test.stories-h91C4JtV.js
@@ -0,0 +1,10 @@
+import{a as e,n as t}from"./chunk-BneVvdWh.js";import{At as n,pn as r}from"./iframe-DlHI38XH.js";import{M as i,N as a,j as o,t as s}from"./src-Df0VItQp.js";import c,{n as l}from"./Logo.stories-DSI4ka3-.js";var u,d,f,p,m;t((()=>{s(),i(),l(),u=e(r()),d=n(),f={...c,title:`Components/Media/Logo`},p={render:e=>(0,d.jsx)(`div`,{style:{display:`flex`,flexWrap:`wrap`,gap:`var(--ams-space-l)`},children:[void 0,...a].map(t=>(0,u.createElement)(o,{...e,brand:t,key:t}))}),tags:[`!dev`,`!autodocs`]},p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
+  render: args => <div style={{
+    display: 'flex',
+    flexWrap: 'wrap',
+    gap: 'var(--ams-space-l)'
+  }}>
+      {[undefined, ...logoBrands].map(brand => <Logo {...args} brand={brand} key={brand} />)}
+    </div>,
+  tags: ['!dev', '!autodocs']
+}`,...p.parameters?.docs?.source}}},m=[`Test`]}))();export{p as Test,m as __namedExportsOrder,f as default};
