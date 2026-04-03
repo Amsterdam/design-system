b089b1779b63475d7d71fbcc31ce2093a0ab78c6
RubenSibon <mail+github@rubensibon.nl>
2026-04-03 15:05:03 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@bab730a09cc5b4a475d4a5e440b30bac91b0c437 🚀

---
+++ demo-DES-1718_llm-code-agents-custom-instructions/assets/Column.test.stories-D5fmQL9K.js
@@ -0,0 +1,7 @@
+import{n as e}from"./chunk-BneVvdWh.js";import{n as t,t as n}from"./renderComponentVariants-Dv1C3gkB.js";import{Q as r,t as i}from"./src-DxvToe18.js";import a,{n as o}from"./Column.stories-5-jQAbnB.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Layout/Column`},c={render:e=>t(r,{args:e,layout:`grid`}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(Column, {
+    args,
+    layout: 'grid'
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
