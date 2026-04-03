85877839f1c399c2c3f4ac0ca3bf185ea2a615cd
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-04-03 15:44:22 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@6b5bb5b2f7c273d35e25e18e29cfa6557d20c38f 🚀

---
+++ demo-develop/assets/Column.test.stories-BPhYV-kC.js
@@ -0,0 +1,7 @@
+import{n as e}from"./chunk-BneVvdWh.js";import{n as t,t as n}from"./renderComponentVariants-C4ziNsv0.js";import{Q as r,t as i}from"./src-DsGGxOin.js";import a,{n as o}from"./Column.stories-C7TJKbPT.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Layout/Column`},c={render:e=>t(r,{args:e,layout:`grid`}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(Column, {
+    args,
+    layout: 'grid'
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
