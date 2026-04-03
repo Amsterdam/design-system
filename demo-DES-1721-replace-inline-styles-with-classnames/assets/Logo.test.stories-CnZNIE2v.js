bc4434847f09d0fb2f4cd114ec842bab12ec0b2f
Evi-2003 <69012052+Evi-2003@users.noreply.github.com>
2026-04-03 09:44:53 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@ded83b0196425ec382ebcdaaa295435ad373748b 🚀

---
+++ demo-DES-1721-replace-inline-styles-with-classnames/assets/Logo.test.stories-CnZNIE2v.js
@@ -0,0 +1,6 @@
+import{a as e,n as t}from"./chunk-BneVvdWh.js";import{t as n}from"./react-XedgCGTy.js";import{t as r}from"./jsx-runtime-BrayXyei.js";import{M as i,N as a,j as o,t as s}from"./src-CDsGk1Ej.js";import c,{n as l}from"./Logo.stories-C2WPCUoH.js";var u,d,f,p,m;t((()=>{s(),i(),l(),u=e(n()),d=r(),f={...c,title:`Components/Media/Logo`},p={render:e=>(0,d.jsx)(`div`,{className:`_ams-gallery`,children:[void 0,...a].map(t=>(0,u.createElement)(o,{...e,brand:t,key:t}))}),tags:[`!dev`,`!autodocs`]},p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
+  render: args => <div className="_ams-gallery">
+      {[undefined, ...logoBrands].map(brand => <Logo {...args} brand={brand} key={brand} />)}
+    </div>,
+  tags: ['!dev', '!autodocs']
+}`,...p.parameters?.docs?.source}}},m=[`Test`]}))();export{p as Test,m as __namedExportsOrder,f as default};
