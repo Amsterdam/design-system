63592b6526c62a837dfe732a8ab551441275c863
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-04-03 13:47:03 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@7f0de1247e6402dde0f3f584ab72962df2218197 🚀

---
+++ demo-4.0.0/assets/Image.test.stories-CHuoAPay.js
@@ -0,0 +1,10 @@
+import{n as e}from"./chunk-BneVvdWh.js";import{n as t,t as n}from"./renderComponentVariants-CTtnL2R2.js";import{it as r,t as i}from"./src-hwrujncY.js";import a,{n as o}from"./Image.stories-CWDzlY1l.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Media/Image`},c={args:{alt:``,src:`https://picsum.photos/id/122/1280/720`},render:e=>t(r,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    alt: '',
+    src: 'https://picsum.photos/id/122/1280/720'
+  },
+  render: args => renderComponentVariants(Image, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
--- demo-4.0.0/assets/Overlap.stories-CxeL0Ulx.js
+++
@@ -1,1 +0,0 @@
-import{a as e,i as t,r as n,t as r}from"./Overlap.stories-DCw8FsNG.js";t();export{r as Default,n as __namedExportsOrder,e as default};
