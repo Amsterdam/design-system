61f9fdc62cb3e1d0c61911da55097625ee571911
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-04-03 13:19:29 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@5bc58fb795f7264c9b714095466bf2b171974456 🚀

--- demo-4.0.0/assets/Body.stories-wwil3Hsb.js
+++
@@ -1,6 +0,0 @@
-import{n as e,r as t}from"./chunk-BneVvdWh.js";import{t as n}from"./jsx-runtime-BrayXyei.js";import{G as r,N as i}from"./iframe-CzcQrkI0.js";var a,o,s=e((()=>{a=n(),o=e=>(0,a.jsx)(`body`,{...e});try{o.displayName=`Body`,o.__docgenInfo={description:`Mock component to render examples in Storybook. Not for reuse.`,displayName:`Body`,props:{}}}catch{}})),c=t({Default:()=>f,__namedExportsOrder:()=>p,default:()=>d}),l,u,d,f,p,m=e((()=>{r(),s(),l=n(),u=e=>(0,l.jsx)(`body`,{...e,className:`ams-body`}),d={title:`Utilities/CSS/Body`,component:o},f={args:{children:(0,l.jsx)(i,{children:`This is the content of the HTML <body> element.`})},render:u},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: <Paragraph>{\`This is the content of the HTML <body> element.\`}</Paragraph>
-  },
-  render
-}`,...f.parameters?.docs?.source}}},p=[`Default`]}));m();export{f as Default,p as __namedExportsOrder,d as default,m as n,c as t};
--- demo-4.0.0/assets/Overlap.stories-C5-TdQa_.js
+++
@@ -1,1 +0,0 @@
-import{a as e,i as t,r as n,t as r}from"./Overlap.stories-C5wOGX_5.js";t();export{r as Default,n as __namedExportsOrder,e as default};
