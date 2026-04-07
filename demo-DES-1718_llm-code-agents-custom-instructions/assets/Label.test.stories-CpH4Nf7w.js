801ca2f50af729f4d0eb07f3598a36a5007d1005
RubenSibon <mail+github@rubensibon.nl>
2026-04-07 07:08:42 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@79f0dda8585a09d608bf703b8f815cd0289a4529 🚀

--- demo-DES-1718_llm-code-agents-custom-instructions/assets/Label.test.stories-CpH4Nf7w.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-BneVvdWh.js";import{n as t,t as n}from"./renderComponentVariants-Dv1C3gkB.js";import{F as r,t as i}from"./src-DxvToe18.js";import a,{n as o}from"./Label.stories-BzAZsm4l.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Label`},c={render:e=>t(r,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(Label, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
