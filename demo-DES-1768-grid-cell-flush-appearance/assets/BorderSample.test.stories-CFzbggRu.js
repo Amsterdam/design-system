634d8445adce375e93892df95c63b4cc9966b7e0
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-04-08 22:14:25 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@61ad97cf05cfe1eb1000e1f3c1787bd484231065 🚀

---
+++ demo-DES-1768-grid-cell-flush-appearance/assets/BorderSample.test.stories-CFzbggRu.js
@@ -0,0 +1,12 @@
+import{n as e}from"./chunk-zsgVPwQN.js";import{At as t}from"./iframe--Z_6vveP.js";import{n,t as r}from"./BorderSample-wmvfoFsr.js";var i,a,o,s;e((()=>{n(),i=t(),a={title:`Components/Docs/Border Sample`},o={render:()=>(0,i.jsxs)(`div`,{style:{display:`flex`,flexWrap:`wrap`,gap:`var(--ams-space-l)`},children:[(0,i.jsx)(r,{lineStyle:`dashed`}),(0,i.jsx)(r,{width:`4px`}),(0,i.jsx)(r,{lineStyle:`dashed`,width:`4px`})]}),tags:[`!dev`,`!autodocs`]},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  render: () => <div style={{
+    display: 'flex',
+    flexWrap: 'wrap',
+    gap: 'var(--ams-space-l)'
+  }}>
+      <BorderSample lineStyle="dashed" />
+      <BorderSample width="4px" />
+      <BorderSample lineStyle="dashed" width="4px" />
+    </div>,
+  tags: ['!dev', '!autodocs']
+}`,...o.parameters?.docs?.source}}},s=[`Test`]}))();export{o as Test,s as __namedExportsOrder,a as default};
---
+++ demo-DES-1768-grid-cell-flush-appearance/assets/Page.test.stories-CBs-3qr2.js
@@ -0,0 +1,6 @@
+import{n as e}from"./chunk-zsgVPwQN.js";import{n as t,t as n}from"./renderComponentVariants-Buy-DWSH.js";import{E as r,t as i}from"./src-d60rfyg2.js";import a,{n as o}from"./Page.stories-BDk_eQ7j.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Containers/Page`},c={render:e=>t(r,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(Page, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
