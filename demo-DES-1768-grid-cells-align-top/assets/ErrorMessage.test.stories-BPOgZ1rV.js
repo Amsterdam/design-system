7c0d35ee901a92de2de931652da76a2077e38572
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-04-08 22:04:36 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@c2af4f31c52fb0a5a24795de329d3b44901192cf 🚀

---
+++ demo-DES-1768-grid-cells-align-top/assets/ErrorMessage.test.stories-BPOgZ1rV.js
@@ -0,0 +1,6 @@
+import{n as e}from"./chunk-zsgVPwQN.js";import{n as t,t as n}from"./renderComponentVariants-Blv2y4t_.js";import{J as r,t as i}from"./src-Di61Idht.js";import a,{n as o}from"./ErrorMessage.stories-BaRToV8o.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Error Message`},c={render:e=>t(r,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(ErrorMessage, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
