ab366221afee0ad55275c8e446bce687d17f38a8
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-04-08 22:13:36 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@6fd48468ad2cef672a05a13bf0fc42ca16cef7a0 🚀

---
+++ demo-DES-1768-grid-cells-align-top/assets/FileInput.test.stories-De0zdKcz.js
@@ -0,0 +1,7 @@
+import{n as e}from"./chunk-zsgVPwQN.js";import{n as t,t as n}from"./renderComponentVariants-CjFtNNsg.js";import{W as r,t as i}from"./src-DQYRN0q9.js";import a,{n as o}from"./FileInput.stories-CORe6vv1.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/File Input`},c={render:e=>t(r,{args:e,variants:[`disabled`,`multiple`]}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(FileInput, {
+    args,
+    variants: ['disabled', 'multiple']
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
---
+++ demo-DES-1768-grid-cells-align-top/assets/Image.test.stories-CSDJBQZQ.js
@@ -0,0 +1,10 @@
+import{n as e}from"./chunk-zsgVPwQN.js";import{n as t,t as n}from"./renderComponentVariants-CjFtNNsg.js";import{it as r,t as i}from"./src-DQYRN0q9.js";import a,{n as o}from"./Image.stories-DMXJiTs6.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Media/Image`},c={args:{alt:``,src:`https://picsum.photos/id/122/1280/720`},render:e=>t(r,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    alt: '',
+    src: 'https://picsum.photos/id/122/1280/720'
+  },
+  render: args => renderComponentVariants(Image, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
