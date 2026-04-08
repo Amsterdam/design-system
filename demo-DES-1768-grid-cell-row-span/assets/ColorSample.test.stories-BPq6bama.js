2b6c95d37ea892a99de29920ca655c4a602f8261
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-04-08 22:26:44 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@da6b4d546d24840b90b1b5a668e5480968e1601c 🚀

---
+++ demo-DES-1768-grid-cell-row-span/assets/ColorSample.test.stories-BPq6bama.js
@@ -0,0 +1,11 @@
+import{n as e}from"./chunk-zsgVPwQN.js";import{At as t}from"./iframe-D7NlNO9p.js";import{n,t as r}from"./ColorSample-CSDiJG7T.js";var i,a,o,s;e((()=>{n(),i=t(),a={title:`Components/Docs/Color Sample`},o={render:()=>(0,i.jsxs)(`div`,{style:{display:`flex`,flexWrap:`wrap`,gap:`var(--ams-space-l)`},children:[(0,i.jsx)(r,{value:`hotpink`}),(0,i.jsx)(r,{value:``})]}),tags:[`!dev`,`!autodocs`]},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  render: () => <div style={{
+    display: 'flex',
+    flexWrap: 'wrap',
+    gap: 'var(--ams-space-l)'
+  }}>
+      <ColorSample value="hotpink" />
+      <ColorSample value="" />
+    </div>,
+  tags: ['!dev', '!autodocs']
+}`,...o.parameters?.docs?.source}}},s=[`Test`]}))();export{o as Test,s as __namedExportsOrder,a as default};
--- demo-DES-1768-grid-cell-row-span/assets/Switch.test.stories-B9wFgl68.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-zsgVPwQN.js";import{n as t,t as n}from"./renderComponentVariants-YdiAkILf.js";import{l as r,t as i}from"./src-ow7wFdYJ.js";import a,{n as o}from"./Switch.stories-BzjpVDS-.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Switch`},c={render:e=>t(r,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(Switch, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
