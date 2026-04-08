7a075a2c34867f2efbec1ac094bd79bfe5f0cf3d
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-04-08 22:47:34 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@3be5cfc9df127545579cd91efeec395754812111 🚀

--- demo-DES-1768-grid-cell-row-span/assets/HomePage.docs-BdC44Fsw.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-zsgVPwQN.js";import{At as t}from"./iframe-BbkhmdOd.js";import{r as n}from"./react-BzVM-3bM.js";import{c as r,f as i}from"./blocks-3Rao3QRG.js";import{t as a}from"./mdx-react-shim-98Yb2F6K.js";import{n as o,t as s}from"./HomePage.stories-BOPR5ERd.js";function c(e){let t={h1:`h1`,p:`p`,...n(),...e.components};return(0,u.jsxs)(u.Fragment,{children:[`
-`,`
-`,(0,u.jsx)(r,{of:s}),`
-`,(0,u.jsx)(t.h1,{id:`home-page`,children:`Home page`}),`
-`,(0,u.jsx)(t.p,{children:`The home page of an internal website can have varying types of content.`}),`
-`,(0,u.jsx)(t.p,{children:`Our single example in this category currently focuses on the overall layout.`})]})}function l(e={}){let{wrapper:t}={...n(),...e.components};return t?(0,u.jsx)(t,{...e,children:(0,u.jsx)(c,{...e})}):c(e)}var u;e((()=>{u=t(),a(),i(),o()}))();export{l as default};
--- demo-DES-1768-grid-cell-row-span/assets/mdx-react-shim-98Yb2F6K.js
+++
@@ -1,1 +0,0 @@
-import{n as e}from"./chunk-zsgVPwQN.js";import{t}from"./react-BzVM-3bM.js";var n=e((()=>{t()}));export{n as t};
