6b250f0fb638896b3757e6f16722e3d0e7b31804
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-04-09 10:16:45 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@a7d45bfe877e5ab7626b0819eb748788114cbd00 🚀

---
+++ demo-DES-1769-progress-list-not-collapsible/assets/Label.test.stories-B4lScgoX.js
@@ -0,0 +1,6 @@
+import{n as e}from"./chunk-zsgVPwQN.js";import{n as t,t as n}from"./renderComponentVariants-DexEjb8d.js";import{F as r,t as i}from"./src-D38eS9b6.js";import a,{n as o}from"./Label.stories-CY03JMOE.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Label`},c={render:e=>t(r,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(Label, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
---
+++ demo-DES-1769-progress-list-not-collapsible/assets/TextInput.test.stories-C5UyzZnZ.js
@@ -0,0 +1,7 @@
+import{n as e}from"./chunk-zsgVPwQN.js";import{n as t,t as n}from"./renderComponentVariants-DexEjb8d.js";import{i as r,t as i}from"./src-D38eS9b6.js";import a,{n as o}from"./TextInput.stories-C-m079ga.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Text Input`},c={render:e=>t(r,{args:e,variants:[`disabled`,`hovered`]}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(TextInput, {
+    args,
+    variants: ['disabled', 'hovered']
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
