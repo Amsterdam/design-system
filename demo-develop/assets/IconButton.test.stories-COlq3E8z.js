e29e0580d50ed09f11920b7939bf83b201095bb7
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-04-10 12:16:38 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@294c119f8a17c1492a122916f56605b14cc8ae10 🚀

---
+++ demo-develop/assets/IconButton.test.stories-COlq3E8z.js
@@ -0,0 +1,7 @@
+import{n as e}from"./chunk-zsgVPwQN.js";import{n as t,t as n}from"./renderComponentVariants-C9DDfDSB.js";import{St as r,t as i}from"./src-BzkyzHK6.js";import a,{n as o}from"./IconButton.stories-DHk3AIeR.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Buttons/Icon Button`},c={render:e=>t(r,{args:e,variants:[`hovered`,`disabled`]}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(IconButton, {
+    args,
+    variants: ['hovered', 'disabled']
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
--- demo-develop/assets/Page.test.stories-D5b57Izg.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-zsgVPwQN.js";import{n as t,t as n}from"./renderComponentVariants-D0SKpzsF.js";import{E as r,t as i}from"./src-D6KngmDC.js";import a,{n as o}from"./Page.stories-DHzmhmhH.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Containers/Page`},c={render:e=>t(r,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(Page, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
