b5e483cd5f55b56c3fab40000d4c809fef125032
GitHub <github-action@users.noreply.github.com>
2026-04-10 22:47:25 +0000

    Remove feature branch folder

--- demo-DES-1465-delete-deprecations-for-v4/assets/CharacterCount.test.stories-tedBB88-.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-zsgVPwQN.js";import{n as t,t as n}from"./renderComponentVariants-BdyxOBsS.js";import{t as r,tt as i}from"./src-Bo3lgq0u.js";import a,{n as o}from"./CharacterCount.stories-BKE79Qa2.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/Forms/Character Count`},c={render:e=>t(i,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(CharacterCount, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
