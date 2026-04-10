e587c8bdf2cb7826ccaa6610bc20d17ebd1037aa
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-04-10 23:24:47 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@6a5e4d624011b050bb63c940879ba2bb39be4f83 🚀

--- demo-dtcg-adherence/assets/Checkbox.test.stories-XUF7MZCf.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-zsgVPwQN.js";import{n as t,t as n}from"./renderComponentVariants-C7mTsse8.js";import{et as r,t as i}from"./src-DExqclz6.js";import a,{n as o}from"./Checkbox.stories-ESEcpbIT.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Checkbox`},c={render:e=>t(r,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(Checkbox, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
---
+++ demo-dtcg-adherence/assets/config-Bg3_X8MT.js
@@ -0,0 +1,1 @@
+import{n as e}from"./chunk-zsgVPwQN.js";import{jt as t}from"./iframe-TvqqwLBz.js";import{n,t as r}from"./Layout-DF3JsUoC.js";var i,a,o=e((()=>{n(),i=t(),a={decorators:[e=>(0,i.jsx)(r,{children:(0,i.jsx)(e,{})})],parameters:{layout:`fullscreen`,themes:{themeOverride:`Spacious`}}}}));export{o as n,a as t};
