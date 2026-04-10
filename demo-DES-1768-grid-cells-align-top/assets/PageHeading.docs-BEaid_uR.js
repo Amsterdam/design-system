ec28d5476be35875a67bd146afa5104882178773
GitHub <github-action@users.noreply.github.com>
2026-04-10 12:14:39 +0000

    Remove feature branch folder

--- demo-DES-1768-grid-cells-align-top/assets/PageHeading.docs-BEaid_uR.js
+++
@@ -1,9 +0,0 @@
-import{n as e}from"./chunk-zsgVPwQN.js";import{jt as t}from"./iframe-ItSOh2xP.js";import{r as n}from"./react-B5cSBHHJ.js";import{c as r,f as i,l as a,s as o}from"./blocks-B2ijLszJ.js";import{n as s,t as c}from"./StatusBadge-CJXbnCPA.js";import{t as l}from"./mdx-react-shim-CM9r17t1.js";import{n as u,t as d}from"./PageHeading.stories-Mz8YrGqj.js";var f,p=e((()=>{f=`<!-- @license CC0-1.0 -->
-
-# Page Heading
-`}));function m(e){return(0,g.jsxs)(g.Fragment,{children:[`
-`,`
-`,(0,g.jsx)(c,{description:`We no longer use this size of headings. Use the Heading component with level 1 instead.`,status:`deprecated`}),`
-`,(0,g.jsx)(r,{of:d}),`
-`,(0,g.jsx)(o,{children:f}),`
-`,(0,g.jsx)(a,{})]})}function h(e={}){let{wrapper:t}={...n(),...e.components};return t?(0,g.jsx)(t,{...e,children:(0,g.jsx)(m,{...e})}):m(e)}var g;e((()=>{g=t(),l(),i(),u(),p(),s()}))();export{h as default};
--- demo-DES-1768-grid-cells-align-top/assets/mdx-react-shim-CM9r17t1.js
+++
@@ -1,1 +0,0 @@
-import{n as e}from"./chunk-zsgVPwQN.js";import{t}from"./react-B5cSBHHJ.js";var n=e((()=>{t()}));export{n as t};
