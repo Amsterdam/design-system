20c9403063aa4adf8d3db8e1a14d8f12bd10adf8
Vincent Smedinga <v.smedinga@amsterdam.nl>
2026-04-10 12:32:31 +0200

    chore(docs): Add example of a full-page sortable table (#2516)

---
+++ storybook/src/pages/internal/TablePage/TablePage.stories.tsx
@@ -0,0 +1,68 @@
+/**
+ * @license EUPL-1.2+
+ * Copyright Gemeente Amsterdam
+ */
+
+import type { Meta, StoryObj } from '@storybook/react-vite'
+
+import { Grid, Heading, Label, Row, Select, Table } from '@amsterdam/design-system-react'
+
+import type { SortOrder } from './common'
+
+import { commonMeta } from '../common/config'
+import { AddressTableBody, AddressTableHeaderRow, bagAddresses, sortAddresses, sortOptions } from './common'
+import './table-page.css'
+
+const params = new URLSearchParams(window.location.search)
+
+const meta = {
+  ...commonMeta,
+  title: 'Pages/Internal/Table Page',
+} satisfies Meta
+
+export default meta
+
+export const SortingWithSelect: StoryObj = {
+  render: () => {
+    const sortOrder = (params.get('sort') ?? 'straat-asc') as SortOrder
+    const addresses = sortAddresses(bagAddresses.slice(0, 30), sortOrder)
+
+    return (
+      <Grid paddingBottom="x-large" paddingTop="large">
+        <Grid.Cell className="ams-grid__cell--transparent" span="all">
+          <Heading level={1}>Vergunninghouders 2026/2027</Heading>
+        </Grid.Cell>
+        <Grid.Cell span="all">
+          <form
+            onSubmit={(e) => {
+              e.preventDefault()
+              const url = new URL(window.location.href)
+              url.searchParams.set('sort', new FormData(e.currentTarget).get('sort') as string)
+              window.location.href = url.toString()
+            }}
+          >
+            <Row align="end" alignVertical="center" wrap>
+              <Label htmlFor="sort">Sorteren op</Label>
+              <Select defaultValue={sortOrder} id="sort" name="sort" onChange={(e) => e.target.form?.requestSubmit()}>
+                {sortOptions.map(({ label, value }) => (
+                  <Select.Option key={value} value={value}>
+                    {label}
+                  </Select.Option>
+                ))}
+              </Select>
+            </Row>
+          </form>
+          <Table>
+            <Table.Caption className="ams-mb-m">
+              <Heading level={2}>Gegevens per adres</Heading>
+            </Table.Caption>
+            <Table.Header>
+              <AddressTableHeaderRow />
+            </Table.Header>
+            <AddressTableBody addresses={addresses} />
+          </Table>
+        </Grid.Cell>
+      </Grid>
+    )
+  },
+}
