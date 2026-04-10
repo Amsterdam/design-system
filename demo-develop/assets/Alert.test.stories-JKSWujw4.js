a133825b483e5bb9981b806aecd0777866b15183
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-04-10 11:23:04 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@75bdb6b3a46be08ebce4050d96c140f9b463abad 🚀

--- demo-develop/assets/Alert.test.stories-JKSWujw4.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-zsgVPwQN.js";import{n as t,t as n}from"./renderComponentVariants-DSCzqTrI.js";import{t as r,vt as i}from"./src-ntZMAl-s.js";import a,{n as o}from"./Alert.stories-Nl4D9bLH.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/Feedback/Alert`},c={render:e=>t(i,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(Alert, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
--- demo-develop/assets/PageHeading.docs-DC9Hoxor.js
+++
@@ -1,9 +0,0 @@
-import{n as e}from"./chunk-zsgVPwQN.js";import{jt as t}from"./iframe-C8KTpe67.js";import{r as n}from"./react-BuEwf0si.js";import{c as r,f as i,l as a,s as o}from"./blocks-DQ56QDhc.js";import{n as s,t as c}from"./StatusBadge-Cd7UiN5G.js";import{t as l}from"./mdx-react-shim-OI92o2hm.js";import{n as u,t as d}from"./PageHeading.stories-BuT1V8V1.js";var f,p=e((()=>{f=`<!-- @license CC0-1.0 -->
-
-# Page Heading
-`}));function m(e){return(0,g.jsxs)(g.Fragment,{children:[`
-`,`
-`,(0,g.jsx)(c,{description:`We no longer use this size of headings. Use the Heading component with level 1 instead.`,status:`deprecated`}),`
-`,(0,g.jsx)(r,{of:d}),`
-`,(0,g.jsx)(o,{children:f}),`
-`,(0,g.jsx)(a,{})]})}function h(e={}){let{wrapper:t}={...n(),...e.components};return t?(0,g.jsx)(t,{...e,children:(0,g.jsx)(m,{...e})}):m(e)}var g;e((()=>{g=t(),l(),i(),u(),p(),s()}))();export{h as default};
