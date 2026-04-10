294c119f8a17c1492a122916f56605b14cc8ae10
Vincent Smedinga <v.smedinga@amsterdam.nl>
2026-04-10 14:14:02 +0200

    feat(Grid): Align cells to the top of the grid row (#2535)
    Co-authored-by: Ruben Sibon <r.sibon@amsterdam.nl>

--- storybook/src/pages/internal/HomePage/HomePage.stories.tsx
+++ storybook/src/pages/internal/HomePage/HomePage.stories.tsx
@@ -5,9 +5,13 @@
 
 import type { Meta, StoryObj } from '@storybook/react-vite'
 
-import { Grid, Heading } from '@amsterdam/design-system-react'
+import { Grid, Heading, Paragraph } from '@amsterdam/design-system-react'
 
+import { exampleParagraph } from '../../../_common/exampleContent'
 import { commonMeta } from '../common/config'
+
+const exampleParagraph1 = exampleParagraph()
+const exampleParagraph2 = exampleParagraph()
 
 const meta = {
   ...commonMeta,
@@ -20,8 +24,12 @@ const meta = {
       </Grid.Cell>
       <Grid.Cell span={{ narrow: 4, medium: 5, wide: 8 }} style={{ height: '12rem' }} />
       <Grid.Cell span={{ narrow: 1, medium: 3, wide: 4 }} style={{ height: '10rem' }} />
-      <Grid.Cell span={{ narrow: 3, medium: 3, wide: 3 }} style={{ height: '8rem' }} />
-      <Grid.Cell span={{ narrow: 4, medium: 5, wide: 9 }} style={{ height: '12rem' }} />
+      <Grid.Cell span={{ narrow: 3, medium: 3, wide: 3 }}>
+        <Paragraph>{exampleParagraph1}</Paragraph>
+      </Grid.Cell>
+      <Grid.Cell span={{ narrow: 4, medium: 5, wide: 9 }}>
+        <Paragraph>{exampleParagraph2}</Paragraph>
+      </Grid.Cell>
       <Grid.Cell span={{ narrow: 2, medium: 3, wide: 6 }} style={{ height: '8rem' }} />
       <Grid.Cell span={{ narrow: 2, medium: 3, wide: 6 }} style={{ height: '8rem' }} />
       <Grid.Cell span={{ narrow: 3, medium: 2, wide: 4 }} style={{ height: '6rem' }} />
