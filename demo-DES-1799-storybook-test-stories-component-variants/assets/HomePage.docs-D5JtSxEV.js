cb46b4a195864a3e463ffff6ca52fee0c075580c
GitHub <github-action@users.noreply.github.com>
2026-04-10 12:59:15 +0000

    Remove feature branch folder

--- demo-DES-1799-storybook-test-stories-component-variants/assets/HomePage.docs-D5JtSxEV.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-zsgVPwQN.js";import{jt as t}from"./iframe-D4anOg9W.js";import{r as n}from"./react-DjoD8R6k.js";import{c as r,f as i}from"./blocks-CjHeWRZl.js";import{t as a}from"./mdx-react-shim-Caa9NLJR.js";import{n as o,t as s}from"./HomePage.stories-DhnHbnK0.js";function c(e){let t={h1:`h1`,p:`p`,...n(),...e.components};return(0,u.jsxs)(u.Fragment,{children:[`
-`,`
-`,(0,u.jsx)(r,{of:s}),`
-`,(0,u.jsx)(t.h1,{id:`home-page`,children:`Home page`}),`
-`,(0,u.jsx)(t.p,{children:`The home page of an internal website can have varying types of content.`}),`
-`,(0,u.jsx)(t.p,{children:`Our single example in this category currently focuses on the overall layout.`})]})}function l(e={}){let{wrapper:t}={...n(),...e.components};return t?(0,u.jsx)(t,{...e,children:(0,u.jsx)(c,{...e})}):c(e)}var u;e((()=>{u=t(),a(),i(),o()}))();export{l as default};
--- demo-DES-1799-storybook-test-stories-component-variants/assets/TableOfContents.test.stories-Do-yEQR4.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-zsgVPwQN.js";import{n as t,t as n}from"./renderComponentVariants-CfifT3Oo.js";import{s as r,t as i}from"./src-d6ZkHMx3.js";import a,{n as o}from"./TableOfContents.stories-CGB649Ae.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/TableOfContents`},c={render:e=>t(r,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(TableOfContents, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
