f51b607e325e9a5e31d5e3dea80dda00ddd12efd
GitHub <github-action@users.noreply.github.com>
2026-04-11 22:10:03 +0000

    Remove feature branch folder

--- demo-update-dependencies-2026-04-11/assets/Badge.test.stories-CgTtYkEi.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-zsgVPwQN.js";import{n as t,t as n}from"./renderComponentVariants-0NqgFMdk.js";import{dt as r,t as i}from"./src-Ce1A7bMY.js";import a,{n as o}from"./Badge.stories-CGFxB3Ln.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Feedback/Badge`},c={render:e=>t(r,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(Badge, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
--- demo-update-dependencies-2026-04-11/assets/Pagination.test.stories-BizlEEjB.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-zsgVPwQN.js";import{n as t,t as n}from"./renderComponentVariants-0NqgFMdk.js";import{b as r,t as i}from"./src-Ce1A7bMY.js";import a,{n as o}from"./Pagination.stories-v8OWse0e.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Navigation/Pagination`},c={render:e=>t(r,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(Pagination, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
