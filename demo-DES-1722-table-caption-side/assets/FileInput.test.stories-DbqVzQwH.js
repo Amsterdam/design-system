451c0aaa1c4e966fccf847748792b34bcd9b10ac
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-04-11 20:33:53 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@83ff323b103525db13f2d1f6385985af0f85120a 🚀

--- demo-DES-1722-table-caption-side/assets/FileInput.test.stories-DbqVzQwH.js
+++
@@ -1,7 +0,0 @@
-import{n as e}from"./chunk-BneVvdWh.js";import{n as t,t as n}from"./renderComponentVariants-CTtnL2R2.js";import{W as r,t as i}from"./src-IF4cVlGA.js";import a,{n as o}from"./FileInput.stories-BDJvFTae.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/File Input`},c={render:e=>t(r,{args:e,variants:[`disabled`,`multiple`]}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(FileInput, {
-    args,
-    variants: ['disabled', 'multiple']
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
