9e047546a3454cc45973437e3064ce3c24b6bd4b
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-04-11 10:38:46 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@e0dec1a9ab6a59cbb8567ff76ba45974186d778f 🚀

---
+++ demo-DES-1768-internal-navigation-page/assets/AppIcons.test.stories-Ba9xTCic.js
@@ -0,0 +1,9 @@
+import{n as e}from"./chunk-zsgVPwQN.js";import{Rt as t}from"./iframe-BNZXMPxS.js";import{a as n,i as r,n as i,r as a,t as o}from"./AppIcons-B-wQN5bH.js";var s,c,l,u;e((()=>{n(),s=t(),c={title:`Components/Docs/App Icons`},l={render:()=>(0,s.jsxs)(s.Fragment,{children:[(0,s.jsx)(o,{}),(0,s.jsx)(i,{}),(0,s.jsx)(a,{}),(0,s.jsx)(r,{})]}),tags:[`!dev`,`!autodocs`]},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  render: () => <>
+      <AppleTouchIcon />
+      <Favicon />
+      <SvgIcon />
+      <WebAppIcons />
+    </>,
+  tags: ['!dev', '!autodocs']
+}`,...l.parameters?.docs?.source}}},u=[`Test`]}))();export{l as Test,u as __namedExportsOrder,c as default};
---
+++ demo-DES-1768-internal-navigation-page/assets/FileList.test.stories-B5-bv3LG.js
@@ -0,0 +1,11 @@
+import{n as e}from"./chunk-zsgVPwQN.js";import{Rt as t}from"./iframe-BNZXMPxS.js";import{n,t as r}from"./renderComponentVariants-BIzfsDKG.js";import{U as i,t as a}from"./src-BzDhucBq.js";import o,{n as s}from"./FileList.stories-Dm4H4zPU.js";var c,l,u,d;e((()=>{a(),r(),s(),c=t(),l={...o,title:`Components/Forms/File List`},u={args:{children:(0,c.jsx)(i.Item,{file:new File([`sample1`],`sample1.txt`,{type:`text/plain`})})},render:e=>n(i,{args:e}),tags:[`!dev`,`!autodocs`]},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: <FileList.Item file={new File(['sample1'], 'sample1.txt', {
+      type: 'text/plain'
+    })} />
+  },
+  render: args => renderComponentVariants(FileList, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...u.parameters?.docs?.source}}},d=[`Test`]}))();export{u as Test,d as __namedExportsOrder,l as default};
