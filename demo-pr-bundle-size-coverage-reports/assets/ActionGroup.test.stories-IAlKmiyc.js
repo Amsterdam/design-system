86f6832d0ec802a0159c0a946cba00f75c0ca651
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-04-13 15:55:25 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@22dc0cbbe183607046266dfd575579c73d00b40c 🚀

---
+++ demo-pr-bundle-size-coverage-reports/assets/ActionGroup.test.stories-IAlKmiyc.js
@@ -0,0 +1,9 @@
+import{n as e}from"./chunk-zsgVPwQN.js";import{t}from"./jsx-runtime-GmyBfqKJ.js";import{n,t as r}from"./renderComponentVariants-CVRu1qnN.js";import{St as i,t as a}from"./src-DQ2K8n2Q.js";import o,{n as s}from"./ActionGroup.stories-AAjuqzap.js";var c,l,u,d;e((()=>{a(),r(),s(),c=t(),l={...o,title:`Components/Layout/Action Group`},u={args:{children:[(0,c.jsx)(`button`,{children:`Stoppen`},1),(0,c.jsx)(`button`,{children:`Verder`},2)]},render:e=>n(i,{args:e}),tags:[`!dev`,`!autodocs`]},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<button key={1}>Stoppen</button>, <button key={2}>Verder</button>]
+  },
+  render: args => renderComponentVariants(ActionGroup, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...u.parameters?.docs?.source}}},d=[`Test`]}))();export{u as Test,d as __namedExportsOrder,l as default};
