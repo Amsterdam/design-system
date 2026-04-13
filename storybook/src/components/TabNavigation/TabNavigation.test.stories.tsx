0995bdd87d0ea60f2990fbad26291a5e86527d3c
Vincent Smedinga <v.smedinga@amsterdam.nl>
2026-04-13 13:53:55 +0200

    Fix visual regression test story

--- storybook/src/components/TabNavigation/TabNavigation.test.stories.tsx
+++ storybook/src/components/TabNavigation/TabNavigation.test.stories.tsx
@@ -5,6 +5,7 @@
 
 import type { Meta, StoryObj } from '@storybook/react-vite'
 
+import { DocumentsIcon } from '@amsterdam/design-system-react-icons'
 import { TabNavigation } from '@amsterdam/design-system-react/src'
 
 import { default as tabNavigationMeta } from './TabNavigation.stories'
@@ -20,36 +21,36 @@ type Story = StoryObj<typeof meta>
 
 export const Test: Story = {
   render: () => (
-    <TabNavigation>
-      <TabNavigation.List>
-        <TabNavigation.Link aria-current="page" href="#">
-          Gegevens
-        </TabNavigation.Link>
-        <TabNavigation.Link href="#">Aanslagen</TabNavigation.Link>
-        <TabNavigation.Link href="#">Documenten</TabNavigation.Link>
-        <TabNavigation.Link className="hover" href="#">
-          Acties
-        </TabNavigation.Link>
-      </TabNavigation.List>
-    </TabNavigation>
-  ),
-  tags: ['!dev', '!autodocs'],
-}
-
-export const TestVertical: Story = {
-  render: () => (
-    <TabNavigation orientation="vertical">
-      <TabNavigation.List>
-        <TabNavigation.Link aria-current="page" href="#">
-          Gegevens
-        </TabNavigation.Link>
-        <TabNavigation.Link href="#">Aanslagen</TabNavigation.Link>
-        <TabNavigation.Link href="#">Documenten</TabNavigation.Link>
-        <TabNavigation.Link className="hover" href="#">
-          Acties
-        </TabNavigation.Link>
-      </TabNavigation.List>
-    </TabNavigation>
+    <div className="_ams-tests-grid">
+      <TabNavigation>
+        <TabNavigation.List>
+          <TabNavigation.Link aria-current="page" href="#">
+            Gegevens
+          </TabNavigation.Link>
+          <TabNavigation.Link href="#">Aanslagen</TabNavigation.Link>
+          <TabNavigation.Link href="#" icon={DocumentsIcon}>
+            Documenten
+          </TabNavigation.Link>
+          <TabNavigation.Link className="hover" href="#">
+            Acties
+          </TabNavigation.Link>
+        </TabNavigation.List>
+      </TabNavigation>
+      <TabNavigation orientation="vertical">
+        <TabNavigation.List>
+          <TabNavigation.Link aria-current="page" href="#">
+            Gegevens
+          </TabNavigation.Link>
+          <TabNavigation.Link href="#">Aanslagen</TabNavigation.Link>
+          <TabNavigation.Link href="#" icon={DocumentsIcon}>
+            Documenten
+          </TabNavigation.Link>
+          <TabNavigation.Link className="hover" href="#">
+            Acties
+          </TabNavigation.Link>
+        </TabNavigation.List>
+      </TabNavigation>
+    </div>
   ),
   tags: ['!dev', '!autodocs'],
 }
