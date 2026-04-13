8a0f722dd4b1d2c8e87e7b4f139da19c4dff523b
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-04-13 09:21:01 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@130bfa7323324bd6bc3ddc342da84fe2feb921f6 🚀

---
+++ demo-dtcg-adherence/assets/Row.test.stories-DtzHP5Zz.js
@@ -0,0 +1,9 @@
+import{n as e}from"./chunk-zsgVPwQN.js";import{jt as t}from"./iframe-CKz_uebh.js";import{n,t as r}from"./renderComponentVariants-DWFpYXbq.js";import{t as i,vt as a}from"./src-BQMlbIZ0.js";import o,{n as s}from"./Row.stories-Ch0MSj97.js";var c,l,u,d;e((()=>{i(),r(),s(),c=t(),l={...o,title:`Components/Layout/Row`},u={args:{children:[(0,c.jsx)(`div`,{className:`_ams-item`},1),(0,c.jsx)(`div`,{className:`_ams-item`},2),(0,c.jsx)(`div`,{className:`_ams-item`},3)]},render:e=>n(a,{args:e}),tags:[`!dev`,`!autodocs`]},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<div className="_ams-item" key={1} />, <div className="_ams-item" key={2} />, <div className="_ams-item" key={3} />]
+  },
+  render: args => renderComponentVariants(Row, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...u.parameters?.docs?.source}}},d=[`Test`]}))();export{u as Test,d as __namedExportsOrder,l as default};
--- demo-dtcg-adherence/assets/mdx-react-shim-BDe_ELLW.js
+++
@@ -1,1 +0,0 @@
-import{n as e}from"./chunk-zsgVPwQN.js";import{t}from"./react-uRVz2e_7.js";var n=e((()=>{t()}));export{n as t};
