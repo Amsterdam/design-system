9840acd338ae07260e7f062503393f56e163711b
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-04-14 15:48:55 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@d6e53fc4a3a5d20ec225d9714eabe238bef7f3d6 🚀

---
+++ demo-DES-1769-progress-list-not-collapsible/assets/IconButton.test.stories-DqwxSDXP.js
@@ -0,0 +1,7 @@
+import{n as e}from"./chunk-zsgVPwQN.js";import{n as t,t as n}from"./renderComponentVariants-CVRu1qnN.js";import{Ct as r,t as i}from"./src-CpFaIplw.js";import a,{n as o}from"./IconButton.stories-D-w8i8Vk.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Buttons/Icon Button`},c={render:e=>t(r,{args:e,variants:[`hovered`,`disabled`]}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(IconButton, {
+    args,
+    variants: ['hovered', 'disabled']
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
---
+++ demo-DES-1769-progress-list-not-collapsible/assets/Page.test.stories-D_vn_5R6.js
@@ -0,0 +1,6 @@
+import{n as e}from"./chunk-zsgVPwQN.js";import{n as t,t as n}from"./renderComponentVariants-CVRu1qnN.js";import{D as r,t as i}from"./src-CpFaIplw.js";import a,{n as o}from"./Page.stories-CSR8JnzV.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Containers/Page`},c={render:e=>t(r,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(Page, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
