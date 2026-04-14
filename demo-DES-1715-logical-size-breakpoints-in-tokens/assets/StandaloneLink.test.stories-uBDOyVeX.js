9f209fcd5905e7f01aa8ffc97f5d5b96a617dda5
RubenSibon <mail+github@rubensibon.nl>
2026-04-14 13:26:28 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@13272b337932cacb5a8cbd16becf965da8b81d81 🚀

---
+++ demo-DES-1715-logical-size-breakpoints-in-tokens/assets/StandaloneLink.test.stories-uBDOyVeX.js
@@ -0,0 +1,6 @@
+import{n as e}from"./chunk-zsgVPwQN.js";import{n as t,t as n}from"./renderComponentVariants-CVRu1qnN.js";import{t as r,u as i}from"./src-DmReZG6B.js";import a,{n as o}from"./StandaloneLink.stories-CklU7_9C.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/StandaloneLink`},c={render:e=>t(i,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(StandaloneLink, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
