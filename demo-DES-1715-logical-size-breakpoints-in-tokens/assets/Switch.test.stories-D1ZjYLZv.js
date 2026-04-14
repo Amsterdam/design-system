730382c0fb5c15935c2776a351f7eebc31a5c136
RubenSibon <mail+github@rubensibon.nl>
2026-04-14 13:06:24 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@a29590dadc2816b247a9ad2e689d4bd921b7875d 🚀

--- demo-DES-1715-logical-size-breakpoints-in-tokens/assets/Switch.test.stories-D1ZjYLZv.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-zsgVPwQN.js";import{n as t,t as n}from"./renderComponentVariants-CVRu1qnN.js";import{l as r,t as i}from"./src-DVAS6rfi.js";import a,{n as o}from"./Switch.stories-DKTjm-7s.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Switch`},c={render:e=>t(r,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(Switch, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
