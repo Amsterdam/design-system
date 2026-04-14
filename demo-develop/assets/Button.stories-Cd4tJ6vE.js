eb254de719b0539ff02d770ffc0f59138ef134f4
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-04-14 16:39:02 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@90c19da8f309eedb926bbd3f65f52990b880b661 🚀

---
+++ demo-develop/assets/Button.stories-Cd4tJ6vE.js
@@ -0,0 +1,38 @@
+import{n as e,r as t}from"./chunk-zsgVPwQN.js";import{t as n}from"./jsx-runtime-GmyBfqKJ.js";import{F as r,I as i,s as a}from"./index.esm-6Mm6hKCe.js";import{st as o,t as s}from"./src-9lT802ar.js";var c=t({Primary:()=>d,Secondary:()=>f,Tertiary:()=>p,TextWrapping:()=>_,WithIcon:()=>m,WithIconBefore:()=>h,WithIconOnly:()=>g,__namedExportsOrder:()=>v,default:()=>u}),l,u,d,f,p,m,h,g,_,v,y=e((()=>{i(),s(),l=n(),u={title:`Components/Buttons/Button`,component:o,args:{children:`Versturen`,disabled:!1,icon:void 0,iconBefore:!1,iconOnly:void 0,variant:`primary`},argTypes:{disabled:{description:`Prevents interaction. Avoid if possible.`},icon:{control:{labels:{undefined:`none`},type:`select`},mapping:r,options:[void 0,...Object.keys(r)]},iconBefore:{control:{type:`boolean`},if:{arg:`icon`}},iconOnly:{control:{type:`boolean`},if:{arg:`icon`}}}},d={},f={args:{children:`Annuleren`,variant:`secondary`}},p={args:{children:`Terug`,variant:`tertiary`}},m={args:{children:`Sluiten`,icon:(0,l.jsx)(a,{})}},h={args:{children:`Sluiten`,icon:(0,l.jsx)(a,{}),iconBefore:!0}},g={args:{children:`Sluiten`,icon:(0,l.jsx)(a,{}),iconOnly:!0,variant:`tertiary`}},_={args:{children:`Vergunningsaanvraag verzenden`},decorators:[e=>(0,l.jsx)(`div`,{style:{maxWidth:`16rem`},children:(0,l.jsx)(e,{})})]},d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{}`,...d.parameters?.docs?.source}}},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Annuleren',
+    variant: 'secondary'
+  }
+}`,...f.parameters?.docs?.source}}},p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Terug',
+    variant: 'tertiary'
+  }
+}`,...p.parameters?.docs?.source}}},m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Sluiten',
+    icon: <CloseIcon />
+  }
+}`,...m.parameters?.docs?.source}}},h.parameters={...h.parameters,docs:{...h.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Sluiten',
+    icon: <CloseIcon />,
+    iconBefore: true
+  }
+}`,...h.parameters?.docs?.source}}},g.parameters={...g.parameters,docs:{...g.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Sluiten',
+    icon: <CloseIcon />,
+    iconOnly: true,
+    variant: 'tertiary'
+  }
+}`,...g.parameters?.docs?.source}}},_.parameters={..._.parameters,docs:{..._.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Vergunningsaanvraag verzenden'
+  },
+  decorators: [Story => <div style={{
+    maxWidth: '16rem'
+  }}>
+        <Story />
+      </div>]
+}`,..._.parameters?.docs?.source}}},v=[`Primary`,`Secondary`,`Tertiary`,`WithIcon`,`WithIconBefore`,`WithIconOnly`,`TextWrapping`]}));y();export{d as Primary,f as Secondary,p as Tertiary,_ as TextWrapping,m as WithIcon,h as WithIconBefore,g as WithIconOnly,v as __namedExportsOrder,u as default,y as n,c as t};
