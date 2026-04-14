a82d3083aa1bcbffe1b2ab08609f32c08bb085ec
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-04-14 11:23:10 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@875a96351d2c910fa2d5596baef8a622dd27912e 🚀

---
+++ demo-pr-bundle-size-coverage-reports/assets/Alert.test.stories-SioAH4jI.js
@@ -0,0 +1,6 @@
+import{n as e}from"./chunk-zsgVPwQN.js";import{n as t,t as n}from"./renderComponentVariants-CVRu1qnN.js";import{t as r,yt as i}from"./src-BF9jBlXB.js";import a,{n as o}from"./Alert.stories-CJ2NAXqf.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/Feedback/Alert`},c={render:e=>t(i,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(Alert, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
--- demo-pr-bundle-size-coverage-reports/assets/ErrorMessage.test.stories-jqPdBiCD.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-zsgVPwQN.js";import{n as t,t as n}from"./renderComponentVariants-CVRu1qnN.js";import{J as r,t as i}from"./src-DdO5ME9D.js";import a,{n as o}from"./ErrorMessage.stories-BZKXPJuD.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Error Message`},c={render:e=>t(r,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(ErrorMessage, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
