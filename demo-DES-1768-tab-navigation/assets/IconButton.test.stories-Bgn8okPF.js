dc4fe419a8a13ae4f0f7dd9934098ad72299e4df
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-04-15 05:44:41 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@1cd491077c780518525b3f96d7d36786c3716990 🚀

---
+++ demo-DES-1768-tab-navigation/assets/IconButton.test.stories-Bgn8okPF.js
@@ -0,0 +1,7 @@
+import{n as e}from"./chunk-zsgVPwQN.js";import{n as t,t as n}from"./renderComponentVariants-C3s3FsoC.js";import{St as r,t as i}from"./src-BKViyX7G.js";import a,{n as o}from"./IconButton.stories-Cr-VHHXO.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Buttons/Icon Button`},c={render:e=>t(r,{args:e,variants:[`hovered`,`disabled`]}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(IconButton, {
+    args,
+    variants: ['hovered', 'disabled']
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
---
+++ demo-DES-1768-tab-navigation/assets/TableOfContents.test.stories-BQvEbIbl.js
@@ -0,0 +1,6 @@
+import{n as e}from"./chunk-zsgVPwQN.js";import{n as t,t as n}from"./renderComponentVariants-C3s3FsoC.js";import{c as r,t as i}from"./src-BKViyX7G.js";import a,{n as o}from"./TableOfContents.stories-CvfDPV-N.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/TableOfContents`},c={render:e=>t(r,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(TableOfContents, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
