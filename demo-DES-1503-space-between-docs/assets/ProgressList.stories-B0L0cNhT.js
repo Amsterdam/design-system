db22465b1528b9af949eaee043be69fa721ee226
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-04-15 13:37:13 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@973d68a349400c5629666bd85aacdc915461196d 🚀

---
+++ demo-DES-1503-space-between-docs/assets/ProgressList.stories-B0L0cNhT.js
@@ -0,0 +1,28 @@
+import{n as e,r as t}from"./chunk-zsgVPwQN.js";import{t as n}from"./jsx-runtime-GmyBfqKJ.js";import{F as r,J as i,q as a}from"./index.esm-BnpK1b4i.js";import{t as o,v as s}from"./src-DIOLMxjD.js";import{l as c,m as l,p as u}from"./exampleContent-up3wPRrW.js";var d=t({Default:()=>g,WithSubsteps:()=>_,__namedExportsOrder:()=>v,default:()=>h}),f,p,m,h,g,_,v,y=e((()=>{i(),o(),l(),f=n(),p=Array.from({length:6},()=>c()),m=u().slice(0,2).map(e=>(0,f.jsx)(a.Item,{children:e},e)),h={title:`Components/Containers/Progress List`,component:s,args:{children:[(0,f.jsx)(s.Step,{heading:`2025`,status:`completed`,children:(0,f.jsx)(r,{children:p[0]})},0),(0,f.jsxs)(s.Step,{heading:`2026`,status:`current`,children:[(0,f.jsx)(r,{className:`ams-mb-m`,children:p[1]}),(0,f.jsx)(a,{children:m})]},1),(0,f.jsx)(s.Step,{heading:`2027`,children:(0,f.jsx)(r,{children:p[2]})},2)],headingLevel:3}},g={},_={args:{children:[(0,f.jsx)(s.Step,{heading:`2026`,status:`completed`,children:(0,f.jsx)(r,{children:p[0]})},1),(0,f.jsxs)(s.Step,{hasSubsteps:!0,heading:`2027`,status:`current`,children:[(0,f.jsx)(r,{children:p[1]}),(0,f.jsxs)(s.Substeps,{children:[(0,f.jsx)(s.Substep,{status:`completed`,children:(0,f.jsx)(r,{children:`Februari: De Clercqstraat en Jan Evertsenstraat`})}),(0,f.jsx)(s.Substep,{status:`completed`,children:(0,f.jsx)(r,{children:`April: Kruispunt Admiraal de Ruijterweg-Jan Evertsenstraat`})}),(0,f.jsx)(s.Substep,{status:`current`,children:(0,f.jsx)(r,{children:`Juni: Raadhuisstraat, Westermarkt en Rozengracht`})}),(0,f.jsx)(s.Substep,{children:(0,f.jsx)(r,{children:`Augustus: Kruispunt Marnixstraat-Rozengracht`})}),(0,f.jsx)(s.Substep,{children:(0,f.jsx)(r,{children:`Oktober: Nieuwezijds Voorburgwal`})})]})]},2),(0,f.jsx)(s.Step,{heading:`2028`,children:(0,f.jsx)(r,{children:p[2]})},3)]}},g.parameters={...g.parameters,docs:{...g.parameters?.docs,source:{originalSource:`{}`,...g.parameters?.docs?.source}}},_.parameters={..._.parameters,docs:{..._.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<ProgressList.Step heading="2026" key={1} status="completed">
+        <Paragraph>{paragraphs[0]}</Paragraph>
+      </ProgressList.Step>, <ProgressList.Step hasSubsteps heading="2027" key={2} status="current">
+        <Paragraph>{paragraphs[1]}</Paragraph>
+        <ProgressList.Substeps>
+          <ProgressList.Substep status="completed">
+            <Paragraph>Februari: De Clercqstraat en Jan Evertsenstraat</Paragraph>
+          </ProgressList.Substep>
+          <ProgressList.Substep status="completed">
+            <Paragraph>April: Kruispunt Admiraal de Ruijterweg-Jan Evertsenstraat</Paragraph>
+          </ProgressList.Substep>
+          <ProgressList.Substep status="current">
+            <Paragraph>Juni: Raadhuisstraat, Westermarkt en Rozengracht</Paragraph>
+          </ProgressList.Substep>
+          <ProgressList.Substep>
+            <Paragraph>Augustus: Kruispunt Marnixstraat-Rozengracht</Paragraph>
+          </ProgressList.Substep>
+          <ProgressList.Substep>
+            <Paragraph>Oktober: Nieuwezijds Voorburgwal</Paragraph>
+          </ProgressList.Substep>
+        </ProgressList.Substeps>
+      </ProgressList.Step>, <ProgressList.Step heading="2028" key={3}>
+        <Paragraph>{paragraphs[2]}</Paragraph>
+      </ProgressList.Step>]
+  }
+}`,..._.parameters?.docs?.source}}},v=[`Default`,`WithSubsteps`]}));y();export{g as Default,_ as WithSubsteps,v as __namedExportsOrder,h as default,y as n,d as t};
