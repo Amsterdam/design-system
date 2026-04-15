875769fdd0ae17f322a169dc28cf814fa20bcf57
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-04-15 22:14:47 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@5a519db5da13e0c0e0e0f77ce0cf4caca66599fa 🚀

---
+++ demo-DES-1503-space-between-docs/assets/Field.test.stories-BTsBy5c0.js
@@ -0,0 +1,13 @@
+import{n as e}from"./chunk-zsgVPwQN.js";import{t}from"./jsx-runtime-GmyBfqKJ.js";import{E as n,J as r}from"./index.esm-BnpK1b4i.js";import{n as i,t as a}from"./renderComponentVariants-CVRu1qnN.js";import{K as o,t as s}from"./src-1lrG7JSl.js";import c,{n as l}from"./Field.stories-BLQ00JMV.js";var u,d,f,p;e((()=>{r(),s(),a(),l(),u=t(),d={...c,title:`Components/Forms/Field`},f={args:{children:[(0,u.jsx)(n,{htmlFor:`input-a1`,children:`Wat is uw achternaam?`},1),(0,u.jsx)(`input`,{id:`input-a1`,value:`Dijkstra`},2)]},render:e=>i(o,{args:e}),tags:[`!dev`,`!autodocs`]},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [
+    // We use Label because \`<label>\` is inline and wouldn’t get the margin we want to test.
+    <Label htmlFor="input-a1" key={1}>
+        Wat is uw achternaam?
+      </Label>, <input id="input-a1" key={2} value="Dijkstra" />]
+  },
+  render: args => renderComponentVariants(Field, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...f.parameters?.docs?.source}}},p=[`Test`]}))();export{f as Test,p as __namedExportsOrder,d as default};
--- demo-DES-1503-space-between-docs/assets/Link.test.stories-C7WyR9mh.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-zsgVPwQN.js";import{n as t,t as n}from"./renderComponentVariants-CVRu1qnN.js";import{N as r,t as i}from"./src-F5PbltyP.js";import a,{n as o}from"./Link.stories-DK183vii.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Navigation/Link`},c={render:e=>t(r,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(Link, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
