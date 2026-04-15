1fb62712a71380f58d4551b6383cc9a2dee1ad76
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-04-15 13:46:35 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@3cf04c3581e28240dec5ae55a7bc2e782422f57b 🚀

---
+++ demo-DES-1503-space-between-docs/assets/Button.test.stories-EPj9CkcE.js
@@ -0,0 +1,7 @@
+import{n as e}from"./chunk-zsgVPwQN.js";import{n as t,t as n}from"./renderComponentVariants-CVRu1qnN.js";import{st as r,t as i}from"./src-DjMx7yGl.js";import a,{n as o}from"./Button.stories-CEe6_tUe.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Buttons/Button`},c={render:e=>t(r,{args:e,variants:[`disabled`,`hovered`]}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(Button, {
+    args,
+    variants: ['disabled', 'hovered']
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
--- demo-DES-1503-space-between-docs/assets/TableOfContents.test.stories-CxCNdMff.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-zsgVPwQN.js";import{n as t,t as n}from"./renderComponentVariants-CVRu1qnN.js";import{s as r,t as i}from"./src-DIOLMxjD.js";import a,{n as o}from"./TableOfContents.stories-BaS_6X9U.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/TableOfContents`},c={render:e=>t(r,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(TableOfContents, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
