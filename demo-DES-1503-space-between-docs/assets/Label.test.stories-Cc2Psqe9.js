aff3afa07c14e1e2e2ba52c956d4ae0ec728b2ef
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-04-15 14:20:07 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@589bd76a6acff2f56ed520ba2c59aea2729da091 🚀

---
+++ demo-DES-1503-space-between-docs/assets/Label.test.stories-Cc2Psqe9.js
@@ -0,0 +1,6 @@
+import{n as e}from"./chunk-zsgVPwQN.js";import{n as t,t as n}from"./renderComponentVariants-CVRu1qnN.js";import{P as r,t as i}from"./src-Dn87C8cL.js";import a,{n as o}from"./Label.stories-Dxgp-7H_.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Label`},c={render:e=>t(r,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(Label, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
