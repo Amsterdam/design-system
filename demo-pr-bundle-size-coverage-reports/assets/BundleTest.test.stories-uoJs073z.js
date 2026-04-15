c909fa1e6e5dd828a6f807ec588e7beb0281fad5
GitHub <github-action@users.noreply.github.com>
2026-04-15 08:07:10 +0000

    Remove feature branch folder

--- demo-pr-bundle-size-coverage-reports/assets/BundleTest.test.stories-uoJs073z.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-zsgVPwQN.js";import{n as t,t as n}from"./renderComponentVariants-CVRu1qnN.js";import{ct as r,t as i}from"./src-FMGcQucb.js";import a,{n as o}from"./BundleTest.stories-D73dBc7D.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Bundle-Test`},c={render:e=>t(r,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(BundleTest, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
