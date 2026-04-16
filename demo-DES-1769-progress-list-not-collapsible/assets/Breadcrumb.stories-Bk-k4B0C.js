b789d0fc698c171d11e34acbee9d9bac32dbdbe9
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-04-16 07:55:53 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@75d6b808ea7762e1e341dab67b782a3aea1ec6c3 🚀

---
+++ demo-DES-1769-progress-list-not-collapsible/assets/Breadcrumb.stories-Bk-k4B0C.js
@@ -0,0 +1,11 @@
+import{n as e,r as t}from"./chunk-zsgVPwQN.js";import{t as n}from"./jsx-runtime-GmyBfqKJ.js";import{dt as r,t as i}from"./src-C7cJ6hLb.js";var a=t({Default:()=>c,__namedExportsOrder:()=>l,default:()=>s}),o,s,c,l,u=e((()=>{i(),o=n(),s={title:`Components/Navigation/Breadcrumb`,component:r},c={args:{children:[(0,o.jsx)(r.Link,{href:`#`,children:`Afval`},1),(0,o.jsx)(r.Link,{href:`#`,children:`Bedrijfsafval`},2),(0,o.jsx)(r.Link,{href:`#`,children:`Recyclepunten voor bedrijven`},3)]}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<Breadcrumb.Link href="#" key={1}>
+        Afval
+      </Breadcrumb.Link>, <Breadcrumb.Link href="#" key={2}>
+        Bedrijfsafval
+      </Breadcrumb.Link>, <Breadcrumb.Link href="#" key={3}>
+        Recyclepunten voor bedrijven
+      </Breadcrumb.Link>]
+  }
+}`,...c.parameters?.docs?.source}}},l=[`Default`]}));u();export{c as Default,l as __namedExportsOrder,s as default,u as n,a as t};
