7c9132380f0ddd703e721cbe2fba5b6f7c3ed132
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-04-16 15:12:16 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@2c8042487f43e61a2ec60be4f5301379bfb01246 🚀

---
+++ demo-DES-1769-progress-list-not-collapsible/assets/Avatar.test.stories-BzBjn5Hm.js
@@ -0,0 +1,6 @@
+import{n as e}from"./chunk-zsgVPwQN.js";import{n as t,t as n}from"./renderComponentVariants-CVRu1qnN.js";import{gt as r,t as i}from"./src-CC5Y1U-A.js";import a,{n as o}from"./Avatar.stories-DDLquvGb.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Feedback/Avatar`},c={render:e=>t(r,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(Avatar, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
