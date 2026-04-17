e9fbc33b21a62d582fc09177e2323cff9cebec24
GitHub <github-action@users.noreply.github.com>
2026-04-17 08:49:42 +0000

    Remove feature branch folder

--- demo-DES-1769-progress-list-not-collapsible/assets/GridColumnsGuide-ClX8Ycx_.js
+++
@@ -1,1 +0,0 @@
-import{n as e}from"./chunk-zsgVPwQN.js";import{t}from"./jsx-runtime-GmyBfqKJ.js";import{U as n,t as r}from"./src-Bswbus1B.js";var i=e((()=>{})),a,o,s=e((()=>{i(),r(),a=t(),o=()=>(0,a.jsx)(n,{className:`_ams-grid-columns-guide`,children:Array.from(Array(12).keys()).map(e=>(0,a.jsx)(n.Cell,{className:`_ams-grid-columns-guide__cell`},e))})}));export{s as n,o as t};
