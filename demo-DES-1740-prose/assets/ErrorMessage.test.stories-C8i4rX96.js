f941866915fd13560686acb762eb9b526e8c7839
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-04-17 15:08:25 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@143cc351a8ae360875c0edbd693f6283dce3db20 🚀

---
+++ demo-DES-1740-prose/assets/ErrorMessage.test.stories-C8i4rX96.js
@@ -0,0 +1,6 @@
+import{n as e}from"./chunk-zsgVPwQN.js";import{n as t,t as n}from"./renderComponentVariants-C3s3FsoC.js";import{X as r,t as i}from"./src-t2umz2AW.js";import a,{n as o}from"./ErrorMessage.stories-DQe7W5G6.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Error Message`},c={render:e=>t(r,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(ErrorMessage, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
