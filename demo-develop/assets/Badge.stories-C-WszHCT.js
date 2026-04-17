b88f8eeeaaa61c088a76a9c8b899e6ab3bf7e4ce
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-04-17 11:45:16 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@1d6e0a32e856775e6d42879c71902d0aa5ddace8 🚀

---
+++ demo-develop/assets/Badge.stories-C-WszHCT.js
@@ -0,0 +1,7 @@
+import{n as e,r as t}from"./chunk-zsgVPwQN.js";import{R as n,b as r,z as i}from"./index.esm-B_OH5ZB5.js";import{gt as a,ht as o,mt as s,t as c}from"./src-bQ4fxWeZ.js";var l=t({Default:()=>d,WithIcon:()=>f,__namedExportsOrder:()=>p,default:()=>u}),u,d,f,p,m=e((()=>{i(),c(),a(),u={title:`Components/Feedback/Badge`,component:s,args:{label:`Tip`},argTypes:{color:{control:{labels:{undefined:`green (default)`}},options:[void 0,...o]},icon:{control:{labels:{undefined:`none`},type:`select`},mapping:n,options:[void 0,...Object.keys(n)]}}},d={},f={args:{color:`red`,icon:r,label:`Beveiligd`}},d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{}`,...d.parameters?.docs?.source}}},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
+  args: {
+    color: 'red',
+    icon: Icons.LockClosedFillIcon,
+    label: 'Beveiligd'
+  }
+}`,...f.parameters?.docs?.source}}},p=[`Default`,`WithIcon`]}));m();export{d as Default,f as WithIcon,p as __namedExportsOrder,u as default,m as n,l as t};
---
+++ demo-develop/assets/Pagination.test.stories-BThO-Uok.js
@@ -0,0 +1,6 @@
+import{n as e}from"./chunk-zsgVPwQN.js";import{n as t,t as n}from"./renderComponentVariants-C3s3FsoC.js";import{C as r,t as i}from"./src-bQ4fxWeZ.js";import a,{n as o}from"./Pagination.stories-BPVCeUu8.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Navigation/Pagination`},c={render:e=>t(r,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(Pagination, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
