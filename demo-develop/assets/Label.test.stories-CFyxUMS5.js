71a426febd41ec77797473642272077acd34fcca
RubenSibon <mail+github@rubensibon.nl>
2026-04-17 08:51:26 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@9b4f4899881ee2bcfa46275d34194d3470d00446 🚀

--- demo-develop/assets/Label.test.stories-CFyxUMS5.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-zsgVPwQN.js";import{n as t,t as n}from"./renderComponentVariants-CVRu1qnN.js";import{P as r,t as i}from"./src-BbeoD9rm.js";import a,{n as o}from"./Label.stories-1VqPLc1S.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Label`},c={render:e=>t(r,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(Label, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
