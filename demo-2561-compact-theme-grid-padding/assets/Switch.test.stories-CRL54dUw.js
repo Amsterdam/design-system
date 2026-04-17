8885c1caa32a7759702972e76d1f58ed7258f77f
dlnr <niels@dlnr.dev>
2026-04-17 12:00:46 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@e2c803d529a847926591def58c6146191883320b 🚀

---
+++ demo-2561-compact-theme-grid-padding/assets/Switch.test.stories-CRL54dUw.js
@@ -0,0 +1,6 @@
+import{n as e}from"./chunk-zsgVPwQN.js";import{n as t,t as n}from"./renderComponentVariants-CVRu1qnN.js";import{l as r,t as i}from"./src-fq-0_b3n.js";import a,{n as o}from"./Switch.stories-CTOwf3vB.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Switch`},c={render:e=>t(r,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(Switch, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
