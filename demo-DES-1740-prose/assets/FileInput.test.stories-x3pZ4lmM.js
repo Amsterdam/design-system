36908acc6bb92852482e856a5f650c6561ef20f9
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-04-17 15:39:38 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@8b2b93ab031135e87af9c86ace93e51f0cdccf17 🚀

---
+++ demo-DES-1740-prose/assets/FileInput.test.stories-x3pZ4lmM.js
@@ -0,0 +1,7 @@
+import{n as e}from"./chunk-zsgVPwQN.js";import{n as t,t as n}from"./renderComponentVariants-C3s3FsoC.js";import{K as r,t as i}from"./src-8v-9AdS6.js";import a,{n as o}from"./FileInput.stories-D090k-7V.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/File Input`},c={render:e=>t(r,{args:e,variants:[`disabled`,`multiple`]}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(FileInput, {
+    args,
+    variants: ['disabled', 'multiple']
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
---
+++ demo-DES-1740-prose/assets/Switch.test.stories-D2N-YuK4.js
@@ -0,0 +1,6 @@
+import{n as e}from"./chunk-zsgVPwQN.js";import{n as t,t as n}from"./renderComponentVariants-C3s3FsoC.js";import{t as r,u as i}from"./src-8v-9AdS6.js";import a,{n as o}from"./Switch.stories-DFH8cqM8.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/Forms/Switch`},c={render:e=>t(i,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(Switch, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
