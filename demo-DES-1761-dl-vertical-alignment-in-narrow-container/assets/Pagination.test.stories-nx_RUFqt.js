57e5bcd14bdb9163d5221dc91b90d49b97061da7
RubenSibon <mail+github@rubensibon.nl>
2026-04-17 14:39:09 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@5043e60dbf975161239b4e4c2ce8393d039fa521 🚀

--- demo-DES-1761-dl-vertical-alignment-in-narrow-container/assets/Pagination.test.stories-nx_RUFqt.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-zsgVPwQN.js";import{n as t,t as n}from"./renderComponentVariants-C3s3FsoC.js";import{C as r,t as i}from"./src-t2umz2AW.js";import a,{n as o}from"./Pagination.stories-D7fp6eYx.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Navigation/Pagination`},c={render:e=>t(r,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(Pagination, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
