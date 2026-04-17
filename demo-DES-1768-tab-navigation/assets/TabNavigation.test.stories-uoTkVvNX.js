30f5d29f12a81a4c0020451f907377306ec84632
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-04-17 11:37:55 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@304cad3d39eb8c845a72275b4d6006af00acb9a7 🚀

---
+++ demo-DES-1768-tab-navigation/assets/TabNavigation.test.stories-uoTkVvNX.js
@@ -0,0 +1,33 @@
+import{n as e}from"./chunk-zsgVPwQN.js";import{t}from"./jsx-runtime-GmyBfqKJ.js";import{l as n,z as r}from"./index.esm-B_OH5ZB5.js";import{s as i,t as a}from"./src-B3a_vf8V.js";import o,{n as s}from"./TabNavigation.stories-D_6wXPBl.js";var c,l,u,d;e((()=>{r(),a(),s(),c=t(),l={...o,title:`Components/Navigation/Tab Navigation`},u={render:()=>(0,c.jsxs)(`div`,{className:`_ams-tests-grid`,children:[(0,c.jsx)(i,{children:(0,c.jsxs)(i.List,{children:[(0,c.jsx)(i.Link,{"aria-current":`page`,href:`#`,children:`Gegevens`}),(0,c.jsx)(i.Link,{href:`#`,children:`Aanslagen`}),(0,c.jsx)(i.Link,{href:`#`,icon:n,children:`Documenten`}),(0,c.jsx)(i.Link,{className:`hover`,href:`#`,children:`Acties`})]})}),(0,c.jsx)(i,{orientation:`vertical`,children:(0,c.jsxs)(i.List,{children:[(0,c.jsx)(i.Link,{"aria-current":`page`,href:`#`,children:`Gegevens`}),(0,c.jsx)(i.Link,{href:`#`,children:`Aanslagen`}),(0,c.jsx)(i.Link,{href:`#`,icon:n,children:`Documenten`}),(0,c.jsx)(i.Link,{className:`hover`,href:`#`,children:`Acties`})]})})]}),tags:[`!dev`,`!autodocs`]},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  render: () => <div className="_ams-tests-grid">
+      <TabNavigation>
+        <TabNavigation.List>
+          <TabNavigation.Link aria-current="page" href="#">
+            Gegevens
+          </TabNavigation.Link>
+          <TabNavigation.Link href="#">Aanslagen</TabNavigation.Link>
+          <TabNavigation.Link href="#" icon={DocumentsIcon}>
+            Documenten
+          </TabNavigation.Link>
+          <TabNavigation.Link className="hover" href="#">
+            Acties
+          </TabNavigation.Link>
+        </TabNavigation.List>
+      </TabNavigation>
+      <TabNavigation orientation="vertical">
+        <TabNavigation.List>
+          <TabNavigation.Link aria-current="page" href="#">
+            Gegevens
+          </TabNavigation.Link>
+          <TabNavigation.Link href="#">Aanslagen</TabNavigation.Link>
+          <TabNavigation.Link href="#" icon={DocumentsIcon}>
+            Documenten
+          </TabNavigation.Link>
+          <TabNavigation.Link className="hover" href="#">
+            Acties
+          </TabNavigation.Link>
+        </TabNavigation.List>
+      </TabNavigation>
+    </div>,
+  tags: ['!dev', '!autodocs']
+}`,...u.parameters?.docs?.source}}},d=[`Test`]}))();export{u as Test,d as __namedExportsOrder,l as default};
