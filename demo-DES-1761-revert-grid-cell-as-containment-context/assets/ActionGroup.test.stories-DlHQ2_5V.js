8b2d440a3cebbebffc73a3a78e75ecc986b7f5d9
GitHub <github-action@users.noreply.github.com>
2026-04-18 19:51:51 +0000

    Remove feature branch folder

--- demo-DES-1761-revert-grid-cell-as-containment-context/assets/ActionGroup.test.stories-DlHQ2_5V.js
+++
@@ -1,9 +0,0 @@
-import{n as e}from"./chunk-zsgVPwQN.js";import{t}from"./jsx-runtime-GmyBfqKJ.js";import{n,t as r}from"./renderComponentVariants-Cw-cAc72.js";import{Tt as i,t as a}from"./src-19zFmoRS.js";import o,{n as s}from"./ActionGroup.stories-DP7U1-Dk.js";var c,l,u,d;e((()=>{a(),r(),s(),c=t(),l={...o,title:`Components/Layout/Action Group`},u={args:{children:[(0,c.jsx)(`button`,{children:`Stoppen`},1),(0,c.jsx)(`button`,{children:`Verder`},2)]},render:e=>n(i,{args:e}),tags:[`!dev`,`!autodocs`]},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: [<button key={1}>Stoppen</button>, <button key={2}>Verder</button>]
-  },
-  render: args => renderComponentVariants(ActionGroup, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...u.parameters?.docs?.source}}},d=[`Test`]}))();export{u as Test,d as __namedExportsOrder,l as default};
