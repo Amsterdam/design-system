db4b27db0eea3363af94aea9e7015205b61c4395
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-04-18 21:17:35 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@15a04b4434e135990c2b56f0ff76284a09e80d2a 🚀

--- demo-develop/assets/Switch.test.stories-Coh8aazj.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-zsgVPwQN.js";import{n as t,t as n}from"./renderComponentVariants-Cw-cAc72.js";import{t as r,u as i}from"./src-PfeWr1kP.js";import a,{n as o}from"./Switch.stories-Db3IytG-.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/Forms/Switch`},c={render:e=>t(i,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(Switch, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
