092c0fff4ebfd9e395caee721cb55732e57d7cc5
Vincent Smedinga <v.smedinga@amsterdam.nl>
2026-04-18 22:12:23 +0200

    feat(Table): Add whitespace below Caption (#2515)

--- storybook/src/components/Table/Table.stories.tsx
+++ storybook/src/components/Table/Table.stories.tsx
@@ -5,7 +5,7 @@
 
 import type { Meta, StoryObj } from '@storybook/react-vite'
 
-import { Heading } from '@amsterdam/design-system-react'
+import { Heading, Paragraph } from '@amsterdam/design-system-react'
 import { Table } from '@amsterdam/design-system-react/src'
 
 const meta = {
@@ -80,6 +80,47 @@ export const WithFooterAndRowHeaders: Story = {
           <Table.Cell>€ 3.292,20</Table.Cell>
         </Table.Row>
       </Table.Footer>,
+    ],
+  },
+}
+
+export const WithDescriptionInCaption: Story = {
+  args: {
+    'aria-labelledby': 'caption-heading',
+    children: [
+      <Table.Caption key="caption">
+        <Heading id="caption-heading" level={3}>
+          Kosten voor de aanvraag
+        </Heading>
+        <Paragraph size="small">
+          Prijzen zijn geldig vanaf 1 januari 2025. Bron: Rijksdienst voor Identiteitsgegevens.
+        </Paragraph>
+      </Table.Caption>,
+      <Table.Header key="header">
+        <Table.Row>
+          <Table.HeaderCell>Type</Table.HeaderCell>
+          <Table.HeaderCell>
+            Normale levertijd
+            <br />1 week
+          </Table.HeaderCell>
+          <Table.HeaderCell>
+            Spoed
+            <br />2 werkdagen
+          </Table.HeaderCell>
+        </Table.Row>
+      </Table.Header>,
+      <Table.Body key="body">
+        <Table.Row>
+          <Table.Cell>Paspoort</Table.Cell>
+          <Table.Cell>€ 77,85</Table.Cell>
+          <Table.Cell>€ 130,80</Table.Cell>
+        </Table.Row>
+        <Table.Row>
+          <Table.Cell>ID-kaart</Table.Cell>
+          <Table.Cell>€ 70,35</Table.Cell>
+          <Table.Cell>€ 123,30</Table.Cell>
+        </Table.Row>
+      </Table.Body>,
     ],
   },
 }
