fc559666876649d7178dc5f3f2792765ce95d266
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-04-18 20:14:48 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@092c0fff4ebfd9e395caee721cb55732e57d7cc5 🚀

--- demo-develop/assets/Label.test.stories-CuJRi0j4.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-zsgVPwQN.js";import{n as t,t as n}from"./renderComponentVariants-Cw-cAc72.js";import{L as r,t as i}from"./src-19zFmoRS.js";import a,{n as o}from"./Label.stories-B0JLDZn-.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Label`},c={render:e=>t(r,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(Label, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
---
+++ demo-develop/assets/Overlap.stories-BC0W6hbZ.js
@@ -0,0 +1,1 @@
+import{a as e,i as t,r as n,t as r}from"./Overlap.stories-B6g9bxbT.js";t();export{r as Default,n as __namedExportsOrder,e as default};
