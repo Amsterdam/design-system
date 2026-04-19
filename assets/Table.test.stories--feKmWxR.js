b614fa870c547beb457abbbab08266267328db45
VincentSmedinga <VincentSmedinga@users.noreply.github.com>
2026-04-19 22:58:18 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@15a04b4434e135990c2b56f0ff76284a09e80d2a 🚀

---
+++ assets/Table.test.stories--feKmWxR.js
@@ -0,0 +1,40 @@
+import{n as e}from"./chunk-zsgVPwQN.js";import{t}from"./jsx-runtime-GmyBfqKJ.js";import{n,t as r}from"./renderComponentVariants-Cw-cAc72.js";import{At as i,ct as a,l as o,t as s}from"./src-eU1Un45E.js";import c,{n as l}from"./Table.stories-LI7exN-X.js";var u,d,f,p;e((()=>{s(),r(),l(),u=t(),d={...c,title:`Components/Containers/Table`},f={args:{"aria-labelledby":`caption-heading`,children:[(0,u.jsxs)(o.Caption,{children:[(0,u.jsx)(i,{id:`caption-heading`,level:3,children:`Kosten voor de aanvraag`}),(0,u.jsx)(a,{size:`small`,children:`Prijzen zijn geldig vanaf 1 januari 2025. Bron: Rijksdienst voor Identiteitsgegevens.`})]},`caption`),(0,u.jsx)(o.Header,{children:(0,u.jsxs)(o.Row,{children:[(0,u.jsx)(o.HeaderCell,{children:`Type`}),(0,u.jsxs)(o.HeaderCell,{children:[`Normale levertijd`,(0,u.jsx)(`br`,{}),`1 week`]}),(0,u.jsxs)(o.HeaderCell,{children:[`Spoed`,(0,u.jsx)(`br`,{}),`2 werkdagen`]})]})},`header`),(0,u.jsxs)(o.Body,{children:[(0,u.jsxs)(o.Row,{children:[(0,u.jsx)(o.Cell,{children:`Paspoort`}),(0,u.jsx)(o.Cell,{children:`€ 77,85`}),(0,u.jsx)(o.Cell,{children:`€ 130,80`})]}),(0,u.jsxs)(o.Row,{children:[(0,u.jsx)(o.Cell,{children:`ID-kaart`}),(0,u.jsx)(o.Cell,{children:`€ 70,35`}),(0,u.jsx)(o.Cell,{children:`€ 123,30`})]})]},`body`)]},render:e=>n(o,{args:e}),tags:[`!dev`,`!autodocs`]},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
+  args: {
+    'aria-labelledby': 'caption-heading',
+    children: [<Table.Caption key="caption">
+        <Heading id="caption-heading" level={3}>
+          Kosten voor de aanvraag
+        </Heading>
+        <Paragraph size="small">
+          Prijzen zijn geldig vanaf 1 januari 2025. Bron: Rijksdienst voor Identiteitsgegevens.
+        </Paragraph>
+      </Table.Caption>, <Table.Header key="header">
+        <Table.Row>
+          <Table.HeaderCell>Type</Table.HeaderCell>
+          <Table.HeaderCell>
+            Normale levertijd
+            <br />1 week
+          </Table.HeaderCell>
+          <Table.HeaderCell>
+            Spoed
+            <br />2 werkdagen
+          </Table.HeaderCell>
+        </Table.Row>
+      </Table.Header>, <Table.Body key="body">
+        <Table.Row>
+          <Table.Cell>Paspoort</Table.Cell>
+          <Table.Cell>€ 77,85</Table.Cell>
+          <Table.Cell>€ 130,80</Table.Cell>
+        </Table.Row>
+        <Table.Row>
+          <Table.Cell>ID-kaart</Table.Cell>
+          <Table.Cell>€ 70,35</Table.Cell>
+          <Table.Cell>€ 123,30</Table.Cell>
+        </Table.Row>
+      </Table.Body>]
+  },
+  render: args => renderComponentVariants(Table, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...f.parameters?.docs?.source}}},p=[`Test`]}))();export{f as Test,p as __namedExportsOrder,d as default};
