8b4c05d4e815cc6c982a668e93fae9ec9293b291
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-04-20 17:54:37 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@82e050c1bb2bce6d883c48e823b6e06ebfd1bdbf 🚀

---
+++ demo-enable-release-please-peer-deps/assets/GridColumnsGuide-CgWMNejz.js
@@ -0,0 +1,1 @@
+import{n as e}from"./chunk-zsgVPwQN.js";import{t}from"./jsx-runtime-GmyBfqKJ.js";import{W as n,t as r}from"./src-D7xwpEN0.js";var i=e((()=>{})),a,o,s=e((()=>{i(),r(),a=t(),o=()=>(0,a.jsx)(n,{className:`_ams-grid-columns-guide`,children:Array.from(Array(12).keys()).map(e=>(0,a.jsx)(n.Cell,{className:`_ams-grid-columns-guide__cell`},e))})}));export{s as n,o as t};
