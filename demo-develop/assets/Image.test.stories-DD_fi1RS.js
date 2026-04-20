830df879c095ef11af5d0040d2e313c9d69d1261
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-04-20 15:11:36 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@7583c2732c7ab71747586259f84f997e562816ae 🚀

---
+++ demo-develop/assets/Image.test.stories-DD_fi1RS.js
@@ -0,0 +1,10 @@
+import{n as e}from"./chunk-zsgVPwQN.js";import{n as t,t as n}from"./renderComponentVariants-Cw-cAc72.js";import{ot as r,t as i}from"./src-C0z72nhj.js";import a,{n as o}from"./Image.stories-DNzkvf_V.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Media/Image`},c={args:{alt:``,src:`https://picsum.photos/id/122/1280/720`},render:e=>t(r,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    alt: '',
+    src: 'https://picsum.photos/id/122/1280/720'
+  },
+  render: args => renderComponentVariants(Image, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
---
+++ demo-develop/assets/Radio.test.stories-D5XxU0MC.js
@@ -0,0 +1,6 @@
+import{n as e}from"./chunk-zsgVPwQN.js";import{n as t,t as n}from"./renderComponentVariants-Cw-cAc72.js";import{t as r,v as i}from"./src-C0z72nhj.js";import a,{n as o}from"./Radio.stories-BzxSXTOq.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/Forms/Radio`},c={render:e=>t(i,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(Radio, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
