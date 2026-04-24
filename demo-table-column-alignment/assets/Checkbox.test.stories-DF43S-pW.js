0fa78b6b80856cbc537e6f57f79ef3e533f21591
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-04-24 09:41:23 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@8f96e54aeb792c197dde46b093095c2860a66691 🚀

---
+++ demo-table-column-alignment/assets/Checkbox.test.stories-DF43S-pW.js
@@ -0,0 +1,6 @@
+import{n as e}from"./chunk-zsgVPwQN.js";import{n as t,t as n}from"./renderComponentVariants-Cw-cAc72.js";import{rt as r,t as i}from"./src-D--Oe89X.js";import a,{n as o}from"./Checkbox.stories-CXOrHm_m.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Checkbox`},c={render:e=>t(r,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(Checkbox, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
