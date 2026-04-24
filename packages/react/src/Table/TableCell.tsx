636eae78d7e095f6af32911921c40337c87a3b7e
Vincent Smedinga <v.smedinga@amsterdam.nl>
2026-04-24 12:41:54 +0200

    feat(Table): Allow centering or right-aligning content of a Cell or Header Cell (#2587)

--- packages/react/src/Table/TableCell.tsx
+++ packages/react/src/Table/TableCell.tsx
@@ -8,11 +8,21 @@ import type { ForwardedRef, PropsWithChildren, TdHTMLAttributes } from 'react'
 import { clsx } from 'clsx'
 import { forwardRef } from 'react'
 
-export type TableCellProps = PropsWithChildren<TdHTMLAttributes<HTMLTableCellElement>>
+export const tableCellAlignOptions = ['center', 'end'] as const
+type TableCellAlign = (typeof tableCellAlignOptions)[number]
+
+export type TableCellProps = {
+  /** The horizontal alignment of the cell’s content. */
+  align?: TableCellAlign
+} & PropsWithChildren<Omit<TdHTMLAttributes<HTMLTableCellElement>, 'align'>>
 
 export const TableCell = forwardRef(
-  ({ children, className, ...restProps }: TableCellProps, ref: ForwardedRef<HTMLTableCellElement>) => (
-    <td {...restProps} className={clsx('ams-table__cell', className)} ref={ref}>
+  ({ align, children, className, ...restProps }: TableCellProps, ref: ForwardedRef<HTMLTableCellElement>) => (
+    <td
+      {...restProps}
+      className={clsx('ams-table__cell', align && `ams-table__cell--align-${align}`, className)}
+      ref={ref}
+    >
       {children}
     </td>
   ),
