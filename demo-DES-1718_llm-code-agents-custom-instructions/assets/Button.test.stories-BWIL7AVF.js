b262bb3a9b000608e89797efd1e198ce9dc3823c
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-04-24 11:34:09 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@f1cf99d74384a459afb56624e96d9a1eed5d70c9 🚀

---
+++ demo-DES-1718_llm-code-agents-custom-instructions/assets/Button.test.stories-BWIL7AVF.js
@@ -0,0 +1,7 @@
+import{n as e}from"./chunk-zsgVPwQN.js";import{n as t,t as n}from"./renderComponentVariants-Cw-cAc72.js";import{t as r,ut as i}from"./src-DDdCnoGZ.js";import a,{n as o}from"./Button.stories-BkBlECeR.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/Buttons/Button`},c={render:e=>t(i,{args:e,variants:[`disabled`,`hovered`]}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(Button, {
+    args,
+    variants: ['disabled', 'hovered']
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
--- demo-DES-1718_llm-code-agents-custom-instructions/assets/Select.test.stories-Dhll0OeG.js
+++
@@ -1,7 +0,0 @@
-import{n as e}from"./chunk-zsgVPwQN.js";import{n as t,t as n}from"./renderComponentVariants-CbKyLAY3.js";import{h as r,t as i}from"./src-D10vFwRq.js";import a,{n as o}from"./Select.stories-DerMQ_vz.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Select`},c={render:e=>t(r,{args:e,variants:[`hovered`,`disabled`]}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(Select, {
-    args,
-    variants: ['hovered', 'disabled']
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
