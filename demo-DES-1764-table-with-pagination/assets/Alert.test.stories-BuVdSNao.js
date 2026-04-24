399b13a42735131ea340ae2381d1c6a428f86cbd
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-04-24 08:55:43 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@a0a04f4dcfd2fb0e2532e0e5ce20998ac8c8b5c4 🚀

--- demo-DES-1764-table-with-pagination/assets/Alert.test.stories-BuVdSNao.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-zsgVPwQN.js";import{n as t,t as n}from"./renderComponentVariants-Cw-cAc72.js";import{bt as r,t as i}from"./src-BOzP9JFa.js";import a,{n as o}from"./Alert.stories-DnjMo-H4.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Feedback/Alert`},c={render:e=>t(r,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(Alert, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
