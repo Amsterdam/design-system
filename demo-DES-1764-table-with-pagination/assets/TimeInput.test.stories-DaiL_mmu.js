06d6a92b2a655685760a234b81346490edd325ea
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-04-24 09:10:37 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@9a5919110c3416422bd15550a79123b268bf97e2 🚀

---
+++ demo-DES-1764-table-with-pagination/assets/TimeInput.test.stories-DaiL_mmu.js
@@ -0,0 +1,7 @@
+import{n as e}from"./chunk-zsgVPwQN.js";import{n as t,t as n}from"./renderComponentVariants-Cw-cAc72.js";import{r,t as i}from"./src-D-3jkDNk.js";import a,{n as o}from"./TimeInput.stories-Cq-NDvqh.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Time Input`},c={render:e=>t(r,{args:e,variants:[`disabled`,`hovered`]}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(TimeInput, {
+    args,
+    variants: ['disabled', 'hovered']
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
