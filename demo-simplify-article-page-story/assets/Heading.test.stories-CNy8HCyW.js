8e189aca8dbcac9b687e029784ccfea9f0fd93b5
GitHub <github-action@users.noreply.github.com>
2026-05-06 14:11:46 +0000

    Remove feature branch folder

--- demo-simplify-article-page-story/assets/Heading.test.stories-CNy8HCyW.js
+++
@@ -1,9 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-Dil7tqoV.js";import{At as r,t as i}from"./src-wuRjY3J3.js";import a,{n as o}from"./Heading.stories-CW1fpWKh.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Text/Heading`},c={args:{children:`Amsterdam Light Festival maakt de stad ook deze winter een beetje mooier`},render:e=>t(r,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: 'Amsterdam Light Festival maakt de stad ook deze winter een beetje mooier'
-  },
-  render: args => renderComponentVariants(Heading, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
--- demo-simplify-article-page-story/assets/TextInput.test.stories-B8DD33f_.js
+++
@@ -1,7 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-Dil7tqoV.js";import{i as r,t as i}from"./src-wuRjY3J3.js";import a,{n as o}from"./TextInput.stories-C4WQKyno.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Text Input`},c={render:e=>t(r,{args:e,variants:[`disabled`,`hovered`]}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(TextInput, {
-    args,
-    variants: ['disabled', 'hovered']
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
