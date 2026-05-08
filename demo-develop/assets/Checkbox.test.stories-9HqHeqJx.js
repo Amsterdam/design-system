6e59870e00ba4fc97f68edc77f424678dfd71e89
RubenSibon <mail+github@rubensibon.nl>
2026-05-08 12:56:19 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@d1b96b907b8d4633ef49d2faf9ef6f481fceb284 🚀

--- demo-develop/assets/Checkbox.test.stories-9HqHeqJx.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-DJI0cd3q.js";import{rt as r,t as i}from"./src-DMrBYFYN.js";import a,{n as o}from"./Checkbox.stories-BAwr0JIP.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Checkbox`},c={render:e=>t(r,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(Checkbox, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
