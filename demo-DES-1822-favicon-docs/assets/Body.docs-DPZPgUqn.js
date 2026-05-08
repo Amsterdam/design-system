793f43fbc7eaf3d2f1b48b0a0f935680ed7e5db8
RubenSibon <mail+github@rubensibon.nl>
2026-05-08 11:43:40 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@cc80a166983286d789094a412c44b94856f5e8c4 🚀

---
+++ demo-DES-1822-favicon-docs/assets/Body.docs-DPZPgUqn.js
@@ -0,0 +1,20 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{Bt as t}from"./iframe-Dl-n54NS.js";import{r as n}from"./react-CR2iwBQx.js";import{c as r,f as i,l as a,s as o}from"./blocks-DyjWmkPY.js";import{t as s}from"./mdx-react-shim-6GRBZG-I.js";import{n as c,t as l}from"./Body.stories-1LI2FX5p.js";var u,d=e((()=>{u=`<!-- @license CC0-1.0 -->
+
+# Body
+
+## Class name
+
+\`ams-body\`
+
+### Guidelines
+
+Add this class to your document’s \`<body>\` tag.
+
+This removes white space that the browser might add.
+
+In Compact Mode, it also sets the background color to match the Page component seamlessly.
+`}));function f(e){return(0,m.jsxs)(m.Fragment,{children:[`
+`,`
+`,(0,m.jsx)(r,{of:l}),`
+`,(0,m.jsx)(o,{children:u}),`
+`,(0,m.jsx)(a,{})]})}function p(e={}){let{wrapper:t}={...n(),...e.components};return t?(0,m.jsx)(t,{...e,children:(0,m.jsx)(f,{...e})}):f(e)}var m;e((()=>{m=t(),s(),i(),c(),d()}))();export{p as default};
---
+++ demo-DES-1822-favicon-docs/assets/Switch.test.stories-5qZMeUcM.js
@@ -0,0 +1,6 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-ExbQP8dx.js";import{t as r,u as i}from"./src-4AXTfyHd.js";import a,{n as o}from"./Switch.stories-DB1fXu0q.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/Forms/Switch`},c={render:e=>t(i,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(Switch, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
