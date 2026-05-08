b90e78a10dcd952a6d3a19780b1f51d76b938ee2
RubenSibon <mail+github@rubensibon.nl>
2026-05-08 15:58:46 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@e564df39610d285f2575d62771788c7da7501454 🚀

---
+++ demo-DES-1831-workflow-to-cleanup-gh-pages-deploys/assets/Image.test.stories-DHK9lqmM.js
@@ -0,0 +1,10 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-B_g59T37.js";import{ot as r,t as i}from"./src-Be9YW_-S.js";import a,{n as o}from"./Image.stories-DtlonfQj.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Media/Image`},c={args:{alt:``,src:`https://picsum.photos/id/122/1280/720`},render:e=>t(r,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    alt: '',
+    src: 'https://picsum.photos/id/122/1280/720'
+  },
+  render: args => renderComponentVariants(Image, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
---
+++ demo-DES-1831-workflow-to-cleanup-gh-pages-deploys/assets/Row.test.stories-DwfojWQi.js
@@ -0,0 +1,9 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{Bt as t}from"./iframe-CpCvOZZd.js";import{n,t as r}from"./renderComponentVariants-B_g59T37.js";import{t as i,xt as a}from"./src-Be9YW_-S.js";import o,{n as s}from"./Row.stories-Q6-bpO8X.js";var c,l,u,d;e((()=>{i(),r(),s(),c=t(),l={...o,title:`Components/Layout/Row`},u={args:{children:[(0,c.jsx)(`div`,{className:`_ams-item`},1),(0,c.jsx)(`div`,{className:`_ams-item`},2),(0,c.jsx)(`div`,{className:`_ams-item`},3)]},render:e=>n(a,{args:e}),tags:[`!dev`,`!autodocs`]},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<div className="_ams-item" key={1} />, <div className="_ams-item" key={2} />, <div className="_ams-item" key={3} />]
+  },
+  render: args => renderComponentVariants(Row, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...u.parameters?.docs?.source}}},d=[`Test`]}))();export{u as Test,d as __namedExportsOrder,l as default};
