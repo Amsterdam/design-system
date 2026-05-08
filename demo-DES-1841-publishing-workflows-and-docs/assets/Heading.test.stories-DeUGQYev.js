a7bba1145221a7c5b04d2f030de3051cb5432628
RubenSibon <mail+github@rubensibon.nl>
2026-05-08 15:14:43 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@ce9ec13fd4ddda31c6c8c95092c0c37ab46981db 🚀

---
+++ demo-DES-1841-publishing-workflows-and-docs/assets/Heading.test.stories-DeUGQYev.js
@@ -0,0 +1,9 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-CJFAbZlS.js";import{At as r,t as i}from"./src-BuF-cXdp.js";import a,{n as o}from"./Heading.stories-FCYFF0Oq.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Text/Heading`},c={args:{children:`Amsterdam Light Festival maakt de stad ook deze winter een beetje mooier`},render:e=>t(r,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Amsterdam Light Festival maakt de stad ook deze winter een beetje mooier'
+  },
+  render: args => renderComponentVariants(Heading, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
---
+++ demo-DES-1841-publishing-workflows-and-docs/assets/PageFooter.docs-C8DqYe6Q.js
@@ -0,0 +1,47 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{Bt as t}from"./iframe-CihWDE4d.js";import{r as n}from"./react-DjDS0y3U.js";import{c as r,f as i,l as a,n as o,s}from"./blocks-dhmhgVEO.js";import{n as c,t as l}from"./DesignTokensTable-BP61PG4g.js";import{t as u}from"./mdx-react-shim--f0-bv46.js";import{a as d,i as f,r as p,t as m}from"./PageFooter.stories-URu0_m6H.js";var h,g=e((()=>{h=`<!-- @license CC0-1.0 -->
+
+# Page Footer
+
+Provides service information at the bottom of every page.
+
+## Guidelines
+
+- The Page Footer must be used on all websites for the City of Amsterdam.
+- It must be the same on every page of the website.
+
+### Content
+
+- This section is required for public websites, but not for internal ones.
+  It offers space for various practical links:
+  - The first column focuses on contact information.
+    Make it specific and tailor it to the website or page.
+  - The second column contains links to other relevant sources.
+  - The third column refers to newsletters, events, social media, etc.
+- Set [the inverse colour](?path=/docs/brand-design-tokens-colour--docs#pairing-foreground-with-background-colours) on all text and links in this area.
+- If the Page Footer contains [Headings](https://designsystem.amsterdam/?path=/docs/components-text-heading--docs), give them level 2 and a size of ‘level-3’.
+  A visually hidden heading to group them is not necessary – the footer’s landmark provides enough guidance.
+
+### Menu
+
+- The menu is for links to privacy policies, cookie statements, and other information about the website itself.
+- It includes a visually hidden heading to provide structural context for screen reader users.
+  The default heading text is ‘Over deze website’.
+  Customize it when the menu serves a different purpose, or the interface language is not Dutch.
+`})),_,v,y=e((()=>{_={"page-footer":{menu:{"background-color":{$value:`{ams.color.background.default}`,$extensions:{"nl.amsterdam.type":`color`}},"column-gap":{$value:`{ams.space.l}`,$extensions:{"nl.amsterdam.subtype":`space`,"nl.amsterdam.type":`dimension`}},"padding-block":{$value:`{ams.space.l}`,$extensions:{"nl.amsterdam.subtype":`space`,"nl.amsterdam.type":`dimension`}},"padding-inline":{$value:`{ams.grid.padding-inline}`,$extensions:{"nl.amsterdam.subtype":`space`,"nl.amsterdam.type":`dimension`}},"row-gap":{$value:`{ams.space.xs}`,$extensions:{"nl.amsterdam.subtype":`space`,"nl.amsterdam.type":`dimension`}},medium:{"padding-inline":{$deprecated:"Use `ams.page-footer.menu.vi-medium.padding-inline` instead. Will be removed on or after 2026-10-20.",$value:`initial`,$extensions:{"nl.amsterdam.subtype":`space`,"nl.amsterdam.type":`dimension`}}},wide:{"padding-inline":{$deprecated:"Use `ams.page-footer.menu.vi-wide.padding-inline` instead. Will be removed on or after 2026-10-20.",$value:`initial`,$extensions:{"nl.amsterdam.subtype":`space`,"nl.amsterdam.type":`dimension`}}},"vi-medium":{"padding-inline":{$value:`{ams.grid.vi-medium.padding-inline}`,$extensions:{"nl.amsterdam.subtype":`space`,"nl.amsterdam.type":`dimension`}}},"vi-wide":{"padding-inline":{$value:`{ams.grid.vi-wide.padding-inline}`,$extensions:{"nl.amsterdam.subtype":`space`,"nl.amsterdam.type":`dimension`}}},link:{color:{$value:`{ams.links.color}`,$extensions:{"nl.amsterdam.type":`color`}},"font-family":{$value:`{ams.typography.font-family}`,$extensions:{"nl.amsterdam.type":`fontFamily`}},"font-size":{$value:`{ams.typography.body-text.font-size}`,$extensions:{"nl.amsterdam.type":`fontSize`}},"font-weight":{$value:`{ams.typography.body-text.font-weight}`,$extensions:{"nl.amsterdam.type":`fontWeight`}},"line-height":{$value:`{ams.typography.body-text.line-height}`,$extensions:{"nl.amsterdam.subtype":`lineHeight`,"nl.amsterdam.type":`number`}},"outline-offset":{$value:`{ams.focus.outline-offset}`,$extensions:{"nl.amsterdam.subtype":`space`,"nl.amsterdam.type":`dimension`}},"text-decoration-line":{$value:`{ams.links.subtle.text-decoration-line}`,$extensions:{"nl.amsterdam.type":`textDecorationLine`}},"text-decoration-thickness":{$value:`{ams.links.text-decoration-thickness}`,$extensions:{"nl.amsterdam.subtype":`space`,"nl.amsterdam.type":`dimension`}},"text-underline-offset":{$value:`{ams.links.text-underline-offset}`,$extensions:{"nl.amsterdam.subtype":`space`,"nl.amsterdam.type":`dimension`}},hover:{color:{$value:`{ams.links.hover.color}`,$extensions:{"nl.amsterdam.type":`color`}},"text-decoration-line":{$value:`{ams.links.subtle.hover.text-decoration-line}`,$extensions:{"nl.amsterdam.type":`textDecorationLine`}}}}},spotlight:{"background-color":{$value:`#004699`,$type:`color`}}}},v={ams:_}}));function b(e){let t={h2:`h2`,h3:`h3`,p:`p`,...n(),...e.components};return(0,S.jsxs)(S.Fragment,{children:[`
+`,`
+`,`
+`,`
+`,(0,S.jsx)(r,{of:f}),`
+`,(0,S.jsx)(s,{children:h}),`
+`,(0,S.jsx)(a,{}),`
+`,(0,S.jsx)(t.h2,{id:`examples`,children:`Examples`}),`
+`,(0,S.jsx)(t.h3,{id:`onderzoek-en-statistiek`,children:`Onderzoek en Statistiek`}),`
+`,(0,S.jsx)(t.p,{children:`The Page Footer for a specific website can be a bit different.
+Here’s an example for the ‘Onderzoek en Statistiek’ department.`}),`
+`,(0,S.jsx)(o,{of:p}),`
+`,(0,S.jsx)(t.h3,{id:`custom-menu-heading`,children:`Custom menu heading`}),`
+`,(0,S.jsx)(t.p,{children:`For an English website, update the visually hidden heading to ‘Support links’.
+If your website is in Dutch, but the default heading of ‘Over deze website’ doesn’t reflect the content of the menu, choose a different heading as well.`}),`
+`,(0,S.jsx)(o,{of:m}),`
+`,(0,S.jsx)(t.h2,{id:`design-tokens`,children:`Design tokens`}),`
+`,(0,S.jsx)(l,{tokens:v})]})}function x(e={}){let{wrapper:t}={...n(),...e.components};return t?(0,S.jsx)(t,{...e,children:(0,S.jsx)(b,{...e})}):b(e)}var S;e((()=>{S=t(),u(),i(),d(),g(),y(),c()}))();export{x as default};
