672f4355304244c91fefbd7512955e4c1fd5d041
RubenSibon <mail+github@rubensibon.nl>
2026-05-12 12:30:27 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@b5a572f2f466e5c91f330730620ca8d60ff48280 🚀

---
+++ demo-DES-1847-upgrade-to-pnpm-11/assets/Badge.test.stories-DNL-72_4.js
@@ -0,0 +1,6 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-CJoFGEvJ.js";import{mt as r,t as i}from"./src-eYhLUzbp.js";import a,{n as o}from"./Badge.stories-CX2u5EoL.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Feedback/Badge`},c={render:e=>t(r,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(Badge, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
---
+++ demo-DES-1847-upgrade-to-pnpm-11/assets/StandaloneLink.test.stories-CFYt7ZGO.js
@@ -0,0 +1,6 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-CJoFGEvJ.js";import{d as r,t as i}from"./src-eYhLUzbp.js";import a,{n as o}from"./StandaloneLink.stories-W6H0WOFM.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/StandaloneLink`},c={render:e=>t(r,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(StandaloneLink, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
