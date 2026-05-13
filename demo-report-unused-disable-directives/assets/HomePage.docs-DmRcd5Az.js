a295d9e91dde7f24ffa55b60256c9ea214c2390e
GitHub <github-action@users.noreply.github.com>
2026-05-13 15:45:50 +0000

    Remove feature branch folder

--- demo-report-unused-disable-directives/assets/HomePage.docs-DmRcd5Az.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{Vt as t}from"./iframe-C_CdGt-L.js";import{r as n}from"./react-ABpSb1xr.js";import{c as r,f as i}from"./blocks-Dy6KHMGM.js";import{t as a}from"./mdx-react-shim-BX97rgWg.js";import{n as o,t as s}from"./HomePage.stories-DqOfMGcU.js";function c(e){let t={h1:`h1`,p:`p`,...n(),...e.components};return(0,u.jsxs)(u.Fragment,{children:[`
-`,`
-`,(0,u.jsx)(r,{of:s}),`
-`,(0,u.jsx)(t.h1,{id:`home-page`,children:`Home page`}),`
-`,(0,u.jsx)(t.p,{children:`The home page of an internal website can have varying types of content.`}),`
-`,(0,u.jsx)(t.p,{children:`Our single example in this category currently focuses on the overall layout.`})]})}function l(e={}){let{wrapper:t}={...n(),...e.components};return t?(0,u.jsx)(t,{...e,children:(0,u.jsx)(c,{...e})}):c(e)}var u;e((()=>{u=t(),a(),i(),o()}))();export{l as default};
--- demo-report-unused-disable-directives/assets/Label.test.stories-CBvMA6e3.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-BLBuoWWR.js";import{L as r,t as i}from"./src-CjhRqLhZ.js";import a,{n as o}from"./Label.stories-CyKZlR25.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Label`},c={render:e=>t(r,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(Label, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
