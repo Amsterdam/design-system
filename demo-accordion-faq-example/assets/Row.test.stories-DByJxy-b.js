7ac09cddf5025973165795bb8433a16e0a3afec6
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-05-13 12:02:13 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@edccf3d92c41a49a98cd8f71369922becf016ab9 🚀

---
+++ demo-accordion-faq-example/assets/Row.test.stories-DByJxy-b.js
@@ -0,0 +1,9 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{Vt as t}from"./iframe-DDO2Ni87.js";import{n,t as r}from"./renderComponentVariants-DiD0yTg2.js";import{t as i,xt as a}from"./src-BYcUJS-5.js";import o,{n as s}from"./Row.stories-lV6350Q0.js";var c,l,u,d;e((()=>{i(),r(),s(),c=t(),l={...o,title:`Components/Layout/Row`},u={args:{children:[(0,c.jsx)(`div`,{className:`_ams-item`},1),(0,c.jsx)(`div`,{className:`_ams-item`},2),(0,c.jsx)(`div`,{className:`_ams-item`},3)]},render:e=>n(a,{args:e}),tags:[`!dev`,`!autodocs`]},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<div className="_ams-item" key={1} />, <div className="_ams-item" key={2} />, <div className="_ams-item" key={3} />]
+  },
+  render: args => renderComponentVariants(Row, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...u.parameters?.docs?.source}}},d=[`Test`]}))();export{u as Test,d as __namedExportsOrder,l as default};
