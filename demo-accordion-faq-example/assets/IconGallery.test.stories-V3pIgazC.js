1878669f9bffea21fc8b7abfc22531dbc7360aa8
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-05-13 12:12:46 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@350744179ac8aa3a942b125be7b51d7c6d2c5711 🚀

--- demo-accordion-faq-example/assets/IconGallery.test.stories-V3pIgazC.js
+++
@@ -1,4 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{Vt as t}from"./iframe-DDO2Ni87.js";import{n,t as r}from"./IconGallery-c9wbTYwM.js";var i,a,o,s;e((()=>{n(),i=t(),a={title:`Components/Docs/Icon Gallery`},o={render:()=>(0,i.jsx)(r,{icons:[`CloseIcon`,`SearchIcon`,`ArrowDownIcon`,`ArrowUpIcon`,`ErrorFillIcon`,`ErrorIcon`]}),tags:[`!dev`,`!autodocs`]},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
-  render: () => <IconGallery icons={['CloseIcon', 'SearchIcon', 'ArrowDownIcon', 'ArrowUpIcon', 'ErrorFillIcon', 'ErrorIcon']} />,
-  tags: ['!dev', '!autodocs']
-}`,...o.parameters?.docs?.source}}},s=[`Test`]}))();export{o as Test,s as __namedExportsOrder,a as default};
