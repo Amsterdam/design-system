35b28d1bda283603b59a26cd5e7231e260bd67b2
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-05-13 15:29:14 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@1d73d6cb57dea47410f649e73a99f52b5897a266 🚀

---
+++ demo-improve-field-set-examples/assets/IconButton.test.stories-Bee9MBJD.js
@@ -0,0 +1,7 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-CCkkjg04.js";import{t as r,wt as i}from"./src-fWiYhZ8G.js";import a,{n as o}from"./IconButton.stories-s7EMVpBw.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/Buttons/Icon Button`},c={render:e=>t(i,{args:e,variants:[`hovered`,`disabled`]}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(IconButton, {
+    args,
+    variants: ['hovered', 'disabled']
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
---
+++ demo-improve-field-set-examples/assets/Switch.test.stories-iSLF262t.js
@@ -0,0 +1,6 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-CCkkjg04.js";import{t as r,u as i}from"./src-fWiYhZ8G.js";import a,{n as o}from"./Switch.stories-DLv4G17G.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/Forms/Switch`},c={render:e=>t(i,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(Switch, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
