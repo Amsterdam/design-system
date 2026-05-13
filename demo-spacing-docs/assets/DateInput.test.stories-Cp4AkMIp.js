2294a0b87d435573cda75c1b96607f76b7a3bd0b
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-05-13 09:26:34 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@2217e5da0800f9b85037f464c16462f98745eeae 🚀

---
+++ demo-spacing-docs/assets/DateInput.test.stories-Cp4AkMIp.js
@@ -0,0 +1,6 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-B9DZ9SAR.js";import{$ as r,t as i}from"./src-BC9mjcF9.js";import a,{n as o}from"./DateInput.stories-Bvwlc_w6.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Date Input`},c={render:e=>t(r,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(DateInput, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
--- demo-spacing-docs/assets/Radio.test.stories-DJotAgzm.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-CSb-GkrI.js";import{t as r,v as i}from"./src-CYboodt-.js";import a,{n as o}from"./Radio.stories-DL7rjins.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/Forms/Radio`},c={render:e=>t(i,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(Radio, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
