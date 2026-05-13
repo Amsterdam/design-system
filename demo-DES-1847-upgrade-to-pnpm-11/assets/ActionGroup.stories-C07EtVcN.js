61f825a7308eec144eed71dd14db8bd2975542c3
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-05-13 10:03:32 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@3a3a65454b34ca3c757e8efc1cb95e7e80ea97c4 🚀

---
+++ demo-DES-1847-upgrade-to-pnpm-11/assets/ActionGroup.stories-C07EtVcN.js
@@ -0,0 +1,17 @@
+import{n as e,r as t}from"./chunk-DnJy8xQt.js";import{Rt as n,U as r,Vt as i,Y as a,c as o,st as s}from"./iframe-mboYilBS.js";import{Tt as c,t as l}from"./src-CGVMfsyN.js";var u=t({Default:()=>p,Stacked:()=>m,WithLink:()=>h,__namedExportsOrder:()=>g,default:()=>f}),d,f,p,m,h,g,_=e((()=>{a(),n(),l(),d=i(),f={title:`Components/Layout/Action Group`,component:c,args:{children:[(0,d.jsx)(o,{children:`Doorgaan`},1),(0,d.jsx)(o,{variant:`secondary`,children:`Stoppen`},2)]}},p={},m={args:{children:[(0,d.jsx)(o,{children:`Adres wijzigen`},1),(0,d.jsx)(o,{variant:`secondary`,children:`Adres verwijderen`},2)],className:`_ams-resize-horizontal`,style:{inlineSize:`16rem`}}},h={args:{children:[(0,d.jsx)(o,{children:`Bewerken`},1),(0,d.jsx)(r,{download:!0,href:`#`,icon:(0,d.jsx)(s,{}),children:`Downloaden`},2)]}},p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{}`,...p.parameters?.docs?.source}}},m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<Button key={1}>Adres wijzigen</Button>, <Button key={2} variant="secondary">
+        Adres verwijderen
+      </Button>],
+    className: '_ams-resize-horizontal',
+    style: {
+      inlineSize: '16rem'
+    }
+  }
+}`,...m.parameters?.docs?.source}}},h.parameters={...h.parameters,docs:{...h.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<Button key={1}>Bewerken</Button>, <StandaloneLink download href="#" icon={<DownloadIcon />} key={2}>
+        Downloaden
+      </StandaloneLink>]
+  }
+}`,...h.parameters?.docs?.source}}},g=[`Default`,`Stacked`,`WithLink`]}));_();export{p as Default,m as Stacked,h as WithLink,g as __namedExportsOrder,f as default,_ as n,u as t};
