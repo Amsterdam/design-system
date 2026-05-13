d1430437fcf11e30672cab509caf911304548a5b
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-05-13 10:30:59 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@bdcb841de503dafe00620a22b114086c28676f00 🚀

---
+++ demo-DES-1831-workflow-to-cleanup-gh-pages-deploys/assets/StandaloneLink.test.stories-CRzgynJZ.js
@@ -0,0 +1,6 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-BJRTifeI.js";import{d as r,t as i}from"./src-CgB8YvSW.js";import a,{n as o}from"./StandaloneLink.stories-DTHqQUY0.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/StandaloneLink`},c={render:e=>t(r,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(StandaloneLink, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
