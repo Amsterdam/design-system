7c8a559755cc2c5b679d60863158e9a1aa8d8303
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-05-13 08:52:57 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@58466d08d96010d51a1ba407560d597f5dca8883 🚀

---
+++ demo-spacing-docs/assets/Button.test.stories-Bqcz7XXD.js
@@ -0,0 +1,7 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-BLjwBwIh.js";import{t as r,ut as i}from"./src-BbAE6BNH.js";import a,{n as o}from"./Button.stories-Cqu9L9XE.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/Buttons/Button`},c={render:e=>t(i,{args:e,variants:[`disabled`,`hovered`]}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(Button, {
+    args,
+    variants: ['disabled', 'hovered']
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
