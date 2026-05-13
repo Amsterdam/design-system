eb84e9090e166bca807ae566b80f2b3f5dd69339
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-05-13 14:19:38 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@49017db465bfe347e9d2231577dbe080898188cd 🚀

---
+++ demo-publish-with-provenance/assets/Label.test.stories-BxafDeCu.js
@@ -0,0 +1,6 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-CC1RHmD8.js";import{L as r,t as i}from"./src-DZdIdMBW.js";import a,{n as o}from"./Label.stories-Yr1P0Uf1.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Label`},c={render:e=>t(r,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(Label, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
