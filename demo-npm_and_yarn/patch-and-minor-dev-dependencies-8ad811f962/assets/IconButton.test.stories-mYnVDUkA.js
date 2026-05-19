d125deaf0d5f716e9b61b996eb8555fd921f34d5
GitHub <github-action@users.noreply.github.com>
2026-05-19 07:59:53 +0000

    Remove feature branch folder

--- demo-npm_and_yarn/patch-and-minor-dev-dependencies-8ad811f962/assets/IconButton.test.stories-mYnVDUkA.js
+++
@@ -1,7 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-ztImBgkX.js";import{t as r,wt as i}from"./src-U2q4T6JQ.js";import a,{n as o}from"./IconButton.stories-HU-rm_o5.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/Buttons/Icon Button`},c={render:e=>t(i,{args:e,variants:[`hovered`,`disabled`]}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(IconButton, {
-    args,
-    variants: ['hovered', 'disabled']
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
--- demo-npm_and_yarn/patch-and-minor-dev-dependencies-8ad811f962/assets/SkipLink.test.stories-CnULWaS0.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-ztImBgkX.js";import{h as r,t as i}from"./src-U2q4T6JQ.js";import a,{n as o}from"./SkipLink.stories-BdqksVi8.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Navigation/Skip Link`},c={render:e=>t(r,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(SkipLink, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
