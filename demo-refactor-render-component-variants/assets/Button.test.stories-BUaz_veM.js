e652e4cb90c28d4da1067bb06f4a606d70bd380c
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-05-19 23:00:10 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@f345785e0316c699ee77644b64d18b9d5f291808 🚀

--- demo-refactor-render-component-variants/assets/Button.test.stories-BUaz_veM.js
+++
@@ -1,7 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-DnfGHVUL.js";import{t as r,ut as i}from"./src-B_G3OGzX.js";import a,{n as o}from"./Button.stories-DJE2kSKO.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/Buttons/Button`},c={render:(e,n)=>t(i,{args:e,variants:[`disabled`,`hovered`]},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Button, {
-    args,
-    variants: ['disabled', 'hovered']
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
---
+++ demo-refactor-render-component-variants/assets/DescriptionList.test.stories-D9Z8YAse.js
@@ -0,0 +1,28 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{Vt as t}from"./iframe-BBrz8Cnb.js";import{n,t as r}from"./renderComponentVariants-DuBUWsQK.js";import{Q as i,t as a}from"./src-CIn4sA58.js";import o,{n as s}from"./DescriptionList.stories-yKWH7Hso.js";var c,l,u,d;e((()=>{a(),r(),s(),c=t(),l={...o,title:`Components/Text/Description List`},u={args:{children:[(0,c.jsx)(i.Term,{children:`Land`},1),(0,c.jsx)(i.Description,{children:`Nederland`},2),(0,c.jsx)(i.Term,{children:`Plaatsnaam`},3),(0,c.jsx)(i.Description,{children:`Amsterdam`},4),(0,c.jsx)(i.Description,{children:`Weesp`},5),(0,c.jsxs)(i.Section,{children:[(0,c.jsx)(i.Term,{children:`Voornaam`}),(0,c.jsx)(i.Term,{children:`Achternaam`}),(0,c.jsx)(i.Description,{children:`Johan Cruijff`})]},6),(0,c.jsxs)(i.Section,{children:[(0,c.jsx)(i.Term,{children:`Stadsdeel`}),(0,c.jsx)(i.Term,{children:`Stadsgebied`}),(0,c.jsx)(i.Description,{children:`Centrum`}),(0,c.jsx)(i.Description,{children:`Oost`}),(0,c.jsx)(i.Description,{children:`Nieuw-West`}),(0,c.jsx)(i.Description,{children:`Weesp`})]},7)]},render:(e,t)=>n(i,{args:e},t),tags:[`!dev`,`!autodocs`]},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [
+    // 1:1
+    <DescriptionList.Term key={1}>Land</DescriptionList.Term>, <DescriptionList.Description key={2}>Nederland</DescriptionList.Description>,
+    // 1:n
+    <DescriptionList.Term key={3}>Plaatsnaam</DescriptionList.Term>, <DescriptionList.Description key={4}>Amsterdam</DescriptionList.Description>, <DescriptionList.Description key={5}>Weesp</DescriptionList.Description>,
+    // n:1
+    <DescriptionList.Section key={6}>
+        <DescriptionList.Term>Voornaam</DescriptionList.Term>
+        <DescriptionList.Term>Achternaam</DescriptionList.Term>
+        <DescriptionList.Description>Johan Cruijff</DescriptionList.Description>
+      </DescriptionList.Section>,
+    // n:m
+    <DescriptionList.Section key={7}>
+        <DescriptionList.Term>Stadsdeel</DescriptionList.Term>
+        <DescriptionList.Term>Stadsgebied</DescriptionList.Term>
+        <DescriptionList.Description>Centrum</DescriptionList.Description>
+        <DescriptionList.Description>Oost</DescriptionList.Description>
+        <DescriptionList.Description>Nieuw-West</DescriptionList.Description>
+        <DescriptionList.Description>Weesp</DescriptionList.Description>
+      </DescriptionList.Section>]
+  },
+  render: (args, context) => renderComponentVariants(DescriptionList, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...u.parameters?.docs?.source}}},d=[`Test`]}))();export{u as Test,d as __namedExportsOrder,l as default};
