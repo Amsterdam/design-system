e387701206c5d265c5288989fec4b0accbecdf19
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-05-19 09:43:14 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@d709ee874f2ba29acdfc28c48a6feeba41d35aec 🚀

--- demo-develop/assets/PasswordInput.test.stories-CkF1eMtv.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-DbbAdJOZ.js";import{S as r,t as i}from"./src-Bl79B58W.js";import a,{n as o}from"./PasswordInput.stories-BA70EK3_.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Password Input`},c={render:e=>t(r,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: args => renderComponentVariants(PasswordInput, {
-    args
-  }),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
---
+++ demo-develop/assets/Switch.test.stories-CtovzUk2.js
@@ -0,0 +1,6 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-ztImBgkX.js";import{t as r,u as i}from"./src-U2q4T6JQ.js";import a,{n as o}from"./Switch.stories-DEiz6dQ5.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/Forms/Switch`},c={render:e=>t(i,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(Switch, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
