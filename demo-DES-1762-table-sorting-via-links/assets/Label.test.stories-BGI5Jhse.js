d445a2c287a2272fc41a5d0274db9503d9dd4c5f
RubenSibon <mail+github@rubensibon.nl>
2026-05-19 08:29:42 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@4a1ea4ed8b314ff3ce2ee8c25afed17efc48d708 🚀

---
+++ demo-DES-1762-table-sorting-via-links/assets/Label.test.stories-BGI5Jhse.js
@@ -0,0 +1,6 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-B3UN6_l0.js";import{L as r,t as i}from"./src-C1PCuyxT.js";import a,{n as o}from"./Label.stories-PQQhS2xs.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Label`},c={render:e=>t(r,{args:e}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(Label, {
+    args
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
