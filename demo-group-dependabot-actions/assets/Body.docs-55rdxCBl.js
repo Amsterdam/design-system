e953e2fae3476002df2f6f9028801cfb28c8a1a1
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-05-19 08:08:05 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@3eff6bf0fd3d4887979b32268084142127809db2 🚀

---
+++ demo-group-dependabot-actions/assets/Body.docs-55rdxCBl.js
@@ -0,0 +1,20 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{Vt as t}from"./iframe-0dYusbQz.js";import{r as n}from"./react-BgbtrQO_.js";import{c as r,f as i,l as a,s as o}from"./blocks-HQ1a9-yC.js";import{t as s}from"./mdx-react-shim-CUeDss2k.js";import{n as c,t as l}from"./Body.stories-Cp_lP-MH.js";var u,d=e((()=>{u=`<!-- @license CC0-1.0 -->
+
+# Body
+
+## Class name
+
+\`ams-body\`
+
+### Guidelines
+
+Add this class to your document’s \`<body>\` tag.
+
+This removes white space that the browser might add.
+
+In Compact Mode, it also sets the background color to match the Page component seamlessly.
+`}));function f(e){return(0,m.jsxs)(m.Fragment,{children:[`
+`,`
+`,(0,m.jsx)(r,{of:l}),`
+`,(0,m.jsx)(o,{children:u}),`
+`,(0,m.jsx)(a,{})]})}function p(e={}){let{wrapper:t}={...n(),...e.components};return t?(0,m.jsx)(t,{...e,children:(0,m.jsx)(f,{...e})}):f(e)}var m;e((()=>{m=t(),s(),i(),c(),d()}))();export{p as default};
---
+++ demo-group-dependabot-actions/assets/Button.test.stories-B-GScI_7.js
@@ -0,0 +1,7 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-BjsWmVYJ.js";import{t as r,ut as i}from"./src-WKz-e1lE.js";import a,{n as o}from"./Button.stories-BThdY1G-.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/Buttons/Button`},c={render:e=>t(i,{args:e,variants:[`disabled`,`hovered`]}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: args => renderComponentVariants(Button, {
+    args,
+    variants: ['disabled', 'hovered']
+  }),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
