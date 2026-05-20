dfce63b339db68abd89ecaa0001f10f6605e9507
GitHub <github-action@users.noreply.github.com>
2026-05-20 07:39:44 +0000

    Remove feature branch folder

--- demo-refactor-render-component-variants-2/assets/HomePage.docs-BZqmjpC0.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{Vt as t}from"./iframe-Btcmy9-i.js";import{r as n}from"./react-DPsVQlha.js";import{c as r,f as i}from"./blocks-CZhiQNJ8.js";import{t as a}from"./mdx-react-shim-B8p8Wh6x.js";import{n as o,t as s}from"./HomePage.stories-DtBP4SFM.js";function c(e){let t={h1:`h1`,p:`p`,...n(),...e.components};return(0,u.jsxs)(u.Fragment,{children:[`
-`,`
-`,(0,u.jsx)(r,{of:s}),`
-`,(0,u.jsx)(t.h1,{id:`home-page`,children:`Home page`}),`
-`,(0,u.jsx)(t.p,{children:`The home page of an internal website can have varying types of content.`}),`
-`,(0,u.jsx)(t.p,{children:`Our single example in this category currently focuses on the overall layout.`})]})}function l(e={}){let{wrapper:t}={...n(),...e.components};return t?(0,u.jsx)(t,{...e,children:(0,u.jsx)(c,{...e})}):c(e)}var u;e((()=>{u=t(),a(),i(),o()}))();export{l as default};
