998fba7a05e3ee34746816988421d8a58bfec37d
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-05-20 07:31:12 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@47b1cca8b090d707acfd150d3997af9218029900 🚀

---
+++ demo-refactor-render-component-variants-2/assets/PageHeader.test.stories-amOx06Im.js
@@ -0,0 +1,45 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{Rt as t,Vt as n,vt as r}from"./iframe-Btcmy9-i.js";import{F as i,P as a,t as o,w as s}from"./src-Db3ba_LY.js";import c,{n as l,t as u}from"./PageHeader.stories-iGu_wM6E.js";var d,f,p,m,h,g;e((()=>{t(),o(),a(),l(),d=n(),{expect:f}=__STORYBOOK_MODULE_TEST__,p={...c,title:`Components/Containers/Page Header`},m=u,h={args:{brandName:`Data Amsterdam`,children:(0,d.jsx)(`p`,{children:`test`}),menuItems:[(0,d.jsx)(s.MenuLink,{href:`#`,lang:`en`,children:`English`},`1`),(0,d.jsx)(s.MenuLink,{fixed:!0,href:`#`,children:`Zoeken`},`2`)]},play:async({canvas:e,userEvent:t})=>{let n=(await e.findByTestId(`interaction-test`)).querySelector(`button`),r=await e.findByTestId(`children`);n&&(await t.click(n),f(r).toBeVisible(),await t.click(n),f(r).not.toBeVisible())},render:e=>(0,d.jsxs)(d.Fragment,{children:[(0,d.jsx)(s,{"data-testid":`interaction-test`,...e,children:(0,d.jsx)(`ul`,{children:(0,d.jsx)(`li`,{"data-testid":`children`,children:`English`})})}),Object.keys(m).filter(e=>e!==`__namedExportsOrder`).sort().map(e=>{let t=m[e];return t?(0,d.jsx)(s,{...t.args},e):null}),i.map(e=>(0,d.jsx)(s,{brandName:`Voorbeeld`,logoBrand:e,menuItems:(0,d.jsx)(s.MenuLink,{fixed:!0,href:`#`,icon:r,children:`Inloggen`})},e))]}),tags:[`!dev`,`!autodocs`]},h.parameters={...h.parameters,docs:{...h.parameters?.docs,source:{originalSource:`{
+  args: {
+    brandName: 'Data Amsterdam',
+    children: <p>test</p>,
+    menuItems: [<PageHeader.MenuLink href="#" key="1" lang="en">
+        English
+      </PageHeader.MenuLink>, <PageHeader.MenuLink fixed href="#" key="2">
+        Zoeken
+      </PageHeader.MenuLink>]
+  },
+  play: async ({
+    canvas,
+    userEvent
+  }) => {
+    const pageHeader = await canvas.findByTestId('interaction-test');
+    const menuButton = pageHeader.querySelector('button');
+    const exampleChildren = await canvas.findByTestId('children');
+    if (!menuButton) return;
+    await userEvent.click(menuButton);
+    expect(exampleChildren).toBeVisible();
+    await userEvent.click(menuButton);
+    expect(exampleChildren).not.toBeVisible();
+  },
+  render: args => <>
+      {/* Interaction test */}
+      <PageHeader data-testid="interaction-test" {...args}>
+        <ul>
+          <li data-testid="children">English</li>
+        </ul>
+      </PageHeader>
+
+      {/* All public stories, sorted by key for deterministic order */}
+      {Object.keys(pageHeaderStories).filter(key => key !== '__namedExportsOrder') // This gets added by babel-plugin-named-exports-order
+    .sort().map(key => {
+      const story = pageHeaderStories[key];
+      return story ? <PageHeader key={key} {...story.args} /> : null;
+    })}
+
+      {/* All logo brands */}
+      {logoBrands.map(brand => <PageHeader brandName="Voorbeeld" key={brand} logoBrand={brand} menuItems={<PageHeader.MenuLink fixed href="#" icon={LogInIcon}>
+              Inloggen
+            </PageHeader.MenuLink>} />)}
+    </>,
+  tags: ['!dev', '!autodocs']
+}`,...h.parameters?.docs?.source}}},g=[`Test`]}))();export{h as Test,g as __namedExportsOrder,p as default};
