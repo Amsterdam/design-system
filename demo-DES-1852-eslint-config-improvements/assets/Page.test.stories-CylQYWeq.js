df3e0eddedefaa59aa3872fa875615029783c8c6
RubenSibon <mail+github@rubensibon.nl>
2026-05-21 09:45:47 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@05eb68970cd9b569101324c982690712d0595103 🚀

---
+++ demo-DES-1852-eslint-config-improvements/assets/Page.test.stories-CylQYWeq.js
@@ -0,0 +1,6 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-CwKyW3r_.js";import{O as r,t as i}from"./src-DoSU5a4-.js";import a,{n as o}from"./Page.stories-Bvd5B_uV.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Containers/Page`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Page, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
