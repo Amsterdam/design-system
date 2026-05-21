8ef0eec1b2b24cbdf208b89ae5065f0b3573c405
RubenSibon <mail+github@rubensibon.nl>
2026-05-21 21:10:51 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@acfae7747bc4f2a8c7f1b99ffa6e0a2d8715169c 🚀

---
+++ demo-DES-1859-workflow-to-cleanup-gh-pages-deploys/assets/Switch.test.stories-BqB915wo.js
@@ -0,0 +1,6 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-D7PAF6T2.js";import{t as r,u as i}from"./src-CpOfzieW.js";import a,{n as o}from"./Switch.stories-CIw8WUoI.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/Forms/Switch`},c={render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Switch, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
