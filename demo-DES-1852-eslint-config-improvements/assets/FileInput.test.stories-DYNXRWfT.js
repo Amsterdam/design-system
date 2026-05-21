d338d43154da91dfce06d19572da97ba3ebc3523
RubenSibon <mail+github@rubensibon.nl>
2026-05-21 15:16:38 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@6dc819e10bec6646297bf656a13c9587e4139b55 🚀

---
+++ demo-DES-1852-eslint-config-improvements/assets/FileInput.test.stories-DYNXRWfT.js
@@ -0,0 +1,7 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-ECdsBGoL.js";import{K as r,t as i}from"./src-B85Bp66m.js";import a,{n as o}from"./FileInput.stories-CMJlUcQy.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/File Input`},c={render:(e,n)=>t(r,{args:e,variants:[`disabled`,`multiple`]},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(FileInput, {
+    args,
+    variants: ['disabled', 'multiple']
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
--- demo-DES-1852-eslint-config-improvements/assets/Page.test.stories-9j37bJew.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-BWnwIShx.js";import{O as r,t as i}from"./src-csVTS6bO.js";import a,{n as o}from"./Page.stories-DisBJ9Lo.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Containers/Page`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Page, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
