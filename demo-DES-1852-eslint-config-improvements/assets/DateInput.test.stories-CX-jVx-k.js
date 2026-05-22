019feac8e43c10e364d2ced18d62074c979a92ba
RubenSibon <mail+github@rubensibon.nl>
2026-05-22 08:24:42 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@c9cd6620fe29725705ef9bd7d998ab4a219d8005 🚀

---
+++ demo-DES-1852-eslint-config-improvements/assets/DateInput.test.stories-CX-jVx-k.js
@@ -0,0 +1,6 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-B_6ZsuKT.js";import{$ as r,t as i}from"./src-D4VbIxbb.js";import a,{n as o}from"./DateInput.stories-D49FJ5BN.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Date Input`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(DateInput, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
---
+++ demo-DES-1852-eslint-config-improvements/assets/Row.test.stories-D7x8iX1i.js
@@ -0,0 +1,9 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{Vt as t}from"./iframe-CZmb9BFW.js";import{n,t as r}from"./renderComponentVariants-B_6ZsuKT.js";import{t as i,xt as a}from"./src-D4VbIxbb.js";import o,{n as s}from"./Row.stories-CAPWg5s5.js";var c,l,u,d;e((()=>{i(),r(),s(),c=t(),l={...o,title:`Components/Layout/Row`},u={args:{children:[(0,c.jsx)(`div`,{className:`_ams-item`},1),(0,c.jsx)(`div`,{className:`_ams-item`},2),(0,c.jsx)(`div`,{className:`_ams-item`},3)]},render:(e,t)=>n(a,{args:e},t),tags:[`!dev`,`!autodocs`]},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<div className="_ams-item" key={1} />, <div className="_ams-item" key={2} />, <div className="_ams-item" key={3} />]
+  },
+  render: (args, context) => renderComponentVariants(Row, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...u.parameters?.docs?.source}}},d=[`Test`]}))();export{u as Test,d as __namedExportsOrder,l as default};
