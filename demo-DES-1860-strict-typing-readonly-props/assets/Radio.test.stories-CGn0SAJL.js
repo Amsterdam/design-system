ff0ad24eef3b66f17537084e6493bceb87c0d1a1
RubenSibon <mail+github@rubensibon.nl>
2026-05-22 09:39:05 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@0c700c559b215f0b95f5ff7c77f00cb066f4be47 🚀

---
+++ demo-DES-1860-strict-typing-readonly-props/assets/Radio.test.stories-CGn0SAJL.js
@@ -0,0 +1,6 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-ChEJxd-e.js";import{t as r,v as i}from"./src-Bu7QFj3U.js";import a,{n as o}from"./Radio.stories-BQ2XT50t.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/Forms/Radio`},c={render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Radio, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
--- demo-DES-1860-strict-typing-readonly-props/assets/mdx-react-shim-DcN7hKZ-.js
+++
@@ -1,1 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{t}from"./react-DdXJvipq.js";var n=e((()=>{t()}));export{n as t};
