a13b9551c52f754450e018774073f6bee5208cdd
RubenSibon <mail+github@rubensibon.nl>
2026-05-22 21:32:34 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@21046753f94f062fe4cc03aac62bca6009a6666e 🚀

---
+++ demo-DES-1522-bug-report-and-feat-request-templates/assets/Icon.test.stories-B7gXV4tA.js
@@ -0,0 +1,6 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-BO1pcMcx.js";import{Dt as r,t as i}from"./src-DXCZ0o4O.js";import a,{n as o}from"./Icon.stories-CRNnKHJB.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Media/Icon`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Icon, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
