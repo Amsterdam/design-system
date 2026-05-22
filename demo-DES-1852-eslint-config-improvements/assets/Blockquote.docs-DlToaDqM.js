244df8b27e0aa2b438fcad02dc2470984d3d944e
RubenSibon <mail+github@rubensibon.nl>
2026-05-22 10:22:07 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@dac1f619def94c2ffea142630d1e7e490f6f9612 🚀

---
+++ demo-DES-1852-eslint-config-improvements/assets/Blockquote.docs-DlToaDqM.js
@@ -0,0 +1,36 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{Vt as t}from"./iframe-DSpmQQ8W.js";import{r as n}from"./react-BVPoE5BP.js";import{c as r,f as i,i as a,l as o,n as s,s as c}from"./blocks-FGZ70S_v.js";import{n as l,t as u}from"./DesignTokensTable-DfLvWQRs.js";import{t as d}from"./mdx-react-shim-4RF0IwNx.js";import{InverseColour as f,n as p,t as m}from"./Blockquote.stories-BsW9HQYJ.js";var h,g=e((()=>{h=`<!-- @license CC0-1.0 -->
+
+# Blockquote
+
+We use blockquotes to emphasize citations.
+A quote appears differently from the main text to make it stand out.
+
+## Guidelines
+
+- Quote someone’s literal written or oral text.
+- A quote has a width of half or a whole column of text.
+- The component adds the correct quotation marks.
+- Use [Margin](/docs/utilities-css-margin--docs) or [Prose](/docs/utilities-css-prose--docs) to add vertical whitespace.
+
+## Relevant WCAG requirements
+
+- [WCAG 3.1.2](https://www.w3.org/WAI/WCAG21/Understanding/language-of-parts.html): if the citation is in a language other than that of the surrounding content, indicate so through the \`lang\` attribute.
+`})),_,v,y=e((()=>{_={blockquote:{color:{$value:`{ams.color.text.default}`,$extensions:{"nl.amsterdam.type":`color`}},"font-family":{$value:`{ams.typography.font-family}`,$extensions:{"nl.amsterdam.type":`fontFamily`}},"font-size":{$value:`{ams.typography.body-text.x-large.font-size}`,$extensions:{"nl.amsterdam.type":`fontSize`}},"font-weight":{$value:`{ams.typography.body-text.bold.font-weight}`,$extensions:{"nl.amsterdam.type":`fontWeight`}},"line-height":{$value:`{ams.typography.body-text.x-large.line-height}`,$extensions:{"nl.amsterdam.subtype":`lineHeight`,"nl.amsterdam.type":`number`}},inverse:{color:{$value:`{ams.color.text.inverse}`,$extensions:{"nl.amsterdam.type":`color`}}}}},v={ams:_}}));function b(e){let t={a:`a`,h2:`h2`,h3:`h3`,li:`li`,p:`p`,ul:`ul`,...n(),...e.components};return(0,S.jsxs)(S.Fragment,{children:[`
+`,`
+`,`
+`,`
+`,(0,S.jsx)(r,{of:m}),`
+`,(0,S.jsx)(c,{children:h}),`
+`,(0,S.jsx)(o,{}),`
+`,(0,S.jsx)(a,{}),`
+`,(0,S.jsx)(t.h2,{id:`examples`,children:`Examples`}),`
+`,(0,S.jsx)(t.h3,{id:`inverse-colour`,children:`Inverse colour`}),`
+`,(0,S.jsx)(t.p,{children:`Use the inverse colour if the Blockquote sits on a dark background.
+This ensures the colour of the text provides enough contrast.`}),`
+`,(0,S.jsx)(s,{of:f}),`
+`,(0,S.jsx)(t.h2,{id:`see-also`,children:`See also`}),`
+`,(0,S.jsxs)(t.ul,{children:[`
+`,(0,S.jsxs)(t.li,{children:[(0,S.jsx)(t.a,{href:`/docs/utilities-css-prose--docs`,children:`Prose`}),` – applies the recommended vertical spacing for editorial content.`]}),`
+`]}),`
+`,(0,S.jsx)(t.h2,{id:`design-tokens`,children:`Design tokens`}),`
+`,(0,S.jsx)(u,{tokens:v})]})}function x(e={}){let{wrapper:t}={...n(),...e.components};return t?(0,S.jsx)(t,{...e,children:(0,S.jsx)(b,{...e})}):b(e)}var S;e((()=>{S=t(),d(),i(),p(),g(),y(),l()}))();export{x as default};
--- demo-DES-1852-eslint-config-improvements/assets/StandaloneLink.test.stories-KdeYRSeO.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-B_6ZsuKT.js";import{d as r,t as i}from"./src-D4VbIxbb.js";import a,{n as o}from"./StandaloneLink.stories-Dnb1PbWJ.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/StandaloneLink`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(StandaloneLink, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
