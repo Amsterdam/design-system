aaaec69b9520fb47aaad4a57d368e78df55110b4
RubenSibon <mail+github@rubensibon.nl>
2026-05-22 11:24:47 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@d074c230e4d0875977644118e427d6004b0495eb 🚀

--- demo-DES-1859-workflow-to-cleanup-gh-pages-deploys/assets/ActionGroup.test.stories-CrOXaVus.js
+++
@@ -1,9 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{Vt as t}from"./iframe-Cjodi8H-.js";import{n,t as r}from"./renderComponentVariants-B8OZ6Vsm.js";import{Tt as i,t as a}from"./src-CEXTVSvP.js";import o,{n as s}from"./ActionGroup.stories-8A55zMiT.js";var c,l,u,d;e((()=>{a(),r(),s(),c=t(),l={...o,title:`Components/Layout/Action Group`},u={args:{children:[(0,c.jsx)(`button`,{children:`Stoppen`},1),(0,c.jsx)(`button`,{children:`Verder`},2)]},render:(e,t)=>n(i,{args:e},t),tags:[`!dev`,`!autodocs`]},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: [<button key={1}>Stoppen</button>, <button key={2}>Verder</button>]
-  },
-  render: (args, context) => renderComponentVariants(ActionGroup, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...u.parameters?.docs?.source}}},d=[`Test`]}))();export{u as Test,d as __namedExportsOrder,l as default};
--- demo-DES-1859-workflow-to-cleanup-gh-pages-deploys/assets/FileInput.test.stories-B_CPR4pn.js
+++
@@ -1,7 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-B8OZ6Vsm.js";import{K as r,t as i}from"./src-CEXTVSvP.js";import a,{n as o}from"./FileInput.stories-DEQCPMCV.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/File Input`},c={render:(e,n)=>t(r,{args:e,variants:[`disabled`,`multiple`]},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(FileInput, {
-    args,
-    variants: ['disabled', 'multiple']
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
