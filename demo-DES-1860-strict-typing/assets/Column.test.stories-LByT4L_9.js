65c7939a1e4170b2a65aa21ef04ebbc265cc9a59
RubenSibon <mail+github@rubensibon.nl>
2026-05-22 13:39:12 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@d2e2171fffab47fa1f702630c6bcaa8f41d4bdf8 🚀

---
+++ demo-DES-1860-strict-typing/assets/Column.test.stories-LByT4L_9.js
@@ -0,0 +1,7 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-CuJKL0mX.js";import{et as r,t as i}from"./src-CSRdYhsZ.js";import a,{n as o}from"./Column.stories-COp8eE5b.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Layout/Column`},c={render:(e,n)=>t(r,{args:e,layout:`grid`},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Column, {
+    args,
+    layout: 'grid'
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
