c56c6b3a64a72e531231d3fbd1aedcca46c166be
RubenSibon <mail+github@rubensibon.nl>
2026-05-22 08:56:03 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@6dc819e10bec6646297bf656a13c9587e4139b55 🚀

---
+++ demo-DES-1860-strict-typing-readonly-props/assets/HomePage.docs-B2E-464J.js
@@ -0,0 +1,6 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{Vt as t}from"./iframe-SRVViyZW.js";import{r as n}from"./react-DdXJvipq.js";import{c as r,f as i}from"./blocks-BpPSh7LL.js";import{t as a}from"./mdx-react-shim-DcN7hKZ-.js";import{n as o,t as s}from"./HomePage.stories-BfIIBAp1.js";function c(e){let t={h1:`h1`,p:`p`,...n(),...e.components};return(0,u.jsxs)(u.Fragment,{children:[`
+`,`
+`,(0,u.jsx)(r,{of:s}),`
+`,(0,u.jsx)(t.h1,{id:`home-page`,children:`Home page`}),`
+`,(0,u.jsx)(t.p,{children:`The home page of an internal website can have varying types of content.`}),`
+`,(0,u.jsx)(t.p,{children:`Our single example in this category currently focuses on the overall layout.`})]})}function l(e={}){let{wrapper:t}={...n(),...e.components};return t?(0,u.jsx)(t,{...e,children:(0,u.jsx)(c,{...e})}):c(e)}var u;e((()=>{u=t(),a(),i(),o()}))();export{l as default};
