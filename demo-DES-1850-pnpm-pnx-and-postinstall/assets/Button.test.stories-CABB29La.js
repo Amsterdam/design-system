88c4024fe7f005ea4197770400d333e7b85aeb55
RubenSibon <mail+github@rubensibon.nl>
2026-05-22 10:01:43 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@f635d76d1dcec6765fef38cc8c3a8c85cdb4b562 🚀

---
+++ demo-DES-1850-pnpm-pnx-and-postinstall/assets/Button.test.stories-CABB29La.js
@@ -0,0 +1,7 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-BYTU3Iiw.js";import{t as r,ut as i}from"./src-B9rJtIFz.js";import a,{n as o}from"./Button.stories-CBgkEZ2D.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/Buttons/Button`},c={render:(e,n)=>t(i,{args:e,variants:[`disabled`,`hovered`]},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Button, {
+    args,
+    variants: ['disabled', 'hovered']
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
---
+++ demo-DES-1850-pnpm-pnx-and-postinstall/assets/Grid.test.stories-DikllzOw.js
@@ -0,0 +1,9 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{Vt as t}from"./iframe-CL5XqyGo.js";import{n,t as r}from"./renderComponentVariants-BYTU3Iiw.js";import{W as i,t as a}from"./src-B9rJtIFz.js";import o,{n as s}from"./Grid.stories-CPO8dMx1.js";var c,l,u,d;e((()=>{a(),r(),s(),c=t(),l={...o,title:`Components/Layout/Grid`},u={args:{children:[(0,c.jsx)(i.Cell,{span:`all`},1),(0,c.jsx)(i.Cell,{span:`all`},2)]},render:(e,t)=>n(i,{args:e},t),tags:[`!dev`,`!autodocs`]},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<Grid.Cell key={1} span="all" />, <Grid.Cell key={2} span="all" />]
+  },
+  render: (args, context) => renderComponentVariants(Grid, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...u.parameters?.docs?.source}}},d=[`Test`]}))();export{u as Test,d as __namedExportsOrder,l as default};
