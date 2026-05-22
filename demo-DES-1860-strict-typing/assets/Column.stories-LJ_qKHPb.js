8011f0d139df7c47a6213eca1c2f5f6e1c2470d7
RubenSibon <mail+github@rubensibon.nl>
2026-05-22 09:40:33 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@6dc819e10bec6646297bf656a13c9587e4139b55 🚀

---
+++ demo-DES-1860-strict-typing/assets/Column.stories-LJ_qKHPb.js
@@ -0,0 +1,33 @@
+import{n as e,r as t}from"./chunk-DnJy8xQt.js";import{F as n,Vt as r,Y as i,b as a,u as o}from"./iframe-CPnnHh7X.js";import{et as s,nt as c,t as l,tt as u}from"./src-D-unc1lw.js";import{a as d,i as f,r as p}from"./types-Bk-hRTdb.js";var m=t({Alignment:()=>v,Default:()=>_,HorizontalAlignment:()=>y,ImproveSemantics:()=>b,__namedExportsOrder:()=>x,default:()=>g}),h,g,_,v,y,b,x,S=e((()=>{i(),l(),c(),f(),h=r(),g={title:`Components/Layout/Column`,component:s,args:{children:[(0,h.jsx)(`div`,{className:`_ams-item`},0),(0,h.jsx)(`div`,{className:`_ams-item`},1),(0,h.jsx)(`div`,{className:`_ams-item`},2)],className:`_ams-column`},argTypes:{align:{control:{labels:{undefined:`start`},type:`radio`},options:[void 0,...d]},alignHorizontal:{control:{labels:{undefined:`stretch`},type:`radio`},options:[void 0,...p]},gap:{control:{labels:{undefined:`medium`},type:`radio`},options:[void 0,...u]}}},_={},v={args:{align:`center`}},y={args:{alignHorizontal:`center`}},b={args:{as:`section`,children:[(0,h.jsx)(a,{level:1,children:`Zoekresultaten`},1),(0,h.jsxs)(o,{children:[(0,h.jsx)(o.Heading,{level:2,children:(0,h.jsx)(o.Link,{href:`#`,children:`Nieuwe manieren om afval op te halen`})}),(0,h.jsx)(n,{children:`Afvalboten, bakfietsen en ondergrondse containers. We experimenteren met nieuwe manieren om afval op te halen in het centrum.`})]},2),(0,h.jsxs)(o,{children:[(0,h.jsx)(o.Heading,{level:2,children:(0,h.jsx)(o.Link,{href:`#`,children:`Verlenging proef ophalen afval per boot`})}),(0,h.jsx)(n,{children:`Een proef met het anders ophalen van afval. We halen vuilniszakken hier op met kleine wagentjes, kleine vuilniswagens en een afvalboot.`})]},3)],className:void 0}},_.parameters={..._.parameters,docs:{..._.parameters?.docs,source:{originalSource:`{}`,..._.parameters?.docs?.source}}},v.parameters={...v.parameters,docs:{...v.parameters?.docs,source:{originalSource:`{
+  args: {
+    align: 'center'
+  }
+}`,...v.parameters?.docs?.source}}},y.parameters={...y.parameters,docs:{...y.parameters?.docs,source:{originalSource:`{
+  args: {
+    alignHorizontal: 'center'
+  }
+}`,...y.parameters?.docs?.source}}},b.parameters={...b.parameters,docs:{...b.parameters?.docs,source:{originalSource:`{
+  args: {
+    as: 'section',
+    children: [<Heading key={1} level={1}>
+        Zoekresultaten
+      </Heading>, <Card key={2}>
+        <Card.Heading level={2}>
+          <Card.Link href="#">Nieuwe manieren om afval op te halen</Card.Link>
+        </Card.Heading>
+        <Paragraph>
+          Afvalboten, bakfietsen en ondergrondse containers. We experimenteren met nieuwe manieren om afval op te halen
+          in het centrum.
+        </Paragraph>
+      </Card>, <Card key={3}>
+        <Card.Heading level={2}>
+          <Card.Link href="#">Verlenging proef ophalen afval per boot</Card.Link>
+        </Card.Heading>
+        <Paragraph>
+          Een proef met het anders ophalen van afval. We halen vuilniszakken hier op met kleine wagentjes, kleine
+          vuilniswagens en een afvalboot.
+        </Paragraph>
+      </Card>],
+    className: undefined
+  }
+}`,...b.parameters?.docs?.source}}},x=[`Default`,`Alignment`,`HorizontalAlignment`,`ImproveSemantics`]}));S();export{v as Alignment,_ as Default,y as HorizontalAlignment,b as ImproveSemantics,x as __namedExportsOrder,g as default,S as n,m as t};
---
+++ demo-DES-1860-strict-typing/assets/FileList.test.stories-DQDatfE9.js
@@ -0,0 +1,11 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{Vt as t}from"./iframe-CPnnHh7X.js";import{n,t as r}from"./renderComponentVariants-DDS2xUvD.js";import{G as i,t as a}from"./src-D-unc1lw.js";import o,{n as s}from"./FileList.stories-eR378eOs.js";var c,l,u,d;e((()=>{a(),r(),s(),c=t(),l={...o,title:`Components/Forms/File List`},u={args:{children:(0,c.jsx)(i.Item,{file:new File([`sample1`],`sample1.txt`,{type:`text/plain`})})},render:(e,t)=>n(i,{args:e},t),tags:[`!dev`,`!autodocs`]},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: <FileList.Item file={new File(['sample1'], 'sample1.txt', {
+      type: 'text/plain'
+    })} />
+  },
+  render: (args, context) => renderComponentVariants(FileList, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...u.parameters?.docs?.source}}},d=[`Test`]}))();export{u as Test,d as __namedExportsOrder,l as default};
