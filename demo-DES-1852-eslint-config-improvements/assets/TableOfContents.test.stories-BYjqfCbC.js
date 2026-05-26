e46fe14f979667c64147af7f4f484b55520379f4
GitHub <github-action@users.noreply.github.com>
2026-05-26 08:07:28 +0000

    Remove feature branch folder

--- demo-DES-1852-eslint-config-improvements/assets/TableOfContents.test.stories-BYjqfCbC.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-yTnrsUuX.js";import{c as r,t as i}from"./src-e0Y2p9Un.js";import a,{n as o}from"./TableOfContents.stories-BhF2sZ-f.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/TableOfContents`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(TableOfContents, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
