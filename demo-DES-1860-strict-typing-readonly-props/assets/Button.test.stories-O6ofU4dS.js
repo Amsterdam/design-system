14b38b3788be05b3c980a4e6dc760617ef82c695
RubenSibon <mail+github@rubensibon.nl>
2026-05-26 12:57:57 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@b063a9ed3fbfeb67b258b299703823af5cd84b94 🚀

--- demo-DES-1860-strict-typing-readonly-props/assets/Button.test.stories-O6ofU4dS.js
+++
@@ -1,7 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-DwbW9oUf.js";import{t as r,ut as i}from"./src-HDap4C34.js";import a,{n as o}from"./Button.stories-C39JAuPx.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/Buttons/Button`},c={render:(e,n)=>t(i,{args:e,variants:[`disabled`,`hovered`]},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Button, {
-    args,
-    variants: ['disabled', 'hovered']
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
---
+++ demo-DES-1860-strict-typing-readonly-props/assets/Logo.test.stories-DGysVHHV.js
@@ -0,0 +1,6 @@
+import{a as e,n as t}from"./chunk-DnJy8xQt.js";import{Vt as n,wn as r}from"./iframe-DsIwlaGH.js";import{F as i,N as a,P as o,t as s}from"./src-Dx-YpMjM.js";import c,{n as l}from"./Logo.stories-BXfCDABE.js";var u,d,f,p,m;t((()=>{s(),o(),l(),u=e(r()),d=n(),f={...c,title:`Components/Media/Logo`},p={render:e=>(0,d.jsx)(`div`,{className:`_ams-tests-grid`,children:[void 0,...i].map(t=>(0,u.createElement)(a,{...e,brand:t,key:t}))}),tags:[`!dev`,`!autodocs`]},p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
+  render: args => <div className="_ams-tests-grid">
+      {[undefined, ...logoBrands].map(brand => <Logo {...args} brand={brand} key={brand} />)}
+    </div>,
+  tags: ['!dev', '!autodocs']
+}`,...p.parameters?.docs?.source}}},m=[`Test`]}))();export{p as Test,m as __namedExportsOrder,f as default};
