46adfe4e50136377f578ee5b8d67f5d7070a8ed7
RubenSibon <mail+github@rubensibon.nl>
2026-05-26 12:52:22 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@b8e961a6faf745ab7e9db69113086625db83ed1b 🚀

--- demo-DES-1860-strict-typing-readonly-props/assets/Avatar.test.stories-j88iyChe.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-raCe96rW.js";import{_t as r,t as i}from"./src-Ce5wfHX6.js";import a,{n as o}from"./Avatar.stories-Bx0URCJs.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Feedback/Avatar`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Avatar, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
