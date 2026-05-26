30ca434b72c74f0b916e87326666c8372f93d4d8
RubenSibon <mail+github@rubensibon.nl>
2026-05-26 14:23:09 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@09516a79d58121ac9abe884b2d7ab00e3a4e578a 🚀

--- demo-DES-1806-inline-size-containment-context-utility/assets/Grid.test.stories-CmHhhpuF.js
+++
@@ -1,9 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{Vt as t}from"./iframe-cGflhH8R.js";import{n,t as r}from"./renderComponentVariants-CWl22YPk.js";import{W as i,t as a}from"./src-CqAOkU4S.js";import o,{n as s}from"./Grid.stories-BnIkW71n.js";var c,l,u,d;e((()=>{a(),r(),s(),c=t(),l={...o,title:`Components/Layout/Grid`},u={args:{children:[(0,c.jsx)(i.Cell,{span:`all`},1),(0,c.jsx)(i.Cell,{span:`all`},2)]},render:(e,t)=>n(i,{args:e},t),tags:[`!dev`,`!autodocs`]},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: [<Grid.Cell key={1} span="all" />, <Grid.Cell key={2} span="all" />]
-  },
-  render: (args, context) => renderComponentVariants(Grid, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...u.parameters?.docs?.source}}},d=[`Test`]}))();export{u as Test,d as __namedExportsOrder,l as default};
---
+++ demo-DES-1806-inline-size-containment-context-utility/assets/Row.test.stories-D9JKVRd8.js
@@ -0,0 +1,9 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{Vt as t}from"./iframe-D7f_LL1C.js";import{n,t as r}from"./renderComponentVariants-irxBTYX1.js";import{t as i,xt as a}from"./src-C2ln0oa_.js";import o,{n as s}from"./Row.stories-C3-Ad-rX.js";var c,l,u,d;e((()=>{i(),r(),s(),c=t(),l={...o,title:`Components/Layout/Row`},u={args:{children:[(0,c.jsx)(`div`,{className:`_ams-item`},1),(0,c.jsx)(`div`,{className:`_ams-item`},2),(0,c.jsx)(`div`,{className:`_ams-item`},3)]},render:(e,t)=>n(a,{args:e},t),tags:[`!dev`,`!autodocs`]},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<div className="_ams-item" key={1} />, <div className="_ams-item" key={2} />, <div className="_ams-item" key={3} />]
+  },
+  render: (args, context) => renderComponentVariants(Row, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...u.parameters?.docs?.source}}},d=[`Test`]}))();export{u as Test,d as __namedExportsOrder,l as default};
