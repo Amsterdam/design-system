7b4511dd57c7ca25590c24c7b6e6f0b19b4e1ce8
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-05-26 08:04:39 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@2f5ef17a658afbc048cc7af8a2cf5f6949d7f187 🚀

---
+++ demo-DES-1852-eslint-config-improvements/assets/Button.test.stories-D0Mub_Im.js
@@ -0,0 +1,7 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-yTnrsUuX.js";import{t as r,ut as i}from"./src-e0Y2p9Un.js";import a,{n as o}from"./Button.stories-D1eh3Dsw.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/Buttons/Button`},c={render:(e,n)=>t(i,{args:e,variants:[`disabled`,`hovered`]},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Button, {
+    args,
+    variants: ['disabled', 'hovered']
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
