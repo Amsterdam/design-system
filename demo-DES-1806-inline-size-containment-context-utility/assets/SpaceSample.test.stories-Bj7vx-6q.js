aeb711e2fd613d82cbcd744b583e22d69a500c85
RubenSibon <mail+github@rubensibon.nl>
2026-05-26 13:23:58 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@13d83bd3f007e7fb0946c8cac09f3ca300a8c338 🚀

--- demo-DES-1806-inline-size-containment-context-utility/assets/SpaceSample.test.stories-Bj7vx-6q.js
+++
@@ -1,7 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{Vt as t}from"./iframe-D8SeYqVt.js";import{n,t as r}from"./SpaceSample-BLCq3Tsu.js";var i,a,o,s;e((()=>{n(),i=t(),a={title:`Components/Docs/Space Sample`},o={render:()=>(0,i.jsxs)(`div`,{className:`_ams-tests-grid`,children:[(0,i.jsx)(r,{value:`16px`}),(0,i.jsx)(r,{})]}),tags:[`!dev`,`!autodocs`]},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
-  render: () => <div className="_ams-tests-grid">
-      <SpaceSample value="16px" />
-      <SpaceSample />
-    </div>,
-  tags: ['!dev', '!autodocs']
-}`,...o.parameters?.docs?.source}}},s=[`Test`]}))();export{o as Test,s as __namedExportsOrder,a as default};
