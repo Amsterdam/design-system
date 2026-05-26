cf6d81eb5b49188f73f84d42760e2cb08ed33aac
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-05-26 22:16:29 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@466625dabfd4f6324ffb56f1583620c6e806a249 🚀

---
+++ demo-develop/assets/AspectRatioSample-DXJj9Xfy.js
@@ -0,0 +1,1 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{Bt as t,Vt as n,zt as r}from"./iframe-DfR5stV9.js";import{n as i,t as a}from"./formatTokenValue-DDc3tUBi.js";var o=e((()=>{})),s,c,l=e((()=>{o(),t(),i(),s=n(),c=({className:e,style:t,value:n,...i})=>(0,s.jsx)(`div`,{...i,className:r(`_ams-aspect-ratio-sample`,`sb-unstyled`,e),style:{...t,aspectRatio:a(n)}})}));export{l as n,c as t};
---
+++ demo-develop/assets/FileList.test.stories-BaSC9MmT.js
@@ -0,0 +1,11 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{Vt as t}from"./iframe-DfR5stV9.js";import{n,t as r}from"./renderComponentVariants-BOLKL5L9.js";import{G as i,t as a}from"./src-CusC1ro0.js";import o,{n as s}from"./FileList.stories-C-FodHIW.js";var c,l,u,d;e((()=>{a(),r(),s(),c=t(),l={...o,title:`Components/Forms/File List`},u={args:{children:(0,c.jsx)(i.Item,{file:new File([`sample1`],`sample1.txt`,{type:`text/plain`})})},render:(e,t)=>n(i,{args:e},t),tags:[`!dev`,`!autodocs`]},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: <FileList.Item file={new File(['sample1'], 'sample1.txt', {
+      type: 'text/plain'
+    })} />
+  },
+  render: (args, context) => renderComponentVariants(FileList, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...u.parameters?.docs?.source}}},d=[`Test`]}))();export{u as Test,d as __namedExportsOrder,l as default};
