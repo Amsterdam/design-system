049c136a3020f53886d37c4b9ba52b8873bc9cfd
RubenSibon <mail+github@rubensibon.nl>
2026-05-26 14:38:52 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@dad66bab235357e9068b38d268779c63fa3db9ba 🚀

--- demo-DES-1860-strict-typing/assets/Overlap.test.stories-3SnK7-Zx.js
+++
@@ -1,9 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{Vt as t}from"./iframe-C6SG-8hB.js";import{n,t as r}from"./renderComponentVariants-CN_I-77i.js";import{k as i,t as a}from"./src-CZPnCfH9.js";import{i as o,r as s}from"./Overlap.stories-CrI2boFb.js";var c,l,u,d;e((()=>{a(),r(),s(),c=t(),l={...o,title:`Components/Layout/Overlap`},u={args:{children:[(0,c.jsx)(`img`,{alt:``,src:`https://picsum.photos/id/122/1280/720`},1),(0,c.jsx)(`p`,{children:`Overlap test`},2)]},render:(e,t)=>n(i,{args:e},t),tags:[`!dev`,`!autodocs`]},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: [<img alt="" key={1} src="https://picsum.photos/id/122/1280/720" />, <p key={2}>Overlap test</p>]
-  },
-  render: (args, context) => renderComponentVariants(Overlap, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...u.parameters?.docs?.source}}},d=[`Test`]}))();export{u as Test,d as __namedExportsOrder,l as default};
---
+++ demo-DES-1860-strict-typing/assets/Pagination.test.stories-DoEGtrOc.js
@@ -0,0 +1,6 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-ZzuwtYqZ.js";import{C as r,t as i}from"./src-cz1uX4Dn.js";import a,{n as o}from"./Pagination.stories-CPemw8h8.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Navigation/Pagination`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Pagination, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
