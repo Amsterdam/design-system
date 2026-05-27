1b19962926cb81f2c078fa1bb04da124de8782cf
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-05-27 07:50:50 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@820b5b98f9ba300bc506444d2268cdc8d259db79 🚀

---
+++ demo-DES-1860-strict-typing-readonly-props/assets/Card.test.stories-C3hUL9l_.js
@@ -0,0 +1,19 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{Vt as t}from"./iframe-nW0IG1dj.js";import{at as n,t as r}from"./src-2Nd2Nubx.js";import i,{n as a}from"./Card.stories-bTYTaM69.js";var o,s,c,l;e((()=>{r(),a(),o=t(),s={...i,title:`Components/Navigation/Card`},c={render:e=>(0,o.jsxs)(n,{...e,style:{maxInlineSize:`24rem`},children:[(0,o.jsx)(n.Image,{alt:``,aspectRatio:`4:3`,src:`https://picsum.photos/id/122/1280/720`}),(0,o.jsx)(n.HeadingGroup,{tagline:`Nieuws`,children:(0,o.jsxs)(n.Heading,{level:3,children:[(0,o.jsx)(n.Link,{href:`/`,children:`Nederlands eerste houten woonwijk komt in Zuidoost`}),(0,o.jsx)(n.Link,{className:`hover`,href:`/`,children:`Nederlands eerste houten woonwijk komt in Zuidoost`})]})}),(0,o.jsx)(`div`,{children:(0,o.jsx)(`p`,{children:`We bouwen een levendige, groene en duurzame woonbuurt tussen de Gooiseweg en het Nelson Mandelapark.`})})]}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: args => <Card {...args} style={{
+    maxInlineSize: '24rem'
+  }}>
+      <Card.Image alt="" aspectRatio="4:3" src="https://picsum.photos/id/122/1280/720" />
+      <Card.HeadingGroup tagline="Nieuws">
+        <Card.Heading level={3}>
+          <Card.Link href="/">Nederlands eerste houten woonwijk komt in Zuidoost</Card.Link>
+          <Card.Link className="hover" href="/">
+            Nederlands eerste houten woonwijk komt in Zuidoost
+          </Card.Link>
+        </Card.Heading>
+      </Card.HeadingGroup>
+      <div>
+        <p>We bouwen een levendige, groene en duurzame woonbuurt tussen de Gooiseweg en het Nelson Mandelapark.</p>
+      </div>
+    </Card>,
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
---
+++ demo-DES-1860-strict-typing-readonly-props/assets/FileList.test.stories-Cu1PzJuF.js
@@ -0,0 +1,11 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{Vt as t}from"./iframe-nW0IG1dj.js";import{n,t as r}from"./renderComponentVariants-B-qPDZOP.js";import{G as i,t as a}from"./src-2Nd2Nubx.js";import o,{n as s}from"./FileList.stories-D_tVyZ29.js";var c,l,u,d;e((()=>{a(),r(),s(),c=t(),l={...o,title:`Components/Forms/File List`},u={args:{children:(0,c.jsx)(i.Item,{file:new File([`sample1`],`sample1.txt`,{type:`text/plain`})})},render:(e,t)=>n(i,{args:e},t),tags:[`!dev`,`!autodocs`]},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: <FileList.Item file={new File(['sample1'], 'sample1.txt', {
+      type: 'text/plain'
+    })} />
+  },
+  render: (args, context) => renderComponentVariants(FileList, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...u.parameters?.docs?.source}}},d=[`Test`]}))();export{u as Test,d as __namedExportsOrder,l as default};
