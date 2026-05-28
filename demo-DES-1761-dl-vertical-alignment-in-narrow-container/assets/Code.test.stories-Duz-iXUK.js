a405a827e35dd7bb24c2954914eca8f138c9d127
RubenSibon <mail+github@rubensibon.nl>
2026-05-28 11:58:21 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@6f217989e974574141465cc9f9446b2b839ff925 🚀

--- demo-DES-1761-dl-vertical-alignment-in-narrow-container/assets/Code.test.stories-Duz-iXUK.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-CdXeta_2.js";import{n as r,t as i}from"./Code-CJeiz6Xb.js";var a,o,s;e((()=>{n(),r(),a={title:`Components/Docs/Code`,args:{children:[`var(--ams-color-text)`]}},o={render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`]},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Code, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...o.parameters?.docs?.source}}},s=[`Test`]}))();export{o as Test,s as __namedExportsOrder,a as default};
--- demo-DES-1761-dl-vertical-alignment-in-narrow-container/assets/Heading.test.stories-FYXqvZTe.js
+++
@@ -1,9 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-CdXeta_2.js";import{At as r,t as i}from"./src-Ar34S4Un.js";import a,{n as o}from"./Heading.stories-D7dUB4Y7.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Text/Heading`},c={args:{children:`Amsterdam Light Festival maakt de stad ook deze winter een beetje mooier`},render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: 'Amsterdam Light Festival maakt de stad ook deze winter een beetje mooier'
-  },
-  render: (args, context) => renderComponentVariants(Heading, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
