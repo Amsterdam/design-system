18d16200d2705341239cee23a1088945669e0a81
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-05-28 11:45:34 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@089be2bafa6a4309bb57217a893c7d68bc710c47 🚀

---
+++ demo-DES-1806-inline-size-containment-context-utility/assets/Avatar.test.stories-B9Dp3wv8.js
@@ -0,0 +1,6 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-B-FfxTT_.js";import{_t as r,t as i}from"./src-BVflIKx0.js";import a,{n as o}from"./Avatar.stories-mRQZv4UD.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Feedback/Avatar`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Avatar, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
