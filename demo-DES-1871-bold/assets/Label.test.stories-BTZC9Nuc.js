85c7bb8dd42d978fbc34abe59dd7638cd5c8d9c7
GitHub <github-action@users.noreply.github.com>
2026-05-28 11:29:55 +0000

    Remove feature branch folder

--- demo-DES-1871-bold/assets/Label.test.stories-BTZC9Nuc.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-m2oJF93h.js";import{L as r,t as i}from"./src-BFAJRFGb.js";import a,{n as o}from"./Label.stories-MS45fzdh.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Label`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Label, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
