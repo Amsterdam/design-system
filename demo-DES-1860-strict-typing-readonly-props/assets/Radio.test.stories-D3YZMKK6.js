d3c3408b8403dca5af7c523f60942939b47d7c78
RubenSibon <mail+github@rubensibon.nl>
2026-05-28 07:47:36 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@26b5ca7c8333e063268af4125cf1148f81ab8e8e 🚀

--- demo-DES-1860-strict-typing-readonly-props/assets/Radio.test.stories-D3YZMKK6.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-C1jH6Icq.js";import{t as r,v as i}from"./src-DSJcBGZn.js";import a,{n as o}from"./Radio.stories-Dd0-w4aL.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/Forms/Radio`},c={render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Radio, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
