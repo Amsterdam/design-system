d7cbb2fbf59562b60d489a6071547189e0fb1b5d
RubenSibon <mail+github@rubensibon.nl>
2026-05-28 13:10:08 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@3c7982bccaf1eae189cbd1dc4abd5168759a30bd 🚀

--- demo-DES-1761-dl-vertical-alignment-in-narrow-container/assets/CallToActionLink.test.stories-CeLvXW_p.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-CkqPqHcP.js";import{lt as r,t as i}from"./src-CnP_oHNK.js";import a,{n as o}from"./CallToActionLink.stories-Briae7sp.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Navigation/Call to Action Link`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(CallToActionLink, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
--- demo-DES-1761-dl-vertical-alignment-in-narrow-container/assets/HomePage.docs-CJi-5hoz.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{Vt as t}from"./iframe-3SAru53b.js";import{r as n}from"./react-DCVqQjRI.js";import{c as r,f as i}from"./blocks-CLNJP2dd.js";import{t as a}from"./mdx-react-shim-q2d6PoCP.js";import{n as o,t as s}from"./HomePage.stories-Bq6hdWbY.js";function c(e){let t={h1:`h1`,p:`p`,...n(),...e.components};return(0,u.jsxs)(u.Fragment,{children:[`
-`,`
-`,(0,u.jsx)(r,{of:s}),`
-`,(0,u.jsx)(t.h1,{id:`home-page`,children:`Home page`}),`
-`,(0,u.jsx)(t.p,{children:`The home page of an internal website can have varying types of content.`}),`
-`,(0,u.jsx)(t.p,{children:`Our single example in this category currently focuses on the overall layout.`})]})}function l(e={}){let{wrapper:t}={...n(),...e.components};return t?(0,u.jsx)(t,{...e,children:(0,u.jsx)(c,{...e})}):c(e)}var u;e((()=>{u=t(),a(),i(),o()}))();export{l as default};
