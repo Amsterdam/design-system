dbb3c4f3b5e045c2b3c4b20bcfc29073251f4e47
RubenSibon <mail+github@rubensibon.nl>
2026-05-28 11:28:46 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@5ef8f1450f5ad03c8fb166783bba07a1da994496 🚀

---
+++ demo-DES-1522-bug-report-and-feat-request-templates/assets/Button.test.stories-47jlbsTV.js
@@ -0,0 +1,7 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-CmtvfYgE.js";import{t as r,ut as i}from"./src-CsRYwHqy.js";import a,{n as o}from"./Button.stories-B_iUsTGK.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/Buttons/Button`},c={render:(e,n)=>t(i,{args:e,variants:[`disabled`,`hovered`]},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Button, {
+    args,
+    variants: ['disabled', 'hovered']
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
--- demo-DES-1522-bug-report-and-feat-request-templates/assets/mdx-react-shim-V7-yJDUK.js
+++
@@ -1,1 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{t}from"./react-B_diMXC_.js";var n=e((()=>{t()}));export{n as t};
