cfdba478cc1b6fcb283bfab1e182c0f630848836
RubenSibon <mail+github@rubensibon.nl>
2026-05-29 11:26:46 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@fe5d09ff34bfb54d3f26110fd956f93c4d4d954b 🚀

--- demo-DES-1859-workflow-to-cleanup-gh-pages-deploys/assets/Column.test.stories-CIPz2PPl.js
+++
@@ -1,7 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-CsJlx38c.js";import{et as r,t as i}from"./src-e7HO3vXR.js";import a,{n as o}from"./Column.stories-Drzt8wnn.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Layout/Column`},c={render:(e,n)=>t(r,{args:e,layout:`grid`},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Column, {
-    args,
-    layout: 'grid'
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
---
+++ demo-DES-1859-workflow-to-cleanup-gh-pages-deploys/assets/IconButton.test.stories-9fSx92sP.js
@@ -0,0 +1,7 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-B0X5brJl.js";import{t as r,wt as i}from"./src-Bh8ZoRYJ.js";import a,{n as o}from"./IconButton.stories-DLzVSHa4.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/Buttons/Icon Button`},c={render:(e,n)=>t(i,{args:e,variants:[`hovered`,`disabled`]},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(IconButton, {
+    args,
+    variants: ['hovered', 'disabled']
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
