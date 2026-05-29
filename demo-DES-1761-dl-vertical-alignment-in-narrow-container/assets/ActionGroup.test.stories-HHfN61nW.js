d825feec82b0028f1c726baf878b676989b910e5
RubenSibon <mail+github@rubensibon.nl>
2026-05-29 08:26:34 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@793678c0b23788fbce8561ed81de5d9442e6917c 🚀

---
+++ demo-DES-1761-dl-vertical-alignment-in-narrow-container/assets/ActionGroup.test.stories-HHfN61nW.js
@@ -0,0 +1,9 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{Vt as t}from"./iframe-DHhH05wY.js";import{n,t as r}from"./renderComponentVariants-Do5jK7MZ.js";import{Tt as i,t as a}from"./src-Cj9L-991.js";import o,{n as s}from"./ActionGroup.stories-DalaFLIO.js";var c,l,u,d;e((()=>{a(),r(),s(),c=t(),l={...o,title:`Components/Layout/Action Group`},u={args:{children:[(0,c.jsx)(`button`,{children:`Stoppen`},1),(0,c.jsx)(`button`,{children:`Verder`},2)]},render:(e,t)=>n(i,{args:e},t),tags:[`!dev`,`!autodocs`]},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<button key={1}>Stoppen</button>, <button key={2}>Verder</button>]
+  },
+  render: (args, context) => renderComponentVariants(ActionGroup, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...u.parameters?.docs?.source}}},d=[`Test`]}))();export{u as Test,d as __namedExportsOrder,l as default};
