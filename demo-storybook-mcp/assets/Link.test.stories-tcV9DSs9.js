0fbcc39b51fcc0dcfe705a62fc1c195f4d44baeb
dlnr <niels@dlnr.dev>
2026-05-29 12:54:10 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@3951e9bb557bbb211edb7dc0853f0b41b265f1ec 🚀

---
+++ demo-storybook-mcp/assets/Link.test.stories-tcV9DSs9.js
@@ -0,0 +1,6 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-C1PvJtGm.js";import{I as r,t as i}from"./src-o42komTM.js";import a,{n as o}from"./Link.stories-BWSepm8z.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Navigation/Link`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Link, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
