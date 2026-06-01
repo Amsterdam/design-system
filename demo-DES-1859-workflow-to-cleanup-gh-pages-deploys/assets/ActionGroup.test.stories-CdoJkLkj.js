67547730b949313b4ae62cbd903f4bf88df709f3
RubenSibon <mail+github@rubensibon.nl>
2026-06-01 13:28:55 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@a76ae08551e5425ca772b931a835175ebd312b10 🚀

--- demo-DES-1859-workflow-to-cleanup-gh-pages-deploys/assets/ActionGroup.test.stories-CdoJkLkj.js
+++
@@ -1,9 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{Ht as t}from"./iframe-eAZaP25R.js";import{n,t as r}from"./renderComponentVariants-BJdD1joJ.js";import{Tt as i,t as a}from"./src-ThoLlEej.js";import o,{n as s}from"./ActionGroup.stories-C5oxr15n.js";var c,l,u,d;e((()=>{a(),r(),s(),c=t(),l={...o,title:`Components/Layout/Action Group`},u={args:{children:[(0,c.jsx)(`button`,{children:`Stoppen`},1),(0,c.jsx)(`button`,{children:`Verder`},2)]},render:(e,t)=>n(i,{args:e},t),tags:[`!dev`,`!autodocs`]},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: [<button key={1}>Stoppen</button>, <button key={2}>Verder</button>]
-  },
-  render: (args, context) => renderComponentVariants(ActionGroup, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...u.parameters?.docs?.source}}},d=[`Test`]}))();export{u as Test,d as __namedExportsOrder,l as default};
--- demo-DES-1859-workflow-to-cleanup-gh-pages-deploys/assets/mdx-react-shim-DIWpHevK.js
+++
@@ -1,1 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{t}from"./react-AGGC9hZZ.js";var n=e((()=>{t()}));export{n as t};
