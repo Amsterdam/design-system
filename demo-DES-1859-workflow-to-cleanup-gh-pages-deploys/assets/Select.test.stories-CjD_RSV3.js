2fe3702957dca86ca5c6927cd1976286a8eba38c
RubenSibon <mail+github@rubensibon.nl>
2026-06-01 13:32:32 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@f3056d4869b51766844d41edd34af29171708f97 🚀

---
+++ demo-DES-1859-workflow-to-cleanup-gh-pages-deploys/assets/Select.test.stories-CjD_RSV3.js
@@ -0,0 +1,7 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-Cw8LEHpj.js";import{g as r,t as i}from"./src-BbFHweeM.js";import a,{n as o}from"./Select.stories-DGvyyMgg.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Select`},c={render:(e,n)=>t(r,{args:e,variants:[`hovered`,`disabled`]},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Select, {
+    args,
+    variants: ['hovered', 'disabled']
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
---
+++ demo-DES-1859-workflow-to-cleanup-gh-pages-deploys/assets/TypographySample.test.stories-CrsdnH9h.js
@@ -0,0 +1,10 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{Ht as t}from"./iframe-D-SASGgw.js";import{n,t as r}from"./TypographySample-CqA5ztPI.js";var i,a,o,s;e((()=>{n(),i=t(),a={title:`Components/Docs/Typography Sample`},o={render:()=>(0,i.jsxs)(`div`,{className:`_ams-tests-grid`,children:[(0,i.jsx)(r,{fontFamily:`arial`}),(0,i.jsx)(r,{fontSize:`1.5rem`}),(0,i.jsx)(r,{fontWeight:`bold`}),(0,i.jsx)(r,{lineHeight:`1.5`}),(0,i.jsx)(r,{compact:!0,fontSize:`1.5rem`})]}),tags:[`!dev`,`!autodocs`]},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  render: () => <div className="_ams-tests-grid">
+      <TypographySample fontFamily="arial" />
+      <TypographySample fontSize="1.5rem" />
+      <TypographySample fontWeight="bold" />
+      <TypographySample lineHeight="1.5" />
+      <TypographySample compact fontSize="1.5rem" />
+    </div>,
+  tags: ['!dev', '!autodocs']
+}`,...o.parameters?.docs?.source}}},s=[`Test`]}))();export{o as Test,s as __namedExportsOrder,a as default};
