5fa4665aa558d33f9d7fb25063dbc2b0883e9c8d
RubenSibon <mail+github@rubensibon.nl>
2026-06-02 11:51:07 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@101ee31b94b407cb0ea75929e986960d13431e54 🚀

--- demo-DES-1859-workflow-to-cleanup-gh-pages-deploys/assets/ErrorMessage.test.stories-CSdRn-Tt.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-BaKQJn-b.js";import{X as r,t as i}from"./src-tRzISph3.js";import a,{n as o}from"./ErrorMessage.stories-DjA3-DcP.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Error Message`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(ErrorMessage, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
