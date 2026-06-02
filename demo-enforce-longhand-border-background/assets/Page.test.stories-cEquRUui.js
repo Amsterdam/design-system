74356f662372167a0241995430013a6dc3740d43
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-06-02 21:11:01 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@f03a72415ee580a27d9fb2e51bbc8fbffd30132b 🚀

--- demo-enforce-longhand-border-background/assets/Page.test.stories-cEquRUui.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-CCNqyCnv.js";import{O as r,t as i}from"./src-Bh8hfaF_.js";import a,{n as o}from"./Page.stories-D3hFYeyZ.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Containers/Page`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Page, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
