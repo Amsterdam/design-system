c5258d86597fc96f2f93904e913020cf5c9405d8
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-06-02 20:49:52 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@e550fae454ba63962344ad349418ece478798a97 🚀

---
+++ demo-enforce-longhand-border-background/assets/ActionGroup.test.stories-CmMSKRIJ.js
@@ -0,0 +1,9 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{Ht as t}from"./iframe-B0l3K_5D.js";import{n,t as r}from"./renderComponentVariants-CCNqyCnv.js";import{Tt as i,t as a}from"./src-Bh8hfaF_.js";import o,{n as s}from"./ActionGroup.stories-BJjgwpA3.js";var c,l,u,d;e((()=>{a(),r(),s(),c=t(),l={...o,title:`Components/Layout/Action Group`},u={args:{children:[(0,c.jsx)(`button`,{children:`Stoppen`},1),(0,c.jsx)(`button`,{children:`Verder`},2)]},render:(e,t)=>n(i,{args:e},t),tags:[`!dev`,`!autodocs`]},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<button key={1}>Stoppen</button>, <button key={2}>Verder</button>]
+  },
+  render: (args, context) => renderComponentVariants(ActionGroup, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...u.parameters?.docs?.source}}},d=[`Test`]}))();export{u as Test,d as __namedExportsOrder,l as default};
