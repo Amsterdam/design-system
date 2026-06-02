b7a26661dfccd7609640d2c8b06189ebe0810315
RubenSibon <mail+github@rubensibon.nl>
2026-06-02 13:18:45 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@d334b8c8c5187a024f3056427d4136b6f2ff1632 🚀

---
+++ demo-DES-1819-collapsible-toc/assets/FileList.stories-CnZrCT5C.js
@@ -0,0 +1,11 @@
+import{a as e,n as t,r as n}from"./chunk-DnJy8xQt.js";import{En as r,Ut as i,Z as a,v as o,y as s}from"./iframe-Cjw0Yyri.js";import{G as c,t as l}from"./src-0nGmVz26.js";var u,d,f,p=t((()=>{a(),u=e(r()),d=i(),f=()=>{let e=(0,u.useRef)(null),[t,n]=(0,u.useState)(null),r=()=>{e.current&&n(e.current.files)},i=r=>{if(t){let i=new DataTransfer;Array.from(t).forEach((e,t)=>{t!==r&&i.items.add(e)}),e.current&&(e.current.files=i.files),n(i.files)}};return(0,d.jsxs)(d.Fragment,{children:[(0,d.jsx)(o,{multiple:!0,onChange:r,ref:e}),t&&(0,d.jsx)(s,{children:Array.from(t).map((e,t)=>(0,d.jsx)(s.Item,{file:e,onDelete:()=>i(t)},e.name))})]})}})),m=n({Default:()=>_,WithInput:()=>v,__namedExportsOrder:()=>y,default:()=>g}),h,g,_,v,y,b=t((()=>{l(),p(),h=i(),g={title:`Components/Forms/File List`,component:c,args:{children:[(0,h.jsx)(c.Item,{file:new File([`sample1`],`sample1.txt`,{lastModified:Date.now(),type:`text/plain`}),onDelete:()=>{}},`1`),(0,h.jsx)(c.Item,{file:new File([`sample2`],`sample2.txt`,{lastModified:Date.now(),type:`text/plain`}),onDelete:()=>{}},`2`)]}},_={},v={parameters:{docs:{canvas:{sourceState:`none`},codePanel:!1}},render:()=>(0,h.jsx)(f,{})},_.parameters={..._.parameters,docs:{..._.parameters?.docs,source:{originalSource:`{}`,..._.parameters?.docs?.source}}},v.parameters={...v.parameters,docs:{...v.parameters?.docs,source:{originalSource:`{
+  parameters: {
+    docs: {
+      canvas: {
+        sourceState: 'none'
+      },
+      codePanel: false
+    }
+  },
+  render: () => <FileInputWithFileList />
+}`,...v.parameters?.docs?.source}}},y=[`Default`,`WithInput`]}));b();export{_ as Default,v as WithInput,y as __namedExportsOrder,g as default,b as n,m as t};
--- demo-DES-1819-collapsible-toc/assets/Link.test.stories-BbUXYSXZ.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-Bmr6KegO.js";import{I as r,t as i}from"./src-DGt0OMPV.js";import a,{n as o}from"./Link.stories-C3PUj7e0.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Navigation/Link`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Link, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
