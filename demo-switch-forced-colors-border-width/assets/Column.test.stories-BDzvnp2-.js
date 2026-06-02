574bbd7e18ec7d8fad642bdada0f711da3c2b5e9
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-06-02 19:42:38 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@9f73b371d3f51f40f671d4d8b7d904aa2b453d2f 🚀

---
+++ demo-switch-forced-colors-border-width/assets/Column.test.stories-BDzvnp2-.js
@@ -0,0 +1,7 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-CLYqO24W.js";import{et as r,t as i}from"./src-DS8z5L3V.js";import a,{n as o}from"./Column.stories-Dsje5Hh9.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Layout/Column`},c={render:(e,n)=>t(r,{args:e,layout:`grid`},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Column, {
+    args,
+    layout: 'grid'
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
--- demo-switch-forced-colors-border-width/assets/Radio.test.stories-CKv09iiS.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-BemEcnhH.js";import{t as r,v as i}from"./src-CyDbzBT-.js";import a,{n as o}from"./Radio.stories-8nqDPadU.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/Forms/Radio`},c={render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Radio, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
