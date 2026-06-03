ba090035e9e93c6e460b89396a8fb9713f197987
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-06-03 12:29:46 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@e62ca1c8f7663e13ab5b972fc2ff2716f02fadbf 🚀

---
+++ demo-DES-1855-calendar/assets/Icon.test.stories-C7LuF0jO.js
@@ -0,0 +1,6 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-BxaFLsxa.js";import{Ot as r,t as i}from"./src-D336yN75.js";import a,{n as o}from"./Icon.stories-BofY0dN2.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Media/Icon`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Icon, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
---
+++ demo-DES-1855-calendar/assets/mdx-react-shim-Blc01Rd3.js
@@ -0,0 +1,1 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{t}from"./react-Cns2Jgtl.js";var n=e((()=>{t()}));export{n as t};
