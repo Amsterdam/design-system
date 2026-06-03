6cfc6d573bb3b10623e19c824eacbdfbf919afbd
GitHub <github-action@users.noreply.github.com>
2026-06-03 12:26:01 +0000

    Remove feature branch folder

--- demo-DES-1855-double-chevron-icons/assets/ActionGroup.test.stories-CYM3ZnEL.js
+++
@@ -1,9 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{Ht as t}from"./iframe-BY9hPBCq.js";import{n,t as r}from"./renderComponentVariants-Byk2VUgA.js";import{Tt as i,t as a}from"./src-Cfh2HA0w.js";import o,{n as s}from"./ActionGroup.stories-hJ5ebg9b.js";var c,l,u,d;e((()=>{a(),r(),s(),c=t(),l={...o,title:`Components/Layout/Action Group`},u={args:{children:[(0,c.jsx)(`button`,{children:`Stoppen`},1),(0,c.jsx)(`button`,{children:`Verder`},2)]},render:(e,t)=>n(i,{args:e},t),tags:[`!dev`,`!autodocs`]},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: [<button key={1}>Stoppen</button>, <button key={2}>Verder</button>]
-  },
-  render: (args, context) => renderComponentVariants(ActionGroup, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...u.parameters?.docs?.source}}},d=[`Test`]}))();export{u as Test,d as __namedExportsOrder,l as default};
--- demo-DES-1855-double-chevron-icons/assets/ErrorMessage.test.stories-Cw2p2bMs.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-Byk2VUgA.js";import{X as r,t as i}from"./src-Cfh2HA0w.js";import a,{n as o}from"./ErrorMessage.stories-DUf2x9gh.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Error Message`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(ErrorMessage, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
