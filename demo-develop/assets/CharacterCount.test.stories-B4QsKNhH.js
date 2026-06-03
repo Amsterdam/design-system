9a7eb681203b29ea2ce05dc3f57a8c153caaaa33
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-06-03 12:27:21 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@ec67467e95d93744cebd4216bb40acb5cdfee0d5 🚀

--- demo-develop/assets/CharacterCount.test.stories-B4QsKNhH.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-C0aWJVrp.js";import{it as r,t as i}from"./src-BwBP2mh8.js";import a,{n as o}from"./CharacterCount.stories-y2dK2M2x.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Character Count`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(CharacterCount, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
---
+++ demo-develop/assets/Overlap.docs-swi_JsEG.js
@@ -0,0 +1,21 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{Ht as t}from"./iframe-sVGWgCr0.js";import{r as n}from"./react-DY-v-PkG.js";import{c as r,f as i,l as a,s as o}from"./blocks-CwVrGAjF.js";import{t as s}from"./mdx-react-shim-CAKBIX2J.js";import{n as c,r as l}from"./Overlap.stories-DD69kNch.js";var u,d=e((()=>{u=`<!-- @license CC0-1.0 -->
+
+# Overlap
+
+Displays two or more components on top of each other.
+`}));function f(e){let t={a:`a`,h2:`h2`,h3:`h3`,li:`li`,p:`p`,ul:`ul`,...n(),...e.components};return(0,m.jsxs)(m.Fragment,{children:[`
+`,`
+`,(0,m.jsx)(r,{of:c}),`
+`,(0,m.jsx)(o,{children:u}),`
+`,(0,m.jsx)(t.h2,{id:`examples`,children:`Examples`}),`
+`,(0,m.jsx)(t.h3,{id:`hero-image-with-search-field`,children:`Hero image with Search Field`}),`
+`,(0,m.jsxs)(t.p,{children:[`This allows for the use of a mood-setting image as a background.
+A `,(0,m.jsx)(t.a,{href:`/docs/components-layout-grid--docs`,children:`Grid`}),` provides horizontal space on both sides and columns for layout.
+Note: the library does not yet offer an option to vertically center the form.`]}),`
+`,(0,m.jsx)(a,{}),`
+`,(0,m.jsx)(t.h2,{id:`guidelines`,children:`Guidelines`}),`
+`,(0,m.jsxs)(t.ul,{children:[`
+`,(0,m.jsxs)(t.li,{children:[`Follow `,(0,m.jsx)(t.a,{href:`/docs/components-media-image--docs#guidelines`,children:`the general guidelines for images`}),`.`]}),`
+`,(0,m.jsx)(t.li,{children:`Use a sufficiently large image.
+The aspect ratio of 32:9 is most suitable.`}),`
+`]})]})}function p(e={}){let{wrapper:t}={...n(),...e.components};return t?(0,m.jsx)(t,{...e,children:(0,m.jsx)(f,{...e})}):f(e)}var m;e((()=>{m=t(),s(),i(),l(),d()}))();export{p as default};
