25bf59ebe23afd3c486d613e5a3231fc55ea23d5
GitHub <github-action@users.noreply.github.com>
2026-06-04 07:45:58 +0000

    Remove feature branch folder

--- demo-ignore-jsdom-27-until-accname-fix/assets/Pagination.test.stories-CRFJoDzp.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-DAZLCt0r.js";import{C as r,t as i}from"./src-C9mWGyhk.js";import a,{n as o}from"./Pagination.stories-rVXewudH.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Navigation/Pagination`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Pagination, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
--- demo-ignore-jsdom-27-until-accname-fix/assets/SpaceSample.test.stories-DupIAR_s.js
+++
@@ -1,7 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{Ht as t}from"./iframe-Be0i3AU2.js";import{n,t as r}from"./SpaceSample-CuNVTRde.js";var i,a,o,s;e((()=>{n(),i=t(),a={title:`Components/Docs/Space Sample`},o={render:()=>(0,i.jsxs)(`div`,{className:`_ams-tests-grid`,children:[(0,i.jsx)(r,{value:`16px`}),(0,i.jsx)(r,{})]}),tags:[`!dev`,`!autodocs`]},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
-  render: () => <div className="_ams-tests-grid">
-      <SpaceSample value="16px" />
-      <SpaceSample />
-    </div>,
-  tags: ['!dev', '!autodocs']
-}`,...o.parameters?.docs?.source}}},s=[`Test`]}))();export{o as Test,s as __namedExportsOrder,a as default};
