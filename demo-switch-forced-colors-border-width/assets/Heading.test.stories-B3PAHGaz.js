f08bd7b3d076609e91f7b245d1f8ba8ead2613d5
GitHub <github-action@users.noreply.github.com>
2026-06-04 08:07:19 +0000

    Remove feature branch folder

--- demo-switch-forced-colors-border-width/assets/Heading.test.stories-B3PAHGaz.js
+++
@@ -1,9 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-C2A5zGRr.js";import{At as r,t as i}from"./src-DUl65xMN.js";import a,{n as o}from"./Heading.stories-Lde9e8Rb.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Text/Heading`},c={args:{children:`Amsterdam Light Festival maakt de stad ook deze winter een beetje mooier`},render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: 'Amsterdam Light Festival maakt de stad ook deze winter een beetje mooier'
-  },
-  render: (args, context) => renderComponentVariants(Heading, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
