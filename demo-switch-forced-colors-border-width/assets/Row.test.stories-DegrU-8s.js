eae7cf0423124ea65280cfb37248e222ab7376c6
RubenSibon <mail+github@rubensibon.nl>
2026-06-04 07:25:12 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@d21e6833217ab2f775631f9db8401c906c2b1bb9 🚀

--- demo-switch-forced-colors-border-width/assets/Row.test.stories-DegrU-8s.js
+++
@@ -1,9 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{Ht as t}from"./iframe-B6ooUJlL.js";import{n,t as r}from"./renderComponentVariants-CLYqO24W.js";import{t as i,xt as a}from"./src-DS8z5L3V.js";import o,{n as s}from"./Row.stories-BmzH-88Z.js";var c,l,u,d;e((()=>{i(),r(),s(),c=t(),l={...o,title:`Components/Layout/Row`},u={args:{children:[(0,c.jsx)(`div`,{className:`_ams-item`},1),(0,c.jsx)(`div`,{className:`_ams-item`},2),(0,c.jsx)(`div`,{className:`_ams-item`},3)]},render:(e,t)=>n(a,{args:e},t),tags:[`!dev`,`!autodocs`]},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: [<div className="_ams-item" key={1} />, <div className="_ams-item" key={2} />, <div className="_ams-item" key={3} />]
-  },
-  render: (args, context) => renderComponentVariants(Row, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...u.parameters?.docs?.source}}},d=[`Test`]}))();export{u as Test,d as __namedExportsOrder,l as default};
