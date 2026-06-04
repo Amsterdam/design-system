5995a2c71373feb7221248ae0bb6898b07e7a8d7
RubenSibon <mail+github@rubensibon.nl>
2026-06-04 07:52:00 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@b4845130ad8ca08df172d30e0b30f5eb1d6e32c3 🚀

--- demo-switch-forced-colors-border-width/assets/CallToActionLink.test.stories-DK2TYSdq.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-Dm92K1C6.js";import{lt as r,t as i}from"./src-m9JNVZmP.js";import a,{n as o}from"./CallToActionLink.stories-CO5onW1w.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Navigation/Call to Action Link`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(CallToActionLink, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
