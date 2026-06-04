d753c68faa327f1224a2f54db89c04aa959dc310
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-06-04 09:29:42 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@576e0b1f0174f7346ca6b599e3df0c97e6ada3fb 🚀

---
+++ demo-develop/assets/FileList.test.stories-BU3LOu82.js
@@ -0,0 +1,11 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{Ht as t}from"./iframe-C-HZiv6Q.js";import{n,t as r}from"./renderComponentVariants-HFwNxiUl.js";import{G as i,t as a}from"./src-CfIhDC9j.js";import o,{n as s}from"./FileList.stories-CU48Eq91.js";var c,l,u,d;e((()=>{a(),r(),s(),c=t(),l={...o,title:`Components/Forms/File List`},u={args:{children:(0,c.jsx)(i.Item,{file:new File([`sample1`],`sample1.txt`,{type:`text/plain`})})},render:(e,t)=>n(i,{args:e},t),tags:[`!dev`,`!autodocs`]},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: <FileList.Item file={new File(['sample1'], 'sample1.txt', {
+      type: 'text/plain'
+    })} />
+  },
+  render: (args, context) => renderComponentVariants(FileList, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...u.parameters?.docs?.source}}},d=[`Test`]}))();export{u as Test,d as __namedExportsOrder,l as default};
