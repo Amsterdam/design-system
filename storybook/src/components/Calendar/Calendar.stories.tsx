ca0e8f8b251fa51945377075d6055ed7621b53b9
Vincent Smedinga <v.smedinga@amsterdam.nl>
2026-06-09 15:38:44 +0200

    Allow copying useful source code for Calendar

--- storybook/src/components/Calendar/Calendar.stories.tsx
+++ storybook/src/components/Calendar/Calendar.stories.tsx
@@ -54,11 +54,30 @@ export default meta
 
 type Story = StoryObj<typeof meta>
 
-export const Default: Story = {}
+export const Default: Story = {
+  parameters: {
+    docs: {
+      source: {
+        code: `<Calendar linkTemplate={(date: Date): string | undefined => \`?date=\${formatDate(date)}\`} />`,
+      },
+    },
+  },
+}
 
 export const DatesWithoutLinks: Story = {
   args: {
     linkTemplate: (date: Date) =>
       date.getDay() === 0 || date.getDay() === 6 ? undefined : `?date=${formatDate(date)}`,
+  },
+  parameters: {
+    docs: {
+      source: {
+        code: `<Calendar
+  linkTemplate={(date: Date) =>
+    date.getDay() === 0 || date.getDay() === 6 ? undefined : \`?date=\${formatDate(date)}\`
+  }
+/>`,
+      },
+    },
   },
 }
