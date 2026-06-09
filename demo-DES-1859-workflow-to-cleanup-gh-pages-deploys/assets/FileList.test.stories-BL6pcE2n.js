22cfcb7a8e8eb4e27165fe2d795dc8e711a4cec9
RubenSibon <mail+github@rubensibon.nl>
2026-06-09 14:56:52 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@9039b2b2de842cc2c43b096a134c7270f80de3c9 🚀

---
+++ demo-DES-1859-workflow-to-cleanup-gh-pages-deploys/assets/FileList.test.stories-BL6pcE2n.js
@@ -0,0 +1,11 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{Ht as t}from"./iframe-MvWACXnh.js";import{n,t as r}from"./renderComponentVariants-zpTGhzrj.js";import{G as i,t as a}from"./src-BGT-d6sO.js";import o,{n as s}from"./FileList.stories-Bgb23mPE.js";var c,l,u,d;e((()=>{a(),r(),s(),c=t(),l={...o,title:`Components/Forms/File List`},u={args:{children:(0,c.jsx)(i.Item,{file:new File([`sample1`],`sample1.txt`,{type:`text/plain`})})},render:(e,t)=>n(i,{args:e},t),tags:[`!dev`,`!autodocs`]},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: <FileList.Item file={new File(['sample1'], 'sample1.txt', {
+      type: 'text/plain'
+    })} />
+  },
+  render: (args, context) => renderComponentVariants(FileList, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...u.parameters?.docs?.source}}},d=[`Test`]}))();export{u as Test,d as __namedExportsOrder,l as default};
---
+++ demo-DES-1859-workflow-to-cleanup-gh-pages-deploys/assets/config-B7rgIi-N.js
@@ -0,0 +1,1 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{Ht as t}from"./iframe-MvWACXnh.js";import{n,t as r}from"./Layout-Bh-lz5V3.js";var i,a,o=e((()=>{n(),i=t(),a={decorators:[e=>(0,i.jsx)(r,{children:(0,i.jsx)(e,{})})],parameters:{layout:`fullscreen`,themes:{themeOverride:`Spacious`}}}}));export{o as n,a as t};
