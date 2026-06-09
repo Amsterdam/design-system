af03a428ac4d984d5ec91d7b9eb96a541b2aef81
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-06-09 09:19:54 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@c310258a9184373bccaf6eaf16e91dac416340f7 🚀

---
+++ demo-DES-1855-rename-double-chevron-icons/assets/HomePage.docs-BQCEcQv3.js
@@ -0,0 +1,6 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{Ht as t}from"./iframe-DI7Avr_E.js";import{r as n}from"./react-7j8gD5oh.js";import{c as r,f as i}from"./blocks-CqGfZ3es.js";import{t as a}from"./mdx-react-shim-BludOZqn.js";import{n as o,t as s}from"./HomePage.stories-Xq69AnaH.js";function c(e){let t={h1:`h1`,p:`p`,...n(),...e.components};return(0,u.jsxs)(u.Fragment,{children:[`
+`,`
+`,(0,u.jsx)(r,{of:s}),`
+`,(0,u.jsx)(t.h1,{id:`home-page`,children:`Home page`}),`
+`,(0,u.jsx)(t.p,{children:`The home page of an internal website can have varying types of content.`}),`
+`,(0,u.jsx)(t.p,{children:`Our single example in this category currently focuses on the overall layout.`})]})}function l(e={}){let{wrapper:t}={...n(),...e.components};return t?(0,u.jsx)(t,{...e,children:(0,u.jsx)(c,{...e})}):c(e)}var u;e((()=>{u=t(),a(),i(),o()}))();export{l as default};
