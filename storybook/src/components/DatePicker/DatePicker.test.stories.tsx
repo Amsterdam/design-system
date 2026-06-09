b6990e6b7edeb14da2ce9c263f1f19f7e8cd0bb4
Vincent Smedinga <v.smedinga@amsterdam.nl>
2026-06-09 21:17:50 +0200

    test(DatePicker): Cover the main states in the visual regression test story

--- storybook/src/components/DatePicker/DatePicker.test.stories.tsx
+++ storybook/src/components/DatePicker/DatePicker.test.stories.tsx
@@ -7,8 +7,10 @@ import type { Meta, StoryObj } from '@storybook/react-vite'
 
 import { DatePicker } from '@amsterdam/design-system-react/src'
 
-import { renderComponentVariants } from '../../_common/renderComponentVariants'
 import { default as datePickerMeta } from './DatePicker.stories'
+
+const march2026 = new Date(2026, 2, 1)
+const noop = () => {}
 
 const meta = {
   ...datePickerMeta,
@@ -20,11 +22,30 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const Test: Story = {
-  args: {
-    defaultMonth: new Date(2026, 2, 1),
-    onChange: () => {},
-    value: new Date(2026, 2, 14),
-  },
-  render: (args, context) => renderComponentVariants(DatePicker, { args }, context),
+  render: () => (
+    <div className="_ams-tests-grid">
+      <DatePicker defaultMonth={march2026} onChange={noop} value={null} />
+      <DatePicker defaultMonth={march2026} onChange={noop} value={new Date(2026, 2, 14)} />
+      <DatePicker
+        defaultMonth={march2026}
+        mode="range"
+        onChange={noop}
+        value={{ start: new Date(2026, 2, 10), end: new Date(2026, 2, 16) }}
+      />
+      <DatePicker
+        defaultMonth={march2026}
+        isDateDisabled={(date) => date.getDay() === 0 || date.getDay() === 6}
+        onChange={noop}
+        value={null}
+      />
+      <DatePicker
+        defaultMonth={march2026}
+        maxDate={new Date(2026, 2, 20)}
+        minDate={new Date(2026, 2, 5)}
+        onChange={noop}
+        value={null}
+      />
+    </div>
+  ),
   tags: ['!dev', '!autodocs'],
 }
