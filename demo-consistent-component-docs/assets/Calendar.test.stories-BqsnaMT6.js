636b42467252788c8e71c1378d80abcd492ae5d6
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-06-10 15:59:26 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@5d5c0203589a200ee8886930d01e179891f2e013 🚀

---
+++ demo-consistent-component-docs/assets/Calendar.test.stories-BqsnaMT6.js
@@ -0,0 +1,9 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-CH6bEml6.js";import{t as r,ut as i}from"./src-CcbKN2oS.js";import a,{n as o}from"./Calendar.stories-kAY6zQki.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/Navigation/Calendar`,argTypes:{...a.argTypes,locale:{control:!1,table:{disable:!0}}}},c={args:{locale:`nl`},render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    locale: 'nl'
+  },
+  render: (args, context) => renderComponentVariants(Calendar, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
