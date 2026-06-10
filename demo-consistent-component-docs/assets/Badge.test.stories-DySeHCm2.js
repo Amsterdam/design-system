dd81a06da9cde2f0797a0bedfe627d4198921657
GitHub <github-action@users.noreply.github.com>
2026-06-10 20:11:17 +0000

    Remove feature branch folder

--- demo-consistent-component-docs/assets/Badge.test.stories-DySeHCm2.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-jo2X1t3p.js";import{ht as r,t as i}from"./src-CMXTLork.js";import a,{n as o}from"./Badge.stories-C6jWt8pP.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Feedback/Badge`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Badge, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
--- demo-consistent-component-docs/assets/Grid.test.stories-DO2levrd.js
+++
@@ -1,9 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{Wt as t}from"./iframe-BVve0ihF.js";import{n,t as r}from"./renderComponentVariants-jo2X1t3p.js";import{W as i,t as a}from"./src-CMXTLork.js";import o,{n as s}from"./Grid.stories-BON5UBUJ.js";var c,l,u,d;e((()=>{a(),r(),s(),c=t(),l={...o,title:`Components/Layout/Grid`},u={args:{children:[(0,c.jsx)(i.Cell,{span:`all`},1),(0,c.jsx)(i.Cell,{span:`all`},2)]},render:(e,t)=>n(i,{args:e},t),tags:[`!dev`,`!autodocs`]},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: [<Grid.Cell key={1} span="all" />, <Grid.Cell key={2} span="all" />]
-  },
-  render: (args, context) => renderComponentVariants(Grid, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...u.parameters?.docs?.source}}},d=[`Test`]}))();export{u as Test,d as __namedExportsOrder,l as default};
