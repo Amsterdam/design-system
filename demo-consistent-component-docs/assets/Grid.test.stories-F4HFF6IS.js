11e8e78102daa9a7311c37cc5d186fb622e20842
VincentSmedinga <VincentSmedinga@users.noreply.github.com>
2026-06-10 15:27:20 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@a8ce155c95b1654495f2e8c8d02b85d8b9ba8fcd 🚀

---
+++ demo-consistent-component-docs/assets/Grid.test.stories-F4HFF6IS.js
@@ -0,0 +1,9 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{Wt as t}from"./iframe-Anc-WxYL.js";import{n,t as r}from"./renderComponentVariants-Iu7I__h4.js";import{W as i,t as a}from"./src-DQdhPKmj.js";import o,{n as s}from"./Grid.stories-D_655BuR.js";var c,l,u,d;e((()=>{a(),r(),s(),c=t(),l={...o,title:`Components/Layout/Grid`},u={args:{children:[(0,c.jsx)(i.Cell,{span:`all`},1),(0,c.jsx)(i.Cell,{span:`all`},2)]},render:(e,t)=>n(i,{args:e},t),tags:[`!dev`,`!autodocs`]},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<Grid.Cell key={1} span="all" />, <Grid.Cell key={2} span="all" />]
+  },
+  render: (args, context) => renderComponentVariants(Grid, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...u.parameters?.docs?.source}}},d=[`Test`]}))();export{u as Test,d as __namedExportsOrder,l as default};
---
+++ demo-consistent-component-docs/assets/mdx-react-shim-CsN0UJAg.js
@@ -0,0 +1,1 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{t}from"./react-Cj0t5CZr.js";var n=e((()=>{t()}));export{n as t};
