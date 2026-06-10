a842b3bb7a86fb372b17de4199d9d83806cf363f
GitHub <github-action@users.noreply.github.com>
2026-06-10 20:26:14 +0000

    Remove feature branch folder

--- demo-consistent-component-docs-2/assets/Code.test.stories-BoEobW-j.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-AKBQa_fu.js";import{n as r,t as i}from"./Code-D8B8RY-a.js";var a,o,s;e((()=>{n(),r(),a={title:`Components/Docs/Code`,args:{children:[`var(--ams-color-text)`]}},o={render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`]},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Code, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...o.parameters?.docs?.source}}},s=[`Test`]}))();export{o as Test,s as __namedExportsOrder,a as default};
--- demo-consistent-component-docs-2/assets/HomePage.docs-eLOo7SJF.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{Wt as t}from"./iframe-Dak--dB-.js";import{r as n}from"./react-Dm5D40f9.js";import{l as r,m as i}from"./blocks-D6FlSyUT.js";import{t as a}from"./mdx-react-shim-BgEP09T9.js";import{n as o,t as s}from"./HomePage.stories-UakLIfvz.js";function c(e){let t={h1:`h1`,p:`p`,...n(),...e.components};return(0,u.jsxs)(u.Fragment,{children:[`
-`,`
-`,(0,u.jsx)(r,{of:s}),`
-`,(0,u.jsx)(t.h1,{id:`home-page`,children:`Home page`}),`
-`,(0,u.jsx)(t.p,{children:`The home page of an internal website can have varying types of content.`}),`
-`,(0,u.jsx)(t.p,{children:`Our single example in this category currently focuses on the overall layout.`})]})}function l(e={}){let{wrapper:t}={...n(),...e.components};return t?(0,u.jsx)(t,{...e,children:(0,u.jsx)(c,{...e})}):c(e)}var u;e((()=>{u=t(),a(),i(),o()}))();export{l as default};
