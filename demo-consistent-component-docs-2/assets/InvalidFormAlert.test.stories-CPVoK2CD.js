d2556435638e78c83ecb3f3911973c1aae2564fd
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-06-10 20:20:49 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@26242eb7b20851996572f58c37439bbd5dbddfcc 🚀

---
+++ demo-consistent-component-docs-2/assets/InvalidFormAlert.test.stories-CPVoK2CD.js
@@ -0,0 +1,6 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-AKBQa_fu.js";import{R as r,t as i}from"./src-Tti4jVCE.js";import a,{n as o}from"./InvalidFormAlert.stories-B3v4ftEx.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Invalid Form Alert`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(InvalidFormAlert, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
