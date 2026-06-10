72c8a375fc6048ee4b05e3eb1cfe13f4108fb97b
GitHub <github-action@users.noreply.github.com>
2026-06-10 15:15:25 +0000

    Remove feature branch folder

--- demo-consistent-component-docs/assets/Button.test.stories-H8KDafoO.js
+++
@@ -1,7 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-DLUi3xSI.js";import{dt as r,t as i}from"./src-aG1Z3iPb.js";import a,{n as o}from"./Button.stories-D_k8FGn7.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Buttons/Button`},c={render:(e,n)=>t(r,{args:e,variants:[`disabled`,`hovered`]},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Button, {
-    args,
-    variants: ['disabled', 'hovered']
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
--- demo-consistent-component-docs/assets/StandaloneLink.test.stories-BffdkmDu.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-DLUi3xSI.js";import{d as r,t as i}from"./src-aG1Z3iPb.js";import a,{n as o}from"./StandaloneLink.stories-CtTLfqjC.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/StandaloneLink`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(StandaloneLink, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
