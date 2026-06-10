170eff36c40793d0f6d8d49bde2d41fd993021e6
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-06-10 19:35:49 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@ca6bd6e2afd48a23bd43e13c220a29fdfe636004 🚀

--- demo-consistent-component-docs/assets/ActionGroup.test.stories-DucpDbHr.js
+++
@@ -1,9 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{Wt as t}from"./iframe-C3vrHJua.js";import{n,t as r}from"./renderComponentVariants-BnT9739C.js";import{Et as i,t as a}from"./src-fyws-gVq.js";import o,{n as s}from"./ActionGroup.stories-Ct2xBOBi.js";var c,l,u,d;e((()=>{a(),r(),s(),c=t(),l={...o,title:`Components/Layout/Action Group`},u={args:{children:[(0,c.jsx)(`button`,{children:`Stoppen`},1),(0,c.jsx)(`button`,{children:`Verder`},2)]},render:(e,t)=>n(i,{args:e},t),tags:[`!dev`,`!autodocs`]},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: [<button key={1}>Stoppen</button>, <button key={2}>Verder</button>]
-  },
-  render: (args, context) => renderComponentVariants(ActionGroup, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...u.parameters?.docs?.source}}},d=[`Test`]}))();export{u as Test,d as __namedExportsOrder,l as default};
---
+++ demo-consistent-component-docs/assets/HomePage.docs-BVXCSHSM.js
@@ -0,0 +1,6 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{Wt as t}from"./iframe-BVve0ihF.js";import{r as n}from"./react-CPrfM0B6.js";import{l as r,m as i}from"./blocks-BqwolfvP.js";import{t as a}from"./mdx-react-shim-BC26Z4Rv.js";import{n as o,t as s}from"./HomePage.stories-ng5MD4RX.js";function c(e){let t={h1:`h1`,p:`p`,...n(),...e.components};return(0,u.jsxs)(u.Fragment,{children:[`
+`,`
+`,(0,u.jsx)(r,{of:s}),`
+`,(0,u.jsx)(t.h1,{id:`home-page`,children:`Home page`}),`
+`,(0,u.jsx)(t.p,{children:`The home page of an internal website can have varying types of content.`}),`
+`,(0,u.jsx)(t.p,{children:`Our single example in this category currently focuses on the overall layout.`})]})}function l(e={}){let{wrapper:t}={...n(),...e.components};return t?(0,u.jsx)(t,{...e,children:(0,u.jsx)(c,{...e})}):c(e)}var u;e((()=>{u=t(),a(),i(),o()}))();export{l as default};
