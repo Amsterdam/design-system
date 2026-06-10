c64ef70761059c8013db5bae8c5f8eb598412b6d
GitHub <github-action@users.noreply.github.com>
2026-06-10 15:50:33 +0000

    Remove feature branch folder

--- demo-consistent-component-docs/assets/Grid.test.stories-D6kve4k3.js
+++
@@ -1,9 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{Wt as t}from"./iframe-D5Jf7JCR.js";import{n,t as r}from"./renderComponentVariants-vJURcK5D.js";import{W as i,t as a}from"./src-B8mpkIUI.js";import o,{n as s}from"./Grid.stories-RtA1300f.js";var c,l,u,d;e((()=>{a(),r(),s(),c=t(),l={...o,title:`Components/Layout/Grid`},u={args:{children:[(0,c.jsx)(i.Cell,{span:`all`},1),(0,c.jsx)(i.Cell,{span:`all`},2)]},render:(e,t)=>n(i,{args:e},t),tags:[`!dev`,`!autodocs`]},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: [<Grid.Cell key={1} span="all" />, <Grid.Cell key={2} span="all" />]
-  },
-  render: (args, context) => renderComponentVariants(Grid, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...u.parameters?.docs?.source}}},d=[`Test`]}))();export{u as Test,d as __namedExportsOrder,l as default};
