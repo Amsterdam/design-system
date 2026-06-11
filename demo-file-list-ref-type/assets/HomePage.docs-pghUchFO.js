216412247bf3758dfbb6303c9d1909c10d41a35e
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-06-11 22:37:37 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@b246e45efc41385ec01cd9580245e7d517891a71 🚀

---
+++ demo-file-list-ref-type/assets/HomePage.docs-pghUchFO.js
@@ -0,0 +1,6 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{Wt as t}from"./iframe-B6Xft96z.js";import{r as n}from"./react-ZsfAZKuX.js";import{l as r,m as i}from"./blocks-BKJriqY3.js";import{t as a}from"./mdx-react-shim-D2n3HLru.js";import{n as o,t as s}from"./HomePage.stories-BZZRtTrb.js";function c(e){let t={h1:`h1`,p:`p`,...n(),...e.components};return(0,u.jsxs)(u.Fragment,{children:[`
+`,`
+`,(0,u.jsx)(r,{of:s}),`
+`,(0,u.jsx)(t.h1,{id:`home-page`,children:`Home page`}),`
+`,(0,u.jsx)(t.p,{children:`The home page of an internal website can have varying types of content.`}),`
+`,(0,u.jsx)(t.p,{children:`Our single example in this category currently focuses on the overall layout.`})]})}function l(e={}){let{wrapper:t}={...n(),...e.components};return t?(0,u.jsx)(t,{...e,children:(0,u.jsx)(c,{...e})}):c(e)}var u;e((()=>{u=t(),a(),i(),o()}))();export{l as default};
