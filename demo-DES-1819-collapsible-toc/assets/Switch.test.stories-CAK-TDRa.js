2d148e88b5a81ed0c0aa36bf156859435bb83702
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-06-11 22:51:48 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@067a0edea91a2bc859771fa539a6c6e3a188f82e 🚀

---
+++ demo-DES-1819-collapsible-toc/assets/Switch.test.stories-CAK-TDRa.js
@@ -0,0 +1,6 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-BHaoovO1.js";import{t as r,u as i}from"./src-BIAD6a1v.js";import a,{n as o}from"./Switch.stories-Cz6BOIQY.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/Forms/Switch`},c={render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Switch, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
