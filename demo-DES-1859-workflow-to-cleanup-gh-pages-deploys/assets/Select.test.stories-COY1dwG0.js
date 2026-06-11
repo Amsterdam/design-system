22c59dafc68bc638539a74443256d7b1235dea3f
RubenSibon <mail+github@rubensibon.nl>
2026-06-11 08:25:55 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@d6b387cdacefd17120b6a3ddc187952d24aec600 🚀

--- demo-DES-1859-workflow-to-cleanup-gh-pages-deploys/assets/Select.test.stories-COY1dwG0.js
+++
@@ -1,7 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-zpTGhzrj.js";import{g as r,t as i}from"./src-BGT-d6sO.js";import a,{n as o}from"./Select.stories-Dg8sss7f.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Select`},c={render:(e,n)=>t(r,{args:e,variants:[`hovered`,`disabled`]},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Select, {
-    args,
-    variants: ['hovered', 'disabled']
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
---
+++ demo-DES-1859-workflow-to-cleanup-gh-pages-deploys/assets/TableOfContents.test.stories--by4H8wP.js
@@ -0,0 +1,6 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-DQKpk98U.js";import{c as r,t as i}from"./src-B9EbjRym.js";import a,{n as o}from"./TableOfContents.stories-2kFzFGN7.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/TableOfContents`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(TableOfContents, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
