03e31e79d8b970c4432c5295c835093b1d3c706b
dlnr <niels@dlnr.dev>
2026-06-11 13:48:42 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@af14548fc72a6d5023f81f4112eb903f954b20e4 🚀

--- demo-DES-1878-new-icons/assets/Alert.test.stories-C3qJIIkP.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-Bo8guW3w.js";import{t as r,xt as i}from"./src-LDVi3i2P.js";import a,{n as o}from"./Alert.stories-DEA2QoiC.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/Feedback/Alert`},c={render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Alert, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
--- demo-DES-1878-new-icons/assets/Blockquote.test.stories-cs4ncXsW.js
+++
@@ -1,9 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-Bo8guW3w.js";import{mt as r,t as i}from"./src-LDVi3i2P.js";import a,{n as o}from"./Blockquote.stories-L6MpZUTE.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Text/Blockquote`},c={args:{children:`Voor Amsterdam was de Jordaan één oproer en één fort. De Amsterdammer begreep de Jordaner niet, die zelden uit zijn buurt komt, soms niet uit zijn straat, wiens stad haar eigen plattegrond heeft, een stad in een stad.`},render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: 'Voor Amsterdam was de Jordaan één oproer en één fort. De Amsterdammer begreep de Jordaner niet, die zelden uit zijn buurt komt, soms niet uit zijn straat, wiens stad haar eigen plattegrond heeft, een stad in een stad.'
-  },
-  render: (args, context) => renderComponentVariants(Blockquote, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
