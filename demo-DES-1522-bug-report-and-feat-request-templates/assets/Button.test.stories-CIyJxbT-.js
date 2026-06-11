6e9cc0e449b12393b8c97a00ab7ff51b65ea1a80
RubenSibon <mail+github@rubensibon.nl>
2026-06-11 09:08:41 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@2a02e8e9134e20c6dab96806c165fb8b67a7c929 🚀

---
+++ demo-DES-1522-bug-report-and-feat-request-templates/assets/Button.test.stories-CIyJxbT-.js
@@ -0,0 +1,7 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-YaYn7UNU.js";import{dt as r,t as i}from"./src-B8_EMRaq.js";import a,{n as o}from"./Button.stories-9ir8N4Gc.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Buttons/Button`},c={render:(e,n)=>t(r,{args:e,variants:[`disabled`,`hovered`]},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Button, {
+    args,
+    variants: ['disabled', 'hovered']
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
