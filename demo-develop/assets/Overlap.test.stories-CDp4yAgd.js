f624818430ee432d5cee743e83140926dddd6b55
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-06-11 09:12:34 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@207cf5970e1d6123d84b5f82dc3906f2311d1872 🚀

---
+++ demo-develop/assets/Overlap.test.stories-CDp4yAgd.js
@@ -0,0 +1,9 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{Wt as t}from"./iframe-C1sqtiEi.js";import{n,t as r}from"./renderComponentVariants-De9KNrbp.js";import{k as i,t as a}from"./src-BWTRo0f4.js";import{i as o,r as s}from"./Overlap.stories--dswj082.js";var c,l,u,d;e((()=>{a(),r(),s(),c=t(),l={...o,title:`Components/Layout/Overlap`},u={args:{children:[(0,c.jsx)(`img`,{alt:``,src:`https://picsum.photos/id/122/1280/720`},1),(0,c.jsx)(`p`,{children:`Overlap test`},2)]},render:(e,t)=>n(i,{args:e},t),tags:[`!dev`,`!autodocs`]},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<img alt="" key={1} src="https://picsum.photos/id/122/1280/720" />, <p key={2}>Overlap test</p>]
+  },
+  render: (args, context) => renderComponentVariants(Overlap, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...u.parameters?.docs?.source}}},d=[`Test`]}))();export{u as Test,d as __namedExportsOrder,l as default};
--- demo-develop/assets/mdx-react-shim-P8fEGct3.js
+++
@@ -1,1 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{t}from"./react-BdJ32BNC.js";var n=e((()=>{t()}));export{n as t};
