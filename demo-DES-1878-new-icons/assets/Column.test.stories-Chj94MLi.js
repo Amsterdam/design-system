75c5f736d330a189b8ad37c3da56501d647b73f4
dlnr <niels@dlnr.dev>
2026-06-11 14:05:43 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@a523b5e0ff1a8b5e0ff583ef74c18a74d111942a 🚀

--- demo-DES-1878-new-icons/assets/Column.test.stories-Chj94MLi.js
+++
@@ -1,7 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-gbjyxvYM.js";import{et as r,t as i}from"./src-Bl8iDVdf.js";import a,{n as o}from"./Column.stories-BptN4rm4.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Layout/Column`},c={render:(e,n)=>t(r,{args:e,layout:`grid`},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Column, {
-    args,
-    layout: 'grid'
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
