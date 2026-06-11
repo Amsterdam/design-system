68b65e9bacf3a25a5eb4dbd4a4aafce4c02a2fd2
GitHub <github-action@users.noreply.github.com>
2026-06-11 09:11:18 +0000

    Remove feature branch folder

--- demo-DES-1859-workflow-to-cleanup-gh-pages-deploys/assets/Radio.test.stories-DjBWHQi_.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-DQKpk98U.js";import{t as r,v as i}from"./src-B9EbjRym.js";import a,{n as o}from"./Radio.stories-C1oeI5KM.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/Forms/Radio`},c={render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Radio, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
