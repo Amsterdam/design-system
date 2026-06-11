65d5091f306e3b0bd897d3b5b0fcbcb5ac839e94
Evi-2003 <69012052+Evi-2003@users.noreply.github.com>
2026-06-11 10:17:12 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@f539af2fac2131365e68a8a2f16f96beae71fecc 🚀

--- demo-DES-1861-progress-list-connector-status/assets/Avatar.test.stories-SRuwkPQS.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-W6MrmFzn.js";import{t as r,vt as i}from"./src-dKuFJXkU.js";import a,{n as o}from"./Avatar.stories-pJVKfP8q.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/Feedback/Avatar`},c={render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Avatar, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
