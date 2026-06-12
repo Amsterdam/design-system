3a6abb322bb87fa79479804c65e7c3d90f57745e
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-06-12 12:40:52 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@b434cb4f636f3e779acfa577ff7f88b53f15b573 🚀

--- demo-develop/assets/HomePage.docs-DRuVMct0.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{Wt as t}from"./iframe-BaDzAcTS.js";import{r as n}from"./react-CaQ62JpC.js";import{l as r,m as i}from"./blocks-CAEULzky.js";import{t as a}from"./mdx-react-shim-0UGdR5Nn.js";import{n as o,t as s}from"./HomePage.stories-COfMBUx5.js";function c(e){let t={h1:`h1`,p:`p`,...n(),...e.components};return(0,u.jsxs)(u.Fragment,{children:[`
-`,`
-`,(0,u.jsx)(r,{of:s}),`
-`,(0,u.jsx)(t.h1,{id:`home-page`,children:`Home page`}),`
-`,(0,u.jsx)(t.p,{children:`The home page of an internal website can have varying types of content.`}),`
-`,(0,u.jsx)(t.p,{children:`Our single example in this category currently focuses on the overall layout.`})]})}function l(e={}){let{wrapper:t}={...n(),...e.components};return t?(0,u.jsx)(t,{...e,children:(0,u.jsx)(c,{...e})}):c(e)}var u;e((()=>{u=t(),a(),i(),o()}))();export{l as default};
---
+++ demo-develop/assets/Menu.test.stories-DR3CU5HT.js
@@ -0,0 +1,20 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{Wt as t}from"./iframe-V168Ju7H.js";import{j as n,t as r}from"./src-rV-ksO7Z.js";import{n as i,t as a}from"./PieChartFill-S4yYqFeU.js";import o,{n as s}from"./Menu.stories-DjntLH6r.js";var c,l,u,d;e((()=>{i(),r(),s(),c=t(),l={...o,title:`Components/Navigation/Menu`},u={render:e=>(0,c.jsxs)(n,{...e,children:[(0,c.jsx)(n.Link,{href:`#`,icon:(0,c.jsx)(a,{}),children:`Dashboard`}),(0,c.jsx)(n.Link,{href:`#`,icon:(0,c.jsx)(a,{}),children:`Projecten`}),(0,c.jsx)(n.Link,{href:`#`,icon:(0,c.jsx)(a,{}),children:`Rapportages`}),(0,c.jsx)(n.Link,{href:`#`,icon:(0,c.jsx)(a,{}),children:`Analyses`}),(0,c.jsx)(n.Link,{className:`hover`,href:`#`,icon:(0,c.jsx)(a,{}),children:`Instellingen`})]}),tags:[`!dev`,`!autodocs`]},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  render: args => <Menu {...args}>
+      <Menu.Link href="#" icon={<SvgPieChartFill />}>
+        Dashboard
+      </Menu.Link>
+      <Menu.Link href="#" icon={<SvgPieChartFill />}>
+        Projecten
+      </Menu.Link>
+      <Menu.Link href="#" icon={<SvgPieChartFill />}>
+        Rapportages
+      </Menu.Link>
+      <Menu.Link href="#" icon={<SvgPieChartFill />}>
+        Analyses
+      </Menu.Link>
+      <Menu.Link className="hover" href="#" icon={<SvgPieChartFill />}>
+        Instellingen
+      </Menu.Link>
+    </Menu>,
+  tags: ['!dev', '!autodocs']
+}`,...u.parameters?.docs?.source}}},d=[`Test`]}))();export{u as Test,d as __namedExportsOrder,l as default};
