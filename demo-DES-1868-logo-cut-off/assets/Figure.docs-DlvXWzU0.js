c581bfdcc32b8811394f16426620be713013417f
Evi-2003 <69012052+Evi-2003@users.noreply.github.com>
2026-06-12 13:51:17 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@abfb949b62129dc8519ea79598155077d2b437f3 🚀

---
+++ demo-DES-1868-logo-cut-off/assets/Figure.docs-DlvXWzU0.js
@@ -0,0 +1,16 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{Wt as t}from"./iframe-s4nwL1J_.js";import{r as n}from"./react-C8y9M_Ug.js";import{a as r,f as i,i as a,l as o,m as s,n as c,u as l}from"./blocks-DwXXp6W7.js";import{n as u,t as d}from"./DesignTokensTable-CVHP4QmF.js";import{t as f}from"./mdx-react-shim-_hM6RSZI.js";import{InverseColour as p,n as m,t as h}from"./Figure.stories-DfgmjB4C.js";var g,_,v=e((()=>{g={figure:{gap:{$value:`{ams.space.s}`,$extensions:{"nl.amsterdam.subtype":`space`,"nl.amsterdam.type":`dimension`}},caption:{color:{$value:`{ams.color.text.default}`,$extensions:{"nl.amsterdam.type":`color`}},"font-family":{$value:`{ams.typography.font-family}`,$extensions:{"nl.amsterdam.type":`fontFamily`}},"font-size":{$value:`{ams.typography.body-text.small.font-size}`,$extensions:{"nl.amsterdam.type":`fontSize`}},"font-weight":{$value:`{ams.typography.body-text.font-weight}`,$extensions:{"nl.amsterdam.type":`fontWeight`}},"line-height":{$value:`{ams.typography.body-text.small.line-height}`,$extensions:{"nl.amsterdam.subtype":`lineHeight`,"nl.amsterdam.type":`number`}},inverse:{color:{$value:`{ams.color.text.inverse}`,$extensions:{"nl.amsterdam.type":`color`}}}}}},_={ams:g}}));function y(e){let t={h2:`h2`,h3:`h3`,p:`p`,...n(),...e.components};return(0,x.jsxs)(x.Fragment,{children:[`
+`,`
+`,`
+`,`
+`,(0,x.jsx)(o,{of:h}),`
+`,(0,x.jsx)(i,{}),`
+`,(0,x.jsx)(r,{of:h}),`
+`,(0,x.jsx)(l,{}),`
+`,(0,x.jsx)(a,{}),`
+`,(0,x.jsx)(t.h2,{id:`examples`,children:`Examples`}),`
+`,(0,x.jsx)(t.h3,{id:`inverse-colour`,children:`Inverse colour`}),`
+`,(0,x.jsx)(t.p,{children:`Use the inverse colour if the Figure Caption sits on a dark background.
+This ensures the colour of the text provides enough contrast.`}),`
+`,(0,x.jsx)(c,{of:p}),`
+`,(0,x.jsx)(t.h2,{id:`design-tokens`,children:`Design tokens`}),`
+`,(0,x.jsx)(d,{tokens:_})]})}function b(e={}){let{wrapper:t}={...n(),...e.components};return t?(0,x.jsx)(t,{...e,children:(0,x.jsx)(y,{...e})}):y(e)}var x;e((()=>{x=t(),f(),s(),m(),v(),u()}))();export{b as default};
---
+++ demo-DES-1868-logo-cut-off/assets/GridColumnsGuide.test.stories-BG5WeRy3.js
@@ -0,0 +1,4 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{Wt as t}from"./iframe-s4nwL1J_.js";import{n,t as r}from"./GridColumnsGuide-DrFNPxQn.js";var i,a,o,s;e((()=>{n(),i=t(),a={title:`Components/Docs/Grid Columns Guide`},o={render:()=>(0,i.jsx)(r,{}),tags:[`!dev`,`!autodocs`]},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  render: () => <GridColumnsGuide />,
+  tags: ['!dev', '!autodocs']
+}`,...o.parameters?.docs?.source}}},s=[`Test`]}))();export{o as Test,s as __namedExportsOrder,a as default};
