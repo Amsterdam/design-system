8d021e3be184baba716d160583eac761f99c793c
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-06-16 10:09:43 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@a5e46c2ecaee44581e8744ead624332ea08149b2 🚀

---
+++ demo-storybook-prop-hiding/assets/Tabs.stories-DtVDDMxP.js
@@ -0,0 +1,27 @@
+import{a as e,i as t}from"./preload-helper-xPQekRTU.js";import{t as n}from"./jsx-runtime-BqsN2jGA.js";import{I as r,X as i}from"./iframe-DB4UCbr9.js";import{t as a,u as o}from"./src-D1hpWpkN.js";var s=e({Default:()=>d,__namedExportsOrder:()=>f,default:()=>l}),c,l,u,d,f,p=t((()=>{i(),a(),c=n(),l={title:`Components/Containers/Tabs`,component:o,argTypes:{onTabChange:{action:`clicked`,description:`Provides the id of the activated tab.`}}},u=e=>(0,c.jsx)(`div`,{style:{maxInlineSize:`20rem`},children:(0,c.jsx)(e,{})}),d={args:{children:[(0,c.jsxs)(o.List,{children:[(0,c.jsx)(o.Button,{"aria-controls":`west`,children:`West`}),(0,c.jsx)(o.Button,{"aria-controls":`centrum`,children:`Centrum`}),(0,c.jsx)(o.Button,{"aria-controls":`oost`,children:`Oost`})]},`list`),(0,c.jsx)(o.Panel,{id:`west`,children:(0,c.jsxs)(r,{children:[`Bos en Lommerplein 250`,(0,c.jsx)(`br`,{}),`1055 EK Amsterdam`]})},`west`),(0,c.jsx)(o.Panel,{id:`centrum`,children:(0,c.jsxs)(r,{children:[`Amstel 1`,(0,c.jsx)(`br`,{}),`1011 PN Amsterdam`]})},`centrum`),(0,c.jsx)(o.Panel,{id:`oost`,children:(0,c.jsxs)(r,{children:[`Oranje-Vrijstaatplein 2`,(0,c.jsx)(`br`,{}),`1093 NG Amsterdam`]})},`oost`)]},decorators:[u]},d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<Tabs.List key="list">
+        <Tabs.Button aria-controls="west">West</Tabs.Button>
+        <Tabs.Button aria-controls="centrum">Centrum</Tabs.Button>
+        <Tabs.Button aria-controls="oost">Oost</Tabs.Button>
+      </Tabs.List>, <Tabs.Panel id="west" key="west">
+        <Paragraph>
+          Bos en Lommerplein 250
+          <br />
+          1055 EK Amsterdam
+        </Paragraph>
+      </Tabs.Panel>, <Tabs.Panel id="centrum" key="centrum">
+        <Paragraph>
+          Amstel 1
+          <br />
+          1011 PN Amsterdam
+        </Paragraph>
+      </Tabs.Panel>, <Tabs.Panel id="oost" key="oost">
+        <Paragraph>
+          Oranje-Vrijstaatplein 2<br />
+          1093 NG Amsterdam
+        </Paragraph>
+      </Tabs.Panel>]
+  },
+  decorators: [maxInlineSizeDecorator]
+}`,...d.parameters?.docs?.source}}},f=[`Default`]}));p();export{d as Default,f as __namedExportsOrder,l as default,p as n,s as t};
---
+++ demo-storybook-prop-hiding/assets/TextArea.test.stories-CWdJgDx8.js
@@ -0,0 +1,10 @@
+import{i as e}from"./preload-helper-xPQekRTU.js";import{n as t,t as n}from"./renderComponentVariants-DErY3BYt.js";import{c as r,t as i}from"./src-D1hpWpkN.js";import a,{n as o}from"./TextArea.stories-CG6EXwI8.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Text Area`},c={args:{defaultValue:`Het waterrijke achterland van de provincie Holland was een paradijs voor vissers. Maar terwijl de visserij floreerde in Holland, was Amsterdam eigenlijk helemaal geen visserijstad. Toch maakten Amsterdammers naam in de vishandel. Zij speelden namelijk een cruciale rol bij het klaarmaken en vervoeren van haring.`},render:(e,n)=>t(r,{args:e,variants:[`disabled`,`hovered`]},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    defaultValue: 'Het waterrijke achterland van de provincie Holland was een paradijs voor vissers. Maar terwijl de visserij floreerde in Holland, was Amsterdam eigenlijk helemaal geen visserijstad. Toch maakten Amsterdammers naam in de vishandel. Zij speelden namelijk een cruciale rol bij het klaarmaken en vervoeren van haring.'
+  },
+  render: (args, context) => renderComponentVariants(TextArea, {
+    args,
+    variants: ['disabled', 'hovered']
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
