cfb6158e241ec6d34793b8695c35a98aa54e7e33
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-06-16 21:52:00 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@5de2721b595aee5695c5b25c15b56a1f6f1ff3cb 🚀

---
+++ demo-DES-1881-logo-licence/assets/Figure.test.stories-DJAG1cXB.js
@@ -0,0 +1,19 @@
+import{i as e}from"./preload-helper-xPQekRTU.js";import{t}from"./jsx-runtime-BqsN2jGA.js";import{t as n,wt as r}from"./src-nyJyAV0L.js";import i,{n as a}from"./Figure.stories-BYFXW13d.js";var o,s,c,l;e((()=>{n(),a(),o=t(),s={...i,title:`Components/Media/Figure`},c={render:e=>(0,o.jsxs)(`div`,{className:`_ams-tests-stack`,children:[(0,o.jsxs)(r,{...e,children:[(0,o.jsx)(`img`,{alt:``,sizes:`(max-width: 36rem) 640px, (max-width: 68rem) 1280px, 1440px`,src:`https://picsum.photos/id/122/1280/720`}),(0,o.jsx)(r.Caption,{children:`Een schuin geplaatste fiets tegen een lantaarnpaal, met op de achtergrond een karakteristiek houten bruggetje.`})]}),(0,o.jsxs)(r,{...e,style:{backgroundColor:`var(--ams-color-highlight-azure)`},children:[(0,o.jsx)(`img`,{alt:``,sizes:`(max-width: 36rem) 640px, (max-width: 68rem) 1280px, 1440px`,src:`https://picsum.photos/id/122/1280/720`}),(0,o.jsx)(r.Caption,{color:`inverse`,children:`Een schuin geplaatste fiets tegen een lantaarnpaal, met op de achtergrond een karakteristiek houten bruggetje.`})]})]}),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: args => <div className="_ams-tests-stack">
+      <Figure {...args}>
+        <img alt="" sizes="(max-width: 36rem) 640px, (max-width: 68rem) 1280px, 1440px" src="https://picsum.photos/id/122/1280/720" />
+        <Figure.Caption>
+          Een schuin geplaatste fiets tegen een lantaarnpaal, met op de achtergrond een karakteristiek houten bruggetje.
+        </Figure.Caption>
+      </Figure>
+      <Figure {...args} style={{
+      backgroundColor: 'var(--ams-color-highlight-azure)'
+    }}>
+        <img alt="" sizes="(max-width: 36rem) 640px, (max-width: 68rem) 1280px, 1440px" src="https://picsum.photos/id/122/1280/720" />
+        <Figure.Caption color="inverse">
+          Een schuin geplaatste fiets tegen een lantaarnpaal, met op de achtergrond een karakteristiek houten bruggetje.
+        </Figure.Caption>
+      </Figure>
+    </div>,
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
