61b4c236e916e95811332ff03615387912faad77
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-06-16 09:48:05 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@f97d2c425c1476dfeee63efa3d80cf891f8ecd97 🚀

---
+++ demo-DES-1856-date-picker/assets/Checkbox.test.stories-BNwNCQPW.js
@@ -0,0 +1,6 @@
+import{i as e}from"./preload-helper-xPQekRTU.js";import{n as t,t as n}from"./renderComponentVariants-DEG0kUD-.js";import{Ut as r,t as i}from"./src-BFSQTAuP.js";import a,{n as o}from"./Checkbox.stories-CXHLwXk_.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Checkbox`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Checkbox, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
