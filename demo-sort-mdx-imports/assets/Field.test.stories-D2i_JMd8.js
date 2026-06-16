7d05d15ccd8100dbfcf929d546a7611296513c5b
RubenSibon <mail+github@rubensibon.nl>
2026-06-16 11:25:14 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@9d207cbd613d4e0a3ed91c060be3bff80777bd70 🚀

---
+++ demo-sort-mdx-imports/assets/Field.test.stories-D2i_JMd8.js
@@ -0,0 +1,13 @@
+import{i as e}from"./preload-helper-xPQekRTU.js";import{t}from"./jsx-runtime-BqsN2jGA.js";import{E as n,X as r}from"./iframe-3H5ouLx4.js";import{n as i,t as a}from"./renderComponentVariants-B0vRSe2r.js";import{Ot as o,t as s}from"./src-DzthRg3P.js";import c,{n as l}from"./Field.stories-MMmH_8ct.js";var u,d,f,p;e((()=>{r(),s(),a(),l(),u=t(),d={...c,title:`Components/Forms/Field`},f={args:{children:[(0,u.jsx)(n,{htmlFor:`input-a1`,children:`Wat is uw achternaam?`},1),(0,u.jsx)(`input`,{id:`input-a1`,value:`Dijkstra`},2)]},render:(e,t)=>i(o,{args:e},t),tags:[`!dev`,`!autodocs`]},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [
+    // We use Label because \`<label>\` is inline and wouldn’t get the margin we want to test.
+    <Label htmlFor="input-a1" key={1}>
+        Wat is uw achternaam?
+      </Label>, <input id="input-a1" key={2} value="Dijkstra" />]
+  },
+  render: (args, context) => renderComponentVariants(Field, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...f.parameters?.docs?.source}}},p=[`Test`]}))();export{f as Test,p as __namedExportsOrder,d as default};
--- demo-sort-mdx-imports/assets/Icon.test.stories-ocqInjii.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./renderComponentVariants-CrQC6DyV.js";import{Ot as r,t as i}from"./src-6YGmoX3p.js";import a,{n as o}from"./Icon.stories-BNnwhp57.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Media/Icon`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Icon, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
