ee7b93d794046f68ffda2530d0d7b25bcccfeadb
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-06-16 07:28:27 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@bc29849b65833843e441621c30f15df0ce06d496 🚀

---
+++ demo-npm_and_yarn/storybook/npm_and_yarn-bed0206944/assets/TextInput.test.stories-CRTldjmM.js
@@ -0,0 +1,7 @@
+import{i as e}from"./preload-helper-xPQekRTU.js";import{n as t,t as n}from"./renderComponentVariants-D_roAHRX.js";import{o as r,t as i}from"./src-8rGGq4Z3.js";import a,{n as o}from"./TextInput.stories-KuDQPawr.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Text Input`},c={render:(e,n)=>t(r,{args:e,variants:[`disabled`,`hovered`]},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(TextInput, {
+    args,
+    variants: ['disabled', 'hovered']
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
