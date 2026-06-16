889767c9adf4fc032b803b5ec40f8e0ba3cff432
GitHub <github-action@users.noreply.github.com>
2026-06-16 09:56:02 +0000

    Remove feature branch folder

--- demo-fix-jsdoc-positions/assets/Select.test.stories-CtXtymNL.js
+++
@@ -1,7 +0,0 @@
-import{i as e}from"./preload-helper-xPQekRTU.js";import{n as t,t as n}from"./renderComponentVariants-DDdll9b1.js";import{D as r,t as i}from"./src-B82lhPrH.js";import a,{n as o}from"./Select.stories-5d1QWzjr.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Select`},c={render:(e,n)=>t(r,{args:e,variants:[`hovered`,`disabled`]},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Select, {
-    args,
-    variants: ['hovered', 'disabled']
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
