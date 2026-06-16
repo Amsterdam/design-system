40e4b1711f982d3498acf9dc054af2afd588fab3
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-06-16 11:55:53 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@c6a00d763b001751905e18e8a4543b9c5adc7adc 🚀

---
+++ demo-develop/assets/SkipLink.docs-CmEuElb6.js
@@ -0,0 +1,38 @@
+import{i as e}from"./preload-helper-xPQekRTU.js";import{t}from"./jsx-runtime-BqsN2jGA.js";import{E as n,a as r,f as i,i as a,l as o,m as s,n as c,u as l}from"./blocks-D4ycsoKH.js";import{n as u,t as d}from"./DesignTokensTable-Bxr3Hmq0.js";import{t as f}from"./mdx-react-shim-M_gE3yRc.js";import{MultipleLinks as p,OnFocus as m,n as h,t as g}from"./SkipLink.stories-B2krYgVC.js";var _,v,y=e((()=>{_={"skip-link":{"background-color":{$value:`{ams.color.interactive.default}`,$extensions:{"nl.amsterdam.type":`color`}},color:{$value:`{ams.color.text.inverse}`,$extensions:{"nl.amsterdam.type":`color`}},"font-family":{$value:`{ams.typography.font-family}`,$extensions:{"nl.amsterdam.type":`fontFamily`}},"font-size":{$value:`{ams.typography.body-text.font-size}`,$extensions:{"nl.amsterdam.type":`fontSize`}},"font-weight":{$value:`{ams.typography.body-text.font-weight}`,$extensions:{"nl.amsterdam.type":`fontWeight`}},"line-height":{$value:`{ams.typography.body-text.line-height}`,$extensions:{"nl.amsterdam.subtype":`lineHeight`,"nl.amsterdam.type":`number`}},"outline-offset":{$value:`{ams.focus.outline-offset}`,$extensions:{"nl.amsterdam.subtype":`space`,"nl.amsterdam.type":`dimension`}},"padding-block":{$value:`{ams.space.s}`,$extensions:{"nl.amsterdam.subtype":`space`,"nl.amsterdam.type":`dimension`}},"padding-inline":{$value:`{ams.space.m}`,$extensions:{"nl.amsterdam.subtype":`space`,"nl.amsterdam.type":`dimension`}},hover:{"background-color":{$value:`{ams.color.interactive.hover}`,$extensions:{"nl.amsterdam.type":`color`}}}}},v={ams:_}}));function b(e){let t={a:`a`,code:`code`,h2:`h2`,h3:`h3`,li:`li`,p:`p`,ul:`ul`,...n(),...e.components};return(0,S.jsxs)(S.Fragment,{children:[`
+`,`
+`,`
+`,`
+`,(0,S.jsx)(o,{of:g}),`
+`,(0,S.jsx)(i,{}),`
+`,(0,S.jsx)(r,{of:g}),`
+`,(0,S.jsx)(l,{}),`
+`,(0,S.jsx)(a,{}),`
+`,(0,S.jsx)(t.h2,{id:`usage-guidelines`,children:`Usage guidelines`}),`
+`,(0,S.jsx)(t.h3,{id:`when-to-use`,children:`When to use`}),`
+`,(0,S.jsxs)(t.p,{children:[`Place the Skip Link as the first element in the `,(0,S.jsx)(t.a,{href:`/docs/components-containers-page--docs`,children:`Page`}),` container.`]}),`
+`,(0,S.jsxs)(t.p,{children:[`Direct the Skip Link to the main content area on the Page.
+Typically, this is the `,(0,S.jsx)(t.code,{children:`main`}),` HTML element.
+Repeated navigation blocks such as Breadcrumbs and back Links do not belong to the main content area.`]}),`
+`,(0,S.jsx)(t.p,{children:`Complex pages with multiple sections may benefit from more than one Skip Link.`}),`
+`,(0,S.jsx)(t.h3,{id:`when-not-to-use`,children:`When not to use`}),`
+`,(0,S.jsx)(t.p,{children:`Skip Links are unnecessary on a simple page with only text and minimal navigation.`}),`
+`,(0,S.jsx)(t.h3,{id:`how-to-use`,children:`How to use`}),`
+`,(0,S.jsxs)(t.p,{children:[`Set `,(0,S.jsx)(t.code,{children:`id="inhoud"`}),` on the `,(0,S.jsx)(t.code,{children:`main`}),` HTML element and then use `,(0,S.jsx)(t.code,{children:`href="#inhoud"`}),` on the Skip Link.
+If you need multiple words to describe a Skip Link target, use kebab case (`,(0,S.jsx)(t.code,{children:`id="op-deze-manier"`}),`).`]}),`
+`,(0,S.jsx)(t.h2,{id:`examples`,children:`Examples`}),`
+`,(0,S.jsx)(t.h3,{id:`display-on-focus`,children:`Display on focus`}),`
+`,(0,S.jsx)(t.p,{children:`A Skip Link is only displayed when it receives focus.`}),`
+`,(0,S.jsx)(c,{of:m}),`
+`,(0,S.jsx)(t.h3,{id:`multiple-links`,children:`Multiple links`}),`
+`,(0,S.jsx)(t.p,{children:`You can use more than one Skip Link if you have a complex page with multiple sections.`}),`
+`,(0,S.jsx)(c,{of:p}),`
+`,(0,S.jsx)(t.h2,{id:`design`,children:`Design`}),`
+`,(0,S.jsx)(t.p,{children:`The Skip Link sits above the Page Header and is as wide as the Page container.
+It remains hidden until activated with the ‘Tab’ key.
+After appearing, it pushes the rest of the page down.`}),`
+`,(0,S.jsx)(t.h2,{id:`see-also`,children:`See also`}),`
+`,(0,S.jsxs)(t.ul,{children:[`
+`,(0,S.jsxs)(t.li,{children:[(0,S.jsx)(t.a,{href:`/docs/components-containers-page--docs`,children:`Page`}),` – contains the Skip Link as its first element.`]}),`
+`]}),`
+`,(0,S.jsx)(t.h2,{id:`design-tokens`,children:`Design tokens`}),`
+`,(0,S.jsx)(d,{tokens:v})]})}function x(e={}){let{wrapper:t}={...n(),...e.components};return t?(0,S.jsx)(t,{...e,children:(0,S.jsx)(b,{...e})}):b(e)}var S;e((()=>{S=t(),f(),s(),h(),y(),u()}))();export{x as default};
