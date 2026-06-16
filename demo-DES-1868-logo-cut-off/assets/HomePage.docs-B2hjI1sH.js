ce4fd41760e01e993691de8771d1f0830c5e488b
GitHub <github-action@users.noreply.github.com>
2026-06-16 11:15:49 +0000

    Remove feature branch folder

--- demo-DES-1868-logo-cut-off/assets/HomePage.docs-B2hjI1sH.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{Wt as t}from"./iframe-DdhP54ey.js";import{r as n}from"./react-BUR_9oqh.js";import{l as r,m as i}from"./blocks-2KbOPfGb.js";import{t as a}from"./mdx-react-shim-C7os_57L.js";import{n as o,t as s}from"./HomePage.stories-DZQpQzde.js";function c(e){let t={h1:`h1`,p:`p`,...n(),...e.components};return(0,u.jsxs)(u.Fragment,{children:[`
-`,`
-`,(0,u.jsx)(r,{of:s}),`
-`,(0,u.jsx)(t.h1,{id:`home-page`,children:`Home page`}),`
-`,(0,u.jsx)(t.p,{children:`The home page of an internal website can have varying types of content.`}),`
-`,(0,u.jsx)(t.p,{children:`Our single example in this category currently focuses on the overall layout.`})]})}function l(e={}){let{wrapper:t}={...n(),...e.components};return t?(0,u.jsx)(t,{...e,children:(0,u.jsx)(c,{...e})}):c(e)}var u;e((()=>{u=t(),a(),i(),o()}))();export{l as default};
