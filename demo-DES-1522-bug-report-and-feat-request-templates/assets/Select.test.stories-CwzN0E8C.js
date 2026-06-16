0ff097bb666eb702e31876250d1091f882050fc0
RubenSibon <mail+github@rubensibon.nl>
2026-06-16 10:07:33 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@7579909fa9b93c4ec89455ba3b85cfabbb525ac7 🚀

--- demo-DES-1522-bug-report-and-feat-request-templates/assets/Select.test.stories-CwzN0E8C.js
+++
@@ -1,7 +0,0 @@
-import{i as e}from"./preload-helper-xPQekRTU.js";import{n as t,t as n}from"./renderComponentVariants-IJtUbyWA.js";import{D as r,t as i}from"./src-Ymh-2C0f.js";import a,{n as o}from"./Select.stories-Du-MaI0V.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Select`},c={render:(e,n)=>t(r,{args:e,variants:[`hovered`,`disabled`]},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Select, {
-    args,
-    variants: ['hovered', 'disabled']
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
