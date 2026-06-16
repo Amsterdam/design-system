95390488f5988b034d1ed4ad70622aa5a52b75db
GitHub <github-action@users.noreply.github.com>
2026-06-16 11:15:01 +0000

    Remove feature branch folder

--- demo-DES-1856-date-picker/assets/FileInput.test.stories-ogt8eGK1.js
+++
@@ -1,7 +0,0 @@
-import{i as e}from"./preload-helper-xPQekRTU.js";import{n as t,t as n}from"./renderComponentVariants-D9sype7b.js";import{St as r,t as i}from"./src-e1sG9xMG.js";import a,{n as o}from"./FileInput.stories-BltAw2xw.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/File Input`},c={render:(e,n)=>t(r,{args:e,variants:[`disabled`,`multiple`]},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(FileInput, {
-    args,
-    variants: ['disabled', 'multiple']
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
