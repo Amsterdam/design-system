2776e77a81df2b3116ccd230c336f5d06b23d5b3
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-06-16 11:16:08 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@b7c5a1091aeafac1b3f75f657c2dce7e92c5da50 🚀

---
+++ demo-develop/assets/Page.test.stories-H-i3LtXT.js
@@ -0,0 +1,6 @@
+import{i as e}from"./preload-helper-xPQekRTU.js";import{n as t,t as n}from"./renderComponentVariants-BvKbe3S5.js";import{K as r,t as i}from"./src-DnDYnxod.js";import a,{n as o}from"./Page.stories-CRYWAGv6.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Containers/Page`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Page, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
--- demo-develop/assets/TableOfContents.test.stories-DCqNavZM.js
+++
@@ -1,6 +0,0 @@
-import{i as e}from"./preload-helper-xPQekRTU.js";import{n as t,t as n}from"./renderComponentVariants-DTDck1lD.js";import{m as r,t as i}from"./src-adaEnSJM.js";import a,{n as o}from"./TableOfContents.stories-GE16YN8I.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/TableOfContents`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(TableOfContents, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
