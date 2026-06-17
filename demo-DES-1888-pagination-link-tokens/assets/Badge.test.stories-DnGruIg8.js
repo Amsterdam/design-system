12366096d58469ecfc754577a329037172876d01
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-06-17 13:59:45 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@a87b67a2048458cb6b9fd1c40a1a4c2dc688f871 🚀

--- demo-DES-1888-pagination-link-tokens/assets/Badge.test.stories-DnGruIg8.js
+++
@@ -1,6 +0,0 @@
-import{i as e}from"./preload-helper-xPQekRTU.js";import{n as t,t as n}from"./renderComponentVariants-CkEMySrM.js";import{pn as r,t as i}from"./src-DUBqdyfg.js";import a,{n as o}from"./Badge.stories-hJuzBJuj.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Feedback/Badge`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Badge, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
---
+++ demo-DES-1888-pagination-link-tokens/assets/mdx-react-shim-CFGfl2oW.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-xPQekRTU.js";import{C as t}from"./blocks-k2iFNE-j.js";var n=e((()=>{t()}));export{n as t};
