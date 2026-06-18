b7aeff08a3aa852c1b7f4d08e12c05f48aea6eb6
Evi Wammes <e.wammes@amsterdam.nl>
2026-06-18 14:12:26 +0200

    chore(Icon): Add visual regression tests for all icons

--- storybook/src/components/Icon/Icon.test.stories.tsx
+++ storybook/src/components/Icon/Icon.test.stories.tsx
@@ -5,6 +5,7 @@
 
 import type { Meta, StoryObj } from '@storybook/react-vite'
 
+import * as Icons from '@amsterdam/design-system-react-icons/src'
 import { Icon } from '@amsterdam/design-system-react/src'
 
 import { renderComponentVariants } from '#storybook/_common/renderComponentVariants'
@@ -20,7 +21,20 @@ export default meta
 
 type Story = StoryObj<typeof meta>
 
+const iconNames = Object.keys(Icons) as (keyof typeof Icons)[]
+
 export const Test: Story = {
-  render: (args, context) => renderComponentVariants(Icon, { args }, context),
+  render: (args, context) => (
+    <div className="_ams-tests-stack">
+      <span>Variants of Icon component</span>
+      {renderComponentVariants(Icon, { args }, context)}
+      <span>All icons</span>
+      <div className="_ams-tests-grid">
+        {iconNames.map((name) => (
+          <Icon key={name} size="heading-2" svg={Icons[name]} />
+        ))}
+      </div>
+    </div>
+  ),
   tags: ['!dev', '!autodocs'],
 }
