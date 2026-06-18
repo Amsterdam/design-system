d3fcaccad75e144440a18e95b62406903326613f
GitHub <github-action@users.noreply.github.com>
2026-06-18 11:00:42 +0000

    Remove feature branch folder

--- demo-DES-1881-logo-licence/assets/Alert.test.stories-lkNAjXQo.js
+++
@@ -1,6 +0,0 @@
-import{i as e}from"./preload-helper-xPQekRTU.js";import{n as t,t as n}from"./renderComponentVariants-DEomfyLS.js";import{t as r,yn as i}from"./src-nyJyAV0L.js";import a,{n as o}from"./Alert.stories-hqmZbX3G.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/Feedback/Alert`},c={render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Alert, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
--- demo-DES-1881-logo-licence/assets/Column.test.stories-DT_nSS0u.js
+++
@@ -1,7 +0,0 @@
-import{i as e}from"./preload-helper-xPQekRTU.js";import{n as t,t as n}from"./renderComponentVariants-DEomfyLS.js";import{Bt as r,t as i}from"./src-nyJyAV0L.js";import a,{n as o}from"./Column.stories-DTV98zta.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Layout/Column`},c={render:(e,n)=>t(r,{args:e,layout:`grid`},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Column, {
-    args,
-    layout: 'grid'
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
