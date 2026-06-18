3c9c8d3d0eee53dd544ff9ec185c9a4934d2124d
GitHub <github-action@users.noreply.github.com>
2026-06-18 18:11:55 +0000

    Remove feature branch folder

--- demo-page-footer-visual-test-fix/assets/CallToActionLink.test.stories-DKeL9RA2.js
+++
@@ -1,6 +0,0 @@
-import{i as e}from"./preload-helper-xPQekRTU.js";import{n as t,t as n}from"./renderComponentVariants-CSxje33f.js";import{t as r,tn as i}from"./src-HkRZ0hnc.js";import a,{n as o}from"./CallToActionLink.stories-CPDVZ-qX.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/Navigation/Call to Action Link`},c={render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(CallToActionLink, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
