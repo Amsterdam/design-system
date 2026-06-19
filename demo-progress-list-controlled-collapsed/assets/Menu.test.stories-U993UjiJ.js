3b93d7e6e2bfce8410d1aec8b6e5e8ea0f9d5acc
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-06-19 22:56:39 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@a071cff61cde5178aef6cc6444213f1209b83ff0 🚀

---
+++ demo-progress-list-controlled-collapsed/assets/Menu.test.stories-U993UjiJ.js
@@ -0,0 +1,20 @@
+import{i as e}from"./preload-helper-xPQekRTU.js";import{t}from"./jsx-runtime-BqsN2jGA.js";import{Q as n,t as r}from"./src-B7ASwlhA.js";import{n as i,t as a}from"./PieChartFill-DBrDEXR9.js";import o,{n as s}from"./Menu.stories-BwAuAmAE.js";var c,l,u,d;e((()=>{i(),r(),s(),c=t(),l={...o,title:`Components/Navigation/Menu`},u={render:e=>(0,c.jsxs)(n,{...e,children:[(0,c.jsx)(n.Link,{href:`#`,icon:(0,c.jsx)(a,{}),children:`Dashboard`}),(0,c.jsx)(n.Link,{href:`#`,icon:(0,c.jsx)(a,{}),children:`Projecten`}),(0,c.jsx)(n.Link,{href:`#`,icon:(0,c.jsx)(a,{}),children:`Rapportages`}),(0,c.jsx)(n.Link,{href:`#`,icon:(0,c.jsx)(a,{}),children:`Analyses`}),(0,c.jsx)(n.Link,{className:`hover`,href:`#`,icon:(0,c.jsx)(a,{}),children:`Instellingen`})]}),tags:[`!dev`,`!autodocs`]},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  render: args => <Menu {...args}>
+      <Menu.Link href="#" icon={<SvgPieChartFill />}>
+        Dashboard
+      </Menu.Link>
+      <Menu.Link href="#" icon={<SvgPieChartFill />}>
+        Projecten
+      </Menu.Link>
+      <Menu.Link href="#" icon={<SvgPieChartFill />}>
+        Rapportages
+      </Menu.Link>
+      <Menu.Link href="#" icon={<SvgPieChartFill />}>
+        Analyses
+      </Menu.Link>
+      <Menu.Link className="hover" href="#" icon={<SvgPieChartFill />}>
+        Instellingen
+      </Menu.Link>
+    </Menu>,
+  tags: ['!dev', '!autodocs']
+}`,...u.parameters?.docs?.source}}},d=[`Test`]}))();export{u as Test,d as __namedExportsOrder,l as default};
---
+++ demo-progress-list-controlled-collapsed/assets/Spotlight.docs-C5SRi04i.js
@@ -0,0 +1,32 @@
+import{i as e}from"./preload-helper-xPQekRTU.js";import{t}from"./jsx-runtime-BqsN2jGA.js";import{E as n,a as r,f as i,i as a,l as o,m as s,n as c,u as l}from"./blocks-CnyfTITp.js";import{n as u,t as d}from"./DesignTokensTable-Dzflt1GR.js";import{t as f}from"./mdx-react-shim-CFzJCioC.js";import{HighlightContent as p,n as m,t as h}from"./Spotlight.stories-BKdJaI0n.js";var g,_,v=e((()=>{g={spotlight:{"background-color":{$value:`{ams.color.highlight.purple}`,$extensions:{"nl.amsterdam.type":`color`}},azure:{"background-color":{$value:`{ams.color.highlight.azure}`,$extensions:{"nl.amsterdam.type":`color`}}},green:{"background-color":{$value:`{ams.color.highlight.green}`,$extensions:{"nl.amsterdam.type":`color`}}},lime:{"background-color":{$value:`{ams.color.highlight.lime}`,$extensions:{"nl.amsterdam.type":`color`}}},magenta:{"background-color":{$value:`{ams.color.highlight.magenta}`,$extensions:{"nl.amsterdam.type":`color`}}},orange:{"background-color":{$value:`{ams.color.highlight.orange}`,$extensions:{"nl.amsterdam.type":`color`}}},yellow:{"background-color":{$value:`{ams.color.highlight.yellow}`,$extensions:{"nl.amsterdam.type":`color`}}}}},_={ams:g}}));function y(e){let t={a:`a`,code:`code`,h2:`h2`,h3:`h3`,li:`li`,p:`p`,ul:`ul`,...n(),...e.components};return(0,x.jsxs)(x.Fragment,{children:[`
+`,`
+`,`
+`,`
+`,(0,x.jsx)(o,{of:h}),`
+`,(0,x.jsx)(i,{}),`
+`,(0,x.jsx)(r,{of:h}),`
+`,(0,x.jsx)(l,{}),`
+`,(0,x.jsx)(a,{}),`
+`,(0,x.jsx)(t.h2,{id:`usage-guidelines`,children:`Usage guidelines`}),`
+`,(0,x.jsx)(t.h3,{id:`when-not-to-use`,children:`When not to use`}),`
+`,(0,x.jsxs)(t.p,{children:[`Do not position a Spotlight on the `,(0,x.jsx)(t.a,{href:`/docs/components-layout-grid--docs`,children:`Grid`}),`.`]}),`
+`,(0,x.jsx)(t.h3,{id:`how-to-use`,children:`How to use`}),`
+`,(0,x.jsxs)(t.p,{children:[`Use the entire width of the `,(0,x.jsx)(t.a,{href:`/docs/components-containers-page--docs`,children:`Page`}),` container.
+Wrap its content in a Grid with extra large vertical padding.`]}),`
+`,(0,x.jsxs)(t.p,{children:[`Use any of the `,(0,x.jsx)(t.a,{href:`/docs/brand-design-tokens-colour--docs`,children:`highlight colours`}),` for the background.
+The design system does not prescribe a meaning to any of these colours.
+The default is purple.
+Select `,(0,x.jsx)(t.a,{href:`/docs/brand-design-tokens-colour--docs#pairing-foreground-with-background-colours`,children:`the correct colour`}),` for text and links against the background you choose.`]}),`
+`,(0,x.jsxs)(t.p,{children:[`Find a fitting setup for the `,(0,x.jsx)(t.a,{href:`/docs/pages-public-introduction--docs#how-to-size-the-grid-cells`,children:`amount and width of columns`}),`.`]}),`
+`,(0,x.jsxs)(t.p,{children:[`Use the `,(0,x.jsx)(t.code,{children:`as`}),` prop to render a meaningful HTML element: `,(0,x.jsx)(t.code,{children:`as="section"`}),` for a self-contained content area, `,(0,x.jsx)(t.code,{children:`as="aside"`}),` for supporting content.`]}),`
+`,(0,x.jsx)(t.h2,{id:`examples`,children:`Examples`}),`
+`,(0,x.jsx)(t.h3,{id:`highlight-content`,children:`Highlight content`}),`
+`,(0,x.jsx)(t.p,{children:`Use a Spotlight to direct the user’s attention to the most important or actionable content on the page.`}),`
+`,(0,x.jsx)(c,{of:p}),`
+`,(0,x.jsx)(t.h2,{id:`see-also`,children:`See also`}),`
+`,(0,x.jsxs)(t.ul,{children:[`
+`,(0,x.jsxs)(t.li,{children:[(0,x.jsx)(t.a,{href:`/docs/components-containers-page--docs`,children:`Page`}),` – contains the Spotlight.`]}),`
+`,(0,x.jsxs)(t.li,{children:[(0,x.jsx)(t.a,{href:`/docs/components-layout-grid--docs`,children:`Grid`}),` – wraps the content of the Spotlight.`]}),`
+`]}),`
+`,(0,x.jsx)(t.h2,{id:`design-tokens`,children:`Design tokens`}),`
+`,(0,x.jsx)(d,{tokens:_})]})}function b(e={}){let{wrapper:t}={...n(),...e.components};return t?(0,x.jsx)(t,{...e,children:(0,x.jsx)(y,{...e})}):y(e)}var x;e((()=>{x=t(),f(),s(),u(),v(),m()}))();export{b as default};
