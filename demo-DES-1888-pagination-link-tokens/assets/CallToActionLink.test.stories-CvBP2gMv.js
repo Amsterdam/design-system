db4ed6b8573a7b284a59a01f19467fc616188441
GitHub <github-action@users.noreply.github.com>
2026-06-19 22:39:48 +0000

    Remove feature branch folder

--- demo-DES-1888-pagination-link-tokens/assets/CallToActionLink.test.stories-CvBP2gMv.js
+++
@@ -1,6 +0,0 @@
-import{i as e}from"./preload-helper-xPQekRTU.js";import{n as t,t as n}from"./renderComponentVariants-V7HkFt5x.js";import{t as r,tn as i}from"./src-DoV2gc7X.js";import a,{n as o}from"./CallToActionLink.stories-CELPcY24.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/Navigation/Call to Action Link`},c={render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(CallToActionLink, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
--- demo-DES-1888-pagination-link-tokens/assets/mdx-react-shim-CNtDwhPq.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-xPQekRTU.js";import{C as t}from"./blocks-CObIvyI3.js";var n=e((()=>{t()}));export{n as t};
