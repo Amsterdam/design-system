5c0d9092899baa40d57a6f8a8adddd6609030eb8
GitHub <github-action@users.noreply.github.com>
2026-06-19 12:08:25 +0000

    Remove feature branch folder

--- demo-DES-650-diaglog-autofocus-documentation/assets/GridColumnsGuide.test.stories-_2ht8c1f.js
+++
@@ -1,4 +0,0 @@
-import{i as e}from"./preload-helper-xPQekRTU.js";import{t}from"./jsx-runtime-BqsN2jGA.js";import{n,t as r}from"./GridColumnsGuide-Chd02bYe.js";var i,a,o,s;e((()=>{n(),i=t(),a={title:`Components/Docs/Grid Columns Guide`},o={render:()=>(0,i.jsx)(r,{}),tags:[`!dev`,`!autodocs`]},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
-  render: () => <GridColumnsGuide />,
-  tags: ['!dev', '!autodocs']
-}`,...o.parameters?.docs?.source}}},s=[`Test`]}))();export{o as Test,s as __namedExportsOrder,a as default};
