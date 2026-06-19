eaddfb75b9256278b56eb3af7fd19fb3e43e9a32
Evi-2003 <69012052+Evi-2003@users.noreply.github.com>
2026-06-19 12:17:56 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@95d4efae02d00b10d56045d723b3ef160455376d 🚀

--- demo-deprecation-message-generator/assets/HomePage.docs-Dtf1GAQR.js
+++
@@ -1,7 +0,0 @@
-import{i as e}from"./preload-helper-xPQekRTU.js";import{t}from"./jsx-runtime-BqsN2jGA.js";import{E as n,l as r,m as i}from"./blocks-BH5Qbaj0.js";import{t as a}from"./mdx-react-shim-uRs_E9Fe.js";import{n as o,t as s}from"./HomePage.stories-jXFxzG2S.js";function c(e){let t={h1:`h1`,p:`p`,...n(),...e.components};return(0,u.jsxs)(u.Fragment,{children:[`
-`,`
-`,`
-`,(0,u.jsx)(r,{of:s}),`
-`,(0,u.jsx)(t.h1,{id:`home-page`,children:`Home page`}),`
-`,(0,u.jsx)(t.p,{children:`The home page of an internal website can have varying types of content.`}),`
-`,(0,u.jsx)(t.p,{children:`Our single example in this category currently focuses on the overall layout.`})]})}function l(e={}){let{wrapper:t}={...n(),...e.components};return t?(0,u.jsx)(t,{...e,children:(0,u.jsx)(c,{...e})}):c(e)}var u;e((()=>{u=t(),a(),i(),o()}))();export{l as default};
--- demo-deprecation-message-generator/assets/Row.test.stories-Bk7HpKPX.js
+++
@@ -1,9 +0,0 @@
-import{i as e}from"./preload-helper-xPQekRTU.js";import{t}from"./jsx-runtime-BqsN2jGA.js";import{n,t as r}from"./renderComponentVariants-lY2mFGRm.js";import{t as i,xn as a}from"./src-C2Oiyh5Y.js";import o,{n as s}from"./Row.stories-C6Y_cJzi.js";var c,l,u,d;e((()=>{i(),r(),s(),c=t(),l={...o,title:`Components/Layout/Row`},u={args:{children:[(0,c.jsx)(`div`,{className:`_ams-item`},1),(0,c.jsx)(`div`,{className:`_ams-item`},2),(0,c.jsx)(`div`,{className:`_ams-item`},3)]},render:(e,t)=>n(a,{args:e},t),tags:[`!dev`,`!autodocs`]},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: [<div className="_ams-item" key={1} />, <div className="_ams-item" key={2} />, <div className="_ams-item" key={3} />]
-  },
-  render: (args, context) => renderComponentVariants(Row, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...u.parameters?.docs?.source}}},d=[`Test`]}))();export{u as Test,d as __namedExportsOrder,l as default};
