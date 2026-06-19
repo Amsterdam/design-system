c5cdfeb5b0110135bc5dcb43c63a32473d2f7f02
RubenSibon <mail+github@rubensibon.nl>
2026-06-19 14:32:40 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@c0491f6398dbac509120c3aaeb2f8dd9a6ef8aa5 🚀

---
+++ demo-DES-1891-nextjs-next-link-new-api/assets/Avatar.test.stories-CYdytOrR.js
@@ -0,0 +1,6 @@
+import{i as e}from"./preload-helper-xPQekRTU.js";import{n as t,t as n}from"./renderComponentVariants-Nfj3QkSD.js";import{gn as r,t as i}from"./src-l0Q8KYe8.js";import a,{n as o}from"./Avatar.stories-DL7ndkYq.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Feedback/Avatar`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Avatar, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
---
+++ demo-DES-1891-nextjs-next-link-new-api/assets/DateInput.test.stories-Cat8L4Oc.js
@@ -0,0 +1,6 @@
+import{i as e}from"./preload-helper-xPQekRTU.js";import{n as t,t as n}from"./renderComponentVariants-Nfj3QkSD.js";import{Rt as r,t as i}from"./src-l0Q8KYe8.js";import a,{n as o}from"./DateInput.stories-Cvgp9M55.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Date Input`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(DateInput, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
