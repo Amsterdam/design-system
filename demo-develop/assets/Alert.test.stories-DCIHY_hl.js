8dc919d0080018826c200055bfcd012d0a5305be
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-06-19 22:41:12 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@f1ad13a5ec9f8dca214090eff8064a0b2e5b24d8 🚀

--- demo-develop/assets/Alert.test.stories-DCIHY_hl.js
+++
@@ -1,6 +0,0 @@
-import{i as e}from"./preload-helper-xPQekRTU.js";import{n as t,t as n}from"./renderComponentVariants-CDMGK4PG.js";import{t as r,yn as i}from"./src-1dKWhbaD.js";import a,{n as o}from"./Alert.stories-TmjUiU_f.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/Feedback/Alert`},c={render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Alert, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
