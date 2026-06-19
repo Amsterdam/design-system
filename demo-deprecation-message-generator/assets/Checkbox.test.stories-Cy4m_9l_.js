d839c4f20779548e949ed5322a08dfe429853bb5
Evi-2003 <69012052+Evi-2003@users.noreply.github.com>
2026-06-19 12:19:41 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@306775a5b9b579021c03fce1c9977a581124c95e 🚀

--- demo-deprecation-message-generator/assets/Checkbox.test.stories-Cy4m_9l_.js
+++
@@ -1,6 +0,0 @@
-import{i as e}from"./preload-helper-xPQekRTU.js";import{n as t,t as n}from"./renderComponentVariants-oPDbnHMc.js";import{Ut as r,t as i}from"./src-CNMu9p9T.js";import a,{n as o}from"./Checkbox.stories-C-Sz-SR0.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Checkbox`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Checkbox, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
--- demo-deprecation-message-generator/assets/Select.test.stories-DFPoToxa.js
+++
@@ -1,7 +0,0 @@
-import{i as e}from"./preload-helper-xPQekRTU.js";import{n as t,t as n}from"./renderComponentVariants-oPDbnHMc.js";import{D as r,t as i}from"./src-CNMu9p9T.js";import a,{n as o}from"./Select.stories-BkpEd8ke.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Select`},c={render:(e,n)=>t(r,{args:e,variants:[`hovered`,`disabled`]},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Select, {
-    args,
-    variants: ['hovered', 'disabled']
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
