dd6abd35935b2448a715fb692c3b4130c876417a
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-06-19 21:33:16 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@02e4f1018be8e5c0916620f00d8207cbde947ec2 🚀

---
+++ demo-develop/assets/ErrorMessage.test.stories-DjALF2PP.js
@@ -0,0 +1,6 @@
+import{i as e}from"./preload-helper-xPQekRTU.js";import{n as t,t as n}from"./renderComponentVariants-CkMCQnXH.js";import{At as r,t as i}from"./src-BAKmQrT0.js";import a,{n as o}from"./ErrorMessage.stories-6aIterKT.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Error Message`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(ErrorMessage, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
---
+++ demo-develop/assets/Overlap.stories-CiGO8M0r.js
@@ -0,0 +1,22 @@
+import{a as e,i as t}from"./preload-helper-xPQekRTU.js";import{t as n}from"./jsx-runtime-BqsN2jGA.js";import{B as r,C as i,X as a,b as o}from"./iframe-Y8U8EQ8i.js";import{J as s,t as c}from"./src-BAKmQrT0.js";var l=e({Default:()=>f,__namedExportsOrder:()=>p,default:()=>d}),u,d,f,p,m=t((()=>{a(),c(),u=n(),d={title:`Components/Layout/Overlap`,component:s},f={args:{children:[(0,u.jsx)(i,{alt:``,aspectRatio:`16:5`,sizes:`(max-width: 37.5rem) 640px, (max-width: 72.5rem) 1280px, 1440px`,src:`https://picsum.photos/1440/450`,srcSet:`https://picsum.photos/640/200 640w, https://picsum.photos/1280/400 1280w, https://picsum.photos/1440/450 1440w`},1),(0,u.jsx)(o,{style:{alignSelf:`center`},children:(0,u.jsx)(o.Cell,{span:{narrow:4,medium:6,wide:8},start:{narrow:1,medium:2,wide:3},children:(0,u.jsxs)(r,{onSubmit:e=>e.preventDefault(),children:[(0,u.jsx)(r.Input,{label:`Zoeken`,placeholder:`Wat kunnen we voor u vinden?`}),(0,u.jsx)(r.Button,{})]})})},2)]}},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<Image alt="" aspectRatio="16:5" key={1} sizes="(max-width: 37.5rem) 640px, (max-width: 72.5rem) 1280px, 1440px" src="https://picsum.photos/1440/450" srcSet="https://picsum.photos/640/200 640w, https://picsum.photos/1280/400 1280w, https://picsum.photos/1440/450 1440w" />, <Grid key={2} style={{
+      alignSelf: 'center'
+    }}>
+        <Grid.Cell span={{
+        narrow: 4,
+        medium: 6,
+        wide: 8
+      }} start={{
+        narrow: 1,
+        medium: 2,
+        wide: 3
+      }}>
+          <SearchField onSubmit={e => e.preventDefault()}>
+            <SearchField.Input label="Zoeken" placeholder="Wat kunnen we voor u vinden?" />
+            <SearchField.Button />
+          </SearchField>
+        </Grid.Cell>
+      </Grid>]
+  }
+}`,...f.parameters?.docs?.source}}},p=[`Default`]}));export{d as i,l as n,m as r,f as t};
