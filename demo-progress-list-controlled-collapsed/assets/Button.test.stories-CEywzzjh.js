d5c3ceeabc4d9f7852100b229bdccc93f55638d5
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-06-19 22:45:03 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@864e97547bbefc797b587be8b66dd77d354a4045 🚀

---
+++ demo-progress-list-controlled-collapsed/assets/Button.test.stories-CEywzzjh.js
@@ -0,0 +1,7 @@
+import{i as e}from"./preload-helper-xPQekRTU.js";import{n as t,t as n}from"./renderComponentVariants-B84mfRPl.js";import{an as r,t as i}from"./src-BH8hgIqP.js";import a,{n as o}from"./Button.stories-CwU0cSUN.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Buttons/Button`},c={render:(e,n)=>t(r,{args:e,variants:[`disabled`,`hovered`]},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Button, {
+    args,
+    variants: ['disabled', 'hovered']
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
