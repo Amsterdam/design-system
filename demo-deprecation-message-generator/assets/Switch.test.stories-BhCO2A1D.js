f0a7c061f0e8ce93caa179b8a6f940be8a652525
GitHub <github-action@users.noreply.github.com>
2026-06-19 12:27:38 +0000

    Remove feature branch folder

--- demo-deprecation-message-generator/assets/Switch.test.stories-BhCO2A1D.js
+++
@@ -1,6 +0,0 @@
-import{i as e}from"./preload-helper-xPQekRTU.js";import{n as t,t as n}from"./renderComponentVariants-B9fHcf2U.js";import{t as r,v as i}from"./src-CSn_xItu.js";import a,{n as o}from"./Switch.stories-Oh-0fFNs.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/Forms/Switch`},c={render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Switch, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
