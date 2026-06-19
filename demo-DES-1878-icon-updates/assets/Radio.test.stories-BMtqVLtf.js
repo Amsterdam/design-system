6f3a3c71e0a04edf7ceb39d7b1ef9ca365d4942a
dlnr <niels@dlnr.dev>
2026-06-19 10:55:42 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@7075d21b6ef76ddda4c5305c248e9a506ceffe87 🚀

---
+++ demo-DES-1878-icon-updates/assets/Radio.test.stories-BMtqVLtf.js
@@ -0,0 +1,6 @@
+import{i as e}from"./preload-helper-xPQekRTU.js";import{n as t,t as n}from"./renderComponentVariants-Bf1bsO6p.js";import{j as r,t as i}from"./src-Dyp61YHD.js";import a,{n as o}from"./Radio.stories-BnptwRGA.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Radio`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Radio, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
