8b538a42e241031d247d66617f336c43708a8bba
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-06-19 14:24:36 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@6228a5cf5df0f6fb97bbadcc84d49b526a35bd73 🚀

---
+++ demo-DES-1819-collapsible-toc/assets/Code.test.stories-G9pmnNsm.js
@@ -0,0 +1,6 @@
+import{i as e}from"./preload-helper-xPQekRTU.js";import{n as t,t as n}from"./renderComponentVariants-Dg_GNTv7.js";import{n as r,t as i}from"./Code-CKgCXhNT.js";var a,o,s;e((()=>{n(),r(),a={title:`Components/Docs/Code`,args:{children:[`var(--ams-color-text)`]}},o={render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`]},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Code, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...o.parameters?.docs?.source}}},s=[`Test`]}))();export{o as Test,s as __namedExportsOrder,a as default};
