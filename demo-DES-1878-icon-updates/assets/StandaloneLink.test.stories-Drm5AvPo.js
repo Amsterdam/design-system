97ccd15245f635cfb62e30826727b4df79173f0a
dlnr <niels@dlnr.dev>
2026-06-19 10:59:00 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@3bc726beeba7ee640943766ab987c0dd3ee246e7 🚀

---
+++ demo-DES-1878-icon-updates/assets/StandaloneLink.test.stories-Drm5AvPo.js
@@ -0,0 +1,6 @@
+import{i as e}from"./preload-helper-xPQekRTU.js";import{n as t,t as n}from"./renderComponentVariants-p_lklo9x.js";import{b as r,t as i}from"./src-DWuVDoAE.js";import a,{n as o}from"./StandaloneLink.stories-DXaF1xtm.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/StandaloneLink`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(StandaloneLink, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
