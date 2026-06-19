b8cca6b2d18e5493ee68b94e2cc684cb1d670abf
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-06-19 12:09:43 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@fbceb1b957ec2e7a058a17ef82e145a84ba1d0b4 🚀

---
+++ demo-develop/assets/PageFooter.test.stories-II1hS9fn.js
@@ -0,0 +1,16 @@
+import{i as e}from"./preload-helper-xPQekRTU.js";import{t}from"./jsx-runtime-BqsN2jGA.js";import{I as n,X as r}from"./iframe-D_ghdw3F.js";import{n as i,t as a}from"./renderComponentVariants-B55Uq0NQ.js";import{W as o,t as s}from"./src-Dgbk6Kty.js";import{a as c,o as l}from"./PageFooter.stories-CFARwc2i.js";var u,d,f,p;e((()=>{r(),s(),a(),c(),u=t(),d={...l,title:`Components/Containers/Page Footer`},f={args:{children:[(0,u.jsx)(o.Spotlight,{children:(0,u.jsx)(n,{color:`inverse`,children:`Adressen en openingstijden`})},1),(0,u.jsxs)(o.Menu,{children:[(0,u.jsx)(o.MenuLink,{href:`#`,children:`Over deze site`}),(0,u.jsx)(o.MenuLink,{href:`#`,children:`Privacy`}),(0,u.jsx)(o.MenuLink,{href:`#`,children:`Cookies op deze site`}),(0,u.jsx)(o.MenuLink,{href:`#`,children:`Webarchief`})]},2)]},render:(e,t)=>i(o,{args:e},t),tags:[`!dev`,`!autodocs`]},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<PageFooter.Spotlight key={1}>
+        <Paragraph color="inverse">Adressen en openingstijden</Paragraph>
+      </PageFooter.Spotlight>, <PageFooter.Menu key={2}>
+        <PageFooter.MenuLink href="#">Over deze site</PageFooter.MenuLink>
+        <PageFooter.MenuLink href="#">Privacy</PageFooter.MenuLink>
+        <PageFooter.MenuLink href="#">Cookies op deze site</PageFooter.MenuLink>
+        <PageFooter.MenuLink href="#">Webarchief</PageFooter.MenuLink>
+      </PageFooter.Menu>]
+  },
+  render: (args, context) => renderComponentVariants(PageFooter, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...f.parameters?.docs?.source}}},p=[`Test`]}))();export{f as Test,p as __namedExportsOrder,d as default};
---
+++ demo-develop/assets/Radio.test.stories-Djb9tp5v.js
@@ -0,0 +1,6 @@
+import{i as e}from"./preload-helper-xPQekRTU.js";import{n as t,t as n}from"./renderComponentVariants-B55Uq0NQ.js";import{j as r,t as i}from"./src-Dgbk6Kty.js";import a,{n as o}from"./Radio.stories-CSBOkB45.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Radio`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Radio, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
