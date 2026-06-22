8770ba637b15eaa9495ca8cdd6b5ab8d79ac4a70
GitHub <github-action@users.noreply.github.com>
2026-06-22 19:44:09 +0000

    Remove feature branch folder

--- demo-introduction-home-page/assets/Link.test.stories-DrkVlaao.js
+++
@@ -1,6 +0,0 @@
-import{i as e}from"./preload-helper-xPQekRTU.js";import{n as t,t as n}from"./renderComponentVariants-Bh4lnXwy.js";import{at as r,t as i}from"./src-Bexm37t_.js";import a,{n as o}from"./Link.stories-C4dlk-Tk.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Navigation/Link`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Link, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
