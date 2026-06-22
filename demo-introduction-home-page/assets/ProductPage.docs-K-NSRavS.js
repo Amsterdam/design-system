f41c6c65436ab59072837b5469982657e0419134
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-06-22 19:35:33 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@4ce7b49b2c204f24127a6a1cc917e9cc6a19d0d4 🚀

---
+++ demo-introduction-home-page/assets/ProductPage.docs-K-NSRavS.js
@@ -0,0 +1,17 @@
+import{i as e}from"./preload-helper-xPQekRTU.js";import{t}from"./jsx-runtime-BqsN2jGA.js";import{l as n,m as r,v as i}from"./iframe-ecQHj14d.js";import{t as a}from"./mdx-react-shim-CyZnHeLr.js";import{n as o,t as s}from"./ProductPage.stories-DEIhWvMP.js";function c(e){let t={h1:`h1`,h2:`h2`,li:`li`,p:`p`,ul:`ul`,...i(),...e.components};return(0,u.jsxs)(u.Fragment,{children:[`
+`,`
+`,`
+`,(0,u.jsx)(n,{of:s}),`
+`,(0,u.jsx)(t.h1,{id:`product-page`,children:`Product page`}),`
+`,(0,u.jsx)(t.p,{children:`Lists all information about a product or service of the City.
+Often includes a Call to Action Link that directs the user to a form.`}),`
+`,(0,u.jsx)(t.h2,{id:`headings`,children:`Headings`}),`
+`,(0,u.jsxs)(t.ul,{children:[`
+`,(0,u.jsx)(t.li,{children:`Use a level 1 Heading for the title of the page.`}),`
+`,(0,u.jsx)(t.li,{children:`Use level 2 Headings for the section titles.`}),`
+`]}),`
+`,(0,u.jsx)(t.h2,{id:`spacing`,children:`Spacing`}),`
+`,(0,u.jsxs)(t.ul,{children:[`
+`,(0,u.jsx)(t.li,{children:`Add a small margin bottom to a Heading 2 with following body text.`}),`
+`,(0,u.jsx)(t.li,{children:`Use an extra large margin between sections and before the Call to Action Link.`}),`
+`]})]})}function l(e={}){let{wrapper:t}={...i(),...e.components};return t?(0,u.jsx)(t,{...e,children:(0,u.jsx)(c,{...e})}):c(e)}var u;e((()=>{u=t(),a(),r(),o()}))();export{l as default};
