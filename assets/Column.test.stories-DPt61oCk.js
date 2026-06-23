4a4a1731f451e381f09d7032d4c5365c84ab9a19
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-06-23 18:12:39 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@e8343d4d72def6501e38d1819848913dd607e397 🚀

--- assets/Column.test.stories-DPt61oCk.js
+++
@@ -1,7 +0,0 @@
-import{i as e}from"./preload-helper-xPQekRTU.js";import{n as t,t as n}from"./renderComponentVariants-jQPe_fz8.js";import{Bt as r,t as i}from"./src-CTWoO1-N.js";import a,{n as o}from"./Column.stories-Vl0deBKJ.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Layout/Column`},c={render:(e,n)=>t(r,{args:e,layout:`grid`},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Column, {
-    args,
-    layout: 'grid'
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
--- assets/Switch.test.stories-D1WM45cn.js
+++
@@ -1,6 +0,0 @@
-import{i as e}from"./preload-helper-xPQekRTU.js";import{n as t,t as n}from"./renderComponentVariants-jQPe_fz8.js";import{t as r,v as i}from"./src-CTWoO1-N.js";import a,{n as o}from"./Switch.stories-4ai_yU2O.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/Forms/Switch`},c={render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Switch, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
