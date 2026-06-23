8eeed58e4be87d7ce4d5135acaae376e2757c40f
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-06-23 11:02:26 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@424556d6faae185315322f68fd633212d1660d51 🚀

---
+++ demo-restore-deprecated-vi-tokens/assets/Field.test.stories-BUm57mse.js
@@ -0,0 +1,13 @@
+import{i as e}from"./preload-helper-xPQekRTU.js";import{t}from"./jsx-runtime-BqsN2jGA.js";import{Ot as n,it as r}from"./iframe-Cigi9oiA.js";import{n as i,t as a}from"./renderComponentVariants-C_WXSeXw.js";import{Ot as o,t as s}from"./src-Dr9YT4tc.js";import c,{n as l}from"./Field.stories-3NGd6cI8.js";var u,d,f,p;e((()=>{n(),s(),a(),l(),u=t(),d={...c,title:`Components/Forms/Field`},f={args:{children:[(0,u.jsx)(r,{htmlFor:`input-a1`,children:`Wat is uw achternaam?`},1),(0,u.jsx)(`input`,{id:`input-a1`,value:`Dijkstra`},2)]},render:(e,t)=>i(o,{args:e},t),tags:[`!dev`,`!autodocs`]},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [
+    // We use Label because \`<label>\` is inline and wouldn’t get the margin we want to test.
+    <Label htmlFor="input-a1" key={1}>
+        Wat is uw achternaam?
+      </Label>, <input id="input-a1" key={2} value="Dijkstra" />]
+  },
+  render: (args, context) => renderComponentVariants(Field, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...f.parameters?.docs?.source}}},p=[`Test`]}))();export{f as Test,p as __namedExportsOrder,d as default};
