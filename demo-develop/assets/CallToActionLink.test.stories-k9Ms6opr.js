01470447df2dc921716bbd8afb0d8c28b90b3e79
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-06-23 15:49:11 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@f14e9e01df3ca1f1bbbd31143114b28369956225 🚀

--- demo-develop/assets/CallToActionLink.test.stories-k9Ms6opr.js
+++
@@ -1,6 +0,0 @@
-import{i as e}from"./preload-helper-xPQekRTU.js";import{n as t,t as n}from"./renderComponentVariants-CbrIoNf8.js";import{t as r,tn as i}from"./src-nbM6ZIx3.js";import a,{n as o}from"./CallToActionLink.stories-6d47Z_jv.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/Navigation/Call to Action Link`},c={render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(CallToActionLink, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
--- demo-develop/assets/Pagination.test.stories-i2i6UITA.js
+++
@@ -1,6 +0,0 @@
-import{i as e}from"./preload-helper-xPQekRTU.js";import{n as t,t as n}from"./renderComponentVariants-CbrIoNf8.js";import{R as r,t as i}from"./src-nbM6ZIx3.js";import a,{n as o}from"./Pagination.stories-CaY6t6fi.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Navigation/Pagination`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Pagination, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
