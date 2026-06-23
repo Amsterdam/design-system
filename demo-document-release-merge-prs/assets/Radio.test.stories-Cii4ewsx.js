21d6608c6c325085d76132947f6144ebe1f16fa5
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-06-23 20:14:40 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@a3bd4304dbf45e6ae29df5b9cf7115bd67e23bb5 🚀

--- demo-document-release-merge-prs/assets/Radio.test.stories-Cii4ewsx.js
+++
@@ -1,6 +0,0 @@
-import{i as e}from"./preload-helper-xPQekRTU.js";import{n as t,t as n}from"./renderComponentVariants-fUE4HDII.js";import{j as r,t as i}from"./src-5ecacehd.js";import a,{n as o}from"./Radio.stories-C75EqfYt.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Radio`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Radio, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
