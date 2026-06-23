ec8404db612bdece05930443c05cbd771d6a74ee
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-06-23 15:46:21 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@db10c55e09c04dca4fde1c64f52e35e98130969a 🚀

--- demo-rename-link-blocks/assets/Heading.test.stories-DM9uVE9A.js
+++
@@ -1,9 +0,0 @@
-import{i as e}from"./preload-helper-xPQekRTU.js";import{n as t,t as n}from"./renderComponentVariants-CRXZzm31.js";import{Nn as r,t as i}from"./src-CVPsePaE.js";import a,{n as o}from"./Heading.stories-D43ro8Xw.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Text/Heading`},c={args:{children:`Amsterdam Light Festival maakt de stad ook deze winter een beetje mooier`},render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: 'Amsterdam Light Festival maakt de stad ook deze winter een beetje mooier'
-  },
-  render: (args, context) => renderComponentVariants(Heading, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
--- demo-rename-link-blocks/assets/SpaceSample.test.stories-acS4y2t_.js
+++
@@ -1,7 +0,0 @@
-import{i as e}from"./preload-helper-xPQekRTU.js";import{t}from"./jsx-runtime-BqsN2jGA.js";import{n,t as r}from"./SpaceSample-Bu2d0cpd.js";var i,a,o,s;e((()=>{n(),i=t(),a={title:`Components/Docs/Space Sample`},o={render:()=>(0,i.jsxs)(`div`,{className:`_ams-tests-grid`,children:[(0,i.jsx)(r,{value:`16px`}),(0,i.jsx)(r,{})]}),tags:[`!dev`,`!autodocs`]},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
-  render: () => <div className="_ams-tests-grid">
-      <SpaceSample value="16px" />
-      <SpaceSample />
-    </div>,
-  tags: ['!dev', '!autodocs']
-}`,...o.parameters?.docs?.source}}},s=[`Test`]}))();export{o as Test,s as __namedExportsOrder,a as default};
