9d6c6103f62e247435f5b5eadb91bf60a4f7c6b9
Evi-2003 <69012052+Evi-2003@users.noreply.github.com>
2026-06-24 12:28:52 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@66b50f6ca5b4bff092b5fe56d0daa4ead35895df 🚀

---
+++ demo-DES-1340-improve-example-wrappers/assets/IconButton.test.stories-BjkIUK1I.js
@@ -0,0 +1,7 @@
+import{i as e}from"./preload-helper-xPQekRTU.js";import{n as t,t as n}from"./renderComponentVariants-CGI1uiNW.js";import{t as r,wn as i}from"./src-A7Q6d95P.js";import a,{n as o}from"./IconButton.stories-oJ4ncyur.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/Buttons/Icon Button`},c={render:(e,n)=>t(i,{args:e,variants:[`hovered`,`disabled`]},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(IconButton, {
+    args,
+    variants: ['hovered', 'disabled']
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
---
+++ demo-DES-1340-improve-example-wrappers/assets/VisuallyHidden.docs-DX9yHPu5.js
@@ -0,0 +1,21 @@
+import{i as e}from"./preload-helper-xPQekRTU.js";import{t}from"./jsx-runtime-BqsN2jGA.js";import{b as n,d as r,f as i,g as a,o,u as s}from"./iframe-CKHs0OTK.js";import{t as c}from"./mdx-react-shim-B5p6cR7-.js";import{n as l,t as u}from"./VisuallyHidden.stories-DsdKl1I3.js";var d,f=e((()=>{d=`<!-- @license CC0-1.0 -->
+
+# Visually Hidden
+
+Hides content from sighted users but keeps it accessible to non-visual user agents, such as screen readers.
+
+## Class name
+
+\`ams-visually-hidden\`
+
+## Guidelines
+
+- In most cases, visually available content should be accessible to non-visual user agents and vice versa.
+  Elaborate instructions or guidance read only by non-visual user agents can do more harm than good.
+`}));function p(e){return(0,h.jsxs)(h.Fragment,{children:[`
+`,`
+`,`
+`,(0,h.jsx)(r,{of:u}),`
+`,(0,h.jsx)(s,{children:d}),`
+`,(0,h.jsx)(i,{}),`
+`,(0,h.jsx)(o,{})]})}function m(e={}){let{wrapper:t}={...n(),...e.components};return t?(0,h.jsx)(t,{...e,children:(0,h.jsx)(p,{...e})}):p(e)}var h;e((()=>{h=t(),c(),a(),f(),l()}))();export{m as default};
