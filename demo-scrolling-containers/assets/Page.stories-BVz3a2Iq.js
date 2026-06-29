96c80082337f7c050a31a2b3338fa505f4657346
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-06-29 14:33:27 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@a51ec26532e3efcafeea34467eda9601a9799726 🚀

---
+++ demo-scrolling-containers/assets/Page.stories-BVz3a2Iq.js
@@ -0,0 +1,54 @@
+import{a as e,i as t}from"./preload-helper-xPQekRTU.js";import{t as n}from"./jsx-runtime-BqsN2jGA.js";import{$ as r,Ot as i,Q as a,bt as o,dt as s,fn as c,ft as l,mt as u,st as d,yn as f}from"./iframe-DFEYK4E4.js";import{K as p,t as m}from"./src-woiLlg7q.js";var h=e({Default:()=>y,WithMenu:()=>b,__namedExportsOrder:()=>x,default:()=>_}),g,_,v,y,b,x,S=t((()=>{i(),f(),m(),g=n(),_={title:`Components/Containers/Page`,component:p,argTypes:{withMenu:{description:`This prop updates automatically to prevent an invalid appearance: a Menu can only be used in Compact Mode.`,table:{readonly:!0}}},decorators:[e=>(0,g.jsx)(`div`,{lang:`nl`,children:(0,g.jsx)(e,{})})],parameters:{layout:`fullscreen`}},v=()=>(0,g.jsx)(a,{paddingVertical:`x-large`,children:(0,g.jsx)(a.Cell,{span:`all`,children:(0,g.jsx)(r,{level:1,children:`Page Body`})})}),y={args:{children:[(0,g.jsx)(l,{brandName:`Page Header`,noMenuButtonOnWideWindow:!0},`header`),(0,g.jsx)(`main`,{id:`inhoud`,children:(0,g.jsx)(v,{})},`main`),(0,g.jsxs)(s,{children:[(0,g.jsx)(s.Spotlight,{children:(0,g.jsx)(a,{paddingVertical:`x-large`,children:(0,g.jsx)(a.Cell,{appearance:`transparent`,span:`all`,children:(0,g.jsx)(u,{color:`inverse`,children:`Page Footer`})})})}),(0,g.jsx)(s.Menu,{children:(0,g.jsx)(s.MenuLink,{href:`/`,children:`Page Footer Menu`})})]},`footer`)]}},b={args:{children:(0,g.jsx)(v,{}),withMenu:!0},globals:{theme:`Compact`},render:({children:e,...t})=>(0,g.jsxs)(p,{...t,children:[(0,g.jsx)(o,{className:`ams-page__area--skip-link`,href:`#inhoud`,children:`Direct naar inhoud`}),(0,g.jsx)(l,{brandName:`Page Header`,className:`ams-page__area--header`,noMenuButtonOnWideWindow:!0,children:(0,g.jsx)(d,{children:(0,g.jsx)(d.Link,{href:`#`,icon:(0,g.jsx)(c,{}),children:`Menu item`})})}),(0,g.jsx)(d,{className:`ams-page__area--menu`,inWideWindow:!0,children:(0,g.jsx)(d.Link,{href:`#`,icon:(0,g.jsx)(c,{}),children:`Menu item`})}),(0,g.jsx)(`main`,{className:`ams-page__area--body`,id:`inhoud`,children:e}),(0,g.jsx)(s,{className:`ams-page__area--footer`,children:(0,g.jsx)(s.Menu,{children:(0,g.jsx)(s.MenuLink,{href:`/`,children:`Page Footer Menu`})})})]})},y.parameters={...y.parameters,docs:{...y.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<PageHeader brandName="Page Header" key="header" noMenuButtonOnWideWindow />, <main id="inhoud" key="main">
+        <PageBody />
+      </main>, <PageFooter key="footer">
+        <PageFooter.Spotlight>
+          <Grid paddingVertical="x-large">
+            <Grid.Cell appearance="transparent" span="all">
+              <Paragraph color="inverse">Page Footer</Paragraph>
+            </Grid.Cell>
+          </Grid>
+        </PageFooter.Spotlight>
+        <PageFooter.Menu>
+          <PageFooter.MenuLink href="/">Page Footer Menu</PageFooter.MenuLink>
+        </PageFooter.Menu>
+      </PageFooter>]
+  }
+}`,...y.parameters?.docs?.source}}},b.parameters={...b.parameters,docs:{...b.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: <PageBody />,
+    withMenu: true
+  },
+  globals: {
+    theme: 'Compact'
+  },
+  render: ({
+    children,
+    ...args
+  }) => <Page {...args}>
+      <SkipLink className="ams-page__area--skip-link" href="#inhoud">
+        Direct naar inhoud
+      </SkipLink>
+      <PageHeader brandName="Page Header" className="ams-page__area--header" noMenuButtonOnWideWindow>
+        <Menu>
+          <Menu.Link href="#" icon={<SettingsFillIcon />}>
+            Menu item
+          </Menu.Link>
+        </Menu>
+      </PageHeader>
+      <Menu className="ams-page__area--menu" inWideWindow>
+        <Menu.Link href="#" icon={<SettingsFillIcon />}>
+          Menu item
+        </Menu.Link>
+      </Menu>
+      <main className="ams-page__area--body" id="inhoud">
+        {children}
+      </main>
+      <PageFooter className="ams-page__area--footer">
+        <PageFooter.Menu>
+          <PageFooter.MenuLink href="/">Page Footer Menu</PageFooter.MenuLink>
+        </PageFooter.Menu>
+      </PageFooter>
+    </Page>
+}`,...b.parameters?.docs?.source}}},x=[`Default`,`WithMenu`]}));S();export{y as Default,b as WithMenu,x as __namedExportsOrder,_ as default,S as n,h as t};
