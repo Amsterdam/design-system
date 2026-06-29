edf34b484e4aa8dbf222eefdbf3810d81ffcb34f
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-06-29 14:48:49 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@c51877904b7767085b55492a9e657c8d8dd17f3e 🚀

---
+++ demo-typography-black-font-weight/assets/ProjectPage.docs-083gcSY4.js
@@ -0,0 +1,18 @@
+import{i as e}from"./preload-helper-xPQekRTU.js";import{t}from"./jsx-runtime-BqsN2jGA.js";import{l as n,m as r,v as i}from"./iframe-CnGwPYUb.js";import{t as a}from"./mdx-react-shim-CYeUVKFT.js";import{n as o,t as s}from"./ProjectPage.stories-B4xyn22m.js";function c(e){let t={h1:`h1`,p:`p`,...i(),...e.components};return(0,u.jsxs)(u.Fragment,{children:[`
+`,`
+`,`
+`,(0,u.jsx)(n,{of:s}),`
+`,(0,u.jsx)(t.h1,{id:`project-page`,children:`Project page`}),`
+`,(0,u.jsx)(t.p,{children:`This page shares all important public details about a construction or traffic project.
+Its main goal is to clearly inform residents and stakeholders.`}),`
+`,(0,u.jsx)(t.p,{children:`The page has a set structure.
+It includes sections that describe what the project is, its location, the reasons for it, and the timeline for planning or execution.
+This standard format helps users compare projects and find information more easily.`}),`
+`,(0,u.jsx)(t.p,{children:`The ‘when’ section usually features a timeline of important milestones, shown in a Progress List.
+This gives a brief overview of the project's current status and future phases.`}),`
+`,(0,u.jsx)(t.p,{children:`Visual elements enhance the text.
+Large images provide context or show the impact.
+A map indicates the project location and the areas affected.
+A video may be included to clarify complex issues or planned changes.`}),`
+`,(0,u.jsx)(t.p,{children:`Related information is organized in link blocks.
+These links lead to news updates, related projects, and supporting documents, allowing users to delve deeper without overwhelming the main content.`})]})}function l(e={}){let{wrapper:t}={...i(),...e.components};return t?(0,u.jsx)(t,{...e,children:(0,u.jsx)(c,{...e})}):c(e)}var u;e((()=>{u=t(),a(),r(),o()}))();export{l as default};
---
+++ demo-typography-black-font-weight/assets/StandaloneLink.docs-DNlFFHhn.js
@@ -0,0 +1,46 @@
+import{i as e}from"./preload-helper-xPQekRTU.js";import{t}from"./jsx-runtime-BqsN2jGA.js";import{a as n,f as r,i,l as a,m as o,n as s,u as c,v as l}from"./iframe-CnGwPYUb.js";import{n as u,t as d}from"./DesignTokensTable-CmMPx64V.js";import{t as f}from"./mdx-react-shim-CYeUVKFT.js";import{ContrastColour as p,InverseColour as m,WithCustomIcon as h,WithHeadingAndParagraph as g,n as _,t as v}from"./StandaloneLink.stories-DLjSJ7TY.js";var y,b,x=e((()=>{y={"standalone-link":{color:{$value:`{ams.links.color}`,$extensions:{"nl.amsterdam.type":`color`}},"column-gap":{$value:`{ams.space.s}`,$extensions:{"nl.amsterdam.subtype":`space`,"nl.amsterdam.type":`dimension`}},"font-family":{$value:`{ams.typography.font-family}`,$extensions:{"nl.amsterdam.type":`fontFamily`}},"font-size":{$value:`{ams.typography.body-text.font-size}`,$extensions:{"nl.amsterdam.type":`fontSize`}},"font-weight":{$value:`{ams.typography.body-text.font-weight}`,$extensions:{"nl.amsterdam.type":`fontWeight`}},"line-height":{$value:`{ams.typography.body-text.line-height}`,$extensions:{"nl.amsterdam.subtype":`lineHeight`,"nl.amsterdam.type":`number`}},"outline-offset":{$value:`{ams.focus.outline-offset}`,$extensions:{"nl.amsterdam.subtype":`space`,"nl.amsterdam.type":`dimension`}},"text-decoration-line":{$value:`{ams.links.subtle.text-decoration-line}`,$extensions:{"nl.amsterdam.type":`textDecorationLine`}},"text-decoration-thickness":{$value:`{ams.links.text-decoration-thickness}`,$extensions:{"nl.amsterdam.subtype":`space`,"nl.amsterdam.type":`dimension`}},"text-underline-offset":{$value:`{ams.links.text-underline-offset}`,$extensions:{"nl.amsterdam.subtype":`space`,"nl.amsterdam.type":`dimension`}},hover:{color:{$value:`{ams.links.hover.color}`,$extensions:{"nl.amsterdam.type":`color`}},"text-decoration-line":{$value:`{ams.links.subtle.hover.text-decoration-line}`,$extensions:{"nl.amsterdam.type":`textDecorationLine`}}},contrast:{color:{$value:`{ams.links.contrast.color}`,$extensions:{"nl.amsterdam.type":`color`}},hover:{color:{$value:`{ams.links.contrast.hover.color}`,$extensions:{"nl.amsterdam.type":`color`}}}},inverse:{color:{$value:`{ams.links.inverse.color}`,$extensions:{"nl.amsterdam.type":`color`}},hover:{color:{$value:`{ams.links.inverse.hover.color}`,$extensions:{"nl.amsterdam.type":`color`}}}}}},b={ams:y}}));function S(e){let t={a:`a`,h2:`h2`,h3:`h3`,h4:`h4`,li:`li`,p:`p`,ul:`ul`,...l(),...e.components};return(0,w.jsxs)(w.Fragment,{children:[`
+`,`
+`,`
+`,`
+`,(0,w.jsx)(a,{of:v}),`
+`,(0,w.jsx)(r,{}),`
+`,(0,w.jsx)(n,{of:v}),`
+`,(0,w.jsx)(c,{}),`
+`,(0,w.jsx)(i,{}),`
+`,(0,w.jsx)(t.h2,{id:`usage-guidelines`,children:`Usage guidelines`}),`
+`,(0,w.jsx)(t.h3,{id:`when-to-use`,children:`When to use`}),`
+`,(0,w.jsx)(t.p,{children:`Use a Standalone Link for a single link on its own line of text.`}),`
+`,(0,w.jsx)(t.h3,{id:`when-not-to-use`,children:`When not to use`}),`
+`,(0,w.jsxs)(t.p,{children:[`Within running body text, use a regular `,(0,w.jsx)(t.a,{href:`/docs/components-navigation-link--docs`,children:`Link`}),` instead.`]}),`
+`,(0,w.jsxs)(t.p,{children:[`For additional guidelines, refer to the `,(0,w.jsx)(t.a,{href:`/docs/components-navigation-link--docs`,children:`Link`}),` component.`]}),`
+`,(0,w.jsxs)(t.p,{children:[`Use `,(0,w.jsx)(t.a,{href:`/docs/utilities-css-margin--docs`,children:`Margin`}),` or `,(0,w.jsx)(t.a,{href:`/docs/utilities-css-prose--docs`,children:`Prose`}),` to add vertical whitespace.`]}),`
+`,(0,w.jsx)(t.h2,{id:`examples`,children:`Examples`}),`
+`,(0,w.jsx)(t.h3,{id:`with-heading-and-paragraph`,children:`With heading and paragraph`}),`
+`,(0,w.jsxs)(t.p,{children:[`A Standalone Link often appears in a block with a `,(0,w.jsx)(t.a,{href:`/docs/components-text-heading--docs`,children:`Heading`}),` and an introducing `,(0,w.jsx)(t.a,{href:`/docs/components-text-paragraph--docs`,children:`Paragraph`}),` to lead to a follow-up page.
+Use a size of ‘level-3’ for the Heading, add a small bottom margin, and a medium bottom margin to the Paragraph.`]}),`
+`,(0,w.jsx)(s,{of:g}),`
+`,(0,w.jsx)(t.h3,{id:`custom-icon`,children:`Custom icon`}),`
+`,(0,w.jsx)(t.p,{children:`If it helps understand the link, a different icon than the default chevron can be used.`}),`
+`,(0,w.jsx)(s,{of:h}),`
+`,(0,w.jsx)(t.h3,{id:`on-a-coloured-background`,children:`On a coloured background`}),`
+`,(0,w.jsxs)(t.p,{children:[`A Standalone Link on a coloured background must set `,(0,w.jsx)(t.a,{href:`/docs/brand-design-tokens-colour--docs#pairing-foreground-with-background-colours`,children:`the correct text colour`}),` to provide enough contrast.
+We have lighter and darker background colours, and links behave differently on each.`]}),`
+`,(0,w.jsx)(t.h4,{id:`contrast-colour`,children:`Contrast colour`}),`
+`,(0,w.jsx)(t.p,{children:`Use the contrast colour on a lime, orange or yellow background.
+It will make the Standalone Link black.`}),`
+`,(0,w.jsx)(s,{of:p}),`
+`,(0,w.jsx)(t.h4,{id:`inverse-colour`,children:`Inverse colour`}),`
+`,(0,w.jsx)(t.p,{children:`Use the inverse colour on an azure, blue, green, magenta or purple background.
+It will make the Standalone Link white.`}),`
+`,(0,w.jsx)(s,{of:m}),`
+`,(0,w.jsx)(t.h2,{id:`design`,children:`Design`}),`
+`,(0,w.jsxs)(t.p,{children:[`The Standalone Link has a chevron icon at the start to signify that it is a link.
+This also makes it visually identical to a single link in a `,(0,w.jsx)(t.a,{href:`/docs/components-navigation-link-list--docs`,children:`Link List`}),`.
+As a result, pages can have sections with either one or several links used interchangeably; they look alike, and their semantics are correct.`]}),`
+`,(0,w.jsx)(t.h2,{id:`see-also`,children:`See also`}),`
+`,(0,w.jsxs)(t.ul,{children:[`
+`,(0,w.jsxs)(t.li,{children:[(0,w.jsx)(t.a,{href:`/docs/components-navigation-link--docs`,children:`Link`}),` – for inline links in running text.`]}),`
+`,(0,w.jsxs)(t.li,{children:[(0,w.jsx)(t.a,{href:`/docs/components-navigation-link-list--docs`,children:`Link List`}),` – for a collection of related links.`]}),`
+`]}),`
+`,(0,w.jsx)(t.h2,{id:`design-tokens`,children:`Design tokens`}),`
+`,(0,w.jsx)(d,{tokens:b})]})}function C(e={}){let{wrapper:t}={...l(),...e.components};return t?(0,w.jsx)(t,{...e,children:(0,w.jsx)(S,{...e})}):S(e)}var w;e((()=>{w=t(),f(),o(),u(),x(),_()}))();export{C as default};
