3ccf1f458d7dfac0675a620c6e2786ed640b3a9b
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-06-29 20:05:28 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@b08629b5812e1aaf1c1083864ea2070755c281db 🚀

--- demo-develop/assets/Grid.test.stories-2n91Vie-.js
+++
@@ -1,9 +0,0 @@
-import{i as e}from"./preload-helper-xPQekRTU.js";import{t}from"./jsx-runtime-BqsN2jGA.js";import{n,t as r}from"./renderComponentVariants-7tg2H4YI.js";import{t as i,vt as a}from"./src-CzD1T4uF.js";import o,{n as s}from"./Grid.stories-pP7CskIm.js";var c,l,u,d;e((()=>{i(),r(),s(),c=t(),l={...o,title:`Components/Layout/Grid`},u={args:{children:[(0,c.jsx)(a.Cell,{span:`all`},1),(0,c.jsx)(a.Cell,{span:`all`},2)]},render:(e,t)=>n(a,{args:e},t),tags:[`!dev`,`!autodocs`]},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: [<Grid.Cell key={1} span="all" />, <Grid.Cell key={2} span="all" />]
-  },
-  render: (args, context) => renderComponentVariants(Grid, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...u.parameters?.docs?.source}}},d=[`Test`]}))();export{u as Test,d as __namedExportsOrder,l as default};
---
+++ demo-develop/assets/Image.test.stories-D1yEGoR4.js
@@ -0,0 +1,10 @@
+import{i as e}from"./preload-helper-xPQekRTU.js";import{n as t,t as n}from"./renderComponentVariants-BK5jYxXW.js";import{Yt as r,t as i}from"./src-DEjBn602.js";import a,{n as o}from"./Image.stories-O0qsGjil.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Media/Image`},c={args:{alt:``,src:`https://picsum.photos/id/122/1280/720`},render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    alt: '',
+    src: 'https://picsum.photos/id/122/1280/720'
+  },
+  render: (args, context) => renderComponentVariants(Image, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
