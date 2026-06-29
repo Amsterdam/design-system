1cc722db2ba1eb711143ebbbeba4a5b8b9fba8bf
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-06-29 16:16:03 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@f024450d3013bb3029f7a393bc7cd8419c1cfe85 🚀

--- demo-develop/assets/Avatar.test.stories-DCljKaat.js
+++
@@ -1,6 +0,0 @@
-import{i as e}from"./preload-helper-xPQekRTU.js";import{n as t,t as n}from"./renderComponentVariants-BbcMU-HA.js";import{gn as r,t as i}from"./src-CfsgRa_Q.js";import a,{n as o}from"./Avatar.stories-C4pNpBYE.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Feedback/Avatar`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Avatar, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
--- demo-develop/assets/Column.test.stories-BY2bkEzw.js
+++
@@ -1,7 +0,0 @@
-import{i as e}from"./preload-helper-xPQekRTU.js";import{n as t,t as n}from"./renderComponentVariants-BbcMU-HA.js";import{Bt as r,t as i}from"./src-CfsgRa_Q.js";import a,{n as o}from"./Column.stories-C0an593j.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Layout/Column`},c={render:(e,n)=>t(r,{args:e,layout:`grid`},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Column, {
-    args,
-    layout: 'grid'
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
