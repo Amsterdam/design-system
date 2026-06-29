890bbfaff61c4fa6fa1f9936ab2eccc42d058253
GitHub <github-action@users.noreply.github.com>
2026-06-29 16:12:34 +0000

    Remove feature branch folder

--- demo-typography-italic-font-style/assets/Label.test.stories-DKZ_6qXr.js
+++
@@ -1,6 +0,0 @@
-import{i as e}from"./preload-helper-xPQekRTU.js";import{n as t,t as n}from"./renderComponentVariants-DZNsGxUu.js";import{st as r,t as i}from"./src-DIzE9HnH.js";import a,{n as o}from"./Label.stories-uDa1poXb.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Label`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Label, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
