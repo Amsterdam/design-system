5c34b237eb0855af7defa8c67a5a316d9ed8952e
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-06-29 15:48:27 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@7ea7519605ec623997d9cc273a848077fd59a781 🚀

---
+++ demo-typography-italic-font-style/assets/Breakout.stories-BtV0_5VW.js
@@ -0,0 +1,56 @@
+import{a as e,i as t}from"./preload-helper-xPQekRTU.js";import{t as n}from"./jsx-runtime-BqsN2jGA.js";import{Ot as r,mt as i,tt as a,xt as o}from"./iframe-DBiM1fCX.js";import{sn as s,t as c}from"./src-Dz0UiXtn.js";import{n as l,t as u}from"./Grid.argTypes-dGm0TVin.js";var d=e({Default:()=>m,VerticalLayout:()=>h,__namedExportsOrder:()=>g,default:()=>p}),f,p,m,h,g,_=t((()=>{r(),c(),l(),f=n(),p={title:`Components/Layout/Breakout`,component:s,argTypes:u},m={args:{children:[(0,f.jsx)(s.Cell,{colSpan:`all`,has:`spotlight`,rowSpan:{narrow:2,medium:2,wide:1},rowStart:2,children:(0,f.jsx)(o,{color:`green`})},1),(0,f.jsx)(s.Cell,{colSpan:{narrow:4,medium:8,wide:6},colStart:1,rowStart:{narrow:3,medium:3,wide:2},children:(0,f.jsx)(i,{color:`inverse`,children:`Het doel van deze club is om ervoor te zorgen dat de Zuidas steeds duurzamer wordt.`})},2),(0,f.jsx)(s.Cell,{colSpan:{narrow:4,medium:8,wide:6},colStart:{narrow:1,medium:1,wide:7},has:`figure`,rowSpan:2,rowStart:1,children:(0,f.jsx)(a,{alt:``,aspectRatio:`1:1`,src:`https://picsum.photos/960/960`})},3)]}},h={args:{children:[(0,f.jsx)(s.Cell,{colSpan:`all`,has:`spotlight`,rowSpan:2,rowStart:2,children:(0,f.jsx)(o,{color:`orange`})},1),(0,f.jsx)(s.Cell,{colSpan:`all`,has:`figure`,rowSpan:2,rowStart:1,children:(0,f.jsx)(a,{alt:``,src:`https://picsum.photos/1440/810`})},2),(0,f.jsxs)(s.Cell,{colSpan:{narrow:4,medium:5,wide:7},colStart:{narrow:1,medium:2,wide:3},rowStart:3,children:[(0,f.jsx)(i,{className:`ams-mb-s`,children:`Vertel ons in het evenementenformulier wat u wilt gaan doen. U checkt daarmee of u een vergunning nodig hebt.`}),(0,f.jsx)(i,{children:`Daarna vraagt u de vergunning aan in hetzelfde formulier. Of doet u een gratis melding of vooraankondiging.`})]},3)]}},m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<Breakout.Cell colSpan="all" has="spotlight" key={1} rowSpan={{
+      narrow: 2,
+      medium: 2,
+      wide: 1
+    }} rowStart={2}>
+        <Spotlight color="green" />
+      </Breakout.Cell>, <Breakout.Cell colSpan={{
+      narrow: 4,
+      medium: 8,
+      wide: 6
+    }} colStart={1} key={2} rowStart={{
+      narrow: 3,
+      medium: 3,
+      wide: 2
+    }}>
+        <Paragraph color="inverse">
+          Het doel van deze club is om ervoor te zorgen dat de Zuidas steeds duurzamer wordt.
+        </Paragraph>
+      </Breakout.Cell>, <Breakout.Cell colSpan={{
+      narrow: 4,
+      medium: 8,
+      wide: 6
+    }} colStart={{
+      narrow: 1,
+      medium: 1,
+      wide: 7
+    }} has="figure" key={3} rowSpan={2} rowStart={1}>
+        <Image alt="" aspectRatio="1:1" src="https://picsum.photos/960/960" />
+      </Breakout.Cell>]
+  }
+}`,...m.parameters?.docs?.source}}},h.parameters={...h.parameters,docs:{...h.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<Breakout.Cell colSpan="all" has="spotlight" key={1} rowSpan={2} rowStart={2}>
+        <Spotlight color="orange" />
+      </Breakout.Cell>, <Breakout.Cell colSpan="all" has="figure" key={2} rowSpan={2} rowStart={1}>
+        <Image alt="" src="https://picsum.photos/1440/810" />
+      </Breakout.Cell>, <Breakout.Cell colSpan={{
+      narrow: 4,
+      medium: 5,
+      wide: 7
+    }} colStart={{
+      narrow: 1,
+      medium: 2,
+      wide: 3
+    }} key={3} rowStart={3}>
+        <Paragraph className="ams-mb-s">
+          Vertel ons in het evenementenformulier wat u wilt gaan doen. U checkt daarmee of u een vergunning nodig hebt.
+        </Paragraph>
+        <Paragraph>
+          Daarna vraagt u de vergunning aan in hetzelfde formulier. Of doet u een gratis melding of vooraankondiging.
+        </Paragraph>
+      </Breakout.Cell>]
+  }
+}`,...h.parameters?.docs?.source}}},g=[`Default`,`VerticalLayout`]}));_();export{m as Default,h as VerticalLayout,g as __namedExportsOrder,p as default,_ as n,d as t};
---
+++ demo-typography-italic-font-style/assets/Image.test.stories-BBQbDHSn.js
@@ -0,0 +1,10 @@
+import{i as e}from"./preload-helper-xPQekRTU.js";import{n as t,t as n}from"./renderComponentVariants-6Il-8-IT.js";import{Yt as r,t as i}from"./src-Dz0UiXtn.js";import a,{n as o}from"./Image.stories-DCEhAsAK.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Media/Image`},c={args:{alt:``,src:`https://picsum.photos/id/122/1280/720`},render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    alt: '',
+    src: 'https://picsum.photos/id/122/1280/720'
+  },
+  render: (args, context) => renderComponentVariants(Image, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
