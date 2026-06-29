d2512ae69831c3e2739a982c6349dfcf72d256cc
GitHub <github-action@users.noreply.github.com>
2026-06-29 20:04:05 +0000

    Remove feature branch folder

--- demo-dialog-prevent-background-scroll/assets/Alert.test.stories-BFt4kxMP.js
+++
@@ -1,6 +0,0 @@
-import{i as e}from"./preload-helper-xPQekRTU.js";import{n as t,t as n}from"./renderComponentVariants-BF2ZY-CC.js";import{t as r,yn as i}from"./src-Cl2A0Hao.js";import a,{n as o}from"./Alert.stories-0_pgF7_S.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/Feedback/Alert`},c={render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Alert, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
