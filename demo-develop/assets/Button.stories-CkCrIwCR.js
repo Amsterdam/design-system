a3a8e3ed053bf72cdb2a82836d1a2d2bcc0c8b83
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-06-30 23:15:27 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@a0b1b84399ea86bffe7497e3e23e8aa65750953e 🚀

---
+++ demo-develop/assets/Button.stories-CkCrIwCR.js
@@ -0,0 +1,34 @@
+import{a as e,i as t}from"./preload-helper-CT_b8DTk.js";import{t as n}from"./jsx-runtime-BCDDjCIb.js";import{G as r,W as i,u as a}from"./index.esm-Cyk4q9Yv.js";import{an as o,t as s}from"./src-CqWyeUDi.js";import{n as c,t as l}from"./decorators-COMB1E6E.js";var u=e({Primary:()=>p,Secondary:()=>m,Tertiary:()=>h,TextWrapping:()=>y,WithIcon:()=>g,WithIconBefore:()=>_,WithIconOnly:()=>v,__namedExportsOrder:()=>b,default:()=>f}),d,f,p,m,h,g,_,v,y,b,x=t((()=>{r(),s(),l(),d=n(),f={title:`Components/Buttons/Button`,component:o,args:{children:`Versturen`,disabled:!1,icon:void 0,iconBefore:!1,iconOnly:void 0,variant:`primary`},argTypes:{disabled:{description:`Prevents interaction. Avoid if possible.`},icon:{control:{labels:{undefined:`none`},type:`select`},mapping:i,options:[void 0,...Object.keys(i)]},iconBefore:{control:{type:`boolean`},if:{arg:`icon`}},iconOnly:{control:{type:`boolean`},if:{arg:`icon`}}}},p={},m={args:{children:`Annuleren`,variant:`secondary`}},h={args:{children:`Terug`,variant:`tertiary`}},g={args:{children:`Sluiten`,icon:(0,d.jsx)(a,{})}},_={args:{children:`Sluiten`,icon:(0,d.jsx)(a,{}),iconBefore:!0}},v={args:{children:`Sluiten`,icon:(0,d.jsx)(a,{}),iconOnly:!0,variant:`tertiary`}},y={args:{children:`Vergunningsaanvraag verzenden`},decorators:[c(`16rem`)]},p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{}`,...p.parameters?.docs?.source}}},m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Annuleren',
+    variant: 'secondary'
+  }
+}`,...m.parameters?.docs?.source}}},h.parameters={...h.parameters,docs:{...h.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Terug',
+    variant: 'tertiary'
+  }
+}`,...h.parameters?.docs?.source}}},g.parameters={...g.parameters,docs:{...g.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Sluiten',
+    icon: <CloseIcon />
+  }
+}`,...g.parameters?.docs?.source}}},_.parameters={..._.parameters,docs:{..._.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Sluiten',
+    icon: <CloseIcon />,
+    iconBefore: true
+  }
+}`,..._.parameters?.docs?.source}}},v.parameters={...v.parameters,docs:{...v.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Sluiten',
+    icon: <CloseIcon />,
+    iconOnly: true,
+    variant: 'tertiary'
+  }
+}`,...v.parameters?.docs?.source}}},y.parameters={...y.parameters,docs:{...y.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Vergunningsaanvraag verzenden'
+  },
+  decorators: [maximiseInlineSize('16rem')]
+}`,...y.parameters?.docs?.source}}},b=[`Primary`,`Secondary`,`Tertiary`,`WithIcon`,`WithIconBefore`,`WithIconOnly`,`TextWrapping`]}));x();export{p as Primary,m as Secondary,h as Tertiary,y as TextWrapping,g as WithIcon,_ as WithIconBefore,v as WithIconOnly,b as __namedExportsOrder,f as default,x as n,u as t};
---
+++ demo-develop/assets/Grid.stories-CY1Dj2Eb.js
@@ -0,0 +1,90 @@
+import{a as e,i as t}from"./preload-helper-CT_b8DTk.js";import{t as n}from"./jsx-runtime-BCDDjCIb.js";import{J as r,P as i}from"./index.esm-D59Hp6_7.js";import{t as a,vt as o}from"./src-CqWyeUDi.js";import{n as s,t as c}from"./GridColumnsGuide-BpyWYI2b.js";import{i as l,t as u}from"./decorators-COMB1E6E.js";import{n as d,t as f}from"./Grid.argTypes-AB-F9ER8.js";var p=e({BackgroundInCompactMode:()=>E,Default:()=>v,ImproveSemantics:()=>D,SpanAllColumns:()=>w,SpanColumns:()=>x,SpanResponsively:()=>C,SpanRows:()=>S,StartPosition:()=>T,VerticalGap:()=>b,VerticalPadding:()=>y,__namedExportsOrder:()=>O,default:()=>h}),m,h,g,_,v,y,b,x,S,C,w,T,E,D,O,k=t((()=>{r(),a(),u(),s(),d(),m=n(),h={title:`Components/Layout/Grid`,component:o,argTypes:f,decorators:[l],parameters:{layout:`fullscreen`}},o.Cell,g={args:{paddingVertical:`x-large`},decorators:[e=>(0,m.jsxs)(m.Fragment,{children:[(0,m.jsx)(c,{}),(0,m.jsx)(e,{})]})]},_={decorators:[e=>(0,m.jsxs)(m.Fragment,{children:[(0,m.jsx)(c,{}),(0,m.jsx)(o,{paddingVertical:`x-large`,children:(0,m.jsx)(e,{})})]})],render:({children:e,...t})=>(0,m.jsx)(o.Cell,{...t,children:e})},v={...g,args:{...g.args,children:[(0,m.jsx)(o.Cell,{className:`_ams-item`,span:`all`},1),(0,m.jsx)(o.Cell,{className:`_ams-item`,span:{narrow:4,medium:4,wide:8}},2),(0,m.jsx)(o.Cell,{className:`_ams-item`,span:4},3)]}},y={...g,args:{...g.args,children:(0,m.jsx)(o.Cell,{className:`_ams-item`,span:`all`})}},b={...g,args:{...g.args,children:[(0,m.jsx)(o.Cell,{className:`_ams-item`,span:`all`},1),(0,m.jsx)(o.Cell,{className:`_ams-item`,span:`all`},2)]}},x={..._,args:{className:`_ams-item`,span:4}},S={...g,args:{children:[(0,m.jsx)(o.Cell,{className:`_ams-item`,rowSpan:2,span:2},1),(0,m.jsx)(o.Cell,{className:`_ams-item`,span:{narrow:2,medium:6,wide:10}},2),(0,m.jsx)(o.Cell,{className:`_ams-item`,span:{narrow:2,medium:6,wide:10}},3)]}},C={..._,args:{className:`_ams-item`,span:{narrow:4,medium:6,wide:8}}},w={..._,args:{className:`_ams-item`,span:`all`}},T={..._,args:{className:`_ams-item`,span:3,start:{narrow:2,medium:4,wide:6}}},E={...g,args:{...g.args,children:[(0,m.jsx)(o.Cell,{span:4,children:(0,m.jsx)(i,{children:`Default`})},1),(0,m.jsx)(o.Cell,{appearance:`flush`,span:4,children:(0,m.jsx)(i,{children:`Flush`})},2),(0,m.jsx)(o.Cell,{appearance:`transparent`,span:4,children:(0,m.jsx)(i,{children:`Transparent`})},3)]},decorators:void 0},D={..._,args:{as:`section`},render:({as:e})=>(0,m.jsx)(o.Cell,{as:e,className:`_ams-item`,span:`all`})},v.parameters={...v.parameters,docs:{...v.parameters?.docs,source:{originalSource:`{
+  ...StoryTemplate,
+  args: {
+    ...StoryTemplate.args,
+    children: [<Grid.Cell className="_ams-item" key={1} span="all" />, <Grid.Cell className="_ams-item" key={2} span={{
+      narrow: 4,
+      medium: 4,
+      wide: 8
+    }} />, <Grid.Cell className="_ams-item" key={3} span={4} />]
+  }
+}`,...v.parameters?.docs?.source}}},y.parameters={...y.parameters,docs:{...y.parameters?.docs,source:{originalSource:`{
+  ...StoryTemplate,
+  args: {
+    ...StoryTemplate.args,
+    children: <Grid.Cell className="_ams-item" span="all" />
+  }
+}`,...y.parameters?.docs?.source}}},b.parameters={...b.parameters,docs:{...b.parameters?.docs,source:{originalSource:`{
+  ...StoryTemplate,
+  args: {
+    ...StoryTemplate.args,
+    children: [<Grid.Cell className="_ams-item" key={1} span="all" />, <Grid.Cell className="_ams-item" key={2} span="all" />]
+  }
+}`,...b.parameters?.docs?.source}}},x.parameters={...x.parameters,docs:{...x.parameters?.docs,source:{originalSource:`{
+  ...CellStoryTemplate,
+  args: {
+    className: '_ams-item',
+    span: 4
+  }
+}`,...x.parameters?.docs?.source}}},S.parameters={...S.parameters,docs:{...S.parameters?.docs,source:{originalSource:`{
+  ...StoryTemplate,
+  args: {
+    children: [<Grid.Cell className="_ams-item" key={1} rowSpan={2} span={2} />, <Grid.Cell className="_ams-item" key={2} span={{
+      narrow: 2,
+      medium: 6,
+      wide: 10
+    }} />, <Grid.Cell className="_ams-item" key={3} span={{
+      narrow: 2,
+      medium: 6,
+      wide: 10
+    }} />]
+  }
+}`,...S.parameters?.docs?.source}}},C.parameters={...C.parameters,docs:{...C.parameters?.docs,source:{originalSource:`{
+  ...CellStoryTemplate,
+  args: {
+    className: '_ams-item',
+    span: {
+      narrow: 4,
+      medium: 6,
+      wide: 8
+    }
+  }
+}`,...C.parameters?.docs?.source}}},w.parameters={...w.parameters,docs:{...w.parameters?.docs,source:{originalSource:`{
+  ...CellStoryTemplate,
+  args: {
+    className: '_ams-item',
+    span: 'all'
+  }
+}`,...w.parameters?.docs?.source}}},T.parameters={...T.parameters,docs:{...T.parameters?.docs,source:{originalSource:`{
+  ...CellStoryTemplate,
+  args: {
+    className: '_ams-item',
+    span: 3,
+    start: {
+      narrow: 2,
+      medium: 4,
+      wide: 6
+    }
+  }
+}`,...T.parameters?.docs?.source}}},E.parameters={...E.parameters,docs:{...E.parameters?.docs,source:{originalSource:`{
+  ...StoryTemplate,
+  args: {
+    ...StoryTemplate.args,
+    children: [<Grid.Cell key={1} span={4}>
+        <Paragraph>Default</Paragraph>
+      </Grid.Cell>, <Grid.Cell appearance="flush" key={2} span={4}>
+        <Paragraph>Flush</Paragraph>
+      </Grid.Cell>, <Grid.Cell appearance="transparent" key={3} span={4}>
+        <Paragraph>Transparent</Paragraph>
+      </Grid.Cell>]
+  },
+  decorators: undefined
+}`,...E.parameters?.docs?.source}}},D.parameters={...D.parameters,docs:{...D.parameters?.docs,source:{originalSource:`{
+  ...CellStoryTemplate,
+  args: {
+    as: 'section'
+  },
+  render: ({
+    as
+  }: GridCellProps) => <Grid.Cell as={as} className="_ams-item" span="all" />
+}`,...D.parameters?.docs?.source}}},O=[`Default`,`VerticalPadding`,`VerticalGap`,`SpanColumns`,`SpanRows`,`SpanResponsively`,`SpanAllColumns`,`StartPosition`,`BackgroundInCompactMode`,`ImproveSemantics`]}));k();export{E as BackgroundInCompactMode,v as Default,D as ImproveSemantics,w as SpanAllColumns,x as SpanColumns,C as SpanResponsively,S as SpanRows,T as StartPosition,b as VerticalGap,y as VerticalPadding,O as __namedExportsOrder,h as default,k as n,p as t};
