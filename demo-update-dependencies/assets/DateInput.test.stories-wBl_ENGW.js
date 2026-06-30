91d8518c12436e388e35aa98c1b7c2347caa7fe5
GitHub <github-action@users.noreply.github.com>
2026-06-30 20:22:29 +0000

    Remove feature branch folder

--- demo-update-dependencies/assets/DateInput.test.stories-wBl_ENGW.js
+++
@@ -1,6 +0,0 @@
-import{i as e}from"./preload-helper-CT_b8DTk.js";import{n as t,t as n}from"./renderComponentVariants-BeYKBBT2.js";import{Rt as r,t as i}from"./src-B1SNo8hV.js";import a,{n as o}from"./DateInput.stories-C6qg9S51.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Date Input`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(DateInput, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
