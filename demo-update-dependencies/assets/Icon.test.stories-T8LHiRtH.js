ccb162c0fcaeb83a5f099721d97f4e002162f3a1
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-06-30 20:18:39 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@72c289ecb86125dfc57f31cfb9e8160d2140726b 🚀

---
+++ demo-update-dependencies/assets/Icon.test.stories-T8LHiRtH.js
@@ -0,0 +1,13 @@
+import{i as e}from"./preload-helper-CT_b8DTk.js";import{t}from"./jsx-runtime-BCDDjCIb.js";import{n,t as r}from"./renderComponentVariants-BeYKBBT2.js";import{An as i,t as a}from"./src-B1SNo8hV.js";import{n as o,t as s}from"./src-Nnmu5y6t.js";import c,{n as l}from"./Icon.stories-VxjxgCib.js";var u,d,f,p,m;e((()=>{s(),a(),r(),l(),u=t(),d={...c,title:`Components/Media/Icon`},f=Object.keys(o).sort((e,t)=>e.localeCompare(t)),p={render:(e,t)=>(0,u.jsxs)(`div`,{className:`_ams-tests-stack`,children:[(0,u.jsx)(`span`,{children:`Variants of Icon component`}),n(i,{args:e},t),(0,u.jsx)(`span`,{children:`All icons`}),(0,u.jsx)(`div`,{className:`_ams-tests-grid`,children:f.map(e=>(0,u.jsx)(i,{size:`heading-2`,svg:o[e]},e))})]}),tags:[`!dev`,`!autodocs`]},p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => <div className="_ams-tests-stack">
+      <span>Variants of Icon component</span>
+      {renderComponentVariants(Icon, {
+      args
+    }, context)}
+      <span>All icons</span>
+      <div className="_ams-tests-grid">
+        {iconNames.map(name => <Icon key={name} size="heading-2" svg={Icons[name]} />)}
+      </div>
+    </div>,
+  tags: ['!dev', '!autodocs']
+}`,...p.parameters?.docs?.source}}},m=[`Test`]}))();export{p as Test,m as __namedExportsOrder,d as default};
---
+++ demo-update-dependencies/assets/Image.test.stories-POYLXH4t.js
@@ -0,0 +1,10 @@
+import{i as e}from"./preload-helper-CT_b8DTk.js";import{n as t,t as n}from"./renderComponentVariants-BeYKBBT2.js";import{Yt as r,t as i}from"./src-B1SNo8hV.js";import a,{n as o}from"./Image.stories-BO-lqSOP.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Media/Image`},c={args:{alt:``,src:`https://picsum.photos/id/122/1280/720`},render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    alt: '',
+    src: 'https://picsum.photos/id/122/1280/720'
+  },
+  render: (args, context) => renderComponentVariants(Image, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
