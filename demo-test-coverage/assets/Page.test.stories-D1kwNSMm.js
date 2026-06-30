285034fa7ac532cea5669f5317f10b054b414f94
GitHub <github-action@users.noreply.github.com>
2026-06-30 11:55:46 +0000

    Remove feature branch folder

--- demo-test-coverage/assets/Page.test.stories-D1kwNSMm.js
+++
@@ -1,6 +0,0 @@
-import{i as e}from"./preload-helper-xPQekRTU.js";import{n as t,t as n}from"./renderComponentVariants-CzEOGbic.js";import{K as r,t as i}from"./src-DqRX3ghB.js";import a,{n as o}from"./Page.stories-aq5QZvEt.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Containers/Page`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Page, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
