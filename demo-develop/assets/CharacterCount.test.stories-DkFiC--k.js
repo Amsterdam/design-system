a9d89823ebdd09f27315318753213519b0e1a47c
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-01 22:39:17 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@9bf29fd8abe62ce95222c7a8216ab0c31986cb7a 🚀

--- demo-develop/assets/CharacterCount.test.stories-DkFiC--k.js
+++
@@ -1,6 +0,0 @@
-import{i as e}from"./preload-helper-CT_b8DTk.js";import{n as t,t as n}from"./renderComponentVariants-CaGN0qTD.js";import{Gt as r,t as i}from"./src-C8Ajw2KT.js";import a,{n as o}from"./CharacterCount.stories-D1SxhojV.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Character Count`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(CharacterCount, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
---
+++ demo-develop/assets/DescriptionList.test.stories-DE3hLQtZ.js
@@ -0,0 +1,28 @@
+import{i as e}from"./preload-helper-CT_b8DTk.js";import{t}from"./jsx-runtime-BCDDjCIb.js";import{n,t as r}from"./renderComponentVariants-BBedZNRR.js";import{Pt as i,t as a}from"./src-Byr4S8_c.js";import o,{n as s}from"./DescriptionList.stories-SViAfu4Y.js";var c,l,u,d,f;e((()=>{a(),r(),s(),c=t(),l={...o,title:`Components/Text/Description List`},u=e=>(0,c.jsxs)(c.Fragment,{children:[(0,c.jsx)(i,{...e}),(0,c.jsx)(`div`,{className:`ams-query-container-inline-size`,style:{inlineSize:`31.99rem`},children:(0,c.jsx)(i,{...e})})]}),d={args:{children:[(0,c.jsx)(i.Term,{children:`Land`},1),(0,c.jsx)(i.Description,{children:`Nederland`},2),(0,c.jsx)(i.Term,{children:`Plaatsnaam`},3),(0,c.jsx)(i.Description,{children:`Amsterdam`},4),(0,c.jsx)(i.Description,{children:`Weesp`},5),(0,c.jsxs)(i.Section,{children:[(0,c.jsx)(i.Term,{children:`Voornaam`}),(0,c.jsx)(i.Term,{children:`Achternaam`}),(0,c.jsx)(i.Description,{children:`Johan Cruijff`})]},6),(0,c.jsxs)(i.Section,{children:[(0,c.jsx)(i.Term,{children:`Stadsdeel`}),(0,c.jsx)(i.Term,{children:`Stadsgebied`}),(0,c.jsx)(i.Description,{children:`Centrum`}),(0,c.jsx)(i.Description,{children:`Oost`}),(0,c.jsx)(i.Description,{children:`Nieuw-West`}),(0,c.jsx)(i.Description,{children:`Weesp`})]},7)]},render:(e,t)=>n(u,{args:e},t),tags:[`!dev`,`!autodocs`]},d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [
+    // 1:1
+    <DescriptionList.Term key={1}>Land</DescriptionList.Term>, <DescriptionList.Description key={2}>Nederland</DescriptionList.Description>,
+    // 1:n
+    <DescriptionList.Term key={3}>Plaatsnaam</DescriptionList.Term>, <DescriptionList.Description key={4}>Amsterdam</DescriptionList.Description>, <DescriptionList.Description key={5}>Weesp</DescriptionList.Description>,
+    // n:1
+    <DescriptionList.Section key={6}>
+        <DescriptionList.Term>Voornaam</DescriptionList.Term>
+        <DescriptionList.Term>Achternaam</DescriptionList.Term>
+        <DescriptionList.Description>Johan Cruijff</DescriptionList.Description>
+      </DescriptionList.Section>,
+    // n:m
+    <DescriptionList.Section key={7}>
+        <DescriptionList.Term>Stadsdeel</DescriptionList.Term>
+        <DescriptionList.Term>Stadsgebied</DescriptionList.Term>
+        <DescriptionList.Description>Centrum</DescriptionList.Description>
+        <DescriptionList.Description>Oost</DescriptionList.Description>
+        <DescriptionList.Description>Nieuw-West</DescriptionList.Description>
+        <DescriptionList.Description>Weesp</DescriptionList.Description>
+      </DescriptionList.Section>]
+  },
+  render: (args, context) => renderComponentVariants(DescriptionListWithContainerQueryExamples, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...d.parameters?.docs?.source}}},f=[`Test`]}))();export{d as Test,f as __namedExportsOrder,l as default};
