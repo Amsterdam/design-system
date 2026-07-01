6e1a0873f2cf77c89711da0977e8378ed7c09fab
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-01 21:44:37 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@2c03295b24b5a243667898a631fdc337eaa9c159 🚀

---
+++ demo-document-check-pr-title-pull-request-target/assets/FileList.stories-C2_fJYRu.js
@@ -0,0 +1,11 @@
+import{a as e,i as t,s as n}from"./preload-helper-CT_b8DTk.js";import{t as r}from"./react-BTu_umhU.js";import{t as i}from"./jsx-runtime-BCDDjCIb.js";import{J as a,_ as o,g as s}from"./index.esm-DEMFzfBr.js";import{bt as c,t as l}from"./src-pwe0nuwo.js";var u,d,f,p=t((()=>{a(),u=n(r()),d=i(),f=()=>{let e=(0,u.useRef)(null),[t,n]=(0,u.useState)(null),r=()=>{e.current&&n(e.current.files)},i=r=>{if(t){let i=new DataTransfer;Array.from(t).forEach((e,t)=>{t!==r&&i.items.add(e)}),e.current&&(e.current.files=i.files),n(i.files)}};return(0,d.jsxs)(d.Fragment,{children:[(0,d.jsx)(s,{multiple:!0,onChange:r,ref:e}),t&&(0,d.jsx)(o,{children:Array.from(t).map((e,t)=>(0,d.jsx)(o.Item,{file:e,onDelete:()=>i(t)},e.name))})]})}})),m=e({Default:()=>_,WithInput:()=>v,__namedExportsOrder:()=>y,default:()=>g}),h,g,_,v,y,b=t((()=>{l(),p(),h=i(),g={title:`Components/Forms/File List`,component:c,args:{children:[(0,h.jsx)(c.Item,{file:new File([`sample1`],`sample1.txt`,{lastModified:Date.now(),type:`text/plain`}),onDelete:()=>{}},`1`),(0,h.jsx)(c.Item,{file:new File([`sample2`],`sample2.txt`,{lastModified:Date.now(),type:`text/plain`}),onDelete:()=>{}},`2`)]}},_={},v={parameters:{docs:{canvas:{sourceState:`none`},codePanel:!1}},render:()=>(0,h.jsx)(f,{})},_.parameters={..._.parameters,docs:{..._.parameters?.docs,source:{originalSource:`{}`,..._.parameters?.docs?.source}}},v.parameters={...v.parameters,docs:{...v.parameters?.docs,source:{originalSource:`{
+  parameters: {
+    docs: {
+      canvas: {
+        sourceState: 'none'
+      },
+      codePanel: false
+    }
+  },
+  render: () => <FileInputWithFileList />
+}`,...v.parameters?.docs?.source}}},y=[`Default`,`WithInput`]}));b();export{_ as Default,v as WithInput,y as __namedExportsOrder,g as default,b as n,m as t};
