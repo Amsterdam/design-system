fe2743b90927052a43268ded6bbd87172fb1ebeb
GitHub <github-action@users.noreply.github.com>
2026-07-01 18:43:07 +0000

    Remove feature branch folder

--- demo-story-lang-follows-locale/assets/IconButton.test.stories-KBjy887Q.js
+++
@@ -1,7 +0,0 @@
-import{i as e}from"./preload-helper-CT_b8DTk.js";import{n as t,t as n}from"./renderComponentVariants-B6bPZSY5.js";import{t as r,wn as i}from"./src-CT6c9PnZ.js";import a,{n as o}from"./IconButton.stories-BQDe_0Xq.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/Buttons/Icon Button`},c={render:(e,n)=>t(i,{args:e,variants:[`hovered`,`disabled`]},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(IconButton, {
-    args,
-    variants: ['hovered', 'disabled']
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
