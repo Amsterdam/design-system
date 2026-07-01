56f735194d94260c9ff6115cb93725fade898f45
GitHub <github-action@users.noreply.github.com>
2026-07-01 22:35:21 +0000

    Remove feature branch folder

--- demo-ci-concurrency-and-timeouts/assets/Row.test.stories-CJW4IgEh.js
+++
@@ -1,9 +0,0 @@
-import{i as e}from"./preload-helper-CT_b8DTk.js";import{t}from"./jsx-runtime-BCDDjCIb.js";import{n,t as r}from"./renderComponentVariants-BVSjiOdf.js";import{t as i,xn as a}from"./src-BqMvvVQq.js";import o,{n as s}from"./Row.stories-DmOwAzfr.js";var c,l,u,d;e((()=>{i(),r(),s(),c=t(),l={...o,title:`Components/Layout/Row`},u={args:{children:[(0,c.jsx)(`div`,{className:`_ams-item`},1),(0,c.jsx)(`div`,{className:`_ams-item`},2),(0,c.jsx)(`div`,{className:`_ams-item`},3)]},render:(e,t)=>n(a,{args:e},t),tags:[`!dev`,`!autodocs`]},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: [<div className="_ams-item" key={1} />, <div className="_ams-item" key={2} />, <div className="_ams-item" key={3} />]
-  },
-  render: (args, context) => renderComponentVariants(Row, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...u.parameters?.docs?.source}}},d=[`Test`]}))();export{u as Test,d as __namedExportsOrder,l as default};
