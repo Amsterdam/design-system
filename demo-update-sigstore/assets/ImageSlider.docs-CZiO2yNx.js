9c6beb2a4f22f3eba8e4537613cb7019823868bc
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-01 22:24:32 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@b76a941c9d6ba971190b5e6291b650d61a42f22c 🚀

---
+++ demo-update-sigstore/assets/ImageSlider.docs-CZiO2yNx.js
@@ -0,0 +1,43 @@
+import{i as e}from"./preload-helper-CT_b8DTk.js";import{t}from"./jsx-runtime-BCDDjCIb.js";import{b as n,d as r,f as i,g as a,i as o,m as s,o as c,s as l}from"./iframe-d91q8Bqg.js";import{n as u,t as d}from"./DesignTokensTable-BGV-q0IW.js";import{t as f}from"./mdx-react-shim-B0BCGjQ9.js";import{ResponsiveImages as p,WithCaptions as m,n as h,t as g}from"./ImageSlider.stories-D4hLisZJ.js";var _,v,y=e((()=>{_={"image-slider":{gap:{$value:`{ams.space.xs}`,$extensions:{"nl.amsterdam.subtype":`space`,"nl.amsterdam.type":`dimension`}},scroller:{gap:{$value:`{ams.space.xs}`,$extensions:{"nl.amsterdam.subtype":`space`,"nl.amsterdam.type":`dimension`}},"outline-offset":{$value:`{ams.focus.outline-offset}`,$extensions:{"nl.amsterdam.subtype":`space`,"nl.amsterdam.type":`dimension`}}},thumbnails:{gap:{$value:`{ams.space.xs}`,$extensions:{"nl.amsterdam.subtype":`space`,"nl.amsterdam.type":`dimension`}},thumbnail:{"background-color":{$value:`transparent`,$type:`color`},cursor:{$value:`{ams.cursor.interactive}`,$extensions:{"nl.amsterdam.type":`cursor`}},opacity:{$value:`40%`,$extensions:{"nl.amsterdam.type":`opacity`}},"outline-offset":{$value:`{ams.focus.outline-offset}`,$extensions:{"nl.amsterdam.subtype":`space`,"nl.amsterdam.type":`dimension`}},"in-view":{opacity:{$value:`100%`,$extensions:{"nl.amsterdam.type":`opacity`}}},hover:{opacity:{$value:`100%`,$extensions:{"nl.amsterdam.type":`opacity`}}}}},figure:{"margin-block-end":{$value:`{ams.space.m}`,$extensions:{"nl.amsterdam.subtype":`space`,"nl.amsterdam.type":`dimension`}}}}},v={ams:_}}));function b(e){let t={a:`a`,code:`code`,h2:`h2`,h3:`h3`,li:`li`,p:`p`,ul:`ul`,...n(),...e.components};return(0,S.jsxs)(S.Fragment,{children:[`
+`,`
+`,`
+`,`
+`,(0,S.jsx)(r,{of:g}),`
+`,(0,S.jsx)(s,{}),`
+`,(0,S.jsx)(l,{of:g}),`
+`,(0,S.jsx)(i,{}),`
+`,(0,S.jsx)(c,{}),`
+`,(0,S.jsx)(t.h2,{id:`usage-guidelines`,children:`Usage guidelines`}),`
+`,(0,S.jsx)(t.h3,{id:`when-to-use`,children:`When to use`}),`
+`,(0,S.jsx)(t.p,{children:`Use an Image Slider for a series of images that belong together.`}),`
+`,(0,S.jsx)(t.h3,{id:`when-not-to-use`,children:`When not to use`}),`
+`,(0,S.jsx)(t.p,{children:`Assume that some or many users will not navigate between the slides and only see the first image.
+Display all images separately if you want each of them to receive attention.`}),`
+`,(0,S.jsx)(t.h3,{id:`how-to-use`,children:`How to use`}),`
+`,(0,S.jsx)(t.p,{children:`Provide at least 2 images and at most 5.
+Feature the most essential image first.`}),`
+`,(0,S.jsxs)(t.p,{children:[`A full-width Image Slider should run from one edge of the `,(0,S.jsx)(t.a,{href:`/docs/components-containers-page--docs`,children:`Page`}),` container to the other; position it next to a Grid, not in a Cell spanning all columns.
+Smaller sliders are fine in a Grid Cell.`]}),`
+`,(0,S.jsx)(t.h2,{id:`examples`,children:`Examples`}),`
+`,(0,S.jsx)(t.h3,{id:`responsive-images`,children:`Responsive images`}),`
+`,(0,S.jsxs)(t.p,{children:[`Provide a set of smaller and larger instances for every image (through `,(0,S.jsx)(t.code,{children:`srcSet`}),`, see `,(0,S.jsx)(t.a,{href:`https://developer.mozilla.org/en-US/docs/Web/HTML/Element/img#srcset`,rel:`nofollow`,children:`MDN`}),`) to let the browser select the optimal source file.
+A mobile device can often download a smaller file, saving the user’s bandwidth and time.
+Don’t forget to still include the required `,(0,S.jsx)(t.code,{children:`src`}),` attribute.`]}),`
+`,(0,S.jsx)(o,{of:p}),`
+`,(0,S.jsx)(t.h3,{id:`with-captions`,children:`With captions`}),`
+`,(0,S.jsx)(t.p,{children:`Add a caption to any image to display a description below it, e.g. for context or attribution.
+Keep captions short – they take up vertical space, especially on mobile.
+Consistent caption lengths across images help avoid large layout shifts when sliding.`}),`
+`,(0,S.jsx)(o,{of:m}),`
+`,(0,S.jsx)(t.h2,{id:`design`,children:`Design`}),`
+`,(0,S.jsx)(t.p,{children:`The first or selected image shows at its maximum size.
+Every image displays a thumbnail at about 20% of its width.
+Users can use buttons, thumbnails, or swiping to navigate between the images.
+The buttons are not displayed on a narrow touch device.
+The images do not slide automatically.`}),`
+`,(0,S.jsx)(t.h2,{id:`see-also`,children:`See also`}),`
+`,(0,S.jsxs)(t.ul,{children:[`
+`,(0,S.jsxs)(t.li,{children:[(0,S.jsx)(t.a,{href:`/docs/components-media-image--docs`,children:`Image`}),` – guidelines for individual images.`]}),`
+`]}),`
+`,(0,S.jsx)(t.h2,{id:`design-tokens`,children:`Design tokens`}),`
+`,(0,S.jsx)(d,{tokens:v})]})}function x(e={}){let{wrapper:t}={...n(),...e.components};return t?(0,S.jsx)(t,{...e,children:(0,S.jsx)(b,{...e})}):b(e)}var S;e((()=>{S=t(),f(),a(),u(),y(),h()}))();export{x as default};
---
+++ demo-update-sigstore/assets/Page.stories-BTCg5f77.js
@@ -0,0 +1,54 @@
+import{a as e,i as t}from"./preload-helper-CT_b8DTk.js";import{t as n}from"./jsx-runtime-BCDDjCIb.js";import{G as r,R as i}from"./index.esm-Cyk4q9Yv.js";import{B as a,D as o,J as s,M as c,P as l,j as u,v as d,y as f}from"./index.esm-Q9etqStw.js";import{K as p,t as m}from"./src-tX8EwbMp.js";var h=e({Default:()=>y,WithMenu:()=>b,__namedExportsOrder:()=>x,default:()=>_}),g,_,v,y,b,x,S=t((()=>{s(),r(),m(),g=n(),_={title:`Components/Containers/Page`,component:p,argTypes:{withMenu:{description:`This prop updates automatically to prevent an invalid appearance: a Menu can only be used in Compact Mode.`,table:{readonly:!0}}},parameters:{layout:`fullscreen`}},v=()=>(0,g.jsx)(d,{paddingVertical:`x-large`,children:(0,g.jsx)(d.Cell,{span:`all`,children:(0,g.jsx)(f,{level:1,children:`Page Body`})})}),y={args:{children:[(0,g.jsx)(c,{brandName:`Page Header`,noMenuButtonOnWideWindow:!0},`header`),(0,g.jsx)(`main`,{id:`inhoud`,children:(0,g.jsx)(v,{})},`main`),(0,g.jsxs)(u,{children:[(0,g.jsx)(u.Spotlight,{children:(0,g.jsx)(d,{paddingVertical:`x-large`,children:(0,g.jsx)(d.Cell,{appearance:`transparent`,span:`all`,children:(0,g.jsx)(l,{color:`inverse`,children:`Page Footer`})})})}),(0,g.jsx)(u.Menu,{children:(0,g.jsx)(u.MenuLink,{href:`/`,children:`Page Footer Menu`})})]},`footer`)]}},b={args:{children:(0,g.jsx)(v,{}),withMenu:!0},globals:{theme:`Compact`},render:({children:e,...t})=>(0,g.jsxs)(p,{...t,children:[(0,g.jsx)(a,{className:`ams-page__area--skip-link`,href:`#inhoud`,children:`Direct naar inhoud`}),(0,g.jsx)(c,{brandName:`Page Header`,className:`ams-page__area--header`,noMenuButtonOnWideWindow:!0,children:(0,g.jsx)(o,{children:(0,g.jsx)(o.Link,{href:`#`,icon:(0,g.jsx)(i,{}),children:`Menu item`})})}),(0,g.jsx)(o,{className:`ams-page__area--menu`,inWideWindow:!0,children:(0,g.jsx)(o.Link,{href:`#`,icon:(0,g.jsx)(i,{}),children:`Menu item`})}),(0,g.jsx)(`main`,{className:`ams-page__area--body`,id:`inhoud`,children:e}),(0,g.jsx)(u,{className:`ams-page__area--footer`,children:(0,g.jsx)(u.Menu,{children:(0,g.jsx)(u.MenuLink,{href:`/`,children:`Page Footer Menu`})})})]})},y.parameters={...y.parameters,docs:{...y.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<PageHeader brandName="Page Header" key="header" noMenuButtonOnWideWindow />, <main id="inhoud" key="main">
+        <PageBody />
+      </main>, <PageFooter key="footer">
+        <PageFooter.Spotlight>
+          <Grid paddingVertical="x-large">
+            <Grid.Cell appearance="transparent" span="all">
+              <Paragraph color="inverse">Page Footer</Paragraph>
+            </Grid.Cell>
+          </Grid>
+        </PageFooter.Spotlight>
+        <PageFooter.Menu>
+          <PageFooter.MenuLink href="/">Page Footer Menu</PageFooter.MenuLink>
+        </PageFooter.Menu>
+      </PageFooter>]
+  }
+}`,...y.parameters?.docs?.source}}},b.parameters={...b.parameters,docs:{...b.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: <PageBody />,
+    withMenu: true
+  },
+  globals: {
+    theme: 'Compact'
+  },
+  render: ({
+    children,
+    ...args
+  }) => <Page {...args}>
+      <SkipLink className="ams-page__area--skip-link" href="#inhoud">
+        Direct naar inhoud
+      </SkipLink>
+      <PageHeader brandName="Page Header" className="ams-page__area--header" noMenuButtonOnWideWindow>
+        <Menu>
+          <Menu.Link href="#" icon={<SettingsFillIcon />}>
+            Menu item
+          </Menu.Link>
+        </Menu>
+      </PageHeader>
+      <Menu className="ams-page__area--menu" inWideWindow>
+        <Menu.Link href="#" icon={<SettingsFillIcon />}>
+          Menu item
+        </Menu.Link>
+      </Menu>
+      <main className="ams-page__area--body" id="inhoud">
+        {children}
+      </main>
+      <PageFooter className="ams-page__area--footer">
+        <PageFooter.Menu>
+          <PageFooter.MenuLink href="/">Page Footer Menu</PageFooter.MenuLink>
+        </PageFooter.Menu>
+      </PageFooter>
+    </Page>
+}`,...b.parameters?.docs?.source}}},x=[`Default`,`WithMenu`]}));S();export{y as Default,b as WithMenu,x as __namedExportsOrder,_ as default,S as n,h as t};
