2a71ebccdd67d45b62a5938c49a73e8c036c05ba
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-01 22:07:07 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@1a9ed1eca2923ec5fd065577c4db94e26f535024 🚀

---
+++ demo-harden-branch-name-injection/assets/Overlap.stories-B2Z9P68s.js
@@ -0,0 +1,22 @@
+import{a as e,i as t}from"./preload-helper-CT_b8DTk.js";import{t as n}from"./jsx-runtime-BCDDjCIb.js";import{J as r,R as i,v as a,x as o}from"./index.esm-Fafmsm66.js";import{J as s,t as c}from"./src-DJMGf5wm.js";import{i as l,t as u}from"./decorators-BCNsLzz6.js";var d=e({Default:()=>m,__namedExportsOrder:()=>h,default:()=>p}),f,p,m,h,g=t((()=>{r(),c(),u(),f=n(),p={title:`Components/Layout/Overlap`,component:s,decorators:[l]},m={args:{children:[(0,f.jsx)(o,{alt:``,aspectRatio:`16:5`,sizes:`(max-width: 37.5rem) 640px, (max-width: 72.5rem) 1280px, 1440px`,src:`https://picsum.photos/1440/450`,srcSet:`https://picsum.photos/640/200 640w, https://picsum.photos/1280/400 1280w, https://picsum.photos/1440/450 1440w`},1),(0,f.jsx)(a,{style:{alignSelf:`center`},children:(0,f.jsx)(a.Cell,{span:{narrow:4,medium:6,wide:8},start:{narrow:1,medium:2,wide:3},children:(0,f.jsxs)(i,{onSubmit:e=>e.preventDefault(),children:[(0,f.jsx)(i.Input,{label:`Zoeken`,placeholder:`Wat kunnen we voor u vinden?`}),(0,f.jsx)(i.Button,{})]})})},2)]}},m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<Image alt="" aspectRatio="16:5" key={1} sizes="(max-width: 37.5rem) 640px, (max-width: 72.5rem) 1280px, 1440px" src="https://picsum.photos/1440/450" srcSet="https://picsum.photos/640/200 640w, https://picsum.photos/1280/400 1280w, https://picsum.photos/1440/450 1440w" />, <Grid key={2} style={{
+      alignSelf: 'center'
+    }}>
+        <Grid.Cell span={{
+        narrow: 4,
+        medium: 6,
+        wide: 8
+      }} start={{
+        narrow: 1,
+        medium: 2,
+        wide: 3
+      }}>
+          <SearchField onSubmit={e => e.preventDefault()}>
+            <SearchField.Input label="Zoeken" placeholder="Wat kunnen we voor u vinden?" />
+            <SearchField.Button />
+          </SearchField>
+        </Grid.Cell>
+      </Grid>]
+  }
+}`,...m.parameters?.docs?.source}}},h=[`Default`]}));export{p as i,d as n,g as r,m as t};
---
+++ demo-harden-branch-name-injection/assets/TableCell.stories-B7d0uGHd.js
@@ -0,0 +1,10 @@
+import{i as e}from"./preload-helper-CT_b8DTk.js";import{t}from"./jsx-runtime-BCDDjCIb.js";import{g as n,t as r}from"./src-DJMGf5wm.js";var i,a,o,s,c=e((()=>{r(),i=t(),a={title:`Components/Containers/Table`,component:n.Cell,argTypes:{align:{control:{labels:{undefined:`start`},type:`radio`},options:[void 0,`center`,`end`]}},decorators:[e=>(0,i.jsx)(n,{children:(0,i.jsx)(n.Body,{children:(0,i.jsx)(n.Row,{children:(0,i.jsx)(e,{})})})})]},o={args:{children:`€ 77,85`},decorators:[e=>(0,i.jsx)(`div`,{style:{inlineSize:`8rem`},children:(0,i.jsx)(e,{})})]},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: '€ 77,85'
+  },
+  decorators: [Story => <div style={{
+    inlineSize: '8rem'
+  }}>
+        <Story />
+      </div>]
+}`,...o.parameters?.docs?.source}}},s=[`Cell`]}));c();export{o as Cell,s as __namedExportsOrder,a as default,c as t};
