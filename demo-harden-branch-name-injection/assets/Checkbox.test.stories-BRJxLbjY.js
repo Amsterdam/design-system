cc76eb789698dd10f49971ec8c088fc37fb21fa9
GitHub <github-action@users.noreply.github.com>
2026-07-01 22:23:23 +0000

    Remove feature branch folder

--- demo-harden-branch-name-injection/assets/Checkbox.test.stories-BRJxLbjY.js
+++
@@ -1,6 +0,0 @@
-import{i as e}from"./preload-helper-CT_b8DTk.js";import{n as t,t as n}from"./renderComponentVariants-LmhciYdV.js";import{Ut as r,t as i}from"./src-Df_TEAGG.js";import a,{n as o}from"./Checkbox.stories-BVH3tCb_.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Checkbox`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Checkbox, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
