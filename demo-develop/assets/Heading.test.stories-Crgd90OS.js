d8437539750661328aa223d6e013d3346cf18197
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-01 12:22:28 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@b4dfbb06649b6051df762e2826b85427d10026b4 🚀

--- demo-develop/assets/Heading.test.stories-Crgd90OS.js
+++
@@ -1,9 +0,0 @@
-import{i as e}from"./preload-helper-CT_b8DTk.js";import{n as t,t as n}from"./renderComponentVariants-CbU5ZSUl.js";import{Nn as r,t as i}from"./src-CqWyeUDi.js";import a,{n as o}from"./Heading.stories-BXDNA0O3.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Text/Heading`},c={args:{children:`Amsterdam Light Festival maakt de stad ook deze winter een beetje mooier`},render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: 'Amsterdam Light Festival maakt de stad ook deze winter een beetje mooier'
-  },
-  render: (args, context) => renderComponentVariants(Heading, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
---
+++ demo-develop/assets/mdx-react-shim-Df5Mok-W.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-CT_b8DTk.js";import{_ as t}from"./iframe-CUmZym78.js";var n=e((()=>{t()}));export{n as t};
