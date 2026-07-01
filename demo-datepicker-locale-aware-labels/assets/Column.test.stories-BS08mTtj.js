5c29a340ae8bd5029992aa35962a1f394aafed58
GitHub <github-action@users.noreply.github.com>
2026-07-01 12:51:43 +0000

    Remove feature branch folder

--- demo-datepicker-locale-aware-labels/assets/Column.test.stories-BS08mTtj.js
+++
@@ -1,7 +0,0 @@
-import{i as e}from"./preload-helper-CT_b8DTk.js";import{n as t,t as n}from"./renderComponentVariants-BW8PDbgP.js";import{Bt as r,t as i}from"./src-DzGboehr.js";import a,{n as o}from"./Column.stories-D574bEDy.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Layout/Column`},c={render:(e,n)=>t(r,{args:e,layout:`grid`},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Column, {
-    args,
-    layout: 'grid'
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
