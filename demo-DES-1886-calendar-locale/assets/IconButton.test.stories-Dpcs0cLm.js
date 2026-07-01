942e1d614d6df59632c988ad4da370c713e71afb
GitHub <github-action@users.noreply.github.com>
2026-07-01 12:20:59 +0000

    Remove feature branch folder

--- demo-DES-1886-calendar-locale/assets/IconButton.test.stories-Dpcs0cLm.js
+++
@@ -1,7 +0,0 @@
-import{i as e}from"./preload-helper-CT_b8DTk.js";import{n as t,t as n}from"./renderComponentVariants-BKD7XKA_.js";import{t as r,wn as i}from"./src-Bt-mde30.js";import a,{n as o}from"./IconButton.stories-BqSNhmHy.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/Buttons/Icon Button`},c={render:(e,n)=>t(i,{args:e,variants:[`hovered`,`disabled`]},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(IconButton, {
-    args,
-    variants: ['hovered', 'disabled']
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
