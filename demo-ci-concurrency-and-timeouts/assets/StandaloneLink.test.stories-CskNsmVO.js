73f347f9060662c994d809f02a80f813fa1b9354
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-01 22:27:55 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@27c1735252a0a290fd564f75eadfc5793ebf5e0b 🚀

--- demo-ci-concurrency-and-timeouts/assets/StandaloneLink.test.stories-CskNsmVO.js
+++
@@ -1,6 +0,0 @@
-import{i as e}from"./preload-helper-CT_b8DTk.js";import{n as t,t as n}from"./renderComponentVariants-CeBHE67C.js";import{b as r,t as i}from"./src-zlZ-Ox4j.js";import a,{n as o}from"./StandaloneLink.stories-Cg6_rYu3.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/StandaloneLink`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(StandaloneLink, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
