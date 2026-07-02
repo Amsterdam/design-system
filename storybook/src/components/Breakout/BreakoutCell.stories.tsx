6e19149ebb63f77c3b6bfabc4d0b792200bb705e
Vincent Smedinga <v.smedinga@amsterdam.nl>
2026-07-02 10:09:03 +0200

    chore(docs): Demonstrate the span and start props in the Breakout Cell story

--- storybook/src/components/Breakout/BreakoutCell.stories.tsx
+++ storybook/src/components/Breakout/BreakoutCell.stories.tsx
@@ -8,6 +8,8 @@ import type { Meta, StoryObj } from '@storybook/react-vite'
 import { Paragraph } from '@amsterdam/design-system-react'
 import { Breakout } from '@amsterdam/design-system-react/src'
 
+import { GridColumnsGuide } from '#storybook/_components/GridColumnsGuide/GridColumnsGuide'
+
 const meta = {
   title: 'Components/Layout/Breakout',
   component: Breakout.Cell,
@@ -16,23 +18,54 @@ const meta = {
       control: { type: 'radio' },
       options: ['article', 'div', 'section'],
     },
+    colSpan: {
+      control: { max: 12, min: 1, type: 'number' },
+    },
+    colStart: {
+      control: { max: 12, min: 1, type: 'number' },
+    },
+    rowSpan: {
+      control: { max: 4, min: 1, type: 'number' },
+    },
+    rowStart: {
+      control: { max: 4, min: 1, type: 'number' },
+    },
   },
   decorators: [
     (Story) => (
-      <Breakout>
-        <Story />
-      </Breakout>
+      <>
+        <GridColumnsGuide />
+        <Breakout>
+          <Breakout.Cell className="_ams-item" colSpan={2} colStart={1} rowStart={1}>
+            <Paragraph>Cell 1</Paragraph>
+          </Breakout.Cell>
+          <Breakout.Cell className="_ams-item" colSpan={2} colStart={1} rowStart={2}>
+            <Paragraph>Cell 2</Paragraph>
+          </Breakout.Cell>
+          <Story />
+        </Breakout>
+      </>
     ),
   ],
 } satisfies Meta<typeof Breakout.Cell>
 
 export default meta
 
-type Story = StoryObj<typeof meta>
+// Typing stories against a minimal meta keeps the union of the span and start values intact.
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const cellMeta = {
+  component: Breakout.Cell,
+} satisfies Meta<typeof Breakout.Cell>
 
-export const Cell: Story = {
+type CellStory = StoryObj<typeof cellMeta>
+
+export const Cell: CellStory = {
   args: {
     children: <Paragraph>Cell content</Paragraph>,
-    colSpan: 'all',
+    className: '_ams-item--highlight',
+    colSpan: 2,
+    colStart: 3,
+    rowSpan: 2,
+    rowStart: 1,
   },
 }
