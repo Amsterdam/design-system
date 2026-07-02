1f99b5b2259b455fd7fc5b10fe5bb6d428bc8924
Vincent Smedinga <v.smedinga@amsterdam.nl>
2026-07-02 09:02:38 +0200

    chore(docs): Restore the Search Field Input and Button subcomponent sections

---
+++ storybook/src/components/SearchField/SearchFieldButton.stories.tsx
@@ -0,0 +1,32 @@
+/**
+ * @license EUPL-1.2+
+ * Copyright Gemeente Amsterdam
+ */
+
+import type { Meta, StoryObj } from '@storybook/react-vite'
+
+import { SearchField } from '@amsterdam/design-system-react/src'
+
+const meta = {
+  title: 'Components/Forms/Search Field',
+  component: SearchField.Button,
+  decorators: [
+    (Story) => (
+      <SearchField>
+        <SearchField.Input />
+        <Story />
+      </SearchField>
+    ),
+  ],
+  render: ({ children, ...args }) => <SearchField.Button {...args}>{children}</SearchField.Button>,
+} satisfies Meta<typeof SearchField.Button>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Button: Story = {
+  args: {
+    children: 'Zoeken',
+  },
+}
