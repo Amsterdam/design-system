780e04997b0092f8a9c6569093ed6236213dc611
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-02 07:53:40 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@8c9c43020aa201bfb86dac9db656bd3d719d7154 🚀

---
+++ demo-docs-subcomponent-stories/assets/Switch.test.stories-DAPsQMHB.js
@@ -0,0 +1,6 @@
+import{i as e}from"./preload-helper-CT_b8DTk.js";import{n as t,t as n}from"./renderComponentVariants-BoJ7jS3U.js";import{t as r,v as i}from"./src-CMPWQYAE.js";import a,{n as o}from"./Switch.stories-Bgycyshq.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/Forms/Switch`},c={render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Switch, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
--- demo-docs-subcomponent-stories/assets/mdx-react-shim-DhsqxXJH.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-CT_b8DTk.js";import{_ as t}from"./iframe-CxT9mrQK.js";var n=e((()=>{t()}));export{n as t};
