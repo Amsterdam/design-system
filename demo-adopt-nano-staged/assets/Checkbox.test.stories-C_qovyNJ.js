3d8788a25d21c20b1666de6a913e08af3f519f4e
GitHub <github-action@users.noreply.github.com>
2026-07-02 15:56:56 +0000

    Remove feature branch folder

--- demo-adopt-nano-staged/assets/Checkbox.test.stories-C_qovyNJ.js
+++
@@ -1,6 +0,0 @@
-import{i as e}from"./preload-helper-CT_b8DTk.js";import{n as t,t as n}from"./renderComponentVariants-CNrcIzrI.js";import{Jt as r,t as i}from"./src-si2NiSEX.js";import a,{n as o}from"./Checkbox.stories-h9jVIoOw.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Checkbox`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Checkbox, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
