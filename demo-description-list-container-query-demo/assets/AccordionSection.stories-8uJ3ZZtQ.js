eb4864ece8f49a6d4d41b0104221e747c75ef1d9
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-03 08:56:55 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@f61ae025126705171dee7d997776eb94c32ed68a 🚀

---
+++ demo-description-list-container-query-demo/assets/AccordionSection.stories-8uJ3ZZtQ.js
@@ -0,0 +1,6 @@
+import{i as e}from"./preload-helper-CT_b8DTk.js";import{t}from"./jsx-runtime-BCDDjCIb.js";import{F as n,Y as r}from"./index.esm-CdKwZNWS.js";import{Ln as i,t as a}from"./src-DCJX5pAk.js";import{l as o,m as s,n as c}from"./exampleContent-CFoxZuYq.js";var l,u,d,f,p=e((()=>{r(),a(),s(),l=t(),u={title:`Components/Containers/Accordion`,component:i.Section,decorators:[e=>(0,l.jsx)(i,{headingLevel:3,children:(0,l.jsx)(e,{})})]},d={args:{children:(0,l.jsx)(n,{children:o()}),label:c()}},d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: <Paragraph>{exampleParagraph()}</Paragraph>,
+    label: exampleAccordionHeading()
+  }
+}`,...d.parameters?.docs?.source}}},f=[`Section`]}));p();export{d as Section,f as __namedExportsOrder,u as default,p as t};
---
+++ demo-description-list-container-query-demo/assets/Label.test.stories-C5NOb-Da.js
@@ -0,0 +1,6 @@
+import{i as e}from"./preload-helper-CT_b8DTk.js";import{n as t,t as n}from"./renderComponentVariants-DCxZY4ME.js";import{ct as r,t as i}from"./src-DCJX5pAk.js";import a,{n as o}from"./Label.stories-Dzym4V_o.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Label`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Label, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
