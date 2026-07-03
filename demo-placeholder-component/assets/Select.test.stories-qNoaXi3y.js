11695ad0b06d19f2d77acae845fa33ab476826d3
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-03 17:38:19 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@db35d950b9e5c7acf0a1bd643100da8e8bedf77c 🚀

--- demo-placeholder-component/assets/Select.test.stories-qNoaXi3y.js
+++
@@ -1,7 +0,0 @@
-import{i as e}from"./preload-helper-CT_b8DTk.js";import{n as t,t as n}from"./renderComponentVariants-DrADh835.js";import{j as r,t as i}from"./src-WuILwX2F.js";import a,{n as o}from"./Select.stories-Dl8mDr0z.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Select`},c={render:(e,n)=>t(r,{args:e,variants:[`hovered`,`disabled`]},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Select, {
-    args,
-    variants: ['hovered', 'disabled']
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
