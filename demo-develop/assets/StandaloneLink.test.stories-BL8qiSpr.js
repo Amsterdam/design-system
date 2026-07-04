a494b3322ba3c32f2f98d347e170abf8e0f35fa2
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-04 16:46:48 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@7d32be245494b9b7307d376bc1257b8bd2040f61 🚀

--- demo-develop/assets/StandaloneLink.test.stories-BL8qiSpr.js
+++
@@ -1,6 +0,0 @@
-import{i as e}from"./preload-helper-CT_b8DTk.js";import{n as t,t as n}from"./renderComponentVariants-rYepU_M6.js";import{b as r,t as i}from"./src-DHhdY-kV.js";import a,{n as o}from"./StandaloneLink.stories-C7xzLmlL.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Navigation/Standalone Link`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(StandaloneLink, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
--- demo-develop/assets/mdx-react-shim-D4rD5qCy.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-CT_b8DTk.js";import{_ as t}from"./iframe-VWO6qfZF.js";var n=e((()=>{t()}));export{n as t};
