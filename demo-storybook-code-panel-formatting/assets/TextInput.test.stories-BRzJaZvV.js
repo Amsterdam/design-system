bfb149f8128a9e68a42b278905644df770d13714
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-04 14:10:50 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@af94a937d428af6aadd8ca2ab7b09d392df94470 🚀

---
+++ demo-storybook-code-panel-formatting/assets/TextInput.test.stories-BRzJaZvV.js
@@ -0,0 +1,7 @@
+import{i as e}from"./preload-helper-CT_b8DTk.js";import{n as t,t as n}from"./renderComponentVariants-Dio4rrki.js";import{o as r,t as i}from"./src-C84TkUEv.js";import a,{n as o}from"./TextInput.stories-BPrg-1Uv.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Text Input`},c={render:(e,n)=>t(r,{args:e,variants:[`disabled`,`hovered`]},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(TextInput, {
+    args,
+    variants: ['disabled', 'hovered']
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
