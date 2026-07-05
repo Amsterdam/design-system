7548856f6a037f71d9909c2a8850fb28c3b13577
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-05 23:06:47 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@a8dc2dd3d46030e55a364e725bdb2495677b6590 🚀

---
+++ demo-page-header-controlled-open/assets/Page.test.stories-bCjPCZPc.js
@@ -0,0 +1,6 @@
+import{i as e}from"./preload-helper-CT_b8DTk.js";import{n as t,t as n}from"./renderComponentVariants-Bte4f9M-.js";import{q as r,t as i}from"./src-B7BfXTkk.js";import a,{n as o}from"./Page.stories-DF1KQl1y.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Containers/Page`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Page, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
