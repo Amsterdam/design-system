4e4208ace6647aca4a4f7e1233e25aec3ff0d460
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-06 19:37:26 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@95d8c632d7fcba80c3ac4c506bafd4afad9136b7 🚀

---
+++ demo-lint-yaml/assets/Badge.test.stories-Ctyk0rsE.js
@@ -0,0 +1,6 @@
+import{i as e}from"./preload-helper-CT_b8DTk.js";import{n as t,t as n}from"./renderComponentVariants-GZE7xLRt.js";import{t as r,xn as i}from"./src-BLp0kk3v.js";import a,{n as o}from"./Badge.stories-DRi5v6lx.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/Feedback/Badge`},c={render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Badge, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
