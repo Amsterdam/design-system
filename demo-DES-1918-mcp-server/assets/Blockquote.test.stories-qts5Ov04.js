2b9a0a7cf95d754b101ae23c542eb536cece6082
GitHub <github-action@users.noreply.github.com>
2026-07-06 21:36:25 +0000

    Remove feature branch folder

--- demo-DES-1918-mcp-server/assets/Blockquote.test.stories-qts5Ov04.js
+++
@@ -1,9 +0,0 @@
-import{i as e}from"./preload-helper-CT_b8DTk.js";import{n as t,t as n}from"./renderComponentVariants-BWa31dBx.js";import{t as r,yn as i}from"./src-CfmjGo7J.js";import a,{n as o}from"./Blockquote.stories-B8N7DenN.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/Text/Blockquote`},c={args:{children:`Voor Amsterdam was de Jordaan één oproer en één fort. De Amsterdammer begreep de Jordaner niet, die zelden uit zijn buurt komt, soms niet uit zijn straat, wiens stad haar eigen plattegrond heeft, een stad in een stad.`},render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: 'Voor Amsterdam was de Jordaan één oproer en één fort. De Amsterdammer begreep de Jordaner niet, die zelden uit zijn buurt komt, soms niet uit zijn straat, wiens stad haar eigen plattegrond heeft, een stad in een stad.'
-  },
-  render: (args, context) => renderComponentVariants(Blockquote, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
--- demo-DES-1918-mcp-server/assets/TimeInput.test.stories-CCEfNGT_.js
+++
@@ -1,7 +0,0 @@
-import{i as e}from"./preload-helper-CT_b8DTk.js";import{n as t,t as n}from"./renderComponentVariants-BWa31dBx.js";import{i as r,t as i}from"./src-CfmjGo7J.js";import a,{n as o}from"./TimeInput.stories-Ga6X11W1.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Time Input`},c={render:(e,n)=>t(r,{args:e,variants:[`disabled`,`hovered`]},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(TimeInput, {
-    args,
-    variants: ['disabled', 'hovered']
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
