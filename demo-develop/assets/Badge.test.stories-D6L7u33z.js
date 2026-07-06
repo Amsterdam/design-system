920073031f7378ac71b464b279f3e4fc5a649dd1
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-06 09:01:59 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@74ddca001d6be5e16f25b8d4da05fee1aea07938 🚀

--- demo-develop/assets/Badge.test.stories-D6L7u33z.js
+++
@@ -1,6 +0,0 @@
-import{i as e}from"./preload-helper-CT_b8DTk.js";import{n as t,t as n}from"./renderComponentVariants-hcaqO3wv.js";import{t as r,xn as i}from"./src-CEQHaBJ_.js";import a,{n as o}from"./Badge.stories-MRgtNaq5.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/Feedback/Badge`},c={render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Badge, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
--- demo-develop/assets/IconButton.test.stories-DU5f9CsV.js
+++
@@ -1,7 +0,0 @@
-import{i as e}from"./preload-helper-CT_b8DTk.js";import{n as t,t as n}from"./renderComponentVariants-hcaqO3wv.js";import{Nn as r,t as i}from"./src-CEQHaBJ_.js";import a,{n as o}from"./IconButton.stories-yJrfQUOW.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Buttons/Icon Button`},c={render:(e,n)=>t(r,{args:e,variants:[`hovered`,`disabled`]},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(IconButton, {
-    args,
-    variants: ['hovered', 'disabled']
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
