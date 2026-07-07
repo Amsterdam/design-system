8175122b7d959de9e3360fea153599175cef98c5
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-07 15:18:57 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@e1de031dbe6d881968e0bdb6ce46e830677ad05c 🚀

--- demo-page-template-code-comments/assets/Avatar.test.stories-CccKTqvm.js
+++
@@ -1,6 +0,0 @@
-import{i as e}from"./preload-helper-CT_b8DTk.js";import{n as t,t as n}from"./renderComponentVariants-bAJl3DN0.js";import{t as r,wn as i}from"./src-SbV8Mbp4.js";import a,{n as o}from"./Avatar.stories-C8acM_kC.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/Feedback/Avatar`},c={render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Avatar, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
--- demo-page-template-code-comments/assets/IconButton.test.stories-BHV8YhDZ.js
+++
@@ -1,7 +0,0 @@
-import{i as e}from"./preload-helper-CT_b8DTk.js";import{n as t,t as n}from"./renderComponentVariants-bAJl3DN0.js";import{Nn as r,t as i}from"./src-SbV8Mbp4.js";import a,{n as o}from"./IconButton.stories-BSDhH8BQ.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Buttons/Icon Button`},c={render:(e,n)=>t(r,{args:e,variants:[`hovered`,`disabled`]},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(IconButton, {
-    args,
-    variants: ['hovered', 'disabled']
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
