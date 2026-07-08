e7ab5e307ba3aefef21fe7016f494e546affcb5a
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-08 11:15:04 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@1f54650027f32b667b17cda08fc15e1ce973e348 🚀

---
+++ demo-use-jsx-boolean-shorthand/assets/SkipLink.test.stories-C4HXPr8U.js
@@ -0,0 +1,6 @@
+import{i as e}from"./preload-helper-CT_b8DTk.js";import{n as t,t as n}from"./renderComponentVariants-C1fq4yCV.js";import{E as r,t as i}from"./src-C74Gr679.js";import a,{n as o}from"./SkipLink.stories-F9jcuA7m.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Navigation/Skip Link`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(SkipLink, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
