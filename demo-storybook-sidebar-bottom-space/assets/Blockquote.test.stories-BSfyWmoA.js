a7e7e4e67a62c9a67288f189cf289ea014210598
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-09 17:16:28 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@35263a48ea0bdff7b4eb2284ee2ffe126d56750f 🚀

--- demo-storybook-sidebar-bottom-space/assets/Blockquote.test.stories-BSfyWmoA.js
+++
@@ -1,9 +0,0 @@
-import{i as e}from"./preload-helper-CT_b8DTk.js";import{n as t,t as n}from"./renderComponentVariants-_XahaJcq.js";import{t as r,xn as i}from"./src-XYD-I-Mr.js";import a,{n as o}from"./Blockquote.stories-DWw3-Tjy.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/Text/Blockquote`},c={args:{children:`Voor Amsterdam was de Jordaan één oproer en één fort. De Amsterdammer begreep de Jordaner niet, die zelden uit zijn buurt komt, soms niet uit zijn straat, wiens stad haar eigen plattegrond heeft, een stad in een stad.`},render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: 'Voor Amsterdam was de Jordaan één oproer en één fort. De Amsterdammer begreep de Jordaner niet, die zelden uit zijn buurt komt, soms niet uit zijn straat, wiens stad haar eigen plattegrond heeft, een stad in een stad.'
-  },
-  render: (args, context) => renderComponentVariants(Blockquote, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
