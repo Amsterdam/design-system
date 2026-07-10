80aa5001d61f2f37125906a5fdb5251f5787dab5
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-10 15:16:22 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@39ca9a154040dd5a4d798492e6ec481fa120d757 🚀

---
+++ demo-develop/assets/Checkbox.test.stories-DqLyPJZa.js
@@ -0,0 +1,6 @@
+import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-Bs9lceE-.js";import{Xt as r,t as i}from"./src-Bx70WjLh.js";import a,{n as o}from"./Checkbox.stories-DyupukXk.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Checkbox`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Checkbox, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
