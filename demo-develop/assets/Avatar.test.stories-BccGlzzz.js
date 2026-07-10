ad7a593222e45172c27712254aa8f36cea86f377
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-10 16:36:55 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@a88d505d0a61b876b8b9b1fa2456a51ae7251c1d 🚀

--- demo-develop/assets/Avatar.test.stories-BccGlzzz.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-CXSMeytW.js";import{En as r,t as i}from"./src-5lrQhDc2.js";import a,{n as o}from"./Avatar.stories-DlZAH3vB.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Feedback/Avatar`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Avatar, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
---
+++ demo-develop/assets/Row.test.stories-CZDon4rq.js
@@ -0,0 +1,9 @@
+import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{t}from"./jsx-runtime-Bq1bXGty.js";import{n,t as r}from"./renderComponentVariants-Db4TvzfH.js";import{jn as i,t as a}from"./src-CIkgooIz.js";import o,{n as s}from"./Row.stories-y8jukxvE.js";var c,l,u,d;e((()=>{a(),r(),s(),c=t(),l={...o,title:`Components/Layout/Row`},u={args:{children:[(0,c.jsx)(`div`,{className:`_ams-item`},1),(0,c.jsx)(`div`,{className:`_ams-item`},2),(0,c.jsx)(`div`,{className:`_ams-item`},3)]},render:(e,t)=>n(i,{args:e},t),tags:[`!dev`,`!autodocs`]},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<div className="_ams-item" key={1} />, <div className="_ams-item" key={2} />, <div className="_ams-item" key={3} />]
+  },
+  render: (args, context) => renderComponentVariants(Row, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...u.parameters?.docs?.source}}},d=[`Test`]}))();export{u as Test,d as __namedExportsOrder,l as default};
