b56a1af58bbd8e934720eeac2940a8ad4bbde0e3
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-10 15:11:39 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@083bb337e8f2d009ca77216d14c3b3aeec58d53b 🚀

---
+++ demo-menu-box-sizing/assets/Page.test.stories-CLSM2SsR.js
@@ -0,0 +1,6 @@
+import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-3_mfYKa-.js";import{Y as r,t as i}from"./src-eHC0zavr.js";import a,{n as o}from"./Page.stories-B5kFFztL.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Containers/Page`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Page, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
