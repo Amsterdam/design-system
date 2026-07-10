27a403edc8dbad906d43d437585e8ced8a11f8fe
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-10 15:01:26 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@0136ed520f67987616f72d8ee9ba0e72f305d007 🚀

---
+++ demo-grid-docs/assets/CardHeading.stories-J3OTZnic.js
@@ -0,0 +1,6 @@
+import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{t}from"./jsx-runtime-Bq1bXGty.js";import{en as n,t as r}from"./src-DLfwfvnM.js";import{c as i,d as a,f as o}from"./argTypes-CQXDxDHP.js";var s,c,l,u,d=e((()=>{r(),a(),s=t(),c={title:`Components/Navigation/Card`,component:n.Heading,argTypes:{color:o,level:i(),size:{control:{labels:{undefined:`level-3 (default)`},type:`select`},options:[void 0,`level-1`,`level-2`,`level-4`,`level-5`]}},decorators:[e=>(0,s.jsx)(n,{children:(0,s.jsx)(e,{})})]},l={args:{children:(0,s.jsx)(n.Link,{href:`#`,children:`Meer plekken voor kunst en cultuur, verspreid over de stad`}),level:3}},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: <Card.Link href="#">Meer plekken voor kunst en cultuur, verspreid over de stad</Card.Link>,
+    level: 3
+  }
+}`,...l.parameters?.docs?.source}}},u=[`Heading`]}));d();export{l as Heading,u as __namedExportsOrder,c as default,d as t};
