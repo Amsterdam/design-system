07f6ddbbd719a396dff48db8c6880ca4359091e1
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-10 20:42:27 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@788ad6dd140603af36e2328ebcf3afd0c1eb2c4b 🚀

---
+++ demo-develop/assets/Column.test.stories-CaZ57Ep0.js
@@ -0,0 +1,7 @@
+import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-DZAEboaf.js";import{Kt as r,t as i}from"./src-3o058qxy.js";import a,{n as o}from"./Column.stories-mHRNyg0h.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Layout/Column`},c={render:(e,n)=>t(r,{args:e,layout:`grid`},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Column, {
+    args,
+    layout: 'grid'
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
---
+++ demo-develop/assets/Link.test.stories-DOcmp0eF.js
@@ -0,0 +1,6 @@
+import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-DZAEboaf.js";import{ct as r,t as i}from"./src-3o058qxy.js";import a,{n as o}from"./Link.stories-BB5msEbV.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Navigation/Link`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Link, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
