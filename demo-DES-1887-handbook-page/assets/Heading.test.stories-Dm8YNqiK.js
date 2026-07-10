60f345a1e284c0c858aded16f1f948dc61816972
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-10 06:51:51 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@3379369d91d6d38e07f7e9bc1822086af94de76a 🚀

--- demo-DES-1887-handbook-page/assets/Heading.test.stories-Dm8YNqiK.js
+++
@@ -1,9 +0,0 @@
-import{i as e}from"./preload-helper-CT_b8DTk.js";import{n as t,t as n}from"./renderComponentVariants-CYfPq-uN.js";import{Wn as r,t as i}from"./src-C1LTgmar.js";import a,{n as o}from"./Heading.stories-DNN0Vz-4.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Text/Heading`},c={args:{children:`Amsterdam Light Festival maakt de stad ook deze winter een beetje mooier`},render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: 'Amsterdam Light Festival maakt de stad ook deze winter een beetje mooier'
-  },
-  render: (args, context) => renderComponentVariants(Heading, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
