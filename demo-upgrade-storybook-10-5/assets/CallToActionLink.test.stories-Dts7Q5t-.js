628f1d5c92444573bdca9e0b3715f0a2d7920e43
GitHub <github-action@users.noreply.github.com>
2026-07-11 16:25:13 +0000

    Remove feature branch folder

--- demo-upgrade-storybook-10-5/assets/CallToActionLink.test.stories-Dts7Q5t-.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-xUQr0iF8.js";import{cn as r,t as i}from"./src-BX64cFDM.js";import a,{n as o}from"./CallToActionLink.stories-DSug0Rch.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Navigation/Call to Action Link`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(CallToActionLink, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
