d53aa94e4165d97e24eae2e5467b5dd92678d497
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-12 08:50:20 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@6f073675458adeb6e7d3d50fb0b8cfa72bcc5c7f 🚀

--- demo-develop/assets/SearchFieldButton.stories-6WJbvHgg.js
+++
@@ -1,5 +0,0 @@
-import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{t}from"./jsx-runtime-Bq1bXGty.js";import{M as n,t as r}from"./src-CuAYwTdi.js";import{d as i,r as a}from"./argTypes-CQXDxDHP.js";var o,s,c,l,u=e((()=>{r(),i(),o=t(),s={title:`Components/Forms/Search Field`,component:n.Button,argTypes:{children:a(`The accessible name of the button.`)},decorators:[e=>(0,o.jsxs)(n,{children:[(0,o.jsx)(n.Input,{}),(0,o.jsx)(e,{})]})],render:({children:e,...t})=>(0,o.jsx)(n.Button,{...t,children:e})},c={args:{children:`Zoeken`}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: 'Zoeken'
-  }
-}`,...c.parameters?.docs?.source}}},l=[`Button`]}));u();export{c as Button,l as __namedExportsOrder,s as default,u as t};
