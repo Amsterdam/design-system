b88578f4d70db8e4ea9e48f4757ac8b794fbd81c
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-12 09:54:14 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@6f073675458adeb6e7d3d50fb0b8cfa72bcc5c7f 🚀

---
+++ assets/Badge.stories-ZDOWXHCe.js
@@ -0,0 +1,7 @@
+import{n as e,r as t}from"./rolldown-runtime-DaJ6WEGw.js";import{G as n,w as r}from"./index.esm-AHCPMkhr.js";import{Cn as i,Tn as a,t as o,wn as s}from"./src-BTJENctl.js";import{d as c,i as l}from"./argTypes-CQXDxDHP.js";import{n as u,t as d}from"./iconArgTypes-BcBExhD3.js";var f=t({Default:()=>m,WithIcon:()=>h,__namedExportsOrder:()=>g,default:()=>p}),p,m,h,g,_=e((()=>{n(),o(),a(),c(),u(),p={title:`Components/Feedback/Badge`,component:i,args:{label:`Tip`},argTypes:{color:l(s,`green`),icon:d()}},m={},h={args:{color:`red`,icon:r,label:`Beveiligd`}},m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`{}`,...m.parameters?.docs?.source}}},h.parameters={...h.parameters,docs:{...h.parameters?.docs,source:{originalSource:`{
+  args: {
+    color: 'red',
+    icon: Icons.LockClosedFillIcon,
+    label: 'Beveiligd'
+  }
+}`,...h.parameters?.docs?.source}}},g=[`Default`,`WithIcon`]}));_();export{m as Default,h as WithIcon,g as __namedExportsOrder,p as default,_ as n,f as t};
---
+++ assets/IconButton.test.stories-Dda_rAOj.js
@@ -0,0 +1,7 @@
+import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-CS__ajXT.js";import{Fn as r,t as i}from"./src-BTJENctl.js";import a,{n as o}from"./IconButton.stories-CPae2_JO.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Buttons/Icon Button`},c={render:(e,n)=>t(r,{args:e,variants:[`hovered`,`disabled`]},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(IconButton, {
+    args,
+    variants: ['hovered', 'disabled']
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
