55d708e15797c415c7ed42630315fe9dbee297c5
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-12 08:40:09 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@1d1d8f1852c7af23a364c1ef452c08187c71ae26 🚀

---
+++ demo-require-default-icon/assets/BreadcrumbLink.stories-DUh2gQnH.js
@@ -0,0 +1,6 @@
+import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{t}from"./jsx-runtime-Bq1bXGty.js";import{t as n,yn as r}from"./src-Dc9-rIyF.js";import{d as i,l as a,p as o,r as s}from"./argTypes-CQXDxDHP.js";var c,l,u,d,f=e((()=>{n(),i(),c=t(),l={title:`Components/Navigation/Breadcrumb`,component:r.Link,argTypes:{children:s(`The link text.`),href:a,linkComponent:o},decorators:[e=>(0,c.jsx)(r,{children:(0,c.jsx)(e,{})})]},u={args:{children:`Home`,href:`#`}},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Home',
+    href: '#'
+  }
+}`,...u.parameters?.docs?.source}}},d=[`Link`]}));f();export{u as Link,d as __namedExportsOrder,l as default,f as t};
--- demo-require-default-icon/assets/StandaloneLink.test.stories-CnQNqAr0.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-CdWjNO50.js";import{b as r,t as i}from"./src-DlnQX7AS.js";import a,{n as o}from"./StandaloneLink.stories-BICL1ykO.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Navigation/Standalone Link`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(StandaloneLink, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
