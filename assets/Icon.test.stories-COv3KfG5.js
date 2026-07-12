9f80159bb4ca5568d56e6dac3f1e62a72b984440
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-12 10:18:42 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@363848c20e6a40865a1d77dba92b7578015d2ede 🚀

---
+++ assets/Icon.test.stories-COv3KfG5.js
@@ -0,0 +1,17 @@
+import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{t}from"./jsx-runtime-Bq1bXGty.js";import{n,t as r}from"./renderComponentVariants-C_uK7mu0.js";import{Vn as i,t as a}from"./src-MAqyczlQ.js";import{n as o,t as s}from"./src-iuRBq9Ge.js";import c,{n as l}from"./Icon.stories-D2THtydG.js";var u,d,f,p,m,h;e((()=>{s(),a(),r(),l(),u=t(),d={...c,title:`Components/Media/Icon`},f=Object.keys(o).sort((e,t)=>e.localeCompare(t)),p=f.filter(e=>o[e]({}).props[`data-directional`]===`true`),m={render:(e,t)=>(0,u.jsxs)(`div`,{className:`_ams-tests-stack`,children:[(0,u.jsx)(`p`,{children:`Variants of Icon component`}),n(i,{args:e},t),(0,u.jsx)(`p`,{children:`All icons`}),(0,u.jsx)(`div`,{className:`_ams-tests-grid`,children:f.map(e=>(0,u.jsx)(i,{size:`heading-2`,svg:o[e]},e))}),(0,u.jsx)(`p`,{children:`Directional icons in RTL`}),(0,u.jsx)(`div`,{className:`_ams-tests-grid`,dir:`rtl`,children:p.map(e=>(0,u.jsx)(i,{size:`heading-2`,svg:o[e]},e))})]}),tags:[`!dev`,`!autodocs`]},m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => <div className="_ams-tests-stack">
+      <p>Variants of Icon component</p>
+      {renderComponentVariants(Icon, {
+      args
+    }, context)}
+      <p>All icons</p>
+      <div className="_ams-tests-grid">
+        {iconNames.map(name => <Icon key={name} size="heading-2" svg={Icons[name]} />)}
+      </div>
+      <p>Directional icons in RTL</p>
+      <div className="_ams-tests-grid" dir="rtl">
+        {directionalIconNames.map(name => <Icon key={name} size="heading-2" svg={Icons[name]} />)}
+      </div>
+    </div>,
+  tags: ['!dev', '!autodocs']
+}`,...m.parameters?.docs?.source}}},h=[`Test`]}))();export{m as Test,h as __namedExportsOrder,d as default};
