98ffecdde0521dd98ec3c700e539cbafd4768775
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-12 20:18:32 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@166b795bb3eb46b86fcd95dfaba82b3cf7c996d8 🚀

---
+++ demo-develop/assets/Menu.test.stories-DUw34stR.js
@@ -0,0 +1,20 @@
+import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{t}from"./jsx-runtime-Bq1bXGty.js";import{t as n,tt as r}from"./src-CSAayq74.js";import{n as i,t as a}from"./PieChartFill-DeABNG5x.js";import o,{n as s}from"./Menu.stories-CQJFGAYU.js";var c,l,u,d;e((()=>{i(),n(),s(),c=t(),l={...o,title:`Components/Navigation/Menu`},u={render:e=>(0,c.jsxs)(r,{...e,children:[(0,c.jsx)(r.Link,{href:`#`,icon:(0,c.jsx)(a,{}),children:`Dashboard`}),(0,c.jsx)(r.Link,{href:`#`,icon:(0,c.jsx)(a,{}),children:`Projecten`}),(0,c.jsx)(r.Link,{href:`#`,icon:(0,c.jsx)(a,{}),children:`Rapportages`}),(0,c.jsx)(r.Link,{href:`#`,icon:(0,c.jsx)(a,{}),children:`Analyses`}),(0,c.jsx)(r.Link,{className:`hover`,href:`#`,icon:(0,c.jsx)(a,{}),children:`Instellingen`})]}),tags:[`!dev`,`!autodocs`]},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  render: args => <Menu {...args}>
+      <Menu.Link href="#" icon={<SvgPieChartFill />}>
+        Dashboard
+      </Menu.Link>
+      <Menu.Link href="#" icon={<SvgPieChartFill />}>
+        Projecten
+      </Menu.Link>
+      <Menu.Link href="#" icon={<SvgPieChartFill />}>
+        Rapportages
+      </Menu.Link>
+      <Menu.Link href="#" icon={<SvgPieChartFill />}>
+        Analyses
+      </Menu.Link>
+      <Menu.Link className="hover" href="#" icon={<SvgPieChartFill />}>
+        Instellingen
+      </Menu.Link>
+    </Menu>,
+  tags: ['!dev', '!autodocs']
+}`,...u.parameters?.docs?.source}}},d=[`Test`]}))();export{u as Test,d as __namedExportsOrder,l as default};
