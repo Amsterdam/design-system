09831c5c77daa5d13ac302dbb84e9a197fd72b41
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-12 18:34:59 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@3ec9fad9152e0bed68aeecfefa16ab8d3fa96895 🚀

---
+++ demo-relax-publish-step-guards/assets/Label.docs-BsqYprhY.js
@@ -0,0 +1,38 @@
+import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{a as t,d as n,h as r,i,n as a,p as o,u as s}from"./blocks-CuAf8lRO.js";import{t as c}from"./jsx-runtime-Bq1bXGty.js";import{i as l}from"./react-BnCtKaSV.js";import{n as u,t as d}from"./DesignTokensTable-COiadGD4.js";import{t as f}from"./mdx-react-shim-9RzcSRdq.js";import{InAFieldSet as p,Optional as m,WithHeading as h,WithHint as g,n as _,t as v}from"./Label.stories-D_Qqo2Pc.js";var y,b,x=e((()=>{y={label:{color:{$value:`{ams.color.text.default}`,$extensions:{"nl.amsterdam.type":`color`}},"font-family":{$value:`{ams.typography.font-family}`,$extensions:{"nl.amsterdam.type":`fontFamily`}},"font-size":{$value:`{ams.typography.heading.3.font-size}`,$extensions:{"nl.amsterdam.type":`fontSize`}},"font-weight":{$value:`{ams.typography.heading.font-weight}`,$extensions:{"nl.amsterdam.type":`fontWeight`}},"line-height":{$value:`{ams.typography.heading.3.line-height}`,$extensions:{"nl.amsterdam.subtype":`lineHeight`,"nl.amsterdam.type":`number`}},"text-wrap":{$value:`{ams.typography.heading.text-wrap}`,$extensions:{"nl.amsterdam.type":`textWrap`}},"in-fieldset":{"font-weight":{$value:`{ams.typography.body-text.font-weight}`,$extensions:{"nl.amsterdam.type":`fontWeight`}}}}},b={ams:y}}));function S(e){let r={a:`a`,code:`code`,h2:`h2`,h3:`h3`,li:`li`,p:`p`,ul:`ul`,...l(),...e.components};return(0,w.jsxs)(w.Fragment,{children:[`
+`,`
+`,`
+`,`
+`,(0,w.jsx)(s,{of:v}),`
+`,(0,w.jsx)(o,{}),`
+`,(0,w.jsx)(t,{of:v}),`
+`,(0,w.jsx)(n,{}),`
+`,(0,w.jsx)(i,{}),`
+`,(0,w.jsx)(r.h2,{id:`usage-guidelines`,children:`Usage guidelines`}),`
+`,(0,w.jsx)(r.h3,{id:`when-to-use`,children:`When to use`}),`
+`,(0,w.jsxs)(r.p,{children:[`Always associate a form element (such as an `,(0,w.jsx)(r.code,{children:`input`}),` or `,(0,w.jsx)(r.code,{children:`textarea`}),`) with a Label.`]}),`
+`,(0,w.jsx)(r.h3,{id:`how-to-use`,children:`How to use`}),`
+`,(0,w.jsxs)(r.p,{children:[`Set `,(0,w.jsx)(r.code,{children:`optional`}),` to `,(0,w.jsx)(r.code,{children:`true`}),` if the input is not required.
+Use `,(0,w.jsx)(r.code,{children:`hint`}),` to show a custom hint text.`]}),`
+`,(0,w.jsx)(r.h2,{id:`examples`,children:`Examples`}),`
+`,(0,w.jsx)(r.h3,{id:`optional`,children:`Optional`}),`
+`,(0,w.jsxs)(r.p,{children:[`Use the `,(0,w.jsx)(r.code,{children:`optional`}),` prop to indicate that the user doesn’t need to fill in the field to submit the form.`]}),`
+`,(0,w.jsx)(a,{of:m}),`
+`,(0,w.jsx)(r.h3,{id:`with-hint`,children:`With hint`}),`
+`,(0,w.jsxs)(r.p,{children:[`Use the `,(0,w.jsx)(r.code,{children:`hint`}),` prop to either customize the text for the optional property or provide another type of remark.`]}),`
+`,(0,w.jsxs)(r.p,{children:[`Note: ensure the hint meets `,(0,w.jsx)(r.a,{href:`https://www.amsterdam.nl/schrijfwijzer/heldere-taal-basis-onze-huisstijl/`,rel:`nofollow`,children:`language level B1`}),`.
+For the same reason, the default hint text in Dutch is ‘niet verplicht’, not ‘optioneel’.`]}),`
+`,(0,w.jsx)(a,{of:g}),`
+`,(0,w.jsx)(r.h3,{id:`with-heading`,children:`With Heading`}),`
+`,(0,w.jsxs)(r.p,{children:[`If the parent Field is the only content on the page, its Label serves as the main heading as well.
+Set `,(0,w.jsx)(r.code,{children:`isPageHeading`}),` to include an `,(0,w.jsx)(r.code,{children:`h1`}),` element to reflect this.`]}),`
+`,(0,w.jsx)(a,{of:h}),`
+`,(0,w.jsx)(r.h3,{id:`in-a-field-set`,children:`In a Field Set`}),`
+`,(0,w.jsxs)(r.p,{children:[`When a Label is used in a Field inside a Field Set, use the `,(0,w.jsx)(r.code,{children:`inFieldSet`}),` prop to show it in a lighter style.`]}),`
+`,(0,w.jsx)(a,{of:p}),`
+`,(0,w.jsx)(r.h2,{id:`see-also`,children:`See also`}),`
+`,(0,w.jsxs)(r.ul,{children:[`
+`,(0,w.jsxs)(r.li,{children:[(0,w.jsx)(r.a,{href:`/docs/components-forms-field--docs`,children:`Field`}),` – wraps a single input together with its Label.`]}),`
+`,(0,w.jsxs)(r.li,{children:[(0,w.jsx)(r.a,{href:`/docs/components-forms-field-set--docs`,children:`Field Set`}),` – uses a legend instead of a Label to name a group of inputs.`]}),`
+`]}),`
+`,(0,w.jsx)(r.h2,{id:`design-tokens`,children:`Design tokens`}),`
+`,(0,w.jsx)(d,{tokens:b})]})}function C(e={}){let{wrapper:t}={...l(),...e.components};return t?(0,w.jsx)(t,{...e,children:(0,w.jsx)(S,{...e})}):S(e)}var w;e((()=>{w=c(),f(),r(),u(),x(),_()}))();export{C as default};
