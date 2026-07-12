1effe360a31ea2ebc8fb2e195ca6dbc794759e3d
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-12 18:57:15 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@6bd83dc317ec7d9fa8aeedbcf236e9ad3e7e691b 🚀

--- demo-develop/assets/Link.test.stories-Bd5dk0wA.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-Cdbg73bp.js";import{ct as r,t as i}from"./src-WyLJlaAD.js";import a,{n as o}from"./Link.stories-X4-GD6NN.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Navigation/Link`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Link, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
