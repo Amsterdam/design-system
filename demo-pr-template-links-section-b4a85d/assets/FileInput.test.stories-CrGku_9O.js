142f096b604da41b878cf48f6352a40f6f3fc893
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-13 15:16:59 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@f26a6d72e8d2689d70d062c94c126f343dd5abde 🚀

---
+++ demo-pr-template-links-section-b4a85d/assets/FileInput.test.stories-CrGku_9O.js
@@ -0,0 +1,7 @@
+import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-1kHuO_jx.js";import{kt as r,t as i}from"./src-CZWZ_gFS.js";import a,{n as o}from"./FileInput.stories-BIO0IlZJ.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/File Input`},c={render:(e,n)=>t(r,{args:e,variants:[`disabled`,`multiple`]},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(FileInput, {
+    args,
+    variants: ['disabled', 'multiple']
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
---
+++ demo-pr-template-links-section-b4a85d/assets/Radio.test.stories-DNeGlW_O.js
@@ -0,0 +1,6 @@
+import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-1kHuO_jx.js";import{F as r,t as i}from"./src-CZWZ_gFS.js";import a,{n as o}from"./Radio.stories-BqT5HPBY.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Radio`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Radio, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
