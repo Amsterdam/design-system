be60af0c0d26697f854a68aaac04f0e4f3b3e519
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-13 15:14:14 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@d1f8a4528dcab9b6c3f486e547eace4122ca1f28 🚀

---
+++ demo-pr-template-links-section-b4a85d/assets/AccordionSection.stories-D2ZeuU4j.js
@@ -0,0 +1,6 @@
+import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{t}from"./jsx-runtime-Bq1bXGty.js";import{P as n,X as r}from"./index.esm-pk1WYyC9.js";import{Hn as i,t as a}from"./src-Bu9TSlPM.js";import{l as o,m as s,n as c}from"./exampleContent-Cni407gr.js";var l,u,d,f,p=e((()=>{r(),a(),s(),l=t(),u={title:`Components/Containers/Accordion`,component:i.Section,argTypes:{onToggle:{action:`toggled`}},decorators:[e=>(0,l.jsx)(i,{headingLevel:3,children:(0,l.jsx)(e,{})})]},d={args:{children:(0,l.jsx)(n,{children:o()}),label:c()}},d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: <Paragraph>{exampleParagraph()}</Paragraph>,
+    label: exampleAccordionHeading()
+  }
+}`,...d.parameters?.docs?.source}}},f=[`Section`]}));p();export{d as Section,f as __namedExportsOrder,u as default,p as t};
