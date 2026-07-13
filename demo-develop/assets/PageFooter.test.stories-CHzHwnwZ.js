d4b9db04be08de7f75e5bb21de7aee3a79326477
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-13 15:31:32 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@fe5ab480c78e3f30b0e3822badcd0e18ca0e7cf4 🚀

---
+++ demo-develop/assets/PageFooter.test.stories-CHzHwnwZ.js
@@ -0,0 +1,16 @@
+import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{t}from"./jsx-runtime-Bq1bXGty.js";import{P as n,X as r}from"./index.esm-BQesaFjl.js";import{n as i,t as a}from"./renderComponentVariants-Cta2hgfb.js";import{J as o,t as s}from"./src-CmL4SlsV.js";import{a as c,o as l}from"./PageFooter.stories-BUL6OTgc.js";var u,d,f,p;e((()=>{r(),s(),a(),c(),u=t(),d={...l,title:`Components/Containers/Page Footer`},f={args:{children:[(0,u.jsx)(o.Spotlight,{children:(0,u.jsx)(n,{color:`inverse`,children:`Adressen en openingstijden`})},1),(0,u.jsxs)(o.Menu,{children:[(0,u.jsx)(o.MenuLink,{href:`#`,children:`Over deze site`}),(0,u.jsx)(o.MenuLink,{href:`#`,children:`Privacy`}),(0,u.jsx)(o.MenuLink,{href:`#`,children:`Cookies op deze site`}),(0,u.jsx)(o.MenuLink,{href:`#`,children:`Webarchief`})]},2)]},render:(e,t)=>i(o,{args:e},t),tags:[`!dev`,`!autodocs`]},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<PageFooter.Spotlight key={1}>
+        <Paragraph color="inverse">Adressen en openingstijden</Paragraph>
+      </PageFooter.Spotlight>, <PageFooter.Menu key={2}>
+        <PageFooter.MenuLink href="#">Over deze site</PageFooter.MenuLink>
+        <PageFooter.MenuLink href="#">Privacy</PageFooter.MenuLink>
+        <PageFooter.MenuLink href="#">Cookies op deze site</PageFooter.MenuLink>
+        <PageFooter.MenuLink href="#">Webarchief</PageFooter.MenuLink>
+      </PageFooter.Menu>]
+  },
+  render: (args, context) => renderComponentVariants(PageFooter, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...f.parameters?.docs?.source}}},p=[`Test`]}))();export{f as Test,p as __namedExportsOrder,d as default};
---
+++ demo-develop/assets/Pagination.test.stories-C_Lxd2ef.js
@@ -0,0 +1,6 @@
+import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-Cta2hgfb.js";import{H as r,t as i}from"./src-CmL4SlsV.js";import a,{n as o}from"./Pagination.stories-COph9iBA.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Navigation/Pagination`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Pagination, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
