42d999890145ba786fcfc8b47ed0024587b276a6
GitHub <github-action@users.noreply.github.com>
2026-07-13 23:35:06 +0000

    Remove feature branch folder

--- demo-compact-transparent-cells/assets/TableOfContentsLink.stories-DrHVzVc8.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{t}from"./jsx-runtime-Bq1bXGty.js";import{h as n,t as r}from"./src-CkqeymYr.js";import{d as i,l as a,p as o}from"./argTypes-CQXDxDHP.js";var s,c,l,u,d=e((()=>{r(),i(),s=t(),c={title:`Components/Navigation/Table of Contents`,component:n.Link,argTypes:{href:a,linkComponent:o},decorators:[e=>(0,s.jsx)(n,{heading:`Op deze pagina`,children:(0,s.jsx)(n.List,{children:(0,s.jsx)(e,{})})})],render:({...e})=>(0,s.jsx)(n.Link,{...e})},l={args:{href:`#wanneer-toepassen`,label:`Wanneer toepassen`}},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
-  args: {
-    href: '#wanneer-toepassen',
-    label: 'Wanneer toepassen'
-  }
-}`,...l.parameters?.docs?.source}}},u=[`Link`]}));d();export{l as Link,u as __namedExportsOrder,c as default,d as t};
