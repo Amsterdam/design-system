c9d84e1e6965553f1024dd1ee96ca7d8b1de595f
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-15 12:02:05 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@15bb190b22f878575a5e09afdf7a1c306580704c 🚀

---
+++ demo-writing-direction-support/assets/Field.test.stories-CJ_N47lN.js
@@ -0,0 +1,13 @@
+import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{t}from"./jsx-runtime-Bq1bXGty.js";import{N as n,dt as r}from"./index.esm-BNvuaS8P.js";import{n as i,t as a}from"./renderComponentVariants-ChVX0Gjj.js";import{Ft as o,t as s}from"./src-BQl4lYG-.js";import c,{n as l}from"./Field.stories-BFGvlwym.js";var u,d,f,p;e((()=>{r(),s(),a(),l(),u=t(),d={...c,title:`Components/Forms/Field`},f={args:{children:[(0,u.jsx)(n,{htmlFor:`input-a1`,children:`Wat is uw achternaam?`},1),(0,u.jsx)(`input`,{id:`input-a1`,value:`Dijkstra`},2)]},render:(e,t)=>i(o,{args:e},t),tags:[`!dev`,`!autodocs`]},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [
+    // We use Label because \`<label>\` is inline and wouldn’t get the margin we want to test.
+    <Label htmlFor="input-a1" key={1}>
+        Wat is uw achternaam?
+      </Label>, <input id="input-a1" key={2} value="Dijkstra" />]
+  },
+  render: (args, context) => renderComponentVariants(Field, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...f.parameters?.docs?.source}}},p=[`Test`]}))();export{f as Test,p as __namedExportsOrder,d as default};
