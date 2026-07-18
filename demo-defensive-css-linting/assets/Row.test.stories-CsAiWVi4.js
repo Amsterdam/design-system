8fb0d7a7da5c0db5a838fcd2e2a41e50dbdb034e
GitHub <github-action@users.noreply.github.com>
2026-07-18 12:57:55 +0000

    Remove feature branch folder

--- demo-defensive-css-linting/assets/Row.test.stories-CsAiWVi4.js
+++
@@ -1,9 +0,0 @@
-import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{t}from"./jsx-runtime-Bq1bXGty.js";import{n,t as r}from"./renderComponentVariants-2X9Tzb0Y.js";import{Pn as i,t as a}from"./src-L7EIZJsK.js";import o,{n as s}from"./Row.stories-D_4KTu-3.js";var c,l,u,d;e((()=>{a(),r(),s(),c=t(),l={...o,title:`Components/Layout/Row`},u={args:{children:[(0,c.jsx)(`div`,{className:`_ams-item`},1),(0,c.jsx)(`div`,{className:`_ams-item`},2),(0,c.jsx)(`div`,{className:`_ams-item`},3)]},render:(e,t)=>n(i,{args:e},t),tags:[`!dev`,`!autodocs`]},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: [<div className="_ams-item" key={1} />, <div className="_ams-item" key={2} />, <div className="_ams-item" key={3} />]
-  },
-  render: (args, context) => renderComponentVariants(Row, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...u.parameters?.docs?.source}}},d=[`Test`]}))();export{u as Test,d as __namedExportsOrder,l as default};
