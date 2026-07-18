ff05c2c1e5f6fb360f9e6d91c516c5340d36ff0e
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-18 08:50:49 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@c6c1cc7badf17cbc8702de8a808487ae5ed44f6b 🚀

--- demo-develop/assets/PageFooterMenuLink.stories-BbODeve1.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{t}from"./jsx-runtime-Bq1bXGty.js";import{J as n,t as r}from"./src-0AnY3V2d.js";import{d as i,l as a,p as o,r as s}from"./argTypes-CQXDxDHP.js";var c,l,u,d,f=e((()=>{r(),i(),c=t(),l={title:`Components/Containers/Page Footer`,component:n.MenuLink,argTypes:{children:s(`The link text.`),href:a,linkComponent:o},decorators:[e=>(0,c.jsx)(n,{children:(0,c.jsx)(n.Menu,{children:(0,c.jsx)(e,{})})})]},u={args:{children:`Over deze site`,href:`#`}},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: 'Over deze site',
-    href: '#'
-  }
-}`,...u.parameters?.docs?.source}}},d=[`MenuLink`]}));f();export{u as MenuLink,d as __namedExportsOrder,l as default,f as t};
