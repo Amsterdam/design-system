a18ce73a75d5a7e04d54634f564b3cc3daaffdf1
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-18 16:13:30 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@73f69287c07e20a1d03ed75ec40eaaaad0d34c5c 🚀

---
+++ demo-writing-direction-support/assets/CallToActionLink.test.stories-aYoBorng.js
@@ -0,0 +1,6 @@
+import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-C9b7BiNY.js";import{t as r,vn as i}from"./src-C7HPihk9.js";import a,{n as o}from"./CallToActionLink.stories-CAAzl7Np.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/Navigation/Call to Action Link`},c={render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(CallToActionLink, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
