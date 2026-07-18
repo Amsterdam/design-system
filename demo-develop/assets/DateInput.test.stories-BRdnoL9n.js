e6902d88b4d8c9afb9c0fe11ce35acc0a370f6a4
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-18 13:57:03 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@a12a6dd04798386f967129b18a312b1dfba4d485 🚀

---
+++ demo-develop/assets/DateInput.test.stories-BRdnoL9n.js
@@ -0,0 +1,6 @@
+import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-DIf_HGYV.js";import{Kt as r,t as i}from"./src-BVkLjE0V.js";import a,{n as o}from"./DateInput.stories-CLCHDK41.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Date Input`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(DateInput, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
--- demo-develop/assets/Pagination.test.stories-DKlGoFdK.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-D7d6Mr82.js";import{H as r,t as i}from"./src-C9ANGYkh.js";import a,{n as o}from"./Pagination.stories-CtB0yA7q.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Navigation/Pagination`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Pagination, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
