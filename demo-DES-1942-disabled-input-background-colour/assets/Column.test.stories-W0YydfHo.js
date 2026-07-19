487720eaa4d30fff809eeb02818be91829b7cf99
GitHub <github-action@users.noreply.github.com>
2026-07-19 23:31:43 +0000

    Remove feature branch folder

--- demo-DES-1942-disabled-input-background-colour/assets/Column.test.stories-W0YydfHo.js
+++
@@ -1,7 +0,0 @@
-import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-C4kq2v8j.js";import{Yt as r,t as i}from"./src-Cx809Dss.js";import a,{n as o}from"./Column.stories-BCEA94p2.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Layout/Column`},c={render:(e,n)=>t(r,{args:e,layout:`grid`},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Column, {
-    args,
-    layout: 'grid'
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
