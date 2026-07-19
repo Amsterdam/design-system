75eb0e692131651d484ea84c930d3a105c0fe9b1
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-19 11:06:17 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@6c841d0a85787e39b76a8f9ef8839fd20474f1b9 🚀

---
+++ demo-DES-1940-switch-disabled-track-colour/assets/Heading.test.stories-4GYdyW4A.js
@@ -0,0 +1,9 @@
+import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-_AM4PSi7.js";import{qn as r,t as i}from"./src-DfwdvthW.js";import a,{n as o}from"./Heading.stories-B8vE0pFC.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Text/Heading`},c={args:{children:`Amsterdam Light Festival maakt de stad ook deze winter een beetje mooier`},render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Amsterdam Light Festival maakt de stad ook deze winter een beetje mooier'
+  },
+  render: (args, context) => renderComponentVariants(Heading, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
---
+++ demo-DES-1940-switch-disabled-track-colour/assets/Label.test.stories-DljftMju.js
@@ -0,0 +1,6 @@
+import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-_AM4PSi7.js";import{dt as r,t as i}from"./src-DfwdvthW.js";import a,{n as o}from"./Label.stories-QrQURVi6.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Label`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Label, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
