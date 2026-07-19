445598333f37ed2adb5e86d0d72aa6f073b8aa7e
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-19 11:16:10 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@952e857ce36a637fbcc1d071dee9b99f23890c36 🚀

---
+++ demo-develop/assets/Badge.test.stories-Do0OvsOh.js
@@ -0,0 +1,6 @@
+import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-ddwTaO2c.js";import{En as r,t as i}from"./src-Cc_k81jg.js";import a,{n as o}from"./Badge.stories-CgK-dj74.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Feedback/Badge`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Badge, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
---
+++ demo-develop/assets/FileInput.test.stories-BbHZgaG-.js
@@ -0,0 +1,7 @@
+import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-ddwTaO2c.js";import{kt as r,t as i}from"./src-Cc_k81jg.js";import a,{n as o}from"./FileInput.stories-0S5YtGV6.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/File Input`},c={render:(e,n)=>t(r,{args:e,variants:[`disabled`,`multiple`]},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(FileInput, {
+    args,
+    variants: ['disabled', 'multiple']
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
