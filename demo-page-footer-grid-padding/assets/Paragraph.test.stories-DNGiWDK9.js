95226f6965a91c89e353ac02bb29209c5a8d5104
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-20 20:56:18 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@1e0111cf6e1ab3ebb98029cd2e80da9f3458ec7a 🚀

---
+++ demo-page-footer-grid-padding/assets/Paragraph.test.stories-DNGiWDK9.js
@@ -0,0 +1,9 @@
+import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-Bcw_M6gG.js";import{ln as r,t as i}from"./src-DB1C8Joe.js";import a,{n as o}from"./Paragraph.stories-BR3jdsnN.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Text/Paragraph`},c={args:{children:`Het waterrijke achterland van de provincie Holland was een paradijs voor vissers. Maar terwijl de visserij floreerde in Holland, was Amsterdam eigenlijk helemaal geen visserijstad. Toch maakten Amsterdammers naam in de vishandel. Zij speelden namelijk een cruciale rol bij het klaarmaken en vervoeren van haring.`},render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Het waterrijke achterland van de provincie Holland was een paradijs voor vissers. Maar terwijl de visserij floreerde in Holland, was Amsterdam eigenlijk helemaal geen visserijstad. Toch maakten Amsterdammers naam in de vishandel. Zij speelden namelijk een cruciale rol bij het klaarmaken en vervoeren van haring.'
+  },
+  render: (args, context) => renderComponentVariants(Paragraph, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
