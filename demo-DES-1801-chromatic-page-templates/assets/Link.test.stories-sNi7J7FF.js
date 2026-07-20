213c83d58fcb81169ee363e586febf5010a9815c
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-20 23:12:54 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@e358a81ea9bd09b3c69158dd53c884df77e33911 🚀

---
+++ demo-DES-1801-chromatic-page-templates/assets/Link.test.stories-sNi7J7FF.js
@@ -0,0 +1,6 @@
+import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-DO5pw751.js";import{lt as r,t as i}from"./src-CQzbxGCF.js";import a,{n as o}from"./Link.stories-BfSHf-2E.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Navigation/Link`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Link, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
---
+++ demo-DES-1801-chromatic-page-templates/assets/Select.test.stories-CM7fJIho.js
@@ -0,0 +1,7 @@
+import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-DO5pw751.js";import{j as r,t as i}from"./src-CQzbxGCF.js";import a,{n as o}from"./Select.stories-p88BhCo9.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Select`},c={render:(e,n)=>t(r,{args:e,variants:[`hovered`,`disabled`]},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Select, {
+    args,
+    variants: ['hovered', 'disabled']
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
