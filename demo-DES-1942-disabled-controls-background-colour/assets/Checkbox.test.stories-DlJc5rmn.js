206921525f6405249bb33b26365e87e24de8310c
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-20 12:42:01 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@b45b1eaf3077c9d9f982972fadb43df9e9be9a65 🚀

---
+++ demo-DES-1942-disabled-controls-background-colour/assets/Checkbox.test.stories-DlJc5rmn.js
@@ -0,0 +1,7 @@
+import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-C1lBZj8N.js";import{$t as r,t as i}from"./src-C5Zn8e2H.js";import a,{n as o}from"./Checkbox.stories-CyAx_AWi.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Checkbox`},c={render:(e,n)=>t(r,{args:e,variants:[`disabled`]},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Checkbox, {
+    args,
+    variants: ['disabled']
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
--- demo-DES-1942-disabled-controls-background-colour/assets/ErrorMessage.test.stories-B9pNKKYT.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-CESv3ZwW.js";import{Lt as r,t as i}from"./src-k33nRoTT.js";import a,{n as o}from"./ErrorMessage.stories-C-KcyF6h.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Error Message`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(ErrorMessage, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
