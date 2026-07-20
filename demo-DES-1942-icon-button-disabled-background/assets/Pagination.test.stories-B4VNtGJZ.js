be591d55b2336e99278bd50d4c764fb2dd34910a
GitHub <github-action@users.noreply.github.com>
2026-07-20 15:03:39 +0000

    Remove feature branch folder

--- demo-DES-1942-icon-button-disabled-background/assets/Pagination.test.stories-B4VNtGJZ.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-DEGAPYQZ.js";import{H as r,t as i}from"./src-BTgew5hY.js";import a,{n as o}from"./Pagination.stories-CdRnfegX.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Navigation/Pagination`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Pagination, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
