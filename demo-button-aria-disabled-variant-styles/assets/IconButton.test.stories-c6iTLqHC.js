66fdc74b52bd3b11b86f5eaba16b59638bd7b13f
GitHub <github-action@users.noreply.github.com>
2026-07-20 11:38:33 +0000

    Remove feature branch folder

--- demo-button-aria-disabled-variant-styles/assets/IconButton.test.stories-c6iTLqHC.js
+++
@@ -1,7 +0,0 @@
-import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-BJmxqdKE.js";import{Rn as r,t as i}from"./src-88ws8IE0.js";import a,{n as o}from"./IconButton.stories-B4C0rMy2.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Buttons/Icon Button`},c={render:(e,n)=>t(r,{args:e,variants:[`hovered`,`disabled`]},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(IconButton, {
-    args,
-    variants: ['hovered', 'disabled']
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
