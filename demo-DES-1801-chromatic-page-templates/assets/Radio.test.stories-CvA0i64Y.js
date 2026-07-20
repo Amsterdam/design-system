3775a3898cbbfcbc142d48d14cc47b611bc821de
GitHub <github-action@users.noreply.github.com>
2026-07-20 23:21:51 +0000

    Remove feature branch folder

--- demo-DES-1801-chromatic-page-templates/assets/Radio.test.stories-CvA0i64Y.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-DO5pw751.js";import{F as r,t as i}from"./src-CQzbxGCF.js";import a,{n as o}from"./Radio.stories-D4kzk4aX.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Radio`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Radio, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
