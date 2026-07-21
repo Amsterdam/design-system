fbdc36fb4c9b82537c069cfa364382d0f3d04eb8
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-21 10:48:46 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@9e414fc5c24fab62b80b0829bfffc33e952660da 🚀

---
+++ demo-unused-token-check/assets/Alert.test.stories-C60dcdNK.js
@@ -0,0 +1,6 @@
+import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-CJi42fVa.js";import{Mn as r,t as i}from"./src-Bitjj1vj.js";import a,{n as o}from"./Alert.stories-Cv4e7h9l.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Feedback/Alert`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Alert, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
