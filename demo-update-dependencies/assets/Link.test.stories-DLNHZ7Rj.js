5e6036694029dc3bbf044fe4be36792a411ddaf5
GitHub <github-action@users.noreply.github.com>
2026-07-21 13:27:51 +0000

    Remove feature branch folder

--- demo-update-dependencies/assets/Link.test.stories-DLNHZ7Rj.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-Bk2YlRTd.js";import{lt as r,t as i}from"./src-BEFV-YxG.js";import a,{n as o}from"./Link.stories-DsSuKFIt.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Navigation/Link`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Link, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
