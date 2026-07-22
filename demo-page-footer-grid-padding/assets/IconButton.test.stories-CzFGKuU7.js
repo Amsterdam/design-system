4837ac64c3dd25a04ee394f2aef4ef37b852e55b
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-22 14:23:20 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@81a453be2ad58ef66266f0bfb15fd397df16400f 🚀

---
+++ demo-page-footer-grid-padding/assets/IconButton.test.stories-CzFGKuU7.js
@@ -0,0 +1,7 @@
+import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-WDTJrXYm.js";import{Rn as r,t as i}from"./src-xcYXCHkC.js";import a,{n as o}from"./IconButton.stories-5UK4Ez5U.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Buttons/Icon Button`},c={render:(e,n)=>t(r,{args:e,variants:[`hovered`,`disabled`]},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(IconButton, {
+    args,
+    variants: ['hovered', 'disabled']
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
--- demo-page-footer-grid-padding/assets/Page.test.stories-B0NpPywU.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-Bl_Yq-Jj.js";import{X as r,t as i}from"./src-CzHySj9s.js";import a,{n as o}from"./Page.stories-In98jPje.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Containers/Page`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Page, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
