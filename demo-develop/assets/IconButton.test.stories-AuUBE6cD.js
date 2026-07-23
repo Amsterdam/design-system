4aa1f2b47ba80192cab762a83e39fa5363517b97
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-23 10:35:49 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@1b8ccd76c45ac5c9a5493029371ec69cd6bf0e56 🚀

---
+++ demo-develop/assets/IconButton.test.stories-AuUBE6cD.js
@@ -0,0 +1,7 @@
+import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-BT3I1sQ3.js";import{Rn as r,t as i}from"./src-CSsadZcQ.js";import a,{n as o}from"./IconButton.stories-tp_7MpNR.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Buttons/Icon Button`},c={render:(e,n)=>t(r,{args:e,variants:[`hovered`,`disabled`]},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(IconButton, {
+    args,
+    variants: ['hovered', 'disabled']
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
