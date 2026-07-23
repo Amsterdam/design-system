9325097e3dcf4aa753745afdd0852c61f8e73bfc
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-23 16:02:04 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@26a239ee3c7d5ba180162d407dbe5a84c28df494 🚀

---
+++ demo-DES-1932-information-page-template/assets/PageFooterMenu.stories-CqzX-URS.js
@@ -0,0 +1,13 @@
+import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{t}from"./jsx-runtime-Bq1bXGty.js";import{J as n,t as r}from"./src-Bf7XaHZZ.js";import{c as i,d as a}from"./argTypes-CQXDxDHP.js";var o,s,c,l,u=e((()=>{r(),a(),o=t(),s={title:`Components/Containers/Page Footer`,component:n.Menu,argTypes:{headingLevel:i([2,3,4],2)},decorators:[e=>(0,o.jsx)(n,{children:(0,o.jsx)(e,{})})]},c={args:{children:[(0,o.jsx)(n.MenuLink,{href:`#`,children:`Over deze site`},`about`),(0,o.jsx)(n.MenuLink,{href:`#`,children:`Privacy`},`privacy`),(0,o.jsx)(n.MenuLink,{href:`#`,children:`Cookies op deze site`},`cookies`),(0,o.jsx)(n.MenuLink,{href:`#`,children:`Webarchief`},`archive`)]}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<PageFooter.MenuLink href="#" key="about">
+        Over deze site
+      </PageFooter.MenuLink>, <PageFooter.MenuLink href="#" key="privacy">
+        Privacy
+      </PageFooter.MenuLink>, <PageFooter.MenuLink href="#" key="cookies">
+        Cookies op deze site
+      </PageFooter.MenuLink>, <PageFooter.MenuLink href="#" key="archive">
+        Webarchief
+      </PageFooter.MenuLink>]
+  }
+}`,...c.parameters?.docs?.source}}},l=[`Menu`]}));u();export{c as Menu,l as __namedExportsOrder,s as default,u as t};
