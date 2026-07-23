d1a1c4d2e9b6cd248fe02c84c7aaecfd281e32e1
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-23 15:40:06 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@a687eb99f415d44e72ccc12f01b59116a3d8cece 🚀

---
+++ demo-DES-1932-information-page-template/assets/Checkbox.test.stories-CTwWA7QF.js
@@ -0,0 +1,6 @@
+import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-Cue6KVzK.js";import{$t as r,t as i}from"./src-CoVH16Cn.js";import a,{n as o}from"./Checkbox.stories-4jiuvHLS.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Checkbox`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Checkbox, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
---
+++ demo-DES-1932-information-page-template/assets/Link.test.stories-C7JNauDi.js
@@ -0,0 +1,6 @@
+import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-Cue6KVzK.js";import{lt as r,t as i}from"./src-CoVH16Cn.js";import a,{n as o}from"./Link.stories-KVLOCqn-.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Navigation/Link`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Link, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
