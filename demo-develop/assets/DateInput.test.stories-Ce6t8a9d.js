763b70d90488d1a8fc984f57e659ca0cba50977c
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-23 13:10:57 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@d80b36cbf27c6bb6e4a8bf1492cfccaada9f0b42 🚀

--- demo-develop/assets/DateInput.test.stories-Ce6t8a9d.js
+++
@@ -1,7 +0,0 @@
-import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-BT3I1sQ3.js";import{Kt as r,t as i}from"./src-CSsadZcQ.js";import a,{n as o}from"./DateInput.stories-BXEsV4_s.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Date Input`},c={render:(e,n)=>t(r,{args:e,variants:[`disabled`]},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(DateInput, {
-    args,
-    variants: ['disabled']
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
