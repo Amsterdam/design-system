e733e9fc0c523201d1b4eeb20115e0122ea5be2d
GitHub <github-action@users.noreply.github.com>
2026-07-23 13:27:23 +0000

    Remove feature branch folder

--- demo-document-internal-page-templates/assets/IconButton.test.stories-CDVDeHkY.js
+++
@@ -1,7 +0,0 @@
-import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-D8X4NMvh.js";import{Rn as r,t as i}from"./src-HDZDVKF5.js";import a,{n as o}from"./IconButton.stories-wU_zbn7G.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Buttons/Icon Button`},c={render:(e,n)=>t(r,{args:e,variants:[`hovered`,`disabled`]},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(IconButton, {
-    args,
-    variants: ['hovered', 'disabled']
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
