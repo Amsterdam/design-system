85ed652982ee901737a84c3d4f4ff7f5b3ada7ec
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-23 23:27:00 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@6ced2845fcceea4fb101c339d7fa48486219ec86 🚀

---
+++ demo-sketch-mode/assets/Badge.test.stories-j4NfJvht.js
@@ -0,0 +1,6 @@
+import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-DE0iMqaE.js";import{En as r,t as i}from"./src-W6GtdwBi.js";import a,{n as o}from"./Badge.stories-BPSum0OM.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Feedback/Badge`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Badge, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
