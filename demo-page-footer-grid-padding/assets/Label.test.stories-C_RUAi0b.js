400ba1d96bc5342889b267d6cae14f12253d8af5
GitHub <github-action@users.noreply.github.com>
2026-07-23 08:01:43 +0000

    Remove feature branch folder

--- demo-page-footer-grid-padding/assets/Label.test.stories-C_RUAi0b.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-Bk4wxGYh.js";import{dt as r,t as i}from"./src-49CsR1HT.js";import a,{n as o}from"./Label.stories-B2AI4N2x.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Label`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Label, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
