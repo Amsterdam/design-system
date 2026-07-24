027dec22f2f8d8e6a2aea3838c5158f0b4bb328c
GitHub <github-action@users.noreply.github.com>
2026-07-24 15:37:48 +0000

    Remove feature branch folder

--- demo-DES-1932-information-page-template/assets/Button.test.stories-Q6KQ2Rss.js
+++
@@ -1,7 +0,0 @@
-import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-Bafi2S6B.js";import{hn as r,t as i}from"./src-Bf7XaHZZ.js";import a,{n as o}from"./Button.stories-D-E-YuI9.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Buttons/Button`},c={render:(e,n)=>t(r,{args:e,variants:[`disabled`,`hovered`]},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Button, {
-    args,
-    variants: ['disabled', 'hovered']
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
