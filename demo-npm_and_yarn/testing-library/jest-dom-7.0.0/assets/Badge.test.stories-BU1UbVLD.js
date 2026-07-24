3cd22302784957e445bf8b0424316c35dcc0f679
GitHub <github-action@users.noreply.github.com>
2026-07-24 12:54:57 +0000

    Remove feature branch folder

--- demo-npm_and_yarn/testing-library/jest-dom-7.0.0/assets/Badge.test.stories-BU1UbVLD.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-CzoQ7l95.js";import{En as r,t as i}from"./src-BR3ksCEE.js";import a,{n as o}from"./Badge.stories-6Lo9yEaZ.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Feedback/Badge`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Badge, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
