e43354c3b1620f5dcfeefc9beba1cb100fa87675
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-24 12:48:22 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@1f33c6b719f7f5c9cd336704a3e0ea06ee730c88 🚀

---
+++ demo-npm_and_yarn/testing-library/jest-dom-7.0.0/assets/IconButton.test.stories-Df5aany7.js
@@ -0,0 +1,7 @@
+import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-CzoQ7l95.js";import{Rn as r,t as i}from"./src-BR3ksCEE.js";import a,{n as o}from"./IconButton.stories-DLVxD3Yw.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Buttons/Icon Button`},c={render:(e,n)=>t(r,{args:e,variants:[`hovered`,`disabled`]},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(IconButton, {
+    args,
+    variants: ['hovered', 'disabled']
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
