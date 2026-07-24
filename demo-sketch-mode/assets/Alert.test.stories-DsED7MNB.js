6f5a768f775db60caa0457782d3619bb62d69d09
GitHub <github-action@users.noreply.github.com>
2026-07-24 14:54:04 +0000

    Remove feature branch folder

--- demo-sketch-mode/assets/Alert.test.stories-DsED7MNB.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-Cu5BQ9kg.js";import{Mn as r,t as i}from"./src-DgsWM1eg.js";import a,{n as o}from"./Alert.stories-R7zl9nEa.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Feedback/Alert`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Alert, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
