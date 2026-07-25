5102b89dd59039a4f70b81550c8fc4dbd995cfd2
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-25 18:56:13 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@325a0446cb2e68f3cab0539fa20dd1ab6e022a6c 🚀

---
+++ demo-skip-link-landmark/assets/Spotlight.docs-DqD44Wub.js
@@ -0,0 +1,39 @@
+import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{a as t,d as n,h as r,i,n as a,p as o,u as s}from"./blocks-CqryReaq.js";import{t as c}from"./jsx-runtime-Bq1bXGty.js";import{i as l}from"./react-BnCtKaSV.js";import{n as u,t as d}from"./DesignTokensTable--5a2Qafw.js";import{t as f}from"./mdx-react-shim-9RzcSRdq.js";import{HighlightContent as p,n as m,t as h}from"./Spotlight.stories-D-F4_MyG.js";var g,_,v=e((()=>{g={spotlight:{"background-color":{$value:`{ams.color.highlight.purple}`,$extensions:{"nl.amsterdam.type":`color`}},azure:{"background-color":{$value:`{ams.color.highlight.azure}`,$extensions:{"nl.amsterdam.type":`color`}}},green:{"background-color":{$value:`{ams.color.highlight.green}`,$extensions:{"nl.amsterdam.type":`color`}}},lime:{"background-color":{$value:`{ams.color.highlight.lime}`,$extensions:{"nl.amsterdam.type":`color`}}},magenta:{"background-color":{$value:`{ams.color.highlight.magenta}`,$extensions:{"nl.amsterdam.type":`color`}}},orange:{"background-color":{$value:`{ams.color.highlight.orange}`,$extensions:{"nl.amsterdam.type":`color`}}},yellow:{"background-color":{$value:`{ams.color.highlight.yellow}`,$extensions:{"nl.amsterdam.type":`color`}}}}},_={ams:g}}));function y(e){let r={a:`a`,code:`code`,h2:`h2`,h3:`h3`,li:`li`,p:`p`,ul:`ul`,...l(),...e.components};return(0,x.jsxs)(x.Fragment,{children:[`
+`,`
+`,`
+`,`
+`,(0,x.jsx)(s,{of:h}),`
+`,(0,x.jsx)(o,{}),`
+`,(0,x.jsx)(t,{of:h}),`
+`,(0,x.jsx)(n,{}),`
+`,(0,x.jsx)(i,{}),`
+`,(0,x.jsx)(r.h2,{id:`usage-guidelines`,children:`Usage guidelines`}),`
+`,(0,x.jsx)(r.h3,{id:`when-not-to-use`,children:`When not to use`}),`
+`,(0,x.jsxs)(r.p,{children:[`Do not position a Spotlight on the `,(0,x.jsx)(r.a,{href:`/docs/components-layout-grid--docs`,children:`Grid`}),`.`]}),`
+`,(0,x.jsx)(r.h3,{id:`how-to-use`,children:`How to use`}),`
+`,(0,x.jsxs)(r.p,{children:[`Use the entire width of the `,(0,x.jsx)(r.a,{href:`/docs/components-containers-page--docs`,children:`Page`}),` container.
+Wrap its content in a Grid with extra large vertical padding.`]}),`
+`,(0,x.jsxs)(r.p,{children:[`Use any of the `,(0,x.jsx)(r.a,{href:`/docs/brand-design-tokens-colour--docs`,children:`highlight colours`}),` for the background.
+The design system does not prescribe a meaning to any of these colours.
+The default is purple.
+Select `,(0,x.jsx)(r.a,{href:`/docs/brand-design-tokens-colour--docs#pairing-foreground-with-background-colours`,children:`the correct colour`}),` for text and links against the background you choose.`]}),`
+`,(0,x.jsxs)(r.p,{children:[`Find a fitting setup for the `,(0,x.jsx)(r.a,{href:`/docs/pages-public-introduction--docs#how-to-size-the-grid-cells`,children:`amount and width of columns`}),`.`]}),`
+`,(0,x.jsxs)(r.p,{children:[`Use the `,(0,x.jsx)(r.code,{children:`as`}),` prop to render a meaningful HTML element: `,(0,x.jsx)(r.code,{children:`as="section"`}),` for a self-contained content area, `,(0,x.jsx)(r.code,{children:`as="aside"`}),` for supporting content.`]}),`
+`,(0,x.jsx)(r.h2,{id:`examples`,children:`Examples`}),`
+`,(0,x.jsx)(r.h3,{id:`highlight-content`,children:`Highlight content`}),`
+`,(0,x.jsx)(r.p,{children:`Use a Spotlight to direct the user’s attention to the most important or actionable content on the page.`}),`
+`,(0,x.jsx)(a,{of:p}),`
+`,(0,x.jsx)(r.h2,{id:`accessibility`,children:`Accessibility`}),`
+`,(0,x.jsxs)(r.p,{children:[`Which foreground colour to use depends on the highlight colour behind it.
+Follow `,(0,x.jsx)(r.a,{href:`/docs/brand-design-tokens-colour--docs#pairing-foreground-with-background-colours`,children:`Pairing foreground with background colours`}),`.`]}),`
+`,(0,x.jsxs)(r.p,{children:[`On a dark background, set `,(0,x.jsx)(r.code,{children:`color="inverse"`}),` on every Heading, Paragraph, and Link inside the Spotlight.
+On a light one, keep the default colour for headings and paragraphs, and set `,(0,x.jsx)(r.code,{children:`color="contrast"`}),` on links.`]}),`
+`,(0,x.jsxs)(r.p,{children:[(0,x.jsx)(r.code,{children:`as="aside"`}),` makes the Spotlight a complementary landmark, and `,(0,x.jsx)(r.code,{children:`as="section"`}),` makes it a region once it has an accessible name.
+Point `,(0,x.jsx)(r.code,{children:`aria-labelledby`}),` at the `,(0,x.jsx)(r.code,{children:`id`}),` of the heading inside the Spotlight to name that landmark, so users of assistive technology can tell it apart from the other landmarks on the page.`]}),`
+`,(0,x.jsx)(r.h2,{id:`see-also`,children:`See also`}),`
+`,(0,x.jsxs)(r.ul,{children:[`
+`,(0,x.jsxs)(r.li,{children:[(0,x.jsx)(r.a,{href:`/docs/components-containers-page--docs`,children:`Page`}),` – contains the Spotlight.`]}),`
+`,(0,x.jsxs)(r.li,{children:[(0,x.jsx)(r.a,{href:`/docs/components-layout-grid--docs`,children:`Grid`}),` – wraps the content of the Spotlight.`]}),`
+`]}),`
+`,(0,x.jsx)(r.h2,{id:`design-tokens`,children:`Design tokens`}),`
+`,(0,x.jsx)(d,{tokens:_})]})}function b(e={}){let{wrapper:t}={...l(),...e.components};return t?(0,x.jsx)(t,{...e,children:(0,x.jsx)(y,{...e})}):y(e)}var x;e((()=>{x=c(),f(),r(),u(),v(),m()}))();export{b as default};
