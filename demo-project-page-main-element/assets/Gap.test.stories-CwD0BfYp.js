584098d7374f67ce0eaefbe1bea3125735ab1535
GitHub <github-action@users.noreply.github.com>
2026-07-26 17:36:32 +0000

    Remove feature branch folder

--- demo-project-page-main-element/assets/Gap.test.stories-CwD0BfYp.js
+++
@@ -1,9 +0,0 @@
-import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{t}from"./jsx-runtime-Bq1bXGty.js";import{P as n,X as r}from"./index.esm-BkLTxZt4.js";import i,{n as a}from"./Gap.stories-C8JIwkGN.js";var o,s,c,l,u;e((()=>{r(),a(),o=t(),s=[`xs`,`s`,`m`,`l`,`xl`],c={...i,title:`Utilities/CSS/Gap`},l={render:()=>(0,o.jsx)(`div`,{className:`_ams-tests-stack`,children:s.map(e=>(0,o.jsxs)(`div`,{className:`ams-gap-${e}`,children:[(0,o.jsx)(n,{children:`ams-gap-${e}`}),(0,o.jsx)(n,{children:`ams-gap-${e}`})]},e))}),tags:[`!dev`,`!autodocs`]},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
-  render: () => <div className="_ams-tests-stack">
-      {sizes.map(size => <div className={\`ams-gap-\${size}\`} key={size}>
-          <Paragraph>{\`ams-gap-\${size}\`}</Paragraph>
-          <Paragraph>{\`ams-gap-\${size}\`}</Paragraph>
-        </div>)}
-    </div>,
-  tags: ['!dev', '!autodocs']
-}`,...l.parameters?.docs?.source}}},u=[`Test`]}))();export{l as Test,u as __namedExportsOrder,c as default};
