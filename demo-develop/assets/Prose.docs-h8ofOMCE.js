161a66b27f671684d40be9d8d20e656c25031fce
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-26 21:13:19 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@1e2f981e7e02c053c43aa3c8890b36eed91ca7fb 🚀

---
+++ demo-develop/assets/Prose.docs-h8ofOMCE.js
@@ -0,0 +1,27 @@
+import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{d as t,h as n,l as r,u as i}from"./blocks-CqryReaq.js";import{t as a}from"./jsx-runtime-Bq1bXGty.js";import{i as o}from"./react-BnCtKaSV.js";import{n as s,t as c}from"./StatusBadge-8rmWaQDr.js";import{t as l}from"./mdx-react-shim-9RzcSRdq.js";import{n as u,t as d}from"./Prose.stories-Chk4OJGf.js";var f,p=e((()=>{f=`<!-- @license CC0-1.0 -->
+
+# Prose
+
+Applies the recommended vertical spacing between the direct children of a container, so editorial content flows without hand-picked margins.
+
+## Class name
+
+\`ams-prose\`
+
+## Guidelines
+
+- Wrap this utility class around editorial content to space its content according to the [vertical space](/docs/docs-designer-guide-vertical-space--docs) recommendations.
+- To add a single ad-hoc gap between two elements, prefer the [Margin Bottom](/docs/utilities-css-margin--docs) utility class on the first one.
+- To add even spacing between a set of siblings, prefer the [Gap](/docs/utilities-css-gap--docs) utility class on their parent.
+`}));function m(e){let n={a:`a`,h2:`h2`,li:`li`,ul:`ul`,...o(),...e.components};return(0,g.jsxs)(g.Fragment,{children:[`
+`,`
+`,`
+`,`
+`,(0,g.jsx)(i,{of:d}),`
+`,(0,g.jsx)(c,{description:`Covers many element combinations. Exceptions will surface – we’ll iterate. API is stable.`,status:`beta`}),`
+`,(0,g.jsx)(r,{children:f}),`
+`,(0,g.jsx)(t,{}),`
+`,(0,g.jsx)(n.h2,{id:`see-also`,children:`See also`}),`
+`,(0,g.jsxs)(n.ul,{children:[`
+`,(0,g.jsxs)(n.li,{children:[(0,g.jsx)(n.a,{href:`/docs/docs-developer-guide-spacing--docs`,children:`Spacing`}),` – which tools to use for white space and when.`]}),`
+`]})]})}function h(e={}){let{wrapper:t}={...o(),...e.components};return t?(0,g.jsx)(t,{...e,children:(0,g.jsx)(m,{...e})}):m(e)}var g;e((()=>{g=a(),l(),n(),s(),p(),u()}))();export{h as default};
