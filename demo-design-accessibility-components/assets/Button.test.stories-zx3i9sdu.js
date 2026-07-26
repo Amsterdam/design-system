40e020dbc71c843cf60d712ab89dc70fb4c9305f
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-26 22:43:31 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@fc602e677ec0fbd7744fbafeb38e788e3ad40fd7 🚀

---
+++ demo-design-accessibility-components/assets/Button.test.stories-zx3i9sdu.js
@@ -0,0 +1,7 @@
+import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-Dx_as7uE.js";import{hn as r,t as i}from"./src-BzfkmJVc.js";import a,{n as o}from"./Button.stories-DIwuY7Xh.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Buttons/Button`},c={render:(e,n)=>t(r,{args:e,variants:[`disabled`,`hovered`]},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Button, {
+    args,
+    variants: ['disabled', 'hovered']
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
