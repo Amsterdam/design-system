e7d5417a1ab851ad6c0b7ad497b7edcb2e98807a
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-26 21:55:14 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@7e6a0978c92d1fcc1b178780acb2e1f2370ddb89 🚀

---
+++ demo-alert-generated-heading-id/assets/Alert.test.stories-wt2kRO7k.js
@@ -0,0 +1,6 @@
+import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-CJDmqwlW.js";import{Mn as r,t as i}from"./src-Ine2ceJL.js";import a,{n as o}from"./Alert.stories-D0CxU2mS.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Feedback/Alert`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Alert, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
---
+++ demo-alert-generated-heading-id/assets/Select.test.stories-DTtC3umA.js
@@ -0,0 +1,7 @@
+import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-CJDmqwlW.js";import{j as r,t as i}from"./src-Ine2ceJL.js";import a,{n as o}from"./Select.stories-C6kqus4i.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Select`},c={render:(e,n)=>t(r,{args:e,variants:[`hovered`,`disabled`]},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Select, {
+    args,
+    variants: ['hovered', 'disabled']
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
