4804f4aa9c92d482a11f2005e211d1c6028d5ab7
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-26 19:39:47 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@c4be5a2307fa037d191d5f183b13a667ae257c77 🚀

--- demo-develop/assets/Avatar.test.stories-B4vsPT6D.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-CBSVtHcE.js";import{kn as r,t as i}from"./src-CwmOeXY7.js";import a,{n as o}from"./Avatar.stories-OOVBmrB6.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Feedback/Avatar`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Avatar, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
