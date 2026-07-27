305679c1119197b3e1481176b6a6cf4d73817468
GitHub <github-action@users.noreply.github.com>
2026-07-27 10:08:10 +0000

    Remove feature branch folder

--- demo-design-accessibility-components/assets/IconButton.test.stories-Da9lTvPx.js
+++
@@ -1,7 +0,0 @@
-import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-CpL4261E.js";import{Rn as r,t as i}from"./src-CHgXk7sk.js";import a,{n as o}from"./IconButton.stories-D8M5_2gY.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Buttons/Icon Button`},c={render:(e,n)=>t(r,{args:e,variants:[`hovered`,`disabled`]},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(IconButton, {
-    args,
-    variants: ['hovered', 'disabled']
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
