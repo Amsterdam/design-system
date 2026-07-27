d3478b30263781d99976670dd6f4a4dc38f5b3a9
GitHub <github-action@users.noreply.github.com>
2026-07-27 07:11:22 +0000

    Remove feature branch folder

--- demo-information-page-caption-sources/assets/Alert.test.stories-B0-NNyeC.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-co8Uhcbs.js";import{Mn as r,t as i}from"./src-C8pwAGjV.js";import a,{n as o}from"./Alert.stories-Bx_z0HbA.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Feedback/Alert`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Alert, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
