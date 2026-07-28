b614cdaf0b982a6e0574bfc2bf29cf7c3f4d61e7
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-28 08:37:06 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@ce4e2d595c22bab4d19b8af64b11f07d91d60b87 🚀

--- demo-develop/assets/Column.test.stories-C3U780su.js
+++
@@ -1,7 +0,0 @@
-import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-BtP0ogwF.js";import{Yt as r,t as i}from"./src-MHc4MCT3.js";import a,{n as o}from"./Column.stories-CNAPcpNM.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Layout/Column`},c={render:(e,n)=>t(r,{args:e,layout:`grid`},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Column, {
-    args,
-    layout: 'grid'
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
--- demo-develop/assets/Heading.test.stories-v93fB9Nk.js
+++
@@ -1,9 +0,0 @@
-import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-BtP0ogwF.js";import{qn as r,t as i}from"./src-MHc4MCT3.js";import a,{n as o}from"./Heading.stories-u4olYQ3q.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Text/Heading`},c={args:{children:`Amsterdam Light Festival maakt de stad ook deze winter een beetje mooier`},render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: 'Amsterdam Light Festival maakt de stad ook deze winter een beetje mooier'
-  },
-  render: (args, context) => renderComponentVariants(Heading, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
