6caf5754253c854006e592e415594624390329a6
GitHub <github-action@users.noreply.github.com>
2026-07-28 08:35:27 +0000

    Remove feature branch folder

--- demo-stylelint-defensive-css-resolution/assets/StandaloneLink.test.stories-CVh4gTs1.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-figL5vWV.js";import{t as r,x as i}from"./src-Ch1A0UTV.js";import a,{n as o}from"./StandaloneLink.stories-De1GvaTb.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/Navigation/Standalone Link`},c={render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(StandaloneLink, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
