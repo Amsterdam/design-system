943f81739f5b3b1fb2e9f8da82f71475175e9988
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-29 21:52:54 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@fd8d57f9114f2b91534161cf82856f399fbdb848 🚀

---
+++ demo-develop/assets/Alert.test.stories-4nh-3B04.js
@@ -0,0 +1,6 @@
+import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-CbLQplZz.js";import{Nn as r,t as i}from"./src-Cne4sxE3.js";import a,{n as o}from"./Alert.stories-BXELow5h.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Feedback/Alert`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Alert, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
--- demo-develop/assets/SkeletonImage.stories-CYlz23kx.js
+++
@@ -1,5 +0,0 @@
-import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{t}from"./jsx-runtime-Bq1bXGty.js";import{k as n,t as r}from"./src-6ewcJEx5.js";import{n as i,t as a}from"./decorators-DFaeY69z.js";import{i as o,t as s}from"./types-Upr6OxRk.js";var c,l,u,d,f=e((()=>{r(),o(),a(),c=t(),l={title:`Components/Feedback/Skeleton`,component:n.Image,argTypes:{aspectRatio:{control:`select`,options:s}},decorators:[i(`24rem`)],render:e=>(0,c.jsx)(n,{children:(0,c.jsx)(n.Image,{...e})})},u={args:{aspectRatio:`16:9`}},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
-  args: {
-    aspectRatio: '16:9'
-  }
-}`,...u.parameters?.docs?.source}}},d=[`Image`]}));f();export{u as Image,d as __namedExportsOrder,l as default,f as t};
