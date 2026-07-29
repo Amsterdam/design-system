24abacbff377d8e8bfd85d24b90e0b60ad110e64
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-29 18:42:49 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@ce4349d0ce5c9b238566499b5912c979c0ead7e5 🚀

---
+++ demo-safe-area-insets/assets/Blockquote.test.stories-MksYgwbL.js
@@ -0,0 +1,9 @@
+import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-Sy2fXyrz.js";import{t as r,wn as i}from"./src-C5Ei5etM.js";import a,{n as o}from"./Blockquote.stories-DgLAcUMU.js";var s,c,l;e((()=>{r(),n(),o(),s={...a,title:`Components/Text/Blockquote`},c={args:{children:`Voor Amsterdam was de Jordaan één oproer en één fort. De Amsterdammer begreep de Jordaner niet, die zelden uit zijn buurt komt, soms niet uit zijn straat, wiens stad haar eigen plattegrond heeft, een stad in een stad.`},render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Voor Amsterdam was de Jordaan één oproer en één fort. De Amsterdammer begreep de Jordaner niet, die zelden uit zijn buurt komt, soms niet uit zijn straat, wiens stad haar eigen plattegrond heeft, een stad in een stad.'
+  },
+  render: (args, context) => renderComponentVariants(Blockquote, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
--- demo-safe-area-insets/assets/Page.test.stories-Dftc4s4I.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-lvZHKNrW.js";import{X as r,t as i}from"./src-DbY8H8BQ.js";import a,{n as o}from"./Page.stories-D3iEc8XQ.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Containers/Page`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Page, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
