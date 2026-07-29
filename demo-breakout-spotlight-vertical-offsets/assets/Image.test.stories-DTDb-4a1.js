b458e000571eb42b32a3021deaa25a220d6f1fd8
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-29 19:12:37 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@35157ae7b2c59fc7b4d28d7bd8efe23c0593b93a 🚀

---
+++ demo-breakout-spotlight-vertical-offsets/assets/Image.test.stories-DTDb-4a1.js
@@ -0,0 +1,10 @@
+import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-CO9lCny3.js";import{on as r,t as i}from"./src-C4-LsVLi.js";import a,{n as o}from"./Image.stories-B_Z03ego.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Media/Image`},c={args:{alt:``,src:`https://picsum.photos/id/122/1280/720`},render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    alt: '',
+    src: 'https://picsum.photos/id/122/1280/720'
+  },
+  render: (args, context) => renderComponentVariants(Image, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
