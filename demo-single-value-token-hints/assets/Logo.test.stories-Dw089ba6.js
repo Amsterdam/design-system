0483ec66e62d121aa6205d336b40bd4a56328dca
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-29 13:14:40 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@cc5bd8d6494222753643f2c4e84ece9dbae02d7a 🚀

---
+++ demo-single-value-token-hints/assets/Logo.test.stories-Dw089ba6.js
@@ -0,0 +1,6 @@
+import{a as e,n as t}from"./rolldown-runtime-DaJ6WEGw.js";import{t as n}from"./react-BDsysHVl.js";import{t as r}from"./jsx-runtime-Bq1bXGty.js";import{ct as i,ot as a,st as o,t as s}from"./src-oZYJPNFi.js";import c,{n as l}from"./Logo.stories-CEziIlF3.js";var u,d,f,p,m;t((()=>{s(),o(),l(),u=e(n()),d=r(),f={...c,title:`Components/Media/Logo`},p={render:e=>(0,d.jsx)(`div`,{className:`_ams-tests-grid`,children:[void 0,...i].map(t=>(0,u.createElement)(a,{...e,brand:t,key:t}))}),tags:[`!dev`,`!autodocs`]},p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
+  render: args => <div className="_ams-tests-grid">
+      {[undefined, ...logoBrands].map(brand => <Logo {...args} brand={brand} key={brand} />)}
+    </div>,
+  tags: ['!dev', '!autodocs']
+}`,...p.parameters?.docs?.source}}},m=[`Test`]}))();export{p as Test,m as __namedExportsOrder,f as default};
