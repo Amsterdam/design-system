c9929a24ecdc9a53d66802fc5c58539b26c3ceca
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-30 23:12:19 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@ba592ed55e652ce2e460408fbb187d8627f65238 🚀

---
+++ demo-develop/assets/Link.stories-BBjHTvHE.js
@@ -0,0 +1,9 @@
+import{n as e,r as t}from"./rolldown-runtime-DaJ6WEGw.js";import{t as n}from"./jsx-runtime-Bq1bXGty.js";import{P as r,X as i}from"./index.esm-C2MNZycB.js";import{lt as a,t as o}from"./src-CO3EEZT7.js";import{a as s,d as c,l,p as u,r as d}from"./argTypes-CQXDxDHP.js";var f=t({ContrastColour:()=>g,Default:()=>h,InverseColour:()=>_,__namedExportsOrder:()=>v,default:()=>m}),p,m,h,g,_,v,y=e((()=>{i(),o(),c(),p=n(),m={title:`Components/Navigation/Link`,component:a,args:{children:`typograaf`,href:`#`},argTypes:{children:d(`The link text.`),color:s,href:l,linkComponent:u},decorators:[(e,{args:t})=>(0,p.jsxs)(r,{color:t.color===`inverse`?`inverse`:void 0,children:[`Jouw `,(0,p.jsx)(e,{}),` biedt mij zulke exquise schreven!`]})]},h={},g={args:{color:`contrast`}},_={args:{color:`inverse`}},h.parameters={...h.parameters,docs:{...h.parameters?.docs,source:{originalSource:`{}`,...h.parameters?.docs?.source}}},g.parameters={...g.parameters,docs:{...g.parameters?.docs,source:{originalSource:`{
+  args: {
+    color: 'contrast'
+  }
+}`,...g.parameters?.docs?.source}}},_.parameters={..._.parameters,docs:{..._.parameters?.docs,source:{originalSource:`{
+  args: {
+    color: 'inverse'
+  }
+}`,..._.parameters?.docs?.source}}},v=[`Default`,`ContrastColour`,`InverseColour`]}));y();export{g as ContrastColour,h as Default,_ as InverseColour,v as __namedExportsOrder,m as default,y as n,f as t};
