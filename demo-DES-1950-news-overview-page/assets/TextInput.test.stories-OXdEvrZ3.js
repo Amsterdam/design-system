05a3e644c59327d7e262402ede9b948e81cfd551
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-30 22:57:38 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@73dcbe4decbd244a8d4f74698d1a232501dd0ad6 🚀

---
+++ demo-DES-1950-news-overview-page/assets/TextInput.test.stories-OXdEvrZ3.js
@@ -0,0 +1,7 @@
+import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-3a7_Ihim.js";import{o as r,t as i}from"./src-COX4OWq3.js";import a,{n as o}from"./TextInput.stories-BX_gWo4K.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Forms/Text Input`},c={render:(e,n)=>t(r,{args:e,variants:[`disabled`,`hovered`]},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(TextInput, {
+    args,
+    variants: ['disabled', 'hovered']
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
