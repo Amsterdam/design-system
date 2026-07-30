df893bd1973d793c914332330e1f021761da808f
GitHub <github-action@users.noreply.github.com>
2026-07-30 23:26:41 +0000

    Remove feature branch folder

--- demo-resolve-dependabot-alerts/assets/Margin.test.stories-BA6z9lvn.js
+++
@@ -1,9 +0,0 @@
-import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{t}from"./jsx-runtime-Bq1bXGty.js";import{P as n,X as r,y as i}from"./index.esm-CCN0_UyI.js";import a,{n as o}from"./Margin.stories-eGhPT1-u.js";var s,c,l,u,d;e((()=>{r(),o(),s=t(),c=[`xs`,`s`,`m`,`l`,`xl`,`2xl`],l={...a,title:`Utilities/CSS/Margin`},u={render:()=>(0,s.jsx)(`div`,{className:`_ams-tests-stack`,children:c.map(e=>(0,s.jsxs)(`div`,{children:[(0,s.jsx)(i,{className:`ams-mb-${e}`,level:2,children:`ams-mb-${e}`}),(0,s.jsx)(n,{children:`It introduces white space between itself and this paragraph.`})]},e))}),tags:[`!dev`,`!autodocs`]},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
-  render: () => <div className="_ams-tests-stack">
-      {sizes.map(size => <div key={size}>
-          <Heading className={\`ams-mb-\${size}\`} level={2}>{\`ams-mb-\${size}\`}</Heading>
-          <Paragraph>It introduces white space between itself and this paragraph.</Paragraph>
-        </div>)}
-    </div>,
-  tags: ['!dev', '!autodocs']
-}`,...u.parameters?.docs?.source}}},d=[`Test`]}))();export{u as Test,d as __namedExportsOrder,l as default};
