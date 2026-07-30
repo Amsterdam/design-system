c2525e9e99ce1c912137ccf3148a34bc74b060f6
GitHub <github-action@users.noreply.github.com>
2026-07-30 17:37:01 +0000

    Remove feature branch folder

--- demo-DES-1755-horizontal-card/assets/SkipLink.test.stories-EVnjJl7v.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-ec30xk80.js";import{D as r,t as i}from"./src-Bbr2riH4.js";import a,{n as o}from"./SkipLink.stories-20ygSlls.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Navigation/Skip Link`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(SkipLink, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
