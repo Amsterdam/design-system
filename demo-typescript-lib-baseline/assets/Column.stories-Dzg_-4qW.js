6a30664b9b71036d3cec4536e3a4e8d2ef6c3b62
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-30 22:49:16 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@9c29fbfe7649fb5560e3bb0a2682702f0ea1c030 🚀

---
+++ demo-typescript-lib-baseline/assets/Column.stories-Dzg_-4qW.js
@@ -0,0 +1,33 @@
+import{n as e,r as t}from"./rolldown-runtime-DaJ6WEGw.js";import{t as n}from"./jsx-runtime-Bq1bXGty.js";import{P as r,X as i,c as a,y as o}from"./index.esm-BF14_nEM.js";import{$t as s,Qt as c,Xt as l,Zt as u,t as d}from"./src-DhWFI1U_.js";import{d as f,t as p}from"./argTypes-CQXDxDHP.js";import{a as m,i as h,r as g}from"./types-Upr6OxRk.js";var _=t({Alignment:()=>x,Default:()=>b,HorizontalAlignment:()=>S,ImproveSemantics:()=>C,__namedExportsOrder:()=>w,default:()=>y}),v,y,b,x,S,C,w,T=e((()=>{i(),d(),s(),h(),f(),v=n(),y={title:`Components/Layout/Column`,component:l,args:{children:[(0,v.jsx)(`div`,{className:`_ams-item`},0),(0,v.jsx)(`div`,{className:`_ams-item`},1),(0,v.jsx)(`div`,{className:`_ams-item`},2)],className:`_ams-column`},argTypes:{align:{control:{labels:{undefined:`start (default)`},type:`select`},options:[void 0,...m]},alignHorizontal:{control:{labels:{undefined:`stretch (default)`},type:`radio`},options:[void 0,...g]},as:p(c),gap:{control:{labels:{undefined:`medium (default)`},type:`select`},options:[void 0,...u]}}},b={},x={args:{align:`center`}},S={args:{alignHorizontal:`center`}},C={args:{as:`section`,children:[(0,v.jsx)(o,{level:1,children:`Zoekresultaten`},1),(0,v.jsxs)(a,{children:[(0,v.jsx)(a.Heading,{level:2,children:(0,v.jsx)(a.Link,{href:`#`,children:`Nieuwe manieren om afval op te halen`})}),(0,v.jsx)(r,{children:`Afvalboten, bakfietsen en ondergrondse containers. We experimenteren met nieuwe manieren om afval op te halen in het centrum.`})]},2),(0,v.jsxs)(a,{children:[(0,v.jsx)(a.Heading,{level:2,children:(0,v.jsx)(a.Link,{href:`#`,children:`Verlenging proef ophalen afval per boot`})}),(0,v.jsx)(r,{children:`Een proef met het anders ophalen van afval. We halen vuilniszakken hier op met kleine wagentjes, kleine vuilniswagens en een afvalboot.`})]},3)],className:void 0}},b.parameters={...b.parameters,docs:{...b.parameters?.docs,source:{originalSource:`{}`,...b.parameters?.docs?.source}}},x.parameters={...x.parameters,docs:{...x.parameters?.docs,source:{originalSource:`{
+  args: {
+    align: 'center'
+  }
+}`,...x.parameters?.docs?.source}}},S.parameters={...S.parameters,docs:{...S.parameters?.docs,source:{originalSource:`{
+  args: {
+    alignHorizontal: 'center'
+  }
+}`,...S.parameters?.docs?.source}}},C.parameters={...C.parameters,docs:{...C.parameters?.docs,source:{originalSource:`{
+  args: {
+    as: 'section',
+    children: [<Heading key={1} level={1}>
+        Zoekresultaten
+      </Heading>, <Card key={2}>
+        <Card.Heading level={2}>
+          <Card.Link href="#">Nieuwe manieren om afval op te halen</Card.Link>
+        </Card.Heading>
+        <Paragraph>
+          Afvalboten, bakfietsen en ondergrondse containers. We experimenteren met nieuwe manieren om afval op te halen
+          in het centrum.
+        </Paragraph>
+      </Card>, <Card key={3}>
+        <Card.Heading level={2}>
+          <Card.Link href="#">Verlenging proef ophalen afval per boot</Card.Link>
+        </Card.Heading>
+        <Paragraph>
+          Een proef met het anders ophalen van afval. We halen vuilniszakken hier op met kleine wagentjes, kleine
+          vuilniswagens en een afvalboot.
+        </Paragraph>
+      </Card>],
+    className: undefined
+  }
+}`,...C.parameters?.docs?.source}}},w=[`Default`,`Alignment`,`HorizontalAlignment`,`ImproveSemantics`]}));T();export{x as Alignment,b as Default,S as HorizontalAlignment,C as ImproveSemantics,w as __namedExportsOrder,y as default,T as n,_ as t};
---
+++ demo-typescript-lib-baseline/assets/Column.test.stories-DNQZSj1t.js
@@ -0,0 +1,7 @@
+import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-IuPGTQZC.js";import{Xt as r,t as i}from"./src-DhWFI1U_.js";import a,{n as o}from"./Column.stories-Dzg_-4qW.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Layout/Column`},c={render:(e,n)=>t(r,{args:e,layout:`grid`},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Column, {
+    args,
+    layout: 'grid'
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
