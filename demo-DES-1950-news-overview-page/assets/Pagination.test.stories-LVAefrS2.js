07afa9e1e3cea1cfd42a2b2fc62de980ee5cb10d
GitHub <github-action@users.noreply.github.com>
2026-07-30 23:10:57 +0000

    Remove feature branch folder

--- demo-DES-1950-news-overview-page/assets/Pagination.test.stories-LVAefrS2.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-3a7_Ihim.js";import{H as r,t as i}from"./src-COX4OWq3.js";import a,{n as o}from"./Pagination.stories-DbZuqwTj.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Navigation/Pagination`},c={render:(e,n)=>t(r,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Pagination, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
