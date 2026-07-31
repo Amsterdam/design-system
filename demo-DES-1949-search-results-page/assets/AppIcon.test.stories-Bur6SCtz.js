2f7151d478f06b344c46672ba76d372526a4387e
GitHub <github-action@users.noreply.github.com>
2026-07-31 00:07:30 +0000

    Remove feature branch folder

--- demo-DES-1949-search-results-page/assets/AppIcon.test.stories-Bur6SCtz.js
+++
@@ -1,9 +0,0 @@
-import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{t}from"./jsx-runtime-Bq1bXGty.js";import{a as n,i as r,n as i,o as a,r as o,t as s}from"./SvgIcon-CylYtrbP.js";import{n as c,t as l}from"./WebAppIcons-CJuvYa5M.js";var u,d,f,p;e((()=>{a(),r(),i(),c(),u=t(),d={title:`Components/Docs/App Icons`},f={render:()=>(0,u.jsxs)(u.Fragment,{children:[(0,u.jsx)(n,{}),(0,u.jsx)(o,{}),(0,u.jsx)(s,{}),(0,u.jsx)(l,{})]}),tags:[`!dev`,`!autodocs`]},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
-  render: () => <>
-      <AppleTouchIcon />
-      <Favicon />
-      <SvgIcon />
-      <WebAppIcons />
-    </>,
-  tags: ['!dev', '!autodocs']
-}`,...f.parameters?.docs?.source}}},p=[`Test`]}))();export{f as Test,p as __namedExportsOrder,d as default};
