06e3434179b7312ace01c8db18166c72d92a99d1
GitHub <github-action@users.noreply.github.com>
2026-07-31 15:08:04 +0000

    Remove feature branch folder

--- demo-page-template-anatomy/assets/Select.test.stories-DBjM9V6h.js
+++
@@ -1,7 +0,0 @@
-import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{n as t,t as n}from"./renderComponentVariants-CkSBOOQa.js";import{n as r,t as i}from"./Select-BRn7e7ax.js";import{c as a,s as o}from"./Select.stories-CPAlunm9.js";var s,c,l;function u(){return(u=e((()=>{r(),n(),o(),s={...a,title:`Components/Forms/Select`},c={render:(e,n)=>t(i,{args:e,variants:[`hovered`,`disabled`]},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Select, {
-    args,
-    variants: ['hovered', 'disabled']
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]})))()}u();export{c as Test,l as __namedExportsOrder,s as default};
