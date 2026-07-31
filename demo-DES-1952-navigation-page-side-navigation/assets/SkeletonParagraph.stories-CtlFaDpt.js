d7c291d70ca4c742443ec8542080693e3fac9a31
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-07-31 23:36:11 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@25bfe4c248e371697df9ccdea8a9b8167081de9d 🚀

---
+++ demo-DES-1952-navigation-page-side-navigation/assets/SkeletonParagraph.stories-CtlFaDpt.js
@@ -0,0 +1,5 @@
+import{n as e,r as t}from"./rolldown-runtime-DkW27tQK.js";import{t as n}from"./jsx-runtime-ATHzeHXA.js";import{n as r,t as i}from"./Skeleton-CMl9EfzM.js";var a=t({Paragraph:()=>c,__namedExportsOrder:()=>l,default:()=>s}),o,s,c,l;function u(){return(u=e((()=>{r(),o=n(),s={title:`Components/Feedback/Skeleton`,component:i.Paragraph,argTypes:{lines:{control:{min:1,step:1,type:`number`}}},render:e=>(0,o.jsx)(i,{children:(0,o.jsx)(i.Paragraph,{...e})})},c={args:{lines:3}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    lines: 3
+  }
+}`,...c.parameters?.docs?.source}}},l=[`Paragraph`]})))()}export{a as n,u as r,c as t};
---
+++ demo-DES-1952-navigation-page-side-navigation/assets/iframe-BbWWmpR4.js
@@ -0,0 +1,1 @@
+import{t as e}from"./iframe-Bd0WlozZ.js";e();
