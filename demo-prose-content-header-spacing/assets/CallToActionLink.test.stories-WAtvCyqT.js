cb864ac37be153ca22d7500d672b0300333e8c15
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-08-01 08:55:57 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@8e0171e8f22a7cddf692aa0be9aaf864811dd7c2 🚀

---
+++ demo-prose-content-header-spacing/assets/CallToActionLink.test.stories-WAtvCyqT.js
@@ -0,0 +1,6 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{n as t,t as n}from"./renderComponentVariants-Bc3zNblj.js";import{n as r,t as i}from"./CallToActionLink-FktyIhM3.js";import{n as a,r as o}from"./CallToActionLink.stories-CljZU5cA.js";var s,c,l;function u(){return(u=e((()=>{r(),n(),a(),s={...o,title:`Components/Navigation/Call to Action Link`},c={render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(CallToActionLink, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]})))()}u();export{c as Test,l as __namedExportsOrder,s as default};
---
+++ demo-prose-content-header-spacing/assets/iframe-Cfw6YNDr.js
@@ -0,0 +1,1 @@
+import{t as e}from"./iframe-Y2ztxcbS.js";e();
