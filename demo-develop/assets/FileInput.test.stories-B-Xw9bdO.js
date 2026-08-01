80298247271c4b66fe24a1237cf2d0b834f2c982
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-08-01 00:38:20 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@648b8b01b60e3e9dcd83b0acefbfb212aec3a86b 🚀

---
+++ demo-develop/assets/FileInput.test.stories-B-Xw9bdO.js
@@ -0,0 +1,7 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{n as t,t as n}from"./renderComponentVariants-_MM1FJ9W.js";import{n as r,t as i}from"./FileInput-rdoirkh1.js";import{o as a,s as o}from"./FileInput.stories-DD2dLmJx.js";var s,c,l;function u(){return(u=e((()=>{r(),n(),a(),s={...o,title:`Components/Forms/File Input`},c={render:(e,n)=>t(i,{args:e,variants:[`disabled`,`multiple`]},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(FileInput, {
+    args,
+    variants: ['disabled', 'multiple']
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]})))()}u();export{c as Test,l as __namedExportsOrder,s as default};
