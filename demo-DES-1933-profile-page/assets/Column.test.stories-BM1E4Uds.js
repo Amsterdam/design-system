bde192f55dd8f226efca160b8d98b2f8834192c3
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-08-02 22:55:04 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@b9096dc3e2a928a0a4b774e265a6be33ce0da4e5 🚀

--- demo-DES-1933-profile-page/assets/Column.test.stories-BM1E4Uds.js
+++
@@ -1,7 +0,0 @@
-import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./renderComponentVariants-D2vdKZuO.js";import{Xt as r,t as i}from"./src-C47T_RZP.js";import a,{n as o}from"./Column.stories-Dpx8NiAo.js";var s,c,l;e((()=>{i(),n(),o(),s={...a,title:`Components/Layout/Column`},c={render:(e,n)=>t(r,{args:e,layout:`grid`},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Column, {
-    args,
-    layout: 'grid'
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]}))();export{c as Test,l as __namedExportsOrder,s as default};
---
+++ demo-DES-1933-profile-page/assets/iframe-p_2x1fSf.js
@@ -0,0 +1,1 @@
+import{t as e}from"./iframe-Ctj_wAn4.js";e();
