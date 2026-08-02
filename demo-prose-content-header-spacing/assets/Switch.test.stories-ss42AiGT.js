6975f6532e14f5116771bb051c22313675a367c4
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-08-02 09:51:59 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@35dba6c859eebaf7b0af8d4e5638dd7b8d4fb846 🚀

--- demo-prose-content-header-spacing/assets/Switch.test.stories-ss42AiGT.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{n as t,t as n}from"./renderComponentVariants-R36eOaCf.js";import{n as r,t as i}from"./Switch-BWcJ0Jm8.js";import{i as a,r as o}from"./Switch.stories-BatLA2w3.js";var s,c,l;function u(){return(u=e((()=>{r(),n(),o(),s={...a,title:`Components/Forms/Switch`},c={render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Switch, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]})))()}u();export{c as Test,l as __namedExportsOrder,s as default};
