a602649ba49569f31e50548e63c2f130027e3c16
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-08-02 22:12:15 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@37936127f8072d882adfa8de65cc9172f20113c4 🚀

---
+++ demo-subgrid-gap/assets/Column.test.stories-CGwixdte.js
@@ -0,0 +1,7 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{n as t,t as n}from"./renderComponentVariants-BOH053T0.js";import{i as r,t as i}from"./Column-CYFhivDV.js";import{a,o}from"./Column.stories-BlKWa2_m.js";var s,c,l;function u(){return(u=e((()=>{r(),n(),a(),s={...o,title:`Components/Layout/Column`},c={render:(e,n)=>t(i,{args:e,layout:`grid`},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Column, {
+    args,
+    layout: 'grid'
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]})))()}u();export{c as Test,l as __namedExportsOrder,s as default};
