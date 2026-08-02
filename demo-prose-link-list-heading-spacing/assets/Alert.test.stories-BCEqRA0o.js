205bd23a80467d56ff9ac02ebb8473d8c65abac4
GitHub <github-action@users.noreply.github.com>
2026-08-02 13:07:47 +0000

    Remove feature branch folder

--- demo-prose-link-list-heading-spacing/assets/Alert.test.stories-BCEqRA0o.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{n as t,t as n}from"./renderComponentVariants-HkRdzxLg.js";import{n as r,t as i}from"./Alert-C2-L0j2q.js";import{c as a,s as o}from"./Alert.stories-B-S6Y9-a.js";var s,c,l;function u(){return(u=e((()=>{r(),n(),o(),s={...a,title:`Components/Feedback/Alert`},c={render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Alert, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]})))()}u();export{c as Test,l as __namedExportsOrder,s as default};
