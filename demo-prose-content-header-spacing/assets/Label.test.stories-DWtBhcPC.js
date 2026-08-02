e8908c146b8e6ff07dfbe6553d9c6fc81f492c18
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-08-02 10:37:07 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@4a160f00d052ecb0d5bce88bf74b4f0293017d08 🚀

---
+++ demo-prose-content-header-spacing/assets/Label.test.stories-DWtBhcPC.js
@@ -0,0 +1,6 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{n as t,t as n}from"./renderComponentVariants-DQP4InVx.js";import{n as r,t as i}from"./Label-Dh6NBDii.js";import{o as a,s as o}from"./Label.stories-5k5J8bxN.js";var s,c,l;function u(){return(u=e((()=>{r(),n(),a(),s={...o,title:`Components/Forms/Label`},c={render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Label, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]})))()}u();export{c as Test,l as __namedExportsOrder,s as default};
