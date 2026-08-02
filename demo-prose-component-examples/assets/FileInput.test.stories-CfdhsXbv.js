9660b40703de9f49665a37fcc18a75ca07bd6548
GitHub <github-action@users.noreply.github.com>
2026-08-02 14:43:34 +0000

    Remove feature branch folder

--- demo-prose-component-examples/assets/FileInput.test.stories-CfdhsXbv.js
+++
@@ -1,7 +0,0 @@
-import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{n as t,t as n}from"./renderComponentVariants-Diauaemm.js";import{n as r,t as i}from"./FileInput-C2g8RwgN.js";import{o as a,s as o}from"./FileInput.stories-DHnOySNo.js";var s,c,l;function u(){return(u=e((()=>{r(),n(),a(),s={...o,title:`Components/Forms/File Input`},c={render:(e,n)=>t(i,{args:e,variants:[`disabled`,`multiple`]},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(FileInput, {
-    args,
-    variants: ['disabled', 'multiple']
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]})))()}u();export{c as Test,l as __namedExportsOrder,s as default};
--- demo-prose-component-examples/assets/TextInput.test.stories-B_fVQc47.js
+++
@@ -1,7 +0,0 @@
-import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{n as t,t as n}from"./renderComponentVariants-Diauaemm.js";import{n as r,t as i}from"./TextInput-DJZifHJg.js";import{f as a,p as o}from"./TextInput.stories-BO6xxwfk.js";var s,c,l;function u(){return(u=e((()=>{r(),n(),a(),s={...o,title:`Components/Forms/Text Input`},c={render:(e,n)=>t(i,{args:e,variants:[`disabled`,`hovered`]},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(TextInput, {
-    args,
-    variants: ['disabled', 'hovered']
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]})))()}u();export{c as Test,l as __namedExportsOrder,s as default};
