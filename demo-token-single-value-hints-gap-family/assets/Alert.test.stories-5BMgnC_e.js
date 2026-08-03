7852b51fa591763788f61623554aa87a81b94893
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-08-03 16:53:40 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@fa0fb00b6464fcee792f98e0e6b08e84d074647a 🚀

---
+++ demo-token-single-value-hints-gap-family/assets/Alert.test.stories-5BMgnC_e.js
@@ -0,0 +1,6 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{n as t,t as n}from"./renderComponentVariants-BJOLHwDR.js";import{n as r,t as i}from"./Alert-CyYil7Ce.js";import{c as a,s as o}from"./Alert.stories-DhcLMpZJ.js";var s,c,l;function u(){return(u=e((()=>{r(),n(),o(),s={...a,title:`Components/Feedback/Alert`},c={render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Alert, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]})))()}u();export{c as Test,l as __namedExportsOrder,s as default};
---
+++ demo-token-single-value-hints-gap-family/assets/iframe-CT0CK9LJ.js
@@ -0,0 +1,1 @@
+import{t as e}from"./iframe-DYrMJ-ut.js";e();
