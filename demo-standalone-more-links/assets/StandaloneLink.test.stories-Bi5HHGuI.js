eb50f44472463e0e7f72be09479d8c09c8c29088
GitHub <github-action@users.noreply.github.com>
2026-08-03 20:54:07 +0000

    Remove feature branch folder

--- demo-standalone-more-links/assets/StandaloneLink.test.stories-Bi5HHGuI.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{n as t,t as n}from"./renderComponentVariants-DyzE4Uv6.js";import{n as r,t as i}from"./StandaloneLink-C7JJzOvU.js";import{o as a,s as o}from"./StandaloneLink.stories-CnnuTLg5.js";var s,c,l;function u(){return(u=e((()=>{r(),n(),a(),s={...o,title:`Components/Navigation/Standalone Link`},c={render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(StandaloneLink, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]})))()}u();export{c as Test,l as __namedExportsOrder,s as default};
--- demo-standalone-more-links/assets/iframe-o_hpoDFl.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./iframe-CMpOem5L.js";e();
