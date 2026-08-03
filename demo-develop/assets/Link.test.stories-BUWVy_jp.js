9a320e86dde2440962bdae416c5840450cdefa4a
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-08-03 10:41:50 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@09907cbed5e2902e4b9d135d16c467e6edfd764d 🚀

---
+++ demo-develop/assets/Link.test.stories-BUWVy_jp.js
@@ -0,0 +1,6 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{n as t,t as n}from"./renderComponentVariants-CQKdUl5H.js";import{n as r,t as i}from"./Link-CMhZesjJ.js";import{a,i as o}from"./Link.stories-RJn3wtzV.js";var s,c,l;function u(){return(u=e((()=>{r(),n(),o(),s={...a,title:`Components/Navigation/Link`},c={render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Link, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]})))()}u();export{c as Test,l as __namedExportsOrder,s as default};
--- demo-develop/assets/iframe-BsdXGPbc.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./iframe-DYPM7hTT.js";e();
