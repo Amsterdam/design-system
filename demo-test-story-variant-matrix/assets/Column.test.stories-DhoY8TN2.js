1faa79dd6b70eadafa7ea503df83e9aed8b320f3
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-08-04 23:47:57 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@b066acb0038be064db088d1ece3f60cb7c5c29bd 🚀

--- demo-test-story-variant-matrix/assets/Column.test.stories-DhoY8TN2.js
+++
@@ -1,7 +0,0 @@
-import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{n as t,t as n}from"./renderComponentVariants-D-zIRr9e.js";import{i as r,t as i}from"./Column-BEDjCW6D.js";import{a,o}from"./Column.stories-B_R1c1OS.js";var s,c,l;function u(){return(u=e((()=>{r(),n(),a(),s={...o,title:`Components/Layout/Column`},c={render:(e,n)=>t(i,{args:e,layout:`grid`},n),tags:[`!dev`,`!autodocs`,`!manifest`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Column, {
-    args,
-    layout: 'grid'
-  }, context),
-  tags: ['!dev', '!autodocs', '!manifest']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]})))()}u();export{c as Test,l as __namedExportsOrder,s as default};
---
+++ demo-test-story-variant-matrix/assets/iframe-BFiVQzHm.js
@@ -0,0 +1,1 @@
+import{t as e}from"./iframe-C6fdcPmk.js";e();
