9dcb01775430748b5e5fc9d51bc38b461094adfc
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-08-04 23:27:52 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@731812009b26cb9fb4fb494ae18ed80066414530 🚀

---
+++ demo-test-story-variant-matrix/assets/Grid.test.stories-Bv_mAZCD.js
@@ -0,0 +1,20 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{t}from"./jsx-runtime-ATHzeHXA.js";import{n,t as r}from"./renderComponentVariants-WgV5U2Im.js";import{i,t as a}from"./Grid-4_rIi-xc.js";import{o,s}from"./Grid.stories-BADnz0xz.js";var c,l,u,d,f,p,m,h,g,_,v,y,b,x,S,C;function w(){return(w=e((()=>{i(),r(),o(),c=t(),l={...s,title:`Components/Layout/Grid`},u={className:`_ams-item`,style:{blockSize:`3rem`}},d={narrow:1,medium:2,wide:3},f={narrow:2,medium:4,wide:6},p={narrow:3,medium:6,wide:9},m={narrow:2,medium:3,wide:4},h={narrow:3,medium:5,wide:7},g={narrow:4,medium:7,wide:10},_=({gapVertical:e,subgridGapVertical:t})=>(0,c.jsxs)(a,{gapVertical:e,paddingVertical:`large`,children:[(0,c.jsx)(a.Cell,{...u,span:d}),(0,c.jsx)(a.Cell,{...u,span:d}),(0,c.jsx)(a.Cell,{...u,span:d}),(0,c.jsx)(a.Cell,{...u,span:d}),(0,c.jsx)(a.Cell,{...u,span:d}),(0,c.jsxs)(a.Subgrid,{gapVertical:t,span:p,start:m,children:[(0,c.jsx)(a.Cell,{...u,span:d}),(0,c.jsx)(a.Cell,{...u,span:d}),(0,c.jsx)(a.Cell,{...u,span:d}),(0,c.jsx)(a.Cell,{...u,span:d}),(0,c.jsx)(a.Cell,{...u,span:d}),(0,c.jsx)(a.Cell,{...u,span:d})]})]}),v=()=>(0,c.jsxs)(a,{paddingVertical:`large`,children:[(0,c.jsx)(a.Cell,{...u,span:d}),(0,c.jsx)(a.Cell,{...u,span:d}),(0,c.jsx)(a.Cell,{...u,span:d}),(0,c.jsx)(a.Cell,{...u,span:d}),(0,c.jsxs)(a.Subgrid,{span:`all`,children:[(0,c.jsx)(a.Cell,{...u,span:d}),(0,c.jsx)(a.Cell,{...u,span:d}),(0,c.jsx)(a.Cell,{...u,span:d}),(0,c.jsx)(a.Cell,{...u,span:d})]}),(0,c.jsxs)(a.Subgrid,{span:f,children:[(0,c.jsx)(a.Cell,{...u,span:d}),(0,c.jsx)(a.Cell,{...u,span:d})]}),(0,c.jsxs)(a.Subgrid,{span:f,start:h,children:[(0,c.jsx)(a.Cell,{...u,span:d}),(0,c.jsx)(a.Cell,{...u,span:d})]})]}),y=()=>(0,c.jsxs)(a,{paddingVertical:`large`,children:[(0,c.jsx)(a.Cell,{...u,span:d}),(0,c.jsxs)(a.Subgrid,{rowSpan:2,span:p,start:m,children:[(0,c.jsx)(a.Cell,{...u,span:d}),(0,c.jsx)(a.Cell,{...u,span:d}),(0,c.jsx)(a.Cell,{...u,span:d}),(0,c.jsx)(a.Cell,{...u,span:d}),(0,c.jsx)(a.Cell,{...u,span:d}),(0,c.jsx)(a.Cell,{...u,span:d})]}),(0,c.jsx)(a.Cell,{...u,span:d})]}),b=()=>(0,c.jsxs)(a,{paddingVertical:`large`,children:[(0,c.jsx)(a.Cell,{...u,rowStart:1,span:d,start:g}),(0,c.jsx)(a.Cell,{...u,span:p}),(0,c.jsx)(a.Cell,{...u,span:p})]}),x=()=>(0,c.jsxs)(a,{paddingVertical:`large`,children:[(0,c.jsx)(a.Cell,{...u,rowStart:1,span:d,start:g}),(0,c.jsxs)(a.Subgrid,{span:p,children:[(0,c.jsx)(a.Cell,{...u,span:d}),(0,c.jsx)(a.Cell,{...u,span:d})]})]}),S={args:{children:[(0,c.jsx)(a.Cell,{span:`all`},1),(0,c.jsx)(a.Cell,{span:`all`},2)]},render:(e,t)=>(0,c.jsxs)(`div`,{className:`_ams-tests-stack`,children:[n(a,{args:e},t),(0,c.jsx)(_,{}),(0,c.jsx)(_,{gapVertical:`none`}),(0,c.jsx)(_,{subgridGapVertical:`2x-large`}),(0,c.jsx)(_,{gapVertical:`none`,subgridGapVertical:`x-large`}),(0,c.jsx)(v,{}),(0,c.jsx)(y,{}),(0,c.jsx)(b,{}),(0,c.jsx)(x,{})]}),tags:[`!dev`,`!autodocs`,`!manifest`]},S.parameters={...S.parameters,docs:{...S.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<Grid.Cell key={1} span="all" />, <Grid.Cell key={2} span="all" />]
+  },
+  render: (args, context) => <div className="_ams-tests-stack">
+      {renderComponentVariants(Grid, {
+      args
+    }, context)}
+      <SubgridCase />
+      <SubgridCase gapVertical="none" />
+      <SubgridCase subgridGapVertical="2x-large" />
+      {/* The reason x-large exists: the Grid drops its gap, and the Subgrid puts the regular one back. */}
+      <SubgridCase gapVertical="none" subgridGapVertical="x-large" />
+      <SubgridPlacementCase />
+      <SubgridRowSpanCase />
+      <RowStartCase />
+      <SubgridRowStartCase />
+    </div>,
+  tags: ['!dev', '!autodocs', '!manifest']
+}`,...S.parameters?.docs?.source}}},C=[`Test`]})))()}w();export{S as Test,C as __namedExportsOrder,l as default};
---
+++ demo-test-story-variant-matrix/assets/TextInput.test.stories-DdVYA1IB.js
@@ -0,0 +1,7 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{n as t,t as n}from"./renderComponentVariants-WgV5U2Im.js";import{n as r,t as i}from"./TextInput-pIHUz67T.js";import{f as a,p as o}from"./TextInput.stories-BcSycOKZ.js";var s,c,l;function u(){return(u=e((()=>{r(),n(),a(),s={...o,title:`Components/Forms/Text Input`},c={render:(e,n)=>t(i,{args:e,variants:[`disabled`,`hovered`]},n),tags:[`!dev`,`!autodocs`,`!manifest`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(TextInput, {
+    args,
+    variants: ['disabled', 'hovered']
+  }, context),
+  tags: ['!dev', '!autodocs', '!manifest']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]})))()}u();export{c as Test,l as __namedExportsOrder,s as default};
