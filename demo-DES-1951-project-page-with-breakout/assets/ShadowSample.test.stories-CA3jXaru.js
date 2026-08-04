389f1eb965aa47112abd625910c133bdf1ff2699
GitHub <github-action@users.noreply.github.com>
2026-08-04 06:41:06 +0000

    Remove feature branch folder

--- demo-DES-1951-project-page-with-breakout/assets/ShadowSample.test.stories-CA3jXaru.js
+++
@@ -1,8 +0,0 @@
-import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{t}from"./jsx-runtime-ATHzeHXA.js";import{n,t as r}from"./ShadowSample-DSkSVMFW.js";var i,a,o,s;function c(){return(c=e((()=>{n(),i=t(),a={title:`Components/Docs/Shadow Sample`},o={render:()=>(0,i.jsxs)(`div`,{className:`_ams-tests-grid`,children:[(0,i.jsx)(r,{value:`0 0 0 0.125rem hotpink`}),(0,i.jsx)(r,{value:`inset 0 -0.125rem 0 0 rebeccapurple`}),(0,i.jsx)(r,{value:`4px 4px 8px 0 rgba(0,0,0,0.25)`})]}),tags:[`!dev`,`!autodocs`,`!manifest`]},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
-  render: () => <div className="_ams-tests-grid">
-      <ShadowSample value="0 0 0 0.125rem hotpink" />
-      <ShadowSample value="inset 0 -0.125rem 0 0 rebeccapurple" />
-      <ShadowSample value="4px 4px 8px 0 rgba(0,0,0,0.25)" />
-    </div>,
-  tags: ['!dev', '!autodocs', '!manifest']
-}`,...o.parameters?.docs?.source}}},s=[`Test`]})))()}c();export{o as Test,s as __namedExportsOrder,a as default};
--- demo-DES-1951-project-page-with-breakout/assets/Switch.test.stories-CSyIMi9n.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{n as t,t as n}from"./renderComponentVariants-sn28Ps1r.js";import{n as r,t as i}from"./Switch-bNnX769x.js";import{i as a,r as o}from"./Switch.stories-DW3sE3sT.js";var s,c,l;function u(){return(u=e((()=>{r(),n(),o(),s={...a,title:`Components/Forms/Switch`},c={render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`,`!manifest`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Switch, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs', '!manifest']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]})))()}u();export{c as Test,l as __namedExportsOrder,s as default};
