3c0b86909cf22fa811e7ff220e854879c730d6e0
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-08-04 22:51:51 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@bc4b6d72bb8e73149a0905462c3ee19388996b7e 🚀

---
+++ demo-compact-mode-docs-anchor/assets/Avatar.test.stories-Bgy4CCCH.js
@@ -0,0 +1,6 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{n as t,t as n}from"./renderComponentVariants-BoMnT5j-.js";import{r,t as i}from"./Avatar-DMR0sqOW.js";import{a,i as o}from"./Avatar.stories-DiP3pv7u.js";var s,c,l;function u(){return(u=e((()=>{r(),n(),o(),s={...a,title:`Components/Feedback/Avatar`},c={render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`,`!manifest`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Avatar, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs', '!manifest']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]})))()}u();export{c as Test,l as __namedExportsOrder,s as default};
---
+++ demo-compact-mode-docs-anchor/assets/iframe-D1Dq_yHO.js
@@ -0,0 +1,1 @@
+import{t as e}from"./iframe-_IvOA4IB.js";e();
