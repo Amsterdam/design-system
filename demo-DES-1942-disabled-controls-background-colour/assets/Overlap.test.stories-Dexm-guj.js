2605edd93180489abd3d3ec06a22d5808be1b05b
GitHub <github-action@users.noreply.github.com>
2026-08-04 22:04:59 +0000

    Remove feature branch folder

--- demo-DES-1942-disabled-controls-background-colour/assets/Overlap.test.stories-Dexm-guj.js
+++
@@ -1,9 +0,0 @@
-import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{t}from"./jsx-runtime-ATHzeHXA.js";import{n,t as r}from"./renderComponentVariants-C_HCcKBF.js";import{a as i,i as a,o,r as s}from"./Overlap.stories-6S7jgRAi.js";var c,l,u,d;function f(){return(f=e((()=>{o(),r(),s(),c=t(),l={...a,title:`Components/Layout/Overlap`},u={args:{children:[(0,c.jsx)(`img`,{alt:``,src:`https://picsum.photos/id/122/1280/720`},1),(0,c.jsx)(`p`,{children:`Overlap test`},2)]},render:(e,t)=>n(i,{args:e},t),tags:[`!dev`,`!autodocs`,`!manifest`]},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: [<img alt="" key={1} src="https://picsum.photos/id/122/1280/720" />, <p key={2}>Overlap test</p>]
-  },
-  render: (args, context) => renderComponentVariants(Overlap, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs', '!manifest']
-}`,...u.parameters?.docs?.source}}},d=[`Test`]})))()}f();export{u as Test,d as __namedExportsOrder,l as default};
