770b920ffa6d60691b625538fb90cdc5a61d775e
GitHub <github-action@users.noreply.github.com>
2026-08-04 09:02:31 +0000

    Remove feature branch folder

--- demo-page-template-descriptions-follow-up/assets/TimeInput.test.stories-CdbnE74h.js
+++
@@ -1,7 +0,0 @@
-import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{n as t,t as n}from"./renderComponentVariants-TjhN7ZLR.js";import{n as r,t as i}from"./TimeInput-iq0kxv24.js";import{o as a,s as o}from"./TimeInput.stories-BHgSLsHw.js";var s,c,l;function u(){return(u=e((()=>{r(),n(),a(),s={...o,title:`Components/Forms/Time Input`},c={render:(e,n)=>t(i,{args:e,variants:[`disabled`,`hovered`]},n),tags:[`!dev`,`!autodocs`,`!manifest`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(TimeInput, {
-    args,
-    variants: ['disabled', 'hovered']
-  }, context),
-  tags: ['!dev', '!autodocs', '!manifest']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]})))()}u();export{c as Test,l as __namedExportsOrder,s as default};
