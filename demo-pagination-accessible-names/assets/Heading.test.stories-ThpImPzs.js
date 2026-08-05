92630e4de56e2b81b1f44ab8347e8832885bfec6
GitHub <github-action@users.noreply.github.com>
2026-08-05 11:51:38 +0000

    Remove feature branch folder

--- demo-pagination-accessible-names/assets/Heading.test.stories-ThpImPzs.js
+++
@@ -1,9 +0,0 @@
-import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{n as t,t as n}from"./renderComponentVariants-DaE5jenG.js";import{r,t as i}from"./Heading-C0pVzqhb.js";import{l as a,u as o}from"./Heading.stories-CR0-e_bp.js";var s,c,l;function u(){return(u=e((()=>{r(),n(),a(),s={...o,title:`Components/Text/Heading`},c={args:{children:`Amsterdam Light Festival maakt de stad ook deze winter een beetje mooier`},render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`,`!manifest`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: 'Amsterdam Light Festival maakt de stad ook deze winter een beetje mooier'
-  },
-  render: (args, context) => renderComponentVariants(Heading, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs', '!manifest']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]})))()}u();export{c as Test,l as __namedExportsOrder,s as default};
--- demo-pagination-accessible-names/assets/iframe-DBjQ74ST.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./iframe-C3S30xu8.js";e();
