7c966d976f36561134e39f009b05c842e8d8e84a
GitHub <github-action@users.noreply.github.com>
2026-08-05 09:19:36 +0000

    Remove feature branch folder

--- demo-test-story-variant-matrix/assets/IconButton.test.stories-DZREShox.js
+++
@@ -1,7 +0,0 @@
-import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{n as t,t as n}from"./renderComponentVariants-D_51l_g0.js";import{n as r,t as i}from"./IconButton-e_7wa_73.js";import{a,i as o}from"./IconButton.stories-BQt4R0DP.js";var s,c,l;function u(){return(u=e((()=>{r(),n(),o(),s={...a,title:`Components/Buttons/Icon Button`},c={render:(e,n)=>t(i,{args:e,variants:[`hovered`,`disabled`]},n),tags:[`!dev`,`!autodocs`,`!manifest`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(IconButton, {
-    args,
-    variants: ['hovered', 'disabled']
-  }, context),
-  tags: ['!dev', '!autodocs', '!manifest']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]})))()}u();export{c as Test,l as __namedExportsOrder,s as default};
