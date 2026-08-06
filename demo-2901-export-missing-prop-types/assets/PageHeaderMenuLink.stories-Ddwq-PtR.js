f8e3c85c6ede37c02ca6781a2f4d981ea14afeb7
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-08-06 23:09:16 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@d39009e9c1c9d35c8ccfa92482fe673cef58728d 🚀

--- demo-2901-export-missing-prop-types/assets/PageHeaderMenuLink.stories-Ddwq-PtR.js
+++
@@ -1,6 +0,0 @@
-import{n as e,r as t}from"./rolldown-runtime-DkW27tQK.js";import{t as n}from"./jsx-runtime-ATHzeHXA.js";import{n as r,t as i}from"./PageHeader-BqQpT6ut.js";import{d as a,l as o,p as s,r as c}from"./argTypes-CTYEle7b.js";import{n as l,t as u}from"./iconArgTypes-CoU157b8.js";var d=t({MenuLink:()=>m,__namedExportsOrder:()=>h,default:()=>p}),f,p,m,h;function g(){return(g=e((()=>{r(),a(),l(),f=n(),p={title:`Components/Containers/Page Header`,component:i.MenuLink,argTypes:{children:c(`The link text.`),href:o,icon:u(),linkComponent:s},decorators:[e=>(0,f.jsx)(i,{menuItems:(0,f.jsx)(e,{})})],render:({children:e,...t})=>(0,f.jsx)(i.MenuLink,{...t,children:e}),tags:[`!manifest`]},m={args:{children:`Zoeken`,href:`#`}},m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: 'Zoeken',
-    href: '#'
-  }
-}`,...m.parameters?.docs?.source}}},h=[`MenuLink`]})))()}export{d as n,g as r,m as t};
