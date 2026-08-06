8fcecceb12db876579adfd8242f0a67585afe355
GitHub <github-action@users.noreply.github.com>
2026-08-06 11:39:00 +0000

    Remove feature branch folder

--- demo-data-page-example/assets/Avatar.test.stories-BN0JoTNz.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{n as t,t as n}from"./renderComponentVariants-4P1ua4w9.js";import{r,t as i}from"./Avatar-DuSG8vlc.js";import{a,i as o}from"./Avatar.stories-BmS2yi7i.js";var s,c,l;function u(){return(u=e((()=>{r(),n(),o(),s={...a,title:`Components/Feedback/Avatar`},c={render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`,`!manifest`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Avatar, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs', '!manifest']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]})))()}u();export{c as Test,l as __namedExportsOrder,s as default};
--- demo-data-page-example/assets/PasswordInput.test.stories-CkoMDS_s.js
+++
@@ -1,7 +0,0 @@
-import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{n as t,t as n}from"./renderComponentVariants-4P1ua4w9.js";import{n as r,t as i}from"./PasswordInput-CY9vCxNR.js";import{a,o}from"./PasswordInput.stories-Bs0lf1QL.js";var s,c,l;function u(){return(u=e((()=>{r(),n(),a(),s={...o,title:`Components/Forms/Password Input`},c={render:(e,n)=>t(i,{args:e,variants:[`disabled`]},n),tags:[`!dev`,`!autodocs`,`!manifest`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(PasswordInput, {
-    args,
-    variants: ['disabled']
-  }, context),
-  tags: ['!dev', '!autodocs', '!manifest']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]})))()}u();export{c as Test,l as __namedExportsOrder,s as default};
