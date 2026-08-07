f9e7d3384dbbce96df4f6ca1e564498c90dcffa3
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-08-07 06:44:32 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@016eec011fbd46ed57a2d01da803421c4d418b28 🚀

---
+++ demo-develop/assets/Label.test.stories-DEIxwlnJ.js
@@ -0,0 +1,6 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{n as t,t as n}from"./renderComponentVariants-WjtxG_UB.js";import{n as r,t as i}from"./Label-Doa5K2kC.js";import{o as a,s as o}from"./Label.stories-BEqwGJiC.js";var s,c,l;function u(){return(u=e((()=>{r(),n(),a(),s={...o,title:`Components/Forms/Label`},c={render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`,`!manifest`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Label, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs', '!manifest']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]})))()}u();export{c as Test,l as __namedExportsOrder,s as default};
---
+++ demo-develop/assets/iframe-S2Ob3ZXQ.js
@@ -0,0 +1,1 @@
+import{t as e}from"./iframe-BMX2XhL7.js";e();
