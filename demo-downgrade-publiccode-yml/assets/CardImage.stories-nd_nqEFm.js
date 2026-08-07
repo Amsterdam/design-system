4d381168cfe5331a2befdac49bb624822fa1dfe8
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-08-07 15:08:11 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@a7ff6ba5c8af6f39925c59b263cba13665ecac67 🚀

---
+++ demo-downgrade-publiccode-yml/assets/CardImage.stories-nd_nqEFm.js
@@ -0,0 +1,13 @@
+import{n as e,r as t}from"./rolldown-runtime-DkW27tQK.js";import{t as n}from"./jsx-runtime-ATHzeHXA.js";import{n as r,t as i}from"./Card-tzdQoqOz.js";import{n as a,t as o}from"./decorators-BFz15WR2.js";import{i as s,t as c}from"./types-C0F630hP.js";var l=t({Image:()=>f,__namedExportsOrder:()=>p,default:()=>d}),u,d,f,p;function m(){return(m=e((()=>{r(),s(),o(),u=n(),d={title:`Components/Navigation/Card`,component:i.Image,argTypes:{aspectRatio:{control:{labels:{undefined:`none`},type:`select`},options:[void 0,...c]}},decorators:[e=>(0,u.jsx)(i,{children:(0,u.jsx)(e,{})})],tags:[`!manifest`]},f={args:{alt:``,aspectRatio:`16:9`,src:`https://picsum.photos/800/450`},argTypes:{alt:{description:"A textual description of the content of the image. Should be empty for `Card.Image`."}},decorators:[a(`24rem`)]},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
+  args: {
+    alt: '',
+    aspectRatio: '16:9',
+    src: 'https://picsum.photos/800/450'
+  },
+  argTypes: {
+    alt: {
+      description: 'A textual description of the content of the image. Should be empty for \`Card.Image\`.'
+    }
+  },
+  decorators: [maximiseInlineSize('24rem')]
+}`,...f.parameters?.docs?.source}}},p=[`Image`]})))()}export{f as n,m as r,l as t};
---
+++ demo-downgrade-publiccode-yml/assets/iframe-DNNR4wHP.js
@@ -0,0 +1,1 @@
+import{t as e}from"./iframe-DaiPnulG.js";e();
