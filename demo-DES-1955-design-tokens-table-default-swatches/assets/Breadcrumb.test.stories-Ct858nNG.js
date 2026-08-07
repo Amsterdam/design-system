a31b811c6c130ed677f9ec6ecf4aea54b76ee9fa
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-08-07 06:16:28 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@3853068c65ff9c300c33ae379b227060d904df09 🚀

---
+++ demo-DES-1955-design-tokens-table-default-swatches/assets/Breadcrumb.test.stories-Ct858nNG.js
@@ -0,0 +1,11 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{t}from"./jsx-runtime-ATHzeHXA.js";import{n,t as r}from"./Breadcrumb-BTgv7o5T.js";import{n as i,r as a}from"./Breadcrumb.stories-DXEMe3dX.js";var o,s,c,l;function u(){return(u=e((()=>{n(),i(),o=t(),s={...a,title:`Components/Navigation/Breadcrumb`},c={render:()=>(0,o.jsxs)(r,{children:[(0,o.jsx)(r.Link,{href:`#`,children:`Home`}),(0,o.jsx)(r.Link,{href:`#`,children:`Afval`}),(0,o.jsx)(r.Link,{href:`#`,children:`Bedrijfsafval`}),(0,o.jsx)(r.Link,{className:`hover`,href:`#`,children:`Recyclepunten voor bedrijven`})]}),tags:[`!dev`,`!autodocs`,`!manifest`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: () => <Breadcrumb>
+      <Breadcrumb.Link href="#">Home</Breadcrumb.Link>
+      <Breadcrumb.Link href="#">Afval</Breadcrumb.Link>
+      <Breadcrumb.Link href="#">Bedrijfsafval</Breadcrumb.Link>
+      <Breadcrumb.Link className="hover" href="#">
+        Recyclepunten voor bedrijven
+      </Breadcrumb.Link>
+    </Breadcrumb>,
+  tags: ['!dev', '!autodocs', '!manifest']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]})))()}u();export{c as Test,l as __namedExportsOrder,s as default};
---
+++ demo-DES-1955-design-tokens-table-default-swatches/assets/iframe-KGUKVag3.js
@@ -0,0 +1,1 @@
+import{t as e}from"./iframe-FopT8726.js";e();
