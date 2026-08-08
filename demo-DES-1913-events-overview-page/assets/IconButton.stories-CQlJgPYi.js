d1db7354e4fa0a1f7b641ed5d0f2e5e38d833f57
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-08-08 09:32:12 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@9cd5757f200ea4f114fc7fc2d28cc26f220e7d00 🚀

--- demo-DES-1913-events-overview-page/assets/IconButton.stories-CQlJgPYi.js
+++
@@ -1,9 +0,0 @@
-import{n as e,r as t}from"./rolldown-runtime-DkW27tQK.js";import{n,r}from"./Icon-CxMynesA.js";import{n as i,t as a}from"./IconButton-DzcLuniE.js";import{a as o,d as s,s as c}from"./argTypes-CTYEle7b.js";import{n as l,t as u}from"./iconArgTypes-CoU157b8.js";var d=t({ContrastColour:()=>m,Default:()=>p,InverseColour:()=>h,__namedExportsOrder:()=>g,default:()=>f}),f,p,m,h,g;function _(){return(_=e((()=>{i(),r(),s(),l(),f={title:`Components/Buttons/Icon Button`,component:a,args:{disabled:!1,label:`Sluiten`},argTypes:{color:o,disabled:c,size:{control:{labels:{undefined:`medium body text (default)`},type:`select`},options:[void 0,...n]},svg:u(`CloseIcon`)}},p={},m={args:{color:`contrast`}},h={args:{color:`inverse`}},p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{}`,...p.parameters?.docs?.source}}},m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`{
-  args: {
-    color: 'contrast'
-  }
-}`,...m.parameters?.docs?.source}}},h.parameters={...h.parameters,docs:{...h.parameters?.docs,source:{originalSource:`{
-  args: {
-    color: 'inverse'
-  }
-}`,...h.parameters?.docs?.source}}},g=[`Default`,`ContrastColour`,`InverseColour`]})))()}export{f as a,_ as i,d as n,h as r,m as t};
