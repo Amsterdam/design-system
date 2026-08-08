9888857fbb59cafd4d094b0f4d132d69072b20d4
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-08-08 19:27:41 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@4215a2736e37d7cbcfa429b1d660d7b5052ca124 🚀

--- demo-develop/assets/BorderSample.test.stories-CpZ1iGMw.js
+++
@@ -1,8 +0,0 @@
-import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{t}from"./jsx-runtime-ATHzeHXA.js";import{n,t as r}from"./BorderSample-BBBwguqP.js";var i,a,o,s;function c(){return(c=e((()=>{n(),i=t(),a={title:`Components/Docs/Border Sample`},o={render:()=>(0,i.jsxs)(`div`,{className:`_ams-tests-grid`,children:[(0,i.jsx)(r,{lineStyle:`dashed`}),(0,i.jsx)(r,{width:`4px`}),(0,i.jsx)(r,{lineStyle:`dashed`,width:`4px`})]}),tags:[`!dev`,`!autodocs`,`!manifest`]},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
-  render: () => <div className="_ams-tests-grid">
-      <BorderSample lineStyle="dashed" />
-      <BorderSample width="4px" />
-      <BorderSample lineStyle="dashed" width="4px" />
-    </div>,
-  tags: ['!dev', '!autodocs', '!manifest']
-}`,...o.parameters?.docs?.source}}},s=[`Test`]})))()}c();export{o as Test,s as __namedExportsOrder,a as default};
---
+++ demo-develop/assets/iframe-Burcc_Sq.js
@@ -0,0 +1,1 @@
+import{t as e}from"./iframe-BrSz72bH.js";e();
