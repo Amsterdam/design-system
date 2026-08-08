2fe786c767a67da8584d1e4ab5451e770286a071
GitHub <github-action@users.noreply.github.com>
2026-08-08 12:40:00 +0000

    Remove feature branch folder

--- demo-grid-medium-column-count/assets/Checkbox.test.stories-BdqNDiTR.js
+++
@@ -1,7 +0,0 @@
-import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{n as t,t as n}from"./renderComponentVariants-CXsc2d23.js";import{n as r,t as i}from"./Checkbox-B0_qhDV_.js";import{c as a,s as o}from"./Checkbox.stories-Cb874ByK.js";var s,c,l;function u(){return(u=e((()=>{r(),n(),o(),s={...a,title:`Components/Forms/Checkbox`},c={render:(e,n)=>t(i,{args:e,variants:[`disabled`]},n),tags:[`!dev`,`!autodocs`,`!manifest`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Checkbox, {
-    args,
-    variants: ['disabled']
-  }, context),
-  tags: ['!dev', '!autodocs', '!manifest']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]})))()}u();export{c as Test,l as __namedExportsOrder,s as default};
