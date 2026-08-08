28393aea76af5ec4a3321c810630fb62656371d3
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-08-08 21:40:39 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@a5ec193c6bede23b2ebf3d61f38150f82e758c0d 🚀

---
+++ assets/TableOfContents.stories-DwmgAd_1.js
@@ -0,0 +1,122 @@
+import{n as e,r as t}from"./rolldown-runtime-DkW27tQK.js";import{t as n}from"./react-B6tGW3fj.js";import{t as r}from"./jsx-runtime-ATHzeHXA.js";import{n as i,t as a}from"./TableOfContents-ckwhXhKn.js";import{f as o,l as s}from"./argTypes-Diu0Wtb4.js";var c=t({Collapsible:()=>m,Controlled:()=>g,Default:()=>f,InitiallyExpanded:()=>h,MultipleLevels:()=>p,__namedExportsOrder:()=>_,default:()=>d}),l,u,d,f,p,m,h,g,_;function v(){return(v=e((()=>{i(),l=n(),o(),u=r(),d={title:`Components/Navigation/Table of Contents`,component:a,args:{children:(0,u.jsxs)(a.List,{children:[(0,u.jsx)(a.Link,{href:`#section-1`,label:`Zo werkt het`}),(0,u.jsx)(a.Link,{href:`#section-2`,label:`Voorwaarden`}),(0,u.jsx)(a.Link,{href:`#section-3`,label:`Aanvragen`}),(0,u.jsx)(a.Link,{href:`#section-4`,label:`Zie ook`})]}),heading:`Op deze pagina`},argTypes:{collapsible:{control:{type:`boolean`}},headingLevel:s([1,2,3,4],2),hideAccessibleLabel:{control:{type:`text`}},showAccessibleLabel:{control:{type:`text`}}},subcomponents:{"TableOfContents.Link":a.Link}},f={},p={args:{children:(0,u.jsxs)(a.List,{children:[(0,u.jsx)(a.Link,{href:`#section-1`,label:`Waarom is het belangrijk om helder te schrijven?`}),(0,u.jsx)(a.Link,{href:`#section-2`,label:`Schrijf in stappen`,children:(0,u.jsxs)(a.List,{children:[(0,u.jsx)(a.Link,{href:`#section-2.1`,label:`Stap 2`}),(0,u.jsx)(a.Link,{href:`#section-2.2`,label:`Stap 3`}),(0,u.jsx)(a.Link,{href:`#section-2.3`,label:`Stap 4`})]})}),(0,u.jsx)(a.Link,{href:`#section-3`,label:`Structuur van een heldere tekst`,children:(0,u.jsxs)(a.List,{children:[(0,u.jsx)(a.Link,{href:`#section-3.1`,label:`Opmaakeisen`}),(0,u.jsx)(a.Link,{href:`#section-3.2`,label:`Tekstlengte`,children:(0,u.jsx)(a.List,{children:(0,u.jsx)(a.Link,{href:`#section-3.2.1`,label:`Boodschap en achtergronden`})})}),(0,u.jsx)(a.Link,{href:`#section-3.3`,label:`Alinea's en tussenkopjes`}),(0,u.jsx)(a.Link,{href:`#section-3.4`,label:`Opsommingen`})]})}),(0,u.jsx)(a.Link,{href:`#section-4`,label:`Taalgebruik in heldere taal`}),(0,u.jsx)(a.Link,{href:`#section-5`,label:`Tekstonderdelen in heldere taal`}),(0,u.jsx)(a.Link,{href:`#section-6`,label:`Moeilijke woordenboek (inclusief niet te gebruiken)`})]}),heading:`Inhoudsopgave`}},m={args:{children:(0,u.jsxs)(a.List,{children:[(0,u.jsx)(a.Link,{href:`#s1`,label:`Inleiding`}),(0,u.jsx)(a.Link,{defaultExpanded:!0,href:`#s2`,label:`Vaststellen en waarderen van functies`,children:(0,u.jsxs)(a.List,{children:[(0,u.jsx)(a.Link,{href:`#s2-1`,label:`Algemeen`}),(0,u.jsx)(a.Link,{defaultExpanded:!0,href:`#s2-2`,label:`Waardering van functies`,children:(0,u.jsxs)(a.List,{children:[(0,u.jsx)(a.Link,{"aria-current":`page`,href:`#s2-2-1`,label:`Methode`}),(0,u.jsx)(a.Link,{href:`#s2-2-2`,label:`Procedure`}),(0,u.jsx)(a.Link,{href:`#s2-2-3`,label:`Bezwaar`})]})}),(0,u.jsx)(a.Link,{href:`#s2-3`,label:`Herwaardering`,children:(0,u.jsxs)(a.List,{children:[(0,u.jsx)(a.Link,{href:`#s2-3-1`,label:`Aanleiding`}),(0,u.jsx)(a.Link,{href:`#s2-3-2`,label:`Procedure`})]})})]})}),(0,u.jsx)(a.Link,{href:`#s3`,label:`Salaristoelagen`,children:(0,u.jsxs)(a.List,{children:[(0,u.jsx)(a.Link,{href:`#s3-1`,label:`Functioneringstoelage`}),(0,u.jsx)(a.Link,{href:`#s3-2`,label:`Arbeidsmarkttoelage`})]})}),(0,u.jsx)(a.Link,{href:`#s4`,label:`Vergoedingen`})]}),collapsible:!0,heading:`Inhoudsopgave`}},h={args:{children:(0,u.jsxs)(a.List,{defaultExpanded:!0,children:[(0,u.jsx)(a.Link,{href:`#s1`,label:`Inleiding`}),(0,u.jsx)(a.Link,{href:`#s2`,label:`Vaststellen en waarderen van functies`,children:(0,u.jsxs)(a.List,{children:[(0,u.jsx)(a.Link,{href:`#s2-1`,label:`Algemeen`}),(0,u.jsx)(a.Link,{href:`#s2-2`,label:`Waardering van functies`,children:(0,u.jsxs)(a.List,{children:[(0,u.jsx)(a.Link,{"aria-current":`page`,href:`#s2-2-1`,label:`Methode`}),(0,u.jsx)(a.Link,{href:`#s2-2-2`,label:`Procedure`}),(0,u.jsx)(a.Link,{href:`#s2-2-3`,label:`Bezwaar`})]})}),(0,u.jsx)(a.Link,{href:`#s2-3`,label:`Herwaardering`,children:(0,u.jsxs)(a.List,{defaultExpanded:!1,children:[(0,u.jsx)(a.Link,{href:`#s2-3-1`,label:`Aanleiding`}),(0,u.jsx)(a.Link,{href:`#s2-3-2`,label:`Procedure`})]})})]})}),(0,u.jsx)(a.Link,{href:`#s3`,label:`Salaristoelagen`,children:(0,u.jsxs)(a.List,{children:[(0,u.jsx)(a.Link,{href:`#s3-1`,label:`Functioneringstoelage`}),(0,u.jsx)(a.Link,{href:`#s3-2`,label:`Arbeidsmarkttoelage`})]})}),(0,u.jsx)(a.Link,{href:`#s4`,label:`Vergoedingen`})]}),collapsible:!0,heading:`Inhoudsopgave`}},g={args:{collapsible:!0,heading:`Inhoudsopgave`},render:e=>{let[t,n]=(0,l.useState)(`functies`);return(0,u.jsx)(a,{...e,children:(0,u.jsxs)(a.List,{children:[(0,u.jsx)(a.Link,{href:`#s1`,label:`Inleiding`}),(0,u.jsx)(a.Link,{expanded:t===`functies`,href:`#s2`,label:`Vaststellen en waarderen van functies`,onToggle:e=>n(e?`functies`:null),children:(0,u.jsxs)(a.List,{children:[(0,u.jsx)(a.Link,{href:`#s2-1`,label:`Algemeen`}),(0,u.jsx)(a.Link,{href:`#s2-2`,label:`Waardering van functies`})]})}),(0,u.jsx)(a.Link,{expanded:t===`toelagen`,href:`#s3`,label:`Salaristoelagen`,onToggle:e=>n(e?`toelagen`:null),children:(0,u.jsxs)(a.List,{children:[(0,u.jsx)(a.Link,{href:`#s3-1`,label:`Functioneringstoelage`}),(0,u.jsx)(a.Link,{href:`#s3-2`,label:`Arbeidsmarkttoelage`})]})}),(0,u.jsx)(a.Link,{href:`#s4`,label:`Vergoedingen`})]})})}},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{}`,...f.parameters?.docs?.source}}},p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: <TableOfContents.List>
+        <TableOfContents.Link href="#section-1" label="Waarom is het belangrijk om helder te schrijven?" />
+        <TableOfContents.Link href="#section-2" label="Schrijf in stappen">
+          <TableOfContents.List>
+            <TableOfContents.Link href="#section-2.1" label="Stap 2" />
+            <TableOfContents.Link href="#section-2.2" label="Stap 3" />
+            <TableOfContents.Link href="#section-2.3" label="Stap 4" />
+          </TableOfContents.List>
+        </TableOfContents.Link>
+        <TableOfContents.Link href="#section-3" label="Structuur van een heldere tekst">
+          <TableOfContents.List>
+            <TableOfContents.Link href="#section-3.1" label="Opmaakeisen" />
+            <TableOfContents.Link href="#section-3.2" label="Tekstlengte">
+              <TableOfContents.List>
+                <TableOfContents.Link href="#section-3.2.1" label="Boodschap en achtergronden" />
+              </TableOfContents.List>
+            </TableOfContents.Link>
+            <TableOfContents.Link href="#section-3.3" label="Alinea's en tussenkopjes" />
+            <TableOfContents.Link href="#section-3.4" label="Opsommingen" />
+          </TableOfContents.List>
+        </TableOfContents.Link>
+        <TableOfContents.Link href="#section-4" label="Taalgebruik in heldere taal" />
+        <TableOfContents.Link href="#section-5" label="Tekstonderdelen in heldere taal" />
+        <TableOfContents.Link href="#section-6" label="Moeilijke woordenboek (inclusief niet te gebruiken)" />
+      </TableOfContents.List>,
+    heading: 'Inhoudsopgave'
+  }
+}`,...p.parameters?.docs?.source}}},m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: <TableOfContents.List>
+        <TableOfContents.Link href="#s1" label="Inleiding" />
+        <TableOfContents.Link defaultExpanded href="#s2" label="Vaststellen en waarderen van functies">
+          <TableOfContents.List>
+            <TableOfContents.Link href="#s2-1" label="Algemeen" />
+            <TableOfContents.Link defaultExpanded href="#s2-2" label="Waardering van functies">
+              <TableOfContents.List>
+                <TableOfContents.Link aria-current="page" href="#s2-2-1" label="Methode" />
+                <TableOfContents.Link href="#s2-2-2" label="Procedure" />
+                <TableOfContents.Link href="#s2-2-3" label="Bezwaar" />
+              </TableOfContents.List>
+            </TableOfContents.Link>
+            <TableOfContents.Link href="#s2-3" label="Herwaardering">
+              <TableOfContents.List>
+                <TableOfContents.Link href="#s2-3-1" label="Aanleiding" />
+                <TableOfContents.Link href="#s2-3-2" label="Procedure" />
+              </TableOfContents.List>
+            </TableOfContents.Link>
+          </TableOfContents.List>
+        </TableOfContents.Link>
+        <TableOfContents.Link href="#s3" label="Salaristoelagen">
+          <TableOfContents.List>
+            <TableOfContents.Link href="#s3-1" label="Functioneringstoelage" />
+            <TableOfContents.Link href="#s3-2" label="Arbeidsmarkttoelage" />
+          </TableOfContents.List>
+        </TableOfContents.Link>
+        <TableOfContents.Link href="#s4" label="Vergoedingen" />
+      </TableOfContents.List>,
+    collapsible: true,
+    heading: 'Inhoudsopgave'
+  }
+}`,...m.parameters?.docs?.source}}},h.parameters={...h.parameters,docs:{...h.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: <TableOfContents.List defaultExpanded>
+        <TableOfContents.Link href="#s1" label="Inleiding" />
+        <TableOfContents.Link href="#s2" label="Vaststellen en waarderen van functies">
+          <TableOfContents.List>
+            <TableOfContents.Link href="#s2-1" label="Algemeen" />
+            <TableOfContents.Link href="#s2-2" label="Waardering van functies">
+              <TableOfContents.List>
+                <TableOfContents.Link aria-current="page" href="#s2-2-1" label="Methode" />
+                <TableOfContents.Link href="#s2-2-2" label="Procedure" />
+                <TableOfContents.Link href="#s2-2-3" label="Bezwaar" />
+              </TableOfContents.List>
+            </TableOfContents.Link>
+            <TableOfContents.Link href="#s2-3" label="Herwaardering">
+              <TableOfContents.List defaultExpanded={false}>
+                <TableOfContents.Link href="#s2-3-1" label="Aanleiding" />
+                <TableOfContents.Link href="#s2-3-2" label="Procedure" />
+              </TableOfContents.List>
+            </TableOfContents.Link>
+          </TableOfContents.List>
+        </TableOfContents.Link>
+        <TableOfContents.Link href="#s3" label="Salaristoelagen">
+          <TableOfContents.List>
+            <TableOfContents.Link href="#s3-1" label="Functioneringstoelage" />
+            <TableOfContents.Link href="#s3-2" label="Arbeidsmarkttoelage" />
+          </TableOfContents.List>
+        </TableOfContents.Link>
+        <TableOfContents.Link href="#s4" label="Vergoedingen" />
+      </TableOfContents.List>,
+    collapsible: true,
+    heading: 'Inhoudsopgave'
+  }
+}`,...h.parameters?.docs?.source}}},g.parameters={...g.parameters,docs:{...g.parameters?.docs,source:{originalSource:`{
+  args: {
+    collapsible: true,
+    heading: 'Inhoudsopgave'
+  },
+  render: args => {
+    const [expandedBranch, setExpandedBranch] = useState<string | null>('functies');
+    return <TableOfContents {...args}>
+        <TableOfContents.List>
+          <TableOfContents.Link href="#s1" label="Inleiding" />
+          <TableOfContents.Link expanded={expandedBranch === 'functies'} href="#s2" label="Vaststellen en waarderen van functies" onToggle={expanded => setExpandedBranch(expanded ? 'functies' : null)}>
+            <TableOfContents.List>
+              <TableOfContents.Link href="#s2-1" label="Algemeen" />
+              <TableOfContents.Link href="#s2-2" label="Waardering van functies" />
+            </TableOfContents.List>
+          </TableOfContents.Link>
+          <TableOfContents.Link expanded={expandedBranch === 'toelagen'} href="#s3" label="Salaristoelagen" onToggle={expanded => setExpandedBranch(expanded ? 'toelagen' : null)}>
+            <TableOfContents.List>
+              <TableOfContents.Link href="#s3-1" label="Functioneringstoelage" />
+              <TableOfContents.Link href="#s3-2" label="Arbeidsmarkttoelage" />
+            </TableOfContents.List>
+          </TableOfContents.Link>
+          <TableOfContents.Link href="#s4" label="Vergoedingen" />
+        </TableOfContents.List>
+      </TableOfContents>;
+  }
+}`,...g.parameters?.docs?.source}}},_=[`Default`,`MultipleLevels`,`Collapsible`,`InitiallyExpanded`,`Controlled`]})))()}export{c as a,p as i,g as n,v as o,h as r,d as s,m as t};
