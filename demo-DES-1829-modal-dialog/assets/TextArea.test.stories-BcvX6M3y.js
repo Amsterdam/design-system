112a0b3ab26c8da2a79bfd5930387096253edefb
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-08-08 13:33:49 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@e53baf6d0fa27458258477e5ed933a4e0c726d3f 🚀

---
+++ demo-DES-1829-modal-dialog/assets/TextArea.test.stories-BcvX6M3y.js
@@ -0,0 +1,11 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{n as t,t as n}from"./renderComponentVariants-C0z_c7x7.js";import{n as r,t as i}from"./TextArea-AOytqe57.js";import{n as a,t as o}from"./disablePageLevelChecks-57VwoK5W.js";import{l as s,u as c}from"./TextArea.stories-EQcyIPGG.js";var l,u,d;function f(){return(f=e((()=>{r(),a(),n(),s(),l={...c,title:`Components/Forms/Text Area`},u={args:{defaultValue:`Het waterrijke achterland van de provincie Holland was een paradijs voor vissers. Maar terwijl de visserij floreerde in Holland, was Amsterdam eigenlijk helemaal geen visserijstad. Toch maakten Amsterdammers naam in de vishandel. Zij speelden namelijk een cruciale rol bij het klaarmaken en vervoeren van haring.`},parameters:o(`label`),render:(e,n)=>t(i,{args:e,variants:[`disabled`,`hovered`]},n),tags:[`!dev`,`!autodocs`,`!manifest`]},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    defaultValue: 'Het waterrijke achterland van de provincie Holland was een paradijs voor vissers. Maar terwijl de visserij floreerde in Holland, was Amsterdam eigenlijk helemaal geen visserijstad. Toch maakten Amsterdammers naam in de vishandel. Zij speelden namelijk een cruciale rol bij het klaarmaken en vervoeren van haring.'
+  },
+  parameters: disablePageLevelChecks('label'),
+  render: (args, context) => renderComponentVariants(TextArea, {
+    args,
+    variants: ['disabled', 'hovered']
+  }, context),
+  tags: ['!dev', '!autodocs', '!manifest']
+}`,...u.parameters?.docs?.source}}},d=[`Test`]})))()}f();export{u as Test,d as __namedExportsOrder,l as default};
---
+++ demo-DES-1829-modal-dialog/assets/iframe-DrhW_z7I.js
@@ -0,0 +1,1 @@
+import{t as e}from"./iframe-CVk6hdq4.js";e();
