ed04ec0cfa6206e721d7f7cf7d058d10eab2b676
GitHub <github-action@users.noreply.github.com>
2026-08-08 19:35:57 +0000

    Remove feature branch folder

--- demo-update-dependencies/assets/Metadata.test.stories-CCO6YJT4.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{n as t,t as n}from"./renderComponentVariants-K5fnUai5.js";import{n as r,t as i}from"./Metadata-CdgL4GQc.js";import{a,o}from"./Metadata.stories-Y7eaWUW9.js";var s,c,l;function u(){return(u=e((()=>{r(),n(),a(),s={...o,title:`Components/Text/Metadata`},c={render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`,`!manifest`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Metadata, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs', '!manifest']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]})))()}u();export{c as Test,l as __namedExportsOrder,s as default};
--- demo-update-dependencies/assets/iframe-BvVaoZJn.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./iframe-CVBK5KKO.js";e();
