e05faa6fe48b6626a77748f356a0c79517dd3fbc
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-08-08 17:05:28 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@a68871ef56d18d7965d127fea470ae6dd73093ee 🚀

---
+++ demo-DES-1829-alert-dialog/assets/AlertDialogHeader.stories-Dvow2OmC.js
@@ -0,0 +1,7 @@
+import{n as e,r as t}from"./rolldown-runtime-DkW27tQK.js";import{t as n}from"./jsx-runtime-ATHzeHXA.js";import{C as r,nt as i}from"./index.esm-M2z_2xS7.js";import{n as a,t as o}from"./AlertDialog-B3EPTsQ0.js";var s=t({Header:()=>u,__namedExportsOrder:()=>d,default:()=>l}),c,l,u,d;function f(){return(f=e((()=>{i(),a(),c=n(),l={title:`Components/Containers/Alert Dialog`,component:o.Header,argTypes:{severity:{control:{labels:{undefined:`none`},type:`select`},options:[void 0,`error`,`warning`,`success`]}},decorators:[e=>(0,c.jsx)(o,{"aria-labelledby":`ams-alert-dialog-header-heading`,ref:e=>{e&&!e.open&&e.show()},style:{position:`static`},children:(0,c.jsx)(e,{})})],tags:[`!manifest`]},u={args:{children:(0,c.jsx)(r,{id:`ams-alert-dialog-header-heading`,level:1,size:`level-3`,children:`Titel van het dialoogvenster`})}},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: <Heading id="ams-alert-dialog-header-heading" level={1} size="level-3">
+        Titel van het dialoogvenster
+      </Heading>
+  }
+}`,...u.parameters?.docs?.source}}},d=[`Header`]})))()}export{u as n,f as r,s as t};
---
+++ demo-DES-1829-alert-dialog/assets/FileList.test.stories-BFskNWnQ.js
@@ -0,0 +1,11 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{t}from"./jsx-runtime-ATHzeHXA.js";import{n,t as r}from"./renderComponentVariants-CfQAmKwy.js";import{n as i,t as a}from"./FileList-Bkaf7F-Q.js";import{i as o,r as s}from"./FileList.stories-DKHOgO-x.js";var c,l,u,d;function f(){return(f=e((()=>{i(),r(),s(),c=t(),l={...o,title:`Components/Forms/File List`},u={args:{children:(0,c.jsx)(a.Item,{file:new File([`sample1`],`sample1.txt`,{type:`text/plain`})})},render:(e,t)=>n(a,{args:e},t),tags:[`!dev`,`!autodocs`,`!manifest`]},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: <FileList.Item file={new File(['sample1'], 'sample1.txt', {
+      type: 'text/plain'
+    })} />
+  },
+  render: (args, context) => renderComponentVariants(FileList, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs', '!manifest']
+}`,...u.parameters?.docs?.source}}},d=[`Test`]})))()}f();export{u as Test,d as __namedExportsOrder,l as default};
