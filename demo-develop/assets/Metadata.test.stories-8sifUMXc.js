d609022c12044cc7b96a058a8be2cbea274c0ae6
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-08-08 09:46:12 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@42fd46ad12f90e37e90b8b1ce5ee60882b698269 🚀

--- demo-develop/assets/Metadata.test.stories-8sifUMXc.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{n as t,t as n}from"./renderComponentVariants-CMoDc7zb.js";import{n as r,t as i}from"./Metadata-Doocr3Is.js";import{a,o}from"./Metadata.stories-DrgiOKFH.js";var s,c,l;function u(){return(u=e((()=>{r(),n(),a(),s={...o,title:`Components/Text/Metadata`},c={render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`,`!manifest`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Metadata, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs', '!manifest']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]})))()}u();export{c as Test,l as __namedExportsOrder,s as default};
