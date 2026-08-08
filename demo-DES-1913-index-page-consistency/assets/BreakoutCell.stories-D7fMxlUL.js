7eff76a5c72a0bb5a96a0c85b6b33d0e03e4ac0c
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-08-08 10:31:57 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@bb1626a0c700f43b93096ca6cca091c8bcedfe4b 🚀

---
+++ demo-DES-1913-index-page-consistency/assets/BreakoutCell.stories-D7fMxlUL.js
@@ -0,0 +1,9 @@
+import{n as e,r as t}from"./rolldown-runtime-DkW27tQK.js";import{t as n}from"./jsx-runtime-ATHzeHXA.js";import{a as r,i,r as a,t as o}from"./Breakout-B9l2vJyY.js";import{n as s,t as c}from"./GridColumnsGuide-C-49HIvF.js";import{d as l,t as u}from"./argTypes-CTYEle7b.js";var d=t({Cell:()=>m,__namedExportsOrder:()=>h,default:()=>p}),f,p,m,h;function g(){return(g=e((()=>{a(),r(),l(),s(),f=n(),p={title:`Components/Layout/Breakout`,component:o.Cell,argTypes:{as:u(i),colSpan:{control:{max:12,min:1,type:`number`}},colStart:{control:{max:12,min:1,type:`number`}},has:{control:!1},rowSpan:{control:{max:4,min:1,type:`number`}},rowStart:{control:{max:4,min:1,type:`number`}}},decorators:[e=>(0,f.jsxs)(f.Fragment,{children:[(0,f.jsx)(c,{}),(0,f.jsxs)(o,{paddingVertical:`x-large`,children:[(0,f.jsx)(o.Cell,{className:`_ams-item`,colSpan:2,colStart:1,rowStart:1}),(0,f.jsx)(o.Cell,{className:`_ams-item`,colSpan:2,colStart:1,rowStart:2}),(0,f.jsx)(e,{})]})]})],parameters:{layout:`fullscreen`},tags:[`!manifest`]},o.Cell,m={args:{className:`_ams-item--highlight`,colSpan:4,colStart:3,rowSpan:2,rowStart:1}},m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`{
+  args: {
+    className: '_ams-item--highlight',
+    colSpan: 4,
+    colStart: 3,
+    rowSpan: 2,
+    rowStart: 1
+  }
+}`,...m.parameters?.docs?.source}}},h=[`Cell`]})))()}export{m as n,g as r,d as t};
--- demo-DES-1913-index-page-consistency/assets/iframe-4LPUUF4m.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./iframe-C_sDERd9.js";e();
