9fb31fc9daed400d36f2617b0eeaa3e6e1e6fec5
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-08-09 09:29:20 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@c4b8afbc2503b02c4a842f87ed2ba170f82902a2 🚀

---
+++ demo-metadata-separator-between-filter-facets/assets/Spotlight.test.stories-DU7XkhiP.js
@@ -0,0 +1,16 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{t}from"./jsx-runtime-ATHzeHXA.js";import{n,r,t as i}from"./Spotlight-DaWWH95i.js";import{i as a,r as o}from"./Spotlight.stories-RScOfHaM.js";var s,c,l,u;function d(){return(d=e((()=>{n(),o(),s=t(),c={...a,title:`Components/Containers/Spotlight`},l={render:()=>(0,s.jsx)(`div`,{className:`_ams-tests-stack`,children:[void 0,...r].map(e=>(0,s.jsx)(i,{color:e,children:(0,s.jsx)(`div`,{style:{padding:`var(--ams-space-xl)`},children:(0,s.jsx)(`h2`,{style:e===`yellow`||e===`orange`||e===`lime`?void 0:{color:`var(--ams-color-text-inverse)`},children:`Voetballen is heel simpel, maar het moeilijkste wat er is, is simpel voetballen.`})})},e))}),tags:[`!dev`,`!autodocs`,`!manifest`]},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  render: () => <div className="_ams-tests-stack">
+      {[undefined, ...spotlightColors].map(color => <Spotlight color={color} key={color}>
+          <div style={{
+        padding: 'var(--ams-space-xl)'
+      }}>
+            <h2 style={color === 'yellow' || color === 'orange' || color === 'lime' ? undefined : {
+          color: 'var(--ams-color-text-inverse)'
+        }}>
+              Voetballen is heel simpel, maar het moeilijkste wat er is, is simpel voetballen.
+            </h2>
+          </div>
+        </Spotlight>)}
+    </div>,
+  tags: ['!dev', '!autodocs', '!manifest']
+}`,...l.parameters?.docs?.source}}},u=[`Test`]})))()}d();export{l as Test,u as __namedExportsOrder,c as default};
