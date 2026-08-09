a96275b8fb516cfd5236790aa89c644b598a1bf5
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-08-09 18:40:25 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@2da02f57420df40b7106d1ce0697a88d37a2181d 🚀

---
+++ demo-develop/assets/MenuLink.stories-BK2cTe4N.js
@@ -0,0 +1,7 @@
+import{n as e,r as t}from"./rolldown-runtime-DkW27tQK.js";import{t as n}from"./jsx-runtime-ATHzeHXA.js";import{F as r,K as i}from"./index.esm-Cf2AflXd.js";import{n as a,t as o}from"./Menu-C_TOoVle.js";import{f as s,m as c,r as l,u}from"./argTypes-Diu0Wtb4.js";import{n as d,r as f}from"./iconArgTypes-YlxcUvp_.js";var p=t({Link:()=>g,__namedExportsOrder:()=>_,default:()=>h}),m,h,g,_;function v(){return(v=e((()=>{i(),a(),s(),d(),m=n(),h={title:`Components/Navigation/Menu`,component:o.Link,argTypes:{children:l(`The link text.`),href:u,icon:f,linkComponent:c},decorators:[e=>(0,m.jsx)(o,{inWideWindow:!0,children:(0,m.jsx)(e,{})})],parameters:{themes:{options:[`Compact`,`Compact Lo-fi`]}},render:({children:e,...t})=>(0,m.jsx)(o.Link,{...t,children:e}),tags:[`!manifest`]},g={args:{children:`Dashboard`,href:`#`,icon:(0,m.jsx)(r,{})}},g.parameters={...g.parameters,docs:{...g.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Dashboard',
+    href: '#',
+    icon: <PieChartFillIcon />
+  }
+}`,...g.parameters?.docs?.source}}},_=[`Link`]})))()}export{p as n,v as r,g as t};
