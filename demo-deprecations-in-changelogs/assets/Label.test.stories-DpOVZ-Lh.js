59d4a03cd6ee756da3c973d35c3d9b014f8afd7e
GitHub <github-action@users.noreply.github.com>
2026-08-09 19:46:58 +0000

    Remove feature branch folder

--- demo-deprecations-in-changelogs/assets/Label.test.stories-DpOVZ-Lh.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{n as t,t as n}from"./renderComponentVariants-WSdDM8s-.js";import{n as r,t as i}from"./Label-CKhSjMTN.js";import{o as a,s as o}from"./Label.stories-Cj6mRK5Y.js";var s,c,l;function u(){return(u=e((()=>{r(),n(),a(),s={...o,title:`Components/Forms/Label`},c={render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`,`!manifest`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Label, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs', '!manifest']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]})))()}u();export{c as Test,l as __namedExportsOrder,s as default};
--- demo-deprecations-in-changelogs/assets/iframe-BYSLDIgO.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./iframe-DsvxCLb1.js";e();
