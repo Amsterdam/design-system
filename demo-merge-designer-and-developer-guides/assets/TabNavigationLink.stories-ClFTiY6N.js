eeb44d32c622b49443dd3bcf0d015a845239242f
GitHub <github-action@users.noreply.github.com>
2026-08-09 19:28:55 +0000

    Remove feature branch folder

--- demo-merge-designer-and-developer-guides/assets/TabNavigationLink.stories-ClFTiY6N.js
+++
@@ -1,7 +0,0 @@
-import{n as e,r as t}from"./rolldown-runtime-DkW27tQK.js";import{t as n}from"./jsx-runtime-ATHzeHXA.js";import{n as r,t as i}from"./TabNavigation-CJl8Oiwt.js";import{f as a,m as o,r as s,u as c}from"./argTypes-Diu0Wtb4.js";import{n as l,t as u}from"./iconArgTypes-YlxcUvp_.js";var d=t({Link:()=>m,__namedExportsOrder:()=>h,default:()=>p}),f,p,m,h;function g(){return(g=e((()=>{r(),a(),l(),f=n(),p={title:`Components/Navigation/Tab Navigation`,component:i.Link,argTypes:{children:s(`The link text.`),href:c,icon:u(),linkComponent:o},decorators:[e=>(0,f.jsx)(i,{children:(0,f.jsx)(i.List,{children:(0,f.jsx)(e,{})})})],render:({children:e,...t})=>(0,f.jsx)(i.Link,{...t,children:e}),tags:[`!manifest`]},m={args:{"aria-current":`page`,children:`Gegevens`,href:`#`}},m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`{
-  args: {
-    'aria-current': 'page',
-    children: 'Gegevens',
-    href: '#'
-  }
-}`,...m.parameters?.docs?.source}}},h=[`Link`]})))()}export{d as n,g as r,m as t};
--- demo-merge-designer-and-developer-guides/assets/iframe-DNoYRn1W.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./iframe-BsjYIK68.js";e();
