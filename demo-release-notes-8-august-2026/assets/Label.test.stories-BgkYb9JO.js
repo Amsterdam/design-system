314460aef68c382ab3aacb707bc75401a5eb98c8
GitHub <github-action@users.noreply.github.com>
2026-08-09 18:03:58 +0000

    Remove feature branch folder

--- demo-release-notes-8-august-2026/assets/Label.test.stories-BgkYb9JO.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{n as t,t as n}from"./renderComponentVariants-DrR3CeUq.js";import{n as r,t as i}from"./Label-B0mwm2gP.js";import{o as a,s as o}from"./Label.stories-na2ObR8E.js";var s,c,l;function u(){return(u=e((()=>{r(),n(),a(),s={...o,title:`Components/Forms/Label`},c={render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`,`!manifest`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Label, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs', '!manifest']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]})))()}u();export{c as Test,l as __namedExportsOrder,s as default};
--- demo-release-notes-8-august-2026/assets/Radio.test.stories-BLhfUND4.js
+++
@@ -1,7 +0,0 @@
-import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{n as t,t as n}from"./renderComponentVariants-DrR3CeUq.js";import{n as r,t as i}from"./Radio-Ca6iA8-1.js";import{o as a,s as o}from"./Radio.stories-DgREV6a_.js";var s,c,l;function u(){return(u=e((()=>{r(),n(),a(),s={...o,title:`Components/Forms/Radio`},c={render:(e,n)=>t(i,{args:e,variants:[`disabled`]},n),tags:[`!dev`,`!autodocs`,`!manifest`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Radio, {
-    args,
-    variants: ['disabled']
-  }, context),
-  tags: ['!dev', '!autodocs', '!manifest']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]})))()}u();export{c as Test,l as __namedExportsOrder,s as default};
