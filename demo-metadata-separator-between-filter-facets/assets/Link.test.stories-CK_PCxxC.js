0fbd38503910455dc26753c8c01db761790c174d
GitHub <github-action@users.noreply.github.com>
2026-08-09 18:04:17 +0000

    Remove feature branch folder

--- demo-metadata-separator-between-filter-facets/assets/Link.test.stories-CK_PCxxC.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{n as t,t as n}from"./renderComponentVariants-DwMmPoeX.js";import{n as r,t as i}from"./Link-DNu-YnRy.js";import{a,i as o}from"./Link.stories-XgBKpcNF.js";var s,c,l;function u(){return(u=e((()=>{r(),n(),o(),s={...a,title:`Components/Navigation/Link`},c={render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`,`!manifest`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Link, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs', '!manifest']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]})))()}u();export{c as Test,l as __namedExportsOrder,s as default};
