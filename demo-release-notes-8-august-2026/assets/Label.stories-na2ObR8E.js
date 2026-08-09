839b17580d327932d89cf4e6208d037ee403c01c
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-08-09 09:31:09 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@179a2530235ba56d22332267ad97f9951902b36e 🚀

---
+++ demo-release-notes-8-august-2026/assets/Label.stories-na2ObR8E.js
@@ -0,0 +1,18 @@
+import{n as e,r as t}from"./rolldown-runtime-DkW27tQK.js";import{n,t as r}from"./Label-B0mwm2gP.js";import{f as i,r as a}from"./argTypes-Diu0Wtb4.js";var o=t({Default:()=>c,InAFieldSet:()=>f,Optional:()=>l,WithHeading:()=>d,WithHint:()=>u,__namedExportsOrder:()=>p,default:()=>s}),s,c,l,u,d,f,p;function m(){return(m=e((()=>{n(),i(),s={title:`Components/Forms/Label`,component:r,args:{children:`Label`,optional:!1},argTypes:{children:a(`The text content.`)}},c={},l={args:{optional:!0}},u={args:{hint:`not required`}},d={args:{isPageHeading:!0}},f={args:{inFieldSet:!0,optional:!0}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{}`,...c.parameters?.docs?.source}}},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  args: {
+    optional: true
+  }
+}`,...l.parameters?.docs?.source}}},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    hint: 'not required'
+  }
+}`,...u.parameters?.docs?.source}}},d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  args: {
+    isPageHeading: true
+  }
+}`,...d.parameters?.docs?.source}}},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
+  args: {
+    inFieldSet: true,
+    optional: true
+  }
+}`,...f.parameters?.docs?.source}}},p=[`Default`,`Optional`,`WithHint`,`WithHeading`,`InAFieldSet`]})))()}export{u as a,d as i,o as n,m as o,l as r,s,f as t};
