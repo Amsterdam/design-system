c9f6f4fc95ae4ef8a8aee5b8d28141d76cceb4f4
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-08-09 23:32:51 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@a82540861bd9c01b4e2d62944290a99b77e64c80 🚀

---
+++ demo-release-notes-rewrite/assets/PageFooter.test.stories-BPa4lgI6.js
@@ -0,0 +1,16 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{t}from"./jsx-runtime-ATHzeHXA.js";import{B as n,nt as r}from"./index.esm-FPQeD5SM.js";import{n as i,t as a}from"./renderComponentVariants-DotclAKe.js";import{n as o,t as s}from"./PageFooter-C-isiHp-.js";import{a as c,i as l}from"./PageFooter.stories-BE9Wj7pk.js";var u,d,f,p;function m(){return(m=e((()=>{r(),o(),a(),l(),u=t(),d={...c,title:`Components/Containers/Page Footer`},f={args:{children:[(0,u.jsx)(s.Spotlight,{children:(0,u.jsx)(n,{color:`inverse`,children:`Adressen en openingstijden`})},1),(0,u.jsxs)(s.Menu,{children:[(0,u.jsx)(s.MenuLink,{href:`#`,children:`Over deze site`}),(0,u.jsx)(s.MenuLink,{href:`#`,children:`Privacy`}),(0,u.jsx)(s.MenuLink,{href:`#`,children:`Cookies op deze site`}),(0,u.jsx)(s.MenuLink,{href:`#`,children:`Webarchief`})]},2)]},render:(e,t)=>i(s,{args:e},t),tags:[`!dev`,`!autodocs`,`!manifest`]},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: [<PageFooter.Spotlight key={1}>
+        <Paragraph color="inverse">Adressen en openingstijden</Paragraph>
+      </PageFooter.Spotlight>, <PageFooter.Menu key={2}>
+        <PageFooter.MenuLink href="#">Over deze site</PageFooter.MenuLink>
+        <PageFooter.MenuLink href="#">Privacy</PageFooter.MenuLink>
+        <PageFooter.MenuLink href="#">Cookies op deze site</PageFooter.MenuLink>
+        <PageFooter.MenuLink href="#">Webarchief</PageFooter.MenuLink>
+      </PageFooter.Menu>]
+  },
+  render: (args, context) => renderComponentVariants(PageFooter, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs', '!manifest']
+}`,...f.parameters?.docs?.source}}},p=[`Test`]})))()}m();export{f as Test,p as __namedExportsOrder,d as default};
---
+++ demo-release-notes-rewrite/assets/iframe-C8IHvCC0.js
@@ -0,0 +1,1 @@
+import{t as e}from"./iframe-c5vctlgU.js";e();
