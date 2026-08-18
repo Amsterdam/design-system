998a9488d5af34a4394029bff6cee576854bfde4
RubenSibon <mail+github@rubensibon.nl>
2026-08-18 12:07:54 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@f8ef51bdd03dcb947d5440e034f9c7e476da787d 🚀

---
+++ demo-DES-1917-switch-with-label-redesign/assets/Link.test.stories-BkKTk7zY.js
@@ -0,0 +1,6 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{n as t,t as n}from"./renderComponentVariants-7XYVvUfj.js";import{n as r,t as i}from"./Link-99wt096M.js";import{a,i as o}from"./Link.stories-C8ZXG63q.js";var s,c,l;function u(){return(u=e((()=>{r(),n(),o(),s={...a,title:`Components/Navigation/Link`},c={render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`,`!manifest`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Link, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs', '!manifest']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]})))()}u();export{c as Test,l as __namedExportsOrder,s as default};
---
+++ demo-DES-1917-switch-with-label-redesign/assets/iframe-B7t99PEj.js
@@ -0,0 +1,1 @@
+import{t as e}from"./iframe-36G1Qckx.js";e();
