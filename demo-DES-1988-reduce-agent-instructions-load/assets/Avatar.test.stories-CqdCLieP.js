fd7ef11946dca51a805d1ff01eb477660fe34135
RubenSibon <mail+github@rubensibon.nl>
2026-08-19 11:46:22 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@8dea5c22527a6125fb752e3c04b543c9b469af39 🚀

---
+++ demo-DES-1988-reduce-agent-instructions-load/assets/Avatar.test.stories-CqdCLieP.js
@@ -0,0 +1,6 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{n as t,t as n}from"./renderComponentVariants-u5UeRBnR.js";import{r,t as i}from"./Avatar-DJAUCKf8.js";import{a,i as o}from"./Avatar.stories-padz7FSW.js";var s,c,l;function u(){return(u=e((()=>{r(),n(),o(),s={...a,title:`Components/Feedback/Avatar`},c={render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`,`!manifest`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Avatar, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs', '!manifest']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]})))()}u();export{c as Test,l as __namedExportsOrder,s as default};
--- demo-DES-1988-reduce-agent-instructions-load/assets/Column.test.stories-BajCFwhl.js
+++
@@ -1,7 +0,0 @@
-import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{n as t,t as n}from"./renderComponentVariants-CY_ulCDs.js";import{i as r,t as i}from"./Column-DckHgM_6.js";import{a,o}from"./Column.stories-CdGByXjf.js";var s,c,l;function u(){return(u=e((()=>{r(),n(),a(),s={...o,title:`Components/Layout/Column`},c={render:(e,n)=>t(i,{args:e,layout:`grid`},n),tags:[`!dev`,`!autodocs`,`!manifest`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(Column, {
-    args,
-    layout: 'grid'
-  }, context),
-  tags: ['!dev', '!autodocs', '!manifest']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]})))()}u();export{c as Test,l as __namedExportsOrder,s as default};
