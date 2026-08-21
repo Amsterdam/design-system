29fc2bb9e83092453f28941653d2cc60f6ab60e9
GitHub <github-action@users.noreply.github.com>
2026-08-21 13:37:20 +0000

    Remove feature branch folder

--- demo-github_actions/patch-and-minor-actions-ebd33580a2/assets/StandaloneLink.test.stories-Ck-iLgqM.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{n as t,t as n}from"./renderComponentVariants-NykolbqO.js";import{n as r,t as i}from"./StandaloneLink-BKeEEULm.js";import{o as a,s as o}from"./StandaloneLink.stories-DLnl1sY5.js";var s,c,l;function u(){return(u=e((()=>{r(),n(),a(),s={...o,title:`Components/Navigation/Standalone Link`},c={render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`,`!manifest`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  render: (args, context) => renderComponentVariants(StandaloneLink, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs', '!manifest']
-}`,...c.parameters?.docs?.source}}},l=[`Test`]})))()}u();export{c as Test,l as __namedExportsOrder,s as default};
