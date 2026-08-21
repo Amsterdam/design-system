c9d0654261f3fa60fc495f9d6eb83fb324fd81a9
RubenSibon <mail+github@rubensibon.nl>
2026-08-21 13:33:34 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@ad82e0987fd03a49427abe5fda08736782afc4b4 🚀

---
+++ demo-develop/assets/Avatar.test.stories-Bdf0SwvP.js
@@ -0,0 +1,6 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{n as t,t as n}from"./renderComponentVariants-NW0ho2pp.js";import{r,t as i}from"./Avatar-BaGmmgMG.js";import{a,i as o}from"./Avatar.stories-CYB-N9ag.js";var s,c,l;function u(){return(u=e((()=>{r(),n(),o(),s={...a,title:`Components/Feedback/Avatar`},c={render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`,`!manifest`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Avatar, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs', '!manifest']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]})))()}u();export{c as Test,l as __namedExportsOrder,s as default};
--- demo-develop/assets/iframe-BiqAcuLJ.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./iframe-BxX5KI1W.js";e();
