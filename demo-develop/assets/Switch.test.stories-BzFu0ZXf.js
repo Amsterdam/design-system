039c76a9b63d31356b029113798167084cb25f9c
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-09-11 16:00:26 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@13b371436b7ea8d9c4a54def967530121cf14cee 🚀

--- demo-develop/assets/Switch.test.stories-BzFu0ZXf.js
+++
@@ -1,7 +0,0 @@
-import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{n as t,t as n}from"./renderComponentVariants-0ir9U8D7.js";import{n as r,t as i}from"./Switch-KHz03uS-.js";import{n as a,t as o}from"./disablePageLevelChecks-57VwoK5W.js";import{a as s,i as c}from"./Switch.stories-CrOU4XDu.js";var l,u,d;function f(){return(f=e((()=>{r(),a(),n(),c(),l={...s,title:`Components/Forms/Switch`},u={parameters:o(`label`),render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`,`!manifest`]},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
-  parameters: disablePageLevelChecks('label'),
-  render: (args, context) => renderComponentVariants(Switch, {
-    args
-  }, context),
-  tags: ['!dev', '!autodocs', '!manifest']
-}`,...u.parameters?.docs?.source}}},d=[`Test`]})))()}f();export{u as Test,d as __namedExportsOrder,l as default};
---
+++ demo-develop/assets/iframe-3gfCK68g.js
@@ -0,0 +1,1 @@
+import{t as e}from"./iframe-DW5dH565.js";e();
