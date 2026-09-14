259cf7841410a61f25474c1bebf65989dcef7aa1
Arman-Kokor <a.kokor@amsterdam.nl>
2026-09-14 08:17:43 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@13b371436b7ea8d9c4a54def967530121cf14cee 🚀

---
+++ demo-DES-2022-spinner/assets/generateAspectRatioClass-DCYqhXk3.js
@@ -0,0 +1,1 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";var t;function n(){return(n=e((()=>{t=e=>e&&`ams-aspect-ratio-${e.replace(`:`,`-`)}`})))()}export{n,t};
---
+++ demo-DES-2022-spinner/assets/iframe-_kUaq5yj.js
@@ -0,0 +1,1 @@
+import{t as e}from"./iframe-EGnHqt_1.js";e();
