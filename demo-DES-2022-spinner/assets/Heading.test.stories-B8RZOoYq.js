0f83198051b9bd2c8613c5de453a2de52cddf206
Arman-Kokor <a.kokor@amsterdam.nl>
2026-09-15 11:11:40 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@1f82aad81ee472dfac26f6187425b3d2a3228657 🚀

---
+++ demo-DES-2022-spinner/assets/Heading.test.stories-B8RZOoYq.js
@@ -0,0 +1,9 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{n as t,t as n}from"./renderComponentVariants-QQMKVlLp.js";import{r,t as i}from"./Heading-dTB4vzw6.js";import{l as a,u as o}from"./Heading.stories-5jtMx3tK.js";var s,c,l;function u(){return(u=e((()=>{r(),n(),a(),s={...o,title:`Components/Text/Heading`},c={args:{children:`Amsterdam Light Festival maakt de stad ook deze winter een beetje mooier`},render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`,`!manifest`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Amsterdam Light Festival maakt de stad ook deze winter een beetje mooier'
+  },
+  render: (args, context) => renderComponentVariants(Heading, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs', '!manifest']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]})))()}u();export{c as Test,l as __namedExportsOrder,s as default};
--- demo-DES-2022-spinner/assets/iframe-B9tmRFnt.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./iframe-DRt2quPB.js";e();
