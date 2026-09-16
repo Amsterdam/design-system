d8c0f4140136b0288b8fdadff271f24870cde1ed
Arman Kokor <a.kokor@amsterdam.nl>
2026-09-16 17:20:15 +0200

    feat(Progress Bar): add initial progress bar and styling

--- packages/react/src/ProgressBar/ProgressBar.tsx
+++ packages/react/src/ProgressBar/ProgressBar.tsx
@@ -3,22 +3,43 @@
  * Copyright Gemeente Amsterdam
  */
 
-import type { HTMLAttributes, PropsWithChildren } from 'react'
+import type { ForwardedRef, HTMLAttributes } from 'react'
 
 import { clsx } from 'clsx'
 import { forwardRef } from 'react'
 
-export type ProgressBarProps = Readonly<PropsWithChildren<HTMLAttributes<HTMLElement>>>
+export type ProgressBarProps = {
+  /**
+   * The value that represents 100% progress.
+   */
+  readonly max: number
+  /**
+   * A description of the progress, such as '4 van de 10 stappen'.
+   * This is not calculated from `value` and `max` and must be provided separately.
+   */
+  readonly text: string
+  /**
+   * The current progress value.
+   */
+  readonly value: number
+} & Readonly<Omit<HTMLAttributes<HTMLDivElement>, 'children'>>
 
 /**
  * A bar that shows how far a user has progressed through a process, such as a form.
  *
  * @see {@link https://designsystem.amsterdam/?path=/docs/components-feedback-progress-bar--docs ProgressBar docs at Amsterdam Design System}
  */
-export const ProgressBar = forwardRef<HTMLElement, ProgressBarProps>(({ children, className, ...restProps }, ref) => (
-  <span {...restProps} className={clsx('ams-progress-bar', className)} ref={ref}>
-    {children}
-  </span>
-))
+export const ProgressBar = forwardRef(
+  ({ className, max, text, value, ...restProps }: ProgressBarProps, ref: ForwardedRef<HTMLDivElement>) => (
+    <div {...restProps} className={clsx('ams-progress-bar', className)} ref={ref}>
+      <p aria-hidden className="ams-progress-bar-text">
+        {text}
+      </p>
+      <progress aria-label={text} className="ams-progress-bar-progress" max={max} value={value}>
+        {text}
+      </progress>
+    </div>
+  ),
+)
 
 ProgressBar.displayName = 'ProgressBar'
