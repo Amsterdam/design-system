2247a4a2709928d1d1e01186d5c1f5eb96d5503c
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-09-17 15:39:46 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@f8acf33ed3010da5e4c60b5f6b9757914da71931 🚀

---
+++ demo-DES-2005-description-list-usage-guidelines/assets/Metadata.test.stories-Csf4SMzd.js
@@ -0,0 +1,6 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{n as t,t as n}from"./renderComponentVariants-CsaixqgW.js";import{n as r,t as i}from"./Metadata-Bsaezvzz.js";import{a,o}from"./Metadata.stories-BwYRAAU_.js";var s,c,l;function u(){return(u=e((()=>{r(),n(),a(),s={...o,title:`Components/Text/Metadata`},c={render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`,`!manifest`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Metadata, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs', '!manifest']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]})))()}u();export{c as Test,l as __namedExportsOrder,s as default};
---
+++ demo-DES-2005-description-list-usage-guidelines/assets/iframe-ChBZKe2r.js
@@ -0,0 +1,1 @@
+import{t as e}from"./iframe-By_Dpvfq.js";e();
