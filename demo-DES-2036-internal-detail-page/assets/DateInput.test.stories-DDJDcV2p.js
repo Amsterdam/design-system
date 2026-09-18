eaff836e6e56e62c1e258d84c6e539ebe8a99abd
dlnr <niels@dlnr.dev>
2026-09-18 12:12:00 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@1e0de1a7a31520198eafef7abbacca23cd1ce2eb 🚀

--- demo-DES-2036-internal-detail-page/assets/DateInput.test.stories-DDJDcV2p.js
+++
@@ -1,8 +0,0 @@
-import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{n as t,t as n}from"./renderComponentVariants-qFErR2JS.js";import{r,t as i}from"./DateInput-CX3VBD0T.js";import{n as a,t as o}from"./disablePageLevelChecks-57VwoK5W.js";import{c as s,s as c}from"./DateInput.stories-69XKmklo.js";var l,u,d;function f(){return(f=e((()=>{r(),a(),n(),c(),l={...s,title:`Components/Forms/Date Input`},u={parameters:o(`label`),render:(e,n)=>t(i,{args:e,variants:[`disabled`]},n),tags:[`!dev`,`!autodocs`,`!manifest`]},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
-  parameters: disablePageLevelChecks('label'),
-  render: (args, context) => renderComponentVariants(DateInput, {
-    args,
-    variants: ['disabled']
-  }, context),
-  tags: ['!dev', '!autodocs', '!manifest']
-}`,...u.parameters?.docs?.source}}},d=[`Test`]})))()}f();export{u as Test,d as __namedExportsOrder,l as default};
---
+++ demo-DES-2036-internal-detail-page/assets/iframe-DVZb2gVB.js
@@ -0,0 +1,1 @@
+import{t as e}from"./iframe-CvA_0Zdt.js";e();
