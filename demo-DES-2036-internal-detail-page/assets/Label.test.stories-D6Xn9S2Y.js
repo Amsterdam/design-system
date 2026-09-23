11b2d4875a2681a5fcf6c0cf561799904f06505d
dlnr <niels@dlnr.dev>
2026-09-23 07:32:55 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@38582fab386f32978ce78038376863607e788549 🚀

---
+++ demo-DES-2036-internal-detail-page/assets/Label.test.stories-D6Xn9S2Y.js
@@ -0,0 +1,6 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{n as t,t as n}from"./renderComponentVariants-BJhn4raV.js";import{n as r,t as i}from"./Label-wGHgZvtk.js";import{o as a,s as o}from"./Label.stories-CT_uNHrN.js";var s,c,l;function u(){return(u=e((()=>{r(),n(),a(),s={...o,title:`Components/Forms/Label`},c={render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`,`!manifest`]},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(Label, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs', '!manifest']
+}`,...c.parameters?.docs?.source}}},l=[`Test`]})))()}u();export{c as Test,l as __namedExportsOrder,s as default};
--- demo-DES-2036-internal-detail-page/assets/iframe-CAhnLpqF.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./iframe-K9OUUu9p.js";e();
