96e8e432be18ee3af2edf68f465f52f2dad1e0f3
RubenSibon <mail+github@rubensibon.nl>
2026-09-23 10:05:02 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@0452e65b74a54eec50a07a8e845c94af79cb7e09 🚀

---
+++ demo-develop/assets/IconButton.test.stories-BND2ai4T.js
@@ -0,0 +1,7 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{n as t,t as n}from"./renderComponentVariants-BuPjL1UG.js";import{n as r,t as i}from"./IconButton-C9gNOJuz.js";import{a,i as o}from"./IconButton.stories-BhPzqBm0.js";var s,c,l;function u(){return(u=e((()=>{r(),n(),o(),s={...a,title:`Components/Buttons/Icon Button`},c={render:(e,n)=>t(i,{args:e,variants:[`hovered`,`disabled`]},n),tags:[`!dev`,`!autodocs`,`!manifest`]},l=[`Test`],c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => renderComponentVariants(IconButton, {
+    args,
+    variants: ['hovered', 'disabled']
+  }, context),
+  tags: ['!dev', '!autodocs', '!manifest']
+}`,...c.parameters?.docs?.source}}}})))()}u();export{c as Test,l as __namedExportsOrder,s as default};
