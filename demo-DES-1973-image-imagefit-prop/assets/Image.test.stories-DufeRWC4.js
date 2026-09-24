0fc9e0bb25550e5edf76bf8ca76822d2e0afaa1b
RubenSibon <mail+github@rubensibon.nl>
2026-09-24 13:54:00 +0000

    Deploying to gh-pages from @ Amsterdam/design-system@1be25f9c54bb0f885174857590b34faeb7c9791c 🚀

---
+++ demo-DES-1973-image-imagefit-prop/assets/Image.test.stories-DufeRWC4.js
@@ -0,0 +1,10 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{n as t,t as n}from"./renderComponentVariants-sOCfKMHZ.js";import{n as r,t as i}from"./Image-D86S8u4i.js";import{a,i as o}from"./Image.stories-BvLgk2U6.js";var s,c,l;function u(){return(u=e((()=>{r(),n(),o(),s={...a,title:`Components/Media/Image`},c={args:{alt:``,src:`https://picsum.photos/id/122/800/1200`},render:(e,n)=>t(i,{args:e},n),tags:[`!dev`,`!autodocs`,`!manifest`]},l=[`Test`],c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    alt: '',
+    src: 'https://picsum.photos/id/122/800/1200'
+  },
+  render: (args, context) => renderComponentVariants(Image, {
+    args
+  }, context),
+  tags: ['!dev', '!autodocs', '!manifest']
+}`,...c.parameters?.docs?.source}}}})))()}u();export{c as Test,l as __namedExportsOrder,s as default};
